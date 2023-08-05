23e3365e51ff5fdafe8a6cd2346092103fe3e126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:30 +0000

    Add package dagster-graphql/0.11.0rc0/dagster-graphql-0.11.0rc0.tar.gz

---
+++ packages/dagster-graphql/dagster-graphql-0.11.0rc0.tar.gz/dagster-graphql-0.11.0rc0/dagster_graphql_tests/graphql/test_assets.py
@@ -0,0 +1,229 @@
+from dagster import AssetKey
+from dagster_graphql.client.query import LAUNCH_PIPELINE_EXECUTION_MUTATION
+from dagster_graphql.test.utils import execute_dagster_graphql, infer_pipeline_selector
+
+from .graphql_context_test_suite import GraphQLContextVariant, make_graphql_context_test_suite
+
+GET_ASSET_KEY_QUERY = """
+    query AssetKeyQuery {
+        assetsOrError {
+            __typename
+            ...on AssetConnection {
+                nodes {
+                    key {
+                        path
+                    }
+                }
+            }
+        }
+    }
+"""
+
+GET_ASSET_MATERIALIZATION = """
+    query AssetQuery($assetKey: AssetKeyInput!) {
+        assetOrError(assetKey: $assetKey) {
+            ... on Asset {
+                assetMaterializations(limit: 1) {
+                    materializationEvent {
+                        materialization {
+                            label
+                        }
+                        assetLineage {
+                            assetKey {
+                                path
+                            }
+                            partitions
+                        }
+                    }
+                }
+                tags {
+                    key
+                    value
+                }
+            }
+            ... on AssetNotFoundError {
+                __typename
+            }
+        }
+    }
+"""
+
+GET_ASSET_MATERIALIZATION_WITH_PARTITION = """
+    query AssetQuery($assetKey: AssetKeyInput!) {
+        assetOrError(assetKey: $assetKey) {
+            ... on Asset {
+                assetMaterializations(limit: 1) {
+                    partition
+                    materializationEvent {
+                        materialization {
+                            label
+                        }
+                    }
+                }
+            }
+        }
+    }
+"""
+
+GET_ASSET_RUNS = """
+    query AssetRunsQuery($assetKey: AssetKeyInput!) {
+        assetOrError(assetKey: $assetKey) {
+            ... on Asset {
+                runs {
+                    runId
+                }
+            }
+        }
+    }
+"""
+
+
+WIPE_ASSET = """
+    mutation AssetKeyWipe($assetKey: AssetKeyInput!) {
+        wipeAsset(assetKey: $assetKey) {
+            __typename
+        }
+    }
+"""
+
+
+def _create_run(graphql_context, pipeline_name, mode="default"):
+    selector = infer_pipeline_selector(graphql_context, pipeline_name)
+    result = execute_dagster_graphql(
+        graphql_context,
+        LAUNCH_PIPELINE_EXECUTION_MUTATION,
+        variables={"executionParams": {"selector": selector, "mode": mode}},
+    )
+    assert result.data["launchPipelineExecution"]["__typename"] == "LaunchPipelineRunSuccess"
+    graphql_context.instance.run_launcher.join()
+    return result.data["launchPipelineExecution"]["run"]["runId"]
+
+
+class TestAssetAwareEventLog(
+    make_graphql_context_test_suite(
+        context_variants=[
+            GraphQLContextVariant.consolidated_sqlite_instance_managed_grpc_env(),
+            GraphQLContextVariant.sqlite_with_default_run_launcher_managed_grpc_env(),
+            GraphQLContextVariant.postgres_with_default_run_launcher_managed_grpc_env(),
+        ]
+    )
+):
+    def test_all_asset_keys(self, graphql_context, snapshot):
+        _create_run(graphql_context, "multi_asset_pipeline")
+        result = execute_dagster_graphql(graphql_context, GET_ASSET_KEY_QUERY)
+        assert result.data
+        assert result.data["assetsOrError"]
+        assert result.data["assetsOrError"]["nodes"]
+
+        # sort by materialization asset key to keep list order is consistent for snapshot
+        result.data["assetsOrError"]["nodes"].sort(key=lambda e: e["key"]["path"][0])
+
+        snapshot.assert_match(result.data)
+
+    def test_get_asset_key_materialization(self, graphql_context, snapshot):
+        _create_run(graphql_context, "single_asset_pipeline")
+        result = execute_dagster_graphql(
+            graphql_context, GET_ASSET_MATERIALIZATION, variables={"assetKey": {"path": ["a"]}}
+        )
+        assert result.data
+        snapshot.assert_match(result.data)
+
+    def test_get_asset_key_not_found(self, graphql_context, snapshot):
+        _create_run(graphql_context, "single_asset_pipeline")
+
+        result = execute_dagster_graphql(
+            graphql_context,
+            GET_ASSET_MATERIALIZATION,
+            variables={"assetKey": {"path": ["bogus", "asset"]}},
+        )
+        assert result.data
+        snapshot.assert_match(result.data)
+
+    def test_get_partitioned_asset_key_materialization(self, graphql_context, snapshot):
+        _create_run(graphql_context, "partitioned_asset_pipeline")
+
+        result = execute_dagster_graphql(
+            graphql_context,
+            GET_ASSET_MATERIALIZATION_WITH_PARTITION,
+            variables={"assetKey": {"path": ["a"]}},
+        )
+        assert result.data
+        snapshot.assert_match(result.data)
+
+    def test_get_asset_key_lineage(self, graphql_context, snapshot):
+        selector = infer_pipeline_selector(graphql_context, "asset_lineage_pipeline")
+        result = execute_dagster_graphql(
+            graphql_context,
+            LAUNCH_PIPELINE_EXECUTION_MUTATION,
+            variables={"executionParams": {"selector": selector, "mode": "default"}},
+        )
+        assert result.data["launchPipelineExecution"]["__typename"] == "LaunchPipelineRunSuccess"
+
+        graphql_context.instance.run_launcher.join()
+
+        result = execute_dagster_graphql(
+            graphql_context,
+            GET_ASSET_MATERIALIZATION,
+            variables={"assetKey": {"path": ["b"]}},
+        )
+        assert result.data
+        snapshot.assert_match(result.data)
+
+    def test_get_partitioned_asset_key_lineage(self, graphql_context, snapshot):
+        selector = infer_pipeline_selector(graphql_context, "partitioned_asset_lineage_pipeline")
+        result = execute_dagster_graphql(
+            graphql_context,
+            LAUNCH_PIPELINE_EXECUTION_MUTATION,
+            variables={"executionParams": {"selector": selector, "mode": "default"}},
+        )
+        assert result.data["launchPipelineExecution"]["__typename"] == "LaunchPipelineRunSuccess"
+
+        graphql_context.instance.run_launcher.join()
+
+        result = execute_dagster_graphql(
+            graphql_context,
+            GET_ASSET_MATERIALIZATION,
+            variables={"assetKey": {"path": ["b"]}},
+        )
+        assert result.data
+        snapshot.assert_match(result.data)
+
+    def test_get_asset_runs(self, graphql_context):
+        single_run_id = _create_run(graphql_context, "single_asset_pipeline")
+        multi_run_id = _create_run(graphql_context, "multi_asset_pipeline")
+        result = execute_dagster_graphql(
+            graphql_context, GET_ASSET_RUNS, variables={"assetKey": {"path": ["a"]}}
+        )
+        assert result.data
+        fetched_runs = [run["runId"] for run in result.data["assetOrError"]["runs"]]
+        assert len(fetched_runs) == 2
+        assert multi_run_id in fetched_runs
+        assert single_run_id in fetched_runs
+
+    def test_asset_wipe(self, graphql_context):
+        _create_run(graphql_context, "single_asset_pipeline")
+        _create_run(graphql_context, "multi_asset_pipeline")
+
+        asset_keys = graphql_context.instance.all_asset_keys()
+        assert AssetKey("a") in asset_keys
+
+        result = execute_dagster_graphql(
+            graphql_context, WIPE_ASSET, variables={"assetKey": {"path": ["a"]}}
+        )
+
+        assert result.data
+        assert result.data["wipeAsset"]
+        assert result.data["wipeAsset"]["__typename"] == "AssetWipeSuccess"
+
+        asset_keys = graphql_context.instance.all_asset_keys()
+        assert AssetKey("a") not in asset_keys
+
+    def test_asset_tags(self, graphql_context, snapshot):
+        _create_run(graphql_context, "asset_tag_pipeline")
+        result = execute_dagster_graphql(
+            graphql_context, GET_ASSET_MATERIALIZATION, variables={"assetKey": {"path": ["a"]}}
+        )
+        assert result.data
+        assert result.data["assetOrError"]
+        assert result.data["assetOrError"]["tags"]
+        snapshot.assert_match(result.data)
