68966579dd495eaedf539bbd65b06b7b49c842a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package numerai-cli/0.3.0.dev23/numerai_cli-0.3.0.dev23-py3-none-any.whl

---
+++ packages/numerai-cli/numerai_cli-0.3.0.dev23-py3-none-any.whl/numerai/cli/__init__.py
@@ -0,0 +1,37 @@
+import click
+from colorama import init
+
+from numerai.cli import \
+    util, \
+    constants, \
+    doctor, \
+    node, \
+    setup, \
+    uninstall, \
+    upgrade, \
+    misc
+from numerai.cli.util import \
+    docker, \
+    files, \
+    keys
+
+
+@click.group()
+def numerai():
+    """
+    This tool helps you setup Numer.ai Prediction Nodes and deploy your models to them.
+    """
+    pass
+
+
+def main():
+    init(autoreset=True)
+
+    numerai.add_command(setup.setup)
+    numerai.add_command(node.node)
+    numerai.add_command(doctor.doctor)
+    numerai.add_command(uninstall.uninstall)
+    numerai.add_command(upgrade.upgrade)
+    numerai.add_command(misc.copy_example)
+    numerai.add_command(constants.size_presets)
+    numerai()
