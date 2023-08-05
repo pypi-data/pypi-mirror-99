788fd40cec3846dec037658eaf8847491ab1a235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package mypo/0.0.48/mypo-0.0.48-py3-none-any.whl

---
+++ packages/mypo/mypo-0.0.48-py3-none-any.whl/mypo/loader.py
@@ -0,0 +1,59 @@
+"""
+Loader class for downloading stock data.
+
+Download stock data from yahoo finance.
+
+"""
+
+
+from collections import OrderedDict
+from typing import Dict
+
+import pandas as pd
+import yfinance as yf
+
+from mypo.market import Market
+
+
+class Loader(object):
+    """Loader class for downloading stock."""
+
+    _tickers: Dict[str, pd.DataFrame]
+    _expense_ratio: Dict[str, float]
+
+    def __init__(self) -> None:
+        self._tickers = OrderedDict()
+        self._expense_ratio = OrderedDict()
+
+    def get(self, ticker: str, expense_ratio: float = 0.0) -> None:
+        """
+        Get stock data of specified ticker.
+
+        Parameters
+        ----------
+        ticker
+            Ticker that you want to download stock data.
+
+        expense_ratio
+            Expense ratio of ticker. The default value is 0.0.
+
+        Returns
+        -------
+        Nothing
+
+        """
+        ticker = ticker.upper()
+        df = yf.Ticker(ticker).history(period="max")
+        df.index = pd.to_datetime(df.index)
+        self._tickers[ticker] = df
+        self._expense_ratio[ticker] = expense_ratio
+
+    def get_market(self) -> Market:
+        """
+        Get Market data.
+
+        Returns
+        -------
+        Market data
+        """
+        return Market(self._tickers, self._expense_ratio)
