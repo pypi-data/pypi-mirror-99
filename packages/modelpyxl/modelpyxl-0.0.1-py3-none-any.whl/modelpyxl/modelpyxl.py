00c048ead3c53c35fc735a1ea122b376abdca5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package modelpyxl/0.0.1/modelpyxl-0.0.1-py3-none-any.whl

---
+++ packages/modelpyxl/modelpyxl-0.0.1-py3-none-any.whl/modelpyxl/modelpyxl.py
@@ -0,0 +1,11 @@
+import openpyxl
+def test(my_test):
+    """
+    Print a line about converting a notebook.
+    Args:
+        my_test (str): any string
+    Returns:
+        None
+    """
+
+    print(f"This function print variable's value = {my_test}.")
