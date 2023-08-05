a2b7e80b23cb3c6d0e204d8c3e2a2dc7b570af7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:19 +0000

    Add package neoload/1.3.0/neoload-1.3.0-py3-none-any.whl

---
+++ packages/neoload/neoload-1.3.0-py3-none-any.whl/neoload/neoload_cli_lib/cli_exception.py
@@ -0,0 +1,26 @@
+import traceback
+
+import click
+
+_CliException__debug = False
+
+
+class CliException(click.ClickException):
+    __debug = False
+
+    @staticmethod
+    def set_debug(boolean: bool):
+        CliException.__debug = boolean
+
+    def __init__(self, message):
+        super().__init__(message)
+
+    def format_message(self):
+        __message = super().format_message()
+        if CliException.__debug:
+            __message = traceback.format_exc() + "\n\n" + __message
+        return __message
+
+    @staticmethod
+    def is_debug():
+        return CliException.__debug
