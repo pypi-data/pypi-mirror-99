eb80d19353661250919cfbabb40848691271c8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:23 +0000

    Add package swz-test-package/1.0.22/swz-test-package-1.0.22.tar.gz

---
+++ packages/swz-test-package/swz-test-package-1.0.22.tar.gz/swz-test-package-1.0.22/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = __import__('test_package').__version__
+
+setup(
+    name='swz-test-package',
+    version=VERSION,
+    author='swz',
+    license='Apache LICENSE 2.0',
+    # scripts=['test_package/bin/cmdt.py'],
+    platforms='any',
+    packages=find_packages(exclude=['tests*']),
+    package_data={
+        '': ['*']
+    },
+    install_requires='cryptography<=2.8',
+    entry_points="""
+    [console_scripts]
+    cmdt=test_package.bin.cmdt:run
+    """,
+    python_requires=">=3.8"
+    # entry_points={
+    #     'console_scripts': [
+    #         'cmdt=est_package.bin.cmdt:run',
+    #     ],
+    # }
+)
---
+++ packages/swz-test-package/swz-test-package-1.0.22.tar.gz/swz-test-package-1.0.22/test.py
@@ -0,0 +1,3 @@
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+# pip install swz-test-package==1.0.19
