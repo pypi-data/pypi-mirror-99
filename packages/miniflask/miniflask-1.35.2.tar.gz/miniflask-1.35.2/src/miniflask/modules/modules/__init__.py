42a8e93daab6ca0d3af3f7080d427530cd7c7375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package miniflask/1.35.2/miniflask-1.35.2.tar.gz

---
+++ packages/miniflask/miniflask-1.35.2.tar.gz/miniflask-1.35.2/src/miniflask/modules/modules/__init__.py
@@ -0,0 +1,10 @@
+def showModules(state, event):
+    event._mf.showModules(with_event=state.all["events"])
+    print()
+
+
+def register(mf):
+    mf.register_globals({
+        "events": False
+    })
+    mf.register_event('init', showModules)
