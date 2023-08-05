2268e605c654980e6a0ab9b0370eaedc83053baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:39 +0000

    Add package cdk-soca/0.2.17/cdk_soca-0.2.17-py3-none-any.whl

---
+++ packages/cdk-soca/cdk_soca-0.2.17-py3-none-any.whl/cdk_soca/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_efs._jsii
+import aws_cdk.aws_elasticsearch._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-soca", "0.2.17", __name__[0:-6], "cdk-soca@0.2.17.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
