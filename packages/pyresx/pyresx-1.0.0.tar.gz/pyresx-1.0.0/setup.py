b623e5b5340a4ee316f7c33e16a711a589dc61b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:01 +0000

    Add package pyresx/1.0.0/pyresx-1.0.0.tar.gz

---
+++ packages/pyresx/pyresx-1.0.0.tar.gz/pyresx-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyresx",
+    version="1.0.0",
+    author="cola314",
+    author_email='ubj020314@gmail.com',
+    description='.resx file writer for python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/cola314/pyresx',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
