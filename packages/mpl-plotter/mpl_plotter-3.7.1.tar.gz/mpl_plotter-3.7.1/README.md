93ceedc203f8384dd2a9714a2a4d77cedf11f377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:34 +0000

    Add package mpl-plotter/3.7.1/mpl_plotter-3.7.1.tar.gz

---
+++ packages/mpl-plotter/mpl_plotter-3.7.1.tar.gz/mpl_plotter-3.7.1/README.md
@@ -0,0 +1,10 @@
+
+## `mpl_plotter`
+
+# `matplotlib` based Python plotting library
+
+`pip install mpl_plotter`
+
+![alt text](_demo/subplot2grid_demo.png "Demo plot")
+
+### Antonio Lopez Rivera, 2020
---
+++ packages/mpl-plotter/mpl_plotter-3.7.1.tar.gz/mpl_plotter-3.7.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mpl_plotter",
+    version="3.7.1",
+    author="Antonio Lopez Rivera",
+    author_email="antonlopezr99@gmail.com",
+    description="Matplotlib-based plotting library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/antonlopezr/mpl_plotter",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scikit-image",
+        "termcolor",
+        "pandas",
+        "PyQt5"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
