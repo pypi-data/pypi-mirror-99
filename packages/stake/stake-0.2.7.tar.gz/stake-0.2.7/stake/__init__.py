07b37323f6dd8d680ecaee9bb9e45f72e0cacea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package stake/0.2.7/stake-0.2.7.tar.gz

---
+++ packages/stake/stake-0.2.7.tar.gz/stake-0.2.7/stake/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+from pathlib import Path
+
+from single_version import get_version
+
+from .client import *
+from .common import *
+from .funding import *
+from .fx import *
+from .market import *
+from .order import *
+from .product import *
+from .trade import *
+from .transaction import *
+from .watchlist import *
+
+__version__ = get_version("stake", Path(__file__).parent.parent)
