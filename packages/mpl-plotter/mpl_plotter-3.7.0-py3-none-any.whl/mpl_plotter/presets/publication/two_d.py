abf107f692ac81a32a145d007773ddb0327a7c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package mpl-plotter/3.7.0/mpl_plotter-3.7.0-py3-none-any.whl

---
+++ packages/mpl-plotter/mpl_plotter-3.7.0-py3-none-any.whl/mpl_plotter/presets/publication/two_d.py
@@ -0,0 +1,19 @@
+from mpl_plotter.two_d import line as mpl_line, scatter as mpl_scatter
+from Alexandria.file.parsers import yaml_parser
+
+
+class line(mpl_line):
+
+    def __init__(self, x, y, **kwargs):
+
+        input = {**yaml_parser("mpl_plotter/presets/publication/publication.yaml"), **kwargs}
+
+        super().__init__(x=x, y=y, **input)
+
+
+class scatter(mpl_scatter):
+
+    def __init__(self, x, y, **kwargs):
+        input = {**yaml_parser("mpl_plotter/presets/publication/publication.yaml"), **kwargs}
+
+        super().__init__(x=x, y=y, **input)
