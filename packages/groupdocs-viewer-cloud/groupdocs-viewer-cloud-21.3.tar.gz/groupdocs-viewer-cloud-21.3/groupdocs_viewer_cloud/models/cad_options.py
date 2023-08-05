7cfd79c4ef84347c07cfa946544ed402dd32aa63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package groupdocs-viewer-cloud/21.3/groupdocs-viewer-cloud-21.3.tar.gz

---
+++ packages/groupdocs-viewer-cloud/groupdocs-viewer-cloud-21.3.tar.gz/groupdocs-viewer-cloud-21.3/groupdocs_viewer_cloud/models/cad_options.py
@@ -0,0 +1,308 @@
+# coding: utf-8
+
+# -----------------------------------------------------------------------------------
+# <copyright company="Aspose Pty Ltd" file="CadOptions.py">
+#   Copyright (c) 2003-2021 Aspose Pty Ltd
+# </copyright>
+# <summary>
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+# </summary>
+# -----------------------------------------------------------------------------------
+
+import pprint
+import re  # noqa: F401
+
+import six
+
+class CadOptions(object):
+    """
+    Rendering options for CAD file formats. CAD file formats include files with extensions: .dwg, .dxf, .dgn, .ifc, .stl
+    """
+
+    """
+    Attributes:
+      swagger_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    swagger_types = {
+        'scale_factor': 'float',
+        'width': 'int',
+        'height': 'int',
+        'tiles': 'list[Tile]',
+        'render_layouts': 'bool',
+        'layout_name': 'str',
+        'layers': 'list[str]'
+    }
+
+    attribute_map = {
+        'scale_factor': 'ScaleFactor',
+        'width': 'Width',
+        'height': 'Height',
+        'tiles': 'Tiles',
+        'render_layouts': 'RenderLayouts',
+        'layout_name': 'LayoutName',
+        'layers': 'Layers'
+    }
+
+    def __init__(self, scale_factor=None, width=None, height=None, tiles=None, render_layouts=None, layout_name=None, layers=None, **kwargs):  # noqa: E501
+        """Initializes new instance of CadOptions"""  # noqa: E501
+
+        self._scale_factor = None
+        self._width = None
+        self._height = None
+        self._tiles = None
+        self._render_layouts = None
+        self._layout_name = None
+        self._layers = None
+
+        if scale_factor is not None:
+            self.scale_factor = scale_factor
+        if width is not None:
+            self.width = width
+        if height is not None:
+            self.height = height
+        if tiles is not None:
+            self.tiles = tiles
+        if render_layouts is not None:
+            self.render_layouts = render_layouts
+        if layout_name is not None:
+            self.layout_name = layout_name
+        if layers is not None:
+            self.layers = layers
+    
+    @property
+    def scale_factor(self):
+        """
+        Gets the scale_factor.  # noqa: E501
+
+        Scale factor allows to change the size of the output document. Values higher than 1 will enlarge output result and values between 0 and 1 will make output result smaller. This option is ignored when either Height or Width options are set.   # noqa: E501
+
+        :return: The scale_factor.  # noqa: E501
+        :rtype: float
+        """
+        return self._scale_factor
+
+    @scale_factor.setter
+    def scale_factor(self, scale_factor):
+        """
+        Sets the scale_factor.
+
+        Scale factor allows to change the size of the output document. Values higher than 1 will enlarge output result and values between 0 and 1 will make output result smaller. This option is ignored when either Height or Width options are set.   # noqa: E501
+
+        :param scale_factor: The scale_factor.  # noqa: E501
+        :type: float
+        """
+        if scale_factor is None:
+            raise ValueError("Invalid value for `scale_factor`, must not be `None`")  # noqa: E501
+        self._scale_factor = scale_factor
+    
+    @property
+    def width(self):
+        """
+        Gets the width.  # noqa: E501
+
+        Width of the output result in pixels          # noqa: E501
+
+        :return: The width.  # noqa: E501
+        :rtype: int
+        """
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        """
+        Sets the width.
+
+        Width of the output result in pixels          # noqa: E501
+
+        :param width: The width.  # noqa: E501
+        :type: int
+        """
+        if width is None:
+            raise ValueError("Invalid value for `width`, must not be `None`")  # noqa: E501
+        self._width = width
+    
+    @property
+    def height(self):
+        """
+        Gets the height.  # noqa: E501
+
+        Height of the output result in pixels       # noqa: E501
+
+        :return: The height.  # noqa: E501
+        :rtype: int
+        """
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        """
+        Sets the height.
+
+        Height of the output result in pixels       # noqa: E501
+
+        :param height: The height.  # noqa: E501
+        :type: int
+        """
+        if height is None:
+            raise ValueError("Invalid value for `height`, must not be `None`")  # noqa: E501
+        self._height = height
+    
+    @property
+    def tiles(self):
+        """
+        Gets the tiles.  # noqa: E501
+
+        The drawing regions to render This option supported only for DWG and DWT file types The RenderLayouts and LayoutName options are ignored when rendering by tiles  # noqa: E501
+
+        :return: The tiles.  # noqa: E501
+        :rtype: list[Tile]
+        """
+        return self._tiles
+
+    @tiles.setter
+    def tiles(self, tiles):
+        """
+        Sets the tiles.
+
+        The drawing regions to render This option supported only for DWG and DWT file types The RenderLayouts and LayoutName options are ignored when rendering by tiles  # noqa: E501
+
+        :param tiles: The tiles.  # noqa: E501
+        :type: list[Tile]
+        """
+        self._tiles = tiles
+    
+    @property
+    def render_layouts(self):
+        """
+        Gets the render_layouts.  # noqa: E501
+
+        Indicates whether layouts from CAD document should be rendered  # noqa: E501
+
+        :return: The render_layouts.  # noqa: E501
+        :rtype: bool
+        """
+        return self._render_layouts
+
+    @render_layouts.setter
+    def render_layouts(self, render_layouts):
+        """
+        Sets the render_layouts.
+
+        Indicates whether layouts from CAD document should be rendered  # noqa: E501
+
+        :param render_layouts: The render_layouts.  # noqa: E501
+        :type: bool
+        """
+        if render_layouts is None:
+            raise ValueError("Invalid value for `render_layouts`, must not be `None`")  # noqa: E501
+        self._render_layouts = render_layouts
+    
+    @property
+    def layout_name(self):
+        """
+        Gets the layout_name.  # noqa: E501
+
+        The name of the specific layout to render. Layout name is case-sensitive  # noqa: E501
+
+        :return: The layout_name.  # noqa: E501
+        :rtype: str
+        """
+        return self._layout_name
+
+    @layout_name.setter
+    def layout_name(self, layout_name):
+        """
+        Sets the layout_name.
+
+        The name of the specific layout to render. Layout name is case-sensitive  # noqa: E501
+
+        :param layout_name: The layout_name.  # noqa: E501
+        :type: str
+        """
+        self._layout_name = layout_name
+    
+    @property
+    def layers(self):
+        """
+        Gets the layers.  # noqa: E501
+
+        The CAD drawing layers to render By default all layers are rendered; Layer names are case-sensitive  # noqa: E501
+
+        :return: The layers.  # noqa: E501
+        :rtype: list[str]
+        """
+        return self._layers
+
+    @layers.setter
+    def layers(self, layers):
+        """
+        Sets the layers.
+
+        The CAD drawing layers to render By default all layers are rendered; Layer names are case-sensitive  # noqa: E501
+
+        :param layers: The layers.  # noqa: E501
+        :type: list[str]
+        """
+        self._layers = layers
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        for attr, _ in six.iteritems(self.swagger_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        return pprint.pformat(self.to_dict())
+
+    def __repr__(self):
+        """For `print` and `pprint`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, CadOptions):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
---
+++ packages/groupdocs-viewer-cloud/groupdocs-viewer-cloud-21.3.tar.gz/groupdocs-viewer-cloud-21.3/groupdocs_viewer_cloud/models/project_management_options.py
@@ -0,0 +1,237 @@
+# coding: utf-8
+
+# -----------------------------------------------------------------------------------
+# <copyright company="Aspose Pty Ltd" file="ProjectManagementOptions.py">
+#   Copyright (c) 2003-2021 Aspose Pty Ltd
+# </copyright>
+# <summary>
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+# </summary>
+# -----------------------------------------------------------------------------------
+
+import pprint
+import re  # noqa: F401
+
+import six
+
+class ProjectManagementOptions(object):
+    """
+    Rendering options for Project file formats. Project file formats include files with extensions: .mpt, .mpp
+    """
+
+    """
+    Attributes:
+      swagger_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    swagger_types = {
+        'page_size': 'str',
+        'time_unit': 'str',
+        'start_date': 'datetime',
+        'end_date': 'datetime'
+    }
+
+    attribute_map = {
+        'page_size': 'PageSize',
+        'time_unit': 'TimeUnit',
+        'start_date': 'StartDate',
+        'end_date': 'EndDate'
+    }
+
+    def __init__(self, page_size=None, time_unit=None, start_date=None, end_date=None, **kwargs):  # noqa: E501
+        """Initializes new instance of ProjectManagementOptions"""  # noqa: E501
+
+        self._page_size = None
+        self._time_unit = None
+        self._start_date = None
+        self._end_date = None
+
+        if page_size is not None:
+            self.page_size = page_size
+        if time_unit is not None:
+            self.time_unit = time_unit
+        if start_date is not None:
+            self.start_date = start_date
+        if end_date is not None:
+            self.end_date = end_date
+    
+    @property
+    def page_size(self):
+        """
+        Gets the page_size.  # noqa: E501
+
+        The size of the page.  # noqa: E501
+
+        :return: The page_size.  # noqa: E501
+        :rtype: str
+        """
+        return self._page_size
+
+    @page_size.setter
+    def page_size(self, page_size):
+        """
+        Sets the page_size.
+
+        The size of the page.  # noqa: E501
+
+        :param page_size: The page_size.  # noqa: E501
+        :type: str
+        """
+        if page_size is None:
+            raise ValueError("Invalid value for `page_size`, must not be `None`")  # noqa: E501
+        allowed_values = ["Unspecified", "Letter", "Ledger", "A0", "A1", "A2", "A3", "A4"]  # noqa: E501
+        if not page_size.isdigit():	
+            if page_size not in allowed_values:
+                raise ValueError(
+                    "Invalid value for `page_size` ({0}), must be one of {1}"  # noqa: E501
+                    .format(page_size, allowed_values))
+            self._page_size = page_size
+        else:
+            self._page_size = allowed_values[int(page_size) if six.PY3 else long(page_size)]
+    
+    @property
+    def time_unit(self):
+        """
+        Gets the time_unit.  # noqa: E501
+
+        The time unit to use as minimal point.  # noqa: E501
+
+        :return: The time_unit.  # noqa: E501
+        :rtype: str
+        """
+        return self._time_unit
+
+    @time_unit.setter
+    def time_unit(self, time_unit):
+        """
+        Sets the time_unit.
+
+        The time unit to use as minimal point.  # noqa: E501
+
+        :param time_unit: The time_unit.  # noqa: E501
+        :type: str
+        """
+        if time_unit is None:
+            raise ValueError("Invalid value for `time_unit`, must not be `None`")  # noqa: E501
+        allowed_values = ["Unspecified", "Days", "ThirdsOfMonths", "Months"]  # noqa: E501
+        if not time_unit.isdigit():	
+            if time_unit not in allowed_values:
+                raise ValueError(
+                    "Invalid value for `time_unit` ({0}), must be one of {1}"  # noqa: E501
+                    .format(time_unit, allowed_values))
+            self._time_unit = time_unit
+        else:
+            self._time_unit = allowed_values[int(time_unit) if six.PY3 else long(time_unit)]
+    
+    @property
+    def start_date(self):
+        """
+        Gets the start_date.  # noqa: E501
+
+        The start date of a Gantt Chart View to render.          # noqa: E501
+
+        :return: The start_date.  # noqa: E501
+        :rtype: datetime
+        """
+        return self._start_date
+
+    @start_date.setter
+    def start_date(self, start_date):
+        """
+        Sets the start_date.
+
+        The start date of a Gantt Chart View to render.          # noqa: E501
+
+        :param start_date: The start_date.  # noqa: E501
+        :type: datetime
+        """
+        if start_date is None:
+            raise ValueError("Invalid value for `start_date`, must not be `None`")  # noqa: E501
+        self._start_date = start_date
+    
+    @property
+    def end_date(self):
+        """
+        Gets the end_date.  # noqa: E501
+
+        The end date of a Gantt Chart View to render.  # noqa: E501
+
+        :return: The end_date.  # noqa: E501
+        :rtype: datetime
+        """
+        return self._end_date
+
+    @end_date.setter
+    def end_date(self, end_date):
+        """
+        Sets the end_date.
+
+        The end date of a Gantt Chart View to render.  # noqa: E501
+
+        :param end_date: The end_date.  # noqa: E501
+        :type: datetime
+        """
+        if end_date is None:
+            raise ValueError("Invalid value for `end_date`, must not be `None`")  # noqa: E501
+        self._end_date = end_date
+
+    def to_dict(self):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        for attr, _ in six.iteritems(self.swagger_types):
+            value = getattr(self, attr)
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: x.to_dict() if hasattr(x, "to_dict") else x,
+                    value
+                ))
+            elif hasattr(value, "to_dict"):
+                result[attr] = value.to_dict()
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], item[1].to_dict())
+                    if hasattr(item[1], "to_dict") else item,
+                    value.items()
+                ))
+            else:
+                result[attr] = value
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        return pprint.pformat(self.to_dict())
+
+    def __repr__(self):
+        """For `print` and `pprint`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, ProjectManagementOptions):
+            return False
+
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        return not self == other
