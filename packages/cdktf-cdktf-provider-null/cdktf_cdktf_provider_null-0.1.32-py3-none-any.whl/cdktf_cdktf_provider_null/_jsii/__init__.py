0d8d6e4c29b78daf0bf411b561a81a29c3d085e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:27 +0000

    Add package cdktf-cdktf-provider-null/0.1.32/cdktf_cdktf_provider_null-0.1.32-py3-none-any.whl

---
+++ packages/cdktf-cdktf-provider-null/cdktf_cdktf_provider_null-0.1.32-py3-none-any.whl/cdktf_cdktf_provider_null/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdktf._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdktf/provider-null", "0.1.32", __name__[0:-6], "provider-null@0.1.32.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
