6fe637a0b4b542484644b90e8d8dfdb7bc4b5af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:45 +0000

    Add package aflr/0.2.0/aflr-0.2.0-py3-none-any.whl

---
+++ packages/aflr/aflr-0.2.0-py3-none-any.whl/aflr/api_resources/mastering.py
@@ -0,0 +1,28 @@
+import aflr
+from aflr.api_request import APIRequest
+
+
+class Mastering(APIRequest):
+    OBJECT_NAME = "mastering"
+
+    def __init__(self):
+        super().__init__()
+        self.url = self.api_base + "/mastering"
+        self.file_url = self.api_base + "/file/mastering"
+
+    def config_test(self):
+        return f"Configured to transact {self.OBJECT_NAME} objects to {self.url} with api_key = {self.api_key}"
+
+    def create(self, **params):
+        return self._post_request(url=self.url, json=params)
+
+    # get mastering file
+    def retrieve(self, scriptId, parameters={}):
+        parameters.update({"scriptId": scriptId})
+        return self._get_request(url=self.file_url, request_params=parameters)
+
+    # download mastering file
+    def download(self, scriptId, parameters={}, destination="."):
+        url = self.retrieve(scriptId=scriptId, parameters=parameters).get("url")
+        local_filename = self._download_request(url=url, destination=destination)
+        return local_filename
