bbf8d2a2a507eeae631752dfa58b6ff56287628f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:10 +0000

    Add package bluesky-adaptive/0.1.0/bluesky_adaptive-0.1.0-py3-none-any.whl

---
+++ packages/bluesky-adaptive/bluesky_adaptive-0.1.0-py3-none-any.whl/bluesky_adaptive/tests/test_per_start.py
@@ -0,0 +1,37 @@
+from queue import Empty
+
+import pytest
+
+from bluesky.tests.utils import DocCollector
+
+from bluesky_adaptive.per_start import (
+    recommender_factory,
+    adaptive_plan,
+)
+from bluesky_adaptive.recommendations import SequenceRecommender
+
+
+def test_seq_recommender(RE, hw):
+
+    recommender = SequenceRecommender([[1,], [2,], [3,]])  # noqa
+
+    cb, queue = recommender_factory(recommender, ["motor"], ["det"])
+    dc = DocCollector()
+
+    # pre-poison the queue to simulate a messy reccomender
+    queue.put(None)
+    queue.put({})
+
+    RE(
+        adaptive_plan([hw.det], {hw.motor: 0}, to_recommender=cb, from_recommender=queue),
+        dc.insert,
+    )
+
+    assert len(dc.start) == 4
+    assert len(dc.event) == 4
+    for ev in dc.event.values():
+        assert len(ev) == 1
+
+    # check that our reccomender does not leave anything behind
+    with pytest.raises(Empty):
+        queue.get(block=False)
