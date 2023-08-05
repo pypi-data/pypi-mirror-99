64dffc0c421887bcf45e105495ad60c546ab2b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:59 +0000

    Add package django-toolshed/0.0.1/django-toolshed-0.0.1.tar.gz

---
+++ packages/django-toolshed/django-toolshed-0.0.1.tar.gz/django-toolshed-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_toolshed']
+
+package_data = \
+{'': ['*'],
+ 'django_toolshed': ['static/django_toolshed/css/*',
+                     'static/django_toolshed/images/*',
+                     'static/django_toolshed/js/*',
+                     'templates/django_toolshed/*']}
+
+install_requires = \
+['Django>=3.1,<4.0', 'ipython_genutils>=0.2.0,<0.3.0']
+
+setup_kwargs = {
+    'name': 'django-toolshed',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Dani Hodovic',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
