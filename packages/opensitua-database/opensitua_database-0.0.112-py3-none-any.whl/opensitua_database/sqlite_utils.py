4f3173bdbf56578225489c602b59a999e90b5d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:49 +0000

    Add package opensitua-database/0.0.112/opensitua_database-0.0.112-py3-none-any.whl

---
+++ packages/opensitua-database/opensitua_database-0.0.112-py3-none-any.whl/opensitua_database/sqlite_utils.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2019 Valerio for Gecosistema S.r.l.
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Name:        opensitua_database
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:    11/09/2019
+#-------------------------------------------------------------------------------
+import hashlib
+
+def sqlite_md5(text):
+    """
+    md5 - Returns the md5 of the text
+    """
+    if (text!=None):
+        hash = hashlib.md5()
+        hash.update(text.encode("utf-8"))
+        return hash.hexdigest()
+    return None
