5ef339ad9eef075aa5c75bb46a424d789b7e6192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:20 +0000

    Add package desed/1.3.5rc0/desed-1.3.5rc0.tar.gz

---
+++ packages/desed/desed-1.3.5rc0.tar.gz/desed-1.3.5rc0/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+def package_file(fname):
+    return os.path.relpath(os.path.join(os.path.dirname(__file__), fname))
+
+
+with open("README.md", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="desed",
+    version="1.3.5rc0",
+    description="DESED dataset utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nicolas Turpault",
+    author_email="turpaultn@gmail.com",
+    url="https://github.com/turpaultn/DESED",
+    license="MIT",
+    package_dir={"": package_file("desed")},
+    packages=find_packages(package_file("desed")),
+    install_requires=[
+        "scaper >= 1.6.5rc0",
+        "numpy >= 1.15.4",
+        "pandas >= 0.24.0",
+        "dcase-util >= 0.2.11",
+        "youtube-dl >= 2021.2.22",  # Better with conda install and conda-forge channel
+        "soundfile >= 0.10.1",
+        "jams >= 0.3.4",
+        "tqdm >= 4.29.1",
+        "requests >= 2.21.0",
+    ],
+)
