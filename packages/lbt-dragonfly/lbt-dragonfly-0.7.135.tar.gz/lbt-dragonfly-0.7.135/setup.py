d3bc2ba867e5551b989887afe6fe105ff10b9529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package lbt-dragonfly/0.7.135/lbt-dragonfly-0.7.135.tar.gz

---
+++ packages/lbt-dragonfly/lbt-dragonfly-0.7.135.tar.gz/lbt-dragonfly-0.7.135/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="lbt-dragonfly",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Collection of all Dragonfly core Python libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/lbt-dragonfly",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent"
+    ],
+    license="AGPL-3.0"
+)
