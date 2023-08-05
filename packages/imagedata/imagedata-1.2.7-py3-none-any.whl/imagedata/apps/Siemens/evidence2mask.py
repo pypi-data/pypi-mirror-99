5b64ccb3602bcaed88a6e5a654a41bcc28d28ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package imagedata/1.2.7/imagedata-1.2.7-py3-none-any.whl

---
+++ packages/imagedata/imagedata-1.2.7-py3-none-any.whl/imagedata/apps/Siemens/evidence2mask.py
@@ -0,0 +1,219 @@
+"""Read Siemens Syngo.VIA Basic Reading
+"""
+
+# Copyright (c) 2017-2020 Erling Andersen, Haukeland University Hospital, Bergen, Norway
+
+import math
+import numpy as np
+import logging
+from imagedata.apps.Siemens.ROI import PolygonROI, EllipseROI
+
+
+def xyz_to_zyx(polygon):
+    """Swap (x,y,z) with (z,y,x) for each point
+
+    Args:
+        polygon: np.array shape (n,3)
+    Returns:
+        rearranged np.array shape (n,3)
+    """
+    assert polygon.ndim == 2, "Array ndim should be 2, not {}".format(polygon.ndim)
+    new_polygon = np.zeros_like(polygon)
+    for i in range(polygon.shape[0]):
+        x, y, z = polygon[i, :]
+        new_polygon[i, :] = z, y, x
+    return new_polygon
+
+
+def evidence2roi(im, uid_table=None, content=None):
+    # def get_rois(image, separate_laterality=False, uid_table=None, content={}):
+
+    """
+    overrows = image[(0x6000, 0x0010)].value
+    overcols = image[(0x6000, 0x0011)].value
+    overkey=(0x6000, 0x3000)
+    overlay=image[overkey].value
+
+    a=bitarray.bitarray(overlay, endian='little')
+
+    while len(a) < len(overlay)*8:
+            a.append(0)
+
+    c = np.array(a.tolist())
+    c = c.astype(np.uint8)
+    #c = c.reshape((overcols, overrows))
+    c = c.reshape((overrows, overcols))
+    """
+
+    # print("get_rois: {}".format(image.InstanceNumber))
+    # content = {}
+    if content is None:
+        content = {}
+    rois = []  # List of ROI tuples
+
+    seqkey = (0x0029, 0x1102)
+    if seqkey in im:
+        presentation_sq = im[seqkey]
+    else:
+        seqkey = (0x0029, 0x1202)
+        if seqkey not in im:
+            logging.debug("get_rois: no presentation sequence (0x0029, 0x1202)")
+            return rois, content
+        presentation_sq = im[seqkey]
+
+    try:
+        measurement = presentation_sq[0][(0x0029, 0x10a7)]
+    except KeyError:
+        logging.debug("get_rois: no measurement sequence (0x0029, 0x10a7)")
+        # return (rois, content)
+        raise KeyError('No measurement sequence (0x0029, 0x10a7) in input data')
+
+    try:
+        findings = measurement[0][(0x0029, 0x1031)]
+    except KeyError:
+        logging.error('get_rois: no findings attribute (0x0029, 0x1031) in input data')
+        raise KeyError('No findings attribute (0x0029, 0x1031) in input data')
+
+    content_sq = presentation_sq[0][(0x0029, 0x10a9)]
+    content['label'] = content_sq[0][(0x0070, 0x0080)].value
+    content['description'] = content_sq[0][(0x0070, 0x0081)].value
+    content['date'] = content_sq[0][(0x0070, 0x0082)].value
+    content['time'] = content_sq[0][(0x0070, 0x0083)].value
+    content['creator'] = '%s' % presentation_sq[0][(0x0070, 0x0084)].value
+
+    for finding in findings:
+        referenced_frame_seq = finding[(0x0029, 0x109a)][0]
+        if referenced_frame_seq[(0x0029, 0x1038)].VM > 1:
+            referenced_syngo_uid = []
+            for u in referenced_frame_seq[(0x0029, 0x1038)].value:
+                referenced_syngo_uid.append(u)
+            referenced_syngo_uid = referenced_frame_seq[(0x0029, 0x1038)].value
+        else:
+            referenced_syngo_uid = referenced_frame_seq[(0x0029, 0x1038)].value.decode().split('\\')
+        if uid_table:
+            stu_ins_uid = uid_table[referenced_syngo_uid[0]]
+            ser_ins_uid = uid_table[referenced_syngo_uid[1]]
+            sop_ins_uid = uid_table[referenced_syngo_uid[2]]
+        else:
+            stu_ins_uid = referenced_syngo_uid[0]
+            ser_ins_uid = referenced_syngo_uid[1]
+            sop_ins_uid = referenced_syngo_uid[2]
+        # sop_cla_uid = referenced_syngo_uid[3] # '1.2.840.10008.5.1.4.1.1.4'
+        # ROI number
+        meas_appl_number = int(finding[(0x0029, 0x1035)].value)
+        roi_type_value = finding[(0x0029, 0x1032)].value.strip()
+        try:
+            roi_name = finding[(0x0029, 0x1030)].value.decode()
+        except AttributeError:
+            roi_name = finding[(0x0029, 0x1030)].value
+        except KeyError:
+            roi_name = 'NONAME'
+        logging.info("Finding: {} {} {}".format(content['creator'], roi_name, roi_type_value))
+        if roi_type_value == 'PolygonApplication3D' or roi_type_value == 'FreehandApplication3D':
+            output = finding[(0x0029, 0x1096)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            polygon = meas_data_points.reshape((meas_data_points.size // 3, 3))
+            logging.debug("XYZ: {}".format(polygon[0]))
+            polygon = xyz_to_zyx(polygon)
+            logging.debug("ZYX: {}".format(polygon[0]))
+
+            rois.append(PolygonROI(polygon, roi_name, stu_ins_uid, ser_ins_uid, sop_ins_uid))
+            logging.debug('ROI {}: {} points'.format(meas_appl_number, len(polygon) // 3))
+        elif roi_type_value == 'EllipseApplication3D':
+            # Ellipsis centre
+            output = finding[(0x0029, 0x1096)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            centre = meas_data_points.reshape((meas_data_points.size // 3, 3))
+            centre = xyz_to_zyx(centre)
+            # Ellipsis angles
+            output = finding[(0x0029, 0x1097)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            angles = meas_data_points.reshape((meas_data_points.size // 3, 3))
+            # Ellipsis thickness
+            output = finding[(0x0029, 0x1099)]
+            if output.VR == "UN":
+                meas_data_points = np.fromstring(output.value, dtype='float32')
+            else:
+                meas_data_points = np.array(output.value)
+            thickness = meas_data_points
+
+            rois.append(EllipseROI(centre, angles, thickness, roi_name, stu_ins_uid, ser_ins_uid, sop_ins_uid))
+        elif roi_type_value == 'StandaloneTextApplication3D':
+            logging.warning("Standalone Text ROI not implemented.")
+            pass
+        elif roi_type_value == 'DistanceLineApplication3D':
+            logging.warning("DistanceLineApplication3D ROI not implemented.")
+            pass
+        else:
+            raise ValueError("ROI type %s not implemented." % roi_type_value)
+
+    return rois, content
+
+
+def make_mask_in_slice(roi_type, si, points, shape):
+    """Make a 3D mask [nz,ny,nx]
+    """
+
+    if roi_type == 'polygon':
+        # polygon = [(x1,y1),(x2,y2),...] or [x1,y1,x2,y2,...]
+        # width = ?
+        # height = ?
+
+        for p in points:
+            save_x(p)
+        polygon = transform_data_points_to_voxels(si, points)
+        points_matrix = roi.get_points_matrix(si)
+        """
+        print("polygon: points cm    {}".format(points))
+        print("polygon: points matrix", polygon)
+        """
+        slice = verify_all_voxels_in_slice(points_matrix)
+
+        mask = np.zeros(shape[1:], dtype=np.bool)
+
+        polygon2D = []
+        for p in points_matrix:
+            z, y, x = p
+            polygon2D.append((x, y))
+
+        img = Image.new('L', (shape[3], shape[2]), 0)
+        ImageDraw.Draw(img).polygon(polygon2D, outline=0, fill=1)
+    elif roi_type == 'ellipse':
+        (centre_cm, angles, thickness) = points
+        centre = transform_data_points_to_voxels(si, centre_cm)[0]
+        slice = centre[0]
+        mask = np.zeros(shape[1:], dtype=np.bool)
+
+        angle1 = angles[1]
+        radius_cm = math.sqrt(angle1[0] * angle1[0] + angle1[1] * angle1[1] + angle1[2] * angle1[2])
+        adjacent_cm = centre_cm + np.array((0, 0, radius_cm))
+        save_x((centre_cm + np.array((0, 0, radius_cm)))[0])
+        save_x((centre_cm - np.array((0, 0, radius_cm)))[0])
+        save_x((centre_cm + np.array((radius_cm, 0, 0)))[0])
+        save_x((centre_cm - np.array((radius_cm, 0, 0)))[0])
+        adjacent = transform_data_points_to_voxels(si, adjacent_cm)[0]
+        radius = abs(centre[1] - adjacent[1])
+
+        yc, xc = centre[1], centre[2]
+        ellipse2D = (xc - radius, yc - radius, xc + radius, yc + radius)
+        img = Image.new('L', (shape[3], shape[2]), 0)
+        ImageDraw.Draw(img).ellipse(ellipse2D, outline=0, fill=1)
+    mask[slice, :, :] = np.array(img)
+    return mask
+
+
+def transform_data_points_to_voxels(si, meas_data_points):
+    polygon = []
+    for point in meas_data_points:
+        x, y, z = si.getVoxelForPosition(point)
+        polygon.append((z, y, x))
+    return polygon
---
+++ packages/imagedata/imagedata-1.2.7-py3-none-any.whl/imagedata/archives/filesystemarchive.py
@@ -0,0 +1,311 @@
+"""Read/Write local image files
+"""
+
+# Copyright (c) 2018 Erling Andersen, Haukeland University Hospital, Bergen, Norway
+
+import os
+import os.path
+import re
+import shutil
+import urllib.parse
+import logging
+from abc import ABC
+
+import imagedata.archives
+import imagedata.transports
+from imagedata.archives.abstractarchive import AbstractArchive
+
+
+class ReadOnlyError(Exception):
+    pass
+
+
+class WriteOnFile(Exception):
+    pass
+
+
+class NoSuchFile(Exception):
+    pass
+
+
+class FilesystemArchive(AbstractArchive, ABC):
+    """Read/write local files."""
+
+    name = "filesystem"
+    description = "Read and write local image files."
+    authors = "Erling Andersen"
+    version = "1.0.0"
+    url = "www.helse-bergen.no"
+    mimetypes = ['*']  # Disregards MIME types
+
+    # self.__dirname: root directory
+    # self.__filelist: list of absolute filename
+
+    @staticmethod
+    def _get_transport(url, mode, read_directory_only):
+        """Get transport plugin from url.
+
+        If the url addressess a missing file in read mode,
+        access the parent directory.
+        """
+
+        url_tuple = urllib.parse.urlsplit(url, scheme='file')
+        logging.debug('FilesystemArchive._get_transport: scheme: %s, netloc: %s' %
+                      (url_tuple.scheme, url_tuple.path))
+
+        try:
+            _transport = imagedata.transports.find_scheme_plugin(
+                url_tuple.scheme,
+                netloc=url_tuple.netloc,
+                root=url_tuple.path,
+                mode=mode,
+                read_directory_only=read_directory_only)
+        except imagedata.transports.RootDoesNotExist:
+            # Mode='r': location does not exist
+            raise
+        except imagedata.transports.RootIsNotDirectory:
+            # Mode='r': Retry with parent directory
+            parent, _ = os.path.split(url_tuple.path)
+            _transport = imagedata.transports.find_scheme_plugin(
+                url_tuple.scheme,
+                netloc=url_tuple.netloc,
+                root=parent,
+                mode=mode,
+                read_directory_only=read_directory_only)
+        return _transport
+
+    def __init__(self, transport=None, url=None, mode='r',
+                 read_directory_only=True, opts=None):
+        super(FilesystemArchive, self).__init__(
+            self.name, self.description,
+            self.authors, self.version, self.url, self.mimetypes)
+        logging.debug("FilesystemArchive.__init__ url: {}".format(url))
+        urldict = urllib.parse.urlsplit(url, scheme="file")
+        if transport is not None:
+            self.__transport = transport
+            self.__path = urldict.path
+        elif url is None:
+            raise ValueError('url not given')
+        else:
+            # Determine transport from url
+            # self.__transport = self._get_transport(url, mode, read_directory_only)
+            # netloc = urldict.netloc
+            # netloc = urldict.path
+            # netloc: where is zipfile
+            # self.__path: zipfile name
+            # netloc, self.__path = os.path.split(urldict.path)
+            # logging.debug('FilesystemArchive.__init__: scheme: %s, netloc: %s path: %s' %
+            #              (urldict.scheme, netloc, self.__path))
+            self.__path = urldict.path
+            logging.debug('FilesystemArchive.__init__: scheme: %s, path: %s' %
+                          (urldict.scheme, self.__path))
+            self.__transport = imagedata.transports.find_scheme_plugin(
+                urldict.scheme,
+                netloc=urldict.netloc,
+                root=self.__path,
+                mode=mode,
+                read_directory_only=read_directory_only,
+                opts=opts)
+        self.__mode = mode
+        self.__files = {}
+
+        logging.debug("FilesystemArchive __init__: {}".format(type(transport)))
+
+        logging.debug("FilesystemArchive path: {}".format(self.__path))
+        # self.__fp = self.__transport.open(
+        #    self.__path, mode=self.__mode + "b")
+        # logging.debug("FilesystemArchive self.__fp: {}".format(type(self.__fp)))
+        logging.debug("FilesystemArchive open zipfile mode %s" % self.__mode)
+
+        # If the URL refers to a single file, let directory_name refer to the
+        # directory and basename to the file
+        logging.debug("FilesystemArchive __init__ verify : {}".format(self.__path))
+        if os.path.isfile(self.__path):
+            self.__dirname = os.path.dirname(self.__path)
+            self.__basename = os.path.basename(self.__path)
+            self.__filelist = [self.__path]
+            logging.debug("FilesystemArchive __init__ directory_name : {}".format(self.__dirname))
+            logging.debug("FilesystemArchive __init__ basename: {}".format(self.__basename))
+            # logging.debug("FilesystemArchive self.__filelist: {}".format(self.__filelist))
+            return
+
+        # The URL refers to a directory. Let directory_name refer to the directory
+        self.__dirname = self.__path
+        self.__basename = None
+        logging.debug("FilesystemArchive __init__ scan directory_name : {}".format(self.__dirname))
+        logging.debug("FilesystemArchive __init__ scan basename: {}".format(self.__basename))
+        self.__filelist = list()
+        # logging.debug("FilesystemArchive walk root: {}".format(self.__urldict.path))
+        for root, dirs, files in self.__transport.walk(self.__path):
+            # logging.debug("FilesystemArchive scan root: {} {}".format(root, files))
+            for filename in files:
+                fname = os.path.join(self.__path, root, filename)
+                # logging.debug("FilesystemArchive scan fname: {}".format(fname))
+                self.__filelist.append(fname)
+                # if transport.isfile(fname):
+                #    if root.startswith(self.__dirname):
+                #        root = root[len(self.__dirname)+1:] # Strip off directory_name
+                #    self.__filelist[fname] = (root,filename)
+                #    logging.debug(fname)
+        self._sort_filelist()
+        # logging.debug("FilesystemArchive self.__filelist: {}".format(self.__filelist))
+
+    @property
+    def transport(self):
+        """Underlying transport plugin
+        """
+        return self.__transport
+
+    def use_query(self):
+        """Do the plugin need the ?query part of the url?"""
+        return False
+
+    def getnames(self, files=None):
+        """Get name list of the members.
+
+        Return:
+            The members as a list of their names.
+                It has the same order as the members of the archive.
+        """
+        # logging.debug('FilesystemArchive getnames: self.__filelist: {}'.format(
+        #    self.__filelist))
+        if files:
+            filelist = list()
+            for filename in self.__filelist:
+                # logging.debug('ZipfileArchive.getmembers: member {}'.format(filename))
+                # filename = member['name']
+                for required_filename in files:
+                    # if filename.endswith(required_filename):
+                    if re.search(required_filename, filename):
+                        filelist.append(filename)
+            if len(filelist) < 1:
+                raise FileNotFoundError('No such file: %s' % files)
+            return filelist
+        else:
+            return self.__filelist
+
+    def basename(self, filehandle):
+        """Basename of file.
+
+        Examples:
+            if archive.basename(filehandle) == "DICOMDIR":
+
+        Args:
+            filehandle: reference to member object
+        """
+        root, filename = filehandle
+        return os.path.basename(filename)
+
+    def open(self, filehandle, mode='rb'):
+        """Open file.
+
+        Returns:
+             a member object for member with filehandle.
+        """
+        # logging.debug("getmember: fname {}".format(filehandle))
+        return self.__transport.open(filehandle, mode)
+
+    def getmembers(self, files=None):
+        """Get the members of the archive.
+
+        Returns:
+            The members of the archive as a list of member objects.
+                The list has the same order as the members in the archive.
+        """
+        # logging.debug("getmembers: files {}".format(files))
+        if files:
+            filelist = list()
+            used = dict()
+            for filename in self.__filelist:
+                for required_filename in files:
+                    if filename.endswith(required_filename):
+                        filelist.append(filename)
+                        used[required_filename] = True
+            # logging.debug("getmembers: filelist {}".format(filelist))
+            for required_filename in files:
+                if required_filename not in used:
+                    raise FileNotFoundError('File %s not found.' %
+                                            os.path.join(
+                                                self.__path,
+                                                required_filename))
+            return filelist
+        else:
+            return self.__filelist
+
+    def to_localfile(self, filehandle):
+        """Access a member object through a local file.
+        """
+        # logging.debug('FilesystemArchive to_localfile: filename %s' %
+        #        filehandle)
+        return filehandle
+
+    def add_localfile(self, local_file, filename):
+        """Add a local file to the archive.
+
+        Args:
+            local_file: named local file
+            filename: filename in the archive
+        Returns:
+            filehandle to file in the archive
+        """
+
+        fname = os.path.join(self.__dirname, filename)
+        if fname not in self.__filelist:
+            # Ensure the directory exists,
+            # create it silently if not.
+            os.makedirs(
+                os.path.dirname(fname),
+                exist_ok=True)
+            shutil.copy(local_file, fname)
+            self.__filelist.append(fname)
+            self._sort_filelist()
+        else:
+            raise imagedata.archives.FileAlreadyExistsError(
+                'File %s already exists' %
+                os.path.join(
+                    self.__path,
+                    filename))
+
+    def writedata(self, filename, data):
+        """Write data to a named file in the archive.
+
+        Args:
+            filename: named file in the archive
+            data: data to write
+        """
+        if self.__mode[0] == 'r':
+            raise ReadOnlyError("Archive is read-only.")
+        if self.__basename is not None:
+            raise WriteOnFile("Do not know how to write a file to a file.")
+        fname = os.path.join(self.__dirname, filename)
+        logging.debug("writedata: fname {}".format(fname))
+        with self.__transport.open(fname, 'wb') as f:
+            f.write(data)
+        self.__filelist.append(fname)
+        self._sort_filelist()
+
+    def close(self):
+        """Close function.
+        """
+        self.__transport.close()
+        return
+
+    def is_file(self, filehandle):
+        """Determine whether the named file is a single file.
+        """
+        return self.__transport.isfile(filehandle)
+
+    def __enter__(self):
+        """Enter context manager.
+        """
+        logging.debug("FilesystemArchive __enter__: {} mode {}".format(type(self.__transport), self.__mode))
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Leave context manager, cleaning up any open files.
+        """
+        self.close()
+
+    def _sort_filelist(self):
+        """Sort self.__filelist, usually after creation or insertion"""
+        self.__filelist = sorted(self.__filelist)
