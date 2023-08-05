5e550f654ff71dc82812de0492d9a64ae6e4e949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package ourtoolkit/0.15/ourtoolkit-0.15.tar.gz

---
+++ packages/ourtoolkit/ourtoolkit-0.15.tar.gz/ourtoolkit-0.15/setup.py
@@ -0,0 +1,28 @@
+VERSION = '0.15'
+from distutils.core import setup
+setup(
+  name = 'ourtoolkit',
+  packages = ['ourtoolkit'],
+  version = VERSION,
+  license='MIT',
+  description = 'A bundle with useless and funny modules',
+  author = 'AlexandreOkita',
+  author_email = 'xande.okita@gmail.com',
+  url = 'https://github.com/AlexandreOkita/our-toolkit',
+  download_url = f'https://github.com/AlexandreOkita/our-toolkit/archive/{VERSION}.tar.gz',
+  keywords = ['funny', 'useless', 'general', 'miscelaneous', "open", 'source'],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
