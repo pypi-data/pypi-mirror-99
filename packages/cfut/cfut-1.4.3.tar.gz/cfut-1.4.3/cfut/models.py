8120af0f78c9193e4f13ba380b538d50c409fd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:23 +0000

    Add package cfut/1.4.3/cfut-1.4.3.tar.gz

---
+++ packages/cfut/cfut-1.4.3.tar.gz/cfut-1.4.3/cfut/models.py
@@ -0,0 +1,39 @@
+from enum import Enum
+from functools import lru_cache
+from typing import Dict, Optional, List, Any
+
+from pydantic import BaseModel, BaseSettings
+
+
+class Capability(str, Enum):
+    CAPABILITY_NAMED_IAM = "CAPABILITY_NAMED_IAM"
+    CAPABILITY_IAM = "CAPABILITY_IAM"
+    CAPABILITY_AUTO_EXPAND = "CAPABILITY_AUTO_EXPAND"
+
+
+class CfnTemplate(BaseModel):
+    name: str
+    path: str
+    capabilities: Optional[List[Capability]]
+    parameters: Optional[Dict[str, Any]]
+
+
+class EcrConfig(BaseModel):
+    repo: str
+    tag = "dev"
+    src = "."  # path to the directory with Dockerfile, e.g. "."
+
+
+class IniFile(BaseModel):
+    ecr: Optional[EcrConfig]
+    profile: Optional[str]
+    templates: Dict[str, CfnTemplate]
+
+
+class EnvVars(BaseSettings):
+    aws_default_region: Optional[str]
+
+
+@lru_cache()
+def get_env():
+    return EnvVars()
