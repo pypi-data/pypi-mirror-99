466ec0405c2b5b77fa17de8f88ee646d48c80bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:36 +0000

    Add package dagster-ge/0.11.0/dagster-ge-0.11.0.tar.gz

---
+++ packages/dagster-ge/dagster-ge-0.11.0.tar.gz/dagster-ge-0.11.0/setup.py
@@ -0,0 +1,38 @@
+from typing import Dict
+
+from setuptools import find_packages, setup  # type: ignore
+
+
+def get_version() -> str:
+    version: Dict[str, str] = {}
+    with open("dagster_ge/version.py") as fp:
+        exec(fp.read(), version)  # pylint: disable=W0122
+
+    return version["__version__"]
+
+
+if __name__ == "__main__":
+    setup(
+        name="dagster-ge",
+        version=get_version(),
+        author="Elementl",
+        license="Apache-2.0",
+        description="Package for GE-specific Dagster framework solid and resource components.",
+        # pylint: disable=line-too-long
+        url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-ge",
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        packages=find_packages(exclude=["test"]),
+        install_requires=[
+            "dagster",
+            "dagster-pandas",
+            "pandas",
+            "great_expectations >=0.11.9, !=0.12.8, <=0.13.10",
+        ],
+        zip_safe=False,
+    )
