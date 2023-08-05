484e69fda09849adb8e88d52d81d86c9c857e230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:54 +0000

    Add package testversion/1.5/testversion-1.5.tar.gz

---
+++ packages/testversion/testversion-1.5.tar.gz/testversion-1.5/README.md
@@ -0,0 +1,1 @@
+This package is to test version format requirement.
---
+++ packages/testversion/testversion-1.5.tar.gz/testversion-1.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testversion',
+    version='1.5',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages()
+)
+
