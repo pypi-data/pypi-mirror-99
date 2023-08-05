47853ea749ff865d5183f93f3f8c6792b7d9be2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:05 +0000

    Add package aws-solutions-constructs.aws-iot-lambda-dynamodb/1.91.0/aws-solutions-constructs.aws-iot-lambda-dynamodb-1.91.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-iot-lambda-dynamodb/aws-solutions-constructs.aws-iot-lambda-dynamodb-1.91.0.tar.gz/aws-solutions-constructs.aws-iot-lambda-dynamodb-1.91.0/src/aws_solutions_constructs/aws_iot_lambda_dynamodb/__init__.py
@@ -0,0 +1,306 @@
+'''
+# aws-iot-lambda-dynamodb module
+
+<!--BEGIN STABILITY BANNER-->---
+
+
+![Stability: Experimental](https://img.shields.io/badge/stability-Experimental-important.svg?style=for-the-badge)
+
+> All classes are under active development and subject to non-backward compatible changes or removal in any
+> future version. These are not subject to the [Semantic Versioning](https://semver.org/) model.
+> This means that while you may use them, you may need to update your source code when upgrading to a newer version of this package.
+
+---
+<!--END STABILITY BANNER-->
+
+| **Reference Documentation**:| <span style="font-weight: normal">https://docs.aws.amazon.com/solutions/latest/constructs/</span>|
+|:-------------|:-------------|
+
+<div style="height:8px"></div>
+
+| **Language**     | **Package**        |
+|:-------------|-----------------|
+|![Python Logo](https://docs.aws.amazon.com/cdk/api/latest/img/python32.png) Python|`aws_solutions_constructs.aws_iot_lambda_dynamodb`|
+|![Typescript Logo](https://docs.aws.amazon.com/cdk/api/latest/img/typescript32.png) Typescript|`@aws-solutions-constructs/aws-iot-lambda-dynamodb`|
+|![Java Logo](https://docs.aws.amazon.com/cdk/api/latest/img/java32.png) Java|`software.amazon.awsconstructs.services.iotlambdadynamodb`|
+
+This AWS Solutions Construct implements an AWS IoT topic rule, an AWS Lambda function and Amazon DynamoDB table with the least privileged permissions.
+
+Here is a minimal deployable pattern definition in Typescript:
+
+```javascript
+const { IotToLambdaToDynamoDBProps,  IotToLambdaToDynamoDB } from '@aws-solutions-constructs/aws-iot-lambda-dynamodb';
+
+const props: IotToLambdaToDynamoDBProps = {
+    lambdaFunctionProps: {
+        code: lambda.Code.fromAsset(`${__dirname}/lambda`),
+        runtime: lambda.Runtime.NODEJS_12_X,
+        handler: 'index.handler'
+    },
+    iotTopicRuleProps: {
+        topicRulePayload: {
+            ruleDisabled: false,
+            description: "Processing of DTC messages from the AWS Connected Vehicle Solution.",
+            sql: "SELECT * FROM 'connectedcar/dtc/#'",
+            actions: []
+        }
+    }
+};
+
+new IotToLambdaToDynamoDB(this, 'test-iot-lambda-dynamodb-stack', props);
+
+```
+
+## Initializer
+
+```text
+new IotToLambdaToDynamoDB(scope: Construct, id: string, props: IotToLambdaToDynamoDBProps);
+```
+
+*Parameters*
+
+* scope [`Construct`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.Construct.html)
+* id `string`
+* props [`IotToLambdaToDynamoDBProps`](#pattern-construct-props)
+
+## Pattern Construct Props
+
+| **Name**     | **Type**        | **Description** |
+|:-------------|:----------------|-----------------|
+|existingLambdaObj?|[`lambda.Function`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.Function.html)|Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.|
+|lambdaFunctionProps?|[`lambda.FunctionProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.FunctionProps.html)|User provided props to override the default props for the Lambda function.|
+|iotTopicRuleProps|[`iot.CfnTopicRuleProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-iot.CfnTopicRuleProps.html)|User provided props to override the default props|
+|dynamoTableProps?|[`dynamodb.TableProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-dynamodb.TableProps.html)|Optional user provided props to override the default props for DynamoDB Table|
+|tablePermissions?|`string`|Optional table permissions to grant to the Lambda function. One of the following may be specified: `All`, `Read`, `ReadWrite`, `Write`.|
+
+## Pattern Properties
+
+| **Name**     | **Type**        | **Description** |
+|:-------------|:----------------|-----------------|
+|iotTopicRule|[`iot.CfnTopicRule`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-iot.CfnTopicRule.html)|Returns an instance of iot.CfnTopicRule created by the construct|
+|lambdaFunction|[`lambda.Function`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.Function.html)|Returns an instance of lambda.Function created by the construct|
+|dynamoTable|[`dynamodb.Table`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-dynamodb.Table.html)|Returns an instance of dynamodb.Table created by the construct|
+
+## Default settings
+
+Out of the box implementation of the Construct without any override will set the following defaults:
+
+### Amazon IoT Rule
+
+* Configure least privilege access IAM role for Amazon IoT
+
+### AWS Lambda Function
+
+* Configure limited privilege access IAM role for Lambda function
+* Enable reusing connections with Keep-Alive for NodeJs Lambda function
+* Enable X-Ray Tracing
+* Set Environment Variables
+
+  * AWS_NODEJS_CONNECTION_REUSE_ENABLED (for Node 10.x and higher functions)
+
+### Amazon DynamoDB Table
+
+* Set the billing mode for DynamoDB Table to On-Demand (Pay per request)
+* Enable server-side encryption for DynamoDB Table using AWS managed KMS Key
+* Creates a partition key called 'id' for DynamoDB Table
+* Retain the Table when deleting the CloudFormation stack
+* Enable continuous backups and point-in-time recovery
+
+## Architecture
+
+![Architecture Diagram](architecture.png)
+
+---
+
+
+© Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_iot
+import aws_cdk.aws_lambda
+import aws_cdk.core
+
+
+class IotToLambdaToDynamoDB(
+    aws_cdk.core.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-solutions-constructs/aws-iot-lambda-dynamodb.IotToLambdaToDynamoDB",
+):
+    def __init__(
+        self,
+        scope: aws_cdk.core.Construct,
+        id: builtins.str,
+        *,
+        iot_topic_rule_props: aws_cdk.aws_iot.CfnTopicRuleProps,
+        dynamo_table_props: typing.Optional[aws_cdk.aws_dynamodb.TableProps] = None,
+        existing_lambda_obj: typing.Optional[aws_cdk.aws_lambda.Function] = None,
+        lambda_function_props: typing.Optional[aws_cdk.aws_lambda.FunctionProps] = None,
+        table_permissions: typing.Optional[builtins.str] = None,
+    ) -> None:
+        '''
+        :param scope: - represents the scope for all the resources.
+        :param id: - this is a a scope-unique id.
+        :param iot_topic_rule_props: User provided props to override the default props. Default: - Default props are used
+        :param dynamo_table_props: Optional user provided props to override the default props. Default: - Default props are used
+        :param existing_lambda_obj: Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored. Default: - None
+        :param lambda_function_props: User provided props to override the default props for the Lambda function. Default: - Default props are used
+        :param table_permissions: Optional table permissions to grant to the Lambda function. One of the following may be specified: "All", "Read", "ReadWrite", "Write". Default: - Read/write access is given to the Lambda function if no value is specified.
+
+        :access: public
+        :since: 0.8.0
+        :summary: Constructs a new instance of the IotToLambdaToDynamoDB class.
+        '''
+        props = IotToLambdaToDynamoDBProps(
+            iot_topic_rule_props=iot_topic_rule_props,
+            dynamo_table_props=dynamo_table_props,
+            existing_lambda_obj=existing_lambda_obj,
+            lambda_function_props=lambda_function_props,
+            table_permissions=table_permissions,
+        )
+
+        jsii.create(IotToLambdaToDynamoDB, self, [scope, id, props])
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="dynamoTable")
+    def dynamo_table(self) -> aws_cdk.aws_dynamodb.Table:
+        return typing.cast(aws_cdk.aws_dynamodb.Table, jsii.get(self, "dynamoTable"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="iotTopicRule")
+    def iot_topic_rule(self) -> aws_cdk.aws_iot.CfnTopicRule:
+        return typing.cast(aws_cdk.aws_iot.CfnTopicRule, jsii.get(self, "iotTopicRule"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="lambdaFunction")
+    def lambda_function(self) -> aws_cdk.aws_lambda.Function:
+        return typing.cast(aws_cdk.aws_lambda.Function, jsii.get(self, "lambdaFunction"))
+
+
+@jsii.data_type(
+    jsii_type="@aws-solutions-constructs/aws-iot-lambda-dynamodb.IotToLambdaToDynamoDBProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "iot_topic_rule_props": "iotTopicRuleProps",
+        "dynamo_table_props": "dynamoTableProps",
+        "existing_lambda_obj": "existingLambdaObj",
+        "lambda_function_props": "lambdaFunctionProps",
+        "table_permissions": "tablePermissions",
+    },
+)
+class IotToLambdaToDynamoDBProps:
+    def __init__(
+        self,
+        *,
+        iot_topic_rule_props: aws_cdk.aws_iot.CfnTopicRuleProps,
+        dynamo_table_props: typing.Optional[aws_cdk.aws_dynamodb.TableProps] = None,
+        existing_lambda_obj: typing.Optional[aws_cdk.aws_lambda.Function] = None,
+        lambda_function_props: typing.Optional[aws_cdk.aws_lambda.FunctionProps] = None,
+        table_permissions: typing.Optional[builtins.str] = None,
+    ) -> None:
+        '''
+        :param iot_topic_rule_props: User provided props to override the default props. Default: - Default props are used
+        :param dynamo_table_props: Optional user provided props to override the default props. Default: - Default props are used
+        :param existing_lambda_obj: Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored. Default: - None
+        :param lambda_function_props: User provided props to override the default props for the Lambda function. Default: - Default props are used
+        :param table_permissions: Optional table permissions to grant to the Lambda function. One of the following may be specified: "All", "Read", "ReadWrite", "Write". Default: - Read/write access is given to the Lambda function if no value is specified.
+
+        :summary: The properties for the IotToLambdaToDynamoDB class.
+        '''
+        if isinstance(iot_topic_rule_props, dict):
+            iot_topic_rule_props = aws_cdk.aws_iot.CfnTopicRuleProps(**iot_topic_rule_props)
+        if isinstance(dynamo_table_props, dict):
+            dynamo_table_props = aws_cdk.aws_dynamodb.TableProps(**dynamo_table_props)
+        if isinstance(lambda_function_props, dict):
+            lambda_function_props = aws_cdk.aws_lambda.FunctionProps(**lambda_function_props)
+        self._values: typing.Dict[str, typing.Any] = {
+            "iot_topic_rule_props": iot_topic_rule_props,
+        }
+        if dynamo_table_props is not None:
+            self._values["dynamo_table_props"] = dynamo_table_props
+        if existing_lambda_obj is not None:
+            self._values["existing_lambda_obj"] = existing_lambda_obj
+        if lambda_function_props is not None:
+            self._values["lambda_function_props"] = lambda_function_props
+        if table_permissions is not None:
+            self._values["table_permissions"] = table_permissions
+
+    @builtins.property
+    def iot_topic_rule_props(self) -> aws_cdk.aws_iot.CfnTopicRuleProps:
+        '''User provided props to override the default props.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("iot_topic_rule_props")
+        assert result is not None, "Required property 'iot_topic_rule_props' is missing"
+        return typing.cast(aws_cdk.aws_iot.CfnTopicRuleProps, result)
+
+    @builtins.property
+    def dynamo_table_props(self) -> typing.Optional[aws_cdk.aws_dynamodb.TableProps]:
+        '''Optional user provided props to override the default props.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("dynamo_table_props")
+        return typing.cast(typing.Optional[aws_cdk.aws_dynamodb.TableProps], result)
+
+    @builtins.property
+    def existing_lambda_obj(self) -> typing.Optional[aws_cdk.aws_lambda.Function]:
+        '''Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.
+
+        :default: - None
+        '''
+        result = self._values.get("existing_lambda_obj")
+        return typing.cast(typing.Optional[aws_cdk.aws_lambda.Function], result)
+
+    @builtins.property
+    def lambda_function_props(
+        self,
+    ) -> typing.Optional[aws_cdk.aws_lambda.FunctionProps]:
+        '''User provided props to override the default props for the Lambda function.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("lambda_function_props")
+        return typing.cast(typing.Optional[aws_cdk.aws_lambda.FunctionProps], result)
+
+    @builtins.property
+    def table_permissions(self) -> typing.Optional[builtins.str]:
+        '''Optional table permissions to grant to the Lambda function.
+
+        One of the following may be specified: "All", "Read", "ReadWrite", "Write".
+
+        :default: - Read/write access is given to the Lambda function if no value is specified.
+        '''
+        result = self._values.get("table_permissions")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "IotToLambdaToDynamoDBProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "IotToLambdaToDynamoDB",
+    "IotToLambdaToDynamoDBProps",
+]
+
+publication.publish()
