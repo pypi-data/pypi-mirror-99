8673fcd7d4d1b8c798881fef320cda7384ca8a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:11 +0000

    Add package aws-lambda-decorators/0.51/aws_lambda_decorators-0.51-py3-none-any.whl

---
+++ packages/aws-lambda-decorators/aws_lambda_decorators-0.51-py3-none-any.whl/aws_lambda_decorators/utils.py
@@ -0,0 +1,153 @@
+"""Utility functions."""
+from functools import lru_cache
+from http import HTTPStatus
+import inspect
+import json
+import keyword
+import logging
+import os
+
+import boto3
+
+
+LOG_LEVEL = getattr(logging, os.getenv("LOG_LEVEL", "INFO"))
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(LOG_LEVEL)
+    return logger
+
+
+def full_name(class_type):
+    """
+    Gets the fully qualified name of a class type.
+
+    From https://stackoverflow.com/questions/2020014/get-fully-qualified-class-name-of-an-object-in-python
+
+    Args:
+        class_type (type): the type of the class.
+
+    Returns:
+        the fully qualified name of the class type.
+    """
+    module = class_type.__class__.__module__
+    if module is None or module == str.__class__.__module__:
+        return class_type.__class__.__name__  # Avoid reporting __builtin__
+    return f"{module}.{class_type.__class__.__name__}"
+
+
+def is_type_in_list(item_type, items):
+    """
+    Checks if there is an item of a given type in the list of items.
+
+    Args:
+        item_type (type): the type of the item.
+        items (list): a list of items.
+
+    Returns:
+        true if an item of the given type exists in the list, otherwise false.
+    """
+    return any(isinstance(item, item_type) for item in items)
+
+
+def is_valid_variable_name(name):
+    """
+    Check if the given name is python allowed variable name.
+
+    Args:
+        name (str): the name to check.
+
+    Returns:
+        true if the name can be used as a python variable name.
+    """
+    return name.isidentifier() and not keyword.iskeyword(name)
+
+
+def all_func_args(func, args, kwargs):
+    """
+    Combine arguments and key word arguments to a dictionary.
+
+    Args:
+        func (function): function whose arguments should be extracted.
+        args (list): list of function args (*args).
+        kwargs (dict): dictionary of function kwargs (**kwargs).
+
+    Returns:
+        dictionary argument name -> argument value.
+    """
+    arg_spec = inspect.getfullargspec(func)[0]
+    arg_dictionary = {arg_spec[idx]: value for idx, value in enumerate(args)}
+    arg_dictionary.update(kwargs)
+    return arg_dictionary
+
+
+def find_key_case_insensitive(key_name, the_dict):
+    """
+    Finds if a dictionary (the_dict) has a string key (key_name) in any string case
+
+    Args:
+        key_name: the key to search in the dictionary
+        the_dict: the dictionary to search
+
+    Returns:
+        The found key name in its original case, if found. Otherwise, returns the searching key name
+
+    """
+    for key in the_dict:
+        if key.lower() == key_name:
+            return key
+    return key_name
+
+
+def failure(errors, status_code=HTTPStatus.BAD_REQUEST):
+    """
+    Returns an error to the caller
+
+    Args:
+        errors (list): a list of errors to be returned
+        status_code (int): the status code of the error
+
+    Returns:
+        An object that contains the status code and the list of errors
+    """
+    return {
+        "statusCode": status_code,
+        "body": json.dumps({"message": errors})
+    }
+
+
+def find_websocket_connection_id(args: list) -> str:
+    """
+    Finds an API Gateway connection id from the event dictionary in the
+    arguments of a lambda
+
+    Args:
+        args (list): a list of arguments from a lambda (*args)
+
+    Returns:
+        The connection id of a user as a string if found
+        None if not
+    """
+    for arg in args:
+        if isinstance(arg, dict) and "requestContext" in arg:
+            return arg["requestContext"].get("connectionId")
+    return None
+
+
+@lru_cache()
+def get_websocket_endpoint(endpoint_url: str) -> "botocore.client.ApiGatewayManagementApi":  # noqa: pyflakes - F821
+    """
+    Gets an instance of ApiGatewayManagementApi for sending messages
+    through websockets
+
+    Args:
+        endpoint_url (str): an api gateway connection url (ish)
+
+    Returns:
+        The api gateway management client (cached)
+    """
+    return boto3.client(
+        "apigatewaymanagementapi",
+        endpoint_url=endpoint_url
+    )
---
+++ packages/aws-lambda-decorators/aws_lambda_decorators-0.51-py3-none-any.whl/aws_lambda_decorators/validators.py
@@ -0,0 +1,380 @@
+"""Validation rules."""
+import datetime
+import re
+from schema import SchemaError
+
+CURRENCIES = {"LKR", "ETB", "RWF", "NZD", "SBD", "MKD", "NPR", "LAK", "KWD", "INR", "HUF", "AFN", "BTN", "ISK", "MVR",
+              "WST", "MNT", "AZN", "SAR", "JMD", "BIF", "BMD", "CAD", "GEL", "MXN", "BHD", "HKD", "RSD", "PKR", "SLL",
+              "NGN", "TOP", "SCR", "SVC", "CHW", "UYW", "IDR", "IQD", "THB", "GBP", "MYR", "SDG", "CNY", "GNF", "LRD",
+              "KHR", "TJS", "BYN", "SHP", "AED", "BOB", "CUC", "PHP", "SSP", "USN", "MZN", "COP", "SEK", "EUR", "CDF",
+              "CRC", "KMF", "JPY", "ZWL", "ALL", "GHS", "GIP", "QAR", "GYD", "HTG", "VUV", "CZK", "ANG", "AWG", "AMD",
+              "DOP", "TRY", "ZMW", "MGA", "KZT", "XUA", "ARS", "XPF", "BRL", "MXV", "LSL", "CLP", "KES", "PYG", "TND",
+              "MAD", "DZD", "MWK", "BSD", "BBD", "FKP", "KGS", "BWP", "CVE", "HRK", "DKK", "COU", "SYP", "LYD", "PLN",
+              "TZS", "KPW", "UGX", "BOV", "UAH", "NAD", "AOA", "VES", "SOS", "CUP", "SGD", "PAB", "UZS", "STN", "SRD",
+              "CHE", "XOF", "DJF", "PGK", "UYI", "XCD", "BZD", "EGP", "ERN", "RON", "TWD", "USD", "FJD", "VND", "SZL",
+              "BND", "HNL", "KRW", "XAF", "MDL", "BDT", "MUR", "PEN", "OMR", "NIO", "TMT", "YER", "TTD", "GMD", "XDR",
+              "CHF", "NOK", "GTQ", "JOD", "KYD", "UYU", "RUB", "ZAR", "AUD", "BGN", "MOP", "LBP", "MRU", "CLF", "XSU",
+              "BAM", "MMK", "IRR", "ILS"}
+
+
+class Validator:  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if the given mandatory value exists."""
+    ERROR_MESSAGE = "Unknown error"
+
+    def __init__(self, error_message, condition=None):
+        """
+        Validates a parameter
+
+        Args:
+            error_message (str): A custom error message to output if validation fails
+            condition (any): A condition to validate
+        """
+        self._error_message = error_message or self.ERROR_MESSAGE
+        self._condition = condition
+
+    def message(self, value=None):  # noqa: pylint - unused-argument
+        """
+        Gets the formatted error message for a failed mandatory check
+
+        Args:
+            value (any): The validated value
+
+        Returns:
+            The error message
+        """
+        return self._error_message.format(value=value, condition=self._condition)
+
+
+class Mandatory(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if the given mandatory value exists."""
+    ERROR_MESSAGE = "Missing mandatory value"
+
+    def __init__(self, error_message=None):
+        """
+        Checks if a parameter has a value
+
+        Args:
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message)
+
+    @staticmethod
+    def validate(value=None):
+        """
+        Check if the given mandatory value exists.
+
+        Args:
+            value (any): Value to be validated.
+        """
+        return value is not None and str(value)
+
+
+class RegexValidator(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if a value matches a regular expression."""
+    ERROR_MESSAGE = "'{value}' does not conform to regular expression '{condition}'"
+
+    def __init__(self, regex="", error_message=None):
+        """
+        Compile a regular expression to a regular expression pattern.
+
+        Args:
+            regex (str): Regular expression for parameter validation.
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, regex)
+        self._regexp = re.compile(regex)
+
+    def validate(self, value=None):
+        """
+        Check if a value adheres to the defined regular expression.
+
+        Args:
+            value (str): Value to be validated.
+        """
+        if value is None:
+            return True
+
+        return self._regexp.fullmatch(value) is not None
+
+
+class SchemaValidator(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if a value matches a regular expression."""
+    ERROR_MESSAGE = "'{value}' does not validate against schema '{condition}'"
+
+    def __init__(self, schema, error_message=None):
+        """
+        Set the schema field.
+
+        Args:
+            schema (Schema): The expected schema.
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, schema)
+
+    def validate(self, value=None):
+        """
+        Check if the object adheres to the defined schema.
+
+        Args:
+            value (object): Value to be validated.
+        """
+        try:
+            if value is None:
+                return True
+
+            return self._condition.validate(value) == value
+        except SchemaError:
+            return False
+
+
+class Minimum(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if a value is greater than a minimum value."""
+    ERROR_MESSAGE = "'{value}' is less than minimum value '{condition}'"
+
+    def __init__(self, minimum: (float, int), error_message=None):
+        """
+        Set the minimum value.
+
+        Args:
+            minimum (float, int): The minimum value.
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, minimum)
+
+    def validate(self, value=None):
+        """
+        Check if the value is greater than the minimum.
+
+        Args:
+            value (float, int): Value to be validated.
+        """
+        if value is None:
+            return True
+
+        if isinstance(value, (float, int)):
+            return self._condition <= value
+
+        return False
+
+
+class Maximum(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if a value is less than a maximum value."""
+    ERROR_MESSAGE = "'{value}' is greater than maximum value '{condition}'"
+
+    def __init__(self, maximum: (float, int), error_message=None):
+        """
+        Set the maximum value.
+
+        Args:
+            maximum (float, int): The maximum value.
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, maximum)
+
+    def validate(self, value=None):
+        """
+        Check if the value is less than the maximum.
+
+        Args:
+            value (float, int): Value to be validated.
+        """
+        if value is None:
+            return True
+
+        if isinstance(value, (float, int)):
+            return self._condition >= value
+
+        return False
+
+
+class MinLength(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if a string is shorter than a minimum length."""
+    ERROR_MESSAGE = "'{value}' is shorter than minimum length '{condition}'"
+
+    def __init__(self, min_length: int, error_message=None):
+        """
+        Set the minimum length.
+
+        Args:
+            min_length (int): The minimum length.
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, min_length)
+
+    def validate(self, value=None):
+        """
+        Check if a string is shorter than the minimum length.
+
+        Args:
+            value (str): String to be validated.
+        """
+        if value is None:
+            return True
+
+        return len(str(value)) >= self._condition
+
+
+class MaxLength(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if a string is longer than a maximum length."""
+    ERROR_MESSAGE = "'{value}' is longer than maximum length '{condition}'"
+
+    def __init__(self, max_length: int, error_message=None):
+        """
+        Set the maximum length.
+
+        Args:
+            max_length (int): The maximum length.
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, max_length)
+
+    def validate(self, value=None):
+        """
+        Check if a string is longer than the maximum length.
+
+        Args:
+            value (str): String to be validated.
+        """
+        if value is None:
+            return True
+
+        return len(str(value)) <= self._condition
+
+
+class Type(Validator):
+    ERROR_MESSAGE = "'{value}' is not of type '{condition.__name__}'"
+
+    def __init__(self, valid_type: type, error_message=None):
+        """
+        Set the valid type.
+
+        Args:
+            valid_type (type): The value type to check.
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, valid_type)
+
+    def validate(self, value=None):
+        """
+        Check if a value is of the right type
+
+        Args:
+            value (object): object to be validated.
+        """
+        if value is None:
+            return True
+
+        return isinstance(value, self._condition)
+
+
+class EnumValidator(Validator):
+    ERROR_MESSAGE = "'{value}' is not in list '{condition}'"
+
+    def __init__(self, *args: list, error_message=None):
+        """
+        Set the list of valid values.
+
+        Args:
+            error_message (str): A custom error message to output if validation fails
+            args (list): The list of valid values
+        """
+        super().__init__(error_message, args)
+
+    def validate(self, value=None):
+        """
+        Check if a value is in a list of valid values
+
+        Args:
+            value (object): object to be validated.
+        """
+        if value is None:
+            return True
+
+        return value in self._condition
+
+
+class NonEmpty(Validator):  # noqa: pylint - too-few-public-methods
+    """Validation rule to check if the given value is empty."""
+    ERROR_MESSAGE = "Value is empty"
+
+    def __init__(self, error_message=None):
+        """
+        Checks if a parameter has a non empty value
+
+        Args:
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message)
+
+    @staticmethod
+    def validate(value=None):
+        """
+        Check if the given value is non empty.
+
+        Args:
+            value (any): Value to be validated.
+        """
+        if value is None or value in (0, 0.0, 0j):
+            return True
+
+        return bool(value)
+
+
+class DateValidator(Validator):
+    """Validation rule to check if a string is a valid date according to some format."""
+    ERROR_MESSAGE = "'{value}' is not a '{condition}' date"
+
+    def __init__(self, date_format: str, error_message=None):
+        """
+        Checks if a string is a date with a given format
+
+        Args:
+            date_format (str): The date format to check against
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message, date_format)
+
+    def validate(self, value=None):
+        """
+        Check if a string is a date with a given format
+
+        Args:
+            value (str): string date to validate against a format
+        """
+        if value is None:
+            return True
+
+        try:
+            datetime.datetime.strptime(value, self._condition)
+        except ValueError:
+            return False
+        else:
+            return True
+
+
+class CurrencyValidator(Validator):
+    """Validation rule to check if a string is a valid currency according to ISO 4217 Currency Code."""
+    ERROR_MESSAGE = "'{value}' is not a valid currency code."
+
+    def __init__(self, error_message=None):
+        """
+        Checks if a string is a valid currency based on ISO 4217
+
+        Args:
+            error_message (str): A custom error message to output if validation fails
+        """
+        super().__init__(error_message)
+
+    @staticmethod
+    def validate(value=None):
+        """
+        Check if a string is a valid currency based on ISO 4217
+
+        Args:
+            value (str): value to validate against a ISO 4217
+        """
+
+        if value is None:
+            return True
+
+        return value.upper() in CURRENCIES
