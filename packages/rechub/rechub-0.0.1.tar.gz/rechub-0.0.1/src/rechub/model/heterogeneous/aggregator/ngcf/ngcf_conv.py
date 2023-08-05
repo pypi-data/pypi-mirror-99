2f19f52790a10f04589ab4897da7d4436eda011f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:25 +0000

    Add package rechub/0.0.1/rechub-0.0.1.tar.gz

---
+++ packages/rechub/rechub-0.0.1.tar.gz/rechub-0.0.1/src/rechub/model/heterogeneous/aggregator/ngcf/ngcf_conv.py
@@ -0,0 +1,84 @@
+import torch
+import torch.nn as nn
+import dgl.function as fn
+# from dgl.base import DGLError
+from dgl.utils import expand_as_pair
+
+
+def has_self_loop(graph):
+    return any(graph.edges()[0] - graph.edges()[1] == 0)
+
+
+class NGCFConv(nn.Module):
+    def __init__(self, in_features, out_features, activation=None):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.activation = activation
+        self.weight_self = nn.Parameter(torch.empty(in_features, out_features))
+        self.weight_interaction = nn.Parameter(
+            torch.empty(in_features, out_features))
+        nn.init.xavier_uniform_(self.weight_self)
+        nn.init.xavier_uniform_(self.weight_interaction)
+
+    def forward(self, graph, feature):
+        with graph.local_scope():
+            # TODO very slowly
+            # if has_self_loop(graph):
+            #     raise DGLError(
+            #         'You should first remove self loop with `dgl.remove_self_loop`.'
+            #     )
+
+            feat_src, feat_dst = expand_as_pair(feature, graph)
+
+            degs = graph.out_degrees().float().clamp(min=1)
+            norm = torch.pow(degs, -0.5)
+            shp = norm.shape + (1, ) * (feat_src.dim() - 1)
+            norm = torch.reshape(norm, shp)
+            feat_src = feat_src * norm
+
+            graph.srcdata['h'] = feat_src
+            graph.dstdata['h'] = feat_dst
+
+            graph.update_all(fn.copy_u('h', 'm'), fn.sum('m', 'h_self'))
+
+            def message_func(edges):
+                return {'m': torch.mul(edges.src['h'], edges.dst['h'])}
+
+            graph.update_all(message_func, fn.sum('m', 'h_interaction'))
+
+            rst = torch.matmul(
+                graph.dstdata['h_self'], self.weight_self) + torch.matmul(
+                    graph.dstdata['h_interaction'], self.weight_interaction)
+            # TODO adding the interaction part will reduce the performance
+
+            degs = graph.in_degrees().float().clamp(min=1)
+            norm = torch.pow(degs, -0.5)
+            shp = norm.shape + (1, ) * (feat_dst.dim() - 1)
+            norm = torch.reshape(norm, shp)
+            rst = rst * norm
+
+            if self.activation is not None:
+                rst = self.activation(rst)
+
+            return rst
+
+    def extra_repr(self):
+        """Set the extra representation of the module,
+        which will come into effect when printing the model.
+        """
+        return f'in={self.in_features}, out={self.out_features}, activation={self.activation}'
+
+
+if __name__ == '__main__':
+    import dgl
+    graph = dgl.graph(([0, 0, 1, 2, 2], [1, 2, 2, 1, 0]))
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    graph = graph.to(device)
+    model = NGCFConv(2, 2).to(device)
+    inputs = torch.tensor([
+        [0.2, 0.5],
+        [0.6, 0.4],
+        [0.3, 0.3],
+    ]).to(device)
+    print(model(graph, inputs))
---
+++ packages/rechub/rechub-0.0.1.tar.gz/rechub-0.0.1/src/rechub/parameters.py
@@ -0,0 +1,111 @@
+import argparse
+from distutils.util import strtobool
+
+
+def str2bool(x):
+    return bool(strtobool(x))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--num_epochs', type=int, default=1000)
+    parser.add_argument('--learning_rate', type=float, default=0.0005)
+    parser.add_argument('--batch_size', type=int, default=4096)
+    parser.add_argument('--num_workers', type=int, default=16)
+    parser.add_argument('--non_graph_embedding_dim', type=int, default=200)
+    parser.add_argument('--graph_embedding_dims',
+                        type=int,
+                        nargs='+',
+                        default=[200, 128, 64])
+    parser.add_argument(
+        '--neighbors_sampling_quantile',
+        type=float,
+        default=0.9,
+        help=
+        'Set the number of sampled neighbors to the quantile of the numbers of neighbors'
+    )
+    parser.add_argument('--min_neighbors_sampled', type=int, default=4)
+    parser.add_argument('--max_neighbors_sampled', type=int, default=512)
+    parser.add_argument('--single_attribute_dim', type=int,
+                        default=40)  # TODO: support attributes
+    parser.add_argument('--attention_query_vector_dim', type=int, default=200)
+    parser.add_argument(
+        '--dnn_predictor_dims',
+        type=int,
+        nargs='+',
+        default=[-1, 128, 1],
+        help=
+        'You can set first dim as -1 to make it automatically fit the input vector'
+    )
+    parser.add_argument('--num_batches_show_loss', type=int, default=50)
+    parser.add_argument('--num_epochs_validate', type=int, default=5)
+    parser.add_argument('--early_stop_patience', type=int, default=20)
+    parser.add_argument('--num_attention_heads', type=int, default=8)
+    parser.add_argument('--save_checkpoint', type=str2bool, default=False)
+    parser.add_argument('--different_embeddings', type=str2bool, default=False)
+    parser.add_argument('--negative_sampling_ratio', type=int, default=4)
+
+    parser.add_argument(
+        '--model_name',
+        type=str,
+        default='GCN',
+        choices=[
+            # Non-graph
+            'NCF',
+
+            # Graph with single type of edge (we think it as homogeneous graph)
+            'GCN',
+            'GAT',
+            'LightGCN',
+            'NGCF',
+
+            # Graph with multiple types of edge (we think it as heterogeneous graph)
+            'HET-GCN',
+            'HET-GAT',
+            'HET-NGCF',
+            'HET-LightGCN',
+
+            # To be categorized
+            'GraphRec',
+            'DeepFM',
+            'DSSM',
+            'DiffNet',
+            'DiffNet++',
+            'DANSER'
+        ])
+    parser.add_argument('--dataset', type=str, default='jd')
+    parser.add_argument('--embedding_aggregator',
+                        type=str,
+                        default='concat',
+                        choices=['concat', 'attn'])
+    parser.add_argument('--predictor',
+                        type=str,
+                        default='dnn',
+                        choices=['dot', 'dnn'])
+    parser.add_argument('--metadata_path', type=str)
+    parser.add_argument('--edge_choice',
+                        type=int,
+                        nargs='+',
+                        default=[],
+                        help='Left empty to use all in metadata file')
+    parser.add_argument('--training_task_choice',
+                        type=int,
+                        nargs='+',
+                        default=[],
+                        help='Left empty to use all in metadata file')
+    parser.add_argument('--evaluation_task_choice',
+                        type=int,
+                        nargs='+',
+                        default=[],
+                        help='Left empty to use all in `training_task_choice`')
+    parser.add_argument('--task_loss_overwrite', type=str, nargs='+')
+    parser.add_argument('--task_weight_overwrite', type=float, nargs='+')
+    args, unknown = parser.parse_known_args()
+    if len(unknown) > 0:
+        print(
+            'Warning: if you are not in testing mode, you may have got some parameters wrong input'
+        )
+    if args.metadata_path is None:
+        args.metadata_path = f'metadata/{args.dataset}.json'
+    return args
