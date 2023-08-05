96258d9a631487b8b3210382c84402425577aa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:53 +0000

    Add package thundra-debugger/0.0.3/thundra_debugger-0.0.3-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/thundra-debugger/thundra_debugger-0.0.3-cp36-cp36m-manylinux2010_x86_64.whl/tracepointdebug/tracepoint/condition/operand/boolean_operand.py
@@ -0,0 +1,17 @@
+from tracepointdebug.tracepoint.condition.operand.typed_operand import TypedOperand
+
+
+class BooleanOperand(TypedOperand):
+
+    def __init__(self, value_provider):
+        super().__init__(bool, value_provider)
+
+    def is_eq(self, value, condition_context):
+        cur_val = self.get_value(condition_context)
+        return cur_val == value
+
+    def is_ne(self, value, condition_context):
+        cur_val = self.get_value(condition_context)
+        return cur_val != value
+
+
