cd5823d736640b3007dfe3638fd38a887346c948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:02 +0000

    Add package vaex-core/4.1.0/vaex_core-4.1.0-cp37-cp37m-win_amd64.whl

---
+++ packages/vaex-core/vaex_core-4.1.0-cp37-cp37m-win_amd64.whl/vaex/agg.py
@@ -0,0 +1,300 @@
+import os
+import numpy as np
+
+from .stat import _Statistic
+from vaex import encoding
+from .datatype import DataType
+
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    import vaex.superagg
+
+
+aggregates = {}
+
+
+def register(f, name=None):
+    name = name or f.__name__
+    aggregates[name] = f
+    return f
+
+
+@encoding.register('aggregation')
+class aggregation_encoding:
+    @staticmethod
+    def encode(encoding, agg):
+        return agg.encode(encoding)
+
+    @staticmethod
+    def decode(encoding, agg_spec):
+        agg_spec = agg_spec.copy()
+        type = agg_spec.pop('aggregation')
+        f = aggregates[type]
+        args = []
+        if type == '_sum_moment':
+            if 'parameters' in agg_spec:  # renameing between spec and implementation
+                agg_spec['moment'] = agg_spec.pop('parameters')[0]
+        if type == 'first':
+            args = agg_spec.pop('expression')
+        return f(*args, **agg_spec)
+
+
+class AggregatorDescriptor(object):
+    def __repr__(self):
+        return 'vaex.agg.{}({!r})'.format(self.short_name, str(self.expression))
+
+
+    def finish(self, value):
+        return value
+
+class AggregatorDescriptorBasic(AggregatorDescriptor):
+    def __init__(self, name, expression, short_name, multi_args=False, agg_args=[], selection=None, edges=False):
+        self.name = name
+        self.short_name = short_name
+        self.expression = str(expression)
+        self.agg_args = agg_args
+        self.edges = edges
+        self.selection = selection
+        if not multi_args:
+            if self.expression == '*':
+                self.expressions = []
+            else:
+                self.expressions = [self.expression]
+        else:
+            self.expressions = expression
+
+    def encode(self, encoding):
+        spec = {'aggregation': self.short_name}
+        if len(self.expressions) == 0:
+            pass
+        elif len(self.expressions) == 1:
+            spec['expression'] = self.expression
+        else:
+            spec['expression'] = [str(k) for k in self.expressions]
+        if self.selection is not None:
+            spec['selection'] = self.selection
+        if self.edges:
+            spec['edges'] = True
+        if self.agg_args:
+            spec['parameters'] = self.agg_args
+        return spec
+
+    def pretty_name(self, id=None):
+        id = id or "_".join(map(str, self.expressions))
+        return '{0}_{1}'.format(id, self.short_name)
+
+    def _prepare_types(self, df):
+        if self.expression == '*':
+            self.dtype_in = DataType(np.dtype('int64'))
+            self.dtype_out = DataType(np.dtype('int64'))
+        else:
+            self.dtype_in = df[str(self.expressions[0])].data_type()
+            self.dtype_out = self.dtype_in
+            if self.short_name == "count":
+                self.dtype_out = DataType(np.dtype('int64'))
+            if self.short_name in ['sum', 'summoment']:
+                self.dtype_out = self.dtype_in.upcast()
+
+    def add_operations(self, agg_task, **kwargs):
+        df = agg_task.df
+        self._prepare_types(df)
+        value = agg_task.add_aggregation_operation(self, **kwargs)
+        @vaex.delayed
+        def finish(value):
+            return self.finish(value)
+        return finish(value)
+
+    def _create_operation(self, df, grid):
+        agg_op_type = vaex.utils.find_type_from_dtype(vaex.superagg, self.name + "_", self.dtype_in)
+        agg_op = agg_op_type(grid, *self.agg_args)
+        return agg_op
+
+    def get_result(self, agg_operation):
+        grid = np.asarray(agg_operation)
+        if not self.edges:
+            grid = vaex.utils.extract_central_part(grid)
+        return grid
+
+
+class AggregatorDescriptorNUnique(AggregatorDescriptorBasic):
+    def __init__(self, name, expression, short_name, dropmissing, dropnan, selection=None, edges=False):
+        super(AggregatorDescriptorNUnique, self).__init__(name, expression, short_name, selection=selection, edges=edges)
+        self.dropmissing = dropmissing
+        self.dropnan = dropnan
+
+    def encode(self, encoding):
+        spec = super().encode(encoding)
+        if self.dropmissing:
+            spec['dropmissing'] = self.dropmissing
+        if self.dropnan:
+            spec['dropnan'] = self.dropnan
+        return spec
+
+    def _create_operation(self, df, grid):
+        self.dtype_in = df[str(self.expressions[0])].dtype
+        self.dtype_out = DataType(np.dtype('int64'))
+        agg_op_type = vaex.utils.find_type_from_dtype(vaex.superagg, self.name + "_", self.dtype_in)
+        agg_op = agg_op_type(grid, self.dropmissing, self.dropnan)
+        return agg_op
+
+
+class AggregatorDescriptorMulti(AggregatorDescriptor):
+    """Uses multiple operations/aggregation to calculate the final aggretation"""
+    def __init__(self, name, expression, short_name, selection=None, edges=False):
+        self.name = name
+        self.short_name = short_name
+        self.expression = str(expression)
+        self.expressions = [self.expression]
+        self.selection = selection
+        self.edges = edges
+
+    def pretty_name(self, id=None):
+        id = id or "_".join(map(str, self.expressions))
+        return '{0}_{1}'.format(id, self.short_name)
+
+
+class AggregatorDescriptorMean(AggregatorDescriptorMulti):
+    def __init__(self, name, expression, short_name="mean", selection=None, edges=False):
+        super(AggregatorDescriptorMean, self).__init__(name, expression, short_name, selection=selection, edges=edges)
+
+    def add_operations(self, agg_task, **kwargs):
+        expression = expression_sum = expression = agg_task.df[str(self.expression)]
+        # ints, floats and bools are upcasted
+        if expression_sum.dtype.kind in "buif":
+            expression = expression_sum = expression_sum.astype('float64')
+
+        sum_agg = sum(expression_sum, selection=self.selection, edges=self.edges)
+        count_agg = count(expression, selection=self.selection, edges=self.edges)
+
+        task_sum = sum_agg.add_operations(agg_task, **kwargs)
+        task_count = count_agg.add_operations(agg_task, **kwargs)
+        self.dtype_in = sum_agg.dtype_in
+        self.dtype_out = sum_agg.dtype_out
+
+        @vaex.delayed
+        def finish(sum, count):
+            sum = np.array(sum)
+            dtype = sum.dtype
+            if sum.dtype.kind == 'M':
+                sum = sum.view('uint64')
+                count = count.view('uint64')
+            with np.errstate(divide='ignore', invalid='ignore'):
+                mean = sum / count
+            if dtype.kind != mean.dtype.kind:
+                # TODO: not sure why view does not work
+                mean = mean.astype(dtype)
+            return mean
+
+        return finish(task_sum, task_count)
+
+
+class AggregatorDescriptorVar(AggregatorDescriptorMulti):
+    def __init__(self, name, expression, short_name="var", ddof=0, selection=None, edges=False):
+        super(AggregatorDescriptorVar, self).__init__(name, expression, short_name, selection=selection, edges=edges)
+        self.ddof = ddof
+
+    def add_operations(self, agg_task, **kwargs):
+        expression_sum = expression = agg_task.df[str(self.expression)]
+        expression = expression_sum = expression.astype('float64')
+        sum_moment = _sum_moment(str(expression_sum), 2, selection=self.selection, edges=self.edges)
+        sum_ = sum(str(expression_sum), selection=self.selection, edges=self.edges)
+        count_ = count(str(expression), selection=self.selection, edges=self.edges)
+
+        task_sum_moment = sum_moment.add_operations(agg_task, **kwargs)
+        task_sum = sum_.add_operations(agg_task, **kwargs)
+        task_count = count_.add_operations(agg_task, **kwargs)
+        self.dtype_in = sum_.dtype_in
+        self.dtype_out = sum_.dtype_out
+        @vaex.delayed
+        def finish(sum_moment, sum, count):
+            sum = np.array(sum)
+            dtype = sum.dtype
+            if sum.dtype.kind == 'M':
+                sum = sum.view('uint64')
+                sum_moment = sum_moment.view('uint64')
+                count = count.view('uint64')
+            with np.errstate(divide='ignore', invalid='ignore'):
+                mean = sum / count
+                raw_moments2 = sum_moment/count
+                variance = (raw_moments2 - mean**2) #* count/(count-self.ddof)
+            if dtype.kind != mean.dtype.kind:
+                # TODO: not sure why view does not work
+                variance = variance.astype(dtype)
+            return self.finish(variance)
+        return finish(task_sum_moment, task_sum, task_count)
+
+
+class AggregatorDescriptorStd(AggregatorDescriptorVar):
+    def finish(self, value):
+        return value**0.5
+
+@register
+def count(expression='*', selection=None, edges=False):
+    '''Creates a count aggregation'''
+    return AggregatorDescriptorBasic('AggCount', expression, 'count', selection=selection, edges=edges)
+
+@register
+def sum(expression, selection=None, edges=False):
+    '''Creates a sum aggregation'''
+    return AggregatorDescriptorBasic('AggSum', expression, 'sum', selection=selection, edges=edges)
+
+@register
+def mean(expression, selection=None, edges=False):
+    '''Creates a mean aggregation'''
+    return AggregatorDescriptorMean('mean', expression, 'mean', selection=selection, edges=edges)
+
+@register
+def min(expression, selection=None, edges=False):
+    '''Creates a min aggregation'''
+    return AggregatorDescriptorBasic('AggMin', expression, 'min', selection=selection, edges=edges)
+
+@register
+def _sum_moment(expression, moment, selection=None, edges=False):
+    '''Creates a sum of moment aggregator'''
+    return AggregatorDescriptorBasic('AggSumMoment', expression, '_sum_moment', agg_args=[moment], selection=selection, edges=edges)
+
+@register
+def max(expression, selection=None, edges=False):
+    '''Creates a max aggregation'''
+    return AggregatorDescriptorBasic('AggMax', expression, 'max', selection=selection, edges=edges)
+
+@register
+def first(expression, order_expression, selection=None, edges=False):
+    '''Creates a max aggregation'''
+    return AggregatorDescriptorBasic('AggFirst', [expression, order_expression], 'first', multi_args=True, selection=selection, edges=edges)
+
+@register
+def std(expression, ddof=0, selection=None, edges=False):
+    '''Creates a standard deviation aggregation'''
+    return AggregatorDescriptorStd('std', expression, 'std', ddof=ddof, selection=selection, edges=edges)
+
+@register
+def var(expression, ddof=0, selection=None, edges=False):
+    '''Creates a variance aggregation'''
+    return AggregatorDescriptorVar('var', expression, 'var', ddof=ddof, selection=selection, edges=edges)
+
+@register
+def nunique(expression, dropna=False, dropnan=False, dropmissing=False, selection=None, edges=False):
+    """Aggregator that calculates the number of unique items per bin.
+
+    :param expression: Expression for which to calculate the unique items
+    :param dropmissing: do not count missing values
+    :param dropnan: do not count nan values
+    :param dropna: short for any of the above, (see :func:`Expression.isna`)
+    """
+    if dropna:
+        dropnan = True
+        dropmissing = True
+    return AggregatorDescriptorNUnique('AggNUnique', expression, 'nunique', dropmissing, dropnan, selection=selection, edges=edges)
+
+# @register
+# def covar(x, y):
+#     '''Creates a standard deviation aggregation'''
+#     return _Statistic('covar', x, y)
+
+# @register
+# def correlation(x, y):
+#     '''Creates a standard deviation aggregation'''
+#     return _Statistic('correlation', x, y)
+
---
+++ packages/vaex-core/vaex_core-4.1.0-cp37-cp37m-win_amd64.whl/vaex/scopes.py
@@ -0,0 +1,270 @@
+from __future__ import division, print_function
+import logging
+import numpy as np
+import pyarrow as pa
+import vaex.array_types
+import vaex.arrow.numpy_dispatch
+
+
+from .utils import (_ensure_strings_from_expressions,
+    _ensure_string_from_expression,
+    _ensure_list,
+    _is_limit,
+    _isnumber,
+    _issequence,
+    _is_string,
+    _parse_reduction,
+    _parse_n,
+    _normalize_selection_name,
+    _normalize,
+    _parse_f,
+    _expand,
+    _expand_shape,
+    _expand_limits,
+    as_flat_float,
+    as_flat_array,
+    _split_and_combine_mask)
+from .expression import expression_namespace
+from vaex.arrow.numpy_dispatch import wrap, unwrap
+import vaex.expression
+
+logger = logging.getLogger('vaex.scopes')
+
+
+class ScopeBase(object):
+    def get(self, attr, default=None):  # otherwise pdb crashes during pytest
+        if attr == "__tracebackhide__":
+            return False
+        return default
+
+
+class UnitScope(ScopeBase):
+    def __init__(self, df, value=None):
+        self.df = df
+        self.value = value
+
+    def __getitem__(self, variable):
+        import astropy.units
+        if variable in self.df.units:
+            unit = self.df.units[variable]
+            return (self.value * unit) if self.value is not None else unit
+        elif variable in self.df.virtual_columns:
+            return eval(self.df.virtual_columns[variable], expression_namespace, self)
+        elif variable in self.df.variables:
+            return astropy.units.dimensionless_unscaled  # TODO units for variables?
+        else:
+            raise KeyError("unkown variable %s" % variable)
+
+
+class _BlockScope(ScopeBase):
+    def __init__(self, df, i1, i2, mask=None, **variables):
+        """
+
+        :param DataFrameLocal DataFrame: the *local*  DataFrame
+        :param i1: start index
+        :param i2: end index
+        :param values:
+        :return:
+        """
+        self.df = df
+        self.i1 = int(i1)
+        self.i2 = int(i2)
+        self.variables = variables
+        self.values = dict(self.variables)
+        self.buffers = {}
+        self.mask = mask if mask is not None else None
+
+    def move(self, i1, i2):
+        length_new = i2 - i1
+        length_old = self.i2 - self.i1
+        if length_new > length_old:  # old buffers are too small, discard them
+            self.buffers = {}
+        else:
+            for name in list(self.buffers.keys()):
+                self.buffers[name] = self.buffers[name][:length_new]
+        self.i1 = int(i1)
+        self.i2 = int(i2)
+        self.values = dict(self.variables)
+
+    def __contains__(self, name):  # otherwise pdb crashes during pytest
+        return name in self.buffers  # not sure this should also include varibles, columns and virtual columns
+
+    def _ensure_buffer(self, column):
+        if column not in self.buffers:
+            logger.debug("creating column for: %s", column)
+            self.buffers[column] = np.zeros(self.i2 - self.i1)
+
+    def evaluate(self, expression, out=None):
+        if isinstance(expression, vaex.expression.Expression):
+            expression = expression.expression
+        try:
+            # logger.debug("try avoid evaluating: %s", expression)
+            result = self[expression]
+        except KeyError:
+            # logger.debug("no luck, eval: %s", expression)
+            # result = ne.evaluate(expression, local_dict=self, out=out)
+            # logger.debug("in eval")
+            # eval("def f(")
+            result = eval(expression, expression_namespace, self)
+            self.values[expression] = wrap(result)
+            # if out is not None:
+            #   out[:] = result
+            #   result = out
+            # logger.debug("out eval")
+        # logger.debug("done with eval of %s", expression)
+        result = unwrap(result)
+        return result
+
+    def __getitem__(self, variable):
+        # logger.debug("get " + variable)
+        # return self.df.columns[variable][self.i1:self.i2]
+        if variable == 'df':
+            return self  # to support df['no!identifier']
+        if variable in expression_namespace:
+            return expression_namespace[variable]
+        try:
+            if variable in self.values:
+                return self.values[variable]
+            elif variable in self.df.columns:
+                offset = self.df._index_start
+                # if self.df._needs_copy(variable):
+                    # self._ensure_buffer(variable)
+                    # self.values[variable] = self.buffers[variable] = self.df.columns[variable][self.i1:self.i2].astype(np.float64)
+                    # Previously we casted anything to .astype(np.float64), this led to rounding off of int64, when exporting
+                    # self.values[variable] = self.df.columns[variable][offset+self.i1:offset+self.i2][:]
+                # else:
+                values = self.df.columns[variable][offset+self.i1:offset+self.i2]
+                if self.mask is not None:
+                    # TODO: we may want to put this in array_types
+                    if isinstance(values, (pa.Array, pa.ChunkedArray)):
+                        values = values.filter(vaex.array_types.to_arrow(self.mask))
+                    else:
+                        values = values[self.mask]
+                self.values[variable] = wrap(values)
+            elif variable in list(self.df.virtual_columns.keys()):
+                expression = self.df.virtual_columns[variable]
+                if isinstance(expression, dict):
+                    function = expression['function']
+                    arguments = [self.evaluate(k) for k in expression['arguments']]
+                    self.values[variable] = wrap(function(*arguments))
+                else:
+                    # self._ensure_buffer(variable)
+                    values = self.evaluate(expression)
+                    self.values[variable] = wrap(values)
+                    # self.values[variable] = self.buffers[variable]
+            elif variable in self.df.functions:
+                f = self.df.functions[variable].f
+                return vaex.arrow.numpy_dispatch.autowrapper(f)
+            if variable not in self.values:
+                raise KeyError("Unknown variables or column: %r" % (variable,))
+
+            return self.values[variable]
+        except:
+            # logger.exception("error in evaluating: %r" % variable)
+            raise
+
+
+class _BlockScopeSelection(ScopeBase):
+    def __init__(self, df, i1, i2, selection=None, cache=False, filter_mask=None):
+        self.df = df
+        self.i1 = i1
+        self.i2 = i2
+        self.selection = selection
+        self.store_in_cache = cache
+        self.filter_mask = filter_mask
+
+    def evaluate(self, expression):
+        if expression is True:
+            expression = "default"
+        try:
+            expression = _ensure_string_from_expression(expression)
+            result = eval(expression, expression_namespace, self)
+        except:
+            import traceback as tb
+            tb.print_stack()
+            raise
+        result = unwrap(result)
+        return result
+
+    def __contains__(self, name):  # otherwise pdb crashes during pytest
+        return False
+
+    def __getitem__(self, variable):
+        if variable == "__tracebackhide__":  # required for tracebacks
+            return False
+        if variable == 'df':
+            return self  # to support df['no!identifier']
+        # logger.debug("getitem for selection: %s", variable)
+        try:
+            selection = self.selection
+            if selection is None and self.df.has_selection(variable):
+                selection = self.df.get_selection(variable)
+            # logger.debug("selection for %r: %s %r", variable, selection, self.df.selection_histories)
+            key = (self.i1, self.i2)
+            if selection:
+                assert variable in self.df._selection_masks, "%s mask not found" % (variable, )
+                cache = self.df._selection_mask_caches[variable]
+                # logger.debug("selection cache: %r" % cache)
+                full_mask = self.df._selection_masks[variable]
+                selection_in_cache, mask = cache.get(key, (None, None))
+
+                # logger.debug("mask for %r is %r", variable, mask)
+                if selection_in_cache == selection:
+                    if self.filter_mask is not None:
+                        return wrap(mask[self.filter_mask])
+                    return wrap(mask)
+                # logger.debug("was not cached")
+                if variable in self.df.variables:
+                    return wrap(self.df.variables[variable])
+                mask_values = selection.evaluate(self.df, variable, self.i1, self.i2, self.filter_mask)
+                    
+                # get a view on a subset of the mask
+                sub_mask = full_mask.view(self.i1, self.i2)
+                sub_mask_array = np.asarray(sub_mask)
+                # and update it
+                if self.filter_mask is not None:  # if we have a mask, the selection we evaluated is also filtered
+                    sub_mask_array[:] = 0
+                    sub_mask_array[:][self.filter_mask] = mask_values
+                else:
+                    sub_mask_array[:] = mask_values
+                # logger.debug("put selection in mask with key %r" % (key,))
+                if self.store_in_cache:
+                    cache[key] = selection, sub_mask_array
+                    # cache[key] = selection, mask_values
+                if self.filter_mask is not None:
+                    return wrap(sub_mask_array[self.filter_mask])
+                else:
+                    return wrap(sub_mask_array)
+                # return mask_values
+            else:
+                offset = self.df._index_start
+                if variable in expression_namespace:
+                    return wrap(expression_namespace[variable])
+                elif variable in self.df.columns:
+                    values = self.df.columns[variable][offset+self.i1:offset+self.i2]
+                    # TODO: we may want to put this in array_types
+                    if self.filter_mask is not None:
+                        if isinstance(values, (pa.Array, pa.ChunkedArray)):
+                            values = values.filter(vaex.array_types.to_arrow(self.filter_mask))
+                        else:
+                            values = values[self.filter_mask]
+                    return wrap(values)
+                elif variable in self.df.variables:
+                    return self.df.variables[variable]
+                elif variable in self.df.virtual_columns:
+                    expression = self.df.virtual_columns[variable]
+                    # self._ensure_buffer(variable)
+                    if expression == variable:
+                        raise ValueError(f'Recursion protection: virtual column {variable} refers to itself')
+                    values = self.evaluate(expression)  # , out=self.buffers[variable])
+                    return wrap(values)
+                elif variable in self.df.functions:
+                    f = self.df.functions[variable].f
+                    return vaex.arrow.numpy_dispatch.autowrapper(f)
+                raise KeyError("Unknown variables or column: %r" % (variable,))
+        except:
+            import traceback as tb
+            tb.print_exc()
+            logger.exception("error in evaluating: %r" % variable)
+            raise
+
