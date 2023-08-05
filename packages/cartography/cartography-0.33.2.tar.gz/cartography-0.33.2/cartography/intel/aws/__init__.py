245b7f3c81509b01e89f1d894062cdf6e9da5b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:21 +0000

    Add package cartography/0.33.2/cartography-0.33.2.tar.gz

---
+++ packages/cartography/cartography-0.33.2.tar.gz/cartography-0.33.2/cartography/intel/aws/__init__.py
@@ -0,0 +1,220 @@
+import logging
+from typing import Any
+from typing import Dict
+from typing import Iterable
+from typing import List
+
+import boto3
+import botocore.exceptions
+import neo4j
+
+from . import ec2
+from . import organizations
+from .resources import RESOURCE_FUNCTIONS
+from cartography.config import Config
+from cartography.intel.aws.util.common import parse_and_validate_aws_requested_syncs
+from cartography.util import run_analysis_job
+from cartography.util import run_cleanup_job
+from cartography.util import timeit
+
+logger = logging.getLogger(__name__)
+
+
+def _build_aws_sync_kwargs(
+    neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, regions: List[str], current_aws_account_id: str,
+    sync_tag: int, common_job_parameters: Dict[str, Any],
+) -> Dict[str, Any]:
+    return {
+        'neo4j_session': neo4j_session,
+        'boto3_session': boto3_session,
+        'regions': regions,
+        'current_aws_account_id': current_aws_account_id,
+        'update_tag': sync_tag,
+        'common_job_parameters': common_job_parameters,
+    }
+
+
+def _sync_one_account(
+    neo4j_session: neo4j.Session,
+    boto3_session: boto3.session.Session,
+    current_aws_account_id: str,
+    update_tag: int,
+    common_job_parameters: Dict[str, Any],
+    regions: List[str] = [],
+    aws_requested_syncs: Iterable[str] = RESOURCE_FUNCTIONS.keys(),
+) -> None:
+    if not regions:
+        regions = _autodiscover_account_regions(boto3_session, current_aws_account_id)
+
+    sync_args = _build_aws_sync_kwargs(
+        neo4j_session, boto3_session, regions, current_aws_account_id, update_tag, common_job_parameters,
+    )
+
+    for func_name in aws_requested_syncs:
+        if func_name in RESOURCE_FUNCTIONS:
+            # Skip permission relationships and tags for now because they rely on data already being in the graph
+            if func_name not in ['permission_relationships', 'resourcegroupstaggingapi']:
+                RESOURCE_FUNCTIONS[func_name](**sync_args)
+            else:
+                continue
+        else:
+            raise ValueError(f'AWS sync function "{func_name}" was specified but does not exist. Did you misspell it?')
+
+    # NOTE clean up all DNS records, regardless of which job created them
+    run_cleanup_job('aws_account_dns_cleanup.json', neo4j_session, common_job_parameters)
+
+    # MAP IAM permissions
+    if 'permission_relationships' in aws_requested_syncs:
+        RESOURCE_FUNCTIONS['permission_relationships'](**sync_args)
+
+    # AWS Tags - Must always be last.
+    if 'resourcegroupstaggingapi' in aws_requested_syncs:
+        RESOURCE_FUNCTIONS['resourcegroupstaggingapi'](**sync_args)
+
+
+def _autodiscover_account_regions(boto3_session: boto3.session.Session, account_id: str) -> List[str]:
+    regions: List[str] = []
+    try:
+        regions = ec2.get_ec2_regions(boto3_session)
+    except botocore.exceptions.ClientError as e:
+        logger.debug("Error occurred getting EC2 regions.", exc_info=True)
+        logger.error(
+            (
+                "Failed to retrieve AWS region list, an error occurred: %s. Could not get regions for account %s."
+            ),
+            e,
+            account_id,
+        )
+        raise
+    return regions
+
+
+def _autodiscover_accounts(
+    neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, account_id: str,
+    sync_tag: int, common_job_parameters: Dict,
+) -> None:
+    logger.info("Trying to autodiscover accounts.")
+    try:
+        # Fetch all accounts
+        client = boto3_session.client('organizations')
+        paginator = client.get_paginator('list_accounts')
+        accounts: List[Dict] = []
+        for page in paginator.paginate():
+            accounts.extend(page['Accounts'])
+
+        # Filter out every account which is not in the ACTIVE status
+        # and select only the Id and Name fields
+        filtered_accounts: Dict[str, str] = {x['Name']: x['Id'] for x in accounts if x['Status'] == 'ACTIVE'}
+
+        # Add them to the graph
+        logger.info("Loading autodiscovered accounts.")
+        organizations.load_aws_accounts(neo4j_session, filtered_accounts, sync_tag, common_job_parameters)
+    except botocore.exceptions.ClientError:
+        logger.warning(f"The current account ({account_id}) doesn't have enough permissions to perform autodiscovery.")
+
+
+def _sync_multiple_accounts(
+    neo4j_session: neo4j.Session,
+    accounts: Dict[str, str],
+    sync_tag: int,
+    common_job_parameters: Dict[str, Any],
+    aws_requested_syncs: List[str] = [],
+) -> None:
+    logger.debug("Syncing AWS accounts: %s", ', '.join(accounts.values()))
+    organizations.sync(neo4j_session, accounts, sync_tag, common_job_parameters)
+
+    for profile_name, account_id in accounts.items():
+        logger.info("Syncing AWS account with ID '%s' using configured profile '%s'.", account_id, profile_name)
+        common_job_parameters["AWS_ID"] = account_id
+        boto3_session = boto3.Session(profile_name=profile_name)
+
+        _autodiscover_accounts(neo4j_session, boto3_session, account_id, sync_tag, common_job_parameters)
+
+        _sync_one_account(
+            neo4j_session,
+            boto3_session,
+            account_id,
+            sync_tag,
+            common_job_parameters,
+            aws_requested_syncs=aws_requested_syncs,  # Could be replaced later with per-account requested syncs
+        )
+
+    del common_job_parameters["AWS_ID"]
+
+    # There may be orphan Principals which point outside of known AWS accounts. This job cleans
+    # up those nodes after all AWS accounts have been synced.
+    run_cleanup_job('aws_post_ingestion_principals_cleanup.json', neo4j_session, common_job_parameters)
+
+    # There may be orphan DNS entries that point outside of known AWS zones. This job cleans
+    # up those entries after all AWS accounts have been synced.
+    run_cleanup_job('aws_post_ingestion_dns_cleanup.json', neo4j_session, common_job_parameters)
+
+
+@timeit
+def start_aws_ingestion(neo4j_session: neo4j.Session, config: Config) -> None:
+    common_job_parameters = {
+        "UPDATE_TAG": config.update_tag,
+        "permission_relationships_file": config.permission_relationships_file,
+    }
+    try:
+        boto3_session = boto3.Session()
+    except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+        logger.debug("Error occurred calling boto3.Session().", exc_info=True)
+        logger.error(
+            (
+                "Unable to initialize the default AWS session, an error occurred: %s. Make sure your AWS credentials "
+                "are configured correctly, your AWS config file is valid, and your credentials have the SecurityAudit "
+                "policy attached."
+            ),
+            e,
+        )
+        return
+
+    if config.aws_sync_all_profiles:
+        aws_accounts = organizations.get_aws_accounts_from_botocore_config(boto3_session)
+    else:
+        aws_accounts = organizations.get_aws_account_default(boto3_session)
+
+    if not aws_accounts:
+        logger.warning(
+            "No valid AWS credentials could be found. No AWS accounts can be synced. Exiting AWS sync stage.",
+        )
+        return
+    if len(list(aws_accounts.values())) != len(set(aws_accounts.values())):
+        logger.warning(
+            (
+                "There are duplicate AWS accounts in your AWS configuration. It is strongly recommended that you run "
+                "cartography with an AWS configuration which has exactly one profile for each AWS account you want to "
+                "sync. Doing otherwise will result in undefined and untested behavior."
+            ),
+        )
+
+    requested_syncs: List[str] = list(RESOURCE_FUNCTIONS.keys())
+    if config.aws_requested_syncs:
+        requested_syncs = parse_and_validate_aws_requested_syncs(config.aws_requested_syncs)
+
+    _sync_multiple_accounts(
+        neo4j_session,
+        aws_accounts,
+        config.update_tag,
+        common_job_parameters,
+        requested_syncs,
+    )
+
+    run_analysis_job(
+        'aws_ec2_asset_exposure.json',
+        neo4j_session,
+        common_job_parameters,
+    )
+
+    run_analysis_job(
+        'aws_ec2_keypair_analysis.json',
+        neo4j_session,
+        common_job_parameters,
+    )
+
+    run_analysis_job(
+        'aws_eks_asset_exposure.json',
+        neo4j_session,
+        common_job_parameters,
+    )
---
+++ packages/cartography/cartography-0.33.2.tar.gz/cartography-0.33.2/cartography/intel/aws/ecr.py
@@ -0,0 +1,157 @@
+import logging
+from typing import Dict
+from typing import List
+
+import boto3
+import neo4j
+
+from cartography.util import aws_handle_regions
+from cartography.util import run_cleanup_job
+from cartography.util import timeit
+
+logger = logging.getLogger(__name__)
+
+
+@timeit
+@aws_handle_regions
+def get_ecr_repositories(boto3_session: boto3.session.Session, region: str) -> List[Dict]:
+    logger.debug("Getting ECR repositories for region '%s'.", region)
+    client = boto3_session.client('ecr', region_name=region)
+    paginator = client.get_paginator('describe_repositories')
+    ecr_repositories: List[Dict] = []
+    for page in paginator.paginate():
+        ecr_repositories.extend(page['repositories'])
+    return ecr_repositories
+
+
+@timeit
+@aws_handle_regions
+def get_ecr_repository_images(boto3_session: boto3.session.Session, region: str, repository_name: str) -> List[Dict]:
+    logger.debug("Getting ECR images in repository '%s' for region '%s'.", repository_name, region)
+    client = boto3_session.client('ecr', region_name=region)
+    paginator = client.get_paginator('list_images')
+    ecr_repository_images: List[Dict] = []
+    for page in paginator.paginate(repositoryName=repository_name):
+        ecr_repository_images.extend(page['imageIds'])
+    return ecr_repository_images
+
+
+@timeit
+def load_ecr_repositories(
+    neo4j_session: neo4j.Session, repos: List[Dict], region: str, current_aws_account_id: str,
+    aws_update_tag: int,
+) -> None:
+    query = """
+    UNWIND {Repositories} as ecr_repo
+        MERGE (repo:ECRRepository{id: ecr_repo.repositoryArn})
+        ON CREATE SET repo.firstseen = timestamp(),
+            repo.arn = ecr_repo.repositoryArn,
+            repo.name = ecr_repo.repositoryName,
+            repo.region = {Region},
+            repo.created_at = ecr_repo.createdAt
+        SET repo.lastupdated = {aws_update_tag},
+            repo.uri = ecr_repo.repositoryUri
+        WITH repo
+
+        MATCH (owner:AWSAccount{id: {AWS_ACCOUNT_ID}})
+        MERGE (owner)-[r:RESOURCE]->(repo)
+        ON CREATE SET r.firstseen = timestamp()
+        SET r.lastupdated = {aws_update_tag}
+    """
+    logger.debug("Loading ECR repositories for region '%s' into graph.", region)
+    neo4j_session.run(
+        query,
+        Repositories=repos,
+        Region=region,
+        aws_update_tag=aws_update_tag,
+        AWS_ACCOUNT_ID=current_aws_account_id,
+    ).consume()  # See issue #440
+
+
+@timeit
+def transform_ecr_repository_images(repo_data: Dict) -> List[Dict]:
+    """
+    Ensure that we only load ECRImage nodes to the graph if they have a defined imageDigest field.
+    """
+    repo_images_list = []
+    for repo_uri, repo_images in repo_data.items():
+        filtered_imgs = []
+        for img in repo_images:
+            if 'imageDigest' in img and img['imageDigest']:
+                filtered_imgs.append(img)
+            else:
+                logger.warning(
+                    "Repo %s has an image that has no imageDigest. Its tag is %s. Continuing on.",
+                    repo_uri,
+                    img.get('imageTag'),
+                )
+
+        repo_images_list.append({
+            'repo_uri': repo_uri,
+            'repo_images': filtered_imgs,
+        })
+    return repo_images_list
+
+
+@timeit
+def load_ecr_repository_images(
+        neo4j_session: neo4j.Session, repo_images_list: List[Dict], region: str, aws_update_tag: int,
+) -> None:
+    query = """
+    UNWIND {RepoList} as repo_item
+        UNWIND repo_item.repo_images as repo_img
+            MERGE (ri:ECRRepositoryImage{id: repo_item.repo_uri + COALESCE(":" + repo_img.imageTag, '')})
+            ON CREATE SET ri.firstseen = timestamp()
+            SET ri.lastupdated = {aws_update_tag},
+                ri.tag = repo_img.imageTag,
+                ri.uri = repo_item.repo_uri + COALESCE(":" + repo_img.imageTag, '')
+            WITH ri, repo_img, repo_item
+
+            MERGE (img:ECRImage{id: repo_img.imageDigest})
+            ON CREATE SET img.firstseen = timestamp(),
+                img.digest = repo_img.imageDigest
+            SET img.lastupdated = {aws_update_tag},
+                img.region = {Region}
+            WITH ri, img, repo_item
+
+            MERGE (ri)-[r1:IMAGE]->(img)
+            ON CREATE SET r1.firstseen = timestamp()
+            SET r1.lastupdated = {aws_update_tag}
+            WITH ri, repo_item
+
+            MATCH (repo:ECRRepository{uri: repo_item.repo_uri})
+            MERGE (repo)-[r2:REPO_IMAGE]->(ri)
+            ON CREATE SET r2.firstseen = timestamp()
+            SET r2.lastupdated = {aws_update_tag}
+    """
+    logger.debug("Loading ECR repository images for region '%s' into graph.", region)
+    neo4j_session.run(
+        query,
+        RepoList=repo_images_list,
+        aws_update_tag=aws_update_tag,
+        Region=region,
+    ).consume()  # See issue #440
+
+
+@timeit
+def cleanup(neo4j_session: neo4j.Session, common_job_parameters: Dict) -> None:
+    logger.debug("Running ECR cleanup job.")
+    run_cleanup_job('aws_import_ecr_cleanup.json', neo4j_session, common_job_parameters)
+
+
+@timeit
+def sync(
+    neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, regions: List[str], current_aws_account_id: str,
+    update_tag: int, common_job_parameters: Dict,
+) -> None:
+    for region in regions:
+        logger.info("Syncing ECR for region '%s' in account '%s'.", region, current_aws_account_id)
+        image_data = {}
+        repositories = get_ecr_repositories(boto3_session, region)
+        for repo in repositories:
+            repo_image_obj = get_ecr_repository_images(boto3_session, region, repo['repositoryName'])
+            image_data[repo['repositoryUri']] = repo_image_obj
+        load_ecr_repositories(neo4j_session, repositories, region, current_aws_account_id, update_tag)
+        repo_images_list = transform_ecr_repository_images(image_data)
+        load_ecr_repository_images(neo4j_session, repo_images_list, region, update_tag)
+    cleanup(neo4j_session, common_job_parameters)
