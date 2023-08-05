0987d9e379ed0adaac10b2ef19950d0a4a392ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package speech-synthesis/1.0/speech-synthesis-1.0.tar.gz

---
+++ packages/speech-synthesis/speech-synthesis-1.0.tar.gz/speech-synthesis-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="speech-synthesis",  # 报名
+      version="1.0",  # 版本号
+      description="使用百度AI语音合成",  # 描述信息
+      long_description="语音合成包",  # 完整描述信息
+      author="haxif",  # 作者
+      author_email="1160370538@qq.com",  # 作者的邮箱
+      # url="https://",  # 作者的主页
+      py_modules=["speechSynthesis"])
