65d6f89fb838615d5591864993cd522a1f75128f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package opensitua-database/0.0.110/opensitua_database-0.0.110.tar.gz

---
+++ packages/opensitua-database/opensitua_database-0.0.110.tar.gz/opensitua_database-0.0.110/opensitua_database/__init__.py
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2019 Valerio for Gecosistema S.r.l.
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Name:        opensitua_database
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:    08/07/2019
+#-------------------------------------------------------------------------------
+import pkg_resources
+
+def get_version():
+    return pkg_resources.get_distribution(PACKAGE_NAME)
+
+
+from .sqlitedb import *
+from .postgresdb import *
+from .usersdb import *
+from .settingsdb import *
+from .http_utils import *
+from .jobsdb import *
