fa0c4700a82b332631ea798865e519903cc52619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package pyams-zmq/1.1.1/pyams_zmq-1.1.1.tar.gz

---
+++ packages/pyams-zmq/pyams_zmq-1.1.1.tar.gz/pyams_zmq-1.1.1/docs/README.txt
@@ -0,0 +1,26 @@
+===========================
+PyAMS ZeroMQ helper package
+===========================
+
+.. contents::
+
+
+What is PyAMS?
+==============
+
+PyAMS (Pyramid Application Management Suite) is a small suite of packages written for applications
+and content management with the Pyramid framework.
+
+**PyAMS** is actually mainly used to manage web sites through content management applications (CMS,
+see PyAMS_content package), but many features are generic and can be used inside any kind of web
+application.
+
+All PyAMS documentation is available on `ReadTheDocs <https://pyams.readthedocs.io>`_; source code
+is available on `Gitlab <https://gitlab.com/pyams>`_ and pushed to `Github
+<https://github.com/py-ams>`_. Doctests are available in the *doctests* source folder.
+
+
+What is PyAMS ZeroMQ helper package?
+====================================
+
+PyAMS_zmq is a utility package which is used to build processes handling ZeroMQ protocol.
---
+++ packages/pyams-zmq/pyams_zmq-1.1.1.tar.gz/pyams_zmq-1.1.1/setup.py
@@ -0,0 +1,70 @@
+#
+# Copyright (c) 2015-2019 Thierry Florac <tflorac AT ulthar.net>
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+
+"""
+This module contains PyAMS ZeroMQ helper package package
+"""
+import os
+from setuptools import setup, find_packages
+
+
+DOCS = os.path.join(os.path.dirname(__file__),
+                    'docs')
+
+README = os.path.join(DOCS, 'README.txt')
+HISTORY = os.path.join(DOCS, 'HISTORY.txt')
+
+version = '1.1.1'
+long_description = open(README).read() + '\n\n' + open(HISTORY).read()
+
+tests_require = [
+    'pyramid_zcml',
+    'zope.exceptions'
+]
+
+setup(name='pyams_zmq',
+      version=version,
+      description="PyAMS ZeroMQ helper package",
+      long_description=long_description,
+      classifiers=[
+          "License :: OSI Approved :: Zope Public License",
+          "Development Status :: 4 - Beta",
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords='Pyramid PyAMS ZeroMQ',
+      author='Thierry Florac',
+      author_email='tflorac@ulthar.net',
+      url='https://pyams.readthedocs.io',
+      license='ZPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      package_data={'': ['*.zcml', '*.txt', '*.pt', '*.pot', '*.po', '*.mo',
+                         '*.png', '*.gif', '*.jpeg', '*.jpg', '*.css', '*.js']},
+      zip_safe=False,
+      # uncomment this to be able to run tests with setup.py
+      test_suite="pyams_zmq.tests.test_utilsdocs.test_suite",
+      tests_require=tests_require,
+      extras_require=dict(test=tests_require),
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'pyams_utils',
+          'pyramid',
+          'pyzmq',
+          'tornado',
+          'zope.interface'
+      ],
+      entry_points="")
