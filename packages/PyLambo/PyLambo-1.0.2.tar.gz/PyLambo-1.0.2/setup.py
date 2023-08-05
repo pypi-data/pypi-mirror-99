33c90e0a6b86b7f0dd402767034c5cf23a815037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package PyLambo/1.0.2/PyLambo-1.0.2.tar.gz

---
+++ packages/PyLambo/PyLambo-1.0.2.tar.gz/PyLambo-1.0.2/setup.py
@@ -0,0 +1,36 @@
+"""Setup script for PyLambo"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="PyLambo",
+    version="1.0.2",
+    description="Read the latest Real Python tutorials",
+    long_description="Something",
+    long_description_content_type="text/markdown",
+    url="https://github.com/realpython/reader",
+    author="Real Python",
+    author_email="info@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=[
+        "feedparser", "html2text", "importlib_resources", "typing"
+    ],
+    entry_points={"console_scripts": ["realpython=reader.__main__:main"]},
+)
