41377e32439ce52e0e1b41eebf5d12349f440639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package pyiron/0.4.3/pyiron-0.4.3.tar.gz

---
+++ packages/pyiron/pyiron-0.4.3.tar.gz/pyiron-0.4.3/setup.py
@@ -0,0 +1,35 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='pyiron',
+    version=versioneer.get_version(),
+    description='pyiron - an integrated development environment (IDE) for computational materials science.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='janssen@mpie.de',
+    license='BSD',
+
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9'],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'pyiron_atomistics>=0.2.8'
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+
+    )
