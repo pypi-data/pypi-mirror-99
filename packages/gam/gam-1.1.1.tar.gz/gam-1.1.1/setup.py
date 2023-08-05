59c76e9d3e9cdcfc2a47d0a6fc3f5c06e5870e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package gam/1.1.1/gam-1.1.1.tar.gz

---
+++ packages/gam/gam-1.1.1.tar.gz/gam-1.1.1/setup.py
@@ -0,0 +1,51 @@
+# SPDX-Copyright: Copyright (c) Capital One Services, LLC
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2021 Capital One Services, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from io import open
+
+from setuptools import setup
+
+CURR_DIR = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(CURR_DIR, "README.md"), encoding="utf-8") as file_open:
+    LONG_DESCRIPTION = file_open.read()
+
+DESCRIPTION = "Global Explanations for Deep Neural Networks"
+
+setup(
+    name="gam",
+    version="1.1.1",
+    packages=["gam", "tests"],
+    maintainer="Brian Barr",
+    maintainer_email="brian.barr@capitalone.com",
+    url="https://github.com/capitalone/global-attribution-mapping",
+    classifiers=["Programming Language :: Python :: 3"],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="Apache License 2.0",
+    install_requires=[
+        "dask[complete] == 2021.2.0",
+        "dask-ml == 1.8.0",
+        "matplotlib > 2.2.0",
+        "pandas > 0.25.1",
+        "scikit-learn > 0.21.3",
+        "numpy > 1.17.2",
+        "dask > 2021.1.0",
+    ],
+    python_requires=">=3.6",
+)
---
+++ packages/gam/gam-1.1.1.tar.gz/gam-1.1.1/tests/test_spearman_distance.py
@@ -0,0 +1,76 @@
+import numpy as np
+import dask.array as da
+from dask.distributed import Client
+
+from gam.spearman_distance import (pairwise_spearman_distance_matrix,
+                                   spearman_squared_distance,
+                                   spearman_squared_distance_legacy)
+
+
+def test_spearman_symmetry():
+    """Checks symmetry holds for distance metric"""
+    r1 = np.array([0.1, 0.2, 0.7])
+    r2 = np.array([0.6, 0.1, 0.3])
+    assert spearman_squared_distance(r1, r1) == 0
+
+    d1 = spearman_squared_distance(r1, r2)
+    d2 = spearman_squared_distance(r2, r1)
+    assert d1 == d2
+
+
+def test_spearman_relative_distances():
+    """Compares relative distances of weighted rankings"""
+    r1 = np.array([0.05, 0.2, 0.7, 0.05])
+    r2 = np.array([0.23, 0.24, 0.26, 0.27])
+    r3 = np.array([0.22, 0.24, 0.26, 0.28])
+    # r2 and r3 should be closer than r2 to r1
+    assert spearman_squared_distance(r2, r3) < spearman_squared_distance(r2, r1)
+
+
+def test_pairwise_distance_matrix():
+    r1 = np.array([0.05, 0.2, 0.7, 0.05])
+    r2 = np.array([0.23, 0.24, 0.26, 0.27])
+    r3 = np.array([0.22, 0.24, 0.26, 0.28])
+
+    rankings = np.array([r1, r2, r3])
+    D = pairwise_spearman_distance_matrix(rankings)
+
+    # check symmetry, within floating point rounding margin
+    assert (D[0][1] - D[1][0]) < 1e-9
+    # check diagonal is zero
+    assert D[1][1] == 0
+    assert D[2][2] == 0
+    # distance between r2 and r3 is closer than r2 and r1
+    assert D[1][2] < D[1][0]
+
+
+def test_spearman_accuracy():
+    """ Floating point accuracy test for testing faster calculation methods """
+    r1 = np.array([0.27, 0.24, 0.26, 0.23])
+    r2 = np.array([0.05, 0.2, 0.7, 0.05])
+
+    d1 = spearman_squared_distance(r1, r2)
+    d2 = spearman_squared_distance_legacy(r2, r1)
+    assert d1 == 363.37999999999994
+    assert d2 == 363.37999999999994
+
+
+def test_dask_pairwise_distance_matrix():
+    r1 = np.array([0.05, 0.2, 0.7, 0.05])
+    r2 = np.array([0.23, 0.24, 0.26, 0.27])
+    r3 = np.array([0.22, 0.24, 0.26, 0.28])
+
+    rankings = np.array([r1, r2, r3])
+    # Testing dask
+    client = Client()
+    D = pairwise_spearman_distance_matrix(da.from_array(rankings))
+
+    # check symmetry, within floating point rounding margin
+    assert (D[0][1] - D[1][0]) < 1e-9
+    # check diagonal is zero
+    assert D[1][1] == 0
+    assert D[2][2] == 0
+    # distance between r2 and r3 is closer than r2 and r1
+    assert D[1][2] < D[1][0]
+    client.close()
+
