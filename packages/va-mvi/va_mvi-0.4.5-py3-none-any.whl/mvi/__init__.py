3cecc81298d902be80371306d9360dbc2f93532b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package va-mvi/0.4.5/va_mvi-0.4.5-py3-none-any.whl

---
+++ packages/va-mvi/va_mvi-0.4.5-py3-none-any.whl/mvi/__init__.py
@@ -0,0 +1,9 @@
+from mvi import communication, utilities, testing
+from mvi._service import SERVICE as service
+
+__all__ = [
+    "service",
+    "communication",
+    "utilities",
+    "testing",
+]
---
+++ packages/va-mvi/va_mvi-0.4.5-py3-none-any.whl/mvi/_service/__init__.py
@@ -0,0 +1,7 @@
+from mvi._service.service import _Service
+
+SERVICE = _Service()
+
+__all__ = [
+    "SERVICE",
+]
