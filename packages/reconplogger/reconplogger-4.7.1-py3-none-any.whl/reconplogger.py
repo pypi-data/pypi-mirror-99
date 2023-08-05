de26779a4364607ffbfb633cac2cc920eebc6b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package reconplogger/4.7.1/reconplogger-4.7.1-py3-none-any.whl

---
+++ packages/reconplogger/reconplogger-4.7.1-py3-none-any.whl/reconplogger.py
@@ -0,0 +1,384 @@
+
+import os
+import yaml
+import logging
+import logging.config
+from logging import CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET
+from typing import Optional, Union
+import uuid
+import sys
+import time
+
+
+__version__ = '4.7.1'
+
+
+try:
+    # If flask is installed import the request context objects
+    from flask import request, g, has_request_context
+    # If requests is installed patch the calls to add the correlation id
+    import requests
+    def _request_patch(slf, *args, **kwargs):
+        headers = kwargs.pop('headers', {})
+        if has_request_context():
+            headers["Correlation-ID"] = g.correlation_id
+        return slf.request_orig(*args, **kwargs, headers=headers)
+    setattr(requests.sessions.Session, 'request_orig', requests.sessions.Session.request)
+    requests.sessions.Session.request = _request_patch
+except:
+    pass
+
+reconplogger_format = '%(asctime)s\t%(levelname)s -- %(filename)s:%(lineno)s -- %(message)s'
+
+reconplogger_default_cfg = {
+    'version': 1,
+    'formatters': {
+        'plain': {
+            'format': reconplogger_format,
+        },
+        'json': {
+            'format': reconplogger_format,
+            'class': 'logmatic.jsonlogger.JsonFormatter'
+        },
+    },
+    'handlers': {
+        'plain_handler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'plain',
+            'level': 'WARNING',
+        },
+        'json_handler': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'json',
+            'level': 'WARNING',
+        },
+        'null_handler': {
+            'class': 'logging.NullHandler',
+        }
+    },
+    'loggers': {
+        'plain_logger': {
+            'level': 'DEBUG',
+            'handlers': ['plain_handler'],
+        },
+        'json_logger': {
+            'level': 'DEBUG',
+            'handlers': ['json_handler'],
+        },
+        'null_logger': {
+            'handlers': ['null_handler'],
+        },
+    },
+}
+
+logging_levels = {
+    'CRITICAL': CRITICAL,
+    'ERROR':    ERROR,
+    'WARNING':  WARNING,
+    'INFO':     INFO,
+    'DEBUG':    DEBUG,
+    'NOTSET':   NOTSET,
+}
+logging_levels.update({v: v for v in logging_levels})
+
+null_logger = logging.Logger('null')
+null_logger.addHandler(logging.NullHandler())
+
+
+def load_config(cfg: Optional[Union[str, dict]] = None):
+    """Loads a logging configuration from path or environment variable or dictionary object.
+
+    Args:
+        cfg: Path to configuration file (json|yaml), or name of environment variable (json|yaml) or configuration object or None/"reconplogger_default_cfg" to use default configuration.
+
+    Returns:
+        The logging package object.
+    """
+    if cfg is None or cfg in {'', 'reconplogger_default_cfg'} or (cfg in os.environ and os.environ[cfg] == 'reconplogger_default_cfg'):
+        cfg_dict = reconplogger_default_cfg
+    elif isinstance(cfg, dict):
+        cfg_dict = cfg
+    elif isinstance(cfg, str):
+        try:
+            if os.path.isfile(cfg):
+                with open(cfg, 'r') as f:
+                    cfg_dict = yaml.safe_load(f.read())
+            elif cfg in os.environ:
+                cfg_dict = yaml.safe_load(os.environ[cfg])
+            else:
+                try:
+                    cfg_dict = yaml.safe_load(cfg)
+                    if not isinstance(cfg_dict, dict):
+                        raise ValueError
+                except Exception:
+                    raise ValueError
+        except Exception:
+            raise ValueError(
+                'Received string which is neither a path to an existing file nor the name of an set environment variable nor a python dictionary string that can be consumed by logging.config.dictConfig.')
+
+    cfg_dict['disable_existing_loggers'] = False
+    logging.config.dictConfig(cfg_dict)
+
+    return logging
+
+
+def replace_logger_handlers(
+    logger: Union[logging.Logger, str],
+    handlers: Union[logging.Logger, str],
+):
+    """Replaces the handlers of a given logger.
+
+    Args:
+        logger: Object or name of logger to replace handlers.
+        handlers: Object or name of logger from which to get handlers.
+    """
+    # Resolve logger
+    if isinstance(logger, str):
+        logger = get_logger(logger)
+    if not isinstance(logger, logging.Logger):
+        raise ValueError('Expected logger to be logger name or Logger object.')
+
+    # Resolve handlers
+    if isinstance(handlers, str):
+        handlers = get_logger(handlers).handlers
+    elif isinstance(handlers, logging.Logger):
+        handlers = handlers.handlers
+    else:
+        raise ValueError(
+            'Expected handlers to be list, logger name or Logger object.')
+
+    ## Replace handlers ##
+    logger.handlers = list(handlers)
+
+
+def add_file_handler(
+    logger: logging.Logger,
+    file_path: str,
+    format: str = reconplogger_format,
+    level: Optional[str] = 'DEBUG',
+):
+    """Adds a file handler to a given logger.
+
+    Args:
+        logger: Logger object where to add the file handler.
+        file_path: Path to log file for handler.
+        format: Format for logging.
+        level: Logging level for the handler.
+    """
+    fileHandler = logging.FileHandler(file_path)
+    fileHandler.setFormatter(logging.Formatter(format))
+    if level is not None:
+        if level not in logging_levels:
+            raise ValueError('Invalid logging level: "'+str(level)+'".')
+        fileHandler.setLevel(logging_levels[level])
+    logger.addHandler(fileHandler)
+
+
+def test_logger(logger: logging.Logger):
+    """Logs one message to each debug, info and warning levels intended for testing."""
+    logger.debug('reconplogger test debug message.')
+    logger.info('reconplogger test info message.')
+    logger.warning('reconplogger test warning message.')
+
+
+def get_logger(logger_name: str) -> logging.Logger:
+    """Returns an already existing logger.
+
+    Args:
+        logger_name:  Name of the logger to get.
+
+    Returns:
+        The logger object.
+
+    Raises:
+        ValueError: If the logger does not exist.
+    """
+    if logger_name not in logging.Logger.manager.loggerDict and logger_name not in logging.root.manager.loggerDict:
+        raise ValueError('Logger "'+str(logger_name)+'" not defined.')
+    return logging.getLogger(logger_name)
+
+
+def logger_setup(
+    logger_name: str = 'plain_logger',
+    config: Optional[str] = None,
+    level: Optional[str] = None,
+    env_prefix: str = 'LOGGER',
+    parent: Optional[logging.Logger] = None,
+    init_messages: bool = False,
+) -> logging.Logger:
+    """Sets up logging configuration and returns the logger.
+
+    Args:
+        logger_name:  Name of the logger that needs to be used.
+        config: Configuration string or path to configuration file or configuration file via environment variable.
+        level: Optional logging level that overrides one in config.
+        env_prefix: Environment variable names prefix for overriding logger configuration.
+        parent: Set for logging delegation to the parent.
+        init_messages: Whether to log init and test messages.
+
+    Returns:
+        The logger object.
+    """
+    if not isinstance(env_prefix, str) or not env_prefix:
+        raise ValueError('env_prefix is required to be a non-empty string.')
+    env_cfg = env_prefix + '_CFG'
+    env_name = env_prefix + '_NAME'
+    env_level = env_prefix + '_LEVEL'
+
+    # Configure logging
+    load_config(os.getenv(env_cfg, config))
+
+    # Get logger
+    logger = get_logger(os.getenv(env_name, logger_name))
+
+    # Override parent
+    logger.parent = parent
+
+    # Override log level if set
+    if env_level in os.environ:
+        level = os.getenv(env_level)
+    if level:
+        if isinstance(level, str):
+            if level not in logging_levels:
+                raise ValueError('Invalid logging level: "'+str(level)+'".')
+            level = logging_levels[level]
+        else:
+            raise ValueError('Expected level argument to be a string.')
+        for handler in logger.handlers:
+            handler.setLevel(level)
+
+    # Log configured done and test logger
+    if init_messages:
+        logger.info('reconplogger (v'+__version__+') logger configured.')
+        test_logger(logger)
+
+    return logger
+
+
+def flask_app_logger_setup(
+    flask_app,
+    logger_name: str = 'plain_logger',
+    config: Optional[str] = None,
+    level: Optional[str] = None,
+    env_prefix: str = 'LOGGER',
+    parent: Optional[logging.Logger] = None,
+) -> logging.Logger:
+    """Sets up logging configuration, configures flask to use it, and returns the logger.
+
+    Args:
+        flask_app (flask.app.Flask): The flask app object.
+        logger_name:  Name of the logger that needs to be used.
+        config: Configuration string or path to configuration file or configuration file via environment variable.
+        level: Optional logging level that overrides one in config.
+        env_prefix: Environment variable names prefix for overriding logger configuration.
+        parent: Set for logging delegation to the parent.
+
+    Returns:
+        The logger object.
+    """
+    # Configure logging and get logger
+    logger = logger_setup(logger_name=logger_name, config=config, level=level, env_prefix=env_prefix, parent=parent)
+
+    # Setup flask logger
+    replace_logger_handlers(flask_app.logger, logger)
+    flask_app.logger.setLevel(logger.level)
+
+    # Add flask before and after request functions to augment the logs
+    def _flask_logging_before_request():
+        g.correlation_id = request.headers.get("Correlation-ID", str(uuid.uuid4()))
+        g.start_time = time.time()
+    flask_app.before_request_funcs.setdefault(None, []).append(_flask_logging_before_request)
+
+    def _flask_logging_after_request(response):
+        response.headers.set("Correlation-ID", g.correlation_id)
+        flask_app.logger.info("Request is completed", extra={
+            "http_endpoint": request.path,
+            "http_method": request.method,
+            "http_response_code": response.status_code,
+            "http_response_size": sys.getsizeof(response),
+            "http_input_payload_size": request.content_length,
+            "http_input_payload_type": request.content_type,
+            "http_response_time": str(time.time() - g.start_time),
+        })
+        return response
+    flask_app.after_request_funcs.setdefault(None, []).append(_flask_logging_after_request)
+
+    # Add logging filter to augment the logs
+    class FlaskLoggingFilter(logging.Filter):
+        def filter(self, record):
+            if has_request_context():
+                record.correlation_id = g.correlation_id
+            return True
+    flask_app.logger.addFilter(FlaskLoggingFilter())
+
+    # Setup werkzeug logger
+    werkzeug_logger = logging.getLogger('werkzeug')
+    replace_logger_handlers(werkzeug_logger, logger)
+    werkzeug_logger.level = WARNING
+
+    return logger
+
+
+def get_correlation_id() -> str:
+    """Returns the current correlation id.
+
+    Raises:
+        ImportError: When flask package not available.
+        RuntimeError: When run outside an application context or if flask app has not been setup.
+    """
+    from flask import g
+    try:
+        has_correlation_id = hasattr(g, 'correlation_id')
+    except RuntimeError:
+        raise RuntimeError('get_correlation_id only intended to be used inside an application context.')
+    if not has_correlation_id:
+        raise RuntimeError('correlation_id not found in flask.g, probably flask app not yet setup.')
+    return g.correlation_id
+
+
+def set_correlation_id(correlation_id: str):
+    """Sets the correlation id for the current application context.
+
+    Raises:
+        ImportError: When flask package not available.
+        RuntimeError: When run outside an application context or if flask app has not been setup.
+    """
+    from flask import g
+    try:
+        hasattr(g, 'correlation_id')
+    except RuntimeError:
+        raise RuntimeError('set_correlation_id only intended to be used inside an application context.')
+    g.correlation_id = str(correlation_id)
+
+
+class RLoggerProperty:
+    """Class designed to be inherited by other classes to add an rlogger property."""
+
+    def __init__(self):
+        """Initializer for LoggerProperty class."""
+        if not hasattr(self, '_rlogger'):
+            self.rlogger = None
+
+    @property
+    def rlogger(self):
+        """The logger property for the class.
+
+        :getter: Returns the current logger.
+        :setter: Sets the reconplogger logger if True or sets null_logger if False or sets the given logger.
+
+        Raises:
+            ValueError: If an invalid logger value is given.
+        """
+        return self._rlogger
+
+    @rlogger.setter
+    def rlogger(
+        self,
+        logger: Optional[Union[bool, logging.Logger]]
+    ):
+        if logger is None or (isinstance(logger, bool) and not logger):
+            self._rlogger = null_logger
+        elif isinstance(logger, bool) and logger:
+            self._rlogger = logger_setup()
+        else:
+            self._rlogger = logger
---
+++ packages/reconplogger/reconplogger-4.7.1-py3-none-any.whl/reconplogger_tests.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import unittest
+import shutil
+import tempfile
+import logging
+import uuid
+import reconplogger
+from testfixtures import LogCapture, compare, Comparison
+from flask import Flask, request
+import requests
+
+
+class TestReconplogger(unittest.TestCase):
+    def test_default_logger(self):
+        """Test load config with the default config and plain logger."""
+        reconplogger.load_config('reconplogger_default_cfg')
+        logger = logging.getLogger('plain_logger')
+        info_msg = 'info message'
+        with LogCapture(names='plain_logger') as log:
+            logger.info(info_msg)
+            log.check(('plain_logger', 'INFO', info_msg))
+
+    def test_log_level(self):
+        """Test load config with the default config and plain logger changing the log level."""
+        logger = reconplogger.logger_setup(level='INFO')
+        self.assertEqual(logger.handlers[0].level, logging.INFO)
+        logger = reconplogger.logger_setup(level='ERROR')
+        self.assertEqual(logger.handlers[0].level, logging.ERROR)
+        os.environ['LOGGER_LEVEL'] = 'WARNING'
+        logger = reconplogger.logger_setup(level='INFO', env_prefix='LOGGER')
+        self.assertEqual(logger.handlers[0].level, logging.WARNING)
+        del os.environ['LOGGER_LEVEL']
+
+    def test_default_logger_with_exception(self):
+        """Test exception logging with the default config and json logger."""
+        reconplogger.load_config('reconplogger_default_cfg')
+        logger = logging.getLogger('json_logger')
+        error_msg = 'error message'
+        exception = RuntimeError('Exception message')
+        with LogCapture(names='json_logger') as log:
+            try:
+                raise exception
+            except Exception as e:
+                logger.error(error_msg, exc_info=True)
+                compare(Comparison(exception), log.records[-1].exc_info[1])
+                log.check(('json_logger', 'ERROR', error_msg))
+
+    def test_plain_logger_setup(self):
+        """Test logger_setup without specifying environment variable names."""
+        logger = reconplogger.logger_setup()
+        info_msg = 'info message'
+        with LogCapture(names='plain_logger') as log:
+            logger.info(info_msg)
+            log.check(('plain_logger', 'INFO', info_msg))
+
+    def test_json_logger_setup(self):
+        """Test logger_setup without specifying environment variable names but changing logger name."""
+        logger = reconplogger.logger_setup(logger_name='json_logger')
+        info_msg = 'info message'
+        with LogCapture(names='json_logger') as log:
+            logger.info(info_msg)
+            log.check(('json_logger', 'INFO', info_msg))
+
+    def test_replace_logger_handlers(self):
+        logger = logging.getLogger('test_replace_logger_handlers')
+        handlers1 = reconplogger.logger_setup(logger_name='plain_logger').handlers
+        handlers2 = reconplogger.logger_setup(logger_name='json_logger').handlers
+
+        self.assertNotEqual(logger.handlers, handlers1)
+        self.assertNotEqual(logger.handlers, handlers2)
+
+        reconplogger.replace_logger_handlers('test_replace_logger_handlers', 'plain_logger')
+        self.assertEqual(logger.handlers, handlers1)
+        self.assertNotEqual(logger.handlers, handlers2)
+
+        reconplogger.replace_logger_handlers('test_replace_logger_handlers', 'json_logger')
+        self.assertEqual(logger.handlers, handlers2)
+        self.assertNotEqual(logger.handlers, handlers1)
+
+        self.assertRaises(
+            ValueError, lambda: reconplogger.replace_logger_handlers(logger, False))
+        self.assertRaises(
+            ValueError, lambda: reconplogger.replace_logger_handlers(False, False))
+
+    def test_init_messages(self):
+        logger = reconplogger.logger_setup(init_messages=True)
+        with self.assertLogs(logger='plain_logger', level='WARNING') as log:
+            reconplogger.test_logger(logger)
+            self.assertTrue(any(['WARNING' in v and 'reconplogger' in v for v in log.output]))
+
+    def test_logger_setup_env_prefix(self):
+        """Test logger setup with specifying environment prefix."""
+        env_prefix = 'RECONPLOGGER'
+        os.environ['RECONPLOGGER_NAME'] = 'example_logger'
+        os.environ['RECONPLOGGER_CFG'] = """{
+            "version": 1,
+            "formatters": {
+                "verbose": {
+                    "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+                }
+            },
+            "handlers": {
+                "console":{
+                    "level": "DEBUG",
+                    "class": "logging.StreamHandler",
+                    "formatter": "verbose"
+                }
+            },
+            "loggers": {
+                "example_logger": {
+                    "handlers": ["console"],
+                    "level": "DEBUG"
+                }
+            }
+        }"""
+        logger = reconplogger.logger_setup(env_prefix=env_prefix)
+        info_msg = 'info message env logger'
+        with LogCapture(names='example_logger') as log:
+            logger.info(info_msg)
+            log.check(('example_logger', 'INFO', info_msg))
+
+        del os.environ['RECONPLOGGER_CFG']
+        del os.environ['RECONPLOGGER_NAME']
+
+        self.assertRaises(
+            ValueError, lambda: reconplogger.logger_setup(env_prefix=None))
+        self.assertRaises(
+            ValueError, lambda: reconplogger.logger_setup(env_prefix=''))
+
+    def test_undefined_logger(self):
+        """Test setting up a logger not already defined."""
+        self.assertRaises(
+            ValueError, lambda: reconplogger.logger_setup('undefined_logger'))
+
+    def test_logger_setup_invalid_level(self):
+        self.assertRaises(
+            ValueError, lambda: reconplogger.logger_setup(level='INVALID'))
+        self.assertRaises(
+            ValueError, lambda: reconplogger.logger_setup(level=True))
+
+    def test_flask_app_logger_setup(self):
+        """Test flask app logger setup with json logger."""
+        env_prefix = 'RECONPLOGGER'
+        os.environ['RECONPLOGGER_CFG'] = 'reconplogger_default_cfg'
+        os.environ['RECONPLOGGER_NAME'] = 'json_logger'
+        app = Flask(__name__)
+        reconplogger.flask_app_logger_setup(
+            env_prefix=env_prefix, flask_app=app)
+        assert app.logger.filters  # pylint: disable=no-member
+        assert app.before_request_funcs
+        assert app.after_request_funcs
+        flask_msg = 'flask message'
+        werkzeug_msg = 'werkzeug message'
+        with LogCapture(names=(app.logger.name, 'werkzeug')) as log:
+            app.logger.warning(flask_msg)  # pylint: disable=no-member
+            logging.getLogger('werkzeug').warning(werkzeug_msg)
+            log.check_present(
+                (app.logger.name, 'WARNING', flask_msg),
+                ('werkzeug', 'WARNING', werkzeug_msg),
+            )
+        del os.environ['RECONPLOGGER_CFG']
+        del os.environ['RECONPLOGGER_NAME']
+
+    def test_flask_app_correlation_id(self):
+        """Test that the flask logs contains the correct correlation id"""
+        env_prefix = 'RECONPLOGGER'
+        os.environ['RECONPLOGGER_CFG'] = 'reconplogger_default_cfg'
+        os.environ['RECONPLOGGER_NAME'] = 'json_logger'
+        app = Flask(__name__)
+        flask_msg = 'flask message with correlation id'
+
+        @app.route('/')
+        def hello_world():
+            if request.args.get('id') is None:
+                correlation_id = reconplogger.get_correlation_id()
+            else:
+                correlation_id = request.args.get('id')
+                reconplogger.set_correlation_id(correlation_id)
+            app.logger.info(flask_msg)  # pylint: disable=no-member
+            return 'correlation_id='+correlation_id
+
+        client = app.test_client()
+        with LogCapture(names=app.logger.name, attributes=('name', 'levelname')) as logs:
+            response = client.get("/")
+            logs.check((app.logger.name, 'ERROR'))
+        self.assertEqual(response.status_code, 500)
+
+        reconplogger.flask_app_logger_setup(
+            env_prefix=env_prefix, flask_app=app)
+        client = app.test_client()
+
+        self.assertRaises(RuntimeError, lambda: reconplogger.get_correlation_id())
+        self.assertRaises(RuntimeError, lambda: reconplogger.set_correlation_id('id'))
+
+        # Check correlation id propagation
+        with LogCapture(names=app.logger.name, attributes=('name', 'levelname', 'getMessage', 'correlation_id')) as logs:
+            correlation_id = str(uuid.uuid4())
+            response = client.get("/", headers={'Correlation-ID': correlation_id})
+            self.assertEqual(response.data.decode('utf-8'), 'correlation_id='+correlation_id)
+            logs.check(
+                (app.logger.name, 'INFO', flask_msg, correlation_id),
+                (app.logger.name, 'INFO', "Request is completed", correlation_id),
+            )
+        # Check correlation id creation
+        with LogCapture(names=app.logger.name, attributes=('name', 'levelname', 'getMessage', 'correlation_id')) as logs:
+            client.get("/")
+            correlation_id = logs.actual()[0][3]
+            uuid.UUID(correlation_id)
+            logs.check(
+                (app.logger.name, 'INFO', flask_msg, correlation_id),
+                (app.logger.name, 'INFO', "Request is completed", correlation_id),
+            )
+        # Check set correlation id
+        with LogCapture(names=app.logger.name, attributes=('name', 'levelname', 'getMessage', 'correlation_id')) as logs:
+            correlation_id = str(uuid.uuid4())
+            response = client.get("/?id="+correlation_id)
+            self.assertEqual(response.data.decode('utf-8'), 'correlation_id='+correlation_id)
+            logs.check(
+                (app.logger.name, 'INFO', flask_msg, correlation_id),
+                (app.logger.name, 'INFO', "Request is completed", correlation_id),
+            )
+
+        del os.environ['RECONPLOGGER_CFG']
+        del os.environ['RECONPLOGGER_NAME']
+
+    @unittest.mock.patch("requests.sessions.Session.request_orig")
+    @unittest.mock.patch("reconplogger.g", spec={}) # https://github.com/pallets/flask/issues/3637 adding spec as workaround for a python 3.8 bug
+    @unittest.mock.patch("reconplogger.has_request_context", return_value=True)
+    def test_requests_patch(self, mock_has_request_context, mock_g, mock_request_orig):
+        mock_g.correlation_id = uuid.uuid4()
+        requests.get("http://dummy")
+        mock_request_orig.assert_called_once_with(
+            allow_redirects=True, headers={'Correlation-ID': mock_g.correlation_id},
+            method='get', params=None, url='http://dummy')
+
+    def test_add_file_handler(self):
+        """Test the use of add_file_handler."""
+        tmpdir = tempfile.mkdtemp(prefix='_reconplogger_test_')
+        error_msg = 'error message'
+        debug_msg = 'debug message'
+
+        log_file = os.path.join(tmpdir, 'file1.log')
+        logger = reconplogger.logger_setup(logger_name='plain_logger', level='ERROR')
+        reconplogger.add_file_handler(logger, file_path=log_file, level='DEBUG')
+        self.assertEqual(logger.handlers[0].level, logging.ERROR)
+        self.assertEqual(logger.handlers[1].level, logging.DEBUG)
+        logger.error(error_msg)
+        logger.debug(debug_msg)
+        logger.handlers[1].close()
+        self.assertTrue(any([error_msg in line for line in open(log_file).readlines()]))
+        self.assertTrue(any([debug_msg in line for line in open(log_file).readlines()]))
+
+        log_file = os.path.join(tmpdir, 'file2.log')
+        logger = reconplogger.logger_setup(logger_name='plain_logger', level='DEBUG')
+        reconplogger.add_file_handler(logger, file_path=log_file, level='ERROR')
+        self.assertEqual(logger.handlers[0].level, logging.DEBUG)
+        self.assertEqual(logger.handlers[1].level, logging.ERROR)
+        logger.error(error_msg)
+        logger.debug(debug_msg)
+        logger.handlers[1].close()
+        self.assertTrue(any([error_msg in line for line in open(log_file).readlines()]))
+        self.assertFalse(any([debug_msg in line for line in open(log_file).readlines()]))
+
+        self.assertRaises(
+            ValueError, lambda: reconplogger.add_file_handler(logger, file_path=log_file, level='INVALID'))
+
+        shutil.rmtree(tmpdir)
+
+    def test_logger_property(self):
+        class MyClass(reconplogger.RLoggerProperty):
+            pass
+
+        myclass = MyClass()
+        myclass.rlogger = False
+        self.assertEqual(myclass.rlogger, reconplogger.null_logger)
+        myclass.rlogger = True
+        self.assertEqual(myclass.rlogger, reconplogger.logger_setup())
+        logger = logging.Logger('test_logger_property')
+        myclass.rlogger = logger
+        self.assertEqual(myclass.rlogger, logger)
+
+
+def run_tests():
+    tests = unittest.defaultTestLoader.loadTestsFromTestCase(TestReconplogger)
+    if not unittest.TextTestRunner(verbosity=2).run(tests).wasSuccessful():
+        sys.exit(True)
+
+
+def run_test_coverage():
+    try:
+        import coverage
+    except:
+        print('error: coverage package not found, run_test_coverage requires it.')
+        sys.exit(True)
+    cov = coverage.Coverage(source=['reconplogger'])
+    cov.start()
+    del sys.modules['reconplogger']
+    import reconplogger
+    run_tests()
+    cov.stop()
+    cov.save()
+    cov.report()
+    if 'xml' in sys.argv:
+        outfile = sys.argv[sys.argv.index('xml')+1]
+        cov.xml_report(outfile=outfile)
+        print('\nSaved coverage report to '+outfile+'.')
+    else:
+        cov.html_report(directory='htmlcov')
+        print('\nSaved html coverage report to htmlcov directory.')
+
+
+if __name__ == '__main__':
+    if 'coverage' in sys.argv:
+        run_test_coverage()
+    else:
+        run_tests()
