05bfd80024d45e14fc4c8061d9623cf8d1c197f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:07 +0000

    Add package gravity-interface/1.4/gravity_interface-1.4.tar.gz

---
+++ packages/gravity-interface/gravity_interface-1.4.tar.gz/gravity_interface-1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='gravity_interface',
+    version='1.4',
+    packages=find_packages(),
+    author='PunchyArchy',
+    author_email='ksmdrmvscthny@gmail.com',
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    install_requires=[
+        'pillow',
+        'pypiwin32',
+        'pyscreenshot',
+        'tkcalendar'
+    ],
+)
