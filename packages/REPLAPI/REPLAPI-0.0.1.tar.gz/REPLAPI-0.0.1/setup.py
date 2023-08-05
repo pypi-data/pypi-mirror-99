d948dee59b0104d75e6fbe3cd19c5499ab02f81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:31 +0000

    Add package REPLAPI/0.0.1/REPLAPI-0.0.1.tar.gz

---
+++ packages/REPLAPI/REPLAPI-0.0.1.tar.gz/REPLAPI-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+#reeee
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name = "REPLAPI",
+  version = "0.0.1",
+  description = "REPLAPI Module",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = "darkdarcool30",
+  author_email = "darkdarcool@gmail.com",#pypi account
+#To find more licenses or classifiers go to: https://pypi.org/classifiers/
+  license = "MIT",
+  packages=['REPLAPI'],
+  classifiers = [
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+  "Operating System :: OS Independent",
+],
+  zip_safe=True,
+  python_requires = ">=3.0",
+)
