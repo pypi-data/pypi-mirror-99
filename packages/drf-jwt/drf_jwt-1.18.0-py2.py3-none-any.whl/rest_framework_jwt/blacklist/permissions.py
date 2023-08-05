bdc70949af326c96f575d87355a9d48f6e2ee2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:56 +0000

    Add package drf-jwt/1.18.0/drf_jwt-1.18.0-py2.py3-none-any.whl

---
+++ packages/drf-jwt/drf_jwt-1.18.0-py2.py3-none-any.whl/rest_framework_jwt/blacklist/permissions.py
@@ -0,0 +1,22 @@
+import jwt
+
+from rest_framework.permissions import BasePermission
+
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework_jwt.blacklist.models import BlacklistedToken
+from rest_framework_jwt.compat import gettext_lazy as _
+
+
+class IsNotBlacklisted(BasePermission):
+    message = _('You have been blacklisted.')
+
+    def has_permission(self, request, view):
+        token = JSONWebTokenAuthentication.get_token_from_request(request)
+
+        # Don't check the blacklist for requests with no token.
+        if token is None:
+            return True
+
+        # The token should already be validated before we call this.
+        payload = jwt.decode(token, None, False)
+        return not BlacklistedToken.is_blocked(token, payload)
