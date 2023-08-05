57bd763524ebd7839ecbe49b30b45e9169fb4ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:21 +0000

    Add package perke/0.2.0/perke-0.2.0.tar.gz

---
+++ packages/perke/perke-0.2.0.tar.gz/perke-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from os.path import join
+from setuptools import (setup,
+                        find_packages)
+
+packages = find_packages(exclude=['tests*'])
+
+with open('requirements.txt') as f:
+    requirements = f.read().split()
+
+with open('README.md') as f:
+    long_description = f.read()
+
+filename = join('perke', 'version.py')
+with open(filename) as f:
+    exec(compile(f.read(), filename, 'exec'))
+
+
+setup(name='perke',
+      version=__version__,
+      url='https://github.com/alirezah320/perke',
+      author='Alireza Hosseini',
+      author_email='alirezah320@yahoo.com',
+      packages=packages,
+      description='A keyphrase extractor for Persian',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      license='MIT',
+      install_requires=requirements)
