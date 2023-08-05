a573a8adfd9bb7e597d4732fa6a54a50d963bf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:23 +0000

    Add package cem/0.1.3/cem-0.1.3.tar.gz

---
+++ packages/cem/cem-0.1.3.tar.gz/cem-0.1.3/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+requirements = [
+    'pandas>=1.0',
+    'scipy>=1.4',
+    'numpy>=1.18',
+    'tqdm>=4.46',
+]
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="Lewis Bails",
+    author_email='lewis.bails@gmail.com',
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="A Python implmentation of Coarsened Exact Matching for causal inference",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords='cem',
+    name='cem',
+    packages=find_packages(include=['cem', 'cem.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/lewisbails/cem',
+    version='0.1.3',
+    zip_safe=False,
+)
