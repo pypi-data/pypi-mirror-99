f597f538672c412f3c93d19c6d6f8e4582f93290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:49 +0000

    Add package river-mwclient/0.3.0/river_mwclient-0.3.0.tar.gz

---
+++ packages/river-mwclient/river_mwclient-0.3.0.tar.gz/river_mwclient-0.3.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="river_mwclient",
+    version="0.3.0",
+    author="RheingoldRiver",
+    author_email="river.esports@gmail.com",
+    description="River's mwclient wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RheingoldRiver/river_mwclient",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=['mwparserfromhell', 'pytz', 'mwclient>=0.10.1', 'python-dateutil', 'Unidecode']
+)
