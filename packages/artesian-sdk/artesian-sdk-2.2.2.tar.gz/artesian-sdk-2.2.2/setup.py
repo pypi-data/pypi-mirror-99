b89aac826896fa02fe53e119ebfee53f5dc233b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package artesian-sdk/2.2.2/artesian-sdk-2.2.2.tar.gz

---
+++ packages/artesian-sdk/artesian-sdk-2.2.2.tar.gz/artesian-sdk-2.2.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="artesian-sdk",
+    version="2.2.2",
+    author="Ark-Lab",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    author_email="niccolo.cecchi@ark-energy.eu",
+    description="Library provides read access to the Artesian API",
+    url="https://github.com/ARKlab/Artesian.SDK-Python",
+    packages=setuptools.find_packages(),
+    py_modules = ['artesian'], 
+    install_requires=[
+        'requests',
+        'six'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+	)
