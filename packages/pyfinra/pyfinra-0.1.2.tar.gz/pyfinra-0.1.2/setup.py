e69434d6fdd9cec35630782110487fd69c9ee7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package pyfinra/0.1.2/pyfinra-0.1.2.tar.gz

---
+++ packages/pyfinra/pyfinra-0.1.2.tar.gz/pyfinra-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyfinra', 'pyfinra.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['finsymbols>=1.3.0,<2.0.0',
+ 'pandas>=1.2.3,<2.0.0',
+ 'pytest>=6.2.2,<7.0.0',
+ 'requests>=2.25.1,<3.0.0',
+ 'selenium>=3.141.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'pyfinra',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Sören Michaels',
+    'author_email': 'soeren.michaels@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
