deee951ba613619ced3bdfe6b5d8ab4b162f3031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:18 +0000

    Add package dfsync/0.2.5/dfsync-0.2.5.tar.gz

---
+++ packages/dfsync/dfsync-0.2.5.tar.gz/dfsync-0.2.5/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dfsync', 'dfsync.backends']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gitpython>=3.1.14,<4.0.0',
+ 'kubernetes>=12.0.1,<13.0.0',
+ 'watchdog>=0.10.2,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['dfsync = dfsync.cli:dfsync']}
+
+setup_kwargs = {
+    'name': 'dfsync',
+    'version': '0.2.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Mihai Balint',
+    'author_email': 'balint.mihai@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
