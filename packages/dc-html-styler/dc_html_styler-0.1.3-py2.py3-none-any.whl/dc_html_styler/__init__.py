ff3951f15be06ea04582b9538dc9d51fb0524c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package dc-html-styler/0.1.3/dc_html_styler-0.1.3-py2.py3-none-any.whl

---
+++ packages/dc-html-styler/dc_html_styler-0.1.3-py2.py3-none-any.whl/dc_html_styler/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for dc_html_styler."""
+
+__author__ = """Jakob Erik Jensen"""
+__email__ = 'jej@danskecommodities.com'
+__version__ = '0.1.3'
+
+from dc_html_styler.dc_html_styler import dcstyler
