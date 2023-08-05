570e9d67c6549e1a9c56cd2796d3f10cd5e2632b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:20 +0000

    Add package pulp-2to3-migration-client/0.10.0.dev1616081287/pulp_2to3_migration-client-0.10.0.dev1616081287.tar.gz

---
+++ packages/pulp-2to3-migration-client/pulp_2to3_migration-client-0.10.0.dev1616081287.tar.gz/pulp_2to3_migration-client-0.10.0.dev1616081287/pulpcore/client/pulp_2to3_migration/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_2to3_migration.api.migration_plans_api import MigrationPlansApi
+from pulpcore.client.pulp_2to3_migration.api.pulp2_content_api import Pulp2ContentApi
+from pulpcore.client.pulp_2to3_migration.api.pulp2_repositories_api import Pulp2RepositoriesApi
