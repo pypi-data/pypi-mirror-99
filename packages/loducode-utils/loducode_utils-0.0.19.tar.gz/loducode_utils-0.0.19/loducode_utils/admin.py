905dc6ed4c7ead3dbce3df0280021e178d4bafce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package loducode-utils/0.0.19/loducode_utils-0.0.19.tar.gz

---
+++ packages/loducode-utils/loducode_utils-0.0.19.tar.gz/loducode_utils-0.0.19/loducode_utils/admin.py
@@ -0,0 +1,52 @@
+from django.contrib import admin
+
+from loducode_utils.models.city import City
+from import_export.admin import ImportExportModelAdmin
+
+class AuditAdmin(admin.ModelAdmin):
+    __readonly_audit_fields = (
+        'created_at', 'modified_at', 'created_by', 'modified_by'
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super(AuditAdmin, self).get_readonly_fields(request, obj)
+        readonly_fields = readonly_fields + self.__readonly_audit_fields
+        return readonly_fields
+
+class AuditStackedInline(admin.StackedInline):
+    __readonly_audit_fields = (
+        'created_at', 'modified_at', 'created_by', 'modified_by'
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super(AuditStackedInline, self).get_readonly_fields(request, obj)
+        readonly_fields = readonly_fields + self.__readonly_audit_fields
+        return readonly_fields
+
+class AuditTabularInline(admin.TabularInline):
+    __readonly_audit_fields = (
+        'created_at', 'modified_at', 'created_by', 'modified_by'
+    )
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super(AuditTabularInline, self).get_readonly_fields(request, obj)
+        readonly_fields = readonly_fields + self.__readonly_audit_fields
+        return readonly_fields
+
+class ReadOnlyAdmin(AuditAdmin):
+    pass
+
+class ReadOnlyStackedInline(AuditStackedInline):
+    pass
+
+class ReadOnlyTabularInline(AuditTabularInline):
+    pass
+
+
+class CityAdmin(ImportExportModelAdmin ,AuditAdmin):
+    list_display = ("name","state")
+    list_display_links = ("name","state")
+    list_filter = ("state",)
+    search_fields = ("name",)
+
+admin.site.register(City, CityAdmin)
