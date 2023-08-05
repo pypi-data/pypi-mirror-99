c079ff4ada576de23fd0ebd44b353716487b2e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package mypo/0.0.47/mypo-0.0.47.tar.gz

---
+++ packages/mypo/mypo-0.0.47.tar.gz/mypo-0.0.47/mypo/settings.py
@@ -0,0 +1,13 @@
+"""Parameters for runner."""
+from dataclasses import dataclass
+
+import numpy as np
+
+
+@dataclass
+class Settings:
+    """Settings for runner."""
+
+    spending: np.float64
+    tax_rate: np.float64
+    fee_rate: np.float64
