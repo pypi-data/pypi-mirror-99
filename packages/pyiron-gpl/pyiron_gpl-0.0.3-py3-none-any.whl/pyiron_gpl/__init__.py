ee95398569b8b2d7f5dd7ede5b155db4312dbb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:27 +0000

    Add package pyiron-gpl/0.0.3/pyiron_gpl-0.0.3-py3-none-any.whl

---
+++ packages/pyiron-gpl/pyiron_gpl-0.0.3-py3-none-any.whl/pyiron_gpl/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "0.1.1"
+__all__ = []
+
+from pyiron_atomistics.project import Project
+from pyiron_base import Notebook, install_dialog, JOB_CLASS_DICT
+
+# Make classes available for new pyiron version
+JOB_CLASS_DICT["ElasticMatrixJob"] = "pyiron_gpl.elastic.elastic"
+JOB_CLASS_DICT["PaceMakerJob"] = "pyiron_gpl.pacemaker.pacemaker"
+JOB_CLASS_DICT["QuickFF"] = "pyiron_gpl.quickff.quickff"
+JOB_CLASS_DICT["Yaff"] = "pyiron_gpl.yaff.yaff"
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+
+def install():
+    install_dialog()
