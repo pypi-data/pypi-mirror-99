57b6fd7d3a7788e6f653bf1271fdeac693bb0bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:31 +0000

    Add package jumpserver-omnidb-sdk/0.1.1/jumpserver-omnidb-sdk-0.1.1.tar.gz

---
+++ packages/jumpserver-omnidb-sdk/jumpserver-omnidb-sdk-0.1.1.tar.gz/jumpserver-omnidb-sdk-0.1.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('jmsdb/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('Cannot find version information')
+
+
+with open('README.md', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+with open('requirements.txt', 'r') as f:
+    requirements = [x.strip() for x in f.readlines()]
+
+setup(
+    name='jumpserver-omnidb-sdk',
+    version=version,
+    description='Jumpserver python sdk tools',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords='jms jumpserver',
+    packages=['jmsdb'],
+    author='bingws',
+    author_email='2423355719@qq.com',
+    install_requires=requirements,
+    include_package_data=True,
+    data_files=[('requirements', ['requirements.txt'])],
+    url='http://jumpserver.org',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
