f267d186839405f7d7d2e100c9026412d577cdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:23 +0000

    Add package amulet-nbt/1.0.3.8/amulet-nbt-1.0.3.8.tar.gz

---
+++ packages/amulet-nbt/amulet-nbt-1.0.3.8.tar.gz/amulet-nbt-1.0.3.8/setup.py
@@ -0,0 +1,54 @@
+import os
+import glob
+import shutil
+from setuptools import setup, find_packages, Extension
+import versioneer
+
+CYTHON_COMPILE = False
+try:
+    from Cython.Build import cythonize
+
+    CYTHON_COMPILE = True
+except Exception:
+    pass
+
+
+class get_numpy_include:
+    def __str__(self):
+        import numpy
+
+        return numpy.get_include()
+
+
+# there were issues with other builds carrying over their cache
+for d in glob.glob("*.egg-info"):
+    shutil.rmtree(d)
+
+with open(os.path.join(".", "requirements.txt")) as requirements_fp:
+    depends_on = [line.strip() for line in requirements_fp.readlines()]
+
+if CYTHON_COMPILE:
+    extensions = [
+        Extension(
+            name="amulet_nbt.amulet_cy_nbt", sources=["amulet_nbt/amulet_cy_nbt.pyx"]
+        )
+    ]
+    ext_modules = cythonize(extensions, language_level=3, annotate=True)
+else:
+    ext_modules = []
+
+setup(
+    name="amulet-nbt",
+    version=versioneer.get_version(),
+    description="Read and write Minecraft NBT and SNBT data.",
+    author="Ben Gothard, James Clare",
+    author_email="amuleteditor@gmail.com",
+    install_requires=depends_on,
+    setup_requires=depends_on,
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    include_dirs=[get_numpy_include()],
+    include_package_data=True,
+    cmdclass=versioneer.get_cmdclass(),
+    zip_safe=False,
+)
