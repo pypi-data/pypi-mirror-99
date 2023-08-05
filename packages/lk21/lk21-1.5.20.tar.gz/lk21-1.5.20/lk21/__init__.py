d5bf2a1d147f406c450f842e20f5cec375c8f1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:53 +0000

    Add package lk21/1.5.20/lk21-1.5.20.tar.gz

---
+++ packages/lk21/lk21-1.5.20.tar.gz/lk21-1.5.20/lk21/__init__.py
@@ -0,0 +1,13 @@
+"""cari anime dan film subtitle Indonesia"""
+
+__version__ = "1.5.20"
+__author__ = "Val"
+__license__ = "MIT"
+__all__ = [
+  "extractors",
+  "Bypass"
+]
+
+from .cli import main
+from .extractors.bypasser import Bypass
+
