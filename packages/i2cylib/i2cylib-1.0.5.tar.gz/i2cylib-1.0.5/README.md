61aa19ea5d3e6a2d904e00825fb2b90e9b95d6ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:27 +0000

    Add package i2cylib/1.0.5/i2cylib-1.0.5.tar.gz

---
+++ packages/i2cylib/i2cylib-1.0.5.tar.gz/i2cylib-1.0.5/README.md
@@ -0,0 +1,13 @@
+# 主要包含
+ - `ICCode` i2cy常用的混淆算法
+ - `Dynkey` 动态验证密匙生成/验证工具
+ - `SQLiteDB` SQLite3数据库面向对象式API
+ - `ICFat64` 类FAT虚拟文件系统
+ - `I2TCP_protocol` 一般的用户层通讯协议
+ - `utils` 各种常用的小工具
+
+# 安装方法
+`pip install i2cylib`
+
+# 环境需求
+`Python3.6+`
---
+++ packages/i2cylib/i2cylib-1.0.5.tar.gz/i2cylib-1.0.5/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: i2cy(i2cy@outlook.com)
+# Filename: setup
+# Created on: 2021/3/6
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="i2cylib", # Replace with your own username
+    version="1.0.5",
+    author="I2cy Cloud",
+    author_email="i2cy@outlook.com",
+    description="A Python library contains a lot of useful functions and tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/i2cy/i2cylib",
+    project_urls={
+        "Bug Tracker": "https://github.com/i2cy/i2cylib/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
