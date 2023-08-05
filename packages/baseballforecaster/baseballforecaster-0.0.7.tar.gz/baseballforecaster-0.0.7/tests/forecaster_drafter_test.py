fd630d0caf579545d8663e268bb1575eaec4e725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:40 +0000

    Add package baseballforecaster/0.0.7/baseballforecaster-0.0.7.tar.gz

---
+++ packages/baseballforecaster/baseballforecaster-0.0.7.tar.gz/baseballforecaster-0.0.7/tests/forecaster_drafter_test.py
@@ -0,0 +1,26 @@
+#import the libraries
+from baseballforecaster import Forecaster, Drafter, entry, get_dates
+
+#perform the analysis:
+if __name__ == "__main__":
+    
+    #get parameters for forecasting
+    simulations, num_competitors, num_rounds, num_iterations = entry()
+    
+    #get the time period for forecastings
+    time_period = get_dates()
+
+    #create Forecaster object
+    forecaster = Forecaster(simulations, num_competitors, num_rounds, num_iterations,time_period)
+
+    #perform monte carlo simulation to forecast player performance
+    forecaster.monte_carlo_forecast()
+    
+    #define the drafter object
+    drafter = Drafter(forecaster)
+    
+    #simulate a fantasy baseball draft
+    drafter.draft()
+    
+    #save the forecasting, clustering, and drafting results
+    drafter.excel_converter()
