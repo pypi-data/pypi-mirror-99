dd5393e0d17e4436c30090200d3e4dda912cb8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package metsrw/0.3.16/metsrw-0.3.16.tar.gz

---
+++ packages/metsrw/metsrw-0.3.16.tar.gz/metsrw-0.3.16/README.md
@@ -0,0 +1,45 @@
+[![PyPI version](https://badge.fury.io/py/metsrw.svg)](https://badge.fury.io/py/metsrw) [![Travis CI](https://travis-ci.org/artefactual-labs/mets-reader-writer.svg?branch=master)](https://travis-ci.org/artefactual-labs/mets-reader-writer) [![Coverage status](https://img.shields.io/coveralls/artefactual-labs/mets-reader-writer/master.svg)](https://coveralls.io/r/artefactual-labs/mets-reader-writer)
+
+# METS Reader & Writer
+
+By [Artefactual](https://www.artefactual.com/)
+
+METSRW is a library to help with parsing and creating METS files.
+It provides an API, and abstracts away the actual creation of the XML.
+METSRW was initially created for use in [Archivematica](https://github.com/artefactual/archivematica/) and is managed as part of that project.
+
+You are free to copy, modify, and distribute metsrw with attribution under the terms of the AGPL license.
+See the [LICENSE](LICENSE) file for details.
+
+
+## Installation & Dependencies
+
+METSRW can be installed with pip.
+
+`pip install metsrw`
+
+METSRW has been tested with:
+
+* Python 2.7
+* Python 3.5
+* Python 3.6
+* Python 3.7
+* Python 3.8
+
+## Basic Usage
+
+Read a METS file
+
+    mets = metsrw.METSDocument.fromfile('path/to/file')  # Reads a file
+    mets = metsrw.METSDocument.fromstring('<mets document>')  # Parses a string
+    mets = metsrw.METSDocument.fromtree(lxml.ElementTree)  # Parses an lxml.Element or lxml.ElementTree
+
+Create a new METS file
+
+    mets = metsrw.METSDocument()
+
+
+## Contributing
+
+METSRW is in early development and welcomes feedback on the API and overall design!
+Design goals, use cases, and a proposed API are in the [Github wiki](https://github.com/artefactual-labs/mets-reader-writer/wiki)
---
+++ packages/metsrw/metsrw-0.3.16.tar.gz/metsrw-0.3.16/metsrw/__init__.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""METS reader and writer."""
+
+from __future__ import absolute_import
+import logging
+
+from .exceptions import MetsError, ParseError
+from .fsentry import FSEntry
+from .metadata import Agent, AltRecordID, AMDSec, SubSection, MDRef, MDWrap
+from .mets import METSDocument
+from .utils import (
+    NAMESPACES,
+    SCHEMA_LOCATIONS,
+    lxmlns,
+    FILE_ID_PREFIX,
+    GROUP_ID_PREFIX,
+    urlencode,
+    urldecode,
+)
+from .validate import (
+    METS_XSD_PATH,
+    AM_SCT_PATH,
+    AM_PNTR_SCT_PATH,
+    get_schematron,
+    validate,
+    get_xmlschema,
+    xsd_validate,
+    schematron_validate,
+    sct_report_string,
+    xsd_error_log_string,
+    report_string,
+)
+from .di import (
+    FeatureBroker,
+    set_feature_broker_to_default_state,
+    feature_broker,
+    Dependency,
+    has_class_methods,
+    has_methods,
+    is_class,
+)
+from . import plugins
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(logging.NullHandler())
+__version__ = "0.3.16"
+
+__all__ = [
+    "Agent",
+    "AltRecordID",
+    "AMDSec",
+    "AM_PNTR_SCT_PATH",
+    "AM_SCT_PATH",
+    "Dependency",
+    "FILE_ID_PREFIX",
+    "FSEntry",
+    "FeatureBroker",
+    "GROUP_ID_PREFIX",
+    "MDRef",
+    "MDWrap",
+    "METSDocument",
+    "METS_XSD_PATH",
+    "MetsError",
+    "NAMESPACES",
+    "ParseError",
+    "SCHEMA_LOCATIONS",
+    "SubSection",
+    "__version__",
+    "feature_broker",
+    "get_schematron",
+    "get_xmlschema",
+    "has_class_methods",
+    "has_methods",
+    "is_class",
+    "lxmlns",
+    "plugins",
+    "report_string",
+    "schematron_validate",
+    "sct_report_string",
+    "set_feature_broker_to_default_state",
+    "urldecode",
+    "urlencode",
+    "validate",
+    "xsd_error_log_string",
+    "xsd_validate",
+]
