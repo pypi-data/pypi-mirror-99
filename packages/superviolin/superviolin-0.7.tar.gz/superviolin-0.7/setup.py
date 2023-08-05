0bbd96710634e52a91656c19220aeabe3df41a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:14 +0000

    Add package superviolin/0.7/superviolin-0.7.tar.gz

---
+++ packages/superviolin/superviolin-0.7.tar.gz/superviolin-0.7/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  9 18:27:08 2021
+
+@author: martin
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+      name = "superviolin",
+      version = 0.7,
+      py_modules = ['plot_cli', 'plot'],
+      url = "",
+      description = "Python-based app to make superplots",
+      author = "Martin Kenny",
+      author_email = "sideproject1892@gmail.com",
+      maintainer = "Martin Kenny",
+      maintainer_email = "sideproject1892@gmail.com",
+      packages = find_packages(),
+      entry_points = {
+                      'console_scripts': ['superviolin=superviolin.plot_cli:cli']
+                      },
+      python_requires = ">=3.6",
+      install_requires = [
+          "appdirs",
+          "click",
+          "matplotlib",
+          "numpy",
+          "pandas",
+          "scipy",
+          "xlrd",
+          "openpyxl",
+          "scikit-posthocs"
+          ],
+      package_data = {'' : ['demo_data.csv',
+                            'args.txt',
+                            'demo_args.txt']
+                            },
+      classifiers = [
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          ],
+      )
