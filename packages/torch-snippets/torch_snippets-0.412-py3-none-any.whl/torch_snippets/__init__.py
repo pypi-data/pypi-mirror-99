5adde0c9e91f95b2ca6964d9a55d112ff053c310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package torch-snippets/0.412/torch_snippets-0.412-py3-none-any.whl

---
+++ packages/torch-snippets/torch_snippets-0.412-py3-none-any.whl/torch_snippets/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.411"
+from .loader import *
+from .fastcores import *
+from .charts import *
+try:
+    from .torch_loader import *
+except:
+    logger.warning('torch is not found. Skipping relevant imports from submodule `torch_loader`')
+    
+try:
+    from .sklegos import *
+except:
+    logger.warning('sklearn is not found. Skipping relevant imports from submodule `sklegos`')
