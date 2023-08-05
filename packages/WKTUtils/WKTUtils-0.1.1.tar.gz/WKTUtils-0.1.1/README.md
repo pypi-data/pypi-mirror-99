d8a6042f65a6ed39227be263eec67ea360a32fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:13 +0000

    Add package WKTUtils/0.1.1/WKTUtils-0.1.1.tar.gz

---
+++ packages/WKTUtils/WKTUtils-0.1.1.tar.gz/WKTUtils-0.1.1/README.md
@@ -0,0 +1,27 @@
+# Discovery-WKTUtils
+
+[![image](https://img.shields.io/pypi/v/wktutils.svg)](https://pypi.python.org/pypi/WKTUtils)
+
+### To Install as Package, add the following line to requirements.txt:
+
+```bash
+git+https://github.com/asfadmin/Discovery-WKTUtils.git@prod#egg=WKTUtils
+```
+
+(The package name will be 'WKTUtils'. You can change 'prod' to desired branch).
+
+### Install the requirements file:
+
+```bash
+python3 -m pip install -r requirements.ext --upgrade
+```
+
+### FilesToWKT:
+You must open shapefiles in read binary mode, before passing them into the function.
+
+```python
+from WKTUtils.FilesToWKT import filesToWKT
+f = open("path/to/file.shp", "rb")
+# NOTE: f can be a list of files too. filesToWKT([f1, f2, ...]).getWKT()
+wkt = filesToWKT(f).getWKT()
+```
---
+++ packages/WKTUtils/WKTUtils-0.1.1.tar.gz/WKTUtils-0.1.1/WKTUtils/asf_env.py
@@ -0,0 +1,92 @@
+import os
+import logging
+
+def get_config():
+    if 'MATURITY' not in os.environ.keys():
+        logging.warning('os.environ[\'MATURITY\'] not set! Defaulting to prod config.]')
+    return {
+        'local': {
+            'this_api': 'http://local.asf.alaska.edu:5000',
+            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
+            'analytics_id': None,
+            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
+            'cmr_health': '/search/health',
+            'cmr_api': '/search/granules.echo10',
+            'cmr_collections': '/search/collections',
+            'cmr_headers': {
+                'Client-Id': 'local_searchapi_asf'
+            }
+        },
+        'devel': {
+            'this_api': 'https://api-dev.asf.alaska.edu',
+            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
+            'analytics_id': 'UA-118881300-4',
+            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
+            'cmr_health': '/search/health',
+            'cmr_api': '/search/granules.echo10',
+            'cmr_collections': '/search/collections',
+            'cmr_headers': {
+                'Client-Id': 'devel_vertex_asf'
+            }
+        },
+        'devel-beanstalk': {
+            'this_api': 'https://api-dev.asf.alaska.edu',
+            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
+            'analytics_id': 'UA-118881300-4',
+            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
+            'cmr_health': '/search/health',
+            'cmr_api': '/search/granules.echo10',
+            'cmr_collections': '/search/collections',
+            'cmr_headers': {
+                'Client-Id': 'devel_searchapi_asf'
+            }
+        },
+        'test': {
+            'this_api': 'https://api-test.asf.alaska.edu',
+            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
+            'analytics_id': 'UA-118881300-3',
+            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
+            'cmr_health': '/search/health',
+            'cmr_api': '/search/granules.echo10',
+            'cmr_collections': '/search/collections',
+            'cmr_headers': {
+                'Client-Id': 'test_vertex_asf'
+            }
+        },
+        'test-beanstalk': {
+            'this_api': 'https://api-test.asf.alaska.edu',
+            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
+            'analytics_id': 'UA-118881300-3',
+            'cmr_base': 'https://cmr.uat.earthdata.nasa.gov',
+            'cmr_health': '/search/health',
+            'cmr_api': '/search/granules.echo10',
+            'cmr_collections': '/search/collections',
+            'cmr_headers': {
+                'Client-Id': 'test_searchapi_asf'
+            }
+        },
+        'prod': {
+            'this_api': 'https://api.daac.asf.alaska.edu',
+            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
+            'analytics_id': 'UA-118881300-2',
+            'cmr_base': 'https://cmr.earthdata.nasa.gov',
+            'cmr_health': '/search/health',
+            'cmr_api': '/search/granules.echo10',
+            'cmr_collections': '/search/collections',
+            'cmr_headers': {
+                'Client-Id': 'searchapi_asf'
+            }
+        },
+        'prod-private': {
+            'this_api': 'https://api-prod-private.asf.alaska.edu',
+            'bulk_download_api': 'https://bulk-download.asf.alaska.edu',
+            'analytics_id': 'UA-118881300-5',
+            'cmr_base': 'https://cmr.earthdata.nasa.gov',
+            'cmr_health': '/search/health',
+            'cmr_api': '/search/granules.echo10',
+            'cmr_collections': '/search/collections',
+            'cmr_headers': {
+                'Client-Id': 'vertex_asf'
+            }
+        }
+    }[os.environ['MATURITY'] if 'MATURITY' in os.environ.keys() else 'prod']
