58fcbc9dbe333bcdd9cc6028adc9939c14237b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:05 +0000

    Add package loducode-utils/0.0.17/loducode_utils-0.0.17.tar.gz

---
+++ packages/loducode-utils/loducode_utils-0.0.17.tar.gz/loducode_utils-0.0.17/loducode_utils/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+class LoducodeUtilsConfig(AppConfig):
+    name = 'loducode_utils'
+    verbose_name = _('utils')
