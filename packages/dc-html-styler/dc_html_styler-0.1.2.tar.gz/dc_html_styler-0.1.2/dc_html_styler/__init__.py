9d6672e6e5c744c80c41c3b4e16486986fc41836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:01 +0000

    Add package dc-html-styler/0.1.2/dc_html_styler-0.1.2.tar.gz

---
+++ packages/dc-html-styler/dc_html_styler-0.1.2.tar.gz/dc_html_styler-0.1.2/dc_html_styler/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for dc_html_styler."""
+
+__author__ = """Jakob Erik Jensen"""
+__email__ = 'jej@danskecommodities.com'
+__version__ = '0.1.1'
+from dc_html_styler import dcstyler
---
+++ packages/dc-html-styler/dc_html_styler-0.1.2.tar.gz/dc_html_styler-0.1.2/tests/test_dc_html_styler.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `dc_html_styler` package."""
+
+
+import unittest
+
+from dc_html_styler import dc_html_styler
+
+
+class TestDc_html_styler(unittest.TestCase):
+    """Tests for `dc_html_styler` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
