df1cee0f1a87a922ebab38ace0a33cfa66dd04ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package quick-config/0.2.4/quick_config-0.2.4-py3-none-any.whl

---
+++ packages/quick-config/quick_config-0.2.4-py3-none-any.whl/quick_config/__init__.py
@@ -0,0 +1,49 @@
+import os
+from quick_config.provider_wrapper import load_config_from_path
+from quick_config.validator import validate_config_dir
+
+
+config = None
+_env_key = 'CONFIG_DIR'
+
+
+__all__ = [
+    'config',
+    'load_config',
+    'setup_config_with_path',
+    'clear_config',
+]
+
+
+def clear_config():
+    global config
+    config = None
+
+
+def load_config():
+    wd = os.getcwd()
+
+    conf_dir = os.environ.get(_env_key)
+    if not conf_dir:
+        conf_dir = "config"
+
+    path = os.path.join(wd, conf_dir)
+    validate_config_dir(path)
+
+    return load_config_from_path(path)
+
+
+def setup_config_with_path(path: str):
+    """
+    sets up the config provider with an explicit path to the location of the desired directory of configs
+
+    :param path: the location of the config directory of a project to parse and load
+    """
+    global config
+    config = load_config_from_path(path)
+
+
+if __name__ != '__main__':
+    if config is None:
+        config = load_config()
+
