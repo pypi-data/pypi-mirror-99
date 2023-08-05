ef1d3290b67aad6cf889cd662e5e293e4f6d65cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package valorant/0.4.0/valorant-0.4.0.tar.gz

---
+++ packages/valorant/valorant-0.4.0.tar.gz/valorant-0.4.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+from valorant import __version__
+
+with open("docs/about.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="valorant",
+    version=__version__,
+    author="frissyn",
+    author_email="author@example.com",
+    description="An unofficial synchronous client package for interacting with Riot Games' Valorant API endpoints.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frissyn/valorant.py",
+    packages=["valorant"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
