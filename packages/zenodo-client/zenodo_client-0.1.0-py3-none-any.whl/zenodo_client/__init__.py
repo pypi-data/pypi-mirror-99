6d415ae1b31bdf58ba298aa3c41a0af840775fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:08:43 +0000

    Add package zenodo-client/0.1.0/zenodo_client-0.1.0-py3-none-any.whl

---
+++ packages/zenodo-client/zenodo_client-0.1.0-py3-none-any.whl/zenodo_client/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""A wrapper for the Zenodo API."""
+
+from .api import Zenodo, create_zenodo, ensure_zenodo, update_zenodo  # noqa:F401
+from .struct import Creator, Metadata  # noqa:F401
