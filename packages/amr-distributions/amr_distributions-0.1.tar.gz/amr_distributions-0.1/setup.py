09717cbee32e1aa918f11dc8ba3012651964b339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:58 +0000

    Add package amr-distributions/0.1/amr_distributions-0.1.tar.gz

---
+++ packages/amr-distributions/amr_distributions-0.1.tar.gz/amr_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='amr_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['amr_distributions'],
+      author = 'Amro Walid',
+      author_email = 'amro.walid.mo@gmail.com',
+      zip_safe=False)
