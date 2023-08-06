f4a53a781110e79d8e8ea2e357cfa9d4d18a4283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:03 +0000

    Add package pythonit-toolkit/0.1.19/pythonit_toolkit-0.1.19-py3-none-any.whl

---
+++ packages/pythonit-toolkit/pythonit_toolkit-0.1.19-py3-none-any.whl/pythonit_toolkit/pastaporto/tokens.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+import jwt
+
+
+def decode_pastaporto(token: str, secret: str) -> dict[str, Any]:
+    return jwt.decode(
+        token,
+        str(secret),
+        issuer="gateway",
+        algorithms=["HS256"],
+        options={"require": ["exp", "iss", "nbf", "iat"]},
+    )
+
+
+def decode_service_to_service_token(
+    token: str, secret: str, *, issuer: str, audience: str
+):
+    return jwt.decode(
+        token,
+        secret,
+        verify=True,
+        audience=audience,
+        issuer=issuer,
+        algorithms=["HS256"],
+        options={"require": ["exp", "iss", "aud", "nbf", "iat"]},
+    )
