37b4af3f0c0fc3d729a8ab3b9a856cec0eb905e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:56 +0000

    Add package modelpyxl/0.0.14/modelpyxl-0.0.14.tar.gz

---
+++ packages/modelpyxl/modelpyxl-0.0.14.tar.gz/modelpyxl-0.0.14/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["openpyxl>=3"]
+
+setup(
+    name="modelpyxl",
+    version="0.0.14",
+    author="Petr Borisenko",
+    author_email="borisenko.petr.a@gmail.com",
+    description="Python package for creation model of excel documents based on a workbook template.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pborisenko/modelpyxl/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
