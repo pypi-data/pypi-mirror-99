ad0ec0dd9da9dae6eb2e4b92afc17040b8cbbd3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:27 +0000

    Add package geocurrency/0.11.2.dev0/geocurrency-0.11.2.dev0-py3-none-any.whl

---
+++ packages/geocurrency/geocurrency-0.11.2.dev0-py3-none-any.whl/geocurrency/countries/settings.py
@@ -0,0 +1,40 @@
+"""
+Settings specific to Country module
+"""
+
+FLAG_SOURCE = 'https://raw.githubusercontent.com/cristiroma/countries/master/data/flags/SVG/{alpha_2}.svg?sanitize=true'
+
+# put in global settings.py to override
+GEOCODING_SERVICE = 'pelias'
+
+# put in global settings.py to override
+GEOCODER_PELIAS_URL = 'https://api.geocode.earth/v1'
+GEOCODER_PELIAS_KEY = ''
+GEOCODER_GOOGLE_KEY = ''
+
+GEOCODING_SERVICE_SETTINGS = {
+    'pelias': {
+        'class': 'geocurrencies.countries.services.pelias',
+        'default_url': 'http://127.0.0.1:3100/v1',
+        'reverse': {
+            'service': 'reverse',
+        },
+        'search': {
+            'service': 'search',  # path of the search service
+            'address': 'text'
+        }
+
+    },
+    'google': {
+        'class': 'geocurrencies.countries.services.google',
+        'default_url': 'https://maps.googleapis.com/maps/api',
+        'reverse': {
+            'service': 'reverse/json',
+        },
+        'search': {
+            'service': 'geocode/json',  # Path of the search service
+            'address': 'address'
+        }
+    }
+
+}
