dcbb7c45a6e02ed7c8f6e93afc11fca222e4a338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:03 +0000

    Add package pybo/0.7.9/pybo-0.7.9.tar.gz

---
+++ packages/pybo/pybo-0.7.9.tar.gz/pybo-0.7.9/tests/test_cli.py
@@ -0,0 +1,21 @@
+from click.testing import CliRunner
+
+from pybo.cli import cli, profile_update
+
+
+def test_tok():
+    runner = CliRunner()
+    runner.invoke(cli, ["tok", "tests/resources/shelving/", "--tags", "pl"])
+
+
+def test_extract_rules():
+    runner = CliRunner()
+    runner.invoke(cli, ["extract-rules", "tests/resources/step2/"])
+
+def test_convert_cql2hfr():
+    runner = CliRunner()
+    runner.invoke(cli, ["convert-cql2hfr", "tests/resources/step2/cql_rules.txt"])
+
+def test_convert_hfr2cql():
+    runner = CliRunner()
+    runner.invoke(cli, ["convert-hfr2cql", "tests/resources/step2/hfr.txt"])
