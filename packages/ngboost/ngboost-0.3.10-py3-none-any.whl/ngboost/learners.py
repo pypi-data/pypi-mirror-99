053c746bcc4eabfd83ffb11c799015922bb61b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:52 +0000

    Add package ngboost/0.3.10/ngboost-0.3.10-py3-none-any.whl

---
+++ packages/ngboost/ngboost-0.3.10-py3-none-any.whl/ngboost/learners.py
@@ -0,0 +1,14 @@
+from sklearn.linear_model import Ridge
+from sklearn.tree import DecisionTreeRegressor
+
+default_tree_learner = DecisionTreeRegressor(
+    criterion="friedman_mse",
+    min_samples_split=2,
+    min_samples_leaf=1,
+    min_weight_fraction_leaf=0.0,
+    max_depth=3,
+    splitter="best",
+    random_state=None,
+)
+
+default_linear_learner = Ridge(alpha=0.0, random_state=None)
