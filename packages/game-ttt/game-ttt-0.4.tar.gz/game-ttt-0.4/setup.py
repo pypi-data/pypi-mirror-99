cd4f3549a70f6d9814cda48e1f1dac28826998a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:57 +0000

    Add package game-ttt/0.4/game-ttt-0.4.tar.gz

---
+++ packages/game-ttt/game-ttt-0.4.tar.gz/game-ttt-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='game-ttt',
+      version='0.4',
+      description='play the game tic_tac_toe',
+      packages=['game_ttt'],
+      author='Dhiraj Kumar Chaudhary',
+      author_email='dhirajc963@gmail.com')
