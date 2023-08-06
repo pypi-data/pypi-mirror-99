9fcf6e24d081d4530b812169471529c17c592943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:35 +0000

    Add package clibf/1.0.2/clibf-1.0.2.tar.gz

---
+++ packages/clibf/clibf-1.0.2.tar.gz/clibf-1.0.2/README.md
@@ -0,0 +1,31 @@
+# clibf
+
+## Install it from PyPi
+
+```
+pip install clibf
+```
+
+## Install it from GitHub
+
+```
+pip install git+https://github.com/donno2048/bf-interpreter
+```
+
+## Execute a brainfuck file
+
+```
+clibf -f my_bf_script.b
+```
+
+## Execute a string as a brainfuck code
+
+```
+clibf -s "my_bf_string"
+```
+
+## Execute a file with a GUI
+
+```
+clibf -g
+```
---
+++ packages/clibf/clibf-1.0.2.tar.gz/clibf-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup,find_packages
+setup(
+    name='clibf',
+    version='1.0.2',
+    description='CLI tool to execute Brainfuck code from files and strings',
+    long_description=open('README.md').read().replace('my_bf_string', open('example.b').read()),
+    long_description_content_type="text/markdown",
+    url='https://github.com/donno2048/bf-interpreter',
+    packages=find_packages(),
+    license='MIT',
+    author='Elisha Hollander',
+    classifiers=['Programming Language :: Python :: 3'],
+    entry_points={ 'console_scripts': [ 'clibf=clibf.__main__:main' ] }
+)
