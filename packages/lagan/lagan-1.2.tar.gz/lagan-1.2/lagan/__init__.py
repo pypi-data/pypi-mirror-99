6dfee8a1140986bc15938cbc8d4e05ae3daca8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:11 +0000

    Add package lagan/1.2/lagan-1.2.tar.gz

---
+++ packages/lagan/lagan-1.2.tar.gz/lagan-1.2/lagan/__init__.py
@@ -0,0 +1,4 @@
+from lagan.lagan import *
+from lagan.error import *
+
+__version__ = '1.2'
---
+++ packages/lagan/lagan-1.2.tar.gz/lagan-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+filepath = 'README.md'
+setup(
+        name="lagan",
+        version="1.2",
+        description="log system",
+        long_description=open(filepath, encoding='utf-8').read(),
+        long_description_content_type="text/markdown",
+        author="jdh99",
+        author_email="jdh821@163.com",
+        url="https://github.com/jdhxyy/lagan-python",
+        packages=find_packages(),
+        data_files=[filepath]
+    )
