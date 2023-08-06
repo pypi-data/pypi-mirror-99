f8702c0aa2c09c65d91037518ee534218e437116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:50 +0000

    Add package mypy-boto3-codeguruprofiler/1.17.33.0/mypy_boto3_codeguruprofiler-1.17.33.0-py3-none-any.whl

---
+++ packages/mypy-boto3-codeguruprofiler/mypy_boto3_codeguruprofiler-1.17.33.0-py3-none-any.whl/mypy_boto3_codeguruprofiler/paginator.pyi
@@ -0,0 +1,53 @@
+"""
+Main interface for codeguruprofiler service client paginators.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_codeguruprofiler import CodeGuruProfilerClient
+    from mypy_boto3_codeguruprofiler.paginator import (
+        ListProfileTimesPaginator,
+    )
+
+    client: CodeGuruProfilerClient = boto3.client("codeguruprofiler")
+
+    list_profile_times_paginator: ListProfileTimesPaginator = client.get_paginator("list_profile_times")
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Iterator
+
+from botocore.paginate import Paginator as Boto3Paginator
+
+from mypy_boto3_codeguruprofiler.type_defs import (
+    ListProfileTimesResponseTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = ("ListProfileTimesPaginator",)
+
+class ListProfileTimesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListProfileTimes documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.33/reference/services/codeguruprofiler.html#CodeGuruProfiler.Paginator.ListProfileTimes)
+    """
+
+    def paginate(
+        self,
+        endTime: datetime,
+        period: Literal["PT5M", "PT1H", "P1D"],
+        profilingGroupName: str,
+        startTime: datetime,
+        orderBy: Literal["TimestampDescending", "TimestampAscending"] = None,
+        PaginationConfig: PaginatorConfigTypeDef = None,
+    ) -> Iterator[ListProfileTimesResponseTypeDef]:
+        """
+        [ListProfileTimes.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.33/reference/services/codeguruprofiler.html#CodeGuruProfiler.Paginator.ListProfileTimes.paginate)
+        """
