7df87e20bb9d3eea26db356f798304eafc1dd175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:10 +0000

    Add package openbci-stream/1.0.0rc1/openbci_stream-1.0.0rc1-py3-none-any.whl

---
+++ packages/openbci-stream/openbci_stream-1.0.0rc1-py3-none-any.whl/openbci_stream/daemons/stream_rpyc.py
@@ -0,0 +1,49 @@
+"""
+=======================
+Distributed acquisition
+=======================
+
+RPyC (Remote Python Call) is the way to handle OpenBCI boards attached remotely.
+
+For examples and descriptions refers to documentation:
+`Data Acquisition - Distributed acquisition <../03-data_acquisition.ipynb/#Distributed-acquisition>`_
+"""
+
+import rpyc
+from openbci_stream.acquisition import CytonRFDuino, CytonWiFi
+
+from openbci_stream.utils import autokill_process
+autokill_process(name='stream_rpyc')
+
+
+########################################################################
+class StremamService(rpyc.Service):
+    """Server with RPyC for control OpenBCI board remotely."""
+
+    # ----------------------------------------------------------------------
+    def exposed_CytonRFDuino(self, *args, **kwargs):
+        return CytonRFDuino(*args, **kwargs)
+
+    # ----------------------------------------------------------------------
+    def exposed_CytonWiFi(self, *args, **kwargs):
+        """"""
+        return CytonWiFi(*args, **kwargs)
+
+
+# ----------------------------------------------------------------------
+def start_service() -> None:
+    """Start the rpyc server."""
+    from rpyc.utils.server import ThreadedServer
+
+    t = ThreadedServer(StremamService,
+                       port=18861,
+                       protocol_config={
+                           'allow_public_attrs': True,
+                           'allow_pickle': True,
+                       }
+                       )
+    t.start()
+
+
+if __name__ == "__main__":
+    start_service()
