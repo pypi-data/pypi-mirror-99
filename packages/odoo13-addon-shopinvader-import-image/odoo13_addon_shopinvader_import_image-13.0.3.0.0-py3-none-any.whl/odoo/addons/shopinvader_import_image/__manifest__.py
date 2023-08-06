6983a7a9cf257fea3daf7404230b3e800e8d5b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:37 +0000

    Add package odoo13-addon-shopinvader-import-image/13.0.3.0.0/odoo13_addon_shopinvader_import_image-13.0.3.0.0-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-import-image/odoo13_addon_shopinvader_import_image-13.0.3.0.0-py3-none-any.whl/odoo/addons/shopinvader_import_image/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2018 Akretion (http://www.akretion.com).
+# Author: Sylvain Calador (<https://www.akretion.com>)
+# Author: Saritha Sahadevan (<https://www.cybrosys.com>)
+# Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
+# @author Simone Orsi <simone.orsi@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Import Shopinvader product image",
+    "version": "13.0.3.0.0",
+    "summary": "Import product images",
+    "author": "Akretion, Camptocamp",
+    "company": "Akretion",
+    "maintainer": "Akretion",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "category": "Product",
+    "depends": ["shopinvader_image"],
+    "external_dependencies": {"python": ["python-magic", "validators"]},
+    "data": [
+        "data/ir_cron.xml",
+        "data/queue_job_channel_data.xml",
+        "data/queue_job_function_data.xml",
+        "security/ir_model_access.xml",
+        "views/import_product_image_view.xml",
+        "views/report_html.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
