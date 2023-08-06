4a860202c6b37e792faeb1ee5ea01c829bfd2b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:19 +0000

    Add package regru-cloudapi/1.2.4/regru_cloudapi-1.2.4.tar.gz

---
+++ packages/regru-cloudapi/regru_cloudapi-1.2.4.tar.gz/regru_cloudapi-1.2.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+
+setup(
+    name='regru_cloudapi',
+    description='Reg.ru CloudAPI Python library',
+    version='1.2.4',
+    url='https://github.com/plvskiy/regru_cloudapi',
+    author='Matthew Polovskiy',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    license='MIT',
+
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License']
+)
+
+
