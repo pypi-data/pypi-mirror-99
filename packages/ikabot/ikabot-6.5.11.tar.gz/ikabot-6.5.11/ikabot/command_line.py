08dfaeec15804c7fa414b3cf268db2aa85dde9c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:05 +0000

    Add package ikabot/6.5.11/ikabot-6.5.11.tar.gz

---
+++ packages/ikabot/ikabot-6.5.11.tar.gz/ikabot-6.5.11/ikabot/command_line.py
@@ -0,0 +1,204 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import gettext
+import multiprocessing
+import time
+import datetime
+from ikabot.config import *
+from ikabot.web.session import *
+from ikabot.helpers.gui import *
+from ikabot.function.donate import donate
+from ikabot.function.update import update
+from ikabot.helpers.pedirInfo import read
+from ikabot.function.getStatus import getStatus
+from ikabot.function.donationBot import donationBot
+from ikabot.helpers.botComm import updateTelegramData, telegramDataIsValid
+from ikabot.helpers.process import updateProcessList
+from ikabot.function.constructionList import constructionList
+from ikabot.function.searchForIslandSpaces import searchForIslandSpaces
+from ikabot.function.alertAttacks import alertAttacks
+from ikabot.function.vacationMode import vacationMode
+from ikabot.function.activateMiracle import activateMiracle
+from ikabot.function.trainArmy import trainArmy
+from ikabot.function.sellResources import sellResources
+from ikabot.function.checkForUpdate import checkForUpdate
+from ikabot.function.distributeResources import distributeResources
+from ikabot.function.alertLowWine import alertLowWine
+from ikabot.function.buyResources import buyResources
+from ikabot.function.loginDaily import loginDaily
+from ikabot.function.sendResources import sendResources
+from ikabot.function.constructBuilding import constructBuilding
+from ikabot.function.shipMovements import shipMovements
+from ikabot.function.importExportCookie import importExportCookie
+from ikabot.function.autoPirate import autoPirate
+from ikabot.function.investigate import investigate
+from ikabot.function.attackBarbarians import attackBarbarians
+from ikabot.function.proxyConf import proxyConf, show_proxy
+from ikabot.function.killTasks import killTasks 
+from ikabot.function.decaptchaConf import decaptchaConf
+
+t = gettext.translation('command_line', localedir, languages=languages, fallback=True)
+_ = t.gettext
+
+
+def menu(session, checkUpdate=True):
+	"""
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+	checkUpdate : bool
+	"""
+	if checkUpdate:
+		checkForUpdate()
+
+	show_proxy(session)
+
+	banner()
+
+	process_list = updateProcessList(session)
+	if len(process_list) > 0:
+		print('|{:^6}|{:^35}|{:^15}|'.format('pid', 'task', 'date'))
+		print('_'*60)
+		for process in process_list:
+			if 'date' in process:
+				print('|{:^6}|{:^35}|{:^15}|'.format(process['pid'], process['action'], datetime.datetime.fromtimestamp(process['date']).strftime('%b %d %H:%M:%S')))
+			else:
+				print('|{:^6}|{:^35}|'.format(process['pid'], process['action']))
+
+		print('')
+
+	menu_actions = {
+		1:			constructionList,
+		2:			sendResources,
+		3:			distributeResources,
+		4:			getStatus,
+		5:			donate,
+		6:			searchForIslandSpaces,
+		7:			loginDaily,
+		8:			alertAttacks,
+		9:			donationBot,
+		10:			alertLowWine,
+		11:			buyResources,
+		12:			sellResources,
+		13:			vacationMode,
+		14:			activateMiracle,
+		15:			trainArmy,
+		16:			shipMovements,
+		17:			constructBuilding,
+		18:			update,
+		19:			importExportCookie,
+		20:			autoPirate,
+		21:			investigate,
+		22:			attackBarbarians,
+		24:			proxyConf,
+		25:			updateTelegramData,
+		26:			killTasks,
+		27:			decaptchaConf,
+					}
+
+	print(_('(0)  Exit'))
+	print(_('(1)  Construction list'))
+	print(_('(2)  Send resources'))
+	print(_('(3)  Distribute resources'))
+	print(_('(4)  Account status'))
+	print(_('(5)  Donate'))
+	print(_('(6)  Search for new spaces'))
+	print(_('(7)  Login daily'))
+	print(_('(8)  Alert attacks'))
+	print(_('(9)  Donate automatically'))
+	print(_('(10) Alert wine running out'))
+	print(_('(11) Buy resources'))
+	print(_('(12) Sell resources'))
+	print(_('(13) Activate vacation mode'))
+	print(_('(14) Activate miracle'))
+	print(_('(15) Train army'))
+	print(_('(16) See movements'))
+	print(_('(17) Construct building'))
+	print(_('(18) Update Ikabot'))
+	print(_('(19) Import / Export cookie'))
+	print(_('(20) Auto-Pirate'))
+	print(_('(21) Investigate'))
+	print(_('(22) Attack barbarians'))
+	print(_('(23) Options / Settings'))
+	total_options = len(menu_actions) + 1
+	selected = read(min=0, max=total_options, digit=True)
+
+	if selected == 23:
+		banner()
+		print(_('(0) Back'))
+		print(_('(1) Configure Proxy'))
+		if telegramDataIsValid(session):
+			print(_('(2) Change the Telegram data'))
+		else:
+			print(_('(2) Enter the Telegram data'))
+		print(_('(3) Kill tasks'))
+		print(_('(4) Configure captcha resolver'))
+
+		selected = read(min=0, max=4, digit=True)
+		if selected > 0:
+			selected += 23
+
+	if selected != 0:
+		try:
+			event = multiprocessing.Event()  # creates a new event
+			process = multiprocessing.Process(target=menu_actions[selected], args=(session, event, sys.stdin.fileno(), config.predetermined_input), name=menu_actions[selected].__name__)
+			process.start()
+			process_list.append({'pid': process.pid, 'action': menu_actions[selected].__name__, 'date': time.time()})
+			updateProcessList(session, programprocesslist=process_list)
+			event.wait()  # waits for the process to fire the event that's been given to it. When it does  this process gets back control of the command line and asks user for more input
+		except KeyboardInterrupt:
+			pass
+		menu(session, checkUpdate=False)
+	else:
+		if isWindows:
+			# in unix, you can exit ikabot and close the terminal and the processes will continue to execute
+			# in windows, you can exit ikabot but if you close the terminal, the processes will die
+			print(_('Closing this console will kill the processes.'))
+			enter()
+		clear()
+		os._exit(0)  # kills the process which executes this statement, but it does not kill it's child processes
+
+
+def init():
+	home = 'USERPROFILE' if isWindows else 'HOME'
+	os.chdir(os.getenv(home))
+	if not os.path.isfile(ikaFile):
+		open(ikaFile, 'w')
+		os.chmod(ikaFile, 0o600)
+
+
+def start():
+	init()
+	for arg in sys.argv:
+		try:
+			config.predetermined_input.append(int(arg))
+		except ValueError:
+			config.predetermined_input.append(arg)
+	config.predetermined_input.pop(0)
+
+	session = Session()
+	try:
+		menu(session)
+	finally:
+		clear()
+		session.logout()
+
+
+def main():
+	manager = multiprocessing.Manager()
+	predetermined_input = manager.list()
+	config.predetermined_input = predetermined_input
+	try:
+		start()
+	except KeyboardInterrupt:
+		clear()
+
+
+if __name__ == '__main__':
+	# On Windows calling this function is necessary.
+	if sys.platform.startswith('win'):
+		multiprocessing.freeze_support()
+	main()
---
+++ packages/ikabot/ikabot-6.5.11.tar.gz/ikabot-6.5.11/ikabot/helpers/botComm.py
@@ -0,0 +1,200 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import os
+import json
+import random
+import gettext
+import sys
+import ikabot.web.session
+from ikabot.config import *
+import ikabot.config as config
+from ikabot.helpers.gui import *
+from ikabot.helpers.pedirInfo import read
+
+t = gettext.translation('botComm', localedir, languages=languages, fallback=True)
+_ = t.gettext
+
+
+def sendToBotDebug(session, msg, debugON):
+	"""This function will send the ``msg`` argument passed to it as a message to the user on Telegram, only if ``debugOn`` is ``True``
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+		Session object
+	msg : str
+		a string representing the message to send to the user on Telegram
+	debugON : bool
+		a boolean indicating whether or not to send the message.
+	"""
+	if debugON:
+		sendToBot(session, msg)
+
+
+def sendToBot(session, msg, Token=False, Photo=None):
+	"""This function will send the ``msg`` argument passed to it as a message to the user on Telegram
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+		Session object
+	msg : str
+		a string representing the message to send to the user on Telegram
+	Token : bool
+		a boolean indicating whether or not to attach the process id, the users server, world and Ikariam username to the message
+	Photo : bytes
+		a bytes object representing a picture to be sent.
+	"""
+	if checkTelegramData(session) is False:
+		return
+	if Token is False:
+		msg = 'pid:{}\n{}\n{}'.format(os.getpid(), config.infoUser, msg)
+
+	sessionData = session.getSessionData()
+	telegram_data = sessionData['shared']['telegram']
+	if Photo is None:
+		ikabot.web.session.normal_get('https://api.telegram.org/bot{}/sendMessage'.format(telegram_data['botToken']), params={'chat_id': telegram_data['chatId'], 'text': msg})
+	else:
+		resp = session.s.post('https://api.telegram.org/bot{}/sendDocument?chat_id={}&caption={}'.format(telegram_data['botToken'], telegram_data['chatId'], msg), files={'document': ('captcha.png', Photo)})
+
+
+def telegramDataIsValid(session):
+	"""This function checks whether or not there is any Telegram data stored in the .ikabot file
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+		Session object
+
+	Returns
+	-------
+	valid : bool
+		a boolean indicating whether or not there is any Telegram data stored in the .ikabot file
+
+	"""
+	sessionData = session.getSessionData()
+	try:
+		return len(sessionData['shared']['telegram']['botToken']) > 0 and len(sessionData['shared']['telegram']['chatId']) > 0
+	except KeyError:
+		return False
+
+
+def getUserResponse(session, fullResponse=False):
+	"""This function will retrieve a list of messages the user sent to the bot on Telegram.
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+		Session object
+
+	Returns
+	-------
+	updates : list[str]
+		a list containing all the messages the user sent to the bot on Telegram
+	"""
+	# returns messages that the user sends to the telegram bot
+
+	if checkTelegramData(session) is False:
+		return []
+
+	sessionData = session.getSessionData()
+	telegram_data = sessionData['shared']['telegram']
+
+	try:
+		updates = ikabot.web.session.normal_get('https://api.telegram.org/bot{}/getUpdates'.format(telegram_data['botToken'])).text
+		updates = json.loads(updates, strict=False)
+		if updates['ok'] is False:
+			return []
+		updates = updates['result']
+		# only return messages from the chatId of our user
+		if fullResponse:
+			return [update['message'] for update in updates if 'message' in update and update['message']['chat']['id'] == int(telegram_data['chatId'])]
+		else:
+			return [update['message']['text'] for update in updates if 'message' in update and update['message']['chat']['id'] == int(telegram_data['chatId'])]
+	except KeyError:
+		return []
+
+
+def checkTelegramData(session):
+	"""This function doesn't actually check any data itself, that is done by the ``telegramDataIsValid`` function. This function returns ``True`` if there is any Telegram data in the .ikabot file, and if there is none, it will ask the user to input it.
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+		Session object
+
+	Returns
+	-------
+	valid : bool
+		a boolean indicating whether or not there is valid Telegram data in the .ikabot file.
+	"""
+	if telegramDataIsValid(session):
+		return True
+	else:
+		banner()
+		print(_('You must provide valid credentials to communicate by telegram.'))
+		print(_('You require the token of the bot you are going to use and your chat_id'))
+		print(_('For more information about how to obtain them read the readme at https://github.com/physics-sp/ikabot'))
+		rta = read(msg=_('Will you provide the credentials now? [y/N]'), values=['y', 'Y', 'n', 'N', ''])
+		if rta.lower() != 'y':
+			return False
+		else:
+			return updateTelegramData(session)
+
+
+def updateTelegramData(session, event=None, stdin_fd=None, predetermined_input=[]):
+	"""This function asks the user to input the Telegram bot's token and the user's own Telegram chat id. After the user has inputted the neccessary data, this function will generate a random 4 digit number, send it to the user as a Telegram message using the token the user provided. It will then ask the user to input that number as validation.
+	Parameters
+	----------
+	session : ikabot.web.session.Session
+		Session object
+	event : multiprocessing.Event
+		an event which, when fired, gives back control of the terminal to the main process
+	stdin_fd : int
+		the standard input file descriptor passed to the function as a means of gaining control of the terminal
+	predetermined_input : multiprocessing.managers.SyncManager.list
+		a process synced list of predetermined inputs
+
+	Returns
+	-------
+	valid : bool
+		a boolean indicating whether or not the Telegram data has been successfully updated
+	"""
+	if event is not None and stdin_fd is not None:
+		sys.stdin = os.fdopen(stdin_fd)  # give process access to terminal
+	config.predetermined_input = predetermined_input
+	banner()
+	botToken = read(msg=_('Bot\'s token:'))
+	chat_id = read(msg=_('Chat_id:'))
+
+	telegram_data = {}
+	telegram_data['telegram'] = {}
+	telegram_data['telegram']['botToken'] = botToken.replace(' ', '').replace('.', '')
+	telegram_data['telegram']['chatId'] = chat_id
+	session.setSessionData(telegram_data, shared=True)
+
+	rand = str(random.randint(0, 9999)).zfill(4)
+	msg = _('The token is:{}').format(rand)
+	sendToBot(session, msg, Token=True)
+
+	rta = read(msg=_('A message was sent by telegram, did you receive it? [Y/n]'), values=['y', 'Y', 'n', 'N', ''])
+	if rta.lower() == 'n':
+		valid = False
+	else:
+		recibido = read(msg=_('Enter the received token in telegram:'))
+		if rand != recibido:
+			print(_('The token is incorrect'))
+			valid = False
+		else:
+			print(_('The token is correct'))
+			valid = True
+
+	if valid is False:
+		telegram_data['telegram']['botToken'] = ''
+		telegram_data['telegram']['chatId'] = ''
+		session.setSessionData(telegram_data, shared=True)
+		print(_('Check the credentials and re-supply them.'))
+	else:
+		print(_('The data was saved.'))
+	enter()
+
+	if event is not None and stdin_fd is not None:
+		event.set()  # give main process control before exiting
+	return valid
