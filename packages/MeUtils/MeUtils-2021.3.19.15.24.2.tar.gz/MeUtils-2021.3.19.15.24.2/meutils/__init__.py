7c96ec6abe9152aa1c28415a8e5e9127ae9fb159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:40 +0000

    Add package MeUtils/2021.3.19.15.24.2/MeUtils-2021.3.19.15.24.2.tar.gz

---
+++ packages/MeUtils/MeUtils-2021.3.19.15.24.2.tar.gz/MeUtils-2021.3.19.15.24.2/meutils/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : X.
+# @File         : __init__.py
+# @Time         : 2020/11/12 10:54 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  :
+
+
+
