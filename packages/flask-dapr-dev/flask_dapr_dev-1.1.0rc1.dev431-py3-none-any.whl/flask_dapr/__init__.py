a66cbf149cbf4fb21308dfd58e6dfa120eb62cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:01 +0000

    Add package flask-dapr-dev/1.1.0rc1.dev431/flask_dapr_dev-1.1.0rc1.dev431-py3-none-any.whl

---
+++ packages/flask-dapr-dev/flask_dapr_dev-1.1.0rc1.dev431-py3-none-any.whl/flask_dapr/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) Microsoft Corporation and Dapr Contributors.
+Licensed under the MIT License.
+"""
+
+from .actor import DaprActor
+
+__all__ = ['DaprActor']
