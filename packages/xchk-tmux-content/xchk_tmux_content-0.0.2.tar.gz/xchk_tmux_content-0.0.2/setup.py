90926f9bc79b7b87778d8b5b3e8c21e0b83c5a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:15 +0000

    Add package xchk-tmux-content/0.0.2/xchk_tmux_content-0.0.2.tar.gz

---
+++ packages/xchk-tmux-content/xchk_tmux_content-0.0.2.tar.gz/xchk_tmux_content-0.0.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xchk_tmux_content']
+
+package_data = \
+{'': ['*'],
+ 'xchk_tmux_content': ['static/*',
+                       'static/xchk_tmux_content/images/*',
+                       'templates/xchk_tmux_content/*']}
+
+setup_kwargs = {
+    'name': 'xchk-tmux-content',
+    'version': '0.0.2',
+    'description': 'Course material related to tmux for the xchk teaching framework',
+    'long_description': None,
+    'author': 'Vincent Nys',
+    'author_email': 'vincentnys@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
