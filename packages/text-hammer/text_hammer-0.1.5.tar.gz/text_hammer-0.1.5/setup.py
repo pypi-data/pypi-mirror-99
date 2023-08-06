bcaa66b1b81a9a325f884fd2b0eef7e93e353ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:32 +0000

    Add package text-hammer/0.1.5/text_hammer-0.1.5.tar.gz

---
+++ packages/text-hammer/text_hammer-0.1.5.tar.gz/text_hammer-0.1.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'text_hammer', #this should be unique,
+	version = '0.1.5',
+	include_package_data=True,
+	author = 'Abhishek Jaiswal',
+	author_email = 'abhishek.jaiswal26102001@gmail.com',
+	description = 'This is text preprocessing package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	install_requires = ['beautifulsoup4==4.9.1','pandas','numpy','spacy','TextBlob'],
+	license = 'Apache License 2.0'
+	)
