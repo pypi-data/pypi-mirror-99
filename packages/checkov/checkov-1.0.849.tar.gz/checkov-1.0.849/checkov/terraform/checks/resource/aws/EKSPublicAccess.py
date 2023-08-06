df171afaa3f09e4f93248726b9676e978a97622c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:29 +0000

    Add package checkov/1.0.849/checkov-1.0.849.tar.gz

---
+++ packages/checkov/checkov-1.0.849.tar.gz/checkov-1.0.849/checkov/terraform/checks/resource/aws/EKSPublicAccess.py
@@ -0,0 +1,20 @@
+from checkov.terraform.checks.resource.base_resource_value_check import BaseResourceValueCheck
+from checkov.common.models.enums import CheckCategories
+
+
+class EKSPublicAccess(BaseResourceValueCheck):
+    def __init__(self):
+        name = "Ensure Amazon EKS public endpoint disabled"
+        id = "CKV_AWS_39"
+        supported_resources = ['aws_eks_cluster']
+        categories = [CheckCategories.KUBERNETES]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def get_inspected_key(self):
+        return 'vpc_config/[0]/endpoint_public_access'
+
+    def get_expected_value(self):
+        return False
+
+
+check = EKSPublicAccess()
