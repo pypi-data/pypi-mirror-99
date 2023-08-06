89723dc47244b80bceb3620e6c450ccb84d141a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:46 +0000

    Add package mbed-tools/7.4.0/mbed_tools-7.4.0-py3-none-any.whl

---
+++ packages/mbed-tools/mbed_tools-7.4.0-py3-none-any.whl/mbed_tools/devices/_internal/exceptions.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2020-2021 Arm Limited and Contributors. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Exceptions internal to the package."""
+
+from mbed_tools.lib.exceptions import ToolsError
+
+
+class SystemException(ToolsError):
+    """Exception with regards to the underlying operating system."""
+
+
+class NoBoardForCandidate(ToolsError):
+    """Raised when board data cannot be determined for a candidate."""
