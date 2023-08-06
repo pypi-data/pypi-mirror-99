80de33e3a790c532b22ff77d94bd502cd1c0d5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:04 +0000

    Add package somde/0.1.8/somde-0.1.8.tar.gz

---
+++ packages/somde/somde-0.1.8.tar.gz/somde-0.1.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.1.8"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="somde",
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy>=1.17.4',
+        'pandas>=0.25.3',
+        'matplotlib>=3.1.1',
+        'somoclu>=1.7.5',
+        'patsy'
+        ],
+    author="Minsheng Hao",
+    author_email="hmsh653@gmail.com",
+    keywords=["spatial transcriptomics", "SpatialDE", "bioinformatics", "self organizing map(SOM)"],
+    description="Algorithm for finding gene spatial pattern based on Gaussian process accelerated by SOM",
+    license="MIT",
+    url='https://github.com/WhirlFirst/somde',
+    long_description_content_type='text/markdown',
+    long_description=long_description
+)
