f663d0491895d0741b2b6e12665fbef34277624e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:51 +0000

    Add package ardicsrtcollector/1.0.5/ardicsrtcollector-1.0.5.tar.gz

---
+++ packages/ardicsrtcollector/ardicsrtcollector-1.0.5.tar.gz/ardicsrtcollector-1.0.5/ardicsrtcollector/__init__.py
@@ -0,0 +1,39 @@
+# __init__.py
+"""
+it is used for the setup.py
+"""
+# Version of the url2srt package
+
+__version__ = "1.0.5"
+
+import sys
+import argparse
+from ardicsrtcollector.youtube_srt_mp3 import YoutubeSrtMp3
+
+
+def get_all_srt_mp3_files():
+    """
+    It is a helper function which checks arguments when its package is running
+    """
+    parser = argparse.ArgumentParser(description='To convert the Youtube URL'
+                                                 'to mp3 and srt file.')
+    parser.add_argument("-sv", '--save_path', type=str,
+                        help="Path to save converted files"
+                             "(default: downloads_convert)")
+    parser.add_argument('-ufp', '--url_file_path', type=str,
+                        required=True,
+                        help="A file which contains youtube URLs")
+
+    args = parser.parse_args()
+
+    if args.save_path is None:
+        print(' # -Path of converted files set to '
+              'default is \'downloads_convert\'')
+        YoutubeSrtMp3(urls_file_path=args.url_file_path).convert()
+    else:
+        YoutubeSrtMp3(urls_file_path=args.url_file_path,
+                      save_dir=args.save_path).convert()
+
+
+if __name__ == '__main__':
+    sys.exit(get_all_srt_mp3_files())
---
+++ packages/ardicsrtcollector/ardicsrtcollector-1.0.5.tar.gz/ardicsrtcollector-1.0.5/setup.py
@@ -0,0 +1,55 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as _in:
+        return _in.read()
+
+# This call to setup() does all the work
+setup(
+    name="ardicsrtcollector",
+    # The version of this library.
+    # Read this as
+    #   - MAJOR VERSION 1
+    #   - MINOR VERSION 0
+    #   - MAINTENANCE VERSION 0
+    version="1.0.5",
+    description="Generates a dataset for the Turkish speech recognition.",
+    # description and Project name| library name
+    long_description=read('README.rst'),
+    long_description_content_type="text/markdown",
+
+    author="ARDIC R&D",
+    author_email="yavuz.erzurumlu@ardictech.com",
+    url="https://github.com/IoT-Ignite/ArdicSrtCollector",
+     # These are the dependencies the library needs in order to run.
+    install_requires=[
+        'youtube-channel-transcript-api',
+        'youtube_dl',
+    ],
+
+    py_modules=["ardicsrtcollector/*" ],
+    packages=find_packages(where="ardicsrtcollector"),
+    packages_dir={'': 'ardicsrtcollector'},
+
+    include_package_data=True,
+
+   
+    entry_points={
+        "console_scripts": [
+            "ardicsrtcollector=ardicsrtcollector:get_all_srt_mp3_files",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers"
+    ],
+
+    # Here are the keywords of my library.
+    keywords='dataset, speech recognition, srt, youtube srt',
+    license="MIT",
+)
