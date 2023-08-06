8e9da3c6684e0c9251f35dbb57a570ceba53934c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:57 +0000

    Add package Adyen/5.0.0/Adyen-5.0.0.tar.gz

---
+++ packages/Adyen/Adyen-5.0.0.tar.gz/Adyen-5.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='Adyen',
+    packages=['Adyen'],
+    version='5.0.0',
+    maintainer='Adyen',
+    maintainer_email='support@adyen.com',
+    description='Adyen Python Api',
+    long_description="A Python client library for accessing Adyen APIs",
+    author='Adyen',
+    author_email='support@adyen.com',
+    url='https://github.com/Adyen/adyen-python-api-library',
+    keywords=['payments', 'adyen', 'fintech'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
