197dc1479ec757983891945828cfbe7b24817bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:29 +0000

    Add package veropt/0.4.3/veropt-0.4.3.tar.gz

---
+++ packages/veropt/veropt-0.4.3.tar.gz/veropt-0.4.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+install_requires = ["botorch", "dill", "click", "scikit-learn", "scipy", "matplotlib", "numpy", "xarray"]
+extras_require = {
+    "gui": ["PySide2"],
+    "multi_processing_smp": ["pathos"],
+    "mpi": ["mpi4py"]
+}
+
+setup(
+    name='veropt',
+    version='0.4.3',
+    packages=find_packages(),
+    url='https://github.com/idax4325/veropt',
+    license='OSI Approved :: MIT License',
+    author='Ida Stoustrup',
+    author_email='Ida.Stoustrup@gmail.com',
+    description='Bayesian Optimisation for the Versatile Ocean Simulator (VEROS)',
+    install_requires=install_requires,
+    extras_require=extras_require
+)
