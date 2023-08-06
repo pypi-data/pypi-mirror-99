7b537cef3b35a7cc537497916794a1304af466a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:21 +0000

    Add package ezst/0.2.46/ezst-0.2.46-py3-none-any.whl

---
+++ packages/ezst/ezst-0.2.46-py3-none-any.whl/ezst/colab/image.py
@@ -0,0 +1,27 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+import cv2
+
+def read(file_path):
+  return mpimg.imread(file_path)
+
+def save(file_path, img):
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+  cv2.imwrite(file_path, img)
+
+def show(img, grid=False, size=(12,12)):
+  plt.figure(figsize=size)
+  plt.grid(grid)
+  plt.imshow(img)
+  plt.show()
+
+def rectangle(img, lt, rb, thickness=None):
+  return cv2.rectangle(img, lt, rb, (255, 0, 0), thickness=thickness)
+
+def line(img, pt1, pt2, thickness=None):
+  cv2.line(img, pt1, pt2, (255, 0, 0), thickness=thickness)
+
+
+def circle(img, center, radius, thickness=None, lineType=None, shift=None):
+  return cv2.circle(img, center, radius, (255, 0, 0), thickness=thickness, lineType=lineType, shift=shift)
