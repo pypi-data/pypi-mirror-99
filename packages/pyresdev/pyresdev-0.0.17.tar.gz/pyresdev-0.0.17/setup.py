00111d22a858b9382140ce75147d5c7310ce2ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:13 +0000

    Add package pyresdev/0.0.17/pyresdev-0.0.17.tar.gz

---
+++ packages/pyresdev/pyresdev-0.0.17.tar.gz/pyresdev-0.0.17/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup,find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pyresdev",
+    version="0.0.17",
+    description="Bairesdev package used in DS",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Patricio Massaro",
+    author_email="patricio.massaro@bairesdev.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["numpy", "pandas", "regex"],
+)
