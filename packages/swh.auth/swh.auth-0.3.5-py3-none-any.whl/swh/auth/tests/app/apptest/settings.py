ac4a79800c3eb2c7dbe8117f874f2f5872349456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:58 +0000

    Add package swh.auth/0.3.5/swh.auth-0.3.5-py3-none-any.whl

---
+++ packages/swh.auth/swh.auth-0.3.5-py3-none-any.whl/swh/auth/tests/app/apptest/settings.py
@@ -0,0 +1,7 @@
+SECRET_KEY = "o+&ayiuk(y^wh4ijz5e=c2$$kjj7g^6r%z+8d*c0lbpfs##k#7"
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "swh.auth.tests.app.apptest.apps.TestApp",
+]
