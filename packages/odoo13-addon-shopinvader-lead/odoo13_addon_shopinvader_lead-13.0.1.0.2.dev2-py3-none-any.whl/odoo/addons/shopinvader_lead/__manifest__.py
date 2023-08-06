7fc6d2ff4499aa38b666656795bdc8e044145674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:26 +0000

    Add package odoo13-addon-shopinvader-lead/13.0.1.0.2.dev2/odoo13_addon_shopinvader_lead-13.0.1.0.2.dev2-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-lead/odoo13_addon_shopinvader_lead-13.0.1.0.2.dev2-py3-none-any.whl/odoo/addons/shopinvader_lead/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Shopinvader Lead",
+    "summary": "Shopinvader Lead Management",
+    "version": "13.0.1.0.1",
+    "category": "e-commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["shopinvader", "crm"],
+    "demo": ["demo/email_demo.xml", "demo/notification_demo.xml"],
+}
