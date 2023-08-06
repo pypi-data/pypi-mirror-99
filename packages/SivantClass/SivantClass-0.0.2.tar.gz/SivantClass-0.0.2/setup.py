5e2aa1b6e19a4f944bb0f9d71abf0c72e2a59040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:49 +0000

    Add package SivantClass/0.0.2/SivantClass-0.0.2.tar.gz

---
+++ packages/SivantClass/SivantClass-0.0.2.tar.gz/SivantClass-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="SivantClass",
+    version="0.0.2",
+    description="A Python package to get weather reports for any location.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sivant1361/sivant_hello",
+    author="Sivant M",
+    author_email="sivant1313@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["SivantClass"],
+    include_package_data=True,
+    # entry_points={
+    #     "console_scripts": [
+    #         "weather-reporter=weather_reporter.cli:main",sivant9994532266
+
+    #     ]
+    # },
+)
