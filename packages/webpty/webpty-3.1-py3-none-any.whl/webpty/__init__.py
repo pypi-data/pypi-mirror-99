cb6c6829aebb86cbed2f253ea321053347a3eade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package webpty/3.1/webpty-3.1-py3-none-any.whl

---
+++ packages/webpty/webpty-3.1-py3-none-any.whl/webpty/__init__.py
@@ -0,0 +1,2 @@
+__author__ = "Satheesh Kumar <mail@satheesh.dev>"
+__version__ = "3.1"
---
+++ packages/webpty/webpty-3.1-py3-none-any.whl/webpty/static/webpty.js
@@ -0,0 +1,54 @@
+// Applying all terminal addons
+Terminal.applyAddon(fullscreen);
+Terminal.applyAddon(fit);
+Terminal.applyAddon(webLinks);
+
+const delay = 50;
+const host = window.location.host;
+const pathname = window.location.pathname;
+const terminal = new Terminal({
+  cursorBlink: true,
+  macOptionIsMeta: true,
+  scrollback: true,
+});
+let webSocketProtocol = "ws";
+if (window.location.protocol.indexOf("https") === 0) {
+  webSocketProtocol = "wss";
+}
+const ws = new WebSocket(`${webSocketProtocol}://${host}${pathname}pty`);
+
+function fitToScreen() {
+  terminal.fit();
+  ws.send(
+    JSON.stringify({
+      action: "resize",
+      data: { cols: terminal.cols, rows: terminal.rows },
+    })
+  );
+}
+
+function resizeDelay(func, delay) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), delay);
+  };
+}
+
+// all on event handlers
+window.onresize = resizeDelay(fitToScreen, delay);
+
+ws.onopen = function () {
+  terminal.open(document.getElementById("terminal"));
+  terminal.toggleFullScreen(true);
+  fitToScreen();
+};
+
+ws.onmessage = function (event) {
+  terminal.write(event.data);
+};
+
+terminal.on("key", (key, event) => {
+  ws.send(JSON.stringify({ action: "input", data: { key: key } }));
+});
