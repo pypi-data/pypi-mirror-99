7a4a69b21ef47465c97d181f685cca55707fb9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:40 +0000

    Add package odoo10-addon-base-sparse-field-list-support/10.0.1.0.0.99.dev7/odoo10_addon_base_sparse_field_list_support-10.0.1.0.0.99.dev7-py2-none-any.whl

---
+++ packages/odoo10-addon-base-sparse-field-list-support/odoo10_addon_base_sparse_field_list_support-10.0.1.0.0.99.dev7-py2-none-any.whl/odoo/addons/base_sparse_field_list_support/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Base Sparse Field List Support",
+    "summary": "add list support to convert_to_cache()",
+    "version": "10.0.1.0.0",
+    "category": "Technical Settings",
+    "website": "https://github.com/shopinvader/odoo-misc",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+}
