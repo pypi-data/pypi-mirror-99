ad6254447977ebd9795413d900406f7b94d5307d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:24 +0000

    Add package ezst/0.2.58/ezst-0.2.58-py3-none-any.whl

---
+++ packages/ezst/ezst-0.2.58-py3-none-any.whl/ezst/cv/__init__.py
@@ -0,0 +1,32 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import requests
+
+import cv2
+from PIL import ImageFont
+
+def read(file_path):
+  return mpimg.imread(file_path)
+
+def save(file_path, img):
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+  cv2.imwrite(file_path, img)
+
+def show(img, grid=False, size=(12,12)):
+  plt.figure(figsize=size)
+  plt.grid(grid)
+  plt.imshow(img)
+  plt.show()
+
+def rectangle(img, lt, rb, thickness=3, color=(255, 0, 0)):
+  return cv2.rectangle(img, lt, rb, color, thickness=thickness)
+
+def line(img, pt1, pt2, thickness=3, color=(255, 0, 0)):
+  return cv2.line(img, pt1, pt2, color, thickness=thickness)
+
+def circle(img, center, axes, thickness=3, color=(255, 0, 0)):
+  return cv2.circle(img, center, radius, color, thickness=thickness)
+
+
+def text(img, text, pt1, size=2, color=(255, 0, 0)):
+  return cv2.putText(img, text, pt1, cv2.CV_FONT_HERSHEY_SIMPLEX, size,color,2)
