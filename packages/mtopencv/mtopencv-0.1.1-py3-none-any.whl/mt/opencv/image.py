08ace421c5f859cf8ab5664d1eea130063e8130d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:33 +0000

    Add package mtopencv/0.1.1/mtopencv-0.1.1-py3-none-any.whl

---
+++ packages/mtopencv/mtopencv-0.1.1-py3-none-any.whl/mt/opencv/image.py
@@ -0,0 +1,166 @@
+'''An self-contained image.'''
+
+
+import json as js
+import numpy as np
+import base64
+import turbojpeg as tj
+_tj = tj.TurboJPEG()
+
+import mt.base.path as bp
+
+
+__all__ = ['PixelFormat', 'Image', 'immload', 'immsave']
+
+
+
+PixelFormat = {
+    'rgb': (tj.TJPF_RGB, 3, tj.TJSAMP_422),
+    'bgr': (tj.TJPF_BGR, 3, tj.TJSAMP_422),
+    'rgba': (tj.TJPF_RGBA, 4, tj.TJSAMP_422),
+    'bgra': (tj.TJPF_BGRA, 4, tj.TJSAMP_422),
+    'argb': (tj.TJPF_ARGB, 4, tj.TJSAMP_422),
+    'abgr': (tj.TJPF_ABGR, 4, tj.TJSAMP_422),
+    'gray': (tj.TJPF_GRAY, 1, tj.TJSAMP_GRAY),
+}
+
+
+class Image(object):
+    '''A self-contained image, where the meta-data associated with the image are kept together with the image itself.
+
+    Parameters
+    ----------
+    image : numpy.array
+        a 2D image of shape (height, width, nchannels) or (height, width) with dtype uint8
+    pixel_format : str
+        one of the keys in the PixelFormat mapping
+    meta : dict
+        A JSON-like object. It holds additional keyword parameters associated with the image.
+    '''
+    
+    def __init__(self, image, pixel_format='rgb', meta={}):
+        self.image = np.ascontiguousarray(image) # need to be contiguous
+        self.pixel_format = pixel_format
+        self.meta = meta
+
+    def check(self):
+        '''Checks for data consistency, raising a ValueError if something has gone wrong.'''
+        if len(self.image.shape) == 2:
+            if self.pixel_format != 'gray':
+                raise ValueError("Pixel format is not 'gray' but image has only one channel.")
+            self.image = self.image.reshape(self.image.shape + (1,))
+        elif len(self.image.shape) == 3:
+            desired_nchannels = PixelFormat[self.pixel_format][1]
+            if self.image.shape[2] != desired_nchannels:
+                raise ValueError("Unexpected number of channels {}. It should be {} for pixel format '{}'.".format(self.image.shape[2], desired_nchannels, self.pixel_format))
+        else:
+            raise ValueError("Unexpected image shape {}. It must have 2 or 3 dimensions.".format(self.image.shape))
+
+    # ---- serialisation -----
+
+    def to_json(self, quality=90):
+        '''Dumps the image to a JSON-like object.
+
+        Parameters
+        ----------
+        quality : int
+            percentage of image quality. Default is 90.
+
+        Returns
+        -------
+        json_obj : dict
+            the serialised json object
+        '''
+
+        # meta
+        json_obj = {}
+        json_obj['pixel_format'] = self.pixel_format
+        json_obj['height'] = self.image.shape[0]
+        json_obj['width'] = self.image.shape[1]
+        json_obj['meta'] = self.meta
+
+        # image
+        tj_params = PixelFormat[self.pixel_format]
+        img_bytes = _tj.encode(self.image, quality=quality, pixel_format=tj_params[0], jpeg_subsample=tj_params[2])
+        encoded = base64.b64encode(img_bytes)
+        json_obj['image'] = encoded.decode('ascii')
+
+        return json_obj
+
+    @staticmethod
+    def from_json(json_obj):
+        '''Loads the image from a JSON-like object produced by :func:`dumps`.
+
+        Parameters
+        ----------
+        json_obj : dict
+            the serialised json object
+
+        Returns
+        -------
+        Image
+            the loaded image with metadata
+        '''
+
+        # meta
+        pixel_format = json_obj['pixel_format']
+        meta = json_obj['meta']
+
+        decoded = base64.b64decode(json_obj['image'])
+        image = _tj.decode(decoded, pixel_format=PixelFormat[pixel_format][0])
+
+        return Image(image, pixel_format=pixel_format, meta=meta)
+
+
+def immload(fp):
+    '''Loads an image with metadata.
+
+    Parameters
+    ----------
+    fp : object
+        string representing a local filepath or an open readable file handle
+
+    Returns
+    -------
+    Image
+        the loaded image with metadata
+
+    Raises
+    ------
+    OSError
+        if an error occured while loading
+    '''
+
+    if isinstance(fp, str):
+        fp = open(fp, 'rt')
+    return Image.from_json(js.load(fp))
+
+
+def immsave(image, fp, file_mode=0o664, quality=90):
+    '''Saves an image with metadata to file.
+
+    Parameters
+    ----------
+    imm : Image
+        an image with metadata
+    fp : object
+        string representing a local filepath or an open writable file handle
+    file_mode : int
+        file mode to be set to using :func:`os.chmod`. Only valid if fp is a string. If None is given, no setting of file mode will happen.
+    quality : int
+        percentage of image quality. Default is 90.
+
+    Raises
+    ------
+    OSError
+        if an error occured while loading
+    '''
+
+    if isinstance(fp, str):
+        fp2 = open(fp, 'wt')
+        js.dump(image.to_json(quality=quality), fp2, indent=4)
+        if file_mode:  # chmod
+            bp.chmod(df_filepath, file_mode)
+    else:
+        js.dump(image.to_json(quality=quality), fp, indent=4)
+        
