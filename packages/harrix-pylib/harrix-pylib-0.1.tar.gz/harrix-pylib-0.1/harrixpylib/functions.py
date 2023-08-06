88bd1d61cce1681d6f8bb5a4c1488077e198cbd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:21 +0000

    Add package harrix-pylib/0.1/harrix-pylib-0.1.tar.gz

---
+++ packages/harrix-pylib/harrix-pylib-0.1.tar.gz/harrix-pylib-0.1/harrixpylib/functions.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+import shutil
+
+
+def path_to_pathlib(path):
+    """
+    If a string is passed to the function, it turns into Path from pathlib.
+    :param path: string of path or Path from pathlib.
+    """
+    if isinstance(path, str):
+        return Path(path)
+    return path
+
+
+def clear_directory(path):
+    """
+    This function clear directory with sub-directories
+    :param path: path of directory from pathlib
+    """
+    path = path_to_pathlib(path)
+    if path.is_dir():
+        shutil.rmtree(path)  # Remove folder
+    path.mkdir(parents=True, exist_ok=True)  # Add folder
+
+
+def open_file(filename):
+    """
+    This function clear directory with sub-directories
+    :param path: path of directory from pathlib
+    """
+    filename = path_to_pathlib(filename)
+    s = ""
+    with open(filename, 'r', encoding='utf8') as file:
+        s = file.read()
+    return s
+
+
+def save_file(text, full_filename):
+    filename = path_to_pathlib(full_filename)
+    with open(full_filename, 'w', encoding='utf8') as file:
+        file.write(text)
