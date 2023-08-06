e9aa81d1c25df70494d3821e4691ffef12be430e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:19 +0000

    Add package brook/0.0.6/brook-0.0.6-py3-none-any.whl

---
+++ packages/brook/brook-0.0.6-py3-none-any.whl/brook/models.py
@@ -0,0 +1,19 @@
+import torch
+
+
+def func1(a, b):
+    return a + b
+
+
+def func2(a, b, c):
+    return a + b + c
+
+
+def is_this_really():
+    return True
+
+
+def what_is_the_problem():
+    return False
+
+x = 1
