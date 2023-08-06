99a41ab46b9de10a2569bcb95f8b7fcb81d54254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:14 +0000

    Add package PerformanceFuzzer/0.0.3/PerformanceFuzzer-0.0.3.tar.gz

---
+++ packages/PerformanceFuzzer/PerformanceFuzzer-0.0.3.tar.gz/PerformanceFuzzer-0.0.3/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+install_requires = [
+    'numpy',
+]
+
+
+try:
+    # $ pip install pypandoc
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
+setuptools.setup (
+    name = 'PerformanceFuzzer',
+    version = '0.0.3',
+    license = 'GPL-3.0 License',
+
+    description = 'PerformanceFuzzer',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+
+    author = 'KKimj',
+    author_email = 'kkimj@hanyang.ac.kr',
+    url = 'https://github.com/KKimj/PerformanceFuzzer',
+
+    install_requires=install_requires,
+
+    py_modules=["performancefuzzer"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers = [
+        # https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Natural Language :: Korean",
+    ],
+    python_requires='>=3',
+)
