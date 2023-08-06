b4063952b40a3cfcc934c5b5d320fa132298d764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:54 +0000

    Add package cdk-ecr-image-scan-notify/0.0.134/cdk_ecr_image_scan_notify-0.0.134-py3-none-any.whl

---
+++ packages/cdk-ecr-image-scan-notify/cdk_ecr_image_scan_notify-0.0.134-py3-none-any.whl/cdk_ecr_image_scan_notify/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ecr._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-ecr-image-scan-notify",
+    "0.0.134",
+    __name__[0:-6],
+    "cdk-ecr-image-scan-notify@0.0.134.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
