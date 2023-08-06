8b69ca12bce1b9565a6035481f63756eab9606d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:32 +0000

    Add package TlsProxys/0.9.0/TlsProxys-0.9.0.tar.gz

---
+++ packages/TlsProxys/TlsProxys-0.9.0.tar.gz/TlsProxys-0.9.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'TlsProxys',
+    version = '0.9.0',
+    descriptor = 'a safe http proxy over tls',
+    long_description_content_type = 'text/markdown',
+    long_description = 'README.md',
+    author = 'github.com/loniceraLeo',
+    url = 'https://www.github.com/loniceraLeo/TlsProxy',
+    packages = ['TlsProxy'],
+    python_requires = '>=3.6.0',
+    license = 'GPL',
+    entry_points = {
+        'console_scripts':
+            [
+                'tpserver = TlsProxy.server:entry',
+                'tpclient = TlsProxy.client:entry'
+            ]
+    }
+)
