b2e9768cd4957d8fd341f95607c2b0744bb0551a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:09 +0000

    Add package Timecard-App/2.0.6/Timecard-App-2.0.6.tar.gz

---
+++ packages/Timecard-App/Timecard-App-2.0.6.tar.gz/Timecard-App-2.0.6/src/timecard/interface/app.py
@@ -0,0 +1,99 @@
+"""App [Timecard]
+Author(s): Jason C. McDonald
+
+The main application classes. Most of the interface modules rely on this,
+so it should not rely on any other module in the interface subpackage.
+"""
+
+import logging
+import os
+from pkg_resources import resource_filename
+
+from PySide2.QtWidgets import QApplication, QVBoxLayout, QWidget
+from PySide2.QtGui import QIcon
+
+from timecard.data.settings import Settings
+
+
+class MainWindow(QWidget):
+    def closeEvent(self, event):
+        """Hide the window on close."""
+        if Settings.get_persist():
+            App.hide_window()
+            event.ignore()
+        else:
+            event.accept()
+
+
+class App:
+    app = QApplication()
+    window = MainWindow()
+    layout = QVBoxLayout()
+
+    icon_path = resource_filename(
+        'timecard',
+        os.path.join('resources', 'timecard.svg')
+    )
+    icon = QIcon(icon_path)
+
+    notify_callback = None
+
+    @classmethod
+    def run(cls):
+        """Run the application."""
+        if cls.app is None:
+            return None
+
+        cls.show_window()
+
+        return cls.app.exec_()
+
+    @classmethod
+    def build(cls):
+        """Construct the application."""
+        logging.debug("Building application.")
+        cls.app.setApplicationName("Timecard")
+        cls.app.setApplicationVersion('2.0.6')
+        cls.app.setDesktopFileName('com.codemouse92.timecard')
+        cls.app.setWindowIcon(cls.icon)
+
+        logging.debug("Building main window.")
+        cls.window.resize(400, 400)
+        cls.window.setLayout(cls.layout)
+
+    @classmethod
+    def toggle_window(cls):
+        if cls.window.isVisible():
+            cls.hide_window()
+        else:
+            cls.show_window()
+
+    @classmethod
+    def show_window(cls):
+        """Display the main window."""
+        cls.window.show()
+
+    @classmethod
+    def hide_window(cls):
+        """Hide the main window."""
+        cls.window.hide()
+        cls.notify("Timecard is still running in the notification area.")
+
+    @classmethod
+    def add_widget(cls, widget):
+        """Add a widget to the main window."""
+        cls.layout.addWidget(widget)
+
+    @classmethod
+    def notify(cls, message):
+        if cls.notify_callback:
+            cls.notify_callback(message)
+
+    @classmethod
+    def connect(cls, notify=None):
+        if notify:
+            cls.notify_callback = notify
+
+    @classmethod
+    def quit(cls):
+        cls.app.quit()
---
+++ packages/Timecard-App/Timecard-App-2.0.6.tar.gz/Timecard-App-2.0.6/src/timecard/interface/workspace.py
@@ -0,0 +1,61 @@
+"""Workspace [Timecard]
+Author(s): Jason C. McDonald
+
+Switches between Views in the application.
+"""
+
+from PySide2.QtWidgets import QWidget, QStackedLayout
+
+from timecard.interface.aboutview import AboutView
+from timecard.interface.editview import EditView
+from timecard.interface.logview import LogView
+from timecard.interface.settingsview import SettingsView
+from timecard.interface.quitview import QuitView
+
+
+class Workspace:
+
+    widget = QWidget()
+    layout = QStackedLayout()
+    previous = None
+
+    @classmethod
+    def build(cls):
+        cls.widget.setLayout(cls.layout)
+        cls.layout.addWidget(LogView.build())
+        cls.layout.addWidget(AboutView.build())
+        cls.layout.addWidget(SettingsView.build())
+        cls.layout.addWidget(QuitView.build())
+        cls.layout.addWidget(EditView.build())
+
+        LogView.connect(edit=cls.set_mode_edit)
+        EditView.connect(done=cls.edit_done)
+
+        return cls.widget
+
+    @classmethod
+    def set_mode_timelog(cls):
+        cls.layout.setCurrentIndex(0)
+        LogView.refresh()
+
+    @classmethod
+    def set_mode_about(cls):
+        cls.layout.setCurrentIndex(1)
+
+    @classmethod
+    def set_mode_settings(cls):
+        cls.layout.setCurrentIndex(2)
+
+    @classmethod
+    def set_mode_quit(cls):
+        cls.layout.setCurrentIndex(3)
+
+    @classmethod
+    def set_mode_edit(cls, entry):
+        cls.layout.setCurrentIndex(4)
+        EditView.load_item(entry)
+
+    @classmethod
+    def edit_done(cls):
+        LogView.unselected()
+        cls.set_mode_timelog()
