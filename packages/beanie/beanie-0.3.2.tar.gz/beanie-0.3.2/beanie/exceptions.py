0153648d800639df02f934768cd83698db2399ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:21 +0000

    Add package beanie/0.3.2/beanie-0.3.2.tar.gz

---
+++ packages/beanie/beanie-0.3.2.tar.gz/beanie-0.3.2/beanie/exceptions.py
@@ -0,0 +1,18 @@
+class WrongDocumentUpdateStrategy(Exception):
+    pass
+
+
+class DocumentNotFound(Exception):
+    pass
+
+
+class DocumentAlreadyCreated(Exception):
+    pass
+
+
+class DocumentWasNotSaved(Exception):
+    pass
+
+
+class CollectionWasNotInitialized(Exception):
+    pass
