741135d935beb2b1ac057044543f1c774a8ac137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:39 +0000

    Add package kcli/99.0.202103241017/kcli-99.0.202103241017-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202103241017-py3-none-any.whl/kvirt/kubeadm/nfs.sh
@@ -0,0 +1,20 @@
+{% if ubuntu %} 
+apt-get -y install nfs-kernel-server
+{% else %}
+yum -y install nfs-utils
+{% endif %}
+for i in `seq -f "%03g" 1 20` ; do
+mkdir /pv${i}
+echo "/pv$i *(rw,no_root_squash)"  >>  /etc/exports
+chcon -t svirt_sandbox_file_t /pv${i}
+chmod 777 /pv${i}
+done
+exportfs -r
+systemctl start nfs-server ; systemctl enable nfs-server
+{% if masters > 1 %}
+IP="{{ api_ip }}"
+{% else %}
+IP=$(hostname -I | cut -d" " -f1)
+{% endif%}
+sed -i "s/IP/$IP/" /root/nfs.yml
+for i in `seq 1 20` ; do j=`printf "%03d" ${i}` ; sed "s/001/$j/" /root/nfs.yml | kubectl create -f - ; done
