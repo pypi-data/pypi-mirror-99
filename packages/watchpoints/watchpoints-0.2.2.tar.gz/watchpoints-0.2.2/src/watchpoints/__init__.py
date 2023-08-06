e6400b7296492a23c1581d61777bdf06b5c9004f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:52 +0000

    Add package watchpoints/0.2.2/watchpoints-0.2.2.tar.gz

---
+++ packages/watchpoints/watchpoints-0.2.2.tar.gz/watchpoints-0.2.2/src/watchpoints/__init__.py
@@ -0,0 +1,17 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/watchpoints/blob/master/NOTICE.txt
+
+
+from .watch import Watch
+
+__version__ = "0.2.2"
+
+
+all = [
+    "watch",
+    "unwatch"
+]
+
+
+watch = Watch()
+unwatch = watch.unwatch
