f0e9e9b963368438b32c2e2014ad983be4b26b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:05 +0000

    Add package tplink-cloud-api/2.3.0/tplink_cloud_api-2.3.0-py3-none-any.whl

---
+++ packages/tplink-cloud-api/tplink_cloud_api-2.3.0-py3-none-any.whl/tplinkcloud/device.py
@@ -0,0 +1,113 @@
+from .device_type import TPLinkDeviceType
+from .device_net_info import DeviceNetInfo
+from .device_time import DeviceTime
+
+
+class TPLinkDevice:
+
+    def __init__(self, client, device_id, device_info, child_id=None):
+        self.device_id = device_id
+        self.device_info = device_info
+        # child_ids are used for addressing children
+        self.child_id = child_id
+        self._client = client
+        self.model_type = TPLinkDeviceType.UNKNOWN
+
+    # This is expected to be overriden for devices that have children
+    def has_children(self):
+        return False
+
+    # This is expected to be overriden for devices that have children
+    def get_children(self):
+        return None
+
+    def get_alias(self):
+        return self.device_info.alias
+
+    # All device requests should go through here
+    def _pass_through_request(self, request_type, sub_request_type, request):
+        request_data = {
+            request_type: {
+                sub_request_type: request
+            }
+        }
+        if self.child_id:
+            request_data['context'] = {
+                'child_ids': [self.child_id] if self.child_id else None
+            }
+        response = self._client.pass_through_request(
+            self.device_id, request_data)
+        if not response:
+            return None
+
+        request_response = response.get(request_type)
+        sub_request_response = request_response.get(sub_request_type)
+        if self.child_id and sub_request_response.get('children'):
+            for child in sub_request_response.get('children'):
+                if child.get('id') == self.child_id:
+                    return child
+
+        return sub_request_response
+
+    def power_on(self):
+        return self._pass_through_request('system', 'set_relay_state', {'state': 1})
+
+    def power_off(self):
+        return self._pass_through_request('system', 'set_relay_state', {'state': 0})
+
+    def toggle(self):
+        if self.is_on():
+            self.power_off()
+        else:
+            self.power_on()
+
+    def _get_sys_info(self):
+        return self._pass_through_request('system', 'get_sysinfo', None)
+
+    # This is intended to be overriden by actual device
+    # implementations where sys info is well-defined
+    def get_sys_info(self):
+        return self._get_sys_info()
+
+    def is_on(self):
+        device_sys_info = self.get_sys_info()
+        sys_info = device_sys_info.__dict__ if hasattr(
+            device_sys_info, '__dict__') else device_sys_info
+        if self.child_id:
+            return sys_info['state'] == 1
+
+        return sys_info['relay_state'] == 1
+
+    def is_off(self):
+        device_sys_info = self.get_sys_info()
+        sys_info = device_sys_info.__dict__ if hasattr(
+            device_sys_info, '__dict__') else device_sys_info
+        if self.child_id:
+            return sys_info['state'] == 0
+
+        return sys_info['relay_state'] == 0
+
+    def set_led_state(self, on):
+        # This is intentional - follows the API contract
+        led_off_state = 0 if on else 1
+        return self._pass_through_request('set_led_off', 'off', led_off_state)
+
+    def get_schedule_rules(self):
+        return self._pass_through_request('schedule', 'get_rules', {})
+
+    def edit_schedule_rule(self, rule):
+        return self._pass_through_request('schedule', 'edit_rule', rule)
+
+    # Get SSID of network to which the device is connected
+    def get_net_info(self):
+        net_info = self._pass_through_request('netif', 'get_stainfo', None)
+        if net_info:
+            return DeviceNetInfo(net_info)
+        return None
+
+    # Get device current time
+    def get_time(self):
+        time = self._pass_through_request('time', 'get_time', {})
+        if time:
+            return DeviceTime(time)
+        return None
---
+++ packages/tplink-cloud-api/tplink_cloud_api-2.3.0-py3-none-any.whl/tplinkcloud/emeter_device.py
@@ -0,0 +1,70 @@
+from .device import TPLinkDevice
+
+
+class CurrentPower:
+
+    def __init__(self, realtime_data):
+        self.voltage_mv = realtime_data.get('voltage_mv')
+        self.current_ma = realtime_data.get('current_ma')
+        self.power_mw = realtime_data.get('power_mw')
+        self.total_wh = realtime_data.get('total_wh')
+
+
+class DayPowerSummary:
+
+    def __init__(self, day_data):
+        self.year = day_data.get('year')
+        self.month = day_data.get('month')
+        self.day = day_data.get('day')
+        self.energy_wh = day_data.get('energy_wh')
+
+
+class MonthPowerSummary:
+
+    def __init__(self, day_data):
+        self.year = day_data.get('year')
+        self.month = day_data.get('month')
+        self.energy_wh = day_data.get('energy_wh')
+
+
+class TPLinkEMeterDevice(TPLinkDevice):
+
+    def __init__(self, client, device_id, device_info, child_id=None):
+        super().__init__(client, device_id, device_info, child_id)
+
+    def get_power_usage_realtime(self):
+        realtime_data = self._pass_through_request(
+            'emeter', 
+            'get_realtime', 
+            None
+        )
+        if realtime_data.get('err_code') == 0:
+            return CurrentPower(realtime_data)
+        return None
+
+    def get_power_usage_day(self, year, month):
+        day_response_data = self._pass_through_request(
+            'emeter',
+            'get_daystat',
+            {
+                'year': year,
+                'month': month
+            }
+        )
+        # If there is no data for the requested month, data will be None
+        if day_response_data.get('err_code') == 0:
+            return [DayPowerSummary(day_data) for day_data in day_response_data['day_list']]
+        return []
+
+    def get_power_usage_month(self, year):
+        month_response_data = self._pass_through_request(
+            'emeter',
+            'get_monthstat',
+            {
+                'year': year
+            }
+        )
+        # If there is no data for the requested year, data will be None
+        if month_response_data.get('err_code') == 0:
+            return [MonthPowerSummary(month_data) for month_data in month_response_data['month_list']]
+        return []
