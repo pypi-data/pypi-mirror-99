19e97c4f39d6a33d108ce020517e991e01f5a0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:29 +0000

    Add package NoobHttp/0.1/NoobHttp-0.1.tar.gz

---
+++ packages/NoobHttp/NoobHttp-0.1.tar.gz/NoobHttp-0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="NoobHttp",
+    version="0.1",
+    author="dedshit",
+    author_email="zrpco#@yopmail.com",
+    description="HTTP request for armatures",
+    url="https://www.youtube.com/c/FunnyBunny-YT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.8',
+)
+
