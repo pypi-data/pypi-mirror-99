26c63652db6450b4ec929d992a859c3c022b5111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:00 +0000

    Add package firefly-messaging/0.1.27/firefly-messaging-0.1.27.tar.gz

---
+++ packages/firefly-messaging/firefly-messaging-0.1.27.tar.gz/firefly-messaging-0.1.27/setup.py
@@ -0,0 +1,45 @@
+#  Copyright (c) 2020 JD Williams
+#
+#  This file is part of Firefly, a Python SOA framework built by JD Williams. Firefly is free software; you can
+#  redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+#  Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+#
+#  Firefly is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+#  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#  Public License for more details. You should have received a copy of the GNU Lesser General Public
+#  License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  You should have received a copy of the GNU General Public License along with Firefly. If not, see
+#  <http://www.gnu.org/licenses/>.
+
+import setuptools
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='firefly-messaging',
+    version='0.1.27',
+    author="",
+    author_email="",
+    description="Put project description here.",
+    long_description=long_description,
+    url="",
+    entry_points={
+        'console_scripts': ['firefly=firefly.presentation.cli:main']
+    },
+    install_requires=[
+        'firefly-dependency-injection>=0.1',
+        'firefly-framework>=1.1.53',
+        'mailchimp3>=3.0.14'
+    ],
+    packages=setuptools.PEP420PackageFinder.find('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ]
+)
