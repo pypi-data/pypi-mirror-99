7660aad54770b09e4f58affc16060b23090268bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:53 +0000

    Add package sumapi/0.1.3/sumapi-0.1.3.tar.gz

---
+++ packages/sumapi/sumapi-0.1.3.tar.gz/sumapi-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sumapi",
+    version="0.1.3",
+    author="Summarify",
+    author_email="info@summarify.io",
+    description="API Library for SumAPI with Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/summarify/sumapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.5.5',
+    install_requires=["requests==2.25.0","tqdm==4.59.0"])
