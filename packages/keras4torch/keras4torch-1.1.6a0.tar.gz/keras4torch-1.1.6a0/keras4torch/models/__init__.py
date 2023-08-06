3b522c76501db5e15158a0a72a6eca54a52942d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:05 +0000

    Add package keras4torch/1.1.6a0/keras4torch-1.1.6a0.tar.gz

---
+++ packages/keras4torch/keras4torch-1.1.6a0.tar.gz/keras4torch-1.1.6a0/keras4torch/models/__init__.py
@@ -0,0 +1,3 @@
+from ._wrapper import Model
+
+__all__ = ['Model']
---
+++ packages/keras4torch/keras4torch-1.1.6a0.tar.gz/keras4torch-1.1.6a0/keras4torch/utils/data.py
@@ -0,0 +1,25 @@
+from torch.utils.data import Dataset
+
+class SlicedDataset(Dataset):
+    """
+    Create a sliced dataset. It just keeps a reference of `array` thus can avoid memory copy.
+
+    Args:
+
+    * `slice` (1D-array or list): The slice sequence.
+
+    * `array` (`ndarray` or `torch.Tensor`): Your data.
+    """
+    def __init__(self, slice, *array):
+        super(SlicedDataset, self).__init__()
+        self.array = array
+        self.slice = slice
+
+    def __len__(self):
+        return len(self.slice)
+
+    def __getitem__(self, index):
+        index = self.slice[index]
+        return [a[index] for a in self.array]
+
+__all__ = ['SlicedDataset']
