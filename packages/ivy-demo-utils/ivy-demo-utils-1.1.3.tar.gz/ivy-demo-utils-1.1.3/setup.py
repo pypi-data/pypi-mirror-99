78de0c0cd2812cccf52f082f05acc9b4a2723fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:50 +0000

    Add package ivy-demo-utils/1.1.3/ivy-demo-utils-1.1.3.tar.gz

---
+++ packages/ivy-demo-utils/ivy-demo-utils-1.1.3.tar.gz/ivy-demo-utils-1.1.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='ivy-demo-utils',
+      version='1.1.3',
+      description='Ivy Demo Utils provides a set of utilities for creating visual demos for Ivy libraries.\n'
+                  'Tested with Ivy 1.1.3',
+      author='Ivy Team',
+      author_email='ivydl.team@gmail.com',
+      packages=setuptools.find_packages(),
+      package_data={'': ['*.npy', '*.ttt']},
+      install_requires=['open3d'],
+      classifiers=['License :: OSI Approved :: Apache Software License'],
+      license='Apache 2.0'
+      )
