bfdd6bdfd10524cfb48b48938ce13f59f2e96763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:18:31 +0000

    Add package taxoniq/0.1.5/taxoniq-0.1.5.tar.gz

---
+++ packages/taxoniq/taxoniq-0.1.5.tar.gz/taxoniq-0.1.5/setup.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+import glob
+import itertools
+import os.path
+from setuptools import setup, find_packages, Extension
+
+MARISA_ROOT_DIR = "marisa-trie/marisa-trie"
+MARISA_SOURCE_DIR = os.path.join(MARISA_ROOT_DIR, "lib")
+MARISA_INCLUDE_DIR = os.path.join(MARISA_ROOT_DIR, "include")
+MARISA_FILES = [
+    "marisa/*.cc",
+    "marisa/grimoire.cc",
+    "marisa/grimoire/io/*.cc",
+    "marisa/grimoire/trie/*.cc",
+    "marisa/grimoire/vector/*.cc",
+]
+MARISA_FILES[:] = itertools.chain(
+    *(glob.glob(os.path.join(MARISA_SOURCE_DIR, path))
+      for path in MARISA_FILES))
+
+setup(
+    name="taxoniq",
+    version="0.1.5",
+    url="https://github.com/chanzuckerberg/taxoniq",
+    project_urls={
+        "Documentation": "https://chanzuckerberg.github.io/taxoniq",
+        "Source Code": "https://github.com/chanzuckerberg/taxoniq",
+        "Issue Tracker": "https://github.com/chanzuckerberg/taxoniq/issues"
+    },
+    license="MIT License",
+    author="Andrey Kislyuk",
+    author_email="akislyuk@chanzuckerberg.com",
+    description="Taxoniq: Taxon Information Query - fast, offline querying of NCBI Taxonomy and related data",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "zstandard >= 0.15.1",
+        "urllib3 >= 1.25.8",
+        "taxoniq-db == 2021.3.23"
+    ],
+    tests_require=["coverage", "flake8", "wheel"],
+    packages=find_packages(exclude=["test"]),
+    extras_require={
+        # "nt_accession_db": []
+        # "nr_accession_db": []
+    },
+    entry_points={
+        "console_scripts": [
+            "taxoniq=taxoniq.cli:cli"
+        ],
+    },
+    platforms=["MacOS X", "Posix"],
+    include_package_data=True,
+    test_suite="test",
+    libraries=[("libmarisa-trie", {
+        "sources": MARISA_FILES,
+        "include_dirs": [MARISA_SOURCE_DIR, MARISA_INCLUDE_DIR]
+    })],
+    ext_modules=[
+        Extension("taxoniq.vendored.marisa_trie", [
+            "marisa-trie/src/agent.cpp",
+            "marisa-trie/src/base.cpp",
+            "marisa-trie/src/iostream.cpp",
+            "marisa-trie/src/key.cpp",
+            "marisa-trie/src/keyset.cpp",
+            "marisa-trie/src/marisa_trie.cpp",
+            "marisa-trie/src/query.cpp",
+            "marisa-trie/src/std_iostream.cpp",
+            "marisa-trie/src/trie.cpp"
+        ], include_dirs=[MARISA_INCLUDE_DIR])
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
---
+++ packages/taxoniq/taxoniq-0.1.5.tar.gz/taxoniq-0.1.5/test/test.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import unittest
+import logging
+from concurrent.futures import ThreadPoolExecutor
+
+import taxoniq
+import taxoniq.cli
+import taxoniq.build
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class TestTaxoniq(unittest.TestCase):
+    def test_taxon_interface(self):
+        t = taxoniq.Taxon(1)
+        self.assertEqual([c.scientific_name for c in t.child_nodes],
+                         ['Viruses', 'cellular organisms', 'unclassified entries', 'other entries'])
+        t2 = taxoniq.Taxon(accession_id="NC_000913.3")
+        self.assertEqual(t2, taxoniq.Taxon(511145))
+        self.assertEqual(t2, taxoniq.Taxon(scientific_name="Escherichia coli str. K-12 substr. MG1655"))
+        self.assertEqual(t2.scientific_name, "Escherichia coli str. K-12 substr. MG1655")
+        self.assertEqual(t2.parent.parent.common_name, "E. coli")
+        self.assertEqual(t2.ranked_lineage, [taxoniq.Taxon(562), taxoniq.Taxon(561), taxoniq.Taxon(543),
+                                             taxoniq.Taxon(91347), taxoniq.Taxon(1236), taxoniq.Taxon(1224),
+                                             taxoniq.Taxon(2)])
+
+    @unittest.skipIf("CI" in os.environ, "Skippinng test that requires eukaryotic database")
+    def test_eukaryote_taxon_interface(self):
+        t = taxoniq.Taxon(accession_id="NC_000001.11")
+        self.assertEqual(t, taxoniq.Taxon(9606))
+        self.assertEqual(t.scientific_name, "Homo sapiens")
+        self.assertEqual(t.common_name, "human")
+        self.assertIn("anatomically modern human", t.description)
+        self.assertEqual(t.parent, taxoniq.Taxon(9605))
+        self.assertEqual(t.ranked_lineage, [taxoniq.Taxon(scientific_name='Homo sapiens'),
+                                            taxoniq.Taxon(scientific_name='Homo'),
+                                            taxoniq.Taxon(scientific_name='Hominidae'),
+                                            taxoniq.Taxon(scientific_name='Primates'),
+                                            taxoniq.Taxon(scientific_name='Mammalia'),
+                                            taxoniq.Taxon(scientific_name='Chordata'),
+                                            taxoniq.Taxon(scientific_name='Metazoa'),
+                                            taxoniq.Taxon(scientific_name='Eukaryota')])
+
+    def test_accession_interface(self):
+        a = taxoniq.Accession(accession_id="NC_000913.3")
+        self.assertEqual(a.length, 4641652)
+        self.assertEqual(a.tax_id, 511145)
+        with a.get_from_s3() as fh:
+            seq = fh.read()
+            assert seq.startswith(b"AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGCT")
+            assert seq.endswith(b"AATGTTGCACCGTTTGCTGCATGATATTGAAAAAAATATCACCAAATAAAAAACGCCTTAGTAAGTATTTTTC")
+            self.assertEqual(fh.read(), b"")
+        with a.get_from_s3() as fh:
+            self.assertEqual(fh.read(1), b"AGCT")
+
+    @unittest.skipIf("CI" in os.environ, "Skippinng test that requires eukaryotic database")
+    def test_eukaryote_accession_interface(self):
+        a = taxoniq.Accession(accession_id="NC_000001.11")
+        self.assertEqual(a.length, 248956420)
+        self.assertEqual(a.tax_id, 9606)
+        a = taxoniq.Accession(accession_id="NZ_CP019573.1")
+        self.assertEqual(a.length, 1745788)
+        self.assertEqual(a.tax_id, 1817405)
+
+    def test_refseq_index(self):
+        t = taxoniq.Taxon(scientific_name="Mumps orthorubulavirus")
+        self.assertEqual(t.refseq_genome_accessions, [taxoniq.Accession('AB040874.1')])
+
+    @unittest.skipIf("CI" in os.environ, "Skippinng test that requires eukaryotic database")
+    def test_refseq_retrieval(self):
+        def fetch_seq(accession):
+            seq = accession.get_from_s3().read()
+            return (accession, seq)
+        taxon = taxoniq.Taxon(scientific_name="Apis mellifera")
+        for accession, seq in ThreadPoolExecutor().map(fetch_seq, taxon.refseq_representative_genome_accessions):
+            assert accession.length == len(seq)
+
+    def test_wikipedia_client(self):
+        client = taxoniq.build.WikipediaDescriptionClient()
+        result_file = client.build_index(destdir="/tmp", max_records=2)
+        with open(result_file) as fh:
+            for line in fh:
+                doc = json.loads(line)
+                for key in "taxid", "wikidata_id", "en_wiki_title", "extract":
+                    self.assertIn(key, doc)
+
+
+if __name__ == "__main__":
+    unittest.main()
