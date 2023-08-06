821642de758163b07b704423175129cad96d2b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:00 +0000

    Add package agent-frame/0.2.7/agent-frame-0.2.7.tar.gz

---
+++ packages/agent-frame/agent-frame-0.2.7.tar.gz/agent-frame-0.2.7/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='agent-frame',
+    version='0.2.7',
+    packages=['agent_frame'],
+    url='http://www.ryanjames.io',
+    license='',
+    author='Ryan McCauley',
+    author_email='ryanmccauley211@gmail.com',
+    description='A framework to make creating AI agents easier'
+)
