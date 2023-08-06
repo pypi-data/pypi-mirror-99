4601f097c0090a7b805565e4b03b22ad4e0bb6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:35 +0000

    Add package NotifierClient/0.1.1/NotifierClient-0.1.1.tar.gz

---
+++ packages/NotifierClient/NotifierClient-0.1.1.tar.gz/NotifierClient-0.1.1/notifier_client/main.py
@@ -0,0 +1,18 @@
+import requests
+from json import dumps
+
+NOTIFIER_URL = "https://notifier.qwhale.ml"
+
+
+class NotifierClient:
+    def __init__(self, app_token: str):
+        self.app_token = app_token
+
+    def send_notification(self, user_token: str, title: str, body: str, url: str = "", data: dict = dict) -> dict:
+        data = dumps(data)
+        response = requests.put(
+            NOTIFIER_URL + f"/notifications/{user_token}?app_token={self.app_token}&title={title}&message={body}&url={url}",
+            data=data
+        )
+        return response.json()
+
