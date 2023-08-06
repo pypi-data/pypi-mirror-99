45c68e082653cb6fbcfbae32ccde31adfe85b454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:54 +0000

    Add package OtoPy/0.1.1/OtoPy-0.1.1.tar.gz

---
+++ packages/OtoPy/OtoPy-0.1.1.tar.gz/OtoPy-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='OtoPy',
+    version='0.1.1',    
+    description='Custom Lib Containing usefool Tools ins class construction',
+    url='https://github.com/MathWintruff/OtoPy.git',
+    author='Otoma Systems',
+    author_email='otoma.system@gmail.com',
+    license='BSD 2-clause',
+    packages=['OtoPy'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',        
+        'Programming Language :: Python :: 3',
+    ],
+)
