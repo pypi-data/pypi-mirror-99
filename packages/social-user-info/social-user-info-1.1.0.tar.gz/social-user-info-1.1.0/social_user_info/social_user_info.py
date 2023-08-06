e1bb9086f02ec32ffe6adac1609e2f969dc9dde6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:34 +0000

    Add package social-user-info/1.1.0/social-user-info-1.1.0.tar.gz

---
+++ packages/social-user-info/social-user-info-1.1.0.tar.gz/social-user-info-1.1.0/social_user_info/social_user_info.py
@@ -0,0 +1,40 @@
+from social_user_info import constants
+from social_user_info import status_codes
+from social_user_info.social_medias import GoogleAPI, FacebookAPI, GitHubAPI, MicrosoftAPI
+
+
+class APIService:
+    SOURCE_TO_SOCIAL_API = {
+        constants.AUTH_SOURCE_GOOGLE: GoogleAPI,
+        constants.AUTH_SOURCE_FACEBOOK: FacebookAPI,
+        constants.AUTH_SOURCE_GITHUB: GitHubAPI,
+        constants.AUTH_SOURCE_MICROSOFT: MicrosoftAPI
+    }
+
+    @classmethod
+    def get_user_info(cls, access_token, auth_source):
+        """
+        Method of a util class that delegates the work for getting user info to the required SocialMedia
+        Service based on the incoming auth source
+        :param access_token:
+        :param auth_source:
+        :return: user_info
+        """
+        user_info = {
+            'status': status_codes.HTTP_BAD_REQUEST,
+            'error': constants.INVALID_AUTH_SOURCE_ERROR.format(auth_source)
+        }
+
+        api_class = cls.get_api(auth_source)
+        if api_class:
+            user_info = api_class.get_user_info(access_token)
+        return user_info
+
+    @classmethod
+    def get_api(cls, auth_source):
+        """
+        Method that returns the social media api class corresponding to source
+        :param auth_source:
+        :return:
+        """
+        return cls.SOURCE_TO_SOCIAL_API.get(auth_source)
