62d35feb43c57d50e2e915ead97e5519186f5d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:16 +0000

    Add package aws-cdk.aws-stepfunctions-tasks/1.95.0/aws-cdk.aws-stepfunctions-tasks-1.95.0.tar.gz

---
+++ packages/aws-cdk.aws-stepfunctions-tasks/aws-cdk.aws-stepfunctions-tasks-1.95.0.tar.gz/aws-cdk.aws-stepfunctions-tasks-1.95.0/setup.py
@@ -0,0 +1,87 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-cdk.aws-stepfunctions-tasks",
+    "version": "1.95.0",
+    "description": "Task integrations for AWS StepFunctions",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_stepfunctions_tasks",
+        "aws_cdk.aws_stepfunctions_tasks._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_stepfunctions_tasks._jsii": [
+            "aws-stepfunctions-tasks@1.95.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_stepfunctions_tasks": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.aws-apigateway==1.95.0",
+        "aws-cdk.aws-apigatewayv2-integrations==1.95.0",
+        "aws-cdk.aws-apigatewayv2==1.95.0",
+        "aws-cdk.aws-batch==1.95.0",
+        "aws-cdk.aws-cloudwatch==1.95.0",
+        "aws-cdk.aws-codebuild==1.95.0",
+        "aws-cdk.aws-databrew==1.95.0",
+        "aws-cdk.aws-dynamodb==1.95.0",
+        "aws-cdk.aws-ec2==1.95.0",
+        "aws-cdk.aws-ecr-assets==1.95.0",
+        "aws-cdk.aws-ecr==1.95.0",
+        "aws-cdk.aws-ecs==1.95.0",
+        "aws-cdk.aws-eks==1.95.0",
+        "aws-cdk.aws-glue==1.95.0",
+        "aws-cdk.aws-iam==1.95.0",
+        "aws-cdk.aws-kms==1.95.0",
+        "aws-cdk.aws-lambda==1.95.0",
+        "aws-cdk.aws-s3==1.95.0",
+        "aws-cdk.aws-sns==1.95.0",
+        "aws-cdk.aws-sqs==1.95.0",
+        "aws-cdk.aws-stepfunctions==1.95.0",
+        "aws-cdk.core==1.95.0",
+        "constructs>=3.3.69, <4.0.0",
+        "jsii>=1.26.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved",
+        "Framework :: AWS CDK",
+        "Framework :: AWS CDK :: 1"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
