badc5d8e962db4ed2b8afb8c0a38ff2174acbfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:52 +0000

    Add package keymapviz/1.5.0/keymapviz-1.5.0-py3-none-any.whl

---
+++ packages/keymapviz/keymapviz-1.5.0-py3-none-any.whl/keymapviz/__init__.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+import json
+
+import regex as re
+
+import keymapviz.keyboards.crkbd
+import keymapviz.keyboards.ergo42
+import keymapviz.keyboards.ergodash
+import keymapviz.keyboards.ergodox
+import keymapviz.keyboards.fortitude60
+import keymapviz.keyboards.lets_split
+import keymapviz.keyboards.kinesis
+import keymapviz.keyboards.helix
+import keymapviz.keyboards.mint60
+import keymapviz.keyboards.kaishi65
+import keymapviz.keyboards.kyria
+import keymapviz.keyboards.lily58
+import keymapviz.keyboards.sweet16
+
+
+KEYBOARDS = {
+    'crkbd': keymapviz.keyboards.crkbd,
+    'ergo42': keymapviz.keyboards.ergo42,
+    'ergodash': keymapviz.keyboards.ergodash,
+    'ergodone': keymapviz.keyboards.ergodox,
+    'ergodox': keymapviz.keyboards.ergodox,
+    'ergodox_ez': keymapviz.keyboards.ergodox,
+    'fortitude60': keymapviz.keyboards.fortitude60,
+    'helix': keymapviz.keyboards.helix,
+    'kinesis': keymapviz.keyboards.kinesis,
+    'lets_split': keymapviz.keyboards.lets_split,
+    'lily58': keymapviz.keyboards.lily58,
+    'mint60': keymapviz.keyboards.mint60,
+    'kaishi65': keymapviz.keyboards.kaishi65,
+    'kyria': keymapviz.keyboards.kyria,
+    'sweet16': keymapviz.keyboards.sweet16,
+}
+
+
+class Keymapviz():
+    def __init__(self, keyboard, keymap_c, legends = None):
+        self.__keymap_c = keymap_c.read()
+        self.keyboard = KEYBOARDS[keyboard]
+        self.keymaps = self.__parse_keymap_c()
+        self.legends = legends if legends else {}
+
+
+    def __parse_keymap_c(self):
+        src = self.__keymap_c.split('\n')
+        src = [re.sub(r'\s*//.*$', '', _) for _ in src]  # remove line comment
+        src = [_.rstrip() for _ in src]                  # remove CRLF
+        src = ''.join(src)
+        src = re.sub(r'/\*.*?\*/', '', src)              # remove block comment
+        src = re.sub(r'\s', '', src)                     # remove space
+        src = re.sub(r'\\', '', src)                     # remove backslash
+
+        keymap_regexp = re.compile(
+            self.keyboard.keymap_keyword + r'(?<rec>\((?:[^()]|(?&rec))*\))'
+        )
+        keycode_regexp = re.compile(
+            r'\s*(\w+(?<rec>\((?:[^()]|(?&rec))*\))*)\s*,?'
+        )
+        keymaps = keymap_regexp.findall(src)
+        keymaps = [_.lstrip('(').rstrip(')') for _ in keymaps]
+        keymaps = [[__[0] for __ in keycode_regexp.findall(_)] for _ in keymaps]
+        return keymaps
+
+
+    def __parse_ascii_art(self, aa):
+        regexp = re.compile(r'{.*?}')
+        matches = regexp.findall(aa)
+        for i, match in enumerate(matches):
+            s = match.lstrip('{').rstrip('}').strip()
+            n = int(s) if s.isdigit() else i
+            len_ = len(match)
+            aa = aa.replace(match, '{{{0}:^{1}.{1}}}'.format(n, len_), 1)
+        return aa.lstrip().rstrip()
+
+
+    def __legends(self, keymap):
+        return [self.legends.get(_, re.sub(r'^KC_', '', _)) for _ in keymap] + [''] * 100  # FIXME :_(
+
+
+    def __json_format(self, json_, keymap):
+        legends = self.__legends(keymap)
+        ret = [[_.format(*legends) if isinstance(_, str) else _ for _ in __]
+               for __ in json_]
+        with io.StringIO() as sio:
+            json.dump(ret, sio, indent=4)
+            ret_str = sio.getvalue()
+        return ret_str
+
+
+    def ascii_art(self):
+        aa = self.__parse_ascii_art(self.keyboard.ascii_art)
+        self.__ascii_art = [aa.format(*self.__legends(_)) for _ in self.keymaps]
+        return self.__ascii_art
+
+
+    def layout_editor_json(self):
+        path_ = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(path_, self.keyboard.layout_editor_json)) as f:
+            json_ = json.load(f)
+        return [self.__json_format(json_, _) for _ in self.keymaps]
+
+
+    def keymap_c(self):
+        class Repl():
+            def __init__(self, list_):
+                self.__list = list_
+                self.__gen  = self.__generator()
+
+            def __generator(self):
+                for _ in self.__list:
+                    yield _
+
+            def next(self, matchobj):
+                try:
+                    ret = next(self.__gen)
+                except StopIteration:
+                    ret = matchobj[0]
+                return ret
+
+        pattern = r'^[ \t\r\f\v]*?/\*((?!\*/).)*?\[keymapviz\].*?\*/\s*?$'
+        repl = Repl(self.__ascii_art)
+        kc = re.sub(pattern, repl.next, self.__keymap_c, flags=re.MULTILINE|re.DOTALL)
+        return kc
---
+++ packages/keymapviz/keymapviz-1.5.0-py3-none-any.whl/keymapviz/keymapviz.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import argparse
+
+import keymapviz
+from keymapviz.legends import *
+
+
+VERSION = 'v1.5.0'
+
+TYPES = {
+    'ascii': 'ascii_art',
+    'json': 'layout_editor_json',
+}
+
+
+def split_path(path):
+    head, tail = os.path.split(path)
+    if (not head) or (head == '/'):
+        return [tail]
+    return split_path(head) + [tail]
+
+
+def parse_arg():
+    keyboards = sorted(keymapviz.KEYBOARDS.keys())
+    types = list(TYPES.keys())
+
+    desc = 'keymap.c visualizer'
+    parser = argparse.ArgumentParser(description=desc,
+                                     epilog='Following keyboards are supported.\n * '+'\n * '.join(keyboards),
+                                     formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='configuration file')
+    parser.add_argument('-k', '--keyboard', type=str, choices=keyboards, help='keyboard of keymap.c', metavar='keyboards')
+    parser.add_argument('-o', '--output',   type=str, help='output file name("{}" is replaced index)')
+    parser.add_argument('-r', '--replace',   action='store_true', help='replace comment block including "[keymapviz]" with ascii art. (make *.bac)')
+    parser.add_argument('-t', '--type',     dest='type_', type=str, choices=types, default=types[0], help='type of output(default:ascii)')
+    parser.add_argument('-v', '--version',  action='version', version='%(prog)s {}'.format(VERSION))
+    parser.add_argument('keymap_c',         type=argparse.FileType('r'), help='keymap.c file name')
+
+    arg = parser.parse_args()
+
+    # Search keyboard name in 'keymap_c' path, if '-k' option is empty.
+    if arg.keyboard is None:
+        path = split_path(os.path.abspath(arg.keymap_c.name))
+        pset = list(set(path) & set(keyboards))  # product set
+        if not pset:
+            print('Sorry. Please choose your keyboard(-k/--keyboard).', file=sys.stderr)
+            sys.exit(1)
+        else:
+            arg.keyboard = pset[0]
+
+    return arg
+
+
+def output_keymaps(output_filename, keymaps):
+    if output_filename is None:
+        # Output for stdout
+        [print(_ + '\n') for _ in keymaps]
+    else:
+        # Output for file
+        for i in range(len(keymaps)):
+            f = output_filename.format(i)
+            if os.path.exists(f):
+                os.remove(f)
+
+        for i, k in enumerate(keymaps):
+            f = output_filename.format(i)
+            with open(f, 'a') as file_:
+                print(k, file=file_)
+
+
+def output_keymap_c(output_filename, keymap_c):
+    if os.path.exists(output_filename):
+        os.rename(output_filename, output_filename + ".bac")
+    with open(output_filename, 'w') as f:
+        print(keymap_c, file=f, end='')
+
+
+def main():
+    arg = parse_arg()
+    kmvz = keymapviz.Keymapviz(arg.keyboard, arg.keymap_c, read_config(arg.config))
+
+    keymaps = getattr(kmvz, TYPES[arg.type_])()
+    output_keymaps(arg.output, keymaps)
+    if arg.replace:
+        output_keymap_c(arg.keymap_c.name, kmvz.keymap_c())
+
