54a232e5bd3379def3783fe96b54a0687146bb00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:41 +0000

    Add package dbt-sugar/0.0.0a7/dbt_sugar-0.0.0a7-py3-none-any.whl

---
+++ packages/dbt-sugar/dbt_sugar-0.0.0a7-py3-none-any.whl/dbt_sugar/core/clients/dbt.py
@@ -0,0 +1,211 @@
+"""Holds methods to interact with dbt API (we mostly don't for now because not stable) and objects."""
+
+from pathlib import Path
+from typing import Any, Dict, Optional, Union
+
+from pydantic import BaseModel, Field
+
+from dbt_sugar.core.clients.yaml_helpers import open_yaml
+from dbt_sugar.core.exceptions import (
+    DbtProfileFileMissing,
+    ProfileParsingError,
+    TargetNameNotProvided,
+)
+from dbt_sugar.core.flags import FlagParser
+from dbt_sugar.core.logger import GLOBAL_LOGGER as logger
+
+DEFAULT_DBT_PROFILE_PATH = Path.home().joinpath(".dbt", "profiles").with_suffix(".yml")
+
+
+class PostgresDbtProfilesModel(BaseModel):
+    """Postgres Dbt credentials validation model."""
+
+    type: str
+    user: str
+    password: str = Field(..., alias="pass")
+    database: str = Field(..., alias="dbname")
+    target_schema: str = Field(..., alias="schema")
+    host: str
+    port: int
+
+
+class SnowflakeDbtProfilesModel(BaseModel):
+    """Snowflake Dbt credentials validation model."""
+
+    type: str
+    account: str
+    user: str
+    password: str
+    database: str
+    target_schema: str = Field(..., alias="schema")
+    role: str
+    warehouse: str
+
+
+class DbtProjectModel(BaseModel):
+    """Defines pydandic validation schema for a dbt_project.yml file."""
+
+    profile: str
+
+
+class BaseYamlConfig:
+    """Base class object which gets extended by objects which will generally read from yaml configs."""
+
+    def _assert_file_exists(self, dir: Path, filename: str = "profiles.yml") -> bool:
+        logger.debug(dir.resolve())
+        full_path_to_file = dir / filename
+        if full_path_to_file.is_file():
+            return True
+        else:
+            raise DbtProfileFileMissing(f"Could not locate `{filename}` in {dir.resolve()}")
+
+
+class DbtProject(BaseYamlConfig):
+    """Holds parsed dbt project information needed for dbt-sugar such as which db profile to target."""
+
+    DBT_PROJECT_FILENAME: str = "dbt_project.yml"
+
+    def __init__(self, project_name: str, project_dir: Path) -> None:
+        """Constructor for DbtProject.
+
+        Given a project name and a project dir it will parse the relevant dbt_project.yml and
+        parse information such as `profile` so dbt-sugar knows which database profile entry from
+        /.dbt/profiles.yml to use.
+
+        Args:
+            project_name (str): Name of the dbt project to read profile from.
+            project_dir (Path): Path object the dbt_project.yml to read from.
+        """
+        self._project_name = project_name
+        self._project_dir = project_dir
+
+        # class "outputs"
+        self.project: DbtProjectModel
+        self.profile_name: str
+
+    @property
+    def _dbt_project_filename(self) -> Path:
+        logger.debug(f"project_dir: {self._project_dir}")
+        return Path(self._project_dir).joinpath(type(self).DBT_PROJECT_FILENAME)
+
+    def read_project(self) -> None:
+        _ = self._assert_file_exists(Path(self._project_dir), filename=self.DBT_PROJECT_FILENAME)
+        _project_dict = open_yaml(self._dbt_project_filename)
+
+        # pass the dict through pydantic for validation and only getting what we need
+        # if the profile is invalid app will crash so no further tests required below.
+        logger.debug(f"the project {_project_dict}")
+        _project = DbtProjectModel(**_project_dict)
+        logger.debug(_project)
+        self.project = _project
+        self.profile_name = self.project.dict().get("profile", str())
+
+        if not self.profile_name:
+            logger.warning(
+                f"[yellow]There was no `profile:` entry in {self._dbt_project_filename}. "
+                "dbt-sugar will try to find a 'default' profile. This might lead to unexpected"
+                "behaviour or an error when no defaulf profile can be found in your dbt profiles.yml"
+            )
+
+
+class DbtProfile(BaseYamlConfig):
+    """Holds parsed profile dict from dbt profiles."""
+
+    CLI_OVERRIDE_FLAGS = [{"cli_arg_name": "schema", "maps_to": "target_schema"}]
+
+    def __init__(
+        self,
+        flags: FlagParser,
+        profile_name: str,
+        target_name: str,
+        profiles_dir: Optional[Path] = None,
+    ) -> None:
+        """Reads, validates and holds dbt profile info required by dbt-sugar (mainly db creds).
+
+        Args:
+            project_name (str): name of the dbt project to read credentials from.
+            target_name (str): name of the target entry. This corresponds to what resides below
+                "outputs" in the dbt's profile.yml (https://docs.getdbt.com/dbt-cli/configure-your-profile/)
+        """
+        # attrs parsed from constructor
+        self._flags = flags
+        self._profile_name = profile_name
+        self._target_name = target_name
+        self._profiles_dir = profiles_dir
+
+        # attrs populated by class methods
+        self.profile: Dict[str, str]
+
+    @property
+    def profiles_dir(self):
+        if self._profiles_dir:
+            return self._profiles_dir
+        return DEFAULT_DBT_PROFILE_PATH
+
+    def _get_target_profile(self, profile_dict: Dict[str, Any]) -> Dict[str, Union[str, int]]:
+        if self._target_name:
+            return profile_dict["outputs"].get(self._target_name)
+        else:
+            self._target_name = profile_dict.get("target", str())
+            if self._target_name:
+                return profile_dict["outputs"].get(self._target_name)
+            else:
+                raise TargetNameNotProvided(
+                    f"No target name provied in {self._profiles_dir} and none provided via "
+                    "--target in CLI. Cannot figure out appropriate profile information to load."
+                )
+
+    def read_profile(self):
+        _ = self._assert_file_exists(
+            self.profiles_dir
+        )  # this will raise so no need to check exists further
+        _profile_dict = open_yaml(self.profiles_dir / "profiles.yml")
+        _profile_dict = _profile_dict.get(self._profile_name, _profile_dict.get(self._profile_name))
+        if _profile_dict:
+
+            # read target name from args or try to get it from the dbt_profile `target:` field.
+            _target_profile = self._get_target_profile(profile_dict=_profile_dict)
+
+            if _target_profile:
+                _profile_type = _target_profile.get("type")
+                # call the right pydantic validator depending on the db type as dbt is not
+                # consistent with it's profiles and it's hell to have all the validation in one
+                # pydantic model.
+                if _profile_type == "snowflake":
+                    # uses pydantic to validate profile. It will raise and break app if invalid.
+                    _target_profile = SnowflakeDbtProfilesModel(**_target_profile)
+                elif _profile_type == "postgres":
+                    _target_profile = PostgresDbtProfilesModel(**_target_profile)
+
+                # if we don't manage to read the db type for some reason.
+                elif _profile_type is None:
+                    raise ProfileParsingError(
+                        f"Could not read or find a database type for {self._profile_name} in your dbt "
+                        "profiles.yml. Check that this field is not missing."
+                    )
+                else:
+                    raise NotImplementedError(f"{_profile_type} is not implemented yet.")
+                logger.debug(_target_profile)
+                self.profile = _target_profile.dict()
+
+                # override profile info with potential CLI args
+                self._integrate_cli_flags()
+
+            else:
+                raise ProfileParsingError(
+                    f"Could not find an entry for target: '{self._target_name}', "
+                    f"for the '{self._profile_name}' profile in your dbt profiles.yml."
+                )
+
+        else:
+            raise ProfileParsingError(
+                f"Could not find an entry for '{self._profile_name}' in your profiles.yml"
+            )
+
+    def _integrate_cli_flags(self) -> None:
+        for flag_override_dict in self.CLI_OVERRIDE_FLAGS:
+            cli_arg_value = getattr(self._flags, flag_override_dict["cli_arg_name"])
+            if cli_arg_value and isinstance(self.profile, dict):
+                self.profile[flag_override_dict["maps_to"]] = cli_arg_value
+            else:
+                logger.debug("No schema passed to CLI will try to read from profile.yml")
---
+++ packages/dbt-sugar/dbt_sugar-0.0.0a7-py3-none-any.whl/dbt_sugar/core/main.py
@@ -0,0 +1,235 @@
+"""Main module for dbt-sugar. Sets up CLI arguments and sets up task handlers."""
+import argparse
+import sys
+from typing import List
+
+import pyfiglet
+from rich.console import Console
+
+from dbt_sugar.core._version import __version__
+from dbt_sugar.core.clients.dbt import DbtProfile, DbtProject
+from dbt_sugar.core.config.config import DbtSugarConfig
+from dbt_sugar.core.flags import FlagParser
+from dbt_sugar.core.logger import GLOBAL_LOGGER as logger
+from dbt_sugar.core.logger import log_manager
+from dbt_sugar.core.task.audit import AuditTask
+from dbt_sugar.core.task.doc import DocumentationTask
+from dbt_sugar.core.ui.traceback_manager import DbtSugarTracebackManager
+from dbt_sugar.core.utils import check_and_compare_version
+
+console = Console()
+
+
+def check_and_print_version() -> str:
+    """Calls check_and_compare_version and formats a message that works both for main and argparse.
+
+    Returns:
+        str: version info message ready for printing
+    """
+    needs_update, latest_version = check_and_compare_version()
+    installed_version_message = f"Installed dbt-sugar version: {__version__}".rjust(40)
+    latest_version_message = f"Latest dbt-sugar version: {latest_version}".rjust(40)
+    if latest_version:
+        return "\n".join([installed_version_message, latest_version_message])
+    return installed_version_message
+
+
+# general parser
+parser = argparse.ArgumentParser(
+    prog="dbt-sugar",
+    formatter_class=argparse.RawTextHelpFormatter,
+    description="CLI tool to help users document their dbt models",
+    epilog="Select onf of the available sub-commands with --help to find out more about them.",
+)
+
+parser.add_argument("-v", "--version", action="version", version=check_and_print_version())
+
+# base sub-parser (sets up args that need to be provided to ALL other sub parsers)
+base_subparser = argparse.ArgumentParser(add_help=False)
+base_subparser.add_argument(
+    "--log-level", help="overrides default log level", type=str, default=str()
+)
+base_subparser.add_argument(
+    "-vv",
+    "--verbose",
+    help="When provided the length of the tracebacks will not be truncated.",
+    action="store_true",
+    default=False,
+)
+base_subparser.add_argument(
+    "--syrup",
+    help="Name of the syrup confi you wish to use. If left empty dbt-sugar will attempt to read your defaults.",
+    type=str,
+    default=str(),
+)
+base_subparser.add_argument(
+    "--config-path", help="Full path to config.yml file if not using default."
+)
+base_subparser.add_argument(
+    "--profiles-dir", help="Alternative path to the dbt profiles.yml file.", type=str, default=str()
+)
+
+# Task-specific argument sub parsers
+sub_parsers = parser.add_subparsers(title="Available dbt-sugar commands", dest="command")
+
+# document task parser
+document_sub_parser = sub_parsers.add_parser(
+    "doc", parents=[base_subparser], help="Runs documentation and test enforement task."
+)
+document_sub_parser.set_defaults(cls=DocumentationTask, which="doc")
+# TODO: We shouldn't be requiring this if we have a `--model` format as it's considered bad practice
+document_sub_parser.add_argument(
+    "-m", "--model", help="Name of the dbt model to document", type=str, default=None, required=True
+)
+document_sub_parser.add_argument(
+    "-s",
+    "--schema",
+    help="Name of the database schema in which the model resides",
+    type=str,
+    default=None,
+)
+document_sub_parser.add_argument(
+    "--dry-run",
+    help="When provided the documentation task will not modify your files",
+    action="store_true",
+    default=False,
+)
+document_sub_parser.add_argument(
+    "-t",
+    "--target",
+    help="Which target from the dbt profile to load.",
+    type=str,
+    default=str(),
+)
+# document_sub_parser.add_argument(
+
+document_sub_parser.add_argument(
+    "--no-ask-tests",
+    help="When provided the documentation task will not ask for adding TAGs into the model.",
+    action="store_false",
+    dest="ask_for_tests",
+)
+
+document_sub_parser.add_argument(
+    "--ask-tests",
+    help="When passed dbt-sugar will ask you if you want to add tests to your models.",
+    action="store_true",
+    dest="ask_for_tests",
+    default=True,
+)
+
+document_sub_parser.add_argument(
+    "--no-ask-tags",
+    help="When provided the documentation task will not ask for adding TAGs into the model.",
+    action="store_false",
+    dest="ask_for_tags",
+)
+
+document_sub_parser.add_argument(
+    "--ask-tags",
+    help="When passed dbt-sugar will ask you if you want to add tests to your models.",
+    action="store_true",
+    dest="ask_for_tags",
+    default=True,
+)
+
+# document task parser
+audit_sub_parser = sub_parsers.add_parser(
+    "audit", parents=[base_subparser], help="Runs audit task."
+)
+audit_sub_parser.set_defaults(cls=AuditTask, which="audit")
+audit_sub_parser.add_argument(
+    "-m",
+    "--model",
+    help="Name of the dbt model to document",
+    type=str,
+    default=None,
+    required=False,
+)
+
+# task handler
+
+
+def handle(
+    parser: argparse.ArgumentParser,
+    test_cli_args: List[str] = list(),
+) -> int:
+    """Task handler factory.
+
+    Args:
+        parser (argparse.ArgumentParser): CLI argument parser object.
+    """
+    flag_parser = FlagParser(parser)
+    flag_parser.consume_cli_arguments(test_cli_args=test_cli_args)
+
+    # set up traceback manager fo prettier errors
+    DbtSugarTracebackManager(flag_parser)
+
+    sugar_config = DbtSugarConfig(flag_parser)
+    sugar_config.load_config()
+
+    # TODO: Revise the index access here when we can support multiple dbt projects properly.
+    # ! REGRESSION
+    dbt_project = DbtProject(
+        sugar_config.config.get("dbt_projects", list())[0].get("name", str()),
+        sugar_config.config.get("dbt_projects", list())[0].get("path", str()),
+    )
+    dbt_project.read_project()
+
+    dbt_profile = DbtProfile(
+        flags=flag_parser,
+        profile_name=dbt_project.profile_name,
+        target_name=flag_parser.target,
+        profiles_dir=flag_parser.profiles_dir,
+    )
+    dbt_profile.read_profile()
+
+    if flag_parser.log_level == "debug":
+        log_manager.set_debug()
+
+    if flag_parser.task == "doc":
+        task: DocumentationTask = DocumentationTask(
+            flag_parser, dbt_profile, sugar_config, dbt_project._project_dir
+        )
+        # TODO: We actually need to change the behaviour of DocumentationTask to provide an interactive
+        # dry run but for now this allows testing without side effects.
+        # the current implementation upsets mypy also.
+        if flag_parser.is_dry_run:
+            logger.warning("[yellow]Running in --dry-run mode no files will be modified")
+            logger.info(f"Would run {task}")
+            return 0
+        return task.run()
+
+    if flag_parser.task == "audit":
+        audit_task: AuditTask = AuditTask(flag_parser, dbt_project._project_dir)
+        return audit_task.run()
+
+    raise NotImplementedError(f"{flag_parser.task} is not supported.")
+
+
+def main(parser: argparse.ArgumentParser = parser, test_cli_args: List[str] = list()) -> int:
+    """Just your boring main."""
+    exit_code = 0
+    _cli_args = list()
+    if test_cli_args:
+        _cli_args = test_cli_args
+
+    # print version on every run unless doing `--version` which is better handled by argparse
+    if "--version" not in sys.argv[1:]:
+        version_message = check_and_print_version()
+        print(version_message)
+        print("\n")
+        # print app logo with pyfiglet
+        logo_str = str(pyfiglet.figlet_format("dbt-sugar", font="slant"))
+        console.print(logo_str, style="blue")
+        print("Getting sweetness out of the cupboard 🍬! \n")
+    # TODO: Update this when a proper dry-run exists.
+    exit_code = handle(parser, _cli_args)  # type: ignore
+
+    if exit_code > 0:
+        logger.error("[red]The process you were running did not complete successfully.")
+    return exit_code
+
+
+if __name__ == "__main__":
+    exit(main())
