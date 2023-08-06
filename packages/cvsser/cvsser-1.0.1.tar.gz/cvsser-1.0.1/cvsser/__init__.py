cab02c71f746cecdecb8f4fdf883f9eb5b0f155b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:26 +0000

    Add package cvsser/1.0.1/cvsser-1.0.1.tar.gz

---
+++ packages/cvsser/cvsser-1.0.1.tar.gz/cvsser-1.0.1/cvsser/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+PyCVSS Library
+~~~~~~~~~~~~~~~~~~~~~
+VS_CVSS is a library, written in Python, for manipulating CVSS vector strings, vector fields, and values.
+
+:copyright: (c) 2021
+:license: Apache 3.0, see LICENSE for more details.
+"""
+
+from .cvss import VectorString
