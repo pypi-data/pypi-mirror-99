5a4907ec0a5be4aee5a3f1689bf17070ac1c9511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:46 +0000

    Add package col-dist/0.1/col-dist-0.1.tar.gz

---
+++ packages/col-dist/col-dist-0.1.tar.gz/col-dist-0.1/README.md
@@ -0,0 +1,1 @@
+#dsnd-distrubutions package
---
+++ packages/col-dist/col-dist-0.1.tar.gz/col-dist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='col-dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['col-dist'],
+      author = "Francisco Colina",
+      zip_safe=False)
