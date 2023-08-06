5b40e63fd2c341c5f9e30dbc18664e4cd91813c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:59 +0000

    Add package distributions-alongoria/0.1/distributions_alongoria-0.1.tar.gz

---
+++ packages/distributions-alongoria/distributions_alongoria-0.1.tar.gz/distributions_alongoria-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_alongoria',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_alongoria'],
+      zip_safe=False)
