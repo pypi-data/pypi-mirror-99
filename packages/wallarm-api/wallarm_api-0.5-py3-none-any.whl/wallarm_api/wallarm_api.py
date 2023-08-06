5845b720d58b472c3aa99c92ec1398593dafc74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:18 +0000

    Add package wallarm-api/0.5/wallarm_api-0.5-py3-none-any.whl

---
+++ packages/wallarm-api/wallarm_api-0.5-py3-none-any.whl/wallarm_api/wallarm_api.py
@@ -0,0 +1,14 @@
+from wallarm_api.core.api.clients_api import ClientsApi
+from wallarm_api.core.api.billing_api import BillingApi
+from wallarm_api.core.api.graph_api import GraphApi
+
+
+class WallarmAPI:
+
+    def __init__(self, uuid='', secret='', api='https://api.wallarm.com'):
+        self.__uuid = uuid
+        self.__secret = secret
+        self.__api = api
+        self.clients_api = ClientsApi(uuid, secret, host=api)
+        self.billing_api = BillingApi(uuid, secret, host=api)
+        self.graph_api = GraphApi(uuid, secret, host=api)
