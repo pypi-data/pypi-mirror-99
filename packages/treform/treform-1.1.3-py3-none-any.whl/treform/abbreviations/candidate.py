32d4c978147794534115a7539844a7cd1f2bf79c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:58 +0000

    Add package treform/1.1.3/treform-1.1.3-py3-none-any.whl

---
+++ packages/treform/treform-1.1.3-py3-none-any.whl/treform/abbreviations/candidate.py
@@ -0,0 +1,10 @@
+
+class Candidate(str):
+    def __init__(self, value):
+        super().__init__()
+        self.start = 0
+        self.stop = 0
+
+    def set_position(self, start, stop):
+        self.start = start
+        self.stop = stop
