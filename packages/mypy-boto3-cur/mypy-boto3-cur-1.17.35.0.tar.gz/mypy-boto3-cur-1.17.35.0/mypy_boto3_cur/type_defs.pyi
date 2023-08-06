17a6c3798bcbf5ac7ec0595bbf3af37d6fa50006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:40 +0000

    Add package mypy-boto3-cur/1.17.35.0/mypy-boto3-cur-1.17.35.0.tar.gz

---
+++ packages/mypy-boto3-cur/mypy-boto3-cur-1.17.35.0.tar.gz/mypy-boto3-cur-1.17.35.0/mypy_boto3_cur/type_defs.pyi
@@ -0,0 +1,94 @@
+"""
+Main interface for cur service type definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_cur.type_defs import ReportDefinitionTypeDef
+
+    data: ReportDefinitionTypeDef = {...}
+    ```
+"""
+import sys
+from typing import List
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "ReportDefinitionTypeDef",
+    "DeleteReportDefinitionResponseTypeDef",
+    "DescribeReportDefinitionsResponseTypeDef",
+    "PaginatorConfigTypeDef",
+)
+
+_RequiredReportDefinitionTypeDef = TypedDict(
+    "_RequiredReportDefinitionTypeDef",
+    {
+        "ReportName": str,
+        "TimeUnit": Literal["HOURLY", "DAILY", "MONTHLY"],
+        "Format": Literal["textORcsv", "Parquet"],
+        "Compression": Literal["ZIP", "GZIP", "Parquet"],
+        "AdditionalSchemaElements": List[Literal["RESOURCES"]],
+        "S3Bucket": str,
+        "S3Prefix": str,
+        "S3Region": Literal[
+            "af-south-1",
+            "ap-east-1",
+            "ap-south-1",
+            "ap-southeast-1",
+            "ap-southeast-2",
+            "ap-northeast-1",
+            "ap-northeast-2",
+            "ap-northeast-3",
+            "ca-central-1",
+            "eu-central-1",
+            "eu-west-1",
+            "eu-west-2",
+            "eu-west-3",
+            "eu-north-1",
+            "eu-south-1",
+            "me-south-1",
+            "sa-east-1",
+            "us-east-1",
+            "us-east-2",
+            "us-west-1",
+            "us-west-2",
+            "cn-north-1",
+            "cn-northwest-1",
+        ],
+    },
+)
+_OptionalReportDefinitionTypeDef = TypedDict(
+    "_OptionalReportDefinitionTypeDef",
+    {
+        "AdditionalArtifacts": List[Literal["REDSHIFT", "QUICKSIGHT", "ATHENA"]],
+        "RefreshClosedReports": bool,
+        "ReportVersioning": Literal["CREATE_NEW_REPORT", "OVERWRITE_REPORT"],
+        "BillingViewArn": str,
+    },
+    total=False,
+)
+
+class ReportDefinitionTypeDef(_RequiredReportDefinitionTypeDef, _OptionalReportDefinitionTypeDef):
+    pass
+
+DeleteReportDefinitionResponseTypeDef = TypedDict(
+    "DeleteReportDefinitionResponseTypeDef", {"ResponseMessage": str}, total=False
+)
+
+DescribeReportDefinitionsResponseTypeDef = TypedDict(
+    "DescribeReportDefinitionsResponseTypeDef",
+    {"ReportDefinitions": List["ReportDefinitionTypeDef"], "NextToken": str},
+    total=False,
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef", {"MaxItems": int, "PageSize": int, "StartingToken": str}, total=False
+)
