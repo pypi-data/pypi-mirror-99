2c534d1418bc229f9f3960147024293d26b7a0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:54 +0000

    Add package overhave/2.0.3/overhave-2.0.3.tar.gz

---
+++ packages/overhave/overhave-2.0.3.tar.gz/overhave-2.0.3/overhave/cli/admin.py
@@ -0,0 +1,19 @@
+import click
+
+from overhave import overhave_app
+from overhave.base_settings import DataBaseSettings, LoggingSettings
+from overhave.cli.group import overhave
+from overhave.factory import get_admin_factory
+
+
+def _run_admin(port: int, debug: bool) -> None:
+    DataBaseSettings().setup_db()
+    LoggingSettings().setup_logging()
+    overhave_app(get_admin_factory()).run(host="0.0.0.0", port=port, debug=debug)
+
+
+@overhave.command(short_help="Run Overhave web-service")
+@click.option("--port", default=8076)
+@click.option("--debug", default=False)
+def admin(port: int, debug: bool) -> None:
+    _run_admin(port, debug)
---
+++ packages/overhave/overhave-2.0.3.tar.gz/overhave-2.0.3/overhave/entities/__init__.py
@@ -0,0 +1,35 @@
+# flake8: noqa
+from .archiver import ArchiveManager
+from .authorization import IAdminAuthorizationManager
+from .converters import (
+    DraftModel,
+    EmulationRunModel,
+    FeatureModel,
+    FeatureTypeModel,
+    PublisherContext,
+    ScenarioModel,
+    SystemUserModel,
+    TestExecutorContext,
+    TestRunModel,
+)
+from .emulator import Emulator
+from .feature import (
+    FeatureExtractor,
+    FeatureTypeExtractionError,
+    FeatureTypeName,
+    IFeatureExtractor,
+    ScenariosTestFileNotFound,
+)
+from .language import StepPrefixesModel, TranslitPack
+from .report_manager import ReportManager, ReportPresenceResolution
+from .settings import (
+    OverhaveDescriptionManagerSettings,
+    OverhaveEmulationSettings,
+    OverhaveFileSettings,
+    OverhaveLanguageSettings,
+    OverhaveRedisSettings,
+    OverhaveReportManagerSettings,
+    OverhaveScenarioCompilerSettings,
+    OverhaveStepContextSettings,
+    ProcessorSettings,
+)
