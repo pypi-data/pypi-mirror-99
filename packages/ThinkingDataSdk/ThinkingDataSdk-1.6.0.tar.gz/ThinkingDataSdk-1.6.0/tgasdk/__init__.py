e16699eddcbdc49448d7c39f511ae5cc3153275e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:59 +0000

    Add package ThinkingDataSdk/1.6.0/ThinkingDataSdk-1.6.0.tar.gz

---
+++ packages/ThinkingDataSdk/ThinkingDataSdk-1.6.0.tar.gz/ThinkingDataSdk-1.6.0/tgasdk/__init__.py
@@ -0,0 +1,13 @@
+from tgasdk.sdk import *
+
+__all__ = [
+    'TGAnalytics',
+    'LoggingConsumer',
+    'BatchConsumer',
+    'DebugConsumer',
+    'AsyncBatchConsumer',
+    'TGAException',
+    'TGAIllegalDataException',
+    'TGANetworkException',
+    'ROTATE_MODE',
+]
