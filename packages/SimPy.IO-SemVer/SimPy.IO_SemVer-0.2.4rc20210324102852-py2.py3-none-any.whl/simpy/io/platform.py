0f94cdcecbbb6840e648431f3e7dbafb617db2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:43 +0000

    Add package SimPy.IO-SemVer/0.2.4rc20210324102852/SimPy.IO_SemVer-0.2.4rc20210324102852-py2.py3-none-any.whl

---
+++ packages/SimPy.IO-SemVer/SimPy.IO_SemVer-0.2.4rc20210324102852-py2.py3-none-any.whl/simpy/io/platform.py
@@ -0,0 +1,14 @@
+import errno
+import platform
+
+if platform.system() == 'Windows':
+    blocking_io_errors = set((errno.EWOULDBLOCK,))
+    connection_errors = set((errno.ECONNABORTED, errno.ECONNRESET,
+        errno.EPIPE))
+    invalid_argument = errno.WSAEINVAL
+else:
+    # Default to a unix system.
+    invalid_argument = errno.EINVAL
+    blocking_io_errors = set((errno.EAGAIN, errno.EINPROGRESS))
+    connection_errors = set((errno.ECONNABORTED, errno.ECONNRESET,
+        errno.EPIPE, errno.ECONNREFUSED))
