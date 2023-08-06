0e5685594d6406a61c50e80d8104fabe9927e904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:38 +0000

    Add package function-scheduling-distributed-framework/9.0/function_scheduling_distributed_framework-9.0.tar.gz

---
+++ packages/function-scheduling-distributed-framework/function_scheduling_distributed_framework-9.0.tar.gz/function_scheduling_distributed_framework-9.0/function_scheduling_distributed_framework/frame_config.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Author  : ydf
+# @Time    : 2019/8/8 0008 9:51
+
+
+# 你项目根目录下自动生成的 distributed_frame_config.py 文件种修改配置，会被自动读取到。
+
+MONGO_CONNECT_URL = f'mongodb://myUserAdmin:8mwTdy1klnSYepNo@{"192.168.199.202"}:27016/admin'
+
+RABBITMQ_USER = 'rabbitmq_user'
+RABBITMQ_PASS = 'rabbitmq_pass'
+RABBITMQ_HOST = '127.0.0.1'
+RABBITMQ_PORT = 5672
+RABBITMQ_VIRTUAL_HOST = 'rabbitmq_virtual_host'
+
+REDIS_HOST = '127.0.0.1'
+REDIS_PASSWORD = ''
+REDIS_PORT = 6379
+REDIS_DB = 7
+
+NSQD_TCP_ADDRESSES = ['127.0.0.1:4150']
+NSQD_HTTP_CLIENT_HOST = '127.0.0.1'
+NSQD_HTTP_CLIENT_PORT = 4151
+
+KAFKA_BOOTSTRAP_SERVERS = ['127.0.0.1:9092']
+
+SQLACHEMY_ENGINE_URL = 'sqlite:////sqlachemy_queues/queues.db'
+
+SQLLITE_QUEUES_PATH = '/sqllite_queues'
+
+ROCKETMQ_NAMESRV_ADDR = '192.168.199.202:9876'
+
+# nb_log包的第几个日志模板，内置了7个模板，可以在你当前项目根目录下的nb_log_config.py文件扩展模板。
+NB_LOG_FORMATER_INDEX_FOR_CONSUMER_AND_PUBLISHER = 7  # 7是简短的不可跳转，5是可点击跳转的
+FSDF_DEVELOP_LOG_LEVEL = 50   # 开发时候的日志，仅供自己用，所以日志级别跳到最高
+
+TIMEZONE = 'Asia/Shanghai'
---
+++ packages/function-scheduling-distributed-framework/function_scheduling_distributed_framework-9.0.tar.gz/function_scheduling_distributed_framework-9.0/setup.py
@@ -0,0 +1,89 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+# with open("README.md", "r",encoding='utf8') as fh:
+#     long_description = fh.read()
+
+setup(
+    name='function_scheduling_distributed_framework',  #
+    version=9.0,
+    description=(
+        'function scheduling distributed framework,support threading,gevent,eventlet,asyncio concurrent,support all kinds of message queue,and has manay control way'
+    ),
+    # long_description=open('README.md', 'r',encoding='utf8').read(),
+    keywords=("distributed-framework","function-scheduling", "rabbitmq", "rocketmq","kafka", "nsq","redis","disk",
+              "sqlachemy","consume-confirm", "timing" ,"task-scheduling", "apscheduler"),
+    long_description_content_type="text/markdown",
+    long_description=open('README.md', 'r', encoding='utf8').read(),
+    author='bfzs',
+    author_email='ydf0509@sohu.com',
+    maintainer='ydf',
+    maintainer_email='ydf0509@sohu.com',
+    license='BSD License',
+    # packages=['douban'], #
+    packages=find_packages(),
+    # packages=['function_scheduling_distributed_framework'], # 这样内层级文件夹的没有打包进去。
+    include_package_data=True,
+    platforms=["all"],
+    url='https://github.com/ydf0509/distributed_framework',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'eventlet==0.25.0',
+        'gevent',
+        'pymongo==3.5.1',
+        'AMQPStorm==2.7.1',
+        'rabbitpy==2.0.1',
+        'decorator==4.4.0',
+        'pysnooper==0.0.11',
+        'Flask',
+        'flask_bootstrap',
+        'tomorrow3==1.1.0',
+        'concurrent-log-handler==0.9.9',
+        'persist-queue>=0.4.2',
+        'elasticsearch',
+        'kafka-python==1.4.6',
+        'requests',
+        'gnsq==1.0.1',
+        'psutil',
+        'sqlalchemy==1.3.10',
+        'sqlalchemy_utils==0.36.1',
+        'apscheduler==3.3.1',
+        'pikav0',
+        'pikav1',
+        'redis2',
+        'redis3',
+        'redis',
+        'nb_log>=3.4',
+        'rocketmq',
+    ]
+)
+"""
+官方 https://pypi.org/simple
+清华 https://pypi.tuna.tsinghua.edu.cn/simple
+豆瓣 https://pypi.douban.com/simple/ 
+阿里云 https://mirrors.aliyun.com/pypi/simple/
+
+打包上传
+python setup.py sdist upload -r pypi
+
+python setup.py sdist & twine upload dist/function_scheduling_distributed_framework-9.0.tar.gz
+
+最快的下载方式，上传立即可安装。阿里云源同步官网pypi间隔要等很久。
+./pip install function_scheduling_distributed_framework==3.5 -i https://pypi.org/simple   
+最新版下载
+./pip install function_scheduling_distributed_framework --upgrade -i https://pypi.org/simple      
+"""
+
