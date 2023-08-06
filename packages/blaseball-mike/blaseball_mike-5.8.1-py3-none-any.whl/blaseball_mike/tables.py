ac151528e65f74388942f407a8376ff3373c88ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:58 +0000

    Add package blaseball-mike/5.8.1/blaseball_mike-5.8.1-py3-none-any.whl

---
+++ packages/blaseball-mike/blaseball_mike-5.8.1-py3-none-any.whl/blaseball_mike/tables.py
@@ -0,0 +1,110 @@
+"""Enum-like tables for static elements"""
+from enum import Enum
+
+
+class Weather(Enum):
+    INVALID = -1, "Invalid Weather"
+    VOID = 0, "Void"
+    SUNNY = 1, "Sun 2"
+    OVERCAST = 2, "Overcast"
+    RAINY = 3, "Rainy"
+    SANDSTORM = 4, "Sandstorm"
+    SNOWY = 5, "Snowy"
+    ACIDIC = 6, "Acidic"
+    SOLAR_ECLIPSE = 7, "Solar Eclipse"
+    GLITTER = 8, "Glitter"
+    BLOODRAIN = 9, "Blooddrain"
+    PEANUTS = 10, "Peanuts"
+    BIRDS = 11, "Birds"
+    FEEDBACK = 12, "Feedback"
+    REVERB = 13, "Reverb"
+    BLACK_HOLE = 14, "Black Hole"
+    COFFEE = 15, "Coffee"
+    COFFEE_2 = 16, "Coffee 2"
+    COFFEE_3S = 17, "Coffee 3s"
+    FLOODING = 18, "Flooding"
+
+    @classmethod
+    def _missing_(cls, value):
+        t = cls.INVALID
+        t._value_ = value
+        return t
+
+    def __new__(cls, keycode, text):
+        obj = object.__new__(cls)
+        obj._value_ = keycode
+        obj.text = text
+        return obj
+
+
+class Tarot(Enum):
+    INVALID = -1, "----"
+    MAGICIAN = 0, "I The Magician"
+    HIGH_PRIESTESS = 1, "II The High Priestess"
+    EMPRESS = 2, "III The Empress"
+    EMPEROR = 3, "IIII The Emperor"
+    POPE = 4, "V The Pope"
+    LOVER = 5, "VI The Lover"
+    CHARIOT = 6, "VII The Chariot"
+    JUSTICE = 7, "VIII Justice"
+    HERMIT = 8, "VIIII The Hermit"
+    WHEEL_OF_FORTUNE = 9, "X The Wheel of Fortune"
+    STRENGTH = 10, "XI Strength"
+    HANGED_MAN = 11, "XII The Hanged Man"
+    DEATH = 12, "XIII"
+    TEMPERANCE = 13, "XIIII Temperance"
+    DEVIL = 14, "XV The Devil"
+    TOWER = 15, "XVI The Tower"
+    STAR = 16, "XVII The Star"
+    MOON = 17, "XVIII The Moon"
+    SUN = 18, "XVIIII The Sun"
+    JUDGMENT = 19, "XX Judgment"
+
+    @classmethod
+    def _missing_(cls, value):
+        t = cls.INVALID
+        t._value_ = value
+        return t
+
+    def __new__(cls, keycode, text):
+        obj = object.__new__(cls)
+        obj._value_ = keycode
+        obj.text = text
+        return obj
+
+
+class DamageType(Enum):
+    INVALID = -1
+    STEAL = 0
+    HOME_STEAL = 1
+    RUN = 2
+    HOME_RUN = 3
+    STRIKE = 4
+    FOUL_BALL = 5
+    STRIKE_OUT = 6
+    FLY_OUT = 7
+    GROUND_OUT = 8
+    SINGLE = 9
+    DOUBLE = 10
+    TRIPLE = 11
+    QUADRUPLE = 12
+    WALK = 13
+    CAUGHT_STEALING = 14
+    BALL = 15
+    SACRIFICE_FLY = 16
+    OUT = 17
+    CURSE_OF_CROWS = 18
+    GIVE_SPIRIT = 19
+    BIG_PEANUT = 20
+    BLOOD_DRAIN = 21
+    PEANUT_SWALLOWED = 22
+    INCINERATION = 23
+    FEEDBACK = 24
+    REVERB = 25
+    UNSHELLED = 26
+    PARTYING = 27
+    LOVE_SPELL = 28
+    PEANUT_YUMMY = 29
+    SUPER_PEANUT_YUMMY = 30
+    SUPER_PEANUT_ALLERGIC = 31
+    REBIRTH = 32
---
+++ packages/blaseball-mike/blaseball_mike-5.8.1-py3-none-any.whl/tests/test_chronicler.py
@@ -0,0 +1,155 @@
+"""
+Unit Tests for Chronicler Endpoints
+"""
+
+import pytest
+import types
+import blaseball_mike.chronicler as chron
+
+
+@pytest.mark.vcr
+def test_chronicler_players():
+    data = chron.get_players()
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 5000))
+def test_chronicler_player_updates(count):
+    data = chron.get_player_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+def test_chronicler_player_names():
+    data = chron.get_player_names()
+    assert isinstance(data, dict)
+    assert len(data.keys()) > 0
+
+
+@pytest.mark.vcr
+def test_chronicler_teams():
+    data = chron.get_teams()
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 5000))
+def test_chronicler_team_updates(count):
+    data = chron.get_team_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 5000))
+def test_chronicler_roster_updates(count):
+    data = chron.get_roster_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 5000))
+def test_chronicler_games(count):
+    data = chron.get_games(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 5000))
+def test_chronicler_game_updates(count):
+    data = chron.get_game_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+def test_chronicler_fights():
+    data = chron.get_fights()
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 1100))
+def test_chronicler_fight_updates(count):
+    data = chron.get_fight_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+def test_chronicler_stadiums():
+    data = chron.get_stadiums()
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 600))
+def test_chronicler_temporal_updates(count):
+    data = chron.get_temporal_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 3000))
+def test_chronicler_sim_updates(count):
+    data = chron.get_sim_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 1500))
+def test_chronicler_globalevent_updates(count):
+    data = chron.get_globalevent_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+def test_chronicler_time_map():
+    data = chron.time_map()
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+
+@pytest.mark.vcr
+def test_chronicler_time_season():
+    data = chron.time_season()
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (1, 100, 1000, 5000))
+def test_chronicler_tribute_updates(count):
+    data = chron.get_tribute_updates(count=count)
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
+@pytest.mark.vcr
+def test_chronicler_none_length():
+    data = chron.get_player_updates(before="2021-03-02T00:00:00Z", after="2021-03-01T00:00:00Z")
+    assert isinstance(data, list)
+    assert len(data) == 3367
+
+
+@pytest.mark.vcr
+@pytest.mark.parametrize("count", (None, 1, 100, 1000, 3000))
+def test_chronicler_lazy(count):
+    data = chron.get_player_updates(before="2021-03-02T00:00:00Z", after="2021-03-01T00:00:00Z", count=count, lazy=True)
+    assert isinstance(data, types.GeneratorType)
+    data = list(data)
+    if count is None:
+        assert len(data) == 3367
+    else:
+        assert len(data) == count
