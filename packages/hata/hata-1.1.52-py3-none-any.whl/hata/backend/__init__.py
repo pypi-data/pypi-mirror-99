5b56e2c62d411876b15732eadaf306d443459e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:07 +0000

    Add package hata/1.1.52/hata-1.1.52-py3-none-any.whl

---
+++ packages/hata/hata-1.1.52-py3-none-any.whl/hata/backend/__init__.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+from .analyzer import *
+from .utils import *
+from .event_loop import *
+from .executor import *
+from .formdata import *
+from .futures import *
+from .http import *
+from .ios import *
+from .websocket import *
+
+__all__ = (
+    *analyzer.__all__           ,
+    *utils.__all__              ,
+    *event_loop.__all__          ,
+    *executor.__all__           ,
+    *formdata.__all__           ,
+    *futures.__all__            ,
+    *http.__all__               ,
+    *ios.__all__                ,
+    *websocket.__all__          ,
+        )
