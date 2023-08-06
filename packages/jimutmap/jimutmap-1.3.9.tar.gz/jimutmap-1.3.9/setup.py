df16b32a6495324fe3becf9274db2be8496c691e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:20 +0000

    Add package jimutmap/1.3.9/jimutmap-1.3.9.tar.gz

---
+++ packages/jimutmap/jimutmap-1.3.9.tar.gz/jimutmap-1.3.9/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jimutmap",
+    version="1.3.9",
+    author="Jimutmap Contributors",
+    author_email="jimutbahanpal@yahoo.com",
+    description="To get enormous amount of Apple Maps tile with ease",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jimut123/jimutmap",
+    install_requires=['certifi==2020.12.5','chardet==4.0.0','chromedriver-autoinstaller==0.2.2','idna==2.10',
+                      'numpy==1.19.5','requests==2.25.1','selenium==3.141.0','tqdm==4.53.0','urllib3==1.26.3'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
