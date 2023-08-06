0854c124a9b42a1b319c44f022b8c3e11fcbacb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:28 +0000

    Add package discordSplash/0.0.7a0/discordSplash-0.0.7a0.tar.gz

---
+++ packages/discordSplash/discordSplash-0.0.7a0.tar.gz/discordSplash-0.0.7a0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="discordSplash",
+    version="0.0.7a0",
+    author="Mineinjava",
+    author_email="Mineinjava@gmail.com",
+    description="An API wrapper for Discord's slash commands. Written in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mineinjava/discord-splash",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.7',
+)
