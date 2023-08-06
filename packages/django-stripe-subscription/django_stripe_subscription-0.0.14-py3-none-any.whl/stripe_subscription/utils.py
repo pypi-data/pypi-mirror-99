4ea127918de78216a463c2e17bb1bf86f85b1d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:26 +0000

    Add package django-stripe-subscription/0.0.14/django_stripe_subscription-0.0.14-py3-none-any.whl

---
+++ packages/django-stripe-subscription/django_stripe_subscription-0.0.14-py3-none-any.whl/stripe_subscription/utils.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+
+from django.conf import settings
+
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+def get_product(plan):
+    """
+    Returns stripe product id for given plan.
+    :param plan: billing.models.Plan
+    """
+
+    if not plan.product_id:
+        product = stripe.Product.create(
+                name=plan.title)
+        plan.product_id = product.id
+
+    return plan.product_id
+
+
+def create_price_on_stripe(plan):
+    """
+    Creates stripe.Price object via SDK.
+    :param plan: billing.models.Plan
+    """
+
+    price = stripe.Price.create(
+        unit_amount=int(plan.price * 100),
+        currency="usd",
+        product=get_product(plan),
+        recurring={"interval": plan.recurring_interval})
+
+    plan.price_id = price.id
+
+
+def update_price_on_stripe(plan):
+    """
+    Removes stripe.Price object via SDK.
+    :param plan: billing.models.Plan
+    """
+    stripe.Price.modify(
+        plan.price_id,
+        active=plan.published)
+
+
+def get_trial_end(days=3):
+    """
+    Returns trial end timestamp.
+    :param days: Trial period
+    :returns int: Trial end timestamp
+    """
+    trial_end = datetime.now() + timedelta(days=days)
+    return int(trial_end.timestamp())
---
+++ packages/django-stripe-subscription/django_stripe_subscription-0.0.14-py3-none-any.whl/stripe_subscription/views.py
@@ -0,0 +1,62 @@
+from django.http import JsonResponse
+from django.middleware.csrf import get_token
+from django.contrib.auth.decorators import login_required
+
+from .models import Plan, Subscription
+from .forms import SubscribeForm
+
+
+def session(request):
+    """
+    Returns current user session.
+    """
+
+    if request.user and request.user.is_authenticated:
+        qs = Plan.objects.list_published()
+
+        return JsonResponse({
+            "status": "ok",
+            "session": {
+                "csrf_token": get_token(request),
+                "user": request.user.email,
+                "plans": [
+                    {'id': p.id, 'title': p.title, 'price': p.price}
+                    for p in qs
+                ]
+            }
+        })
+
+    return JsonResponse({"status": "error"}, status=400)
+
+
+@login_required
+def subscribe(request):
+    """
+    Creates new subscription for current user with payment token and plan.
+    """
+
+    if request.method == "POST":
+        form = SubscribeForm(request.POST)
+
+        if form.is_valid():
+            token = form.cleaned_data['stripe_token']
+            plan = form.cleaned_data['plan']
+            quiz = form.cleaned_data['quiz']
+
+            sub = Subscription.create_from_token(
+                    token,
+                    plan,
+                    request.user,
+                    quiz)
+
+            if sub:
+                return JsonResponse({
+                    "status": "ok",
+                    "deep_link": sub.deep_link
+                })
+        else:
+            return JsonResponse(
+                    {"status": "error", "errors": form.errors},
+                    status=400)
+
+    return JsonResponse({"status": "error"}, status=400)
