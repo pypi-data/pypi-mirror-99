f5b4b4d7aab9d0963faf47156ccd49e2653775e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:55 +0000

    Add package npx/0.0.2/npx-0.0.2.tar.gz

---
+++ packages/npx/npx-0.0.2.tar.gz/npx-0.0.2/npx/__init__.py
@@ -0,0 +1,12 @@
+from .__about__ import __version__
+from .main import add_at, dot, solve, subtract_at, sum_at, unique_rows
+
+__all__ = [
+    "__version__",
+    "dot",
+    "solve",
+    "sum_at",
+    "add_at",
+    "subtract_at",
+    "unique_rows",
+]
