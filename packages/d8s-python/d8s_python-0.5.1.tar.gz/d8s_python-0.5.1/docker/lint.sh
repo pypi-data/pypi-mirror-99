1a907c74836510faf753207166dcebdaf273bedd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:33 +0000

    Add package d8s-python/0.5.1/d8s_python-0.5.1.tar.gz

---
+++ packages/d8s-python/d8s_python-0.5.1.tar.gz/d8s_python-0.5.1/docker/lint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+echo "Running linters and formatters..."
+
+isort d8s_python/ tests/
+
+black d8s_python/ tests/
+
+mypy d8s_python/ tests/
+
+pylint --fail-under 9 d8s_python/*.py
+
+flake8 d8s_python/ tests/
+
+bandit -r d8s_python/
+
+# we run black again at the end to undo any odd changes made by any of the linters above
+black d8s_python/ tests/
