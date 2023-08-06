a030c6fc6a4d68882eea428c442a89d5b50c1b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:12 +0000

    Add package homematicip/0.13.1.post0.dev15/homematicip-0.13.1.post0.dev15.tar.gz

---
+++ packages/homematicip/homematicip-0.13.1.post0.dev15.tar.gz/homematicip-0.13.1.post0.dev15/homematicip/connection.py
@@ -0,0 +1,62 @@
+import hashlib
+import json
+import locale
+import logging
+import platform
+
+import requests
+
+from homematicip.base.base_connection import BaseConnection
+
+logger = logging.getLogger(__name__)
+
+
+class Connection(BaseConnection):
+    def init(
+        self,
+        accesspoint_id,
+        lookup=True,
+        lookup_url="https://lookup.homematic.com:48335/getHost",
+        **kwargs
+    ):
+        self.set_token_and_characteristics(accesspoint_id)
+
+        if lookup:
+            while True:
+                try:
+                    result = requests.post(
+                        lookup_url, json=self.clientCharacteristics, timeout=3
+                    )
+                    js = json.loads(result.text)
+                    self._urlREST = js["urlREST"]
+                    self._urlWebSocket = js["urlWebSocket"]
+                    break
+                except Exception as e:
+                    pass
+        else:  # pragma: no cover
+            self._urlREST = "https://ps1.homematic.com:6969"
+            self._urlWebSocket = "wss://ps1.homematic.com:8888"
+
+    def _restCall(self, path, body=None):
+        result = None
+        requestPath = "{}/hmip/{}".format(self._urlREST, path)
+        logger.debug("_restcall path(%s) body(%s)", requestPath, body)
+        for i in range(0, self._restCallRequestCounter):
+            try:
+                result = requests.post(
+                    requestPath,
+                    data=body,
+                    headers=self.headers,
+                    timeout=self._restCallTimout,
+                )
+                ret = result.json() if len(result.content) != 0 else ""
+                logger.debug(
+                    "_restcall result: Errorcode=%s content(%s)",
+                    result.status_code,
+                    ret,
+                )
+                return ret
+            except requests.Timeout:
+                logger.error("call to '%s' failed due Timeout", requestPath)
+                pass
+        return {"errorCode": "TIMEOUT"}
---
+++ packages/homematicip/homematicip-0.13.1.post0.dev15.tar.gz/homematicip-0.13.1.post0.dev15/setup.py
@@ -0,0 +1,46 @@
+import io
+import os
+import sys
+from distutils.core import setup
+
+from setuptools import find_packages
+from setuptools.command.install import install
+
+import versioneer
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+setup(
+    name="homematicip",
+    packages=find_packages(exclude="tests"),
+    long_description=long_description,
+    description="An API for the homematicip cloud",
+    author="Heimo Stieg",
+    author_email="stieg@corona-bytes.net",
+    url="https://github.com/coreGreenberet/homematicip-rest-api",
+    download_url="https://github.com/coreGreenberet/homematicip-rest-api/tarball/"
+    + versioneer.get_version(),
+    keywords=["homematicip"],  # arbitrary keywords
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=[
+        "requests>=2.24.0",
+        "websocket-client>=0.57.0",
+        "websockets>=8.1",
+        "aiohttp>=3.6.2",
+        "async_timeout>=3.0.1",
+        "aenum>=2.2.4",
+    ],
+    package_data={"homematicip_demo": ["json_data/*.json"],},  # Optional
+    scripts=["hmip_cli.py", "hmip_generate_auth_token.py"],
+    python_requires="~=3.6",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
