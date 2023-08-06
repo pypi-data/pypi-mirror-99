8ee3199d433169a33a4730f940ad1af0d893947f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:35 +0000

    Add package vk-slaves/1.1.1/vk_slaves-1.1.1.tar.gz

---
+++ packages/vk-slaves/vk_slaves-1.1.1.tar.gz/vk_slaves-1.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='vk_slaves',
+    version='1.1.1',
+    description='Simple library for working with the VK Slaves',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='WTFPL',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    author='MaxUNof',
+    author_email='maxunof@pm.me',
+    keywords=['VK', 'Slaves', 'VKSlaves'],
+    url='https://github.com/MaxUNof/vk-slaves',
+    download_url='https://pypi.org/project/vk_slaves/',
+    install_requires=['requests']
+)
