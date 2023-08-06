336cafb9929bee257ff2b74aecc771d4dc19a5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:17 +0000

    Add package odoo10-addon-shopinvader-sale-report/10.0.1.0.1.99.dev1/odoo10_addon_shopinvader_sale_report-10.0.1.0.1.99.dev1-py2-none-any.whl

---
+++ packages/odoo10-addon-shopinvader-sale-report/odoo10_addon_shopinvader_sale_report-10.0.1.0.1.99.dev1-py2-none-any.whl/odoo/addons/shopinvader_sale_report/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Shopinvader sale report",
+    "summary": """Shopinvader addons to extend sale report with backend""",
+    "author": "ACSONE SA/NV",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "category": "e-commerce",
+    "version": "10.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["sale", "shopinvader"],
+}
