8cc566235665b31240bc3776dcc18e735de75b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:33 +0000

    Add package acabim-common-services-cas/1.0.48/acabim-common-services-cas-1.0.48.tar.gz

---
+++ packages/acabim-common-services-cas/acabim-common-services-cas-1.0.48.tar.gz/acabim-common-services-cas-1.0.48/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='acabim-common-services-cas',
+    version='1.0.48',
+    author='Compliance Audit Systems',
+    description='Contains the model classes and interfaces which provide the services required for ACABIM Backend '
+                'Services in Python',
+    packages=setuptools.find_packages()
+)
