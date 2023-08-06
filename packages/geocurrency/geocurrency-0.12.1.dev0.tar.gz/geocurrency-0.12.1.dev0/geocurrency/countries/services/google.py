c08fcf9bdea8da73ce49ffc3a833802d7f5eb847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:06 +0000

    Add package geocurrency/0.12.1.dev0/geocurrency-0.12.1.dev0.tar.gz

---
+++ packages/geocurrency/geocurrency-0.12.1.dev0.tar.gz/geocurrency-0.12.1.dev0/geocurrency/countries/services/google.py
@@ -0,0 +1,118 @@
+"""
+Google Geocoder service
+"""
+import json
+import logging
+
+import requests
+from django.conf import settings
+
+from . import Geocoder
+
+GEOCODER_GOOGLE_URL = 'https://maps.googleapis.com/maps/api'
+
+
+class GoogleGeocoder(Geocoder):
+    """
+    Google geocoder class
+    search and reverse from Google
+    """
+    coder_type = 'google'
+    key = None
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Initialize
+        """
+        return super(Geocoder, cls).__new__(cls)
+
+    def __init__(self, key:str = None, *args, **kwargs):
+        """
+        Google geocode engine
+        Init: Geocoder('google', 'API key')
+        """
+        try:
+            if not key or not settings.GEOCODER_GOOGLE_KEY:
+                raise ValueError(
+                    "This geocoder needs an API key, please provide a key"
+                    " or set GEOCODER_GOOGLE_KEY in configuration")
+        except AttributeError:
+            raise ValueError(
+                "This geocoder needs an API key, please provide a key "
+                "or set GEOCODER_GOOGLE_KEY in configuration")
+        self.key = key or settings.GEOCODER_GOOGLE_KEY
+
+    def search(self, address: str, language: str = None, bounds=None,
+               region: str = None, components: str = "") -> dict:
+        """
+        Google geocoding search
+        Retrieves coordinates based on address
+        """
+        try:
+            response = requests.get('{}/{}'.format(
+                GEOCODER_GOOGLE_URL,
+                'geocode/json'
+            ), {
+                'address': address,
+                'key': self.key,
+                'language': language
+            })
+            data = response.json()
+            if data.get('status') != "OK":
+                return {}
+            return data
+        except json.JSONDecodeError as e:
+            logging.error("Invalid response")
+            logging.error(e)
+        except ValueError as e:
+            logging.error("Invalid API configuration")
+            logging.error(e)
+        except IOError as e:
+            logging.error("Invalid request")
+            logging.error(e)
+        return {}
+
+    def reverse(self, lat:str, lng:str) -> dict:
+        """
+        Google geocoding reverse
+        :param lat: latitude
+        :param lng: longitude
+        """
+        try:
+            response = requests.get('{}/{}'.format(
+                GEOCODER_GOOGLE_URL,
+                'geocode/json'
+            ),
+                {
+                    'latlng': ",".join(map(str, [lat, lng])),
+                    'key': self.key,
+                })
+            data = response.json()
+            if data.get('status') != "OK":
+                return {}
+            return data
+        except json.JSONDecodeError as e:
+            logging.error("Invalid response")
+            logging.error(e)
+        except ValueError as e:
+            logging.error("Invalid API configuration")
+            logging.error(e)
+        except IOError as e:
+            logging.error("Invalid request")
+            logging.error(e)
+        return {}
+
+    def parse_countries(self, data: dict) -> [str]:
+        """
+        parse response from google service
+        :params data: geocoding / reverse geocoding json
+        :return: array of alpha2 codes
+        """
+        alphas = []
+        if not data:
+            return alphas
+        for feature in data.get('results'):
+            for address_component in feature.get('address_components'):
+                if 'country' in address_component.get('types'):
+                    alphas.append(address_component.get('short_name'))
+        return alphas
---
+++ packages/geocurrency/geocurrency-0.12.1.dev0.tar.gz/geocurrency-0.12.1.dev0/geocurrency/currencies/models.py
@@ -0,0 +1,136 @@
+"""
+Currencies models
+"""
+import logging
+from datetime import date
+from typing import Iterator
+
+from django.contrib.auth.models import User
+from django.core.cache import cache
+from django.db import models
+from geocurrency.countries.models import Country
+from iso4217 import Currency as Iso4217
+
+from . import CURRENCY_SYMBOLS, DEFAULT_SYMBOL
+
+
+class CurrencyNotFoundError(Exception):
+    """
+    Exception for not found currency
+    """
+    msg = 'Currency not found'
+
+
+class Currency:
+    """
+    Mock for hinting
+    """
+    pass
+
+
+class Currency:
+    """
+    Currency class, wrapper for ISO-4217
+    """
+    code = None
+    name = None
+    currency_name = None
+    exponent = None
+    number = 0
+    value = None
+
+    def __init__(self, code):
+        """
+        Initialize an iso4217.Currency instance
+        """
+        try:
+            i = Iso4217(code)
+            for a in ['code', 'name', 'currency_name', 'exponent', 'number', 'value']:
+                setattr(self, a, getattr(i, a))
+        except ValueError:
+            raise CurrencyNotFoundError('Invalid currency code')
+
+    @classmethod
+    def is_valid(cls, cur: str) -> bool:
+        """
+        Checks if currency is part of iso4217
+        """
+        try:
+            Currency(cur)
+            return True
+        except CurrencyNotFoundError:
+            return False
+
+    @classmethod
+    def all_currencies(cls, ordering: str = 'name') -> Iterator[Currency]:
+        """
+        Returns a sorted list of currencies
+        :param ordering: sort attribute
+        """
+        if ordering not in ['code', 'name', 'currency_name', 'exponent', 'number', 'value']:
+            ordering = 'name'
+        return sorted([Currency(c.code) for c in Iso4217], key=lambda x: getattr(x, ordering))
+
+    @property
+    def countries(self) -> Iterator[Country]:
+        """
+        List countries using this currency
+        :return: List of Country objects
+        """
+        countries = []
+        if cached_countries := cache.get(self.code + 'COUNTRIES'):
+            return [Country(alpha_2) for alpha_2 in cached_countries]
+        for country in Country.all_countries():
+            try:
+                if self.code in country.currencies():
+                    countries.append(country)
+            except KeyError:
+                # Some countries listed in pycountry are not present in countryinfo
+                pass
+        cache.set(self.code + 'COUNTRIES', [country.alpha_2 for country in countries])
+        return countries
+
+    @classmethod
+    def get_for_country(cls, alpha2: str) -> Iterator[Currency]:
+        """
+        Return a list of currencies for an alpha2 country code
+        :params alpha2: alpha2 code of a country
+        """
+        try:
+            country = Country(alpha2)
+            return [Currency(cur) for cur in country.currencies()]
+        except CurrencyNotFoundError as e:
+            logging.error("Error fetching currency")
+            logging.error(e)
+            return []
+
+    def get_rates(self, user: User = None, key: str = None, base_currency: str = None,
+                  start_date: date = None,
+                  end_date: date = None) -> Iterator:
+        """
+        Return a list of rates for this currency and an optional base currency between two dates
+        :params base_currency: code of the base currency
+        :params start_date: rates from that date included
+        :params end_date: rates to that date included
+        :return: List of rates
+        """
+        from geocurrency.rates.models import Rate
+        qs = Rate.objects.filter(currency=self.code)
+        if user:
+            qs = qs.filter(models.Q(user=user) | models.Q(user=None))
+        if key:
+            qs = qs.filter(key=key)
+        if base_currency:
+            qs = qs.filter(base_currency=base_currency)
+        if start_date:
+            qs = qs.filter(value_date__gte=start_date)
+        if end_date:
+            qs = qs.filter(value_date__lte=end_date)
+        return qs
+
+    @property
+    def symbol(self):
+        """
+        Returns the symbol for this currency based on __init__.CURRENCY_SYMBOLS
+        """
+        return CURRENCY_SYMBOLS.get(self.code, DEFAULT_SYMBOL)
