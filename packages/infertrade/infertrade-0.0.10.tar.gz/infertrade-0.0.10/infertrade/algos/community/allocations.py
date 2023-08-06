b0b775d5d4dae4c0513ebb1cc229a7a7d9ee5fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:25 +0000

    Add package infertrade/0.0.10/infertrade-0.0.10.tar.gz

---
+++ packages/infertrade/infertrade-0.0.10.tar.gz/infertrade-0.0.10/infertrade/algos/community/allocations.py
@@ -0,0 +1,81 @@
+"""
+Functions used to compute allocations - % of your portfolio to invest in a market or asset.
+
+Copyright 2021 InferStat Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Created by: Joshua Mason
+Created date: 11/03/2021
+"""
+
+
+import pandas as pd
+from sklearn.preprocessing import FunctionTransformer
+from infertrade.PandasEnum import PandasEnum
+
+
+def fifty_fifty(dataframe) -> pd.DataFrame:
+    """Allocates 50% of strategy budget to asset, 50% to cash."""
+    dataframe[PandasEnum.ALLOCATION.value] = 0.5
+    return dataframe
+
+
+def buy_and_hold(dataframe) -> pd.DataFrame:
+    """Allocates 100% of strategy budget to asset, holding to end of period (or security bankruptcy)."""
+    dataframe[PandasEnum.ALLOCATION.value] = 1.0
+    return dataframe
+
+
+def constant_allocation_size(dataframe: pd.DataFrame, fixed_allocation_size: float = 1.0) -> pd.DataFrame:
+    """
+    Returns a constant allocation, controlled by the constant_position_size parameter.
+
+    parameters:
+    constant_allocation_size: determines allocation size.
+    """
+    dataframe[PandasEnum.ALLOCATION.value] = fixed_allocation_size
+    return dataframe
+
+
+def high_low_difference(dataframe: pd.DataFrame, scale: float = 1.0, constant: float = 0.0) -> pd.DataFrame:
+    """
+    Returns an allocation based on the difference in high and low values. This has been added as an
+    example with multiple series and parameters
+
+    parameters:
+    scale: determines amplitude factor.
+    """
+    dataframe[PandasEnum.ALLOCATION.value] = (dataframe["high"] - dataframe["low"]) * scale + constant
+    return dataframe
+
+
+infertrade_export_allocations = {
+    "fifty_fifty": {"function": fifty_fifty, "parameters": {}, "series": []},
+    "buy_and_hold": {"function": buy_and_hold, "parameters": {}, "series": []},
+    "constant_allocation_size": {
+        "function": constant_allocation_size,
+        "parameters": {"fixed_allocation_size": 1.0},
+        "series": [],
+    },
+    "high_low_difference": {
+        "function": high_low_difference,
+        "parameters": {"scale": 1.0, "constant": 0.0},
+        "series": ["high", "low"],
+    },
+}
+
+
+def scikit_allocation_factory(allocation_function: callable) -> FunctionTransformer:
+    """This creates a SciKit Learn compatible Transformer embedding the position calculation."""
+    return FunctionTransformer(allocation_function)
---
+++ packages/infertrade/infertrade-0.0.10.tar.gz/infertrade-0.0.10/infertrade/base.py
@@ -0,0 +1,66 @@
+"""
+Base functionality
+
+Copyright 2021 InferStat Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Created by: Joshua Mason and Thomas Oliver
+Created date: 11/03/2021
+"""
+
+import pandas as pd
+from copy import deepcopy
+from infertrade.utilities.performance import calculate_portfolio_performance_python
+
+
+def get_signal_calc(func: callable, adapter: callable = None) -> callable:
+    """An adapter to calculate a signal prior to usage within a trading rule."""
+    if adapter:
+        func = adapter(func)
+    return func
+
+
+def get_positions_calc(df: pd.DataFrame, func: callable) -> callable:
+    """Pass through method to return the results of the allocation calculation."""
+    try:
+        df_with_positions = func(df)
+    except IndexError as index_error:
+        df_with_positions = deepcopy(df)
+        print("Index error occurred: ", index_error)
+        df_with_positions[PandasEnum.ALLOCATION.value] = 1.0
+
+    return df_with_positions
+
+
+def get_portfolio_calc(func: callable) -> callable:
+    """Given a position calculation generates the portfolio valuation index."""
+
+    def get_portfolio(df: pd.DataFrame):
+        """Inner function to apply the position calculation."""
+        position_data = func(df)
+        return _get_portfolio(position_data)
+
+    return get_portfolio
+
+
+def _get_portfolio(df: pd.DataFrame) -> pd.DataFrame:
+    """Calculates the cumulative portfolio performance."""
+    try:
+        df_with_returns = calculate_portfolio_performance_python(df)
+    except IndexError as index_error:
+        df_with_returns = deepcopy(df)
+        print("Index error occurred: ", index_error)
+        df_with_returns["portfolio_returns"] = 1.0
+
+    return df_with_returns
