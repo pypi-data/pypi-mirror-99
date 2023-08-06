7379cc121df508c60f54897b1f4d9c4779c8bb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:42 +0000

    Add package geotiff/0.1.2/geotiff-0.1.2.tar.gz

---
+++ packages/geotiff/geotiff-0.1.2.tar.gz/geotiff-0.1.2/geotiff/utils/crs_code_guess.py
@@ -0,0 +1,47 @@
+from tifffile.tifffile_geodb import Proj, GCSE, PCS, GCS, Ellipse, DatumE, Datum  # type: ignore
+from typing import List, Tuple
+from difflib import SequenceMatcher
+
+PreDict = List[Tuple[str, int]]
+
+
+def crs_code_gusser(GTCitationGeo: str) -> Tuple[int, float]:
+    """This is a very hacky solution to the problem of finding the correct 
+    crs code based on the GTCitationGeoKey string.
+
+    Args:
+        GTCitationGeo (str): GT Citation Geo Key
+
+    Returns:
+        Tuple[int, float]: ([The crs code], [the score of the guess from 0 to 1])
+    """
+    crs_code: int = 32767
+    projs: PreDict = [(name, member.value)
+                      for name, member in Proj.__members__.items()]
+    pcss: PreDict = [(name, member.value)
+                     for name, member in PCS.__members__.items()]
+    gcse: PreDict = [(name, member.value)
+                     for name, member in GCSE.__members__.items()]
+    gcs: PreDict = [(name, member.value)
+                    for name, member in GCS.__members__.items()]
+    # TODO
+    # ! handel these!
+    ellipse: PreDict = [(name, member.value)
+                        for name, member in Ellipse.__members__.items()]
+    datumE: PreDict = [(name, member.value)
+                       for name, member in DatumE.__members__.items()]
+    datum: PreDict = [(name, member.value)
+                      for name, member in Datum.__members__.items()]
+    all_crs = dict(projs + pcss + gcse + gcs)  # + ellipse + datumE + datum)
+    # takes a guess based on the GTCitationGeoKey
+    info_str: str = GTCitationGeo
+    best_score: float = 0.0
+    crs_key: str = ""
+    for crs in all_crs.keys():
+        score: float = SequenceMatcher(None, info_str, str(crs)).ratio()
+        if score > best_score:
+            best_score = score
+            crs_key = crs
+            crs_code = all_crs[crs_key]
+
+    return(crs_code, best_score)
---
+++ packages/geotiff/geotiff-0.1.2.tar.gz/geotiff-0.1.2/setup.py
@@ -0,0 +1,58 @@
+import setuptools
+import os
+import sys
+from setuptools.command.install import install
+
+VERSION = "0.1.2"
+
+# Send to pypi
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag == None:
+            info = "No new version to upload"
+            print(info)
+        elif tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+setuptools.setup(
+    name="geotiff",
+    version=VERSION,
+    author="Kipling Crossing",
+    author_email="kip.crossing@gmail.com",
+    description="A noGDAL tool for reading and writing geotiff files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Open-Source-Agriculture/geotiff",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'shapely',
+        'tifffile',
+        'numpy',
+        'pyproj',
+        'zarr',
+        'pycrs',
+    ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
+)
