32f3f6e5cd050cd565ec6d61541064abdd3b0b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package cythereal-dashboard/0.28.7/cythereal-dashboard-0.28.7.tar.gz

---
+++ packages/cythereal-dashboard/cythereal-dashboard-0.28.7.tar.gz/cythereal-dashboard-0.28.7/cythereal_dashboard/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cythereal_dashboard.api.binaries_api import BinariesApi
+from cythereal_dashboard.api.campaigns_api import CampaignsApi
+from cythereal_dashboard.api.dashboard_api import DashboardApi
+from cythereal_dashboard.api.emails_api import EmailsApi
+from cythereal_dashboard.api.files_api import FilesApi
+from cythereal_dashboard.api.reports_api import ReportsApi
+from cythereal_dashboard.api.signatures_api import SignaturesApi
+from cythereal_dashboard.api.update_api import UpdateApi
+from cythereal_dashboard.api.workspaces_api import WorkspacesApi
