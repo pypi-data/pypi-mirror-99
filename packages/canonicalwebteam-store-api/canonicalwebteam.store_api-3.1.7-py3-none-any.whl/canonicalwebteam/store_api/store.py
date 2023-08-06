3ff5e2440f04084ded1d542ff40739e506a005b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:11 +0000

    Add package canonicalwebteam-store-api/3.1.7/canonicalwebteam.store_api-3.1.7-py3-none-any.whl

---
+++ packages/canonicalwebteam-store-api/canonicalwebteam.store_api-3.1.7-py3-none-any.whl/canonicalwebteam/store_api/store.py
@@ -0,0 +1,195 @@
+from canonicalwebteam.store_api.exceptions import (
+    StoreApiConnectionError,
+    StoreApiResponseDecodeError,
+    StoreApiResponseError,
+    StoreApiResponseErrorList,
+)
+
+
+class Store:
+    def __init__(self, session, store=None):
+        self.config = {1: {"base_url": "", "headers": {}}}
+        self.session = session
+
+    def process_response(self, response):
+        # 5xx responses are not in JSON format
+        if response.status_code >= 500:
+            raise StoreApiConnectionError("Service Unavailable")
+
+        try:
+            body = response.json()
+        except ValueError as decode_error:
+            raise StoreApiResponseDecodeError(
+                "JSON decoding failed: {}".format(decode_error)
+            )
+
+        if not response.ok:
+            if "error_list" in body or "error-list" in body:
+                # V1 and V2 error handling
+                error_body = (
+                    body["error_list"]
+                    if "error_list" in body
+                    else body["error-list"]
+                )
+                raise StoreApiResponseErrorList(
+                    "The API returned a list of errors",
+                    response.status_code,
+                    error_body,
+                )
+            else:
+                raise StoreApiResponseError(
+                    "Unknown error from API", response.status_code
+                )
+
+        if "_embedded" in body:
+            body["results"] = body["_embedded"]["clickindex:package"]
+            del body["_embedded"]
+
+        return body
+
+    def get_endpoint_url(self, endpoint, api_version=1):
+        base_url = self.config[api_version]["base_url"]
+        return f"{base_url}{endpoint}"
+
+    def find(self, query="", category="", fields=[]):
+        """
+        Given a search term, return an array of matching search results.
+        v2 API only.
+        """
+        url = self.get_endpoint_url("find", 2)
+        headers = self.config[2].get("headers")
+
+        params = {"q": query, "category": category}
+
+        if fields:
+            params["fields"] = ",".join(fields)
+
+        return self.process_response(
+            self.session.get(url, params=params, headers=headers)
+        )
+
+    def search(
+        self,
+        search,
+        size=100,
+        page=1,
+        category=None,
+        arch="wide",
+        api_version=1,
+    ):
+        url = self.get_endpoint_url("search", api_version)
+        headers = self.config[api_version].get("headers")
+
+        params = {
+            "q": search,
+            "size": size,
+            "page": page,
+            "scope": "wide",
+            "confinement": "strict,classic",
+            "fields": ",".join(
+                [
+                    "package_name",
+                    "title",
+                    "summary",
+                    "icon_url",
+                    "architecture",
+                    "media",
+                    "publisher",
+                    "developer_validation",
+                    "origin",
+                    "apps",
+                    "sections",
+                ]
+            ),
+        }
+
+        # Arch behaves a bit oddly, but these two together should cover
+        # all bases. That is: "X-Ubuntu-Architecture" header will be used
+        # if present, and "arch" will be ignored *unless* "arch" == "wide".
+        # "wide" doesn't work in "X-Ubuntu-Architecture", but it doesn't
+        # matter because if "arch" == "wide" then "X-Ubuntu-Architecture"
+        # is ignored and all architectures are returned
+        # See: https://api.snapcraft.io/docs/search.html#snap_search
+        headers["X-Ubuntu-Architecture"] = arch
+        params["arch"] = arch
+
+        if category:
+            params["section"] = category
+
+        return self.process_response(
+            self.session.get(url, params=params, headers=headers)
+        )
+
+    def get_all_items(self, size, api_version=1):
+        url = self.get_endpoint_url("search", api_version)
+
+        return self.process_response(
+            self.session.get(
+                url,
+                params={"scope": "wide", "size": size},
+                headers=self.config[api_version].get("headers"),
+            )
+        )
+
+    def get_category_items(self, category, size=10, page=1, api_version=1):
+        return self.search(
+            search="",
+            category=category,
+            size=size,
+            page=page,
+            api_version=api_version,
+        )
+
+    def get_featured_items(self, size=10, page=1, api_version=1):
+        return self.search(
+            search="",
+            category="featured",
+            size=size,
+            page=page,
+            api_version=api_version,
+        )
+
+    def get_publisher_items(self, publisher, size=500, page=1, api_version=1):
+        return self.search(
+            search="publisher:" + publisher,
+            size=size,
+            page=page,
+            api_version=api_version,
+        )
+
+    def get_item_details(self, name, channel=None, fields=[], api_version=2):
+        url = self.get_endpoint_url("info/" + name, api_version)
+
+        params = {"fields": ",".join(fields)}
+
+        if channel:
+            params["channel"] = channel
+
+        return self.process_response(
+            self.session.get(
+                url,
+                params=params,
+                headers=self.config[api_version].get("headers"),
+            )
+        )
+
+    def get_public_metrics(self, json, api_version=1):
+        url = self.get_endpoint_url("metrics")
+
+        headers = self.config[api_version].get("headers", {})
+        headers.update({"Content-Type": "application/json"})
+
+        return self.process_response(
+            self.session.post(url, headers=headers, json=json)
+        )
+
+    def get_categories(self, api_version=2, type="shared"):
+        url = self.get_endpoint_url("categories", api_version)
+
+        return self.process_response(
+            self.session.get(
+                url,
+                headers=self.config[api_version].get("headers"),
+                params={"type": type},
+            )
+        )
---
+++ packages/canonicalwebteam-store-api/canonicalwebteam.store_api-3.1.7-py3-none-any.whl/canonicalwebteam/store_api/stores/charmstore.py
@@ -0,0 +1,179 @@
+from os import getenv
+
+import requests
+
+from canonicalwebteam.store_api.store import Store
+from canonicalwebteam.store_api.publisher import Publisher
+
+CHARMSTORE_API_URL = getenv("CHARMSTORE_API_URL", "https://api.charmhub.io/")
+CHARMSTORE_PUBLISHER_API_URL = getenv(
+    "CHARMSTORE_PUBLISHER_API_URL", "https://api.charmhub.io/"
+)
+CHARMSTORE_VALID_PACKAGE_TYPES = ["charm", "bundle"]
+
+
+class CharmStore(Store):
+    def __init__(self, session=requests.Session(), store=None):
+        super().__init__(session, store)
+
+        self.config = {
+            2: {"base_url": f"{CHARMSTORE_API_URL}v2/charms/"},
+        }
+
+
+class CharmPublisher(Publisher):
+    def __init__(self, session=requests.Session()):
+        super().__init__(session)
+
+        self.config = {
+            1: {"base_url": f"{CHARMSTORE_PUBLISHER_API_URL}v1/"},
+        }
+
+        self.session.headers.update({"Bakery-Protocol-Version": "2"})
+
+    def _get_authorization_header(self, publisher_auth):
+        return {"Macaroons": publisher_auth}
+
+    def get_macaroon(self):
+        """
+        Return a bakery v2 macaroon from the publisher API to be discharged
+        """
+        response = self.session.get(url=self.get_endpoint_url("tokens"))
+
+        return self.process_response(response)["macaroon"]
+
+    def whoami(self, publisher_auth):
+        response = self.session.get(
+            url=self.get_endpoint_url("whoami"),
+            headers=self._get_authorization_header(publisher_auth),
+        )
+
+        return self.process_response(response)
+
+    def get_account_packages(self, publisher_auth, package_type, status=None):
+        """
+        Return publisher packages
+
+        Args:
+            publisher_auth: Serialized macaroon to consume the API.
+            package_type: Type of packages to obtain.
+            status (optional): Only packages with the given status
+
+        Returns:
+            A list of packages
+        """
+
+        if package_type not in CHARMSTORE_VALID_PACKAGE_TYPES:
+            raise ValueError(
+                "Invalid package type. Expected one of: %s"
+                % CHARMSTORE_VALID_PACKAGE_TYPES
+            )
+
+        response = self.session.get(
+            url=self.get_endpoint_url(package_type),
+            headers=self._get_authorization_header(publisher_auth),
+        )
+        packages = self.process_response(response)["results"]
+
+        if status:
+            packages = [p for p in packages if p["status"] == status]
+
+        return packages
+
+    def get_package_metadata(self, publisher_auth, package_type, name):
+        """
+        Get general metadata for a package.
+
+        Args:
+            publisher_auth: Serialized macaroon to consume the API.
+            package_type: Type of packages to obtain.
+
+        Returns:
+            Package general metadata
+        """
+
+        if package_type not in CHARMSTORE_VALID_PACKAGE_TYPES:
+            raise ValueError(
+                "Invalid package type. Expected one of: %s"
+                % CHARMSTORE_VALID_PACKAGE_TYPES
+            )
+
+        response = self.session.get(
+            url=self.get_endpoint_url(f"{package_type}/{name}"),
+            headers=self._get_authorization_header(publisher_auth),
+        )
+
+        return self.process_response(response)["metadata"]
+
+    def update_package_metadata(
+        self, publisher_auth, package_type, name, data
+    ):
+        """
+        Update general metadata for a package.
+
+        Args:
+            publisher_auth: Serialized macaroon to consume the API.
+            package_type: Type of packages to obtain.
+            name: Package name
+            data: Dict with changes to apply
+
+        Returns:
+            Package general metadata with changes applied
+        """
+
+        if package_type not in CHARMSTORE_VALID_PACKAGE_TYPES:
+            raise ValueError(
+                "Invalid package type. Expected one of: %s"
+                % CHARMSTORE_VALID_PACKAGE_TYPES
+            )
+
+        response = self.session.patch(
+            url=self.get_endpoint_url(f"{package_type}/{name}"),
+            headers=self._get_authorization_header(publisher_auth),
+            json=data,
+        )
+
+        return self.process_response(response)["metadata"]
+
+    def register_package_name(self, publisher_auth, data):
+        """
+        Register a package name.
+
+        Args:
+            publisher_auth: Serialized macaroon to consume the API.
+            data: Dict with name, type and visibility of the package
+
+        Returns:
+            Newly registered name id
+        """
+
+        response = self.session.post(
+            url=self.get_endpoint_url("charm"),
+            headers=self._get_authorization_header(publisher_auth),
+            json=data,
+        )
+
+        return self.process_response(response)
+
+    def get_charm_libraries(self, charm_name):
+        response = self.session.post(
+            url=self.get_endpoint_url("charm/libraries/bulk"),
+            json=[{"charm-name": charm_name}],
+        )
+
+        return self.process_response(response)
+
+    def get_charm_library(self, charm_name, library_id, api_version=None):
+        params = {}
+
+        if api_version is not None:
+            params["api"] = api_version
+
+        response = self.session.get(
+            url=self.get_endpoint_url(
+                f"charm/libraries/{charm_name}/{library_id}"
+            ),
+            params=params,
+        )
+
+        return self.process_response(response)
