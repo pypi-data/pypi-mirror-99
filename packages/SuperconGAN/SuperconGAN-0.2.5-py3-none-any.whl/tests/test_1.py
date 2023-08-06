dfbcf75b02d604d6a208be4fc4cc33cfab4aa724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:48 +0000

    Add package SuperconGAN/0.2.5/SuperconGAN-0.2.5-py3-none-any.whl

---
+++ packages/SuperconGAN/SuperconGAN-0.2.5-py3-none-any.whl/tests/test_1.py
@@ -0,0 +1,15 @@
+"""
+Tests to ensure that everything is working correctly.
+"""
+
+import SuperconGAN
+
+
+
+def test1():
+    """
+    Test that will instantiate a model object and fit it.
+    """
+    model = SuperconGAN.Synthesizer()
+    model.fit()
+    pass
