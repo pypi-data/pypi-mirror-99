51a8afd934ef28c1e4933457ba200e0c0b043997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:12 +0000

    Add package SFLvault-client-qt/0.9.1/SFLvault-client-qt-0.9.1.tar.gz

---
+++ packages/SFLvault-client-qt/SFLvault-client-qt-0.9.1.tar.gz/SFLvault-client-qt-0.9.1/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+
+setup(
+    name='SFLvault-client-qt',
+    version="0.9.1",
+    description='Networked credentials store and authentication manager - Qt Client',
+    author='Savoir-faire Linux',
+    author_email='contact@savoirfairelinux.com',
+    url='https://www.sflvault.org',
+    license='GPLv2',
+    install_requires=[
+                    #"PyQt",
+                      "SFLvault-client~=0.9",
+                      "SFLvault-common~=0.9",
+                      ],
+    include_package_data=True,
+    packages=find_packages(exclude=['ez_setup']),
+    test_suite='nose.collector',
+    #message_extractors = {'sflvault': [
+    #        ('**.py', 'python', None),
+    #        ('templates/**.mako', 'mako', None),
+    #        ('public/**', 'ignore', None)]},
+    entry_points="""
+    [console_scripts]
+    sflvault-client-qt = sflvault.clientqt:main
+    """,
+)
+
+
