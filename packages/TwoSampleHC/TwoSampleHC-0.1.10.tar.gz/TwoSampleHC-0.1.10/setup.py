04a42cb9d46bfcf866cd0cdf68035feeed63eae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:48 +0000

    Add package TwoSampleHC/0.1.10/TwoSampleHC-0.1.10.tar.gz

---
+++ packages/TwoSampleHC/TwoSampleHC-0.1.10.tar.gz/TwoSampleHC-0.1.10/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("./TwoSampleHC/README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TwoSampleHC", 
+    version="0.1.10",
+    author="Alon Kipnis",
+    author_email="alonkipnis@gmail.com",
+    description="Two-sample Higher Criticism",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alonkipnis/TwoSampleHC",
+    download_url="https://github.com/alonkipnis/TwoSampleHC",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = [
+    'numpy>=1.6',
+    'scipy>=1.2',
+    'pandas>=0.24'
+]
+)
