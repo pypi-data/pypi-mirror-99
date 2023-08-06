390030e568738981397e31079ebee09384beb5d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:09 +0000

    Add package bigfix-prefetch/0.0.6/bigfix_prefetch-0.0.6-py3-none-any.whl

---
+++ packages/bigfix-prefetch/bigfix_prefetch-0.0.6-py3-none-any.whl/bigfix_prefetch/__init__.py
@@ -0,0 +1,11 @@
+"""
+import functions so they can be run as the module
+"""
+
+from .prefetch_from_dictionary import prefetch_from_dictionary
+from .prefetch_from_file import file_to_prefetch as prefetch_from_file
+from .prefetch_from_url import url_to_prefetch as prefetch_from_url
+from .prefetch_parse import parse_prefetch as prefetch_parse
+from .prefetch_validate import validate_prefetch as prefetch_validate
+from .prefetch import prefetch
+from .prefetches_have_matching_hashes import prefetches_have_matching_hashes
