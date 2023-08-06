a592aa9f284298268a578b2edfbd7459aed212c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:21 +0000

    Add package percentage/0.0.0b1/percentage-0.0.0b1.tar.gz

---
+++ packages/percentage/percentage-0.0.0b1.tar.gz/percentage-0.0.0b1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+setuptools.setup(
+    name="percentage",
+    version="0.0.0b1",
+    license='BSD',
+    author="jeong seojin",
+    author_email="seojin123456@naver.com",
+    description="Korean High school curriculum(part of percentage) with python code!",
+    long_description=open('README.md').read(),
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+    ],
+)
