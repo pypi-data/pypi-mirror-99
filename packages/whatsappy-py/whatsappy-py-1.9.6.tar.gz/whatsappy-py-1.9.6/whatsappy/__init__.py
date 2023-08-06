923bbe43cc3e47dedb58ae29e9623efd00d795c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:14 +0000

    Add package whatsappy-py/1.9.6/whatsappy-py-1.9.6.tar.gz

---
+++ packages/whatsappy-py/whatsappy-py-1.9.6.tar.gz/whatsappy-py-1.9.6/whatsappy/__init__.py
@@ -0,0 +1,47 @@
+from .chat import (
+    last_message, new_message, send, reply, last,
+    send_file, reply_privately, reply_file_privately
+)
+from .actions import (
+    add_to_group, remove_from_group, make_group_admin, 
+    select_chat_by_number, select_chat_by_name, create_group
+)
+from .group import change_group_description, change_group_name, leave_group
+from .get import get_pinned_chats, get_recent_chats, get_group_invite_link
+from .login import login, close
+
+last = last
+
+class Whatsapp:
+
+    login                       = login
+    close                       = close
+
+    add_to_group                = add_to_group
+    remove_from_group           = remove_from_group
+    make_group_admin            = make_group_admin
+
+    last_message                = last_message
+    new_message                 = new_message
+    send                        = send
+    send_file                   = send_file
+    reply                       = reply
+    reply_privately             = reply_privately
+    reply_file_privately        = reply_file_privately
+    select_chat_by_name         = select_chat_by_name
+    select_chat_by_number       = select_chat_by_number
+
+    get_pinned_chats            = get_pinned_chats
+    get_recent_chats            = get_recent_chats
+    get_group_invite_link       = get_group_invite_link
+
+    change_group_description    = change_group_description
+    change_group_name           = change_group_name
+    leave_group                 = leave_group
+    create_group                = create_group
+
+    # TODO: Change profile picture
+
+    # TODO: Change group profile picture url
+
+whatsapp = Whatsapp()
---
+++ packages/whatsappy-py/whatsappy-py-1.9.6.tar.gz/whatsappy-py-1.9.6/whatsappy/get.py
@@ -0,0 +1,91 @@
+from .tool import error_log
+import traceback
+
+def get_recent_chats(self):
+    """Returns a list of all recent chats
+
+    Returns:
+        List: All the recent chats
+    """
+    
+    try:
+        array = self.driver.execute_script('''
+        let asd = document.querySelectorAll('._2Z4DV');
+        let newArray = new Array();
+        for (var i = 0; i < asd.length; i++){
+            newArray.push(asd[i].querySelector('.N2dUK').textContent);
+        };
+
+        return newArray;
+        ''')
+
+        return array
+    except:
+        error_log(traceback.format_exc())
+
+
+def get_pinned_chats(self):
+    """Returns a list of all pinned chats
+
+    Returns:
+        List: All the pinned chats
+    """
+
+    try:
+        array = self.driver.execute_script('''
+        let pin = document.querySelectorAll('._3EhXO');
+        let newArray = new Array();
+        for (var i = 0; i < pin.length; i++) {
+            newArray.push(pin[i].parentNode.parentNode.parentNode.parentNode.querySelector('._3Dr46').textContent)
+        }
+
+        return newArray
+        ''')
+
+        return array
+    except:
+        error_log(traceback.format_exc())
+
+
+def get_group_invite_link(self):
+    """Returns group invite link
+
+    Returns:
+        str: Group invite link
+    """
+
+    try:
+
+        self.driver.find_element_by_css_selector(
+            '#main > header > div._2uaUb > div.z4t2k > div > span').click()
+
+        sleep(1)
+
+        try:
+            self.driver.find_element_by_css_selector(
+                '#app > div > div > div.Akuo4 > div._1Flk2._3xysY > span > div > span > div > div > section > div._3ZEdX._3hiFt._82zXh > div._3NATg > div > div > span._2zDdK > div')
+
+        except:
+            print('You are not a group admin!')
+            return
+
+        self.driver.find_element_by_css_selector(
+            '#app > div > div > div.Akuo4 > div._1Flk2._3xysY > span > div > span > div > div > section > div:nth-child(5) > div:nth-child(3)').click()
+        
+        sleep(1)
+
+        group_link = self.driver.find_element_by_css_selector('#group-invite-link-anchor').text
+
+        self.driver.find_element_by_css_selector('#app > div > div > div.Akuo4 > div._1Flk2._3xysY > span > div > span > div > header > div > div._215wZ > button').click()
+        
+        sleep(1)
+
+        try:
+            self.driver.find_element_by_css_selector('#app > div > div > div.Akuo4 > div._1Flk2._3xysY > span > div > span > div > header > div > div._215wZ > button').click()
+        except:
+            pass
+
+        return group_link
+    
+    except:
+        error_log(traceback.format_exc())
