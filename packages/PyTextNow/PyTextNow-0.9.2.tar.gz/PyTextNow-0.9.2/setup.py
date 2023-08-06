fcb33ee876d58b94e849c446d70c66b7cff1312d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:46 +0000

    Add package PyTextNow/0.9.2/PyTextNow-0.9.2.tar.gz

---
+++ packages/PyTextNow/PyTextNow-0.9.2.tar.gz/PyTextNow-0.9.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyTextNow",
+    version="0.9.2",
+    author="Leo Wu-Gomez",
+    author_email="leojwu18@gmail.com",
+    description="Texting python package which utilizes TextNow.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leogomezz4t/PyTextNow_API",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    scripts=["bin/tnapi.py"]
+)
