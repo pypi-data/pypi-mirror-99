4a43d1c0f7669493b2d0e7e2848d0db993280370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:50 +0000

    Add package split-dataset/0.4.1/split_dataset-0.4.1.tar.gz

---
+++ packages/split-dataset/split_dataset-0.4.1.tar.gz/split_dataset-0.4.1/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = ["flammkuchen", "numpy", "dask[array]", "h5py"]
+
+with open("requirements_dev.txt") as f:
+    requirements_dev = f.read().splitlines()
+
+setup(
+    author="Vilim Stih & Luigi Petrucco @portugueslab",
+    author_email="luigi.petrucco@gmail.com",
+    python_requires=">=3.5",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="A package for HDF5-based chunked arrays",
+    install_requires=requirements,
+    extras_require=dict(dev=requirements_dev),
+    license="GNU General Public License v3",
+    long_description=readme + "\n\n" + history,
+    include_package_data=True,
+    keywords="split_dataset",
+    name="split_dataset",
+    packages=find_packages(include=["split_dataset", "split_dataset.*"]),
+    test_suite="tests",
+    url="https://github.com/portugueslab/split_dataset",
+    version="0.4.1",
+    zip_safe=False,
+)
