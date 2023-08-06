8b46ab698e67345241082c326ac342a0fd2a3bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:59 +0000

    Add package pklib/0.0.2/pklib-0.0.2.tar.gz

---
+++ packages/pklib/pklib-0.0.2.tar.gz/pklib-0.0.2/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pklib",
+    version="0.0.2",
+    author="Parthiban Kannan",
+    author_email="parthiban_kannan@outlook.com",
+    description="Machine Learning Simplified Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+
+        'Programming Language :: Python :: 3.6',
+
+    ],
+    python_requires=">=3.6",
+)
