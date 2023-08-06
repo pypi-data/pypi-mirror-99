228e0acbc5a7658d34a2d2f02e6b33b0440f12a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:21 +0000

    Add package pltviz/0.1.2.4/pltviz-0.1.2.4-py3-none-any.whl

---
+++ packages/pltviz/pltviz-0.1.2.4-py3-none-any.whl/pltviz/__init__.py
@@ -0,0 +1,6 @@
+from pltviz.bar import bar
+from pltviz.comp_line import comp_line
+from pltviz.gini import gini
+from pltviz import legend
+from pltviz.pie import pie
+from pltviz.semipie import semipie
