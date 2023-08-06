6075c9464892239db086e96a14f0d3e3bf361106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:47 +0000

    Add package pythondata-misc-opentitan/0.0.post5540/pythondata_misc_opentitan-0.0.post5540-py2-none-any.whl

---
+++ packages/pythondata-misc-opentitan/pythondata_misc_opentitan-0.0.post5540-py2-none-any.whl/pythondata_misc_opentitan/__init__.py
@@ -0,0 +1,52 @@
+import os.path
+__dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
+data_location = os.path.join(__dir__, "resources")
+src = "https://github.com/lowRISC/opentitan"
+
+# Module version
+version_str = "0.0.post5540"
+version_tuple = (0, 0, 5540)
+try:
+    from packaging.version import Version as V
+    pversion = V("0.0.post5540")
+except ImportError:
+    pass
+
+# Data version info
+data_version_str = "0.0.post5445"
+data_version_tuple = (0, 0, 5445)
+try:
+    from packaging.version import Version as V
+    pdata_version = V("0.0.post5445")
+except ImportError:
+    pass
+data_git_hash = "d8f9b6c9a34f1df0a12bd78aabc8f817d4ee42cb"
+data_git_describe = "v0.0-5445-gd8f9b6c9a"
+data_git_msg = """\
+commit d8f9b6c9a34f1df0a12bd78aabc8f817d4ee42cb
+Author: Eunchan Kim <eunchan@opentitan.org>
+Date:   Tue Mar 23 17:00:26 2021 -0700
+
+    [kmac] Add kmac_pkg.core
+    
+    Signed-off-by: Eunchan Kim <eunchan@opentitan.org>
+
+"""
+
+# Tool version info
+tool_version_str = "0.0.post95"
+tool_version_tuple = (0, 0, 95)
+try:
+    from packaging.version import Version as V
+    ptool_version = V("0.0.post95")
+except ImportError:
+    pass
+
+
+def data_file(f):
+    """Get absolute path for file inside pythondata_misc_opentitan."""
+    fn = os.path.join(data_location, f)
+    fn = os.path.abspath(fn)
+    if not os.path.exists(fn):
+        raise IOError("File {f} doesn't exist in pythondata_misc_opentitan".format(f))
+    return fn
