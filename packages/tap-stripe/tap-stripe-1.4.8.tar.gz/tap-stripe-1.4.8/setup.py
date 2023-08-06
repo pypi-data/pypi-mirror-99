e22443e71bcb26ab04e8e1fccb0f5bb2f88ef727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:39 +0000

    Add package tap-stripe/1.4.8/tap-stripe-1.4.8.tar.gz

---
+++ packages/tap-stripe/tap-stripe-1.4.8.tar.gz/tap-stripe-1.4.8/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-stripe",
+    version="1.4.8",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_stripe"],
+    install_requires=[
+        "singer-python==5.5.1",
+        "stripe==2.10.1",
+    ],
+    extras_require={
+        'dev': [
+            'ipdb==0.11',
+            'pylint==2.7.2',
+            'astroid==2.5.1',
+            'nose==1.3.7'
+        ]
+    },
+    entry_points="""
+    [console_scripts]
+    tap-stripe=tap_stripe:main
+    """,
+    packages=["tap_stripe"],
+    package_data = {
+        "schemas": ["tap_stripe/schemas/*.json"]
+    },
+    include_package_data=True,
+)
