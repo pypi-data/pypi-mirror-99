10348d7990008b29af9c5a2a88f1dac0e2bb69b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:19 +0000

    Add package fibo-sum/1.0.1/fibo_sum-1.0.1.tar.gz

---
+++ packages/fibo-sum/fibo_sum-1.0.1.tar.gz/fibo_sum-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='fibo_sum',
+    packages=['fibo_sum'],
+    version='1.0.1',
+    license='MIT',
+    description='Returns the sum of N numbers of the fibonacci sequence',
+    author='Lew Andrade',
+    author_email='andrade.lew@gmail.com',
+    url='https://github.com/LewAndrade/fibo-sum',
+    download_url='https://github.com/LewAndrade/fibo-sum/archive/refs/tags/1.0.tar.gz',
+    keywords=['Fibonacci', 'Sum'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
