f92f881fc53799081e66245c1623640ae29bcbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:11 +0000

    Add package pyzohar/0.1.4/pyzohar-0.1.4-py3-none-any.whl

---
+++ packages/pyzohar/pyzohar-0.1.4-py3-none-any.whl/pyzohar/sub_slt_spd/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 2021.03.19
+@author: zoharslong
+"""
+from pyzohar.sub_slt_spd.spz import spz
+
+__all__ = ['spz']
+print('info: web spider slot inserted.')
