18e28c3c6cdf8d5ab2bc763e11430ac1e7d62194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:10 +0000

    Add package openbci-stream/1.0.0rc1/openbci-stream-1.0.0rc1.tar.gz

---
+++ packages/openbci-stream/openbci-stream-1.0.0rc1.tar.gz/openbci-stream-1.0.0rc1/cmd/stream_configure_kafka.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+function secure_file {
+    if test -f $1.orig; then
+        rm $1
+        cp $1.orig $1
+    else
+        cp $1 $1.orig
+    fi
+}
+
+FILE=/etc/kafka/server.properties
+secure_file $FILE
+echo "
+advertised.listeners=PLAINTEXT://192.168.1.1:9092
+listener.security.protocol.map=PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL
+" >> $FILE
+
+
+echo "Starting daemons"
+systemctl enable kafka.service zookeeper@kafka.service
+systemctl start kafka.service zookeeper@kafka.service
+
+echo "Creating partitions"
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic binary
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic eeg
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic marker
+kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic annotation
+
+echo "Setting retention"
+kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name binary --alter --add-config retention.ms=1000
+kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name eeg --alter --add-config retention.ms=1000
+kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name marker --alter --add-config retention.ms=1000
+kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name annotation --alter --add-config retention.ms=1000
+
