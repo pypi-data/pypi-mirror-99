24d7c15b059bca1476c9d237dc69294bec6b3652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:28 +0000

    Add package andebox/0.2.0/andebox-0.2.0.tar.gz

---
+++ packages/andebox/andebox-0.2.0.tar.gz/andebox-0.2.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+requirements = [line.strip() for line in open('requirements.txt').readlines()]
+
+setup(name='andebox',
+      version='0.2.0',
+      scripts=['andebox'],
+      install_requires=requirements,
+      )
