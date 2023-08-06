fa70bb11af4b966e1bbf5bf5edcf6bacda4cc969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:07 +0000

    Add package jelapi/0.1.0rc2/jelapi-0.1.0rc2.tar.gz

---
+++ packages/jelapi/jelapi-0.1.0rc2.tar.gz/jelapi-0.1.0rc2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+__version__ = "0.1.0rc2"
+
+install_requires = ["httpx[http2]"]
+test_requires = ["respx", "pytest-cov"]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jelapi",
+    version=__version__,
+    author="Didier Raboud",
+    author_email="didier.raboud@liip.ch",
+    license="GPLv3+",
+    description="jelapi: A Jelastic API Python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liip/jelapi",
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        "test": test_requires,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Software Development :: Libraries",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires=">=3.5",
+)
