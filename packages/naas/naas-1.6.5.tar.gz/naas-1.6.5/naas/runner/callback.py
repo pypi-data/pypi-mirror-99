a1ccfa5466192ae725791218be61ab69777a4b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:40 +0000

    Add package naas/1.6.5/naas-1.6.5.tar.gz

---
+++ packages/naas/naas-1.6.5.tar.gz/naas-1.6.5/naas/runner/callback.py
@@ -0,0 +1,147 @@
+from naas.runner.env_var import n_env
+from naas.types import copy_button
+import pandas as pd
+import requests
+import time
+import json
+
+
+class Callback:
+    logger = None
+
+    headers = None
+
+    def __init__(self, logger=None):
+        self.headers = {"Authorization": f"token {n_env.token}"}
+        self.logger = logger
+
+    def add(
+        self,
+        response={},
+        response_headers={},
+        auto_delete=True,
+        default_result=None,
+        no_override=False,
+        user=None,
+        uuid=None,
+    ):
+        try:
+            data = {
+                "response": response,
+                "autoDelete": auto_delete,
+                "responseHeaders": response_headers,
+            }
+            if no_override:
+                data["responseHeaders"]["naas_no_override"] = no_override
+            if default_result:
+                data["result"] = default_result
+            if user:
+                data["user"] = user
+            if uuid:
+                data["uuid"] = uuid
+            req = requests.post(
+                url=f"{n_env.callback_api}/", headers=self.headers, json=data
+            )
+            req.raise_for_status()
+            jsn = req.json()
+            print("👌 🔙 Callback has been created successfully !")
+            url = f"{n_env.callback_api}/{jsn.get('uuid')}"
+            copy_button(url)
+            return {"url": url, "uuid": jsn.get("uuid")}
+        except Exception as err:
+            if self.logger is not None:
+                self.logger.error(
+                    json.dumps({"id": None, "type": "email error", "error": str(err)})
+                )
+            else:
+                print(err)
+
+    def __get(self, uuid, user=None):
+        try:
+            data = {
+                "uuid": uuid,
+            }
+            if user:
+                data["user"] = user
+            req = requests.get(
+                url=f"{n_env.callback_api}/",
+                params=data,
+                headers=self.headers,
+            )
+            req.raise_for_status()
+            jsn = req.json()
+            return jsn
+        except Exception as err:
+            if self.logger is not None:
+                self.logger.error(
+                    json.dumps({"id": uuid, "type": "email error", "error": str(err)})
+                )
+            else:
+                print(err)
+
+    def get(self, uuid, wait_until_data=False, timeout=3000, raw=False, user=None):
+        data = None
+        total = 0
+        while data is None or data.get("result") is None:
+            if total > timeout:
+                print("🥲 Callback Get timeout !")
+                return None
+            data = self.__get(uuid, user)
+            time.sleep(1)
+            total += 1
+            if wait_until_data:
+                break
+        if data and data.get("result") and data.get("result") != "":
+            print("👌 🔙 Callback has been trigger, here your data !")
+        else:
+            print("🥲 Callback is empty !")
+        return data if raw else data.get("result")
+
+    def delete(self, uuid, user=None):
+        try:
+            data = {
+                "uuid": uuid,
+            }
+            if user:
+                data["user"] = user
+            req = requests.delete(
+                url=f"{n_env.callback_api}/", headers=self.headers, json=data
+            )
+            req.raise_for_status()
+            print("👌 🔙 Callback has been delete successfully !")
+            return
+        except Exception as err:
+            if self.logger is not None:
+                self.logger.error(
+                    json.dumps({"id": uuid, "type": "email error", "error": str(err)})
+                )
+            else:
+                print(err)
+
+    def status(self):
+        req = requests.get(url=f"{n_env.callback_api}/")
+        req.raise_for_status()
+        jsn = req.json()
+        return jsn
+
+    def list(self, user=None):
+        data = {}
+        if user:
+            data["user"] = user
+        req = requests.get(
+            url=f"{n_env.callback_api}/",
+            params=data,
+            headers=self.headers,
+        )
+        req.raise_for_status()
+        jsn = req.json()
+        return pd.DataFrame(data=jsn.get("callbacks"))
+
+    def list_all(self):
+        req = requests.get(
+            url=f"{n_env.callback_api}/admin",
+            headers=self.headers,
+        )
+        req.raise_for_status()
+        jsn = req.json()
+        return pd.DataFrame(data=jsn.get("callbacks"))
---
+++ packages/naas/naas-1.6.5.tar.gz/naas-1.6.5/naas/runner/controllers/notebooks.py
@@ -0,0 +1,138 @@
+from naas.types import t_notebook, t_health, t_error, t_start, t_delete
+from sanic.views import HTTPMethodView
+from sanic.exceptions import ServerError
+import urllib
+import uuid
+
+
+def rename_keys(old_dict):
+    new_dict = {}
+    for key in old_dict.keys():
+        new_key = key.replace(" ", "_")
+        new_key = new_key.replace("-", "_")
+        new_dict[new_key] = old_dict[key]
+    return new_dict
+
+
+def parse_data(request):
+    req_data = {}
+    if request.headers.get("content-type") == "multipart/form-data":
+        req_data = request.files
+    elif request.headers.get("content-type") == "application/json":
+        req_data = request.json
+    elif request.headers.get("content-type") == "application/x-www-form-urlencoded":
+        req_data = dict(urllib.parse.parse_qsl(request.body.decode("utf-8")))
+    req_data = rename_keys(req_data)
+    args = dict(
+        urllib.parse.parse_qsl(request.query_string)
+    )  # fix to don't have array for each args
+    data = {"params": args, "body": req_data, "headers": dict(request.headers)}
+    return data
+
+
+class NbController(HTTPMethodView):
+    __logger = None
+
+    def __init__(self, logger, jobs, nb, *args, **kwargs):
+        super(NbController, self).__init__(*args, **kwargs)
+        self.__logger = logger
+        self.__jobs = jobs
+        self.__nb = nb
+
+    async def _get(self, data, token):
+        uid = str(uuid.uuid4())
+        job = await self.__jobs.find_by_value(uid, token, t_notebook)
+        if job and job.get("status") != t_delete:
+            value = job.get("value", None)
+            file_filepath = job.get("path")
+            cur_job = job.copy()
+            cur_job["params"] = {**(job.get("params", dict())), **(data)}
+            self.__logger.info(
+                {
+                    "id": uid,
+                    "type": t_notebook,
+                    "status": t_start,
+                    "filepath": file_filepath,
+                    "token": token,
+                }
+            )
+            await self.__jobs.update(
+                uid, file_filepath, t_notebook, value, job.get("params"), t_start
+            )
+            res = await self.__nb.exec(uid, cur_job)
+            if res.get("error"):
+                self.__logger.error(
+                    {
+                        "main_id": uid,
+                        "id": uid,
+                        "type": t_notebook,
+                        "status": t_error,
+                        "filepath": file_filepath,
+                        "duration": res.get("duration"),
+                        "error": str(res.get("error")),
+                    }
+                )
+                await self.__jobs.update(
+                    uid,
+                    file_filepath,
+                    t_notebook,
+                    value,
+                    job.get("params"),
+                    t_error,
+                    res.get("duration"),
+                )
+                raise ServerError(
+                    {
+                        "id": uid,
+                        "error": res.get("error"),
+                        "data": data,
+                        "token": token,
+                    },
+                    status_code=500,
+                )
+            self.__logger.info(
+                {
+                    "main_id": uid,
+                    "id": uid,
+                    "type": t_notebook,
+                    "status": t_health,
+                    "filepath": file_filepath,
+                    "duration": res.get("duration"),
+                }
+            )
+            await self.__jobs.update(
+                uid,
+                file_filepath,
+                t_notebook,
+                value,
+                job.get("params"),
+                t_health,
+                res.get("duration"),
+            )
+            return self.__nb.response(
+                uid, file_filepath, res, res.get("duration"), job.get("params")
+            )
+        self.__logger.error(
+            {
+                "id": uid,
+                "type": t_notebook,
+                "status": t_error,
+                "token": token,
+                "error": "Cannot find your token",
+            }
+        )
+        raise ServerError(
+            {
+                "id": uid,
+                "error": "Cannot find your token",
+                "data": data,
+                "token": token,
+            },
+            status_code=404,
+        )
+
+    async def get(self, request, token):
+        return await self._get(parse_data(request), token)
+
+    async def post(self, request, token):
+        return await self._get(parse_data(request), token)
