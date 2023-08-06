49163198d397da4aabc6b31c5bffa50d9e8bf3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:55 +0000

    Add package django-extra-field-validation/1.1.0/django_extra_field_validation-1.1.0-py3-none-any.whl

---
+++ packages/django-extra-field-validation/django_extra_field_validation-1.1.0-py3-none-any.whl/extra_validator/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for django-extra-field-validation."""
+
+__author__ = """Tonye Jack"""
+__email__ = "jtonye@ymail.com"
+__version__ = "1.1.0"
+
+from .field_validation import FieldValidationMixin
+
+__all__ = ["FieldValidationMixin"]
---
+++ packages/django-extra-field-validation/django_extra_field_validation-1.1.0-py3-none-any.whl/extra_validator/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DynamicValidatorConfig(AppConfig):
+    name = "extra_validator"
