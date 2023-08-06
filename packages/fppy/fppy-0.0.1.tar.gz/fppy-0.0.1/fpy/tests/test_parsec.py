09af5b742dd1cf3d2b7c664d3ab89d0d5733d2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:22 +0000

    Add package fppy/0.0.1/fppy-0.0.1.tar.gz

---
+++ packages/fppy/fppy-0.0.1.tar.gz/fppy-0.0.1/fpy/tests/test_parsec.py
@@ -0,0 +1,90 @@
+from fpy.parsec.parsec import (
+    parser,
+    one,
+    neg,
+    pmaybe,
+    many,
+    many1,
+    ptrans,
+    peek,
+    skip,
+    pseq,
+)
+from fpy.data.either import fromRight
+
+import unittest
+
+toks = [1, 2, 3]
+
+even = lambda x: x % 2 == 0
+odd = lambda x: x % 2 != 0
+
+
+class TestParsec(unittest.TestCase):
+    def testParseOne(self):
+        p = one(odd)
+        self.assertTrue(p(toks))
+        p = one(even)
+        self.assertFalse(p(toks))
+
+    def testParseNeg(self):
+        p = neg(odd)
+        self.assertFalse(p(toks))
+        p = neg(even)
+        self.assertTrue(p(toks))
+
+    def testParseCat(self):
+        p1 = one(odd)
+        p2 = one(even)
+        p = p1 + p2
+        self.assertTrue(p(toks))
+
+    def testParseLR(self):
+        p1 = one(odd)
+        p2 = one(even)
+        pl = p1 << p2
+        pr = p1 >> p2
+
+        self.assertTrue(pl(toks))
+        self.assertTrue(pr(toks))
+
+    def testParseChoice(self):
+        p1 = one(odd)
+        p2 = one(even)
+        p = p1 | p2
+        self.assertTrue((p + p)(toks))
+
+    def testMany(self):
+        p1 = one(odd)
+        p2 = one(even)
+        p = many1(p1)
+        self.assertTrue(p(toks))
+        p = many(p2)
+        self.assertTrue(p(toks))
+
+    def testSkip(self):
+        p1 = one(odd)
+        p2 = one(even)
+        p = skip(p1) + p2
+        self.assertTrue(p(toks))
+
+    def testPSeq(self):
+        seq = [1, 2, 3]
+        p = pseq(seq)
+        self.assertTrue(p(toks))
+        self.assertFalse(pseq([2, 3, 4])(toks))
+
+    def testPeek(self):
+        p1 = one(odd)
+        p = peek(p1)
+        res = p(toks)
+        self.assertTrue(res)
+        head, rest = fromRight(None, res)
+        self.assertEqual(head, [1])
+        self.assertIs(toks, rest)
+
+    def testTimeN(self):
+        p1 = one(odd)
+        p2 = one(even)
+        p = p1 | p2
+        self.assertTrue((p * 2)(toks))
---
+++ packages/fppy/fppy-0.0.1.tar.gz/fppy-0.0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+VERSION = "0.0.1"
+
+DESCRIPTION = "Python module for composing computations"
+CLASSIFIERS = [
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
+def main():
+    try:
+        from setuptools import setup
+    except ImportError:
+        from distutils.core import setup
+
+    with open("README.mkd") as fin:
+        desc = fin.read().strip()
+
+    options = {
+        "name": "fppy",
+        "version": VERSION,
+        "license": "GPLv3",
+        "description": DESCRIPTION,
+        "long_description": desc,
+        "url": "https://github.com/Z-Shang/fpy",
+        "author": "zshang",
+        "author_email": "z@gilgamesh.me",
+        "classifiers": CLASSIFIERS,
+        "packages": ["fpy", "fpy.tests"],
+        "install_requires": ["bytecode"],
+    }
+    setup(**options)
+
+
+if __name__ == "__main__":
+    main()
