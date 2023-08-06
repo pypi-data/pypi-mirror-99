62a5370af2d3d10814aa8369393af4f269fe2e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:10 +0000

    Add package glances-api/0.2.1/glances_api-0.2.1-py3-none-any.whl

---
+++ packages/glances-api/glances_api-0.2.1-py3-none-any.whl/glances_api/exceptions.py
@@ -0,0 +1,19 @@
+"""Exceptions for the client."""
+
+
+class GlancesApiError(Exception):
+    """General GlancesApiError exception occurred."""
+
+    pass
+
+
+class GlancesApiConnectionError(GlancesApiError):
+    """When a connection error is encountered."""
+
+    pass
+
+
+class GlancesApiNoDataAvailable(GlancesApiError):
+    """When no data is available."""
+
+    pass
