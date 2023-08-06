9d9b6e44eddd5dd179cfc95d6fa35ed96baa675c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:47 +0000

    Add package ctpysql/0.0.2/ctpysql-0.0.2.tar.gz

---
+++ packages/ctpysql/ctpysql-0.0.2.tar.gz/ctpysql-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ctpysql',
+    version='0.0.2',
+    description='This library is for run MYSQL queries as fast as possible 🚄🔥!',
+    py_modules=["ctpysql"],
+    package_dir={'': 'src'},
+    install_requires = [
+        "mysql-connector ~= 2.2.9",
+        "mysql-connector-python ~= 8.0.23",
+    ],
+    extras_require = {
+        "dev": [
+            "pytest>=6.2.2"
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MahyarNV/cautious-pysql",
+    author="Mahyar Behzadi",
+    author_email="mahyarbhz@gmail.com"
+)
