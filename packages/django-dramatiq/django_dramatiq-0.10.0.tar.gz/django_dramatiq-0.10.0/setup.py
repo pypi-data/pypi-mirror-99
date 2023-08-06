c71114a99576cc9009bf49bbb81793e18dcce90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:18 +0000

    Add package django-dramatiq/0.10.0/django_dramatiq-0.10.0.tar.gz

---
+++ packages/django-dramatiq/django_dramatiq-0.10.0.tar.gz/django_dramatiq-0.10.0/setup.py
@@ -0,0 +1,64 @@
+import os
+
+from setuptools import setup
+
+
+def rel(*xs):
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *xs)
+
+
+with open(rel("django_dramatiq", "__init__.py"), "r") as f:
+    version_marker = "__version__ = "
+    for line in f:
+        if line.startswith(version_marker):
+            _, version = line.split(version_marker)
+            version = version.strip().strip('"')
+            break
+    else:
+        raise RuntimeError("Version marker not found.")
+
+
+setup(
+    name="django_dramatiq",
+    version=version,
+    description="A Django app for Dramatiq.",
+    long_description="Visit https://github.com/Bogdanp/django_dramatiq for more information.",
+    packages=[
+        "django_dramatiq",
+        "django_dramatiq.management",
+        "django_dramatiq.management.commands",
+        "django_dramatiq.migrations",
+    ],
+    install_requires=[
+        "django>=1.11",
+        "dramatiq>=0.18.0",
+    ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    extras_require={
+        "dev": [
+            "bumpversion",
+            "flake8",
+            "flake8-quotes",
+            "isort",
+            "pytest",
+            "pytest-cov",
+            "pytest-django",
+            "tox",
+            "twine",
+        ]
+    },
+    python_requires=">=3.5",
+    include_package_data=True,
+)
