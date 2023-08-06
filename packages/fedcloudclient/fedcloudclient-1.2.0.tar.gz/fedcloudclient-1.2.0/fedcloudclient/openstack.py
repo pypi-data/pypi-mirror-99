a99107a09291f2590f6fdb10c8711f2675f7af2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:18 +0000

    Add package fedcloudclient/1.2.0/fedcloudclient-1.2.0.tar.gz

---
+++ packages/fedcloudclient/fedcloudclient-1.2.0.tar.gz/fedcloudclient-1.2.0/fedcloudclient/openstack.py
@@ -0,0 +1,319 @@
+"""
+Implementation of "fedcloud openstack" or "fedcloud openstack-int" for performing Openstack commands on sites
+"""
+
+import concurrent.futures
+import json
+import os
+from distutils.spawn import find_executable
+import click
+
+# Subprocess is required for invoking openstack client, so ignored bandit check
+import subprocess  # nosec
+
+from fedcloudclient.checkin import get_access_token, oidc_params
+from fedcloudclient.decorators import openstack_params, DEFAULT_PROTOCOL, DEFAULT_AUTH_TYPE, DEFAULT_IDENTITY_PROVIDER
+from fedcloudclient.sites import find_endpoint_and_project_id, list_sites, site_vo_params
+
+__OPENSTACK_CLIENT = "openstack"
+__MAX_WORKER_THREAD = 30
+
+
+def fedcloud_openstack_full(
+        oidc_access_token,
+        openstack_auth_protocol,
+        openstack_auth_type,
+        checkin_identity_provider,
+        site,
+        vo,
+        openstack_command,
+        json_output=True
+):
+    """
+    Calling openstack client with full options specified, including support
+    for other identity providers and protocols
+
+    :param oidc_access_token: Checkin access token. Passed to openstack client as --os-access-token
+    :param openstack_auth_protocol: Checkin protocol (openid, oidc). Passed to openstack client as --os-protocol
+    :param openstack_auth_type: Checkin authentication type (v3oidcaccesstoken). Passed to openstack client as --os-auth-type
+    :param checkin_identity_provider: Checkin identity provider in mapping (egi.eu). Passed to openstack client as --os-identity-provider
+    :param site: site ID in GOCDB
+    :param vo: VO name
+    :param openstack_command: Openstack command in tuple, e.g. ("image", "list", "--long")
+    :param json_output: if result is JSON object or string. Default:True
+
+    :return: error code, result or error message
+    """
+
+    endpoint, project_id, protocol = find_endpoint_and_project_id(site, vo)
+    if endpoint is None:
+        return 11, ("VO %s not found on site %s\n" % (vo, site))
+
+    if protocol is None:
+        protocol = openstack_auth_protocol
+
+    options = ("--os-auth-url", endpoint,
+               "--os-auth-type", openstack_auth_type,
+               "--os-protocol", protocol,
+               "--os-identity-provider", checkin_identity_provider,
+               "--os-access-token", oidc_access_token
+               )
+
+    if vo:
+        options = options + ("--os-project-id", project_id)
+
+    # Output JSON format is useful for further machine processing
+    if json_output:
+        options = options + ("--format", "json")
+
+    # Calling openstack client as subprocess, caching stdout/stderr
+    # Ignore bandit warning
+    completed = subprocess.run((__OPENSTACK_CLIENT,) + openstack_command + options,  # nosec
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    error_code = completed.returncode
+    error_message = completed.stderr.decode('utf-8')
+    result_str = completed.stdout.decode('utf-8')
+
+    if error_code == 0:
+        if json_output:
+            # Test if openstack command ignore JSON format option
+            try:
+                json_object = json.loads(result_str)
+                return error_code, json_object
+            except ValueError:
+                return error_code, result_str
+        else:
+            return error_code, result_str
+    else:
+        # If error code != 0, return error message instead of result
+        # But attach also result string, as applications may print error messages to stdout instead stderr
+        return error_code, error_message + result_str
+
+
+def fedcloud_openstack(
+        oidc_access_token,
+        site,
+        vo,
+        openstack_command,
+        json_output=True
+):
+    """
+    Simplified version of fedcloud_openstack_full() function using
+    default EGI setting for identity provider and protocols
+    Call openstack client with default options for EGI Check-in
+
+    :param oidc_access_token: Checkin access token. Passed to openstack client as --os-access-token
+    :param site: site ID in GOCDB
+    :param vo: VO name
+    :param openstack_command: Openstack command in tuple, e.g. ("image", "list", "--long")
+    :param json_output: if result is JSON object or string. Default:True
+
+    :return: error code, result or error message
+    """
+
+    return fedcloud_openstack_full(
+        oidc_access_token,
+        DEFAULT_PROTOCOL,
+        DEFAULT_AUTH_TYPE,
+        DEFAULT_IDENTITY_PROVIDER,
+        site,
+        vo,
+        openstack_command,
+        json_output
+    )
+
+
+def check_openstack_client_installation():
+    """
+    Check if openstack command-line client is installed and available via $PATH
+
+    :return: True if available
+    """
+
+    return find_executable(__OPENSTACK_CLIENT) is not None
+
+
+def print_result(site, vo, command, exc_msg, error_code, result, json_output, ignore_missing_vo, first):
+    """
+    Print output from an Openstack command
+
+    :param site:
+    :param vo:
+    :param command:
+    :param exc_msg:
+    :param error_code:
+    :param result:
+    :param json_output:
+    :param ignore_missing_vo:
+    :param first:
+    :return:
+    """
+
+    command = " ".join(command)
+    if not json_output:
+        if exc_msg:
+            print("Site: %s, VO: %s, command: %s" % (site, vo, command))
+            print('%s generated an exception: %s' % (site, exc_msg))
+            print("Error message: %s" % result)
+
+        elif error_code != 0:
+            if not ignore_missing_vo or (error_code != 11):
+                print("Site: %s, VO: %s, command: %s" % (site, vo, command))
+                print("Error code: ", error_code)
+                print("Error message: ", result)
+        else:
+            print("Site: %s, VO: %s, command: %s" % (site, vo, command))
+            print(result)
+    else:
+        site_data = {"Site": site,
+                     "VO": vo,
+                     "command": command,
+                     "Exception": exc_msg,
+                     "Error code": error_code,
+                     "Result": result,
+                     }
+        separator = "[" if first else ","
+        print(separator)
+        print(json.dumps(site_data, indent=2))
+
+
+@click.command(context_settings={"ignore_unknown_options": True})
+@oidc_params
+@openstack_params
+@site_vo_params
+@click.option(
+    '--ignore-missing-vo', '-i',
+    help="Ignore sites that do not support the VO",
+    is_flag=True,
+)
+@click.option(
+    '--json-output', '-j',
+    help="Print output as a big JSON object",
+    is_flag=True,
+)
+@click.argument(
+    "openstack_command",
+    required=True,
+    nargs=-1
+)
+def openstack(
+        oidc_client_id,
+        oidc_client_secret,
+        oidc_refresh_token,
+        oidc_access_token,
+        oidc_url,
+        oidc_agent_account,
+        openstack_auth_protocol,
+        openstack_auth_type,
+        openstack_auth_provider,
+        site,
+        vo,
+        ignore_missing_vo,
+        json_output,
+        openstack_command
+):
+    """
+    Executing Openstack commands on site and VO
+    """
+
+    if not check_openstack_client_installation():
+        print("Error: Openstack command-line client \"openstack\" not found")
+        exit(1)
+
+    access_token = get_access_token(oidc_access_token,
+                                    oidc_refresh_token,
+                                    oidc_client_id,
+                                    oidc_client_secret,
+                                    oidc_url,
+                                    oidc_agent_account)
+
+    if site == "ALL_SITES":
+        sites = list_sites()
+    else:
+        sites = [site]
+
+    # Multi-thread execution of Openstack commands
+    with concurrent.futures.ThreadPoolExecutor(max_workers=__MAX_WORKER_THREAD) as executor:
+        # Start Openstack operation with each site
+        results = {executor.submit(fedcloud_openstack_full,
+                                   access_token,
+                                   openstack_auth_protocol,
+                                   openstack_auth_type,
+                                   openstack_auth_provider,
+                                   site,
+                                   vo,
+                                   openstack_command,
+                                   json_output,
+                                   ): site for site in sites}
+
+        # Get results and print them
+        first = True
+
+        # Get the result, first come first serve
+        for future in concurrent.futures.as_completed(results):
+            site = results[future]
+            exc_msg = None
+            try:
+                error_code, result = future.result()
+            except Exception as exc:
+                exc_msg = exc
+
+            # Print result
+            print_result(site, vo, openstack_command, exc_msg, error_code, result, json_output, ignore_missing_vo, first)
+            first = False
+
+        # Print list enclosing ']' for JSON
+        if json_output:
+            print("]")
+
+
+@click.command()
+@oidc_params
+@openstack_params
+@site_vo_params
+def openstack_int(
+        oidc_client_id,
+        oidc_client_secret,
+        oidc_refresh_token,
+        oidc_access_token,
+        oidc_url,
+        oidc_agent_account,
+        openstack_auth_protocol,
+        openstack_auth_type,
+        openstack_auth_provider,
+        site,
+        vo,
+):
+    """
+    Interactive Openstack client on site and VO
+    """
+
+    if not check_openstack_client_installation():
+        print("Error: Openstack command-line client \"openstack\" not found")
+        exit(1)
+
+    access_token = get_access_token(oidc_access_token,
+                                    oidc_refresh_token,
+                                    oidc_client_id,
+                                    oidc_client_secret,
+                                    oidc_url,
+                                    oidc_agent_account)
+
+    endpoint, project_id, protocol = find_endpoint_and_project_id(site, vo)
+    if endpoint is None:
+        raise SystemExit("Error: VO %s not found on site %s" % (vo, site))
+
+    if protocol is None:
+        protocol = openstack_auth_protocol
+    my_env = os.environ.copy()
+    my_env["OS_AUTH_URL"] = endpoint
+    my_env["OS_AUTH_TYPE"] = openstack_auth_type
+    my_env["OS_PROTOCOL"] = protocol
+    my_env["OS_IDENTITY_PROVIDER"] = openstack_auth_provider
+    my_env["OS_ACCESS_TOKEN"] = access_token
+    my_env["OS_PROJECT_ID"] = project_id
+
+    # Calling Openstack client as subprocess
+    # Ignore bandit warning
+    subprocess.run(__OPENSTACK_CLIENT, env=my_env)  # nosec
---
+++ packages/fedcloudclient/fedcloudclient-1.2.0.tar.gz/fedcloudclient-1.2.0/fedcloudclient/sites.py
@@ -0,0 +1,272 @@
+"""
+"fedcloud site" commands will read site configurations and manipulate with them. If the local site configurations
+exist at ~/.fedcloud-site-config/, fedcloud will read them from there, otherwise the commands will read from GitHub
+repository.
+
+By default, fedcloud does not save anything on local disk, users have to save the site configuration to local disk
+explicitly via "fedcloud site save-config" command. The advantage of having local site configurations, beside faster
+loading, is to give users ability to make customizations, e.g. add additional VOs, remove sites they do not have
+access, and so on.
+"""
+
+import builtins
+import json
+from pathlib import Path
+from urllib.request import urlopen, Request
+from jsonschema import validate
+import pkg_resources
+import click
+import yaml
+
+from fedcloudclient.decorators import site_vo_params, site_params
+
+__REMOTE_CONFIG_FILE = "https://raw.githubusercontent.com/tdviet/fedcloudclient/master/config/sites.yaml"
+
+__LOCAL_CONFIG_DIR = ".config/fedcloud/site-config/"
+
+__site_config_data = []
+
+__FILE_SIZE_LIMIT = 1024 * 1024  # Max size for config files
+
+
+def read_site_schema():
+    """
+    Read schema.json for validating site configuration
+
+    :return: JSON object from schema.json
+    """
+    file = pkg_resources.resource_stream("fedcloudclient", "schema.json")
+    schema = json.load(file)
+    return schema
+
+
+def read_site_config():
+    """
+    Read site configurations from local config dir if exist, otherwise from default GitHub location. Storing
+    site configurations in global variable, that will be used by other functions. Call read_local_site_config()
+    or read_default_site_config()
+
+    :return: None
+    """
+    if len(__site_config_data) > 0:
+        return
+    config_dir = Path.home() / __LOCAL_CONFIG_DIR
+    if config_dir.exists():
+        read_local_site_config(config_dir)
+    else:
+        read_default_site_config()
+
+
+def safe_read_yaml_from_url(url, max_length):
+    """
+    Safe reading from URL. Check URL and size before reading
+
+    :param url:
+    :param max_length:
+    :return: data from URL
+    """
+    if isinstance(url, str) and url.lower().startswith('https://'):  # Only read from HTTPS location
+        req = Request(url)
+    else:
+        raise SystemExit("Error: remote filename not starting with https:// : %s" % url)
+
+    # URLs already checked, so ignore bandit test
+    data = None
+    try:
+        with urlopen(req) as yaml_file:  # nosec
+            if int(yaml_file.headers['Content-Length']) > max_length:
+                raise SystemExit("Error: remote file %s is larger than limit %d " % (url, max_length))
+            data = yaml.safe_load(yaml_file)
+    except Exception as e:
+        print("Error during reading data from %s" % url)
+        raise SystemExit("Exception: %s" % e)
+    return data
+
+
+def read_default_site_config():
+    """
+    Read default site configurations from GitHub.  Storing
+    site configurations in a global variable, that will be used by other functions
+
+    :return: None
+    """
+    __site_config_data.clear()
+    schema = read_site_schema()
+
+    site_list = safe_read_yaml_from_url(__REMOTE_CONFIG_FILE, __FILE_SIZE_LIMIT)
+
+    # Check data format of site list
+    if not isinstance(site_list, builtins.list):
+        raise SystemExit("Error: site list is in a wrong format")
+
+    for filename in site_list:
+        site_info = safe_read_yaml_from_url(filename, __FILE_SIZE_LIMIT)
+
+        # Validating site config after reading
+        try:
+            validate(instance=site_info, schema=schema)
+        except Exception as e:
+            print("Site config in file %s is in wrong format" % filename)
+            raise SystemExit("Exception: %s" % e)
+
+        __site_config_data.append(site_info)
+
+
+def read_local_site_config(config_dir):
+    """
+    Read site configurations from local directory specified in config_dir. Storing
+    site configurations in global variable, that will be used by other functions
+
+    :param config_dir: path to directory containing site configuration
+    :return: None
+    """
+    __site_config_data.clear()
+    schema = read_site_schema()
+    config_dir = Path(config_dir)
+    for f in sorted(config_dir.glob('*.yaml')):
+        try:
+            yaml_file = f.open()
+            site_info = yaml.safe_load(yaml_file)
+            validate(instance=site_info, schema=schema)
+            __site_config_data.append(site_info)
+        except Exception as e:
+            print("Error during reading site config from %s" % f)
+            raise SystemExit("Exception: %s" % e)
+
+
+def save_site_config(config_dir):
+    """
+    Save site configs to local directory specified in config_dir. Overwrite local configs if exist
+
+    :param config_dir: path to directory containing site configuration
+    :return: None
+    """
+    config_dir = Path(config_dir)
+    config_dir.mkdir(parents=True, exist_ok=True)
+    for site_info in __site_config_data:
+        config_file = config_dir / (site_info["gocdb"] + ".yaml")
+        with config_file.open("w", encoding="utf-8") as f:
+            yaml.dump(site_info, f)
+
+
+def list_sites():
+    """
+    List of all sites IDs in site configurations
+
+    :return: list of site IDs
+    """
+    read_site_config()
+    result = []
+    for site_info in __site_config_data:
+        result.append(site_info["gocdb"])
+    return result
+
+
+def find_site_data(site_name):
+    """
+    Return configuration of the correspondent site with site_name
+
+    :param site_name: site ID in GOCDB
+    :return: configuration of site if found, otherwise None
+    """
+    read_site_config()
+
+    for site_info in __site_config_data:
+        if site_info["gocdb"] == site_name:
+            return site_info
+    return None
+
+
+def find_endpoint_and_project_id(site_name, vo):
+    """
+    Return Keystone endpoint and project ID from site name and VO according to site configuration
+
+    :param site_name: site ID in GOCDB
+    :param vo: VO name. None if finding only site endpoint
+    :return: endpoint, project_id, protocol if the VO exist on the site, otherwise None, None, None
+    """
+    site_info = find_site_data(site_name)
+    if site_info is None:
+        return None, None, None
+
+    protocol = site_info.get("protocol")
+    # If only site name is given, return endpoint without project ID
+    if vo is None:
+        return site_info["endpoint"], None, protocol
+
+    for vo_info in site_info["vos"]:
+        if vo_info["name"] == vo:
+            return site_info["endpoint"], vo_info["auth"]["project_id"], protocol
+
+    # Return None, None if VO not found
+    return None, None, None
+
+
+@click.group()
+def site():
+    """
+    Site command group for manipulation with site configurations
+    """
+    pass
+
+
+@site.command()
+@site_params
+def show(site):
+    """
+    Print configuration of the specified site
+    """
+    site_info = find_site_data(site)
+    if site_info:
+        print(yaml.dump(site_info, sort_keys=True))
+    else:
+        print("Site %s not found" % site)
+        return 1
+
+
+@site.command()
+@site_vo_params
+def show_project_id(site, vo):
+    """
+    Printing Keystone endpoint and project ID of the VO on the site
+    """
+    endpoint, project_id, protocol = find_endpoint_and_project_id(site, vo)
+    if endpoint:
+        print('export OS_AUTH_URL="%s"' % endpoint)
+        print('export OS_PROJECT_ID="%s"' % project_id)
+    else:
+        print("VO %s not found on site %s" % (vo, site))
+        return 1
+
+
+@site.command()
+def show_all():
+    """
+    Print all site configuration
+    """
+    read_site_config()
+    for site_info in __site_config_data:
+        site_info_str = yaml.dump(site_info, sort_keys=True)
+        print(site_info_str)
+
+
+@site.command()
+def save_config():
+    """
+    Read default site configs from GitHub and save them to local folder in home directory
+    Overwrite local configs if exist
+    """
+    read_default_site_config()
+    config_dir = Path.home() / __LOCAL_CONFIG_DIR
+    print("Saving site configs to directory %s" % config_dir)
+    save_site_config(config_dir)
+
+
+@site.command()
+def list():
+    """
+    List site IDs
+    """
+    read_site_config()
+    for site_info in __site_config_data:
+        print(site_info["gocdb"])
