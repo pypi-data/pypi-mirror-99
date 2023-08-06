63a4c021e54d47df78ede5cb7df99baac3b6c365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:27 +0000

    Add package mbed-tools/7.5.0/mbed_tools-7.5.0-py3-none-any.whl

---
+++ packages/mbed-tools/mbed_tools-7.5.0-py3-none-any.whl/mbed_tools/cli/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) 2020-2021 Arm Limited and Contributors. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""mbed_tools command line interface."""
+
+from mbed_tools.cli.main import cli, LOGGER
