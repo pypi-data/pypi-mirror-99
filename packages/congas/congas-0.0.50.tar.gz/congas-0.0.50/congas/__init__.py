5c1e286a38a3bf4e2cbadf95d110bc6f8e7c5daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:07 +0000

    Add package congas/0.0.50/congas-0.0.50.tar.gz

---
+++ packages/congas/congas-0.0.50.tar.gz/congas-0.0.50/congas/__init__.py
@@ -0,0 +1,20 @@
+""" ANNEAL: rnA cNvs iNferEnce And cLustering (with Pyro)
+
+A set of Pyro models and an interface for simultaneous CNV clustering and inference
+
+
+"""
+
+
+from congas.Interface import Interface
+from congas.utils import *
+from congas.model_selection import *
+import os
+
+
+
+location = os.path.dirname(os.path.realpath(__file__))
+my_file = os.path.join(location, 'data')
+
+simulation_data = load_simulation_seg(my_file,"example1")
+
