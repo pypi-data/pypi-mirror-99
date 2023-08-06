395dadb1939b4a223c28aaf7c7652785869a08a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:23 +0000

    Add package dicom-to-cnn/0.42/dicom_to_cnn-0.42.tar.gz

---
+++ packages/dicom-to-cnn/dicom_to_cnn-0.42.tar.gz/dicom_to_cnn-0.42/library_dicom/export_segmentation/rtstruct/RTROIObservationsSequence.py
@@ -0,0 +1,25 @@
+import pydicom 
+import numpy as np
+from library_dicom.export_segmentation.tools.rtss_writer_tools import *
+
+class RTROIObservationsSequence :
+
+    def __init__(self, mask, results):
+        self.mask = mask
+        self.number_of_roi = get_number_of_roi(self.mask)
+        self.results = results
+
+
+    def create_RTROIObservationsSequence(self):
+        RTROIObservationsSequence = pydicom.sequence.Sequence()
+        for number_roi in range(1, self.number_of_roi +1) : 
+            dataset = pydicom.dataset.Dataset()
+            dataset.ReferencedROINumber = number_roi
+            dataset.ObservationNumber = number_roi 
+
+            dataset.RTROIInterpretedType = self.results["segmentAttributes"][0][number_roi-1]['RTROIInterpretedType']
+            dataset.ROIInterpreter = ''
+            
+            RTROIObservationsSequence.append(dataset)
+
+        return RTROIObservationsSequence 
---
+++ packages/dicom-to-cnn/dicom_to_cnn-0.42.tar.gz/dicom_to_cnn-0.42/library_dicom/export_segmentation/tools/rtss_writer_tools.py
@@ -0,0 +1,69 @@
+import numpy as np 
+from skimage.measure import label
+
+def get_image_position_per_slice(liste_instances):
+    liste = []
+    for i in range(len(liste_instances)):
+        liste.append(liste_instances[i].get_image_position())
+    return liste 
+
+
+def find_corresponding_z_spatial(liste_position, z_matrix):
+    z_spatial = liste_position[z_matrix][2]
+    return z_spatial
+
+
+def get_number_of_roi(mask):
+    if len(mask.shape) == 3 : 
+        if np.max(mask) == 1 : 
+            return 1 
+        else : return int(np.max(mask))
+
+    elif len(mask.shape) == 4 : 
+        if np.max(mask) == 1 : 
+            return mask.shape[3]
+        else : return int(np.max(mask))
+
+
+def get_list_SOPInstance_UID(liste_instances):
+    liste = []
+    for instance in liste_instances : 
+        liste.append(instance.get_SOPInstanceUID())
+
+    return liste 
+
+
+def clean_mask(mask):
+    """[summary]
+
+    Args:
+        mask ([type]): [binary mask [x,y,z]]
+    """
+    #Check if binary mask 
+    
+    if int(np.max(mask)) != 1 :
+        raise Exception("Not a binary mask")
+
+    empty_mask = np.zeros((mask.shape))
+    for s in mask.shape[2] : 
+        slice = mask[:,:,s]
+        if int(np.max(slice)) == 0 : 
+            empty_mask[:,:,s] = slice
+        else : 
+            lw, num = label(slice, connectivity=2)
+            item = np.arange(1, num+1).tolist()
+            area = []
+            for it in item : 
+                area.append(len(np.where(slice == it)[0]))
+            for ar in area : 
+                label = area.index(ar) + 1 
+                if int(ar) < 3 : 
+                    
+
+
+
+
+
+
+
+    
