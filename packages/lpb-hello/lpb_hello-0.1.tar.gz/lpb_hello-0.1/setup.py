b86a3db2015b1fe2e2bceee16f15a383becc1cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:29 +0000

    Add package lpb-hello/0.1/lpb_hello-0.1.tar.gz

---
+++ packages/lpb-hello/lpb_hello-0.1.tar.gz/lpb_hello-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="lpb_hello",
+	version="0.1",
+	license="MIT Licence",
+
+	url="",
+	author="liupb",
+	author_email="1053504313@qq.com",
+
+	packages=find_packages(),
+	include_package_data=True,
+	platforms="any",
+	install_requires=[]
+)
