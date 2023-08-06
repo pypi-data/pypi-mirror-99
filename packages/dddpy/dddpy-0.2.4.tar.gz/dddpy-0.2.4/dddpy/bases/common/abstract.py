201258bc26af68861a576292276f5b5df83683b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:51 +0000

    Add package dddpy/0.2.4/dddpy-0.2.4.tar.gz

---
+++ packages/dddpy/dddpy-0.2.4.tar.gz/dddpy-0.2.4/dddpy/bases/common/abstract.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Type, Any, Optional
+
+T = TypeVar("T")
+GenericAlias = type(Optional[int])
+
+
+class Restoreable(ABC):
+    @classmethod
+    @abstractmethod
+    def __restore__(cls: Type[T], value) -> T:
+        ...
+
+
+class Generatable(ABC):
+    @classmethod
+    @abstractmethod
+    def __generate__(cls: Type[T]) -> T:
+        ...
+
+
+class JsonSerializable(ABC):
+    @abstractmethod
+    def __json__(self: T) -> Any:
+        ...
+
+
+def find_abstract_constraint(annotation, abstract: Type[T]) -> Optional[T]:
+    if type(annotation) is GenericAlias:
+        for ann in annotation.__args__:
+            constraint = find_abstract_constraint(ann, abstract)
+            if constraint:
+                return constraint
+    try:
+        if issubclass(annotation, abstract):
+            return annotation
+    except TypeError:
+        return None
+
+
+def restore(annotation, value):
+    if value is None and find_abstract_constraint(annotation, type(None)):
+        return value
+    if typ := find_abstract_constraint(annotation, Restoreable):
+        return typ.__restore__(value)
+    return value
