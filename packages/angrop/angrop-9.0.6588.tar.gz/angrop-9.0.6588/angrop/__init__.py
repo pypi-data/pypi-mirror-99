63cf2ab3b846c39b5e933cb6997c16bf1fc9218b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:02 +0000

    Add package angrop/9.0.6588/angrop-9.0.6588.tar.gz

---
+++ packages/angrop/angrop-9.0.6588.tar.gz/angrop-9.0.6588/angrop/__init__.py
@@ -0,0 +1,3 @@
+__version__ = (9, 0, 6588)
+
+from . import rop
---
+++ packages/angrop/angrop-9.0.6588.tar.gz/angrop-9.0.6588/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='angrop',
+    version='9.0.6588',
+    description='The rop chain builder based off of angr',
+    packages=['angrop'],
+    install_requires=[
+        'progressbar',
+        'angr==9.0.6588',
+    ],
+)
