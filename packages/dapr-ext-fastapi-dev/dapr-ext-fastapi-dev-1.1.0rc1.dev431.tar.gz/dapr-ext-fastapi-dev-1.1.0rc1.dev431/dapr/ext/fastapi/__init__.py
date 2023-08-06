7acd7ad2062a164529d604553895a21314946aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:01 +0000

    Add package dapr-ext-fastapi-dev/1.1.0rc1.dev431/dapr-ext-fastapi-dev-1.1.0rc1.dev431.tar.gz

---
+++ packages/dapr-ext-fastapi-dev/dapr-ext-fastapi-dev-1.1.0rc1.dev431.tar.gz/dapr-ext-fastapi-dev-1.1.0rc1.dev431/dapr/ext/fastapi/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) Microsoft Corporation and Dapr Contributors.
+Licensed under the MIT License.
+"""
+
+from .actor import DaprActor
+
+
+__all__ = [
+    'DaprActor',
+]
