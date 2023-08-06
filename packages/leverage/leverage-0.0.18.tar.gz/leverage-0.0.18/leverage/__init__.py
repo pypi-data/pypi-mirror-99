5a296cd89bb4e52a8afcc62fabc7ace65cf7d64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package leverage/0.0.18/leverage-0.0.18.tar.gz

---
+++ packages/leverage/leverage-0.0.18.tar.gz/leverage-0.0.18/leverage/__init__.py
@@ -0,0 +1,14 @@
+"""
+BinBash Reference Architecture Task Runner
+"""
+
+__version__ = "0.0.18"
+__license__ = "MIT License"
+__contact__ = "http://github.com/binbashar/"
+
+from .leverage import task, main
+import pkgutil
+
+__path__ = pkgutil.extend_path(__path__, __name__)
+
+__all__ = ["task",  "main"]
