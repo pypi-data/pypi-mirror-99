3c87ee782d0b01f8d0652de268595d6d4bcc495d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:07 +0000

    Add package pywebio/1.2.3/pywebio-1.2.3.tar.gz

---
+++ packages/pywebio/pywebio-1.2.3.tar.gz/pywebio-1.2.3/demos/config.py
@@ -0,0 +1,5 @@
+# The deployment address of the demos module
+demo_host = 'http://pywebio-demos.demo.wangweimin.site'
+
+# The deployment address of https://github.com/wang0618/pywebio-chart-gallery repo
+charts_demo_host = 'http://pywebio-charts.demo.wangweimin.site'
