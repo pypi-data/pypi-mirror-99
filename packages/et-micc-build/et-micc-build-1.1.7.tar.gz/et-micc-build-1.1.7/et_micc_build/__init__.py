00f8c9e7291e26e98adb3d2b4b07a1c4c3f71f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:18 +0000

    Add package et-micc-build/1.1.7/et-micc-build-1.1.7.tar.gz

---
+++ packages/et-micc-build/et-micc-build-1.1.7.tar.gz/et-micc-build-1.1.7/et_micc_build/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package et_micc_build
+=====================
+
+Top-level package for et_micc_build.
+"""
+import et_micc_build.cli_micc_build
+import et_micc
+
+# et-micc and et--micc-build have identical version string, although
+# they are different packages. 
+__version__ = et_micc.__version__
