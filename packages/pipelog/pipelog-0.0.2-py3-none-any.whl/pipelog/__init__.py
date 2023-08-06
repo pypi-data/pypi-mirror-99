ff0ab497b7d334bd7378c15160c25c76243325cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:26 +0000

    Add package pipelog/0.0.2/pipelog-0.0.2-py3-none-any.whl

---
+++ packages/pipelog/pipelog-0.0.2-py3-none-any.whl/pipelog/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for pipelog."""
+from pipelog.pipe_tracker import PipeLogger
+
+__all__ = ("PipeLogger",)
+
+__author__ = """Adem Frenk"""
+__email__ = "adem.frenk@gmail.com"
+__version__ = "0.0.2"
