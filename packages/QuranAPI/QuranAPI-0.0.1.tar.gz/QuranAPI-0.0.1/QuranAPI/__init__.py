4a576f960ab832fd6cd5abf632594e2bd02200ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:09 +0000

    Add package QuranAPI/0.0.1/QuranAPI-0.0.1.tar.gz

---
+++ packages/QuranAPI/QuranAPI-0.0.1.tar.gz/QuranAPI-0.0.1/QuranAPI/__init__.py
@@ -0,0 +1,17 @@
+import discord
+import asyncio
+import aiohttp
+
+async def get_quran(verse_id, channel):
+  chapter = int(verse_id.split(':')[0])
+  verse = int(verse_id.split(':')[1])
+  async with aiohttp.ClientSession() as cs:
+    async with cs.get(f"http://quranapi.azurewebsites.net/api/verse/?chapter={chapter}&number={verse}&lang=en") as r:
+      res = await r.json()
+      embed = discord.Embed(
+        color = discord.Color.purple()
+      )
+      embed.set_author(name=res["ChapterName"])
+      embed.add_field(name="Verse:", value=res["Text"])
+      embed.set_footer(text="Powered by QuranAPI")
+      await channel.send(embed=embed)
