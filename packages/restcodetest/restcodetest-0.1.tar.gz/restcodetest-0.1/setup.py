9bc7af15e462dc8c504de7caf643e6ef0373687f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:43 +0000

    Add package restcodetest/0.1/restcodetest-0.1.tar.gz

---
+++ packages/restcodetest/restcodetest-0.1.tar.gz/restcodetest-0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="restcodetest", 
+        version=VERSION,
+        author="Jason Dsouza",
+        author_email="chaithrags@gnani.ai",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['requests'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+        ]
+)
