2446cc1ec3f57d2f4a8a8b124665a8578a36c433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:26 +0000

    Add package SivantClass/0.0.1/SivantClass-0.0.1.tar.gz

---
+++ packages/SivantClass/SivantClass-0.0.1.tar.gz/SivantClass-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="SivantClass",
+    version="0.0.1",
+    description="A Python package",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sivant1361/sivant_hello",
+    author="Sivant M",
+    author_email="sivant1313@gmail.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["SivantClass"],
+    include_package_data=True,
+)
