4ae883c77ff3b763e29d4b35bd7bcc96514d1ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:08 +0000

    Add package Montreal-Forced-Aligner/2.0.0a6/Montreal_Forced_Aligner-2.0.0a6-py3-none-any.whl

---
+++ packages/Montreal-Forced-Aligner/Montreal_Forced_Aligner-2.0.0a6-py3-none-any.whl/montreal_forced_aligner/__init__.py
@@ -0,0 +1,29 @@
+__ver_major__ = 2
+__ver_minor__ = 0
+__ver_patch__ = '0a6'
+__version__ = "{}.{}.{}".format(__ver_major__, __ver_minor__, __ver_patch__)
+
+__all__ = ['aligner', 'command_line', 'models', 'corpus', 'config', 'dictionary', 'exceptions',
+            'helper', 'multiprocessing', 'textgrid', 'g2p', '__version__']
+
+import montreal_forced_aligner.aligner as aligner
+
+import montreal_forced_aligner.command_line as command_line
+
+import montreal_forced_aligner.models as models
+
+import montreal_forced_aligner.corpus as corpus
+
+import montreal_forced_aligner.dictionary as dictionary
+
+import montreal_forced_aligner.exceptions as exceptions
+
+import montreal_forced_aligner.helper as helper
+
+import montreal_forced_aligner.config as config
+
+import montreal_forced_aligner.multiprocessing as multiprocessing
+
+import montreal_forced_aligner.textgrid as textgrid
+
+import montreal_forced_aligner.g2p as g2p
