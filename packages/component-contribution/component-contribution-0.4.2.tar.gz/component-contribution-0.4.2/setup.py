3e92e3d89ae308bca8691666998ce45b102ffd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:33 +0000

    Add package component-contribution/0.4.2/component-contribution-0.4.2.tar.gz

---
+++ packages/component-contribution/component-contribution-0.4.2.tar.gz/component-contribution-0.4.2/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup
+
+import versioneer
+
+
+# Most arguments are set in the `setup.cfg`.
+setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
