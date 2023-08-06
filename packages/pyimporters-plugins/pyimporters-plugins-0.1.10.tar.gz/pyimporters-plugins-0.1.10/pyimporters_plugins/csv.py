89ca8d0e9d0a80d6dc0e65d9b97c0a53580261fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:05 +0000

    Add package pyimporters-plugins/0.1.10/pyimporters-plugins-0.1.10.tar.gz

---
+++ packages/pyimporters-plugins/pyimporters-plugins-0.1.10.tar.gz/pyimporters-plugins-0.1.10/pyimporters_plugins/csv.py
@@ -0,0 +1,23 @@
+from typing import Type
+
+from pydantic import Field, BaseModel
+from pyimporters_plugins.base import KnowledgeParserOptions, KnowledgeParserBase, Concept
+
+
+class CSVOptions(KnowledgeParserOptions):
+    """
+    Options for the CSV knowledge import
+    """
+    sep: str = Field(',', description="Field separator")
+
+
+class CSVKnowledgeParser(KnowledgeParserBase):
+    def parse(self, source, options : KnowledgeParserOptions):
+        for name, value in sorted(source.items()):
+            concept : Concept = Concept(identifier=name, prefLabel=value)
+            yield Concept(identifier="1", prefLabel="foo")
+            yield Concept(identifier="2", prefLabel="bar")
+
+    @classmethod
+    def get_schema(cls) -> Type[BaseModel]:
+        return CSVOptions
