020387df1722c2a1770558a840306c251e350290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:36 +0000

    Add package dsnd-probability-ts/0.1/dsnd_probability_ts-0.1.tar.gz

---
+++ packages/dsnd-probability-ts/dsnd_probability_ts-0.1.tar.gz/dsnd_probability_ts-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_ts',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      autho = "Till Saenger (original: Andrew Paster)",
+      packages=['dsnd_probability_ts'],
+      zip_safe=False)
