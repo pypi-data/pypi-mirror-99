55bac310148beb018859a2177c194f8577d1be04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:53 +0000

    Add package tnnt-templates/1.5.1/tnnt_templates-1.5.1-py3-none-any.whl

---
+++ packages/tnnt-templates/tnnt_templates-1.5.1-py3-none-any.whl/tnnt_templates/context_processors.py
@@ -0,0 +1,49 @@
+"""
+tnnt template settings
+"""
+
+from django.conf import settings
+
+
+def tnnt_settings(request):
+    return_value = dict()
+
+    # AA logo
+    return_value["TNNT_TEMPLATE_AA_LOGO"] = "/static/icons/allianceauth.png"
+
+    # entity ID
+    # 1 if none is given
+    try:
+        return_value["TNNT_TEMPLATE_ENTITY_ID"] = settings.TNNT_TEMPLATE_ENTITY_ID
+    except AttributeError:
+        return_value["TNNT_TEMPLATE_ENTITY_ID"] = 1
+
+    # entity type
+    # 'alliance' if none is given
+    try:
+        return_value["TNNT_TEMPLATE_ENTITY_TYPE"] = settings.TNNT_TEMPLATE_ENTITY_TYPE
+    except AttributeError:
+        return_value["TNNT_TEMPLATE_ENTITY_TYPE"] = "alliance"
+
+    # entity name
+    # empty if none is given
+    try:
+        return_value["TNNT_TEMPLATE_ENTITY_NAME"] = settings.TNNT_TEMPLATE_ENTITY_NAME
+    except AttributeError:
+        return_value["TNNT_TEMPLATE_ENTITY_NAME"] = ""
+
+    try:
+        return_value[
+            "TNNT_TEMPLATE_URLS_OWN_WEBSITES"
+        ] = settings.TNNT_TEMPLATE_URLS_OWN_WEBSITES
+    except AttributeError:
+        pass
+
+    try:
+        return_value[
+            "TNNT_TEMPLATE_URLS_OTHER_WEBSITES"
+        ] = settings.TNNT_TEMPLATE_URLS_OTHER_WEBSITES
+    except AttributeError:
+        pass
+
+    return return_value
