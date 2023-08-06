60bcf3564205fe2885e01bdb748c5ce84d6554bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:42 +0000

    Add package libmp/0.1/libmp-0.1.tar.gz

---
+++ packages/libmp/libmp-0.1.tar.gz/libmp-0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'libmp',
+  packages = ['libmp'],
+  version = '0.1',
+  license='MIT',
+  description = 'LIBRARY FOR SUPPORT FUNCTIONS AND METHODS AT WORK FOR GROUP MULTIPECAS FROM BRAZIL',
+  author = 'LUCAS GARCIA RIBEIRO',
+  author_email = 'lucasgr7@gmail.com',
+  url = 'https://onlinemotopecas.com.br/',
+  download_url = 'https://github.com/lucasgr7/libmp/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['LIBRARY', 'MULTIPECAS', 'API', 'MERCADOLIVRE'],
+  #install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
