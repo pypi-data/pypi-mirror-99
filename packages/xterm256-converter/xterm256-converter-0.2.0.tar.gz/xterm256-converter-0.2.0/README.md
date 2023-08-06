8df802725b74b692c0614b841e1d49d4b8e7f8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:34 +0000

    Add package xterm256-converter/0.2.0/xterm256-converter-0.2.0.tar.gz

---
+++ packages/xterm256-converter/xterm256-converter-0.2.0.tar.gz/xterm256-converter-0.2.0/README.md
@@ -0,0 +1,23 @@
+# xterm256 converter
+
+[![PyPi](https://badge.fury.io/py/xterm256-converter.svg)](https://pypi.python.org/pypi/xterm256-converter/)
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+This util can convert hex color code to nearest xterm 256 color.
+
+## Requirement
+
+- Python3
+
+## Usage
+
+```sh
+$ xterm256-converter <hex color code>
+# ex) xterm256-converter ffffff
+```
+
+## Installation
+
+```sh
+$ pip install xterm256-converter
+```
---
+++ packages/xterm256-converter/xterm256-converter-0.2.0.tar.gz/xterm256-converter-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="xterm256-converter",
+    version="0.2.0",
+    description="This util can convert hex color code to nearest xterm 256 color.",
+    license="MIT",
+    author="averak",
+    packages=find_packages(),
+    package_data={'xterm256_converter': ['*.csv']},
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={
+        'console_scripts': [
+            'xterm256-converter=xterm256_converter.core:main',
+        ]
+    },
+)
