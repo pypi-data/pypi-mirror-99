442981c98a61d0392803c4681b0a6234c2e7db4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:05 +0000

    Add package neo-python-sdk/0.3.2/neo-python-sdk-0.3.2.tar.gz

---
+++ packages/neo-python-sdk/neo-python-sdk-0.3.2.tar.gz/neo-python-sdk-0.3.2/setup.py
@@ -0,0 +1,26 @@
+
+from distutils.core import setup
+setup(
+  name = 'neo-python-sdk',
+  packages = ['neo-python-sdk'],
+  version = '0.3.2',
+  license='	apache-2.0',
+  description = 'Neo SDK for Python with some additional libraries to support the development of Neo Sentinels (NSX).',
+  author = 'Jan-Eric Gaidusch <Neohelden GmbH>',
+  url = 'https://github.com/neohelden/neo-python-sdk',
+  keywords = ['neohelden', 'neo', 'neo-sdk'],
+  install_requires=[            # I get to this in a second
+          'python>=3.5',
+          'asyncio-nats-client=0.10.0',
+          'asyncio=3.4.3',
+          'sentry-sdk=1.0.0'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
