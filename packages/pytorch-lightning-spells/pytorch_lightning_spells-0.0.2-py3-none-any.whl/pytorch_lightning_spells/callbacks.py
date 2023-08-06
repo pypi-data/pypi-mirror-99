d3c0d7cd81e3beb5cd04d92d3212f3c2edbee690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:14 +0000

    Add package pytorch-lightning-spells/0.0.2/pytorch_lightning_spells-0.0.2-py3-none-any.whl

---
+++ packages/pytorch-lightning-spells/pytorch_lightning_spells-0.0.2-py3-none-any.whl/pytorch_lightning_spells/callbacks.py
@@ -0,0 +1,331 @@
+import socket
+from copy import deepcopy
+from datetime import datetime
+from typing import Optional, Sequence, Tuple
+
+import torch
+import torch.nn.functional as F
+import numpy as np
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.base import Callback
+from pytorch_lightning.callbacks import ModelCheckpoint
+# from pytorch_lightning.utilities.exceptions import MisconfigurationException
+
+from .cutmix_utils import cutmix_bbox_and_lam, rand_bbox, rand_bbox_minmax
+from .snapmix_utils import get_spm
+
+
+class RandomAugmentationChoiceCallback(Callback):
+    def __init__(
+            self, callbacks: Sequence[Callback], p: Sequence[Callback],
+            no_op_warmup: int = 0, no_op_prob: float = 0):
+        self.p = np.asarray(p) / np.sum(p)
+        self.callbacks = callbacks
+        self.no_op_warmup = no_op_warmup
+        self.step = 0
+        self.no_op_prob = no_op_prob
+        assert len(p) == len(callbacks)
+
+    def get_callback(self):
+        return np.random.choice(self.callbacks, p=self.p)
+
+    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        self.step += 1
+        if self.no_op_warmup >= self.step:
+            return
+        if self.no_op_prob and np.random.random() < self.no_op_prob:
+            return
+        self.get_callback().on_train_batch_start(
+            trainer, pl_module, batch, batch_idx, dataloader_idx)
+
+
+class SnapMixCallback(Callback):
+    """Callback that perform SnapMix augmentation on the input batch.
+
+    Reference: `Shaoli-Huang/SnapMix <https://github.com/Shaoli-Huang/SnapMix/>`_
+
+    **Requires the model to have implemented `extract_features` and `get_fc` methods.**
+    """
+
+    def __init__(
+            self, model, image_size, half: bool = False,
+            minmax: Optional[Tuple[float, float]] = None,
+            cutmix_bbox: bool = False,  # cutmix style randbox
+            alpha: float = 0.4, softmax_target: bool = True):
+        self._model = model
+        self._half = half
+        self.image_size = image_size
+        self.alpha = alpha
+        self.minmax = minmax
+        self.cutmix_bbox = cutmix_bbox
+        assert softmax_target, "SnapMix only support softmax_target=True"
+
+    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        old_batch = batch
+        batch, targets = batch
+        target_activation_map = get_spm(
+            batch, targets, self._model, self.image_size, self._half
+        )
+        bs = batch.size(0)
+        lamb_1 = np.clip(np.random.beta(self.alpha, self.alpha), 0.05, 0.95)
+        lamb_2 = np.clip(np.random.beta(self.alpha, self.alpha), 0.05, 0.95)
+        rand_index = torch.randperm(bs).cuda()
+        target_activation_map_b = target_activation_map[rand_index, :, :]
+
+        # same_label = target == target_b
+
+        area_1, area_2, cnt = 0, 0, 0
+        while area_1 <= 0 or area_2 <= 0:
+            if self.minmax:
+                bby1_1, bby2_1, bbx1_1, bbx2_1 = rand_bbox_minmax(
+                    batch.size(), self.minmax)
+                bby1_2, bby2_2, bbx1_2, bbx2_2 = rand_bbox_minmax(
+                    batch.size(), self.minmax)
+            else:
+                bby1_1, bby2_1, bbx1_1, bbx2_1 = rand_bbox(batch.size(), lamb_1)
+                bby1_2, bby2_2, bbx1_2, bbx2_2 = rand_bbox(batch.size(), lamb_2)
+            if self.cutmix_bbox is True:
+                # Use only one random bounding box
+                bby1_2, bby2_2, bbx1_2, bbx2_2 = bby1_1, bby2_1, bbx1_1, bbx2_1
+            area_1 = (bby2_1-bby1_1) * (bbx2_1-bbx1_1)
+            area_2 = (bby2_2-bby1_2) * (bbx2_2-bbx1_2)
+            cnt += 1
+            # Avoid infinite loops when something goes wrong
+            assert cnt < 10, f"{lamb_1}, {lamb_2}"
+
+        # print(bby1_1, bby2_1, bbx1_1, bbx2_1)
+        # print(bby1_2, bby2_2, bbx1_2, bbx2_2)
+        cropped = batch[rand_index, :, bby1_2:bby2_2, bbx1_2:bbx2_2].clone()
+        cropped = F.interpolate(
+            cropped,
+            size=(bby2_1-bby1_1, bbx2_1-bbx1_1),
+            mode='bilinear',
+            align_corners=True
+        )
+        batch[:, :, bby1_1:bby2_1, bbx1_1:bbx2_1] = cropped
+        lamb_1 = (
+            1 - target_activation_map[
+                :, bby1_1:bby2_1, bbx1_1:bbx2_1
+            ].sum(1).sum(1)
+        )
+        lamb_2 = target_activation_map_b[
+            :, bby1_2:bby2_2, bbx1_2:bbx2_2
+        ].sum(1).sum(1)
+
+        # Fall back to Cutmix lambda
+        lamb_cutmix = 1 - (
+            (bbx2_1 - bbx1_1) * (bby2_1 - bby1_1) /
+            (batch.size(2) * batch.size(3))
+        )
+        lamb_1[torch.isnan(lamb_1)] = lamb_cutmix
+        lamb_2[torch.isnan(lamb_2)] = 1 - lamb_cutmix
+        # Combine targets
+        new_targets = torch.stack([
+            targets.float(), targets[rand_index].float(),
+            lamb_1.to(targets.device), lamb_2.to(targets.device)
+        ], dim=1)
+        old_batch[0] = batch
+        old_batch[1] = new_targets
+
+
+class CutMixCallback(Callback):
+    """Callback that perform CutMix augmentation on the input batch.
+
+    Assumes the first dimension is batch.
+
+    Reference: `rwightman/pytorch-image-models/ <https://github.com/rwightman/pytorch-image-models/blob/8c9814e3f500e8b37aae86dd4db10aba2c295bd2/timm/data/mixup.py>`_
+    """
+
+    def __init__(self, alpha: float = 0.4, softmax_target: bool = False, minmax: Optional[Tuple[float, float]] = None):
+        super().__init__()
+        self.alpha = alpha
+        self.softmax_target = softmax_target
+        self.minmax = minmax
+
+    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        old_batch = batch
+        batch, targets = batch
+        batch_flipped = batch.flip(0).clone()
+        lambd = np.random.beta(self.alpha, self.alpha, batch.size(0))
+        for i in range(batch.shape[0]):
+            (yl, yh, xl, xh), lambd_tmp = cutmix_bbox_and_lam(
+                batch.shape, lambd[i], ratio_minmax=self.minmax, correct_lam=True)
+            lambd[i] = lambd_tmp
+            # fill in the cut regions
+            batch[i, :, yl:yh, xl:xh] = batch_flipped[i, :, yl:yh, xl:xh]
+        # Create the tensor and expand (for target)
+        lambd_tensor = batch.new(lambd).view(
+            -1, *[1 for _ in range(len(targets.size())-1)]
+        ).expand(-1, *targets.shape[1:])
+        # Combine targets
+        if self.softmax_target:
+            new_targets = torch.stack([
+                targets.float(), targets.flip(0).float(), lambd_tensor
+            ], dim=1)
+        else:
+            new_targets = (
+                targets * lambd_tensor +
+                targets.flip(0) * (1-lambd_tensor)
+            )
+        old_batch[0] = batch
+        old_batch[1] = new_targets
+
+
+class MixUpCallback(Callback):
+    """Callback that perform MixUp augmentation on the input batch.
+
+    Assumes the first dimension is batch.
+
+    Works best with pytorch_lightning_spells.losses.MixupSoftmaxLoss
+
+    Reference: `Fast.ai's implementation <https://github.com/fastai/fastai/blob/master/fastai/callbacks/mixup.py>`_
+    """
+
+    def __init__(self, alpha: float = 0.4, softmax_target: bool = False):
+        super().__init__()
+        self.alpha = alpha
+        self.softmax_target = softmax_target
+
+    def on_train_batch_start(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        old_batch = batch
+        batch, targets = batch
+        batch_flipped = batch.flip(0).clone()
+        lambd = np.random.beta(self.alpha, self.alpha, batch.size(0))
+        lambd = np.concatenate(
+            [lambd[:, np.newaxis], 1-lambd[:, np.newaxis]], axis=1
+        ).max(axis=1)
+        # Create the tensor and expand (for batch inputs)
+        lambd_tensor = batch.new(lambd).view(
+            -1, *[1 for _ in range(len(batch.size())-1)]
+        ).expand(-1, *batch.shape[1:])
+        # Combine input batch
+        new_batch = (batch * lambd_tensor +
+                     batch_flipped * (1-lambd_tensor))
+        # Create the tensor and expand (for target)
+        lambd_tensor = batch.new(lambd).view(
+            -1, *[1 for _ in range(len(targets.size())-1)]
+        ).expand(-1, *targets.shape[1:])
+        # Combine targets
+        if self.softmax_target:
+            new_targets = torch.stack([
+                targets.float(), targets.flip(0).float(), lambd_tensor
+            ], dim=1)
+        else:
+            new_targets = (
+                targets * lambd_tensor +
+                targets.flip(0) * (1-lambd_tensor)
+            )
+        old_batch[0] = new_batch
+        old_batch[1] = new_targets
+
+
+class TelegramCallback(Callback):
+    """A Telegram notification callback
+
+    Reference: `huggingface/knockknock <https://github.com/huggingface/knockknock>`_
+    """
+    DATE_FORMAT = "%Y-%m-%d %H:%M:%d"
+
+    def __init__(self, token: str, chat_id: int, name: str, report_evals: bool = False):
+        try:
+            import telegram
+        except ImportError:
+            raise ImportError(
+                "Please install 'python-telegram-bot' before using TelegramCallback.")
+        self._token = token
+        self.telegram_bot = telegram.Bot(token=self._token)
+        self.host_name = socket.gethostname()
+        self.report_evals = report_evals
+        self.chat_id = chat_id
+        self.name = name
+        self.start_time = datetime.now()
+
+    def send_message(self, text):
+        try:
+            self.telegram_bot.send_message(chat_id=self.chat_id, text=text)
+        except telegram.error.TimeOut:
+            # Ignore timeouts and continue training
+            pass
+
+    def on_train_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
+        self.start_time = datetime.now()
+        contents = [
+            f'{self.name} has started training 🎬',
+            'Machine name: %s' % self.host_name,
+            'Starting date: %s' % self.start_time.strftime(
+                TelegramCallback.DATE_FORMAT)
+        ]
+        text = '\n'.join(contents)
+        self.send_message(text=text)
+
+    def on_train_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
+        end_time = datetime.now()
+        elapsed_time = end_time - self.start_time
+        contents = [
+            f'{self.name} has finished training 🎉',
+            'Machine name: %s' % self.host_name,
+            'Starting date: %s' % self.start_time.strftime(
+                TelegramCallback.DATE_FORMAT),
+            'End date: %s' % end_time.strftime(
+                TelegramCallback.DATE_FORMAT),
+            'Training duration: %s' % str(elapsed_time)
+        ]
+        text = '\n'.join(contents)
+        self.send_message(text=text)
+
+    def on_validation_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule):
+        metrics, meta = self._collect_metrics(trainer)
+        if self.report_evals is False:
+            return
+        contents = [
+            f"Metrics from {self.name} at step {meta['step']} (epoch {meta['epoch']}):"
+        ]
+        contents += [
+            f"{metric_name}: {metric_value:.6f}"
+            for metric_name, metric_value in metrics.items()
+            if metric_name != "epoch"
+        ]
+        text = '\n'.join(contents)
+        self.send_message(text=text)
+
+    def _collect_metrics(self, trainer):
+        ckpt_name_metrics = deepcopy(trainer.logger_connector.logged_metrics)
+        # ckpt_name_metrics.update(trainer.logger_connector.callback_metrics)
+        # ckpt_name_metrics.update(trainer.logger_connector.progress_bar_metrics)
+        meta = {"step": trainer.global_step, "epoch": trainer.current_epoch}
+        return ckpt_name_metrics, meta
+
+
+class LookaheadCallback(Callback):
+    """Switch to the slow weights before evaluation and switch back after.
+    """
+
+    def on_validation_start(self, trainer, pl_module):
+        optimizer = trainer.optimizers(use_pl_optimizer=False)
+        if hasattr(optimizer, "_backup_and_load_cache"):
+            print("load slow parameters")
+            optimizer._backup_and_load_cache()
+
+    def on_validation_end(self, trainer, pl_module):
+        optimizer = trainer.optimizers(use_pl_optimizer=False)
+        if hasattr(optimizer, "_clear_and_load_backup"):
+            print("load fast parameters")
+            optimizer._clear_and_load_backup()
+
+
+class LookaheadModelCheckpoint(ModelCheckpoint):
+    """Combines LookaheadCallback and ModelCheckpoint
+    """
+
+    def on_validation_start(self, trainer, pl_module):
+        for optimizer in trainer.optimizers:
+            if hasattr(optimizer, "_backup_and_load_cache"):
+                print("load slow parameters")
+                optimizer._backup_and_load_cache()
+
+    def on_validation_end(self, trainer, pl_module):
+        self.save_checkpoint(trainer, pl_module)
+        for optimizer in trainer.optimizers:
+            if hasattr(optimizer, "_clear_and_load_backup"):
+                print("load fast parameters")
+                optimizer._clear_and_load_backup()
---
+++ packages/pytorch-lightning-spells/pytorch_lightning_spells-0.0.2-py3-none-any.whl/pytorch_lightning_spells/utils.py
@@ -0,0 +1,105 @@
+import warnings
+from typing import Sequence, Union
+
+import torch
+import numpy as np
+
+Layer = Union[torch.nn.Module, torch.nn.ModuleList]
+
+
+class EMATracker:
+    """Keeps the exponential moving average for a single series.
+
+    Parameters
+    ----------
+    alpha : float, optional
+        the weight of the new value, by default 0.05
+    """
+
+    def __init__(self, alpha: float = 0.05):
+        super().__init__()
+        self.alpha = alpha
+        self._value = None
+
+    def update(self, new_value):
+        new_value = new_value.detach()
+        if torch.isnan(new_value):
+            warnings.warn("NaN encountered as training loss!")
+            return
+        if self._value is None:
+            self._value = new_value
+        else:
+            self._value = (
+                new_value * self.alpha +
+                self._value * (1-self.alpha)
+            )
+
+    @property
+    def value(self):
+        return self._value
+
+
+def count_parameters(parameters):
+    return int(np.sum(list(p.numel() for p in parameters)))
+
+# -----------------------------------
+# Layer freezing from fast.ai v1
+# -----------------------------------
+
+
+def children(m):
+    return m if isinstance(m, (list, tuple)) else list(m.children())
+
+
+def set_trainable_attr(m, b):
+    m.trainable = b
+    for p in m.parameters():
+        p.requires_grad = b
+
+
+def apply_leaf(m, f):
+    c = children(m)
+    if isinstance(m, torch.nn.Module):
+        f(m)
+    if len(c) > 0:
+        for l in c:
+            apply_leaf(l, f)
+
+
+def set_trainable(layer: Layer, trainable: bool):
+    """Freeze or unfreeze all parameters in the layer."""
+    apply_leaf(layer, lambda m: set_trainable_attr(m, trainable))
+
+
+def freeze_layers(layer_groups: Sequence[Layer], freeze_flags: Sequence[bool]):
+    """Freeze or unfreeze groups of layers"""
+    assert len(freeze_flags) == len(layer_groups)
+    for layer, flag in zip(layer_groups, freeze_flags):
+        set_trainable(layer, not flag)
+
+# -----------------------------------------------------------
+# Separate BatchNorm2d and GroupNorm paremeters from others
+# -----------------------------------------------------------
+
+
+def seperate_parameters(module):
+    decay, no_decay = [], []
+    if isinstance(module, list):
+        for entry in module:
+            tmp = seperate_parameters(entry)
+            decay.extend(tmp[0])
+            no_decay.extend(tmp[1])
+    elif isinstance(module, torch.nn.Parameter):
+        no_decay.append(module)
+    else:
+        for submodule in module.modules():
+            if not list(submodule.children()):  # leaf node
+                if isinstance(submodule, (
+                        torch.nn.GroupNorm, torch.nn.BatchNorm2d,
+                        torch.nn.LayerNorm, torch.nn.BatchNorm1d)):
+                    no_decay.extend(list(submodule.parameters()))
+                elif isinstance(submodule, torch.nn.Identity):
+                    continue
+                else:
+                    decay.extend(list(submodule.parameters()))
+    return decay, no_decay
