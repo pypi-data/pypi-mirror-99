ad8438ae420a5cc08d690d3dacec68f3864bb328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:18 +0000

    Add package applipy-inject/1.1.0/applipy_inject-1.1.0-py3-none-any.whl

---
+++ packages/applipy-inject/applipy_inject-1.1.0-py3-none-any.whl/applipy_inject/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    'Injector',
+    'named',
+    'with_names',
+]
+
+
+from applipy_inject.version import __version__  # noqa
+from applipy_inject.inject import named, with_names, Injector
