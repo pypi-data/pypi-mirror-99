dfd2e8d33be47295e906f7c8cd5e296ce96ae524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:17 +0000

    Add package xtgeo/2.15a2/xtgeo-2.15a2-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.15a2-cp38-cp38-macosx_10_9_x86_64.whl/xtgeo/plot/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""The XTGeo plot package"""
+
+
+# flake8: noqa
+from xtgeo.plot.xsection import XSection
+from xtgeo.plot.xtmap import Map
+from xtgeo.plot.grid3d_slice import Grid3DSlice
+
+# from ._colortables import random, random40, xtgeocolors, colorsfromfile
