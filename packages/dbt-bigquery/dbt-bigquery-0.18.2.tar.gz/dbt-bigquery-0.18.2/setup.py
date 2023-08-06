2bb2cd7ee9b496e7d313d9ab9e1decb84888bd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:13 +0000

    Add package dbt-bigquery/0.18.2/dbt-bigquery-0.18.2.tar.gz

---
+++ packages/dbt-bigquery/dbt-bigquery-0.18.2.tar.gz/dbt-bigquery-0.18.2/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if sys.version_info < (3, 6):
+    print('Error: dbt does not support this version of Python.')
+    print('Please upgrade to Python 3.6 or higher.')
+    sys.exit(1)
+
+
+from setuptools import setup
+try:
+    from setuptools import find_namespace_packages
+except ImportError:
+    # the user has a downlevel version of setuptools.
+    print('Error: dbt requires setuptools v40.1.0 or higher.')
+    print('Please upgrade setuptools with "pip install --upgrade setuptools" '
+          'and try again')
+    sys.exit(1)
+
+
+package_name = "dbt-bigquery"
+package_version = "0.18.2"
+description = """The bigquery adapter plugin for dbt (data build tool)"""
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Fishtown Analytics",
+    author_email="info@fishtownanalytics.com",
+    url="https://github.com/fishtown-analytics/dbt",
+    packages=find_namespace_packages(include=['dbt', 'dbt.*']),
+    package_data={
+        'dbt': [
+            'include/bigquery/dbt_project.yml',
+            'include/bigquery/sample_profiles.yml',
+            'include/bigquery/macros/*.sql',
+            'include/bigquery/macros/**/*.sql',
+        ]
+    },
+    install_requires=[
+        'dbt-core=={}'.format(package_version),
+        'protobuf>=3.6.0,<3.12',
+        'google-cloud-core>=1.3.0,<1.4',
+        'google-cloud-bigquery>=1.25.0,<1.26.0',
+        'google-api-core>=1.16.0,<1.17.0',
+        'googleapis-common-protos>=1.6.0,<1.7.0',
+        'six>=1.14.0',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires=">=3.6.2",
+)
