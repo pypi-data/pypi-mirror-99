3a5af506411f680c09a894d506bfce78254225ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:11 +0000

    Add package blp-helpers-data-science/2.6/blp_helpers_data_science-2.6.tar.gz

---
+++ packages/blp-helpers-data-science/blp_helpers_data_science-2.6.tar.gz/blp_helpers_data_science-2.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blp_helpers_data_science", # Replace with your own username
+    version="2.6",
+    author="Leopold Bosankic",
+    author_email="leopold.bosankic@outlook.com",
+    description="Helpers I use for Data Science projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://researchly@bitbucket.org/researchly/helpers.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
