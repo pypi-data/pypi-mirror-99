ef9529750432e8bd5de76f413b7eea2662b963c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:00 +0000

    Add package pdm-pep517/0.5.9/pdm_pep517-0.5.9-py3-none-any.whl

---
+++ packages/pdm-pep517/pdm_pep517-0.5.9-py3-none-any.whl/pdm/pep517/base.py
@@ -0,0 +1,377 @@
+import atexit
+import glob
+import os
+import textwrap
+from pathlib import Path
+from typing import Dict, Iterator, List, Tuple, Union
+
+from pdm.pep517.metadata import Metadata
+from pdm.pep517.utils import is_python_package, normalize_path
+
+OPEN_README = """import codecs
+
+with codecs.open({readme!r}, encoding="utf-8") as fp:
+    long_description = fp.read()
+"""
+
+SETUP_FORMAT = """
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+{before}
+setup_kwargs = {{
+    'name': {name!r},
+    'version': {version!r},
+    'description': {description!r},
+    'long_description': long_description,
+    'license': {license!r},
+    'author': {author!r},
+    'author_email': {author_email!r},
+    'maintainer': {maintainer!r},
+    'maintainer_email': {maintainer_email!r},
+    'url': {url!r},
+{extra}
+}}
+{after}
+
+setup(**setup_kwargs)
+"""
+
+METADATA_BASE = """\
+Metadata-Version: 2.1
+Name: {name}
+Version: {version}
+Summary: {description}
+License: {license}
+"""
+
+
+class BuildError(RuntimeError):
+    pass
+
+
+def _match_path(path: str, pattern: str) -> bool:
+    return normalize_path(os.path.abspath(path)) == normalize_path(
+        os.path.abspath(pattern)
+    )
+
+
+def _merge_globs(
+    include_globs: Dict[str, str], excludes_globs: Dict[str, str]
+) -> Tuple[List[str], List[str]]:
+    includes, excludes = [], []
+    for path, key in include_globs.items():
+        # The longer glob pattern wins
+        if path in excludes_globs:
+            if len(key) <= excludes_globs[path]:
+                continue
+            else:
+                del excludes_globs[path]
+        includes.append(path)
+    excludes = list(excludes_globs)
+    return includes, excludes
+
+
+def _find_top_packages(root: str) -> List[str]:
+    result = []
+    for path in os.listdir(root):
+        path = os.path.join(root, path)
+        if is_python_package(path):
+            result.append(path)
+    return result
+
+
+def _format_list(data: List[str], indent: int = 4) -> str:
+    result = ["["]
+    for row in data:
+        result.append(" " * indent + repr(row) + ",")
+    result.append(" " * (indent - 4) + "]")
+    return "\n".join(result)
+
+
+def _format_dict_list(data: Dict[str, List[str]], indent: int = 4) -> str:
+    result = ["{"]
+    for key, value in data.items():
+        result.append(
+            " " * indent + repr(key) + ": " + _format_list(value, indent + 4) + ","
+        )
+    result.append(" " * (indent - 4) + "}")
+    return "\n".join(result)
+
+
+class Builder:
+    """Base class for building and distributing a package from given path."""
+
+    DEFAULT_EXCLUDES = ["ez_setup", "*__pycache__", "tests", "tests.*"]
+
+    def __init__(self, location: Union[str, Path]) -> None:
+        self._old_cwd = None
+        self.location = Path(location).absolute()
+        self._meta = None
+
+    @property
+    def meta(self) -> Metadata:
+        if not self._meta:
+            self._meta = Metadata(self.location / "pyproject.toml")
+        return self._meta
+
+    def __enter__(self) -> "Builder":
+        self._old_cwd = os.getcwd()
+        os.chdir(self.location)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        os.chdir(self._old_cwd)
+
+    def build(self, build_dir: str, **kwargs) -> str:
+        raise NotImplementedError
+
+    def _find_files_iter(self, include_build: bool = False) -> Iterator[str]:
+        includes = []
+        find_froms = []
+        excludes = []
+        dont_find_froms = ["tests"]
+
+        if not self.meta.includes:
+            find_froms = _find_top_packages(self.meta.package_dir or ".")
+            if not find_froms:
+                includes = [
+                    f"{self.meta.package_dir}/*.py" if self.meta.package_dir else "*.py"
+                ]
+        else:
+            for pat in self.meta.includes:
+                if os.path.basename(pat) == "*":
+                    pat = pat[:-2]
+                if "*" in pat or os.path.isfile(pat):
+                    includes.append(pat)
+                else:
+                    find_froms.append(pat)
+
+        if self.meta.excludes:
+            for pat in self.meta.excludes:
+                if "*" in pat or os.path.isfile(pat):
+                    excludes.append(pat)
+                else:
+                    dont_find_froms.append(pat)
+
+        include_globs = {path: key for key in includes for path in glob.glob(key)}
+        excludes_globs = {path: key for key in excludes for path in glob.glob(key)}
+
+        includes, excludes = _merge_globs(include_globs, excludes_globs)
+        for path in find_froms:
+            if any(_match_path(path, item) for item in dont_find_froms):
+                continue
+            path_base = os.path.dirname(path)
+            if not path_base or path_base == ".":
+                # the path is top level itself
+                path_base = path
+
+            for root, dirs, filenames in os.walk(path):
+
+                for filename in filenames:
+                    if filename.endswith(".pyc") or any(
+                        _match_path(os.path.join(root, filename), item)
+                        for item in excludes
+                    ):
+                        continue
+                    yield os.path.join(root, filename)
+
+        for path in includes:
+            if os.path.isfile(path):
+                yield path
+        if not include_build:
+            return
+
+        if self.meta.build and os.path.isfile(self.meta.build):
+            yield self.meta.build
+
+        for pat in ("COPYING", "LICENSE"):
+            for path in glob.glob(pat + "*"):
+                if os.path.isfile(path):
+                    yield path
+
+        if self.meta.readme and os.path.isfile(self.meta.readme):
+            yield self.meta.readme
+
+        if self.meta.filepath.exists():
+            yield "pyproject.toml"
+
+    def find_files_to_add(self, include_build: bool = False) -> List[Path]:
+        """Traverse the project path and return a list of file names
+        that should be included in a sdist distribution.
+        If include_build is True, will include files like LICENSE, README and pyproject
+        Produce a paths list relative to the source dir.
+        """
+        return sorted(set(Path(p) for p in self._find_files_iter(include_build)))
+
+    def format_setup_py(self) -> str:
+        before, extra, after = [], [], []
+        meta = self.meta
+        kwargs = {
+            "name": meta.name,
+            "version": meta.version,
+            "author": meta.author,
+            "license": meta.license_type,
+            "author_email": meta.author_email,
+            "maintainer": meta.maintainer,
+            "maintainer_email": meta.maintainer_email,
+            "description": meta.description,
+            "url": (meta.project_urls or {}).get("homepage", ""),
+        }
+
+        if meta.build:
+            # The build script must contain a `build(setup_kwargs)`, we just import
+            # and execute it.
+            after.extend(
+                [
+                    "from {} import build\n".format(meta.build.split(".")[0]),
+                    "build(setup_kwargs)\n",
+                ]
+            )
+
+        package_paths = meta.convert_package_paths()
+        if package_paths["packages"]:
+            extra.append(
+                "    'packages': {},\n".format(
+                    _format_list(package_paths["packages"], 8)
+                )
+            )
+        if package_paths["package_dir"]:
+            extra.append(
+                "    'package_dir': {!r},\n".format(package_paths["package_dir"])
+            )
+        if package_paths["package_data"]:
+            extra.append(
+                "    'package_data': {!r},\n".format(package_paths["package_data"])
+            )
+        if package_paths["exclude_package_data"]:
+            extra.append(
+                "    'exclude_package_data': {!r},\n".format(
+                    package_paths["exclude_package_data"]
+                )
+            )
+        if meta.readme:
+            before.append(OPEN_README.format(readme=meta.readme))
+        elif meta.long_description:
+            before.append(
+                "long_description = '''{}'''\n".format(
+                    repr(meta.long_description)[1:-1]
+                )
+            )
+        else:
+            before.append("long_description = None\n")
+        if meta.long_description_content_type:
+            extra.append(
+                "    'long_description_content_type': {!r},\n".format(
+                    meta.long_description_content_type
+                )
+            )
+
+        if meta.keywords:
+            extra.append("    'keywords': {!r},\n".format(meta.keywords))
+        if meta.classifiers:
+            extra.append(
+                "    'classifiers': {},\n".format(_format_list(meta.classifiers, 8))
+            )
+        if meta.dependencies:
+            before.append(
+                "INSTALL_REQUIRES = {}\n".format(_format_list(meta.dependencies))
+            )
+            extra.append("    'install_requires': INSTALL_REQUIRES,\n")
+        if meta.optional_dependencies:
+            before.append(
+                "EXTRAS_REQUIRE = {}\n".format(
+                    _format_dict_list(meta.optional_dependencies)
+                )
+            )
+            extra.append("    'extras_require': EXTRAS_REQUIRE,\n")
+        if meta.requires_python:
+            extra.append("    'python_requires': {!r},\n".format(meta.requires_python))
+        if meta.entry_points:
+            before.append(
+                "ENTRY_POINTS = {}\n".format(_format_dict_list(meta.entry_points))
+            )
+            extra.append("    'entry_points': ENTRY_POINTS,\n")
+        return SETUP_FORMAT.format(
+            before="".join(before), after="".join(after), extra="".join(extra), **kwargs
+        )
+
+    def format_pkginfo(self, full=True) -> str:
+        meta = self.meta
+        content = METADATA_BASE.format(
+            name=meta.name or "UNKNOWN",
+            version=meta.version or "UNKNOWN",
+            license=meta.license_type or "UNKNOWN",
+            description=meta.description or "UNKNOWN",
+        )
+
+        # Optional fields
+        if meta.keywords:
+            content += "Keywords: {}\n".format(",".join(meta.keywords))
+
+        if meta.author:
+            content += "Author: {}\n".format(meta.author)
+
+        if meta.author_email:
+            content += "Author-email: {}\n".format(meta.author_email)
+
+        if meta.maintainer:
+            content += "Maintainer: {}\n".format(meta.maintainer)
+
+        if meta.maintainer_email:
+            content += "Maintainer-email: {}\n".format(meta.maintainer_email)
+
+        if meta.requires_python:
+            content += "Requires-Python: {}\n".format(meta.requires_python)
+
+        for classifier in meta.classifiers or []:
+            content += "Classifier: {}\n".format(classifier)
+
+        if full:
+            for dep in sorted(meta.dependencies):
+                content += "Requires-Dist: {}\n".format(dep)
+
+        for extra, reqs in sorted(meta.requires_extra.items()):
+            content += "Provides-Extra: {}\n".format(extra)
+            if full:
+                for dep in reqs:
+                    content += "Requires-Dist: {}\n".format(dep)
+
+        for url in sorted(meta.project_urls or {}):
+            content += "Project-URL: {}, {}\n".format(url, meta.project_urls[url])
+
+        if meta.long_description_content_type:
+            content += "Description-Content-Type: {}\n".format(
+                meta.long_description_content_type
+            )
+        if meta.long_description:
+            readme = meta.long_description
+            if full:
+                content += "\n" + readme + "\n"
+            else:
+                content += "Description: {}\n".format(
+                    textwrap.indent(readme, " " * 8, lambda line: True).lstrip()
+                )
+
+        return content
+
+    def ensure_setup_py(self, clean: bool = True) -> Path:
+        """Ensures the requirement has a setup.py ready."""
+        # XXX: Currently only handle PDM project, and do nothing if not.
+
+        setup_py_path = self.location.joinpath("setup.py")
+        if setup_py_path.is_file():
+            return setup_py_path
+
+        setup_py_path.write_text(self.format_setup_py(), encoding="utf-8")
+
+        # Clean this temp file when process exits
+        def cleanup():
+            try:
+                setup_py_path.unlink()
+            except OSError:
+                pass
+
+        if clean:
+            atexit.register(cleanup)
+        return setup_py_path
---
+++ packages/pdm-pep517/pdm_pep517-0.5.9-py3-none-any.whl/pdm/pep517/metadata.py
@@ -0,0 +1,357 @@
+import glob
+import os
+import re
+from pathlib import Path
+from typing import Any, Callable, Dict, Generic, List, Optional, Type, TypeVar, Union
+
+from pdm.pep517._vendor import toml
+from pdm.pep517._vendor.packaging.requirements import Requirement
+from pdm.pep517._vendor.packaging.specifiers import SpecifierSet
+from pdm.pep517._vendor.packaging.version import Version
+from pdm.pep517.legacy import convert_legacy
+from pdm.pep517.license import get_license_classifier, license_lookup
+from pdm.pep517.scm import get_version_from_scm
+from pdm.pep517.utils import (
+    cd,
+    ensure_pep440_req,
+    find_packages_iter,
+    is_dict_like,
+    merge_marker,
+    safe_name,
+)
+
+T = TypeVar("T")
+
+AVAILABLE_PYTHON_VERSIONS = (
+    "2.7",
+    "3.4",
+    "3.5",
+    "3.6",
+    "3.7",
+    "3.8",
+    "3.9",
+)
+
+
+class ProjectError(ValueError):
+    pass
+
+
+class MetaField(Generic[T]):
+    def __init__(
+        self, name: str, fget: Optional[Callable[["Metadata", Any], T]] = None
+    ) -> None:
+        self.name = name
+        self.fget = fget
+
+    def __get__(self, instance: "Metadata", owner: Type["Metadata"]) -> T:
+        if instance is None:
+            return self
+        try:
+            rv = instance._metadata[self.name]
+            if self.fget is not None:
+                rv = self.fget(instance, rv)
+            return rv
+        except KeyError:
+            return None
+
+
+_NAME_EMAIL_RE = re.compile(r"^\s*([^<>]+?)\s*<([^<>]+)>")
+
+
+class Metadata:
+    """A class that holds all metadata that Python packaging requries."""
+
+    DEFAULT_ENCODING = "utf-8"
+    SUPPORTED_CONTENT_TYPES = ("text/markdown", "text/x-rst", "text/plain")
+
+    def __init__(self, filepath: Union[str, Path]) -> None:
+        self.filepath = Path(filepath).absolute()
+        self._metadata = self._read_pyproject(self.filepath)
+
+    @staticmethod
+    def _read_pyproject(filepath: Path) -> Dict[str, Any]:
+        try:
+            data = toml.loads(filepath.read_text(encoding="utf-8"))
+        except FileNotFoundError:
+            raise ProjectError("pyproject.toml does not exist.")
+        except toml.TomlDecodeError:
+            raise ProjectError("The project's pyproject.toml is not valid.")
+        else:
+            if "project" in data:
+                return data["project"]
+            elif "tool" in data and "pdm" in data["tool"]:
+                return convert_legacy(data["tool"]["pdm"])
+            else:
+                raise ProjectError("No [project] config in pyproject.toml")
+
+    name: MetaField[str] = MetaField("name")
+
+    def _get_version(self, value):
+        if isinstance(value, str):
+            return value
+        if not self.dynamic or "version" not in self.dynamic:
+            raise ProjectError(
+                "'value' must be in 'dynamic' field to let pdm-pep517 fill in the value"
+            )
+        version_source = value.get("from")
+        if version_source:
+            with self.filepath.parent.joinpath(version_source).open(
+                encoding="utf-8"
+            ) as fp:
+                version = re.findall(
+                    r"^__version__\s*=\s*[\"'](.+?)[\"']\s*$", fp.read(), re.M
+                )[0]
+        elif value.get("use_scm", False):
+            version = get_version_from_scm(self.filepath.parent)
+        else:
+            version = None
+        return version
+
+    version: MetaField[str] = MetaField("version", _get_version)
+    description: MetaField[str] = MetaField("description")
+
+    def _get_readme_file(self, value):
+        if is_dict_like(value):
+            return value.get("file")
+        return value
+
+    def _get_readme_content(self, value):
+        if is_dict_like(value):
+            if "file" in value and "text" in value:
+                raise ProjectError(
+                    "readme table shouldn't specify both 'file' "
+                    "and 'text' at the same time"
+                )
+            if "text" in value:
+                return value["text"]
+            file_path = value.get("file")
+            encoding = value.get("charset", self.DEFAULT_ENCODING)
+            return Path(file_path).read_text(encoding=encoding)
+        return Path(value).read_text(encoding=self.DEFAULT_ENCODING)
+
+    def _get_content_type(self, value):
+        if is_dict_like(value):
+            content_type = value.get("content-type")
+            if not content_type:
+                raise ProjectError("'content-type' is missing in the readme table")
+            if content_type not in self.SUPPORTED_CONTENT_TYPES:
+                raise ProjectError(f"Unsupported readme content-type: {content_type}")
+            return content_type
+        if value.lower().endswith(".md"):
+            return "text/markdown"
+        elif value.lower().endswith(".rst"):
+            return "text/x-rst"
+        raise ProjectError(f"Unsupported readme suffix: {value}")
+
+    readme: MetaField[str] = MetaField("readme", _get_readme_file)
+    long_description: MetaField[str] = MetaField("readme", _get_readme_content)
+    long_description_content_type: MetaField[str] = MetaField(
+        "readme", _get_content_type
+    )
+
+    def _get_license(self, value):
+        if not is_dict_like(value):
+            return ""
+        if "file" in value and "text" in value:
+            raise ProjectError(
+                "license table shouldn't specify both 'file' "
+                "and 'text' at the same time"
+            )
+        return (
+            Path(value["file"]).read_text(encoding=self.DEFAULT_ENCODING)
+            if "file" in value
+            else value.get("text")
+        )
+
+    def _get_license_type(self, value):
+        if is_dict_like(value):
+            if value.get("text", "") in license_lookup:
+                return value.get("text")
+        else:
+            return value
+
+    license: MetaField[str] = MetaField("license", _get_license)
+    license_type: MetaField[str] = MetaField("license", _get_license_type)
+
+    def _get_name(self, value):
+        result = []
+        for item in value:
+            if "email" not in item and "name" in item:
+                result.append(item.get("name"))
+        return ",".join(result)
+
+    def _get_email(self, value):
+        result = []
+        for item in value:
+            if "email" not in item:
+                continue
+            email = (
+                item["email"]
+                if "name" not in item
+                else "{name} <{email}>".format(**item)
+            )
+            result.append(email)
+        return ",".join(result)
+
+    author: MetaField[str] = MetaField("authors", _get_name)
+    author_email: MetaField[str] = MetaField("authors", _get_email)
+    maintainer: MetaField[str] = MetaField("maintainers", _get_name)
+    maintainer_email: MetaField[str] = MetaField("maintainers", _get_email)
+
+    @property
+    def classifiers(self):
+        classifers = set(self._metadata.get("classifiers", []))
+
+        if self.dynamic and "classifiers" in self.dynamic:
+            python_constraint = (
+                SpecifierSet(self.requires_python)
+                if self.requires_python
+                else SpecifierSet()
+            )
+
+            for version in AVAILABLE_PYTHON_VERSIONS:
+                if python_constraint.contains(Version(version)):
+                    classifers.add(f"Programming Language :: Python :: {version[0]}")
+                    classifers.add(f"Programming Language :: Python :: {version}")
+
+            if self.license_type:
+                classifers.add(get_license_classifier(self.license_type))
+
+        return sorted(classifers)
+
+    keywords: MetaField[str] = MetaField("keywords")
+    project_urls: MetaField[Dict[str, str]] = MetaField("urls")
+    includes: MetaField[List[str]] = MetaField("includes")
+    excludes: MetaField[List[str]] = MetaField("excludes")
+    build: MetaField[str] = MetaField("build")
+
+    def _convert_dependencies(self, deps):
+        return list(filter(None, map(ensure_pep440_req, deps)))
+
+    def _convert_optional_dependencies(self, deps):
+        return {k: self._convert_dependencies(deps[k]) for k in deps}
+
+    dependencies: MetaField[List[str]] = MetaField(
+        "dependencies", _convert_dependencies
+    )
+    optional_dependencies: MetaField[Dict[str, List[str]]] = MetaField(
+        "optional-dependencies", _convert_optional_dependencies
+    )
+    dynamic: MetaField[List[str]] = MetaField("dynamic")
+
+    @property
+    def project_name(self) -> str:
+        return safe_name(self.name)
+
+    @property
+    def requires_extra(self) -> Dict[str, List[str]]:
+        """For PKG-INFO metadata"""
+        if not self.optional_dependencies:
+            return {}
+        result = {}
+        for name, reqs in self.optional_dependencies.items():
+            current = result[name] = []
+            for r in reqs:
+                parsed = Requirement(r)
+                merge_marker(parsed, f"extra == {name!r}")
+                current.append(str(parsed))
+        return result
+
+    @property
+    def requires_python(self) -> str:
+        return self._metadata.get("requires-python", "")
+
+    @property
+    def entry_points(self) -> Dict[str, List[str]]:
+        result = {}
+        settings = self._metadata
+        if "scripts" in settings:
+            result["console_scripts"] = [
+                f"{key} = {value}" for key, value in settings["scripts"].items()
+            ]
+        if "gui-scripts" in settings:
+            result["gui_scripts"] = [
+                f"{key} = {value}" for key, value in settings["gui-scripts"].items()
+            ]
+        if "entry-points" in settings:
+            for plugin, value in settings["entry-points"].items():
+                if plugin in ("console_scripts", "gui_scripts"):
+                    raise ProjectError(
+                        f"'project.entry-points.{plugin}'' should be defined "
+                        f"in 'project.{plugin.replace('_', '-')}'"
+                    )
+                result[plugin] = [f"{k} = {v}" for k, v in value.items()]
+        return result
+
+    @property
+    def package_dir(self) -> str:
+        """A directory that will be used to looking for packages."""
+        if "package-dir" in self._metadata:
+            return self._metadata["package-dir"]
+        elif self.filepath.parent.joinpath("src").is_dir():
+            return "src"
+        return ""
+
+    def convert_package_paths(self) -> Dict[str, Union[List, Dict]]:
+        """Return a {package_dir, packages, package_data, exclude_package_data} dict."""
+        packages = []
+        py_modules = []
+        package_data = {"": ["*"]}
+        exclude_package_data = {}
+
+        with cd(self.filepath.parent.as_posix()):
+            src_dir = Path(self.package_dir or ".")
+            if not self.includes:
+                packages = list(
+                    find_packages_iter(
+                        self.package_dir or ".",
+                        exclude=["tests", "tests.*"],
+                        src=src_dir,
+                    )
+                )
+                if not packages:
+                    py_modules = [path.name[:-3] for path in src_dir.glob("*.py")]
+            else:
+                packages_set = set()
+                includes = self.includes
+                for include in includes[:]:
+                    if include.replace("\\", "/").endswith("/*"):
+                        include = include[:-2]
+                    if "*" not in include and os.path.isdir(include):
+                        dir_name = include.rstrip("/\\")
+                        temp = list(
+                            find_packages_iter(dir_name, src=self.package_dir or ".")
+                        )
+                        if os.path.isfile(os.path.join(dir_name, "__init__.py")):
+                            temp.insert(0, dir_name)
+                        packages_set.update(temp)
+                        includes.remove(include)
+                packages[:] = list(packages_set)
+                for include in includes:
+                    for path in glob.glob(include):
+                        if "/" not in path.lstrip("./") and path.endswith(".py"):
+                            # Only include top level py modules
+                            py_modules.append(path.lstrip("./")[:-3])
+                    if include.endswith(".py"):
+                        continue
+                    for package in packages:
+                        relpath = os.path.relpath(include, package)
+                        if not relpath.startswith(".."):
+                            package_data.setdefault(package, []).append(relpath)
+                for exclude in self.excludes or []:
+                    for package in packages:
+                        relpath = os.path.relpath(exclude, package)
+                        if not relpath.startswith(".."):
+                            exclude_package_data.setdefault(package, []).append(relpath)
+            if packages and py_modules:
+                raise ProjectError(
+                    "Can't specify packages and py_modules at the same time."
+                )
+        return {
+            "package_dir": {"": self.package_dir} if self.package_dir else {},
+            "packages": packages,
+            "py_modules": py_modules,
+            "package_data": package_data,
+            "exclude_package_data": exclude_package_data,
+        }
