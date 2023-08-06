63d3eb3182378a27a607a274fc4d854398e77027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:01 +0000

    Add package jbird/0.0/jbird-0.0.tar.gz

---
+++ packages/jbird/jbird-0.0.tar.gz/jbird-0.0/jbird/Jbird.py
@@ -0,0 +1,4 @@
+class Jbird:
+    
+    def __init__(self):
+        pass
---
+++ packages/jbird/jbird-0.0.tar.gz/jbird-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jbird',
+      version='0.0',
+      description='JSON NoSQL database',
+      packages=['jbird'],
+      author_email='notdepot@gmail.com',
+      zip_safe=False)
