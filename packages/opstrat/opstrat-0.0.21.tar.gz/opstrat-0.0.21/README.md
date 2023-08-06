0631c79389d990e6abbff807a2a9fcd9f7792345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:33 +0000

    Add package opstrat/0.0.21/opstrat-0.0.21.tar.gz

---
+++ packages/opstrat/opstrat-0.0.21.tar.gz/opstrat-0.0.21/README.md
@@ -0,0 +1,31 @@
+# opstrat
+Python library for visualizing options.
+
+## Latest Version 
+0.0.21
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install opstrat.
+
+```bash
+pip install opstrat
+```
+
+## Usage
+
+```python
+import opstrat
+
+opstrat.basic.option_plotter() # plots a basic option payoff diagram
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Content License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+
---
+++ packages/opstrat/opstrat-0.0.21.tar.gz/opstrat-0.0.21/setup.py
@@ -0,0 +1,83 @@
+from codecs import open
+import os
+from setuptools import setup, find_packages
+#import pypandoc
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+
+'''  
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+'''
+
+'''
+try:
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r","") # Do not forget this line
+except OSError:
+    print("Pandoc not found. Long_description conversion failure.")
+    import io
+    # pandoc is not installed, fallback to using raw contents
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+'''
+        
+'''
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+'''
+
+VERSION = '0.0.21'
+DESCRIPTION = 'Option stategy visualizer'
+#LONG_DESCRIPTION = DESCRIPTION
+URL = 'https://github.com/abhijith-git/opstrat'
+
+# Setting up
+setup(
+    name="opstrat",
+    version=VERSION,
+    author="Abhijith Chandradas",
+    author_email="<abhijith.chandradas@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type='text/markdown',
+    long_description=README,
+    url=URL,
+    license='MIT',
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requires=['matplotlib', 
+                      'pandas', 
+                      'numpy',
+                      'seaborn',
+                      'yfinance'],
+    keywords=['python', 
+              'options', 
+              'finance', 
+              'opstrat', 
+              'data visualization', 
+              'stock market'],
+    classifiers=[
+            "Development Status :: 1 - Planning",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Education",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Financial and Insurance Industry",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Topic :: Scientific/Engineering :: Visualization",
+            'Topic :: Scientific/Engineering :: Information Analysis',
+            "Topic :: Office/Business :: Financial :: Investment",
+            "Topic :: Office/Business :: Financial",
+            "Programming Language :: Python :: 3"]
+)
+
+#Display README.md in PYPI
+#https://stackoverflow.com/questions/26737222/how-to-make-pypi-description-markdown-work
+
