24b971aed6bd308aa9b4889a208e50a8bd2d2f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:19 +0000

    Add package eGUI/0.0.2/eGUI-0.0.2.tar.gz

---
+++ packages/eGUI/eGUI-0.0.2.tar.gz/eGUI-0.0.2/eGUI/interface/登录.py
@@ -0,0 +1,40 @@
+import PySimpleGUI as Sg
+
+
+def 登录界面(窗口标题: str, 用户名_密码_字典: dict, 主题: str = "Python"):
+    """简单登录界面
+
+    :param 窗口标题: 窗口标题名称
+    :param 用户名_密码_字典: 查询字典
+    :param 主题: 默认使用Python主题
+    :return: 返回登录的用户名和密码
+    """
+    Sg.theme(主题)
+    login_layout = [
+        [Sg.Frame(layout=[
+            [Sg.Text('账户'), Sg.Input(key="username")],
+            [Sg.Text('密码'), Sg.Input(key="password", password_char="*")],
+            [Sg.Button("登录")]
+        ], title="登录账户")],
+    ]
+    login_window = Sg.Window(窗口标题, login_layout)
+    while True:
+        event, values = login_window.read()
+        if event == Sg.WIN_CLOSED:
+            break
+        elif event == "登录":
+            for i in 用户名_密码_字典:
+                user = values['username']
+                password = values['password']
+                if user == i and password == 用户名_密码_字典[i]:
+                    Sg.popup('登录成功!')
+                    return user, password
+            else:
+                Sg.popup('用户名或登录失败!', text_color='red')
+                login_window['password'].update("")
+
+
+if __name__ == "__main__":
+    用户密码字典 = {"123": "456", "789": "654"}
+    返回结果 = 登录界面("我的登录", 用户密码字典)
+    print(返回结果)
