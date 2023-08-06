02c2fb33c9db35d17c525afdc2eeced053613278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:44 +0000

    Add package nxpprog/0.1.4/nxpprog-0.1.4.tar.gz

---
+++ packages/nxpprog/nxpprog-0.1.4.tar.gz/nxpprog-0.1.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name='nxpprog',
+    version='0.1.4',
+    description='Programmer for NXP arm processors using ISP protocol',
+    long_description_content_type='text/markdown',
+    long_description=README,
+    license='MIT',
+    packages=setuptools.find_packages(),
+    author='SJSU-Dev2 Organization',
+    author_email='kammcecorp@gmail.com',
+    keywords=['NXP', 'SJSU', 'SJSU-Dev2'],
+    url='https://github.com/SJSU-Dev2/nxpprog',
+    download_url='https://pypi.org/project/nxpprog/'
+)
+
+install_requires = [
+    'intelhex',
+    'pyserial'
+]
+
+if __name__ == '__main__':
+    setuptools.setup(**setup_args, install_requires=install_requires)
