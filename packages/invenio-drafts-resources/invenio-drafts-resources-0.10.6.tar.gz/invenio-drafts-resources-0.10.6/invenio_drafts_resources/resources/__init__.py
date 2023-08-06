f5a5bfbe7244f5a0ea81a0a82b3b2b3c307a920f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:12 +0000

    Add package invenio-drafts-resources/0.10.6/invenio-drafts-resources-0.10.6.tar.gz

---
+++ packages/invenio-drafts-resources/invenio-drafts-resources-0.10.6.tar.gz/invenio-drafts-resources-0.10.6/invenio_drafts_resources/resources/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Drafts Resources module to create REST APIs."""
+
+from .drafts import DraftActionResource, DraftActionResourceConfig, \
+    DraftLinksSchema, DraftResource, DraftResourceConfig
+from .files import DraftFileActionResource, DraftFileActionResourceConfig, \
+    DraftFileResource, DraftFileResourceConfig, RecordFileActionResource, \
+    RecordFileActionResourceConfig, RecordFileResource, \
+    RecordFileResourceConfig
+from .records import RecordResource, RecordResourceConfig, \
+    RecordVersionsResource, RecordVersionsResourceConfig
+from .userrecords import UserRecordsResource
+from .userrecords_config import UserRecordsResourceConfig
+
+__all__ = (
+    "DraftFileResourceConfig",
+    "DraftFileResource",
+    "DraftFileActionResourceConfig",
+    "DraftFileActionResource",
+    "RecordFileResourceConfig",
+    "RecordFileResource",
+    "RecordFileActionResourceConfig",
+    "RecordFileActionResource",
+    "DraftActionResource",
+    "DraftActionResourceConfig",
+    "DraftLinksSchema",
+    "DraftResource",
+    "DraftResourceConfig",
+    "RecordResource",
+    "RecordResourceConfig",
+    "RecordVersionsResource",
+    "RecordVersionsResourceConfig",
+    "UserRecordsResource",
+    "UserRecordsResourceConfig",
+)
