7c994d68b675b02a3eeeeff5f21c63cec0af4103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:56 +0000

    Add package clicktypes/0.0.5/clicktypes-0.0.5-py2.py3-none-any.whl

---
+++ packages/clicktypes/clicktypes-0.0.5-py2.py3-none-any.whl/clicktypes/network/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Import relevant classes for networking
+
+## IP Networks
+from .network import (
+    IPNetworkParam,
+    IPv4NetworkParam,
+    IPv6NetworkParam,
+    IPNetworkStringParam,
+    IPv4NetworkStringParam,
+    IPv6NetworkStringParam,
+)
+
+## IP Addresses
+from .address import (
+    IPAddressParam,
+    IPv4AddressParam,
+    IPv6AddressParam,
+    IPStringParam,
+    IPv4StringParam,
+    IPv6StringParam,
+)
