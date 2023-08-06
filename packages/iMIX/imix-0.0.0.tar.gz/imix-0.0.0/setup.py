625aa1f396c7a8436f18818822e91a2f12b617f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:48 +0000

    Add package iMIX/0.0.0/imix-0.0.0.tar.gz

---
+++ packages/iMIX/imix-0.0.0.tar.gz/imix-0.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+  name='imix',
+  version='0.0.0',
+  author='HSSLab',
+  author_email='hsslab.inspur@gmail.com',
+  description='A framework for Multimodal Intelligence',
+  long_description='',
+  long_description_content_type='text/markdown',
+  url='https://github.com/inspur-hsslab/iMIX.git',
+  packages=setuptools.find_packages(),
+  classifiers=[
+      'Programming Language :: Python :: 3',
+      'Operating System :: OS Independent',
+  ],
+)
