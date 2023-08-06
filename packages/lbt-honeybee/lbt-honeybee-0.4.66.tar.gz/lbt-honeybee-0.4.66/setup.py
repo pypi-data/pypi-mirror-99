1db6883b076eb4f560b2b1362e400646734d52e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:08 +0000

    Add package lbt-honeybee/0.4.66/lbt-honeybee-0.4.66.tar.gz

---
+++ packages/lbt-honeybee/lbt-honeybee-0.4.66.tar.gz/lbt-honeybee-0.4.66/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="lbt-honeybee",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Collection of all Honeybee core Python libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/lbt-honeybee",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent"
+    ],
+    license="AGPL-3.0"
+)
