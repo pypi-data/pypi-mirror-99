6af91f0fe409da0fa6f8607adadd76a3cf88dbb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:04 +0000

    Add package pyramid-oereb/1.9.1.dev20210324/pyramid_oereb-1.9.1.dev20210324.tar.gz

---
+++ packages/pyramid-oereb/pyramid_oereb-1.9.1.dev20210324.tar.gz/pyramid_oereb-1.9.1.dev20210324/pyramid_oereb/standard/drop_tables.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import optparse
+from pyramid_oereb.standard import drop_tables_from_standard_configuration
+
+
+def drop_standard_tables():
+    parser = optparse.OptionParser(
+        usage='usage: %prog [options]',
+        description='Create all content for the standard database'
+    )
+    parser.add_option(
+        '-c', '--configuration',
+        dest='configuration',
+        metavar='YAML',
+        type='string',
+        help='The absolute path to the configuration yaml file (standard is: pyramid_oereb.yml).'
+    )
+    parser.add_option(
+        '-s', '--section',
+        dest='section',
+        metavar='SECTION',
+        type='string',
+        default='pyramid_oereb',
+        help='The section which contains configruation (default is: pyramid_oereb).'
+    )
+    options, args = parser.parse_args()
+    if not options.configuration:
+        parser.error('No configuration file set.')
+    drop_tables_from_standard_configuration(
+        configuration_yaml_path=options.configuration,
+        section=options.section
+    )
