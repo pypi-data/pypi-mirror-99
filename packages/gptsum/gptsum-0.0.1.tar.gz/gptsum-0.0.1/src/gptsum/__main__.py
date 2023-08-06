b12e4f3e9f72687df6be9c41dfc359b2baec0f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package gptsum/0.0.1/gptsum-0.0.1.tar.gz

---
+++ packages/gptsum/gptsum-0.0.1.tar.gz/gptsum-0.0.1/src/gptsum/__main__.py
@@ -0,0 +1,9 @@
+"""Main entrypoint for the gptsum package.
+
+Simply invokes :func:`gptsum.cli.main`.
+"""
+
+import gptsum.cli
+
+if __name__ == "__main__":
+    gptsum.cli.main()  # pragma: no cover
