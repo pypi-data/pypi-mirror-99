e36d61bd44bca769ea8d471d274a0b5583e90b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:59 +0000

    Add package whatsappy-py/0.1.8.2/whatsappy-py-0.1.8.2.tar.gz

---
+++ packages/whatsappy-py/whatsappy-py-0.1.8.2.tar.gz/whatsappy-py-0.1.8.2/whatsappy/__init__.py
@@ -0,0 +1,39 @@
+from .cmds.chat import select_chat, last_message, new_message, send, reply, send_file
+from .cmds.group import change_group_description, change_group_name, leave_group
+from .cmds.actions import add_to_group, remove_from_group, make_group_admin
+from .cmds.get import get_pinned_chats, get_recent_chats
+from .cmds.login import get_qrcode, login, close
+
+class Whatsapp:
+
+    login                       = login
+    get_qrcode                  = get_qrcode
+    close                       = close
+    add_to_group                = add_to_group
+    remove_from_group           = remove_from_group
+    make_group_admin            = make_group_admin
+    select_chat                 = select_chat
+    last_message                = last_message
+    new_message                 = new_message
+    send                        = send
+    reply                       = reply
+    send_file                   = send_file
+    get_pinned_chats            = get_pinned_chats
+    get_recent_chats            = get_recent_chats
+    change_group_description    = change_group_description
+    change_group_name           = change_group_name
+    leave_group                 = leave_group
+
+    # TODO: get group invite link
+
+    # TODO: invite by number
+
+    # TODO: private answer
+
+    # TODO: Get group info (maybe turn it into a class)
+
+    # TODO: Decent error message
+
+    # TODO: Create group
+
+whatsapp = Whatsapp()
