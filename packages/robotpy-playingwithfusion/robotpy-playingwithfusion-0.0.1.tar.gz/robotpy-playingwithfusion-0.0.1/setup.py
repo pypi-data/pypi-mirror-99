48a1ba095362f748af33fcec82b2002bcf698d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:28 +0000

    Add package robotpy-playingwithfusion/0.0.1/robotpy-playingwithfusion-0.0.1.tar.gz

---
+++ packages/robotpy-playingwithfusion/robotpy-playingwithfusion-0.0.1.tar.gz/robotpy-playingwithfusion-0.0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="robotpy-playingwithfusion",
+    version="0.0.1",
+    description="Placeholder",
+    author="Dustin Spicuzza",
+    author_email="robotpy@googlegroups.com",
+    license="BSD-3-Clause",
+    packages=["px.b"],
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development",
+    ],
+)
