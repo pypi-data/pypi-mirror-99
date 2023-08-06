f82d843494934f9d52774bf74f92f51f9643d50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:03 +0000

    Add package condastats/0.1.5/condastats-0.1.5.tar.gz

---
+++ packages/condastats/condastats-0.1.5.tar.gz/condastats-0.1.5/condastats/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for condastats."""
+
+__author__ = """Sophia Man Yang"""
+__version__ = '0.1.2'
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ packages/condastats/condastats-0.1.5.tar.gz/condastats-0.1.5/setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import versioneer
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ 
+    'numpy>=1.16.5',
+    'dask[dataframe]',
+    'fastparquet',
+    's3fs'
+]
+
+import os
+if not os.getenv('READTHEDOCS'):
+    requirements.append('python-snappy')
+
+setup_requirements = [ ]
+
+test_requirements = [
+    'pytest'
+ ]
+
+setup(
+    author="Sophia Man Yang",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Conda package stats CLI",
+    entry_points={
+        'console_scripts': [
+            'condastats=condastats.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='condastats',
+    name='condastats',
+    packages=find_packages(include=['condastats']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/sophiamyang/condastats',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    zip_safe=False,
+)
