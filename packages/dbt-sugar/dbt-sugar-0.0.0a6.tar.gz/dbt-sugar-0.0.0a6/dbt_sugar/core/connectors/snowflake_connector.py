74cd9da08bbacde770f6aad9702539620826e112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:39 +0000

    Add package dbt-sugar/0.0.0a6/dbt-sugar-0.0.0a6.tar.gz

---
+++ packages/dbt-sugar/dbt-sugar-0.0.0a6.tar.gz/dbt-sugar-0.0.0a6/dbt_sugar/core/connectors/snowflake_connector.py
@@ -0,0 +1,39 @@
+"""
+Module Snowflake connector.
+
+Module dependent of the base connector.
+"""
+from typing import Dict
+
+import sqlalchemy
+from snowflake.sqlalchemy import URL
+
+from dbt_sugar.core.connectors.base import BaseConnector
+
+
+class SnowflakeConnector(BaseConnector):
+    """
+    Connection class for Snowflake.
+
+    Child class of base connector.
+    """
+
+    def __init__(
+        self,
+        connection_params: Dict[str, str],
+    ) -> None:
+        """
+        Creates the URL and the Engine for future connections.
+
+        Args:
+            connection_params (Dict[str, str]): Dict containing database connection
+                parameters and credentials.
+        """
+        self.connection_url = URL(
+            drivername="postgresql+psycopg2",
+            user=connection_params.get("user", str()),
+            password=connection_params.get("password", str()),
+            database=connection_params.get("database", str()),
+            account=connection_params.get("account", str()),
+        )
+        self.engine = sqlalchemy.create_engine(self.connection_url)
---
+++ packages/dbt-sugar/dbt-sugar-0.0.0a6.tar.gz/dbt-sugar-0.0.0a6/dbt_sugar/core/flags.py
@@ -0,0 +1,66 @@
+"""Flags module containing the FlagParser "Factory"."""
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import List
+
+
+class FlagParser:
+    """Sets flags from defaults or by parsing CLI arguments.
+
+    In order to not have to always parse args when testing etc. We set the defaults explicitly here.
+    This is a bit strict and not very DRY but it saves from surprises which for now is good.
+    """
+
+    def __init__(self, cli_parser: ArgumentParser) -> None:
+        """Constructor for FlagParser.
+
+        Holds explicit defaults and consumes parsed flags if asked for it.
+
+        Args:
+            cli_parser (ArgumentParser): CLI parser.
+        """
+        self.cli_parser = cli_parser
+        self.model: str = "test_model"
+        self.schema: str = ""
+        self.log_level: str = "info"
+        self.syrup: str = str()
+        self.config_path: Path = Path(str())
+        self.profiles_dir: Path = Path(str())
+        self.is_dry_run: bool = False
+        self.ask_for_tests: bool = True
+        self.ask_for_tags: bool = True
+        self.target: str = str()
+        self.verbose: bool = False
+
+    def consume_cli_arguments(self, test_cli_args: List[str] = list()) -> None:
+        if test_cli_args:
+            _cli_args = test_cli_args
+        else:
+            _cli_args = sys.argv[1:]
+        self.args = self.cli_parser.parse_args(_cli_args)
+
+        self.task = self.args.command
+
+        # base flags that need to be set no matter what
+        if self.args:
+            self.log_level = self.args.log_level
+            self.verbose = self.args.verbose
+            self.syrup = self.args.syrup
+            if self.args.profiles_dir:
+                self.profiles_dir = Path(self.args.profiles_dir).expanduser()
+            if self.args.config_path:
+                self.config_path = Path(self.args.config_path).expanduser()
+
+        # task specific args consumption
+        if self.task == "doc":
+            self.model = self.args.model
+            self.schema = self.args.schema
+            self.is_dry_run = self.args.dry_run
+            self.target = self.args.target
+            # we reverse the flag so that we don't have double negatives later in the code
+            self.ask_for_tests = self.args.ask_for_tests
+            self.ask_for_tags = self.args.ask_for_tags
+
+        if self.task == "audit":
+            self.model = self.args.model
