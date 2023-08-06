7da2cf071997f31f12f0949090b95ef0628e41da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package odoo13-addon-shopinvader-delivery-carrier/13.0.3.0.0/odoo13_addon_shopinvader_delivery_carrier-13.0.3.0.0-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-delivery-carrier/odoo13_addon_shopinvader_delivery_carrier-13.0.3.0.0-py3-none-any.whl/odoo/addons/shopinvader_delivery_carrier/models/__init__.py
@@ -0,0 +1,6 @@
+from . import backend
+from . import delivery_carrier
+from . import sale
+from . import shopinvader_notification
+from . import stock_picking
+from . import shopinvader_backend
---
+++ packages/odoo13-addon-shopinvader-delivery-carrier/odoo13_addon_shopinvader_delivery_carrier-13.0.3.0.0-py3-none-any.whl/odoo/addons/shopinvader_delivery_carrier/tests/__init__.py
@@ -0,0 +1,5 @@
+from . import test_carrier
+from . import test_notification
+from . import test_delivery_carrier
+from . import test_delivery_service
+from . import test_sale_service
