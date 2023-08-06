23744ed770ee5b8017546d83784198ce2191da5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:33 +0000

    Add package ssh-crypt/1.1/ssh-crypt-1.1.tar.gz

---
+++ packages/ssh-crypt/ssh-crypt-1.1.tar.gz/ssh-crypt-1.1/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+
+def get_requirements():
+    basedir = os.path.dirname(__file__)
+    try:
+        with open(os.path.join(basedir, 'requirements.txt')) as f:
+            return f.readlines()
+    except FileNotFoundError:
+        raise RuntimeError('No requirements info found.')
+
+
+setup(
+    name='ssh-crypt',
+    version='1.1',
+    license='BSD',
+    author='Maxim Nikitenko',
+    author_email='iam@sets88.com',
+    packages=find_packages(),
+    description='ssh-crypt is a tool to encrypt/decrypt data using your ssh key from ssh-agent',\
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=get_requirements(),
+    python_requires='>=3.5',
+    entry_points={
+        'console_scripts': [
+            'ssh-crypt = ssh_crypt:main',
+        ]
+    }
+)
