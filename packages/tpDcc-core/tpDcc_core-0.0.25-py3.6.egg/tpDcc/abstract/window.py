82ca86b18868f8fd163aab267fbfc83ba095cca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:10 +0000

    Add package tpDcc-core/0.0.25/tpDcc_core-0.0.25-py3.6.egg

---
+++ packages/tpDcc-core/tpDcc_core-0.0.25-py3.6.egg/tpDcc/abstract/window.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains DCC window abstract implementation
+"""
+
+from __future__ import print_function, division, absolute_import
+
+from tpDcc.libs.python import decorators
+
+
+class AbstractWindow(object):
+    pass
