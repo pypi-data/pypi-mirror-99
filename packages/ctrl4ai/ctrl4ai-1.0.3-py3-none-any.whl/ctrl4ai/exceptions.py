fd84611497d574977cf5ff6feb67dcc403917d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:22 +0000

    Add package ctrl4ai/1.0.3/ctrl4ai-1.0.3-py3-none-any.whl

---
+++ packages/ctrl4ai/ctrl4ai-1.0.3-py3-none-any.whl/ctrl4ai/exceptions.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 23:43:31 2020
+
+@author: Shaji,Charu,Selva
+"""
+
+class ParameterError(Exception):
+  """
+  User Defined Exception
+  Thrown because the argumrnt is incorrect
+  """
+  pass
+
