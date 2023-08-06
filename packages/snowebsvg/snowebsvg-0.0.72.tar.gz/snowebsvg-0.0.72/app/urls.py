fd91dbccd03d78d4bae0de6fcd43c484b1136098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:30 +0000

    Add package snowebsvg/0.0.72/snowebsvg-0.0.72.tar.gz

---
+++ packages/snowebsvg/snowebsvg-0.0.72.tar.gz/snowebsvg-0.0.72/app/urls.py
@@ -0,0 +1,35 @@
+from django.contrib.sitemaps.views import sitemap
+from django.urls import path
+
+from app.views import \
+    CollectionListView, \
+    GroupSvgListView, \
+    SvgListView, \
+    SvgSearchView, \
+    SvgSettingsView
+
+from app.sitemaps import \
+    StaticSitemap, \
+    CollectionSitemap, \
+    GroupSvgSitemap, \
+    SvgSitemap
+
+app_name = 'app'
+
+sitemaps = {
+    'static': StaticSitemap,
+    'collection': CollectionSitemap,
+    'svg': SvgSitemap,
+    'group_svg': GroupSvgSitemap,
+}
+
+urlpatterns = [
+    path('', CollectionListView.as_view(), name='collection'),
+    path('collection/<collection_key>/', GroupSvgListView.as_view(), name='group'),
+    path('svg/<collection_key>/<group_key>/<svg_key>/', SvgListView.as_view(), name='svg'),
+    path('search/', SvgSearchView.as_view(), name='svg_search'),
+    path('search/<key>/', SvgSearchView.as_view(), name='svg_search'),
+    path('settings/', SvgSettingsView.as_view(), name='svg_settings'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
+]
