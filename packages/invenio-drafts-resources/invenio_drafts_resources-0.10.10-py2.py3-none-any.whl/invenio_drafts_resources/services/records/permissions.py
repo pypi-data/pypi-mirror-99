0dfa37fdc42b18c231aeb2cc0ed50d94400ac9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:30 +0000

    Add package invenio-drafts-resources/0.10.10/invenio_drafts_resources-0.10.10-py2.py3-none-any.whl

---
+++ packages/invenio-drafts-resources/invenio_drafts_resources-0.10.10-py2.py3-none-any.whl/invenio_drafts_resources/services/records/permissions.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Drafts permissions."""
+
+from invenio_records_permissions.generators import AnyUser
+from invenio_records_permissions.policies.records import RecordPermissionPolicy
+
+
+class RecordDraftPermissionPolicy(RecordPermissionPolicy):
+    """Custom permission policy."""
+
+    # FIXME: Revist this along the development
+    # Default create should be "authenticated"?
+    # TODO: Subclass records-resources policy and add *_draft actions
+    can_create = [AnyUser()]
+    can_new_version = [AnyUser()]
+    can_edit = [AnyUser()]
+    can_publish = [AnyUser()]
+    can_read_draft = [AnyUser()]
+    can_update_draft = [AnyUser()]
+    can_delete_draft = [AnyUser()]
