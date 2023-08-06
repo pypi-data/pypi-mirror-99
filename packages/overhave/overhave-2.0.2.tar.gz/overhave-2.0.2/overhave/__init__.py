a2ec0dff256e182137388e7f545d463d41b3ce53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:02 +0000

    Add package overhave/2.0.2/overhave-2.0.2.tar.gz

---
+++ packages/overhave/overhave-2.0.2.tar.gz/overhave-2.0.2/overhave/__init__.py
@@ -0,0 +1,44 @@
+# flake8: noqa
+from overhave.admin import OverhaveAdminApp, overhave_app
+from overhave.base_settings import DataBaseSettings as OverhaveDBSettings
+from overhave.base_settings import LoggingSettings as OverhaveLoggingSettings
+from overhave.cli import group, set_config_to_context
+from overhave.entities import (
+    OverhaveDescriptionManagerSettings,
+    OverhaveEmulationSettings,
+    OverhaveFileSettings,
+    OverhaveLanguageSettings,
+    OverhaveRedisSettings,
+    OverhaveScenarioCompilerSettings,
+    OverhaveStepContextSettings,
+    StepPrefixesModel,
+    TranslitPack,
+)
+from overhave.entities.authorization.settings import (
+    AuthorizationStrategy,
+    OverhaveAdminSettings,
+    OverhaveAuthorizationSettings,
+    OverhaveLdapClientSettings,
+)
+from overhave.factory import ConsumerFactory as OverhaveConsumerFactory
+from overhave.factory import IAdminFactory as OverhaveAdminFactory
+from overhave.factory import IEmulationFactory as OverhaveEmulationFactory
+from overhave.factory import IPublicationFactory as OverhavePublicationFactory
+from overhave.factory import ITestExecutionFactory as OverhaveTestExecutionFactory
+from overhave.factory import (
+    OverhaveAdminContext,
+    OverhaveEmulationContext,
+    OverhavePublicationContext,
+    OverhaveTestExecutionContext,
+)
+from overhave.factory import get_admin_factory as overhave_admin_factory
+from overhave.factory import get_emulation_factory as overhave_emulation_factory
+from overhave.factory import get_publication_factory as overhave_publication_factory
+from overhave.factory import get_test_execution_factory as overhave_test_execution_factory
+from overhave.publication import OverhaveStashPublisherSettings
+from overhave.pytest_plugin import IProxyManager as OverhaveProxyManager
+from overhave.pytest_plugin import get_description_manager
+from overhave.pytest_plugin import get_proxy_manager as overhave_proxy_manager
+from overhave.test_execution import OverhaveProjectSettings, OverhaveTestSettings
+from overhave.transport import OverhaveS3ManagerSettings, OverhaveStashClientSettings
+from overhave.transport import RedisStream as OverhaveRedisStream
---
+++ packages/overhave/overhave-2.0.2.tar.gz/overhave-2.0.2/overhave/db/users.py
@@ -0,0 +1,68 @@
+import enum
+from typing import TYPE_CHECKING, Optional, cast
+
+import sqlalchemy as sa
+import sqlalchemy_utils as su
+
+from overhave.db.base import BaseTable, PrimaryKeyMixin
+from overhave.db.types import LONG_STR_TYPE, SHORT_STR_TYPE
+
+
+class Role(str, enum.Enum):
+    """ Enum that declares user access roles. """
+
+    user = "user"
+    admin = "admin"
+
+
+class BaseUser:
+    """ Base user class. """
+
+    login: str = sa.Column(SHORT_STR_TYPE, nullable=False, unique=True)
+    password: Optional[str] = sa.Column(LONG_STR_TYPE, nullable=True)
+    role: str = sa.Column(sa.Enum(Role), nullable=False, default=Role.user)
+
+    if TYPE_CHECKING:
+        id: int
+
+    @property
+    def is_authenticated(self) -> bool:
+        return True
+
+    @property
+    def is_active(self) -> bool:
+        return True
+
+    @property
+    def is_anonymous(self) -> bool:
+        return False
+
+    def get_id(self) -> int:
+        return self.id
+
+    # Required for administrative interface
+    def __unicode__(self) -> str:
+        return cast(str, self.login)
+
+    def __str__(self) -> str:
+        return cast(str, self.login)
+
+
+@su.generic_repr("login")
+class UserRole(BaseTable, PrimaryKeyMixin, BaseUser):
+    """ User access table. """
+
+    def __init__(self, login: str, password: Optional[str], role: Role) -> None:
+        self.login = login
+        self.password = password
+        self.role = role
+
+
+@su.generic_repr("group")
+class GroupRole(BaseTable, PrimaryKeyMixin):
+    """ Group access table. """
+
+    group = sa.Column(SHORT_STR_TYPE, nullable=False, unique=True)
+
+    def __init__(self, group: str) -> None:
+        self.group = group
