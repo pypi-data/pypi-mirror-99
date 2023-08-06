15477cec4eb91d76a917383050b24597b9cd76bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:14 +0000

    Add package django-geonode-mapstore-client/2.1.2/django_geonode_mapstore_client-2.1.2-py2.py3-none-any.whl

---
+++ packages/django-geonode-mapstore-client/django_geonode_mapstore_client-2.1.2-py2.py3-none-any.whl/geonode_mapstore_client/apps.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright 2015-2018, GeoSolutions Sas.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#########################################################################
+from django.apps import AppConfig as BaseAppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+def run_setup_hooks(*args, **kwargs):
+    from geonode.urls import urlpatterns
+    from django.conf.urls import url, include
+
+    urlpatterns += [
+        url(r'^mapstore/', include('mapstore2_adapter.urls')),
+        url(r'^', include('mapstore2_adapter.geoapps.geostories.api.urls')),
+    ]
+
+
+class AppConfig(BaseAppConfig):
+
+    name = "geonode_mapstore_client"
+    label = "geonode_mapstore_client"
+
+    def ready(self):
+        run_setup_hooks()
+        super(AppConfig, self).ready()
