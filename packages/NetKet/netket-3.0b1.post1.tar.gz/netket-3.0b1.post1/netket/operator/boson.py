97461537315c4a0587a5f25970027aec3130799a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:34 +0000

    Add package NetKet/3.0b1.post1/netket-3.0b1.post1.tar.gz

---
+++ packages/NetKet/netket-3.0b1.post1.tar.gz/netket-3.0b1.post1/netket/operator/boson.py
@@ -0,0 +1,132 @@
+# Copyright 2021 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from numpy.typing import DTypeLike
+
+from netket.hilbert import AbstractHilbert
+
+from ._local_operator import LocalOperator as _LocalOperator
+
+
+def destroy(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the boson destruction operator :math:`\\hat{a}` acting on the `site`-th of the
+     Hilbert space `hilbert`.
+
+    If `hilbert` is a non-Bosonic space of local dimension M, it is considered
+    as a bosonic space of local dimension M.
+
+    Args:
+        hilbert: The hilbert space
+        site: the site on which this operator acts
+
+    Returns:
+        The resulting Local Operator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+
+    D = np.array([np.sqrt(m) for m in np.arange(1, N)])
+    mat = np.diag(D, 1)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+def create(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the boson creation operator :math:`\\hat{a}^\\dagger` acting on the `site`-th of the
+     Hilbert space `hilbert`.
+
+    If `hilbert` is a non-Bosonic space of local dimension M, it is considered
+    as a bosonic space of local dimension M.
+
+    Args:
+        hilbert: The hilbert space
+        site: the site on which this operator acts
+
+    Returns:
+        The resulting Local Operator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+
+    D = np.array([np.sqrt(m) for m in np.arange(1, N)])
+    mat = np.diag(D, -1)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+def number(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the number operator :math:`\\hat{a}^\\dagger\\hat{a}`  acting on the `site`-th of the
+    Hilbert space `hilbert`.
+
+    If `hilbert` is a non-Bosonic space of local dimension M, it is considered
+    as a bosonic space of local dimension M.
+
+    Args:
+        hilbert: The hilbert space
+        site: the site on which this operator acts
+
+    Returns:
+        The resulting Local Operator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+
+    D = np.array([m for m in np.arange(0, N)])
+    mat = np.diag(D, 0)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+def proj(
+    hilbert: AbstractHilbert, site: int, n: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the projector operator :math:`|n\\rangle\\langle n |` acting on the `site`-th of the
+    Hilbert space `hilbert` and collapsing on the state with `n` bosons.
+
+    If `hilbert` is a non-Bosonic space of local dimension M, it is considered
+    as a bosonic space of local dimension M.
+
+    Args:
+        hilbert: The hilbert space
+        site: the site on which this operator acts
+        n: the state on which to project
+
+    Returns:
+        the resulting operator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+
+    if n >= N:
+        raise ValueError("Cannot project on a state above the cutoff.")
+
+    D = np.array([0 for m in np.arange(0, N)])
+    D[n] = 1
+    mat = np.diag(D, 0)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+# clean up the module
+del AbstractHilbert
---
+++ packages/NetKet/netket-3.0b1.post1.tar.gz/netket-3.0b1.post1/netket/operator/spin.py
@@ -0,0 +1,144 @@
+# Copyright 2021 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from netket.hilbert import AbstractHilbert
+from numpy.typing import DTypeLike
+
+from ._local_operator import LocalOperator as _LocalOperator
+
+
+def sigmax(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the :math:`\\sigma^x` operator acting on the `site`-th of the Hilbert
+    space `hilbert`.
+
+    If `hilbert` is a non-Spin space of local dimension M, it is considered
+    as a (M-1)/2 - spin space.
+
+    :param hilbert: The hilbert space
+    :param site: the site on which this operator acts
+    :return: a nk.operator.LocalOperator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+    S = (N - 1) / 2
+
+    D = [np.sqrt((S + 1) * 2 * a - a * (a + 1)) for a in np.arange(1, N)]
+    mat = np.diag(D, 1) + np.diag(D, -1)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+def sigmay(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = complex
+) -> _LocalOperator:
+    """
+    Builds the :math:`\\sigma^y` operator acting on the `site`-th of the Hilbert
+    space `hilbert`.
+
+    If `hilbert` is a non-Spin space of local dimension M, it is considered
+    as a (M-1)/2 - spin space.
+
+    :param hilbert: The hilbert space
+    :param site: the site on which this operator acts
+    :return: a nk.operator.LocalOperator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+    S = (N - 1) / 2
+
+    D = np.array([1j * np.sqrt((S + 1) * 2 * a - a * (a + 1)) for a in np.arange(1, N)])
+    mat = np.diag(D, -1) + np.diag(-D, 1)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+def sigmaz(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the :math:`\\sigma^z` operator acting on the `site`-th of the Hilbert
+    space `hilbert`.
+
+    If `hilbert` is a non-Spin space of local dimension M, it is considered
+    as a (M-1)/2 - spin space.
+
+    :param hilbert: The hilbert space
+    :param site: the site on which this operator acts
+    :return: a nk.operator.LocalOperator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+    S = (N - 1) / 2
+
+    D = np.array([2 * m for m in np.arange(S, -(S + 1), -1)])
+    mat = np.diag(D, 0)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+def sigmam(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the :math:`\\sigma^{-} = \\sigma^x - i \\sigma^y` operator acting on the
+    `site`-th of the Hilbert space `hilbert`.
+
+    If `hilbert` is a non-Spin space of local dimension M, it is considered
+    as a (M-1)/2 - spin space.
+
+    :param hilbert: The hilbert space
+    :param site: the site on which this operator acts
+    :return: a nk.operator.LocalOperator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+    S = (N - 1) / 2
+
+    S2 = (S + 1) * S
+    D = np.array([np.sqrt(S2 - m * (m - 1)) for m in np.arange(S, -S, -1)])
+    mat = np.diag(D, -1)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+def sigmap(
+    hilbert: AbstractHilbert, site: int, dtype: DTypeLike = float
+) -> _LocalOperator:
+    """
+    Builds the :math:`\\sigma^{+} = \\sigma^x + i \\sigma^y` operator acting on the
+    `site`-th of the Hilbert space `hilbert`.
+
+    If `hilbert` is a non-Spin space of local dimension M, it is considered
+    as a (M-1)/2 - spin space.
+
+    :param hilbert: The hilbert space
+    :param site: the site on which this operator acts
+    :return: a nk.operator.LocalOperator
+    """
+    import numpy as np
+
+    N = hilbert.size_at_index(site)
+    S = (N - 1) / 2
+
+    S2 = (S + 1) * S
+    D = np.array([np.sqrt(S2 - m * (m + 1)) for m in np.arange(S - 1, -(S + 1), -1)])
+    mat = np.diag(D, 1)
+    return _LocalOperator(hilbert, mat, [site], dtype=dtype)
+
+
+# clean up the module
+del AbstractHilbert
