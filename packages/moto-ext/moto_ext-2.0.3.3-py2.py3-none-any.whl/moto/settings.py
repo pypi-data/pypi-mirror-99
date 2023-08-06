143ae0e38e8813f8bf53a4e7aa2b50c3984f569a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:24 +0000

    Add package moto-ext/2.0.3.3/moto_ext-2.0.3.3-py2.py3-none-any.whl

---
+++ packages/moto-ext/moto_ext-2.0.3.3-py2.py3-none-any.whl/moto/settings.py
@@ -0,0 +1,16 @@
+import os
+
+TEST_SERVER_MODE = os.environ.get("TEST_SERVER_MODE", "0").lower() == "true"
+INITIAL_NO_AUTH_ACTION_COUNT = float(
+    os.environ.get("INITIAL_NO_AUTH_ACTION_COUNT", float("inf"))
+)
+DEFAULT_CONTAINER_REGISTRY = os.environ.get("DEFAULT_CONTAINER_REGISTRY", "docker.io")
+
+
+def get_sf_execution_history_type():
+    """
+    Determines which execution history events `get_execution_history` returns
+    :returns: str representing the type of Step Function Execution Type events should be
+        returned. Default value is SUCCESS, currently supports (SUCCESS || FAILURE)
+    """
+    return os.environ.get("SF_EXECUTION_HISTORY_TYPE", "SUCCESS")
