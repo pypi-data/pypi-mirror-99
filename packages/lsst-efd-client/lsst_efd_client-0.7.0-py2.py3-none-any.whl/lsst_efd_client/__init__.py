6ae4d310d8a76987808299aefcc1c212f2221c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:34 +0000

    Add package lsst-efd-client/0.7.0/lsst_efd_client-0.7.0-py2.py3-none-any.whl

---
+++ packages/lsst-efd-client/lsst_efd_client-0.7.0-py2.py3-none-any.whl/lsst_efd_client/__init__.py
@@ -0,0 +1,10 @@
+"""
+Collection of EFD utilities
+"""
+__version__ = "__version__ = '0.7.0'"
+from .auth_helper import NotebookAuth
+from .efd_helper import EfdClient
+from .efd_utils import resample, rendezvous_dataframes, merge_packed_time_series
+
+__all__ = ['NotebookAuth', 'EfdClient', 'resample', 'rendezvous_dataframes',
+           'merge_packed_time_series']
