abfbd02c98f2b426dd75dd0080898fc72dd942e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:52 +0000

    Add package water-healer/0.0.35/water-healer-0.0.35.tar.gz

---
+++ packages/water-healer/water-healer-0.0.35.tar.gz/water-healer-0.0.35/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+__packagename__ = 'water-healer'
+
+setuptools.setup(
+    name = __packagename__,
+    packages = setuptools.find_packages(),
+    version = '0.0.35',
+    python_requires = '>=3.7.*',
+    description = 'Extension of Kafka Streamz to update consumer offset for successful sink',
+    author = 'huseinzol05',
+    author_email = 'husein.zol05@gmail.com',
+    url = 'https://github.com/huseinzol05/water-healer',
+    install_requires = [
+        'streamz==0.5.2',
+        'tornado',
+        'expiringdict',
+        'prometheus_client',
+        'APScheduler',
+        'confluent_kafka',
+        'networkx==2.3',
+        'graphviz',
+    ],
+    license = 'MIT',
+    classifiers = [
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
