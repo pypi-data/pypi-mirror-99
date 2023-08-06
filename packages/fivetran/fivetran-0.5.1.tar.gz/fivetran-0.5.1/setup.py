3c443c9e7bb789294984a586a2d548c95d2fcbb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package fivetran/0.5.1/fivetran-0.5.1.tar.gz

---
+++ packages/fivetran/fivetran-0.5.1.tar.gz/fivetran-0.5.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fivetran']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'fivetran',
+    'version': '0.5.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Michael Cooper',
+    'author_email': 'macoop2363@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8.5,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
