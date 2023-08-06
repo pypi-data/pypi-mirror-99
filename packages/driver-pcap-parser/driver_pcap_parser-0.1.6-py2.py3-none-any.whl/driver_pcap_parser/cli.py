11c77cd0cd91ed5563b116d499b9019e7973e1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:43 +0000

    Add package driver-pcap-parser/0.1.6/driver_pcap_parser-0.1.6-py2.py3-none-any.whl

---
+++ packages/driver-pcap-parser/driver_pcap_parser-0.1.6-py2.py3-none-any.whl/driver_pcap_parser/cli.py
@@ -0,0 +1,20 @@
+"""Console script for driver_pcap_parser."""
+import argparse
+import sys
+import socket
+from driver_pcap_parser import parse_file
+
+def parse_args():
+    """Console script for driver_pcap_parser."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filename', required=True)
+    parser.add_argument('-m', '--filter_dst_ip')
+    parser.add_argument('-L', '--logstash')
+    return parser.parse_args()
+
+def main():
+    return parse_file(parse_args())
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
