7c439f1a3c1a4a2cf2e4a0d0297483ec20b90067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:14 +0000

    Add package deepl-fastapi/0.1.0/deepl_fastapi-0.1.0-py3-none-any.whl

---
+++ packages/deepl-fastapi/deepl_fastapi-0.1.0-py3-none-any.whl/deepl_fastapi/__init__.py
@@ -0,0 +1,8 @@
+"""Init."""
+# need this to rid of loop is already running
+# get_page (runs pyppeteeer after main loop in uvicorn already started) after
+import nest_asyncio
+
+nest_asyncio.apply()
+
+__version__ = "0.1.0"
