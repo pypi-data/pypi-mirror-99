a92c1d3d6facad55a4bbb9e36d53358d0b05293f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:23 +0000

    Add package willacy-cardGames/0.2/willacy_cardGames-0.2.tar.gz

---
+++ packages/willacy-cardGames/willacy_cardGames-0.2.tar.gz/willacy_cardGames-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='willacy_cardGames',
+      version='0.2',
+      description='Card game Old Maid',
+      packages=['willacy_cardGames'],
+      author="Andrew Willacy",
+      email="andrewwillacy@gmail.com",
+      zip_safe=False)
