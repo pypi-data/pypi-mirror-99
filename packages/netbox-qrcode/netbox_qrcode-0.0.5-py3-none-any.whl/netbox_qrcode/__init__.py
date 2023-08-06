6b0237af5bc7648d7659e5aa1eddb46057eda928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:49 +0000

    Add package netbox-qrcode/0.0.5/netbox_qrcode-0.0.5-py3-none-any.whl

---
+++ packages/netbox-qrcode/netbox_qrcode-0.0.5-py3-none-any.whl/netbox_qrcode/__init__.py
@@ -0,0 +1,38 @@
+from extras.plugins import PluginConfig
+from .version import __version__
+
+
+class QRCodeConfig(PluginConfig):
+    name = 'netbox_qrcode'
+    verbose_name = 'qrcode'
+    description = 'Generate QR codes for the objects'
+    version = __version__
+    author = 'Nikolay Yuzefovich'
+    author_email = 'mgk.kolek@gmail.com'
+    required_settings = []
+    default_settings = {
+        'with_text': True,
+        'text_fields': ['name', 'serial'],
+        'font': 'TahomaBold',
+        'custom_text': None,
+        'qr_version': 1,
+        'qr_error_correction': 0,
+        'qr_box_size': 6,
+        'qr_border': 4,
+        'device': {
+            'text_fields': ['name', 'serial']
+        },
+        'rack': {
+            'text_fields': ['name']
+        },
+        'cable': {
+            'text_fields': [
+                '_termination_a_device',
+                'termination_a',
+                '_termination_b_device',
+                'termination_b',
+                ]
+        }
+    }
+
+config = QRCodeConfig # noqa E305
