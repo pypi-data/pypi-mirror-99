99dad376b26c2a81121abc2f687b4022ee2295b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:01 +0000

    Add package aws-solutions-constructs.aws-lambda-elasticsearch-kibana/1.92.0/aws-solutions-constructs.aws-lambda-elasticsearch-kibana-1.92.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-lambda-elasticsearch-kibana/aws-solutions-constructs.aws-lambda-elasticsearch-kibana-1.92.0.tar.gz/aws-solutions-constructs.aws-lambda-elasticsearch-kibana-1.92.0/src/aws_solutions_constructs/aws_lambda_elasticsearch_kibana/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_cognito._jsii
+import aws_cdk.aws_elasticsearch._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-lambda-elasticsearch-kibana",
+    "1.92.0",
+    __name__[0:-6],
+    "aws-lambda-elasticsearch-kibana@1.92.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
