710cce16bc939983e5395ebda535aaf3676f3387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:47 +0000

    Add package pynemo/0.0.1/pynemo-0.0.1.tar.gz

---
+++ packages/pynemo/pynemo-0.0.1.tar.gz/pynemo-0.0.1/README.md
@@ -0,0 +1,24 @@
+Pythonic Neo4j Modelling
+========================
+
+[![PyPi Version](https://img.shields.io/pypi/v/pynemo)](https://pypi.org/project/pynemo/)
+[![Supported Python Versions](https://img.shields.io/pypi/pyversions/pynemo.svg)](https://pypi.org/project/pynemo/)
+[![License](https://img.shields.io/github/license/ssripilaipong/pynemo)](https://github.com/SSripilaipong/pynemo/blob/master/LICENSE.md)
+
+# Overview
+Pythonic Neo4j Modelling. Coming soon.
+
+
+# Requirements
+- Python 3.7+
+- Works on Linux, Windows, macOS, BSD
+
+# Installation
+using pip:
+```
+pip install pynemo
+```
+
+[comment]: <> (# Documentation)
+
+[comment]: <> ([ReadTheDocs]&#40;&#41; &#40;in progress&#41;)
---
+++ packages/pynemo/pynemo-0.0.1.tar.gz/pynemo-0.0.1/setup.py
@@ -0,0 +1,62 @@
+import setuptools
+from setuptools import setup
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+try:
+    import pypandoc
+    LONG_DESCRIPTION = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    LONG_DESCRIPTION = open('README.md').read()
+
+
+NAME = 'pynemo'
+VERSION = '0.0.1'
+URL = 'https://github.com/SSripilaipong/pynemo'
+PROJECT_URLS = {
+    'Documentation': URL,
+    'Source Code': URL,
+}
+LICENSE = 'MIT'
+AUTHOR = 'SSripilaipong'
+EMAIL = 'SHSnail@mail.com'
+
+DESCRIPTION = ('Pythonic Neo4j Modelling. '
+               'Coming soon.')
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=setuptools.find_packages(),
+    url=URL,
+    project_urls=PROJECT_URLS,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    python_requires='>=3.7',
+    install_requires=requirements,
+    classifiers=CLASSIFIERS,
+)
