6ac5a2fd30d2906c842678ba7fff4596e38aab7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:59 +0000

    Add package prestoweb/0.15.0/prestoweb-0.15.0.tar.gz

---
+++ packages/prestoweb/prestoweb-0.15.0.tar.gz/prestoweb-0.15.0/prestoweb/prestoweb-run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+uwsgi --http-socket 0.0.0.0:8001 \
+    --master \
+    --workers 1 \
+    --module prestoweb.wsgi:application \
+    --env DJANGO_SETTINGS_MODULE=prestoweb.settings \
+    --vacuum \
+    --die-on-term
