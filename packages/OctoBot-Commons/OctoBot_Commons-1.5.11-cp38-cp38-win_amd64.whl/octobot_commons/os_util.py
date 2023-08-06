c8797d572c07e54ca2ee627fc35b77516de22a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:05 +0000

    Add package OctoBot-Commons/1.5.11/OctoBot_Commons-1.5.11-cp38-cp38-win_amd64.whl

---
+++ packages/OctoBot-Commons/OctoBot_Commons-1.5.11-cp38-cp38-win_amd64.whl/octobot_commons/os_util.py
@@ -0,0 +1,82 @@
+#  Drakkar-Software OctoBot
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+
+import sys
+import os
+import platform
+
+import octobot_commons.constants as constants
+import octobot_commons.enums as enums
+
+
+def get_current_platform():
+    """
+    Return the current platform details
+    Return examples
+    For Windows :
+    >>> 'Windows:10:AMD64'
+    For Linux :
+    >>> 'Linux:4.15.0-46-generic:x86_64'
+    For Raspberry :
+    >>> 'Linux:4.14.98-v7+:armv7l'
+    :return: the current platform details
+    """
+    return (
+        f"{platform.system()}{constants.PLATFORM_DATA_SEPARATOR}{platform.release()}{constants.PLATFORM_DATA_SEPARATOR}"
+        f"{platform.machine()}"
+    )
+
+
+def get_octobot_type():
+    """
+    Return OctoBot running type from OctoBotTypes
+    :return: the OctoBot running type
+    """
+    try:
+        execution_arg = sys.argv[0]
+        # sys.argv[0] is always the name of the python script called when using a command "python xyz.py"
+        if execution_arg.endswith(".py"):
+            if _is_on_docker():
+                return enums.OctoBotTypes.DOCKER.value
+            return enums.OctoBotTypes.PYTHON.value
+        # sys.argv[0] is the name of the binary when using a binary version: ends with nothing or .exe"
+        return enums.OctoBotTypes.BINARY.value
+    except IndexError:
+        return enums.OctoBotTypes.BINARY.value
+
+
+def get_os():
+    """
+    Return the OS name
+    :return: the OS name
+    """
+    return enums.PlatformsName(os.name)
+
+
+def _is_on_docker():
+    """
+    Check if the current platform is docker
+    :return: True if OctoBot is running with docker
+    """
+    file_to_check = "/proc/self/cgroup"
+    try:
+        return os.path.exists("/.dockerenv") or (
+            os.path.isfile(file_to_check)
+            and any("docker" in line for line in open(file_to_check))
+        )
+    except FileNotFoundError:
+        return False
---
+++ packages/OctoBot-Commons/OctoBot_Commons-1.5.11-cp38-cp38-win_amd64.whl/octobot_commons/tentacles_management/abstract_tentacle.py
@@ -0,0 +1,56 @@
+#  Drakkar-Software OctoBot-Commons
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+import abc
+import copy
+
+
+class AbstractTentacle:
+    """
+    The parent class of any OctoBot tentacle
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        self.logger = None
+
+    @classmethod
+    def get_name(cls) -> str:
+        """
+        Tentacle name based on class name
+        :return: the tentacle name
+        """
+        return cls.__name__
+
+    @classmethod
+    def get_all_subclasses(cls) -> list:
+        """
+        Return all subclasses of this tentacle
+        :return: the subclasses
+        """
+        subclasses_list = cls.__subclasses__()
+        if cls.__subclasses__():
+            for subclass in copy.deepcopy(subclasses_list):
+                subclasses_list += subclass.get_all_subclasses()
+        return subclasses_list
+
+    @classmethod
+    def get_user_commands(cls) -> dict:
+        """
+        Return the dict of user commands for this tentacle
+        :return: the commands dict
+        """
+        return {}
