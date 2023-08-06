38609f90a2a0e27e4fa6c55cf4db8bd5432eae7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:35 +0000

    Add package pystoxx/0.0.7/pystoxx-0.0.7.tar.gz

---
+++ packages/pystoxx/pystoxx-0.0.7.tar.gz/pystoxx-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="pystoxx",
+        version="0.0.7",
+        author="Saleh Alkhalifa",
+        author_email="alkhalifas@recursiv.tech",
+        description="Search and retrieve current data and historical information for publicly traded companies",
+        url="https://github.com/recursivllc/pystoxx",
+        keywords=['stock', 'prices', 'sentiment', 'historical', 'news', 'dataset', 'api'],
+        install_requires=['requests'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            ],
+        packages=setuptools.find_packages(),
+        python_requires='>3.6',
+        long_description=long_description,
+        long_description_content_type='text/markdown')
