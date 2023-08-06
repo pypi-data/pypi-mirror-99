60cffa1d20d4b7d1c5543f16991207e4f448c7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:15 +0000

    Add package lumen/0.3.3a1/lumen-0.3.3a1-py2.py3-none-any.whl

---
+++ packages/lumen/lumen-0.3.3a1-py2.py3-none-any.whl/lumen/sources/ae5.py
@@ -0,0 +1,184 @@
+import datetime as dt
+
+import param
+import requests
+
+from ae5_tools.api import AEUserSession
+from panel import state
+
+from .base import Source, cached
+from ..util import get_dataframe_schema
+
+
+class AE5Source(Source):
+    """
+    The AE5Source provides a number of tables, which allow monitoring
+    the nodes, deployments, sessions and resource profiles of an
+    Anaconda Enterprise 5 installation.
+    """
+
+    hostname = param.String(doc="URL of the AE5 host.")
+
+    username = param.String(doc="Username to authenticate with AE5.")
+
+    password = param.String(doc="Password to authenticate with AE5.")
+
+    k8s_endpoint = param.String(default='k8s')
+
+    pool_size = param.Integer(default=100, doc="""
+      Size of HTTP socket pool.""")
+
+    private = param.Boolean(default=True, doc="""
+      Whether to limit the deployments visible to a user based on
+      their authorization.""")
+
+    source_type = 'ae5'
+
+    _deployment_columns = [
+        'id', 'name', 'url', 'owner', 'resource_profile', 'public', 'state',
+        'cpu', 'cpu_percent', 'memory', 'memory_percent', 'uptime', 'restarts'
+    ]
+
+    _session_columns = [
+        'id', 'name', 'url', 'owner', 'resource_profile', 'state'
+    ]
+
+    _tables = ['deployments', 'nodes', 'resources', 'sessions']
+
+    _units = {
+        'm': 0.001,
+        None: 1,
+        'Ki': 1024,
+        'Mi': 1024**2,
+        'Gi': 1024**3,
+        'Ti': 1024**4
+    }
+
+    def __init__(self, **params):
+        super().__init__(**params)
+        self._session = AEUserSession(
+            self.hostname, self.username, self.password, persist=False,
+            k8s_endpoint=self.k8s_endpoint
+        )
+        adapter = requests.adapters.HTTPAdapter(pool_maxsize=self.pool_size)
+        self._session.session.mount('https://', adapter)
+        self._session.session.mount('http://', adapter)
+
+    @classmethod
+    def _convert_value(cls, value):
+        if value == '0':
+            return 0
+        val_unit = [m for m in cls._units if m is not None and value.endswith(m)]
+        val_unit = val_unit[0] if val_unit else None
+        if not val_unit:
+            return float(value)
+        scale_factor = cls._units[val_unit]
+        return float(value[:-len(val_unit)]) * scale_factor
+
+    @property
+    def _user(self):
+        return state.headers.get('Anaconda-User') if self.private else None
+
+    def _process_deployment(self, deployment):
+        container_info = deployment.get('_k8s', {}).get('containers', {})
+        if 'app' not in container_info:
+            nan = float('NaN')
+            deployment['cpu'] = nan
+            deployment['memory'] = nan
+            deployment['restarts'] = nan
+            deployment['uptime'] = '-'
+            return deployment
+
+        container = container_info['app']
+        limits = container['limits']
+        usage = container['usage']
+
+        # CPU Usage
+        cpu_cap = self._convert_value(limits['cpu'])
+        if 'cpu' in usage:
+            cpu = self._convert_value(usage['cpu'])
+            deployment['cpu'] = cpu
+            deployment['cpu_percent'] = round((cpu / cpu_cap)*100, 2)
+        else:
+            deployment['cpu_percent'] = float('nan')
+            deployment['cpu'] = float('nan')
+
+        # Memory usage
+        mem_cap = self._convert_value(limits['memory'])
+        if 'memory' in usage:
+            mem = self._convert_value(usage['memory'])
+            deployment['memory'] = mem
+            deployment['memory_percent'] = round((mem / mem_cap)*100, 2)
+        else:
+            deployment['memory'] = float('nan')
+            deployment['memory_percent'] = float('nan')
+
+        # Uptime
+        started = container.get('since')
+        deployment["restarts"] = container['restarts']
+        if started:
+            started_dt = dt.datetime.fromisoformat(started.replace('Z', ''))
+            uptime = str(dt.datetime.now()-started_dt)
+            deployment["uptime"] = uptime[:uptime.index('.')]
+        else:
+            deployment["uptime"] = '-'
+
+        return deployment
+
+    def _process_nodes(self, node):
+        caps = {
+            'cpu': self._convert_value(node['capacity/cpu']),
+            'gpu': self._convert_value(node['capacity/gpu']),
+            'mem': self._convert_value(node['capacity/mem']),
+            'pod': int(node['capacity/pod'])
+        }
+        for column in node.index:
+            if 'capacity' in column or '/' not in column:
+                continue
+            vtype = column.split('/')[1]
+            cap = caps[vtype]
+            value = node[column]
+            if vtype != 'pod':
+                value = self._convert_value(value)
+            node[column] = value
+            node[f'{column}_percent'] = 0 if cap == 0 else round((value/caps[vtype])*100, 2)
+        return node
+
+    def _get_deployments(self):
+        user = self._user
+        deployments = self._session.deployment_list(
+            k8s=True, format='dataframe', collaborators=bool(user)
+        ).apply(self._process_deployment, axis=1)
+        if user is None:
+            return deployments[self._deployment_columns]
+        return deployments[
+            deployments.public |
+            (deployments.owner == user) |
+            deployments._collaborators.apply(lambda x: user in x)
+        ][self._deployment_columns]
+
+    def _get_nodes(self):
+        nodes = self._session.node_list(format='dataframe').apply(self._process_nodes, axis=1)
+        return nodes[[c for c in nodes.columns if not c.startswith('_')]]
+
+    def _get_resources(self):
+        return self._session.resource_profile_list(format='dataframe')
+
+    def _get_sessions(self):
+        sessions = self._session.session_list(format='dataframe')
+        if self._user:
+            sessions = sessions[sessions.owner==self._user]
+        return sessions[self._session_columns]
+
+    @cached(with_query=False)
+    def get(self, table, **query):
+        if table not in self._tables:
+            raise ValueError(f"AE5Source has no '{table}' table, choose from {repr(self._tables)}.")
+        return getattr(self, f'_get_{table}')()
+
+    def get_schema(self, table=None):
+        schemas = {}
+        for t in self._tables:
+            if table is None or t == table:
+                schemas[t] = get_dataframe_schema(self.get(t))['items']['properties']
+        return schemas if table is None else schemas[table]
---
+++ packages/lumen/lumen-0.3.3a1-py2.py3-none-any.whl/lumen/util.py
@@ -0,0 +1,208 @@
+import datetime as dt
+import re
+import os
+import sys
+import subprocess
+
+import panel as pn
+import param
+
+from jinja2 import Environment, Undefined, DebugUndefined
+from pandas.core.dtypes.dtypes import CategoricalDtype
+from panel import state
+from panel.widgets.indicators import Indicator
+from panel.template.base import BasicTemplate
+from panel.template import DefaultTheme, DarkTheme
+
+_LAYOUTS = {
+    'accordion': pn.Accordion,
+    'column'   : pn.Column,
+    'grid'     : pn.GridBox,
+    'row'      : pn.Row,
+    'tabs'     : pn.Tabs
+}
+
+_TEMPLATES = {
+    k[:-8].lower(): v for k, v in param.concrete_descendents(BasicTemplate).items()
+}
+
+_THEMES = {'default': DefaultTheme, 'dark': DarkTheme}
+
+_INDICATORS = {k.lower(): v for k, v in param.concrete_descendents(Indicator).items()}
+
+
+
+
+def get_dataframe_schema(df, columns=None):
+    """
+    Returns a JSON schema optionally filtered by a subset of the columns.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame or dask.DataFrame
+        The DataFrame to describe with the schema
+    columns: list(str) or None
+        List of columns to include in schema
+
+    Returns
+    -------
+    dict
+        The JSON schema describing the DataFrame
+    """
+    if 'dask.dataframe' in sys.modules:
+        import dask.dataframe as dd
+        is_dask = isinstance(df, dd.DataFrame)
+    else:
+        is_dask = False
+
+    if columns is None:
+        columns = list(df.columns)
+
+    schema = {'type': 'array', 'items': {'type': 'object', 'properties': {}}}
+    properties = schema['items']['properties']
+    for name in columns:
+        dtype = df.dtypes[name]
+        column = df[name]
+        if dtype.kind in 'uifM':
+            vmin, vmax = column.min(), column.max()
+            if is_dask:
+                vmin, vmax = dd.compute(vmin, vmax)
+            if dtype.kind == 'M':
+                kind = 'string'
+                vmin, vmax = vmin.isoformat(), vmax.isoformat()
+            else:
+                if dtype.kind == 'f':
+                    cast = float
+                    kind = 'number'
+                else:
+                    cast = int
+                    kind = 'integer'
+                vmin, vmax = cast(vmin), cast(vmax)
+            properties[name] = {
+                'type': kind,
+                'inclusiveMinimum': vmin,
+                'inclusiveMaximum': vmax
+            }
+            if dtype.kind == 'M':
+                properties[name]['format'] = 'datetime'
+        elif dtype.kind == 'O':
+            if isinstance(dtype, CategoricalDtype) and len(dtype.categories):
+                cats = list(dtype.categories)
+            else:
+                try:
+                    cats = column.unique()
+                    if is_dask:
+                        cats = cats.compute()
+                except Exception:
+                    cats = []
+                cats = list(cats)
+            properties[name] = {'type': 'string', 'enum': cats}
+    return schema
+
+_period_regex = re.compile(r'((?P<weeks>\d+?)w)?((?P<days>\d+?)d)?((?P<hours>\d+?)h)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)?')
+
+
+def parse_timedelta(time_str):
+    parts = _period_regex.match(time_str)
+    if not parts:
+        return
+    parts = parts.groupdict()
+    time_params = {}
+    for (name, p) in parts.items():
+        if p:
+            time_params[name] = int(p)
+    return dt.timedelta(**time_params)
+
+
+def _j_getenv(x):
+    if isinstance(x, Undefined):
+        x = x._undefined_name
+    return os.getenv(x, '')
+
+def _j_getshell(x):
+    if isinstance(x, Undefined):
+        x = x._undefined_name
+    try:
+        return subprocess.check_output(x).decode()
+    except (IOError, OSError):
+        return ""
+
+def _j_getheaders(x):
+    if isinstance(x, Undefined):
+        x = x._undefined_name
+    return state.headers.get(x, '')
+
+def _j_getcookies(x):
+    if isinstance(x, Undefined):
+        x = x._undefined_name
+    return state.cookies.get(x, '')
+
+def _j_getoauth(x):
+    if isinstance(x, Undefined):
+        x = x._undefined_name
+    if state.user_info is None:
+        return ''
+    return state.user_info.get(x, '')
+
+def expand_spec(pars, context={}, getenv=True, getshell=True, getheaders=True,
+                getcookies=True, getoauth=True):
+    """
+    Render variables in context into the set of parameters with jinja2.
+
+    For variables that are not strings, nothing happens.
+
+    Parameters
+    ----------
+    pars: dict
+        values are strings containing some jinja2 controls
+    context: dict
+        values to use while rendering
+
+    Returns
+    -------
+    dict with the same keys as ``pars``, but updated values
+    """
+    if isinstance(pars, dict):
+        return {k: expand_spec(
+            v, context, getenv, getshell, getheaders, getcookies, getoauth
+        ) for k, v in pars.items()}
+    elif isinstance(pars, (list, tuple, set)):
+        return type(pars)(expand_spec(
+            v, context, getenv, getshell, getheaders, getcookies, getoauth
+        ) for v in pars)
+    elif isinstance(pars, str):
+        jinja = Environment(undefined=DebugUndefined)
+        if getenv:
+            jinja.globals['env'] = _j_getenv
+        if getshell:
+            jinja.globals['shell'] = _j_getshell
+        if getheaders:
+            jinja.globals['header'] = _j_getheaders
+        if getcookies:
+            jinja.globals['cookie'] = _j_getcookies
+        if getoauth:
+            jinja.globals['oauth'] = _j_getoauth
+        return jinja.from_string(pars).render(context)
+    else:
+        # no expansion
+        return pars
+
+
+def merge_schemas(schema, old_schema):
+    """
+    Merges two JSON schemas on a column.
+    """
+    if old_schema is None:
+        return schema
+    elif schema['type'] != old_schema['type']:
+        return old_schema
+    elif 'enum' in schema and 'enum' in old_schema:
+        merged_enum = list(old_schema['enum'])
+        for enum in schema['enum']:
+            if enum not in merged_enum:
+                merged_enum.append(enum)
+        return dict(old_schema, enum=merged_enum)
+    elif 'inclusiveMinimum' in schema and 'inclusiveMinimum' in old_schema:
+        merged_min = min(schema['inclusiveMinimum'], old_schema['inclusiveMinimum'])
+        merged_max = min(schema['inclusiveMaximum'], old_schema['inclusiveMaximum'])
+        return dict(old_schema, inclusiveMinimum=merged_min, inclusiveMaximum=merged_max)
