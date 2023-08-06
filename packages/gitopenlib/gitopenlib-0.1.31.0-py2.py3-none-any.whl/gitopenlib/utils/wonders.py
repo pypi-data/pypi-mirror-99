6cf13359a6f2a966853ee977a993800701a349a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:15 +0000

    Add package gitopenlib/0.1.31.0/gitopenlib-0.1.31.0-py2.py3-none-any.whl

---
+++ packages/gitopenlib/gitopenlib-0.1.31.0-py2.py3-none-any.whl/gitopenlib/utils/wonders.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# Copyright (c) 2021
+# @Author :  GitOPEN
+# @Email  :  gitopen@gmail.com
+# @Date   :  2021-02-01 10:25:51
+# @Description :  一些”魔术“方法
+
+__version__ = "0.2.0"
+
+from functools import wraps
+from time import time
+from typing import Callable
+
+from gitopenlib.utils import basics as gb
+
+
+def timing(f: Callable):
+    """A simple timer decorator.
+
+    装饰器，用于统计函数的运行时间。
+
+    """
+
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start_ = time()
+        result = f(*args, **kwargs)
+        end_ = time()
+        print(
+            f"Elapsed time #{f.__name__}# : {gb.time_formatter(end_ - start_, False)}"
+        )
+        return result
+
+    return wrapper
