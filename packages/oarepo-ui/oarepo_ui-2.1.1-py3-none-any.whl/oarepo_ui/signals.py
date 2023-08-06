3174f95a7f756444817b09852a8278545360bf6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:02 +0000

    Add package oarepo-ui/2.1.1/oarepo_ui-2.1.1-py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-2.1.1-py3-none-any.whl/oarepo_ui/signals.py
@@ -0,0 +1,14 @@
+from blinker import Namespace
+
+_signals = Namespace()
+
+before_facet_options = _signals.signal('before-facet-options')
+"""Signal is sent before facet options are returned.
+
+   :param source: view instance
+   :param index_name: name of index
+   :param index: Facets index instance ({aggs: ..., filters: ...})
+   :param request: request instance
+   :param view_args: view args
+   :param view_kwargs: view kwargs
+"""
