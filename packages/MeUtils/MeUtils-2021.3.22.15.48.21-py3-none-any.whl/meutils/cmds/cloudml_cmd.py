2200d26f9a29eb7ee7680fb456de1dab88a0e675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:06 +0000

    Add package MeUtils/2021.3.22.15.48.21/MeUtils-2021.3.22.15.48.21-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2021.3.22.15.48.21-py3-none-any.whl/meutils/cmds/cloudml_cmd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : cloudml_cmd
+# @Time         : 2021/1/20 10:16 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
