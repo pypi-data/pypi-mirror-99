9843057e302bc0acbdedd813940e3bb2991a62ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:00 +0000

    Add package biotite/0.27.0/biotite-0.27.0-cp37-cp37m-win32.whl

---
+++ packages/biotite/biotite-0.27.0-cp37-cp37m-win32.whl/biotite/structure/io/general.py
@@ -0,0 +1,253 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+"""
+This module contains a convenience function for loading structures from
+general structure files.
+"""
+
+__name__ = "biotite.structure.io"
+__author__ = "Patrick Kunzmann"
+__all__ = ["load_structure", "save_structure"]
+
+import os.path
+import io
+from ..atoms import AtomArray, AtomArrayStack
+
+
+def load_structure(file_path, template=None, **kwargs):
+    """
+    Load an :class:`AtomArray` or class`AtomArrayStack` from a structure
+    file without the need to manually instantiate a :class:`File`
+    object.
+    
+    Internally this function uses a :class:`File` object, based on the
+    file extension.
+    Trajectory files furthermore require specification of the `template`
+    parameter.
+    
+    Parameters
+    ----------
+    file_path : str
+        The path to structure file.
+    template : AtomArray or AtomArrayStack or file-like object or str, optional
+        Only required when reading a trajectory file.
+    kwargs
+        Additional parameters will be passed to either the
+        :func:`get_structure()` or :func:`read()` method of the file
+        object.
+        This does not affect files given via the `template` parameter.
+    
+    Returns
+    -------
+    array : AtomArray or AtomArrayStack
+        If the file contains multiple models, an AtomArrayStack is
+        returned, otherwise an AtomArray is returned.
+    
+    Raises
+    ------
+    ValueError
+        If the file format (i.e. the file extension) is unknown.
+    TypeError
+        If a trajectory file is loaded without specifying the
+        `template` parameter.
+    """
+    # Optionally load template from file
+    if isinstance(template, (io.IOBase, str)):
+        template = load_structure(template)
+
+    # We only need the suffix here
+    _, suffix = os.path.splitext(file_path)
+    if suffix == ".pdb":
+        from .pdb import PDBFile
+        file = PDBFile.read(file_path)
+        array = file.get_structure(**kwargs)
+        if isinstance(array, AtomArrayStack) and array.stack_depth() == 1:
+            # Stack containing only one model -> return as atom array
+            return array[0]
+        else:
+            return array
+    elif suffix == ".pdbqt":
+        from .pdbqt import PDBQTFile
+        file = PDBQTFile.read(file_path)
+        array = file.get_structure(**kwargs)
+        if isinstance(array, AtomArrayStack) and array.stack_depth() == 1:
+            # Stack containing only one model -> return as atom array
+            return array[0]
+        else:
+            return array
+    elif suffix == ".cif" or suffix == ".pdbx":
+        from .pdbx import PDBxFile, get_structure
+        file = PDBxFile.read(file_path)
+        array = get_structure(file, **kwargs)
+        if isinstance(array, AtomArrayStack) and array.stack_depth() == 1:
+            # Stack containing only one model -> return as atom array
+            return array[0]
+        else:
+            return array
+    elif suffix == ".gro":
+        from .gro import GROFile
+        file = GROFile.read(file_path)
+        array = file.get_structure(**kwargs)
+        if isinstance(array, AtomArrayStack) and array.stack_depth() == 1:
+            # Stack containing only one model -> return as atom array
+            return array[0]
+        else:
+            return array
+    elif suffix == ".mmtf":
+        from .mmtf import MMTFFile, get_structure
+        file = MMTFFile.read(file_path)
+        array = get_structure(file, **kwargs)
+        if isinstance(array, AtomArrayStack) and array.stack_depth() == 1:
+            # Stack containing only one model -> return as atom array
+            return array[0]
+        else:
+            return array
+    elif suffix == ".npz":
+        from .npz import NpzFile
+        file = NpzFile.read(file_path)
+        array = file.get_structure(**kwargs)
+        if isinstance(array, AtomArrayStack) and array.stack_depth() == 1:
+            # Stack containing only one model -> return as atom array
+            return array[0]
+        else:
+            return array
+    elif suffix in [".trr", ".xtc", ".tng", ".dcd", ".netcdf"]:
+        if template is None:
+            raise TypeError("Template must be specified for trajectory files")
+        from .trr import TRRFile
+        from .xtc import XTCFile
+        from .tng import TNGFile
+        from .dcd import DCDFile
+        from .netcdf import NetCDFFile
+        if suffix == ".trr":
+            traj_file_cls = TRRFile
+        if suffix == ".xtc":
+            traj_file_cls = XTCFile
+        if suffix == ".tng":
+            traj_file_cls = TNGFile
+        if suffix == ".dcd":
+            traj_file_cls = DCDFile
+        if suffix == ".netcdf":
+            traj_file_cls = NetCDFFile
+        file = traj_file_cls.read(file_path, **kwargs)
+        return file.get_structure(template)
+    else:
+        raise ValueError(f"Unknown file format '{suffix}'")
+
+
+def save_structure(file_path, array, **kwargs):
+    """
+    Save an :class:`AtomArray` or class`AtomArrayStack` to a structure
+    file without the need to manually instantiate a :class:`File`
+    object.
+    
+    Internally this function uses a :class:`File` object, based on the
+    file extension.
+    
+    Parameters
+    ----------
+    file_path : str
+        The path to structure file.
+    array : AtomArray or AtomArrayStack
+        The structure to be saved.
+    kwargs
+        Additional parameters will be passed to the respective `set_structure`
+        method.
+
+    Raises
+    ------
+    ValueError
+        If the file format (i.e. the file extension) is unknown.
+    """
+    # We only need the suffix here
+    _, suffix = os.path.splitext(file_path)
+    if suffix == ".pdb":
+        from .pdb import PDBFile
+        file = PDBFile()
+        file.set_structure(array, **kwargs)
+        file.write(file_path)
+    elif suffix == ".pdbqt":
+        from .pdbqt import PDBQTFile
+        file = PDBQTFile()
+        file.set_structure(array, **kwargs)
+        file.write(file_path)
+    elif suffix == ".cif" or suffix == ".pdbx":
+        from .pdbx import PDBxFile, set_structure
+        file = PDBxFile()
+        set_structure(file, array, data_block="STRUCTURE", **kwargs)
+        file.write(file_path)
+    elif suffix == ".gro":
+        from .gro import GROFile
+        file = GROFile()
+        file.set_structure(array, **kwargs)
+        file.write(file_path)
+    elif suffix == ".mmtf":
+        from .mmtf import MMTFFile, set_structure
+        file = MMTFFile()
+        set_structure(file, array, **kwargs)
+        file.write(file_path)
+    elif suffix == ".npz":
+        from .npz import NpzFile
+        file = NpzFile()
+        file.set_structure(array, **kwargs)
+        file.write(file_path)
+    elif suffix in [".trr", ".xtc", ".tng", ".dcd", ".netcdf"]:
+        from .trr import TRRFile
+        from .xtc import XTCFile
+        from .tng import TNGFile
+        from .dcd import DCDFile
+        from .netcdf import NetCDFFile
+        if suffix == ".trr":
+            traj_file_cls = TRRFile
+        if suffix == ".xtc":
+            traj_file_cls = XTCFile
+        if suffix == ".tng":
+            traj_file_cls = TNGFile
+        if suffix == ".dcd":
+            traj_file_cls = DCDFile
+        if suffix == ".netcdf":
+            traj_file_cls = NetCDFFile
+        file = traj_file_cls()
+        file.set_structure(array, **kwargs)
+        file.write(file_path)
+    else:
+        raise ValueError(f"Unknown file format '{suffix}'")
+
+
+# Helper function to estimate elements from atom names
+_elements = [elem.upper() for elem in 
+["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg",
+"Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe",
+"Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y",
+"Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te",
+"I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb",
+"Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt",
+"Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa",
+"U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf",
+"Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts",
+"Og"]
+]
+def _guess_element(atom_name):
+    # remove digits (1H -> H)
+    elem = "".join([i for i in atom_name if not i.isdigit()])
+    elem = elem.upper()
+
+    # Some often used elements for biomolecules
+    if elem.startswith("C") or elem.startswith("N") or \
+        elem.startswith("O") or elem.startswith("S") or \
+        elem.startswith("H"):
+        return elem[0]
+
+    # Exactly match element abbreviations
+    try:
+        return _elements[_elements.index(elem[:2])]
+    except ValueError:
+        try:
+            return _elements[_elements.index(elem[0])]
+        except ValueError:
+            pass
+
+    return ""
+ 
---
+++ packages/biotite/biotite-0.27.0-cp37-cp37m-win32.whl/biotite/visualize.py
@@ -0,0 +1,243 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+__name__ = "biotite"
+__author__ = "Patrick Kunzmann"
+__all__ = ["colors", "set_font_size_in_coord"]
+
+import abc
+from collections import OrderedDict
+import numpy as np
+from numpy.linalg import norm
+
+
+def set_font_size_in_coord(text, width=None, height=None, mode="unlocked"):
+    """
+    Specifiy the font size of an existing `Text` object in coordinates
+    of the object's reference coordiante system.
+
+    Instead of having the font size fixed in 'pt', the size of the text
+    scales to the specied width/height and adapts to changes in the
+    plot's width/height.
+    The scaling can be proportional or non-proportional, depending 
+    the `mode`.
+
+    Parameters
+    ----------
+    text : Text:
+        The matplotlib `Text` to be scaled.
+    width, height : float, optional
+        The new width/height of `text` in its
+        reference coordinate system.
+        At least one value must be supplied.
+    mode : {'proportional', 'unlocked', 'maximum', 'minimum'}, optional
+        The scaling mode:
+
+            - *proportional* - The width and height are scaled by the
+              same extent.
+              Either `width` or `height` must be set for this mode.
+            - *unlocked* - The width and the height are scaled by
+              different extents, changing the aspect ratio.
+              Both `width` and `height` must be set for this mode.
+            - *maximum* - The width and the height are scaled by
+              the same extent, so that they are at maximum as large
+              as the supplied `width`/`height`.
+              Both `width` and `height` must be set for this mode.
+            - *minimum* - The width and the height are scaled by
+              the same extent, so that they are at minimum as large
+              as the supplied `width`/`height`.
+              Both `width` and `height` must be set for this mode.
+
+    Notes
+    -----
+    This function uses the :func:`get_window_extent()` method of the
+    :class:`Text` object.
+    According to experience, this function does not give the the exact
+    visual boundaries of the text.
+    Consequently, the scaled text might be slightly smaller or larger
+    than the specified width/height.
+    This behavior is not equal for all initial font sizes (in 'pt'),
+    the boundaries for an initial size of 1 'pt' seem to be most exact.
+    """
+    from matplotlib.transforms import Bbox
+    from matplotlib.text import Text
+    from matplotlib.patheffects import AbstractPathEffect
+
+    class TextScaler(AbstractPathEffect):
+        def __init__(self, text, width, height, mode):
+            self._text = text
+            self._mode = mode
+            self._width = width
+            self._height = height
+
+        def draw_path(self, renderer, gc, tpath, affine, rgbFace=None):
+            ax = self._text.axes
+            renderer = ax.get_figure().canvas.get_renderer()
+            bbox = text.get_window_extent(renderer=renderer)
+            bbox = Bbox(ax.transData.inverted().transform(bbox))
+            
+            if self._mode == "proportional":
+                if self._width is None:
+                    # Proportional scaling based on height
+                    scale_y = self._height / bbox.height
+                    scale_x = scale_y
+                elif self._height is None:
+                    # Proportional scaling based on width
+                    scale_x = self._width / bbox.width
+                    scale_y = scale_x
+            elif self._mode == "unlocked":
+                scale_x = self._width / bbox.width
+                scale_y = self._height / bbox.height
+            elif self._mode == "minimum":
+                scale_x = self._width / bbox.width
+                scale_y = self._height / bbox.height
+                scale = max(scale_x, scale_y)
+                scale_x, scale_y = scale, scale
+            elif self._mode == "maximum":
+                scale_x = self._width / bbox.width
+                scale_y = self._height / bbox.height
+                scale = min(scale_x, scale_y)
+                scale_x, scale_y = scale, scale
+
+            affine = affine.identity().scale(scale_x, scale_y) + affine
+            renderer.draw_path(gc, tpath, affine, rgbFace)
+    
+    if mode in ["unlocked", "minimum", "maximum"]:
+        if width is None or height is None:
+            raise TypeError(
+                f"Width and height must be set in '{mode}' mode"
+            )
+    elif mode == "proportional":
+        if  not (width  is None and height is not None) or \
+            not (height is None and width  is not None):
+                raise TypeError(
+                    f"Either width or height must be set in '{mode}' mode"
+                )
+    else:
+        raise ValueError(
+                f"Unknown mode '{mode}'"
+            )
+    text.set_path_effects([TextScaler(text, width, height, mode)])
+
+try:
+    # Only create this class when matplotlib is installed
+    from matplotlib.transforms import Bbox
+    from matplotlib.patches import FancyArrow
+    from matplotlib.patheffects import AbstractPathEffect
+
+    class AdaptiveFancyArrow(FancyArrow):
+        """
+        A `FancyArrow` with fixed head shape.
+
+        The length of the head is proportional to the width of the head
+        in display coordinates.
+        If the head length is longer than the length of the entire
+        arrow, the head length is limited to the arrow length.
+
+        Parameters
+        ----------
+        x,y  : float
+            The arrow's start position.
+        dx, dy : float
+            The arrow's direction vector, inclduing the arrow head.
+        tail_width, head_width : float
+            The width of the arrow's tail and head in its reference
+            coordinate system
+        head_ratio : float, optional
+            The length of the arrow head as faction of the arrow width
+            (display coordinates).
+        draw_head: bool, optional
+            If false, the arrow has no head. The result is a rectangle.
+        shape : str, optional
+            The `shape` parameter in the constructor of `FancyArrow`.
+        **kwargs
+            Other parameters that are used in the constructor of
+            `FancyArrow`.
+        """
+        
+        def __init__(self, x, y, dx, dy,
+                     tail_width, head_width, head_ratio, draw_head=True,
+                     shape="full", **kwargs):
+            import matplotlib.pyplot as plt
+            self._x = x
+            self._y = y
+            self._dx = dx
+            self._dy = dy
+            self._tail_width = tail_width
+            self._head_width = head_width
+            self._head_ratio = head_ratio
+            self._draw_head = draw_head
+            self._shape = shape
+            self._kwargs = kwargs
+            if not draw_head:
+                head_width = tail_width
+            super().__init__(
+                x, y, dx, dy,
+                width=tail_width, head_width=head_width,
+                overhang=0, shape=shape,
+                length_includes_head=True, **kwargs
+            )
+
+        def draw(self, renderer):
+            arrow_box = Bbox([(0,0), (0,self._head_width)])
+            arrow_box_display = self.axes.transData.transform_bbox(arrow_box)
+            head_length_display = np.abs(
+                arrow_box_display.height * self._head_ratio
+            )
+            arrow_box_display.x1 = arrow_box_display.x0 + head_length_display
+            # Transfrom back to data coordinates for plotting
+            arrow_box = self.axes.transData.inverted().transform_bbox(
+                arrow_box_display
+            )
+            head_length = arrow_box.width
+            arrow_length = norm((self._dx, self._dy))
+            if head_length > arrow_length:
+                # If the head would be longer than the entire arrow,
+                # only draw the arrow head with reduced length
+                head_length = arrow_length
+            if not self._draw_head:
+                head_length = 0 
+
+            # Renew the arrow's properties
+            super().__init__(
+                self._x, self._y, self._dx, self._dy,
+                width=self._tail_width, head_width=self._head_width,
+                overhang=0, shape=self._shape,
+                head_length=head_length, length_includes_head=True,
+                axes=self.axes, transform=self.get_transform(), **self._kwargs
+            )
+            self.set_clip_path(self.axes.patch)
+            super().draw(renderer)
+        
+        # Override to replace docstring
+        # Removes warning:
+        # unknown document: /tutorials/intermediate/constrainedlayout_guide
+        def get_in_layout(self):
+            """
+            """
+            return super().get_in_layout()
+        def set_in_layout(self, in_layout):
+            """
+            """
+            return super().set_in_layout(in_layout)
+    
+    __all__.append("AdaptiveFancyArrow")
+
+except ImportError:
+    pass
+    
+
+# Biotite themed colors
+colors = OrderedDict([
+    ("brightorange" , "#ffb569ff"),
+    ("lightorange"  , "#ff982dff"),
+    ("orange"       , "#ff8405ff"),
+    ("dimorange"    , "#dc7000ff"),
+    ("darkorange"   , "#b45c00ff"),
+    ("brightgreen"  , "#98e97fff"),
+    ("lightgreen"   , "#6fe04cff"),
+    ("green"        , "#52da2aff"),
+    ("dimgreen"     , "#45bc20ff"),
+    ("darkgreen"    , "#389a1aff"),
+])
