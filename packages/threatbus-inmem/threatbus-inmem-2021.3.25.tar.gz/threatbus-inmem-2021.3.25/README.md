969cb7287b12f4a8ec9cea5593119468458e61b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:51 +0000

    Add package threatbus-inmem/2021.3.25/threatbus-inmem-2021.3.25.tar.gz

---
+++ packages/threatbus-inmem/threatbus-inmem-2021.3.25.tar.gz/threatbus-inmem-2021.3.25/README.md
@@ -0,0 +1,57 @@
+Threat Bus In-Memory Backbone Plugin
+====================================
+
+<h4 align="center">
+
+[![PyPI Status][pypi-badge]][pypi-url]
+[![Build Status][ci-badge]][ci-url]
+[![License][license-badge]][license-url]
+
+</h4>
+
+A very simplistic Threat Bus plugin that provides an in-memory backbone for data
+provisioning.
+
+### Message Routing
+
+Messages are routed based on their data type. Stix2 data, like Indicators and
+Sightings are routed via the `stix2/` topic prefix. Threat Bus-internal messages
+like SnapshotRequests and SnapshotEnvelopes are routed via the `threatbus/`
+topic prefix.
+
+
+### Subscriptions
+
+Application plugins can subscribe to topics and topic prefixes.
+
+## Installation
+
+```sh
+pip install threatbus-inmem
+```
+
+## Configuration
+
+Add a placeholder for this plugin's config:
+
+```yaml
+...
+plugins:
+  backbones:
+    inmem:
+...
+```
+
+There is no further configuration needed.
+
+
+## License
+
+Threat Bus comes with a [3-clause BSD license][license-url].
+
+[pypi-badge]: https://img.shields.io/pypi/v/threatbus-inmem.svg
+[pypi-url]: https://pypi.org/project/threatbus-inmem
+[ci-url]: https://github.com/tenzir/threatbus/actions?query=branch%3Amaster
+[ci-badge]: https://github.com/tenzir/threatbus/workflows/Python%20Egg/badge.svg?branch=master
+[license-badge]: https://img.shields.io/badge/license-BSD-blue.svg
+[license-url]: https://github.com/tenzir/threatbus/blob/master/COPYING
---
+++ packages/threatbus-inmem/threatbus-inmem-2021.3.25.tar.gz/threatbus-inmem-2021.3.25/threatbus_inmem/plugin.py
@@ -0,0 +1,97 @@
+from collections import defaultdict
+from confuse import Subview
+from multiprocessing import JoinableQueue
+from queue import Empty
+import threading
+import threatbus
+from typing import Dict, List, Set
+from stix2 import Sighting, Indicator
+
+"""In-Memory backbone plugin for Threat Bus"""
+
+plugin_name = "inmem"
+
+subscriptions: Dict[str, Set[JoinableQueue]] = defaultdict(set)
+lock = threading.Lock()
+workers: List[threatbus.StoppableWorker] = list()
+
+
+def validate_config(config):
+    return True
+
+
+class Provisioner(threatbus.StoppableWorker):
+    """
+    Provisions all messages that arrive on the inq to all subscribers of that topic.
+    @param inq The in-queue to read messages from
+    """
+
+    def __init__(self, inq: JoinableQueue):
+        self.inq = inq
+        super(Provisioner, self).__init__()
+
+    def run(self):
+        global subscriptions, lock, logger
+        while self._running():
+            try:
+                msg = self.inq.get(block=True, timeout=1)
+            except Empty:
+                continue
+            logger.debug(f"Backbone got message {msg}")
+            topic_prefix = "threatbus"
+            if type(msg) is Indicator or type(msg) is Sighting:
+                topic_prefix = "stix2"
+            topic = f"{topic_prefix}/{type(msg).__name__.lower()}"
+            lock.acquire()
+            for t in filter(
+                lambda t: str(topic).startswith(str(t)), subscriptions.keys()
+            ):
+                for outq in subscriptions[t]:
+                    outq.put(msg)
+            lock.release()
+            self.inq.task_done()
+
+
+@threatbus.backbone
+def subscribe(topic: str, q: JoinableQueue):
+    global logger, subscriptions, lock
+    logger.info(f"Adding subscription to: {topic}")
+    lock.acquire()
+    subscriptions[topic].add(q)
+    lock.release()
+
+
+@threatbus.backbone
+def unsubscribe(topic: str, q: JoinableQueue):
+    global logger, subscriptions, lock
+    logger.info(f"Removing subscription from: {topic}")
+    lock.acquire()
+    if q in subscriptions[topic]:
+        subscriptions[topic].remove(q)
+    lock.release()
+
+
+@threatbus.backbone
+def run(config: Subview, logging: Subview, inq: JoinableQueue):
+    global logger, workers
+    logger = threatbus.logger.setup(logging, __name__)
+    config = config[plugin_name]
+    try:
+        validate_config(config)
+    except Exception as e:
+        logger.fatal("Invalid config for plugin {}: {}".format(plugin_name, str(e)))
+    workers.append(Provisioner(inq))
+    for w in workers:
+        w.start()
+    logger.info("In-memory backbone started.")
+
+
+@threatbus.backbone
+def stop():
+    global logger, workers
+    for w in workers:
+        if not w.is_alive():
+            continue
+        w.stop()
+        w.join()
+    logger.info("In-memory backbone stopped")
