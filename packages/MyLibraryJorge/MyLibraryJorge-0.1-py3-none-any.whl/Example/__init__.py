12583e2266667fdf23d1833e3f6861568a871367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:14 +0000

    Add package MyLibraryJorge/0.1/MyLibraryJorge-0.1-py3-none-any.whl

---
+++ packages/MyLibraryJorge/MyLibraryJorge-0.1-py3-none-any.whl/Example/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 21 16:41:52 2021
+
+@author: jorgelopez
+"""
+def coolprint():
+    print("This is UAmerica :)")
+    
+def calculatesum(x,y):
+    return x + y
