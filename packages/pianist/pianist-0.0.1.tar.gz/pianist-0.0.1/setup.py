ea16de5efecd973cf7fe39cf301c51fc4278a01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:23 +0000

    Add package pianist/0.0.1/pianist-0.0.1.tar.gz

---
+++ packages/pianist/pianist-0.0.1.tar.gz/pianist-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pianist',
+    version='0.0.1',
+    description='',
+    long_description='Orchestration',
+    long_description_content_type='text/plain',
+    url='https://argodis.de',
+    author='Argodis GmbH',
+    author_email='info@argodis.de',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
