d92adb2cdca975e6dab4b3342f2609c7982071b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:55 +0000

    Add package udata-gouvfr/2.5.3/udata_gouvfr-2.5.3-py2.py3-none-any.whl

---
+++ packages/udata-gouvfr/udata_gouvfr-2.5.3-py2.py3-none-any.whl/udata_gouvfr/__init__.py
@@ -0,0 +1,18 @@
+'''
+uData customizations for Data.gouv.fr
+'''
+
+__version__ = '2.5.3'
+__description__ = 'uData customizations for Data.gouv.fr'
+
+
+APIGOUVFR_EXTRAS_KEY = 'apigouvfr:apis'
+APIGOUVFR_EXPECTED_FIELDS = [
+    'title',
+    'tagline',
+    'path',
+    'slug',
+    'owner',
+    'openness',
+    'logo',
+]
