dfa2dbf10fd174003a0fdcd8c436746bbb954524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:43 +0000

    Add package pdm-pep517/0.5.10/pdm_pep517-0.5.10-py3-none-any.whl

---
+++ packages/pdm-pep517/pdm_pep517-0.5.10-py3-none-any.whl/pdm/pep517/utils.py
@@ -0,0 +1,204 @@
+import distutils.util
+import os
+import re
+import sys
+import urllib.parse as urllib_parse
+import urllib.request as urllib_request
+import warnings
+from contextlib import contextmanager
+from fnmatch import fnmatchcase
+from sysconfig import get_config_var
+from typing import Iterable, Optional
+
+from pdm.pep517._vendor.packaging import tags
+from pdm.pep517._vendor.packaging.markers import Marker
+from pdm.pep517._vendor.packaging.requirements import Requirement
+from pdm.pep517._vendor.packaging.version import InvalidVersion, Version
+
+
+def safe_name(name: str) -> str:
+    """Convert an arbitrary string to a standard distribution name
+
+    Any runs of non-alphanumeric/. characters are replaced with a single '-'.
+    """
+    return re.sub("[^A-Za-z0-9.]+", "-", name)
+
+
+def safe_version(version):
+    """
+    Convert an arbitrary string to a standard version string
+    """
+    try:
+        # normalize the version
+        return str(Version(version))
+    except InvalidVersion:
+        version = version.replace(" ", ".")
+        return re.sub("[^A-Za-z0-9.]+", "-", version)
+
+
+def to_filename(name):
+    """Convert a project or version name to its filename-escaped form
+
+    Any '-' characters are currently replaced with '_'.
+    """
+    return name.replace("-", "_")
+
+
+def is_dict_like(value):
+    """Determine if an object is dict-like."""
+    return bool(getattr(value, "items", None) and getattr(value, "__getitem__", None))
+
+
+def is_python_package(fullpath):
+    if not os.path.isdir(fullpath):
+        return False
+    if os.path.basename(fullpath.rstrip("/")) in ("__pycache__", "__pypackages__"):
+        return False
+    return os.path.isfile(os.path.join(fullpath, "__init__.py"))
+
+
+def merge_marker(requirement: Requirement, marker: str) -> None:
+    """Merge the target marker str with the requirement markers"""
+    if not requirement.marker:
+        requirement.marker = Marker(marker)
+        return
+    old_marker = requirement.marker
+    if "or" in old_marker._markers:
+        new_marker = Marker(f"({old_marker}) and {marker}")
+    else:
+        new_marker = Marker(f"{old_marker} and {marker}")
+    requirement.marker = new_marker
+
+
+def find_packages_iter(
+    where: os.PathLike = ".",
+    exclude: Iterable[str] = (),
+    include: Iterable[str] = ("*",),
+    src: os.PathLike = ".",
+) -> Iterable[str]:
+    """
+    All the packages found in 'where' that pass the 'include' filter, but
+    not the 'exclude' filter.
+    """
+
+    def _build_filter(patterns):
+        """
+        Given a list of patterns, return a callable that will be true only if
+        the input matches at least one of the patterns.
+        """
+        return lambda name: any(fnmatchcase(name, pat=pat) for pat in patterns)
+
+    exclude, include = _build_filter(exclude), _build_filter(include)
+    for root, dirs, files in os.walk(where, followlinks=True):
+        # Copy dirs to iterate over it, then empty dirs.
+        all_dirs = dirs[:]
+        dirs[:] = []
+
+        for dir in all_dirs:
+            full_path = os.path.join(root, dir)
+            rel_path = os.path.relpath(full_path, src)
+            package = rel_path.replace(os.path.sep, ".")
+            # Skip directory trees that are not valid packages
+            if "." in dir:
+                continue
+
+            # Should this package be included?
+            if (
+                os.path.isfile(os.path.join(full_path, "__init__.py"))
+                and include(package)
+                and not exclude(package)
+            ):
+                yield package
+
+            # Keep searching subdirectories, as there may be more packages
+            # down there, even if the parent was excluded.
+            dirs.append(dir)
+
+
+@contextmanager
+def cd(path: str):
+    _old_cwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(_old_cwd)
+
+
+def normalize_path(filename: os.PathLike) -> str:
+    """Normalize a file/dir name for comparison purposes"""
+    filename = os.path.abspath(filename) if sys.platform == "cygwin" else filename
+    return os.path.normcase(os.path.realpath(os.path.normpath(filename)))
+
+
+def path_to_url(path: str) -> str:
+    """
+    Convert a path to a file: URL.  The path will be made absolute and have
+    quoted path parts.
+    """
+    path = os.path.normpath(os.path.abspath(path))
+    url = urllib_parse.urljoin("file:", urllib_request.pathname2url(path))
+    return url
+
+
+def get_platform() -> str:
+    """Return our platform name 'win32', 'linux_x86_64'"""
+    result = distutils.util.get_platform().replace(".", "_").replace("-", "_")
+    if result == "linux_x86_64" and sys.maxsize == 2147483647:
+        # pip pull request #3497
+        result = "linux_i686"
+    return result
+
+
+def get_flag(var, fallback, expected=True, warn=True):
+    """Use a fallback value for determining SOABI flags if the needed config
+    var is unset or unavailable."""
+    val = get_config_var(var)
+    if val is None:
+        if warn:
+            warnings.warn(
+                "Config variable '{0}' is unset, Python ABI tag may "
+                "be incorrect".format(var),
+                RuntimeWarning,
+                2,
+            )
+        return fallback
+    return val == expected
+
+
+def get_abi_tag():
+    """Return the ABI tag based on SOABI (if available) or emulate SOABI
+    (CPython 2, PyPy)."""
+    soabi = get_config_var("SOABI")
+    impl = tags.interpreter_name()
+    is_cpython = impl == "cp"
+    if not soabi and impl in ("cp", "pp") and hasattr(sys, "maxunicode"):
+        d = ""
+        m = ""
+        u = ""
+        if get_flag("Py_DEBUG", hasattr(sys, "gettotalrefcount"), warn=is_cpython):
+            d = "d"
+        if sys.version_info < (3, 8) and get_flag(
+            "WITH_PYMALLOC", is_cpython, warn=is_cpython
+        ):
+            m = "m"
+        if sys.version_info < (3, 3) and get_flag(
+            "Py_UNICODE_SIZE", sys.maxunicode == 0x10FFFF, expected=4, warn=is_cpython
+        ):
+            u = "u"
+        abi = "%s%s%s%s%s" % (impl, tags.interpreter_version(), d, m, u)
+    elif soabi and soabi.startswith("cpython-"):
+        abi = "cp" + soabi.split("-")[1]
+    elif soabi:
+        abi = soabi.replace(".", "_").replace("-", "_")
+    else:
+        abi = None
+    return abi
+
+
+def ensure_pep440_req(req: str) -> Optional[str]:
+    """Discard all non-PEP 440 requirements, e.g. editable VCS requirements."""
+
+    if req.strip().startswith("-e"):
+        return None
+    return req
---
+++ packages/pdm-pep517/pdm_pep517-0.5.10-py3-none-any.whl/pdm/pep517/wheel.py
@@ -0,0 +1,241 @@
+import contextlib
+import glob
+import hashlib
+import os
+import shutil
+import stat
+import subprocess
+import sys
+import tempfile
+import zipfile
+from base64 import urlsafe_b64encode
+from io import StringIO
+from pathlib import Path
+from typing import List, Tuple, Union
+
+from pdm.pep517 import __version__
+from pdm.pep517._vendor.packaging.markers import default_environment
+from pdm.pep517._vendor.packaging.specifiers import SpecifierSet
+from pdm.pep517.base import Builder, BuildError
+from pdm.pep517.utils import get_abi_tag, get_platform, safe_version, to_filename
+
+WHEEL_FILE_FORMAT = (
+    """\
+Wheel-Version: 1.0
+Generator: pdm-pep517 %s
+Root-Is-Purelib: {pure_lib}
+Tag: {tag}
+"""
+    % __version__
+)
+
+
+class WheelBuilder(Builder):
+    def __init__(self, location: Union[str, Path]) -> None:
+        super().__init__(location)
+        self._records = []  # type: List[Tuple[str, str, str]]
+
+    def build(self, build_dir: str, **kwargs) -> str:
+        if not os.path.exists(build_dir):
+            os.makedirs(build_dir, exist_ok=True)
+
+        self._records.clear()
+        fd, temp_path = tempfile.mkstemp(suffix=".whl")
+        os.close(fd)
+
+        with zipfile.ZipFile(
+            temp_path, mode="w", compression=zipfile.ZIP_DEFLATED
+        ) as zip_file:
+            self._copy_module(zip_file)
+            self._build(zip_file)
+            self._write_metadata(zip_file)
+
+        target = os.path.join(build_dir, self.wheel_filename)
+        if os.path.exists(target):
+            os.unlink(target)
+        shutil.move(temp_path, target)
+
+        return target
+
+    @property
+    def wheel_filename(self) -> str:
+        name = to_filename(self.meta.project_name)
+        version = to_filename(safe_version(self.meta.version))
+        return f"{name}-{version}-{self.tag}.whl"
+
+    @property
+    def tag(self) -> str:
+        if self.meta.build:
+            info = default_environment()
+            platform = get_platform()
+            implementation = info["implementation_name"]
+            impl_name = (
+                "cp"
+                if implementation.startswith("cp")
+                else "jp"
+                if implementation.startswith("jp")
+                else "ip"
+                if implementation.startswith("ir")
+                else "pp"
+                if implementation.startswith("pypy")
+                else "unknown"
+            )
+            impl_ver = (
+                info["python_full_version"].replace(".", "")
+                if impl_name == "pp"
+                else info["python_version"].replace(".", "")
+            )
+            impl = impl_name + impl_ver
+            abi_tag = get_abi_tag()
+            tag = (impl, abi_tag, platform)
+        else:
+            platform = "any"
+            if self.meta.requires_python and SpecifierSet(
+                self.meta.requires_python
+            ).contains("2.7"):
+                impl = "py2.py3"
+            else:
+                impl = "py3"
+
+            tag = (impl, "none", platform)
+
+        return "-".join(tag)
+
+    @property
+    def dist_info_name(self) -> str:
+        name = to_filename(self.meta.project_name)
+        version = to_filename(safe_version(self.meta.version))
+        return f"{name}-{version}.dist-info"
+
+    def _write_record(self, fp):
+        for row in self._records:
+            fp.write("{},sha256={},{}\n".format(*row))
+        fp.write(self.dist_info_name + "/RECORD,,\n")
+
+    def _write_metadata(self, wheel):
+        dist_info = self.dist_info_name
+        if self.meta.entry_points:
+            with self._write_to_zip(wheel, dist_info + "/entry_points.txt") as f:
+                self._write_entry_points(f)
+
+        with self._write_to_zip(wheel, dist_info + "/WHEEL") as f:
+            self._write_wheel_file(f)
+
+        with self._write_to_zip(wheel, dist_info + "/METADATA") as f:
+            self._write_metadata_file(f)
+
+        for pat in ("COPYING", "LICENSE"):
+            for path in glob.glob(pat + "*"):
+                if os.path.isfile(path):
+                    self._add_file(wheel, path, f"{dist_info}/{path}")
+
+        with self._write_to_zip(wheel, dist_info + "/RECORD") as f:
+            self._write_record(f)
+
+    @contextlib.contextmanager
+    def _write_to_zip(self, wheel, rel_path):
+        sio = StringIO()
+        yield sio
+
+        # The default is a fixed timestamp rather than the current time, so
+        # that building a wheel twice on the same computer can automatically
+        # give you the exact same result.
+        date_time = (2016, 1, 1, 0, 0, 0)
+        zi = zipfile.ZipInfo(rel_path, date_time)
+        b = sio.getvalue().encode("utf-8")
+        hashsum = hashlib.sha256(b)
+        hash_digest = urlsafe_b64encode(hashsum.digest()).decode("ascii").rstrip("=")
+
+        wheel.writestr(zi, b, compress_type=zipfile.ZIP_DEFLATED)
+        print(f" - Adding {rel_path}")
+        self._records.append((rel_path, hash_digest, str(len(b))))
+
+    def _build(self, wheel):
+        if not self.meta.build:
+            return
+        setup_py = self.ensure_setup_py()
+        build_args = [
+            sys.executable,
+            setup_py,
+            "build",
+            "-b",
+            str(self.location / "build"),
+        ]
+        try:
+            subprocess.check_call(build_args)
+        except subprocess.CalledProcessError as e:
+            raise BuildError(f"Error occurs when running {build_args}:\n{e}")
+        build_dir = self.location / "build"
+        lib_dir = next(build_dir.glob("lib.*"), None)
+        if not lib_dir:
+            return
+        for pkg in lib_dir.glob("**/*"):
+            if pkg.is_dir():
+                continue
+
+            rel_path = pkg.relative_to(lib_dir).as_posix()
+
+            if rel_path in wheel.namelist():
+                continue
+            self._add_file(wheel, pkg, rel_path)
+
+    def _copy_module(self, wheel):
+        for path in self.find_files_to_add():
+            rel_path = None
+            if self.meta.package_dir:
+                try:
+                    rel_path = path.relative_to(self.meta.package_dir).as_posix()
+                except ValueError:
+                    pass
+            self._add_file(wheel, str(path), rel_path)
+
+    def _add_file(self, wheel, full_path, rel_path=None):
+        if not rel_path:
+            rel_path = full_path
+        if os.sep != "/":
+            # We always want to have /-separated paths in the zip file and in RECORD
+            rel_path = rel_path.replace(os.sep, "/")
+        print(f" - Adding {rel_path}")
+        zinfo = zipfile.ZipInfo(rel_path)
+
+        # Normalize permission bits to either 755 (executable) or 644
+        st_mode = os.stat(full_path).st_mode
+
+        if stat.S_ISDIR(st_mode):
+            zinfo.external_attr |= 0x10  # MS-DOS directory flag
+
+        hashsum = hashlib.sha256()
+        with open(full_path, "rb") as src:
+            while True:
+                buf = src.read(1024 * 8)
+                if not buf:
+                    break
+                hashsum.update(buf)
+
+            src.seek(0)
+            wheel.writestr(zinfo, src.read(), compress_type=zipfile.ZIP_DEFLATED)
+
+        size = os.stat(full_path).st_size
+        hash_digest = urlsafe_b64encode(hashsum.digest()).decode("ascii").rstrip("=")
+
+        self._records.append((rel_path, hash_digest, str(size)))
+
+    def _write_metadata_file(self, fp):
+        fp.write(self.format_pkginfo())
+
+    def _write_wheel_file(self, fp):
+        fp.write(
+            WHEEL_FILE_FORMAT.format(
+                pure_lib=self.meta.build is None,
+                tag=self.tag,
+            )
+        )
+
+    def _write_entry_points(self, fp):
+        entry_points = self.meta.entry_points
+        for group_name in sorted(entry_points):
+            fp.write("[{}]\n".format(group_name))
+            for ep in sorted(entry_points[group_name]):
+                fp.write(ep.replace(" ", "") + "\n")
+
+            fp.write("\n")
