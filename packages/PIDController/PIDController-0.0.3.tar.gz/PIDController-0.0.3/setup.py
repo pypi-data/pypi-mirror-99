fe5a6092f90d5fb001d07e39b935d58d678294eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:52 +0000

    Add package PIDController/0.0.3/PIDController-0.0.3.tar.gz

---
+++ packages/PIDController/PIDController-0.0.3.tar.gz/PIDController-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+from pidcontroller import __version__
+setup(
+    name='PIDController',
+    version=__version__,
+    author='xiaochuan',
+    url='https://github.com/xiaochuan-li/PIDController',
+    author_email='lixiaochuan822@gmail.com',
+    description=u'PIDController',
+    packages=['pidcontroller'],
+    install_requires=['matplotlib', 'numpy'],
+    entry_points={
+        'console_scripts': [
+            'sayit=controller:sayit',
+        ]
+    }
+)
