9390a264e5fb9d4d31f3d631efc2571023e92252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:15 +0000

    Add package pydotmap/0.1.0/pydotmap-0.1.0-py3-none-any.whl

---
+++ packages/pydotmap/pydotmap-0.1.0-py3-none-any.whl/pydotmap/__init__.py
@@ -0,0 +1,15 @@
+from .pymap import DotMap
+from .pymap import OrderedDotMap
+from .pymap import dotmap
+from .pymap import ordered_dotmap
+from ._version import __version__ as _version
+
+
+__version__ = _version
+__author__ = "Atul Singh"
+__copyright__ = "Copyright 2021"
+__credits__ = ["https://stackoverflow.com/users/845296/epool", "https://stackoverflow.com/users/1509797/volodymyr"]
+__license__ = "MIT"
+__maintainer__ = "Atul Singh"
+__email__ = "atulsingh0401@gmail.com"
+__status__ = "Production"
