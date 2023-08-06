a8b9942b4d6bd16ef4aecfb1cea8d0fd41b3ba9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:14 +0000

    Add package cli-essentials/0.0.2/cli-essentials-0.0.2.tar.gz

---
+++ packages/cli-essentials/cli-essentials-0.0.2.tar.gz/cli-essentials-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cli-essentials",
+    version="0.0.2",
+    author="Daniel Gill",
+    description="Python CLI building essentials",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanGill/cli-essentials",
+    py_modules=["CLI", "colors"],
+    packages=setuptools.find_packages(),
+    keywords="cli-essentials cli essentials simple color colour easy basic",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='~=3.3',
+    license="MIT",
+)
