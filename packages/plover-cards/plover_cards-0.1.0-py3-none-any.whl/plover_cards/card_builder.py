0a2bcf3ef93e0ee36ef08aeb289f3bc50a78b0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:05 +0000

    Add package plover-cards/0.1.0/plover_cards-0.1.0-py3-none-any.whl

---
+++ packages/plover-cards/plover_cards-0.1.0-py3-none-any.whl/plover_cards/card_builder.py
@@ -0,0 +1,332 @@
+from pathlib import Path
+
+from enum import IntEnum
+
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+
+from plover.oslayer.config import CONFIG_DIR as PLOVER_CONFIG_DIR
+from plover.gui_qt.tool import Tool
+
+from .anki_utils import ANKI_BASE_DIR, get_models
+from .cards import Cards
+from .config import read_config, save_config
+from .card_builder_ui import Ui_CardBuilder
+from .card_suggestions import CardSuggestions
+
+
+class CardTableColumn(IntEnum):
+    FREQUENCY = 0
+    TRANSLATION = 1
+    STROKES = 2
+    IGNORED = 3
+
+
+class CardTableModel(QtCore.QAbstractTableModel):
+    # pylint: disable=no-self-use
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.cards = None
+
+    def set_cards_(self, cards):
+        self.cards = cards
+        self.sort(CardTableColumn.FREQUENCY, QtCore.Qt.DescendingOrder)
+
+    def refresh_(self, card_index):
+        self.dataChanged.emit(
+            self.index(card_index, 0),
+            self.index(card_index, self.columnCount()),
+        )
+
+    def rowCount(self, _parent=None):  # pylint: disable=invalid-name
+        return len(self.cards)
+
+    def columnCount(self, _parent=None):  # pylint: disable=invalid-name
+        return 4
+
+    def data(self, index, role):
+        if role != QtCore.Qt.DisplayRole:
+            return QtCore.QVariant()
+        card = self.cards[index.row()]
+
+        if index.column() == CardTableColumn.FREQUENCY:
+            return card.frequency
+        if index.column() == CardTableColumn.TRANSLATION:
+            return card.translation
+        if index.column() == CardTableColumn.STROKES:
+            if card.ignored:
+                return "(ignored)"
+            return card.chosen_strokes
+        if index.column() == CardTableColumn.IGNORED:
+            return ", ".join(card.similar_ignored)
+        return "??"
+
+    def headerData(self, section, orientation, role):  # pylint: disable=invalid-name
+        if role != QtCore.Qt.DisplayRole or orientation != QtCore.Qt.Horizontal:
+            return QtCore.QVariant()
+
+        if section == CardTableColumn.FREQUENCY:
+            return "Count"
+        if section == CardTableColumn.TRANSLATION:
+            return "Translation"
+        if section == CardTableColumn.STROKES:
+            return "Stroke"
+        if section == CardTableColumn.IGNORED:
+            return "Similar\nIgnored"
+
+        return "??"
+
+    def sort(self, column, order=QtCore.Qt.AscendingOrder):
+        def key(card):
+            if column == CardTableColumn.FREQUENCY:
+                return card.frequency
+            if column == CardTableColumn.TRANSLATION:
+                return card.translation.lower()
+            if column == CardTableColumn.STROKES:
+                if card.chosen_strokes is not None:
+                    return card.chosen_strokes
+            if column == CardTableColumn.IGNORED:
+                if card.similar_ignored is not None:
+                    return card.similar_ignored
+            return ""
+
+        self.cards.sort(key=key, reverse=order == QtCore.Qt.DescendingOrder)
+        self.dataChanged.emit(
+            self.index(0, 0),
+            self.index(self.rowCount(), self.columnCount()),
+        )
+
+
+class CardBuilder(Tool, Ui_CardBuilder):
+    TITLE = "Card Builder"
+    ICON = ":/plover_cards/cards.svg"
+    ROLE = "cardbuilder"
+
+    def __init__(self, engine):
+        super().__init__(engine)
+        self.engine = engine
+
+        hook = self.engine._running_extensions.get("plover_cards_hook")
+        if hook:
+            self.card_suggestions = hook.card_suggestions
+            self.card_suggestions.save()
+        else:
+            self.card_suggestions = CardSuggestions()
+
+        self.setupUi(self)
+
+        self.config = read_config()
+
+        self.setup_disable_start_if_invalid()
+        self.clear_output.setChecked(
+            self.config["options"]["clear_output_on_start"] == "True"
+        )
+        self.setup_file_inputs()
+        self.setup_buttons()
+        self.setup_suggestions()
+
+        self.current_card_index = 0
+        self.cards = None
+        self.card_view_model = None
+
+        self.pages.setCurrentIndex(0)
+        self.start.setFocus()
+        self.show()
+
+    def setup_file_inputs(self):
+        self.anki_path.textChanged.connect(self.update_note_types)
+
+        self.anki_path.setText(self.config["paths"]["anki_collection"])
+        self.anki_browse.clicked.connect(
+            self.on_browse(
+                self.anki_path,
+                "Open Anki collection",
+                ANKI_BASE_DIR,
+                "Anki Collections (*.anki2)",
+            )
+        )
+
+        self.ignore_path.setText(self.config["paths"]["ignore"])
+        self.ignore_browse.clicked.connect(
+            self.on_browse(
+                self.ignore_path,
+                "Open ignore file",
+                PLOVER_CONFIG_DIR,
+                "Text Files (*.txt)",
+            )
+        )
+
+        self.output_path.setText(self.config["paths"]["output"])
+        self.output_browse.clicked.connect(
+            self.on_browse(
+                self.output_path,
+                "Open output file",
+                PLOVER_CONFIG_DIR,
+                "Text Files (*.txt)",
+            )
+        )
+
+    def update_note_types(self, anki_path):
+        self.note_type.clear()
+        for i, model in enumerate(get_models(anki_path)):
+            self.note_type.addItem(model.name, str(model.id))
+            if model.name == self.config["anki"]["note_type"]:
+                self.note_type.setCurrentIndex(i)
+
+    def on_browse(self, label, title, location, extensions):
+        def func():
+            filename = QtWidgets.QFileDialog.getOpenFileName(
+                self, title, location, f"{extensions};;All Files (*)"
+            )[0]
+            label.setText(filename)
+
+        return func
+
+    def setup_disable_start_if_invalid(self):
+        text_inputs = [
+            self.anki_path,
+            self.ignore_path,
+            self.output_path,
+        ]
+
+        def on_text_changed(new_text):
+            if new_text == "":
+                self.start.setEnabled(False)
+                return
+
+            if (
+                all((input.text() != "" for input in text_inputs))
+                and self.note_type.currentText() != ""
+            ):
+                self.start.setEnabled(True)
+            else:
+                self.start.setEnabled(False)
+
+        for text_input in text_inputs:
+            text_input.textChanged.connect(on_text_changed)
+
+    def setup_buttons(self):
+        self.start.clicked.connect(self.on_start)
+        self.prev_card.clicked.connect(self.on_prev_card)
+        self.next_card.clicked.connect(self.on_next_card)
+        self.ignore_card.clicked.connect(self.on_ignore_card)
+        self.clear_card.clicked.connect(self.on_clear_card)
+        self.finish.clicked.connect(self.on_finish)
+
+    def setup_suggestions(self):
+        self.suggestions_model = QtGui.QStandardItemModel(self.suggestions)
+        self.suggestions.setModel(self.suggestions_model)
+        self.suggestions.clicked.connect(self.on_suggestion_click)
+
+    def setup_cards(self):
+        self.cards = Cards(self.config, self.card_suggestions)
+        self.card_view_model = CardTableModel(self.card_view)
+        self.card_view_model.set_cards_(self.cards)
+        self.card_view.setModel(self.card_view_model)
+        self.card_view.horizontalHeader().setSectionResizeMode(
+            QtWidgets.QHeaderView.Interactive
+        )
+        self.card_view.clicked.connect(self.on_card_click)
+
+    def on_start(self):
+        self.config["paths"] = {
+            "anki_collection": self.anki_path.text(),
+            "ignore": self.ignore_path.text(),
+            "output": self.output_path.text(),
+        }
+        self.config["anki"]["note_type"] = self.note_type.currentText()
+        self.config["options"]["clear_output_on_start"] = str(
+            self.clear_output.isChecked()
+        )
+        save_config(self.config)
+
+        self.setup_cards()
+        if self.config["options"]["clear_output_on_start"]:
+            Path(self.output_path.text()).write_text("")
+
+        self.pages.setCurrentIndex(1)
+        if len(self.cards) > 0:
+            self.show_card()
+
+    def show_card(self):
+        if self.current_card_index == 0:
+            self.prev_card.setEnabled(False)
+        else:
+            self.prev_card.setEnabled(True)
+
+        if self.current_card_index == len(self.cards) - 1:
+            self.next_card.setEnabled(False)
+            self.finish.setFocus()
+        else:
+            self.next_card.setEnabled(True)
+            self.next_card.setFocus()
+
+        self.progress.setText(
+            f"Suggestion {self.current_card_index + 1} of {len(self.cards)}"
+        )
+        self.card_view.setCurrentIndex(
+            self.card_view_model.index(self.current_card_index, 0)
+        )
+
+        card = self.cards[self.current_card_index]
+
+        self.translation.setText(card.translation)
+
+        self.suggestions_model.clear()
+        for suggestion in card.stroke_suggestions:
+            item = QtGui.QStandardItem(suggestion)
+            item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+            self.suggestions_model.appendRow(item)
+            if suggestion == card.chosen_strokes:
+                self.suggestions.setCurrentIndex(
+                    self.suggestions_model.indexFromItem(item)
+                )
+
+        if not card.ignored and card.chosen_strokes is None:
+            index = self.suggestions_model.index(0, 0)
+            self.suggestions.setCurrentIndex(index)
+            self.on_suggestion_click(index)
+
+        self.suggestions.show()
+
+    def on_prev_card(self):
+        self.current_card_index -= 1
+        self.show_card()
+
+    def on_next_card(self):
+        self.current_card_index += 1
+        self.show_card()
+
+    def on_clear_card(self):
+        self.suggestions.clearSelection()
+        self.cards.choose_strokes(self.current_card_index, None)
+        self.card_view_model.refresh_(self.current_card_index)
+
+    def on_ignore_card(self):
+        self.cards.ignore(self.current_card_index)
+        self.card_view_model.refresh_(self.current_card_index)
+
+    def on_suggestion_click(self, list_item):
+        self.cards.choose_strokes(self.current_card_index, list_item.data())
+        self.card_view_model.refresh_(self.current_card_index)
+
+    def on_card_click(self, list_item):
+        self.current_card_index = list_item.row()
+        self.show_card()
+
+    def on_finish(self):
+        self.cards.save()
+        save_config(self.config)
+        self.close()
+
+
+if __name__ == "__main__":
+
+    class EngineMock:
+        def __init__(self):
+            self._running_extensions = {}
+
+    app = QtWidgets.QApplication([])
+    dialog = CardBuilder(EngineMock())
+    app.exec_()
---
+++ packages/plover-cards/plover_cards-0.1.0-py3-none-any.whl/plover_cards/cards.py
@@ -0,0 +1,179 @@
+import sqlite3
+
+import csv
+from dataclasses import dataclass, field
+from pathlib import Path
+import re
+
+from .anki_utils import get_models
+
+
+def get_existing_notes(anki_path, note_type):
+    models = get_models(anki_path)
+    note_id = next((model.id for model in models if model.name == note_type))
+
+    conn = sqlite3.connect(f"file:{anki_path}?mode=ro", uri=True)
+    with conn:
+        cursor = conn.cursor()
+
+        cursor.execute("select sfld from notes where mid=?;", [note_id])
+        notes = cursor.fetchall()
+
+    return set(map(lambda r: r[0], notes))
+
+
+def get_ignored_from_file(ignore_file):
+    if not ignore_file.exists():
+        return set()
+    return set(ignore_file.read_text().splitlines())
+
+
+def get_new_notes(new_notes_file):
+    if not new_notes_file.exists():
+        return {}
+
+    lines = new_notes_file.read_text().splitlines()
+    reader = csv.reader(lines)
+    result = {}
+    for line in reader:
+        if len(line) == 2:
+            result[line[0]] = line[1]
+
+    return result
+
+
+@dataclass
+class Card:
+    translation: str
+    stroke_suggestions: list[str]
+    frequency: int
+    chosen_strokes: str = None
+    ignored: bool = False
+    similar_ignored: list[str] = field(default_factory=list)
+
+    def choose_strokes(self, strokes):
+        self.ignored = False
+        self.chosen_strokes = strokes
+
+    def ignore(self):
+        self.ignored = True
+        self.chosen_strokes = None
+
+    def as_note(self):
+        def escape(text):
+            result = text.replace('"', '""')
+            return f'"{result}"'
+
+        return f"{escape(self.translation)},{escape(self.chosen_strokes)}"
+
+
+def strokes_sort_key(strokes):
+    num_strokes = strokes.count("/")
+    return f"{num_strokes}{len(strokes)}{strokes}"
+
+
+def similar_words(word):
+    # does the reverse of tucked in suffix keys
+    # https://github.com/openstenoproject/plover/blob/91a84e16403e9d7470d0192c3b5484e422060a0b/plover/system/english_stenotype.py#L32
+
+    replacements = [
+        ("s$", ""),
+        ("es$", ""),
+        ("ies$", "y"),
+        ("ves$", "f"),
+        ("ing$", ""),
+        (r"(.)\1ing$", r"\1"),
+        ("ing$", "e"),
+        ("ying$", "ie"),
+        ("ed$", ""),
+        ("ied$", "y"),
+        ("eed$", "ee"),
+    ]
+
+    words = set()
+    for replacement in replacements:
+        (similar_word, count) = re.subn(replacement[0], replacement[1], word)
+        if count > 0:
+            words.add(similar_word)
+
+    words.add(word.lower())
+
+    return words
+
+
+def create_cards(card_suggestions, ignored, new_notes):
+    suggestions = card_suggestions.card_suggestions
+
+    cards = []
+    for phrase, data in suggestions.copy().items():
+        if phrase in ignored:
+            card_suggestions.delete(phrase)
+        else:
+            card = Card(
+                translation=phrase,
+                stroke_suggestions=sorted(list(data["strokes"]), key=strokes_sort_key),
+                frequency=data["frequency"],
+                chosen_strokes=new_notes.get(phrase, None),
+                similar_ignored=list(similar_words(phrase).intersection(ignored)),
+            )
+            cards.append(card)
+
+    return cards
+
+
+class Cards:
+    def __init__(self, config, card_suggestions):
+        self.ignore_path = Path(config["paths"]["ignore"])
+        self.output_path = Path(config["paths"]["output"])
+
+        existing_notes = get_existing_notes(
+            config["paths"]["anki_collection"], config["anki"]["note_type"]
+        )
+        self.ignored = get_ignored_from_file(self.ignore_path)
+
+        new_notes = get_new_notes(self.output_path)
+        self.cards = create_cards(
+            card_suggestions,
+            existing_notes.union(self.ignored),
+            new_notes,
+        )
+
+        self.new_ignored = set()
+
+    def __getitem__(self, index):
+        return self.cards[index]
+
+    def __len__(self):
+        return len(self.cards)
+
+    def choose_strokes(self, index, strokes):
+        card = self.cards[index]
+        card.choose_strokes(strokes)
+        self.new_ignored.discard(card.translation)
+
+    def ignore(self, index):
+        card = self.cards[index]
+        card.ignore()
+        self.new_ignored.add(card.translation)
+
+    def save(self):
+        self.output_path.parent.mkdir(parents=True, exist_ok=True)
+        with self.output_path.open("a") as f:
+            f.write(self._as_notes())
+            f.write("\n")
+
+        all_ignored = self.ignored.union(self.new_ignored)
+        self.ignore_path.parent.mkdir(parents=True, exist_ok=True)
+        self.ignore_path.write_text("\n".join(sorted(list(all_ignored))))
+
+    def sort(self, *args, **kwargs):
+        self.cards.sort(*args, **kwargs)
+
+    def _as_notes(self):
+        return "\n".join(
+            [
+                card.as_note()
+                for card in self.cards
+                if not card.ignored and card.chosen_strokes
+            ]
+        )
