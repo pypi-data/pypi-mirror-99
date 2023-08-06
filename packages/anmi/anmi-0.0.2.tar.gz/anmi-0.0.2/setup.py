ef3dbd8008e9b796f89290f7ae00864dee79abf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:00 +0000

    Add package anmi/0.0.2/anmi-0.0.2.tar.gz

---
+++ packages/anmi/anmi-0.0.2.tar.gz/anmi-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name="anmi",  # Replace with your own username
+    version="0.0.2",
+    author="alexmascension",
+    author_email="alexmascension@gmail.com",
+    description="Archivos para correr ANMI",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexmascension/ANMI",
+    project_urls={"Bug Tracker": "https://github.com/alexmascension/ANMI/issues",},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
