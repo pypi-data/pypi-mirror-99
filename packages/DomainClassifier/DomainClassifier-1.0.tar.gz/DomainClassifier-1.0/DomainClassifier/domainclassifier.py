7044119624cddcdc8bc6637618fae121ba043d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:12 +0000

    Add package DomainClassifier/1.0/DomainClassifier-1.0.tar.gz

---
+++ packages/DomainClassifier/DomainClassifier-1.0.tar.gz/DomainClassifier-1.0/DomainClassifier/domainclassifier.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+"""DomainClassifier is a simple Python library to extract and classify Internet
+domains from raw text files following their existence, localization or
+attributes.
+"""
+
+import re
+import dns.resolver
+import IPy
+import socket
+import time
+from datetime import date, timedelta
+
+try:
+    #python 3
+    import urllib.request as urllib
+except:
+    #python 2
+    import urllib2 as urllib
+
+try:
+   from pybgpranking import BGPRanking
+except:
+    print ("pybgpranking is not installed - ranking of ASN values won't be possible")
+__author__ = "Alexandre Dulaunoy"
+__copyright__ = "Copyright 2012-2021, Alexandre Dulaunoy"
+__license__ = "AGPL version 3"
+__version__ = "0.9"
+
+
+class Extract:
+
+    """DomainClassifier Extract class is the base class for extracting domains
+    from a rawtext stream. When call, the rawtext parameter is a string
+    containing the raw data to be process."""
+
+    def __init__(self, rawtext=None, nameservers=['8.8.8.8']):
+        self.rawtext = rawtext
+        self.presolver = dns.resolver.Resolver()
+        self.presolver.nameservers = nameservers
+        self.presolver.lifetime = 1.0
+        self.bgprankingserver = 'pdns.circl.lu'
+        self.vdomain = []
+        self.listtld = []
+        self.domain = self.potentialdomain()
+
+    """__origin is a private function to the ASN lookup for an IP address via
+    the Team Cymru DNS interface. ipadd is a string contain the IP address in a
+    decimal form."""
+
+    def __origin(self, ipaddr=None):
+
+        if ipaddr:
+            clook = IPy.IP(str(ipaddr)).reverseName().replace('.in-addr.arpa.', '.origin.asn.cymru.com')
+            try:
+                a = self.presolver.query(clook, 'TXT')
+            except dns.resolver.NXDOMAIN:
+                return None
+            except dns.exception.Timeout:
+                return None
+        if a:
+            x = str(a[0]).split("|")
+            # why so many spaces?
+            x = list( map(lambda t: t.replace("\"", "").strip(), x) )
+            return (x[0], x[2], a[0])
+        else:
+            return None
+    """__bgpanking return the ranking the float value of an ASN.
+    """
+    def __bgpranking(self, asn=None):
+        if asn:
+            bgpranking = BGPRanking()
+            value = bgpranking.query(asn, date=(date.today() - timedelta(1)).isoformat())
+            return value['response']['ranking']['rank']
+
+    def __updatelisttld(self):
+        ianatldlist = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
+        req = urllib.Request(ianatldlist)
+        req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0')
+        tlds = ( urllib.urlopen(req).read() ).decode('utf8')
+        tlds = tlds.split("\n")
+        for tld in tlds:
+            self.listtld.append(tld.lower())
+
+    def __listtld(self):
+        if not self.listtld:
+            self.__updatelisttld()
+        self.cleandomain = []
+        if self.domain is None:
+            return False
+        for domain in self.domain:
+            lastpart = domain.rsplit(".")[-1:][0]
+            for tld in self.listtld:
+                if lastpart == tld:
+                    self.cleandomain.append(domain)
+
+        return self.cleandomain
+
+    def text(self, rawtext=False):
+        if rawtext:
+            self.rawtext = rawtext
+            self.domain = self.potentialdomain()
+            self.vdomain = []
+            return True
+        return False
+    """potentialdomain method extracts potential domains matching any
+    string that is a serie of string with maximun 63 character separated by a
+    dot. The method used the rawtext defined at the instantiation of the class.
+    This return a list of a potential domain."""
+    def potentialdomain(self, validTLD=True):
+        self.domain = []
+        domain = re.compile(r'\b([a-zA-Z\d-]{,63}(\.[a-zA-Z\d-]{,63})+)\b')
+        for x in domain.findall(self.rawtext):
+            if x[0]:
+                self.domain.append(x[0])
+        if validTLD:
+            self.domain = self.__listtld()
+        return self.domain
+
+    """validdomain method used the extracted domains from the domain method to
+    generate a list of valid domain (at least existing in the authoritative DNS
+    server". The records type used are A, AAAA, SOA, MX and CNAME records. This
+    returns a list of existing domain. If the extended flag is true, a set is
+    return with the associated DNS resources found."""
+
+    def validdomain(self, rtype=['A', 'AAAA', 'SOA', 'MX', 'CNAME'], extended=True, passive_dns=False):
+        if extended is False:
+            self.vdomain = set()
+        else:
+            self.vdomain = []
+
+        for domain in self.domain:
+            for dnstype in rtype:
+                try:
+                    answers = self.presolver.query(domain, dnstype)
+                except:
+                    pass
+                else:
+                    # Pasive DNS output
+                    # timestamp||dns-client ||dns-server||RR class||Query||Query Type||Answer||TTL||Count
+                    if passive_dns:
+                        rrset = answers.rrset.to_text().splitlines()
+                        for dns_resp in rrset:
+                            dns_resp = dns_resp.split()
+                            passive_dns_out = '{}||127.0.0.1||{}||{}||{}||{}||{}||{}||1\n'.format(time.time(), self.presolver.nameservers[0], dns_resp[2], domain, dnstype, dns_resp[4], answers.ttl)
+                            self.vdomain.add((passive_dns_out))
+                    elif extended:
+                        self.vdomain.append((domain, dnstype, answers[0]))
+                    else:
+                        self.vdomain.add((domain))
+        return self.vdomain
+
+    """ipaddress method extracts from the domain list the valid IPv4 addresses"""
+
+    def ipaddress(self, extended=False):
+
+        if extended is False:
+            self.ipaddresses = []
+        else:
+            self.ipaddresses = set()
+
+        for d in self.domain:
+            try:
+                ip = socket.gethostbyname(d)
+            except:
+                continue
+
+            if extended is False:
+                self.ipaddresses.append((ip))
+            else:
+                orig = self.__origin(ipaddr=ip)
+                self.ipaddresses.add((ip, str(orig)))
+
+        return self.ipaddresses
+
+    """localizedomain method use the validdomain list (in extended format) to
+    localize per country code the associated resources. The cc argument specifies the
+    country code in ISO 3166-1 alpha-2 format to check for."""
+
+    def localizedomain(self, cc=None):
+        self.localdom = []
+
+        for dom in self.vdomain:
+            if dom[1] == 'A':
+                ip = dom[2]
+                try:
+                    orig = self.__origin(ipaddr=dom[2])[1]
+                except:
+                    continue
+                if(orig == cc):
+                    self.localdom.append(dom)
+            elif dom[1] == 'CNAME':
+                cname = str(dom[2])
+                ip = socket.gethostbyname(cname)
+                try:
+                    orig = self.__origin(ipaddr=ip)[1]
+                except:
+                    continue
+                if(orig == cc):
+                    self.localdom.append(dom)
+        return self.localdom
+
+    """rankdomain method use the validdomain list (in extended format to rank
+    each domain with an IP address. Return a sorted list of tuples (ranking,
+    domain).
+    """
+
+    def rankdomain(self):
+        self.rankdom = []
+
+        if self.vdomain:
+            for dom in self.vdomain:
+                rank = None
+                asn = None
+                if dom[1] == 'A':
+                    ip = dom[2]
+                    o = self.__origin(ipaddr=dom[2])
+                    if o:
+                        asn = o[0]
+                    rank = self.__bgpranking(asn)
+                    t = (rank, dom[0])
+                    self.rankdom.append(t)
+                elif dom[1] == 'CNAME':
+                    cname = str(dom[2])
+                    try:
+                        ip = socket.gethostbyname(cname)
+                    except:
+                        continue
+                    try:
+                        asn = self.__origin(ipaddr=ip)[0]
+                    except TypeError:
+                        continue
+                    rank = self.__bgpranking(asn)
+                    t = (rank, dom[0])
+                    self.rankdom.append(t)
+            return sorted(self.rankdom, key=lambda d: d[0])
+
+    """exclude domains from a regular expression. If validdomain was called,
+    it's only on the valid domain list."""
+
+    """exclude domains from a regular expression. If validdomain was called,
+    it's only on the valid domain list."""
+
+    def exclude(self, expression=None):
+        self.cleandomain = []
+
+        excludefilter = re.compile(expression)
+
+        if not self.vdomain:
+            domains = self.domain
+        else:
+            domains = self.vdomain
+
+        for dom in domains:
+            if type(dom) == tuple:
+                dom = dom[0]
+
+            if excludefilter.search(dom):
+                pass
+            else:
+                self.cleandomain.append(dom)
+        return self.cleandomain
+
+    def include(self, expression=None):
+        self.cleandomain = []
+
+        includefilter = re.compile(expression)
+
+        if not self.vdomain:
+            domains = self.domain
+        else:
+            domains = self.vdomain
+
+        for dom in domains:
+            if type(dom) == tuple:
+                dom = dom[0]
+            if includefilter.search(dom):
+                    self.cleandomain.append(dom)
+
+        return set(self.cleandomain)
+
+if __name__ == "__main__":
+    c = Extract(rawtext="www.foo.lu www.xxx.com this is a text with a domain called test@foo.lu another test abc.lu something a.b.c.d.e end of 1.2.3.4 foo.be www.belnet.be http://www.cert.be/ www.public.lu www.allo.lu quuxtest www.eurodns.com something-broken-www.google.com www.google.lu trailing test www.facebook.com www.nic.ru www.youporn.com 8.8.8.8 201.1.1.1 abc.dontexist")
+    c.text(rawtext="www.abc.lu www.xxx.com random text a test bric broc www.lemonde.fr www.belnet.be www.foo.be")
+    print (c.potentialdomain())
+    print (c.potentialdomain(validTLD=True))
+    print (c.validdomain(extended=True))
+    print ("US:")
+    print (c.localizedomain(cc='US'))
+    print ("LU:")
+    print (c.localizedomain(cc='LU'))
+    print ("BE:")
+    print (c.localizedomain(cc='BE'))
+    print ("Ranking:")
+    print (c.rankdomain())
+    print ("List of ip addresses:")
+    print (c.ipaddress(extended=False))
+    print ("Include dot.lu:")
+    print (c.include(expression=r'\.lu$'))
+    print ("Exclude dot.lu:")
+    print (c.exclude(expression=r'\.lu$'))
+    c.text(rawtext="www.lwn.net www.undeadly.org")
+    print (c.potentialdomain(validTLD=True))
+    c.validdomain()
+    print (c.localizedomain(cc='US'))
+    print(c.validdomain(extended=False, passive_dns=True))
---
+++ packages/DomainClassifier/DomainClassifier-1.0.tar.gz/DomainClassifier-1.0/README.md
@@ -0,0 +1,120 @@
+DomainClassifier
+================
+
+DomainClassifier is a simple Python library to extract and classify Internet domains/hostnames/IP addresses from raw unstructured text files following their existence, localization or attributes.
+
+DomainClassifier can be used to extract Internet hosts from any free texts or collected unstructured information. A passive dns output is also available.
+
+![An overview of the DomainClassifier methods](https://raw.github.com/adulau/DomainClassifier/master/doc/domainclassifier-flow.png)
+
+Install
+-------
+
+[DomainClassifier](https://pypi.python.org/pypi/DomainClassifier/) is part of the pypi package. It can be installed using the pip command:
+
+`pip install DomainClassifier`
+
+```python
+
+In [11]: c = DomainClassifier.domainclassifier.Extract(rawtext="www.google.com foo.bar ppp.ppp")
+
+In [12]: c.potentialdomain()
+Out[12]: ['www.google.com', 'foo.bar']
+```
+
+How To Use It
+-------------
+
+[![asciicast](https://asciinema.org/a/c039VES8Ksi9pWbii5F8FqeA6.svg)](https://asciinema.org/a/c039VES8Ksi9pWbii5F8FqeA6)
+
+
+```python
+import DomainClassifier.domainclassifier
+
+c = DomainClassifier.domainclassifier.Extract( rawtext = "www.xxx.com this is a text with a domain called test@foo.lu another test abc.lu something a.b.c.d.e end of 1.2.3.4 foo.be www.belnet.be ht
+tp://www.cert.be/ www.public.lu www.allo.lu quuxtest www.eurodns.com something-broken-www.google.com www.google.lu trailing test www.facebook.com www.nic.ru www.youporn.com 8.8.8.
+8 201.1.1.1")
+
+# extracting potentially valid domains from rawtext
+print c.potentialdomain()
+
+# reduce set of potentially valid domains to existing domains
+# (based on SOA,A,AAAA,CNAME,MX records)
+print c.validdomain(extended=True)
+
+# reduce set of valid domains with DNS records associated to a
+# specified country
+print "US:"
+print c.localizedomain(cc='US')
+print "LU:"
+print c.localizedomain(cc='LU')
+print "BE:"
+print c.localizedomain(cc='BE')
+print "Ranking:"
+print c.rankdomain()
+
+# extract valid IPv4 addresses (using the potential list of valid domains)
+print "List of ip addresses:"
+print c.ipaddress(extended=True)
+
+# some more filtering
+print "Include dot.lu:"
+print c.include(expression=r'\.lu$')
+print "Exclude dot.lu:"
+print c.exclude(expression=r'\.lu$')
+```
+
+### Sample output
+
+```python
+['www.xxx.com', 'foo.lu', 'abc.lu', 'a.b.c.d.e', '1.2.3.4', 'foo.be', 'www.belnet.be', 'www.cert.be', 'www.public.lu', 'www.allo.lu', 'www.eurodns.com', 'something-broken-www.google.com', 'www.google.lu', 'www.facebook.com', 'www.nic.ru', 'www.youporn.com', '8.8.8.8', '201.1.1.1']
+[('www.xxx.com', 'A', <DNS IN A rdata: 67.23.112.226>), ('abc.lu', 'SOA', <DNS IN SOA rdata: neptun.vo.lu. Administrator.vo.lu. 2006063001 86400 7200 2419200 3600>), ('abc.lu', 'MX', <DNS IN MX rdata: 10 proteus.vo.lu.>), ('foo.be', 'A', <DNS IN A rdata: 188.65.217.78>), ('foo.be', 'AAAA', <DNS IN AAAA rdata: 2001:6f8:202:2df::2>), ('foo.be', 'SOA', <DNS IN SOA rdata: ka.quuxlabs.com. adulau.foo.be. 2010121901 21600 3600 604800 86400>), ('foo.be', 'MX', <DNS IN MX rdata: 10 mail.foo.be.>), ('www.belnet.be', 'A', <DNS IN A rdata: 193.190.130.15>), ('www.belnet.be', 'AAAA', <DNS IN AAAA rdata: 2001:6a8:3c80:8300::15>), ('www.belnet.be', 'CNAME', <DNS IN CNAME rdata: fiorano.belnet.be.>), ('www.cert.be', 'A', <DNS IN A rdata: 193.190.198.61>), ('www.cert.be', 'AAAA', <DNS IN AAAA rdata: 2001:6a8:3c80::61>), ('www.cert.be', 'SOA', <DNS IN SOA rdata: ns.belnet.be. hostmaster.belnet.be. 2013053039 360 180 1209600 3600>), ('www.cert.be', 'MX', <DNS IN MX rdata: 10 asp-mxa.belnet.be.>), ('www.cert.be', 'CNAME', <DNS IN CNAME rdata: cert.be.>), ('www.public.lu', 'A', <DNS IN A rdata: 194.154.200.74>), ('www.allo.lu', 'A', <DNS IN A rdata: 80.90.47.69>), ('www.eurodns.com', 'A', <DNS IN A rdata: 80.92.65.165>), ('www.google.lu', 'A', <DNS IN A rdata: 173.194.66.94>), ('www.google.lu', 'AAAA', <DNS IN AAAA rdata: 2a00:1450:400c:c03::5e>), ('www.facebook.com', 'A', <DNS IN A rdata: 31.13.64.1>), ('www.facebook.com', 'AAAA', <DNS IN AAAA rdata: 2a03:2880:10:8f07:face:b00c::1>), ('www.facebook.com', 'MX', <DNS IN MX rdata: 10 msgin.t.facebook.com.>), ('www.facebook.com', 'CNAME', <DNS IN CNAME rdata: star.c10r.facebook.com.>), ('www.nic.ru', 'A', <DNS IN A rdata: 194.85.61.42>), ('www.nic.ru', 'MX', <DNS IN MX rdata: 0 nomail.nic.ru.>), ('www.youporn.com', 'A', <DNS IN A rdata: 31.192.116.24>), ('www.youporn.com', 'SOA', <DNS IN SOA rdata: pdns1.ultradns.net. dns.manwin.com. 2012041840 86400 86400 86400 86400>), ('www.youporn.com', 'MX', <DNS IN MX rdata: 20 smtp-scan01.mx.reflected.net.>), ('www.youporn.com', 'CNAME', <DNS IN CNAME rdata: youporn.com.>)]
+US:
+[('www.xxx.com', 'A', <DNS IN A rdata: 67.23.112.226>), ('www.google.lu', 'A', <DNS IN A rdata: 173.194.66.94>)]
+LU:
+[('www.public.lu', 'A', <DNS IN A rdata: 194.154.200.74>), ('www.allo.lu', 'A', <DNS IN A rdata: 80.90.47.69>), ('www.eurodns.com', 'A', <DNS IN A rdata: 80.92.65.165>)]
+BE:
+[('foo.be', 'A', <DNS IN A rdata: 188.65.217.78>), ('www.belnet.be', 'A', <DNS IN A rdata: 193.190.130.15>), ('www.belnet.be', 'CNAME', <DNS IN CNAME rdata: fiorano.belnet.be.>), ('www.cert.be', 'A', <DNS IN A rdata: 193.190.198.61>), ('www.cert.be', 'CNAME', <DNS IN CNAME rdata: cert.be.>)]
+Ranking:
+[(1.0, 'www.youporn.com'), (1.0, 'www.youporn.com'), (1.0000120563271599, 'www.belnet.be'), (1.0000120563271599, 'www.belnet.be'), (1.0000120563271599, 'www.cert.be'), (1.0000120563271599, 'www.cert.be'), (1.0000372023809501, 'foo.be'), (1.0001395089285701, 'www.public.lu'), (1.00015419407895, 'www.allo.lu'), (1.0003662109375, 'www.eurodns.com'), (1.0004111842105301, 'www.xxx.com'), (1.0005944293478299, 'www.nic.ru'), (1.0024646577381, 'www.facebook.com'), (1.0024646577381, 'www.facebook.com'), (1.002635288165, 'www.google.lu')]
+List of ip addresses:
+('15169', 'AU', <DNS IN TXT rdata: "15169 | 1.2.3.0/24 | AU | apnic | 2011-08-11">)
+('15169', 'US', <DNS IN TXT rdata: "15169 | 8.8.8.0/24 | US | arin | 1992-12-01">)
+('27699', 'BR', <DNS IN TXT rdata: "27699 | 201.1.0.0/17 | BR | lacnic | 2003-12-08">)
+set([('201.1.1.1', '(\'27699\', \'BR\', <DNS IN TXT rdata: "27699 | 201.1.0.0/17 | BR | lacnic | 2003-12-08">)'), ('8.8.8.8', '(\'15169\', \'US\', <DNS IN TXT rdata: "15169 | 8.8.8.0/24 | US | arin | 1992-12-01">)'), ('1.2.3.4', '(\'15169\', \'AU\', <DNS IN TXT rdata: "15169 | 1.2.3.0/24 | AU | apnic | 2011-08-11">)')])
+Include dot.lu:
+['abc.lu', 'abc.lu', 'www.public.lu', 'www.allo.lu', 'www.google.lu', 'www.google.lu']
+Exclude dot.lu:
+['www.xxx.com', 'foo.be', 'foo.be', 'foo.be', 'foo.be', 'www.belnet.be', 'www.belnet.be', 'www.belnet.be', 'www.cert.be', 'www.cert.be', 'www.cert.be', 'www.cert.be', 'www.cert.be', 'www.eurodns.com', 'www.facebook.com', 'www.facebook.com', 'www.facebook.com', 'www.facebook.com', 'www.nic.ru', 'www.nic.ru', 'www.youporn.com', 'www.youporn.com', 'www.youporn.com', 'www.youporn.com']
+```
+
+### Software Required
+
+* Python (tested successfully on version 2.6, 2.7 and 3.5)
+* dnspython library - http://www.dnspython.org/
+* IPy library
+* [pybgpranking](https://github.com/D4-project/BGP-Ranking/tree/master/client) to get malicious ranking of BGP AS number via [BGP Ranking](https://github.com/D4-project/BGP-Ranking)
+
+### Software using DomainClassifier
+
+* [AIL framework - Analysis Information Leak framework](https://github.com/ail-project/ail-framework)
+
+### License
+
+~~~~
+Copyright (C) 2012-2021 Alexandre Dulaunoy - a(at)foo.be
+Copyright (C) 2021 Aurelien Thirion
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+~~~~
