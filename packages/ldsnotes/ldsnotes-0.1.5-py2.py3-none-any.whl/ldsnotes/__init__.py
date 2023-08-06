aceb3964d5dc55cbd637164ec10c7b2a8d641ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:30 +0000

    Add package ldsnotes/0.1.5/ldsnotes-0.1.5-py2.py3-none-any.whl

---
+++ packages/ldsnotes/ldsnotes-0.1.5-py2.py3-none-any.whl/ldsnotes/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for LDS Notes."""
+# flake8: noqa
+
+__author__ = """Easton Potokar"""
+__email__ = 'contagon6@gmail.com'
+__version__ = '0.1.5'
+
+from ldsnotes.content import Content
+from ldsnotes.annotations import Bookmark, Journal, Highlight, Reference, Annotation
+from ldsnotes.note import Notes, Tag, Folder
