8167cf892af7856110004e0cb20f782ea08b78d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:32 +0000

    Add package we-pyutils/0.0.9/we_pyutils-0.0.9.tar.gz

---
+++ packages/we-pyutils/we_pyutils-0.0.9.tar.gz/we_pyutils-0.0.9/we_pyutils/t/proxy/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    : __init__.py.py
+@Time    : 2020-02-09 8:56
+@Author  : ZENKR
+@Email   : zenkr@qq.com
+@Software: PyCharm
+@Desc    :
+@license : Copyright (c) 2020 WingEase Technology Co.,Ltd. All Rights Reserved.
+"""
