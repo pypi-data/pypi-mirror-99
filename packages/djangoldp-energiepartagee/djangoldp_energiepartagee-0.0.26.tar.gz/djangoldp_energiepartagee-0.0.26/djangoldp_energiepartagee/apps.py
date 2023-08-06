ae273d7d106be5e23e5374323c2f259ca604f9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:52 +0000

    Add package djangoldp-energiepartagee/0.0.26/djangoldp_energiepartagee-0.0.26.tar.gz

---
+++ packages/djangoldp-energiepartagee/djangoldp_energiepartagee-0.0.26.tar.gz/djangoldp_energiepartagee-0.0.26/djangoldp_energiepartagee/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoldEnergiepartageeConfig(AppConfig):
+    name = 'djangoldp_energiepartagee'
---
+++ packages/djangoldp-energiepartagee/djangoldp_energiepartagee-0.0.26.tar.gz/djangoldp_energiepartagee-0.0.26/djangoldp_energiepartagee/djangoldp_settings.py
@@ -0,0 +1,1 @@
+REGISTRATION_FIELDS = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
