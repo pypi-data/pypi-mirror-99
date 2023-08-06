65787e5e043bbb587fe5b51f0d9cebcbb8e420c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:02 +0000

    Add package scprep/1.1.0/scprep-1.1.0-py3-none-any.whl

---
+++ packages/scprep/scprep-1.1.0-py3-none-any.whl/scprep/sanitize.py
@@ -0,0 +1,117 @@
+from . import utils
+
+import numpy as np
+import pandas as pd
+import warnings
+
+
+def check_numeric(data, dtype="float", copy=None, suppress_errors=False):
+    """Check a matrix contains only numeric data.
+
+    Parameters
+    ----------
+    data : array-like, shape=[n_samples, n_features]
+        Input data
+    dtype : str or `np.dtype`, optional (default: 'float')
+        Data type to which to coerce the data
+    copy : bool or None, optional (default: None)
+        Copy the data before coercion. If None, default to
+        False for all datatypes except pandas.SparseDataFrame
+    suppress_errors : bool, optional (default: False)
+        Suppress errors from non-numeric data
+
+    Returns
+    -------
+    data : array-like, shape=[n_samples, n_features]
+        Output data as numeric type
+
+    Raises
+    ------
+    TypeError : if `data` cannot be coerced to `dtype`
+    """
+    if copy is None:
+        copy = utils.is_SparseDataFrame(data)
+    try:
+        return data.astype(dtype, copy=copy)
+    except TypeError as e:
+        if utils.is_SparseDataFrame(data):
+            if not copy:
+                raise TypeError(
+                    "pd.SparseDataFrame does not support "
+                    "copy=False. Please use copy=True."
+                )
+            else:
+                return data.astype(dtype)
+        else:
+            raise e
+    except ValueError:
+        if suppress_errors:
+            warnings.warn(
+                "Data is not numeric. Many scprep functions will not work.",
+                RuntimeWarning,
+            )
+            return data
+        else:
+            raise
+
+
+def check_index(data, copy=False):
+    """Ensure that the data index is unique in a pandas DataFrame.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Input data
+    copy : bool, optional (default: True)
+        If True, return a modified copy of the data. Otherwise modify it in place.
+
+    Returns
+    -------
+    data : pd.DataFrame
+        Sanitized data
+    """
+    if not hasattr(data, "index"):
+        warnings.warn(
+            "scprep.sanitize.check_index only accepts pandas input", UserWarning
+        )
+        return data
+    duplicated = data.index.duplicated()
+    if np.any(duplicated):
+        new_index = list(data.index)
+        is_mi = isinstance(data.index, pd.MultiIndex)
+        for idx in np.unique(data.index[duplicated]):
+            if is_mi:
+                rename = np.all(
+                    [
+                        data.index.get_level_values(data.index.names[i]) == idx[i]
+                        for i in range(len(idx))
+                    ],
+                    axis=0,
+                )
+            else:
+                rename = data.index == idx
+            rename = np.argwhere(rename).flatten()
+
+            for i, idx in enumerate(rename):
+                if i == 0:
+                    continue
+                if is_mi:
+                    new_index[idx] = new_index[idx][:-1] + (
+                        "{}.{}".format(new_index[idx][-1], i),
+                    )
+                else:
+                    new_index[idx] = "{}.{}".format(new_index[idx], i)
+
+            print_new_index = ", ".join([str(new_index[r]) for r in rename])
+            warnings.warn(
+                "Renamed {} copies of index {} to ({})".format(
+                    len(rename), new_index[rename[0]], print_new_index
+                ),
+                RuntimeWarning,
+            )
+        if copy:
+            data = data.copy()
+        if is_mi:
+            new_index = pd.MultiIndex.from_tuples(new_index, names=data.index.names)
+        data.index = new_index
+    return data
---
+++ packages/scprep/scprep-1.1.0-py3-none-any.whl/scprep/transform.py
@@ -0,0 +1,133 @@
+from . import utils
+from scipy import sparse
+
+import numpy as np
+import warnings
+
+
+def sqrt(data):
+    """Square root transform.
+
+    Parameters
+    ----------
+    data : array-like, shape=[n_samples, n_features]
+        Input data
+
+    Returns
+    -------
+    data : array-like, shape=[n_samples, n_features]
+        Square root transformed output data
+
+    Raises
+    ------
+    ValueError : if data has negative values
+    """
+    if not utils.matrix_non_negative(data, allow_equal=True):
+        raise ValueError("Cannot square root transform negative values")
+    return utils.matrix_transform(data, np.sqrt)
+
+
+def log(data, pseudocount=1, base=10):
+    """Log transform.
+
+    Parameters
+    ----------
+    data : array-like, shape=[n_samples, n_features]
+        Input data
+    pseudocount : int, optional (default: 1)
+        Pseudocount to add to values before log transform.
+        If data is sparse, pseudocount must be 1 such that
+        log(0 + pseudocount) = 0
+    base : {2, 'e', 10}, optional (default: 10)
+        Logarithm base.
+
+    Returns
+    -------
+    data : array-like, shape=[n_samples, n_features]
+        Log transformed output data
+
+    Raises
+    ------
+    ValueError : if data has zero or negative values
+    RuntimeWarning : if data is sparse and pseudocount != 1
+    """
+    data_min = utils.matrix_min(data)
+    if pseudocount + data_min <= 0:
+        raise ValueError(
+            "Required pseudocount + min(data) ({}) > 0. "
+            "Got pseudocount = {}".format(utils.matrix_min(data), pseudocount)
+        )
+    elif pseudocount + data_min != 1 and (
+        sparse.issparse(data)
+        or utils.is_SparseDataFrame(data)
+        or utils.is_sparse_dataframe(data)
+    ):
+        req = "1 - min(data) ({})".format(1 - data_min) if data_min != 0 else "1"
+        warnings.warn(
+            "log transform on sparse data requires "
+            "pseudocount = {}. Got {}".format(req, pseudocount),
+            RuntimeWarning,
+        )
+        pseudocount = data_min + 1
+    if base == 2:
+        log = np.log2
+    elif base == "e":
+        log = np.log
+    elif base == 10:
+        log = np.log10
+    else:
+        raise ValueError("Expected base in [2, 'e', 10]. Got {}".format(base))
+    return utils.matrix_transform(data, lambda data: log(data + pseudocount))
+
+
+def arcsinh(data, cofactor=5):
+    """Inverse hyperbolic sine transform.
+
+    Parameters
+    ----------
+    data : array-like, shape=[n_samples, n_features]
+        Input data
+    cofactor : float or None, optional (default: 5)
+        Factor by which to divide data before arcsinh transform
+
+    Returns
+    -------
+    data : array-like, shape=[n_samples, n_features]
+        Inverse hyperbolic sine transformed output data
+
+    Raises
+    ------
+    ValueError : if cofactor <= 0
+    """
+    if cofactor <= 0:
+        raise ValueError("Expected cofactor > 0 or None. " "Got {}".format(cofactor))
+    if cofactor is not None:
+        data = data / cofactor
+    return utils.matrix_transform(data, np.arcsinh)
+
+
+def sqrt_transform(*args, **kwargs):  # noqa
+    warnings.warn(
+        "scprep.transform.sqrt_transform is deprecated. Please use "
+        "scprep.transform.sqrt in future.",
+        FutureWarning,
+    )
+    return sqrt(*args, **kwargs)
+
+
+def log_transform(*args, **kwargs):  # noqa
+    warnings.warn(
+        "scprep.transform.log_transform is deprecated. Please use "
+        "scprep.transform.log in future.",
+        FutureWarning,
+    )
+    return log(*args, **kwargs)
+
+
+def arcsinh_transform(*args, **kwargs):  # noqa
+    warnings.warn(
+        "scprep.transform.arcsinh_transform is deprecated. Please "
+        "use scprep.transform.arcsinh in future.",
+        FutureWarning,
+    )
+    return arcsinh(*args, **kwargs)
