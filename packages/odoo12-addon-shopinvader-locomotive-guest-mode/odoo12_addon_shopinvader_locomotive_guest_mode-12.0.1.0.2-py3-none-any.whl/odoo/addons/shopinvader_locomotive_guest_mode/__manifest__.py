c2c96fd409fa9a189c36b3acaad03a80ec4793ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:04 +0000

    Add package odoo12-addon-shopinvader-locomotive-guest-mode/12.0.1.0.2/odoo12_addon_shopinvader_locomotive_guest_mode-12.0.1.0.2-py3-none-any.whl

---
+++ packages/odoo12-addon-shopinvader-locomotive-guest-mode/odoo12_addon_shopinvader_locomotive_guest_mode-12.0.1.0.2-py3-none-any.whl/odoo/addons/shopinvader_locomotive_guest_mode/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Locomotive Guest Mode",
+    "summary": """
+        Shopinvader guest mode for locomotive""",
+    "version": "12.0.1.0.2",
+    "license": "AGPL-3",
+    "development_status": "Stable/Production",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["shopinvader_guest_mode", "shopinvader_locomotive"],
+    "installable": True,
+}
