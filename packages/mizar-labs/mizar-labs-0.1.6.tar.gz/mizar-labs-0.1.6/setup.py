675e37801367c9bcfcf2ea78006a0475b9b9b657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:17 +0000

    Add package mizar-labs/0.1.6/mizar-labs-0.1.6.tar.gz

---
+++ packages/mizar-labs/mizar-labs-0.1.6.tar.gz/mizar-labs-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages
+from setuptools import setup
+
+__version__ = "0.1.6"
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mizar-labs",
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    author="MizarAI",
+    author_email="info@mizar.ai",
+    install_requires=requirements,
+    description="Package for building financial machine learning models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MizarAI/mizar-labs",
+    project_urls={
+        "Bug Tracker": "https://github.com/MizarAI/mizar-labs/issues",
+    },
+    python_requires=">=3.8"
+)
