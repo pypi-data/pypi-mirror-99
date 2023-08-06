c5e6cdde297d23da3f9b7fc64f651e248084ffe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:00 +0000

    Add package userlib-spinD/0.0.22/userlib_spinD-0.0.22-py3-none-any.whl

---
+++ packages/userlib-spinD/userlib_spinD-0.0.22-py3-none-any.whl/python3/testsignatures.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+r"""
+This constants library holds the test signatures. Test signatures have different use cases:
+
+- creating unique _temp_-folders for tests.
+
+- identify test with single integer
+"""
+# Test Ids Multilayer Test =============================================================================================
+MULTI_PRECISIONWITHOUTDAMPING = 1
+MULTI_PRECISIONWITHDAMPING = 2
+MULTI_DOMAINWALLS_Z = 3
