b359b517ab166c1362258af0e93d18f9cc14cc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:42 +0000

    Add package adafruit-circuitpython-imageload/0.14.0/adafruit-circuitpython-imageload-0.14.0.tar.gz

---
+++ packages/adafruit-circuitpython-imageload/adafruit-circuitpython-imageload-0.14.0.tar.gz/adafruit-circuitpython-imageload-0.14.0/docs/index.rst
@@ -0,0 +1,58 @@
+.. include:: ../README.rst
+
+Table of Contents
+=================
+
+.. toctree::
+    :maxdepth: 4
+    :hidden:
+
+    self
+
+.. toctree::
+    :caption: Examples
+
+    examples
+
+.. toctree::
+    :caption: API Reference
+    :maxdepth: 3
+
+    api
+
+.. toctree::
+    :caption: Tutorials
+
+    CircuitPython displayio Setup for TFT FeatherWings <https://learn.adafruit.com/using-circuitpython-displayio-with-a-tft-featherwing>
+
+.. toctree::
+    :caption: Development
+    :maxdepth: 3
+
+    developing
+
+.. toctree::
+    :caption: Related Products
+
+    Adafruit Feather M4 Express - Featuring ATSAMD51 - ATSAMD51 Cortex M4<https://www.adafruit.com/product/3857>
+    Adafruit TFT FeatherWing - 3.5" 480x320 Touchscreen for Feathers <https://www.adafruit.com/product/3651>
+    TFT FeatherWing - 2.4" 320x240 Touchscreen For All Feathers <https://www.adafruit.com/product/3315>
+    Adafruit Mini Color TFT with Joystick FeatherWing <https://www.adafruit.com/product/3321>
+
+.. toctree::
+    :caption: Other Links
+
+    Download <https://github.com/adafruit/Adafruit_CircuitPython_ImageLoad/releases/latest>
+    CircuitPython Reference Documentation <https://circuitpython.readthedocs.io>
+    CircuitPython Support Forum <https://forums.adafruit.com/viewforum.php?f=60>
+    Discord Chat <https://adafru.it/discord>
+    Adafruit Learning System <https://learn.adafruit.com>
+    Adafruit Blog <https://blog.adafruit.com>
+    Adafruit Store <https://www.adafruit.com>
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/adafruit-circuitpython-imageload/adafruit-circuitpython-imageload-0.14.0.tar.gz/adafruit-circuitpython-imageload-0.14.0/tests/test_palette_c_interface.py
@@ -0,0 +1,107 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Matt Land
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""
+`adafruit_imageload.tests.test_palette_c_interface`
+====================================================
+
+These tests are to validate the displayio_shared_bindings classes that other tests are built on.
+
+* Author(s):  Matt Land
+
+"""
+from unittest import TestCase
+from .displayio_shared_bindings import Palette_C_Interface
+
+
+class TestPalette_C_Interface(TestCase):
+    @staticmethod
+    def test_init_mono():
+        Palette_C_Interface(1)
+
+    @staticmethod
+    def test_init_color():
+        Palette_C_Interface(256)
+
+    @staticmethod
+    def test_set_int():
+        palette = Palette_C_Interface(1)
+        palette[0] = 0xFFFFFF
+
+    def test_get_int(self):
+        palette = Palette_C_Interface(1)
+        palette[0] = 0xFFFFFF
+        self.assertEqual(0xFFFFFF, palette[0])
+
+    @staticmethod
+    def test_set_byte():
+        palette = Palette_C_Interface(1)
+        palette[0] = b"\xFF\xFF\xFF"
+
+    def test_get_byte(self):
+        palette = Palette_C_Interface(1)
+        palette[0] = b"\xFF\xFF\xFF"
+        self.assertEqual(b"\xFF\xFF\xFF", palette[0])
+
+    @staticmethod
+    def test_set_bytearray():
+        palette = Palette_C_Interface(1)
+        palette[0] = bytearray(b"\xFF\xFF\xFF")
+
+    def test_prevents_out_of_range(self):
+        palette = Palette_C_Interface(1)
+        try:
+            palette[1] = 0xFFFFFF
+            self.fail("exception should have already thrown")
+        except ValueError as err:
+            if "greater than allowed" not in str(err):
+                raise
+
+    def test_prevents_set_non_allowed(self):
+        palette = Palette_C_Interface(1)
+        try:
+            palette[0] = "\xFF\xFF\xFF"  # attempt with a string, which is not allowed
+            self.fail("exception should have thrown")
+        except ValueError as err:
+            if "should be" not in str(err):
+                raise
+
+    @staticmethod
+    def test_validate_success():
+        palette = Palette_C_Interface(1)
+        palette[0] = b"\xFF\xFF\xFF"
+        palette.validate()
+
+    def test_validate_fails(self):
+        palette = Palette_C_Interface(2)
+        palette[1] = b"\xFF\xFF\xFF"
+        try:
+            palette.validate()
+            self.fail("exception should have thrown")
+        except IndexError as err:
+            if "palette was initialized" not in str(err):
+                raise
+
+    @staticmethod
+    def test_str():
+        palette = Palette_C_Interface(1)
+        palette[0] = b"\xFF\xFF\xFF"
+        print(str(palette))
