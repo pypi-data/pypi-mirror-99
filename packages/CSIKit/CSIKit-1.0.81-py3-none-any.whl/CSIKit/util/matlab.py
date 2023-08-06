60a407e9de192883653f0e575998de4ab5df198b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:51 +0000

    Add package CSIKit/1.0.81/CSIKit-1.0.81-py3-none-any.whl

---
+++ packages/CSIKit/CSIKit-1.0.81-py3-none-any.whl/CSIKit/util/matlab.py
@@ -0,0 +1,11 @@
+import numpy as np
+np.seterr(divide="ignore")
+
+def dbinv(x: float) -> float:
+    return np.power(10, x/10)
+
+def db(x: float, metric: str="voltage") -> float:
+    if metric == "voltage":
+        return 20 * np.log10(x)
+    elif metric == "pow":
+        return 10 * np.log10(x)
