a3230c3109eeaa70c705c1f3ac45663a783d590a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:40 +0000

    Add package HMOBSTER/0.0.18/HMOBSTER-0.0.18-py3-none-any.whl

---
+++ packages/HMOBSTER/HMOBSTER-0.0.18-py3-none-any.whl/mobster/interface_mobster.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import tqdm
+
+import pyro as pyro
+import numpy as np
+import math
+from pyro.infer.autoguide import AutoDelta
+import pyro.poutine as poutine
+import torch
+import mobster
+from mobster.utils_mobster import *
+from mobster.stopping_criteria import *
+import mobster.model_selection_mobster as ms
+from mobster.calculate_posteriors import *
+from pyro.util import ignore_jit_warnings
+
+
+def fit_mobster(data, K, tail=1, purity=0.96,alpha_prior_concentration = 5, alpha_prior_rate = 10, number_of_trials_clonal_mean=500.,number_of_trials_k=300.,
+         prior_lims_clonal=[0.1, 100000.], prior_lims_k=[0.1, 100000.], stopping = ELBO_stopping_criteria, lr = 0.05,
+                max_it = 5000, e = 0.001, compile = False, CUDA = False, seed = 3):
+
+
+    pyro.set_rng_seed(seed)
+
+    if CUDA:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    if compile:
+        loss = pyro.infer.JitTraceGraph_ELBO
+    else:
+        loss = pyro.infer.TraceGraph_ELBO
+
+    model = mobster.model
+    guide = mobster.guide
+
+    svi = pyro.infer.SVI(model=model,
+                     guide=guide,
+                     optim=pyro.optim.ClippedAdam({"lr": lr}),
+                     loss= loss())
+
+    print('Running MOBSTER on {} karyotypes with {} subclones.'.format(len(data), K), flush=True)
+    if tail == 1:
+        print("Fitting a model with tail", flush = True)
+    else:
+        print("Fitting a model without tail", flush=True)
+    params = {
+        'K' : K,
+        'tail' : tail,
+        'purity' : purity,
+        'alpha_prior_concentration' : alpha_prior_concentration,
+        'alpha_prior_rate' : alpha_prior_rate,
+        'number_of_trials_clonal_mean' : number_of_trials_clonal_mean,
+        'number_of_trials_k' : number_of_trials_k,
+        'prior_lims_clonal' : prior_lims_clonal,
+        'prior_lims_k' : prior_lims_k
+    }
+    loss = run(data, params, svi, stopping, max_it, e)
+
+    params_dict = retrieve_params()
+    print("", flush=True)
+    print("Computing cluster assignements.", flush=True)
+    params_dict = retrieve_posterior_probs(data, params_dict, tail)
+
+
+    ### Caclculate information criteria
+    print("Computing information criteria.", flush=True)
+    likelihood = ms.likelihood(data, params_dict, tail)
+    AIC = ms.AIC(data, params_dict, tail)
+    BIC = ms.BIC(data, params_dict, tail)
+    ICL = ms.ICL(data, params_dict, tail)
+
+    params_dict = format_parameters_for_export(data, params_dict, tail)
+
+
+
+    information_dict =  {"likelihood": likelihood.detach().numpy(),
+                         "AIC": AIC.detach().numpy(),
+                        "BIC" : BIC.detach().numpy(),
+                        "ICL" : ICL.detach().numpy()}
+
+
+    final_dict = {
+        "information_criteria" : information_dict,
+        "model_parameters" : params_dict,
+        "run_parameters" : params,
+        "loss": np.array(loss)
+    }
+    print("Done!", flush = True)
+
+    return final_dict
+
+
+
+def run(data, params, svi, stopping, max_it, e):
+
+    N = ms.number_of_samples(data)
+    data_dict = params.copy()
+    data_dict["data"] = data
+    pyro.clear_param_store()
+    loss = new = svi.step(**data_dict)
+    losses = []
+    t = trange(max_it, desc='Bar desc', leave=True)
+    for i in t:
+
+        t.set_description('ELBO: {:.9f}  '.format(loss / N))
+        t.refresh()
+
+        loss = svi.step(**data_dict)
+        losses.append(loss)
+
+        old, new = new, loss
+
+        if stopping(old, new, e):
+            break
+        if np.isinf(loss) or math.isinf(loss):
+            break
+
+    return losses
+
+def retrieve_params():
+    param_names = pyro.get_param_store()
+    res = {nms: pyro.param(nms) for nms in param_names}
+    return res
---
+++ packages/HMOBSTER/HMOBSTER-0.0.18-py3-none-any.whl/mobster/plotting_mobster.py
@@ -0,0 +1,92 @@
+from scipy.stats import beta, pareto, lognorm
+
+import matplotlib as mpl
+mpl.use('Agg')
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
+import mobster.utils_mobster as mut
+
+
+def plot_results(data, inf_res, bins=50, output = "results.png",fig_height = 4, fig_width = 3, drivers = None):
+
+    all_params = inf_res["model_parameters"]
+    tail = inf_res['run_parameters']['tail']
+    nKar = len(all_params)
+    plt.rcParams["figure.figsize"] = (fig_height * nKar, fig_width * nKar)
+    fig, axs = plt.subplots(nKar)
+    for i, kr in enumerate(data):
+        data_mut = data[kr].detach().numpy()
+        if nKar == 1:
+            axs.hist(data_mut, bins=bins, density=True, alpha=0.48)
+            axs.title.set_text("Karyotype = " + kr)
+        else:
+            axs[i].hist(data_mut, bins=bins, density=True, alpha=0.48)
+            axs[i].title.set_text("Karyotype = " + kr)
+        params = all_params[kr]
+
+        karyos = list(data.keys())
+
+        major = [int(str(i).split(":")[0]) for i in karyos]
+        minor = [int(str(i).split(":")[1]) for i in karyos]
+        theoretical_num_clones = [mut.theo_clonal_list[kr] for kr in karyos]
+        assignment_probs = params["mixture_probs"]
+
+        for j in range(tail, len(assignment_probs)):
+
+            if theoretical_num_clones[i] == 1:
+                cl = "tab:green" if (j - tail) < 1 else "tab:red"
+
+            else:
+                cl = "tab:green" if (j - tail) < 2 else "tab:red"
+
+            a = params["beta_concentration1"][j - tail]
+            b = params["beta_concentration2"][j - tail]
+
+            x = np.linspace(0.05, 1, 200)
+
+            p = beta.pdf(x, a, b) * assignment_probs[j]
+            if nKar == 1:
+                axs.plot(x, p, linewidth=1.5, color=cl)
+            else:
+                axs[i].plot(x, p, linewidth=1.5, color=cl)
+
+        if tail == 1:
+            alpha = params["tail_shape"]
+            nall = mut.theo_allele_list[kr]
+            x = np.linspace(0.05, 1, 200)
+            p = pareto.pdf(x, alpha * nall, scale=params["tail_scale"]) * assignment_probs[0]
+            ci = 2 * (1/params["tail_noise"])
+            p1 = pareto.pdf(x, np.abs(alpha - ci) * nall, scale=params["tail_scale"]) * assignment_probs[0]
+            p2 = pareto.pdf(x, np.abs(alpha + ci)  * nall, scale=params["tail_scale"]) * assignment_probs[0]
+            if nKar == 1:
+                axs.plot(x, p, linewidth=1.5, color="tab:pink")
+
+                axs.fill_between(x, p1, p2, color='pink', alpha=.8)
+            else:
+                axs[i].plot(x, p, linewidth=1.5, color="tab:pink")
+                axs[i].fill_between(x, p1, p2, color='pink', alpha=.8)
+        if drivers is not None:
+            drivers_mut = data_mut[drivers[kr]]
+
+            if(len(drivers_mut) > 0):
+                if nKar == 1:
+                    p = axs.patches
+                    heights = [patch.get_height() for patch in p]
+                    axs.vlines(drivers_mut, 0, np.max(heights))
+                else:
+                    p = axs[i].patches
+                    heights = [patch.get_height() for patch in p]
+                    axs[i].vlines(drivers_mut, 0, np.max(heights))
+
+
+    pink_patch = mpatches.Patch(color='tab:pink', label='Tail')
+    red_patch = mpatches.Patch(color='tab:red', label='Subclonal')
+    green_patch = mpatches.Patch(color='tab:green', label='Clonal')
+
+    plt.legend(handles=[pink_patch, red_patch, green_patch])
+
+    plt.savefig(output, dpi=300)
+
+    plt.clf()
