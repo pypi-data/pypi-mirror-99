a862cb8344f07babe008aea6e2d69cbf74281b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:35 +0000

    Add package tycho-api/1.0.11/tycho-api-1.0.11.tar.gz

---
+++ packages/tycho-api/tycho-api-1.0.11.tar.gz/tycho-api-1.0.11/tycho/exceptions.py
@@ -0,0 +1,14 @@
+class TychoException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message)
+        self.details = details
+
+class StartException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
+class DeleteException(Exception):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
+class ContextException(TychoException):
+    def __init__(self, message, details=""):
+        super().__init__(message, details)
