e00bd296bbd9434b5e9b5142ba02e20e6200fd6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:02 +0000

    Add package labstep/3.0.4/labstep-3.0.4.tar.gz

---
+++ packages/labstep/labstep-3.0.4.tar.gz/labstep-3.0.4/labstep/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylama:ignore=W0611
+# Author: Barney Walker <barney@labstep.com>
+
+"""Labstep Python SDK"""
+
+from .entities.user.facade import login, authenticate
---
+++ packages/labstep/labstep-3.0.4.tar.gz/labstep-3.0.4/labstep/entities/file/model.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Barney Walker <barney@labstep.com>
+
+import os
+from labstep.generic.entity.model import Entity
+
+
+class File(Entity):
+    __entityName__ = "file"
+
+    def getData(self):
+        """
+        Retrieve the contents of a File for manipulation within Python.
+
+        Example
+        -------
+        ::
+
+            entities = user.getFiles(search_query='bacteria')
+            file = entities[0]
+            data = file.getData()
+        """
+        from labstep.entities.file.repository import fileRepository
+
+        return fileRepository.downloadFile(self.__user__, self.id)
+
+    def save(self, folder=None, name=None):
+        """
+        Save a Labstep file to the local filesystem.
+
+        Parameters
+        ----------
+        folder (str)
+            The path to the folder where the file should be saved
+            (defaults to the current working directory).
+        name (str)
+            Optionally give the file a new name.
+
+        Returns
+        -------
+        None
+
+        Example
+        -------
+        ::
+
+            entities = user.getFiles(search_query='bacteria')
+            file = entities[0]
+            file.save()
+        """
+        if name is None:
+            name = self.name
+
+        if folder is not None:
+            filepath = os.path.join(folder, name)
+        else:
+            filepath = name
+
+        data = self.getData()
+        open(filepath, "wb").write(data)
+
+    def export(self, path):
+        """
+        Export the file to the directory specified. 
+
+        Paramers
+        -------
+        path (str)
+            The path to the directory to save the file.
+
+        Example
+        -------
+        ::
+
+            file = user.getFile(17000)
+            file.export('/my_folder')
+        """
+        from labstep.entities.file.repository import fileRepository
+
+        return fileRepository.exportFile(self, path)
