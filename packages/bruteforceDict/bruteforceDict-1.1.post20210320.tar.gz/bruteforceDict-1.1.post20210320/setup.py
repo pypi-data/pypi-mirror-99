02cecba69a5bcf5949df1ca7e9c44c583f358f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:16 +0000

    Add package bruteforceDict/1.1.post20210320/bruteforceDict-1.1.post20210320.tar.gz

---
+++ packages/bruteforceDict/bruteforceDict-1.1.post20210320.tar.gz/bruteforceDict-1.1.post20210320/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+setup(name='bruteforceDict',
+      version='1.1',
+      description='Bruteforce to file, to array',
+      long_description=long_description,
+      packages=['bruteforceDict'],
+      author_email='yura@anisimoff.tel',
+      zip_safe=False)
