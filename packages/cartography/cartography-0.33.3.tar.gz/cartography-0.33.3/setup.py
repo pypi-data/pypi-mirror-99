fb5048b138073ea53428b8c1d8b469b7da052a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:46 +0000

    Add package cartography/0.33.3/cartography-0.33.3.tar.gz

---
+++ packages/cartography/cartography-0.33.3.tar.gz/cartography-0.33.3/setup.py
@@ -0,0 +1,76 @@
+from setuptools import find_packages
+from setuptools import setup
+
+__version__ = '0.33.3'
+
+
+setup(
+    name='cartography',
+    version=__version__,
+    description='Explore assets and their relationships across your technical infrastructure.',
+    url='https://www.github.com/lyft/cartography',
+    maintainer='Lyft',
+    maintainer_email='security@lyft.com',
+    license='apache2',
+    packages=find_packages(exclude=['tests*']),
+    package_data={
+        'cartography.data': [
+            '*.cypher',
+            '*.yaml',
+        ],
+        'cartography.data.jobs.analysis': [
+            '*.json',
+        ],
+        'cartography.data.jobs.cleanup': [
+            '*.json',
+        ],
+    },
+    dependency_links=[],
+    install_requires=[
+        "boto3>=1.15.1",
+        "botocore>=1.18.1",
+        "dnspython>=1.15.0",
+        "neo4j>=1.7.6,<4.0.0",
+        "neobolt>=1.7.0,<4.0.0",
+        "policyuniverse>=1.1.0.0",
+        "google-api-python-client>=1.7.8",
+        "oauth2client>=4.1.3",
+        "marshmallow>=3.0.0rc7",
+        "okta<1.0.0",
+        "pyyaml>=5.3.1",
+        "requests>=2.22.0",
+        "statsd",
+        "packaging",
+        "cryptography<3.4,>=3.2",
+        "python-digitalocean>=1.16.0",
+        "PyJWT==1.7.1",     # azure-cli-core has pinned the PyJWT version as 1.7.1
+        "adal>=1.2.4",
+        "azure-cli-core>=2.12.0",
+        "azure-mgmt-compute>=5.0.0",
+        "azure-mgmt-resource>=10.2.0",
+        "msrestazure >= 0.6.4",
+    ],
+    extras_require={
+        ':python_version<"3.7"': [
+            "importlib-resources",
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'cartography = cartography.cli:main',
+            'cartography-detectdrift = cartography.driftdetect.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
