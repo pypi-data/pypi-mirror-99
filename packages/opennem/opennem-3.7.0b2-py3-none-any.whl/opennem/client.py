f6e8003757ab1d929b5e9cede368811904653a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:15 +0000

    Add package opennem/3.7.0b2/opennem-3.7.0b2-py3-none-any.whl

---
+++ packages/opennem/opennem-3.7.0b2-py3-none-any.whl/opennem/client.py
@@ -0,0 +1,15 @@
+from opennem.utils.http import http
+
+
+class OpenNEMClient(object):
+    """
+    OpenNEM Core API Client
+    """
+
+    base_url: str = None
+
+    def __init__():
+        pass
+
+    def get(self):
+        pass
