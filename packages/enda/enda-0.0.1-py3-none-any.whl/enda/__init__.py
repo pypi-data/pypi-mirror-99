40b9cda305b0e9c3662b17a4a6e5dcb1955904a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:02 +0000

    Add package enda/0.0.1/enda-0.0.1-py3-none-any.whl

---
+++ packages/enda/enda-0.0.1-py3-none-any.whl/enda/__init__.py
@@ -0,0 +1,22 @@
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("pandas",)
+# note that we also need python-dateutil and pytz but pandas already depends on them, so importing pandas is enough
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(dependency)
+
+if len(missing_dependencies) > 0:
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(missing_dependencies)
+    )
+del hard_dependencies, dependency, missing_dependencies
+
+# import some ubclasses here so users can use for instance : 'enda.Contracts' without knowing the internal structure.
+# Do not import classes that need a specific package like
+from enda.contracts import (Contracts)
+from enda.feature_engineering.datetime_features import (DatetimeFeature)
+from enda.timeseries import (TimeSeries)
