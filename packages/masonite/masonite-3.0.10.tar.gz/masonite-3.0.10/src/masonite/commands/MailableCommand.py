c5493b0abd3dafc571b417931377e61a0a755fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:04 +0000

    Add package masonite/3.0.10/masonite-3.0.10.tar.gz

---
+++ packages/masonite/masonite-3.0.10.tar.gz/masonite-3.0.10/src/masonite/commands/MailableCommand.py
@@ -0,0 +1,16 @@
+"""New Job Command."""
+from ..commands import BaseScaffoldCommand
+
+
+class MailableCommand(BaseScaffoldCommand):
+    """
+    Creates a new Mailable.
+
+    mailable
+        {name : Name of the job you want to create}
+    """
+
+    scaffold_name = "Mailable"
+    template = "/masonite/snippets/scaffold/mailable"
+    base_directory = "app/mailable/"
+    postfix = "Mailable"
---
+++ packages/masonite/masonite-3.0.10.tar.gz/masonite-3.0.10/src/masonite/contracts/UploadContract.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class UploadContract(ABC):
+    @abstractmethod
+    def accept(self):
+        pass
+
+    @abstractmethod
+    def validate_extension(self):
+        pass
+
+    @abstractmethod
+    def store(self):
+        pass
