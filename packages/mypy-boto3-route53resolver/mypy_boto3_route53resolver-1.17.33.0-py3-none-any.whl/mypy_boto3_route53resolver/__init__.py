97558cf22f0e7f5aeeaebb9c4d383c62a8d6b5cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:28 +0000

    Add package mypy-boto3-route53resolver/1.17.33.0/mypy_boto3_route53resolver-1.17.33.0-py3-none-any.whl

---
+++ packages/mypy-boto3-route53resolver/mypy_boto3_route53resolver-1.17.33.0-py3-none-any.whl/mypy_boto3_route53resolver/__init__.py
@@ -0,0 +1,62 @@
+"""
+Main interface for route53resolver service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_route53resolver import (
+        Client,
+        ListResolverDnssecConfigsPaginator,
+        ListResolverEndpointIpAddressesPaginator,
+        ListResolverEndpointsPaginator,
+        ListResolverQueryLogConfigAssociationsPaginator,
+        ListResolverQueryLogConfigsPaginator,
+        ListResolverRuleAssociationsPaginator,
+        ListResolverRulesPaginator,
+        ListTagsForResourcePaginator,
+        Route53ResolverClient,
+    )
+
+    session = boto3.Session()
+
+    client: Route53ResolverClient = boto3.client("route53resolver")
+    session_client: Route53ResolverClient = session.client("route53resolver")
+
+    list_resolver_dnssec_configs_paginator: ListResolverDnssecConfigsPaginator = client.get_paginator("list_resolver_dnssec_configs")
+    list_resolver_endpoint_ip_addresses_paginator: ListResolverEndpointIpAddressesPaginator = client.get_paginator("list_resolver_endpoint_ip_addresses")
+    list_resolver_endpoints_paginator: ListResolverEndpointsPaginator = client.get_paginator("list_resolver_endpoints")
+    list_resolver_query_log_config_associations_paginator: ListResolverQueryLogConfigAssociationsPaginator = client.get_paginator("list_resolver_query_log_config_associations")
+    list_resolver_query_log_configs_paginator: ListResolverQueryLogConfigsPaginator = client.get_paginator("list_resolver_query_log_configs")
+    list_resolver_rule_associations_paginator: ListResolverRuleAssociationsPaginator = client.get_paginator("list_resolver_rule_associations")
+    list_resolver_rules_paginator: ListResolverRulesPaginator = client.get_paginator("list_resolver_rules")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+from mypy_boto3_route53resolver.client import Route53ResolverClient
+from mypy_boto3_route53resolver.paginator import (
+    ListResolverDnssecConfigsPaginator,
+    ListResolverEndpointIpAddressesPaginator,
+    ListResolverEndpointsPaginator,
+    ListResolverQueryLogConfigAssociationsPaginator,
+    ListResolverQueryLogConfigsPaginator,
+    ListResolverRuleAssociationsPaginator,
+    ListResolverRulesPaginator,
+    ListTagsForResourcePaginator,
+)
+
+Client = Route53ResolverClient
+
+
+__all__ = (
+    "Client",
+    "ListResolverDnssecConfigsPaginator",
+    "ListResolverEndpointIpAddressesPaginator",
+    "ListResolverEndpointsPaginator",
+    "ListResolverQueryLogConfigAssociationsPaginator",
+    "ListResolverQueryLogConfigsPaginator",
+    "ListResolverRuleAssociationsPaginator",
+    "ListResolverRulesPaginator",
+    "ListTagsForResourcePaginator",
+    "Route53ResolverClient",
+)
