1cabbe53540c221d1e4ac9149c7fbe43ec74c75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:31 +0000

    Add package hyperglot/0.2.6/hyperglot-0.2.6.tar.gz

---
+++ packages/hyperglot/hyperglot-0.2.6.tar.gz/hyperglot-0.2.6/lib/hyperglot/__init__.py
@@ -0,0 +1,31 @@
+"""
+Gather a few package wide constants
+"""
+from os import path
+__version__ = "0.2.6"
+
+DB = path.abspath(path.join(path.dirname(__file__), "hyperglot.yaml"))
+
+SUPPORTLEVELS = {
+    "base": "base",
+    "aux": "auxiliary"
+}
+
+# Note that order matters, since these may be used like a logging level
+VALIDITYLEVELS = [
+    "todo",
+    "draft",
+    "preliminary",
+    "verified"
+]
+
+
+# Note that "secondary" as status is also used, but on orthographies!
+STATUSES = [
+    "historical",
+    "constructed",
+    "ancient",
+    "living",
+    "extinct",
+    "deprecated"
+]
