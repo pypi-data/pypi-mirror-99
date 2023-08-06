2066b3c6a71b34e6ff6ee8c5440fdd35ce7049c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:33 +0000

    Add package evescript/0.1.1/evescript-0.1.1-py2.py3-none-any.whl

---
+++ packages/evescript/evescript-0.1.1-py2.py3-none-any.whl/evescript/eveparser/parser_output/EveScriptLexer.py
@@ -0,0 +1,143 @@
+# Generated from EveScript.g4 by ANTLR 4.9.2
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\31")
+        buf.write("\u00b1\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\4\24\t\24\4\25\t\25\4\26\t\26\4\27\t\27\4\30\t\30")
+        buf.write("\4\31\t\31\4\32\t\32\4\33\t\33\4\34\t\34\3\2\3\2\3\2\3")
+        buf.write("\3\3\3\3\4\3\4\3\5\3\5\3\6\3\6\3\7\3\7\3\7\3\b\3\b\3\b")
+        buf.write("\3\t\3\t\3\n\3\n\3\13\3\13\3\13\3\f\3\f\3\r\3\r\3\r\3")
+        buf.write("\16\3\16\3\16\3\17\3\17\3\17\3\20\3\20\3\20\3\20\3\20")
+        buf.write("\3\21\3\21\3\21\3\21\3\21\3\21\3\22\3\22\3\23\3\23\7\23")
+        buf.write("l\n\23\f\23\16\23o\13\23\3\24\3\24\3\24\7\24t\n\24\f\24")
+        buf.write("\16\24w\13\24\3\24\3\24\3\25\3\25\3\25\3\26\5\26\177\n")
+        buf.write("\26\3\26\3\26\3\26\3\26\3\26\5\26\u0086\n\26\3\26\5\26")
+        buf.write("\u0089\n\26\3\27\3\27\3\27\5\27\u008e\n\27\3\30\3\30\3")
+        buf.write("\30\3\30\3\30\3\30\3\31\3\31\3\32\3\32\3\32\7\32\u009b")
+        buf.write("\n\32\f\32\16\32\u009e\13\32\5\32\u00a0\n\32\3\33\3\33")
+        buf.write("\7\33\u00a4\n\33\f\33\16\33\u00a7\13\33\3\33\3\33\3\34")
+        buf.write("\6\34\u00ac\n\34\r\34\16\34\u00ad\3\34\3\34\2\2\35\3\3")
+        buf.write("\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25\f\27\r\31\16")
+        buf.write("\33\17\35\20\37\21!\22#\23%\24\'\25)\26+\27-\2/\2\61\2")
+        buf.write("\63\2\65\30\67\31\3\2\t\5\2C\\aac|\6\2\62;C\\aac|\4\2")
+        buf.write("$$^^\n\2$$\61\61^^ddhhppttvv\5\2\62;CHch\4\2\f\f\17\17")
+        buf.write("\5\2\13\f\17\17\"\"\2\u00b7\2\3\3\2\2\2\2\5\3\2\2\2\2")
+        buf.write("\7\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3")
+        buf.write("\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2")
+        buf.write("\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2")
+        buf.write("\2\2!\3\2\2\2\2#\3\2\2\2\2%\3\2\2\2\2\'\3\2\2\2\2)\3\2")
+        buf.write("\2\2\2+\3\2\2\2\2\65\3\2\2\2\2\67\3\2\2\2\39\3\2\2\2\5")
+        buf.write("<\3\2\2\2\7>\3\2\2\2\t@\3\2\2\2\13B\3\2\2\2\rD\3\2\2\2")
+        buf.write("\17G\3\2\2\2\21J\3\2\2\2\23L\3\2\2\2\25N\3\2\2\2\27Q\3")
+        buf.write("\2\2\2\31S\3\2\2\2\33V\3\2\2\2\35Y\3\2\2\2\37\\\3\2\2")
+        buf.write("\2!a\3\2\2\2#g\3\2\2\2%i\3\2\2\2\'p\3\2\2\2)z\3\2\2\2")
+        buf.write("+\u0088\3\2\2\2-\u008a\3\2\2\2/\u008f\3\2\2\2\61\u0095")
+        buf.write("\3\2\2\2\63\u009f\3\2\2\2\65\u00a1\3\2\2\2\67\u00ab\3")
+        buf.write("\2\2\29:\7k\2\2:;\7h\2\2;\4\3\2\2\2<=\7*\2\2=\6\3\2\2")
+        buf.write("\2>?\7+\2\2?\b\3\2\2\2@A\7}\2\2A\n\3\2\2\2BC\7\177\2\2")
+        buf.write("C\f\3\2\2\2DE\7~\2\2EF\7~\2\2F\16\3\2\2\2GH\7(\2\2HI\7")
+        buf.write("(\2\2I\20\3\2\2\2JK\7#\2\2K\22\3\2\2\2LM\7@\2\2M\24\3")
+        buf.write("\2\2\2NO\7@\2\2OP\7?\2\2P\26\3\2\2\2QR\7>\2\2R\30\3\2")
+        buf.write("\2\2ST\7>\2\2TU\7?\2\2U\32\3\2\2\2VW\7?\2\2WX\7?\2\2X")
+        buf.write("\34\3\2\2\2YZ\7#\2\2Z[\7?\2\2[\36\3\2\2\2\\]\7v\2\2]^")
+        buf.write("\7t\2\2^_\7w\2\2_`\7g\2\2` \3\2\2\2ab\7h\2\2bc\7c\2\2")
+        buf.write("cd\7n\2\2de\7u\2\2ef\7g\2\2f\"\3\2\2\2gh\7.\2\2h$\3\2")
+        buf.write("\2\2im\t\2\2\2jl\t\3\2\2kj\3\2\2\2lo\3\2\2\2mk\3\2\2\2")
+        buf.write("mn\3\2\2\2n&\3\2\2\2om\3\2\2\2pu\7$\2\2qt\5-\27\2rt\n")
+        buf.write("\4\2\2sq\3\2\2\2sr\3\2\2\2tw\3\2\2\2us\3\2\2\2uv\3\2\2")
+        buf.write("\2vx\3\2\2\2wu\3\2\2\2xy\7$\2\2y(\3\2\2\2z{\7&\2\2{|\5")
+        buf.write("%\23\2|*\3\2\2\2}\177\7/\2\2~}\3\2\2\2~\177\3\2\2\2\177")
+        buf.write("\u0080\3\2\2\2\u0080\u0081\5\63\32\2\u0081\u0082\7\60")
+        buf.write("\2\2\u0082\u0083\5\63\32\2\u0083\u0089\3\2\2\2\u0084\u0086")
+        buf.write("\7/\2\2\u0085\u0084\3\2\2\2\u0085\u0086\3\2\2\2\u0086")
+        buf.write("\u0087\3\2\2\2\u0087\u0089\5\63\32\2\u0088~\3\2\2\2\u0088")
+        buf.write("\u0085\3\2\2\2\u0089,\3\2\2\2\u008a\u008d\7^\2\2\u008b")
+        buf.write("\u008e\t\5\2\2\u008c\u008e\5/\30\2\u008d\u008b\3\2\2\2")
+        buf.write("\u008d\u008c\3\2\2\2\u008e.\3\2\2\2\u008f\u0090\7w\2\2")
+        buf.write("\u0090\u0091\5\61\31\2\u0091\u0092\5\61\31\2\u0092\u0093")
+        buf.write("\5\61\31\2\u0093\u0094\5\61\31\2\u0094\60\3\2\2\2\u0095")
+        buf.write("\u0096\t\6\2\2\u0096\62\3\2\2\2\u0097\u00a0\7\62\2\2\u0098")
+        buf.write("\u009c\4\63;\2\u0099\u009b\4\62;\2\u009a\u0099\3\2\2\2")
+        buf.write("\u009b\u009e\3\2\2\2\u009c\u009a\3\2\2\2\u009c\u009d\3")
+        buf.write("\2\2\2\u009d\u00a0\3\2\2\2\u009e\u009c\3\2\2\2\u009f\u0097")
+        buf.write("\3\2\2\2\u009f\u0098\3\2\2\2\u00a0\64\3\2\2\2\u00a1\u00a5")
+        buf.write("\7%\2\2\u00a2\u00a4\n\7\2\2\u00a3\u00a2\3\2\2\2\u00a4")
+        buf.write("\u00a7\3\2\2\2\u00a5\u00a3\3\2\2\2\u00a5\u00a6\3\2\2\2")
+        buf.write("\u00a6\u00a8\3\2\2\2\u00a7\u00a5\3\2\2\2\u00a8\u00a9\b")
+        buf.write("\33\2\2\u00a9\66\3\2\2\2\u00aa\u00ac\t\b\2\2\u00ab\u00aa")
+        buf.write("\3\2\2\2\u00ac\u00ad\3\2\2\2\u00ad\u00ab\3\2\2\2\u00ad")
+        buf.write("\u00ae\3\2\2\2\u00ae\u00af\3\2\2\2\u00af\u00b0\b\34\2")
+        buf.write("\2\u00b08\3\2\2\2\16\2msu~\u0085\u0088\u008d\u009c\u009f")
+        buf.write("\u00a5\u00ad\3\b\2\2")
+        return buf.getvalue()
+
+
+class EveScriptLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    T__3 = 4
+    T__4 = 5
+    T__5 = 6
+    T__6 = 7
+    T__7 = 8
+    T__8 = 9
+    T__9 = 10
+    T__10 = 11
+    T__11 = 12
+    T__12 = 13
+    T__13 = 14
+    T__14 = 15
+    T__15 = 16
+    T__16 = 17
+    KEYWORD = 18
+    STRING = 19
+    VARIABLE = 20
+    NUMBER = 21
+    COMMENT = 22
+    WS = 23
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'if'", "'('", "')'", "'{'", "'}'", "'||'", "'&&'", "'!'", "'>'", 
+            "'>='", "'<'", "'<='", "'=='", "'!='", "'true'", "'false'", 
+            "','" ]
+
+    symbolicNames = [ "<INVALID>",
+            "KEYWORD", "STRING", "VARIABLE", "NUMBER", "COMMENT", "WS" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", 
+                  "T__7", "T__8", "T__9", "T__10", "T__11", "T__12", "T__13", 
+                  "T__14", "T__15", "T__16", "KEYWORD", "STRING", "VARIABLE", 
+                  "NUMBER", "ESC", "UNICODE", "HEX", "INT", "COMMENT", "WS" ]
+
+    grammarFileName = "EveScript.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.9.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
