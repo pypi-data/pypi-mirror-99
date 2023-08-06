45de801024e2adc3150fab59952009cc2fe2fc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:12 +0000

    Add package roin/4/roin-4.tar.gz

---
+++ packages/roin/roin-4.tar.gz/roin-4/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from distutils.command.install import install
+from subprocess import call
+class Install(install):
+    def run(self):
+        install.run(self)
+        for line in open('requirements.txt').readlines():
+            if len(line) > 1: call(['pip', 'install', line.replace('\n', '')])
+setup(
+    name = 'roin',
+    version = '4',
+    description = 'This is just a module for easier installation of Rosehip dependencies',
+    long_description = open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/donno2048/roin',
+    license = 'MIT',
+    author = 'Elisha Hollander',
+    author_email = 'just4now666666@gmail.com',
+    cmdclass = {'install': Install}
+)
