a2e16b76ced3d0f807eb92e9dc24036720a2fde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:23 +0000

    Add package feapder/1.3.5/feapder-1.3.5.tar.gz

---
+++ packages/feapder/feapder-1.3.5.tar.gz/feapder-1.3.5/tests/air-spider/test_air_spider.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020/4/22 10:41 PM
+---------
+@summary:
+---------
+@author: Boris
+@email: boris@bzkj.tech
+"""
+
+import feapder
+
+
+class TestAirSpider(feapder.AirSpider):
+    # __custom_setting__ = dict(
+    #     LOG_LEVEL = "INFO"
+    # )
+
+    def start_requests(self, *args, **kws):
+        yield feapder.Request("https://www.baidu.com")
+
+    def download_midware(self, request):
+        # request.headers = {'User-Agent': ""}
+        # request.proxies = {"https":"https://12.12.12.12:6666"}
+        # request.cookies = {}
+        return request
+
+    def parse(self, request, response):
+        print(response.xpath("//title").extract_first())
+
+
+if __name__ == "__main__":
+    TestAirSpider().start()
