aaf2dea93e7de1cf8e198d7233270d57ba8f5057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:52 +0000

    Add package ezst/0.2.74/ezst-0.2.74-py3-none-any.whl

---
+++ packages/ezst/ezst-0.2.74-py3-none-any.whl/ezst/ai/model.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def cnn():
+  cnn = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+  ])
+
+
+  cnn.compile(optimizer=RMSprop(lr=0.001),
+              loss='binary_crossentropy',
+              metrics = ['accuracy'])
+
+def train(train_dir, epochs=10):
+  train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+  test_datagen  = ImageDataGenerator( rescale = 1.0/255. )
+
+  train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+  history = model.fit(train_generator,
+                    steps_per_epoch=100,
+                    epochs=epochs,
+                    verbose=2)
+
+  return history
