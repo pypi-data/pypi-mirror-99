5d2b6a6222a0bcc70040e2da01f2dac0f56fc5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:39 +0000

    Add package appsync-tools/2.0.4/appsync-tools-2.0.4.tar.gz

---
+++ packages/appsync-tools/appsync-tools-2.0.4.tar.gz/appsync-tools-2.0.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.8
+import io
+from setuptools import setup
+
+
+setup(
+    name='appsync-tools',
+    version='2.0.4',
+    description='Tools for handling appsync responses and routes.',
+    author='Mathew Moon',
+    author_email='mmoon@quinovas.com',
+    url='https://github.com/QuiNovas/appsync-tools',
+    license='Apache 2.0',
+    long_description=io.open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    packages=['appsync_tools'],
+    package_dir={'appsync_tools': 'src/appsync_tools'},
+    install_requires=["typeguard"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
