61838b3b1991bb012eb6fed4619bb9016a5a37ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:28 +0000

    Add package blackfire/1.5.5/blackfire-1.5.5-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/blackfire/blackfire-1.5.5-cp37-cp37m-macosx_10_14_x86_64.whl/blackfire/hooks/flask/patch.py
@@ -0,0 +1,39 @@
+import traceback
+from blackfire.utils import wrap, import_module, get_logger
+
+log = get_logger(__name__)
+
+
+def _wrap_app(instance, *args, **kwargs):
+    try:
+        from blackfire.hooks.flask.middleware import BlackfireFlaskMiddleware
+
+        _ = BlackfireFlaskMiddleware(instance)
+
+        log.debug("Flask middleware enabled.")
+    except Exception as e:
+        log.exception(e)
+
+
+def patch():
+    module = import_module('flask')
+    if not module:
+        return False
+
+    # already patched?
+    if getattr(module, '_blackfire_patch', False):
+        return
+
+    try:
+        module.Flask.__init__ = wrap(module.Flask.__init__, post_func=_wrap_app)
+
+        flask_version = getattr(module, '__version__', None)
+        log.debug('Flask version %s patched.', (flask_version))
+
+        setattr(module, '_blackfire_patch', True)
+
+        return True
+    except Exception as e:
+        log.exception(e)
+
+    return False
