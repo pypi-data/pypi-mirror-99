36a8e5b821290d5dfd9741992281f9dd13648edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:12 +0000

    Add package Complex-API/0.0.7/Complex-API-0.0.7.tar.gz

---
+++ packages/Complex-API/Complex-API-0.0.7.tar.gz/Complex-API-0.0.7/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Complex-API",
+    version="0.0.7",
+    description="It makes it easier to use the API I have built/working on",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JagTheFriend/Complex-API",
+    author="JagTheFriend",
+    author_email="jagthefriend12@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["Complex_API"],
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "setuptools"
+    ],
+    entry_points={
+        "console_scripts": [
+            "Complex_API=Complex_API.complex_api:main",
+        ]
+    },
+)
