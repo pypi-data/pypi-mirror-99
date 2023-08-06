7e7af8af5a1f1dba6ef4cf322da819fae26a0b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:25 +0000

    Add package az-iranian-bank-gateways/1.6.8a1/az-iranian-bank-gateways-1.6.8a1.tar.gz

---
+++ packages/az-iranian-bank-gateways/az-iranian-bank-gateways-1.6.8a1.tar.gz/az-iranian-bank-gateways-1.6.8a1/azbankgateways/banks/__init__.py
@@ -0,0 +1,7 @@
+from .banks import BaseBank
+from .bmi import BMI
+from .sep import SEP
+from .zarinpal import Zarinpal
+from .idpay import IDPay
+from .zibal import Zibal
+from .bahamta import Bahamta
