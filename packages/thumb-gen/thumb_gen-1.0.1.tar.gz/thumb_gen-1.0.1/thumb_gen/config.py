b60802f9476c156ce4fe65934bd512dd9b8ece7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:08 +0000

    Add package thumb-gen/1.0.1/thumb_gen-1.0.1.tar.gz

---
+++ packages/thumb-gen/thumb_gen-1.0.1.tar.gz/thumb_gen-1.0.1/thumb_gen/config.py
@@ -0,0 +1,105 @@
+import os
+import configparser
+
+from .utils     import get_datadir, CheckIfFileExists
+from .version   import config_version
+
+def create_config(IMAGES=12, IMAGE_QUALITY=80, FONT='', FONT_SIZE=30, CUSTOM_TEXT=''):
+    my_datadir = get_datadir() / "thumb-gen"
+
+    try:
+        my_datadir.mkdir(parents=True)
+
+    except FileExistsError:
+        pass
+
+    finally:
+        configfile_path = os.path.join(str(my_datadir), "config.ini")
+        config_object = configparser.ConfigParser()
+        config_object["DEFAULT"] = {"images": IMAGES, "image_quality": IMAGE_QUALITY, "font":FONT, "font_size":FONT_SIZE, "custom_text":CUSTOM_TEXT}
+        config_object["VERSION"] = {"config_version": config_version}
+        with open(configfile_path, 'w') as conf:
+            config_object.write(conf)
+
+        return True
+
+def modify_config(options, value):
+    my_datadir = get_datadir() / "thumb-gen"
+    configfile_path = os.path.join(str(my_datadir), "config.ini")
+    config_object  = configparser.ConfigParser()
+
+    config_object.read(configfile_path)
+    userinfo = config_object["DEFAULT"]
+
+    if options == "images":
+        userinfo["images"] = str(value)
+
+    elif options == "image_quality":
+        userinfo["image_quality"] = str(value)
+
+    elif options == "font":
+        userinfo["font"] = str(value)
+
+    elif options == "font_size":
+        userinfo["font_size"] = str(value)
+
+    elif options == "custom_text":
+        if value == '000' or value == 'clear':
+            userinfo["custom_text"] = ''
+        else:
+            userinfo["custom_text"] = str(value)
+
+    with open(configfile_path, 'w') as conf:
+        config_object.write(conf)
+    
+    return True
+
+def read_config(option):
+    my_datadir = get_datadir() / "thumb-gen"
+    configfile_path = os.path.join(str(my_datadir), "config.ini")
+    config_object = configparser.ConfigParser()
+
+    if CheckIfFileExists(configfile_path):
+        config_object.read(configfile_path)
+
+        try:
+            config_object["VERSION"]["config_version"] == config_version
+        except KeyError:
+            create_config()
+
+        config_object.read(configfile_path)
+        if config_object["VERSION"]["config_version"] == config_version:
+            default = config_object["DEFAULT"]
+
+        else:
+            create_config_return = create_config()
+            if create_config_return:
+                config_object.read(configfile_path)
+                default = config_object["DEFAULT"]
+
+    else:
+        create_config_return = create_config()
+        if create_config_return:
+            config_object.read(configfile_path)
+            default = config_object["DEFAULT"]
+
+    loop = True
+    while loop:
+        try:
+            if option == 'images':
+                return int(default['images'])
+            elif option == 'image_quality':
+                return int(default['image_quality'])
+            elif option == 'font':
+                return str(default['font'])
+            elif option == 'font_size':
+                return int(default['font_size'])
+            elif option == 'custom_text':
+                return str(default['custom_text'])
+            loop = False
+
+        except KeyError:
+            create_config_return = create_config()
+            if create_config_return:
+                config_object.read(configfile_path)
+                default = config_object["DEFAULT"]
---
+++ packages/thumb-gen/thumb_gen-1.0.1.tar.gz/thumb_gen-1.0.1/thumb_gen/worker.py
@@ -0,0 +1,45 @@
+import os
+import re
+import tempfile
+
+from .application   import screenshots, resize, thumb
+from .viewer        import print_process, print_success
+from .utils         import listToString
+
+class Generator:
+    def __init__(self, video_path, output_path='', custom_text='True', font_dir='', font_size=0):
+        self.video_path = video_path
+        self.font_dir = font_dir
+
+        if isinstance(font_size, int):
+            self.font_size = font_size
+        elif isinstance(font_size, str):
+            raise ValueError("Font size must be an integer")
+
+        if output_path == '':
+            self.output_path = self.video_path[:-4]
+            self.output_folder = listToString(re.split(pattern = r"[/\\]", string = self.video_path)[:-1], "sys")
+
+        else:
+            self.filename = re.split(pattern = r"[/\\]", string = self.video_path)[-1]
+            self.output_path = os.path.join(output_path, self.filename[:-4])
+            self.output_folder = self.output_path
+
+        self.custom_text = str(custom_text)
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.secure_temp = self.temp_dir.name
+        self.screenshot_folder = os.path.join(self.secure_temp, 'screenshots')
+        self.resize_folder = os.path.join(self.secure_temp, 'resized')
+        os.mkdir(self.screenshot_folder)
+        os.mkdir(self.resize_folder)
+
+    def run(self):
+        print_process(self.video_path)
+        screenshots(self.video_path, self.screenshot_folder)
+        resize(self.screenshot_folder, self.resize_folder)
+        thumb_out = thumb(self.video_path, self.output_path, self.resize_folder, self.secure_temp, self.custom_text, self.font_dir, self.font_size)
+
+        if thumb_out:
+            print_success(self.output_folder)
+
+        return 1
