ab5ede6c1e2aaba7f258a9bf5c23536922840d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:14 +0000

    Add package large-image/1.4.4.dev13/large_image-1.4.4.dev13-py2.py3-none-any.whl

---
+++ packages/large-image/large_image-1.4.4.dev13-py2.py3-none-any.whl/large_image/constants.py
@@ -0,0 +1,66 @@
+#############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#############################################################################
+
+
+class SourcePriority:
+    NAMED = 0    # Explicitly requested
+    PREFERRED = 1
+    HIGHER = 2
+    HIGH = 3
+    MEDIUM = 4
+    LOW = 5
+    LOWER = 6
+    FALLBACK_HIGH = 7
+    FALLBACK = 8
+    MANUAL = 9   # Will never be selected automatically
+
+
+TILE_FORMAT_IMAGE = 'image'
+TILE_FORMAT_PIL = 'PIL'
+TILE_FORMAT_NUMPY = 'numpy'
+
+
+TileOutputMimeTypes = {
+    # JFIF forces conversion to JPEG through PIL to ensure the image is in a
+    # common colorspace.  JPEG colorspace is complex: see
+    #   https://docs.oracle.com/javase/8/docs/api/javax/imageio/metadata/
+    #                           doc-files/jpeg_metadata.html
+    'JFIF': 'image/jpeg',
+    'JPEG': 'image/jpeg',
+    'PNG': 'image/png',
+    'TIFF': 'image/tiff',
+}
+TileOutputPILFormat = {
+    'JFIF': 'JPEG'
+}
+
+
+TileInputUnits = {
+    None: 'base_pixels',
+    'base': 'base_pixels',
+    'base_pixel': 'base_pixels',
+    'base_pixels': 'base_pixels',
+    'pixel': 'mag_pixels',
+    'pixels': 'mag_pixels',
+    'mag_pixel': 'mag_pixels',
+    'mag_pixels': 'mag_pixels',
+    'magnification_pixel': 'mag_pixels',
+    'magnification_pixels': 'mag_pixels',
+    'mm': 'mm',
+    'millimeter': 'mm',
+    'millimeters': 'mm',
+    'fraction': 'fraction',
+}
