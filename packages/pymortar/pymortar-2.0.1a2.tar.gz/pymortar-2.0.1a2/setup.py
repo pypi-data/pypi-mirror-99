f9748fd90d7138ff87000d36e486e921f1fc3cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:21 +0000

    Add package pymortar/2.0.1a2/pymortar-2.0.1a2.tar.gz

---
+++ packages/pymortar/pymortar-2.0.1a2.tar.gz/pymortar-2.0.1a2/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pymortar']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['brickschema==0.3.0a4',
+ 'googleapis-common-protos>=1.52.0,<2.0.0',
+ 'pandas>=1.1.5,<2.0.0',
+ 'pyarrow>=2.0.0,<3.0.0',
+ 'rdflib>=5.0.0,<6.0.0',
+ 'requests>=2.25.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pymortar',
+    'version': '2.0.1a2',
+    'description': '',
+    'long_description': None,
+    'author': 'Gabe Fierro',
+    'author_email': 'gtfierro@cs.berkeley.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
