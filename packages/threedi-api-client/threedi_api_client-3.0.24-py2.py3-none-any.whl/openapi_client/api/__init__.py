fc07c365ac7858bac31fb716b768edc2c43045cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:43 +0000

    Add package threedi-api-client/3.0.24/threedi_api_client-3.0.24-py2.py3-none-any.whl

---
+++ packages/threedi-api-client/threedi_api_client-3.0.24-py2.py3-none-any.whl/openapi_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.auth_api import AuthApi
+from openapi_client.api.contracts_api import ContractsApi
+from openapi_client.api.files_api import FilesApi
+from openapi_client.api.inpy_versions_api import InpyVersionsApi
+from openapi_client.api.organisations_api import OrganisationsApi
+from openapi_client.api.permissions_api import PermissionsApi
+from openapi_client.api.repositories_api import RepositoriesApi
+from openapi_client.api.revisions_api import RevisionsApi
+from openapi_client.api.roles_api import RolesApi
+from openapi_client.api.simulations_api import SimulationsApi
+from openapi_client.api.statuses_api import StatusesApi
+from openapi_client.api.threedimodels_api import ThreedimodelsApi
+from openapi_client.api.usage_api import UsageApi
