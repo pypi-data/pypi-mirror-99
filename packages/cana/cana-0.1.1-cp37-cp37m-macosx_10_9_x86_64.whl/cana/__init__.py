73b09cea8569f1596cc15b991158f817ff577878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:29 +0000

    Add package cana/0.1.1/cana-0.1.1-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/cana/cana-0.1.1-cp37-cp37m-macosx_10_9_x86_64.whl/cana/__init__.py
@@ -0,0 +1,19 @@
+__package__ = 'cana'
+__title__ = u'CANAlization: Control & Redundancy in Boolean Networks'
+__description__ = u'This package implements a series of methods used to study control, canalization and redundancy in Boolean networks.'
+
+__author__ = """\n""".join([
+    'Rion Brattig Correia <rionbr@gmail.com>',
+    'Alex Gates <ajgates42@gmail.com>',
+    'Xuan Wang <xw47@indiana.edu>',
+    'Thomas Parmer <tjparmer@indiana.edu>',
+    'Etienne Nzabarushimana <enzabaru@indiana.edu>',
+    'Luis M. Rocha <rocha@indiana.edu>'
+])
+
+__copyright__ = u'2021, Correia, R. B., Gates, A., Rocha, L. M.'
+
+__version__ = '0.1.1'
+__release__ = '0.1.1'
+#
+__all__ = ['boolean_network', 'boolean_node']
