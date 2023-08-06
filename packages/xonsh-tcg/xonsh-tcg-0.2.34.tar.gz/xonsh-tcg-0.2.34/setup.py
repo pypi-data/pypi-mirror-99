b6f48869d66a42a6ac66af3365fd35d5ee9d1de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:48 +0000

    Add package xonsh-tcg/0.2.34/xonsh-tcg-0.2.34.tar.gz

---
+++ packages/xonsh-tcg/xonsh-tcg-0.2.34.tar.gz/xonsh-tcg-0.2.34/setup.py
@@ -0,0 +1,33 @@
+# Copyright 2019 Xiang Gao and collaborators.
+# This program is distributed under the MIT license.
+
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+def get_version(version):
+    distance = version.distance if version.distance is not None else 0
+    v = f'{str(version.tag)}.{distance}'
+    return v
+
+setup(
+    name='xonsh-tcg',
+    use_scm_version={
+        'root': '../../',
+        'relative_to': __file__,
+        'version_scheme': get_version,
+        'local_scheme': lambda x:"",
+        'fallback_version': '0.0.1',
+    },
+    setup_requires=['setuptools_scm'],
+    license='MIT',
+    url='https://github.com/zasdfgbnm/tcg',
+    description='tcg support for the Xonsh shell',
+    long_description=long_description,
+    author='Xiang Gao',
+    author_email='tcg@emailaddress.biz',
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.xsh']},
+)
