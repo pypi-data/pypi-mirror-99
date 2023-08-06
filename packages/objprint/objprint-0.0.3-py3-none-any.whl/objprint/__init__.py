5d6cc591b1df83cea222aa36b07cf409ae7c3b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:46 +0000

    Add package objprint/0.0.3/objprint-0.0.3-py3-none-any.whl

---
+++ packages/objprint/objprint-0.0.3-py3-none-any.whl/objprint/__init__.py
@@ -0,0 +1,23 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/objprint/blob/master/NOTICE.txt
+
+__version__ = "0.0.3"
+
+
+from .objprint import ObjPrint
+from .decorator import add_objprint
+
+_objprint = ObjPrint()
+objprint = _objprint.objprint
+objstr = _objprint.objstr
+config = _objprint.config
+install = _objprint.install
+
+
+__all__ = [
+    "objprint",
+    "objstr",
+    "config",
+    "add_objprint",
+    "install"
+]
