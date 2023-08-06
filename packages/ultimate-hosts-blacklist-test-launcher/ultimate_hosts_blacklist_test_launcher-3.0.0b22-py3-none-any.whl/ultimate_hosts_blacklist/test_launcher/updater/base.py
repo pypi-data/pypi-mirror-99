0b6b5679a4555e295ec1ff41bdca8f4e42f039cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:46 +0000

    Add package ultimate-hosts-blacklist-test-launcher/3.0.0b22/ultimate_hosts_blacklist_test_launcher-3.0.0b22-py3-none-any.whl

---
+++ packages/ultimate-hosts-blacklist-test-launcher/ultimate_hosts_blacklist_test_launcher-3.0.0b22-py3-none-any.whl/ultimate_hosts_blacklist/test_launcher/updater/base.py
@@ -0,0 +1,81 @@
+"""
+The test launcher of the Ultimate-Hosts-Blacklist project.
+
+This is the module that provides the base of all updater.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+License:
+::
+
+
+    MIT License
+
+    Copyright (c) 2019, 2020, 2021 Ultimate-Hosts-Blacklist
+    Copyright (c) 2019, 2020, 2021 Nissar Chababy
+    Copyright (c) 2019, 2020, 2021 Mitchell Krog
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
+import functools
+from typing import Optional
+
+from ultimate_hosts_blacklist.test_launcher.administration import Administration
+
+
+class UpdaterBase:
+    """
+    This is the base of all update.
+    """
+
+    administration: Optional[Administration] = None
+
+    def __init__(self, administration: Administration) -> None:
+        self.administration = administration
+
+    def execute_if_authorized(func):
+        """
+        Launches the decorated method only if we are authorized to process.
+        """
+
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            if self.authorized:
+                return func(self, *args, **kwargs)
+            return None
+
+        return wrapper
+
+    @property
+    def authorized(self) -> bool:
+        """
+        Provides the authorization to launch.
+        """
+
+        return False
+
+    @execute_if_authorized
+    def start(self) -> "UpdaterBase":
+        """
+        Starts the update process.
+        """
+
+        raise NotImplementedError()
