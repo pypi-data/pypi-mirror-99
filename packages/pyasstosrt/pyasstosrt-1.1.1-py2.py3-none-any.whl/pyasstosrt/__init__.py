777e0143ce8263b45e1c814e037ed200ae14f636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:26 +0000

    Add package pyasstosrt/1.1.1/pyasstosrt-1.1.1-py2.py3-none-any.whl

---
+++ packages/pyasstosrt/pyasstosrt-1.1.1-py2.py3-none-any.whl/pyasstosrt/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+:authors: GitBib
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2021 GitBib
+"""
+from .pyasstosrt import Subtitle
+from .time import Time
+from .dialogue import Dialogue
+
+VERSION = (1, 1, 1)
+__version__ = ".".join(map(str, VERSION))
+
+__author__ = 'GitBib'
+__email__ = 'pyasstosrt@bnff.website'
+
+__all__ = [
+    "Subtitle", "Time", "Dialogue"
+]
