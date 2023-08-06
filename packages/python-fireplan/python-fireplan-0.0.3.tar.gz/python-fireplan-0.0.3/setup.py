f3bdf75a8d753dce573244640ae81f88f939182e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:05 +0000

    Add package python-fireplan/0.0.3/python-fireplan-0.0.3.tar.gz

---
+++ packages/python-fireplan/python-fireplan-0.0.3.tar.gz/python-fireplan-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-fireplan",
+    version="0.0.3",
+    author="bouni",
+    author_email="bouni@owee.de",
+    description="python wrapper for fireplan API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bouni/python-fireplan",
+    project_urls={
+       "Bug Tracker": "https://github.com/bouni/python-fireplan/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+        "voluptuous"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
