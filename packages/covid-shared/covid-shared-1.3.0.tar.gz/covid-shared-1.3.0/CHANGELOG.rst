8c1d3c046ae2ea042a4b0a6af17c9d3ece931a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:04 +0000

    Add package covid-shared/1.3.0/covid-shared-1.3.0.tar.gz

---
+++ packages/covid-shared/covid-shared-1.3.0.tar.gz/covid-shared-1.3.0/CHANGELOG.rst
@@ -0,0 +1,272 @@
+**1.3.0 - 03/19/2021**
+ - Add waning immunity path
+
+**1.2.0 - 03/12/2021**
+ - Prevent quick runs from being marked as "latest".
+
+**1.1.3 - 03/11/2021**
+ - v1.1.2 did not properly deploy. Added wheel to build depenencies.
+
+**1.1.2 - 03/11/2021**
+ - v1.1.1 did not properly deploy.
+
+**1.1.1 - 03/11/2021**
+ - Updated Variants data path.
+
+**1.1.0 - 03/08/2021**
+ - Add validation module with check that quick runs are not marked best or for production
+
+**1.0.59 - 03/02/2021**
+
+ - Add Mexico CFR-age paths.
+
+**1.0.58 - 02/17/2021**
+
+ - Add Vaccine path
+
+**1.0.57 - 02/01/2021**
+
+ - Metadata fix.
+
+**1.0.56 - 02/01/2021**
+
+ - Add variant path.
+
+**1.0.55 - 01/25/2021**
+
+ - Add new ratio paths.
+
+**1.0.54 - 01/23/2021**
+
+ - Update HHS path.
+
+**1.0.53 - 01/21/2021**
+
+ - Remove NOAA paths
+
+**1.0.52 - 01/18/2021**
+
+ - Add new infections path.
+
+**1.0.51 - 01/16/2021**
+
+ - Separate verbose, with_debugger, and output root, cleanup imports.
+
+**1.0.50 - 01/16/2021**
+
+ - Separate mark best and production tag decorators for independent use.
+
+**1.0.49 - 01/09/2021**
+
+ - Add ratio paths.
+
+**1.0.48 - 01/04/2021**
+
+ - Add SEIR diagnostics path.
+
+**1.0.47 - 12/17/2020**
+
+ - Add visit-volume directory to paths.
+
+**1.0.46 - 12/14/2020**
+
+ - More CI bugfixes.
+
+**1.0.45 - 12/14/2020**
+
+ - Only deploy on successful test pass.
+
+**1.0.44 - 12/14/2020**
+
+ - CI bugfixes.
+
+**1.0.43 - 12/14/2020**
+
+ - Switch to github actions for ci workflow.
+
+**1.0.42 - 12/11/2020**
+
+ - Add path for HHS data on J drive
+
+**1.0.41 - 12/09/2020**
+
+ - Add path for CFR-age MaxPlanck data on J drive
+
+**1.0.40 - 11/19/2020**
+
+ - Add path for survey data on J drive
+ - Remove old paths
+ - Return non-zero exit codes on application failure
+
+**1.0.39 - 10/21/2020**
+
+ - Add path for survey data on J drive
+
+**1.0.38 - 10/21/2020**
+
+ - Add path for new vaccine-coverage covariate.
+
+**1.0.37 - 10/20/2020**
+
+ - Add path for serosurvey supplemental metadata.
+
+ **1.0.36 - 10/19/2020**
+
+ - Change CDC race/ethnicity data path (old path had truncated data).
+
+**1.0.35 - 09/24/2020**
+
+ - Add path for SEIR final outputs.
+
+**1.0.34 - 08/21/2020**
+
+ - Add path for CDC race/ethnicity data.
+
+**1.0.33 - 07/14/2020**
+
+ - Add path for webscraper outputs.
+
+**1.0.32 - 06/12/2020**
+
+ - Expand cli tools interface.
+
+**1.0.31 - 06/12/2020**
+
+ - Add path for shapefiles.
+
+**1.0.30 - 06/11/2020**
+
+ - Add path for mask/contact interaction covariate.
+
+**1.0.29 - 06/08/2020**
+
+ - Add path for contact covariate.
+
+**1.0.28 - 05/31/2020**
+
+ - Add path for pneumonia covariate.
+
+**1.0.27 - 05/29/2020**
+
+ - Hotfix for bug in decorator method
+
+**1.0.26 - 05/29/2020**
+
+ - Create common click option decorators for CLI methods
+
+**1.0.25 - 05/28/2020**
+
+ - Hotfix in import paths to preserve backwards compatibility.
+
+**1.0.24 - 05/28/2020**
+
+ - Refactor cli_tools as a subpackage
+ - Add mobility gpr path
+
+**1.0.23 - 05/25/2020**
+
+ - Redirect deaths outputs.
+
+**1.0.22 - 05/24/2020**
+
+ - New outputs root for deaths
+
+**1.0.21 - 05/21/2020**
+
+ - Mask use paths.
+ - shared repo path.
+
+**1.0.20 - 05/21/2020**
+
+ - Fix yaml warning.
+
+**1.0.19 - 05/21/2020**
+
+ - Update singularity paths for executing R scripts.
+
+**1.0.18 - 05/20/2020**
+
+ - Add more seiir paths.
+
+**1.0.17 - 05/19/2020**
+
+ - Fix default R singularity image path
+
+**1.0.16 - 05/18/2020**
+
+ - Bugfix in metadata file comparison.
+
+**1.0.15 - 05/16/2020**
+
+ - Add shared paths used in testing covariate.
+ - Change lots of stuff to path objects.
+ - Add update from path method to run metadata.
+ - Add paths for seiir inputs and outputs.
+
+**1.0.14 - 05/12/2020**
+
+ - Add static data root.
+
+**1.0.13 - 05/12/2020**
+
+ - Add unversioned inputs root.
+
+**1.0.12 - 05/12/2020**
+
+ - Refactor of get_last_stage_directory to be a bit smarter.
+ - Add NOAA data.
+
+**1.0.11 - 05/01/2020**
+
+ - Add a path for raw covariates.
+
+**1.0.10 - 04/30/2020**
+
+ - Add new paths for covariate gathering
+ - Update cli tools to support QC functions.
+
+**1.0.9 - 04/28/2020**
+
+ - Adjust update with previous metadata.
+
+**1.0.8 - 04/28/2020**
+
+ - Error on bad production dir name.
+ - Add output root for deaths model.
+ - Extract some convenience functions to reduce cli boilerplate.
+
+**1.0.7 - 04/26/2020**
+
+ - Additional logging utilities
+ - Better mkdir support
+ - Expanded marking functions.
+
+**1.0.6 - 04/22/2020**
+
+ - Bugfix in symlink handling.
+ - Add general method to create dirs with reasonable permissions.
+
+**1.0.5 - 04/18/2020**
+
+ - Add NY times output directory name.
+
+**1.0.4 - 04/18/2020**
+
+ - Add NY times repo path.
+ - Add success flag to metadata when successful.
+
+**1.0.3 - 04/16/2020**
+
+ - Add tool tracking to metadata.
+
+**1.0.2 - 04/14/2020**
+
+ - Add authors, code of conduct, contributing guide.
+
+**1.0.1 - 04/14/2020**
+
+ - Deployment updates.
+
+**1.0.0 - 04/14/2020**
+
+ - Initial release.
---
+++ packages/covid-shared/covid-shared-1.3.0.tar.gz/covid-shared-1.3.0/src/covid_shared/paths.py
@@ -0,0 +1,158 @@
+"""Manages all path metadata."""
+from pathlib import Path
+from datetime import datetime
+
+# Input data paths
+JOHNS_HOPKINS_REPO = 'https://github.com/CSSEGISandData/COVID-19/archive/master.zip'
+ITALY_REPO = 'https://github.com/pcm-dpc/COVID-19/archive/master.zip'
+DESCARTES_REPO = 'https://github.com/descarteslabs/DL-COVID-19/archive/master.zip'
+NY_TIMES_REPO = 'https://github.com/nytimes/covid-19-data/archive/master.zip'
+ONEDRIVE_PATH = "covid-onedrive:'COVID-19 Resource Hub'"
+CITYMAPPER_MOBILITY_TEMPLATE = "https://cdn.citymapper.com/data/cmi/Citymapper_Mobility_Index_{DATE}.csv"
+OPEN_COVID19_GROUP_REPO = "https://github.com/beoutbreakprepared/nCoV2019/archive/master.zip"
+CDC_DEATHS_BY_RACE_ETHNICITY_AGE_STATE = "https://data.cdc.gov/api/views/ks3g-spdg/rows.csv?accessType=DOWNLOAD"
+
+# J: drive paths
+DATA_INTAKE_J_DIR = Path('/home/j/Project/covid/data_intake')
+SEROLOGY_DATA = DATA_INTAKE_J_DIR / 'serology'
+SEROSURVEY_SUPPLEMENTAL_DATA = SEROLOGY_DATA / 'supplemental_serosurvey_metadata'
+WANING_IMMUNITY_DATA = SEROLOGY_DATA / 'waning_immunity'
+US_SYMPTOM_SURVEY_DATA = DATA_INTAKE_J_DIR / 'symptom_survey/us'
+GLOBAL_SYMPTOM_SURVEY_DATA = DATA_INTAKE_J_DIR / 'symptom_survey/global'
+PULSE_SURVEY_DATA = DATA_INTAKE_J_DIR / 'pulse_survey/US Census_pulse surveys'
+SOCIAL_DISTANCING_DATA = DATA_INTAKE_J_DIR / 'social distancing'
+CFR_AGE_MAX_PLANCK_DATA = DATA_INTAKE_J_DIR / 'CFR-age/MaxPlanck'
+HHS_DATA = DATA_INTAKE_J_DIR / 'hhs'
+VACCINE_DATA = DATA_INTAKE_J_DIR / 'vaccine'
+MEXICO_CFR_AGE = DATA_INTAKE_J_DIR / 'data' / 'out' / 'cfr-age' / 'mexico' / 'latest'
+MEXICO_HFR_AGE = DATA_INTAKE_J_DIR / 'data' / 'out' / 'hospitalization-age' / 'mexico' / 'latest'
+VARIANTS_DATA = DATA_INTAKE_J_DIR / 'GISAID'
+
+# Shared paths
+EXEC_R_SCRIPT_PATH = Path('/share/singularity-images/lbd/shells/singR.sh')
+R_SINGULARITY_IMAGE_PATH = Path('/ihme/singularity-images/lbd/releases/lbd_full_20200128.simg')
+
+RCLONE_CONFIG_PATH = Path('/ihme/covid-19/.config/rclone/rclone.conf')
+
+UNVERSIONED_INPUTS_ROOT = Path('/ihme/covid-19/unversioned-inputs')
+STATIC_DATA_INPUTS_ROOT = Path('/ihme/covid-19/static-data')
+SNAPSHOT_ROOT = Path('/ihme/covid-19/snapshot-data/')
+MODEL_INPUTS_ROOT = Path('/ihme/covid-19/model-inputs/')
+VISIT_VOLUME_ROOT = Path('/ihme/covid-19/visit-volume/')
+MORTALITY_RATIO_ROOT = Path('/ihme/covid-19/mortality-ratio')
+INFECTION_FATALITY_RATIO_ROOT = Path('/ihme/covid-19/infection-fatality-ratio')
+INFECTION_HOSPITALIZATION_RATIO_ROOT = Path('/ihme/covid-19/infection-hospitalization-ratio')
+INFECTION_DETECTION_RATE_ROOT = Path('/ihme/covid-19/infection-detection-rate')
+HOSPITAL_DEATH_RATIO_ROOT = Path('/ihme/covid-19/hospitalization-death-ratio')
+DEATHS_SPLINE_OUTPUT_ROOT = Path('/ihme/covid-19/deaths-outputs/')
+PAST_INFECTIONS_ROOT = Path('/ihme/covid-19/past-infections')
+INFECTIONATOR_OUTPUTS = Path('/ihme/covid-19/seir-inputs')
+SHAPEFILE_ROOT = Path('/ihme/covid-19/shapefiles')
+WEBSCRAPER_ROOT = Path('/ihme/covid-19/webscrape')
+
+VARIANT_OUTPUT_ROOT = Path('/ihme/covid-19/variant-scaleup')
+MASK_USE_OUTPUT_ROOT = Path('/ihme/covid-19/mask-use-outputs')
+PNEUMONIA_OUTPUT_ROOT = Path('/ihme/covid-19/pneumonia')
+POPULATION_DENSITY_OUTPUT_ROOT = Path('/ihme/covid-19/population-density')
+MOBILITY_COVARIATES_OUTPUT_ROOT = Path('/ihme/covid-19/mobility-covariate')
+MOBILITY_COVARIATES_GPR_OUTPUT_ROOT = MOBILITY_COVARIATES_OUTPUT_ROOT / 'gpr_outputs'
+TESTING_OUTPUT_ROOT = Path('/ihme/covid-19/testing-outputs')
+VACCINE_COVERAGE_OUTPUT_ROOT = Path('/ihme/covid-19/vaccine-coverage')
+SEIR_COVARIATES_OUTPUT_ROOT = Path('/ihme/covid-19/seir-covariates')
+
+SEIR_REGRESSION_OUTPUTS = Path('/ihme/covid-19/seir-regression')
+SEIR_FORECAST_OUTPUTS = Path('/ihme/covid-19/seir-forecast')
+SEIR_FINAL_OUTPUTS = Path('/ihme/covid-19/seir-outputs')
+SEIR_DIAGNOSTICS_OUTPUTS = Path('/ihme/covid-19/seir-diagnostics')
+
+
+# Shared file names
+METADATA_FILE_NAME = Path('metadata.yaml')
+BEST_LINK = Path('best')
+LATEST_LINK = Path('latest')
+PRODUCTION_RUN = Path('production-runs')
+
+JOHNS_HOPKINS_OUTPUT_DIR_NAME = Path('johns_hopkins_repo')
+ITALY_OUTPUT_DIR_NAME = Path('italy_repo')
+NY_TIMES_OUTPUT_DIR_NAME = Path('ny_times_repo')
+CDC_OUTPUT_DIR_NAME = Path('cdc_data')
+MOBILITY_OUTPUT_DIR_NAME = Path('mobility_data')
+ONEDRIVE_OUTPUT_DIR_NAME = Path('covid_onedrive')
+OPEN_COVID19_OUTPUT_DIR_NAME = Path('open_covid19_working_group')
+SEROSURVEY_OUTPUT_DIR_NAME = Path('serosurvey_data')
+SEROSURVEY_SUPPLEMENTAL_OUTPUT_DIR_NAME = Path('supplemental_serosurvey_metadata')
+SYMPTOM_SURVEY_OUTPUT_DIR_NAME = Path('symptom_survey')
+US_SYMPTOM_SURVEY_OUTPUT_DIR_NAME = Path('us')
+GLOBAL_SYMPTOM_SURVEY_OUTPUT_DIR_NAME = Path('global')
+PULSE_SURVEY_OUTPUT_DIR_NAME = Path('pulse_survey')
+PULSE_SURVEY_US_CENSUS_OUTPUT_DIR_NAME = Path('US Census_pulse surveys')
+SOCIAL_DISTANCING_DIR_NAME = Path('social distancing')
+CFR_AGE_DIR_NAME = Path('cfr_age')
+CFR_AGE_MAX_PLANCK_DIR_NAME = Path('MaxPlanck')
+HHS_DIR_NAME = Path('hhs_data')
+VACCINE_DIR_NAME = Path('vaccine')
+MEXICO_CFR_AGE_DIR_NAME = Path('mexico_cfr_age')
+MEXICO_HFR_AGE_DIR_NAME = Path('mexico_hfr_age')
+VARIANTS_DIR_NAME = Path('Variants')
+SEROLOGY_DIR_NAME = Path('serology')
+WANING_IMMUNITY_DIR_NAME = Path('waning_immunity')
+
+LOG_DIR = Path("logs")
+LOG_FILE_NAME = Path("master_log.txt")
+DETAILED_LOG_FILE_NAME = Path("master_log.json")
+
+
+def latest_production_snapshot_path():
+    return _latest_prod_path(SNAPSHOT_ROOT)
+
+
+def latest_production_etl_path():
+    return _latest_prod_path(MODEL_INPUTS_ROOT)
+
+
+def _latest_prod_path(prefix: Path):
+    prod_run_dir = prefix / PRODUCTION_RUN
+    prod_runs = [d for d in prod_run_dir.iterdir()]
+    sorted_runs = list(sorted(prod_runs, key=lambda p: datetime.strptime(p.stem, '%Y_%m_%d')))
+    return sorted_runs[-1]
+
+
+#################
+# I/O utilities #
+#################
+
+# TODO: Set the ihme-covid user group.  Don't think I can do this without sudo.
+DIRECTORY_PERMISSIONS = 0o775
+FILE_PERMISSIONS = 0o664
+
+
+def make_dir_tree(directory: Path):
+    """Makes all directories and their parents with the correct permissions.
+
+    Parameters
+    ----------
+    directory
+        The directory to make.
+
+    This skirts around the default behavior of :func:`Path.mkdir` which
+    mimics `mkdir -p` which will create a dir with requested permissions,
+    but all parent directories with default permissions.
+
+    """
+    to_create = []
+    p = directory
+    while not p.exists():
+        to_create.append(p)
+        p = p.parent
+    while to_create:
+        to_create.pop().mkdir(DIRECTORY_PERMISSIONS)
+
+
+def recursive_set_permissions(path: Path):
+    """Recursively set permissions to defaults."""
+    if path.is_file():
+        path.chmod(FILE_PERMISSIONS)
+    else:
+        path.chmod(DIRECTORY_PERMISSIONS)
+        for p in path.iterdir():
+            recursive_set_permissions(p)
