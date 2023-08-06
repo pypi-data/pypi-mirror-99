f74c39b7b86aa4a4fbf4843f374f3360ff7b0b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:08 +0000

    Add package pytorch-ignite/0.5.0.dev20210319/pytorch_ignite-0.5.0.dev20210319-py3-none-any.whl

---
+++ packages/pytorch-ignite/pytorch_ignite-0.5.0.dev20210319-py3-none-any.whl/ignite/contrib/metrics/regression/mean_normalized_bias.py
@@ -0,0 +1,54 @@
+from typing import Tuple
+
+import torch
+
+from ignite.contrib.metrics.regression._base import _BaseRegression
+from ignite.exceptions import NotComputableError
+
+
+class MeanNormalizedBias(_BaseRegression):
+    r"""Calculates the Mean Normalized Bias.
+
+    .. math::
+        \text{MNB} = \frac{1}{n}\sum_{j=1}^n\frac{A_j - P_j}{A_j}
+
+    where :math:`A_j` is the ground truth and :math:`P_j` is the predicted value.
+
+    More details can be found in the reference `Botchkarev 2018`__.
+
+    - ``update`` must receive output of the form ``(y_pred, y)`` or ``{'y_pred': y_pred, 'y': y}``.
+    - `y` and `y_pred` must be of same shape `(N, )` or `(N, 1)`.
+
+    __ https://arxiv.org/abs/1809.03006
+
+    Parameters are inherited from ``Metric.__init__``.
+
+    Args:
+        output_transform: a callable that is used to transform the
+            :class:`~ignite.engine.engine.Engine`'s ``process_function``'s output into the
+            form expected by the metric. This can be useful if, for example, you have a multi-output model and
+            you want to compute the metric with respect to one of the outputs.
+            By default, metrics require the output as ``(y_pred, y)`` or ``{'y_pred': y_pred, 'y': y}``.
+        device: specifies which device updates are accumulated on. Setting the
+            metric's device to be the same as your ``update`` arguments ensures the ``update`` method is
+            non-blocking. By default, CPU.
+    """
+
+    def reset(self) -> None:
+        self._sum_of_errors = 0.0
+        self._num_examples = 0
+
+    def _update(self, output: Tuple[torch.Tensor, torch.Tensor]) -> None:
+        y_pred, y = output
+
+        if (y == 0).any():
+            raise NotComputableError("The ground truth has 0.")
+
+        errors = (y.view_as(y_pred) - y_pred) / y
+        self._sum_of_errors += torch.sum(errors).item()
+        self._num_examples += y.shape[0]
+
+    def compute(self) -> float:
+        if self._num_examples == 0:
+            raise NotComputableError("MeanNormalizedBias must have at least one example before it can be computed.")
+        return self._sum_of_errors / self._num_examples
