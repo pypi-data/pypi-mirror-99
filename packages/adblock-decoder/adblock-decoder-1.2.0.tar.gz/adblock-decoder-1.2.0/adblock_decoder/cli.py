67c351ac18ca31b9322f2b9289105e686c9e6be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:40 +0000

    Add package adblock-decoder/1.2.0/adblock-decoder-1.2.0.tar.gz

---
+++ packages/adblock-decoder/adblock-decoder-1.2.0.tar.gz/adblock-decoder-1.2.0/adblock_decoder/cli.py
@@ -0,0 +1,142 @@
+"""
+The tool to check the availability or syntax of domains, IPv4, IPv6 or URL.
+
+::
+
+
+    ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+    ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+    ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+    ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+    ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+    ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+This project is part of the PyFunceble project and infrastructure.
+
+Provides the CLI entrypoints.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.github.io/special-thanks.html
+
+Contributors:
+    https://pyfunceble.github.io/contributors.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io/en/dev/
+
+Project homepage:
+    https://pyfunceble.github.io/
+
+License:
+::
+
+
+    Copyright 2017, 2018, 2019, 2020, 2021 Nissar Chababy
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import argparse
+
+from colorama import Fore, Style
+
+from adblock_decoder.core.adblock2hosts import AdBlock2Hosts
+from adblock_decoder.core.adblock2plain import AdBlock2Plain
+
+
+def adblock2plain() -> None:
+    """
+    Provides the CLI entrypoint of the adblock2plain tool.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="An AdBlock2plain (text) converter.",
+        epilog=f"Crafted with {Fore.RED}{Style.BRIGHT}♥{Style.RESET_ALL} "
+        f"by {Fore.YELLOW}{Style.BRIGHT}Nissar Chababy (Funilrys){Style.RESET_ALL}!",
+        add_help=True,
+    )
+
+    parser.add_argument(
+        "input_file",
+        type=argparse.FileType("r", encoding="utf-8"),
+        help="The input file to work with.",
+    )
+
+    parser.add_argument(
+        "--aggressive",
+        action="store_true",
+        help="[USE AT YOUR OWN RISK AS IT IS EXPERIMENTAL] Activates the extraction "
+        "of everything regardless of the interpretation of AdBlock/UBlock.",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=argparse.FileType("w", encoding="utf-8"),
+        help="The file to write to.",
+    )
+
+    args = parser.parse_args()
+
+    AdBlock2Plain(
+        args.input_file, args.aggressive, output=args.output
+    ).process_conversion()
+
+
+def adblock2host() -> None:
+    """
+    Provides the CLI entrypoint of the adblock2hosts tool.
+    """
+
+    parser = argparse.ArgumentParser(
+        description="An AdBlock2hosts converter.",
+        epilog=f"Crafted with {Fore.RED}{Style.BRIGHT}♥{Style.RESET_ALL} "
+        f"by {Fore.YELLOW}{Style.BRIGHT}Nissar Chababy (Funilrys){Style.RESET_ALL}!",
+        add_help=True,
+    )
+
+    parser.add_argument(
+        "input_file",
+        type=argparse.FileType("r"),
+        help="The input file to work with.",
+    )
+
+    parser.add_argument(
+        "--aggressive",
+        action="store_true",
+        help="[USE AT YOUR OWN RISK AS IT IS EXPERIMENTAL] Activates the extraction "
+        "of everything regardless of the interpretation of AdBlock/UBlock.",
+    )
+
+    parser.add_argument(
+        "--ip",
+        type=str,
+        help="Sets the IP to use while generating the hosts file.",
+        default="0.0.0.0",
+    )
+
+    parser.add_argument(
+        "-o", "--output", type=argparse.FileType("w"), help="The file to write to."
+    )
+
+    args = parser.parse_args()
+
+    AdBlock2Hosts(
+        args.input_file, args.aggressive, output=args.output, hosts_ip=args.ip
+    ).process_conversion()
---
+++ packages/adblock-decoder/adblock-decoder-1.2.0.tar.gz/adblock-decoder-1.2.0/setup.py
@@ -0,0 +1,127 @@
+"""
+The tool to check the availability or syntax of domains, IPv4, IPv6 or URL.
+
+::
+
+
+    ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+    ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+    ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+    ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+    ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+    ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+
+This project is part of the PyFunceble project and infrastructure.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Special thanks:
+    https://pyfunceble.github.io/special-thanks.html
+
+Contributors:
+    https://pyfunceble.github.io/contributors.html
+
+Project link:
+    https://github.com/funilrys/PyFunceble
+
+Project documentation:
+    https://pyfunceble.readthedocs.io/en/dev/
+
+Project homepage:
+    https://pyfunceble.github.io/
+
+License:
+::
+
+
+    Copyright 2017, 2018, 2019, 2020, 2021 Nissar Chababy
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+from re import compile as comp
+
+from setuptools import find_packages, setup
+
+
+def get_requirements():
+    """
+    Extracts all requirements from requirements.txt.
+    """
+
+    with open("requirements.txt") as file:
+        requirements = file.read().splitlines()
+
+    return requirements
+
+
+def get_version():
+    """
+    Extracts the version from adblock_decoder/__init__.py
+    """
+
+    to_match = comp(r'__version__\s=\s"(.*)"')
+
+    return to_match.findall(
+        open("adblock_decoder/__init__.py", encoding="utf-8").read()
+    )[0]
+
+
+def get_long_description():
+    """
+    Provides the long description.
+    """
+
+    return open("README.rst", encoding="utf-8").read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="adblock-decoder",
+        version=get_version(),
+        python_requires=">=3.6, <4",
+        install_requires=get_requirements(),
+        description="A set of tool for the decoding and conversion of AdBlock and Filter Lists.",
+        author="funilrys",
+        author_email="contact@funilrys.com",
+        license="Apache 2.0",
+        url="https://github.com/PyFunceble/adblock-decoder",
+        platforms=["any"],
+        packages=find_packages(),
+        long_description=get_long_description(),
+        project_urls={
+            "Funding": "https://github.com/sponsors/funilrys",
+            "Source": "https://github.com/PyFunceble/adblock-decoder",
+            "Tracker": "https://github.com/funilrys/PyFunceble/issues",
+        },
+        keywords=["PyFunceble", "AdBlock", "Filter", "list", "decoder"],
+        classifiers=[
+            "Environment :: Console",
+            "Topic :: Internet",
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Information Technology",
+            "Intended Audience :: System Administrators",
+            "Intended Audience :: Other Audience",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved",
+        ],
+        entry_points={
+            "console_scripts": [
+                "adblock2hosts=adblock_decoder.cli:adblock2host",
+                "adblock2plain=adblock_decoder.cli:adblock2plain",
+            ]
+        },
+    )
