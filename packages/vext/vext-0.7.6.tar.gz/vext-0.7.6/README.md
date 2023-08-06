d6911aef35717f965d962cc2b52ee58e9ebc3f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:26 +0000

    Add package vext/0.7.6/vext-0.7.6.tar.gz

---
+++ packages/vext/vext-0.7.6.tar.gz/vext-0.7.6/README.md
@@ -0,0 +1,284 @@
+Some things don't easily work in virtualenv and venv, so use them from the system 
+python, without --system-site-packages.
+
+[![Build Status](https://travis-ci.org/stuaxo/vext.svg?branch=master)](https://travis-ci.org/stuaxo/vext)
+
+Example using pygame
+====================
+
+After installing pygame to the system python, install vext.pygame in your virtualenv:
+```bash
+(my-virtualenv) $ pip install vext.pygame
+```
+
+Available packages
+==================
+
+These can be installed using pip as long as corresponding package is installed in the system python
+
+  vext.panda3d
+  
+  vext.pygame
+
+  vext.pygtk
+  
+  vext.gi
+
+  vext.pyqt4
+
+  vext.pyqt5
+
+  vext.vtk
+
+
+Deprecated Packages
+===================
+
+Vext packages get deprecated when the originating project supports installation
+from pip and virtualenv / venv...  
+
+let's try and deprecate them all so vext is no longer needed.
+
+  vext.opencv
+  
+  OpenCV now supports installation through pip and virtualenv, so vext.opencv is not needed,
+  good work OpenCV Team !
+  
+  
+See the section "Fixing packages is better" below.
+
+Blerb
+=====
+
+Virtualenv / venv are really well supported in the web development community.  
+
+Graphics and audio and GUI packages often don't work with virtualenv / venv.
+
+Vext lets you use access specific modules in the system python, using
+just pip and standard requirements.
+
+
+Why not use --system-site-packages?
+-----------------------------------
+
+Pulling in everything gets things working quickly, but negates
+many of the advantages of virtualenv / venv.
+
+For instance it can be easy to forget to add packages to 
+requirements.txt when everything "works for me(TM)".
+
+What about symlinks?
+--------------------
+
+This is quite a clean solution, but doesn't work in Windows.
+
+
+Vext
+----
+
+Vext becomes a gatekeeper to system modules, allowing access
+to particular libraries.
+
+Vext works on the same OSs as virtualenv and venv.
+
+Adding packages
+===============
+
+Fixing packages is better
+-------------------------
+
+Vext is a workaround, it is always better to fix packages to
+work with virtualenv / venv if you can. 
+
+
+Packages that use C extensions often don't work, because their
+setup.py doesn't know about virtualenv / venv.
+
+Pillow, Numpy and recent versions of OpenCV are notable 
+packages that can be installed in virtualenv / venv with pip.
+
+
+Using Vext Instead
+------------------
+
+To enable a vext package, create a .vext file.  
+As an example, here pygtk.vext
+
+```yml
+# Pygtk vext
+#
+
+# Vext will allow access to these modules
+modules:
+    cairo
+    gtk
+    pygtk
+    glib
+    gobject
+
+test_import:
+    # Vext will try and import any modules here
+    # To check if the install worked
+    gtk, pygtk
+
+install_hint:
+   This message will be displayed if the system dependencies
+   are not installed.
+
+# pygtk.pth does most of the initialisation of pygtk
+pths:
+    pygtk.pth
+```
+
+A setup.py is needed to install the vext, see the repository for
+vext.pygtk.
+
+Vext is cross platform, so please try and test your vext specs
+on windows as well as osx and linux !
+
+
+Next, open a ticket on the Vext github.
+
+
+Vext Spec File Format
+=====================
+
+modules
+-------
+List of modules to allow importing from the system site packages.
+
+
+test-import
+------------
+These are modules that should be able to be imported if everything
+is working correctly, you can test this using vext -c, for instance
+
+```
+$ vext -c pygtk
+pygtk.vext
+import gtk: [success]
+import pygtk: [success]
+```
+
+You can also use ```vext -c '*'``` to check every vext files imports,
+which can be useful to help diagnose problems.
+
+
+pths
+----
+Specify any pth files that the module uses to initialise itself.
+
+
+Limitations
+===========
+
+Vext won't stop all imports, (especially when pth files are used)
+the idea is that an error will be thrown fairly early though
+if you are running Vext and try and import a module.
+
+
+Usage
+=====
+
+Mostly you will use vext just by installing a package like 'vext.pygtk'
+the commandline options allow you to get status and enable / disable
+Vext.
+
+```
+optional arguments:
+  -l, --list     List external packages "Vext"
+  -e, --enable   Disable Vext loader
+  -d, --disable  Enable Vext loader
+  -s, --status   Show Vext status
+  -c CHECK, --check CHECK [external package] Test imports for external package
+  -i [filename.vext [filename.vext ...]], --install [INSTALL [INSTALL ...]]  Install vext file.
+```
+
+If Vext is causing an issue with other tools try disabling it,
+also feel free to send a bug report with any information and
+the output of ```vext -l```.
+
+Implementation
+==============
+
+Vext works as a finder and loader of modules, it installs itself on the
+end of the system path, and then will check for any allowed modules.
+
+If Vext runs any .pth files they will be installed after it in the .pth
+practically it guards the base level of sitepackages.
+
+So in a virtualenv ```import pygtk``` should definitally fail if vext is 
+installed but not vext.pygtk
+
+
+Environment Variables
+=====================
+
+```VEXT_DEBUG_LOG=1``` enables extra debugging.
+
+```VEXT_LOG_BLOCKS=1``` store blocked imports in vext.blocked_imports.
+
+```VEXT_DISABLED=1``` disable vext when set.
+
+Blocked Imports
+===============
+Vext acts as gatekeeper to system modules, to aid the process of
+creating a vext file it can log blocked imports or just save
+them to a local data structure.
+
+Logging Blocked Imports
+-----------------------
+To see which modules Vext is blocking set VEXT_LOG_BLOCKS instead of
+standard ImportErrors, you will get ones like this:
+
+
+```bash
+$ VEXT_LOG_BLOCKS=1
+```
+```python
+>>> import pyaudio
+ImportError("Vext blocked import of pyaudio")
+```
+
+Remembering Blocked Imports
+---------------------------
+
+Vext can store all blocked imports in vext.blocked_imports by setting
+vext.remember_blocks to True:
+
+```
+>>> import vext
+>>> vext.remember_blocks = True
+>>> vext.blocked_imports
+set([])
+>>> import pygtk
+ImportError:
+>>> vext.blocked_imports
+set(["pygtk"])
+```
+
+Help out
+========
+
+Please test this, so far it has only been tested on Ubuntu and Windows.
+
+
+TODO
+====
+
+The 'test import' part of the .vext file is not implemented yet,
+this would stop installs unless the system requirements are
+available (or at least write a warning to the console).
+
+
+Merging the finder and loader should make it possible to know if we
+are importing an allowed module, this would mean the submodules wouldn't
+need to be specified in the .vext file.
+
+
+Thanks
+======
+ruamel/venvgtk - for showing something like this is possible
+
+pymotw article on modules and imports
+
---
+++ packages/vext/vext-0.7.6.tar.gz/vext-0.7.6/setup.py
@@ -0,0 +1,345 @@
+from __future__ import print_function
+
+import logging
+import os
+import pkg_resources
+import subprocess
+import sys
+import traceback
+
+from distutils.version import StrictVersion
+from pkg_resources import DistributionNotFound
+from vext import logger
+
+ignore_reload_errors = "VEXT_RELOAD_HACK" in os.environ
+
+
+MIN_SETUPTOOLS = "18.8"
+os.environ['VEXT_DISABLED'] = '1'   # Disable vext loading in subprocesses of setup.py, avoiding memory leak.
+version = "0.7.6"
+
+try:
+    reload
+except NameError:
+    # python 3
+    from importlib import reload
+
+
+def upgrade_setuptools():
+    """
+    setuptools 12.2 can trigger a really nasty bug
+    that eats all memory, so upgrade it to
+    18.8, which is known to be good.
+    """
+    # Note - I tried including the higher version in
+    # setup_requires, but was still able to trigger
+    # the bug. - stu.axon
+    global MIN_SETUPTOOLS
+    r = None
+    try:
+        r = pkg_resources.require(["setuptools"])[0]
+    except DistributionNotFound:
+        # ok, setuptools will be installed later
+        return
+
+    if StrictVersion(r.version) >= StrictVersion(MIN_SETUPTOOLS):
+        return
+    else:
+        print("Upgrading setuptools...")
+        subprocess.call("%s -mpip install 'setuptools>=%s'" % (sys.executable, MIN_SETUPTOOLS), shell=True)
+
+
+def do_reload(module):
+    if ignore_reload_errors:
+        try:
+            reload(pkg_resources)
+        except Exception as e:
+            # Horrible hack to workaround travis problem
+            print("VEXT_RELOAD_HACK set, ignoring %s" % e)
+    else:
+        reload(pkg_resources)
+
+if "install" in sys.argv:
+    upgrade_setuptools()
+    do_reload(pkg_resources)
+    try:
+        r = pkg_resources.require(["setuptools"])[0]
+        print("setuptools version: %s" % r.version)
+    except DistributionNotFound:
+        # ok, setuptools will be installed later
+        print("setuptools not found.")
+        sys.exit(1)
+
+from glob import glob
+from os.path import abspath, basename, dirname, join, normpath, relpath
+from shutil import rmtree
+from textwrap import dedent
+
+from distutils.command.build import build
+from setuptools import setup
+from setuptools import Command
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from setuptools.command.easy_install import easy_install
+from setuptools.command.install_lib import install_lib
+
+here = normpath(abspath(dirname(__file__)))
+
+
+
+class InstallLib(install_lib):
+    #
+    # Next part is when run InstallLib is run - we need to go find
+    # any packages that depend on vext that have been installed
+    # before and install the .vext files they provide
+    #
+    def installed_packages(self):
+        """ :return: list of installed packages """
+        packages = []
+        CMDLINE = [sys.executable, "-mpip", "freeze"]
+        try:
+            for package in subprocess.check_output(CMDLINE) \
+                    .decode('utf-8'). \
+                    splitlines():
+                for comparator in ["==", ">=", "<=", "<", ">"]:
+                    if comparator in package:
+                        # installed package names usually look like Pillow==2.8.1
+                        # ignore others, like external packages that pip show
+                        # won't understand
+                        name = package.partition(comparator)[0]
+                        packages.append(name)
+        except RuntimeError as e:
+            if logger.isEnabledFor(logging.DEBUG):
+                logger.debug("Exception checking existing packages.")
+                logger.debug("cmdline: %s", CMDLINE)
+                ex_type, ex, tb = sys.exc_info()
+                traceback.print_tb(tb)
+                logger.debug()
+        return packages
+
+    def package_info(self):
+        """
+        :return: list of package info on installed packages
+        """
+        import subprocess
+        # create a commandline like  pip show Pillow show
+        package_names = self.installed_packages()
+        if not package_names:
+            # No installed packages yet, so nothign to do here...
+            return []
+
+        cmdline = [sys.executable, "-mpip"]
+        for name in package_names:
+            cmdline.extend(["show", name])
+
+        output = subprocess.check_output(cmdline)
+        # Python 3 fix
+        if not isinstance(output, str):
+            # Some package info is encoded in Latin-1 or something other than
+            # UTF8.  Replace non-UTF characters with '?' instead of crashing.
+            output = str(output, encoding='UTF-8', errors='replace')
+
+        # parse output that looks like this example
+        """
+        ---
+        Name: Pillow
+        Version: 2.8.1
+        Location: /mnt/data/home/stu/.virtualenvs/shoebot-setup/lib/python2.7/site-packages/Pillow-2.8.1-py2.7-linux-x86_64.egg
+        Requires:
+        ---
+        Name: vext.gi
+        Version: 0.5.6.25
+        Location: /mnt/data/home/stu/.virtualenvs/shoebot-setup/lib/python2.7/site-packages/vext.gi-0.5.6.25-py2.7.egg
+        Requires: vext
+
+        """
+        results = []
+        for info in output[3:].split("---"):
+            d = {}
+            for line in info[1:].splitlines():
+                arg, _, value = line.partition(': ')
+                arg = arg.lower()
+                if arg == 'requires':
+                    value = value.split(', ')
+                d[arg] = value
+            results.append(d)
+        return results
+
+    def depends_on(self, dependency):
+        """
+        List of packages that depend on dependency
+        :param dependency: package name, e.g.  'vext' or 'Pillow'
+        """
+        packages = self.package_info()
+        return [package for package in packages if dependency in package.get("requires", "")]
+
+    def find_vext_files(self):
+        """
+        :return:  Absolute paths to any provided vext files
+        """
+        packages = self.depends_on("vext")
+        vext_files = []
+        for location in [package.get("location") for package in packages]:
+            if not location:
+                continue
+            vext_files.extend(glob(join(location, "*.vext")))
+        return vext_files
+
+    def manually_install_vext(self, vext_files):
+        if vext_files:
+            code = dedent("""
+                from vext.install import install_vexts
+                for status in install_vexts(%s, verify=False):
+                    print(status)
+            """ % vext_files)
+            cmd = [sys.executable, '-c', code]
+            output = subprocess.check_output(cmd)
+            print(output)  # console spam
+
+    def enable_vext(self):
+        code = dedent("""
+            from vext.install import create_pth
+            create_pth()
+        """)
+        cmd = [sys.executable, '-c', code]
+        print("Enable Vext...")
+        try:
+            subprocess.check_output(cmd)
+            return True
+        except subprocess.CalledProcessError as e:
+            print(e.output) # console spam
+            return False
+
+    def run(self):
+        """
+        Need to find any pre-existing vext contained in dependent packages
+        and install them
+
+        example:
+
+        you create a setup.py with install_requires["vext.gi"]:
+
+        - vext.gi gets installed using bdist_egg
+        - vext itself is now called with bdist_egg and we end up here
+
+        Vext now needs to find and install .vext files in vext.gi
+        [or any other files that depend on vext]
+
+        :return:
+        """
+        logger.debug("vext InstallLib [started]")
+
+        # Find packages that depend on vext and check for .vext files...
+
+        logger.debug("find_vext_files")
+        vext_files = self.find_vext_files()
+        logger.debug("manually_install_vext: ", vext_files)
+        self.manually_install_vext(vext_files)
+        logger.debug("enable vext")
+        self.enable_vext()
+        logger.debug("install_lib.run")
+        install_lib.run(self)
+        logger.debug("vext InstallLib [finished]")
+
+
+#
+# - end of InstallLib code
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info ./__pycache__'.split(' ')
+
+    # Support the "all" option. Setuptools expects it in some situations.
+    user_options = [
+        ('all', 'a',
+         "provided for compatibility, has no extra functionality")
+    ]
+
+    boolean_options = ['all']
+
+    def initialize_options(self):
+        self.all = None
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob(normpath(join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % relpath(path))
+                rmtree(path)
+
+setup(
+    cmdclass={
+        'install_lib': InstallLib,
+        'clean': CleanCommand,
+    },
+
+    name='vext',
+    version=version,
+    # We need to have a real directory not a zip file:
+    zip_safe=False,
+
+    description='Use system python packages from a virtualenv',
+    long_description=dedent("""
+        Use specific system python packages from virtualenv without --system-site-packages.
+
+        Supports:
+        pygtk, gi (gtk3), qt4, qt5, panda3d, vtk, wx
+        """),
+    url='https://github.com/stuaxo/vext',
+    author='Stuart Axon',
+    author_email='stuaxo2@yahoo.com',
+    license='MIT',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='setuptools development',
+    packages=[
+        'vext',
+        'vext.cmdline',
+        'vext.conf',
+        'vext.env',
+        'vext.helpers',
+        'vext.gatekeeper',
+        'vext.registry',
+        'vext.install',
+    ],
+
+    setup_requires=["setuptools>=18.0.1", "pip>=1.5.6"],
+    install_requires=["ruamel.yaml>=0.11.10"],
+
+    # Install the import hook
+    # data_files=[
+    #    (site_packages_path, site_packages_files),
+    # ],
+
+    entry_points={
+        'console_scripts': [
+            'vext = vext.cmdline:main'
+        ]
+    },
+)
