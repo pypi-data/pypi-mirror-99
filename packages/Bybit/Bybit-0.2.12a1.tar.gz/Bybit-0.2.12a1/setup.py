5ce3b8de5d072b7f241eeb6e2cecf4b8e92ac9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:05 +0000

    Add package Bybit/0.2.12a1/Bybit-0.2.12a1.tar.gz

---
+++ packages/Bybit/Bybit-0.2.12a1.tar.gz/Bybit-0.2.12a1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup
+from os.path import join, dirname
+
+here = dirname(__file__)
+
+setup(name='Bybit',
+      version='0.2.12a1',
+      description='Client for connecting to the Bybit Restful API.',
+      author='Bybit',
+      author_email='api@bybit.com',
+      packages=['bybit', 'BybitAuthenticator'],
+      url='https://github.com/bybit-exchange/api-connectors',
+      install_requires=[
+          'urllib3==1.24.2',
+          'bravado==10.4.1'
+      ]
+      )
+
