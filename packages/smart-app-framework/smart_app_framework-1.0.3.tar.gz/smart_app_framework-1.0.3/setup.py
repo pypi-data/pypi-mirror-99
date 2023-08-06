cc1c29333c0aa015dd319430c37c31e2063298d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:04 +0000

    Add package smart-app-framework/1.0.3/smart_app_framework-1.0.3.tar.gz

---
+++ packages/smart-app-framework/smart_app_framework-1.0.3.tar.gz/smart_app_framework-1.0.3/setup.py
@@ -0,0 +1,62 @@
+from setuptools import find_packages, setup
+import versioneer
+
+
+with open("README.md", "r", encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name="smart_app_framework",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="SberDevices",
+    author_email="developer@sberdevices.ru",
+    description="SmartApp Framework — это фреймворк, "
+                "который позволяет создавать смартапы "
+                "с поддержкой виртуальных ассистентов Салют.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="sberpl-2",
+    packages=find_packages(exclude=[]),
+    include_package_data=True,
+    install_requires=[
+        'h5py<3.0.0',
+        'tatsu==4.4.0',
+        'keras==2.3.1',
+        'tensorflow==1.15',
+        'nltk',
+        'rusenttokenize',
+        'rnnmorph',
+        "tqdm",
+        "tabulate",
+        "lazy",
+        'setuptools',
+        'Jinja2==2.10.1',
+        'timeout-decorator==0.4.1',
+        'Twisted',
+        'confluent_kafka==1.1.0',
+        'pymorphy2==0.8',
+        'pymorphy2_dicts==2.4.393442.3710985',
+        'dawg==0.8.0',
+        'python-dateutil==2.7.3',
+        'ics==0.6',
+        'prometheus-client==0.7.1',
+        'boto==2.49.0',
+        'pyignite>=0.3.4',
+        'python-json-logger==0.1.11',
+        'PyYAML==5.3',
+        'requests==2.22.0',
+        'objgraph==3.4.1',
+        'psutil==5.8.0',
+        'jaeger_client==4.3.0',
+        'dill',
+        'redis',
+        'aiohttp==3.7.3',
+        'aioredis',
+        'croniter',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
---
+++ packages/smart-app-framework/smart_app_framework-1.0.3.tar.gz/smart_app_framework-1.0.3/tests/scenarios_tests/behaviors_test/test_behavior_model.py
@@ -0,0 +1,171 @@
+# coding: utf-8
+import unittest
+from collections import OrderedDict
+from collections import namedtuple
+from unittest.mock import Mock
+import scenarios.behaviors.behaviors
+
+
+class BehaviorsTest(unittest.TestCase):
+    def setUp(self):
+        self.user = Mock()
+        self.user.settings = Mock()
+        self.user.settings.app_name = "app_name"
+        self.user.local_vars.values = {"test_local_var_key": "test_local_var_value"}
+        self.description = Mock()
+        self.description.timeout = Mock(return_value=10)
+        self.success_action = Mock()
+        self.success_action.run = Mock()
+        self.fail_action = Mock()
+        self.timeout_action = Mock()
+
+        self.description.success_action = self.success_action
+        self.description.fail_action = self.fail_action
+        self.description.timeout_action = self.timeout_action
+        self.descriptions = {"test": self.description}
+        self._callback = namedtuple('Callback', 'behavior_id expire_time scenario_id')
+
+    def test_success(self):
+        callback_id = "123"
+        behavior_id = "test"
+        item = {"behavior_id": behavior_id, "expire_time": 2554416000, "scenario_id": None,
+                "text_preprocessing_result": {}}
+        items = {str(callback_id): item}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        behaviors.success(callback_id)
+        # self.success_action.run.assert_called_once_with(self.user, TextPreprocessingResult({}))
+        self.success_action.run.assert_called_once()
+        self.assertDictEqual(behaviors.raw, {})
+
+    def test_success_2(self):
+        callback_id = "123"
+        items = {}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        behaviors.success(callback_id)
+        self.success_action.run.assert_not_called()
+
+    def test_fail(self):
+        callback_id = "123"
+        behavior_id = "test"
+        item = {"behavior_id": behavior_id, "expire_time": 2554416000, "scenario_id": None}
+        items = {str(callback_id): item}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        behaviors.fail(callback_id)
+        self.fail_action.run.assert_called_once()
+        self.assertDictEqual(behaviors.raw, {})
+
+    def test_timeout(self):
+        callback_id = "123"
+        behavior_id = "test"
+        item = {"behavior_id": behavior_id, "expire_time": 2554416000, "scenario_id": None}
+        items = {str(callback_id): item}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        behaviors.timeout(callback_id)
+        self.timeout_action.run.assert_called_once()
+        self.assertDictEqual(behaviors.raw, {})
+
+    def test_expire(self):
+        callback_id = "123"
+        behavior_id = "test"
+        item = {"behavior_id": behavior_id, "expire_time": 1548079039, "scenario_id": None}
+        items = {str(callback_id): item}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        behaviors.expire()
+        self.assertDictEqual(behaviors.raw, {})
+
+    @unittest.mock.patch.object(scenarios.behaviors.behaviors, "time", return_value=9999999999)
+    def test_add_1(self, time):
+        items = {}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        callback_id = "123"
+        behavior_id = "test"
+        text_preprocessing_result = {}
+        behaviors.add(callback_id, behavior_id)
+        _time = int(time()) + self.description.timeout(None) + scenarios.behaviors.behaviors.Behaviors.EXPIRATION_DELAY
+
+        exp = OrderedDict(behavior_id=behavior_id, expire_time=_time, scenario_id=None,
+                          text_preprocessing_result=text_preprocessing_result,
+                          action_params={'local_vars': {'test_local_var_key': 'test_local_var_value'}})
+        self.assertDictEqual(behaviors.raw, {callback_id: exp})
+
+    @unittest.mock.patch.object(scenarios.behaviors.behaviors, "time", return_value=9999999999)
+    def test_add_2(self, time):
+        items = {}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        callback_id = "123"
+        behavior_id = "test"
+        scenario_id = "test_scen"
+        text_preprocessing_result = {"test_scen": 1}
+
+        behaviors.add(callback_id, behavior_id, scenario_id, text_preprocessing_result)
+        _time = int(time()) + self.description.timeout(None) + scenarios.behaviors.behaviors.Behaviors.EXPIRATION_DELAY
+        exp = OrderedDict(behavior_id=behavior_id, expire_time=_time, scenario_id=scenario_id,
+                          text_preprocessing_result=text_preprocessing_result,
+                          action_params={'local_vars': {'test_local_var_key': 'test_local_var_value'}})
+        self.assertDictEqual(behaviors.raw, {callback_id: exp})
+
+    def test_check_1(self):
+        callback_id = "123"
+        behavior_id = "test"
+        item = {"behavior_id": behavior_id, "expire_time": 1548079039, "scenario_id": None}
+        items = {str(callback_id): item}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        self.assertTrue(behaviors.check_got_saved_id(behavior_id))
+
+    def test_check_2(self):
+        callback_id = "123"
+        behavior_id = "test"
+        item = {"behavior_id": behavior_id, "expire_time": 1548079039, "scenario_id": None}
+        items = {str(callback_id): item}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        with self.assertRaises(KeyError):
+            behaviors.check_got_saved_id("test2")
+
+    def test_check_misstate_1(self):
+        callback_id = "123"
+        behavior_id = "test"
+        scenario_id = "test_scen"
+        item = {"behavior_id": behavior_id, "expire_time": 1548079039, "scenario_id": scenario_id}
+        items = {str(callback_id): item}
+        self.user.last_scenarios = Mock()
+        self.user.last_scenarios.last_scenario_name = "test_scen2"
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        self.assertTrue(behaviors.check_misstate(callback_id))
+
+    def test_check_misstate_2(self):
+        callback_id = "123"
+        behavior_id = "test"
+        scenario_id = "test_scen"
+        item = {"behavior_id": behavior_id, "expire_time": 1548079039, "scenario_id": scenario_id}
+        items = {str(callback_id): item}
+        self.user.last_scenarios = Mock()
+        self.user.last_scenarios.last_scenario_name = "test_scen"
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        self.assertFalse(behaviors.check_misstate(callback_id))
+
+    def test_raw(self):
+        callback_id = "123"
+        behavior_id = "test"
+        scenario_id = "test_scen"
+        text_preprocessing_result = {1: 2}
+
+        item = {"behavior_id": behavior_id, "expire_time": 1548079039, "scenario_id": scenario_id,
+                "text_preprocessing_result": text_preprocessing_result}
+
+        items = {str(callback_id): item}
+        behaviors = scenarios.behaviors.behaviors.Behaviors(items, self.descriptions, self.user)
+        behaviors.initialize()
+        expected = OrderedDict(behavior_id=behavior_id, expire_time=1548079039, scenario_id=scenario_id,
+                               text_preprocessing_result=text_preprocessing_result, action_params={})
+        self.assertEqual(behaviors.raw, {callback_id: expected})
