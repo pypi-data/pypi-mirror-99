bdce367665f319819b400bbb1885a1b77ef91275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:52 +0000

    Add package auto-machine-learning/0.0.2/auto-machine-learning-0.0.2.tar.gz

---
+++ packages/auto-machine-learning/auto-machine-learning-0.0.2.tar.gz/auto-machine-learning-0.0.2/auto_machine_learning/AutoML/auto_model_trainer.py
@@ -0,0 +1,194 @@
+from auto_machine_learning.data_preprocessing.preprocessing import *
+from auto_machine_learning.feature_engineering.select_from_model import select_from_model
+from auto_machine_learning.feature_engineering.anova import anova_classifier, anova_regressor
+from auto_machine_learning.feature_engineering.correlation import correlation
+from auto_machine_learning.feature_engineering.pca import pca
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, ExtraTreesRegressor, BaggingRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, GradientBoostingClassifier, ExtraTreesClassifier
+from auto_machine_learning.hyperparameter_optimization.hpo_methods import *
+from auto_machine_learning.hyperparameter_optimization.hpo import get_trained_model
+from auto_machine_learning.utils import *
+from auto_machine_learning.metrics.metrics import get_model_metrics
+import pandas as pd
+
+default_feature_engineering_methods = ['all_features','anova', 'correlation', 'pca', 'select_from_model']
+default_hyperparamter_methods = ['standard','grid_search', 'random_search', 'bayesian_tpe']
+models = list(map_model.keys())
+
+
+techniques_dict = {
+    mod.__name__ : mod for mod in [anova_classifier, anova_regressor, correlation, pca, select_from_model, grid_search, random_search, bayesian_tpe, bayesian_gp]
+}
+
+
+def train(dataset, label, task, feature_engineering_method='all_features', hpo_method='standard', model_name=None, threshold=0.9, max_evals=500, test_size=0.3, random_state=1, download_model=None):
+    '''
+    Implements a pipeline for training the machine learning model. Consists of the stages: Datapreprocessing -> Feature Engineering -> training -> Hpo.
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+                    task (string) : type of task
+                    feature_engineering_methods(string): feature engineering method to be implemented
+                    hpo_methods(string): list of hpo methods to be implemented
+                    model_name(string) : Name of the model to be trained
+                    threshold(float) : to decide the useful features to be kept
+                    max_evals(int) : max number of evaluations to be done
+                    test_size(float) : fraction of data to be used for testing
+                    random_state(int) : random state to follow
+                    download_model(string) : name of the file to be used for saving model
+
+
+            Returns:
+                    model (mode object): trained model
+    '''
+
+    # Preprocessing
+    dataset = preprocess_data(dataset, label, task)
+
+    # Feature Engineering
+    if feature_engineering_method == 'all_features':
+        pass
+    elif feature_engineering_method == 'anova':
+        if task == 'prediction':
+            dataset = anova_regressor(dataset, label)
+        else:
+            dataset = anova_classifier(dataset, label)
+    elif feature_engineering_method == 'correlation':
+        dataset = correlation(dataset, label, threshold)
+    elif feature_engineering_method == 'pca':
+        dataset = pca(dataset, label)
+    elif feature_engineering_method == 'select_from_model':
+        if task == 'prediction':
+            dataset = select_from_model(dataset, label, RandomForestRegressor)
+        else:
+            dataset = select_from_model(dataset, label, RandomForestClassifier)
+    print('Feature Engineering Complete')
+
+    # Hyperparameter optimisation and training the model
+    trained_model = get_trained_model(dataset, label, model_name, task, hpo_method, max_evals, test_size, random_state)
+    print('Model trained')
+
+    #download model
+    if download_model:
+        pickle_model(trained_model, download_model)
+        print('Pickle file generated.')
+    return trained_model
+
+#---------------------------------------------------------------------------------------------------------------------#
+
+def auto_trainer(dataset,label,task,feature_engineering_methods=default_feature_engineering_methods, hpo_methods=default_hyperparamter_methods, models=[] ,anova_estimator=None, sortby=None, download_model = None,excel_file=None, threshold=0.9, max_evals=500, test_size=0.3, random_state=1):
+    '''
+    Implements the whole automl pipeline. Consists of the stages: Datapreprocessing -> Feature Engineering -> HPO.
+    Helps in generating results for various combinations of Feature Engineering methods, HPO methods and models
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+                    task (string) : type of task
+                    feature_engineering_methods(list): list of feature engineering methods to be implemented
+                    hpo_methods(list): list of hpo methods to be implemented
+                    models(list): list of models to be used for generating results
+                    anova_estimator (class of model) : model to be used as base for featureengineering technique
+                    sortby(string) : sort the result as per metric
+                    download_model(string) : name of the file to be used for saving model
+                    excel_file(string) : name of the file to be used for saving stats
+                    threshold(float) : to decide the useful features to be kept
+                    max_evals(int) : max number of evaluations to be done
+                    test_size(float) : fraction of data to be used for testing
+                    random_state(int) : random state to follow
+            Returns:
+                    stats (dictionary): contains the metrics for given data
+                    model (mode object): trained model
+
+    '''
+
+    # dataset = preprocess_data(dataset,label)
+    stats = []
+    if task=='prediction':
+        if models == []:
+            models = ['LinearRegression', 'Ridge', 'Lasso', 'DecisionTreeRegressor', 'RandomForestRegressor', 'AdaBoostRegressor', 'ExtraTreesRegressor', 'BaggingRegressor', 'GradientBoostingRegressor']
+        notallowed=['LogisticRegression','RandomForestClassifier', 'AdaBoostClassifier', 'BaggingClassifier', 'GradientBoostingClassifier', 'ExtraTreesClassifier', 'DecisionTreeClassifier']
+        print(task)
+        print(models)
+        for model in models:
+            if model in notallowed:
+                raise Exception("Input valid model list for the given task")
+
+    else:
+        if models == []:
+            models = ['LogisticRegression','RandomForestClassifier', 'AdaBoostClassifier', 'BaggingClassifier', 'GradientBoostingClassifier', 'ExtraTreesClassifier', 'DecisionTreeClassifier']
+        notallowed=['LinearRegression', 'Ridge', 'Lasso', 'DecisionTreeRegressor', 'RandomForestRegressor', 'AdaBoostRegressor', 'ExtraTreesRegressor', 'BaggingRegressor', 'GradientBoostingRegressor']
+        for model in models:
+            if model in notallowed:
+                raise Exception("Input valid model list for the given task")
+    try:
+        dataset = remove_null(dataset,label) #Change to cumulated function
+        dataset = label_encode(dataset,label)
+        correlation_matrix(dataset,label)
+    except Exception as e:
+        raise type(e)("Please check the data and the label given exists in the dataset")
+
+    #Feature Engineering, Hyperparameter Optimization, Model Training
+    original_dataset = dataset.copy()
+    for feature_engineering_method in feature_engineering_methods:
+        if feature_engineering_method == 'anova':
+            feature_engineering_method = 'anova_regressor' if task == 'prediction' else 'anova_classifier'
+        for hpo_method in hpo_methods:
+            if hpo_method == 'standard':
+                hpo_method='standard'
+            for model_name in models:
+                print(feature_engineering_method, hpo_method, model_name)
+                if task != 'prediction' and feature_engineering_method != 'pca':
+                    try:
+                        dataset = oversampling(dataset,label)
+                    except Exception as e:
+                        raise type(e)("Please check the data and label given properly")
+                if feature_engineering_method.startswith('anova') and anova_estimator:
+                    try:
+                        dataset = techniques_dict[feature_engineering_method](dataset, label, anova_estimator)
+                    except Exception as e:
+                        raise type(e)("Please check the data, label and the anova_estimator provided properly")
+                elif feature_engineering_method == 'correlation':
+                    dataset = techniques_dict[feature_engineering_method](dataset, label, threshold)
+                elif feature_engineering_method == 'select_from_model':
+                    dataset = techniques_dict[feature_engineering_method](dataset, label, map_model[model_name])
+                elif feature_engineering_method == 'all_features':
+                    pass
+                else:
+                    dataset = techniques_dict[feature_engineering_method](dataset, label)
+
+                #Model Training
+                if model_name=='LogisticRegression' and len(dataset[label].unique())>2:
+                    print("The logistic regression requires the output to be binary classification problem")
+                else:
+                    model = get_trained_model(dataset, label, model_name, task,hpo_method, max_evals, test_size, random_state)
+                    features = get_features(dataset, label)
+                    X, Y = dataset[features], dataset[label]
+                    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size,random_state=random_state)
+                    model_metrics = get_model_metrics(model,label,task,X_test, Y_test)
+                    column_names = ['Estimator', 'Feature Engineering Method', 'Hyperparameter Optimization Method']
+                    column_names.extend(list(model_metrics.keys()))
+                    model_metrics = list(map(lambda value : round(value, 4), model_metrics.values()))
+                    stats.append([model,model_name,feature_engineering_method,hpo_method]+list(model_metrics))
+
+
+                dataset = original_dataset.copy()
+    #To sort on basis of metric provided
+    if sortby:
+        index = column_names.index(sortby)
+        stats.sort(key= lambda x: x[index],reverse=True)
+
+    # To download model
+    if download_model:
+        pickle_model(stats[0][0],download_model)
+    pd_stats = pd.DataFrame(stats)
+    pd_stats.drop(pd_stats.columns[0], axis=1,inplace=True)
+    pd_stats.columns = column_names
+
+    #Download excelsheet
+    if excel_file:
+        get_csv(pd_stats,excel_file)
+
+    #Return statistics in form of dataframe and model
+    return pd_stats,stats[0][0]
---
+++ packages/auto-machine-learning/auto-machine-learning-0.0.2.tar.gz/auto-machine-learning-0.0.2/auto_machine_learning/data_preprocessing/preprocessing.py
@@ -0,0 +1,194 @@
+from sklearn.preprocessing import LabelEncoder
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
+from auto_machine_learning.utils import get_features
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#---------------------------------------------------------------------------------------------------------------------#
+
+def preprocess_data(dataset, label, task='classification'):
+    '''
+    Implements all the preprocessing steps: remove null, label encode, oversampling, data split, correlation matrix
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+                    task (string) : type of task default is classification
+
+            Returns:
+                    dataset(dataframe) : processed data to be used for training model
+    '''
+
+    try:
+        dataset = remove_null(dataset,label)
+    except Exception as e:
+        raise type(e)("Error at remove_null. Check the input data and label")
+        
+    try:
+        dataset = label_encode(dataset,label) 
+    except Exception as e:
+        raise type(e)("Error at label_encode. Check the input data and label")
+
+    if task == 'classification':
+        dataset = oversampling(dataset,label)
+
+    correlation_matrix(dataset,label)
+
+    # return oversampling(label_encode(remove_null(dataset,label),label),label)
+    return dataset
+    
+
+#---------------------------------------------------------------------------------------------------------------------#
+
+def remove_null(dataset, label):
+    '''
+    Removes the null rows and features.
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+
+            Returns:
+                    dataset(dataframe) : processed data to be used for training model
+    
+    '''
+    #get features
+    features = get_features(dataset,label)
+
+    # Removing Columns with more than 50% null data
+    for feature in features:
+        if (dataset[feature].isnull().sum()/len(dataset) > 0.5):
+            dataset.drop([feature], axis=1, inplace=True)
+            features.remove(feature)
+
+    # Removing rows having null values
+    dataset.dropna(inplace=True)
+
+    if 'Unnamed: 0' in dataset.columns:
+        dataset.drop(['Unnamed: 0'],axis=1,inplace=True)
+
+    return dataset
+
+#---------------------------------------------------------------------------------------------------------------------#
+
+def label_encode(dataset, label):
+    '''
+    Label encode the data
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+
+            Returns:
+                    dataset(dataframe) : processed data to be used for training model
+    
+    '''
+    #get features
+    features = get_features(dataset,label)
+
+    #for features
+    for feature in features:
+        if dataset[feature].dtype == object:
+
+            encoder = LabelEncoder()
+            encoder.fit(dataset[feature])
+            dataset[feature] = encoder.transform(dataset[feature])
+    #for label
+    if dataset[label].dtype == object:
+        dataset[label] = LabelEncoder().fit_transform(dataset[label])
+
+    if 'Unnamed: 0' in dataset.columns:
+        dataset.drop(['Unnamed: 0'],axis=1,inplace=True)
+
+    return dataset
+
+#---------------------------------------------------------------------------------------------------------------------#
+
+def oversampling(dataset, label):
+    '''
+    Oversamples the data to get rid of skewness from the dataset.
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+
+            Returns:
+                    dataset(dataframe) : processed data to be used for training model
+    '''
+    
+    #get features
+    features = get_features(dataset,label)
+    new_dataset = None
+
+    if True:
+        oversampler=RandomOverSampler()
+        X,Y=oversampler.fit_resample(dataset[features],dataset[label])
+
+        new_dataset = pd.DataFrame(X,columns=features)
+        new_dataset[label] = pd.DataFrame(Y)
+    else:
+        new_dataset = dataset
+
+    if 'Unnamed: 0' in new_dataset.columns:
+        new_dataset.drop(['Unnamed: 0'],axis=1,inplace=True)
+
+    return new_dataset
+
+#---------------------------------------------------------------------------------------------------------------------#
+
+def dataset_split(dataset, label, test_size=0.3, random_state = 1):
+    '''
+    Splits the dataset in train and test data on the basis of given fraction.
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+                    test_size (float) : fraction of data to be alloted for testing
+                    random_state (int) : Random state for the dataset
+
+            Returns:
+                    dataset(dataframe) : processed data to be used for training model
+    '''
+    
+    #get features
+    features = get_features(dataset, label)
+
+    try:
+        X, Y = dataset[features], dataset[label]
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=random_state)
+    except Exception as e:
+        print(e+' '+"Check data and test_size")
+
+    return X_train, X_test, Y_train, Y_test
+
+#---------------------------------------------------------------------------------------------------------------------#
+
+def correlation_matrix(dataset, label):
+    '''
+    Plots the heat map for correlations between the features and labels.
+
+            Parameters:
+                    dataset(dataframe) : data to be used for training model
+                    label (string): target column of the dataframe
+
+
+    
+    '''
+    #get features
+    features = get_features(dataset,label)
+    
+    try:
+        correlation = dataset[features].corr().abs()
+        f, ax = plt.subplots(figsize=(20,20))
+        sns.heatmap(correlation, cmap='coolwarm', annot=True, ax=ax)
+    except Exception as e:
+        print(e+' '+"Please check the data and label")
+    
+    #plot the correlation matrix
+    #plt.show()
+    plt.savefig('correlation_matrix.png')
+
+#---------------------------------------------------------------------------------------------------------------------#
