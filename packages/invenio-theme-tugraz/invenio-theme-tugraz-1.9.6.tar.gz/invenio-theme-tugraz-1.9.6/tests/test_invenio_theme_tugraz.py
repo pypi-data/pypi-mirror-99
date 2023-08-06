ff5a84f067895da6d3482eda4d3f2a63a13d31a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:55 +0000

    Add package invenio-theme-tugraz/1.9.6/invenio-theme-tugraz-1.9.6.tar.gz

---
+++ packages/invenio-theme-tugraz/invenio-theme-tugraz-1.9.6.tar.gz/invenio-theme-tugraz-1.9.6/tests/test_invenio_theme_tugraz.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2021 Graz University of Technology.
+#
+# invenio-theme-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Module tests."""
+
+from flask import Flask
+
+from invenio_theme_tugraz import InvenioThemeTugraz
+
+
+def test_version():
+    """Test version import."""
+    from invenio_theme_tugraz import __version__
+
+    assert __version__
+
+
+def test_init():
+    """Test extension initialization."""
+    app = Flask("testapp")
+    ext = InvenioThemeTugraz(app)
+    assert "invenio-theme-tugraz" in app.extensions
+
+    app = Flask("testapp")
+    ext = InvenioThemeTugraz()
+    assert "invenio-theme-tugraz" not in app.extensions
+    ext.init_app(app)
+    assert "invenio-theme-tugraz" in app.extensions
+
+
+def test_app(app):
+    """Test extension initialization."""
+    theme = InvenioThemeTugraz(app)
