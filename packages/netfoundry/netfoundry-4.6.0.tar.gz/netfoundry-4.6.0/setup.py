9c22923a5be39f47f1e3be0cc5da56c013cb2941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:13 +0000

    Add package netfoundry/4.6.0/netfoundry-4.6.0.tar.gz

---
+++ packages/netfoundry/netfoundry-4.6.0.tar.gz/netfoundry-4.6.0/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='netfoundry',
+    version='4.6.0',
+    py_modules=['netfoundry'],
+    url='https://developer.netfoundry.io/v2/tools/',
+    description='Interface to the NetFoundry network-as-code orchestration Platform',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author='Kenneth Bingham',
+    author_email='support@netfoundry.io',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests >= 2.25.1',
+        'pysocks >= 1.7.1',
+        'pyjwt >= 2.0.0'
+    ]
+)
+
