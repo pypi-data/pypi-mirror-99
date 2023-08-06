36851493b3f8625ad9b088afc2d804f98b2992ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:40 +0000

    Add package ural/0.29.1/ural-0.29.1.tar.gz

---
+++ packages/ural/ural-0.29.1.tar.gz/ural-0.29.1/ural/__init__.py
@@ -0,0 +1,14 @@
+# =============================================================================
+# Ural Library Endpoint
+# =============================================================================
+#
+from ural.ensure_protocol import ensure_protocol
+from ural.force_protocol import force_protocol
+from ural.get_domain_name import get_domain_name, get_hostname
+from ural.infer_redirection import infer_redirection
+from ural.is_shortened_url import is_shortened_url
+from ural.is_url import is_url
+from ural.normalize_url import normalize_url, get_normalized_hostname
+from ural.strip_protocol import strip_protocol
+from ural.urls_from_text import urls_from_text
+from ural.urls_from_html import urls_from_html
