4546b1841719bbf68e5c4e2a757a5c2786fd994c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:03 +0000

    Add package jlab-create-sas-file/0.1.0/jlab_create_sas_file-0.1.0.tar.gz

---
+++ packages/jlab-create-sas-file/jlab_create_sas_file-0.1.0.tar.gz/jlab_create_sas_file-0.1.0/src/iconImport.ts
@@ -0,0 +1,8 @@
+import { LabIcon } from '@jupyterlab/ui-components';
+
+import sasSVGstr from '../style/icons/sas.svg';
+
+export const sasIcon = new LabIcon({
+    name: 'jlab_create_sas_file:sas2',
+    svgstr: sasSVGstr
+  });
