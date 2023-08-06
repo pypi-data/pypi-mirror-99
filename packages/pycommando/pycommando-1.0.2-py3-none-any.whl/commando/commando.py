1fea7e1e23833086e2121de7cd3891f27989113a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:18:31 +0000

    Add package pycommando/1.0.2/pycommando-1.0.2-py3-none-any.whl

---
+++ packages/pycommando/pycommando-1.0.2-py3-none-any.whl/commando/commando.py
@@ -0,0 +1,43 @@
+import subprocess
+import logging
+import sys
+
+
+logger = logging.getLogger(__name__)
+
+
+class Commando:
+
+    def __init__(self):
+        self.__commands = []
+
+    def add(self, cmd):
+        self.__commands.append(cmd)
+
+    def list(self):
+        return self.__commands
+
+    def execute(self):
+        commands = self.__commands
+        for cmd in commands:
+            logger.debug(cmd)
+            # 呼び出し可能かどうかで条件分岐
+            if callable(cmd):
+                try:
+                    cmd()
+                except Exception:
+                    logger.exception("Runtime error")
+                    sys.exit(1)
+            else:
+                try:
+                    proc = subprocess.run(cmd, check=True, capture_output=True)
+                    stdout = proc.stdout.decode(sys.getfilesystemencoding())
+                    # 特に表示するものがないときは logging いらんよね?
+                    if stdout != "":
+                        logger.info(stdout)
+                except subprocess.CalledProcessError as e:
+                    logger.exception(e.stderr.decode(
+                        sys.getfilesystemencoding()))
+                    sys.exit(1)
+        # execute が完了したら commands の要素をすべて削除する
+        self.__commands.clear()
