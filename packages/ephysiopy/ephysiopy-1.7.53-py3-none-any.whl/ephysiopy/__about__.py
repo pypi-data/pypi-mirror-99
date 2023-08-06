06758e0bc07ce3ee26ee46ff373a6a779544783a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:14 +0000

    Add package ephysiopy/1.7.53/ephysiopy-1.7.53-py3-none-any.whl

---
+++ packages/ephysiopy/ephysiopy-1.7.53-py3-none-any.whl/ephysiopy/__about__.py
@@ -0,0 +1,4 @@
+__project__ = "ephysiopy"
+__author__  = "Robin Hayman"
+__version__ = "1.7.53"
+__license__ = "MIT"
---
+++ packages/ephysiopy/ephysiopy-1.7.53-py3-none-any.whl/ephysiopy/__init__.py
@@ -0,0 +1,11 @@
+from ephysiopy import __about__
+import os
+try:
+    kk_path = os.path.join(os.environ.get('HOME'), 'klustakwik', 'Klustakwik')
+except:
+    kk_path = None
+# used to bandpass filter the continuous raw openephys data when converting to
+# Axona eeg/ egf format
+lfp_lowcut = 1
+lfp_highcut = 500
+fs = 30000 # sampling rate of continuous data in openephys
