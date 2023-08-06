f96284742e4da9fa1af26f20654b5a9afc238c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:16 +0000

    Add package bbknn/1.4.1/bbknn-1.4.1.tar.gz

---
+++ packages/bbknn/bbknn-1.4.1.tar.gz/bbknn-1.4.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='bbknn',
+	version='1.4.1',
+	description='Batch balanced KNN',
+	url='https://github.com/Teichlab/bbknn',
+	packages=find_packages(exclude=['docs', 'figures', 'examples']),
+	install_requires=['Cython','numpy','scipy','annoy','umap-learn','scikit-learn','packaging'],
+	extras_require=dict(
+		faiss=['faiss']
+	),
+	author='Krzysztof Polanski, Jongeun Park',
+	author_email='kp9@sanger.ac.uk',
+	license='MIT'
+)
