b95de8eba9298fa95982a08471348c06b670e8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:07 +0000

    Add package itk-iofdf/1.0.1/itk_iofdf-1.0.1-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/itk-iofdf/itk_iofdf-1.0.1-cp37-cp37m-manylinux2014_x86_64.whl/itk/Configuration/IOFDFConfig.py
@@ -0,0 +1,5 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('FDFImageIO', 'itk::FDFImageIO', 'itkFDFImageIO', True),
+  ('FDFImageIOFactory', 'itk::FDFImageIOFactory', 'itkFDFImageIOFactory', True),
+)
