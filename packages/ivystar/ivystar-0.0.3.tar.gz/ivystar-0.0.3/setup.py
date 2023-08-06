b7b19d2d603236e10f2977c955e209cf305fa406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:43 +0000

    Add package ivystar/0.0.3/ivystar-0.0.3.tar.gz

---
+++ packages/ivystar/ivystar-0.0.3.tar.gz/ivystar-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ivystar",
+    packages=find_packages(),
+    version='0.0.3',
+    description="python tools package of ivystar",
+    author="qinhaining",
+    author_email='qhn614@126.com',
+    url="",
+    download_url="",
+    keywords=['command', 'line', 'tool'],
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+    ]
+    },
+    install_requires=[
+        'numpy',
+        'requests',
+    ]
+)
