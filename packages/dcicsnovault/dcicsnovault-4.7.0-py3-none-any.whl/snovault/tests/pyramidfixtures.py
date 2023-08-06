849dcea55418463a02248a0c04f105d70b234052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:32 +0000

    Add package dcicsnovault/4.7.0/dcicsnovault-4.7.0-py3-none-any.whl

---
+++ packages/dcicsnovault/dcicsnovault-4.7.0-py3-none-any.whl/snovault/tests/pyramidfixtures.py
@@ -0,0 +1,35 @@
+"""
+Fixtures  for pyramid, embedding
+"""
+
+import pytest
+
+from dcicutils.qa_utils import notice_pytest_fixtures
+from pyramid.request import apply_request_extensions
+from pyramid.testing import setUp, tearDown
+from pyramid.threadlocal import manager
+
+
+@pytest.yield_fixture
+def config():
+    yield setUp()
+    tearDown()
+
+
+@pytest.yield_fixture
+def threadlocals(request, dummy_request, registry):
+    notice_pytest_fixtures(request, dummy_request, registry)
+    manager.push({'request': dummy_request, 'registry': registry})
+    yield manager.get()
+    manager.pop()
+
+
+@pytest.fixture
+def dummy_request(root, registry, app):
+    request = app.request_factory.blank('/dummy')
+    request.root = root
+    request.registry = registry
+    request._stats = {}
+    request.invoke_subrequest = app.invoke_subrequest
+    apply_request_extensions(request)
+    return request
