5febf1c625edf5383581ecddc498d68b0af29702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:31 +0000

    Add package cardanopy/0.1.7.dev16/cardanopy-0.1.7.dev16.tar.gz

---
+++ packages/cardanopy/cardanopy-0.1.7.dev16.tar.gz/cardanopy-0.1.7.dev16/cardanopy/run_cmd.py
@@ -0,0 +1,39 @@
+import click
+import subprocess
+from .core.cardanopy_config import CardanoPyConfig
+from .core.substitution import Substitution
+
+
+@click.command("run")
+@click.option('-d', '--dry-run', 'dry_run', is_flag=True, help="print the mutable commands")
+@click.option('-s', '--sub', 'subs', multiple=True, type=str, default=tuple(), help="Substitutions for configs")
+@click.argument('target_config_dir_or_file', type=str)
+@click.pass_context
+def run_cmd(ctx, dry_run, subs, target_config_dir_or_file):
+    """Run command"""
+
+    try:
+        target_config_dir = CardanoPyConfig.try_get_valid_config_dir(target_config_dir_or_file)
+        target_config_file = CardanoPyConfig.try_get_valid_config_file(target_config_dir_or_file)
+
+        cardanopy_config = CardanoPyConfig()
+        cardanopy_config.load(target_config_file, subs)
+
+        Substitution.generate(dry_run, target_config_dir, cardanopy_config, subs)
+
+        cardano_node_cmd = ["cardano-node",
+                            "run",
+                            "--config", cardanopy_config.configPath,
+                            "--topology", cardanopy_config.topologyPath,
+                            "--database-path", cardanopy_config.databasePath,
+                            "--host-addr", cardanopy_config.hostAddr,
+                            "--port", f"{cardanopy_config.port}",
+                            "--socket-path", cardanopy_config.socketPath]
+        if dry_run:
+            print(" ".join(cardano_node_cmd))
+        else:
+            subprocess.run(cardano_node_cmd, cwd=target_config_dir)
+    except Exception as ex:
+        ctx.fail(f"run_cmd(dry_run={dry_run}, target_config_dir_or_file='{target_config_dir_or_file}') failed: {type(ex).__name__} {ex.args}")
+        return 1
+
---
+++ packages/cardanopy/cardanopy-0.1.7.dev16.tar.gz/cardanopy-0.1.7.dev16/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+datadir = Path(__file__).parent / 'cardanopy' / 'data'
+files = ['data/' + str(p.relative_to(datadir)) for p in datadir.rglob('*')]
+
+setup(name='cardanopy',
+      version='0.1.7-dev16',
+      description='Cardano CLI tools for python3',
+      author='Bourke Floyd',
+      author_email='chbfiv@floydcraft.com',
+      url='https://github.com/floydcraft/cardano-py',
+      # package_dir={'cardanopy': '.'},
+      package_data={'cardanopy': files},
+      packages=find_packages(exclude=['tests']),
+      keywords='cardano,ada,cli',
+      python_requires='>=3.5.3,<4',
+      install_requires=[
+        'click~=7.1.2',
+        'pyyaml~=5.4.1',
+        'jsonschema~=3.2.0'
+      ],
+      entry_points={
+          'console_scripts': [
+              'cardanopy=cardanopy:main',
+          ],
+      },
+      project_urls={
+          'Bug Reports': 'https://github.com/floydcraft/cardano-py/issues',
+          'Chat': 'https://discord.gg/FyDz4Xrt4x',
+          'Source': 'https://github.com/floydcraft/cardano-py',
+      }
+)
