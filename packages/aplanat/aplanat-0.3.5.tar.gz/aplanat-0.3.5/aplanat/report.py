071bb66a14743f5e0783d024a7feb95ed19f54a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:18 +0000

    Add package aplanat/0.3.5/aplanat-0.3.5.tar.gz

---
+++ packages/aplanat/aplanat-0.3.5.tar.gz/aplanat-0.3.5/aplanat/report.py
@@ -0,0 +1,235 @@
+"""Report building from multiple items."""
+
+from collections import OrderedDict
+import uuid
+
+from bokeh.embed import components
+from bokeh.model import Model
+from bokeh.models.sources import ColumnDataSource
+from bokeh.models.widgets import DataTable, TableColumn
+from bokeh.resources import INLINE
+from jinja2 import Template
+import markdown
+
+
+def _maybe_new_report(section, require_keys=False):
+    """Create a new report section, or returns input."""
+    if section is None:
+        section = HTMLSection(require_keys=require_keys)
+    else:
+        if not isinstance(section, HTMLSection):
+            raise TypeError("`section` should be an `HTMLSection`.")
+    return section
+
+
+class HTMLSection(OrderedDict):
+    """A section of a report.
+
+    The class can be extended o provide novel report components by
+    overriding `._extra_components()`.
+    """
+
+    def __init__(self, require_keys=False):
+        """Initialize the report item collection.
+
+        :param title: report title.
+        :param lead: report strapline, shown below title.
+        :param require_keys: require keys when adding items.
+        """
+        self.require_keys = require_keys
+        self.plots = list()
+        self.md = markdown.Markdown()
+
+    def _add_item(self, item, key=None):
+        """Add an item to the report.
+
+        :param item: item to add.
+        :param key: unique key for item.
+        """
+        if key is None:
+            if self.require_keys:
+                raise ValueError('A key is required.')
+            else:
+                key = str(uuid.uuid4())
+        self[key] = item
+
+    def placeholder(self, key):
+        """Add a placeholder to be filled in later."""
+        self._add_item(None, key=key)
+
+    def plot(self, plot, key=None):
+        """Add a plot to the report.
+
+        :param plot: bokeh plot instance.
+        :param key: unique key for item.
+        """
+        self._add_item(plot, key=key)
+
+    def table(self, df, index=True, key=None, shrink=True, **kwargs):
+        """Add a pandas dataframe to the report.
+
+        :param df: pandas dataframe instance.
+        :param index: include dataframe index in output.
+        :param key: unique key for item.
+        :param shrink: shrink wrap the table to avoid whitespace.
+        :param kwargs: passed to bokeh DataTable.
+        """
+        plot = bokeh_table(df, index=index, **kwargs)
+        plot.height = min(plot.height, 25*(len(df) + 1))
+        self.plot(plot, key)
+
+    def markdown(self, text, key=None):
+        """Add markdown formatted text to the report.
+
+        :param text: markdown formatted text.
+        :param key: unique key for item.
+        """
+        if text is None or text == '':
+            return
+        html = self.md.convert(text)
+        self.md.reset()
+        self._add_item(html, key=key)
+
+    def _plot_components(self):
+        """Return html script and div tags for bokeh plots."""
+        # handle bokeh plots
+        scripts = list()
+        divs = dict()
+        plots = {k: v for k, v in self.items() if isinstance(v, Model)}
+        if len(plots) > 0:
+            scripts, divs = components(plots)
+            scripts = [scripts]
+        return scripts, divs
+
+    def _table_components(self):
+        """Return html script and div for tables."""
+        # TODO: reimplement `.table()` to not use bokeh?
+        pass
+
+    def _extra_components(self):
+        """Return additional script and divs."""
+        # hook for derived classes:
+        # return: list of scripts, dict of divs keyed on self.keys
+        return list(), dict()
+
+    def components(self):
+        """Fetch script and div tags for report."""
+        scripts = list()
+        specials = dict()
+        # retrieve bokeh plot divs
+        plot_scripts, plot_divs = self._plot_components()
+        scripts.extend(plot_scripts)
+        specials.update(plot_divs)
+        # TODO: add special handing for new tables?
+        # retrieve any extra specials
+        extra_scripts, extra_divs = self._extra_components()
+        scripts.extend(extra_scripts)
+        specials.update(extra_divs)
+
+        # put things together in order
+        divs = list()
+        for k in self.keys():
+            try:
+                divs.append(specials[k])
+            except KeyError:
+                if self[k] is None:
+                    raise ValueError(
+                        "Placeholder `{}` was not assigned "
+                        "a value.".format(k)
+                        )
+                # fall back to putting items in directly
+                divs.append(self[k])
+        return scripts, divs
+
+
+class HTMLReport(HTMLSection):
+    """Generate HTML Report from a series of bokeh figures.
+
+    Items added to the report take an optional key argument, adding items
+    with the same key allows an update in place whilst maintaining the order
+    in which items were added. Items can be grouped into sections for easier
+    out of order addition.
+    """
+
+    def __init__(self, title="", lead="", require_keys=False):
+        """Initialize the report item collection.
+
+        :param title: report title.
+        :param lead: report strapline, shown below title.
+        :param require_keys: require keys when adding items.
+        """
+        super().__init__(require_keys=require_keys)
+        self.title = title
+        self.lead = lead
+        self.sections = OrderedDict()
+        self.sections['main'] = self
+        self.template = Template(
+            """\
+            <!doctype html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>{{ title }}</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+                    {{ resources }}
+                    {{ script }}
+                </head>
+                <body>
+            <div class="container">
+              <h1>{{ title }}</h1>
+              <p class="lead">{{ lead }}
+            {{ div }}
+                </body>
+            </html>
+            """  # noqa
+        )
+
+    def add_section(self, key=None, section=None, require_keys=False):
+        """Add a section (grouping of items) to the report.
+
+        :param key: unique key for section.
+        :param section: `HTMLSection` to add rather than creating anew.
+
+        :returns: the report section.
+
+        """
+        if key is None:
+            key = str(uuid.uuid4())
+        section = _maybe_new_report(section, require_keys=require_keys)
+        self.sections[key] = section
+        return self.sections[key]
+
+    def render(self):
+        """Generate HTML report containing figures."""
+        resources = INLINE.render()
+
+        all_scripts = list()
+        all_divs = list()
+        for sec_name, section in self.sections.items():
+            scripts, divs = section.components()
+            all_scripts.extend(scripts)
+            all_divs.extend(divs)
+
+        script = '\n'.join(all_scripts)
+        divs = '\n'.join(all_divs)
+        return self.template.render(
+            title=self.title, lead=self.lead,
+            resources=resources, script=script, div=divs)
+
+    def write(self, path):
+        """Write html report to file."""
+        with open(path, "w", encoding='utf8') as outfile:
+            outfile.write(self.render())
+
+
+def bokeh_table(df, index=True, **kwargs):
+    """Generate a bokeh table from a pandas dataframe."""
+    columns = [TableColumn(field=x, title=x) for x in df.columns]
+    if index:
+        columns = [TableColumn(field="index", title="Feature")] + columns
+    return DataTable(
+            columns=columns, source=ColumnDataSource(df), index_position=None,
+            **kwargs)
---
+++ packages/aplanat/aplanat-0.3.5.tar.gz/aplanat-0.3.5/aplanat/spatial.py
@@ -0,0 +1,182 @@
+"""Plotting of spatial/3D data."""
+
+from bokeh.models import ColorBar, LinearColorMapper, Range1d
+from bokeh.palettes import Blues9
+from bokeh.plotting import figure
+from bokeh.transform import linear_cmap, log_cmap
+from bokeh.util.hex import hexbin
+import numpy as np
+import pandas as pd
+
+from aplanat import util
+
+
+def heatmap(x, y, z, name=None, **kwargs):
+    """Create a heatmap from three columns.
+
+    :param x: x-axis coordinates.
+    :param y: y-axis coordinates.
+    :param z: z-axis coordinates (e.g. counts or frequencies).
+    :param name: title for z-axis.
+    :param kwargs: kwargs for bokeh figure.
+
+    :returns: a bokeh plot.
+
+    """
+    data = {'x': x, 'y': y, 'z': z}
+
+    defaults = {
+        "output_backend": "webgl",
+        "height": 300, "width": 600}
+    defaults.update(kwargs)
+    p = figure(**defaults)
+
+    # define a colour map
+    colors = Blues9[::-1]
+    mapper = LinearColorMapper(
+        palette=colors, low=min(data['z']), high=max(data['z']))
+
+    # plot heatmap rectangles
+    p.rect(
+        source=data, x="x", y="y", width=1, height=1,
+        fill_color={'field': 'z', 'transform': mapper},
+        line_color=None)
+
+    # add colour scale
+    color_bar = ColorBar(
+        title=name, color_mapper=mapper, label_standoff=10,
+        location=(0, 0))
+    p.add_layout(color_bar, 'right')
+
+    # hide some plotting artefacts
+    p.x_range.range_padding = 0
+    p.y_range.range_padding = 0
+    p.xaxis.visible = False
+    p.yaxis.visible = False
+    # set up the axes
+    xbounds = util.pad(data['x'])
+    ybounds = util.pad(data['y'])
+    p.x_range = Range1d(start=xbounds[0], end=xbounds[1], bounds=xbounds)
+    p.y_range = Range1d(start=ybounds[0], end=ybounds[1], bounds=ybounds)
+    return p
+
+
+def heatmap2(
+        x, y, name=None, x_bins=50, y_bins=50, log=False,
+        xlim=(None, None), ylim=(None, None), zlim=(None, None),
+        **kwargs):
+    """Create a heatmap from two columns, using integrate binning.
+
+    In contrast to `heatmap` which takes coordinates and counts, this function
+    takes only coordinates and will count observations within bins.
+
+    :param x: x-axis coordinates.
+    :param y: y-axis coordinates.
+    :param name: title for z-axis.
+    :param x_bins: number of bins in x-direction.
+    :param y_bins: number of bins in y-direction.
+    :param xlim: tuple for plotting limits (start, end). A value None will
+        trigger calculation from the data.
+    :param ylim: tuple for plotting limits (start, end). A value None will
+        trigger calculation from the data.
+    :param zlim: tuple for colour map limits (start, end). A value None will
+        trigger calculation from the data.
+    :param log: use a log-scaled colour map.
+
+    :param kwargs: kwargs for bokeh figure.
+
+    :returns: a bokeh plot.
+
+    """
+    hist, x_edges, y_edges = np.histogram2d(x, y, bins=(x_bins, y_bins))
+    table = np.empty(
+        x_bins * y_bins,
+        dtype=[('row', float), ('col', float), ('value', float)])
+    table['value'] = hist.flatten()
+    table['row'] = np.repeat(x_edges[:x_bins], y_bins)
+    table['col'] = np.tile(y_edges[:y_bins], x_bins)
+    table = pd.DataFrame(table)
+
+    defaults = {
+        "output_backend": "webgl",
+        "height": 300, "width": 600}
+    defaults.update(kwargs)
+    p = figure(**defaults)
+    # define a colour map
+    cmap = log_cmap if log else linear_cmap
+    cmin, cmax = zlim
+    if cmin is None:
+        cmin = min(table['value'])
+    if cmax is None:
+        cmax = max(table['value'])
+    mapper = cmap('counts', 'Viridis256', cmin, cmax)
+    # plot heatmap rectangles
+    width = (x_edges[-1] - x_edges[0]) / x_bins
+    height = (y_edges[-1] - y_edges[0]) / y_bins
+    p.rect(
+        x="row", y="col", source=table,
+        height=height * 1.05, width=width * 1.05,  # fudge to hide artefacts
+        fill_color={'field': 'value', 'transform': mapper['transform']},
+        line_color=None)
+    # add colour scale
+    color_bar = ColorBar(
+        title=name, color_mapper=mapper['transform'], label_standoff=10,
+        location=(0, 0))
+    p.add_layout(color_bar, 'right')
+    # hide some plotting artefacts
+    p.x_range.range_padding = 0
+    p.y_range.range_padding = 0
+    # set up the axes
+    xbounds = util.pad(x_edges[:x_bins])
+    ybounds = util.pad(y_edges[:y_bins])
+    x_lim = util.Limiter().accumulate(xbounds).fix(*xlim)
+    y_lim = util.Limiter().accumulate(ybounds).fix(*ylim)
+    p.x_range = Range1d(
+        start=x_lim.min, end=x_lim.max, bounds=(x_lim.min, x_lim.max))
+    p.y_range = Range1d(
+        start=y_lim.min, end=y_lim.max, bounds=(y_lim.min, y_lim.max))
+    return p
+
+
+def hexmap(
+        x, y, name=None, log=False, match_aspect=True, kernel_size=0.1,
+        xlim=(None, None), ylim=(None, None), **kwargs):
+    """Create a heatmap from two columns.
+
+    :param x: x-axis coordinates.
+    :param y: y-axis coordinates.
+    :param name: title for z-axis.
+    :param kwargs: kwargs for bokeh figure.
+
+    :returns: a bokeh plot.
+
+    """
+    bins = hexbin(x, y, kernel_size)
+
+    defaults = {
+        "output_backend": "webgl",
+        "height": 400, "width": 500,
+        "background_fill_color": "#440154"}
+    defaults.update(kwargs)
+    p = figure(**defaults)
+
+    cmap = log_cmap if log else linear_cmap
+    mapper = cmap('counts', 'Viridis256', 0, max(bins.counts))
+    p.hex_tile(
+        q="q", r="r", size=kernel_size, line_color=None, source=bins,
+        fill_color=mapper)
+
+    color_bar = ColorBar(
+        title="observations", color_mapper=mapper['transform'],
+        label_standoff=10, location=(0, 0))
+    p.add_layout(color_bar, 'right')
+    p.grid.visible = False
+
+    x_lim = util.Limiter().accumulate(x).fix(*xlim)
+    y_lim = util.Limiter().accumulate(y).fix(*ylim)
+    # limit the display range
+    p.x_range = Range1d(
+        start=x_lim.min, end=x_lim.max, bounds=(x_lim.min, x_lim.max))
+    p.y_range = Range1d(
+        start=y_lim.min, end=y_lim.max, bounds=(y_lim.min, y_lim.max))
+    return p, bins
