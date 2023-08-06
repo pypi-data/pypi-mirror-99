5b20fd55d27c1887cfb7f2326dd538f43ef42a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:53 +0000

    Add package irt-parameter-estimation/0.3.0/irt_parameter_estimation-0.3.0.tar.gz

---
+++ packages/irt-parameter-estimation/irt_parameter_estimation-0.3.0.tar.gz/irt_parameter_estimation-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+# Read the version number
+with open("irt_parameter_estimation/_version.py") as f:
+    exec(f.read())
+
+setup(
+    name='irt_parameter_estimation',
+    version=__version__, # use the same version that's in _version.py
+    author='David N. Mashburn',
+    author_email='david.n.mashburn@gmail.com',
+    packages=['irt_parameter_estimation'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/irt_parameter_estimation/',
+    license='LICENSE.txt',
+    description='Parameter estimation routines for logistic Item Characteristic Curves (ICC) from Item Response Theory (IRT)',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires=['numpy>=1.0',
+                      'scipy>=0.8'
+                     ],
+)
