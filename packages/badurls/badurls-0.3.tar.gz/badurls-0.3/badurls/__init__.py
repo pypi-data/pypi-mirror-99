badc29a624a62444b997d45114ac4f222352dbcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:50 +0000

    Add package badurls/0.3/badurls-0.3.tar.gz

---
+++ packages/badurls/badurls-0.3.tar.gz/badurls-0.3/badurls/__init__.py
@@ -0,0 +1,1 @@
+import badurls.url
---
+++ packages/badurls/badurls-0.3.tar.gz/badurls-0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'badurls',         
+  packages = ['badurls'],   
+  version = '0.3',    
+  license='GPLv3',        
+  description = 'Simple URL obfuscation tool', 
+  long_description='Python Program to obfuscate URLs to make Phishing attacks more difficult to detect. Uses Active open redirect list and other URL obfuscation techniques.',
+  author = 'Anish M',                  
+  author_email = 'aneesh25861@gmail.com',     
+  url = 'https://github.com/Anish-M-code/URL-obfuscator',     
+  keywords = ['social engineering','cybersecurity'],  
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+  ],
+)
+
