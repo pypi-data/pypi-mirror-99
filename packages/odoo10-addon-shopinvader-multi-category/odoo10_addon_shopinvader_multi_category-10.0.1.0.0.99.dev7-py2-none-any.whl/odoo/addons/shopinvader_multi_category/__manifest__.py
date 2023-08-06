1d79e09e64cac10e481bfb2a1b6b35b510d55733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:16 +0000

    Add package odoo10-addon-shopinvader-multi-category/10.0.1.0.0.99.dev7/odoo10_addon_shopinvader_multi_category-10.0.1.0.0.99.dev7-py2-none-any.whl

---
+++ packages/odoo10-addon-shopinvader-multi-category/odoo10_addon_shopinvader_multi_category-10.0.1.0.0.99.dev7-py2-none-any.whl/odoo/addons/shopinvader_multi_category/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2018 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Many Categories",
+    "summary": "Shopinvader Many Categories",
+    "version": "10.0.1.0.0",
+    "category": "e-commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "depends": ["shopinvader", "product_multi_category"],
+    "installable": True,
+    "auto_install": True,
+}
