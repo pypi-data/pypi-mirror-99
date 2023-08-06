3df6cf2dd388bd8940eae48aa4aff413330053aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:38 +0000

    Add package bmcs-cross-section/0.0.32a0/bmcs_cross_section-0.0.32a0-py2.py3-none-any.whl

---
+++ packages/bmcs-cross-section/bmcs_cross_section-0.0.32a0-py2.py3-none-any.whl/bmcs_cross_section/cs_design/__init__.py
@@ -0,0 +1,5 @@
+
+from .cs_design import CrossSectionDesign
+from .cs_layout import CrossSectionLayout
+from .cs_shape import CustomShape, TShape, Rectangle, Circle, ICrossSectionShape
+from .cs_reinf_layer import FabricLayer, BarLayer
