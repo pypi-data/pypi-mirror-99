d829aca34829d2853c7f7c71364382dcaf7114f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:36 +0000

    Add package liualgotrader/0.1.1/liualgotrader-0.1.1.tar.gz

---
+++ packages/liualgotrader/liualgotrader-0.1.1.tar.gz/liualgotrader-0.1.1/liualgotrader/fincalcs/vwap.py
@@ -0,0 +1,78 @@
+from datetime import datetime
+
+import pandas as pd
+from pandas import DataFrame as df
+from pandas import Timestamp as ts
+from tabulate import tabulate
+
+from liualgotrader.common import config
+from liualgotrader.common.tlog import tlog
+
+
+def add_daily_vwap(minute_data: df, debug=False) -> bool:
+    back_time = ts(config.market_open)
+
+    try:
+        back_time_index = minute_data["close"].index.get_loc(
+            back_time, method="nearest"
+        )
+    except Exception as e:
+        if debug:
+            tlog(
+                f"IndexError exception {e} in add_daily_vwap for {minute_data}"
+            )
+        return False
+
+    minute_data["pv"] = minute_data.apply(
+        lambda x: (x["close"] + x["high"] + x["low"]) / 3 * x["volume"], axis=1
+    )
+    minute_data["apv"] = minute_data["pv"][back_time_index:].cumsum()
+    minute_data["av"] = minute_data["volume"][back_time_index:].cumsum()
+
+    minute_data["average"] = minute_data["apv"] / minute_data["av"]
+    minute_data["vwap"] = minute_data.apply(
+        lambda x: (x["close"] + x["high"] + x["low"]) / 3, axis=1
+    )
+
+    # print(f"\n{tabulate(minute_data, headers='keys', tablefmt='psql')}")
+    if debug:
+        tlog(
+            f"\n{tabulate(minute_data[-110:-100], headers='keys', tablefmt='psql')}"
+        )
+        tlog(
+            f"\n{tabulate(minute_data[-10:], headers='keys', tablefmt='psql')}"
+        )
+
+    return True
+
+
+def anchored_vwap(
+    ohlc_data: df, start_time: datetime, debug=False
+) -> pd.Series:
+    try:
+        start_time_index = ohlc_data["close"].index.get_loc(
+            start_time, method="nearest"
+        )
+    except Exception as e:
+        if debug:
+            tlog(f"IndexError exception {e} in anchored_vwap for {ohlc_data}")
+        return pd.Series()
+
+    df = ohlc_data.copy()
+    df["pv"] = df.apply(
+        lambda x: (x["close"] + x["high"] + x["low"]) / 3 * x["volume"], axis=1
+    )
+    df["apv"] = df["pv"][start_time_index:].cumsum()
+    df["av"] = df["volume"][start_time_index:].cumsum()
+
+    df["average"] = df["apv"] / df["av"]
+
+    if debug:
+        tlog(
+            f"\n{tabulate(df.average[start_time_index:][-15:], headers='keys', tablefmt='psql')}"
+        )
+        tlog(
+            f"\n{tabulate(df.average[start_time_index:][:15], headers='keys', tablefmt='psql')}"
+        )
+
+    return df.average[start_time_index:]
---
+++ packages/liualgotrader/liualgotrader-0.1.1.tar.gz/liualgotrader-0.1.1/liualgotrader/producer.py
@@ -0,0 +1,266 @@
+"""
+Get Market data from Polygon and pump to consumers
+"""
+import asyncio
+import json
+import os
+import random
+import sys
+import traceback
+from datetime import datetime, timedelta
+from multiprocessing import Queue
+from queue import Empty, Full
+from typing import Dict, List
+
+import alpaca_trade_api as tradeapi
+from pytz import timezone
+from pytz.tzinfo import DstTzInfo
+
+from liualgotrader.common import config
+from liualgotrader.common.data_loader import DataLoader
+from liualgotrader.common.database import create_db_connection
+from liualgotrader.common.tlog import tlog
+from liualgotrader.common.types import QueueMapper, WSEventType
+from liualgotrader.data.data_factory import streaming_factory
+from liualgotrader.models.trending_tickers import TrendingTickers
+from liualgotrader.trading.alpaca import AlpacaTrader
+
+last_msg_tstamp: datetime = datetime.now()
+symbols: List[str]
+data_channels: List = []
+queue_id_hash: Dict[str, int]
+symbol_strategy: Dict = {}
+
+nyc = timezone("America/New_York")
+
+
+async def scanner_input(
+    scanner_queue: Queue,
+    queues: List[Queue],
+    num_consumer_processes: int,
+) -> None:
+    tlog("scanner_input() task starting ")
+    global data_channels
+    global queue_id_hash
+    global symbol_strategy
+    global symbols
+
+    while True:
+        try:
+            symbols_details = scanner_queue.get(timeout=1)
+            if symbols_details:
+                symbols_details = json.loads(symbols_details)
+                new_symbols: List = []
+                new_channels: List = []
+                for symbol_details in symbols_details:
+                    if symbol_details["symbol"] not in symbols:
+                        new_symbols.append(symbol_details["symbol"])
+                        symbol_strategy[
+                            symbol_details["symbol"]
+                        ] = symbol_details["target_strategy_name"]
+                        streaming_factory().get_instance().queues[
+                            symbol_details["symbol"]
+                        ] = queues[
+                            random.SystemRandom().randint(
+                                0, num_consumer_processes - 1
+                            )
+                        ]
+
+                if len(new_symbols):
+                    await streaming_factory().get_instance().subscribe(
+                        new_symbols, [WSEventType.SEC_AGG, WSEventType.MIN_AGG]
+                    )
+                    symbols += new_symbols
+                    data_channels += new_channels
+
+                    trending_db = TrendingTickers(config.batch_id)
+                    await trending_db.save(new_symbols)
+
+                    tlog(
+                        f"added {len(new_symbols)}:{new_symbols} TOTAL: {len(symbols)}"
+                    )
+                    await asyncio.sleep(1)
+
+        except Empty:
+            await asyncio.sleep(30)
+        except asyncio.CancelledError:
+            tlog("scanner_input() task task cancelled ")
+            break
+        except Exception as e:
+            tlog(
+                f"Exception in scanner_input(): exception of type {type(e).__name__} with args {e.args}"
+            )
+            exc_info = sys.exc_info()
+            lines = traceback.format_exception(*exc_info)
+            for line in lines:
+                tlog(f"error: {line}")
+            traceback.print_exception(*exc_info)
+            del exc_info
+
+    tlog("scanner_input() task completed")
+
+
+async def trade_run(qm: QueueMapper) -> None:
+    tlog("trade_run() starting using Alpaca trading ")
+    at = AlpacaTrader(qm)
+    await at.run()
+    tlog("trade_run() completed")
+
+
+async def run(
+    queues: List[Queue],
+    qm: QueueMapper,
+) -> None:
+    global data_channels
+    global queue_id_hash
+
+    ps = streaming_factory()(qm)
+    await ps.run()
+    for symbol in symbols:
+        qm[symbol] = queues[queue_id_hash[symbol]]
+    await ps.subscribe(symbols, [WSEventType.SEC_AGG, WSEventType.MIN_AGG])
+
+
+async def teardown_task(tasks: List[asyncio.Task]) -> None:
+    tlog("producer teardown_task() starting")
+    if not config.market_close:
+        tlog(
+            "we're probably in market schedule by-pass mode, exiting poylgon_producer tear-down task"
+        )
+        return
+
+    dt = datetime.now().astimezone(nyc)
+    to_market_close: timedelta
+    try:
+        to_market_close = (
+            config.market_close - dt
+            if config.market_close > dt
+            else timedelta(hours=24) + (config.market_close - dt)
+        )
+        tlog(
+            f"producer tear-down task waiting for market close: {to_market_close}"
+        )
+    except Exception as e:
+        tlog(
+            f"producer - exception of type {type(e).__name__} with args {e.args}"
+        )
+        return
+
+    try:
+        await asyncio.sleep(to_market_close.total_seconds() + 60 * 5)
+
+        tlog("closing Stream")
+        await streaming_factory().get_instance().close()
+
+        tlog("producer teardown closing web-sockets")
+        tlog("producer teardown closing tasks")
+
+        for task in tasks:
+            tlog(
+                f"teardown_task(): requesting task {task.get_name()} to cancel"
+            )
+            task.cancel()
+            try:
+                await task
+            except asyncio.CancelledError:
+                tlog("teardown_task(): task is cancelled now")
+
+        # asyncio.get_running_loop().stop()
+
+    except asyncio.CancelledError:
+        tlog("teardown_task() cancelled during sleep")
+
+    finally:
+        tlog("teardown_task() done.")
+
+
+"""
+process main
+"""
+
+
+async def producer_async_main(
+    queues: List[Queue],
+    scanner_queue: Queue,
+    num_consumer_processes: int,
+):
+    await create_db_connection(str(config.dsn))
+    qm = QueueMapper()
+    await run(queues=queues, qm=qm)
+
+    at = AlpacaTrader(qm)
+    trade_updates_task = await at.run()
+
+    scanner_input_task = asyncio.create_task(
+        scanner_input(scanner_queue, queues, num_consumer_processes),
+        name="scanner_input",
+    )
+    tear_down = asyncio.create_task(
+        teardown_task(
+            [scanner_input_task],
+        )
+    )
+
+    await asyncio.gather(
+        trade_updates_task,
+        scanner_input_task,
+        tear_down,
+        return_exceptions=True,
+    )
+
+    tlog("producer_async_main() completed")
+
+
+def producer_main(
+    unique_id: str,
+    queues: List[Queue],
+    current_symbols: List[str],
+    current_queue_id_hash: Dict[str, int],
+    market_close: datetime,
+    conf_dict: Dict,
+    scanner_queue: Queue,
+    num_consumer_processes: int,
+) -> None:
+    tlog(f"*** producer_main() starting w pid {os.getpid()} ***")
+    try:
+        config.market_close = market_close
+        config.batch_id = unique_id
+        events = conf_dict.get("events", None)
+
+        if not events:
+            config.WS_DATA_CHANNELS = ["A", "AM", "T", "Q"]
+        else:
+            config.WS_DATA_CHANNELS = []
+
+            if "second" in events:
+                config.WS_DATA_CHANNELS.append("AM")
+            if "minute" in events:
+                config.WS_DATA_CHANNELS.append("A")
+            if "trade" in events:
+                config.WS_DATA_CHANNELS.append("T")
+            if "quote" in events:
+                config.WS_DATA_CHANNELS.append("Q")
+
+        tlog(
+            f"producer_main(): listening for events {config.WS_DATA_CHANNELS}"
+        )
+        global symbols
+        global queue_id_hash
+
+        symbols = current_symbols
+        queue_id_hash = current_queue_id_hash
+        if not asyncio.get_event_loop().is_closed():
+            asyncio.get_event_loop().close()
+        asyncio.run(
+            producer_async_main(queues, scanner_queue, num_consumer_processes)
+        )
+
+    except KeyboardInterrupt:
+        tlog("producer_main() - Caught KeyboardInterrupt")
+    except Exception as e:
+        tlog(
+            f"producer_main() - exception of type {type(e).__name__} with args {e.args}"
+        )
+        traceback.print_exc()
+
+    tlog("*** producer_main() completed ***")
