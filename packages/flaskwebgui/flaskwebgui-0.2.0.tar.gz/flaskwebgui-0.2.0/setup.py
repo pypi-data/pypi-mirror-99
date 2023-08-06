c2570db7e5b752525dbdb13f1df93598d8ba628b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:54 +0000

    Add package flaskwebgui/0.2.0/flaskwebgui-0.2.0.tar.gz

---
+++ packages/flaskwebgui/flaskwebgui-0.2.0.tar.gz/flaskwebgui-0.2.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+#Distribute py wheels
+#python3 setup.py bdist_wheel sdist
+#twine check dist/*
+#cd dist 
+#twine upload *
+
+#Basic GIT
+#git init/clone
+#git add --all
+#git commit -m "made some changes"
+#git push origin master
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup (
+	name="flaskwebgui",
+	version="0.2.0",
+	description="Create desktop applications with Flask/Django/FastAPI!",
+	url="https://github.com/ClimenteA/flaskwebgui",
+	author="Climente Alin",
+	author_email="climente.alin@gmail.com",
+	license='MIT',
+	py_modules=["flaskwebgui"],
+	install_requires=['psutil','whichcraft', 'waitress'],
+	packages=find_packages(),
+	long_description=long_description,
+    long_description_content_type="text/markdown",
+	package_dir={"":"src"}
+)
