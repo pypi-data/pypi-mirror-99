baf2efd5807e982ec569572bdd1342788f75dd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:36 +0000

    Add package esptester/0.0/esptester-0.0.tar.gz

---
+++ packages/esptester/esptester-0.0.tar.gz/esptester-0.0/esptester/__init__.py
@@ -0,0 +1,1 @@
+print("HELLO, from my first module!")
---
+++ packages/esptester/esptester-0.0.tar.gz/esptester-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='esptester',
+      version='0.0',
+      description='My first test module',
+      packages=['esptester'],
+      author_email='igorkorobigor@gmail.com',
+      zip_safe=False)
