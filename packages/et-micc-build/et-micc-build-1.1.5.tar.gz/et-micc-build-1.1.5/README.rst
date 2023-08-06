34b2df8fb7bad869870e0528f0de0d2fe4f4bf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:05 +0000

    Add package et-micc-build/1.1.5/et-micc-build-1.1.5.tar.gz

---
+++ packages/et-micc-build/et-micc-build-1.1.5.tar.gz/et-micc-build-1.1.5/README.rst
@@ -0,0 +1,10 @@
+=============
+et-micc-build
+=============
+
+`Micc-build <https://github.com/etijskens/et-micc-build>`_ is a companion to 
+`Micc <https://github.com/etijskens/et-micc>`_ aiming at building binary extension
+modules written in C++ or Fortran. 
+
+* Free software: MIT license
+* Documentation: https://et-micc-build.readthedocs.io.
---
+++ packages/et-micc-build/et-micc-build-1.1.5.tar.gz/et-micc-build-1.1.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['et_micc_build']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['et-micc==1.1.5', 'numpy>=1.17.0,<2.0.0', 'pybind11>=2.6.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['micc-build = et_micc_build:cli_micc_build.main']}
+
+setup_kwargs = {
+    'name': 'et-micc-build',
+    'version': '1.1.5',
+    'description': 'CLI micc-build and module for building binary extensions created with micc.',
+    'long_description': '=============\net-micc-build\n=============\n\n`Micc-build <https://github.com/etijskens/et-micc-build>`_ is a companion to \n`Micc <https://github.com/etijskens/et-micc>`_ aiming at building binary extension\nmodules written in C++ or Fortran. \n\n* Free software: MIT license\n* Documentation: https://et-micc-build.readthedocs.io.\n',
+    'author': 'Engelbert Tijskens',
+    'author_email': 'engelbert.tijskens@uantwerpen.be',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/etijskens/et-micc-build',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
