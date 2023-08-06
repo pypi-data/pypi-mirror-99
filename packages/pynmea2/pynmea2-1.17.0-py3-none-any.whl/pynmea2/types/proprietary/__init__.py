6410bfe10ae672bfd88005c82a247c77bfbfbf24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:57 +0000

    Add package pynmea2/1.17.0/pynmea2-1.17.0-py3-none-any.whl

---
+++ packages/pynmea2/pynmea2-1.17.0-py3-none-any.whl/pynmea2/types/proprietary/__init__.py
@@ -0,0 +1,11 @@
+from . import ash
+from . import grm
+from . import kwd
+from . import mgn
+from . import rdi
+from . import srf
+from . import sxn
+from . import tnl
+from . import ubx
+from . import vtx
+
