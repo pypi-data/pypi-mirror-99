44335f55be33323e3a04306db11cac11a6138d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:48 +0000

    Add package thonny-lahendus/2.0.5/thonny_lahendus-2.0.5-py3-none-any.whl

---
+++ packages/thonny-lahendus/thonny_lahendus-2.0.5-py3-none-any.whl/thonnycontrib/easy/__init__.py
@@ -0,0 +1,33 @@
+from thonny import get_workbench, THONNY_USER_DIR
+
+from thonnycontrib.easy.ui import ExercisesView
+
+
+class EasyExercisesView(ExercisesView):
+    def __init__(self, master):
+        from thonnycontrib.easy.easy_provider import EasyExerciseProvider
+        super(EasyExercisesView, self).__init__(master, EasyExerciseProvider)
+
+
+class DemoExercisesView(ExercisesView):
+    def __init__(self, master):
+        from thonnycontrib.easy.demo_exercise_provider import DemoExerciseProvider
+        super(DemoExercisesView, self).__init__(master, DemoExerciseProvider)
+
+
+def load_plugin():
+    import logging
+    import os.path
+    import platform
+    formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
+    log_file = os.path.join(THONNY_USER_DIR, "lahendus.log")
+    file_handler = logging.FileHandler(log_file, encoding="UTF-8", mode="a")
+    file_handler.setFormatter(formatter)
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    logger.info(f"Starting plug-in on '{platform.platform()}'")
+
+    # get_workbench().add_view(DemoExercisesView, "DemoEx", "ne")
+    get_workbench().add_view(EasyExercisesView, "Lahendus", "ne")
