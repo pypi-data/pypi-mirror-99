27b162af1847d47c85c362ae12f9ce0c4d524e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:34 +0000

    Add package tg-tool/0.2.0/tg-tool-0.2.0.tar.gz

---
+++ packages/tg-tool/tg-tool-0.2.0.tar.gz/tg-tool-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tg_tool']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['tg-tool = tg_tool.main:app']}
+
+setup_kwargs = {
+    'name': 'tg-tool',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': '',
+    'author': 'sellercoder',
+    'author_email': 'veniamin4e@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
