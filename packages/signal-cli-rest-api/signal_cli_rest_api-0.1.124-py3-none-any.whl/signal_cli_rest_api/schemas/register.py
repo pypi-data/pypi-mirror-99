f50e02e41eef967a1802e299160aff60639a9b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:39 +0000

    Add package signal-cli-rest-api/0.1.124/signal_cli_rest_api-0.1.124-py3-none-any.whl

---
+++ packages/signal-cli-rest-api/signal_cli_rest_api-0.1.124-py3-none-any.whl/signal_cli_rest_api/schemas/register.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Verification(BaseModel):
+    verification_code: str
+    pin: Optional[str] = None
+
+
+class Registration(BaseModel):
+    voice_verification: bool = False
+    captcha: Optional[str]
