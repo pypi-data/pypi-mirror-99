9e9ceb866eacdfaed3e4fa720a3399a3de7f7d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:23 +0000

    Add package rf-kiticalibrary/1.4a3/rf_kiticalibrary-1.4a3-py3-none-any.whl

---
+++ packages/rf-kiticalibrary/rf_kiticalibrary-1.4a3-py3-none-any.whl/KiticaLibrary/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019 Spiralworks Technologies Inc.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#  _     _       _             _  _ _
+# | |   (_)  _  (_)           | |(_) |
+# | |  _ _ _| |_ _  ____ _____| | _| |__   ____ _____  ____ _   _
+# | |_/ ) (_   _) |/ ___|____ | || |  _ \ / ___|____ |/ ___) | | |
+# |  _ (| | | |_| ( (___/ ___ | || | |_) ) |   / ___ | |   | |_| |
+# |_| \_)_|  \__)_|\____)_____|\_)_|____/|_|   \_____|_|    \__  |
+#                                                          (____/
+# kitica DevicePool API
+# Created by    : Joshua Kim Rivera
+# Date          : February 4 2020 14:55 UTC-8
+# Company       : Spiralworks Technologies Inc.
+#
+from KiticaLibrary.KiticaLibrary import KiticaLibrary
