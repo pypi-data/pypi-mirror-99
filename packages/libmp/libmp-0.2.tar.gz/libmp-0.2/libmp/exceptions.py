b2f2df9b882ca3e5d15119cfc3128a84670459c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:10 +0000

    Add package libmp/0.2/libmp-0.2.tar.gz

---
+++ packages/libmp/libmp-0.2.tar.gz/libmp-0.2/libmp/exceptions.py
@@ -0,0 +1,22 @@
+class TiException(Exception):
+    pass
+
+
+class SkyHubException(Exception):
+    pass
+
+
+class CerberusException(Exception):
+    pass
+
+
+class OcException(Exception):
+    pass
+
+
+class MlException(Exception):
+    pass
+
+
+class VariavelAmbienteException(Exception):
+    pass
