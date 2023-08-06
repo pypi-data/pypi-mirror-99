767c4b53fe6e788525fd3c21b9d8504be2b82c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:54 +0000

    Add package probabfunc/0.2/probabfunc-0.2.tar.gz

---
+++ packages/probabfunc/probabfunc-0.2.tar.gz/probabfunc-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probabfunc',
+      version='0.2',
+      description='Gaussian and Binomial/Normal distributions',
+      packages=['probabfunc'],
+      author='Dhiraj Kumar Chaudhary',
+      author_email='dhirajc963@gmail.com',
+      zip_safe=False)
