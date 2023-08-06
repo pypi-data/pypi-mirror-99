9085727574367bfcbe6c7368f5dfa8d1fa72c701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:20 +0000

    Add package extra-boolean/0.1.9/extra-boolean-0.1.9.tar.gz

---
+++ packages/extra-boolean/extra-boolean-0.1.9.tar.gz/extra-boolean-0.1.9/extra_boolean/nor.py
@@ -0,0 +1,20 @@
+from .or_ import or_
+
+
+def nor(a=False, b=False, c=False, d=False, e=False, f=False, g=False, h=False):
+  """Checks if all values are false. `📘`_
+
+  - a: 1st boolean
+  - b: 2nd boolean
+  - ...
+
+  Example:
+    >>> nor(false, false)               == true
+    >>> nor(true, false)                == false
+    >>> nor(false, false, false, false) == true
+    >>> nor(false, false, true, false)  == false
+
+  .. _📘:
+    https://github.com/python3f/extra-boolean/wiki/nor
+  """
+  return not or_(a, b, c, d, e, f, g, h)
