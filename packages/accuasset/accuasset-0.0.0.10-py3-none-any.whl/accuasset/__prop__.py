ce788026fa34c789fe39961d46dac319a58fcf03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:11 +0000

    Add package accuasset/0.0.0.10/accuasset-0.0.0.10-py3-none-any.whl

---
+++ packages/accuasset/accuasset-0.0.0.10-py3-none-any.whl/accuasset/__prop__.py
@@ -0,0 +1,11 @@
+"""Contains meta-information about the accuasset package.
+"""
+
+__all__ = ("__author__", "__url__", '__desc__')
+
+
+__author__ = "SK holdings C&C"
+
+__url__ = ""
+
+__desc__ = "SK Accuinsight Asset package"
