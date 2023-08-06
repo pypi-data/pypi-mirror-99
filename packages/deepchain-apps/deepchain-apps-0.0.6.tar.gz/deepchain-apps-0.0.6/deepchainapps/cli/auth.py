5651fe7ddbbafb500dd52506f1809c7f875fb7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:08 +0000

    Add package deepchain-apps/0.0.6/deepchain-apps-0.0.6.tar.gz

---
+++ packages/deepchain-apps/deepchain-apps-0.0.6.tar.gz/deepchain-apps-0.0.6/deepchainapps/cli/auth.py
@@ -0,0 +1,25 @@
+import getpass
+import json
+import os
+from pathlib import Path
+
+
+def login_args_configuration(sub_parser):
+    login_parser = sub_parser.add_parser(
+        name="login", help="login to deepchain")
+    login_parser.set_defaults(func=login)
+
+
+def login(_):
+    path = Path.home().joinpath(".deep-chain")
+    path.mkdir(exist_ok=True)
+    path = path.joinpath("config")
+
+    data = {}
+    with open(path, "w+") as config_file:
+        try:
+            data = json.load(config_file)
+        except:
+            pass
+        data["pat"] = getpass.getpass("PAT:")
+        json.dump(data, config_file)
