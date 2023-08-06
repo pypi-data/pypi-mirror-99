e940ae69ade68036aace202da63403b2a9bb3b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:21 +0000

    Add package beanie/0.3.1/beanie-0.3.1-py3-none-any.whl

---
+++ packages/beanie/beanie-0.3.1-py3-none-any.whl/beanie/__init__.py
@@ -0,0 +1,6 @@
+from beanie.cursor import Cursor
+from beanie.documents import Document
+from beanie.general import init_beanie
+
+__version__ = "0.3.1"
+__all__ = ["Document", "Cursor", "init_beanie"]
