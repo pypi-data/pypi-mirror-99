8c3ec74818e26c30cb6e1d3981f32da7478957df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:18 +0000

    Add package aiogram2/0.0a3/aiogram2-0.0a3.tar.gz

---
+++ packages/aiogram2/aiogram2-0.0a3.tar.gz/aiogram2-0.0a3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='aiogram2',
+    version='0.0a3',
+    packages=['aiogram', 'aiogram.bot', 'aiogram.types', 'aiogram.utils', 'aiogram.contrib',
+              'aiogram.contrib.fsm_storage', 'aiogram.contrib.middlewares', 'aiogram.dispatcher',
+              'aiogram.dispatcher.filters'],
+    url='',
+    license='MIT',
+    author='LDmitriy7',
+    author_email='ldm.work2019@gmail.com',
+    description='TODO',
+)
