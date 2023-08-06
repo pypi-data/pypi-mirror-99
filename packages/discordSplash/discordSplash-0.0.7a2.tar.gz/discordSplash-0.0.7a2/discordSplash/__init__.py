691d2405e4e5958b253e2a03a7021f32097a38d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:28 +0000

    Add package discordSplash/0.0.7a2/discordSplash-0.0.7a2.tar.gz

---
+++ packages/discordSplash/discordSplash-0.0.7a2.tar.gz/discordSplash-0.0.7a2/discordSplash/__init__.py
@@ -0,0 +1,7 @@
+from .main import *
+import os
+import sys
+PACKAGE_PARENT = '.'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
