e4cbbd77cabeef76ed4cb5177008640e19f8a236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:18 +0000

    Add package honeybee-grasshopper-radiance/1.9.1/honeybee_grasshopper_radiance-1.9.1-py2.py3-none-any.whl

---
+++ packages/honeybee-grasshopper-radiance/honeybee_grasshopper_radiance-1.9.1-py2.py3-none-any.whl/honeybee_grasshopper_radiance/src/HB Direct Sun Hours.py
@@ -0,0 +1,77 @@
+# Honeybee: A Plugin for Environmental Analysis (GPL)
+# This file is part of Honeybee.
+#
+# Copyright (c) 2021, Ladybug Tools.
+# You should have received a copy of the GNU General Public License
+# along with Honeybee; If not, see <http://www.gnu.org/licenses/>.
+# 
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+"""
+Calculate the number of hours of direct sun received by grids of sensors in a
+Honeybee model.
+
+-
+    Args:
+        _model: A Honeybee Model for which Direct Sun Hours will be simulated.
+            Note that this model should have grids assigned to it in order
+            to produce meaningfule results.
+        _wea: A Wea object produced from the Wea components that are under the Light
+            Sources tab. This can also be the path to a .wea or a .epw file.
+        north_: A number between -360 and 360 for the counterclockwise difference
+            between the North and the positive Y-axis in degrees. This can
+            also be Vector for the direction to North. (Default: 0).
+        grid_filter_: Text for a grid identifer or a pattern to filter the sensor grids of
+            the model that are simulated. For instance, first_floor_* will simulate
+            only the sensor grids that have an identifier that starts with
+            first_floor_. By default, all grids in the model will be simulated.
+        sensor_count_: Integer for the maximum number of sensor grid points per
+            parallel execution. (Default: 200).
+        run_settings_: Settings from the "HB Recipe Settings" component that specify
+            how the recipe should be run. This can also be a text string of
+            recipe settings.
+        _run: Set to True to run the recipe and get results.
+
+    Returns:
+        report: Reports, errors, warnings, etc.
+        results: Folder with raw result files (.ill) that contain the number of timesteps
+            that each sensor is exposed to sun. The units are the timestep of
+            input wea file. For an hourly wea, each value corresponds to an hour
+            of direct sun.
+        hours: The cumulative number of timesteps that each sensor sees the sun. If the input
+            wea timestep is 1, the results are the number of direct sun hours.
+"""
+
+ghenv.Component.Name = 'HB Direct Sun Hours'
+ghenv.Component.NickName = 'DirectSunHours'
+ghenv.Component.Message = '1.2.1'
+ghenv.Component.Category = 'HB-Radiance'
+ghenv.Component.SubCategory = '3 :: Recipes'
+ghenv.Component.AdditionalHelpFromDocStrings = '1'
+
+try:
+    from lbt_recipes.recipe import Recipe
+except ImportError as e:
+    raise ImportError('\nFailed to import lbt_recipes:\n\t{}'.format(e))
+
+try:
+    from ladybug_rhino.grasshopper import all_required_inputs, recipe_result
+except ImportError as e:
+    raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
+
+
+if all_required_inputs(ghenv.Component) and _run:
+    # create the recipe and set the input arguments
+    recipe = Recipe('direct-sun-hours')
+    recipe.input_value_by_name('model', _model)
+    recipe.input_value_by_name('wea', _wea)
+    recipe.input_value_by_name('north', north_)
+    recipe.input_value_by_name('sensor-grid', grid_filter_)
+    recipe.input_value_by_name('sensor-count', sensor_count_)
+
+    # run the recipe
+    project_folder = recipe.run(run_settings_, radiance_check=True)
+
+    # load the results
+    results = recipe_result(recipe.output_value_by_name('direct-sun-hours', project_folder))
+    hours = recipe_result(recipe.output_value_by_name('cumulative-sun-hours', project_folder))
---
+++ packages/honeybee-grasshopper-radiance/honeybee_grasshopper_radiance-1.9.1-py2.py3-none-any.whl/honeybee_grasshopper_radiance/src/HB Sensor Grid from Rooms.py
@@ -0,0 +1,132 @@
+# Honeybee: A Plugin for Environmental Analysis (GPL)
+# This file is part of Honeybee.
+#
+# Copyright (c) 2021, Ladybug Tools.
+# You should have received a copy of the GNU General Public License
+# along with Honeybee; If not, see <http://www.gnu.org/licenses/>.
+# 
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+"""
+Generate SensorGrid objects from the floors of honeybee Rooms.
+These SensorGrids can be used in a grid-based recipe.
+-
+The names of the grids will be the same as the rooms that they came from.
+-
+
+    Args:
+        _rooms: A list of honeybee Rooms for which sensor grids will be generated.
+        _grid_size: Number for the size of the grid cells.
+        _dist_floor_: Number for the distance to move points from the floors of
+            the input rooms. The default is 0.8 meters.
+        quad_only_: Boolean to note whether meshing should be done using Rhino's
+            defaults (False), which fills the entire _geometry to the edges
+            with both quad and tringulated faces, or a mesh with only quad
+            faces should be generated.
+            _
+            FOR ADVANCED USERS: This input can also be a vector object that will
+            be used to set the orientation of the quad-only grid. Note that,
+            if a vector is input here that is not aligned with the plane of
+            the room's floors, an error will be raised.
+        remove_out_: Boolean to note whether an extra check should be run to remove
+            sensor points that lie outside the Room volume. Note that this can
+            add significantly to the component's run time and this check is
+            usually not necessary in the case that all walls are vertical
+            and all floors are horizontal (Default: False).
+
+    Returns:
+        grid: A SensorGrid object that can be used in a grid-based recipe.
+        points: The points that are at the center of each grid cell.
+        mesh: Analysis mesh that can be passed to the 'Color Mesh' component.
+"""
+
+ghenv.Component.Name = 'HB Sensor Grid from Rooms'
+ghenv.Component.NickName = 'GridRooms'
+ghenv.Component.Message = '1.2.0'
+ghenv.Component.Category = 'HB-Radiance'
+ghenv.Component.SubCategory = '0 :: Basic Properties'
+ghenv.Component.AdditionalHelpFromDocStrings = '4'
+
+try:  # import the ladybug_geometry dependencies
+    from ladybug_geometry.geometry3d.plane import Plane
+    from ladybug_geometry.geometry3d.face import Face3D
+    from ladybug_geometry.geometry3d.mesh import Mesh3D
+except ImportError as e:
+    raise ImportError('\nFailed to import ladybug_geometry:\n\t{}'.format(e))
+
+try:  # import the core honeybee dependencies
+    from honeybee.facetype import Floor
+    from honeybee.typing import clean_rad_string
+except ImportError as e:
+    raise ImportError('\nFailed to import honeybee:\n\t{}'.format(e))
+
+try:  # import the honeybee-radiance dependencies
+    from honeybee_radiance.sensorgrid import SensorGrid
+except ImportError as e:
+    raise ImportError('\nFailed to import honeybee_radiance:\n\t{}'.format(e))
+
+try:  # import ladybug_rhino dependencies
+    from ladybug_rhino.config import conversion_to_meters, tolerance
+    from ladybug_rhino.togeometry import to_joined_gridded_mesh3d, to_vector3d
+    from ladybug_rhino.fromgeometry import from_mesh3d, from_point3d, from_face3d
+    from ladybug_rhino.grasshopper import all_required_inputs, list_to_data_tree
+except ImportError as e:
+    raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
+
+
+if all_required_inputs(ghenv.Component):
+    # set defaults for any blank inputs and process the quad_only_
+    if _dist_floor_ is None:
+        _dist_floor_ = 0.8 / conversion_to_meters()
+    try:
+        x_axis = to_vector3d(quad_only_)
+    except AttributeError:
+        x_axis = None
+
+    # create lists to be filled with content
+    grid = []
+    points = []
+    mesh = []
+
+    for room in _rooms:
+        # get all of the floor faces of the room as Breps
+        lb_floors = [face.geometry.flip() for face in room.faces if isinstance(face.type, Floor)]
+
+        if len(lb_floors) != 0:
+            # create the gridded ladybug Mesh3D
+            if quad_only_:  # use Ladybug's built-in meshing methods
+                if x_axis:
+                    lb_floors = [Face3D(f.boundary, Plane(f.normal, f[0], x_axis), f.holes)
+                                 for f in lb_floors]
+                lb_meshes = [geo.mesh_grid(_grid_size, offset=_dist_floor_) for geo in lb_floors]
+                lb_mesh = lb_meshes[0] if len(lb_meshes) == 1 else Mesh3D.join_meshes(lb_meshes)
+            else:  # use Rhino's default meshing
+                floor_faces = [from_face3d(face) for face in lb_floors]
+                lb_mesh = to_joined_gridded_mesh3d(floor_faces, _grid_size, _dist_floor_)
+
+            # remove points outside of the room volume if requested
+            if remove_out_:
+                pattern = [room.geometry.is_point_inside(pt)
+                           for pt in lb_mesh.face_centroids]
+                lb_mesh, vertex_pattern = lb_mesh.remove_faces(pattern)
+
+            # extract positions and directions from the mesh
+            base_points = [from_point3d(pt) for pt in lb_mesh.face_centroids]
+            base_poss = [(pt.x, pt.y, pt.z) for pt in lb_mesh.face_centroids]
+            base_dirs = [(vec.x, vec.y, vec.z) for vec in lb_mesh.face_normals]
+
+            # create the sensor grid
+            s_grid = SensorGrid.from_position_and_direction(
+                clean_rad_string(room.display_name), base_poss, base_dirs)
+            s_grid.display_name = room.display_name
+            s_grid.room_identifier = room.identifier
+            s_grid.mesh = lb_mesh
+            s_grid.base_geometry = tuple(f.move(f.normal * _dist_floor_) for f in lb_floors)
+
+            # append everything to the lists
+            grid.append(s_grid)
+            points.append(base_points)
+            mesh.append(from_mesh3d(lb_mesh))
+
+    # convert the lists of points to data trees
+    points = list_to_data_tree(points)
