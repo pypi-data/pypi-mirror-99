c49e2d72ac0714fcd77bf7e3db30f5391e4526db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:01 +0000

    Add package mineauth/1.1/mineauth-1.1.tar.gz

---
+++ packages/mineauth/mineauth-1.1.tar.gz/mineauth-1.1/mineauth/__init__.py
@@ -0,0 +1,58 @@
+__author__ = "Foster Reichert"
+__version__ = 1.1
+__license__ = "MIT"
+
+import requests, json
+
+class Profile:
+    def __init__(self, name, id, raw):
+        self.name = name
+        self.id = id
+        self._raw = raw
+
+    def raw(self):
+        return self._raw
+
+class User:
+    def __init__(self, email, password):
+        self.email = email
+        self.agent = {
+            "name": "Minecraft",
+            "version": 1
+        }
+        data = {
+            "agent": self.agent,
+            "username": self.email,
+            "password": password,
+        }
+        data = json.dumps(data)
+        headers = {
+            "Content-Type": "application/json"
+        }
+        result = requests.post(
+            "https://authserver.mojang.com/authenticate",
+            data=data,
+            headers=headers
+        )
+        self._raw = json.loads(result.text)
+        try:
+            self.client_token = self._raw["clientToken"]
+            self.access_token = self._raw["accessToken"]
+            self.selected_profile = Profile(
+                self._raw["selectedProfile"]["name"],
+                self._raw["selectedProfile"]["id"],
+                self._raw["selectedProfile"]
+            )
+            self.available_profiles = []
+            for profile in self._raw["availableProfiles"]:
+                self.available_profiles.append(Profile(
+                    profile["name"],
+                    profile["id"],
+                    profile
+                ))
+            self.authenticated = True
+        except:
+            self.authenticated = False
+        
+    def raw(self):
+        return self._raw
---
+++ packages/mineauth/mineauth-1.1.tar.gz/mineauth-1.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'mineauth',         # How you named your package folder (MyLib)
+  packages = ['mineauth'],   # Chose the same as "name"
+  version = '1.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Minecraft authentication API wrapper',   # Give a short description about your library
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Foster Reichert',                   # Type in your name
+  author_email = 'fosterhreichert@gmail.com',      # Type in your E-Mail
+  install_requires=[            # I get to this in a second
+          'requests',
+      ]
+)
