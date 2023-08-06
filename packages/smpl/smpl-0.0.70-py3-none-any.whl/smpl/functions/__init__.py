e2bbcfc752fb7e8b3c9580ae87ffbe2e9b54779a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:55 +0000

    Add package smpl/0.0.70/smpl-0.0.70-py3-none-any.whl

---
+++ packages/smpl/smpl-0.0.70-py3-none-any.whl/smpl/functions/__init__.py
@@ -0,0 +1,20 @@
+"""
+A list of various functions
+"""
+from .functions import const, lorentz,linear,line,cos_abs,gauss,square,cube,exp,log,sqrt,tan,cos,sin
+__all__ = [
+    'const', 
+    'lorentz',
+    'cos',
+    'sin',
+    'tan',
+    'linear',
+    'line',
+    'cos_abs',
+    'gauss',
+    'square',
+    'cube',
+    'sqrt',
+    'exp',
+    'log'
+    ]
