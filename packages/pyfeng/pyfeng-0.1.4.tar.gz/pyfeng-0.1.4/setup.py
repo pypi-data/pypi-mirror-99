6afcfc6fd3b6e4a33a2a83d8aa9005c84d29843a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:43 +0000

    Add package pyfeng/0.1.4/pyfeng-0.1.4.tar.gz

---
+++ packages/pyfeng/pyfeng-0.1.4.tar.gz/pyfeng-0.1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pyfeng',
+    version='0.1.4',
+    description='Python Financial Engineering',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Jaehyuk Choi',
+    author_email='pyfe@eml.cc',
+    url='https://github.com/PyFE/PyFENG',
+    project_urls={
+        "Bug Tracker": "https://github.com/PyFE/PyFENG/issues",
+    },
+    packages=['pyfeng'],
+    test_suite="tests",
+    install_requires=[
+        'numpy', 'scipy'
+    ]
+)
