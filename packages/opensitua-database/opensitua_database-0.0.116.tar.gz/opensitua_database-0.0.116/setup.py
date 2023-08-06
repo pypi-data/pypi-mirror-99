c6e7aaed55c8468ddd7c7389c3e65e09296d8291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:17 +0000

    Add package opensitua-database/0.0.116/opensitua_database-0.0.116.tar.gz

---
+++ packages/opensitua-database/opensitua_database-0.0.116.tar.gz/opensitua_database-0.0.116/setup.py
@@ -0,0 +1,28 @@
+import os,re
+import setuptools
+
+VERSION         =  "0.0.116"
+
+PACKAGE_NAME    = "opensitua_database"
+AUTHOR          = "Valerio Luzzi"
+EMAIL           = "valluzzi@gmail.com"
+GITHUB          = "https://github.com/valluzzi/%s.git"%(PACKAGE_NAME)
+DESCRIPTION     = "A core database functions package"
+
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    url=GITHUB,
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=['opensitua_core','psycopg2']
+)
