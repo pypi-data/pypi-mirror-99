5c1feeaffbdae782667d1aa739b433e6cc1ca3c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:20 +0000

    Add package oauth2-lib/1.0.2/oauth2_lib-1.0.2-py3-none-any.whl

---
+++ packages/oauth2-lib/oauth2_lib-1.0.2-py3-none-any.whl/oauth2_lib/async_api_client.py
@@ -0,0 +1,203 @@
+# Copyright 2019-2020 SURF.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import contextlib
+from asyncio import new_event_loop
+from http import HTTPStatus
+from typing import Any, Dict, Generator, Optional
+
+import structlog
+import urllib3
+from authlib.integrations.base_client import BaseOAuth, RemoteApp
+from opentelemetry import context
+from opentelemetry.instrumentation.utils import http_status_to_status_code
+from opentelemetry.instrumentation.version import __version__
+from opentelemetry.propagators import inject
+from opentelemetry.trace import Span, SpanKind, get_tracer
+from opentelemetry.trace.status import Status
+
+logger = structlog.get_logger(__name__)
+
+_SUPPRESS_HTTP_INSTRUMENTATION_KEY = "suppress_http_instrumentation"
+
+
+def is_api_exception(ex: Exception) -> bool:
+    """Test for swagger-codegen ApiException.
+
+    For each API, swagger-codegen generates a new ApiException class. These are not organized into
+    a hierarchy. Hence testing whether one is dealing with one of the ApiException classes without knowing how
+    many there are and where they are located, needs some special logic.
+
+    Args:
+        ex: the Exception to be tested.
+
+    Returns:
+        True if it is an ApiException, False otherwise.
+
+    """
+    return ex.__class__.__name__ == "ApiException"
+
+
+def _apply_response(span: Span, response: urllib3.response.HTTPResponse) -> None:
+    if not span.is_recording():
+        return
+    span.set_attribute("http.status_code", response.status)
+    span.set_attribute("http.status_text", response.reason)
+    span.set_status(Status(http_status_to_status_code(response.status)))
+
+
+@contextlib.contextmanager
+def _suppress_further_instrumentation() -> Generator:
+    token = context.attach(context.set_value(_SUPPRESS_HTTP_INSTRUMENTATION_KEY, True))
+    try:
+        yield
+    finally:
+        context.detach(token)
+
+
+def _is_instrumentation_suppressed() -> bool:
+    return bool(context.get_value("suppress_instrumentation") or context.get_value(_SUPPRESS_HTTP_INSTRUMENTATION_KEY))
+
+
+class AsyncAuthMixin:
+    """Authorization mixin for swagger-codegen generated ApiClients.
+
+    This mixin ensures the proper OAuth2 header is set for API calls if OAuth2 has been enabled. It will also try
+    to refresh the token and retry the call if it expects the token to be expired.
+
+    IMPORTANT: AsyncAuthMixin should be the first class in the inheritance list!
+
+    Given a Fubar API. Usage is::
+
+        import fubar_client
+
+        class FubarApiClient(AuthMixin, fubar_client.ApiClient)
+            pass
+
+    Calls to the Fubar API can now be made as follows:
+
+        from fubar_client import BlahApi
+
+        fac = FubarApiClient('https://api.staging.automation.surf.net/fubar')
+        foo = await BlaApi(fac).get_foo_by_id(foo_id)
+
+    """
+
+    _token: Optional[Dict]
+
+    def __init__(
+        self,
+        oauth_client: BaseOAuth,
+        oauth_client_name: str,
+        oauth_active: bool,
+        tracing_enabled: bool,
+        *args: Any,
+        **kwargs: Any,
+    ):
+        super().__init__(*args, **kwargs)  # type:ignore
+        self._oauth_client: RemoteApp = getattr(oauth_client, oauth_client_name)
+        self._oauth_active = oauth_active
+        self._tracing_enabled = tracing_enabled
+        self._token = None
+
+    def add_client_creds_token_header(self, headers: Dict[str, Any]) -> None:
+        """Add header with credentials to an existing set of headers.
+
+        This function assumes the `access_token` has been set in the application configuration
+        by `refresh_client_creds_token or by acquire_token`.
+
+        Args:
+            headers: Existing set of headers that need to be extended with an Authorization header.
+
+        Returns:
+            New set of headers.
+
+        """
+        if not self._token and self._oauth_active:
+            loop = new_event_loop()
+            loop.run_until_complete(self.refresh_client_creds_token())
+        if self._token:
+            access_token = self._token
+            headers["Authorization"] = f"bearer {access_token['access_token']}"
+
+    async def refresh_client_creds_token(self, force: bool = False) -> None:
+        """
+        Conditionally fetch access_token.
+
+        This method will either set the token if it is not set or reset the token if Force is added,
+        otherwise it will just return.
+
+        Args:
+            force: Force the fetch, even if the access_token is already in the application configuration.
+
+        """
+        if self._token and not force:
+            return
+        elif force:
+            self._token = await self._oauth_client.fetch_access_token()
+        else:
+            self._token = await self._oauth_client.fetch_access_token()
+
+    def request(  # type:ignore
+        self,
+        method,
+        url,
+        query_params=None,
+        headers=None,
+        post_params=None,
+        body=None,
+        _preload_content=True,
+        _request_timeout=None,
+    ):
+        headers = {} if headers is None else headers
+
+        span_attributes = {
+            "http.method": method,
+            "http.url": url,
+        }
+
+        with get_tracer(__name__, __version__).start_as_current_span(
+            f"External Api Call {self.__class__.__name__}", kind=SpanKind.CLIENT, attributes=span_attributes
+        ) as span:
+            try:
+                self.add_client_creds_token_header(headers)
+
+                if self._tracing_enabled and not _is_instrumentation_suppressed():
+                    inject(type(headers).__setitem__, headers)
+
+                with _suppress_further_instrumentation():
+                    response = super().request(  # type:ignore
+                        method, url, query_params, headers, post_params, body, _preload_content, _request_timeout
+                    )
+                _apply_response(span, response)
+                return response
+            except Exception as ex:
+                if is_api_exception(ex) and ex.status in (HTTPStatus.UNAUTHORIZED, HTTPStatus.FORBIDDEN):  # type:ignore
+                    logger.warning("Access Denied. Token expired? Retrying.", api_exception=str(ex))
+                    loop = new_event_loop()
+                    loop.run_until_complete(self.refresh_client_creds_token(force=True))
+                    self.add_client_creds_token_header(headers)
+
+                    if self._tracing_enabled and not _is_instrumentation_suppressed():
+                        inject(type(headers).__setitem__, headers)
+
+                    with _suppress_further_instrumentation():
+                        response = super().request(  # type:ignore
+                            method, url, query_params, headers, post_params, body, _preload_content, _request_timeout
+                        )
+                    _apply_response(span, response)
+                    return response
+
+                else:
+                    logger.exception("Could not call API.", client=self.__class__.__name__)
+                    _apply_response(span, ex)
+                    raise
---
+++ packages/oauth2-lib/oauth2_lib-1.0.2-py3-none-any.whl/oauth2_lib/fastapi.py
@@ -0,0 +1,345 @@
+# Copyright 2019-2020 SURF.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import re
+from http import HTTPStatus
+from json import JSONDecodeError
+from typing import Any, AsyncGenerator, Callable, Coroutine, List, Mapping, Optional, Set, cast
+
+from fastapi.exceptions import HTTPException
+from fastapi.param_functions import Depends
+from fastapi.requests import Request
+from fastapi.security.http import HTTPBearer
+from httpx import AsyncClient, BasicAuth, NetworkError
+from pydantic import BaseModel
+from starlette.requests import ClientDisconnect
+from structlog import get_logger
+
+logger = get_logger(__name__)
+
+
+class OIDCUserModel(dict):
+    """The standard claims of a OIDCUserModel object. Defined per `Section 5.1`_.
+
+    .. _`Section 5.1`: http://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
+    """
+
+    #: registered claims that OIDCUserModel supports
+    REGISTERED_CLAIMS = [
+        "sub",
+        "name",
+        "given_name",
+        "family_name",
+        "middle_name",
+        "nickname",
+        "preferred_username",
+        "profile",
+        "picture",
+        "website",
+        "email",
+        "email_verified",
+        "gender",
+        "birthdate",
+        "zoneinfo",
+        "locale",
+        "phone_number",
+        "phone_number_verified",
+        "address",
+        "updated_at",
+    ]
+
+    def __getattr__(self, key: str) -> Any:
+        try:
+            return object.__getattribute__(self, key)
+        except AttributeError as error:
+            if key in self.REGISTERED_CLAIMS:
+                return self.get(key)
+            raise error
+
+    """
+    Below this line are SURFnet specific properties that are used often in code to either display or check on.
+    """
+
+    @property
+    def user_name(self) -> str:
+        if "user_name" in self.keys():
+            return cast(str, self["user_name"])
+        elif "unspecified_id" in self.keys():
+            return cast(str, self["unspecified_id"])
+        else:
+            return ""
+
+    @property
+    def display_name(self) -> str:
+        return self.get("display_name", "")
+
+    @property
+    def principal_name(self) -> str:
+        return self.get("eduperson_principal_name", "")
+
+    @property
+    def memberships(self) -> List[str]:
+        return self.get("edumember_is_member_of", [])
+
+    @property
+    def teams(self) -> Set[str]:
+        prefix = "urn:collab:group:surfteams.nl:nl:surfnet:diensten:"
+        length = len(prefix)
+        return {urn[length:] for urn in self.memberships if urn.startswith(prefix)}
+
+    @property
+    def entitlements(self) -> List[str]:
+        return self.get("eduperson_entitlement", [])
+
+    @property
+    def roles(self) -> Set[str]:
+        prefix = "urn:mace:surfnet.nl:surfnet.nl:sab:role:"
+        length = len(prefix)
+        return {urn[length:] for urn in self.entitlements if urn.startswith(prefix)}
+
+    @property
+    def organization_codes(self) -> Set[str]:
+        prefix = "urn:mace:surfnet.nl:surfnet.nl:sab:organizationCode:"
+        length = len(prefix)
+        return {urn[length:] for urn in self.entitlements if urn.startswith(prefix)}
+
+    @property
+    def organization_guids(self) -> Set[str]:
+        prefix = "urn:mace:surfnet.nl:surfnet.nl:sab:organizationGUID:"
+        length = len(prefix)
+        return {urn[length:] for urn in self.entitlements if urn.startswith(prefix)}
+
+    @property
+    def scopes(self) -> Set[str]:
+        if isinstance(self.get("scope"), list):
+            return set(self.get("scope"))  # type: ignore
+        return set(re.split("[ ,]", self.get("scope", "")))
+
+
+async def async_client() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient() as client:
+        yield client
+
+
+class OIDCConfig(BaseModel):
+    issuer: str
+    authorization_endpoint: str
+    token_endpoint: str
+    userinfo_endpoint: str
+    introspect_endpoint: str
+    jwks_uri: str
+    response_types_supported: List[str]
+    response_modes_supported: List[str]
+    grant_types_supported: List[str]
+    subject_types_supported: List[str]
+    id_token_signing_alg_values_supported: List[str]
+    scopes_supported: List[str]
+    token_endpoint_auth_methods_supported: List[str]
+    claims_supported: List[str]
+    claims_parameter_supported: bool
+    request_parameter_supported: bool
+    code_challenge_methods_supported: List[str]
+
+
+class OPAResult(BaseModel):
+    result: bool = False
+    decision_id: str
+
+
+class OIDCUser(HTTPBearer):
+    """
+    OIDCUser class extends the HTTPBearer class to do extra verification.
+
+    The class will act as follows:
+        1. Validate the Credentials at SURFconext by calling the UserInfo endpoint
+        2. When receiving an active token it will enrich the response through the database roles
+    """
+
+    openid_config: Optional[OIDCConfig] = None
+    openid_url: str
+    resource_server_id: str
+    resource_server_secret: str
+    enabled: bool
+
+    def __init__(
+        self,
+        openid_url: str,
+        resource_server_id: str,
+        resource_server_secret: str,
+        enabled: bool = True,
+        auto_error: bool = True,
+        scheme_name: Optional[str] = None,
+    ):
+        super().__init__(auto_error=auto_error)
+        self.openid_url = openid_url
+        self.resource_server_id = resource_server_id
+        self.resource_server_secret = resource_server_secret
+        self.enabled = enabled
+        self.scheme_name = scheme_name or self.__class__.__name__
+
+    async def __call__(  # type: ignore
+        self, request: Request, async_client: AsyncClient = Depends(async_client)
+    ) -> Optional[OIDCUserModel]:
+        """
+        Return the OIDC user from OIDC introspect endpoint.
+
+        This is used as a security module in Fastapi projects
+
+        Args:
+            request: Starlette request method.
+            async_client: The httpx client
+
+        Returns:
+            OIDCUserModel object.
+
+        """
+        if self.enabled:
+
+            await self.check_openid_config(async_client)
+
+            credentials = await super().__call__(request)
+            if credentials:
+                user_info = await self.introspect_token(async_client, credentials.credentials)
+
+                if not user_info.get("active", False):
+                    logger.debug("Token is invalid")
+                    raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail="Access token is invalid")
+
+                logger.debug("OIDCUserModel object.", user_info=user_info)
+                return user_info
+
+        return None
+
+    async def check_openid_config(self, async_client: AsyncClient) -> None:
+        """Check of openid config is loaded and load if not."""
+        if self.openid_config is not None:
+            return
+
+        response = await async_client.get(self.openid_url + "/.well-known/openid-configuration")
+        self.openid_config = OIDCConfig.parse_obj(response.json())
+
+    async def introspect_token(self, async_client: AsyncClient, token: str) -> OIDCUserModel:
+        """
+        Introspect the access token to retrieve the user info.
+
+        Args:
+            token: the access_token
+
+        Returns:
+            OIDCUserModel from openid server
+
+        """
+        await self.check_openid_config(async_client)
+        assert self.openid_config
+
+        response = await async_client.post(
+            self.openid_config.introspect_endpoint,
+            params={"token": token},
+            auth=BasicAuth(self.resource_server_id, self.resource_server_secret),
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        try:
+            data = dict(response.json())
+        except JSONDecodeError:
+            logger.debug("Unable to parse introspect response")
+            raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail=response.text)
+        logger.debug("Response from openid introspect", response=data)
+
+        if response.status_code not in range(200, 300):
+            logger.debug("Introspect cannot find an active token, user unauthorized")
+
+            raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail=response.text)
+
+        return OIDCUserModel(data)
+
+
+def opa_decision(
+    opa_url: str,
+    oidc_security: OIDCUser,
+    enabled: bool = True,
+    auto_error: bool = True,
+    opa_kwargs: Optional[Mapping[str, str]] = None,
+) -> Callable[[Request, OIDCUserModel, AsyncClient], Coroutine[Any, Any, Optional[bool]]]:
+    async def _opa_decision(
+        request: Request,
+        user_info: OIDCUserModel = Depends(oidc_security),
+        async_request: AsyncClient = Depends(async_client),
+    ) -> Optional[bool]:
+        """
+        Check OIDCUserModel against the OPA policy.
+
+        This is used as a security module in Fastapi projects
+        This method will make an async call towards the Policy agent.
+
+        Args:
+            request: Request object that will be used to retrieve request metadata.
+            user_info: The OIDCUserModel object that will be checked
+        """
+
+        if enabled:
+            try:
+                json = await request.json()
+            # Silencing the Decode error or Type error when request.json() does not return anything sane.
+            # Some requests do not have a json respone therefore as this code gets called on every request
+            # we need to suppress the `None` case (TypeError) or the `other than json` case (JSONDecodeError)
+            except (JSONDecodeError, TypeError, ClientDisconnect):
+                json = {}
+
+            opa_input = {
+                "input": {
+                    **(opa_kwargs or {}),
+                    **user_info,
+                    "resource": request.url.path,
+                    "method": request.method,
+                    "arguments": {"path": request.path_params, "query": {**request.query_params}, "json": json},
+                }
+            }
+
+            logger.debug("Posting input json to Policy agent", input=opa_input)
+
+            try:
+                result = await async_request.post(opa_url, json=opa_input)
+            except (NetworkError, TypeError):
+                raise HTTPException(status_code=HTTPStatus.SERVICE_UNAVAILABLE, detail="Policy agent is unavailable")
+
+            data = OPAResult.parse_obj(result.json())
+
+            if not data.result and auto_error:
+                logger.debug(
+                    "User is not allowed to access the resource",
+                    decision_id=data.decision_id,
+                    resource=request.url.path,
+                    method=request.method,
+                    user_info=user_info,
+                    input=opa_input,
+                )
+                raise HTTPException(
+                    status_code=HTTPStatus.FORBIDDEN,
+                    detail=f"User is not allowed to access resource: {request.url.path} Decision was taken with id: {data.decision_id}",
+                )
+            else:
+                if data.result:
+                    logger.debug(
+                        "User is authorized to access the resource",
+                        decision_id=data.decision_id,
+                        resource=request.url.path,
+                        method=request.method,
+                        user_info=user_info,
+                        input=opa_input,
+                    )
+
+                return data.result
+
+        return None
+
+    return _opa_decision
