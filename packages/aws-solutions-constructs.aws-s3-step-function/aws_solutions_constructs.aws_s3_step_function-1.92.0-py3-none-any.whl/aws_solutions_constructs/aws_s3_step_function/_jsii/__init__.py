0bcb03b7638f255bcb2d63dedbb947c03fba9cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:47 +0000

    Add package aws-solutions-constructs.aws-s3-step-function/1.92.0/aws_solutions_constructs.aws_s3_step_function-1.92.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-s3-step-function/aws_solutions_constructs.aws_s3_step_function-1.92.0-py3-none-any.whl/aws_solutions_constructs/aws_s3_step_function/_jsii/__init__.py
@@ -0,0 +1,36 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_cloudtrail._jsii
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_stepfunctions._jsii
+import aws_cdk.aws_stepfunctions_tasks._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.aws_events_rule_step_function._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-s3-step-function",
+    "1.92.0",
+    __name__[0:-6],
+    "aws-s3-step-function@1.92.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
