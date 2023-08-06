a14eeb4032a2c192b99c9cf8738db95734f426a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:44 +0000

    Add package ywh2bt/2.1.1/ywh2bt-2.1.1.tar.gz

---
+++ packages/ywh2bt/ywh2bt-2.1.1.tar.gz/ywh2bt-2.1.1/README.md
@@ -0,0 +1,106 @@
+# ywh2bt
+
+ywh2bt synchronizes your vulnerability reports from the [Yes We Hack platform][YesWeHack-Platform]
+with issues of your bug tracker(s). It automatically retrieves reports you want to copy in your bug tracker,
+creates the related issue, and syncs further updates between issues and reports.  
+It comes with a handy GUI to set up and test the integration,
+while completely controlling the information you allow to be synchronized from both side.
+
+![Screenshot of GUI with loaded example file](docs/img/screenshot-gui-example.png)
+
+## Table of contents
+
+- [User Guide](#user-guide)
+- [Architecture](#architecture)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Supported trackers](#supported-trackers)
+- [Changelog](#changelog)
+- [Local development](#local-development)
+    - [Requirements](#requirements-1)
+    - [Installation](#installation-1)
+    - [Usage](#usage-1)
+    - [Updating User Guide](#updating-user-guide)
+
+## User Guide
+
+A User Guide is available in [PDF][User-Guide-pdf] and [HTML][User-Guide-html] formats.
+
+## Architecture
+
+YWH2BT embeds both the GUI to set up the integration,
+and the application to be scheduled on your server to periodically poll and synchronize new reports.  
+You can either run both on a single machine, or prepare the configuration file
+on a computer (with the GUI) and transfer it on the server and use it through a scheduled command.
+
+Since data is pulled from YWH platform to your server, only regular outbound web connections need to be authorized on your server.
+
+## Requirements
+
+- `python` >= 3.7,<=3.9
+- [`pip`](https://pip.pypa.io/en/stable/installing/)
+
+## Supported trackers
+
+- github
+- gitlab
+- jira / jiracloud
+
+## Changelog
+
+- v2.1:
+    - added feedback feature (synchronize from bug tracker to report)
+    - added [docker image yeswehack/ywh2bugtracker](https://hub.docker.com/r/yeswehack/ywh2bugtracker)
+    - added User Guide [PDF][User-Guide-pdf] and [HTML][User-Guide-html]
+- v0.* to v2.0.0:
+    - behavior changes:
+        - reports logs can selectively be synchronized with the trackers:
+            - public comments
+            - private comments
+            - report details changes
+            - report status changes
+            - rewards
+        - a program can now only be synchronized with 1 tracker
+    - added support for JSON configuration files
+    - removed `ywh-bugtracker` command (use `ywh2bt synchronize`)
+    - added `ywh2bt` command:
+        - added `ywh2bt synchronize`:
+            - note: `ywh2bt synchronize --config-file FILE --config-format FORMAT` 
+              is the equivalent of `ywh-bugtracker -n -f FILE` in v0.*
+        - added `ywh2bt validate`
+        - added `ywh2bt test`
+        - added `ywh2bt convert`
+        - added `ywh2bt schema`
+    - removed command line interactive mode
+    - added GUI via `ywh2bt-gui` command
+
+## Local development
+
+### Requirements
+
+- [`poetry`](https://python-poetry.org/) (`pip install poetry`)
+
+### Installation
+
+- `make install` (or `poetry install`): creates a virtualenv and install dependencies
+
+### Usage
+
+Instead of `ywh2bt [command]`, run commands using `poetry run ywh2bt [command]`.
+
+Same goes for `ywh2bt-gui`, run `poetry run ywh2bt-gui` instead.
+
+### Updating User Guide
+
+[PDF][User-Guide-pdf] and [HTML][User-Guide-html] versions of the User Guide are generated via Pandoc
+using [docs/User-Guide.md][User-Guide-md] as an input file.  
+Any changes made to [docs/User-Guide.md][User-Guide-md] **must be followed** by the execution of the command
+`make user-guide` in order to regenerate the PDF and HTML files, **otherwise the CI will fail**.
+
+[YesWeHack-Platform]: https://www.yeswehack.com/
+
+[User-Guide-md]: docs/User-Guide.md
+
+[User-Guide-pdf]: docs/user-guide/User-Guide.pdf
+
+[User-Guide-html]: docs/user-guide/User-Guide.html
---
+++ packages/ywh2bt/ywh2bt-2.1.1.tar.gz/ywh2bt-2.1.1/ywh2bt/core/api/tracker.py
@@ -0,0 +1,315 @@
+"""Models and functions used for data synchronisation between YesWeHack and trackers."""
+from __future__ import annotations
+
+from abc import (
+    ABC,
+    abstractmethod,
+)
+from dataclasses import dataclass
+from datetime import datetime
+from typing import (
+    Any,
+    Dict,
+    Generic,
+    List,
+    Optional,
+    TypeVar,
+)
+
+from ywh2bt.core.api.client import TestableApiClient
+from ywh2bt.core.api.models.report import (
+    Log,
+    Report,
+)
+from ywh2bt.core.configuration.tracker import TrackerConfiguration
+from ywh2bt.core.exceptions import CoreException
+from ywh2bt.core.state.state import State
+
+
+class TrackerClientError(CoreException):
+    """A base tracker client error."""
+
+
+class TrackerIssueState(State):
+    """A state of an issue."""
+
+    _changed: bool = False
+
+    _closed: bool = False
+    _bugtracker_name: Optional[str] = None
+    _downloaded_comments: Optional[List[str]] = None
+
+    def __init__(
+        self,
+        closed: bool = False,
+        bugtracker_name: Optional[str] = None,
+        downloaded_comments: Optional[List[str]] = None,
+    ):
+        """
+        Initialize self.
+
+        Args:
+            closed: a flag indicating the closed status of the tracker issue to which this state belongs
+            bugtracker_name: a bug tracker name of the tracker issue to which this state belongs
+            downloaded_comments:
+            a list of comment ids that have been downloaded (from the bug tracker to yeswehack platform)
+        """
+        self._closed = closed
+        self._bugtracker_name = bugtracker_name
+        self._downloaded_comments = downloaded_comments
+
+    def as_dict(self) -> Dict[str, Any]:
+        """
+        Get the fields as a new dict.
+
+        Returns:
+            the fields as a new dict
+        """
+        return {
+            'closed': self._closed,
+            'bugtracker_name': self._bugtracker_name,
+            'downloaded_comments': list(set(self._downloaded_comments)) if self._downloaded_comments else None,
+        }
+
+    @property
+    def changed(self) -> bool:
+        """
+        Indicate if the underlying values of the state have changed.
+
+        Returns:
+            True if the underlying values of the state have changed
+        """
+        return self._changed
+
+    @property
+    def closed(self) -> bool:
+        """
+        Indicate the closed status of the tracker issue to which this state belongs.
+
+        Returns:
+            True if the tracker issue is closed
+        """
+        return self._closed
+
+    @closed.setter
+    def closed(
+        self,
+        closed: bool,
+    ) -> None:
+        """
+        Set the closed status of the tracker issue to which this state belongs.
+
+        Args:
+            closed: True if the tracker issue is closed
+        """
+        if self._closed == closed:
+            return
+        self._changed = True
+        self._closed = closed
+
+    @property
+    def bugtracker_name(self) -> Optional[str]:
+        """
+        Get the bug tracker name of the tracker issue to which this state belongs.
+
+        Returns:
+            the bug tracker name
+        """
+        return self._bugtracker_name
+
+    @bugtracker_name.setter
+    def bugtracker_name(
+        self,
+        bugtracker_name: str,
+    ) -> None:
+        """
+        Set the bug tracker name of the tracker issue to which this state belongs.
+
+        Args:
+            bugtracker_name: the bug tracker name
+        """
+        if self._bugtracker_name == bugtracker_name:
+            return
+        self._changed = True
+        self._bugtracker_name = bugtracker_name
+
+    @property
+    def downloaded_comments(self) -> Optional[List[str]]:
+        """
+        Get the list of comment ids that have been downloaded (from the bug tracker to yeswehack platform).
+
+        Returns:
+            the list of comment ids
+        """
+        return self._downloaded_comments
+
+    @downloaded_comments.setter
+    def downloaded_comments(
+        self,
+        downloaded_comments: Optional[List[str]],
+    ) -> None:
+        """
+        Set the list of comment ids that have been downloaded (from the bug tracker to yeswehack platform).
+
+        Args:
+            downloaded_comments: the list of comment ids
+        """
+        if self._downloaded_comments == downloaded_comments:
+            return
+        self._changed = True
+        self._downloaded_comments = downloaded_comments
+
+    def add_downloaded_comment(
+        self,
+        comment_id: str,
+    ) -> None:
+        """
+        Add a downloaded comment to the state.
+
+        Args:
+            comment_id: a comment id
+        """
+        if not self.downloaded_comments:
+            self.downloaded_comments = []
+        self.downloaded_comments.append(comment_id)
+
+    def __eq__(
+        self,
+        other: Optional[object],
+    ) -> bool:
+        """
+        Indicate if 2 states are the same.
+
+        Args:
+            other: a state
+
+        Returns:
+            True if the states are the same
+        """
+        return isinstance(other, TrackerIssueState) and all(
+            (
+                self._closed == other.closed,
+                self._bugtracker_name == other.bugtracker_name,
+                self._downloaded_comments == other.downloaded_comments,
+            ),
+        )
+
+
+@dataclass
+class TrackerIssue:
+    """Information about a tracker issue."""
+
+    tracker_url: str
+    project: str
+    issue_id: str
+    issue_url: str
+    closed: bool
+
+
+@dataclass
+class TrackerIssueComment:
+    """Information about a tracker comment on an issue."""
+
+    attachments: Dict[str, TrackerAttachment]
+    author: str
+    comment_id: str
+    created_at: datetime
+    body: str
+
+
+TrackerIssueComments = List[TrackerIssueComment]
+
+
+@dataclass
+class TrackerAttachment:
+    """Attachment on a tracker issue or comment."""
+
+    filename: str
+    mime_type: str
+    content: bytes
+
+
+@dataclass
+class SendLogsResult:
+    """A result of sending logs to a tracker."""
+
+    tracker_issue: TrackerIssue
+    added_comments: TrackerIssueComments
+
+
+T = TypeVar('T', bound=TrackerConfiguration)
+
+
+class TrackerClient(TestableApiClient, ABC, Generic[T]):
+    """An abstract base tracker client."""
+
+    configuration: T
+
+    def __init__(
+        self,
+        configuration: T,
+    ):
+        """
+        Initialize self.
+
+        Args:
+            configuration: a tracker configuration
+        """
+        self.configuration = configuration
+
+    @property
+    @abstractmethod
+    def tracker_type(self) -> str:
+        """Get the type of the tracker client."""
+
+    @abstractmethod
+    def get_tracker_issue(
+        self,
+        issue_id: str,
+    ) -> Optional[TrackerIssue]:
+        """
+        Get a tracker issue.
+
+        Args:
+            issue_id: an issue id
+        """
+
+    @abstractmethod
+    def get_tracker_issue_comments(
+        self,
+        issue_id: str,
+        exclude_comments: Optional[List[str]] = None,
+    ) -> TrackerIssueComments:
+        """
+        Get a list of comments on an issue.
+
+        Args:
+            issue_id: an issue id
+            exclude_comments: an optional list of comment to exclude
+        """
+
+    @abstractmethod
+    def send_report(
+        self,
+        report: Report,
+    ) -> TrackerIssue:
+        """
+        Send a report to the tracker.
+
+        Args:
+            report: a report
+        """
+
+    @abstractmethod
+    def send_logs(
+        self,
+        tracker_issue: TrackerIssue,
+        logs: List[Log],
+    ) -> SendLogsResult:
+        """
+        Send logs to the tracker.
+
+        Args:
+            tracker_issue: information about the tracker issue
+            logs: a list of comments
+        """
