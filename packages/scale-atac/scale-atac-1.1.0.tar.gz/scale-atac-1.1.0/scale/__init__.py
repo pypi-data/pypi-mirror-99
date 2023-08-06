733bc0c517c76b7747866c9e5956088278dbc29f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:15 +0000

    Add package scale-atac/1.1.0/scale-atac-1.1.0.tar.gz

---
+++ packages/scale-atac/scale-atac-1.1.0.tar.gz/scale-atac-1.1.0/scale/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+# Author: Xiong Lei
+# Created Time : Thu 31 May 2018 08:45:33 PM CST
+
+# File Name: __init__.py
+# Description:
+
+"""
+
+__author__ = "Lei Xiong"
+__email__ = "jsxlei@gmail.com"
+
+from .layer import *
+from .model import *
+from .loss import *
+
+
