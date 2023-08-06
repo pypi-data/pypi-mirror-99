db2a5f8c0ae3eb5c395392727e2484a77d68ad37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:21 +0000

    Add package acmenewscollectors-pkg-rioatmadja2018/0.1.1616490875/acmenewscollectors_pkg_rioatmadja2018-0.1.1616490875-py3-none-any.whl

---
+++ packages/acmenewscollectors-pkg-rioatmadja2018/acmenewscollectors_pkg_rioatmadja2018-0.1.1616490875-py3-none-any.whl/acme_news/browser/acme_browser.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from selenium.webdriver import PhantomJS
+from selenium.webdriver.phantomjs.webdriver import WebDriver
+
+class ACMEBrowser(object):
+    """
+    NAME
+        ACMEBrowser
+
+    DESCRIPTION
+    -----------
+        This is a parent class for all the engines and a wrapper class that calls PhantomJS browser
+
+
+    FUNCTIONS
+        __browser__
+        browser
+    """
+
+    def __init__(self):
+        self.url: str = ""
+        self.driver = None
+
+    def __browser__(self, url: str):
+        try:
+            self.driver.get(url=url)
+            return self.driver
+
+        except ConnectionError as e:
+            raise ConnectionError(f"Unable to connect to the following URLs {url}") from e
+
+    def browser(self, url: str) -> WebDriver:
+        if not url:
+            raise ValueError(f"Please provide the right URL.")
+
+        self.url = url
+        try:
+            self.driver = PhantomJS()
+
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f"Unable to find phantomjs binary") from e
+
+        else:
+            return self.__browser__(url=self.url)
