f25104db68cd76cc359580bf72c93bb3d6f77481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:05 +0000

    Add package mudkip/0.4.1/mudkip-0.4.1.tar.gz

---
+++ packages/mudkip/mudkip-0.4.1.tar.gz/mudkip-0.4.1/mudkip/preset.py
@@ -0,0 +1,51 @@
+from .server import livereload_dev_server
+
+
+def preset(func):
+    Preset.register(Preset(func.__name__, func))
+    return func
+
+
+class Preset:
+    registry = {}
+
+    def __init__(self, name, callback=None):
+        self.name = name
+        self.callback = callback
+
+    def execute(self, config):
+        if self.callback:
+            self.callback(config)
+
+    @classmethod
+    def register(cls, preset):
+        cls.registry[preset.name] = preset
+        return preset
+
+    @classmethod
+    def get(cls, name):
+        return cls.registry[name]
+
+
+@preset
+def xml(config):
+    pass
+
+
+@preset
+def alabaster(config):
+    config.dev_server = livereload_dev_server
+    config.sphinx_buildername = "dirhtml"
+
+
+@preset
+def rtd(config):
+    alabaster(config)
+    config.override.setdefault("html_theme", "sphinx_rtd_theme")
+
+
+@preset
+def furo(config):
+    alabaster(config)
+    config.override.setdefault("html_theme", "furo")
+    config.override.setdefault("html_css_files", []).append("mudkip_furo.css")
