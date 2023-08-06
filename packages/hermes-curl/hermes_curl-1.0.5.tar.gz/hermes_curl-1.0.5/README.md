5777dfa0150aa61610454ce65ab7c3671f8ef6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:28 +0000

    Add package hermes-curl/1.0.5/hermes_curl-1.0.5.tar.gz

---
+++ packages/hermes-curl/hermes_curl-1.0.5.tar.gz/hermes_curl-1.0.5/README.md
@@ -0,0 +1,103 @@
+# Hermes cURL
+
+Hermes cURL is a lightweight wrapper on top of cURL that provides reusable HTTP request configurations in YAML. Configuration files can import attributes from other files which allows for common settings such as hostnames and authentication headers to be shared by multiple cURL calls.
+
+## Usage
+
+Set up a base configuration for an entire API:
+
+```yaml
+# api_base.yml
+headers:
+  Authorization: Token MySecretAPIToken
+  Content-Type: application/json
+host: localhost:5000
+curl_flags:
+   "--location":
+```
+
+Set up a configuration for a specific endpoint and import our the base settings:
+
+```yaml
+# api.yml
+from: api_base.yml
+method: PUT
+path: /my/api
+body: >
+  {"hello": "world"}
+```
+
+Run the `hermes` command:
+
+```
+$ hermes api.yml
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+100   128  100   112  100    16   3960    565 --:--:-- --:--:-- --:--:--  4000
+{
+   "msg": "hello Mr world"
+}
+```
+
+See [examples](https://github.com/newswangerd/hermes-curl/tree/master/examples) for a working example.
+
+## Installation
+
+```
+python3 -m pip install hermes-curl
+```
+
+## Configuration file reference
+
+### Options
+
+- `from` (optional): specify a path to another configuration file to inherit configurations from. This path is relative to the current file.
+- `path` (requires): path on the server to send the request to, including any query params.
+- `method` (required): HTTP method to use.
+- `host` (required): server host, including protocol and port.
+- `headers` (optional): dictionary of headers in the form. Ex`Header: value`.
+- `body` (optional): request body.
+- `curl_flags` (optional): a dictionary with any other curl flags to add. Ex `"--cacert: certfile"`.
+- `template_defaults` (optional): a dictionary of default template variables.
+
+### Config Inheritance
+
+Configurations are merged in the following fashion:
+
+- Dictionaries are merged. Values set in child configs having precedence and override
+  any values from the parent configuration.
+- `path` is concatenated, so setting `path: /foo/` in the parent and `path: /bar/`
+  in the child will result in `path: /foo/bar/`
+- All other top level configurations use whatever value is set in the child configuration.
+  This includes `method`, `body`, and `host`.
+
+### Template Variables
+
+Configurations can contain variables using the `{variable_name}` syntax. Variables can
+be passed into the configuration by either setting `-t variable_name=value` on the cli
+or by setting `template_defaults` in the configuration. Values passed on the cli have
+precedence over values set in `template_defaults.`
+
+#### Example
+
+Running `hermes -t pk=2 example.yml` will `GET http://localhost/api/thing/2/` and running
+`hermes example.yml` will `GET http://localhost/api/thing/1/`
+
+```yaml
+host: http://localhost/
+path: api/thing/{pk}/
+method: GET
+template_defaults:
+  pk: 1
+```
+
+#### Example
+
+Template variables can also be used via environment variables. This will inject the `TOKEN`
+environment variable into the `Token` header.
+
+```
+headers:
+  Content-Type: application/json
+  Authorization: "Token {env[TOKEN]}"
+```
---
+++ packages/hermes-curl/hermes_curl-1.0.5.tar.gz/hermes_curl-1.0.5/hermes/main.py
@@ -0,0 +1,173 @@
+import yaml
+import argparse
+import os
+import json
+import urllib.parse
+
+from pathlib import Path
+
+
+class Config:
+
+    _context = {
+        "env": os.environ,
+    }
+
+    def __init__(self, config, templates):
+        self._config = config
+        self._cli_templates = self.load_templates(templates)
+        self._config_templates = self._config.get('template_defaults', {})
+
+    def __str__(self):
+        return json.dumps(self._config, indent=2)
+
+    def load_templates(self, templates):
+        t = {}
+        if templates is None:
+            return t
+        for x in templates:
+            vals = x.split('=', 1)
+            t[vals[0]] = vals[1]
+        return t
+
+    def __getitem__(self, key):
+        # return config item with all {templated} items rendered
+        # cli flags have prescendece
+        return self._format_config(self._config[key], key)
+
+    def get(self, *args, **kwargs):
+        return self._config.get(*args, **kwargs)
+
+    def get_headers(self):
+        header_vals = []
+        headers = self.get('headers', {})
+        for header in headers:
+            header_vals.append("-H '{header}: {value}'".format(header=header, value=headers[header]))
+
+        return self._format_config(' '.join(header_vals), 'headers')
+
+    def get_curl_flags(self):
+        flag_vals = []
+        flags = self.get('curl_flags', {})
+
+        print(flags)
+        for flag in flags:
+            val = flags[flag]
+            if not val:
+                val = ''
+            flag_vals.append("{flag} {value}".format(flag=flag, value=val))
+
+        return self._format_config(' '.join(flag_vals), 'curl_flags')
+
+    def _format_config(self, value, key):
+        merged_template = {**self._context, **self._config_templates, **self._cli_templates}
+
+        try:
+            return value.format(**merged_template)
+        except KeyError as e:
+            variable = str(e).replace("'", "")
+            print(f"A template variable '{variable}' was set in '{key}', but no value "
+                  f"has been configured for it. Either pass '-t {variable}=<value>' or "
+                  f"set '{variable}' under 'template_defaults'")
+            exit(1)
+
+
+def pars_args():
+    parser = argparse.ArgumentParser(description='Run curl from simple yaml configurations.')
+    parser.add_argument('config', nargs=1, help='Configuration file to use.')
+    parser.add_argument('--print', '-p', action='store_true', dest='print_cmd', help='Print the curl command instead of executing it.')
+    parser.add_argument('-v', action='store_true', dest='verbose', help='Verbose.')
+    parser.add_argument('--template', '-t', type=str, action='append', dest='template_vars', help='Specify values for templated variables.')
+
+    return parser.parse_args()
+
+
+def merge_config(parent, child):
+    '''
+    Merges two configs together. Values set in the child take precendece over
+    values in the parent.
+    '''
+
+    # Merge paths by concatenating them.
+    if parent.get('path') and child.get('path'):
+        child['path'] = urllib.parse.urljoin(parent['path'], child['path'])
+
+    merged = {**parent, **child}
+
+    for val in merged:
+        if isinstance(merged[val], dict):
+            merged[val] = {**parent.get(val, {}), **child.get(val, {})}
+
+    return merged
+
+
+def load_config(config_file, child_config={}):
+    config = {}
+    with open(config_file, 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            exit(1)
+
+    if config.get('from'):
+        from_path = os.path.join(Path(config_file).parent, config.get('from'))
+        parent_config = load_config(from_path, config)
+        del config['from']
+        return merge_config(parent_config, config)
+
+    return config
+
+
+def main():
+    args = pars_args()
+
+    config = Config(
+        load_config(os.path.join(os.getcwd(), args.config[0])),
+        args.template_vars
+    )
+
+    body = config.get('body', '').strip()
+    headers = config.get_headers()
+    curl_flags = config.get_curl_flags()
+
+    try:
+        url = urllib.parse.urljoin(config['host'], config['path'])
+        method = config['method']
+    except KeyError:
+        print("Host, path and method must be set.")
+        exit(1)
+
+    curl_cmd = [
+        'curl',
+        '-X ' + method,
+    ]
+
+    if (body != ''):
+        curl_cmd.append("-d '{body}'".format(body=body))
+    if (headers != ''):
+        curl_cmd.append(headers)
+    if (curl_flags != ''):
+        curl_cmd.append(curl_flags)
+
+    curl_cmd.append(url)
+
+    if (args.print_cmd):
+        print(' \\\n  '.join(curl_cmd))
+        exit(0)
+
+    cmd = ' '.join(curl_cmd)
+    if args.verbose:
+        print('Curl Command:')
+        print(cmd)
+        print('Parsed Config:')
+        print(config)
+
+    stream = os.popen(cmd)
+    for line in stream.readlines():
+        try:
+            print(json.dumps(json.loads(line), indent=2))
+        except json.decoder.JSONDecodeError:
+            print(line)
+
+    print(f"{method}: {url}")
