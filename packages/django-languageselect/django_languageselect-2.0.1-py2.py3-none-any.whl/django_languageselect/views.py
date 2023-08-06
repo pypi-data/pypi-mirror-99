ad53e67c63d9707664210cd907996caf55f1ae18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:47 +0000

    Add package django-languageselect/2.0.1/django_languageselect-2.0.1-py2.py3-none-any.whl

---
+++ packages/django-languageselect/django_languageselect-2.0.1-py2.py3-none-any.whl/django_languageselect/views.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+
+from django.views.generic import View
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.utils import translation
+
+from . import forms
+
+
+class IndexView(View):
+    def get(self, request):
+        next = "/"
+        if "next" in request.GET:
+            next = request.GET.get("next")
+
+        response = HttpResponseRedirect(next)
+
+        if not request.GET:
+            return response
+
+        form = forms.LanguageCodeForm(data=request.GET)
+        if not form.is_valid():
+            return response
+
+        language = form.cleaned_data['language']
+
+        if hasattr(request, "session"):
+            request.session[translation.LANGUAGE_SESSION_KEY] = language
+        else:
+            response.set_cookie(settings.LANGUAGE_COOKIE_NAME, language)
+        translation.activate(language)
+
+        return response
