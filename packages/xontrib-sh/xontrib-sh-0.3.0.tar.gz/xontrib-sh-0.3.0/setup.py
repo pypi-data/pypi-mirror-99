2ee459cb0e193c787b9d1f27fa20814132f826e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:28 +0000

    Add package xontrib-sh/0.3.0/xontrib-sh-0.3.0.tar.gz

---
+++ packages/xontrib-sh/xontrib-sh-0.3.0.tar.gz/xontrib-sh-0.3.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+try:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+except (IOError, OSError):
+    long_description = ''
+
+setup(
+    name='xontrib-sh',
+    version='0.3.0',
+    license='BSD',
+    author='anki',
+    author_email='author@example.com',
+    description="Paste and run commands from bash, zsh, fish, tcsh, pwsh, cmd in xonsh shell.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.py']},
+    platforms='any',
+    url='https://github.com/anki-code/xontrib-sh',
+    project_urls={
+        "Documentation": "https://github.com/anki-code/xontrib-sh/blob/master/README.md",
+        "Code": "https://github.com/anki-code/xontrib-sh",
+        "Issue tracker": "https://github.com/anki-code/xontrib-sh/issues",
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Unix Shell",
+        "Topic :: System :: Shells",
+        "Topic :: System :: System Shells",
+        "Topic :: Terminals",
+        "Topic :: System :: Networking",
+        "License :: OSI Approved :: BSD License"
+    ]
+)
