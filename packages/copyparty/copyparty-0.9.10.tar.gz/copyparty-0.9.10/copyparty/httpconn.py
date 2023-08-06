3946259805a90e2ec5aed2eeef070f9c08d60250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:07 +0000

    Add package copyparty/0.9.10/copyparty-0.9.10.tar.gz

---
+++ packages/copyparty/copyparty-0.9.10.tar.gz/copyparty-0.9.10/copyparty/httpconn.py
@@ -0,0 +1,193 @@
+# coding: utf-8
+from __future__ import print_function, unicode_literals
+
+import os
+import sys
+import time
+import socket
+
+HAVE_SSL = True
+try:
+    import ssl
+except:
+    HAVE_SSL = False
+
+try:
+    import jinja2
+except ImportError:
+    print(
+        """\033[1;31m
+  you do not have jinja2 installed,\033[33m
+  choose one of these:\033[0m
+   * apt install python-jinja2
+   * {} -m pip install --user jinja2
+   * (try another python version, if you have one)
+   * (try copyparty.sfx instead)
+""".format(
+            os.path.basename(sys.executable)
+        )
+    )
+    sys.exit(1)
+
+from .__init__ import E
+from .util import Unrecv
+from .httpcli import HttpCli
+from .u2idx import U2idx
+
+
+class HttpConn(object):
+    """
+    spawned by HttpSrv to handle an incoming client connection,
+    creates an HttpCli for each request (Connection: Keep-Alive)
+    """
+
+    def __init__(self, sck, addr, hsrv):
+        self.s = sck
+        self.addr = addr
+        self.hsrv = hsrv
+
+        self.args = hsrv.args
+        self.auth = hsrv.auth
+        self.cert_path = hsrv.cert_path
+
+        self.t0 = time.time()
+        self.nbyte = 0
+        self.workload = 0
+        self.u2idx = None
+        self.log_func = hsrv.log
+        self.set_rproxy()
+
+        env = jinja2.Environment()
+        env.loader = jinja2.FileSystemLoader(os.path.join(E.mod, "web"))
+        self.tpl_mounts = env.get_template("splash.html")
+        self.tpl_browser = env.get_template("browser.html")
+        self.tpl_msg = env.get_template("msg.html")
+        self.tpl_md = env.get_template("md.html")
+        self.tpl_mde = env.get_template("mde.html")
+
+    def set_rproxy(self, ip=None):
+        if ip is None:
+            color = 36
+            ip = self.addr[0]
+            self.rproxy = None
+        else:
+            color = 34
+            self.rproxy = ip
+
+        self.ip = ip
+        self.log_src = "{} \033[{}m{}".format(ip, color, self.addr[1]).ljust(26)
+        return self.log_src
+
+    def respath(self, res_name):
+        return os.path.join(E.mod, "web", res_name)
+
+    def log(self, msg, c=0):
+        self.log_func(self.log_src, msg, c)
+
+    def get_u2idx(self):
+        if not self.u2idx:
+            self.u2idx = U2idx(self.args, self.log_func)
+
+        return self.u2idx
+
+    def _detect_https(self):
+        method = None
+        if self.cert_path:
+            try:
+                method = self.s.recv(4, socket.MSG_PEEK)
+            except socket.timeout:
+                return
+            except AttributeError:
+                # jython does not support msg_peek; forget about https
+                method = self.s.recv(4)
+                self.sr = Unrecv(self.s)
+                self.sr.buf = method
+
+                # jython used to do this, they stopped since it's broken
+                # but reimplementing sendall is out of scope for now
+                if not getattr(self.s, "sendall", None):
+                    self.s.sendall = self.s.send
+
+            if len(method) != 4:
+                err = "need at least 4 bytes in the first packet; got {}".format(
+                    len(method)
+                )
+                self.log(err)
+                self.s.send(b"HTTP/1.1 400 Bad Request\r\n\r\n" + err.encode("utf-8"))
+                return
+
+        return method not in [None, b"GET ", b"HEAD", b"POST", b"PUT ", b"OPTI"]
+
+    def run(self):
+        self.sr = None
+        if self.args.https_only:
+            is_https = True
+        elif self.args.http_only or not HAVE_SSL:
+            is_https = False
+        else:
+            is_https = self._detect_https()
+
+        if is_https:
+            if self.sr:
+                self.log("TODO: cannot do https in jython", c="1;31")
+                return
+
+            self.log_src = self.log_src.replace("[36m", "[35m")
+            try:
+                ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+                ctx.load_cert_chain(self.cert_path)
+                if self.args.ssl_ver:
+                    ctx.options &= ~self.args.ssl_flags_en
+                    ctx.options |= self.args.ssl_flags_de
+                    # print(repr(ctx.options))
+
+                if self.args.ssl_log:
+                    try:
+                        ctx.keylog_filename = self.args.ssl_log
+                    except:
+                        self.log("keylog failed; openssl or python too old")
+
+                if self.args.ciphers:
+                    ctx.set_ciphers(self.args.ciphers)
+
+                self.s = ctx.wrap_socket(self.s, server_side=True)
+                msg = [
+                    "\033[1;3{:d}m{}".format(c, s)
+                    for c, s in zip([0, 5, 0], self.s.cipher())
+                ]
+                self.log(" ".join(msg) + "\033[0m")
+
+                if self.args.ssl_dbg and hasattr(self.s, "shared_ciphers"):
+                    overlap = [y[::-1] for y in self.s.shared_ciphers()]
+                    lines = [str(x) for x in (["TLS cipher overlap:"] + overlap)]
+                    self.log("\n".join(lines))
+                    for k, v in [
+                        ["compression", self.s.compression()],
+                        ["ALPN proto", self.s.selected_alpn_protocol()],
+                        ["NPN proto", self.s.selected_npn_protocol()],
+                    ]:
+                        self.log("TLS {}: {}".format(k, v or "nah"))
+
+            except Exception as ex:
+                em = str(ex)
+
+                if "ALERT_BAD_CERTIFICATE" in em:
+                    # firefox-linux if there is no exception yet
+                    self.log("client rejected our certificate (nice)")
+
+                elif "ALERT_CERTIFICATE_UNKNOWN" in em:
+                    # chrome-android keeps doing this
+                    pass
+
+                else:
+                    self.log("handshake\033[0m " + em, c=5)
+
+                return
+
+        if not self.sr:
+            self.sr = Unrecv(self.s)
+
+        while True:
+            cli = HttpCli(self)
+            if not cli.run():
+                return
---
+++ packages/copyparty/copyparty-0.9.10.tar.gz/copyparty-0.9.10/copyparty/web/mde.js
@@ -0,0 +1,226 @@
+"use strict";
+
+var dom_wrap = ebi('mw');
+var dom_nav = ebi('mn');
+var dom_doc = ebi('m');
+var dom_md = ebi('mt');
+
+(function () {
+    var n = document.location + '';
+    n = n.substr(n.indexOf('//') + 2).split('?')[0].split('/');
+    n[0] = 'top';
+    var loc = [];
+    var nav = [];
+    for (var a = 0; a < n.length; a++) {
+        if (a > 0)
+            loc.push(n[a]);
+
+        var dec = uricom_dec(n[a])[0].replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+
+        nav.push('<a href="/' + loc.join('/') + '">' + dec + '</a>');
+    }
+    dom_nav.innerHTML = nav.join('');
+})();
+
+var mde = (function () {
+    var tbar = [
+        {
+            name: "light",
+            title: "light",
+            className: "fa fa-lightbulb",
+            action: lightswitch
+        }, {
+            name: "save",
+            title: "save",
+            className: "fa fa-save",
+            action: save
+        }, '|',
+        'bold', 'italic', 'strikethrough', 'heading', '|',
+        'code', 'quote', 'unordered-list', 'ordered-list', 'clean-block', '|',
+        'link', 'image', 'table', 'horizontal-rule', '|',
+        'preview', 'side-by-side', 'fullscreen', '|',
+        'undo', 'redo'];
+
+    var mde = new EasyMDE({
+        autoDownloadFontAwesome: false,
+        autofocus: true,
+        spellChecker: false,
+        renderingConfig: {
+            markedOptions: {
+                breaks: true,
+                gfm: true
+            }
+        },
+        shortcuts: {
+            "save": "Ctrl-S"
+        },
+        insertTexts: ["[](", ")"],
+        indentWithTabs: false,
+        tabSize: 2,
+        toolbar: tbar,
+        previewClass: 'mdo',
+        onToggleFullScreen: set_jumpto,
+    });
+    md_changed(mde, true);
+    mde.codemirror.on("change", function () {
+        md_changed(mde);
+    });
+    var loader = ebi('ml');
+    loader.parentNode.removeChild(loader);
+    return mde;
+})();
+
+function set_jumpto() {
+    document.querySelector('.editor-preview-side').onclick = jumpto;
+}
+
+function jumpto(ev) {
+    var tgt = ev.target || ev.srcElement;
+    var ln = null;
+    while (tgt && !ln) {
+        ln = tgt.getAttribute('data-ln');
+        tgt = tgt.parentElement;
+    }
+    var ln = parseInt(ln);
+    console.log(ln);
+    var cm = mde.codemirror;
+    var y = cm.heightAtLine(ln - 1, 'local');
+    var y2 = cm.heightAtLine(ln, 'local');
+    cm.scrollTo(null, y + (y2 - y) - cm.getScrollInfo().clientHeight / 2);
+}
+
+function md_changed(mde, on_srv) {
+    if (on_srv)
+        window.md_saved = mde.value();
+
+    var md_now = mde.value();
+    var save_btn = document.querySelector('.editor-toolbar button.save');
+
+    if (md_now == window.md_saved)
+        save_btn.classList.add('disabled');
+    else
+        save_btn.classList.remove('disabled');
+
+    set_jumpto();
+}
+
+function save(mde) {
+    var save_btn = document.querySelector('.editor-toolbar button.save');
+    if (save_btn.classList.contains('disabled')) {
+        alert('there is nothing to save');
+        return;
+    }
+    var force = save_btn.classList.contains('force-save');
+    if (force && !confirm('confirm that you wish to lose the changes made on the server since you opened this document')) {
+        alert('ok, aborted');
+        return;
+    }
+
+    var txt = mde.value();
+
+    var fd = new FormData();
+    fd.append("act", "tput");
+    fd.append("lastmod", (force ? -1 : last_modified));
+    fd.append("body", txt);
+
+    var url = (document.location + '').split('?')[0];
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.responseType = 'text';
+    xhr.onreadystatechange = save_cb;
+    xhr.btn = save_btn;
+    xhr.mde = mde;
+    xhr.txt = txt;
+    xhr.send(fd);
+}
+
+function save_cb() {
+    if (this.readyState != XMLHttpRequest.DONE)
+        return;
+
+    if (this.status !== 200) {
+        alert('Error!  The file was NOT saved.\n\n' + this.status + ": " + (this.responseText + '').replace(/^<pre>/, ""));
+        return;
+    }
+
+    var r;
+    try {
+        r = JSON.parse(this.responseText);
+    }
+    catch (ex) {
+        alert('Failed to parse reply from server:\n\n' + this.responseText);
+        return;
+    }
+
+    if (!r.ok) {
+        if (!this.btn.classList.contains('force-save')) {
+            this.btn.classList.add('force-save');
+            var msg = [
+                'This file has been modified since you started editing it!\n',
+                'if you really want to overwrite, press save again.\n',
+                'modified ' + ((r.now - r.lastmod) / 1000) + ' seconds ago,',
+                ((r.lastmod - last_modified) / 1000) + ' sec after you opened it\n',
+                last_modified + ' lastmod when you opened it,',
+                r.lastmod + ' lastmod on the server now,',
+                r.now + ' server time now,\n',
+            ];
+            alert(msg.join('\n'));
+        }
+        else {
+            alert('Error! Save failed.  Maybe this JSON explains why:\n\n' + this.responseText);
+        }
+        return;
+    }
+
+    this.btn.classList.remove('force-save');
+    //alert('save OK -- wrote ' + r.size + ' bytes.\n\nsha512: ' + r.sha512);
+
+    // download the saved doc from the server and compare
+    var url = (document.location + '').split('?')[0] + '?raw';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'text';
+    xhr.onreadystatechange = save_chk;
+    xhr.btn = this.save_btn;
+    xhr.mde = this.mde;
+    xhr.txt = this.txt;
+    xhr.lastmod = r.lastmod;
+    xhr.send();
+}
+
+function save_chk() {
+    if (this.readyState != XMLHttpRequest.DONE)
+        return;
+
+    if (this.status !== 200) {
+        alert('Error!  The file was NOT saved.\n\n' + this.status + ": " + (this.responseText + '').replace(/^<pre>/, ""));
+        return;
+    }
+
+    var doc1 = this.txt.replace(/\r\n/g, "\n");
+    var doc2 = this.responseText.replace(/\r\n/g, "\n");
+    if (doc1 != doc2) {
+        alert(
+            'Error! The document on the server does not appear to have saved correctly (your editor contents and the server copy is not identical). Place the document on your clipboard for now and check the server logs for hints\n\n' +
+            'Length: yours=' + doc1.length + ', server=' + doc2.length
+        );
+        alert('yours, ' + doc1.length + ' byte:\n[' + doc1 + ']');
+        alert('server, ' + doc2.length + ' byte:\n[' + doc2 + ']');
+        return;
+    }
+
+    last_modified = this.lastmod;
+    md_changed(this.mde, true);
+
+    var ok = document.createElement('div');
+    ok.setAttribute('style', 'font-size:6em;font-family:serif;font-weight:bold;color:#cf6;background:#444;border-radius:.3em;padding:.6em 0;position:fixed;top:30%;left:calc(50% - 2em);width:4em;text-align:center;z-index:9001;transition:opacity 0.2s ease-in-out;opacity:1');
+    ok.innerHTML = 'OK✔️';
+    var parent = ebi('m');
+    document.documentElement.appendChild(ok);
+    setTimeout(function () {
+        ok.style.opacity = 0;
+    }, 500);
+    setTimeout(function () {
+        ok.parentNode.removeChild(ok);
+    }, 750);
+}
