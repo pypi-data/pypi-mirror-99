c769ecb6393ea304672f5297c13e00c2193cc9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:38 +0000

    Add package bmcs-cross-section/0.0.32a0/bmcs_cross_section-0.0.32a0.tar.gz

---
+++ packages/bmcs-cross-section/bmcs_cross_section-0.0.32a0.tar.gz/bmcs_cross_section-0.0.32a0/bmcs_cross_section/cs_design/cs_shape.py
@@ -0,0 +1,212 @@
+import traits.api as tr
+from bmcs_utils.api import InteractiveModel
+import numpy as np
+import sympy as sp
+from bmcs_utils.api import View, Item, Float
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Polygon
+from matplotlib.patches import Circle as MPL_Circle
+
+
+class ICrossSectionShape(tr.Interface):
+    """This interface lists the functions need to be implemented by cross section classes."""
+
+    def get_cs_area(self):
+        """Returns the area of the cross section."""
+
+    def get_cs_i(self):
+        """Returns the moment of inertia of the cross section
+         with respect to an axis passing through its centroid ."""
+
+    def get_b(self, z_positions_array):
+        """Returns b values that correspond to the positions z_positions_array."""
+
+
+class CrossSectionShapeBase(InteractiveModel):
+    """"This class describes the geometry of the cross section."""
+    name = 'Cross Section Shape'
+
+    H = Float(400, GEO=True)
+
+    ipw_view = View(
+        Item('H', minmax=(10, 3000), latex='H [mm]')
+    )
+
+
+@tr.provides(ICrossSectionShape)
+class Rectangle(CrossSectionShapeBase):
+
+    B = Float(200, GEO=True)
+
+    ipw_view = View(
+        *CrossSectionShapeBase.ipw_view.content,        # this will add View Items of the base class CrossSectionShapeBase
+        Item('B', minmax=(10, 500), latex='B \mathrm{[mm]}')
+    )
+
+    def get_cs_area(self):
+        return self.B * self.H
+
+    def get_cs_i(self):
+        return self.B * self.H ** 3 / 12
+
+    def get_b(self, z_positions_array):
+        return np.full_like(z_positions_array, self.B)
+
+    def update_plot(self, ax):
+
+        ax.fill([-self.B/2, self.B/2, self.B/2, -self.B/2, -self.B/2], [0, 0, self.H, self.H, 0], color='gray', alpha=0.2 )
+        ax.plot([-self.B/2, self.B/2, self.B/2, -self.B/2, -self.B/2], [0, 0, self.H, self.H, 0], color='black')
+        ax.autoscale_view()
+        ax.set_aspect('equal')
+
+        ax.annotate('Area = {} $mm^2$'.format(int(Rectangle.get_cs_area(self)), 0),
+                    xy=(-self.H/2 * 0.8, self.H/2), color='blue')
+        ax.annotate('I = {} $mm^4$'.format(int(Rectangle.get_cs_i(self)), 0),
+                    xy=(-self.H/2 * 0.8, self.H/2 * 0.8), color='blue')
+
+
+@tr.provides(ICrossSectionShape)
+class Circle(CrossSectionShapeBase):
+
+    # TODO->Rostia: provide input field instead minmax range
+    # H from the base class is used as the D, for the diameter of the circular section
+
+    H = Float(250, GEO=True)
+
+    ipw_view = View(
+        Item('H', minmax=(10, 3000), latex='D [mm]'),
+    )
+
+    def get_cs_area(self):
+        return np.pi * self.H * self.H
+
+    def get_cs_i(self):
+        return np.pi * self.H ** 4 / 4
+
+    def get_b(self, z_positions_array):
+        return np.full_like(z_positions_array, self.H)
+
+        # TODO->Saeed: complete this
+
+
+    def update_plot(self, ax):
+        # TODO->Saeed: fix this
+
+        circle = MPL_Circle((0, self.H/2), self.H/2, facecolor=(.5,.5,.5,0.2), edgecolor=(0,0,0,1))
+
+        ax.add_patch(circle)
+        ax.autoscale_view()
+        ax.set_aspect('equal')
+        ax.annotate('Area = {} $mm^2$'.format(int(self.get_cs_area()), 0),
+                    xy=(-self.H/2 * 0.8, self.H/2), color='blue')
+        ax.annotate('I = {} $mm^4$'.format(int(self.get_cs_i()), 0),
+                    xy=(-self.H/2 * 0.8, self.H/2 * 0.8), color='blue')
+
+
+@tr.provides(ICrossSectionShape)
+class TShape(CrossSectionShapeBase):
+
+    B_f = Float(200, GEO=True)
+    B_w = Float(50, GEO=True)
+    H_w = Float(150, GEO=True)
+
+    ipw_view = View(
+        *CrossSectionShapeBase.ipw_view.content,
+        Item('B_f', minmax=(10, 3000), latex=r'B_f \mathrm{[mm]}'),
+        Item('B_w', minmax=(10, 3000), latex=r'B_w \mathrm{[mm]}'),
+        Item('H_w', minmax=(10, 3000), latex=r'H_w \mathrm{[mm]}'),
+    )
+
+    def get_cs_area(self):
+        return self.B_w * self.H_w + self.B_f * (self.H - self.H_w)
+
+    def get_cs_i(self):
+        A_f = self.B_f * (self.H - self.H_w)
+        Y_f = (self.H - self.H_w) / 2 + self.H_w
+        I_f = self.B_f * (self.H - self.H_w) ** 3 / 12
+        A_w = self. B_w * self.H_w
+        Y_w = self.H_w / 2
+        I_w = self.B_w * self.H_w ** 3 /12
+        Y_c = (Y_f * A_f + Y_w * A_w) / (A_f + A_w)
+        I_c = I_f + A_f * (Y_c - Y_f)**2 + I_w + A_w * (Y_c - Y_w)**2
+        return Y_c, I_c
+
+
+    get_b = tr.Property(tr.Callable, depends_on='+input')
+
+    @tr.cached_property
+    def _get_get_b(self):
+        z_ = sp.Symbol('z')
+        b_p = sp.Piecewise(
+            (self.B_w, z_ < self.H_w),
+            (self.B_f, True)
+        )
+        return sp.lambdify(z_, b_p, 'numpy')
+
+    def update_plot(self, ax):
+        # TODO [Saeed]: fix this
+        # Start drawing from bottom center of the cross section
+        cs_points = np.array([  [self.B_w/2, 0],
+                                [self.B_w/2, self.H_w],
+                                [self.B_f/2, self.H_w],
+                                [self.B_f/2, self.H],
+                                [-self.B_f/2, self.H],
+                                [-self.B_f/2, self.H_w],
+                                [-self.B_w/2, self.H_w],
+                                [-self.B_w/2, 0]])
+
+        cs = Polygon(cs_points)
+
+        patch_collection = PatchCollection([cs], facecolor=(.5,.5,.5,0.2), edgecolors=(0,0,0,1))
+
+        ax.add_collection(patch_collection)
+        ax.scatter(0, TShape.get_cs_i(self)[0], color ='white', s = self.B_w, marker ="+")
+
+        ax.autoscale_view()
+        ax.set_aspect('equal')
+
+        ax.annotate('Area = {} $mm^2$'.format(int(TShape.get_cs_area(self)), 0),
+                    xy=(-self.H/2 * 0.8, (self.H / 2 + self.H_w / 2)), color='blue')
+        ax.annotate('I = {} $mm^4$'.format(int(TShape.get_cs_i(self)[1]), 0),
+                    xy=(-self.H/2 * 0.8, (self.H / 2 + self.H_w / 2) * 0.9), color='blue')
+        ax.annotate('$Y_c$',
+                    xy=(0, TShape.get_cs_i(self)[0] * 0.85), color='purple')
+
+        # ax.annotate('$B_w$', xy=(-self.B_w / 2 , -self.B_w * 0.1), xytext=(self.B_w / 2 , -self.B_w * 0.1),
+        #             arrowprops={'arrowstyle': '<->'}, va='center', ha ='center')
+        # ax.annotate('$B_f$', xy=(-self.B_f / 2 , self.H * 1.1), xytext=(self.B_f / 2 , self.H * 1.1),
+        #             arrowprops={'arrowstyle': '<->'}, va='center', ha ='center')
+        # ax.annotate('$H$', xy=(-self.B_f / 2 , self.H * 1.1), xytext=(-self.B_f / 2 , self.H * 1.1 ),
+        #             arrowprops={'arrowstyle': '<->'}, va='center', ha ='center')
+        # ax.annotate('$H_w$', xy=(-self.B_f / 2 * 1.1, 0), xytext=(-self.B_f / 2 * 1.1 , self.H_w),
+        #             arrowprops={'arrowstyle': '<->'}, va='center', ha ='center')
+
+
+# TODO->Saeed: maybe complete this
+@tr.provides(ICrossSectionShape)
+class CustomShape(CrossSectionShapeBase):
+
+    def get_cs_area(self):
+        pass
+
+    def get_cs_i(self):
+        pass
+
+    def get_b(self, z_positions_array):
+        pass
+
+    def update_plot(self, ax):
+        # TODO->Saeed: fix this to use it for the CustomShape
+        # self.update_plot(ax)
+        z = np.linspace(0, self.H, 100)
+        b = self.get_b(z)
+        ax.axis([0, np.max(b), 0, self.H])
+        ax.axis('equal')
+        ax.fill(b, z, color='gray')
+        ax.plot(b, z, color='black')
+        ax.autoscale_view()
+        ax.set_aspect('equal')
+        ax.annotate('Area = {} $mm^2$'.format(int(CustomShape.get_cs_area(self)), 0),
+                    xy=(-self.H/2 * 0.8, self.H/2), color='blue')
+        ax.annotate('I = {} $mm^4$'.format(int(CustomShape.get_cs_i(self)), 0),
+                    xy=(-self.H/2 * 0.8, self.H/2 * 0.8), color='blue')
---
+++ packages/bmcs-cross-section/bmcs_cross_section-0.0.32a0.tar.gz/bmcs_cross_section-0.0.32a0/bmcs_cross_section/mkappa/mkappa.py
@@ -0,0 +1,426 @@
+import numpy as np
+import sympy as sp
+import traits.api as tr
+from bmcs_cross_section.cs_design import CrossSectionDesign
+from scipy.optimize import root
+from bmcs_utils.api import \
+    InteractiveModel, Instance, Item, View, mpl_align_xaxis, ParametricStudy, \
+    SymbExpr, InjectSymbExpr, Float, Int, FloatRangeEditor, FloatEditor, HistoryEditor
+
+class SolutionNotFoundError(ValueError):
+    pass
+
+
+class MKappaSymbolic(SymbExpr):
+    """This class handles all the symbolic calculations
+    so that the class MomentCurvature doesn't use sympy ever
+    """
+    # -------------------------------------------------------------------------
+    # Symbolic derivation of expressions
+    # -------------------------------------------------------------------------
+    kappa = sp.Symbol('kappa', real=True)
+    eps_top = sp.symbols('varepsilon_top', real=True)
+    eps_bot = sp.symbols('varepsilon_bot', real=True)
+    b, h, z = sp.symbols('b, h, z', real=True, nonnegative=True)
+    eps = sp.Symbol('varepsilon', real=True)
+
+    # -------------------------------------------------------------------------
+    # Symbolic derivation of expressions
+    # -------------------------------------------------------------------------
+    # Linear profile of strain over the cross section height
+    eps_z_ = eps_bot + z * (eps_top - eps_bot) / h
+    eps_top_solved = {eps_top: sp.solve(kappa + eps_z_.diff(z), eps_top)[0]}
+    eps_z = eps_z_.subs(eps_top_solved)
+
+    steel_material_factor = 1. / 1.15
+    carbon_material_factor = 1. / 1.5       # 1.3
+    concrete_material_factor = 0.85 / 1.5
+
+    # ----------------------------------------------------------------
+    # SymbExpr protocol: Parameter names to be fetched from the model
+    # ----------------------------------------------------------------
+    symb_model_params = ()
+
+    symb_expressions = [
+        ('eps_z', ('kappa', 'eps_bot', 'z')),
+    ]
+
+
+class MKappa(InteractiveModel, InjectSymbExpr):
+    """Class returning the moment curvature relationship."""
+    name = 'Moment-Curvature'
+
+    symb_class = MKappaSymbolic
+    cs_design = Instance(CrossSectionDesign, ())
+
+    tree = ['cs_design']
+    # Use PrototypedFrom only when the prototyped object is a class
+    # (The prototyped attribute behaves similarly
+    # to a delegated attribute, until it is explicitly
+    # changed; from that point forward, the prototyped attribute
+    # changes independently from its prototype.)
+    # (it's kind of like tr.DelegatesTo('cs_design.cross_section_shape'))
+    cross_section_shape = tr.DelegatesTo('cs_design')
+    cross_section_shape_ = tr.DelegatesTo('cs_design')
+    cross_section_layout = tr.DelegatesTo('cs_design')
+    matrix_ = tr.DelegatesTo('cs_design')
+
+    # Geometry
+    H = tr.DelegatesTo('cross_section_shape_')
+
+    DEPSTR = 'state_changed'
+
+    n_m = Int(100, DSC=True, desc='Number of discretization points along the height of the cross-section')
+
+    # @todo: fix the dependency - `H` should be replaced by _GEO
+    z_m = tr.Property(depends_on=DEPSTR)
+
+    @tr.cached_property
+    def _get_z_m(self):
+        return np.linspace(0, self.H, self.n_m)
+
+    low_kappa = Float(0.0, BC=True, GEO=True)
+    high_kappa = Float(0.00002, BC=True, GEO=True)
+    n_kappa = Int(100, BC=True)
+    step_kappa = tr.Property(Float, depends_on='low_kappa, high_kappa')
+    @tr.cached_property
+    def _get_step_kappa(self):
+        return float((self.high_kappa-self.low_kappa)/self.n_kappa)
+
+    kappa_slider = Float(0.0000001)
+
+    ipw_view = View(
+        Item('low_kappa', latex=r'\text{Low}~\kappa', editor=FloatEditor(step=0.00001)),
+        Item('high_kappa', latex=r'\text{High}~\kappa', editor=FloatEditor(step=0.00001)),
+        Item('n_kappa', latex='n_{\kappa}'),
+        Item('n_m', latex='n_m'),
+        Item('kappa_slider', latex='\kappa', readonly=True),
+             # editor=FloatRangeEditor(low_name='low_kappa',
+             #                         high_name='high_kappa',
+             #                         n_steps_name='n_kappa')
+             # ),
+        time_editor=HistoryEditor(var='kappa_slider',
+                                  min_var='low_kappa',
+                                  max_var='high_kappa',
+                                  ),
+    )
+
+
+    idx = tr.Property(depends_on='kappa_slider')
+
+    apply_material_safety_factors = tr.Bool(False)
+
+    @tr.cached_property
+    def _get_idx(self):
+        ks = self.kappa_slider
+        idx = np.argmax(ks <= self.kappa_t)
+        return idx
+
+    kappa_t = tr.Property(tr.Array(np.float_), depends_on=DEPSTR)
+    '''Curvature values for which the bending moment must be found
+    '''
+
+    @tr.cached_property
+    def _get_kappa_t(self):
+        return np.linspace(self.low_kappa, self.high_kappa, self.n_kappa)
+
+    z_j = tr.Property
+    def _get_z_j(self):
+        return self.cross_section_layout.z_j
+
+    A_j = tr.Property
+    def _get_A_j(self):
+        return self.cross_section_layout.A_j
+
+    # Normal force in steel (tension and compression)
+    def get_N_s_tj(self, kappa_t, eps_bot_t):
+        # get the strain at the height of the reinforcement
+        eps_z_tj = self.symb.get_eps_z(
+            kappa_t[:, np.newaxis], eps_bot_t[:, np.newaxis],
+            self.z_j[np.newaxis, :]
+        )
+        # Get the crack bridging force in each reinforcement layer
+        # given the corresponding crack-bridge law.
+        N_s_tj = self.cross_section_layout.get_N_tj(eps_z_tj)
+        return N_s_tj
+
+    # TODO - [RC] avoid repeated evaluations of stress profile in
+    #            N and M calculations for the same inputs as it
+    #            is the case now.
+    def get_sig_c_z(self, kappa_t, eps_bot_t, z_tm):
+        """Get the stress profile over the height"""
+        eps_z = self.symb.get_eps_z(kappa_t[:, np.newaxis], eps_bot_t[:, np.newaxis], z_tm)
+        sig_c_z = self.matrix_.get_sig(eps_z)
+        return sig_c_z
+
+    # Normal force in concrete (tension and compression)
+    def get_N_c_t(self, kappa_t, eps_bot_t):
+        z_tm = self.z_m[np.newaxis, :]
+        b_z_m = self.cross_section_shape_.get_b(z_tm)
+        N_z_tm2 = b_z_m * self.get_sig_c_z(kappa_t, eps_bot_t, z_tm)
+        return np.trapz(N_z_tm2, x=z_tm, axis=-1)
+
+    def get_N_t(self, kappa_t, eps_bot_t):
+        N_s_t = np.sum(self.get_N_s_tj(kappa_t, eps_bot_t), axis=-1)
+        N_c_t = self.get_N_c_t(kappa_t, eps_bot_t)
+        return N_c_t + N_s_t
+
+    # SOLVER: Get eps_bot to render zero force
+
+    # num_of_trials = tr.Int(30)
+
+    eps_bot_t = tr.Property(depends_on=DEPSTR)
+    r'''Resolve the tensile strain to get zero normal force for the prescribed curvature'''
+
+    # @tr.cached_property
+    # def _get_eps_bot_t(self):
+    #     initial_step = (self.high_kappa - self.low_kappa) / self.num_of_trials
+    #     for i in range(self.num_of_trials):
+    #         print('Solution started...')
+    #         res = root(lambda eps_bot_t: self.get_N_t(self.kappa_t, eps_bot_t),
+    #                    0.0000001 + np.zeros_like(self.kappa_t), tol=1e-6)
+    #         if res.success:
+    #             print('success high_kappa: ', self.high_kappa)
+    #             if i == 0:
+    #                 print('Note: high_kappa success from 1st try! selecting a higher value for high_kappa may produce '
+    #                       'a more reliable result!')
+    #             return res.x
+    #         else:
+    #             print('failed high_kappa: ', self.high_kappa)
+    #             self.high_kappa -= initial_step
+    #             self.kappa_t = np.linspace(self.low_kappa, self.high_kappa, self.n_kappa)
+    #
+    #     print('No solution', res.message)
+    #     return res.x
+
+    # @tr.cached_property
+    # def _get_eps_bot_t(self):
+    #     res = root(lambda eps_bot_t: self.get_N_t(self.kappa_t, eps_bot_t),
+    #                0.0000001 + np.zeros_like(self.kappa_t), tol=1e-6)
+    #     if not res.success:
+    #         raise SolutionNotFoundError('No solution', res.message)
+    #     return res.x
+
+
+    @tr.cached_property
+    def _get_eps_bot_t(self):
+        res = root(lambda eps_bot_t: self.get_N_t(self.kappa_t, eps_bot_t),
+                   0.0000001 + np.zeros_like(self.kappa_t), tol=1e-6)
+        if not res.success:
+            print('No solution', res.message)
+        return res.x
+
+    # POSTPROCESSING
+
+    kappa_cr = tr.Property(depends_on=DEPSTR)
+    '''Curvature at which a critical strain is attained at the eps_bot'''
+
+    @tr.cached_property
+    def _get_kappa_cr(self):
+        res = root(lambda kappa: self.get_N_t(kappa, self.eps_cr),
+                   0.0000001 + np.zeros_like(self.eps_cr), tol=1e-10)
+        if not res.success:
+            print('No kappa_cr solution (for plot_norm() function)', res.message)
+        return res.x
+
+    M_s_t = tr.Property(depends_on=DEPSTR)
+    '''Bending moment (steel)
+    '''
+
+    @tr.cached_property
+    def _get_M_s_t(self):
+        eps_z_tj = self.symb.get_eps_z(
+            self.kappa_t[:, np.newaxis], self.eps_bot_t[:, np.newaxis],
+            self.z_j[np.newaxis, :]
+        )
+
+        # Get the crack bridging force in each reinforcement layer
+        # given the corresponding crack-bridge law.
+        N_tj = self.cross_section_layout.get_N_tj(eps_z_tj)
+        return -np.einsum('tj,j->t', N_tj, self.z_j)
+
+    M_c_t = tr.Property(depends_on=DEPSTR)
+    '''Bending moment (concrete)
+    '''
+
+    @tr.cached_property
+    def _get_M_c_t(self):
+        z_tm = self.z_m[np.newaxis, :]
+        b_z_m = self.cross_section_shape_.get_b(z_tm)
+        N_z_tm2 = b_z_m * self.get_sig_c_z(self.kappa_t, self.eps_bot_t, z_tm)
+        return -np.trapz(N_z_tm2 * z_tm, x=z_tm, axis=-1)
+
+    M_t = tr.Property(depends_on=DEPSTR)
+    '''Bending moment
+    '''
+    @tr.cached_property
+    def _get_M_t(self):
+        print('M - k recalculated')
+        eta_factor = 1.
+        return eta_factor * (self.M_c_t + self.M_s_t)
+
+    # @tr.cached_property
+    # def _get_M_t(self):
+    #     initial_step = (self.high_kappa - self.low_kappa) / self.num_of_trials
+    #     for i in range(self.num_of_trials):
+    #         try:
+    #             M_t = self.M_c_t + self.M_s_t
+    #         except SolutionNotFoundError:
+    #             print('failed high_kappa: ', self.high_kappa)
+    #             self.high_kappa -= initial_step
+    #         else:
+    #             # This will run when no exception has been received
+    #             print('success high_kappa: ', self.high_kappa)
+    #             if i == 0:
+    #                 print('Note: high_kappa success from 1st try! selecting a higher value for high_kappa may produce '
+    #                       'a more reliable result!')
+    #             return M_t
+    #     print('No solution has been found!')
+    #     return M_t
+
+    N_s_tj = tr.Property(depends_on=DEPSTR)
+    '''Normal forces (steel)
+    '''
+
+    @tr.cached_property
+    def _get_N_s_tj(self):
+        return self.get_N_s_tj(self.kappa_t, self.eps_bot_t)
+
+    eps_tm = tr.Property(depends_on=DEPSTR)
+    '''strain profiles
+    '''
+
+    @tr.cached_property
+    def _get_eps_tm(self):
+        return self.get_eps_z(self.kappa_t[:, np.newaxis],
+                              self.eps_bot_t[:, np.newaxis], self.z_m[np.newaxis, :])
+
+    sig_tm = tr.Property(depends_on=DEPSTR)
+    '''strain profiles
+    '''
+
+    @tr.cached_property
+    def _get_sig_tm(self):
+        return self.get_sig_c_z(
+            self.kappa_t, self.eps_bot_t, self.z_m[np.newaxis, :]
+        )
+
+    M_norm = tr.Property(depends_on=DEPSTR)
+    '''
+    '''
+
+    @tr.cached_property
+    def _get_M_norm(self):
+        # Section modulus @TODO optimize W for var b
+        W = (self.b * self.H ** 2) / 6
+        sig_cr = self.E_ct * self.eps_cr
+        return W * sig_cr
+
+    kappa_norm = tr.Property()
+
+    def _get_kappa_norm(self):
+        return self.kappa_cr
+
+    inv_M_kappa = tr.Property(depends_on=DEPSTR)
+    '''Return the inverted data points
+    '''
+    @tr.cached_property
+    def _get_inv_M_kappa(self):
+        """cut off the descending tails"""
+        M_t = self.M_t
+        I_max = np.argmax(M_t)
+        I_min = np.argmin(M_t)
+        M_I = self.M_t[I_min:I_max + 1]
+        kappa_I = self.kappa_t[I_min:I_max + 1]
+        # find the index corresponding to zero kappa
+        idx = np.argmax(0 <= self.kappa_t)
+        # and modify the values such that the
+        # Values of moment are non-descending
+        M_plus = M_I[idx:]
+        M_diff = M_plus[:, np.newaxis] - M_plus[np.newaxis, :]
+        n_ij = len(M_plus)
+        ij = np.mgrid[0:n_ij:1, 0:n_ij:1]
+        M_diff[np.where(ij[1] >= ij[0])] = 0
+        i_x = np.argmin(M_diff, axis=1)
+        M_I[idx:] = M_plus[i_x]
+        return M_I, kappa_I
+
+    def get_kappa_M(self, M):
+        try:
+            M_I, kappa_I = self.inv_M_kappa
+            return np.interp(M, M_I, kappa_I)
+        except ValueError:
+            print('M inverse has not succeeded, the M-Kappa solution may have failed due to a wrong kappa range!')
+
+    def plot_norm(self, ax1, ax2):
+        idx = self.idx
+        ax1.plot(self.kappa_t / self.kappa_norm, self.M_t / self.M_norm)
+        ax1.plot(self.kappa_t[idx] / self.kappa_norm, self.M_t[idx] / self.M_norm, marker='o')
+        ax2.barh(self.z_j, self.N_s_tj[idx, :], height=2, color='red', align='center')
+        # ax2.fill_between(eps_z_arr[idx,:], z_arr, 0, alpha=0.1);
+        ax3 = ax2.twiny()
+        #  ax3.plot(self.eps_tm[idx, :], self.z_m, color='k', linewidth=0.8)
+        ax3.plot(self.sig_tm[idx, :], self.z_m)
+        ax3.axvline(0, linewidth=0.8, color='k')
+        ax3.fill_betweenx(self.z_m, self.sig_tm[idx, :], 0, alpha=0.1)
+        mpl_align_xaxis(ax2, ax3)
+
+    M_scale = Float(1e+6)
+
+    def plot(self, ax1, ax2, ax3):
+        self.plot_mk_and_stress_profile(ax1, ax2)
+        try:
+            M, kappa = self.inv_M_kappa
+            ax3.plot(M / self.M_scale, kappa)
+        except ValueError:
+            print('M inverse has not succeeded, the M-Kappa solution may have failed due to a wrong kappa range!')
+
+        ax3.set_xlabel('Moment [kNm]')
+        ax3.set_ylabel('Curvature[mm$^{-1}$]')
+
+    @staticmethod
+    def subplots(fig):
+        ax1, ax2, ax3 = fig.subplots(1, 3)
+        return ax1, ax2, ax3
+
+    def update_plot(self, axes):
+        self.plot(*axes)
+
+    def plot_mk_and_stress_profile(self, ax1, ax2):
+        self.plot_mk(ax1)
+        idx = self.idx
+        ax1.plot(self.kappa_t[idx], self.M_t[idx] / self.M_scale, color='orange', marker='o')
+
+        ax2.barh(self.z_j, self.N_s_tj[idx, :]/self.A_j, height=4, color='red', align='center')
+        ax2.set_ylabel('z [mm]')
+        ax2.set_xlabel('$\sigma_r$ [MPa]')
+
+        ax22 = ax2.twiny()
+        ax22.set_xlabel('$\sigma_c$ [MPa]')
+        ax22.plot(self.sig_tm[idx, :], self.z_m)
+        ax22.axvline(0, linewidth=0.8, color='k')
+        ax22.fill_betweenx(self.z_m, self.sig_tm[idx, :], 0, alpha=0.1)
+        mpl_align_xaxis(ax2, ax22)
+
+    def plot_mk(self, ax1):
+        ax1.plot(self.kappa_t, self.M_t / self.M_scale, label='bmcs_cs_mkappa')
+        ax1.set_ylabel('Moment [kNm]')
+        ax1.set_xlabel('Curvature [mm$^{-1}$]')
+        ax1.legend()
+
+
+    def get_mk(self):
+        return self.M_t / self.M_scale, self.kappa_t
+
+
+class MKappaParamsStudy(ParametricStudy):
+    """TODO - put into a separate python module"""
+    def __init__(self, mc):
+        self.mc = mc
+
+    def plot(self, ax, param_name, value):
+        ax.plot(self.mc.kappa_t, self.mc.M_t / self.mc.M_scale, label=param_name + '=' + str(value), lw=2)
+        ax.set_ylabel('Moment [kNm]')
+        ax.set_xlabel('Curvature [mm$^{-1}$]')
+        ax.set_title(param_name)
+        ax.legend()
