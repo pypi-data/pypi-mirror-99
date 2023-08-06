9f4b2d909bb6c3190da09fd07834c2bd76917497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:11 +0000

    Add package synonym-finder/0.0.4/synonym_finder-0.0.4.tar.gz

---
+++ packages/synonym-finder/synonym_finder-0.0.4.tar.gz/synonym_finder-0.0.4/synonym_finder/_version.py
@@ -0,0 +1,15 @@
+"""
+Provides synonym_finder version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update synonym_finder` to change this file.
+
+from incremental import Version
+
+__version__ = Version('synonym_finder', 21, 3, 0)
+__all__ = ["__version__"]
+
+use_incremental=True,
+setup_requires=['incremental'],
+install_requires=['incremental']
