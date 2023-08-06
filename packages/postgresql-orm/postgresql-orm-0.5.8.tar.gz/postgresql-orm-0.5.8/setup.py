42b2312c63f24c451fdd734865e1ef02416c560f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:04 +0000

    Add package postgresql-orm/0.5.8/postgresql-orm-0.5.8.tar.gz

---
+++ packages/postgresql-orm/postgresql-orm-0.5.8.tar.gz/postgresql-orm-0.5.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='postgresql-orm',
+      description='Easyest PostgreSQL ORM',
+      long_description=open("README.md").read(),
+      long_description_content_type="text/markdown",
+      author='Vakhtang Zardiashvili',
+      author_email='hazardland@gmail.com',
+      license='MIT',
+      version='0.5.8',
+      keywords='orm, pgsql, postgresql, model',
+      packages=["sql"],
+      url='https://github.com/hazardland/sql.py',
+      python_requires='>=3.6',
+      install_requires=['python_dateutil']
+     )
