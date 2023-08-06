872eb678876079047cbc72d302ce1520b1476041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:07 +0000

    Add package pimpmydb/0.0.1.dev1/pimpmydb-0.0.1.dev1.tar.gz

---
+++ packages/pimpmydb/pimpmydb-0.0.1.dev1.tar.gz/pimpmydb-0.0.1.dev1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pimpmydb",
+    version='0.0.1.dev1',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'mysql-connector-python',
+        'python-dotenv'
+    ]
+)
