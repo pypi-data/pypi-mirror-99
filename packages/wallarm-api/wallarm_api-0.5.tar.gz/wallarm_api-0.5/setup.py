015c4567f41cae5f8cc245a27d348f068285fe69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:18 +0000

    Add package wallarm-api/0.5/wallarm_api-0.5.tar.gz

---
+++ packages/wallarm-api/wallarm_api-0.5.tar.gz/wallarm_api-0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='wallarm_api',
+    packages=['wallarm_api'],
+    version='0.5',
+    license='Apache license 2.0',
+    description='Wallarm API client',
+    author='Vladimir Salykin',
+    author_email='neraverin@gmail.com',
+    url='https://github.com/Neraverin/wallarm-api-python',
+    download_url='https://github.com/Neraverin/wallarm-api-python/archive/v0.5.tar.gz',
+    keywords=['Wallarm', 'API client'],
+    install_requires=[
+        'requests',
+    ],
+    setup_requires=['wheel'],
+    python_requires='>=3.6, <4',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
