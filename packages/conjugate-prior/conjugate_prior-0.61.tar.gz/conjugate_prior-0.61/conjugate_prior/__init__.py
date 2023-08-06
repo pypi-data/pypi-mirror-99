c364f24eb6517ae6992b846025df85669959dc65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:58 +0000

    Add package conjugate-prior/0.61/conjugate_prior-0.61.tar.gz

---
+++ packages/conjugate-prior/conjugate_prior-0.61.tar.gz/conjugate_prior-0.61/conjugate_prior/__init__.py
@@ -0,0 +1,6 @@
+from .dirichlet import *
+from .gamma import *
+from .beta import *
+from .normal import *
+from .invgamma import *
+__version__ = '0.6'
---
+++ packages/conjugate-prior/conjugate_prior-0.61.tar.gz/conjugate_prior-0.61/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name="conjugate_prior",
+    packages=["conjugate_prior"],
+    install_requires=[
+        'setuptools',
+        'scipy',
+        'numpy',
+        'matplotlib',
+    ],
+    long_description="https://github.com/urigoren/conjugate_prior/blob/master/README.md",
+    long_description_content_type="text/markdown",
+    version="0.61",
+    description='Bayesian Statistics conjugate prior distributions',
+    author='Uri Goren',
+    author_email='uri@goren.ml',
+    url='https://github.com/urigoren/conjugate_prior',
+    keywords=['conjugate', 'bayesian', 'stats', 'statistics', 'bayes', 'distribution', 'probability', 'hypothesis',
+              'modelling', 'thompson sampling'],
+    classifiers=[],
+)
