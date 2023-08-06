2bd35220aa3ffc67b9591e984dfe38e30438558f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:04 +0000

    Add package odoo12-addon-shopinvader-partner-vat/12.0.1.0.1/odoo12_addon_shopinvader_partner_vat-12.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo12-addon-shopinvader-partner-vat/odoo12_addon_shopinvader_partner_vat-12.0.1.0.1-py3-none-any.whl/odoo/addons/shopinvader_partner_vat/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader VAT",
+    "summary": "Shopinvader Check VAT with invader environnement",
+    "version": "12.0.1.0.1",
+    "category": "e-commerce",
+    "development_status": "Stable/Production",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "installable": True,
+    "external_dependencies": {"python": ["stdnum"]},
+    "depends": ["shopinvader"],
+}
