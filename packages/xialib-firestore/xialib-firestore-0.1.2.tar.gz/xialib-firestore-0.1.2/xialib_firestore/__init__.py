5d7848853b493c9e71fdfb6b87ef1e4e01e2057a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:51 +0000

    Add package xialib-firestore/0.1.2/xialib-firestore-0.1.2.tar.gz

---
+++ packages/xialib-firestore/xialib-firestore-0.1.2.tar.gz/xialib-firestore-0.1.2/xialib_firestore/__init__.py
@@ -0,0 +1,7 @@
+from xialib_firestore.firestore_depositor import FirestoreDepositor
+from xialib_firestore.firestore_ager import FirestoreAger
+
+__all__ = ['FirestoreDepositor', 'FirestoreAger']
+
+
+__version__ = "0.1.2"
