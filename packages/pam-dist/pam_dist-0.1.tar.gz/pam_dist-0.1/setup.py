dc576d0d426a56a8eaa5d9ff5b4640a84b67f256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:39 +0000

    Add package pam-dist/0.1/pam_dist-0.1.tar.gz

---
+++ packages/pam-dist/pam_dist-0.1.tar.gz/pam_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pam_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pam_dist'],
+      author = 'Pablo Araya-Melo',
+      author_email = 'arayamelo.pablo@gmail.com', 
+      zip_safe=False)
