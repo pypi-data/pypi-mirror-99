03e3e1320df53c4dc594ef23f194905b2d249c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:26 +0000

    Add package medgeconv/1.0.1/medgeconv-1.0.1.tar.gz

---
+++ packages/medgeconv/medgeconv-1.0.1.tar.gz/medgeconv-1.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as fobj:
+    requirements = [l.strip() for l in fobj.readlines()]
+
+with open("README.rst") as fh:
+    long_description = fh.read()
+
+setup(
+    name='medgeconv',
+    author='Stefan Reck',
+    author_email='stefan.reck@fau.de',
+    long_description=long_description,
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
+    install_requires=requirements,
+    packages=find_packages(exclude=("tests", )),
+    include_package_data=True,
+    license='MIT',
+)
