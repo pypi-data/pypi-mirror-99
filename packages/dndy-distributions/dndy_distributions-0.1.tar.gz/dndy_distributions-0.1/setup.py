4ccd3bc5243cb1a2809cf56c3b07e23ef9fd9798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:36 +0000

    Add package dndy-distributions/0.1/dndy_distributions-0.1.tar.gz

---
+++ packages/dndy-distributions/dndy_distributions-0.1.tar.gz/dndy_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dndy_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dndy_distributions'],
+      author='Clera Gan',
+      zip_safe=False)
