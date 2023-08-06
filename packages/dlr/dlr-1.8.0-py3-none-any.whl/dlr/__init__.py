18d529a98f3556e52f4a377a40d44c402a270dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:49 +0000

    Add package dlr/1.8.0/dlr-1.8.0-py3-none-any.whl

---
+++ packages/dlr/dlr-1.8.0-py3-none-any.whl/dlr/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+"""
+DLR: Compact Runtime for Machine Learning Models
+"""
+
+from __future__ import absolute_import as _abs
+
+from .api import DLRModel
+from .metadata import NAME, VERSION
+
+__all__ = NAME
+
+__version__ = VERSION
