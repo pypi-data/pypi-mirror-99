edffb27378df854d9a62c225f4346ef504aa25b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:42 +0000

    Add package pyimporters-dummy/0.0.6/pyimporters_dummy-0.0.6-py3-none-any.whl

---
+++ packages/pyimporters-dummy/pyimporters_dummy-0.0.6-py3-none-any.whl/pyimporters_dummy/dummy.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import Type, Dict, Any
+from progress.bar import Bar
+from pydantic import Field, BaseModel
+from pyimporters_plugins.base import KnowledgeParserOptions, KnowledgeParserBase, Concept
+
+class DummyOptions(KnowledgeParserOptions):
+    """
+    Options for the Dummy knowledge import
+    """
+    foo : str = Field("bar", description="Dummy parameter")
+
+class DummyKnowledgeParser(KnowledgeParserBase):
+    def parse(self, source : Path, options : Dict[str,Any], bar : Bar):
+        options = DummyOptions(**options)
+        bar.max = 1
+        bar.next()
+        yield Concept(identifier=options.foo, prefLabel=options.foo)
+        bar.finish()
+
+    @classmethod
+    def get_schema(cls) -> Type[BaseModel]:
+        return DummyOptions
+
