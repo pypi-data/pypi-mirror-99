c17352ecadb06f20b2e3c8f0e7600ceb795ee701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:57 +0000

    Add package nxp-ls/1.1.2/nxp_ls-1.1.2.tar.gz

---
+++ packages/nxp-ls/nxp_ls-1.1.2.tar.gz/nxp_ls-1.1.2/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+from shutil import copy
+
+
+name = "nxp_ls"
+script = "lava_docker_slave"
+copy(script, name)
+
+setup(
+    name=name,
+    version="1.1.2",
+    author="Larry Shen",
+    author_email="larry.shen@nxp.com",
+    license="MIT",
+
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+    ],
+    entry_points={
+        'console_scripts': [
+            'lava_docker_slave = nxp_ls:main',
+        ]
+    },
+
+    package_data={'': [script]},
+)
+
+os.remove(os.path.join(name, script))
