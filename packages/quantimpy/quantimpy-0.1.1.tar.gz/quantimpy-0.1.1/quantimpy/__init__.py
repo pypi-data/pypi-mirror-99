f100ed3e001484e56684042862849a9050129da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:29 +0000

    Add package quantimpy/0.1.1/quantimpy-0.1.1.tar.gz

---
+++ packages/quantimpy/quantimpy-0.1.1.tar.gz/quantimpy-0.1.1/quantimpy/__init__.py
@@ -0,0 +1,19 @@
+r"""
+Welcome to QuantImPy, a Python Library for scientific image processing.
+
+This code performs morphological operations on Numpy arrays and can compute the
+Minkowski functionals and functions. 
+
+This code is inspired by and partly based on the `QuantIm library`_ C/C++ library
+for scientific image processing. The source code is available on `Github`_.
+
+This library can be installed using pip:
+
+.. code-block:: python
+
+    pip install quantimpy
+
+.. _QuantIm library: https://www.ufz.de/index.php?en=39198
+.. _Github: https://github.com/boeleman/quantimpy
+
+"""
