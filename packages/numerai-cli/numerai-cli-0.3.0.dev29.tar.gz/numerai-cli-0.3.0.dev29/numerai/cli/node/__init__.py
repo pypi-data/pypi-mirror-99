2618379c71bcb85fb4a0b8ff042619126fe62841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:14 +0000

    Add package numerai-cli/0.3.0.dev29/numerai-cli-0.3.0.dev29.tar.gz

---
+++ packages/numerai-cli/numerai-cli-0.3.0.dev29.tar.gz/numerai-cli-0.3.0.dev29/numerai/cli/node/__init__.py
@@ -0,0 +1,63 @@
+import json
+
+from numerapi import base_api
+
+from numerai.cli.constants import *
+from numerai.cli.node.create import create
+from numerai.cli.node.deploy import deploy
+from numerai.cli.node.destroy import destroy
+from numerai.cli.node.test import LOG_TYPES, test, status
+from numerai.cli.util.keys import get_numerai_keys
+
+
+@click.group()
+@click.option(
+    '--model-name', '-m', type=str, prompt=True,
+    help=f"The name of one of your models to configure the Prediction Node for."
+         f"It defaults to the first model returned from your account."
+)
+@click.option(
+    '--signals', '-s', is_flag=True,
+    help=f"Target a signals model with this name. Defaults to false."
+)
+@click.pass_context
+def node(ctx, model_name, signals):
+    """
+    Commands to create, deploy, test, and destroy Prediction Nodes.
+    """
+    if not os.path.exists(CONFIG_PATH):
+        click.secho('cannot find .numerai config directory, '
+                    'run "numerai setup"', fg='red')
+        exit(1)
+
+    if signals:
+        tournament = TOURNAMENT_SIGNALS
+        name_prefix = 'signals'
+    else:
+        tournament = TOURNAMENT_NUMERAI
+        name_prefix = 'numerai'
+    napi = base_api.Api(*get_numerai_keys())
+    models = napi.get_models(tournament)
+
+    try:
+        model_id = models[model_name]
+        ctx.ensure_object(dict)
+        ctx.obj['model'] = {
+            'id': model_id,
+            'name': f'{name_prefix}-{model_name}'
+        }
+
+    except IndexError:
+        click.secho(
+            f'No tournament {tournament} model with name {model_name} '
+            f'found in list of models:\n{json.dumps(models, indent=2)}',
+            fg='red'
+        )
+        return
+
+
+node.add_command(create)
+node.add_command(deploy)
+node.add_command(destroy)
+node.add_command(test)
+node.add_command(status)
