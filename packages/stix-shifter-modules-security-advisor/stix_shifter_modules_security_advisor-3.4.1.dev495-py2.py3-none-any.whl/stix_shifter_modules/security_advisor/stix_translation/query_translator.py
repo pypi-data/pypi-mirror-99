88bd12ebc37207f9dae1fb3ec7c66808dc230e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:03 +0000

    Add package stix-shifter-modules-security-advisor/3.4.1.dev495/stix_shifter_modules_security_advisor-3.4.1.dev495-py2.py3-none-any.whl

---
+++ packages/stix-shifter-modules-security-advisor/stix_shifter_modules_security_advisor-3.4.1.dev495-py2.py3-none-any.whl/stix_shifter_modules/security_advisor/stix_translation/query_translator.py
@@ -0,0 +1,16 @@
+from stix_shifter_utils.modules.base.stix_translation.empty_query_translator import EmptyQueryTranslator
+
+
+class QueryTranslator(EmptyQueryTranslator):
+
+    def transform_query(self, data):
+        """
+        Takes in passed in query string and returns it
+        :param data: query string that gets returned
+        :type data: str
+        :param mapping: This is unused
+        :type mapping: str
+        :return: the passed in data
+        :rtype: str
+        """
+        return {'queries': [data]}
