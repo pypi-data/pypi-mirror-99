b38178b66badfcc02ad2210ca49bfa86b3aa12ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:17 +0000

    Add package mitama/4.6.15/mitama-4.6.15-py3-none-any.whl

---
+++ packages/mitama/mitama-4.6.15-py3-none-any.whl/mitama/_extra.py
@@ -0,0 +1,34 @@
+import re
+
+class _Singleton:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance == None:
+            cls._instance = super(_Singleton, cls).__new__(cls)
+        return cls._instance
+
+
+class _classproperty:
+    def __init__(self, fget=None, doc=None):
+        self.fget = fget
+        self.__doc__ = self.fget.__doc__ if doc == None else doc
+
+    def __get__(self, obj, cls=None):
+        if cls is None:
+            cls = type(obj)
+        return self.fget(cls)
+
+
+def tosnake(s):
+    return re.sub(
+        "(.[A-Z])", lambda x: x.group(1)[0] + "_" + x.group(1)[1], s
+    ).lower()
+
+
+def deepupdate(base, other):
+    for k, v in other.items():
+        if isinstance(other[k], dict) and k in base:
+            deepupdate(base[k], v)
+        else:
+            base[k] = v
