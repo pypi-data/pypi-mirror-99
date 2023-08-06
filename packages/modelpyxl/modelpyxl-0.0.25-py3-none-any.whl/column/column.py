a13dbbc9d046d2fbaf1347ade6c4ad6801bed0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:47 +0000

    Add package modelpyxl/0.0.25/modelpyxl-0.0.25-py3-none-any.whl

---
+++ packages/modelpyxl/modelpyxl-0.0.25-py3-none-any.whl/column/column.py
@@ -0,0 +1,10 @@
+def test(my_test):
+    """
+    Print a line about converting a notebook.
+    Args:
+        my_test (str): any string
+    Returns:
+        None
+    """
+
+    print(f"This function in column print variable's value = {my_test}.")
