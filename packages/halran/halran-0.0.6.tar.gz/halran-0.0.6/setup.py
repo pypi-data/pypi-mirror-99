ae8dcdf66df862d65091b2d25076908d659d8833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:30 +0000

    Add package halran/0.0.6/halran-0.0.6.tar.gz

---
+++ packages/halran/halran-0.0.6.tar.gz/halran-0.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='halran',
+    version='0.0.6',
+    description='New random algorythm',
+    packages=['halran'],
+    author_email='halolxz@gmail.com',
+    zip_save=False
+)
