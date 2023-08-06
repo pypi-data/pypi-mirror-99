262a9794242d94aa0d9b3ea8293bfed1efe608e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:33 +0000

    Add package xgbse/0.2.1/xgbse-0.2.1-py3-none-any.whl

---
+++ packages/xgbse/xgbse-0.2.1-py3-none-any.whl/xgbse/__init__.py
@@ -0,0 +1,17 @@
+# Allow for easier imports
+
+from ._debiased_bce import XGBSEDebiasedBCE
+from ._kaplan_neighbors import XGBSEKaplanNeighbors, XGBSEKaplanTree
+from ._stacked_weibull import XGBSEStackedWeibull
+from ._meta import XGBSEBootstrapEstimator
+
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "XGBSEDebiasedBCE",
+    "XGBSEKaplanNeighbors",
+    "XGBSEKaplanTree",
+    "XGBSEStackedWeibull",
+    "XGBSEBootstrapEstimator",
+]
