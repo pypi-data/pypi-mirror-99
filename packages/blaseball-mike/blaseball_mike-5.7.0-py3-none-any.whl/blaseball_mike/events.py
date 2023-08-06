f076b1dcf63d0d8860adf4114ecb9da81523447c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:15 +0000

    Add package blaseball-mike/5.7.0/blaseball_mike-5.7.0-py3-none-any.whl

---
+++ packages/blaseball-mike/blaseball_mike-5.7.0-py3-none-any.whl/blaseball_mike/events.py
@@ -0,0 +1,36 @@
+"""
+Wrapper around the SSE events API.
+"""
+import asyncio
+from concurrent import futures
+import ujson
+
+from aiohttp_sse_client import client as sse_client
+from aiohttp.client_exceptions import ClientPayloadError, ClientConnectorError, ServerDisconnectedError
+
+
+async def stream_events(url='https://www.blaseball.com/events/streamData', retry_base=0.01, retry_max=300):
+    """
+    Async generator for the events API.
+    `retry_base` will be the minimum time to delay if there's a connection error
+    `retry_max` is the maximum time to delay if there's a connection error
+    """
+    retry_delay = retry_base
+    while True:
+        try:
+            async with sse_client.EventSource(url, read_bufsize=2 ** 18) as src:
+                async for event in src:
+                    retry_delay = retry_base  # reset backoff
+                    if not event.data:
+                        continue
+                    payload = ujson.loads(event.data)['value']
+                    yield payload
+        except (ConnectionError,
+                TimeoutError,
+                ClientPayloadError,
+                futures.TimeoutError,
+                asyncio.exceptions.TimeoutError,
+                ClientConnectorError,
+                ServerDisconnectedError):
+            await asyncio.sleep(retry_delay)
+            retry_delay = min(retry_delay * 2, retry_max)
---
+++ packages/blaseball-mike/blaseball_mike-5.7.0-py3-none-any.whl/blaseball_mike/session.py
@@ -0,0 +1,24 @@
+import requests_cache
+from json.decoder import JSONDecodeError
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+_SESSIONS_BY_EXPIRY = {}
+
+
+def session(expiry=0):
+    """Get a caching HTTP session"""
+    if expiry not in _SESSIONS_BY_EXPIRY:
+        _SESSIONS_BY_EXPIRY[expiry] = requests_cache.CachedSession(backend="memory", expire_after=expiry)
+    return _SESSIONS_BY_EXPIRY[expiry]
+
+
+def check_network_response(response):
+    """Verify that network response is correct and is valid JSON"""
+    response.raise_for_status()
+
+    try:
+        data = response.json()
+    except JSONDecodeError:
+        raise ValueError("Network response is not valid JSON")
+
+    return data
