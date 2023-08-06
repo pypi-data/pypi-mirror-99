4259dd34f7c9ce1a8db4883a9c43520d7ddb7736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package odoo13-addon-shopinvader-product-media/13.0.1.0.1/odoo13_addon_shopinvader_product_media-13.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-product-media/odoo13_addon_shopinvader_product_media-13.0.1.0.1-py3-none-any.whl/odoo/addons/shopinvader_product_media/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Product Media",
+    "summary": "Index storage media data into external search engine",
+    "version": "13.0.1.0.1",
+    "category": "e-commerce",
+    "website": "https://akretion.com",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": ["shopinvader", "storage_media_product"],
+    "data": ["data/ir_product_export.xml"],
+}
