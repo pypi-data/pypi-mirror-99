029ac80eeaca2801abe5b1a651f8d05500fafd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:20 +0000

    Add package pybaycor/0.2.0/pybaycor-0.2.0-py3-none-any.whl

---
+++ packages/pybaycor/pybaycor-0.2.0-py3-none-any.whl/pybaycor/__init__.py
@@ -0,0 +1,13 @@
+""" pybaycor: a package for estimating (robust) Bayesian correlation coefficients in python
+
+"""
+
+
+from . import pybaycor
+from .pybaycor import BayesianCorrelation
+from .pybaycor import RobustBayesianCorrelation
+from .pybaycor import HierarchicalBayesianCorrelation
+from .pybaycor import HierarchicalRobustBayesianCorrelation
+
+__version__ = "0.2.0"
+__all__=["pybaycor"]
