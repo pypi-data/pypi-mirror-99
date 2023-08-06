67d5772f24163e4bcf72f72561a7b46b9415d010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:49 +0000

    Add package nnabla/1.18.0/nnabla-1.18.0-cp38-cp38-win_amd64.whl

---
+++ packages/nnabla/nnabla-1.18.0-cp38-cp38-win_amd64.whl/nnabla/utils/cli/utility.py
@@ -0,0 +1,426 @@
+# Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import re
+import sys
+import numpy as np
+import timeit
+from contextlib import contextmanager
+
+import nnabla as nn
+import nnabla.utils.callback as callback
+from nnabla.utils.communicator_util import current_communicator
+from nnabla.utils import nnabla_pb2
+from nnabla.utils.get_file_handle import get_file_handle_save, get_decorated_file_loader, load_files
+from nnabla.logger import logger
+from nnabla.config import nnabla_config
+
+cpu_load_backend_ok = True
+try:
+    import psutil
+except Exception:
+    # measure cpu load only if psutil installed
+    cpu_load_backend_ok = False
+
+gpu_load_backend_ok = True
+try:
+    from nnabla.utils.nvml import pynvml
+    pynvml.nvmlInit()
+except Exception:
+    # measure gpu load only if nvml installed
+    gpu_load_backend_ok = False
+
+try:
+    _ANALYSE_GPU_STATUS_INTERVAL = int(
+        nnabla_config.get('MULTINODE', 'analyse_gpu_status_interval'))
+    _GPU_SLOWING_WARNING_THRESHOLD = float(
+        nnabla_config.get('MULTINODE', 'gpu_slowing_warning_threshold'))
+    _GPU_SLOWING_ERROR_THRESHOLD = float(
+        nnabla_config.get('MULTINODE', 'gpu_slowing_error_threshold'))
+except Exception:
+    _ANALYSE_GPU_STATUS_INTERVAL = 20
+    _GPU_SLOWING_WARNING_THRESHOLD = 1.4
+    _GPU_SLOWING_ERROR_THRESHOLD = 2
+
+# load variable
+# ============
+gpu_m_count = 0
+gpu_a_load = {}
+if cpu_load_backend_ok:
+    p_handler = psutil.Process()
+    p_handler_avg = psutil.Process()
+    p_handler_avg.cpu_percent()
+
+
+def is_float(x):
+    # x is string
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+
+
+def str_to_num(str):
+    # Convert str to num.
+    #   12345678
+    #   123e123  123 ^ 123
+    #   1234K    1234 * 1000
+    #   1234k    1234 * 1024
+    #   1234M    1234 * 1000^2
+    #   1234m    1234 * 1024^2
+    #   1234G    1234 * 1000^3
+    #   1234g    1234 * 1024^3
+    #   1234T    1234 * 1000^4
+    #   1234t    1234 * 1024^4
+    #   1234P    1234 * 1000^5
+    #   1234p    1234 * 1024^5
+    import re
+    m = re.match(
+        r'(^[1-9][0-9]*$|^([1-9][0-9]*(\.[0-9]+)?)((e)([0-9]+)|[KkMmGgTtPp]))$', str)
+    print(m)
+    if m:
+        if m.group(4):
+            if m.group(5):
+                return int(float(m.group(2)) ** int(m.group(6)))
+            else:
+                return int(float(m.group(2)) *
+                           {'K': 1000, 'k': 1024,
+                            'M': 1000 ** 2, 'm': 1024 ** 2,
+                            'G': 1000 ** 3, 'g': 1024 ** 3,
+                            'T': 1000 ** 4, 't': 1024 ** 4,
+                            'P': 1000 ** 5, 'p': 1024 ** 5}[m.group(4)])
+        else:
+            return int(m.group(1))
+    else:
+        return -1
+
+
+def compute_full_path(root_path, file_path):
+    full_path = os.path.join(root_path, file_path)
+    full_path = full_path.replace('\\', os.path.sep)
+    full_path = full_path.replace('/', os.path.sep)
+    full_path = full_path.replace(os.path.sep + '.' + os.path.sep, os.path.sep)
+    return full_path
+
+
+def let_data_to_variable(variable, data, ctx=None, data_name=None, variable_name=None):
+    try:
+        if data.dtype <= np.float64:
+            variable.data.cast(data.dtype)[...] = data
+        else:
+            variable.d = data
+    except:
+        if variable.shape != data.shape:
+            logger.critical('Shape does not match between data{} and variable{} ({} != {}).'.format(
+                ' "' + data_name + '"' if data_name else '',
+                ' "' + variable_name + '"' if variable_name else '',
+                data.shape, variable.shape))
+        raise
+    variable.need_grad = False
+
+    # Copy to device
+    if ctx:
+        try:
+            variable.data.cast(variable.data.dtype, ctx)
+        except:
+            if ctx.array_class != 'CpuArray':
+                # Fallback to cpu
+                ctx.array_class = 'CpuArray'
+                variable.data.cast(variable.data.dtype, ctx)
+            else:
+                raise
+
+
+def collect_and_shape_result(c_load, g_load):
+    # c_load : float e.g. 58.5
+    # g_load : [[nvidia_device_id, gpu_load]]
+
+    comm = current_communicator()
+    if comm:
+        res = [[comm.rank, c_load], *g_load[:1]]
+        t_load_ndarray = np.array(res).reshape(-1)
+
+        load_var = nn.Variable([len(t_load_ndarray), ])
+        load_var.d = t_load_ndarray
+        load_list_var = [nn.Variable([len(t_load_ndarray), ])
+                         for _ in range(comm.size)]
+        comm.all_gather(load_var.data, [a.data for a in load_list_var])
+        result_arr = [[*np.round(a.d.astype(float), decimals=1)]
+                      for a in load_list_var]
+    else:
+        res = [[0, c_load], *g_load[:1]]
+        t_load_ndarray = np.round(np.array(res).reshape(-1), decimals=1)
+        result_arr = [[*t_load_ndarray.astype(float)]]
+
+    result_arr = sorted(result_arr, key=lambda x: x[0])
+
+    return result_arr
+
+
+def measure_cpu_gpu_instant_load():
+    # Get current cpu gpu load, as
+    # load = [rank, cpu_load, nvidia_device_id, gpu_load]
+    # result_arr: [load, load, ...]
+
+    gpu_load = []
+    if gpu_load_backend_ok:
+        global gpu_a_load
+        global gpu_m_count
+
+        gpu_m_count += 1
+        try:
+            comm = current_communicator()
+            if comm:
+                index = comm.local_rank
+            elif 'cuda' in str(nn.get_current_context().backend):
+                index = 0
+            else:
+                raise Exception
+            handler = pynvml.nvmlDeviceGetHandleByIndex(index)
+            gpu_load = [
+                [index, pynvml.nvmlDeviceGetUtilizationRates(handler).gpu]]
+
+            if index in gpu_a_load.keys():
+                gpu_a_load[index]['name'] = pynvml.nvmlDeviceGetName(
+                    handler).decode("utf-8")
+                o_load = gpu_a_load[index]['load']
+                n_load = gpu_load[0][1]
+                gpu_a_load[index]['load'] = (
+                    (gpu_m_count - 1) * o_load + n_load) / gpu_m_count
+            else:
+                gpu_a_load[index] = {
+                    'name': pynvml.nvmlDeviceGetName(handler).decode("utf-8"),
+                    'load': gpu_load[0][1]
+                }
+
+        except Exception:
+            gpu_load = []
+
+    if cpu_load_backend_ok:
+        global p_handler
+        cpu_load = p_handler.cpu_percent()
+        callback.update_status(
+            ('cpu_gpu_load', collect_and_shape_result(cpu_load, gpu_load)))
+
+
+def get_cpu_gpu_average_load():
+    # Get average cpu gpu load, as
+    # load = [rank, cpu_load, nvidia_device_id, gpu_load]
+    # result_arr: [load, load, ...]
+
+    g_load = []
+    if gpu_load_backend_ok:
+        global gpu_a_load
+        global gpu_m_count
+
+        load_info = {**gpu_a_load}
+        gpu_a_load = {}
+        gpu_m_count = 0
+
+        # adjust data type then transfer them to numpy ndarray
+        g_load = [[float(a), float(load_info[a]['load'])]
+                  for a in load_info.keys()]
+        g_load = g_load[:1] if len(g_load) else []
+
+    if cpu_load_backend_ok:
+        global p_handler_avg
+        c_load = p_handler_avg.cpu_percent()
+        return collect_and_shape_result(c_load, g_load)
+
+
+def _create_optimizer_lite(opti):
+    proto_o = nnabla_pb2.Optimizer()
+    proto_o.name = opti.name
+    proto_o.solver.type = re.sub(r'(|Cuda)$', '', str(opti.solver.name))
+    opti.solver.set_states_to_protobuf(proto_o)
+    return proto_o
+
+
+def save_optimizer_states(filebase, ext, train_config):
+    filelist = []
+    if ext == '.protobuf':
+        filename = filebase + '_optimizer.protobuf.optimizer'
+        proto = nnabla_pb2.NNablaProtoBuf()
+        proto_optimizers = []
+        for o in train_config.optimizers.values():
+            proto_optimizers.append(_create_optimizer_lite(o.optimizer))
+        proto.optimizer.extend(proto_optimizers)
+        with get_file_handle_save(filename, '.protobuf') as f:
+            f.write(proto.SerializeToString())
+            filelist.append(filename)
+    else:
+        for o in train_config.optimizers.values():
+            f_name = '{}_{}_optimizer.h5'.format(
+                o.optimizer.name,
+                re.sub(r'(|Cuda)$', '', str(o.optimizer.solver.name))
+            )
+            filename = '{}_{}'.format(filebase, f_name)
+            o.optimizer.solver.save_states(filename)
+            name_ext = '{}.optimizer'.format(filename)
+            os.rename(filename, name_ext)
+            filelist.append(name_ext)
+    return filelist
+
+
+class NodeTimeInfoCollector:
+    def __init__(self):
+        self.timelist = []
+        self._frequency = _ANALYSE_GPU_STATUS_INTERVAL
+        self._warning_threshold = _GPU_SLOWING_WARNING_THRESHOLD
+        self._error_threshold = _GPU_SLOWING_ERROR_THRESHOLD
+
+    @contextmanager
+    def collect_cost_time(self, comm, iter_n):
+        if comm and iter_n % self._frequency == 0:
+            t1 = timeit.default_timer()
+            yield
+            t2 = timeit.default_timer()
+            self.timelist.append(t2 - t1)
+            self._get_analysis(comm)
+        else:
+            yield
+
+    def _reap_data(self):
+        result = self.timelist[:]
+        self.timelist = []
+        return result
+
+    def _collect_info_from_multinode(self, comm, _d_ndarray):
+        load_var = nn.Variable([len(_d_ndarray), ])
+        load_var.d = _d_ndarray
+        load_list_var = [nn.Variable([len(_d_ndarray), ])
+                         for _ in range(comm.size)]
+        comm.all_gather(load_var.data, [a.data for a in load_list_var])
+
+        return load_list_var
+
+    def _reap_multinode_data(self, comm):
+        c_data = np.array([comm.rank, sum(self._reap_data())])
+        multinode_data = self._collect_info_from_multinode(comm, c_data)
+        result_arr = [a.d for a in multinode_data]
+
+        # result_arr is list(<class 'numpy.ndarray'>, ...) like
+        return result_arr
+
+    def _get_analysis(self, comm):
+
+        def _analyse_gpu_cost_time(result, threshold):
+            aver = np.mean(result, axis=0)[1]
+            _node_l = [*filter(lambda n: n[1] > aver * threshold, result)]
+            if len(_node_l):
+                ranks = ', '.join([str(int(n[0])) for n in _node_l])
+                _str = ('Gpu of Rank {} ran slower than average '
+                        'by a factor of {} or more'.format(ranks, threshold))
+                return _str
+            return ''
+
+        result = self._reap_multinode_data(comm)
+        if comm.rank == 0:
+            error_str = _analyse_gpu_cost_time(result, self._error_threshold)
+            if error_str:
+                logger.error(error_str)
+                raise Exception(error_str)
+            else:
+                warning_str = _analyse_gpu_cost_time(
+                    result, self._warning_threshold)
+                if warning_str:
+                    logger.warning(warning_str)
+
+
+def load_train_state(filename, info):
+    info.exclude_parameter = False
+    info.parameter_only = False
+    file_loaders = get_decorated_file_loader()
+    info.parameter_scope = nn.parameter.get_current_parameter_scope()
+    load_files(info, file_loaders, filename)
+    logger.info("Load training resume states: {}".format(filename))
+
+
+from contextlib import contextmanager
+import nnabla.logger as logger
+from nnabla.lms import SwapInOutScheduler
+
+
+@contextmanager
+def sechdule_scope(scheduler):
+    scheduler.update_pre_hook()
+    yield scheduler
+    scheduler.update_post_hook()
+
+
+def lms_scheduler(ctx, use_lms, gpu_memory_size=None, window_length=None):
+    _check_list = [x.split(":")[0] for x in ctx.backend]
+    if "cudnn" not in _check_list and "cuda" not in _check_list:
+        logger.warn(
+            "ctx passed to scheduler doesn't have cuda/cudnn backend. lms scheduler will not be used.")
+        use_lms = False
+
+    comm = current_communicator()
+    if comm:
+        logger.log(99, f'[OoC] Currently OoC is disabled for Multi-GPU training.')
+        use_lms = False
+
+    if use_lms:
+        gpu_index = 0
+        if 'cuda' in str(ctx.backend):
+            gpu_index = int(ctx.device_id)
+        else:
+            logger.log(99, f'[OoC] OoC is only enabled for GPU training.')
+            raise Exception
+
+        # It is better to use nvml to get GPU infomation but due to windows problem, temporarily get information with `nvidia-smi`.
+        if gpu_memory_size is None or gpu_memory_size == 0:
+            try:
+                import subprocess
+                gpu_memory_size = int(int(subprocess.check_output('nvidia-smi --query-gpu=index,memory.total --format=csv').decode(
+                ).splitlines()[1:][gpu_index].split(',')[1].strip().split()[0]) * (1024 ** 2) * 0.7)
+            except:
+                logger.log(99, f'[OoC] Could not get GPU memory size using default value(6GB).')
+                gpu_memory_size = 6e9  # default 6 GiB
+                pass
+
+        if window_length is None or window_length == 0:
+            window_length = int(gpu_memory_size * 1.5)
+
+        logger.log(99, f'[OoC] gpu_memory_limit: {gpu_memory_size / 1e9}GB, prefetch_window_length: {window_length / 1e9}GB')
+        # Change array preference so that lms works well.
+        # import nnabla_ext.cuda.init as cuda_init
+        # cuda_init.prefer_cpu_pinned_array()
+        # cuda_init.prefer_cuda_virtual_array()
+        from nnabla.ext_utils import get_extension_context
+        be, tc = ctx.backend[0].split(":")
+        cpu_ctx = get_extension_context("cpu", device_id="", type_config=tc)
+        return SwapInOutScheduler(cpu_ctx, ctx, gpu_memory_size, window_length)
+    else:
+        class DummyScheduler(object):
+            function_pre_hook = None
+            function_post_hook = None
+            update_pre_hook = None
+            update_post_hook = None
+
+            def start_scheduling(self):
+                return None
+
+            def end_scheduling(self):
+                return None
+
+            def __enter__(self):
+                return self
+
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                pass
+
+        return DummyScheduler()
---
+++ packages/nnabla/nnabla-1.18.0-cp38-cp38-win_amd64.whl/nnabla/utils/create_cache.py
@@ -0,0 +1,187 @@
+import os
+import csv
+import h5py
+import numpy
+import shutil
+import collections
+from contextlib import closing
+from multiprocessing.pool import ThreadPool
+
+from nnabla.utils.data_source_implements import CsvDataSource
+from nnabla.utils.data_source_loader import FileReader
+
+from nnabla.config import nnabla_config
+from nnabla.logger import logger
+from nnabla.utils.progress import progress
+
+
+class CreateCache(CsvDataSource):
+    '''Create dataset cache from local file.
+
+    If you want to create cache data from remote resource, use data_iterator_csv_dataset instead.
+
+    '''
+
+    def _save_cache(self, args):
+        position = args[0]
+        cache_csv = args[1]
+        # conv dataset
+        cache_data = [tuple(self._process_row(row)) for row in cache_csv]
+
+        start_position = position + 1 - len(cache_data)
+        end_position = position
+        cache_filename = os.path.join(
+            self._cache_dir, '{}_{:08d}_{:08d}{}'.format(self._cache_file_name_prefix,
+                                                         start_position,
+                                                         end_position,
+                                                         self._cache_file_format))
+
+        logger.info('Creating cache file {}'.format(cache_filename))
+
+        data = collections.OrderedDict(
+            [(n, []) for n in self._variables])
+        for _, cd in enumerate(cache_data):
+            for i, n in enumerate(self._variables):
+                if isinstance(cd[i], numpy.ndarray):
+                    d = cd[i]
+                else:
+                    d = numpy.array(cd[i]).astype(numpy.float32)
+                data[n].append(d)
+
+        try:
+            if self._cache_file_format == ".h5":
+                h5 = h5py.File(cache_filename, 'w')
+                for k, v in data.items():
+                    h5.create_dataset(k, data=v)
+                h5.close()
+            else:
+                retry_count = 1
+                is_create_cache_incomplete = True
+                while is_create_cache_incomplete:
+                    try:
+                        with open(cache_filename, 'wb') as f:
+                            for v in data.values():
+                                numpy.save(f, v)
+                        is_create_cache_incomplete = False
+                    except OSError:
+                        retry_count += 1
+                        if retry_count > 10:
+                            raise
+                        logger.info(
+                            'Creating cache retry {}/10'.format(retry_count))
+        except:
+            logger.critical(
+                'An error occurred while creating cache file from dataset.')
+            for k, v in data.items():
+                size = v[0].shape
+                for d in v:
+                    if size != d.shape:
+                        logger.critical('The sizes of data "{}" are not the same. ({} != {})'.format(
+                            k, size, d.shape))
+            raise
+
+        return cache_filename, len(cache_data)
+
+    def __init__(self, input_csv_filename, rng=None, shuffle=False, num_of_threads=None):
+        self._cache_size = int(nnabla_config.get(
+            'DATA_ITERATOR', 'data_source_file_cache_size'))
+        logger.info('Cache size is {}'.format(self._cache_size))
+
+        self._filereader = FileReader(input_csv_filename)
+        self._original_source_uri = input_csv_filename
+        if rng is None:
+            self._rng = numpy.random.RandomState(313)
+        else:
+            self._rng = rng
+        self._shuffle = shuffle
+
+        # read index.csv
+        self._file = open(input_csv_filename, 'r', encoding='utf-8-sig')
+        csvreader = csv.reader(self._file)
+
+        header = next(csvreader)
+
+        # Store file positions of each data.
+        self._csv_data = list(csvreader)
+        self._size = len(self._csv_data)
+
+        self._file.close()
+
+        self._remove_comment_cols(header, self._csv_data)
+        self._process_header(header)
+        self._variables = tuple(self._variables_dict.keys())
+
+        self._original_order = list(range(self._size))
+
+        # Shuffle, the order is processing csv file order
+        if self._shuffle:
+            self._order = list(
+                self._rng.permutation(list(range(self._size))))
+        else:
+            self._order = list(range(self._size))
+
+        if num_of_threads:
+            self._num_of_threads = num_of_threads
+        else:
+            self._num_of_threads = int(nnabla_config.get(
+                'DATA_ITERATOR', 'data_source_file_cache_num_of_threads'))
+        logger.info('Num of thread is {}'.format(self._num_of_threads))
+
+    def create(self, output_cache_dirname, normalize=True, cache_file_name_prefix='cache'):
+
+        self._normalize = normalize
+        self._cache_file_name_prefix = cache_file_name_prefix
+        self._cache_dir = output_cache_dirname
+
+        self._cache_file_format = nnabla_config.get(
+            'DATA_ITERATOR', 'cache_file_format')
+        logger.info('Cache file format is {}'.format(self._cache_file_format))
+
+        progress(None)
+
+        csv_position_and_data = []
+        csv_row = []
+        for _position in range(self._size):
+            csv_row.append(self._csv_data[self._order[_position]])
+            if len(csv_row) == self._cache_size:
+                csv_position_and_data.append((_position, csv_row))
+                csv_row = []
+        if len(csv_row):
+            csv_position_and_data.append((self._size-1, csv_row))
+
+        progress('Create cache', 0)
+        with closing(ThreadPool(processes=self._num_of_threads)) as pool:
+            cache_index_rows = pool.map(
+                self._save_cache, csv_position_and_data)
+        progress('Create cache', 1.0)
+
+        # Create Index
+        index_filename = os.path.join(output_cache_dirname, "cache_index.csv")
+        with open(index_filename, 'w') as f:
+            writer = csv.writer(f, lineterminator='\n')
+            for row in cache_index_rows:
+                if row:
+                    # row: (file_path, data_nums)
+                    writer.writerow((os.path.basename(row[0]), row[1]))
+
+        # Create Info
+        if self._cache_file_format == ".npy":
+            info_filename = os.path.join(
+                output_cache_dirname, "cache_info.csv")
+            with open(info_filename, 'w') as f:
+                writer = csv.writer(f, lineterminator='\n')
+                for variable in self._variables:
+                    writer.writerow((variable, ))
+
+        # Create original.csv
+        if self._original_source_uri is not None:
+            shutil.copy(self._original_source_uri, os.path.join(
+                output_cache_dirname, "original.csv"))
+
+        # Create order.csv
+        if self._order is not None and \
+                self._original_order is not None:
+            with open(os.path.join(output_cache_dirname, "order.csv"), 'w') as o:
+                writer = csv.writer(o, lineterminator='\n')
+                for orders in zip(self._original_order, self._order):
+                    writer.writerow(list(orders))
