bf3d92dded3a31a09cdb025c244e95ed0def381a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:02 +0000

    Add package MathFunctions/0.3/MathFunctions-0.3.tar.gz

---
+++ packages/MathFunctions/MathFunctions-0.3.tar.gz/MathFunctions-0.3/MathFunctions/__init__.py
@@ -0,0 +1,3 @@
+from MathFunctions.Polynomial import Polynomial, Linear
+from MathFunctions.Trigonometry import Sin, Cos, sin, cos, tan
+from MathFunctions.Exponential import Exponential, Logarithm, ln, log2, log10, exp
