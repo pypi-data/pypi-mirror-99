3ca24ec0c9314b416999d325992bdcac97cd661f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:37 +0000

    Add package bapy/0.23.11/bapy-0.23.11-py3-none-any.whl

---
+++ packages/bapy/bapy-0.23.11-py3-none-any.whl/bapy/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.9
+# -*- coding: utf-8 -*-
+"""Bapy Package."""
+import typer
+
+from .core import *
+# from .ipfull import *
+# from .libfull import *
+# from .mongofull import *
+# __version__ = core.__version__
+
+__all__ = core.__all__
+# __all__ = core.__all__ + ipfull.__all__ + libfull.__all__ + mongofull.__all__
+
+if __name__ == '__main__':
+    try:
+        typer.Exit(app())
+    except KeyboardInterrupt:
+        red('Aborted!')
+        typer.Exit()
