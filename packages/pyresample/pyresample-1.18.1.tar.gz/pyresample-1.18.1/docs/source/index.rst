b0e678d4dc48c3a65989db26abc6dcffe81d45e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:56 +0000

    Add package pyresample/1.18.1/pyresample-1.18.1.tar.gz

---
+++ packages/pyresample/pyresample-1.18.1.tar.gz/pyresample-1.18.1/docs/source/index.rst
@@ -0,0 +1,50 @@
+Pyresample
+==========
+
+Pyresample is a python package for resampling geospatial image data. It is the
+primary method for resampling in the `SatPy <https://github.com/pytroll/satpy>`_
+library, but can also be used as a standalone library. Resampling or
+reprojection is the process of mapping input geolocated data points to a
+new target geographic projection and area.
+
+Pyresample can operate on both fixed grids of data and geolocated swath data.
+To describe these data Pyresample uses various "geometry" objects including
+the `AreaDefinition` and `SwathDefinition` classes.
+
+Pyresample offers multiple resampling algorithms including:
+
+- Nearest Neighbor
+- Elliptical Weighted Average (EWA)
+- Bilinear
+- Bucket resampling (count hits per bin, averaging, ratios)
+
+For nearest neighbor and bilinear interpolation pyresample uses a kd-tree
+approach by using the fast KDTree implementation provided by the
+`pykdtree <https://github.com/storpipfugl/pykdtree>`_ library.
+Pyresample works with numpy arrays and numpy masked arrays. Interfaces to
+XArray objects (including dask array support) are provided in separate
+Resampler class interfaces and are in active development.
+Utility functions are available to easily plot data using Cartopy.
+
+.. versionchanged:: 1.15
+
+    Dropped Python 2 and Python <3.4 support.
+
+Documentation
+-------------
+.. toctree::
+   :maxdepth: 2
+
+   installation
+   geo_def
+   geometry_utils
+   geo_filter
+   grid
+   swath
+   multi
+   preproc
+   plot
+   data_reduce
+   API <api/pyresample>
+
+
---
+++ packages/pyresample/pyresample-1.18.1.tar.gz/pyresample-1.18.1/setup.py
@@ -0,0 +1,95 @@
+# pyresample, Resampling of remote sensing image data in python
+#
+# Copyright (C) 2012, 2014, 2015  Esben S. Nielsen
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# workaround python bug: http://bugs.python.org/issue15881#msg170215
+# remove when python 2 support is dropped
+"""The setup module."""
+import multiprocessing  # noqa: F401
+import versioneer
+import sys
+import numpy as np
+
+from setuptools import Extension, find_packages, setup
+from Cython.Build import cythonize
+
+requirements = ['setuptools>=3.2', 'pyproj>=2.2', 'configobj',
+                'pykdtree>=1.3.1', 'pyyaml', 'numpy>=1.10.0',
+                ]
+extras_require = {'numexpr': ['numexpr'],
+                  'quicklook': ['matplotlib', 'cartopy', 'pillow'],
+                  'rasterio': ['rasterio'],
+                  'dask': ['dask>=0.16.1'],
+                  'cf': ['xarray'],
+                  'gradient_search': ['shapely'],
+                  'xarray_bilinear': ['xarray', 'dask', 'zarr']}
+
+setup_requires = ['numpy>=1.10.0', 'cython']
+test_requires = ['rasterio', 'dask', 'xarray', 'cartopy', 'pillow', 'matplotlib', 'scipy', 'zarr']
+
+if sys.platform.startswith("win"):
+    extra_compile_args = []
+else:
+    extra_compile_args = ["-O3", "-Wno-unused-function"]
+
+extensions = [
+    Extension("pyresample.ewa._ll2cr", sources=["pyresample/ewa/_ll2cr.pyx"],
+              include_dirs=[np.get_include()],
+              extra_compile_args=extra_compile_args),
+    Extension("pyresample.ewa._fornav",
+              sources=["pyresample/ewa/_fornav.pyx",
+                       "pyresample/ewa/_fornav_templates.cpp"],
+              include_dirs=[np.get_include()],
+              language="c++", extra_compile_args=extra_compile_args,
+              depends=["pyresample/ewa/_fornav_templates.h"]),
+    Extension("pyresample.gradient._gradient_search",
+              sources=["pyresample/gradient/_gradient_search.pyx"],
+              include_dirs=[np.get_include()],
+              extra_compile_args=extra_compile_args),
+]
+
+cmdclass = versioneer.get_cmdclass()
+
+if __name__ == "__main__":
+    README = open('README.md', 'r').read()
+    setup(name='pyresample',
+          url='https://github.com/pytroll/pyresample',
+          version=versioneer.get_version(),
+          cmdclass=cmdclass,
+          description='Geospatial image resampling in Python',
+          long_description=README,
+          long_description_content_type='text/markdown',
+          author='Thomas Lavergne',
+          author_email='t.lavergne@met.no',
+          package_dir={'pyresample': 'pyresample'},
+          packages=find_packages(),
+          package_data={'pyresample.test': ['test_files/*']},
+          python_requires='>=3.4',
+          setup_requires=setup_requires,
+          install_requires=requirements,
+          extras_require=extras_require,
+          tests_require=test_requires,
+          ext_modules=cythonize(extensions),
+          zip_safe=False,
+          classifiers=[
+              'Development Status :: 5 - Production/Stable',
+              'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+              'Programming Language :: Python',
+              'Operating System :: OS Independent',
+              'Intended Audience :: Science/Research',
+              'Topic :: Scientific/Engineering'
+          ]
+          )
