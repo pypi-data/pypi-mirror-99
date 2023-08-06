55d874002cc0a77998a6c8d2b2742b7bb2706666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:49 +0000

    Add package azureml-contrib-services/1.25.0/azureml_contrib_services-1.25.0-py3-none-any.whl

---
+++ packages/azureml-contrib-services/azureml_contrib_services-1.25.0-py3-none-any.whl/azureml/contrib/services/__init__.py
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""services package."""
+
+from .aml_request import rawhttp as rawhttp
---
+++ packages/azureml-contrib-services/azureml_contrib_services-1.25.0-py3-none-any.whl/azureml_contrib_services-1.25.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+AzureML Contrib Services
+
+
+
