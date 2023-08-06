058b3fb8afa926a812cc7853e229b52995bc6ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:07 +0000

    Add package oarepo-micro-api/3.3.56/oarepo_micro_api-3.3.56-py2.py3-none-any.whl

---
+++ packages/oarepo-micro-api/oarepo_micro_api-3.3.56-py2.py3-none-any.whl/oarepo_micro_api/version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CESNET.
+#
+# OARepo Micro API is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Version information for OARepo Micro API.
+
+This file is imported by ``oarepo_micro_api.__init__``,
+and parsed by ``setup.py``.
+"""
+
+from __future__ import absolute_import, print_function
+
+__version__ = '3.3.56'
