02349a22ef04589478cacb2ca9b9dc9fdb3bd5f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:40 +0000

    Add package fabric-orchestrator-client/0.0.13/fabric-orchestrator-client-0.0.13.tar.gz

---
+++ packages/fabric-orchestrator-client/fabric-orchestrator-client-0.0.13.tar.gz/fabric-orchestrator-client-0.0.13/fabric_cf/orchestrator/swagger_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from fabric_cf.orchestrator.swagger_client.api.default_api import DefaultApi
+from fabric_cf.orchestrator.swagger_client.api.resources_api import ResourcesApi
+from fabric_cf.orchestrator.swagger_client.api.slices_api import SlicesApi
+from fabric_cf.orchestrator.swagger_client.api.slivers_api import SliversApi
