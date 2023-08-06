b304f72aea92a5431b39905c27d269432ea2df19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:00 +0000

    Add package pylangtools/0.0.1/pylangtools-0.0.1.tar.gz

---
+++ packages/pylangtools/pylangtools-0.0.1.tar.gz/pylangtools-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+    name="pylangtools", # Replace with your own package name
+    version="0.0.1",
+    author="Rosefun",
+    author_email="rosefun@foxmail.com",
+    description="繁体简体转换",
+    long_description=long_description,
+    #long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+)
