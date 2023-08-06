6c5f5f245e87cde62591c188942689a2b8b7f1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:25 +0000

    Add package bootleg/1.0.1/bootleg-1.0.1-py3-none-any.whl

---
+++ packages/bootleg/bootleg-1.0.1-py3-none-any.whl/bootleg/run.py
@@ -0,0 +1,460 @@
+"""Bootleg run command."""
+
+import argparse
+import logging
+import os
+import shutil
+import subprocess
+import sys
+from functools import partial
+
+import numpy as np
+import torch
+
+import emmental
+from bootleg import log_rank_0_debug, log_rank_0_info
+from bootleg.data import get_dataloader_embeddings, get_dataloaders, get_slicedatasets
+from bootleg.optimizers.sparsedenseadam import SparseDenseAdamW
+from bootleg.symbols.constants import DEV_SPLIT, TEST_SPLIT, TRAIN_SPLIT
+from bootleg.symbols.entity_symbols import EntitySymbols
+from bootleg.task_config import NED_TASK, TYPE_PRED_TASK
+from bootleg.tasks import ned_task, type_pred_task
+from bootleg.utils import eval_utils, utils
+from bootleg.utils.model_utils import count_parameters
+from bootleg.utils.parser.parser_utils import parse_boot_and_emm_args
+from bootleg.utils.utils import (
+    dump_yaml_file,
+    load_yaml_file,
+    try_rmtree,
+    write_to_file,
+)
+from emmental.learner import EmmentalLearner
+from emmental.model import EmmentalModel
+
+logger = logging.getLogger(__name__)
+
+
+def parse_cmdline_args():
+    """Takes an input config file and parses it into the correct subdictionary
+    groups for the model.
+
+    Returns:
+        model run mode of train, eval, or dumping
+        parsed Dict config
+        path to original config path
+    """
+    # Parse cmdline args to specify config and mode
+    cli_parser = argparse.ArgumentParser(
+        description="Bootleg CLI Config",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    cli_parser.add_argument(
+        "--config_script",
+        type=str,
+        default="",
+        help="Should mimic the config_args found in utils/parser/bootleg_args.py with parameters you want to override."
+        "You can also override the parameters from config_script by passing them in directly after config_script. "
+        "E.g., --train_config.batch_size 5",
+    )
+    cli_parser.add_argument(
+        "--mode",
+        type=str,
+        default="train",
+        choices=["train", "eval", "dump_preds", "dump_embs"],
+    )
+    cli_parser.add_argument(
+        "--local_rank",
+        type=int,
+        default=-1,
+        help="When using torch.distributed it passes local_rank as command arg. We must capture it here.",
+    )
+
+    # you can add other args that will override those in the config_script
+    # parse_known_args returns 'args' that are the same as what parse_args() returns
+    # and 'unknown' which are args that the parser doesn't recognize but you want to keep.
+    # 'unknown' are what we pass on to our override any args from the second phase of arg parsing from the json file
+    cli_args, unknown = cli_parser.parse_known_args()
+    if len(cli_args.config_script) == 0:
+        raise ValueError(f"You must pass a config script via --config.")
+    config = parse_boot_and_emm_args(cli_args.config_script, unknown)
+
+    #  Modify the local rank param from the cli args
+    config.learner_config.local_rank = cli_args.local_rank
+    mode = cli_args.mode
+    return mode, config, cli_args.config_script
+
+
+def setup(config, run_config_path=None):
+    """
+    Setup distributed backend and save configuration files.
+    Args:
+        config: config
+        run_config_path: path for original run config
+
+    Returns:
+    """
+    # torch.multiprocessing.set_sharing_strategy("file_system")
+    # spawn method must be fork to work with Meta.config
+    torch.multiprocessing.set_start_method("fork", force=True)
+    """
+    ulimit -n 500000
+    python3 -m torch.distributed.launch --nproc_per_node=2  bootleg/run.py --config_script ...
+    """
+    log_level = logging.getLevelName(config.run_config.log_level.upper())
+    emmental.init(
+        log_dir=config["meta_config"]["log_path"],
+        config=config,
+        use_exact_log_path=config["meta_config"]["use_exact_log_path"],
+        local_rank=config.learner_config.local_rank,
+        level=log_level,
+    )
+
+    # Set up distributed backend
+    emmental.Meta.init_distributed_backend()
+
+    cmd_msg = " ".join(sys.argv)
+    # Log configuration into filess
+    if config.learner_config.local_rank in [0, -1]:
+        write_to_file(f"{emmental.Meta.log_path}/cmd.txt", cmd_msg)
+        dump_yaml_file(
+            f"{emmental.Meta.log_path}/parsed_config.yaml", emmental.Meta.config
+        )
+        # Dump the run config (does not contain defaults)
+        if run_config_path is not None:
+            dump_yaml_file(
+                f"{emmental.Meta.log_path}/run_config.yaml",
+                load_yaml_file(run_config_path),
+            )
+
+    log_rank_0_info(logger, f"COMMAND: {cmd_msg}")
+    log_rank_0_info(
+        logger, f"Saving config to {emmental.Meta.log_path}/parsed_config.yaml"
+    )
+
+    git_hash = "Not able to retrieve git hash"
+    try:
+        git_hash = subprocess.check_output(
+            ["git", "log", "-n", "1", "--pretty=tformat:%h-%ad", "--date=short"]
+        ).strip()
+    except subprocess.CalledProcessError:
+        pass
+    log_rank_0_info(logger, f"Git Hash: {git_hash}")
+
+
+def configure_optimizer(config):
+    """Configures the optimizer for Bootleg. By default, we use
+    SparseDenseAdam. We always change the parameter group for layer norms
+    following standard BERT finetuning methods.
+
+    Args:
+        config: config
+
+    Returns:
+    """
+    # Set default Bootleg optimizer if config doesn't override it
+    if config.learner_config.optimizer_config.optimizer is None:
+        log_rank_0_debug(logger, f"Setting default optimizer to be SparseDenseAdam")
+        custom_optimizer = partial(
+            SparseDenseAdamW,
+            lr=config.learner_config.optimizer_config.lr,
+            weight_decay=config.learner_config.optimizer_config.l2,
+            betas=config.learner_config.optimizer_config.adamw_config.betas,
+            eps=config.learner_config.optimizer_config.adamw_config.eps,
+        )
+        custom_optim_config = {
+            "learner_config": {"optimizer_config": {"optimizer": custom_optimizer}}
+        }
+        emmental.Meta.update_config(custom_optim_config)
+
+    # Specify parameter group for Adam BERT
+    def grouped_parameters(model):
+        no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+        return [
+            {
+                "params": [
+                    p
+                    for n, p in model.named_parameters()
+                    if not any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": emmental.Meta.config["learner_config"][
+                    "optimizer_config"
+                ]["l2"],
+            },
+            {
+                "params": [
+                    p
+                    for n, p in model.named_parameters()
+                    if any(nd in n for nd in no_decay)
+                ],
+                "weight_decay": 0.0,
+            },
+        ]
+
+    emmental.Meta.config["learner_config"]["optimizer_config"][
+        "parameters"
+    ] = grouped_parameters
+
+
+# TODO: optimize slices so we split them based on max aliases (save A LOT of memory)
+def run_model(mode, config, run_config_path=None):
+    """
+    Main run method for Emmental Bootleg models.
+    Args:
+        mode: run mode (train, eval, dump_preds, dump_embs)
+        config: parsed model config
+        run_config_path: original config path (for saving)
+
+    Returns:
+
+    """
+
+    # Set up distributed backend and save configuration files
+    setup(config, run_config_path)
+
+    # Load entity symbols
+    log_rank_0_info(logger, f"Loading entity symbols...")
+    entity_symbols = EntitySymbols.load_from_cache(
+        load_dir=os.path.join(
+            config.data_config.entity_dir, config.data_config.entity_map_dir
+        ),
+        alias_cand_map_file=config.data_config.alias_cand_map,
+        alias_idx_file=config.data_config.alias_idx_map,
+    )
+    # Create tasks
+    tasks = [NED_TASK]
+    if config.data_config.type_prediction.use_type_pred is True:
+        tasks.append(TYPE_PRED_TASK)
+
+    # Create splits for data loaders
+    data_splits = [TRAIN_SPLIT, DEV_SPLIT, TEST_SPLIT]
+    # Slices are for eval so we only split on test/dev
+    slice_splits = [DEV_SPLIT, TEST_SPLIT]
+    # If doing eval, only run on test data
+    if mode in ["eval", "dump_preds", "dump_embs"]:
+        data_splits = [TEST_SPLIT]
+        slice_splits = [TEST_SPLIT]
+        # We only do dumping if weak labels is True
+        if mode in ["dump_preds", "dump_embs"]:
+            if config.data_config[f"{TEST_SPLIT}_dataset"].use_weak_label is False:
+                raise ValueError(
+                    f"When calling dump_preds or dump_embs, we require use_weak_label to be True."
+                )
+
+    # Gets embeddings that need to be prepped during data prep or in the __get_item__ method
+    batch_on_the_fly_kg_adj = get_dataloader_embeddings(config, entity_symbols)
+    # Gets dataloaders
+    dataloaders = get_dataloaders(
+        config,
+        tasks,
+        data_splits,
+        entity_symbols,
+        batch_on_the_fly_kg_adj,
+    )
+    slice_datasets = get_slicedatasets(config, slice_splits, entity_symbols)
+
+    configure_optimizer(config)
+
+    # Create models and add tasks
+    if config.model_config.attn_class == "BERTNED":
+        log_rank_0_info(logger, f"Starting NED-Base Model")
+        assert (
+            config.data_config.type_prediction.use_type_pred is False
+        ), f"NED-Base does not support type prediction"
+        assert (
+            config.data_config.word_embedding.use_sent_proj is False
+        ), f"NED-Base requires word_embeddings.use_sent_proj to be False"
+        model = EmmentalModel(name="NED-Base")
+        model.add_tasks(ned_task.create_task(config, entity_symbols, slice_datasets))
+    else:
+        log_rank_0_info(logger, f"Starting Bootleg Model")
+        model = EmmentalModel(name="Bootleg")
+        # TODO: make this more general for other tasks -- iterate through list of tasks
+        # and add task for each
+        model.add_task(ned_task.create_task(config, entity_symbols, slice_datasets))
+        if TYPE_PRED_TASK in tasks:
+            model.add_task(
+                type_pred_task.create_task(config, entity_symbols, slice_datasets)
+            )
+            # Add the mention type embedding to the embedding payload
+            type_pred_task.update_ned_task(model)
+
+    # Print param counts
+    if mode == "train":
+        log_rank_0_debug(logger, "PARAMS WITH GRAD\n" + "=" * 30)
+        total_params = count_parameters(model, requires_grad=True, logger=logger)
+        log_rank_0_info(logger, f"===> Total Params With Grad: {total_params}")
+        log_rank_0_debug(logger, "PARAMS WITHOUT GRAD\n" + "=" * 30)
+        total_params = count_parameters(model, requires_grad=False, logger=logger)
+        log_rank_0_info(logger, f"===> Total Params Without Grad: {total_params}")
+
+    # Load the best model from the pretrained model
+    if config["model_config"]["model_path"] is not None:
+        model.load(config["model_config"]["model_path"])
+
+    # Barrier
+    if config["learner_config"]["local_rank"] == 0:
+        torch.distributed.barrier()
+
+    # Train model
+    if mode == "train":
+        emmental_learner = EmmentalLearner()
+        emmental_learner._set_optimizer(model)
+        emmental_learner.learn(model, dataloaders)
+        if config.learner_config.local_rank in [0, -1]:
+            model.save(f"{emmental.Meta.log_path}/last_model.pth")
+
+    # Multi-gpu DataParallel eval (NOT distributed)
+    if mode in ["eval", "dump_embs", "dump_preds"]:
+        # This happens inside EmmentalLearner for training
+        if (
+            config["learner_config"]["local_rank"] == -1
+            and config["model_config"]["dataparallel"]
+        ):
+            model._to_dataparallel()
+
+    # If just finished training a model or in eval mode, run eval
+    if mode in ["train", "eval"]:
+        scores = model.score(dataloaders)
+        # Save metrics and models
+        log_rank_0_info(logger, f"Saving metrics to {emmental.Meta.log_path}")
+        log_rank_0_info(logger, f"Metrics: {scores}")
+        scores["log_path"] = emmental.Meta.log_path
+        if config.learner_config.local_rank in [0, -1]:
+            write_to_file(f"{emmental.Meta.log_path}/{mode}_metrics.txt", scores)
+            eval_utils.write_disambig_metrics_to_csv(
+                f"{emmental.Meta.log_path}/{mode}_disambig_metrics.csv", scores
+            )
+        return scores
+
+    # If you want detailed dumps, save model outputs
+    assert mode in [
+        "dump_preds",
+        "dump_embs",
+    ], 'Mode must be "dump_preds" or "dump_embs"'
+    dump_embs = False if mode != "dump_embs" else True
+    assert (
+        len(dataloaders) == 1
+    ), f"We should only have length 1 dataloaders for dump_embs and dump_preds!"
+    final_result_file, final_out_emb_file = None, None
+    if config.learner_config.local_rank in [0, -1]:
+        # Setup files/folders
+        filename = os.path.basename(dataloaders[0].dataset.raw_filename)
+        log_rank_0_debug(
+            logger,
+            f"Collecting sentence to mention map {os.path.join(config.data_config.data_dir, filename)}",
+        )
+        sentidx2num_mentions, sent_idx2row = eval_utils.get_sent_idx2num_mens(
+            os.path.join(config.data_config.data_dir, filename)
+        )
+        log_rank_0_debug(logger, f"Done collecting sentence to mention map")
+        eval_folder = eval_utils.get_eval_folder(filename)
+        subeval_folder = os.path.join(eval_folder, "batch_results")
+        utils.ensure_dir(subeval_folder)
+        # Will keep track of sentences dumped already. These will only be ones with mentions
+        all_dumped_sentences = set()
+        number_dumped_batches = 0
+        total_mentions_seen = 0
+        all_result_files = []
+        all_out_emb_files = []
+        # Iterating over batches of predictions
+        for res_i, res_dict in enumerate(
+            eval_utils.batched_pred_iter(
+                model,
+                dataloaders[0],
+                config.run_config.eval_accumulation_steps,
+                sentidx2num_mentions,
+            )
+        ):
+            (
+                result_file,
+                out_emb_file,
+                final_sent_idxs,
+                mentions_seen,
+            ) = eval_utils.disambig_dump_preds(
+                res_i,
+                total_mentions_seen,
+                config,
+                res_dict,
+                sentidx2num_mentions,
+                sent_idx2row,
+                subeval_folder,
+                entity_symbols,
+                dump_embs,
+                NED_TASK,
+            )
+            all_dumped_sentences.update(final_sent_idxs)
+            all_result_files.append(result_file)
+            all_out_emb_files.append(out_emb_file)
+            total_mentions_seen += mentions_seen
+            number_dumped_batches += 1
+
+        # Dump the sentences that had no mentions and were not already dumped
+        # Assert all remaining sentences have no mentions
+        assert all(
+            v == 0
+            for k, v in sentidx2num_mentions.items()
+            if k not in all_dumped_sentences
+        ), (
+            f"Sentences with mentions were not dumped: "
+            f"{[k for k, v in sentidx2num_mentions.items() if k not in all_dumped_sentences]}"
+        )
+        empty_sentidx2row = {
+            k: v for k, v in sent_idx2row.items() if k not in all_dumped_sentences
+        }
+        empty_resultfile = eval_utils.get_result_file(
+            number_dumped_batches, subeval_folder
+        )
+        all_result_files.append(empty_resultfile)
+        # Dump the outputs
+        eval_utils.write_data_labels_single(
+            sentidx2row=empty_sentidx2row,
+            output_file=empty_resultfile,
+            filt_emb_data=None,
+            sental2embid={},
+            alias_cand_map=entity_symbols.get_alias2qids(),
+            qid2eid=entity_symbols.get_qid2eid(),
+            result_alias_offset=total_mentions_seen,
+            train_in_cands=config.data_config.train_in_candidates,
+            max_cands=entity_symbols.max_candidates,
+            dump_embs=dump_embs,
+        )
+
+        log_rank_0_info(
+            logger, f"Finished dumping. Merging results across accumulation steps."
+        )
+        # Final result files for labels and embeddings
+        final_result_file = os.path.join(
+            eval_folder, config.run_config.result_label_file
+        )
+        # Copy labels
+        output = open(final_result_file, "wb")
+        for file in all_result_files:
+            shutil.copyfileobj(open(file, "rb"), output)
+        output.close()
+        log_rank_0_info(logger, f"Bootleg labels saved at {final_result_file}")
+        # Try to copy embeddings
+        if dump_embs:
+            final_out_emb_file = os.path.join(
+                eval_folder, config.run_config.result_emb_file
+            )
+            log_rank_0_info(
+                logger,
+                f"Trying to merge numpy embedding arrays. "
+                f"If your machine is limited in memory, this may cause OOM errors. "
+                f"Is that happens, result files should be saved in {subeval_folder}.",
+            )
+            all_arrays = []
+            for i, npfile in enumerate(all_out_emb_files):
+                all_arrays.append(np.load(npfile))
+            np.save(final_out_emb_file, np.concatenate(all_arrays))
+            log_rank_0_info(logger, f"Bootleg embeddings saved at {final_out_emb_file}")
+
+        # Cleanup
+        try_rmtree(subeval_folder)
+    return final_result_file, final_out_emb_file
+
+
+if __name__ == "__main__":
+    mode, config, run_config_path = parse_cmdline_args()
+    run_model(mode, config, run_config_path)
---
+++ packages/bootleg/bootleg-1.0.1-py3-none-any.whl/bootleg/utils/utils.py
@@ -0,0 +1,344 @@
+import json
+import logging
+import os
+import pathlib
+import shutil
+import time
+import unicodedata
+from importlib import import_module
+from itertools import chain, islice
+
+import marisa_trie
+import ujson
+import yaml
+from tqdm import tqdm
+
+from bootleg import log_rank_0_info
+
+logger = logging.getLogger(__name__)
+
+
+def ensure_dir(d):
+    """Checks if a directory exists. If not, it makes it.
+
+    Args:
+        d: path
+
+    Returns:
+    """
+    pathlib.Path(d).mkdir(exist_ok=True, parents=True)
+
+
+def exists_dir(d):
+    """Checks if directory exists.
+
+    Args:
+        d: path
+
+    Returns:
+    """
+    return pathlib.Path(d).exists()
+
+
+def dump_json_file(filename, contents):
+    """Dumps dictionary to json file.
+
+    Args:
+        filename: file to write to
+        contents: dictionary to save
+
+    Returns:
+    """
+    filename = pathlib.Path(filename)
+    filename.parent.mkdir(exist_ok=True, parents=True)
+    with open(filename, "w") as f:
+        try:
+            ujson.dump(contents, f)
+        except OverflowError:
+            json.dump(contents, f)
+
+
+def dump_yaml_file(filename, contents):
+    """Dumps dictionary to yaml file.
+
+    Args:
+        filename: file to write to
+        contents: dictionary to save
+
+    Returns:
+    """
+    filename = pathlib.Path(filename)
+    filename.parent.mkdir(exist_ok=True, parents=True)
+    with open(filename, "w") as f:
+        yaml.dump(contents, f)
+
+
+def load_json_file(filename):
+    """Loads dictionary from json file.
+
+    Args:
+        filename: file to read from
+
+    Returns: Dict
+    """
+    with open(filename, "r") as f:
+        contents = ujson.load(f)
+    return contents
+
+
+def load_yaml_file(filename):
+    """Loads dictionary from yaml file.
+
+    Args:
+        filename: file to read from
+
+    Returns: Dict
+    """
+    with open(filename) as f:
+        contents = yaml.load(f, Loader=yaml.FullLoader)
+    return contents
+
+
+def assert_keys_in_dict(allowable_keys, d):
+    """
+    Checks that all keys in d are in allowable keys
+    Args:
+        allowable_keys: Set or List of allowable keys
+        d: Dict
+
+    Returns: Boolean if satisfied, None if correct/key that is not in allowable keys
+    """
+    for k in d:
+        if k not in allowable_keys:
+            return False, k
+    return True, None
+
+
+def write_to_file(filename, value):
+    """Write generic value to a file. If value is not string, will cast to
+    str()
+
+    Args:
+        filename: file to write to
+        value: context to write
+
+    Returns: Dict
+    """
+    ensure_dir(os.path.dirname(filename))
+    if not isinstance(value, str):
+        value = str(value)
+    fout = open(filename, "w")
+    fout.write(value + "\n")
+    fout.close()
+
+
+def load_jsonl(filepath):
+    """Loads jsonlines data from jsonl file.
+
+    Args:
+        filepath: file to read from
+
+    Returns: List[Dict] of data
+    """
+    st = time.time()
+    lines = []
+    num_lines = sum([1 for _ in open(filepath)])
+    with open(filepath, "r") as in_f:
+        for line in tqdm(in_f, total=num_lines):
+            lines.append(ujson.loads(line))
+    print("time", time.time() - st)
+    return lines
+
+
+def write_jsonl(filepath, values):
+    """Writes List[Dict] data to jsonlines file.
+
+    Args:
+        filepath: file to write to
+        values: list of dictionary data to write
+
+    Returns:
+    """
+    with open(filepath, "w") as out_f:
+        for val in values:
+            out_f.write(ujson.dumps(val) + "\n")
+    return
+
+
+def chunks(iterable, n):
+    """Chunks data.
+
+    chunks(ABCDE,2) => AB CD E.
+
+    Args:
+        iterable: iterable input
+        n: number of chunks
+
+    Returns: next chunk
+    """
+    iterable = iter(iterable)
+    while True:
+        try:
+            yield chain([next(iterable)], islice(iterable, n - 1))
+        except StopIteration:
+            return None
+
+
+def chunk_file(in_file, out_dir, num_lines, prefix="out_"):
+    """Chunks a file into num_lines chunks.
+
+    Args:
+        in_file: input file
+        out_dir: output directory
+        num_lines: number of lines in each chunk
+        prefix: prefix for output files in out_dir
+
+    Returns: total number of lines read, dictionary of output file path -> number of lines in that file (for tqdms)
+    """
+    ensure_dir(out_dir)
+    out_files = {}
+    total_lines = 0
+    ending = os.path.splitext(in_file)[1]
+    with open(in_file) as bigfile:
+        i = 0
+        while True:
+            try:
+                lines = next(chunks(bigfile, num_lines))
+            except StopIteration:
+                break
+            except RuntimeError:
+                break
+            file_split = os.path.join(out_dir, f"{prefix}{i}{ending}")
+            total_file_lines = 0
+            i += 1
+            with open(file_split, "w") as f:
+                while True:
+                    try:
+                        line = next(lines)
+                    except StopIteration:
+                        break
+                    total_lines += 1
+                    total_file_lines += 1
+                    f.write(line)
+            out_files[file_split] = total_file_lines
+    return total_lines, out_files
+
+
+def create_single_item_trie(in_dict, out_file=""):
+    """Creates a marisa trie from the input dictionary. We assume the
+    dictionary has string keys and integer values.
+
+    Args:
+        in_dict: Dict[str] -> Int
+        out_file: marisa file to save (useful for reading as memmap) (optional)
+
+    Returns: marisa trie of in_dict
+    """
+    keys = []
+    values = []
+    for k in in_dict:
+        assert type(in_dict[k]) is int
+        keys.append(k)
+        # Tries require list of item for the record trie
+        values.append(tuple([in_dict[k]]))
+    fmt = "<l"
+    trie = marisa_trie.RecordTrie(fmt, zip(keys, values))
+    if out_file != "":
+        trie.save(out_file)
+    return trie
+
+
+def load_single_item_trie(file):
+    """Load a marisa trie with integer values from memmap file.
+
+    Args:
+        file: marisa input file
+
+    Returns: marisa trie
+    """
+    assert exists_dir(file)
+    return marisa_trie.RecordTrie("<l").mmap(file)
+
+
+def import_class(prefix_string, base_string):
+    """
+    Takes the prefix path and import that plus all but the rightmost modules in base string.
+    This can be used in conjunciton with
+        `getattr(mod, load_class)(...)`
+    to initialize the base_string class
+    Ex: import_class("bootleg.embeddings", "LearnedEntityEmb") will return
+    bootleg.embeddings module and "LearnedEntityEmb"
+
+    Args:
+        prefix_string: prefix path
+        base_string: base string
+
+    Returns: imported module, class string
+
+    """
+    if "." in base_string:
+        path, load_class = base_string.rsplit(".", 1)
+        mod = import_module(f"{prefix_string}.{path}")
+    else:
+        load_class = base_string
+        mod = import_module(f"{prefix_string}")
+    return mod, load_class
+
+
+def get_lnrm(s, strip, lower):
+    """Convert a string to its lnrm form We form the lower-cased normalized
+    version l(s) of a string s by canonicalizing its UTF-8 characters,
+    eliminating diacritics, lower-casing the UTF-8 and throwing out all ASCII-
+    range characters that are not alpha-numeric.
+
+    from http://nlp.stanford.edu/pubs/subctackbp.pdf Section 2.3
+
+    Args:
+        s: input string
+        strip: boolean for stripping alias or not
+        lower: boolean for lowercasing alias or not
+
+    Returns: the lnrm form of the string
+    """
+    if not strip and not lower:
+        return s
+    lnrm = str(s)
+    if lower:
+        lnrm = lnrm.lower()
+    if strip:
+        lnrm = unicodedata.normalize("NFD", lnrm)
+        lnrm = "".join(
+            [
+                x
+                for x in lnrm
+                if (not unicodedata.combining(x) and x.isalnum() or x == " ")
+            ]
+        ).strip()
+    # will remove if there are any duplicate white spaces e.g. "the  alias    is here"
+    lnrm = " ".join(lnrm.split())
+    return lnrm
+
+
+def try_rmtree(rm_dir):
+    """In the case a resource is open, rmtree will fail. This retries to rmtree
+    after 1 second waits for 5 times.
+
+    Args:
+        rm_dir: directory to remove
+
+    Returns:
+    """
+    num_retries = 0
+    max_retries = 5
+    while num_retries < max_retries:
+        try:
+            shutil.rmtree(rm_dir)
+            break
+        except OSError:
+            time.sleep(1)
+            num_retries += 1
+            if num_retries >= max_retries:
+                log_rank_0_info(
+                    logger,
+                    f"{rm_dir} was not able to be deleted. This is okay but will have to manually be removed.",
+                )
