08daec5e8dbf21af0d2c9fcc9748de74cfa61ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:32 +0000

    Add package RBX.py/0.6/RBX.py-0.6.tar.gz

---
+++ packages/RBX.py/RBX.py-0.6.tar.gz/RBX.py-0.6/RBX_py/__init__.py
@@ -0,0 +1,10 @@
+__title__ = 'RBX_py'
+__author__ = 'Token'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Token'
+__version__ = '0.6'
+
+from RBX_py.Utils import *
+from RBX_py.Asset import *
+from RBX_py.Errors import *
+from RBX_py.Client import *
