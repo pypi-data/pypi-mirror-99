3e6e3b4fa515b15efd60f980df0a34df706cc81a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:41 +0000

    Add package spyder/5.0.0a7/spyder-5.0.0a7-py3-none-any.whl

---
+++ packages/spyder/spyder-5.0.0a7-py3-none-any.whl/spyder/plugins/application/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © Spyder Project Contributors
+# Licensed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+
+"""
+spyder.plugins.application
+==========================
+
+Application Plugin.
+"""
+
+from spyder.plugins.application.plugin import Application
+
+# The following statement is required to be able to grab internal plugins.
+PLUGIN_CLASSES = [Application]
---
+++ packages/spyder/spyder-5.0.0a7-py3-none-any.whl/spyder/plugins/breakpoints/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) 2009- Spyder Project Contributors
+#
+# Distributed under the terms of the MIT License
+# (see spyder/__init__.py for details)
+# -----------------------------------------------------------------------------
+
+from spyder.plugins.breakpoints.plugin import Breakpoints
+
+# The following statement is required to be able to grab internal plugins.
+PLUGIN_CLASSES = [Breakpoints]
