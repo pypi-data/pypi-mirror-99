9131b5bdac0839842c789fef4760dc5b76dd373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:21 +0000

    Add package xchainpy-crypto/0.1.2/xchainpy_crypto-0.1.2-py3-none-any.whl

---
+++ packages/xchainpy-crypto/xchainpy_crypto-0.1.2-py3-none-any.whl/xchainpy_crypto/models/KdfParams.py
@@ -0,0 +1,45 @@
+class KdfParams:
+    def __init__(self, prf : str , dklen : int , salt : str , c : int):
+        self._prf = prf
+        self._dklen = dklen
+        self._salt = salt
+        self._c = c
+
+    @classmethod
+    def from_dict(cls, kdfparams):
+        new_kdfparams = cls.__new__(cls)
+        for key in kdfparams:
+            setattr(new_kdfparams, key, kdfparams[key])
+        return new_kdfparams
+    
+    @property
+    def prf(self):
+        return self._prf
+
+    @prf.setter
+    def prf(self, prf):
+        self._prf = prf
+
+    @property
+    def dklen(self):
+        return self._dklen
+    
+    @dklen.setter
+    def dklen(self, dklen):
+        self._dklen = dklen
+    
+    @property
+    def salt(self):
+        return self._salt
+
+    @salt.setter
+    def salt(self, salt):
+        self._salt = salt
+
+    @property
+    def c(self):
+        return self._c
+
+    @c.setter
+    def c(self, c):
+        self._c = c
