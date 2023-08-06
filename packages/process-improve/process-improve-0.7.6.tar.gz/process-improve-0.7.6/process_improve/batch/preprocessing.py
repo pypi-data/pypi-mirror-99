369914a6d3dd03c187b3e878434f2ecc0447fb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:05 +0000

    Add package process-improve/0.7.6/process-improve-0.7.6.tar.gz

---
+++ packages/process-improve/process-improve-0.7.6.tar.gz/process-improve-0.7.6/process_improve/batch/preprocessing.py
@@ -0,0 +1,386 @@
+from typing import Dict, List
+import numpy as np
+import pandas as pd
+import logging
+import plotly.graph_objects as go
+
+from .alignment_helpers import distance_matrix, backtrack_optimal_path
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(f"{__file__}.log")
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+# dtwalign: https://github.com/statefb/dtwalign
+# from dtwalign import dtw_from_distance_matrix
+
+epsqrt = np.sqrt(np.finfo(float).eps)
+
+
+def determine_scaling(
+    batches: Dict[str, pd.DataFrame],
+    columns_to_align: List = None,
+    settings: dict = None,
+) -> pd.DataFrame:
+    """
+    Scales the batch data according to the variable ranges.
+
+    Parameters
+    ----------
+    dict_df : Dict[str, pd.DataFrame]
+        Batch data, in the standard format.
+
+    Returns
+    -------
+    range_scalers: DataFrame
+
+        J rows
+        2 columns:
+            column 1 = Range of each tag. Approximated as the typical delta from q98 - q02
+            column 2 = Typical minimum of each tag. Robustly calculated.
+
+    TODO: put this in a scikit-learn style: .fit() and .apply() style
+    """
+    # This will be clumsy, until we have Python 3.9
+    default_settings = {"robust": True}
+    if settings:
+        default_settings.update(settings)
+
+    settings = default_settings
+    if columns_to_align is None:
+        columns_to_align = batches[list(batches.keys())[0]].columns
+
+    collector_rnge = []
+    collector_mins = []
+    for _, batch in batches.items():
+        if settings["robust"]:
+            rnge = batch[columns_to_align].quantile(0.98) - batch[
+                columns_to_align
+            ].quantile(0.02)
+        else:
+            rnge = batch[columns_to_align].max() - batch[columns_to_align].min()
+
+        rnge[rnge.values == 0] = 1.0
+        collector_rnge.append(rnge)
+        collector_mins.append(batch.min())
+
+    if settings["robust"]:
+        scalings = pd.concat(
+            [
+                pd.DataFrame(collector_rnge).median(),
+                pd.DataFrame(collector_mins).median(),
+            ],
+            axis=1,
+        )
+    else:
+        scalings = pd.concat(
+            [pd.DataFrame(collector_rnge).mean(), pd.DataFrame(collector_mins).mean()],
+            axis=1,
+        )
+    scalings.columns = ["Range", "Minimum"]
+    scalings["Minimum"] = 0.0
+    return scalings
+
+
+def apply_scaling(
+    batches: Dict[str, pd.DataFrame],
+    scale_df: pd.DataFrame,
+    columns_to_align: List = None,
+) -> dict:
+    """Scales the batches according to the information in the scaling dataframe.
+
+    Parameters
+    ----------
+    batches : Dict[str, pd.DataFrame]
+        The batches, in standard format.
+    scale_df : pd.DataFrame
+        The scaling dataframe, from `determine_scaling`
+
+    Returns
+    -------
+    dict
+        The scaled batch data.
+    """
+    # TODO: handle the case of DataFrames still
+    if columns_to_align is None:
+        if isinstance(batches, dict):
+            batch1 = batches[list(batches.keys())[0]]
+            columns_to_align = batch1.columns
+        else:
+            columns_to_align = batches.columns
+    out = {}
+    for batch_id, batch in batches.items():
+        out[batch_id] = batch[columns_to_align].copy()
+        for tag, column in out[batch_id].iteritems():
+            out[batch_id][tag] = (column - scale_df.loc[tag, "Minimum"]) / scale_df.loc[
+                tag, "Range"
+            ]
+    return out
+
+
+def reverse_scaling(
+    batches: Dict[str, pd.DataFrame],
+    scale_df: pd.DataFrame,
+    columns_to_align: List = None,
+):
+    # TODO: handle the case of DataFrames still
+    if columns_to_align is None:
+        if isinstance(batches, dict):
+            columns_to_align = batches[list(batches.keys())[0]].columns
+        else:
+            columns_to_align = batches.columns
+    out = {}
+    for batch_id, batch in batches.items():
+        out[batch_id] = batch[columns_to_align].copy()
+        for tag, column in out[batch_id].iteritems():
+            out[batch_id][tag] = (
+                column * scale_df.loc[tag, "Range"] + scale_df.loc[tag, "Minimum"]
+            )
+    return out
+
+
+class DTWresult:
+    """Result class."""
+
+    def __init__(
+        self,
+        synced: np.ndarray,
+        penalty_matrix: np.ndarray,
+        md_path: np.ndarray,  # multi-dimensional path through distance mesh D = penalty_matrix
+        warping_path: np.ndarray,
+        distance: float,
+        normalized_distance: float,
+    ):
+        self.synced = synced
+        self.penalty_matrix = penalty_matrix
+        self.md_path = md_path
+        self.warping_path = warping_path
+        self.distance = distance
+        self.normalized_distance = normalized_distance
+
+
+def align_with_path(md_path, batch, initial_row):
+    row = 0
+    nr = md_path[:, 0].max() + 1  # to account for the zero-based indexing
+    synced = np.zeros((nr, batch.shape[1]))
+    synced[row, :] = batch.iloc[md_path[0, 1], :]
+    temp = initial_row
+    for idx in np.arange(1, md_path.shape[0]):
+        if md_path[idx, 0] != md_path[idx - 1, 0]:
+            row += 1
+            synced[row, :] = temp = batch.iloc[md_path[idx, 1], :]
+
+        else:
+            # TODO : Come back to page 181 of thesis: where more than 1 point in the target
+            #        trajectory is aligned with the reference: compute the average,
+            temp = np.vstack((temp, batch.iloc[md_path[idx, 1], :]))
+            synced[row, :] = np.nanmean(temp, axis=0)
+
+    return pd.DataFrame(synced, columns=batch.columns)
+
+
+def dtw_core(test, ref, weight_matrix: np.ndarray):
+
+    show_plot = False
+    nt = test.shape[0]  # 'test' data; will be align to the 'reference' data
+    nr = ref.shape[0]
+    assert test.shape[1] == ref.shape[1]
+
+    D = distance_matrix(test.values, ref.values, weight_matrix)
+    md_path, distance = backtrack_optimal_path(D)
+    warping_path = np.zeros(nr)
+    for idx in range(nr):
+        warping_path[idx] = md_path[np.where(md_path[:, 0] == idx)[0][-1], 1]
+
+    # Now align the `test` batch:
+    initial_row = ref.iloc[md_path[0, 0], :].copy()
+    synced = align_with_path(md_path=md_path, batch=test, initial_row=initial_row)
+
+    if show_plot:  # for debugging
+        X = np.arange(0, nt, 1)
+        Y = np.arange(0, nr, 1)
+        X, Y = np.meshgrid(X, Y)
+        fig = go.Figure(
+            data=[
+                go.Mesh3d(
+                    x=X.ravel(),
+                    y=Y.ravel(),
+                    z=D.ravel(),
+                    color="lightpink",
+                    opacity=0.90,
+                )
+            ]
+        )
+        fig.show()
+
+    return DTWresult(
+        synced,
+        D,
+        md_path,
+        warping_path,
+        distance,
+        normalized_distance=distance / (nr + nt),
+    )
+
+
+def one_iteration_dtw(
+    batches_scaled: dict,
+    refbatch_sc: pd.DataFrame,
+    weight_matrix: np.ndarray,
+):
+    aligned_batches = {}
+    distances = []
+    average_batch = refbatch_sc.copy().reset_index(drop=True) * 0.0
+    successful_alignments = 0
+    for batch_id, batch in batches_scaled.items():
+        logger.debug(f"  * {batch_id}")
+        try:
+            # see Kassidas, page 180
+            result = dtw_core(batch, refbatch_sc, weight_matrix=weight_matrix)
+            average_batch += result.synced
+            aligned_batches[batch_id] = result
+            successful_alignments += 1
+            distances.append(
+                {
+                    "batch_id": batch_id,
+                    "Distance": result.distance,
+                    "Normalized distance": result.normalized_distance,
+                }
+            )
+        except ValueError:
+            assert False, f"Failed on batch {batch_id}"
+
+    average_batch /= successful_alignments
+
+    return aligned_batches, average_batch
+
+
+def batch_dtw(
+    batches: Dict[str, pd.DataFrame],
+    columns_to_align: list,
+    reference_batch: str,
+    settings: dict = None,
+) -> dict:
+    """
+    Synchronize, via iterative DTW, with weighting.
+
+    Algorithm: Kassidas et al. (2004):  https://doi.org/10.1002/aic.690440412
+
+    Parameters
+    ----------
+    batches : Dict[str, pd.DataFrame]
+        Batch data, in the standard format.
+    columns_to_align : list
+        Which columns to use during the alignment process. The others are aligned, but
+        get no weight, and therefore do not influence the objective function.
+    reference_batch : str
+        Which key in the `batches` is the reference batch.
+    maximum_iterations : int
+        The maximum number of iterations allowed.
+
+    Returns
+    -------
+    dict
+        Various outputs relevant to the alignment.
+        TODO: Document completely later.
+
+    Notation
+    --------
+    I = number of batches: index = i
+    i = index for the batches
+    J = number of tags (columns in each batch)
+    j = index for the tags
+    k = index into the rows of each batch, the samples: 0 ... k ... K_i
+    """
+    default_settings = {"maximum_iterations": 25, "robust": True, "tolerance": 0.1}
+    if settings:
+        default_settings.update(settings)
+    settings = default_settings
+    assert settings["maximum_iterations"] >= 3, "At least 3 iterations are required"
+
+    scale_df = determine_scaling(
+        batches=batches, columns_to_align=columns_to_align, settings=settings
+    )
+    batches_scaled = apply_scaling(batches, scale_df, columns_to_align)
+    refbatch_sc = batches_scaled[reference_batch]
+    weight_vector = np.ones(refbatch_sc.shape[1])
+    weight_matrix = np.diag(weight_vector)
+    weight_history = np.zeros_like(weight_vector) * np.nan
+
+    delta_weight = np.linalg.norm(weight_vector)
+    iter = 0
+    while (np.linalg.norm(delta_weight) > settings["tolerance"]) and (
+        iter <= settings["maximum_iterations"]
+    ):
+        logger.debug(f"Iter = {iter} and norm = {np.linalg.norm(delta_weight)}")
+        iter += 1
+        weight_matrix = np.diag(weight_vector)
+        weight_history = np.vstack((weight_history, weight_vector.copy()))
+
+        aligned_batches, average_batch = one_iteration_dtw(
+            batches_scaled=batches_scaled,
+            refbatch_sc=refbatch_sc,
+            weight_matrix=weight_matrix,
+        )
+
+        # Deviations from the average batch:
+        next_weights = np.zeros((1, refbatch_sc.shape[1]))
+        for _, result in aligned_batches.items():
+            next_weights += np.nansum(
+                np.power(result.synced - average_batch, 2), axis=0
+            )
+            # TODO: use quadratic weights for now, but try sum of the absolute values instead
+            #  np.abs(result.synced - average_batch).sum(axis=0)
+
+            # TODO: leave out worst batches when computing the weights
+            # dist_df = pd.DataFrame(distances).set_index("batch_id")
+            # dist_df.hist("Distance", bins=50)
+            # Now find the average trajectory, but ignore problematic batches:
+            #      for example, top 5% of the distances.
+            # TODO: make this a configurable setting
+            # problematic_threshold = dist_df["Distance"].quantile(0.95)
+
+        next_weights = 1.0 / np.where(next_weights > epsqrt, next_weights, 10000)
+        weight_vector = (
+            next_weights / np.sum(next_weights) * len(columns_to_align)
+        ).ravel()
+        # If change in delta_weight is small, we terminate early; no need to fine-tune excessively.
+        delta_weight = np.diag(weight_matrix) - weight_vector  # old - new
+
+    # OK, the weights are found: now use the last iteration's result to get back to original
+    # scaling for the trajectories
+    weight_history = weight_history[1:, :]
+    aligned_df = pd.DataFrame()
+    for batch_id, result in aligned_batches.items():
+        initial_row = batches[batch_id].iloc[result.md_path[0, 0], :].copy()
+        synced = align_with_path(
+            result.md_path, batches[batch_id], initial_row=initial_row
+        )
+        synced.insert(1, "sequence", list(range(synced.shape[0])))
+        aligned_df = aligned_df.append(synced)
+
+    max_places = int(np.ceil(np.log10(aligned_df["sequence"].max())))
+    aligned_wide_df = aligned_df.pivot(index="batch_id", columns="sequence")
+    new_labels = [
+        "-".join(item)
+        for item in zip(
+            aligned_wide_df.columns.get_level_values(0),
+            [
+                str(val).zfill(max_places)
+                for val in aligned_wide_df.columns.get_level_values(1)
+            ],
+        )
+    ]
+    aligned_wide_df.columns = new_labels
+    last_average_batch = reverse_scaling(dict(avg=average_batch), scale_df)["avg"]
+
+    return dict(
+        scale_df=scale_df,
+        aligned_batch_objects=aligned_batches,
+        last_average_batch=last_average_batch,
+        weight_history=weight_history,
+        aligned_wide_df=aligned_wide_df,
+    )
---
+++ packages/process-improve/process-improve-0.7.6.tar.gz/process-improve-0.7.6/tests/test_doe.py
@@ -0,0 +1,255 @@
+import os
+import unittest
+import numpy as np
+import pandas as pd
+
+
+from process_improve.structures import gather, create_names, c
+from process_improve.models import lm
+
+
+class TestStructures(unittest.TestCase):
+    """
+    Test the data structures.
+    """
+
+    def setUp(self):
+        self.A = [-1, +1, -1, +1, 0, +1]
+        self.B = [-1, -1, +1, +1, 0, 0]
+        self.C = [4, 5, 6, 4, 6]
+        self.D = [0, 1, "green"]
+        self.y = [52, 74, 62, 80, 50, 65]
+
+    def test_create_names(self):
+        """Creating factor names." """
+
+        self.assertListEqual(create_names(5), ["A", "B", "C", "D", "E"])
+
+        self.assertListEqual(create_names(3, letters=False), ["X1", "X2", "X3"])
+
+        self.assertListEqual(
+            create_names(3, letters=False, prefix="Q", start_at=9, padded=True),
+            ["Q09", "Q10", "Q11"],
+        )
+
+        self.assertListEqual(
+            create_names(4, letters=False, prefix="Z", start_at=99, padded=True),
+            ["Z099", "Z100", "Z101", "Z102"],
+        )
+
+    def test_create_factors(self):
+        A1 = c(*(self.A))
+        A2 = c(*(self.A), index=["lo", "hi", "lo", "hi", "cp", "hi"])
+        B = c(*(self.B), name="B")
+        C1 = c(*(self.C), range=(4, 6))
+        C2 = c(*(self.C), center=5, range=(4, 6))
+        C3 = c(*(self.C), lo=4, hi=6)
+        C4 = c(*(self.C), lo=4, hi=6, name="C4")
+        C5 = c([4, 5, 6, 4, 6], lo=4, hi=6, name="C5")
+        C6 = c(*(self.C), lo=5, hi=6, name="C6")
+        y = c(*(self.y), name="conversion", units="%")
+
+        self.assertTrue(isinstance(A1, pd.Series))
+        self.assertTrue(A1.shape == (6,))
+        self.assertTrue(hasattr(A1, "pi_index"))
+        self.assertTrue(hasattr(A1, "name"))
+
+        self.assertTrue(A1.name == "Unnamed [coded]")
+        self.assertTrue(hasattr(A1, "pi_lo"))
+        self.assertTrue(A1.pi_lo == -1)
+        self.assertTrue(hasattr(A1, "pi_hi"))
+        self.assertTrue(A1.pi_hi == +1)
+        self.assertTrue(hasattr(A1, "pi_range"))
+        self.assertTrue(A1.pi_range[0] == -1)
+        self.assertTrue(A1.pi_range[1] == +1)
+        self.assertTrue(hasattr(A1, "pi_center"))
+        self.assertTrue(A1.pi_center == 0)
+
+        self.assertTrue(isinstance(A2.index, pd.Index))
+        self.assertTrue(hasattr(A2, "pi_index"))
+        self.assertTrue(A2.name == "Unnamed [coded]")
+
+        with self.assertRaises(IndexError):
+            A2 = c(*(self.A), index=["lo", "hi", "lo", "hi", "cp"])
+
+        self.assertTrue(B.shape == (6,))
+        self.assertTrue(B.name == "B [coded]")
+
+        self.assertTrue(C1.pi_range == (4, 6))
+        self.assertTrue(C2.pi_center == 5)
+        self.assertTrue(C2.pi_range == (4, 6))
+        self.assertTrue(C3.pi_lo == 4)
+        self.assertTrue(C3.pi_hi == 6)
+        self.assertTrue(C4.pi_lo == 4)
+        self.assertTrue(C4.pi_hi == 6)
+        self.assertTrue(C5.pi_hi == 6)
+        self.assertTrue(C5.name == "C5")
+
+        # User says the low is 5, but the minimum is actually different
+        self.assertTrue(C6.pi_lo == 5)
+        self.assertTrue(C6.pi_range == (5, 6))
+
+        D = c(*(self.D))
+        self.assertTrue(D.pi_numeric is True)
+
+        self.assertTrue(len(y) == 6)
+        self.assertTrue(y.name == "conversion [%]")
+
+    def test_column_math(self):
+        self.A = [-1, +1, -1, +1, 0, +1]
+        self.B = [-1, -1, +1, +1, 0, 0]
+        A = c(*(self.A))
+        B = c(*(self.B), name="B")
+        C1 = A * B
+        C2 = B * A
+
+        self.assertTrue(np.all(C1.values == [+1, -1, -1, +1, 0, 0]))
+        self.assertTrue(np.all(C1.index == A.index))
+        self.assertTrue(np.all(C2.values == [+1, -1, -1, +1, 0, 0]))
+        self.assertTrue(np.all(C2.index == A.index))
+
+    def test_gather(self):
+        A = c(*(self.A))
+        B = c(*(self.B))
+        y = c(*(self.y), name="conversion")
+
+        expt = gather(A=A, B=B, y=y)
+        self.assertTrue(expt.shape == (6, 3))
+
+        expt = gather(A=A, B=B, y=y, title="Testing expt name")
+
+        # Eventually this method must go to the "DF" class; currently in the
+        # model class; not really appropriate there.
+        self.assertTrue(expt.get_title() == "Testing expt name")
+
+
+class TestModels(unittest.TestCase):
+    def setUp(self):
+
+        A = c(-1, +1, -1, +1)
+        B = c(-1, -1, +1, +1)
+        C = A * B
+        y = c(41, 27, 35, 20, name="Stability", units="days")
+        self.expt = gather(A=A, B=B, C=C, y=y, title="Half-fraction, using C = A*B")
+        self.model_stability_poshalf = lm("y ~ A*B*C", self.expt)
+
+        """
+        Results from R:
+        A = c(-1, +1, -1, +1)
+        B = c(-1, -1, +1, +1)
+        C = A*B
+        y = c(41, 27, 35, 20)
+        data = data.frame(A=A, B=B, C=C, y=y)
+        model = lm(y ~ A*B*C, data=data)
+        summary(model)
+
+        Call:
+            lm(formula = y ~ A * B * C, data = data)
+
+            Residuals:
+            ALL 4 residuals are 0: no residual degrees of freedom!
+
+            Coefficients: (4 not defined because of singularities)
+                        Estimate Std. Error t value Pr(>|t|)
+            (Intercept)    30.75         NA      NA       NA
+            A              -7.25         NA      NA       NA
+            B              -3.25         NA      NA       NA
+            C              -0.25         NA      NA       NA
+            A:B               NA         NA      NA       NA
+            A:C               NA         NA      NA       NA
+            B:C               NA         NA      NA       NA
+            A:B:C             NA         NA      NA       NA
+
+            Residual standard error: NaN on 0 degrees of freedom
+            Multiple R-squared:      1,	Adjusted R-squared:    NaN
+            F-statistic:   NaN on 3 and 0 DF,  p-value: NA
+        """
+
+    def test_half_fraction(self):
+        """
+        Testing attributes for the half-fraction model.
+        """
+        self.assertTrue(self.model_stability_poshalf.nobs == 4)
+        self.assertTrue(self.model_stability_poshalf.df_resid == 0)
+        beta = self.model_stability_poshalf.get_parameters(drop_intercept=False)
+
+        self.assertAlmostEqual(beta["A"], -7.25)
+        self.assertAlmostEqual(beta["B"], -3.25)
+        self.assertAlmostEqual(beta["C"], -0.25)
+        self.assertAlmostEqual(beta["Intercept"], 30.75)
+        self.assertTrue(
+            self.model_stability_poshalf.get_aliases()
+            == ["A + B:C", "B + A:C", "C + A:B"]
+        )
+
+        for resid in self.model_stability_poshalf.residuals:
+            self.assertAlmostEqual(resid, 0.0)
+
+
+class Test_API_usage(unittest.TestCase):
+    def setUp(self):
+        full_path = os.path.join(
+            os.path.split(__file__)[0], "fixtures", "test_doe1.csv"
+        )
+        self.assertTrue(os.path.exists(full_path))
+        self.df1 = pd.read_csv(full_path)
+        self.df1.set_index("Run order", inplace=True)
+
+    def test_case_1(self):
+
+        index = self.df1.index
+        C = c(
+            self.df1["C"],
+            lo=self.df1["C"].min(),
+            hi=self.df1["C"].max(),
+            index=index,
+            name="C",
+        )
+        M = c(self.df1["M"], levels=self.df1["M"].unique(), name="M")
+        V = c(self.df1["V"], lo=self.df1["V"].min(), hi=self.df1["V"].max(), name="V")
+        B = c(self.df1["B"], name="B")
+        self.assertTrue(B.pi_levels["B"] == ["Ard", "Eme"])
+
+        y = self.df1["y"]
+
+        expt = gather(C=C, M=M, V=V, B=B, y=y)
+        self.assertTrue(np.all(C.index == M.index))
+
+        _ = lm("np.log10(y) ~ C*M*B*V", expt)
+        # summary(model)
+        # pareto_plot(model)
+        # contour_plot(model, C, M)
+        # contour_plot(model, "C", "M")
+        # predict_plot(model)
+
+    def test_aliasing(self):
+        d4 = c(24, 48, 36, 36, 60, units="hours", lo=24, high=48)
+        y4 = c(31, 65, 52, 54, 69)
+        expt4 = gather(d=d4, y=y4, title="RW units")
+        model4 = lm("y ~ d + I(np.power(d, 2))", data=expt4)
+        self.assertEqual(len(model4.aliasing), 0)
+
+        model5 = lm("y ~ d + I(np.power(d, 2))", data=expt4, alias_threshold=0.99)
+        self.assertEqual(len(model5.aliasing), 2)
+
+    def test_realworld_coded(self):
+        """
+        Test conversion between real-world and coded units.
+        """
+        c1 = c(2.5, 3, 2.5, 3, center=2.75, range=[2.5, 3], name="cement")
+        c1_coded = c1.to_coded()
+        self.assertListEqual(c1_coded.to_list(), [-1.0, 1.0, -1.0, 1.0])
+
+        c2 = c(
+            [-1.0, -1.0, +1.0, +1.0],
+            center=2.75,
+            range=[2.5, 3],
+            name="cement",
+            coded=True,
+        )
+        c2_rw = c2.to_realworld()
+        self.assertListEqual(c2_rw.to_list(), [2.5, 2.5, 3.0, 3.0])
+
+
+if __name__ == "__main__":
+    unittest.main()
