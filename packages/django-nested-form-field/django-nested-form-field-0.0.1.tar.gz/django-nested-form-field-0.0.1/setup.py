60833cf80058b7b2ea5db79964c57b3a1259b18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:58 +0000

    Add package django-nested-form-field/0.0.1/django-nested-form-field-0.0.1.tar.gz

---
+++ packages/django-nested-form-field/django-nested-form-field-0.0.1.tar.gz/django-nested-form-field-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="django-nested-form-field",
+    version="0.0.1",
+    description="nest django forms as fields in other forms",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Niels Lerche Sørensen",
+    url="https://github.com/ebanalyse/django-nested-form-field",
+    packages=["nested_form_field"],
+    install_requires=["django>=3.1"],  # external packages as dependencies
+)
