65a457173ba421f8268afbbac6f558d1d9c1c365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:20 +0000

    Add package sparse/0.12.0/sparse-0.12.0.tar.gz

---
+++ packages/sparse/sparse-0.12.0.tar.gz/sparse-0.12.0/docs/index.rst
@@ -0,0 +1,96 @@
+Sparse
+======
+
+.. image:: logo.png
+   :alt: Logo
+   :align: center
+   :width: 20em
+
+This implements sparse arrays of arbitrary dimension on top of :obj:`numpy` and :obj:`scipy.sparse`.
+It generalizes the :obj:`scipy.sparse.coo_matrix` and :obj:`scipy.sparse.dok_matrix` layouts,
+but extends beyond just rows and columns to an arbitrary number of dimensions.
+
+Additionally, this project maintains compatibility with the :obj:`numpy.ndarray` interface
+rather than the :obj:`numpy.matrix` interface used in :obj:`scipy.sparse`
+
+These differences make this project useful in certain situations
+where scipy.sparse matrices are not well suited,
+but it should not be considered a full replacement.
+It lacks layouts that are not easily generalized like CSR/CSC
+and depends on scipy.sparse for some computations.
+
+
+Motivation
+----------
+
+Sparse arrays, or arrays that are mostly empty or filled with zeros,
+are common in many scientific applications.
+To save space we often avoid storing these arrays in traditional dense formats,
+and instead choose different data structures.
+Our choice of data structure can significantly affect our storage and computational
+costs when working with these arrays.
+
+
+Design
+------
+
+The main data structure in this library follows the
+`Coordinate List (COO) <https://en.wikipedia.org/wiki/Sparse_matrix#Coordinate_list_(COO)>`_
+layout for sparse matrices, but extends it to multiple dimensions.
+
+The COO layout, which stores the row index, column index, and value of every element:
+
+=== === ====
+row col data
+=== === ====
+  0   0   10
+  0   2   13
+  1   3    9
+  3   8   21
+=== === ====
+
+It is straightforward to extend the COO layout to an arbitrary number of
+dimensions:
+
+==== ==== ==== === ====
+dim1 dim2 dim3 ... data
+==== ==== ==== === ====
+  0    0     0   .   10
+  0    0     3   .   13
+  0    2     2   .    9
+  3    1     4   .   21
+==== ==== ==== === ====
+
+This makes it easy to *store* a multidimensional sparse array, but we still
+need to reimplement all of the array operations like transpose, reshape,
+slicing, tensordot, reductions, etc., which can be challenging in general.
+
+Fortunately in many cases we can leverage the existing :obj:`scipy.sparse`
+algorithms if we can intelligently transpose and reshape our multi-dimensional
+array into an appropriate 2-d sparse matrix, perform a modified sparse matrix
+operation, and then reshape and transpose back.  These reshape and transpose
+operations can all be done at numpy speeds by modifying the arrays of
+coordinates.  After scipy.sparse runs its operations (often written in C) then
+we can convert back to using the same path of reshapings and transpositions in
+reverse.
+
+LICENSE
+-------
+
+This library is licensed under BSD-3
+
+.. toctree::
+   :maxdepth: 3
+   :hidden:
+
+   install
+   quickstart
+   construct
+   operations
+   generated/sparse
+   roadmap
+   contributing
+   changelog
+   conduct
+
+.. _scipy.sparse: https://docs.scipy.org/doc/scipy/reference/sparse.html
---
+++ packages/sparse/sparse-0.12.0.tar.gz/sparse-0.12.0/sparse/tests/test_dok.py
@@ -0,0 +1,266 @@
+import pytest
+
+import numpy as np
+
+import sparse
+from sparse import DOK
+from sparse._utils import assert_eq
+
+
+@pytest.mark.parametrize("shape", [(2,), (2, 3), (2, 3, 4)])
+@pytest.mark.parametrize("density", [0.1, 0.3, 0.5, 0.7])
+def test_random_shape_nnz(shape, density):
+    s = sparse.random(shape, density, format="dok")
+
+    assert isinstance(s, DOK)
+
+    assert s.shape == shape
+    expected_nnz = density * np.prod(shape)
+    assert np.floor(expected_nnz) <= s.nnz <= np.ceil(expected_nnz)
+
+
+def test_convert_to_coo():
+    s1 = sparse.random((2, 3, 4), 0.5, format="dok")
+    s2 = sparse.COO(s1)
+
+    assert_eq(s1, s2)
+
+
+def test_convert_from_coo():
+    s1 = sparse.random((2, 3, 4), 0.5, format="coo")
+    s2 = DOK(s1)
+
+    assert_eq(s1, s2)
+
+
+def test_convert_from_numpy():
+    x = np.random.rand(2, 3, 4)
+    s = DOK(x)
+
+    assert_eq(x, s)
+
+
+def test_convert_to_numpy():
+    s = sparse.random((2, 3, 4), 0.5, format="dok")
+    x = s.todense()
+
+    assert_eq(x, s)
+
+
+@pytest.mark.parametrize(
+    "shape, data",
+    [
+        (2, {0: 1}),
+        ((2, 3), {(0, 1): 3, (1, 2): 4}),
+        ((2, 3, 4), {(0, 1): 3, (1, 2, 3): 4, (1, 1): [6, 5, 4, 1]}),
+    ],
+)
+def test_construct(shape, data):
+    s = DOK(shape, data)
+    x = np.zeros(shape, dtype=s.dtype)
+
+    for c, d in data.items():
+        x[c] = d
+
+    assert_eq(x, s)
+
+
+@pytest.mark.parametrize("shape", [(2,), (2, 3), (2, 3, 4)])
+@pytest.mark.parametrize("density", [0.1, 0.3, 0.5, 0.7])
+def test_getitem_single(shape, density):
+    s = sparse.random(shape, density, format="dok")
+    x = s.todense()
+
+    for _ in range(s.nnz):
+        idx = np.random.randint(np.prod(shape))
+        idx = np.unravel_index(idx, shape)
+        print(idx)
+
+        assert np.isclose(s[idx], x[idx])
+
+
+@pytest.mark.parametrize(
+    "shape, density, indices",
+    [
+        ((2, 3), 0.5, (slice(1),)),
+        ((5, 5), 0.2, (slice(0, 4, 2),)),
+        ((10, 10), 0.2, (slice(5), slice(0, 10, 3))),
+        ((5, 5), 0.5, (slice(0, 4, 4), slice(0, 4, 4))),
+        ((5, 5), 0.4, (1, slice(0, 4, 1))),
+        ((10, 10), 0.8, ([0, 4, 5], [3, 2, 4])),
+        ((10, 10), 0, (slice(10), slice(10))),
+    ],
+)
+def test_getitem(shape, density, indices):
+    s = sparse.random(shape, density, format="dok")
+    x = s.todense()
+
+    sparse_sliced = s[indices]
+    dense_sliced = x[indices]
+
+    assert_eq(sparse_sliced.todense(), dense_sliced)
+
+
+@pytest.mark.parametrize(
+    "shape, density, indices",
+    [
+        ((10, 10), 0.8, ([0, 4, 5],)),
+        ((5, 5, 5), 0.5, ([1, 2, 3], [0, 2, 2])),
+    ],
+)
+def test_getitem_notimplemented_error(shape, density, indices):
+    s = sparse.random(shape, density, format="dok")
+
+    with pytest.raises(NotImplementedError):
+        s[indices]
+
+
+@pytest.mark.parametrize(
+    "shape, density, indices",
+    [
+        ((10, 10), 0.8, ([0, 4, 5], [0, 2])),
+        ((5, 5, 5), 0.5, ([1, 2, 3], [0], [2, 3, 4])),
+    ],
+)
+def test_getitem_index_error(shape, density, indices):
+    s = sparse.random(shape, density, format="dok")
+
+    with pytest.raises(IndexError):
+        s[indices]
+
+
+@pytest.mark.parametrize(
+    "shape, index, value",
+    [
+        ((2,), slice(None), np.random.rand()),
+        ((2,), slice(1, 2), np.random.rand()),
+        ((2,), slice(0, 2), np.random.rand(2)),
+        ((2,), 1, np.random.rand()),
+        ((2, 3), (0, slice(None)), np.random.rand()),
+        ((2, 3), (0, slice(1, 3)), np.random.rand()),
+        ((2, 3), (1, slice(None)), np.random.rand(3)),
+        ((2, 3), (0, slice(1, 3)), np.random.rand(2)),
+        ((2, 3), (0, slice(2, 0, -1)), np.random.rand(2)),
+        ((2, 3), (slice(None), 1), np.random.rand()),
+        ((2, 3), (slice(None), 1), np.random.rand(2)),
+        ((2, 3), (slice(1, 2), 1), np.random.rand()),
+        ((2, 3), (slice(1, 2), 1), np.random.rand(1)),
+        ((2, 3), (0, 2), np.random.rand()),
+        ((2, 3), ([0, 1], [1, 2]), np.random.rand(2)),
+        ((2, 3), ([0, 1], [1, 2]), np.random.rand()),
+        ((4,), ([1, 3]), np.random.rand()),
+        ((2, 3), ([0, 1], [1, 2]), 0),
+    ],
+)
+def test_setitem(shape, index, value):
+    s = sparse.random(shape, 0.5, format="dok")
+    x = s.todense()
+
+    s[index] = value
+    x[index] = value
+
+    assert_eq(x, s)
+
+
+@pytest.mark.parametrize(
+    "shape, index, value",
+    [
+        ((2, 3), ([0, 1.5], [1, 2]), np.random.rand()),
+        ((2, 3), ([0, 1], [1]), np.random.rand()),
+        ((2, 3), ([[0], [1]], [1, 2]), np.random.rand()),
+    ],
+)
+def test_setitem_index_error(shape, index, value):
+    s = sparse.random(shape, 0.5, format="dok")
+
+    with pytest.raises(IndexError):
+        s[index] = value
+
+
+@pytest.mark.parametrize(
+    "shape, index, value",
+    [
+        ((2, 3), ([0, 1],), np.random.rand()),
+    ],
+)
+def test_setitem_notimplemented_error(shape, index, value):
+    s = sparse.random(shape, 0.5, format="dok")
+
+    with pytest.raises(NotImplementedError):
+        s[index] = value
+
+
+@pytest.mark.parametrize(
+    "shape, index, value",
+    [
+        ((2, 3), ([0, 1], [1, 2]), np.random.rand(1, 2)),
+        ((2, 3), ([0, 1], [1, 2]), np.random.rand(3)),
+        ((2,), 1, np.random.rand(2)),
+    ],
+)
+def test_setitem_value_error(shape, index, value):
+    s = sparse.random(shape, 0.5, format="dok")
+
+    with pytest.raises(ValueError):
+        s[index] = value
+
+
+def test_default_dtype():
+    s = DOK((5,))
+
+    assert s.dtype == np.float64
+
+
+def test_int_dtype():
+    data = {1: np.uint8(1), 2: np.uint16(2)}
+
+    s = DOK((5,), data)
+
+    assert s.dtype == np.uint16
+
+
+def test_float_dtype():
+    data = {1: np.uint8(1), 2: np.float32(2)}
+
+    s = DOK((5,), data)
+
+    assert s.dtype == np.float32
+
+
+def test_set_zero():
+    s = DOK((1,), dtype=np.uint8)
+    s[0] = 1
+    s[0] = 0
+
+    assert s[0] == 0
+    assert s.nnz == 0
+
+
+@pytest.mark.parametrize("format", ["coo", "dok"])
+def test_asformat(format):
+    s = sparse.random((2, 3, 4), density=0.5, format="dok")
+    s2 = s.asformat(format)
+
+    assert_eq(s, s2)
+
+
+def test_coo_fv_interface():
+    s1 = sparse.full((5, 5), fill_value=1 + np.random.rand())
+    s2 = sparse.DOK(s1)
+    assert_eq(s1, s2)
+    s3 = sparse.COO(s2)
+    assert_eq(s1, s3)
+
+
+def test_empty_dok_dtype():
+    d = sparse.DOK(5, dtype=np.uint8)
+    s = sparse.COO(d)
+    assert s.dtype == d.dtype
+
+
+def test_zeros_like():
+    s = sparse.random((2, 3, 4), density=0.5)
+    s2 = sparse.zeros_like(s, format="dok")
+    assert s.shape == s2.shape
+    assert s.dtype == s2.dtype
+    assert isinstance(s2, sparse.DOK)
