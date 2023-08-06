2a98305237162437cf0aabe3bceee70b3924efb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:11 +0000

    Add package tplink-cloud-api/2.2.0/tplink-cloud-api-2.2.0.tar.gz

---
+++ packages/tplink-cloud-api/tplink-cloud-api-2.2.0.tar.gz/tplink-cloud-api-2.2.0/tplinkcloud/hs300_child.py
@@ -0,0 +1,40 @@
+from .device_type import TPLinkDeviceType
+from .emeter_device import TPLinkEMeterDevice
+
+
+class HS300ChildAction:
+    def __init__(self, child_action):
+        self.type = child_action.get('type')
+        self.schd_sec = child_action.get('schd_sec')
+        self.action = child_action.get('action')
+
+
+class HS300ChildSysInfo:
+
+    def __init__(self, child_info):
+        self.id = child_info.get('id')
+        self.state = child_info.get('state')
+        self.alias = child_info.get('alias')
+        self.on_time = child_info.get('on_time')
+        self.next_action = HS300ChildAction(child_info.get('next_action'))
+
+
+class HS300Child(TPLinkEMeterDevice):
+
+    def __init__(self, client, parent_device_id, child_device_id, device_info):
+        super().__init__(
+            client,
+            parent_device_id,
+            device_info,
+            child_id=child_device_id
+        )
+        self.model_type = TPLinkDeviceType.HS300CHILD
+
+    def get_sys_info(self):
+        sys_info = self._get_sys_info()
+        
+        if not sys_info:
+            print("Something went wrong with your request; please try again")
+            return None
+        
+        return HS300ChildSysInfo(sys_info)
