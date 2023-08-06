2aaa6135580967e52da7900bfe24b40a922fa953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:02 +0000

    Add package s4f/2.6/s4f-2.6.tar.gz

---
+++ packages/s4f/s4f-2.6.tar.gz/s4f-2.6/setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup_args = {
+    'name': 's4f',
+    'version': '2.6',
+    'author': 'John Lewis',
+    'author_email': "john.lewis@takealot.com",
+}
+
+setup(**setup_args)
