d84701bb48fa0ef69bed12ede7a95f77a222c4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:29 +0000

    Add package libknot/3.0.5/libknot-3.0.5.tar.gz

---
+++ packages/libknot/libknot-3.0.5.tar.gz/libknot-3.0.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+from os import path
+here = path.dirname (path.realpath (__file__))
+
+setuptools.setup(
+    name='libknot',
+    version='3.0.5',
+    description='Python bindings for libknot',
+    author='Daniel Salzman',
+    author_email='daniel.salzman@nic.cz',
+    url='https://gitlab.nic.cz/knot/knot-dns',
+    license='GPL-3.0',
+    packages=['libknot'],
+    package_dir = {
+        'libknot': path.join (here, 'libknot'),
+    },
+    classifiers=[ # See https://pypi.org/classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+    ]
+)
