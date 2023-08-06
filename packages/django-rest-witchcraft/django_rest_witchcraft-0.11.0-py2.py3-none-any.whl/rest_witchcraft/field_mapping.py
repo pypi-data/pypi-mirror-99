76853d842ae1eedd36cc180e273426d74b1d8de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:11 +0000

    Add package django-rest-witchcraft/0.11.0/django_rest_witchcraft-0.11.0-py2.py3-none-any.whl

---
+++ packages/django-rest-witchcraft/django_rest_witchcraft-0.11.0-py2.py3-none-any.whl/rest_witchcraft/field_mapping.py
@@ -0,0 +1,95 @@
+"""Field mapping from SQLAlchemy type's to DRF fields."""
+import datetime
+import decimal
+
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql import sqltypes
+
+from django_sorcery.db import meta
+
+from rest_framework import fields
+
+from rest_enumfield import EnumField
+
+from .fields import CharMappingField
+
+
+def get_detail_view_name(model):
+    """Given a model class, return the view name to use for URL relationships
+    that rever to instances of the model."""
+    # TODO split camel case
+    return "{}-detail".format(model.__name__.lower())
+
+
+def get_url_kwargs(model):
+    """Gets kwargs for the UriField."""
+    info = meta.model_info(model)
+    lookup_field = list(info.primary_keys.keys())[0]
+
+    field_kwargs = {
+        "read_only": True,
+        "view_name": get_detail_view_name(model),
+        "lookup_field": lookup_field,
+        "lookup_url_kwarg": "pk",
+    }
+
+    return field_kwargs
+
+
+SERIALIZER_FIELD_MAPPING = {
+    # sqlalchemy types
+    postgresql.HSTORE: CharMappingField,
+    sqltypes.Enum: EnumField,
+    # python types
+    datetime.date: fields.DateField,
+    datetime.datetime: fields.DateTimeField,
+    datetime.time: fields.TimeField,
+    datetime.timedelta: fields.DurationField,
+    decimal.Decimal: fields.DecimalField,
+    float: fields.FloatField,
+    int: fields.IntegerField,
+    str: fields.CharField,
+}
+
+try:  # pragma: no cover
+    from sqlalchemy_utils import types
+
+    SERIALIZER_FIELD_MAPPING[types.IPAddressType] = fields.IPAddressField
+    SERIALIZER_FIELD_MAPPING[types.UUIDType] = fields.UUIDField
+    SERIALIZER_FIELD_MAPPING[types.URLType] = fields.URLField
+except ImportError:  # pragma: no cover
+    pass
+
+
+def get_field_type(column):
+    """Returns the field type to be used determined by the sqlalchemy column
+    type or the column type's python type."""
+    if isinstance(column.type, sqltypes.Enum) and not column.type.enum_class:
+        return fields.ChoiceField
+
+    if isinstance(column.type, postgresql.ARRAY):
+        child_field = SERIALIZER_FIELD_MAPPING.get(column.type.item_type.__class__) or SERIALIZER_FIELD_MAPPING.get(
+            column.type.item_type.python_type
+        )
+
+        if child_field is None:
+            raise KeyError("Could not figure out field for ARRAY item type '{}'".format(column.type.__class__))
+
+        class ArrayField(fields.ListField):
+            """Nested array field for PostreSQL's ARRAY type."""
+
+            def __init__(self, *args, **kwargs):
+                kwargs["child"] = child_field()
+                super().__init__(*args, **kwargs)
+
+        return ArrayField
+
+    if column.type.__class__ in SERIALIZER_FIELD_MAPPING:
+        return SERIALIZER_FIELD_MAPPING.get(column.type.__class__)
+
+    if issubclass(column.type.python_type, bool):
+        return fields.NullBooleanField if column.nullable else fields.BooleanField
+
+    for typ in column.type.python_type.mro():
+        if typ in SERIALIZER_FIELD_MAPPING:
+            return SERIALIZER_FIELD_MAPPING.get(typ)
---
+++ packages/django-rest-witchcraft/django_rest_witchcraft-0.11.0-py2.py3-none-any.whl/rest_witchcraft/generics.py
@@ -0,0 +1,66 @@
+from sqlalchemy.exc import InvalidRequestError
+
+from django.http import Http404
+
+from django_sorcery.db.meta import model_info
+from django_sorcery.utils import suppress
+
+from rest_framework import generics
+
+
+class GenericAPIView(generics.GenericAPIView):
+    """Base class for sqlalchemy specific views."""
+
+    @classmethod
+    def get_model(cls):
+        """Returns the model class."""
+        model = None
+
+        with suppress(AttributeError, InvalidRequestError):
+            model = cls.queryset._only_full_mapper_zero("get").class_
+
+        if model:
+            return model
+
+        with suppress(AttributeError):
+            model = cls.serializer_class.Meta.model
+
+        assert model is not None, (
+            "Couldn't figure out the model for {viewset} attribute, either provide a"
+            "queryset or a serializer with a Meta.model".format(viewset=cls.__name__)
+        )
+
+        return model
+
+    def get_session(self):
+        """Returns the session."""
+        queryset = self.get_queryset()
+        return queryset.session
+
+    def get_object(self):
+        """Returns the object the view is displaying.
+
+        We ignore the `lookup_field` and `lookup_url_kwarg` values only
+        when tere are multiple primary keys
+        """
+        queryset = self.get_queryset()
+        model = self.get_model()
+        info = model_info(model)
+        kwargs = self.kwargs.copy()
+
+        # we want to honor DRF lookup_field and lookup_url_kwarg API
+        # but only if they are defined and there is single primary key.
+        # When there are multiple, all bets are off so we restrict url kwargs
+        # to model column names
+        if len(info.primary_keys) == 1:
+            lookup_field = self.lookup_field
+            lookup_url_kwarg = self.lookup_url_kwarg or lookup_field
+
+            kwargs[lookup_field] = kwargs.pop(lookup_url_kwarg)
+
+        obj = queryset.get(info.primary_keys_from_dict(kwargs))
+
+        if not obj:
+            raise Http404("No %s matches the given query." % model.__name__)
+
+        return obj
