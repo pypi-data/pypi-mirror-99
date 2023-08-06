14bcab774fa1147f670d70a1251c8d8d892177a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:12 +0000

    Add package django-backend-auth-api/0.2.1/django-backend-auth-api-0.2.1.tar.gz

---
+++ packages/django-backend-auth-api/django-backend-auth-api-0.2.1.tar.gz/django-backend-auth-api-0.2.1/README.rst
@@ -0,0 +1,28 @@
+================
+BACKEND-AUTH-API
+================
+
+BACKEND-AUTH-API is a app to manage your authentication backend api with integrated doc.
+
+Detailed documentation is in the "docs" directory.
+
+Quick start
+-----------
+
+1. Add "authapi" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'authapi',
+    ]
+
+2. Include the authapi URLconf in your project urls.py like this::
+
+    path('authapi/', include('authapi.urls')),
+
+3. Run `python manage.py migrate` to create the authapi models.
+
+4. Start the development server and visit http://127.0.0.1:8000/admin/
+   to see docs (you'll need the Admin app enabled).
+
+5. Visit http://127.0.0.1:8000/authapi/ to see all urls related to  a backend authentication.
---
+++ packages/django-backend-auth-api/django-backend-auth-api-0.2.1.tar.gz/django-backend-auth-api-0.2.1/authapi/admin.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+
+# Register your models here.
+# Import our models module.
+from . import models
+from authapi.models import *
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+
+# Register our "Message" model with the Django Admin/
+
+
+# class UserAdmin(admin.ModelAdmin):
+#     readonly_fields = ('date_joined',)
+#     list_display = ('email', )
+    
+class UserResource(resources.ModelResource):
+
+    class Meta:
+        model = User
+
+class UserAdmin(ImportExportModelAdmin):
+    readonly_fields = ('date_joined',)
+    list_display = ('email', )
+    resource_class = UserResource
+    
+
+admin.site.register(User,  UserAdmin)
+
