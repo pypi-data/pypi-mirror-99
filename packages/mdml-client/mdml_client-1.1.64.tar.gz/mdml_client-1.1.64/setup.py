ca5b2011539b2b92019b1687b6cef443a2652be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:06 +0000

    Add package mdml-client/1.1.64/mdml_client-1.1.64.tar.gz

---
+++ packages/mdml-client/mdml_client-1.1.64.tar.gz/mdml_client-1.1.64/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "mdml_client",
+    version = "1.1.64",
+    author = "Jakob Elias",
+    author_email = "jelias@anl.gov",
+    description = "Client to connect to the MDML",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/anl-mdml/MDML_Client",
+    packages = setuptools.find_packages(),
+    install_requires=[
+        "opencv-python",
+        "pandas",
+        "funcx",
+        "paho-mqtt",
+        "boto3",
+	"matplotlib"
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
