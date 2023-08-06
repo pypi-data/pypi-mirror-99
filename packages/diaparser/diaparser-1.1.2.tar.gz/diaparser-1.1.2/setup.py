ed1d16e7ca85a9bcea253a74f30158eba648ec22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:38 +0000

    Add package diaparser/1.1.2/diaparser-1.1.2.tar.gz

---
+++ packages/diaparser/diaparser-1.1.2.tar.gz/diaparser-1.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='diaparser',
+    version='1.1.2',
+    author='Yu Zhang, Giuseppe Attardi',
+    author_email='yzhang.cs@outlook.com, attardi@di.unipi.it',
+    description='Direct Attentive Dependency Parser',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Unipisa/diaparser',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Text Processing :: Linguistic'
+    ],
+    setup_requires=[
+        'setuptools>=18.0',
+    ],
+    install_requires=['torch>=1.6', 'transformers', 'nltk', 'stanza', 'numpy==1.19.4'],
+    entry_points={
+        'console_scripts': [
+            'diaparser=diaparser.cmds.biaffine_dependency:main',
+        ]
+    },
+    python_requires='>=3.6',
+    zip_safe=False
+)
