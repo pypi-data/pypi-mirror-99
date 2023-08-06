1f152f257b4e422f310d0aca14a23e9726310074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:23 +0000

    Add package bifrostlib/2.1.10/bifrostlib-2.1.10.tar.gz

---
+++ packages/bifrostlib/bifrostlib-2.1.10.tar.gz/bifrostlib-2.1.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bifrostlib',
+    version='2.1.10',
+    description='Datahandling functions for bifrost (later to be API interface)',
+    url='https://github.com/ssi-dk/bifrostlib',
+    author="Kim Ng, Martin Basterrechea",
+    author_email="kimn@ssi.dk",
+    packages=find_packages(),
+    install_requires=[
+        'pymongo', 
+        'python-magic', 
+        'dnspython', 
+        'jsmin', 
+        'warlock',
+        'pandas',
+        'libmagic',
+    ],
+    package_data={"bifrostlib": ['./schemas/bifrost.jsonc']},
+    include_package_data=True
+    )
