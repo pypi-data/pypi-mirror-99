1cf4c25560d09393a2f776f0d136840280b6da62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:04 +0000

    Add package pyimporters-plugins/0.1.11/pyimporters-plugins-0.1.11.tar.gz

---
+++ packages/pyimporters-plugins/pyimporters-plugins-0.1.11.tar.gz/pyimporters-plugins-0.1.11/pyimporters_plugins/rdf.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from typing import Type, Dict, Any
+
+from pydantic import BaseModel, Field
+from pyimporters_plugins.base import KnowledgeParserBase, KnowledgeParserOptions, Concept
+
+class RDFFormat(str, Enum):
+    xml = 'xml'
+    n3 = 'n3'
+    turtle = 'turtle'
+    nt = 'nt'
+
+class RDFOptions(KnowledgeParserOptions):
+    """
+    Options for the RDF knowledge import
+    """
+    rdf_format: RDFFormat = Field(RDFFormat.xml, description="RDF format")
+
+
+class RDFKnowledgeParser(KnowledgeParserBase):
+    def parse(self, source, options : Dict[str,Any]):
+        options = RDFOptions(**options)
+        # for name, value in sorted(source.items()):
+        #     concept : Concept = Concept(identifier=name, prefLabel=value)
+        yield Concept(identifier="http://kt.com#1", prefLabel="foo", altLabels=["Foo", "fooo"])
+        yield Concept(identifier="http://kt.com#2", prefLabel="bar", altLabels=["BAR", "baar"])
+
+    @classmethod
+    def get_schema(cls) -> Type[BaseModel]:
+        return RDFOptions
