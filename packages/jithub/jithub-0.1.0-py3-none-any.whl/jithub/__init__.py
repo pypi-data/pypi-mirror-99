9201233d23cac1b52415e977506db832a58c8953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:18 +0000

    Add package jithub/0.1.0/jithub-0.1.0-py3-none-any.whl

---
+++ packages/jithub/jithub-0.1.0-py3-none-any.whl/jithub/__init__.py
@@ -0,0 +1,10 @@
+"""
+JITHUB models
+"""
+#import .models
+from .models.model_classes import IzhiModel, MATModel, ADEXPModel
+from .models.backends import izhikevich
+from .models.backends import mat_nu
+from .models.backends import adexp
+
+#from .backends import izhikevich
