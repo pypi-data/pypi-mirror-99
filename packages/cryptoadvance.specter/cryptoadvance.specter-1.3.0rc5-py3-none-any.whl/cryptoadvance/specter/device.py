8320fa6862593316ef034c2c1d2b14556931c132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:44 +0000

    Add package cryptoadvance.specter/1.3.0rc5/cryptoadvance.specter-1.3.0rc5-py3-none-any.whl

---
+++ packages/cryptoadvance.specter/cryptoadvance.specter-1.3.0rc5-py3-none-any.whl/cryptoadvance/specter/device.py
@@ -0,0 +1,138 @@
+import json
+from .key import Key
+from .persistence import read_json_file, write_json_file
+import logging
+from .helpers import is_testnet
+
+logger = logging.getLogger()
+
+
+class Device:
+    device_type = None  # this is saved to json
+    name = "Unknown device"  # this is how device appears in UI
+    icon = "other_icon.svg"
+
+    # override these vars to add support
+    # of different communication methods
+    sd_card_support = False
+    qr_code_support = False
+    qr_code_support_verify = False
+    hwi_support = False
+    supports_hwi_toggle_passphrase = False
+    supports_hwi_multisig_display_address = False
+    hot_wallet = False
+
+    def __init__(self, name, alias, keys, fullpath, manager):
+        """
+        From child classes call super().__init__ and also set
+        support for communication methods
+        """
+        self.name = name
+        self.alias = alias
+        self.keys = keys
+        self.fullpath = fullpath
+        self.manager = manager
+
+    def create_psbts(self, base64_psbt, wallet):
+        """
+        Overwrite this method for a device.
+        Possible keys:
+        hwi, qrcode, sdcard
+        """
+        return {}
+
+    @classmethod
+    def from_json(cls, device_dict, manager, default_alias="", default_fullpath=""):
+        name = device_dict["name"] if "name" in device_dict else ""
+        alias = device_dict["alias"] if "alias" in device_dict else default_alias
+        keys = [Key.from_json(key_dict) for key_dict in device_dict["keys"]]
+        fullpath = (
+            device_dict["fullpath"] if "fullpath" in device_dict else default_fullpath
+        )
+        return cls(name, alias, keys, fullpath, manager)
+
+    @property
+    def json(self):
+        return {
+            "name": self.name,
+            "alias": self.alias,
+            "type": self.device_type,
+            "keys": [key.json for key in self.keys],
+            "fullpath": self.fullpath,
+        }
+
+    def _update_keys(self):
+        write_json_file(self.json, self.fullpath)
+        self.manager.update()
+
+    def remove_key(self, key):
+        self.keys = [k for k in self.keys if k != key]
+        self._update_keys()
+
+    def add_keys(self, keys):
+        for key in keys:
+            if key not in self.keys:
+                self.keys.append(key)
+        self._update_keys()
+
+    def rename(self, new_name):
+        logger.info("Renaming {}".format(self.alias))
+        self.name = new_name
+        write_json_file(self.json, self.fullpath)
+        self.manager.update()
+
+    def wallets(self, wallet_manager):
+        if wallet_manager is None:
+            return []
+        wallets = []
+        for wallet in wallet_manager.wallets.values():
+            if self in wallet.devices:
+                wallets.append(wallet)
+        return wallets
+
+    def set_type(self, device_type):
+        self.device_type = device_type
+
+        write_json_file(self.json, self.fullpath)
+        self.manager.update()
+
+    def key_types(self, network="main"):
+        test = is_testnet(network)
+        return [key.key_type for key in self.keys if (key.is_testnet == test)]
+
+    def has_key_types(self, wallet_type, network="main"):
+        if wallet_type == "multisig":
+            for key_type in self.key_types(network):
+                if key_type in ["", "sh-wsh", "wsh"]:
+                    return True
+        elif wallet_type == "simple":
+            for key_type in self.key_types(network):
+                if key_type in ["", "sh-wpkh", "wpkh"]:
+                    return True
+        return "" in self.key_types(network)
+
+    def no_key_found_reason(self, wallet_type, network="main"):
+        if self.has_key_types(wallet_type, network=network):
+            return ""
+        reverse_network = "main" if is_testnet(network) else "test"
+        if wallet_type == "multisig":
+            for key_type in self.key_types(reverse_network):
+                if key_type in ["", "sh-wsh", "wsh"]:
+                    return "Multisig compatible keys were found, but for the wrong network, make sure to add keys for the right network."
+        elif wallet_type == "simple":
+            for key_type in self.key_types(reverse_network):
+                if key_type in ["", "sh-wpkh", "wpkh"]:
+                    return "Single-sig compatible keys were found, but for the wrong network, make sure to add keys for the right network.".format(
+                        "Single key" if wallet_type == "simple" else "Multisig"
+                    )
+        return "No keys found with the correct derivation type for a {} wallet.".format(
+            "single key" if wallet_type == "simple" else "multisig"
+        )
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.alias == other.alias
+
+    def __hash__(self):
+        return hash(self.alias)
---
+++ packages/cryptoadvance.specter/cryptoadvance.specter-1.3.0rc5-py3-none-any.whl/cryptoadvance/specter/user.py
@@ -0,0 +1,172 @@
+import os
+import shutil
+import hashlib
+import binascii
+import json
+from flask_login import UserMixin
+from .specter_error import SpecterError
+from .persistence import read_json_file, write_json_file, delete_folder
+
+
+def hash_password(password):
+    """Hash a password for storing."""
+    salt = binascii.b2a_base64(hashlib.sha256(os.urandom(60)).digest()).strip()
+    pwdhash = (
+        binascii.b2a_base64(
+            hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), salt, 10000)
+        )
+        .strip()
+        .decode()
+    )
+    return {"salt": salt.decode(), "pwdhash": pwdhash}
+
+
+def verify_password(stored_password, provided_password):
+    """Verify a stored password against one provided by user"""
+    pwdhash = hashlib.pbkdf2_hmac(
+        "sha256",
+        provided_password.encode("utf-8"),
+        stored_password["salt"].encode(),
+        10000,
+    )
+    return pwdhash == binascii.a2b_base64(stored_password["pwdhash"])
+
+
+class User(UserMixin):
+    def __init__(self, id, username, password, config, specter, is_admin=False):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.config = config
+        self.is_admin = is_admin
+        self.specter = specter
+        self.wallet_manager = None
+        self.device_manager = None
+        self.manager = None
+
+    @property
+    def folder_id(self):
+        if self.is_admin:
+            return ""
+        return f"_{self.id}"
+
+    @classmethod
+    def from_json(cls, user_dict, specter):
+        # TODO: Unify admin in backwards compatible way
+        try:
+            if not user_dict["is_admin"]:
+                return cls(
+                    user_dict["id"],
+                    user_dict["username"],
+                    user_dict["password"],
+                    user_dict["config"],
+                    specter,
+                )
+            else:
+                return cls(
+                    user_dict["id"],
+                    user_dict["username"],
+                    user_dict["password"],
+                    {},
+                    specter,
+                    is_admin=True,
+                )
+        except:
+            raise SpecterError("Unable to parse user JSON.")
+
+    @property
+    def json(self):
+        user_dict = {
+            "id": self.id,
+            "username": self.username,
+            "password": self.password,
+            "is_admin": self.is_admin,
+        }
+        if not self.is_admin:
+            user_dict["config"] = self.config
+        return user_dict
+
+    def save_info(self, delete=False):
+        if self.manager is None:
+            self.manager = self.specter.user_manager
+        users = self.manager.users
+        existing = self in users
+
+        # update specter users
+        if not existing and not delete:
+            self.specter.add_user(self)
+        if existing and delete:
+            self.specter.delete_user(self)
+        self.manager.save()
+
+    def set_explorer(self, explorer_id, explorer):
+        if "explorers" not in self.config:
+            self.config["explorers"] = (
+                {"main": "", "test": "", "regtest": "", "signet": ""},
+            )
+        self.config["explorers"][self.specter.chain] = explorer
+        if "explorer_id" not in self.config:
+            self.config["explorer_id"] = {
+                "main": "CUSTOM",
+                "test": "CUSTOM",
+                "regtest": "CUSTOM",
+                "signet": "CUSTOM",
+            }
+        self.config["explorer_id"][self.specter.chain] = explorer_id
+        self.save_info()
+
+    def set_fee_estimator(self, fee_estimator, custom_url):
+        self.config["fee_estimator"] = fee_estimator
+        if fee_estimator == "custom":
+            self.config["fee_estimator_custom_url"] = custom_url
+        self.save_info()
+
+    def set_hwi_bridge_url(self, url):
+        self.config["hwi_bridge_url"] = url
+        self.save_info()
+
+    def set_unit(self, unit):
+        self.config["unit"] = unit
+        self.save_info()
+
+    def set_price_check(self, price_check_bool):
+        self.config["price_check"] = price_check_bool
+        self.save_info()
+
+    def set_price_provider(self, price_provider):
+        self.config["price_provider"] = price_provider
+        self.save_info()
+
+    def set_weight_unit(self, weight_unit):
+        self.config["weight_unit"] = weight_unit
+        self.save_info()
+
+    def set_alt_rate(self, alt_rate):
+        self.config["alt_rate"] = alt_rate
+        self.save_info()
+
+    def set_alt_symbol(self, alt_symbol):
+        self.config["alt_symbol"] = alt_symbol
+        self.save_info()
+
+    def delete(self):
+        # we delete wallet manager and device manager in save_info
+        self.save_info(delete=True)
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.id == other
+        return self.id == other.id
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        # to make lookups in dicts by user id
+        return hash(self.id)
+
+    def __str__(self):
+        return self.id
+
+    def __repr__(self):
+        return f"User({self.id})"
