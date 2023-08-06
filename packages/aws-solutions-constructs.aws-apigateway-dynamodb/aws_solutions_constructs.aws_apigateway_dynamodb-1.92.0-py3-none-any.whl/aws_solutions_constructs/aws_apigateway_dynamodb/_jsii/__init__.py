54b0be04d39e3f57e7c566db121c5176d00d3d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:43 +0000

    Add package aws-solutions-constructs.aws-apigateway-dynamodb/1.92.0/aws_solutions_constructs.aws_apigateway_dynamodb-1.92.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-apigateway-dynamodb/aws_solutions_constructs.aws_apigateway_dynamodb-1.92.0-py3-none-any.whl/aws_solutions_constructs/aws_apigateway_dynamodb/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_apigateway._jsii
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-apigateway-dynamodb",
+    "1.92.0",
+    __name__[0:-6],
+    "aws-apigateway-dynamodb@1.92.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
