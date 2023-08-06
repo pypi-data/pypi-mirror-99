8bb7e94af3c1469e3c48c65d7dd75c0e61e57137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:26 +0000

    Add package JsonReader-heureka-code/0.0.3/JsonReader_heureka_code-0.0.3-py3-none-any.whl

---
+++ packages/JsonReader-heureka-code/JsonReader_heureka_code-0.0.3-py3-none-any.whl/JsonReader_heureka_code/__init__.py
@@ -0,0 +1,7 @@
+from .__jsonReader import JsonReader
+from .__jsonReader import __doc__ as JsonReader__doc__
+
+__doc__ = JsonReader__doc__
+__author__ = "heureka-code"
+__maintainer__ = "heureka-code"
+__version__ = "0.0.3"
