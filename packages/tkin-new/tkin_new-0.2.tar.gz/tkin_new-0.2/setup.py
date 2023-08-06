7a5e4eae549a77cdc9a12f11454589f98575ef98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package tkin-new/0.2/tkin_new-0.2.tar.gz

---
+++ packages/tkin-new/tkin_new-0.2.tar.gz/tkin_new-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='tkin_new',
+      version='0.2',
+      description='tki',
+      url='',
+      author='klmsathish',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      author_email='klmsathish@gmail.com',
+      keywords=['tkinter'])
