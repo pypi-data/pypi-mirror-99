106733bd15848c7423f5e2748bf98f420b76d2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:12 +0000

    Add package undetected-chromedriver/2.2.0/undetected-chromedriver-2.2.0.tar.gz

---
+++ packages/undetected-chromedriver/undetected-chromedriver-2.2.0.tar.gz/undetected-chromedriver-2.2.0/setup.py
@@ -0,0 +1,40 @@
+"""
+
+         888                                                  888         d8b
+         888                                                  888         Y8P
+         888                                                  888
+ .d8888b 88888b.  888d888 .d88b.  88888b.d88b.   .d88b.   .d88888 888d888 888 888  888  .d88b.  888d888
+d88P"    888 "88b 888P"  d88""88b 888 "888 "88b d8P  Y8b d88" 888 888P"   888 888  888 d8P  Y8b 888P"
+888      888  888 888    888  888 888  888  888 88888888 888  888 888     888 Y88  88P 88888888 888
+Y88b.    888  888 888    Y88..88P 888  888  888 Y8b.     Y88b 888 888     888  Y8bd8P  Y8b.     888
+ "Y8888P 888  888 888     "Y88P"  888  888  888  "Y8888   "Y88888 888     888   Y88P    "Y8888  888   88888888
+
+BY ULTRAFUNKAMSTERDAM (https://github.com/ultrafunkamsterdam)"""
+
+from setuptools import setup
+
+
+setup(
+    name="undetected-chromedriver",
+    version="2.2.0",
+    packages=["undetected_chromedriver"],
+    install_requires=["selenium",],
+    url="https://github.com/ultrafunkamsterdam/undetected-chromedriver",
+    license="GPL-3.0",
+    author="UltrafunkAmsterdam",
+    author_email="info@blackhat-security.nl",
+    description="""\
+    selenium.webdriver.Chrome replacement with focus on stealth.
+    not triggered by Distil / CloudFlare / Imperva / DataDome / hCaptcha and such.
+    
+    NOTE: results may vary due to many factors. No guarantees are given, except for ongoing efforts in understanding detection algorithms.
+    """,
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
+
