3e89e2b67dba794e78c24f889ccb2e8df045ea08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:04 +0000

    Add package pippin-wallet/1.1.20/pippin-wallet-1.1.20.tar.gz

---
+++ packages/pippin-wallet/pippin-wallet-1.1.20.tar.gz/pippin-wallet-1.1.20/CHANGELOG.md
@@ -0,0 +1,122 @@
+# Changelog
+
+## [1.1.20] - 2021-03-24
+
+- Only receive cemented blocks
+
+## [1.1.19] - 2021-03-14
+
+- Support work_generate action, supports 'subtype' to automatically choose correct difficulty
+- Subscribe to active_difficulty websocket for dynamic PoW
+
+## [1.1.18] - 2021-02-23
+
+- Fix server hanging when DPoW/BPoW is unavailable
+- Fix auto receives working for unopened accounts
+
+## [1.1.17] - 2020-08-20
+
+- Fix transactions made to accounts with xrb_ prefix
+- Fix pyyaml dependency
+
+## [1.1.16] - 2020-08-17
+
+- Fix backwards compatibility
+
+## [1.1.15] - 2020-08-17
+
+- Support Nano v21 PoW Difficulty
+
+## [1.1.13] - 2020-08-09
+
+- Require aioredlock 0.3.0
+
+## [1.1.12] - 2020-05-19
+
+- Fix pending RPC when receive_minimum is < 1
+
+## [1.1.10] - 2020-05-05
+
+- Remove non-voting default rep
+
+## [1.1.9] - 2020-05-04
+
+- Windows compatibility
+
+## [1.1.7] - 2020-04-27
+
+- Reduce log spam
+
+## [1.1.6] - 2020-04-27
+
+- Fix difficulty adjustment
+
+## [1.1.5] - 2020-04-26
+
+- Keep sane difficulty levels in DPoW and BPoW requests
+
+## [1.1.4] - 2020-04-22
+
+- Bump tortoise-orm minimum version to 0.15.24
+
+## [1.1.3] - 2020-04-20
+
+- Don't generate sample config if config.yaml exists already
+
+## [1.1.2] - 2020-04-20
+
+- Add log_to_stdout option
+- Changes for docker environments
+
+## [1.1.1] - 2020-04-18
+
+- Fix ipv6 rpc/work URLs
+
+## [1.1.0] - 2020-04-15
+
+- Make requirements less strict
+- Fix aiohttp TCPConnector in some cases
+
+## [1.0.8] - 2019-12-16
+
+- Fix update_existing with wallet_representative_set when accounts aren't open
+- Add CLI commands: wallet_destroy, wallet_representative_set, wallet_representative_get
+- Add setuptools dependency
+
+## [1.0.7] - 2019-12-14
+
+- (Fix) Compatibility with PyYAML < 5.1
+
+## [1.0.6] - 2019-12-14
+
+- Compatibility with PyYAML < 5.1
+
+## [1.0.5] - 2019-12-14
+
+- Add support for dynamic PoW (requires websocket configured)
+- Fix issues related to PyYAML dependency
+
+## [1.0.4] - 2019-12-13
+
+- Minor bug fixes
+
+## [1.0.3] - 2019-12-13
+
+- Add support for Python 3.6
+
+## [1.0.2] - 2019-12-13
+
+- Restructure project for pypi compatibility
+
+## [1.0.1] - 2019-12-12
+
+- Fix an issue with multiple work peers
+- Add [Distributed PoW](https://dpow.nanocenter.org) and [BoomPoW support](https://bpow.banano.cc)
+- Add account_create to CLI
+- Fix issues with closing websockets when Pippin stopped
+- Use `subtype` when sending RPC process to the node
+- Fix so RPC send/receive/account_rep_set return the exact same response as node wallet
+
+## [1.0.0] - 2019-12-10
+
+- Initial Release
---
+++ packages/pippin-wallet/pippin-wallet-1.1.20.tar.gz/pippin-wallet-1.1.20/pippin/network/work_client.py
@@ -0,0 +1,153 @@
+import socket
+from logging import config
+from typing import List
+
+import aiohttp
+import asyncio
+import pippin.config as config
+import nanopy
+import os
+import rapidjson as json
+
+from pippin.db.redis import RedisDB
+from pippin.network.dpow_websocket import ConnectionClosed, DpowClient
+from pippin.util.nano_util import NanoUtil
+
+class WorkClient(object):
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError('Call instance() instead')
+
+    @classmethod
+    def instance(cls) -> 'WorkClient':
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+            cls.work_urls = config.Config.instance().work_peers
+            if config.Config.instance().node_work_generate:
+                cls.work_urls.append(config.Config.instance().node_url)                
+            cls.session = aiohttp.ClientSession(json_serialize=json.dumps)
+            cls.dpow_client = None
+            cls.dpow_futures = {}
+            cls.dpow_id = 1
+            # Construct DPoW Client
+            cls.dpow_user = os.getenv('DPOW_USER', None)
+            cls.dpow_key = os.getenv('DPOW_KEY', None)
+            if cls.dpow_user is not None and cls.dpow_key is not None:
+                cls.dpow_client = DpowClient(
+                    cls.dpow_user,
+                    cls.dpow_key,
+                    work_futures=cls.dpow_futures,
+                    bpow=False
+                )
+                cls.dpow_fallback_url = 'https://dpow.nanocenter.org/service/'
+            else:
+                cls.dpow_user = os.getenv('BPOW_USER', None)
+                cls.dpow_key = os.getenv('BPOW_KEY', None)
+                if cls.dpow_user is not None and cls.dpow_key is not None:
+                    cls.dpow_client = DpowClient(
+                        cls.dpow_user,
+                        cls.dpow_key,
+                        work_futures=cls.dpow_futures,
+                        bpow=True
+                    )
+                    cls.dpow_fallback_url = 'https://bpow.banano.cc/service/'
+
+        return cls._instance
+
+    @classmethod
+    async def close(cls):
+        if hasattr(cls, 'session') and cls.session is not None:
+            await cls.session.close()
+        if cls._instance is not None:
+            cls._instance = None
+
+    async def make_request(self, url: str, req_json: dict):
+        async with self.session.post(url ,json=req_json, timeout=300) as resp:
+            return await resp.json()
+
+    async def work_generate(self, hash: str, difficulty: str) -> str:
+        work_generate = {
+            'action': 'work_generate',
+            'hash': hash,
+            'difficulty': difficulty
+        }
+
+        # Build work_generate requests
+        tasks = []
+        for p in self.work_urls:
+            tasks.append(self.make_request(p, work_generate))
+
+        # Use DPoW if applicable
+        if self.dpow_client is not None:
+            dpow_id = str(self.dpow_id)
+            self.dpow_id += 1
+            self.dpow_futures[dpow_id] = asyncio.get_event_loop().create_future()
+            try:
+                success = await self.dpow_client.request_work(dpow_id, hash, difficulty=difficulty)
+                tasks.append(self.dpow_futures[dpow_id])
+            except ConnectionClosed:
+                # HTTP fallback for this request
+                dp_req = {
+                    "user": self.dpow_user,
+                    "api_key": self.dpow_key,
+                    "hash": hash,
+                    "difficulty": difficulty
+                }
+                tasks.append(self.make_request(self.dpow_fallback_url, dp_req))
+
+        # Do it locally if no peers or if peers have been failing
+        if await RedisDB.instance().exists("work_failure") or (len(self.work_urls) == 0 and self.dpow_client is None):
+            tasks.append(
+                NanoUtil.instance().work_generate(hash, difficulty=difficulty)
+            )
+
+        # Post work_generate to all peers simultaneously
+        final_result = None
+        while len(tasks) > 0:
+            # Fire all tasks simultaneously and fire when first one is completed
+            done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED, timeout=100)
+            # Set to True if any tasks completed before the timeout
+            has_done = len(done) > 0
+            try:
+                for task in done:
+                    result = task.result()
+                    # Some tasks return different types of responses, e.g. DPoW is different than a normal work peer
+                    if result is None:
+                        aiohttp.log.server_logger.info("work_generate task returned None")
+                    if isinstance(result, list):
+                        result = json.loads(result[1])
+                    elif isinstance(result, str):
+                        result = {'work':result}
+                    if result is not None and 'work' in result:
+                        cancel_json = {
+                            'action': 'work_cancel',
+                            'hash': hash
+                        }
+                        for p in self.work_urls:
+                            asyncio.ensure_future(self.make_request(p, cancel_json))
+                        final_result = result['work']
+                    elif result is not None and 'error' in result:
+                        aiohttp.log.server_logger.info(f'work_generate task returned error {result["error"]}')
+            except Exception:
+                aiohttp.log.server_logger.exception("work_generate task raised Exception")
+            finally:
+                # Either nothing finished before the timeout or something did
+                # Cancel any pending tasks
+                if not has_done or final_result is not None or len(pending) == 0:
+                    for p in pending:
+                        try:
+                            p.cancel()
+                        except Exception:
+                            pass
+                    break
+                elif len(pending) > 0:
+                    # wait again for pending tasks
+                    tasks = pending
+
+        if final_result is not None:
+            return final_result
+
+        # IF we're still here then all requests failed, set failure flag
+        await RedisDB.instance().set(f"work_failure", "aa", expires=300)
+        return await NanoUtil.instance().work_generate(hash, difficulty=difficulty)
