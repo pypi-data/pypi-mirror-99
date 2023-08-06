9432277bf9d588d91e622a09cb76ab28f270b9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:38 +0000

    Add package simple-playgrounds/0.9.23/simple_playgrounds-0.9.23-py3-none-any.whl

---
+++ packages/simple-playgrounds/simple_playgrounds-0.9.23-py3-none-any.whl/simple_playgrounds/agents/__init__.py
@@ -0,0 +1,2 @@
+from simple_playgrounds.agents.agents import BaseAgent, HeadAgent, HeadEyeAgent,\
+    FullAgent, TurretAgent
---
+++ packages/simple-playgrounds/simple_playgrounds-0.9.23-py3-none-any.whl/simple_playgrounds/playgrounds/collection/__init__.py
@@ -0,0 +1,2 @@
+from simple_playgrounds.playgrounds.collection.rl.basic import *
+from simple_playgrounds.playgrounds.collection.test.test_scene_elements import *
