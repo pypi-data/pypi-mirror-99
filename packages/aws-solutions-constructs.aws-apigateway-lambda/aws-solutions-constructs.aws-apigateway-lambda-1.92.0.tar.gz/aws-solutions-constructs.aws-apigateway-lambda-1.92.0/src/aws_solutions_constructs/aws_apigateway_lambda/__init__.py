5138c2b4c32285b97692c31673b8887984d28bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:48 +0000

    Add package aws-solutions-constructs.aws-apigateway-lambda/1.92.0/aws-solutions-constructs.aws-apigateway-lambda-1.92.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-apigateway-lambda/aws-solutions-constructs.aws-apigateway-lambda-1.92.0.tar.gz/aws-solutions-constructs.aws-apigateway-lambda-1.92.0/src/aws_solutions_constructs/aws_apigateway_lambda/__init__.py
@@ -0,0 +1,281 @@
+'''
+# aws-apigateway-lambda module
+
+<!--BEGIN STABILITY BANNER-->---
+
+
+![Stability: Experimental](https://img.shields.io/badge/stability-Experimental-important.svg?style=for-the-badge)
+
+> All classes are under active development and subject to non-backward compatible changes or removal in any
+> future version. These are not subject to the [Semantic Versioning](https://semver.org/) model.
+> This means that while you may use them, you may need to update your source code when upgrading to a newer version of this package.
+
+---
+<!--END STABILITY BANNER-->
+
+| **Reference Documentation**:| <span style="font-weight: normal">https://docs.aws.amazon.com/solutions/latest/constructs/</span>|
+|:-------------|:-------------|
+
+<div style="height:8px"></div>
+
+| **Language**     | **Package**        |
+|:-------------|-----------------|
+|![Python Logo](https://docs.aws.amazon.com/cdk/api/latest/img/python32.png) Python|`aws_solutions_constructs.aws_apigateway_lambda`|
+|![Typescript Logo](https://docs.aws.amazon.com/cdk/api/latest/img/typescript32.png) Typescript|`@aws-solutions-constructs/aws-apigateway-lambda`|
+|![Java Logo](https://docs.aws.amazon.com/cdk/api/latest/img/java32.png) Java|`software.amazon.awsconstructs.services.apigatewaylambda`|
+
+## Overview
+
+This AWS Solutions Construct implements an Amazon API Gateway REST API connected to an AWS Lambda function pattern.
+
+Here is a minimal deployable pattern definition in Typescript:
+
+```python
+# Example automatically generated without compilation. See https://github.com/aws/jsii/issues/826
+from aws_solutions_constructs.aws_apigateway_lambda import ApiGatewayToLambda
+
+ApiGatewayToLambda(self, "ApiGatewayToLambdaPattern",
+    lambda_function_props=FunctionProps(
+        runtime=lambda_.Runtime.NODEJS_10_X,
+        handler="index.handler",
+        code=lambda_.Code.from_asset(f"{__dirname}/lambda")
+    )
+)
+```
+
+## Initializer
+
+```text
+new ApiGatewayToLambda(scope: Construct, id: string, props: ApiGatewayToLambdaProps);
+```
+
+*Parameters*
+
+* scope [`Construct`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.Construct.html)
+* id `string`
+* props [`ApiGatewayToLambdaProps`](#pattern-construct-props)
+
+## Pattern Construct Props
+
+| **Name**     | **Type**        | **Description** |
+|:-------------|:----------------|-----------------|
+|existingLambdaObj?|[`lambda.Function`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.Function.html)|Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.|
+|lambdaFunctionProps?|[`lambda.FunctionProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.FunctionProps.html)|User provided props to override the default props for the Lambda function.|
+|apiGatewayProps?|[`api.LambdaRestApiProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-apigateway.LambdaRestApi.html)|Optional user-provided props to override the default props for the API.|
+|logGroupProps?|[`logs.LogGroupProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-logs.LogGroupProps.html)|User provided props to override the default props for for the CloudWatchLogs LogGroup.|
+
+## Pattern Properties
+
+| **Name**     | **Type**        | **Description** |
+|:-------------|:----------------|-----------------|
+|lambdaFunction|[`lambda.Function`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.Function.html)|Returns an instance of the Lambda function created by the pattern.|
+|apiGateway|[`api.LambdaRestApi`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-apigateway.LambdaRestApi.html)|Returns an instance of the API Gateway REST API created by the pattern.|
+|apiGatewayCloudWatchRole|[`iam.Role`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-iam.Role.html)|Returns an instance of the iam.Role created by the construct for API Gateway for CloudWatch access.|
+|apiGatewayLogGroup|[`logs.LogGroup`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-logs.LogGroup.html)|Returns an instance of the LogGroup created by the construct for API Gateway access logging to CloudWatch.|
+
+## Default settings
+
+Out of the box implementation of the Construct without any override will set the following defaults:
+
+### Amazon API Gateway
+
+* Deploy an edge-optimized API endpoint
+* Enable CloudWatch logging for API Gateway
+* Configure least privilege access IAM role for API Gateway
+* Set the default authorizationType for all API methods to IAM
+* Enable X-Ray Tracing
+
+### AWS Lambda Function
+
+* Configure limited privilege access IAM role for Lambda function
+* Enable reusing connections with Keep-Alive for NodeJs Lambda function
+* Enable X-Ray Tracing
+* Set Environment Variables
+
+  * AWS_NODEJS_CONNECTION_REUSE_ENABLED (for Node 10.x and higher functions)
+
+## Architecture
+
+![Architecture Diagram](architecture.png)
+
+---
+
+
+© Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+import aws_cdk.aws_apigateway
+import aws_cdk.aws_iam
+import aws_cdk.aws_lambda
+import aws_cdk.aws_logs
+import aws_cdk.core
+
+
+class ApiGatewayToLambda(
+    aws_cdk.core.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-solutions-constructs/aws-apigateway-lambda.ApiGatewayToLambda",
+):
+    '''
+    :summary: The ApiGatewayToLambda class.
+    '''
+
+    def __init__(
+        self,
+        scope: aws_cdk.core.Construct,
+        id: builtins.str,
+        *,
+        api_gateway_props: typing.Any = None,
+        existing_lambda_obj: typing.Optional[aws_cdk.aws_lambda.Function] = None,
+        lambda_function_props: typing.Optional[aws_cdk.aws_lambda.FunctionProps] = None,
+        log_group_props: typing.Optional[aws_cdk.aws_logs.LogGroupProps] = None,
+    ) -> None:
+        '''
+        :param scope: - represents the scope for all the resources.
+        :param id: - this is a a scope-unique id.
+        :param api_gateway_props: Optional user-provided props to override the default props for the API. Default: - Default props are used.
+        :param existing_lambda_obj: Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored. Default: - None
+        :param lambda_function_props: User provided props to override the default props for the Lambda function. Default: - Default props are used.
+        :param log_group_props: User provided props to override the default props for the CloudWatchLogs LogGroup. Default: - Default props are used
+
+        :access: public
+        :since: 0.8.0
+        :summary: Constructs a new instance of the ApiGatewayToLambda class.
+        '''
+        props = ApiGatewayToLambdaProps(
+            api_gateway_props=api_gateway_props,
+            existing_lambda_obj=existing_lambda_obj,
+            lambda_function_props=lambda_function_props,
+            log_group_props=log_group_props,
+        )
+
+        jsii.create(ApiGatewayToLambda, self, [scope, id, props])
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="apiGateway")
+    def api_gateway(self) -> aws_cdk.aws_apigateway.RestApi:
+        return typing.cast(aws_cdk.aws_apigateway.RestApi, jsii.get(self, "apiGateway"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="apiGatewayCloudWatchRole")
+    def api_gateway_cloud_watch_role(self) -> aws_cdk.aws_iam.Role:
+        return typing.cast(aws_cdk.aws_iam.Role, jsii.get(self, "apiGatewayCloudWatchRole"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="apiGatewayLogGroup")
+    def api_gateway_log_group(self) -> aws_cdk.aws_logs.LogGroup:
+        return typing.cast(aws_cdk.aws_logs.LogGroup, jsii.get(self, "apiGatewayLogGroup"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="lambdaFunction")
+    def lambda_function(self) -> aws_cdk.aws_lambda.Function:
+        return typing.cast(aws_cdk.aws_lambda.Function, jsii.get(self, "lambdaFunction"))
+
+
+@jsii.data_type(
+    jsii_type="@aws-solutions-constructs/aws-apigateway-lambda.ApiGatewayToLambdaProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "api_gateway_props": "apiGatewayProps",
+        "existing_lambda_obj": "existingLambdaObj",
+        "lambda_function_props": "lambdaFunctionProps",
+        "log_group_props": "logGroupProps",
+    },
+)
+class ApiGatewayToLambdaProps:
+    def __init__(
+        self,
+        *,
+        api_gateway_props: typing.Any = None,
+        existing_lambda_obj: typing.Optional[aws_cdk.aws_lambda.Function] = None,
+        lambda_function_props: typing.Optional[aws_cdk.aws_lambda.FunctionProps] = None,
+        log_group_props: typing.Optional[aws_cdk.aws_logs.LogGroupProps] = None,
+    ) -> None:
+        '''The properties for the ApiGatewayToLambda class.
+
+        :param api_gateway_props: Optional user-provided props to override the default props for the API. Default: - Default props are used.
+        :param existing_lambda_obj: Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored. Default: - None
+        :param lambda_function_props: User provided props to override the default props for the Lambda function. Default: - Default props are used.
+        :param log_group_props: User provided props to override the default props for the CloudWatchLogs LogGroup. Default: - Default props are used
+        '''
+        if isinstance(lambda_function_props, dict):
+            lambda_function_props = aws_cdk.aws_lambda.FunctionProps(**lambda_function_props)
+        if isinstance(log_group_props, dict):
+            log_group_props = aws_cdk.aws_logs.LogGroupProps(**log_group_props)
+        self._values: typing.Dict[str, typing.Any] = {}
+        if api_gateway_props is not None:
+            self._values["api_gateway_props"] = api_gateway_props
+        if existing_lambda_obj is not None:
+            self._values["existing_lambda_obj"] = existing_lambda_obj
+        if lambda_function_props is not None:
+            self._values["lambda_function_props"] = lambda_function_props
+        if log_group_props is not None:
+            self._values["log_group_props"] = log_group_props
+
+    @builtins.property
+    def api_gateway_props(self) -> typing.Any:
+        '''Optional user-provided props to override the default props for the API.
+
+        :default: - Default props are used.
+        '''
+        result = self._values.get("api_gateway_props")
+        return typing.cast(typing.Any, result)
+
+    @builtins.property
+    def existing_lambda_obj(self) -> typing.Optional[aws_cdk.aws_lambda.Function]:
+        '''Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.
+
+        :default: - None
+        '''
+        result = self._values.get("existing_lambda_obj")
+        return typing.cast(typing.Optional[aws_cdk.aws_lambda.Function], result)
+
+    @builtins.property
+    def lambda_function_props(
+        self,
+    ) -> typing.Optional[aws_cdk.aws_lambda.FunctionProps]:
+        '''User provided props to override the default props for the Lambda function.
+
+        :default: - Default props are used.
+        '''
+        result = self._values.get("lambda_function_props")
+        return typing.cast(typing.Optional[aws_cdk.aws_lambda.FunctionProps], result)
+
+    @builtins.property
+    def log_group_props(self) -> typing.Optional[aws_cdk.aws_logs.LogGroupProps]:
+        '''User provided props to override the default props for the CloudWatchLogs LogGroup.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("log_group_props")
+        return typing.cast(typing.Optional[aws_cdk.aws_logs.LogGroupProps], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "ApiGatewayToLambdaProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "ApiGatewayToLambda",
+    "ApiGatewayToLambdaProps",
+]
+
+publication.publish()
