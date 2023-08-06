724efd94b7e6a64da69582f1946fac317d49b958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:23 +0000

    Add package skillsnetwork/0.2/skillsnetwork-0.2.tar.gz

---
+++ packages/skillsnetwork/skillsnetwork-0.2.tar.gz/skillsnetwork-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='skillsnetwork',
+    version='0.2',
+    license='BSD',
+    author='Bradley Steinfeld',
+    author_email='bs@ibm.com',
+    url='http://vision.skills.network',
+    long_description="README.txt",
+    packages=['skillsnetwork'],
+    install_requires=['requests'],
+    description="Skills Network",
+)
