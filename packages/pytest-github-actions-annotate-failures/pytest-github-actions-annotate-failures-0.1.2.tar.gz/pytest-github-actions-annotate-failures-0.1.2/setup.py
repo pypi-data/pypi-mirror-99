a783962ad47e846b8366be03d5908e3947784b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:41 +0000

    Add package pytest-github-actions-annotate-failures/0.1.2/pytest-github-actions-annotate-failures-0.1.2.tar.gz

---
+++ packages/pytest-github-actions-annotate-failures/pytest-github-actions-annotate-failures-0.1.2.tar.gz/pytest-github-actions-annotate-failures-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+
+with open("./README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="pytest-github-actions-annotate-failures",
+    version="0.1.2",
+    description="pytest plugin to annotate failed tests with a workflow command for GitHub Actions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="utgwkk",
+    author_email="utagawakiki@gmail.com",
+    url="https://github.com/utgwkk/pytest-github-actions-annotate-failures",
+    license="MIT",
+    classifiers=["Framework :: Pytest",],
+    packages=find_packages(),
+    entry_points={
+        "pytest11": [
+            "pytest_github_actions_annotate_failures = pytest_github_actions_annotate_failures.plugin",
+        ],
+    },
+    install_requires=["pytest>=4.0.0",],
+)
