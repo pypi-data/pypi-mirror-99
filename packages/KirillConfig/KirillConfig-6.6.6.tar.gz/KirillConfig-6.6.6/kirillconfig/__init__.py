dd515603b8dd6423a881c7a1986e8a168e3dc0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:57 +0000

    Add package KirillConfig/6.6.6/KirillConfig-6.6.6.tar.gz

---
+++ packages/KirillConfig/KirillConfig-6.6.6.tar.gz/KirillConfig-6.6.6/kirillconfig/__init__.py
@@ -0,0 +1,7 @@
+from .configKirill import phrase_list_male
+from .configKirill import phrase_list_female
+from .configKirill import pozor_list
+from .configKirill import ban_list
+from .configKirill import hmtai_categories
+from .configKirill import hc_len
+from .configKirill import list_len
---
+++ packages/KirillConfig/KirillConfig-6.6.6.tar.gz/KirillConfig-6.6.6/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='KirillConfig',
+      version='6.6.6',
+      description='Kirill config YEE',
+      packages=['kirillconfig'],
+      author_email='fantiktop212@gmail.com',
+      zip_safe=False)
