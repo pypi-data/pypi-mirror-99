a86a3596890b90f247988fbba1bdd193f3a63568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:13 +0000

    Add package ocrmypdf/11.7.3/ocrmypdf-11.7.3-py36-none-any.whl

---
+++ packages/ocrmypdf/ocrmypdf-11.7.3-py36-none-any.whl/ocrmypdf/__init__.py
@@ -0,0 +1,31 @@
+# © 2017 James R. Barlow: github.com/jbarlow83
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from pluggy import HookimplMarker as _HookimplMarker
+
+from ocrmypdf import helpers, hocrtransform, leptonica, pdfa, pdfinfo
+from ocrmypdf._jobcontext import PageContext, PdfContext
+from ocrmypdf._version import PROGRAM_NAME, __version__
+from ocrmypdf.api import Verbosity, configure_logging, ocr
+from ocrmypdf.exceptions import (
+    BadArgsError,
+    DpiError,
+    EncryptedPdfError,
+    ExitCode,
+    ExitCodeException,
+    InputFileError,
+    MissingDependencyError,
+    OutputFileAccessError,
+    PdfMergeFailedError,
+    PriorOcrFoundError,
+    SubprocessOutputError,
+    TesseractConfigError,
+    UnsupportedImageFormatError,
+)
+from ocrmypdf.pluginspec import OcrEngine, OrientationConfidence
+
+hookimpl = _HookimplMarker('ocrmypdf')
