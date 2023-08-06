e8988686c6888f7273cd042871394c1dd379a0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:10 +0000

    Add package dagster-gcp/0.11.1rc0/dagster_gcp-0.11.1rc0-py3-none-any.whl

---
+++ packages/dagster-gcp/dagster_gcp-0.11.1rc0-py3-none-any.whl/dagster_gcp/__init__.py
@@ -0,0 +1,34 @@
+from dagster.core.utils import check_dagster_package_version
+
+from .bigquery.resources import bigquery_resource
+from .bigquery.solids import (
+    bq_create_dataset,
+    bq_delete_dataset,
+    bq_solid_for_queries,
+    import_df_to_bq,
+    import_file_to_bq,
+    import_gcs_paths_to_bq,
+)
+from .bigquery.types import BigQueryError
+from .dataproc.resources import dataproc_resource
+from .dataproc.solids import dataproc_solid
+from .gcs import GCSFileHandle, gcs_file_manager, gcs_resource
+from .version import __version__
+
+check_dagster_package_version("dagster-gcp", __version__)
+
+__all__ = [
+    "BigQueryError",
+    "GCSFileHandle",
+    "bigquery_resource",
+    "bq_create_dataset",
+    "bq_delete_dataset",
+    "bq_solid_for_queries",
+    "dataproc_resource",
+    "dataproc_solid",
+    "gcs_resource",
+    "gcs_file_manager",
+    "import_df_to_bq",
+    "import_file_to_bq",
+    "import_gcs_paths_to_bq",
+]
