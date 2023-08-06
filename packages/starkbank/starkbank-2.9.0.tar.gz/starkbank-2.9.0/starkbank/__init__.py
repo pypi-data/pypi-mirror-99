cbd9757fec384fee1607f682d4f6bb4917a2fb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:26 +0000

    Add package starkbank/2.9.0/starkbank-2.9.0.tar.gz

---
+++ packages/starkbank/starkbank-2.9.0.tar.gz/starkbank-2.9.0/starkbank/__init__.py
@@ -0,0 +1,59 @@
+version = "2.9.0"
+
+user = None
+language = "en-US"
+timeout = 15
+
+from .user.__organization import Organization
+from .user.__project import Project
+
+from . import transaction
+from .transaction.__transaction import Transaction
+
+from . import balance
+from .balance.__balance import Balance
+
+from . import boleto
+from .boleto.__boleto import Boleto
+
+from . import transfer
+from .transfer.__transfer import Transfer
+
+from . import boletoholmes
+from .boletoholmes.__boletoholmes import BoletoHolmes
+
+from . import brcodepayment
+from .brcodepayment.__brcodepayment import BrcodePayment
+
+from . import brcodepreview
+from .brcodepreview.__brcodepreview import BrcodePreview
+
+from . import boletopayment
+from .boletopayment.__boletopayment import BoletoPayment
+
+from . import utilitypayment
+from .utilitypayment.__utilitypayment import UtilityPayment
+
+from . import webhook
+from .webhook.__webhook import Webhook
+
+from . import workspace
+from .workspace.__workspace import Workspace
+
+from . import event
+from .event.__event import Event
+
+from . import paymentrequest
+from .paymentrequest.__paymentrequest import PaymentRequest
+
+from . import invoice
+from .invoice.__invoice import Invoice
+
+from . import dictkey
+from .dictkey.__dictkey import DictKey
+
+from . import deposit
+from .deposit.__deposit import Deposit
+
+from . import error
+from . import key
