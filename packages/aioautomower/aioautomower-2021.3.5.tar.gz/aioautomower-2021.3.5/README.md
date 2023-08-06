f81835880cdcc7577031723ce7b4695567967f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:42 +0000

    Add package aioautomower/2021.3.5/aioautomower-2021.3.5.tar.gz

---
+++ packages/aioautomower/aioautomower-2021.3.5.tar.gz/aioautomower-2021.3.5/README.md
@@ -0,0 +1,3 @@
+# husqvarna_automower_py
+
+Package to communicate with the Automower Connect API
---
+++ packages/aioautomower/aioautomower-2021.3.5.tar.gz/aioautomower-2021.3.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aioautomower",
+    version="2021.3.5",
+    author="Thomas Protzner",
+    author_email="thomas.protzner@gmail.com",
+    description="module to communicate to Husqvarna Automower API",
+    license="Apache License 2.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Thomas55555/husqvarna_automower_py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=list(val.strip() for val in open("requirements.txt")),
+)
