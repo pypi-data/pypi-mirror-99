5d2df8cfddd84856c1c1b19c753bb58ecd9a66b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:28 +0000

    Add package package-cb65/0.0.1/package_cb65-0.0.1-py3-none-any.whl

---
+++ packages/package-cb65/package_cb65-0.0.1-py3-none-any.whl/package_cb65/__init__.py
@@ -0,0 +1,7 @@
+"""
+__init__
+========
+"""
+__version__ = "1.0.0"
+
+from .impl import build
---
+++ packages/package-cb65/package_cb65-0.0.1-py3-none-any.whl/package_cb65/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+# Import
+
+from package_cb65 import build
+
+
+def main():
+    build()
+
+
+# Execution
+
+if __name__ == "__main__":
+    main()
