57ba96c129171dc91021fe90ec7f24e6439465c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:54 +0000

    Add package encourage/0.0.0.6/encourage-0.0.0.6.tar.gz

---
+++ packages/encourage/encourage-0.0.0.6.tar.gz/encourage-0.0.0.6/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup,find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.MD'), encoding='utf-8').read()
+
+setup(
+    name = "encourage",
+    version = "0.0.0.6",
+
+    author = "lindaye",
+    author_email = "454784911@qq.com",
+    url = "https://github.com/stellaye/encourage",
+    license = "MIT",
+
+    description = "A command line tool speak beautiful words to you to encourage you ",
+    long_description = README,
+    long_description_content_type="text/markdown",
+    packages=find_packages("encourage"),
+    install_requires=[
+     "pyttsx3"
+    ],
+    platforms = 'any',
+    zip_safe = True,
+    include_package_data = True,
+    entry_points = {'console_scripts': [
+            'e = encourage.main:main',
+        ]}
+)
