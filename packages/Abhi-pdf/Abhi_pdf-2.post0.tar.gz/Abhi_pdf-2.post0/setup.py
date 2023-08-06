878a395ecde4071e5fdc6925923214f6efcdc9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:08 +0000

    Add package Abhi-pdf/2.post0/Abhi_pdf-2.post0.tar.gz

---
+++ packages/Abhi-pdf/Abhi_pdf-2.post0.tar.gz/Abhi_pdf-2.post0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Abhi_pdf',
+    version='2-0',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages()
+)
