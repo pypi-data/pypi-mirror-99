756601078e9f39f99950d1604b48180e1d4e2d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:17 +0000

    Add package tplink-cloud-api/2.4.2/tplink_cloud_api-2.4.2-py3-none-any.whl

---
+++ packages/tplink-cloud-api/tplink_cloud_api-2.4.2-py3-none-any.whl/tplinkcloud/device_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class TPLinkDeviceType(Enum):
+    HS100 = 100
+    HS103 = 103
+    HS105 = 105
+    HS110 = 110
+    HS300 = 300
+    HS300CHILD = 3000
+    KP115 = 115
+    KP303 = 303
+    KP303CHILD = 3030
+    UNKNOWN = 9999
