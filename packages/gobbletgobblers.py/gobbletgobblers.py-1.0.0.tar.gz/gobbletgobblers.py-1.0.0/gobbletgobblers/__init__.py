af19b42d948f08a1be8ac488fbb55a01bc1b44d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:22 +0000

    Add package gobbletgobblers.py/1.0.0/gobbletgobblers.py-1.0.0.tar.gz

---
+++ packages/gobbletgobblers.py/gobbletgobblers.py-1.0.0.tar.gz/gobbletgobblers.py-1.0.0/gobbletgobblers/__init__.py
@@ -0,0 +1,7 @@
+from __main__ import *
+
+__copyright__    = 'Copyright (C) 2021 Vel Aomi'
+__version__      = '1.0.0'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Aomi Vel / 碧海ベル'
+__url__          = 'http://github.com/account/repository'
