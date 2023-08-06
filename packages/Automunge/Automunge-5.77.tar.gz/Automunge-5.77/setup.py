f24e5342a2aaf94429b0b316f2d3d9e6206c6c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:00 +0000

    Add package Automunge/5.77/Automunge-5.77.tar.gz

---
+++ packages/Automunge/Automunge-5.77.tar.gz/Automunge-5.77/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Automunge",
+    version="5.77",
+    author="Nicholas Teague",
+    author_email="pitg888@gmail.com",
+    description="platform for preparing tabular data for machine learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Automunge/AutoMunge",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'pandas', 'scikit-learn', 'scipy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
