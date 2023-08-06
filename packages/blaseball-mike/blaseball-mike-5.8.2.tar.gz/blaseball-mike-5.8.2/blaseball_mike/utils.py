a1f8b14066e46cd5a422b3691ca56d20b20df256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:30 +0000

    Add package blaseball-mike/5.8.2/blaseball-mike-5.8.2.tar.gz

---
+++ packages/blaseball-mike/blaseball-mike-5.8.2.tar.gz/blaseball-mike-5.8.2/blaseball_mike/utils.py
@@ -0,0 +1,64 @@
+"""Misc utils"""
+
+
+def print_stlats(*players, headers=None):
+    """
+    Pretty print the stlats for the given list of players
+    """
+    headers = headers or [
+        'base_thirst',
+        'continuation',
+        'ground_friction',
+        'indulgence',
+        'laserlikeness',
+        'divinity',
+        'martyrdom',
+        'moxie',
+        'musclitude',
+        'patheticism',
+        'thwackability',
+        'tragicness',
+        'anticapitalism',
+        'chasiness',
+        'omniscience',
+        'tenaciousness',
+        'watchfulness',
+        'coldness',
+        'overpowerment',
+        'ruthlessness',
+        'shakespearianism',
+        'unthwackability',
+        'buoyancy',
+        'cinnamon',
+        'deceased',
+        'peanut_allergy',
+        'pressurization',
+        'soul',
+        'total_fingers',
+    ]
+
+    # hey wanna see something messed up
+    print('name        ' + ('{:<6}' * len(headers)).format(*[h[:4] for h in headers]))
+    for player in players:
+        print('{:<12}'.format(player.name[:10]) + ('{:<6.2f}' * len(headers)).format(*[float(getattr(player, h)) for h in headers]))
+
+
+def csv_format(*models, headers=None):
+    """
+    Transforms an arbitrary list of models into a list of lists for easy export, ie to CSV.
+    By default, will extract all headers from the given models' json, but specific headers can be given with
+    the `headers` param as a list.
+    """
+    blobs = [model.json() for model in models]
+    if not headers:
+        # extract from models
+        headers = []
+        for blob in blobs:
+            for key in blob:
+                if key not in headers:
+                    headers.append(key)
+
+    res = [headers]
+    for model in models:
+        res.append([getattr(model, header, None) for header in headers])
+    return res
