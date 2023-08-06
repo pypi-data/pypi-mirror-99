b272dc313ee12433972bd0c877b38c85227b2594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:29 +0000

    Add package cnspy-trajectory-evaluation/0.1.0/cnspy_trajectory_evaluation-0.1.0-py3-none-any.whl

---
+++ packages/cnspy-trajectory-evaluation/cnspy_trajectory_evaluation-0.1.0-py3-none-any.whl/cnspy_trajectory_evaluation/TrajectoryAlignmentTypes.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# Software License Agreement (GNU GPLv3  License)
+#
+# Copyright (c) 2020, Roland Jung (roland.jung@aau.at) , AAU, KPK, NAV
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Requirements:
+# numpy, enum, cnspy_trajectory, cnspy_trajectory_evaluation
+########################################################################################################################
+import numpy as np
+from enum import Enum
+
+from cnspy_trajectory.Trajectory import Trajectory
+from cnspy_trajectory_evaluation.SpatialAlignment import SpatialAlignement
+
+
+class TrajectoryAlignmentTypes(Enum):
+    sim3 = 'sim3'
+    se3 = 'se3'
+    posyaw = 'posyaw'
+    pos = 'pos'
+    none = 'none'
+
+    def __str__(self):
+        return self.value
+
+    @staticmethod
+    def trajectory_aligment(traj_est, traj_gt, method='sim3', num_frames=-1):
+        """
+        calculate s, R, t so that:
+            gt = R * s * est + t
+        method can be: sim3, se3, posyaw, none;
+        n_aligned: -1 means using all the frames
+
+        """
+        assert (isinstance(traj_est, Trajectory))
+        assert (isinstance(traj_gt, Trajectory))
+
+        p_es = traj_est.p_vec
+        q_es = traj_est.q_vec
+        p_gt = traj_gt.p_vec
+        q_gt = traj_gt.q_vec
+
+        p_es, p_gt, q_es, q_gt
+        assert p_es.shape[1] == 3
+        assert p_gt.shape[1] == 3
+        assert q_es.shape[1] == 4
+        assert q_gt.shape[1] == 4
+        assert p_es.shape[0] == p_gt.shape[0]
+        assert q_es.shape[0] == q_gt.shape[0]
+
+        s = 1
+        R = np.identity(3)
+        t = np.zeros((3,))
+
+        # TODO: hackish, but it is somehow an Enum bug!
+        method = TrajectoryAlignmentTypes(str(method))
+        if method == TrajectoryAlignmentTypes.sim3:
+            assert num_frames >= 2 or num_frames == -1, "sim3 uses at least 2 frames"
+            s, R, t = SpatialAlignement.align_SIM3(p_es, p_gt, num_frames)
+        elif method == TrajectoryAlignmentTypes.se3:
+            R, t = SpatialAlignement.align_SE3(p_es, p_gt, q_es, q_gt, num_frames)
+        elif method == TrajectoryAlignmentTypes.posyaw:
+            R, t = SpatialAlignement.align_position_yaw(p_es, p_gt, q_es, q_gt, num_frames)
+        elif method == TrajectoryAlignmentTypes.pos:
+            R, t = SpatialAlignement.align_SE3(p_es, p_gt, q_es, q_gt, num_frames)
+            R = np.identity(3)
+        elif method == TrajectoryAlignmentTypes.none:
+            R = np.identity(3)
+            t = np.zeros((3,))
+        else:
+            assert False, 'unknown alignment method'
+
+        return s, R, t
---
+++ packages/cnspy-trajectory-evaluation/cnspy_trajectory_evaluation-0.1.0-py3-none-any.whl/test/test_EvaluationReport.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Software License Agreement (GNU GPLv3  License)
+#
+# Copyright (c) 2020, Roland Jung (roland.jung@aau.at) , AAU, KPK, NAV
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+########################################################################################################################
+import os
+import unittest
+import time
+from cnspy_trajectory_evaluation.EvaluationReport import *
+
+SAMPLE_DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sample_data')
+
+class EvaluationReport_Test(unittest.TestCase):
+    start_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self, info="Process time"):
+        print(str(info) + " took : " + str((time.time() - self.start_time)) + " [sec]")
+
+    def get_fn(self):
+        fn_gt_csv = str(SAMPLE_DATA_DIR + '/ID1-pose-gt.csv')
+        fn_est_csv = str(SAMPLE_DATA_DIR + '/ID1-pose-est-cov.csv')
+        return fn_gt_csv, fn_est_csv
+
+    def test_init(self):
+        self.start()
+        report = EvaluationReport()
+        report.directory = ''
+        report.fn_gt = str(SAMPLE_DATA_DIR + '/ID1-pose-gt.csv')
+        report.fn_est = str(SAMPLE_DATA_DIR + '/ID1-pose-est-cov.csv')
+        report.ANEES_p = 0.1
+        report.ANEES_q = 0.2
+        report.ARMSE_p = 0.3
+        report.ARMSE_q = 0.4
+
+        fn = str(SAMPLE_DATA_DIR + '/results/eval-report.ini')
+        report.save(fn)
+
+        report_ = EvaluationReport()
+        report_.load(fn)
+
+        self.assertTrue(report.fn_gt == report_.fn_gt)
+
+        self.stop()
+
+
+if __name__ == "__main__":
+    unittest.main()
