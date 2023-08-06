1407520615508ad0e4869e11d51e8f081ef9419e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:13 +0000

    Add package SPDE/0.0.2/SPDE-0.0.2.zip

---
+++ packages/SPDE/SPDE-0.0.2.zip/SPDE-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup,Extension
+from Cython.Build import cythonize
+
+sourcefiles = ['src/spde.pyx', 'src/TurboOptimizer.cpp']
+
+extensions = [Extension("SPDE", sourcefiles,language='c++')]
+
+
+setup(name="SPDE",
+	  version="0.0.2",
+		author = 'Nicolas Desassis and Didier Renard',
+		author_email = 'nicolas.desassis@gmail.com',
+		url = 'https://github.com/NDesassis/spde', 
+    ext_modules = cythonize(extensions)
+)
+
