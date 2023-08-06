261bcaa3792aae138cc245d2e8632ee3ac974833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:03 +0000

    Add package wgdi/0.4.6/wgdi-0.4.6.tar.gz

---
+++ packages/wgdi/wgdi-0.4.6.tar.gz/wgdi-0.4.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+required = ['pandas>=1.1.0', 'numpy', 'biopython', 'matplotlib', 'scipy']
+
+setup(
+    name="wgdi",
+    version="0.4.6",
+    author="Pengchuan Sun",
+    author_email="sunpengchuan@gmail.com",
+    description="Whole Genome Duplication Identification",
+    license="BSD License",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SunPengChuan/wgdi",
+    packages=find_packages(),
+    package_data={'': ['*.conf','*.ini', '*.csv']},
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'wgdi = wgdi.run:main',
+        ]
+    },
+    zip_safe=True,
+    install_requires=required
+)
