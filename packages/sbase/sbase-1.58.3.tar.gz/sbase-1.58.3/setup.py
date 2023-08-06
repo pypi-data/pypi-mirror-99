b3babe0a76ad225328cfe96984e9addcaaf911d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:28 +0000

    Add package sbase/1.58.3/sbase-1.58.3.tar.gz

---
+++ packages/sbase/sbase-1.58.3.tar.gz/sbase-1.58.3/setup.py
@@ -0,0 +1,112 @@
+"""
+A proxy for installing seleniumbase dependencies and plugins
+"""
+
+from setuptools import setup, find_packages  # noqa
+import os
+import sys
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+long_description = None
+total_description = None
+try:
+    with open(os.path.join(this_directory, 'README.md'), 'rb') as f:
+        total_description = f.read().decode('utf-8')
+    description_lines = total_description.split('\n')
+    long_description_lines = []
+    for line in description_lines:
+        if not line.startswith("<meta ") and not line.startswith("<link "):
+            long_description_lines.append(line)
+    long_description = "\n".join(long_description_lines)
+except IOError:
+    long_description = 'The complete web automation library.'
+
+if sys.argv[-1] == 'publish':
+    reply = None
+    input_method = input
+    if not sys.version_info[0] >= 3:
+        input_method = raw_input  # noqa
+    reply = str(input_method(
+        '>>> Confirm release PUBLISH to PyPI? (yes/no): ')).lower().strip()
+    if reply == 'yes':
+        print("\n*** Checking code health with flake8:\n")
+        os.system("python -m pip install 'flake8==3.9.0'")
+        flake8_status = os.system("flake8 --exclude=temp")
+        if flake8_status != 0:
+            print("\nWARNING! Fix flake8 issues before publishing to PyPI!\n")
+            sys.exit()
+        else:
+            print("*** No flake8 issues detected. Continuing...")
+        print("\n*** Rebuilding distribution packages: ***\n")
+        os.system('rm -f dist/*.egg; rm -f dist/*.tar.gz; rm -f dist/*.whl')
+        os.system('python setup.py sdist bdist_wheel')  # Create new tar/wheel
+        print("\n*** Installing twine: *** (Required for PyPI uploads)\n")
+        os.system("python -m pip install 'twine>=1.15.0'")
+        print("\n*** Installing tqdm: *** (Required for PyPI uploads)\n")
+        os.system("python -m pip install --upgrade 'tqdm>=4.59.0'")
+        print("\n*** Publishing The Release to PyPI: ***\n")
+        os.system('python -m twine upload dist/*')  # Requires ~/.pypirc Keys
+        print("\n*** The Release was PUBLISHED SUCCESSFULLY to PyPI! :) ***\n")
+    else:
+        print("\n>>> The Release was NOT PUBLISHED to PyPI! <<<\n")
+    sys.exit()
+
+setup(
+    name='sbase',
+    version='1.58.3',
+    description='A complete web automation framework for end-to-end testing.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/seleniumbase/SeleniumBase',
+    platforms=["Windows", "Linux", "Mac OS-X"],
+    author='Michael Mintz',
+    author_email='mdmintz@gmail.com',
+    maintainer='Michael Mintz',
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: MacOS X",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: Web Environment",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Testing :: Acceptance",
+        "Topic :: Software Development :: Testing :: Traffic Generation",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    install_requires=[
+        'seleniumbase>=1.58.3',
+        ],
+    packages=[
+        ],
+    entry_points={
+        'nose.plugins': [
+            ],
+        'pytest11': [
+            ]
+        }
+    )
+
+print("\n*** SeleniumBase Installation Complete! ***\n")
