f8cd1e66a56fff57bb6e9f3124670ddc06c02467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:46 +0000

    Add package odoo12-addon-shopinvader-image/12.0.1.1.0/odoo12_addon_shopinvader_image-12.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo12-addon-shopinvader-image/odoo12_addon_shopinvader_image-12.0.1.1.0-py3-none-any.whl/odoo/addons/shopinvader_image/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2018 Akretion (http://www.akretion.com)
+# Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Shopinvader image",
+    "summary": "Add the export of Image for Shopinvader",
+    "version": "12.0.1.1.0",
+    "category": "e-commerce",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "depends": ["shopinvader", "storage_image_product"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/shopinvader_backend_view.xml",
+        "views/shopinvader_image_resize_view.xml",
+        "data/ir_export_product.xml",
+        "data/ir_export_category.xml",
+        "data/shopinvader_image_resize.xml",
+    ],
+    "demo": [
+        "demo/storage_image_product_image_tag_demo.xml",
+        "demo/backend_demo.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+}
