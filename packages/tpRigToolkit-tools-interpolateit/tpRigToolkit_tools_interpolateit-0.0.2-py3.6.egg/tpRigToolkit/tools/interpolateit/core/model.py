5d7537f4f90996735d7d36c398e300bc29b5564d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:09 +0000

    Add package tpRigToolkit-tools-interpolateit/0.0.2/tpRigToolkit_tools_interpolateit-0.0.2-py3.6.egg

---
+++ packages/tpRigToolkit-tools-interpolateit/tpRigToolkit_tools_interpolateit-0.0.2-py3.6.egg/tpRigToolkit/tools/interpolateit/core/model.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Interpolate It widget model class implementation
+"""
+
+from __future__ import print_function, division, absolute_import
+
+from Qt.QtCore import Signal, QObject
+
+
+class InterpolateItModel(QObject, object):
+
+    addInterpolatorWidget = Signal(object)
+    removeInterpolatorWidget = Signal(object)
+    prepareLoad = Signal()
+
+    def __init__(self):
+        super(InterpolateItModel, self).__init__()
+
+        self._interpolator_widgets = list()
+
+    @property
+    def interpolator_widgets(self):
+        return self._interpolator_widgets
