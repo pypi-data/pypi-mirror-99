d061f70eb508ec231c9e043fc5a7485f37163416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:51 +0000

    Add package pydory/1.0.0/pydory-1.0.0.tar.gz

---
+++ packages/pydory/pydory-1.0.0.tar.gz/pydory-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+os.environ["CC"] = "gcc-10.2"
+os.environ['CFLAGS'] = '-O3 -fopenmp -pthread -D SAVEPD'
+setup(name="pydory",
+      version="1.0.0",
+      python_requires='>=3',
+      description="Python interface for Dory",
+      author="Manu Aggarwal",
+      author_email="manu.aggarwal@nih.gov",
+      ext_modules=[Extension("pydory", ["dorymodule.c"],headers=['sort.h'\
+          ,'sort2.h','sort3.h','sort4.h','sort5.h','sort6.h'\
+          ,'sort8.h','sort9.h'\
+          ])],
+      )
