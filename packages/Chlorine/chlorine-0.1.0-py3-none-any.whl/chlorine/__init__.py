6dfbd79327697f26bf61f1b38b487e5463994f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:33 +0000

    Add package Chlorine/0.1.0/chlorine-0.1.0-py3-none-any.whl

---
+++ packages/Chlorine/chlorine-0.1.0-py3-none-any.whl/chlorine/__init__.py
@@ -0,0 +1,12 @@
+"""
+    chlorine
+    ~~~~~~~~~~~~~~
+    :copyright: (c) 2021 by the Fluorine Team.
+    :license: GPL-3.0,  see LICENSE for more details.
+"""
+
+from .translate import Translate
+
+__all__ = [
+    "Translate"
+]
