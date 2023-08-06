6bbc7a51ba299837f900b6238251c6f10d8e7390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:03 +0000

    Add package ma3xcli/0.1.10/ma3xcli-0.1.10.tar.gz

---
+++ packages/ma3xcli/ma3xcli-0.1.10.tar.gz/ma3xcli-0.1.10/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ma3xcli']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['init-pyenv = ma3xcli.init-pyenv:main',
+                     'two = ma3xcli.two:main']}
+
+setup_kwargs = {
+    'name': 'ma3xcli',
+    'version': '0.1.10',
+    'description': '',
+    'long_description': None,
+    'author': '1v1a3x',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
