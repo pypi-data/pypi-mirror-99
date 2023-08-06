7628065d33e5bd887303d052bbd7e0bea1ce36c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:49 +0000

    Add package large-image/1.4.4.dev15/large-image-1.4.4.dev15.tar.gz

---
+++ packages/large-image/large-image-1.4.4.dev15.tar.gz/large-image-1.4.4.dev15/girder_annotation/girder_large_image_annotation/web_client/views/index.js
@@ -0,0 +1,21 @@
+import * as viewers from '@girder/large_image/views/imageViewerWidget';
+
+import ConfigView from './configView';
+import ImageViewerSelectWidget from './imageViewerSelectWidget';
+import ItemListWidget from './itemList';
+import ImageViewerWidgetAnnotationExtension from './imageViewerWidget/base';
+import * as extensions from './imageViewerWidget';
+
+for (var key in viewers) {
+    viewers[key] = viewers[key].extend(ImageViewerWidgetAnnotationExtension);
+    if (extensions[key]) {
+        viewers[key] = extensions[key](viewers[key]);
+    }
+}
+
+export {
+    ConfigView,
+    ImageViewerSelectWidget,
+    ItemListWidget,
+    viewers as ViewerWidget
+};
