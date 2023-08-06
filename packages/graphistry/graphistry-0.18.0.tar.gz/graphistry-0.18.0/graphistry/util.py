ae441b909206dc69289a14307bed57b8b6d87656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:22 +0000

    Add package graphistry/0.18.0/graphistry-0.18.0.tar.gz

---
+++ packages/graphistry/graphistry-0.18.0.tar.gz/graphistry-0.18.0/graphistry/util.py
@@ -0,0 +1,87 @@
+import hashlib, platform as p, random, string, sys, uuid, warnings
+from distutils.version import LooseVersion, StrictVersion
+
+
+def cmp(x, y):
+    return (x > y) - (x < y)
+
+
+def make_iframe(url, height):
+    id = uuid.uuid4()
+
+    scrollbug_workaround = '''
+            <script>
+                try {
+                  $("#%s").bind('mousewheel', function(e) { e.preventDefault(); });
+                } catch (e) { console.error('exn catching scroll', e); }
+            </script>
+        ''' % id
+
+    iframe = '''
+            <iframe id="%s" src="%s"
+                    allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"
+                    oallowfullscreen="true" msallowfullscreen="true"
+                    style="width:100%%; height:%dpx; border: 1px solid #DDD; overflow: hidden">
+            </iframe>
+        ''' % (id, url, height)
+
+    return iframe + scrollbug_workaround
+
+
+def fingerprint():
+    md5 = hashlib.md5()
+    # Hostname, OS, CPU, MAC,
+    data = [p.node(), p.system(), p.machine(), str(uuid.getnode())]
+    md5.update(''.join(data).encode('utf8'))
+
+    from ._version import get_versions
+    __version__ = get_versions()['version']
+
+    return "%s-pygraphistry-%s" % (md5.hexdigest()[:8], __version__)
+
+
+def random_string(length):
+    gibberish = [random.choice(string.ascii_uppercase + string.digits) for _ in range(length)]
+    return ''.join(gibberish)
+
+
+def compare_versions(v1, v2):
+    try:
+        return cmp(StrictVersion(v1), StrictVersion(v2))
+    except ValueError:
+        return cmp(LooseVersion(v1), LooseVersion(v2))
+
+
+def in_ipython():
+    try:
+        if hasattr(__builtins__, '__IPYTHON__'):
+            return True
+    except NameError:
+        pass
+    try:
+        from IPython import get_ipython
+        cfg = get_ipython()
+        if not (cfg is None) and ('IPKernelApp' in get_ipython().config):
+            return True
+    except ImportError:
+        pass
+    return False
+
+def warn(msg):
+    try:
+        if in_ipython():
+            import IPython
+            IPython.utils.warn.warn(msg)
+            return
+    except:
+        'ok'
+    warnings.warn(RuntimeWarning(msg))
+
+
+def error(msg):
+    raise ValueError(msg)
+
+def merge_two_dicts(a, b):
+    c = a.copy()
+    c.update(b)
+    return c
---
+++ packages/graphistry/graphistry-0.18.0.tar.gz/graphistry-0.18.0/setup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+#FIXME: prevents pyproject.toml - same as https://github.com/SciTools/cartopy/issues/1270
+import versioneer
+
+def unique_flatten_dict(d):
+  return list(set(sum( d.values(), [] )))
+
+core_requires = ['numpy', 'pandas >= 0.17.0', 'pyarrow >= 0.15.0', 'requests', 'protobuf >= 2.6.0']
+
+stubs = [
+  'pandas-stubs'
+]
+
+dev_extras = {
+    'docs': ['sphinx==3.4.3', 'sphinx_autodoc_typehints==1.11.1', 'sphinx-rtd-theme==0.5.1'],
+    'test': ['flake8', 'mock', 'mypy', 'pytest'] + stubs,
+    'build': ['build']
+}
+
+base_extras = {
+    'igraph': ['python-igraph'],
+    'networkx': ['networkx==2.2'],
+    'bolt': ['neo4j', 'neotime'],
+    'nodexl': ['openpyxl', 'xlrd'],
+    'jupyter': ['ipython']
+}
+
+extras_require = {
+
+  **base_extras,
+  **dev_extras,
+
+  #kitchen sink for users -- not recommended
+  'all': unique_flatten_dict(base_extras),
+
+  #kitchen sink for contributors
+  'dev': unique_flatten_dict(base_extras) + unique_flatten_dict(dev_extras),
+
+}
+
+setup(
+    name='graphistry',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages = ['graphistry'],
+    platforms='any',
+    description = 'A visual graph analytics library for extracting, transforming, displaying, and sharing big graphs with end-to-end GPU acceleration',
+    long_description=open("./README.md").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/graphistry/pygraphistry',
+    download_url= 'https://pypi.python.org/pypi/graphistry/',
+    python_requires='>=3.6',
+    author='The Graphistry Team',
+    author_email='pygraphistry@graphistry.com',
+    install_requires=core_requires,
+    extras_require=extras_require,
+    license='BSD',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Environment :: Console',
+        'Environment :: GPU :: NVIDIA CUDA',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: Log Analysis',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Sociology',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Widget Sets',
+        'Topic :: System :: Distributed Computing'
+    ],
+    keywords=['cugraph', 'cudf', 'dask', 'GPU', 'Graph',  'GraphX', 'Gremlin', 'igraph', 'Jupyter', 'Neo4j', 'Network', 'NetworkX',  'Notebook', 'Pandas', 'Plot', 'Rapids', 'RDF', 'Splunk', 'Spark', 'Tinkerpop', 'Visualization']
+)
