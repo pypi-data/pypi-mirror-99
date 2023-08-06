7349a06591285c9c7a668e7178c9141c2378a96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:21 +0000

    Add package jupyterlab-hide-code/3.0.4/jupyterlab_hide_code-3.0.4.tar.gz

---
+++ packages/jupyterlab-hide-code/jupyterlab_hide_code-3.0.4.tar.gz/jupyterlab_hide_code-3.0.4/README.md
@@ -0,0 +1,66 @@
+# jupyterlab-hide-code
+
+![Build](https://github.com/osscar-org/jupyterlab-hide-code/workflows/Build/badge.svg)
+
+![demo](./docs/hide-input.gif)
+
+A button in JupyterLab to run the code cells and then to hide the code cells.
+This JupyterLab extension was inspired by the
+[jlab-hide-code](https://github.com/AixViPMaP/jlab-hide-code) JupyterLab
+extension from Aachen (Aix) Virtual Platform for Materials Processing.
+
+## Requirements
+
+* JupyterLab >= 3.0
+
+
+## Install
+
+```bash
+pip install jupyterlab-hide-code
+```
+
+## Uninstall
+
+```bash
+pip uninstall jupyterlab-hide-code
+```
+
+## Contributing
+
+### Build
+
+The `jlpm` command is JupyterLab's pinned version of
+[yarn](https://yarnpkg.com/) that is installed with JupyterLab. You may use
+`yarn` or `npm` in lieu of `jlpm` below.
+
+```bash
+# Clone the repo to your local environment
+# Move to jupyterlab-hide-code directory
+# Install dependencies
+jlpm
+# Build Typescript source
+jlpm build
+# Link your development version of the extension with JupyterLab
+jupyter labextension link .
+# Rebuild Typescript source after making changes
+jlpm build
+# Rebuild JupyterLab after making any changes
+jupyter lab build
+```
+
+You can watch the source directory and run JupyterLab in watch mode to watch for changes in the extension's source and automatically rebuild the extension and application.
+
+```bash
+# Watch the source directory in another terminal tab
+jlpm watch
+# Run jupyterlab in watch mode in one terminal tab
+jupyter lab --watch
+```
+
+## Acknowlegements
+
+We acknowledge support from:
+* EPFL Open Science Fund
+
+<img src='http://www.osscar.org/wp-content/uploads/2019/03/OSSCAR-logo.png' width='240'>
---
+++ packages/jupyterlab-hide-code/jupyterlab_hide_code-3.0.4.tar.gz/jupyterlab_hide_code-3.0.4/src/index.ts
@@ -0,0 +1,90 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IDisposable, DisposableDelegate } from '@lumino/disposable';
+
+import { PanelLayout } from '@lumino/widgets';
+
+import { ToolbarButton } from '@jupyterlab/apputils';
+
+import { DocumentRegistry } from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions,
+  NotebookPanel,
+  INotebookModel
+} from '@jupyterlab/notebook';
+
+import '../style/index.css';
+
+/**
+ * Initialization data for the jupyterlab-hide-code extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-hide-code',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+    console.log('JupyterLab extension jupyterlab-hide-code is activated!');
+  }
+};
+
+export class ButtonExtension
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  createNew(
+    panel: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    const hideInputCode = () => {
+      NotebookActions.runAll(panel.content, context.sessionContext);
+
+      panel.content.widgets.forEach(cell => {
+        if (cell.model.type === 'code') {
+          const layout = cell.layout as PanelLayout;
+          layout.widgets[1].hide();
+        }
+      });
+      buttonHideInput.hide();
+      buttonShowInput.show();
+    };
+    const showInputCode = () => {
+      panel.content.widgets.forEach(cell => {
+        if (cell.model.type === 'code') {
+          const layout = cell.layout as PanelLayout;
+          layout.widgets[1].show();
+        }
+      });
+
+      buttonHideInput.show();
+      buttonShowInput.hide();
+    };
+
+    const buttonHideInput = new ToolbarButton({
+      className: 'myButton',
+      iconClass: 'fa fa-sm fa-eye-slash fontawesome-colors',
+      onClick: hideInputCode,
+      tooltip: 'Hide Input'
+    });
+
+    const buttonShowInput = new ToolbarButton({
+      className: 'myButton',
+      iconClass: 'fa fa-sm fa-eye fontawesome-colors',
+      onClick: showInputCode,
+      tooltip: 'Show Input'
+    });
+
+    buttonShowInput.hide();
+
+    panel.toolbar.insertItem(11, 'hideInput', buttonHideInput);
+    panel.toolbar.insertItem(11, 'showInput', buttonShowInput);
+
+    return new DisposableDelegate(() => {
+      buttonHideInput.dispose();
+      buttonShowInput.dispose();
+    });
+  }
+}
+
+export default extension;
