fe113a2ea7556457d5de9553247760d22e78cddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:38 +0000

    Add package odoo13-addon-shopinvader-assortment/13.0.1.0.1.dev1/odoo13_addon_shopinvader_assortment-13.0.1.0.1.dev1-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-assortment/odoo13_addon_shopinvader_assortment-13.0.1.0.1.dev1-py3-none-any.whl/odoo/addons/shopinvader_assortment/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Assortment",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV",
+    "website": "http://github.com/shopinvader/odoo-shopinvader",
+    "depends": [
+        "product_assortment",
+        "shopinvader",
+        "shopinvader_search_engine",
+    ],
+    "data": ["views/shopinvader_backend.xml"],
+    "demo": ["demo/shopinvader_assortment_demo.xml"],
+    "installable": True,
+}
