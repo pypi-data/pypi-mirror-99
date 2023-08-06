8d6466d1171e235721886169ddaa2312e6e107c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:42 +0000

    Add package irradiance-pv/1.21/irradiance_pv-1.21.tar.gz

---
+++ packages/irradiance-pv/irradiance_pv-1.21.tar.gz/irradiance_pv-1.21/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+# with open("irradiance_pv/README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="irradiance_pv",
+    version=1.21,
+    description="Calculate in plane irradiance for a surface",
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["irradiance_pv"],
+    author="Sergio Badillo",
+    author_email="sbadilloworks@gmail.com",
+    zip_safe=False,
+)
