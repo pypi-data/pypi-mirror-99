135b2e586c6e49891f82e90b8035bb0c55db9512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:14 +0000

    Add package cdk-iam-floyd/0.148.0/cdk_iam_floyd-0.148.0-py3-none-any.whl

---
+++ packages/cdk-iam-floyd/cdk_iam_floyd-0.148.0-py3-none-any.whl/cdk_iam_floyd/_jsii/__init__.py
@@ -0,0 +1,21 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-iam-floyd", "0.148.0", __name__[0:-6], "cdk-iam-floyd@0.148.0.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
