8134018be557073b1077937c12cc030586522899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:47 +0000

    Add package dython/0.6.4/dython-0.6.4.tar.gz

---
+++ packages/dython/dython-0.6.4.tar.gz/dython-0.6.4/README.md
@@ -0,0 +1,36 @@
+# Dython
+
+[![PyPI Version](https://img.shields.io/pypi/v/dython.svg)](https://pypi.org/project/dython/)
+[![Python Version](https://img.shields.io/pypi/pyversions/dython.svg)](https://pypi.org/project/dython/)
+[![PyPI - Downloads](https://img.shields.io/pypi/dm/dython)](https://pypistats.org/packages/dython)
+[![License](https://img.shields.io/pypi/l/dython)](https://github.com/shakedzy/dython/blob/master/LICENSE)
+
+A set of **D**ata analysis tools in p**YTHON** 3.x.
+
+Dython was designed with analysis usage in mind - meaning ease-of-use, functionality and readability are the core 
+values of this library. Production-grade performance, on the other hand, were not considered.
+
+## Documentation:
+Modules documentation can be found on [shakedzy.xyz/dython](http://shakedzy.xyz/dython)
+
+## Installation:
+Dython can be installed directly using `pip`:
+```
+pip install dython
+```
+If you wish to install from source:
+```
+pip install git+https://github.com/shakedzy/dython.git
+```
+
+**Dependencies:** `numpy`, `pandas`, `seaborn`, `scipy`, `matplotlib`, `sklearn`
+
+## Contributing:
+Contributions are always welcomed - if you found something you can fix, or have an idea for a new feature, feel free to write it and open a pull request. Please make sure to go over the [contributions guidelines](https://github.com/shakedzy/dython/blob/master/CONTRIBUTING.md).
+
+## Change log:
+Change log is available, see [CHANGELOG.md](https://github.com/shakedzy/dython/blob/master/CHANGELOG.md).
+
+## Related blogposts:
+* Read more about the categorical tools on [The Search for Categorical Correlation](https://medium.com/@shakedzy/the-search-for-categorical-correlation-a1cf7f1888c9)
+* Read more about using ROC graphs on [Hard ROC: Really Understanding & Properly Using ROC and AUC](https://medium.com/@shakedzy/hard-roc-really-understanding-and-properly-using-roc-and-auc-13413cf0dc24)
---
+++ packages/dython/dython-0.6.4.tar.gz/dython-0.6.4/dython/model_utils.py
@@ -0,0 +1,331 @@
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, precision_recall_curve, auc
+from ._private import convert
+
+__all__ = [
+    'random_forest_feature_importance',
+    'metric_graph',
+    'roc_graph'
+]
+
+_ROC_PLOT_COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'darkorange']
+
+
+def _display_metric_plot(metric, naives, xlim, ylim, legend, title, filename):
+    for n in naives:
+        plt.plot([n[0], n[1]], [n[2], n[3]], color=n[4], lw=1, linestyle='--')
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    if metric == 'roc':
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title(title or 'Receiver Operating Characteristic')
+    else:  # metric == 'pr'
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.title(title or 'Precision-Recall Curve')
+    if legend:
+        plt.legend(loc=legend)
+    if filename:
+        plt.savefig(filename)
+    plt.show()
+
+
+def _draw_estimated_optimal_threshold_mark(metric, x_axis, y_axis, thresholds, color, ms, fmt, ax):
+    annotation_offset = (-.027, .03)
+    a = np.zeros((len(x_axis), 2))
+    a[:, 0] = x_axis
+    a[:, 1] = y_axis
+    if metric == 'roc':
+        dist = lambda row: row[0]**2 + (1 - row[1])**2  # optimal: (0,1)
+    else:  # metric == 'pr'
+        dist = lambda row: (1 - row[0]) ** 2 + (1 - row[1]) ** 2  # optimal: (1,1)
+    amin = np.apply_along_axis(dist, 1, a).argmin()
+    plt.plot(x_axis[amin], y_axis[amin], color=color, marker='o', ms=ms)
+    ax.annotate("{th:{fmt}}".format(th=thresholds[amin], fmt=fmt),
+                xy=(x_axis[amin], y_axis[amin]), color=color,
+                xytext=(x_axis[amin] + annotation_offset[0],
+                        y_axis[amin] + annotation_offset[1]))
+    return thresholds[amin]
+
+
+def _plot_macro_metric(x_axis, y_axis, n, lw, fmt, ax):
+    all_x_axis = np.unique(np.concatenate([x_axis[i] for i in range(n)]))
+    mean_y_axis = np.zeros_like(all_x_axis)
+    for i in range(n):
+        mean_y_axis += np.interp(all_x_axis, x_axis[i], y_axis[i])
+    mean_y_axis /= n
+    x_axis_macro = all_x_axis
+    y_axis_macro = mean_y_axis
+    auc_macro = auc(x_axis_macro, y_axis_macro)
+    label = 'ROC curve: macro (AUC = {auc:{fmt}})'.format(auc=auc_macro, fmt=fmt)
+    ax.plot(x_axis_macro,
+            y_axis_macro,
+            label=label,
+            color='navy',
+            ls=':',
+            lw=lw)
+
+
+def _binary_metric_graph(metric, y_true, y_pred, eoptimal, class_label, color, lw, ls, ms, fmt, ax):
+    y_true = convert(y_true, 'array')
+    y_pred = convert(y_pred, 'array')
+    if y_pred.shape != y_true.shape:
+        raise ValueError('y_true and y_pred must have the same shape')
+    elif len(y_pred.shape) == 1:
+        y_t = y_true
+        y_p = y_pred
+    else:
+        y_t = [np.argmax(x) for x in y_true]
+        y_p = [x[1] for x in y_pred]
+    if metric == 'roc':
+        x_axis, y_axis, th = roc_curve(y_t, y_p)  # x = fpr, y = tpr
+    else:  # metric == 'pr'
+        y_axis, x_axis, th = precision_recall_curve(y_t, y_p)  # x = recall, y = precision
+    auc_score = auc(x_axis, y_axis)
+    if class_label is not None:
+        class_label = ': ' + class_label
+    else:
+        class_label = ''
+    label = '{metric} curve{class_label} (AUC = {auc:{fmt}}'.format(metric=metric.upper(), class_label=class_label,
+                                                                    auc=auc_score, fmt=fmt)
+    if eoptimal:
+        eopt = _draw_estimated_optimal_threshold_mark(metric, x_axis, y_axis, th, color, ms, fmt, ax)
+        label += ', eOpT = {th:{fmt}})'.format(th=eopt, fmt=fmt)
+    else:
+        eopt = None
+        label += ')'
+    ax.plot(x_axis,
+            y_axis,
+            color=color,
+            lw=lw,
+            ls=ls,
+            label=label)
+    return {'x': x_axis, 'y': y_axis, 'thresholds': th,
+            'auc': auc_score, 'eopt': eopt,
+            'y_t_ratio': np.sum(y_t)/y_t.size}
+
+
+def metric_graph(y_true,
+                 y_pred,
+                 metric,
+                 micro=True,
+                 macro=True,
+                 eopt=True,
+                 class_names=None,
+                 colors=None,
+                 ax=None,
+                 figsize=None,
+                 xlim=(0.,1.),
+                 ylim=(0.,1.02),
+                 lw=2,
+                 ls='-',
+                 ms=10,
+                 fmt='.2f',
+                 legend='best',
+                 plot=True,
+                 title=None,
+                 filename=None,
+                 force_multiclass=False
+                 ):
+    """
+    Plot a ROC graph of predictor's results (including AUC scores), where each
+    row of y_true and y_pred represent a single example.
+    If there are 1 or two columns only, the data is treated as a binary
+    classification (see input example below).
+    If there are more then 2 columns, each column is considered a
+    unique class, and a ROC graph and AUC score will be computed for each.
+    A Macro-ROC and Micro-ROC are computed and plotted too by default.
+
+    Based on sklearn examples (as was seen on April 2018):
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc.html
+
+    Parameters:
+    -----------
+    y_true : list / NumPy ndarray
+        The true classes of the predicted data
+    y_pred : list / NumPy ndarray
+        The predicted classes
+    metric : string
+        The metric graph to plot. Currently supported: 'roc' for Receiver Operating Characteristic curve and
+        'pr' for Precision-Recall curve
+    micro : Boolean, default = True
+        Whether to calculate a Micro graph (not applicable for binary cases)
+    macro : Boolean, default = True
+        Whether to calculate a Macro graph (ROC metric only, not applicable for binary cases)
+    eopt : Boolean, default = True
+        Whether to calculate and display the estimated-optimal threshold
+        for each metric graph. For ROC curves, the estimated-optimal threshold is the closest
+        computed threshold with (fpr,tpr) values closest to (0,1). For PR curves, it is
+        the closest one to (1,1) (perfect recall and precision)
+    class_names: list or string, default = None
+        Names of the different classes. In a multi-class classification, the
+        order must match the order of the classes probabilities in the input
+        data. In a binary classification, can be a string or a list. If a list,
+        only the last element will be used.
+    colors : list of Matplotlib color strings or None, default = None
+        List of colors to be used for the plotted curves. If `None`, falls back
+        to a predefined default.
+    ax : matplotlib ax, default = None
+        Matplotlib Axis on which the curves will be plotted
+    figsize : (int,int) or None, default = None
+        a Matplotlib figure-size tuple. If `None`, falls back to Matplotlib's
+        default. Only used if `ax=None`.
+    xlim : (float, float), default = (0.,1.)
+        X-axis limits.
+    ylim : (float,float), default = (0.,1.02)
+        Y-axis limits.
+    lw : int, default = 2
+        Line-width.
+    ls : string, default = '-'
+        Matplotlib line-style string
+    ms : int, default = 10,
+        Marker-size.
+    fmt : string, default = '.2f'
+        String formatting of displayed AUC and threshold numbers.
+    legend : string or None, default = 'best'
+        Position graph legend.
+    plot : Boolean, default = True
+        Display graph
+    title : string or None, default = None
+        Plotted graph title. If None, default title is used
+    filename : string or None, default = None
+        If not None, plot will be saved to the given file name
+    force_multiclass : Boolean, default = False
+        Only applicable if `y_true` and `y_pred` have two columns. If so,
+        consider the data as a multiclass data rather than binary (useful when plotting
+        curves of different models one against the other)
+
+    Returns:
+    --------
+    A dictionary, one key for each class. Each value is another dictionary,
+    holding AUC and eOpT values.
+
+    Binary Classification Input Example:
+    ------------------------------------
+    Consider a data-set of two data-points where the true class of the first line
+    is class 0, which was predicted with a probability of 0.6, and the second line's
+    true class is 1, with predicted probability of 0.8.
+    ```python
+    # First option:
+    >> metric_graph(y_true=[0,1], y_pred=[0.6,0.8], metric='roc')
+    # Second option:
+    >> metric_graph(y_true=[[1,0],[0,1]], y_pred=[[0.6,0.4],[0.2,0.8]], metric='roc')
+    # Both yield the same result
+    ```
+
+    Example:
+    --------
+    See `roc_graph_example` and pr_graph_example` under `dython.examples`
+    """
+    if metric is None or metric.lower() not in ['roc', 'pr']:
+        raise ValueError(f'Invalid metric {metric}')
+    else:
+        metric = metric.lower()
+    all_x_axis = list()
+    all_y_axis = list()
+    y_true = convert(y_true, 'array')
+    y_pred = convert(y_pred, 'array')
+    if y_pred.shape != y_true.shape:
+        raise ValueError('y_true and y_pred must have the same shape')
+    if class_names is not None:
+        if not isinstance(class_names, str):
+            class_names = convert(class_names, 'list')
+        else:
+            class_names = [class_names]
+    if ax is None:
+        plt.figure(figsize=figsize)
+        ax = plt.gca()
+    if isinstance(colors, str):
+        colors = [colors]
+    colors = colors or _ROC_PLOT_COLORS
+    output_dict = dict()
+    pr_naives = list()
+    if len(y_pred.shape) == 1 or y_pred.shape[1] == 1 or (y_pred.shape[1] == 2 and not force_multiclass):
+        class_label = class_names[-1] if class_names is not None else None
+        color = colors[-1]
+        d = _binary_metric_graph(metric, y_true, y_pred, eoptimal=eopt,
+                                 class_label=class_label, color=color,
+                                 lw=lw, ls=ls, ms=ms, fmt=fmt, ax=ax)
+        class_label = class_label or '0'
+        output_dict[class_label] = {'auc': d['auc'],
+                                    'eopt': d['eopt']}
+        pr_naives.append([0, 1, d['y_t_ratio'], d['y_t_ratio'], color])
+    else:
+        n = y_pred.shape[1]
+        if class_names is not None:
+            if not isinstance(class_names, list):
+                raise ValueError('class_names must be a list of items in multi-class classification.')
+            if len(class_names) != n:
+                raise ValueError('Number of class names does not match input data size.')
+        for i in range(0, n):
+            class_label = class_names[i] if class_names is not None else str(i)
+            color = colors[i % len(colors)]
+            d = _binary_metric_graph(metric,
+                                     y_true[:, i],
+                                     y_pred[:, i],
+                                     eoptimal=eopt,
+                                     color=color,
+                                     class_label=class_label,
+                                     lw=lw, ls=ls, ms=ms, fmt=fmt, ax=ax)
+            all_x_axis.append(d['x'])
+            all_y_axis.append(d['y'])
+            output_dict[class_label] = {'auc': d['auc'],
+                                        'eopt': d['eopt']}
+            pr_naives.append([0, 1, d['y_t_ratio'], d['y_t_ratio'], color])
+        if micro:
+            _binary_metric_graph(metric,
+                                 y_true.ravel(),
+                                 y_pred.ravel(),
+                                 eoptimal=False,
+                                 ls=':',
+                                 color='deeppink',
+                                 class_label='micro',
+                                 lw=lw, ms=ms, fmt=fmt, ax=ax)
+        if macro and metric == 'roc':
+            _plot_macro_metric(all_x_axis, all_y_axis, n, lw, fmt, ax)
+    if plot:
+        if metric == 'roc':
+            naives = [[0, 1, 0, 1, 'grey']]
+        elif metric == 'pr':
+            naives = pr_naives
+        else:
+            raise ValueError(f'Invalid metric {metric}')
+        _display_metric_plot(metric, naives, xlim=xlim, ylim=ylim, legend=legend, title=title, filename=filename)
+    output_dict['ax'] = ax
+    return output_dict
+
+
+def random_forest_feature_importance(forest, features, precision=4):
+    """
+    Given a trained `sklearn.ensemble.RandomForestClassifier`, plot the
+    different features based on their importance according to the classifier,
+    from the most important to the least.
+
+    Parameters:
+    -----------
+    forest : sklearn.ensemble.RandomForestClassifier
+        A trained `RandomForestClassifier`
+    features : list
+        A list of the names of the features the classifier was trained on,
+        ordered by the same order the appeared
+        in the training data
+    precision : int, default = 4
+        Precision of feature importance
+    """
+    return sorted(zip(
+        map(lambda x: round(x, precision),
+            forest.feature_importances_), features),
+                  reverse=True)
+
+
+def roc_graph(y_true, y_pred, *args, **kwargs):
+    """
+    This method is deprecated. Please use `metric_graph(metric='roc',...)`
+    """
+    warnings.warn("The 'roc_graph' method is deprecated and will be removed in future versions. " +
+                  "Please use 'metric_graph(y_true, y_pred, metric='roc',...)' instead.",
+                  DeprecationWarning)
+    return metric_graph(y_true, y_pred, 'roc', *args, **kwargs)
