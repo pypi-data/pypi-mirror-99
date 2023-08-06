f6888c03c9221e1021eefc01985e384ad8f4e48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:02 +0000

    Add package kcli/99.0.202103241537/kcli-99.0.202103241537-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202103241537-py3-none-any.whl/kvirt/internalplans/__init__.py
@@ -0,0 +1,67 @@
+haproxy = """
+#jinja2: lstrip_blocks: True
+parameters:
+ image: CentOS-7-x86_64-GenericCloud.qcow2
+ name: haproxy
+ nets:
+ - default
+ vms: []
+
+loadbalancer-{{ ports | join('+') }}:
+ type: profile
+ image: {{ image }}
+ nets: {{ nets }}
+
+{{ name }}:
+ profile: loadbalancer-{{ ports | join('+') }}
+ files:
+  - path: /root/haproxy.cfg
+    content:   |
+      global
+         log         127.0.0.1 local2
+         chroot      /var/lib/haproxy
+         pidfile     /var/run/haproxy.pid
+         maxconn     4000
+         user        haproxy
+         group       haproxy
+         nbproc 4
+         daemon
+      defaults
+        mode        http
+        log         global
+        option      dontlognull
+        # option      httpclose
+        # option      httplog
+        # option      forwardfor
+        # option      redispatch
+        stats enable
+        stats uri /stats
+        stats realm HAProxy\ Statistics
+        stats auth admin:password
+        timeout connect 10000
+        timeout client 300000
+        timeout server 300000
+        maxconn     60000
+        retries     3
+      {% for port in ports %}
+      listen {{ name }}_{{ port }} *:{{ port }}
+      {% if port in [80, 443] %}
+        mode http
+        # option httpchk HEAD {{ checkpath }} HTTP/1.0
+      {% else %}
+        mode tcp
+      {% endif %}
+        balance roundrobin
+        cookie JSESSIONID prefix
+        {% for vm in vms %}
+        server {{ vm.name }} {{ vm.ip }}:{{ port }} cookie A check
+        {% endfor %}
+       {% endfor %}
+ cmds:
+  - yum -y install haproxy
+  - sed -i "s/SELINUX=enforcing/SELINUX=permissive/" /etc/selinux/config
+  - setenforce 0
+  - cp /root/haproxy.cfg /etc/haproxy
+  - systemctl start haproxy
+  - systemctl enable haproxy
+"""
