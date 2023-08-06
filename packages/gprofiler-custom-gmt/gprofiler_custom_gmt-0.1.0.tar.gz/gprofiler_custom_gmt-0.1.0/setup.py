a53c8cf95c636675d275657d242553b1a0fdbffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:46 +0000

    Add package gprofiler-custom-gmt/0.1.0/gprofiler_custom_gmt-0.1.0.tar.gz

---
+++ packages/gprofiler-custom-gmt/gprofiler_custom_gmt-0.1.0.tar.gz/gprofiler_custom_gmt-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gprofiler_custom_gmt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gprofiler-official>=1.0.0,<2.0.0',
+ 'pandas>=1.2.3,<2.0.0',
+ 'requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'gprofiler-custom-gmt',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Hugo Martiniano',
+    'author_email': 'hugomartiniano@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
