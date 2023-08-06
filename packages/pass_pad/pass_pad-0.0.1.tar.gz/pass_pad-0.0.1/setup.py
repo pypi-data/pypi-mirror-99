2149763b6009f60941e3f25ce4c2d255d6577e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:05 +0000

    Add package pass_pad/0.0.1/pass_pad-0.0.1.tar.gz

---
+++ packages/pass_pad/pass_pad-0.0.1.tar.gz/pass_pad-0.0.1/setup.py
@@ -0,0 +1,25 @@
+####
+#####!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='pass_pad',
+    version='0.0.1',
+    author='prgrmz07',
+    author_email='prgrmz07@163.com',
+    url='https://blog.csdn.net/hfcaoguilin',
+    description=u'pass_pad',
+    packages=['pass_pad'],
+    install_requires=[
+        'pycryptodomex>=3.10.1',
+        'pandas>=1.1.3'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pass_pad=pass_pad:main',
+            # 'pass_pad_test=pass_pad:test'
+        ]
+    }
+)
