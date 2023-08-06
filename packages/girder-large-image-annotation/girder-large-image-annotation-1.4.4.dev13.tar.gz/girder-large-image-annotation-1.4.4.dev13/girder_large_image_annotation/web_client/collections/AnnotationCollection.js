1904f56f3fc9f847fa5202333d8e80c7b929f2df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:21 +0000

    Add package girder-large-image-annotation/1.4.4.dev13/girder-large-image-annotation-1.4.4.dev13.tar.gz

---
+++ packages/girder-large-image-annotation/girder-large-image-annotation-1.4.4.dev13.tar.gz/girder-large-image-annotation-1.4.4.dev13/girder_large_image_annotation/web_client/collections/AnnotationCollection.js
@@ -0,0 +1,14 @@
+import Collection from '@girder/core/collections/Collection';
+import { SORT_DESC } from '@girder/core/constants';
+
+import AnnotationModel from '../models/AnnotationModel';
+
+export default Collection.extend({
+    resourceName: 'annotation',
+    model: AnnotationModel,
+    // this is a large number so that we probably never need to page
+    // annotations.
+    pageLimit: 10000,
+    sortField: 'created',
+    sortDir: SORT_DESC
+});
