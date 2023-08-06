67d026f914824956d192a67ad925cad0da4602ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:11 +0000

    Add package mkimgproxy/0.2/mkimgproxy-0.2.tar.gz

---
+++ packages/mkimgproxy/mkimgproxy-0.2.tar.gz/mkimgproxy-0.2/README.rst
@@ -0,0 +1,24 @@
+############
+mkimgproxy
+############
+
+Generates URL for imgproxy image processing server
+
+Use following command line to generate IMGPROXY_KEY and IMGPROXY_SALT
+
+.. code:: bash
+    echo $(xxd -g 2 -l 64 -p /dev/random | tr -d '\n')
+
+.. code:: python
+
+    from mkimgproxy import ImgProxy
+
+    IMGPROXY_URL = "http://my-imgproxy-server/path"
+    IMGPROXY_KEY = "9cbc4f564037858e5b9f2304f8540aa606943bddeaecb00a0b4a498092d0d65c079e291d3a2ddceafd23f1a29bb914fbf91a8464515826bb6a9f609800781182"
+    IMGPROXY_SALT = "3dae9fbe7138431c57d59625d19175901df23786b1b8b6c65a39a0ac26f344809478bb3c7f6a838a1a45dbe123f85a16d8ce74c2f595cbf61d12a8470c588201"
+
+    imgProxy = ImgProxy(IMGPROXY_KEY, IMGPROXY_SALT, IMGPROXY_URL)
+    resizedImageUrl = imgProxy.generate("http://example.com/images/curiosity.jpg", {
+        "s": "800:500", "rt": "fit", "g": "sm", "q": 70, "sm": "true"}, "jpeg")
+
+    print(resizedImageUrl)
---
+++ packages/mkimgproxy/mkimgproxy-0.2.tar.gz/mkimgproxy-0.2/mkimgproxy/__init__.py
@@ -0,0 +1,35 @@
+"""Generates URL for imgproxy image processing server"""
+
+__version__ = '0.2'
+
+import hmac
+import hashlib
+import base64
+
+
+class ImgProxy:
+    def __init__(self, key, salt, server_url):
+        self._key = bytes.fromhex(key)
+        self._salt = bytes.fromhex(salt)
+        self.server_url = server_url
+
+    def generate(self, url, processing_options, extension):
+        b64_url = base64.urlsafe_b64encode(
+            str.encode(url)).rstrip(b'=').decode()
+
+        processing_options_str = ""
+        for command in processing_options:
+            processing_options_str += f"{command}:{processing_options[command]}/"
+
+        print(processing_options_str)
+        path = f'/{processing_options_str}/{b64_url}'
+        if extension is not None:
+            path += f'.{extension}'
+
+        sha256_hmac = hmac.new(self._key, self._salt +
+                               str.encode(path), digestmod=hashlib.sha256)
+
+        protection = base64.urlsafe_b64encode(
+            sha256_hmac.digest()).rstrip(b'=').decode()
+
+        return f'{self.server_url}/{protection}{path}'
