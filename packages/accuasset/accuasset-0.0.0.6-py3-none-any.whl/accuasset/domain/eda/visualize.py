5bfb7eb3352735c2077e1afeb029e32718cb2ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:15 +0000

    Add package accuasset/0.0.0.6/accuasset-0.0.0.6-py3-none-any.whl

---
+++ packages/accuasset/accuasset-0.0.0.6-py3-none-any.whl/accuasset/domain/eda/visualize.py
@@ -0,0 +1,35 @@
+from wordcloud import WordCloud
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import os
+
+
+def test():
+    print('\n', )
+    print('domain > eda에 visualize입니다.')
+
+
+
+FILE = os.path.dirname(__file__)
+FONT_PATH = os.environ.get('FONT_PATH', os.path.join(FILE, 'NanumGothic.ttf'))
+
+def get_cloud(df, target_column, conditional_value='', conditional_column='all'):
+
+    if conditional_column=='all':
+        text = df[target_column].to_list()
+    else:
+        text = df[df[conditional_column]==conditional_value][target_column].to_list()
+
+    count = Counter(text)
+    words = dict(count.most_common())
+
+    plt.figure(figsize=(14,10)) #이미지 사이즈 지정
+
+    wordcloud = WordCloud(font_path=FONT_PATH, background_color='white').generate_from_frequencies(words)
+
+    plt.imshow(wordcloud,interpolation='lanczos')
+    plt.axis('off')
+    #plt.set_title(str(target_column), fontsize=20)
+    plt.show()
