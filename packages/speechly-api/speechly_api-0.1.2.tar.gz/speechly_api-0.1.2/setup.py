67ba08f639327b46574adb453abe6f88e212a7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:33 +0000

    Add package speechly-api/0.1.2/speechly_api-0.1.2.tar.gz

---
+++ packages/speechly-api/speechly_api-0.1.2.tar.gz/speechly_api-0.1.2/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+
+from setuptools import setup, find_namespace_packages
+
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='speechly_api',
+    version='0.1.2',
+    description='Speechly Public Protobuf Stubs',
+    include_package_data=True,
+    install_requires=['grpcio>1.30', 'protobuf>3.14'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/speechly/api/tree/master/python',
+    author='Speechly',
+    keywords=['speech', 'asr', 'language', 'nlp'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_namespace_packages(where='src/', include='*'),
+    package_dir={
+        '': 'src'
+    },
+    python_requires='>=3.6',
+    project_urls={
+        'Speechly': 'https://www.speechly.com/',
+        'Source': 'https://github.com/speechly/api'
+    }
+)
