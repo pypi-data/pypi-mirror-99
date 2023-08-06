fb9c114f4e76505a106185bcd67d277fb1cf9742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:27 +0000

    Add package xialib/0.3.2/xialib-0.3.2-py3-none-any.whl

---
+++ packages/xialib/xialib-0.3.2-py3-none-any.whl/xialib/__init__.py
@@ -0,0 +1,52 @@
+from xialib import adaptors
+from xialib import archivers
+from xialib import controllers
+from xialib import decoders
+from xialib import depositors
+from xialib import flowers
+from xialib import formatters
+from xialib import publishers
+from xialib import storers
+from xialib import subscribers
+from xialib import translators
+
+from xialib.adaptors import SQLiteAdaptor, JsonAdaptor
+from xialib.archivers import IoListArchiver
+from xialib.controllers import BasicController
+from xialib.decoders import BasicDecoder, ZipDecoder
+from xialib.depositors import FileDepositor
+from xialib.formatters import BasicFormatter, CSVFormatter, ZstFormatter
+from xialib.flowers import BasicFlower, SegmentFlower
+from xialib.publishers import BasicPublisher
+from xialib.storers import BasicStorer
+from xialib.subscribers import BasicSubscriber
+from xialib.translators import BasicTranslator, SapTranslator
+
+from xialib.adaptor import Adaptor, DbapiAdaptor, DbapiQmarkAdaptor
+from xialib.archiver import Archiver, ListArchiver
+from xialib.controller import Controller
+from xialib.decoder import Decoder
+from xialib.depositor import Depositor
+from xialib.flower import Flower
+from xialib.formatter import Formatter
+from xialib.publisher import Publisher
+from xialib.storer import Storer, RWStorer, IOStorer
+from xialib.subscriber import Subscriber
+from xialib.translator import Translator
+
+from xialib.service import Service, service_factory, backlog, secret_composer
+
+__all__ = \
+    adaptors.__all__ + \
+    archivers.__all__ + \
+    controllers.__all__ + \
+    decoders.__all__ + \
+    depositors.__all__ + \
+    flowers.__all__ + \
+    formatters.__all__ + \
+    publishers.__all__ + \
+    storers.__all__ + \
+    subscribers.__all__ + \
+    translators.__all__
+
+__version__ = "0.3.2"
