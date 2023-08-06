19fe5399b71ce10a5f1065b8d4c8dd217ba7503a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:26 +0000

    Add package socatlord/1.2b1/socatlord-1.2b1-py2.py3-none-any.whl

---
+++ packages/socatlord/socatlord-1.2b1-py2.py3-none-any.whl/socatlord/parse_config.py
@@ -0,0 +1,36 @@
+from satella.files import read_in_file
+import sys
+
+def parse_etc_socatlord():
+    try:
+        data = read_in_file('/etc/socatlord', 'utf-8').replace('\r\n', '\n').split('\n')
+        data = (line.strip() for line in data)
+        data = (line for line in data if line)
+        data = (line for line in data if not line.startswith('#'))
+        data = (line.split('->') for line in data)
+        data = ((host1.strip(), host2.strip()) for host1, host2 in data)
+
+        for host1, host2 in data:
+            if host1.startswith('udp '):
+                proto = 'udp'
+                host1 = host1.replace('udp ', '')
+            else:
+                if host1.startswith('tcp '):
+                    host1 = host1.replace('tcp ', '')
+                proto = 'tcp'
+
+            if ':' not in host1:
+                port1 = int(host1)
+                host1 = '0.0.0.0'
+            else:
+                host1, port1 = host1.split(':')
+                port1 = int(port1)
+
+            host2, port2 = host2.split(':')
+            port2 = int(port2)
+
+            yield proto, host1, port1, host2, port2
+
+    except (KeyError,ValueError,TypeError) as e:
+        print('Error parsing /etc/socatlord: %s' % (e, ))
+        sys.exit(1)
---
+++ packages/socatlord/socatlord-1.2b1-py2.py3-none-any.whl/socatlord/run.py
@@ -0,0 +1,108 @@
+import pkg_resources
+from satella.files import write_to_file, read_in_file
+from .parse_config import parse_etc_socatlord
+import subprocess
+import os
+import sys
+from . import __version__
+
+
+verbose = False
+
+
+def kill_all_socats():
+    global verbose
+    for socat in os.listdir('/var/run/socatlord'):
+        path = os.path.join('/var/run/socatlord', socat)
+        pid = int(read_in_file(path, 'utf-8'))
+        try:
+            if verbose:
+                print('Killing %s' % (pid, ))
+            os.kill(pid, 9)
+            if verbose:
+                print('Killed %s OK' % (pid, ))
+        except PermissionError:
+            print('Failed to kill %s with EPERM' % (pid, ))
+        except OSError:
+            print('Failed to kill %s' % (pid, ))
+        os.unlink(path)
+
+
+def print_help():
+    print('''Socatlord v%s by Dronhub Innovations sp. z o. o.
+Manage multiple instances of socat with a single configuration file.
+Configuration file location: /etc/socatlord
+ 
+Usage:
+
+    socatlord install - installs itself as a systemd service
+    socatlord run - stop all currently running socats and launch them anew
+    socatlord stop - stop all currently running socats
+    socatlord help - display this message
+    
+You can also run this as a systemd service, eg. 
+    systemctl start socatlord.service
+    systemctl stop socatlord.service
+    systemctl restart socatlord.service
+''' % (__version__, ))
+
+
+def run():
+    global verbose
+
+    verbose = '-v' in sys.argv
+    if verbose:
+        del sys.argv[sys.argv.index('-v')]
+
+    if not os.path.exists('/etc/socatlord'):
+        write_to_file('/etc/socatlord', b'''# Put your redirections here
+# eg. 
+# 443 -> 192.168.1.1:443
+# will redirect all TCP traffic that comes to this host (0.0.0.0) to specified host and port
+# to redirect UDP traffic just prefix your config with udp, eg.
+# udp 443 -> 192.168.1.1:443
+# You can additionally specify explicit interfaces to listen on eg.
+# 192.168.1.2:443 -> 192.168.1.1:443
+''')
+        if verbose:
+            print('/etc/socatlord created')
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'install':
+            filename = pkg_resources.resource_filename(__name__, 'systemd/socatlord.service')
+            contents = read_in_file(filename, 'utf-8')
+            write_to_file('/lib/systemd/system/socatlord.service', contents, 'utf-8')
+            os.system('systemctl daemon-reload')
+            os.system('systemctl enable socatlord.service')
+        elif sys.argv[1] == 'stop':
+            kill_all_socats()
+        elif sys.argv[1] == 'run':
+
+            if not os.path.exists('/var/run/socatlord'):
+                os.mkdir('/var/run/socatlord')
+            os.chmod(0o600, '/var/run/socatlord')
+
+            kill_all_socats()
+
+            for i, row in enumerate(parse_etc_socatlord()):
+                proto, host1, port1, host2, port2 = row
+                command = ['socat', '%s-listen:%s,bind=%s,reuseaddr,fork' % (proto, port1, host1),
+                           '%s:%s:%s' % (proto, host2, port2)]
+                kwargs = {'stdin': subprocess.DEVNULL, 'stdout': subprocess.DEVNULL,
+                        'stderr': subprocess.DEVNULL}
+                if verbose:
+                    print('Calling %s' % (command, ))
+                    kwargs = {}
+                proc = subprocess.Popen(command, **kwargs)
+                write_to_file(os.path.join('/var/run/socatlord', str(i)), str(proc.pid), 'utf-8')
+        elif sys.argv[1] == 'help':
+            print_help()
+        else:
+            print('''Unknown command''')
+            print_help()
+            sys.exit(1)
+    else:
+        print_help()
+    sys.exit(0)
+
+
