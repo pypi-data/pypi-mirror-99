e2a7032b3cf4942ffda665ee3b2828ef4e16ff51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:26 +0000

    Add package bosch-thermostat-client/0.11.4/bosch_thermostat_client-0.11.4-py3-none-any.whl

---
+++ packages/bosch-thermostat-client/bosch_thermostat_client-0.11.4-py3-none-any.whl/bosch_thermostat_client/connectors/ivt.py
@@ -0,0 +1,59 @@
+"""XMPP Connector to talk to bosch."""
+import aioxmpp
+from bosch_thermostat_client.const import PUT, GET, USER_AGENT, CONTENT_TYPE, APP_JSON
+from bosch_thermostat_client.const.ivt import TELEHEATER
+from .xmpp import XMPPBaseConnector
+
+
+class IVTXMPPConnector(XMPPBaseConnector):
+    _accesskey_prefix = "C6u9jPue_"
+    _rrc_contact_prefix = "contact_"
+    _rrc_gateway_prefix = "gateway_"
+    xmpp_host = "wa2-mz36-qrmzh6.bosch.de"
+
+    def __init__(self, host, loop, access_key, encryption):
+        """IVTConnector constructor
+
+        Args:
+            host (str): serialnumber of target
+            loop (loop): asyncio loop
+            access_key (str): access key to bosch
+            encryption (obj): Encryption object
+        """
+        self._seqno = 1
+        super().__init__(host, loop, access_key, encryption)
+
+    def _build_message(self, method, path, data=None):
+        if not path:
+            return
+        msg = aioxmpp.stanza.Message(
+            to=self._to,
+            type_=aioxmpp.MessageType.CHAT,
+        )
+        msg.autoset_id()
+        if method == GET:
+            body = "\r\r".join(
+                [
+                    f"GET {path} HTTP/1.1",
+                    f"{USER_AGENT}: {TELEHEATER}",
+                    f"Seq-No: {self._seqno}",
+                    "\r\r",
+                ]
+            )
+        elif method == PUT and data:
+            body = "\r\r".join(
+                [
+                    f"PUT {path} HTTP/1.1",
+                    f"{USER_AGENT}: {TELEHEATER}",
+                    f"{CONTENT_TYPE}: {APP_JSON}",
+                    f"Content-Length: {len(data)}",
+                    f"Seq-No: {self._seqno}",
+                    "",
+                    data.decode("utf-8"),
+                ]
+            )
+        else:
+            return
+        msg.body[None] = body
+        self._seqno += 1
+        return msg
---
+++ packages/bosch-thermostat-client/bosch_thermostat_client-0.11.4-py3-none-any.whl/bosch_thermostat_client/gateway/ivt_gateway.py
@@ -0,0 +1,88 @@
+"""Gateway module connecting to Bosch thermostat."""
+
+import logging
+import json
+from .base_gateway import BaseGateway
+from bosch_thermostat_client.const import (
+    GATEWAY,
+    MODELS,
+    EMS,
+    SYSTEM_BUS,
+    VALUE,
+    VALUES,
+)
+from bosch_thermostat_client.const.ivt import IVT, SYSTEM_INFO, CAN
+from bosch_thermostat_client.connectors import connector_ivt_chooser
+from bosch_thermostat_client.encryption import IVTEncryption as Encryption
+from bosch_thermostat_client.exceptions import DeviceException
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class IVTGateway(BaseGateway):
+    """IVT Gateway"""
+
+    device_type = IVT
+
+    def __init__(
+        self, session, session_type, host, access_token, access_key=None, password=None
+    ):
+        """IVT Gateway constructor
+
+        Args:
+            session (loop): loop or websession
+            session_type (str): either HTTP or XMPP
+            host (str): host IP or hostname for HTTP or serial number for XMPP
+            access_key (str): access key to Bosch Gateway
+            password (str, optional): Password to Bosch Gateway. Defaults to None.
+        """
+        self._access_token = access_token.replace("-", "")
+        if password:
+            access_key = self._access_token
+        Connector = connector_ivt_chooser(session_type)
+        self._connector = Connector(
+            host=host,
+            loop=session,
+            access_key=self._access_token,
+            encryption=Encryption(access_key, password),
+        )
+        super().__init__(host)
+
+    async def _update_info(self, initial_db):
+        """Update gateway info from Bosch device."""
+        for name, uri in initial_db.items():
+            try:
+                response = await self._connector.get(uri)
+                if VALUE in response:
+                    self._data[GATEWAY][name] = response[VALUE]
+                elif name == SYSTEM_INFO:
+                    self._data[GATEWAY][SYSTEM_INFO] = response.get(VALUES, [])
+            except DeviceException as err:
+                _LOGGER.debug("Can't fetch data for update_info %s", err)
+                pass
+
+    async def get_device_model(self, _db):
+        """Find device model."""
+        system_bus = self._data[GATEWAY].get(SYSTEM_BUS)
+        model_scheme = _db[MODELS]
+        if system_bus == CAN:
+            self._bus_type = CAN
+            return model_scheme.get(CAN)
+        self._bus_type = EMS
+        system_info = self._data[GATEWAY].get(SYSTEM_INFO)
+        attached_devices = {}
+        if system_info:
+            for info in system_info:
+                _id = info.get("Id", -1)
+                model = model_scheme.get(_id)
+                if model is not None:
+                    _LOGGER.debug("Found supported device %s with id %s", model, _id)
+                    attached_devices[_id] = model
+            if attached_devices:
+                found_model = attached_devices[sorted(attached_devices.keys())[0]]
+                _LOGGER.debug("Using model %s as database schema", found_model[VALUE])
+                return found_model
+        _LOGGER.error(
+            "I cannot find supported device. Your devices: %s", json.dumps(system_info)
+        )
