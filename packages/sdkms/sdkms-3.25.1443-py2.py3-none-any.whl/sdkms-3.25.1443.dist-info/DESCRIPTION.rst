e55e881cf206d773be41c80d459e708863f2264a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:59 +0000

    Add package sdkms/3.25.1443/sdkms-3.25.1443-py2.py3-none-any.whl

---
+++ packages/sdkms/sdkms-3.25.1443-py2.py3-none-any.whl/sdkms-3.25.1443.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This is a set of REST APIs for accessing the Fortanix Self-Defending Key Management System. This includes APIs for managing accounts, and for performing cryptographic and key management operations. 
+
+
---
+++ packages/sdkms/sdkms-3.25.1443-py2.py3-none-any.whl/sdkms/v1/apis/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .aws_api import AWSApi
+from .accounts_api import AccountsApi
+from .approval_requests_api import ApprovalRequestsApi
+from .apps_api import AppsApi
+from .authentication_api import AuthenticationApi
+from .child_account_users_api import ChildAccountUsersApi
+from .child_accounts_api import ChildAccountsApi
+from .digest_api import DigestApi
+from .encryption_and_decryption_api import EncryptionAndDecryptionApi
+from .groups_api import GroupsApi
+from .logs_api import LogsApi
+from .plugins_api import PluginsApi
+from .security_objects_api import SecurityObjectsApi
+from .sign_and_verify_api import SignAndVerifyApi
+from .stats_api import StatsApi
+from .two_factor_authentication_api import TwoFactorAuthenticationApi
+from .users_api import UsersApi
+from .wrapping_and_unwrapping_api import WrappingAndUnwrappingApi
