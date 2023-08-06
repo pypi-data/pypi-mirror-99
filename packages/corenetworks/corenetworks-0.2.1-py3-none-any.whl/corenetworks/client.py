1d497b592e02e6e757b95e49f446112c6ef0af9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:56 +0000

    Add package corenetworks/0.2.1/corenetworks-0.2.1-py3-none-any.whl

---
+++ packages/corenetworks/corenetworks-0.2.1-py3-none-any.whl/corenetworks/client.py
@@ -0,0 +1,406 @@
+# -*- coding: utf-8 -*-
+"""API  client."""
+
+import copy
+import json
+import os
+from collections import defaultdict
+
+import jsonschema
+from requests import ConnectionError
+from requests import HTTPError
+from requests import Request
+from requests import Session
+from six import iteritems
+
+import corenetworks
+
+from .authenticators import CoreNetworksBasicAuth
+from .authenticators import CoreNetworksTokenAuth
+from .exceptions import AuthError
+from .exceptions import CorenetworksError
+from .exceptions import ValidationError
+
+
+class CoreNetworks():
+    """Create authenticated API client."""
+
+    def __init__(self, user=None, password=None, api_token=None, auto_commit=None):
+        self.__endpoint = "https://beta.api.core-networks.de"
+        self.__user_agent = "Core Networks Python API {version}".format(
+            version=corenetworks.__version__
+        )
+        self.config = self._config(user, password, api_token, auto_commit)
+
+        self._schema = {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string"
+                },
+                "ttl": {
+                    "type": "number"
+                },
+                "type": {
+                    "type": "string"
+                },
+                "data": {
+                    "type": "string"
+                },
+            },
+        }
+
+        self._filter_schema = {
+            "type": "object",
+            "properties": {
+                "name": {
+                    "anyOf": [{
+                        "type": "string",
+                    }, {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        },
+                    }],
+                },
+                "ttl": {
+                    "anyOf": [{
+                        "type": "number",
+                    }, {
+                        "type": "array",
+                        "items": {
+                            "type": "number"
+                        },
+                    }],
+                },
+                "type": {
+                    "anyOf": [{
+                        "type": "string",
+                    }, {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        },
+                    }],
+                },
+                "data": {
+                    "anyOf": [{
+                        "type": "string",
+                    }, {
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        },
+                    }],
+                },
+            },
+        }
+
+        if self.config["api_token"]:
+            self._auth = CoreNetworksTokenAuth(self.config["api_token"])
+        else:
+            self._auth = CoreNetworksBasicAuth(
+                self.config["user"], self.config["password"], self.__endpoint
+            )
+
+    @staticmethod
+    def _config(user, password, api_token, auto_commit):
+        cfg = defaultdict(
+            dict, {
+                "user": None,
+                "password": None,
+                "api_token": None,
+                "auto_commit": False
+            }
+        )
+
+        cfg["user"] = os.getenv("CN_API_USER")
+        cfg["password"] = os.getenv("CN_API_PASSWORD")
+        cfg["api_token"] = os.getenv("CN_API_TOKEN")
+
+        cfg["user"] = user or cfg["user"]
+        cfg["password"] = password or cfg["password"]
+        cfg["api_token"] = api_token or cfg["api_token"]
+        cfg["auto_commit"] = auto_commit or cfg["auto_commit"]
+
+        if not (cfg["user"] and cfg["password"]) and not cfg["api_token"]:
+            raise AuthError("Insufficient authentication details provided")
+
+        return cfg
+
+    # ZONES
+
+    def zones(self):
+        """
+        Get the list of DNS zones.
+
+        Returns:
+            list: List of zones.
+
+        """
+        result = self.__rest_helper("/dnszones/", method="GET")
+
+        return self.__normalize(result)
+
+    def zone(self, zone):
+        """
+        Get details about a DNS zone.
+
+        Args:
+            zone (str): Name of the target DNS zone.
+
+        Returns:
+            list: List of zones.
+
+        """
+        result = self.__rest_helper("/dnszones/{zone}".format(zone=zone), method="GET")
+
+        return self.__normalize(result)
+
+    # RECORDS
+
+    def records(self, zone, params={}):
+        """
+        Get the list of records for the specific domain.
+
+        Args:
+            zone (str): Name of the target DNS zone.
+            params (dict): Dictionary of filter parameters.
+                See https://beta.api.core-networks.de/doc/#functon_dnszones_records
+                but keep in mind that you have to pass a dict not a string. The required
+                filter string will be created automatically.
+
+                Example: `params={"type": ["NS", "SOA"]}` will result in
+                `filter=?type[]=NS&type[]=SOA`
+
+        Returns:
+            list: List of matching records.
+
+        """
+        schema = copy.deepcopy(self._filter_schema)
+        self.__validate(params, schema)
+
+        filter_string = self.__json_to_filter(params)
+        result = self.__rest_helper(
+            "/dnszones/{zone}/records/{filter}".format(zone=zone, filter=filter_string),
+            method="GET"
+        )
+
+        return self.__normalize(result)
+
+    def add_record(self, zone, params):
+        """
+        Create a record for the given domain.
+
+        Args:
+            zone (str): Name of the target DNS zone.
+            params (dict): Dictionary of record parameters.
+                See https://beta.api.core-networks.de/doc/#functon_dnszones_records_add
+
+        Returns:
+            list: List of added records.
+
+        """
+        schema = copy.deepcopy(self._schema)
+        schema["required"] = ["name", "type", "data", "ttl"]
+        self.__validate(params, schema)
+
+        if params["type"] == "CNAME":
+            if params["name"] == "@":
+                raise CorenetworksError("CNAME records are not allowed for the zone itself.")
+
+        records = self.records(zone, params={"name": params["name"]})
+        for r in records:
+            if params["type"] == "CNAME" and r["type"] != "CNAME":
+                raise CorenetworksError(
+                    "A record with the same name already exist ({name}). "
+                    "CNAME records cannot use the same name with other records.".format(
+                        name=r["name"]
+                    )
+                )
+            if params["type"] != "CNAME" and r["type"] == "CNAME":
+                raise CorenetworksError(
+                    "A CNAME record with the same name already exist ({name}). "
+                    "CNAME records cannot use the same name with other records.".format(
+                        name=r["name"]
+                    )
+                )
+            if params["type"] == "CNAME" and r["data"] != params["data"]:
+                self._delete_record_raw(zone, params=r)
+
+        curr = copy.deepcopy(params)
+        curr.pop("ttl")
+        records = self.records(zone, curr)
+
+        if len(records) > 1:
+            raise CorenetworksError(
+                "More than one record already exists for the given attributes. "
+                "That should be impossible, please open an issue!"
+            )
+
+        # delete existing record with different ttl for a fake update
+        for r in records:
+            r["ttl"] = int(r["ttl"])
+            if r["ttl"] != params["ttl"]:
+                self._delete_record_raw(zone, params=r)
+
+        self.__rest_helper(
+            "/dnszones/{zone}/records/".format(zone=zone), data=params, method="POST"
+        )
+
+        result = self.records(zone=zone, params=params)
+
+        if self.config["auto_commit"]:
+            self.commit(zone=zone)
+
+        return self.__normalize(result)
+
+    def _delete_record_raw(self, zone, params):
+        r = self.__rest_helper(
+            "/dnszones/{zone}/records/delete".format(zone=zone), data=params, method="POST"
+        )
+
+        return r
+
+    def delete_record(self, zone, params):
+        """
+        Delete all DNS records of a zone that match the data.
+
+        Args:
+            zone (str): Name of the target DNS zone.
+            params (dict): Dictionary of record parameters.
+                See https://beta.api.core-networks.de/doc/#functon_dnszones_records_add
+
+        Returns:
+            list: Empty list.
+
+        """
+        schema = copy.deepcopy(self._schema)
+        schema["properties"]["force_all"] = {"type": "boolean"}
+        schema["anyOf"] = [{
+            "required": ["name"]
+        }, {
+            "required": ["type"]
+        }, {
+            "required": ["data"]
+        }, {
+            "required": ["force_all"]
+        }]
+        self.__validate(params, schema)
+
+        if params.get("force_all"):
+            params = {}
+
+        result = self._delete_record_raw(zone, params)
+
+        if self.config["auto_commit"]:
+            self.commit(zone=zone)
+
+        return self.__normalize(result)
+
+    def commit(self, zone):
+        """
+        Commit changed records to the given DNS zone.
+
+        Args:
+            zone (str): Name of the target DNS zone.
+
+        Returns:
+            list: Empty list.
+
+        """
+        result = self.__rest_helper(
+            "/dnszones/{zone}/records/commit".format(zone=zone), method="POST"
+        )
+
+        return self.__normalize(result)
+
+    def __rest_helper(self, url, data=None, params=None, method="GET"):
+        """Handle requests to the Core Networks API."""
+        url = self.__endpoint + url
+        headers = {
+            "User-Agent": self.__user_agent,
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+
+        if data:
+            json_data = json.dumps(data)
+        else:
+            json_data = None
+
+        request = Request(
+            method=method,
+            url=url,
+            headers=headers,
+            data=json_data,
+            params=params,
+            auth=self._auth
+        )
+
+        prepared_request = request.prepare()
+
+        r_json, r_headers = self.__request_helper(prepared_request)
+
+        return r_json
+
+    @staticmethod
+    def __request_helper(request):
+        """Handle firing off requests and exception raising."""
+        try:
+            session = Session()
+            handle = session.send(request)
+
+            handle.raise_for_status()
+        except HTTPError as e:
+            raise CorenetworksError(
+                "Invalid response: {code} {reason}".format(
+                    code=e.response.status_code, reason=e.response.reason
+                ),
+                payload=e
+            )
+        except ConnectionError:
+            raise
+
+        if handle.status_code == 200:
+            response = handle.json()
+        else:
+            response = []
+
+        return response, handle.headers
+
+    @staticmethod
+    def __normalize(result):
+        if isinstance(result, list):
+            return [el for el in result]
+        elif isinstance(result, dict):
+            return [result]
+        else:
+            raise CorenetworksError("Unknown type: {}".format(type(result)))
+
+    @staticmethod
+    def __json_to_filter(data):
+        filter_list = []
+
+        for (key, value) in iteritems(data):
+            if isinstance(value, list):
+                for item in value:
+                    filter_list.append("{key}[]={value}".format(key=key, value=item))
+            elif isinstance(value, str) or isinstance(value, int):
+                filter_list.append("{key}={value}".format(key=key, value=value))
+            else:
+                raise CorenetworksError("Unknown type: {}".format(type(value)))
+
+        filter_string = "&".join(filter_list)
+
+        if filter_string:
+            filter_string = "?{filter}".format(filter=filter_string)
+
+        return filter_string
+
+    @staticmethod
+    def __validate(data, schema):
+        try:
+            jsonschema.validate(data, schema)
+        except jsonschema.exceptions.ValidationError as e:
+            raise ValidationError("Dataset invalid: {reason}".format(reason=e.message), payload=e)
---
+++ packages/corenetworks/corenetworks-0.2.1-py3-none-any.whl/corenetworks/test/unit/test_client.py
@@ -0,0 +1,193 @@
+"""Test client class."""
+
+import pytest
+import requests
+from six.moves.urllib.parse import parse_qs  # noqa
+from six.moves.urllib.parse import unquote  # noqa
+
+from corenetworks import CoreNetworks
+from corenetworks.authenticators import CoreNetworksBasicAuth
+from corenetworks.exceptions import AuthError
+from corenetworks.exceptions import CorenetworksError
+from corenetworks.exceptions import ValidationError
+from corenetworks.test.fixtures.callback import records_error_callback
+from corenetworks.test.fixtures.callback import records_get_callback
+from corenetworks.test.fixtures.callback import records_post_callback
+
+
+@pytest.fixture
+def client(mocker):
+    mocker.patch.object(CoreNetworksBasicAuth, "_login", return_value=("testtoken", 3600))
+    client = CoreNetworks(user="testuser", password="testpass")
+
+    return client
+
+
+def test_auth_error(requests_mock):
+    with pytest.raises(AuthError) as e:
+        assert CoreNetworks(user="test")
+    assert str(e.value) == "Insufficient authentication details provided"
+
+
+def test_request_error(requests_mock, client):
+    requests_mock.post(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/commit",
+        text=records_error_callback,
+    )
+
+    with pytest.raises(requests.ConnectionError):
+        assert client.commit(zone="example.com")
+
+
+def test_zones(requests_mock, client):
+    requests_mock.get(
+        "https://beta.api.core-networks.de/dnszones/",
+        text='[{"name":"example.com","type":"master"}]'
+    )
+
+    resp = client.zones()
+    assert resp == [{"name": "example.com", "type": "master"}]
+
+
+def test_zone(requests_mock, client):
+    requests_mock.get(
+        "https://beta.api.core-networks.de/dnszones/example.com",
+        json={
+            "active": True,
+            "dnssec": True,
+            "name": "example.com",
+            "type": "master"
+        }
+    )
+
+    resp = client.zone("example.com")
+    assert resp == [{"active": True, "dnssec": True, "name": "example.com", "type": "master"}]
+
+
+def test_records(requests_mock, client):
+    requests_mock.get(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/",
+        text=records_get_callback
+    )
+
+    exp = [{
+        "type": "A",
+        "ttl": 1800,
+        "name": "test",
+        "data": "127.0.0.1"
+    }, {
+        "type": "AAAA",
+        "ttl": 1800,
+        "name": "test",
+        "data": "::1"
+    }]
+
+    resp = client.records(zone="example.com")
+    assert resp == exp
+
+
+def test_no_records(requests_mock, client):
+
+    requests_mock.get(
+        "https://beta.api.core-networks.de/dnszones/missing/records/",
+        text=records_get_callback,
+    )
+
+    with pytest.raises(CorenetworksError) as e:
+        assert client.records(zone="missing")
+    assert str(e.value) == "Invalid response: 404 None"
+
+
+def test_type_records(requests_mock, client):
+
+    requests_mock.get(
+        "https://beta.api.core-networks.de/dnszones/dict/records/",
+        text=records_get_callback,
+    )
+
+    dictresp = client.records(zone="dict")
+    assert dictresp == [{}]
+
+
+def test_filter_records(requests_mock, client):
+    requests_mock.get(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/",
+        text=records_get_callback,
+    )
+
+    resp = client.records(zone="example.com", params={"type": ["A"]})
+    assert resp == [{"type": "A", "ttl": 1800, "name": "test", "data": "127.0.0.1"}]
+
+
+def test_add_record(requests_mock, client, mocker):
+    requests_mock.post(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/",
+        text=records_post_callback,
+    )
+    requests_mock.get(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/",
+        text=records_get_callback,
+    )
+
+    record = {"type": "A", "ttl": 1800, "name": "test", "data": "127.0.0.1"}
+
+    resp = client.add_record(zone="example.com", params=record)
+    assert resp == [{"type": "A", "ttl": 1800, "name": "test", "data": "127.0.0.1"}]
+
+    mocker.patch.object(client, "commit")
+    client.config["auto_commit"] = True
+    client.add_record(zone="example.com", params=record)
+    client.commit.assert_called_once_with(zone="example.com")
+
+
+def test_delete_record(requests_mock, client, mocker):
+    requests_mock.post(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/delete",
+        text=records_post_callback,
+    )
+
+    filtered = client.delete_record(zone="example.com", params={
+        "type": "A",
+    })
+    assert filtered == []
+
+    forced = client.delete_record(zone="example.com", params={
+        "force_all": True,
+    })
+    assert forced == []
+
+    mocker.patch.object(client, "commit")
+    client.config["auto_commit"] = True
+    client.delete_record(zone="example.com", params={
+        "type": "A",
+    })
+    client.commit.assert_called_once_with(zone="example.com")
+
+
+def test_delete_record_invalid(requests_mock, client):
+    requests_mock.post(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/delete",
+        text=records_post_callback,
+    )
+
+    with pytest.raises(ValidationError) as wrong:
+        assert client.delete_record(zone="example.com", params={"wrong": "attr"})
+    assert str(wrong.value).startswith("Dataset invalid:")
+
+    with pytest.raises(ValidationError) as ntype:
+        assert client.delete_record(zone="example.com", params={"type": 1})
+    assert str(ntype.value).startswith("Dataset invalid:")
+
+    with pytest.raises(ValidationError) as empty:
+        assert client.delete_record(zone="example.com", params={})
+    assert str(empty.value).startswith("Dataset invalid:")
+
+
+def test_commit(requests_mock, client):
+    requests_mock.post(
+        "https://beta.api.core-networks.de/dnszones/example.com/records/commit",
+        text=records_post_callback,
+    )
+
+    resp = client.commit(zone="example.com")
+    assert resp == []
