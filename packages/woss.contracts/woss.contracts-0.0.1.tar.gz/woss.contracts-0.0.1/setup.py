fa18b71c75f5eb30b6070056203a3794dc9f888a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:25 +0000

    Add package woss.contracts/0.0.1/woss.contracts-0.0.1.tar.gz

---
+++ packages/woss.contracts/woss.contracts-0.0.1.tar.gz/woss.contracts-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['woss', 'woss.contracts']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'woss.contracts',
+    'version': '0.0.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Anderson Carlos Woss',
+    'author_email': 'acwoss@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
