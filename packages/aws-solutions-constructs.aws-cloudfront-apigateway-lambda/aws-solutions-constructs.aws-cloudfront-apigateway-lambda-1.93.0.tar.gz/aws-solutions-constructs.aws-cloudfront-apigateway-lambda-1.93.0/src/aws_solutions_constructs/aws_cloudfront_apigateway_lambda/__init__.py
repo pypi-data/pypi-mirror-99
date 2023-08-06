46405fc125141059c0cbe9646003521cbbaec329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:48 +0000

    Add package aws-solutions-constructs.aws-cloudfront-apigateway-lambda/1.93.0/aws-solutions-constructs.aws-cloudfront-apigateway-lambda-1.93.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-cloudfront-apigateway-lambda/aws-solutions-constructs.aws-cloudfront-apigateway-lambda-1.93.0.tar.gz/aws-solutions-constructs.aws-cloudfront-apigateway-lambda-1.93.0/src/aws_solutions_constructs/aws_cloudfront_apigateway_lambda/__init__.py
@@ -0,0 +1,339 @@
+'''
+# aws-cloudfront-apigateway-lambda module
+
+<!--BEGIN STABILITY BANNER-->---
+
+
+![Stability: Experimental](https://img.shields.io/badge/stability-Experimental-important.svg?style=for-the-badge)
+
+> All classes are under active development and subject to non-backward compatible changes or removal in any
+> future version. These are not subject to the [Semantic Versioning](https://semver.org/) model.
+> This means that while you may use them, you may need to update your source code when upgrading to a newer version of this package.
+
+---
+<!--END STABILITY BANNER-->
+
+| **Reference Documentation**:| <span style="font-weight: normal">https://docs.aws.amazon.com/solutions/latest/constructs/</span>|
+|:-------------|:-------------|
+
+<div style="height:8px"></div>
+
+| **Language**     | **Package**        |
+|:-------------|-----------------|
+|![Python Logo](https://docs.aws.amazon.com/cdk/api/latest/img/python32.png) Python|`aws_solutions_constructs.aws_cloudfront_apigateway_lambda`|
+|![Typescript Logo](https://docs.aws.amazon.com/cdk/api/latest/img/typescript32.png) Typescript|`@aws-solutions-constructs/aws-cloudfront-apigateway-lambda`|
+|![Java Logo](https://docs.aws.amazon.com/cdk/api/latest/img/java32.png) Java|`software.amazon.awsconstructs.services.cloudfrontapigatewaylambda`|
+
+This AWS Solutions Construct implements an AWS CloudFront fronting an Amazon API Gateway Lambda backed REST API.
+
+Here is a minimal deployable pattern definition in Typescript:
+
+```python
+# Example automatically generated without compilation. See https://github.com/aws/jsii/issues/826
+from aws_solutions_constructs.aws_cloudfront_apigateway_lambda import CloudFrontToApiGatewayToLambda
+
+CloudFrontToApiGatewayToLambda(self, "test-cloudfront-apigateway-lambda",
+    lambda_function_props=FunctionProps(
+        code=lambda_.Code.from_asset(f"{__dirname}/lambda"),
+        runtime=lambda_.Runtime.NODEJS_12_X,
+        handler="index.handler"
+    )
+)
+```
+
+## Initializer
+
+```text
+new CloudFrontToApiGatewayToLambda(scope: Construct, id: string, props: CloudFrontToApiGatewayToLambdaProps);
+```
+
+*Parameters*
+
+* scope [`Construct`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.Construct.html)
+* id `string`
+* props [`CloudFrontToApiGatewayToLambdaProps`](#pattern-construct-props)
+
+## Pattern Construct Props
+
+| **Name**     | **Type**        | **Description** |
+|:-------------|:----------------|-----------------|
+|existingLambdaObj?|[`lambda.Function`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.Function.html)|Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.|
+|lambdaFunctionProps?|[`lambda.FunctionProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.FunctionProps.html)|User provided props to override the default props for the Lambda function.|
+|apiGatewayProps?|[`api.LambdaRestApiProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-apigateway.LambdaRestApiProps.html)|Optional user provided props to override the default props for API Gateway|
+|cloudFrontDistributionProps?|[`cloudfront.DistributionProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cloudfront.DistributionProps.html)|Optional user provided props to override the default props for CloudFront Distribution|
+|insertHttpSecurityHeaders?|`boolean`|Optional user provided props to turn on/off the automatic injection of best practice HTTP security headers in all responses from CloudFront|
+|logGroupProps?|[`logs.LogGroupProps`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-logs.LogGroupProps.html)|User provided props to override the default props for for the CloudWatchLogs LogGroup.|
+
+## Pattern Properties
+
+| **Name**     | **Type**        | **Description** |
+|:-------------|:----------------|-----------------|
+|cloudFrontWebDistribution|[`cloudfront.CloudFrontWebDistribution`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-cloudfront.CloudFrontWebDistribution.html)|Returns an instance of cloudfront.CloudFrontWebDistribution created by the construct|
+|edgeLambdaFunctionVersion|[`lambda.Version`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.Version.html)|Returns an instance of the edge Lambda function version created by the pattern.|
+|cloudFrontLoggingBucket|[`s3.Bucket`](https://docs.aws.amazon.com/cdk/api/latest/docs/aws-s3-readme.html)|Returns an instance of the logging bucket for CloudFront WebDistribution.|
+|apiGateway|[`api.RestApi`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-apigateway.RestApi.html)|Returns an instance of the API Gateway REST API created by the pattern.|
+|apiGatewayCloudWatchRole|[`iam.Role`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-iam.Role.html)|Returns an instance of the iam.Role created by the construct for API Gateway for CloudWatch access.|
+|apiGatewayLogGroup|[`logs.LogGroup`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-logs.LogGroup.html)|Returns an instance of the LogGroup created by the construct for API Gateway access logging to CloudWatch.|
+|lambdaFunction|[`lambda.Function`](https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_aws-lambda.Function.html)|Returns an instance of the Lambda function created by the pattern.|
+
+## Default settings
+
+Out of the box implementation of the Construct without any override will set the following defaults:
+
+### Amazon CloudFront
+
+* Configure Access logging for CloudFront WebDistribution
+* Enable automatic injection of best practice HTTP security headers in all responses from CloudFront WebDistribution
+
+### Amazon API Gateway
+
+* Deploy a regional API endpoint
+* Enable CloudWatch logging for API Gateway
+* Configure least privilege access IAM role for API Gateway
+* Set the default authorizationType for all API methods to NONE
+* Enable X-Ray Tracing
+
+### AWS Lambda Function
+
+* Configure limited privilege access IAM role for Lambda function
+* Enable reusing connections with Keep-Alive for NodeJs Lambda function
+* Enable X-Ray Tracing
+* Set Environment Variables
+
+  * AWS_NODEJS_CONNECTION_REUSE_ENABLED (for Node 10.x and higher functions)
+
+## Architecture
+
+![Architecture Diagram](architecture.png)
+
+---
+
+
+© Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+import aws_cdk.aws_apigateway
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_iam
+import aws_cdk.aws_lambda
+import aws_cdk.aws_logs
+import aws_cdk.aws_s3
+import aws_cdk.core
+
+
+class CloudFrontToApiGatewayToLambda(
+    aws_cdk.core.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-solutions-constructs/aws-cloudfront-apigateway-lambda.CloudFrontToApiGatewayToLambda",
+):
+    def __init__(
+        self,
+        scope: aws_cdk.core.Construct,
+        id: builtins.str,
+        *,
+        api_gateway_props: typing.Any = None,
+        cloud_front_distribution_props: typing.Any = None,
+        existing_lambda_obj: typing.Optional[aws_cdk.aws_lambda.Function] = None,
+        insert_http_security_headers: typing.Optional[builtins.bool] = None,
+        lambda_function_props: typing.Optional[aws_cdk.aws_lambda.FunctionProps] = None,
+        log_group_props: typing.Optional[aws_cdk.aws_logs.LogGroupProps] = None,
+    ) -> None:
+        '''
+        :param scope: - represents the scope for all the resources.
+        :param id: - this is a a scope-unique id.
+        :param api_gateway_props: Optional user provided props to override the default props for the API Gateway. Default: - Default props are used
+        :param cloud_front_distribution_props: Optional user provided props to override the default props. Default: - Default props are used
+        :param existing_lambda_obj: Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored. Default: - None
+        :param insert_http_security_headers: Optional user provided props to turn on/off the automatic injection of best practice HTTP security headers in all responses from cloudfront. Default: - true
+        :param lambda_function_props: User provided props to override the default props for the Lambda function. Default: - Default props are used
+        :param log_group_props: User provided props to override the default props for the CloudWatchLogs LogGroup. Default: - Default props are used
+
+        :access: public
+        :since: 0.8.0
+        :summary: Constructs a new instance of the CloudFrontToApiGatewayToLambda class.
+        '''
+        props = CloudFrontToApiGatewayToLambdaProps(
+            api_gateway_props=api_gateway_props,
+            cloud_front_distribution_props=cloud_front_distribution_props,
+            existing_lambda_obj=existing_lambda_obj,
+            insert_http_security_headers=insert_http_security_headers,
+            lambda_function_props=lambda_function_props,
+            log_group_props=log_group_props,
+        )
+
+        jsii.create(CloudFrontToApiGatewayToLambda, self, [scope, id, props])
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="apiGateway")
+    def api_gateway(self) -> aws_cdk.aws_apigateway.RestApi:
+        return typing.cast(aws_cdk.aws_apigateway.RestApi, jsii.get(self, "apiGateway"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="apiGatewayCloudWatchRole")
+    def api_gateway_cloud_watch_role(self) -> aws_cdk.aws_iam.Role:
+        return typing.cast(aws_cdk.aws_iam.Role, jsii.get(self, "apiGatewayCloudWatchRole"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="apiGatewayLogGroup")
+    def api_gateway_log_group(self) -> aws_cdk.aws_logs.LogGroup:
+        return typing.cast(aws_cdk.aws_logs.LogGroup, jsii.get(self, "apiGatewayLogGroup"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="cloudFrontWebDistribution")
+    def cloud_front_web_distribution(self) -> aws_cdk.aws_cloudfront.Distribution:
+        return typing.cast(aws_cdk.aws_cloudfront.Distribution, jsii.get(self, "cloudFrontWebDistribution"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="lambdaFunction")
+    def lambda_function(self) -> aws_cdk.aws_lambda.Function:
+        return typing.cast(aws_cdk.aws_lambda.Function, jsii.get(self, "lambdaFunction"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="cloudFrontLoggingBucket")
+    def cloud_front_logging_bucket(self) -> typing.Optional[aws_cdk.aws_s3.Bucket]:
+        return typing.cast(typing.Optional[aws_cdk.aws_s3.Bucket], jsii.get(self, "cloudFrontLoggingBucket"))
+
+    @builtins.property # type: ignore[misc]
+    @jsii.member(jsii_name="edgeLambdaFunctionVersion")
+    def edge_lambda_function_version(
+        self,
+    ) -> typing.Optional[aws_cdk.aws_lambda.Version]:
+        return typing.cast(typing.Optional[aws_cdk.aws_lambda.Version], jsii.get(self, "edgeLambdaFunctionVersion"))
+
+
+@jsii.data_type(
+    jsii_type="@aws-solutions-constructs/aws-cloudfront-apigateway-lambda.CloudFrontToApiGatewayToLambdaProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "api_gateway_props": "apiGatewayProps",
+        "cloud_front_distribution_props": "cloudFrontDistributionProps",
+        "existing_lambda_obj": "existingLambdaObj",
+        "insert_http_security_headers": "insertHttpSecurityHeaders",
+        "lambda_function_props": "lambdaFunctionProps",
+        "log_group_props": "logGroupProps",
+    },
+)
+class CloudFrontToApiGatewayToLambdaProps:
+    def __init__(
+        self,
+        *,
+        api_gateway_props: typing.Any = None,
+        cloud_front_distribution_props: typing.Any = None,
+        existing_lambda_obj: typing.Optional[aws_cdk.aws_lambda.Function] = None,
+        insert_http_security_headers: typing.Optional[builtins.bool] = None,
+        lambda_function_props: typing.Optional[aws_cdk.aws_lambda.FunctionProps] = None,
+        log_group_props: typing.Optional[aws_cdk.aws_logs.LogGroupProps] = None,
+    ) -> None:
+        '''
+        :param api_gateway_props: Optional user provided props to override the default props for the API Gateway. Default: - Default props are used
+        :param cloud_front_distribution_props: Optional user provided props to override the default props. Default: - Default props are used
+        :param existing_lambda_obj: Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored. Default: - None
+        :param insert_http_security_headers: Optional user provided props to turn on/off the automatic injection of best practice HTTP security headers in all responses from cloudfront. Default: - true
+        :param lambda_function_props: User provided props to override the default props for the Lambda function. Default: - Default props are used
+        :param log_group_props: User provided props to override the default props for the CloudWatchLogs LogGroup. Default: - Default props are used
+
+        :summary: The properties for the CloudFrontToApiGatewayToLambda Construct
+        '''
+        if isinstance(lambda_function_props, dict):
+            lambda_function_props = aws_cdk.aws_lambda.FunctionProps(**lambda_function_props)
+        if isinstance(log_group_props, dict):
+            log_group_props = aws_cdk.aws_logs.LogGroupProps(**log_group_props)
+        self._values: typing.Dict[str, typing.Any] = {}
+        if api_gateway_props is not None:
+            self._values["api_gateway_props"] = api_gateway_props
+        if cloud_front_distribution_props is not None:
+            self._values["cloud_front_distribution_props"] = cloud_front_distribution_props
+        if existing_lambda_obj is not None:
+            self._values["existing_lambda_obj"] = existing_lambda_obj
+        if insert_http_security_headers is not None:
+            self._values["insert_http_security_headers"] = insert_http_security_headers
+        if lambda_function_props is not None:
+            self._values["lambda_function_props"] = lambda_function_props
+        if log_group_props is not None:
+            self._values["log_group_props"] = log_group_props
+
+    @builtins.property
+    def api_gateway_props(self) -> typing.Any:
+        '''Optional user provided props to override the default props for the API Gateway.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("api_gateway_props")
+        return typing.cast(typing.Any, result)
+
+    @builtins.property
+    def cloud_front_distribution_props(self) -> typing.Any:
+        '''Optional user provided props to override the default props.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("cloud_front_distribution_props")
+        return typing.cast(typing.Any, result)
+
+    @builtins.property
+    def existing_lambda_obj(self) -> typing.Optional[aws_cdk.aws_lambda.Function]:
+        '''Existing instance of Lambda Function object, if this is set then the lambdaFunctionProps is ignored.
+
+        :default: - None
+        '''
+        result = self._values.get("existing_lambda_obj")
+        return typing.cast(typing.Optional[aws_cdk.aws_lambda.Function], result)
+
+    @builtins.property
+    def insert_http_security_headers(self) -> typing.Optional[builtins.bool]:
+        '''Optional user provided props to turn on/off the automatic injection of best practice HTTP security headers in all responses from cloudfront.
+
+        :default: - true
+        '''
+        result = self._values.get("insert_http_security_headers")
+        return typing.cast(typing.Optional[builtins.bool], result)
+
+    @builtins.property
+    def lambda_function_props(
+        self,
+    ) -> typing.Optional[aws_cdk.aws_lambda.FunctionProps]:
+        '''User provided props to override the default props for the Lambda function.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("lambda_function_props")
+        return typing.cast(typing.Optional[aws_cdk.aws_lambda.FunctionProps], result)
+
+    @builtins.property
+    def log_group_props(self) -> typing.Optional[aws_cdk.aws_logs.LogGroupProps]:
+        '''User provided props to override the default props for the CloudWatchLogs LogGroup.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("log_group_props")
+        return typing.cast(typing.Optional[aws_cdk.aws_logs.LogGroupProps], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "CloudFrontToApiGatewayToLambdaProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "CloudFrontToApiGatewayToLambda",
+    "CloudFrontToApiGatewayToLambdaProps",
+]
+
+publication.publish()
