4c581d00beb0d192a4d1c5aa2887def33b774819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:48 +0000

    Add package django-dnoticias-services/1.0.7.8/django_dnoticias_services-1.0.7.8.tar.gz

---
+++ packages/django-dnoticias-services/django_dnoticias_services-1.0.7.8.tar.gz/django_dnoticias_services-1.0.7.8/README.rst
@@ -0,0 +1,10 @@
+=====
+dnoticias_services
+=====
+
+dnoticias_services is a Django app to make the requests to the overall micro services of DNOTICIAS
+
+Currently:
+- send_mail
+- create_campaign
+- send_campaign
---
+++ packages/django-dnoticias-services/django_dnoticias_services-1.0.7.8.tar.gz/django_dnoticias_services-1.0.7.8/dnoticias_services/mail/__init__.py
@@ -0,0 +1,8 @@
+from dnoticias_services.mail.campaign import create_campaign, send_campaign
+from dnoticias_services.mail.mail import send_email
+
+__all__ = (
+    "create_campaign",
+    "send_campaign",
+    "send_email",
+)
