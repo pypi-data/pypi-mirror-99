9fd29ef3367ccb5d989a0a9bb6b358eec9845a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:01 +0000

    Add package cptools/1.4.0/cptools-1.4.0.tar.gz

---
+++ packages/cptools/cptools-1.4.0.tar.gz/cptools-1.4.0/cptools/__init__.py
@@ -0,0 +1,8 @@
+from .hello import *
+from .progress import *
+from .notify import *
+from .logger import *
+from .function import *
+
+__name__ = "cptools"
+__version__ = '1.4.0'
---
+++ packages/cptools/cptools-1.4.0.tar.gz/cptools-1.4.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from cptools import __version__
+import setuptools
+
+
+with open("README.md", 'r', encoding='utf8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'cptools',  # 包名称
+    version = __version__,
+    author = 'GentleCP',
+    author_email = '574881148@qq.com',
+    description = 'Tools used by CP',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        'requests >= 2.23',
+        'tqdm >= 4.45',
+        'DingtalkChatbot >= 1.5.2',
+        'yagmail >=0.11.224',
+    ],
+    url = 'https://github.com/GentleCP/cptools',   # 包的主页，如果你发布在了github就填写github链接
+    packages = setuptools.find_packages(),  # 让setuptools自动发现包
+    platforms = 'any',  # 包使用的平台
+    classifiers = [
+        'Programming Language :: Python :: 3',  # 采用编程语言
+        'License :: OSI Approved :: MIT License',  # 采用的许可证协议
+        'Operating System :: OS Independent',  # 操作系统
+    ]
+)
