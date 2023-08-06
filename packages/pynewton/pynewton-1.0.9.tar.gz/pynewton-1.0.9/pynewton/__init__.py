8f397a8171d3a66eb7ae0abc267d59aafe4cc69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:56 +0000

    Add package pynewton/1.0.9/pynewton-1.0.9.tar.gz

---
+++ packages/pynewton/pynewton-1.0.9.tar.gz/pynewton-1.0.9/pynewton/__init__.py
@@ -0,0 +1,8 @@
+""""
+An asnycio-based wrapper for `https://newton.now.sh`
+"""
+
+from .extensions import *
+
+__version__ = "1.0.9"
+__author__ = "Nils Theres"
---
+++ packages/pynewton/pynewton-1.0.9.tar.gz/pynewton-1.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pynewton']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx[http2]>=0.17.1,<0.18.0']
+
+setup_kwargs = {
+    'name': 'pynewton',
+    'version': '1.0.9',
+    'description': 'An asyncio-based wrapper for the newton-api',
+    'long_description': '# aionewton\n\nAn `asnycio`-based  wrapper for [Newton](https://newton.now.sh).\nThe Github project can be found [here.](https://github.com/aunyks/newton-api)\n\n## Installation\n```\npip install git+https://github.com/HitaloSama/aionewton\n```\n\n## Example\n\n```py\nimport asyncio\nimport aionewton\n\n# Get event loop\nloop = asyncio.get_event_loop()\n\nasync def main():\n    # Get calculation for `to_calculate`.\n    to_calculate = input("Expression: ") # 2^2+2(2)\n    \n    # Return a Result object with `operation`, `expression`\n    # and `result` as attributes.\n    result = await aionewton.simplify(to_calculate)\n    print(result)\n\nloop.run_until_complete(main())\n```\n',
+    'author': 'Nils Theres',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/HitaloSama/pynewton',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
