46258411b2b370fbb3ee82a860718798ad9a8860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:05 +0000

    Add package django-autologin/0.5.1/django-autologin-0.5.1.tar.gz

---
+++ packages/django-autologin/django-autologin-0.5.1.tar.gz/django-autologin-0.5.1/django_autologin/utils.py
@@ -0,0 +1,55 @@
+from six.moves.urllib import parse as urlparse
+
+from django.conf import settings
+from django.contrib import auth
+from django.core.signing import TimestampSigner, BadSignature
+
+from . import app_settings
+
+SEPARATOR = ':'
+
+def get_automatic_login_token(user):
+    signer = TimestampSigner(salt=get_user_salt(user), sep=SEPARATOR)
+    return signer.sign(user.pk)
+
+
+def validate_token(user, token) -> bool:
+    if SEPARATOR not in token:
+        return False
+
+    try:
+        signer = TimestampSigner(salt=get_user_salt(user), sep=SEPARATOR)
+        user_id = signer.unsign(token, max_age=app_settings.MAX_AGE)
+        return user_id == str(user.id)
+    except BadSignature:
+        return False
+
+
+def strip_token(url):
+    bits = urlparse.urlparse(url)
+    original_query = urlparse.parse_qsl(bits.query)
+
+    query = [(k, v) for k, v in original_query if k != app_settings.KEY]
+
+    query = urlparse.urlencode(query)
+
+    return urlparse.urlunparse((bits[0], bits[1], bits[2], bits[3], query, bits[5]),)
+
+
+def login(request, user):
+    user.backend = settings.AUTHENTICATION_BACKENDS[0]
+    auth.login(request, user)
+
+
+def get_user_salt(user):
+    parts = []
+
+    for field in app_settings.SALT_FIELDS:
+        # Follow "django__join__notation'
+        part = user
+        for x in field.split("__"):
+            part = getattr(part, x)
+
+        parts.append(part)
+
+    return "".join(str(x) for x in parts)
---
+++ packages/django-autologin/django-autologin-0.5.1.tar.gz/django-autologin-0.5.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="django-autologin",
+    url="https://chris-lamb.co.uk/projects/django-autologin",
+    version='0.5.1',
+    description="Token generator and processor to provide automatic login links for users",
+    author="Chris Lamb",
+    author_email="chris@chris-lamb.co.uk",
+    license="BSD",
+    packages=find_packages(),
+    install_requires=("Django>=1.8", "six>=1.10",),
+)
