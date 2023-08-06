b0b62717b781fe5539b401c2109e84978e738158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:56 +0000

    Add package tg-tool/0.2.2/tg-tool-0.2.2.tar.gz

---
+++ packages/tg-tool/tg-tool-0.2.2.tar.gz/tg-tool-0.2.2/tg_tool/__main__.py
@@ -0,0 +1,14 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def conquer(name: str):
+    typer.echo(f"Conquering reign: {name}")
+
+
+@app.command()
+def destroy(name: str):
+    typer.echo(f"Destroying reign: {name}")
+
+
