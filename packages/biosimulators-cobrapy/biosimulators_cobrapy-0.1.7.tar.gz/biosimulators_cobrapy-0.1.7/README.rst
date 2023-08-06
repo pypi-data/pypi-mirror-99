aaa588f1b0110fca633557ce4b2dc82daf8ccba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:47 +0000

    Add package biosimulators-cobrapy/0.1.7/biosimulators_cobrapy-0.1.7.tar.gz

---
+++ packages/biosimulators-cobrapy/biosimulators_cobrapy-0.1.7.tar.gz/biosimulators_cobrapy-0.1.7/README.rst
@@ -0,0 +1,148 @@
+|Latest release| |PyPI| |CI status| |Test coverage|
+
+BioSimulators-COBRApy
+=====================
+
+BioSimulators-compliant command-line interface and Docker image for the
+`COBRApy <https://opencobra.github.io/cobrapy/>`__ simulation program.
+
+This command-line interface and Docker image enable users to use COBRApy
+to execute `COMBINE/OMEX archives <https://combinearchive.org/>`__ that
+describe one or more simulation experiments (in `SED-ML
+format <https://sed-ml.org>`__) of one or more models (in `SBML
+format <http://sbml.org]>`__).
+
+A list of the algorithms and algorithm parameters supported by COBRApy
+is available at
+`BioSimulators <https://biosimulators.org/simulators/cobrapy>`__.
+
+A simple web application and web service for using COBRApy to execute
+COMBINE/OMEX archives is also available at
+`runBioSimulations <https://run.biosimulations.org>`__.
+
+Contents
+--------
+
+-  `Installation <#installation>`__
+-  `Usage <#usage>`__
+-  `Documentation <#documentation>`__
+-  `License <#license>`__
+-  `Development team <#development-team>`__
+-  `Questions and comments <#questions-and-comments>`__
+
+Installation
+------------
+
+Install Python package
+~~~~~~~~~~~~~~~~~~~~~~
+
+::
+
+   pip install biosimulators-cobrapy
+
+Install Docker image
+~~~~~~~~~~~~~~~~~~~~
+
+::
+
+   docker pull ghcr.io/biosimulators/cobrapy
+
+Usage
+-----
+
+SED-ML targets for simulation predictions
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+BioSimulators-COBRApy recognizes the following targets for simulation
+predictions:
+
+-  FBA (``KISAO_0000437``), parsimonious FBA (``KISAO_0000528``),
+   geometric FBA (``KISAO_0000527``):
+
+   -  Objective: ``fbc:objective/@value``
+   -  Reaction flux: ``sbml:reaction/@flux``
+   -  Reaction reduced cost: ``sbml:reaction/@reducedCost``
+   -  Species shadow price: ``sbml:species/@shadowPrice``
+
+-  FVA (``KISAO_0000526``):
+
+   -  Minimum reaction flux: ``sbml:reaction/@minFlux``
+   -  Maximum reaction flux: ``sbml:reaction/@maxFlux``
+
+Please see https://biosimulators.org/standards/simulation-experiments
+for more information.
+
+Local usage
+~~~~~~~~~~~
+
+::
+
+   usage: cobrapy [-h] [-d] [-q] -i ARCHIVE [-o OUT_DIR] [-v]
+
+   BioSimulators-compliant command-line interface to the COBRApy simulation program <https://opencobra.github.io/cobrapy/>.
+
+   optional arguments:
+     -h, --help            show this help message and exit
+     -d, --debug           full application debug mode
+     -q, --quiet           suppress all console output
+     -i ARCHIVE, --archive ARCHIVE
+                           Path to OMEX file which contains one or more SED-ML-
+                           encoded simulation experiments
+     -o OUT_DIR, --out-dir OUT_DIR
+                           Directory to save outputs
+     -v, --version         show program's version number and exit
+
+Usage through Docker container
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The entrypoint to the Docker image supports the same command-line
+interface described above.
+
+For example, the following command could be used to use the Docker image
+to execute the COMBINE/OMEX archive ``./modeling-study.omex`` and save
+its outputs to ``./``.
+
+::
+
+   docker run \
+     --tty \
+     --rm \
+     --mount type=bind,source="$(pwd)",target=/root/in,readonly \
+     --mount type=bind,source="$(pwd)",target=/root/out \
+     ghcr.io/biosimulators/cobrapy:latest \
+       -i /root/in/modeling-study.omex \
+       -o /root/out
+
+Documentation
+-------------
+
+Documentation is available at
+https://biosimulators.github.io/Biosimulators_COBRApy/.
+
+License
+-------
+
+This package is released under the `MIT license <LICENSE>`__.
+
+Development team
+----------------
+
+This package was developed by the `Center for Reproducible Biomedical
+Modeling <http://reproduciblebiomodels.org>`__ and the `Karr
+Lab <https://www.karrlab.org>`__ at the Icahn School of Medicine at
+Mount Sinai.
+
+Questions and comments
+----------------------
+
+Please contact the `BioSimulators
+Team <mailto:info@biosimulators.org>`__ with any questions or comments.
+
+.. |Latest release| image:: https://img.shields.io/github/v/tag/biosimulators/Biosimulators_COBRApy
+   :target: https://github.com/biosimulations/Biosimulators_COBRApy/releases
+.. |PyPI| image:: https://img.shields.io/pypi/v/biosimulators_cobrapy
+   :target: https://pypi.org/project/biosimulators_cobrapy/
+.. |CI status| image:: https://github.com/biosimulators/Biosimulators_COBRApy/workflows/Continuous%20integration/badge.svg
+   :target: https://github.com/biosimulators/Biosimulators_COBRApy/actions?query=workflow%3A%22Continuous+integration%22
+.. |Test coverage| image:: https://codecov.io/gh/biosimulators/Biosimulators_COBRApy/branch/dev/graph/badge.svg
+   :target: https://codecov.io/gh/biosimulators/Biosimulators_COBRApy
---
+++ packages/biosimulators-cobrapy/biosimulators_cobrapy-0.1.7.tar.gz/biosimulators_cobrapy-0.1.7/biosimulators_cobrapy/utils.py
@@ -0,0 +1,188 @@
+""" Utilities for working with COBRApy
+
+:Author: Jonathan Karr <karr@mssm.edu>
+:Date: 2020-12-31
+:Copyright: 2020, BioSimulators Team
+:License: MIT
+"""
+
+from biosimulators_utils.report.data_model import VariableResults
+from biosimulators_utils.sedml.data_model import Variable  # noqa: F401
+from biosimulators_utils.utils.core import validate_str_value, parse_value
+import cobra  # noqa: F401
+import libsbml
+import numpy
+import re
+
+__all__ = [
+    'get_active_objective_sbml_fbc_id',
+    'set_simulation_method_arg',
+    'apply_variables_to_simulation_method_args',
+    'validate_variables',
+    'get_results_of_variables',
+]
+
+
+def get_active_objective_sbml_fbc_id(model_source):
+    """ Get the SBML-FBC id of the active objective
+
+    Args:
+        model_source (:obj:`str`): path to model
+
+    Returns:
+        :obj:`str`: SBML-FBC id of the active objective
+    """
+    doc = libsbml.readSBMLFromFile(model_source)
+    model = doc.getModel()
+    model_fbc = model.getPlugin("fbc")
+    return model_fbc.getListOfObjectives().getActiveObjective()
+
+
+def set_simulation_method_arg(method_props, argument_change, model, model_method_kw_args):
+    """ Set the value of an argument of a simulation method based on a SED
+    algorithm parameter change
+
+    Args:
+        method_props (:obj:`dict`): properties of the simulation method
+        argument_change (:obj:`AlgorithmParameterChange`): algorithm parameter change
+        model (:obj:`cobra.core.model.Model`)
+        model_method_kw_args (:obj:`dict`): keyword arguments for the simulation method
+            for the model
+
+    Raises:
+        :obj:`NotImplementedError`: if the simulation method doesn't support the parameter
+        :obj:`ValueError`: if the new value is not a valid value of the parameter
+    """
+    parameter_kisao_id = argument_change.kisao_id
+    parameter = method_props['parameters'].get(parameter_kisao_id, None)
+    if parameter is None:
+        msg = "".join([
+            "{} ({}) does not support parameter `{}`. ".format(
+                method_props['name'], method_props['kisao_id'], argument_change.kisao_id),
+            "The parameters of {} must have one of the following KiSAO ids:\n  - {}".format(
+                method_props['name'],
+                '\n  - '.join(
+                    '{} ({}): {}'.format(kisao_id, parameter['name'], parameter['description'])
+                    for kisao_id, parameter in method_props['parameters'].items())),
+        ])
+        raise NotImplementedError(msg)
+
+    value = argument_change.new_value
+    if not validate_str_value(value, parameter['type']):
+        msg = "`{}` is not a valid value for parameter {} ({}) of {} ({})".format(
+            value, parameter['name'], parameter_kisao_id,
+            method_props['name'], method_props['kisao_id'])
+        raise ValueError(msg)
+    enum = parameter.get('enum', None)
+    if enum:
+        if value.lower() not in enum.__members__:
+            msg = ("`{}` is not a valid value for parameter {} ({}) of {} ({}). "
+                   "The value of {} must be one of the following:\n  - {}").format(
+                value, parameter['name'], parameter_kisao_id,
+                method_props['name'], method_props['kisao_id'],
+                method_props['name'],
+                '\n  - '.join(sorted('`' + value + '`' for value in enum.__members__.keys())))
+            raise ValueError(msg)
+
+    parsed_value = parse_value(value, parameter['type'])
+    if enum:
+        parsed_value = enum[value.lower()].value
+
+    if 'alg_arg' in parameter:
+        model_method_kw_args[parameter['alg_arg']] = parsed_value
+    else:
+        setattr(model, parameter['model_arg'], parsed_value)
+
+
+def apply_variables_to_simulation_method_args(target_x_paths_ids, method_props, variables, model_method_kw_args):
+    """ Encode the desired output variables into arguments to simulation methods
+
+    Args:
+        target_x_paths_ids (:obj:`dict` of :obj:`str` to :obj:`str`): dictionary that maps each XPath to the
+            SBML id of the corresponding model object
+        method_props (:obj:`dict`): properties of the simulation method
+        variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
+        model_method_kw_args (:obj:`dict`): keyword arguments for the simulation method
+            for the model
+    """
+    if method_props['kisao_id'] == 'KISAO_0000526':
+        reaction_list = set()
+        for variable in variables:
+            reaction_id = target_x_paths_ids[variable.target]
+            reaction_list.add(reaction_id[2:] if reaction_id.startswith('R_') else reaction_id)
+        model_method_kw_args['reaction_list'] = sorted(reaction_list)
+
+
+def validate_variables(method, variables):
+    """ Validate the desired output variables of a simulation
+
+    Args:
+        method (:obj:`dict`): properties of desired simulation method
+        variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
+    """
+    invalid_symbols = set()
+    invalid_targets = set()
+    for variable in variables:
+        if variable.symbol:
+            invalid_symbols.add(variable.symbol)
+
+        else:
+            valid = False
+            for variable_pattern in method['variables']:
+                if re.match(variable_pattern['target'], variable.target):
+                    valid = True
+                    break
+
+            if not valid:
+                invalid_targets.add(variable.target)
+
+    if invalid_symbols:
+        raise NotImplementedError("{} ({}) doesn't support variables with symbols".format(
+            method['name'], method['kisao_id']))
+
+    if invalid_targets:
+        msg = (
+            "{} ({}) doesn't support variables with the following target XPATHs:\n  - {}\n\n"
+            "The targets of variables should match one of the following patterns of XPATHs:\n  - {}"
+        ).format(
+            method['name'], method['kisao_id'],
+            '\n  - '.join(sorted('`' + target + '`' for target in invalid_targets)),
+            '\n  - '.join(sorted('{}: `{}`'.format(
+                variable_pattern['description'], variable_pattern['target'])
+                for variable_pattern in method['variables']))
+        )
+        raise ValueError(msg)
+
+
+def get_results_of_variables(target_x_paths_ids, target_x_paths_fbc_ids, active_objective_fbc_id, method, variables, solution):
+    """ Get the results of the desired variables
+
+    Args:
+        target_x_paths_ids (:obj:`dict` of :obj:`str` to :obj:`str`): dictionary that maps each XPath to the
+            SBML id of the corresponding model object
+        target_x_paths_fbc_ids (:obj:`dict` of :obj:`str` to :obj:`str`): dictionary that maps each XPath to the
+            SBML-FBC id of the corresponding model object
+        active_objective_fbc_id (:obj:`str`): SBML-FBC id of the active objective
+        method (:obj:`dict`): properties of desired simulation method
+        variables (:obj:`list` of :obj:`Variable`): variables that should be recorded
+        solution (:obj:`cobra.core.solution.Solution`): solution of method
+
+    Returns:
+        :obj:`VariableResults`: the results of desired variables
+    """
+    variable_results = VariableResults()
+
+    for variable in variables:
+        target = variable.target
+        for variable_pattern in method['variables']:
+            if re.match(variable_pattern['target'], target):
+                variable_target_id = target_x_paths_ids[target]
+                variable_target_fbc_id = target_x_paths_fbc_ids[target]
+                result = variable_pattern['get_result'](active_objective_fbc_id, variable_target_id,
+                                                        variable_target_fbc_id, solution)
+
+                break
+
+        variable_results[variable.id] = numpy.array(result)
+
+    return variable_results
