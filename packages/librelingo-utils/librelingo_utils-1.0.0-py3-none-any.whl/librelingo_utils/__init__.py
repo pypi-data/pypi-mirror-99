bb6458de959a9cd331a317245c8fb6e8a9fdacc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:05 +0000

    Add package librelingo-utils/1.0.0/librelingo_utils-1.0.0-py3-none-any.whl

---
+++ packages/librelingo-utils/librelingo_utils-1.0.0-py3-none-any.whl/librelingo_utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+librelingo-utils contains utility functions that are meant to make it easier
+to create Python software that works with LibreLingo courses.
+"""
+
+
+from librelingo_utils.utils import *
+
+__version__ = '1.0.0'
