ee3299550832d284521c417a00d8da88bfaa463b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:50 +0000

    Add package threatbus-zeek/2021.3.25/threatbus-zeek-2021.3.25.tar.gz

---
+++ packages/threatbus-zeek/threatbus-zeek-2021.3.25.tar.gz/threatbus-zeek-2021.3.25/threatbus_zeek/message_mapping.py
@@ -0,0 +1,140 @@
+import broker
+import re
+from stix2 import Indicator, Sighting
+from threatbus.data import (
+    Operation,
+    Subscription,
+    ThreatBusSTIX2Constants,
+    Unsubscription,
+)
+from threatbus.stix2_helpers import is_point_equality_ioc, split_object_path_and_value
+from typing import Union
+from urllib.parse import urlparse
+
+# See the documentation for the Zeek INTEL framework [1] and STIX-2 cyber
+# observable objects [2]
+# [1] https://docs.zeek.org/en/stable/scripts/base/frameworks/intel/main.zeek.html#type-Intel::Type
+# [2] https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_mlbmudhl16lr
+zeek_intel_type_map = {
+    "domain-name:value": "DOMAIN",
+    "email-addr:value": "EMAIL",
+    "file:name": "FILE_NAME",
+    "file:hashes.MD5": "FILE_HASH",
+    "file:hashes.'SHA-1'": "FILE_HASH",
+    "file:hashes.'SHA-256'": "FILE_HASH",
+    "file:hashes.'SHA-512'": "FILE_HASH",
+    "file:hashes.'SHA3-256'": "FILE_HASH",
+    "file:hashes.'SHA3-512'": "FILE_HASH",
+    "file:hashes.SSDEEP": "FILE_HASH",
+    "file:hashes.TLSH": "FILE_HASH",
+    "ipv4-addr:value": "ADDR",
+    "ipv6-addr:value": "ADDR",
+    "software:name": "SOFTWARE",
+    "url:value": "URL",
+    "user:user_id": "USER_NAME",
+    "user:account_login": "USER_NAME",
+    "x509-certificate:hashes.'SHA-1'": "CERT_HASH",  # Zeek only supports SHA-1
+}
+
+
+def map_management_message(
+    broker_data, module_namespace: str, logger
+) -> Union[Subscription, Unsubscription, None]:
+    """
+    Maps a management message to an actionable instruction for Threat Bus.
+    @param broker_data The raw data that was received via broker
+    @param module_namespace A Zeek namespace to accept events from
+    @return A Subscription/Unsubscription object or None in case there is no
+    valid mapping.
+    """
+    event = broker.zeek.Event(broker_data)
+    name, args = event.name(), event.args()
+    module_namespace = module_namespace + "::" if module_namespace else ""
+    name = name[name.startswith(module_namespace) and len(module_namespace) :]
+    if name == "subscribe" and len(args) == 2:
+        (topic, snapshot_delta) = args
+        if topic:
+            return Subscription(topic, snapshot_delta)
+    elif name == "unsubscribe" and len(args) == 1:
+        topic = args[0]
+        if topic:
+            return Unsubscription(topic)
+    logger.debug(f"Discarding Broker management message with unknown type: {name}")
+    return None
+
+
+def map_broker_event_to_sighting(broker_data, module_namespace, logger):
+    """
+    Maps a Broker message, based on the event name, to a STIX-2 indicator or
+    STIX-2 Sighting.
+    @param broker_data The raw data that was received via broker
+    @param module_namespace A Zeek namespace to accept events from
+    """
+    event = broker.zeek.Event(broker_data)
+    name, args = event.name(), event.args()
+    module_namespace = module_namespace + "::" if module_namespace else ""
+    name = name[name.startswith(module_namespace) and len(module_namespace) :]
+    if name != "sighting" or len(args) != 3:
+        if logger:
+            logger.debug(f"Discarding Broker event with unknown type: {name}")
+        return None
+    # convert args to STIX-2 sighting
+    (timestamp, ioc_id, context) = args
+    return Sighting(
+        created=timestamp,
+        sighting_of_ref=str(ioc_id),
+        custom_properties={
+            ThreatBusSTIX2Constants.X_THREATBUS_SIGHTING_CONTEXT.value: context
+        },
+    )
+
+
+def map_indicator_to_broker_event(
+    indicator: Indicator, module_namespace: str, logger
+) -> Union[broker.zeek.Event, None]:
+    """
+    Maps STIX-2 Indicators to Broker events using the Zeek Intel format
+    @see https://docs.zeek.org/en/current/scripts/base/frameworks/intel/main.zeek.html#type-Intel::Type
+    @param indicator The STIX-2 Indicator to convert
+    @param module_namespace A Zeek namespace to use for sending the event
+    @return The mapped broker event or None
+    """
+    if type(indicator) is not Indicator:
+        logger.debug(f"Discarding message, expected STIX-2 Indicator: {indicator}")
+        return None
+
+    if not is_point_equality_ioc(indicator.pattern):
+        logger.debug(
+            f"Zeek only supports point-IoCs. Cannot map compound pattern to a Zeek Intel item: {indicator.pattern}"
+        )
+        return None
+    object_path, ioc_value = split_object_path_and_value(indicator.pattern)
+
+    # get matching Zeek intel type
+    zeek_type = zeek_intel_type_map.get(object_path, None)
+    if not zeek_type:
+        logger.debug(
+            f"No matching Zeek type found for STIX-2 indicator type '{object_path}'"
+        )
+        return None
+
+    if zeek_type == "URL":
+        # remove leading protocol, if any
+        parsed = urlparse(ioc_value)
+        scheme = f"{parsed.scheme}://"
+        ioc_value = parsed.geturl().replace(scheme, "", 1)
+    elif zeek_type == "ADDR" and re.match(".+/.+", ioc_value):
+        # elevate to subnet if possible
+        zeek_type = "SUBNET"
+
+    operation = "ADD"  ## Zeek operation to add a new Intel item
+    if (
+        ThreatBusSTIX2Constants.X_THREATBUS_UPDATE.value
+        in indicator.object_properties()
+        and indicator.x_threatbus_update == Operation.REMOVE.value
+    ):
+        operation = "REMOVE"
+    return broker.zeek.Event(
+        f"{module_namespace}::intel",
+        (indicator.created, str(indicator.id), zeek_type, ioc_value, operation),
+    )
---
+++ packages/threatbus-zeek/threatbus-zeek-2021.3.25.tar.gz/threatbus-zeek-2021.3.25/threatbus_zeek/test_message_mapping.py
@@ -0,0 +1,203 @@
+import broker
+from datetime import datetime, timedelta, timezone
+import json
+from logging import getLogger
+from stix2 import Indicator, Sighting, parse
+from threatbus.data import (
+    Operation,
+    Subscription,
+    Unsubscription,
+    ThreatBusSTIX2Constants,
+)
+from threatbus_zeek.message_mapping import (
+    map_broker_event_to_sighting,
+    map_indicator_to_broker_event,
+    map_management_message,
+)
+import unittest
+
+
+class TestMessageMapping(unittest.TestCase):
+    def setUp(self):
+        self.ts = datetime.now(timezone.utc).astimezone()
+        self.indicator_id = "indicator--de0c3d3f-02ee-4086-88f1-51200ac831f7"
+        self.point_ioc = "evil.com"
+        self.pattern = f"[domain-name:value = '{self.point_ioc}']"
+        self.indicator = Indicator(
+            id=self.indicator_id,
+            created=self.ts,
+            modified=self.ts,
+            pattern_type="stix",
+            pattern=self.pattern,
+        )
+        self.module_namespace = "TestNamespace"
+        self.logger = getLogger("test")
+
+    def test_invalid_indicator_inputs(self):
+        self.assertIsNone(map_indicator_to_broker_event(None, None, self.logger))
+        self.assertIsNone(map_indicator_to_broker_event(None, "", self.logger))
+        self.assertIsNone(
+            map_indicator_to_broker_event(None, self.module_namespace, self.logger)
+        )
+        self.assertIsNone(
+            map_indicator_to_broker_event(42, self.module_namespace, self.logger)
+        )
+        self.assertIsNone(
+            map_indicator_to_broker_event(object, self.module_namespace, self.logger)
+        )
+        self.assertIsNone(
+            map_indicator_to_broker_event(
+                Sighting(sighting_of_ref=self.indicator_id),
+                self.module_namespace,
+                self.logger,
+            )
+        )
+
+    def test_invalid_zeek_inputs(self):
+        broker_data = broker.zeek.Event("Hello")  # unknown event
+        self.assertIsNone(map_broker_event_to_sighting(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_broker_event_to_sighting(
+                broker_data, self.module_namespace, self.logger
+            )
+        )
+        self.assertIsNone(map_management_message(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_management_message(broker_data, self.module_namespace, self.logger)
+        )
+
+        # not enough arguments provided
+        broker_data = broker.zeek.Event("sighting", 1, 2)
+        self.assertIsNone(map_broker_event_to_sighting(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_broker_event_to_sighting(
+                broker_data, self.module_namespace, self.logger
+            )
+        )
+        self.assertIsNone(map_management_message(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_management_message(broker_data, self.module_namespace, self.logger)
+        )
+
+        broker_data = broker.zeek.Event("intel", 42, {})
+        self.assertIsNone(map_broker_event_to_sighting(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_broker_event_to_sighting(
+                broker_data, self.module_namespace, self.logger
+            )
+        )
+        self.assertIsNone(map_management_message(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_management_message(broker_data, self.module_namespace, self.logger)
+        )
+
+        broker_data = broker.zeek.Event("subscribe", "topic")
+        self.assertIsNone(map_broker_event_to_sighting(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_broker_event_to_sighting(
+                broker_data, self.module_namespace, self.logger
+            )
+        )
+        self.assertIsNone(map_management_message(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_management_message(broker_data, self.module_namespace, self.logger)
+        )
+
+        broker_data = broker.zeek.Event("unsubscribe")
+        self.assertIsNone(map_broker_event_to_sighting(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_broker_event_to_sighting(
+                broker_data, self.module_namespace, self.logger
+            )
+        )
+        self.assertIsNone(map_management_message(broker_data, None, self.logger))
+        self.assertIsNone(
+            map_management_message(broker_data, self.module_namespace, self.logger)
+        )
+
+    def test_valid_indicator(self):
+        # test indicator added
+        broker_msg = map_indicator_to_broker_event(
+            self.indicator, self.module_namespace, None
+        )
+        self.assertEqual(broker_msg.name(), self.module_namespace + "::intel")
+        self.assertEqual(
+            broker_msg.args(),
+            [(self.ts, self.indicator_id, "DOMAIN", self.point_ioc, "ADD")],
+        )
+
+        # test indicator removed
+        # deep copy indicator, add custom property that indicates deletion
+        i_dct = json.loads(self.indicator.serialize())  # deep copy
+        i_dct[ThreatBusSTIX2Constants.X_THREATBUS_UPDATE.value] = Operation.REMOVE.value
+        indicator_copy = parse(json.dumps(i_dct), allow_custom=True)
+
+        broker_msg = map_indicator_to_broker_event(
+            indicator_copy, self.module_namespace, None
+        )
+        self.assertEqual(broker_msg.name(), self.module_namespace + "::intel")
+        self.assertEqual(
+            broker_msg.args(),
+            [(self.ts, self.indicator_id, "DOMAIN", self.point_ioc, "REMOVE")],
+        )
+
+    def test_valid_zeek_sighting(self):
+        context = {"last_seen": 1234, "count": 13, "source": "Zeek"}
+        # without namespace:
+        event = broker.zeek.Event("sighting", self.ts, self.indicator_id, context)
+        sighting = map_broker_event_to_sighting(event, self.module_namespace, None)
+        self.assertEqual(type(sighting), Sighting)
+        self.assertEqual(sighting.created, self.ts)
+        self.assertEqual(sighting.sighting_of_ref, self.indicator_id)
+        self.assertTrue(
+            ThreatBusSTIX2Constants.X_THREATBUS_SIGHTING_CONTEXT.value
+            in sighting.object_properties()
+        )
+        self.assertEqual(sighting.x_threatbus_sighting_context, context)
+
+        # with namespace:
+        event = broker.zeek.Event(
+            self.module_namespace + "::sighting", self.ts, self.indicator_id, context
+        )
+        sighting = map_broker_event_to_sighting(event, self.module_namespace, None)
+        self.assertEqual(type(sighting), Sighting)
+        self.assertEqual(sighting.created, self.ts)
+        self.assertEqual(sighting.sighting_of_ref, self.indicator_id)
+        self.assertTrue(
+            ThreatBusSTIX2Constants.X_THREATBUS_SIGHTING_CONTEXT.value
+            in sighting.object_properties()
+        )
+        self.assertEqual(sighting.x_threatbus_sighting_context, context)
+
+    def test_valid_subscription(self):
+        td = timedelta(days=5)
+        topic = "some/topic"
+        expected = Subscription(topic, td)
+
+        # without namespace
+        event = broker.zeek.Event("subscribe", topic, td)
+        subscription = map_management_message(event, self.module_namespace, self.logger)
+        self.assertEqual(subscription, expected)
+
+        # with namespace:
+        event = broker.zeek.Event(self.module_namespace + "::subscribe", topic, td)
+        subscription = map_management_message(event, self.module_namespace, self.logger)
+        self.assertEqual(subscription, expected)
+
+    def test_valid_unsubscription(self):
+        topic = "some/topic"
+        expected = Unsubscription(topic)
+
+        # without namespace
+        event = broker.zeek.Event("unsubscribe", topic)
+        unsubscription = map_management_message(
+            event, self.module_namespace, self.logger
+        )
+        self.assertEqual(unsubscription, expected)
+
+        # with namespace:
+        event = broker.zeek.Event(self.module_namespace + "::unsubscribe", topic)
+        unsubscription = map_management_message(
+            event, self.module_namespace, self.logger
+        )
+        self.assertEqual(unsubscription, expected)
