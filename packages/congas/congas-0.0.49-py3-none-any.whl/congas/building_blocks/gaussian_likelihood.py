0ba1647d50dc91de99972a3a5c1a87d93470899a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:43 +0000

    Add package congas/0.0.49/congas-0.0.49-py3-none-any.whl

---
+++ packages/congas/congas-0.0.49-py3-none-any.whl/congas/building_blocks/gaussian_likelihood.py
@@ -0,0 +1,23 @@
+import pyro.distributions as dist
+import torch
+from congas.utils import log_sum_exp
+
+def gaussian_likelihood_aux(x, segment_fact_marg, sd, mod = "rna"):
+
+    I, N = x._data['data_{}'.format(mod)].shape
+
+    K = x._params['K']
+
+    mean = (segment_fact_marg.reshape([K, I, 1]) * x._data['norm_factor_{}'.format(mod)].reshape([1, N]))  # / norm_f
+
+    lk = dist.Normal(mean, sd.reshape([1,I,1])).log_prob(
+        x._data['data_{}'.format(mod)])
+
+    return lk
+
+
+def gaussian_likelihood(x, segment_fact_marg, weights, sd, mod = "rna"):
+    lk = gaussian_likelihood_aux(x, segment_fact_marg, sd, mod)
+    lk = lk + torch.log(weights).reshape([x._params['K'], 1, 1])
+    summed_lk = log_sum_exp(lk)
+    return summed_lk.sum()
