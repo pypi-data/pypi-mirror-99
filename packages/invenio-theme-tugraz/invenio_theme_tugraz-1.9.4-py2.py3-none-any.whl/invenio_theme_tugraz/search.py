a737cd5b0ebbdee33fbee3aef88a4ea33392662c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:02 +0000

    Add package invenio-theme-tugraz/1.9.4/invenio_theme_tugraz-1.9.4-py2.py3-none-any.whl

---
+++ packages/invenio-theme-tugraz/invenio_theme_tugraz-1.9.4-py2.py3-none-any.whl/invenio_theme_tugraz/search.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2021 Graz University of Technology.
+#
+# invenio-theme-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Frontpage records."""
+
+from __future__ import absolute_import, print_function
+
+from elasticsearch_dsl.query import Q
+from invenio_search.api import RecordsSearch
+
+
+class FrontpageRecordsSearch(RecordsSearch):
+    """Search class for records that goes on the frontpage."""
+
+    class Meta:
+        """Default index and filter for frontpage search."""
+
+        index = "rdmrecords-records"
+        default_filter = Q("query_string", query=("access.record:public"))
