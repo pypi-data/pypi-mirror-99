b4fa9686e98d1348c9854fb14999bfb25b3eb77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:00 +0000

    Add package torch-resize-images/0.1.6/torch-resize-images-0.1.6.tar.gz

---
+++ packages/torch-resize-images/torch-resize-images-0.1.6.tar.gz/torch-resize-images-0.1.6/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+from os import path
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='torch-resize-images',
+    version='0.1.6',
+    description='A Python library to resize images using PyTorch',
+    url='https://github.com/NilsHendrikLukas/torch-resize-images',
+    author='Nils Lukas',
+    long_description=long_description,
+    author_email='nlukas@uwaterloo.ca',
+    scripts=["./torch-resize"],
+    license='MIT',
+    keywords='resize image pytorch',
+    packages=find_packages(),
+    install_requires=['Pillow>=5.1.0', 'torch', 'torchvision', 'gputil', 'tqdm']
+)
