a8849312af378034b7838120de58b1b988b4567f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:41 +0000

    Add package vectorai-nightly/0.2.5.2021.3.23/vectorai-nightly-0.2.5.2021.3.23.tar.gz

---
+++ packages/vectorai-nightly/vectorai-nightly-0.2.5.2021.3.23.tar.gz/vectorai-nightly-0.2.5.2021.3.23/vectorai/__init__.py
@@ -0,0 +1,8 @@
+"""Vecdb Client
+"""
+__version__ = "0.2.2"
+
+from .api import *
+from .client import *
+from .read import *
+from .write import *
---
+++ packages/vectorai-nightly/vectorai-nightly-0.2.5.2021.3.23.tar.gz/vectorai-nightly-0.2.5.2021.3.23/vectorai/api/dimensionality_reduction.py
@@ -0,0 +1,103 @@
+import requests
+from typing import List
+from .utils import retry, return_curl_or_response
+
+class ViDimensionalityReductionClient:
+    """
+    Dimensionality Reduction
+    """
+    def __init__(self, username: str, api_key: str, url=None):
+        self.username = username
+        self.api_key = api_key
+        if url:
+            self.url = url
+        else:
+            self.url = "https://api.vctr.ai"
+
+    @retry()
+    def dimensionality_reduce(
+        self,
+        collection_name: str,
+        vectors: List[List[float]],
+        vector_field: str,
+        n_components: int,
+        alias: str = "default",
+        return_curl: bool=False,
+        **kwargs
+    ):
+        """
+Trains a Dimensionality Reduction model on the collection
+
+Dimensionality reduction allows your vectors to be reduced down to any dimensions greater than 0 using unsupervised machine learning. This is useful for even faster search and visualising the vectors.
+        
+Args:
+	vector_field:
+		Vector field to perform dimensionality reduction on
+	alias:
+		Alias is used to name the dimensionality reduced vectors
+	n_components:
+		The size/length to reduce the vector down to. If 0 is set then highest possible is of components is set, when this is done you can get reduction on demand of any length.
+	refresh:
+		Whether to refresh the whole collection and retrain the dimensionality reduction model
+	collection_name:
+		Name of Collection
+"""
+        params = {
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "vectors": vectors,
+            "vector_field": vector_field,
+            "alias": alias,
+            "n_components": n_components,
+        }
+        params.update(kwargs)
+        response = requests.get(
+            url="{}/collection/dimensionality_reduce".format(self.url),
+            params=params
+        )
+        return return_curl_or_response(response, 'json', return_curl=return_curl)
+
+    @retry()
+    def dimensionality_reduction_job(
+        self,
+        collection_name: str,
+        vector_field: str,
+        n_components: int = 0,
+        alias: str = "default",
+        refresh: bool = True,
+        return_curl: bool=False,
+        **kwargs
+    ):
+        """
+Trains a Dimensionality Reduction model on the collection
+
+Dimensionality reduction allows your vectors to be reduced down to any dimensions greater than 0 using unsupervised machine learning. This is useful for even faster search and visualising the vectors.
+        
+Args:
+	vector_field:
+		Vector field to perform dimensionality reduction on
+	alias:
+		Alias is used to name the dimensionality reduced vectors
+	n_components:
+		The size/length to reduce the vector down to. If 0 is set then highest possible is of components is set, when this is done you can get reduction on demand of any length.
+	refresh:
+		Whether to refresh the whole collection and retrain the dimensionality reduction model
+	collection_name:
+		Name of Collection
+"""
+        params = {
+            "username": self.username,
+            "api_key": self.api_key,
+            "collection_name": collection_name,
+            "vector_field": vector_field,
+            "alias": alias,
+            "n_components": n_components,
+            "refresh": refresh,
+        }
+        params.update(kwargs)
+        response = requests.get(
+            url="{}/collection/jobs/dimensionality_reduction".format(self.url),
+            params=params
+        )
+        return return_curl_or_response(response, 'json', return_curl=return_curl)
