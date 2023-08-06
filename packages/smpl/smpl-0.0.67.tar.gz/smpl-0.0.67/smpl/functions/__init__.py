057bcfeae6d8504169dcf5f1003e3d9808571562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:04 +0000

    Add package smpl/0.0.67/smpl-0.0.67.tar.gz

---
+++ packages/smpl/smpl-0.0.67.tar.gz/smpl-0.0.67/smpl/functions/__init__.py
@@ -0,0 +1,14 @@
+"""
+A list of various functions
+"""
+from .functions import const, lorentz,linear,line,cos_abs,gauss,quadratic,exponential
+__all__ = [
+    'const', 
+    'lorentz',
+    'linear',
+    'line',
+    'cos_abs',
+    'gauss',
+    'quadratic',
+    'exponential'
+    ]
---
+++ packages/smpl/smpl-0.0.67.tar.gz/smpl-0.0.67/smpl/parallel/__init__.py
@@ -0,0 +1,5 @@
+"""
+Very basic parallelization
+"""
+from .parallel import gen,calc,par,res
+__all__ = ['gen','calc','par','res']
