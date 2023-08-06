5feb2778edfc4e5e0f0a1c6f0c0fc447132b06e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:35 +0000

    Add package sparkpipelineframework/0.1.81/sparkpipelineframework-0.1.81-py3-none-any.whl

---
+++ packages/sparkpipelineframework/sparkpipelineframework-0.1.81-py3-none-any.whl/spark_pipeline_framework/transformers/framework_drop_duplicates_transformer/v1/framework_drop_duplicates_transformer.py
@@ -0,0 +1,79 @@
+from typing import Dict, Any, Optional, List
+
+# noinspection PyProtectedMember
+from pyspark import keyword_only
+from pyspark.ml.param import Param
+from pyspark.sql.dataframe import DataFrame
+from spark_pipeline_framework.logger.yarn_logger import get_logger
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+from spark_pipeline_framework.transformers.framework_transformer.v1.framework_transformer import (
+    FrameworkTransformer,
+)
+
+
+class FrameworkDropDuplicatesTransformer(FrameworkTransformer):
+    # noinspection PyUnusedLocal
+    @keyword_only
+    def __init__(
+        self,
+        # add your parameters here (be sure to add them to setParams below too)
+        view: str,
+        columns: List[str],
+        name: Optional[str] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        progress_logger: Optional[ProgressLogger] = None,
+    ):
+        super().__init__(
+            name=name, parameters=parameters, progress_logger=progress_logger
+        )
+
+        self.logger = get_logger(__name__)
+
+        assert view
+        assert columns
+        assert isinstance(columns, list)
+
+        # add a param
+        self.view: Param = Param(self, "view", "")
+        self._setDefault(view=view)
+
+        self.columns: Param = Param(self, "columns", "")
+        self._setDefault(columns=columns)
+
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring, PyUnusedLocal
+    @keyword_only
+    def setParams(
+        self,
+        # add your parameters here
+        view: str,
+        columns: List[str],
+        name: Optional[str] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        progress_logger: Optional[ProgressLogger] = None,
+    ) -> Any:
+        kwargs = self._input_kwargs
+        super().setParams(
+            name=name, parameters=parameters, progress_logger=progress_logger
+        )
+        return self._set(**kwargs)
+
+    def _transform(self, df: DataFrame) -> DataFrame:
+        view: str = self.getView()
+        columns: List[str] = self.getColumns()
+
+        result_df: DataFrame = df.sql_ctx.table(view)
+        result_df = result_df.drop_duplicates(columns)
+        result_df.createOrReplaceTempView(view)
+
+        return df
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getView(self) -> str:
+        return self.getOrDefault(self.view)  # type: ignore
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getColumns(self) -> List[str]:
+        return self.getOrDefault(self.columns)  # type: ignore
---
+++ packages/sparkpipelineframework/sparkpipelineframework-0.1.81-py3-none-any.whl/spark_pipeline_framework/transformers/framework_transformer/v1/framework_transformer.py
@@ -0,0 +1,81 @@
+from typing import Dict, Any, List, Optional
+
+from pyspark.ml.base import Transformer
+from pyspark.ml.param import Param
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql.dataframe import DataFrame
+
+from spark_pipeline_framework.logger.yarn_logger import get_logger
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+
+
+class FrameworkTransformer(
+    Transformer,  # type: ignore
+    DefaultParamsReadable,  # type: ignore
+    DefaultParamsWritable,  # type: ignore
+):
+    # noinspection PyUnusedLocal
+    def __init__(
+        self,
+        name: Optional[str] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        progress_logger: Optional[ProgressLogger] = None,
+    ):
+        super(FrameworkTransformer, self).__init__()
+
+        self.logger = get_logger(__name__)
+
+        self.name: Param = Param(self, "name", "")
+        self._setDefault(name=None)
+
+        self.progress_logger: Param = Param(self, "progress_logger", "")
+        self._setDefault(progress_logger=None)
+
+        self.parameters: Param = Param(self, "parameters", "")
+        self._setDefault(parameters=None)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring, PyUnusedLocal
+    def setParams(
+        self,
+        name: Optional[str] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        progress_logger: Optional[ProgressLogger] = None,
+    ) -> Any:
+        kwargs = self._input_kwargs
+        return self._set(**kwargs)
+
+    def _transform(self, df: DataFrame) -> DataFrame:
+        return df
+
+    @property
+    def transformers(self) -> List[Transformer]:
+        return [self]
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def setName(self, value: str) -> "FrameworkTransformer":
+        self._paramMap[self.name] = value
+        return self
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getName(self) -> Optional[str]:
+        return self.getOrDefault(self.name)  # type: ignore
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def setProgressLogger(
+        self, value: Optional[ProgressLogger]
+    ) -> "FrameworkTransformer":
+        self._paramMap[self.progress_logger] = value
+        return self
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getProgressLogger(self) -> Optional[ProgressLogger]:
+        return self.getOrDefault(self.progress_logger)  # type: ignore
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def setParameters(self, value: Optional[Dict[str, Any]]) -> "FrameworkTransformer":
+        self._paramMap[self.parameters] = value
+        return self
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getParameters(self) -> Optional[Dict[str, Any]]:
+        return self.getOrDefault(self.parameters)  # type: ignore
