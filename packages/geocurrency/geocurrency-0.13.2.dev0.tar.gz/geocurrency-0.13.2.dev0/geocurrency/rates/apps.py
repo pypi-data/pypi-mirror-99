5965f2693f82a39338c4afbfb49edf1a572a493d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:23 +0000

    Add package geocurrency/0.13.2.dev0/geocurrency-0.13.2.dev0.tar.gz

---
+++ packages/geocurrency/geocurrency-0.13.2.dev0.tar.gz/geocurrency-0.13.2.dev0/geocurrency/rates/apps.py
@@ -0,0 +1,14 @@
+"""
+Geocurrency Rates app config
+"""
+
+from django.apps import AppConfig
+
+
+class RatesConfig(AppConfig):
+    """
+    Rates configuration
+    """
+
+    name = "rates"
+    verbose_name = "Rates"
