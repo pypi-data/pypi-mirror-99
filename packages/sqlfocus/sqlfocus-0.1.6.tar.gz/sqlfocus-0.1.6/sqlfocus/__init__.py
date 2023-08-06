7372b2a1e4dfe7df037e59a006e61ac063546784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:58 +0000

    Add package sqlfocus/0.1.6/sqlfocus-0.1.6.tar.gz

---
+++ packages/sqlfocus/sqlfocus-0.1.6.tar.gz/sqlfocus-0.1.6/sqlfocus/__init__.py
@@ -0,0 +1,9 @@
+from .table import SQLTable
+
+__version__ = "0.1.6"
+
+
+class SQLFocus:
+    def __init__(self, module, db_name="sqlite"):
+        self.conn = module.connect(db_name)
+        self.table = SQLTable
