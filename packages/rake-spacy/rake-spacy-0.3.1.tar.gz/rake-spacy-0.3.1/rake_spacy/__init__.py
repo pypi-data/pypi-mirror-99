338e405bf166a3875560c9007710776a5148cc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:45 +0000

    Add package rake-spacy/0.3.1/rake-spacy-0.3.1.tar.gz

---
+++ packages/rake-spacy/rake-spacy-0.3.1.tar.gz/rake-spacy-0.3.1/rake_spacy/__init__.py
@@ -0,0 +1,9 @@
+from . import aggregators  # noqa: F401
+from . import cog  # noqa: F401
+from . import mappers  # noqa: F401
+from . import phrasers  # noqa: F401
+from . import rake  # noqa: F401
+from . import scorers  # noqa: F401
+from . import stop_tokens  # noqa: F401
+from .__version__ import __version__  # noqa: F401
+from .rake import Rake  # noqa: F401
