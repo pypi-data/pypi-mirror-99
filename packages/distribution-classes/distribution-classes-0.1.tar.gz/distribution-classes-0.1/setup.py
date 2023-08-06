ec4d9d4007b287b7170bd8e6ac2afb6f40b79512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:49 +0000

    Add package distribution-classes/0.1/distribution-classes-0.1.tar.gz

---
+++ packages/distribution-classes/distribution-classes-0.1.tar.gz/distribution-classes-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution-classes',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution-classes'],
+      zip_safe=False)
