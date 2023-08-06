8e985fe49268471a84bc4cddfe42bc0733242d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:28 +0000

    Add package DSErrorsGenerator/0.0.1/DSErrorsGenerator-0.0.1.tar.gz

---
+++ packages/DSErrorsGenerator/DSErrorsGenerator-0.0.1.tar.gz/DSErrorsGenerator-0.0.1/README.md
@@ -0,0 +1,3 @@
+Generate embed with your personal texts of errors.
+
+[More details.](https://github.com/FeeFort/discord-error-generator)
---
+++ packages/DSErrorsGenerator/DSErrorsGenerator-0.0.1.tar.gz/DSErrorsGenerator-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup
+
+setup(name='DSErrorsGenerator',
+      version='0.0.1',
+      description='Generate embeds with texts of your personal errors.',
+      packages=['DSErrorsGenerator'],
+      author='FeeFort',
+      long_description='Documentation: https://bit.ly/3sa1aL6',
+      url='https://github.com/FeeFort/discord-error-generator',
+      author_email='bikov22022006@gmail.com',
+      zip_safe=False)
+
+os.system("python -m pip install emoji")
+os.system("python -m pip install discord.py")
