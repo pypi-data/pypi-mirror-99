e68e1090f1b5da9bdfd5cbbbf76798a8cbe10ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:33 +0000

    Add package blackbricks/0.6.2/blackbricks-0.6.2-py3-none-any.whl

---
+++ packages/blackbricks/blackbricks-0.6.2-py3-none-any.whl/blackbricks/blackbricks.py
@@ -0,0 +1,175 @@
+import itertools
+from dataclasses import dataclass
+from typing import Iterator
+
+import black
+import sqlparse
+from blib2to3.pytree import Leaf
+
+_black_default_str = black.Line.__str__
+_black_default_visit_string = black.LineGenerator.visit_STRING
+
+COMMAND = "# COMMAND ----------"
+HEADER = "# Databricks notebook source"
+NOFMT = "-- nofmt"
+
+
+@dataclass(frozen=True)
+class FormatConfig:
+    "Data-only class to hold format configuration options and their defaults."
+    line_length: int = black.DEFAULT_LINE_LENGTH
+    sql_upper: bool = True
+    two_space_indent: bool = True
+
+
+def _make_black_use_two_spaces(do_it: bool) -> None:
+    """Force black to use two spaces for indentation.
+
+    This is a copy of:
+
+    `black.Line.__str__` with only one change:
+        indent = "  " * self.depth
+
+    `black.LineGenerator.visit_STRING` with only one change:
+        indent = " " * 2 * self.current_line.depth
+
+    I.e. changing the indentation used.
+
+    Change only applied if `do_it` is True.
+    """
+
+    def patch(self) -> str:
+        """Render the line."""
+        if not self:
+            return "\n"
+
+        indent = "  " * self.depth
+        leaves = iter(self.leaves)
+        first = next(leaves)
+        res = f"{first.prefix}{indent}{first.value}"
+        for leaf in leaves:
+            res += str(leaf)
+        for comment in itertools.chain.from_iterable(self.comments.values()):
+            res += str(comment)
+        return res + "\n"
+
+    def patch_visit_STRING(self, leaf: Leaf) -> Iterator[black.Line]:
+        if black.is_docstring(leaf) and "\\\n" not in leaf.value:
+            # We're ignoring docstrings with backslash newline escapes because changing
+            # indentation of those changes the AST representation of the code.
+            prefix = black.get_string_prefix(leaf.value)
+            lead_len = len(prefix) + 3
+            tail_len = -3
+            indent = " " * 2 * self.current_line.depth
+            docstring = black.fix_docstring(leaf.value[lead_len:tail_len], indent)
+            if docstring:
+                if leaf.value[lead_len - 1] == docstring[0]:
+                    docstring = " " + docstring
+                if leaf.value[tail_len + 1] == docstring[-1]:
+                    docstring = docstring + " "
+            leaf.value = leaf.value[0:lead_len] + docstring + leaf.value[tail_len:]
+
+        yield from self.visit_default(leaf)
+
+    if do_it:
+        black.Line.__str__ = patch
+        black.LineGenerator.visit_STRING = patch_visit_STRING
+    else:
+        black.Line.__str__ = _black_default_str
+        black.LineGenerator.visit_STRING = _black_default_visit_string
+
+
+def format_str(content: str, config: FormatConfig = FormatConfig()) -> str:
+    """
+    Format the content of a notebook according to the format config provided.
+
+    This assumes that `content` is the _full_ content of a notebook file, and that
+    the notebook is a Python notebook.
+
+    :param content: A string holding the entire content of a notebook.
+    :param config: An object holding the desired formatting options.
+    :return: The content of the file, formatted according to the configuration.
+    """
+    _make_black_use_two_spaces(config.two_space_indent)
+
+    cells = content.replace(HEADER, "", 1).split(COMMAND)
+
+    output_cells = []
+    for cell in cells:
+        cell = cell.strip()
+
+        if "# MAGIC %sql" in cell:
+            output_cells.append(
+                _format_sql_cell(
+                    cell, sql_keyword_case="upper" if config.sql_upper else "lower"
+                )
+            )
+        elif "# MAGIC" in cell:
+            output_cells.append(cell)  # Generic magic cell - output as-is.
+        else:
+            output_cells.append(
+                black.format_str(
+                    cell, mode=black.FileMode(line_length=config.line_length)
+                )
+            )
+
+    output = (
+        f"{HEADER}\n"
+        + f"\n\n{COMMAND}\n\n".join(
+            "".join(line.rstrip() + "\n" for line in cell.splitlines()).rstrip()
+            for cell in output_cells
+        ).rstrip()
+        + "\n"
+    )
+
+    return output
+
+
+def _format_sql_cell(cell: str, sql_keyword_case: str = "upper") -> str:
+    """
+    Format a MAGIC %sql cell.
+
+    :param cell: The content of an SQL cell.
+    :param sql_keyword_case: One of ["upper", "lower"], setting the case for SQL keywords.
+    :return: The cell with formatting applied.
+    """
+
+    # Formatting can be disabled on a cell-by-cell basis by addint `-- nofmt` to the first line.
+    if NOFMT in cell.lstrip().splitlines()[0]:
+        return cell
+
+    magics = []
+    sql_lines = []
+    for line in cell.strip().splitlines():
+        if line.strip().startswith("# MAGIC %sql"):
+            continue
+        words = line.split()
+        magic, sql = words[:2], words[2:]
+        magics.append(magic)
+        sql_lines.append(" ".join(sql).strip())
+
+    return "# MAGIC %sql\n" + "\n".join(
+        f"# MAGIC {sql}"
+        for sql in sqlparse.format(
+            "\n".join(sql_lines), reindent=True, keyword_case=sql_keyword_case
+        ).splitlines()
+    )
+
+
+def unified_diff(a: str, b: str, a_name: str, b_name: str) -> str:
+    """
+    Return a unified diff string between strings `a` and `b`.
+
+    :param a: The first string (e.g. before).
+    :param b: The second string (e.g. after).
+    :param a_name: The "filename" to display for the `a` string.
+    :param b_name: The "filename" to display for the `b` string.
+    :return: A `git diff` like diff of the two strings.
+    """
+    import difflib
+
+    a_lines = [line + "\n" for line in a.split("\n")]
+    b_lines = [line + "\n" for line in b.split("\n")]
+    return "".join(
+        difflib.unified_diff(a_lines, b_lines, fromfile=a_name, tofile=b_name, n=5)
+    )
---
+++ packages/blackbricks/blackbricks-0.6.2-py3-none-any.whl/blackbricks/databricks_sync.py
@@ -0,0 +1,144 @@
+import base64
+import os
+from configparser import ConfigParser
+
+import typer
+from databricks_cli.sdk.api_client import ApiClient
+from databricks_cli.sdk.service import WorkspaceService
+
+
+class DatabricksAPI:
+    def __init__(
+        self, databricks_host: str, databricks_token: str, username: str = None
+    ) -> WorkspaceService:
+        self.client = WorkspaceService(
+            ApiClient(host=databricks_host, token=databricks_token)
+        )
+        self.username = username
+
+    def __resolve_path(self, path) -> str:
+        """
+        Resolve path to notebook.
+
+        If `path` is not absolute and a username is in use, interpret the path as
+        relative to the user direcotry.
+
+        Example:
+
+            tmp/notebook.py  -> /Users/{username}/tmp/notebook.py
+            /tmp/notebook.py -> /tmp/notebook.py
+        """
+        if not path.startswith("/") and self.username is not None:
+            path = f"/Users/{self.username}/{path}"
+        return path
+
+    def read_notebook(self, path) -> str:
+        path = self.__resolve_path(path)
+        response = self.client.export_workspace(path, format="SOURCE")
+
+        assert (
+            response["file_type"] == "py"
+        ), f"Cannot read a non-python notebook: {path}"
+
+        return base64.decodebytes(response["content"].encode()).decode()
+
+    def write_notebook(self, path: str, content: str) -> None:
+        path = self.__resolve_path(path)
+        self.client.import_workspace(
+            path,
+            format="SOURCE",
+            language="PYTHON",
+            content=base64.b64encode(content.encode()).decode(),
+            overwrite=True,
+        )
+
+
+def get_api_client(profile_name: str):
+    config = ConfigParser()
+    config_path = os.path.expanduser("~/.databrickscfg")
+    try:
+        config.read(config_path)
+    except FileNotFoundError:
+        typer.echo(
+            typer.style("Error:", fg=typer.colors.RED, bold=True)
+            + typer.style(
+                f" Your ~/.databrickscfg file is missing.",
+                bold=True,
+            ),
+        )
+        typer.echo(
+            "If you haven't already, first install the databricks cli,\n"
+            "then run `databricks configure` and check your ~/.databrickscfg file."
+        )
+        raise typer.Abort()
+    except Exception as e:
+        typer.echo(
+            typer.style("Error:", fg=typer.colors.RED, bold=True)
+            + typer.style(
+                f" An error occurred while reading your ~/.databrickscfg file.",
+                bold=True,
+            )
+        )
+        raise e
+
+    if profile_name != "DEFAULT" and not config.has_section(profile_name):
+        typer.echo(
+            typer.style("Error:", fg=typer.colors.RED, bold=True)
+            + typer.style(
+                f" Could not find {profile_name} in ~/.databrickscfg", bold=True
+            ),
+        )
+        typer.echo(
+            "Run `databricks configure` and/or check your ~/.databrickscfg file."
+        )
+        raise typer.Abort()
+
+    host = config.get(profile_name, "host")  # Will throw if missing, always needed.
+    username = config.get(profile_name, "username", fallback=None)
+    token = config.get(profile_name, "token", fallback=None)
+    password = config.get(profile_name, "password", fallback=None)
+    credentials = token if token is not None else password
+
+    # Handle no username:
+    if username is None:
+        typer.echo(
+            typer.style("Warning:", fg=typer.colors.YELLOW, bold=True)
+            + typer.style(" No username in ~/.databrickscfg.", bold=True)
+        )
+        typer.echo(
+            "Please add `username = ...` to your profile (and/or the [DEFAULT] profile)"
+            ", or be sure to always write full paths to notebooks."
+        )
+
+    # Handle no token and no password:
+    if credentials is None:
+        typer.echo(
+            typer.style("Error:", fg=typer.colors.RED, bold=True)
+            + typer.style(
+                f" Found neither token nor password in ~/.databrickscfg for profile {profile_name}",
+                bold=True,
+            ),
+        )
+        raise typer.Abort()
+
+    # Handle password without a username:
+    if token is None and password is not None and username is None:
+        typer.echo(
+            typer.style("Error:", fg=typer.colors.RED, bold=True)
+            + typer.style(
+                f" Profile {profile_name} is missing a username in ~/.databrickscfg",
+                bold=True,
+            ),
+        )
+        raise typer.Abort()
+
+    # Handle both token and password (one might be from DEFAULT):
+    if token is not None and password is not None:
+        if password != config.defaults().get("password"):
+            credentials = password
+        if token != config.defaults().get("token"):
+            credentials = token
+
+    return DatabricksAPI(
+        databricks_host=host, databricks_token=credentials, username=username
+    )
