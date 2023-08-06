cec178699ee3cc8bed4860374daf44ec18f20272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:42 +0000

    Add package bruteforceDict/0.0/bruteforceDict-0.0.tar.gz

---
+++ packages/bruteforceDict/bruteforceDict-0.0.tar.gz/bruteforceDict-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bruteforceDict',
+      version='0.0',
+      description='Bruteforce to file, to array',
+      packages=['bruteforceDict'],
+      author_email='yura@anisimoff.tel',
+      zip_safe=False)
