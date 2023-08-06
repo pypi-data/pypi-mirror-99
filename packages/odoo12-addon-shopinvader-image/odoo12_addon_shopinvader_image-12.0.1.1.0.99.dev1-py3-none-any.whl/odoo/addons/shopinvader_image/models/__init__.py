23ba1f196934129600c86df4b9a29e3e312e0915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:03 +0000

    Add package odoo12-addon-shopinvader-image/12.0.1.1.0.99.dev1/odoo12_addon_shopinvader_image-12.0.1.1.0.99.dev1-py3-none-any.whl

---
+++ packages/odoo12-addon-shopinvader-image/odoo12_addon_shopinvader_image-12.0.1.1.0.99.dev1-py3-none-any.whl/odoo/addons/shopinvader_image/models/__init__.py
@@ -0,0 +1,5 @@
+from . import shopinvader_backend
+from . import shopinvader_image_mixin
+from . import shopinvader_image_resize
+from . import shopinvader_category
+from . import shopinvader_variant
