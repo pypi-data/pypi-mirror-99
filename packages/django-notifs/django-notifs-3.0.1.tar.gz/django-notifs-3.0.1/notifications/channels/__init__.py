7984a957a6231b804be7e7d4b38575b1006e0c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:21 +0000

    Add package django-notifs/3.0.1/django-notifs-3.0.1.tar.gz

---
+++ packages/django-notifs/django-notifs-3.0.1.tar.gz/django-notifs-3.0.1/notifications/channels/__init__.py
@@ -0,0 +1,4 @@
+from .console import ConsoleChannel  # noqa
+from .base import BaseNotificationChannel  # noqa
+from .channels_websocket import WebSocketChannel  # noqa
+from .push_notification import PushNotificationChannel  # noqa
