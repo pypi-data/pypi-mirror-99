f4853677d3bab7a032135e49ecd7f12f04d70535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:12 +0000

    Add package signal-processing-algorithms/2.0.0/signal_processing_algorithms-2.0.0-cp38-cp38-manylinux_2_12_x86_64.manylinux1_x86_64.whl

---
+++ packages/signal-processing-algorithms/signal_processing_algorithms-2.0.0-cp38-cp38-manylinux_2_12_x86_64.manylinux1_x86_64.whl/signal_processing_algorithms/determinism.py
@@ -0,0 +1,42 @@
+"""Functions to help setup deterministic results when using random numbers."""
+# E-Divisive's definition requires it to permute change-windows
+# which leads to non-determinism: we need to always get the
+# same change-point results when running on the same input.
+import random
+
+from contextlib import contextmanager
+from typing import Generator
+
+import numpy as np
+
+
+@contextmanager
+def deterministic_random(seed: float) -> Generator:
+    """
+    Call random.seed(seed) during invocation and then restore state after.
+
+    :param seed: RNG seed.
+    :return: Deterministic random context.
+    """
+    state = random.getstate()
+    random.seed(seed)
+    try:
+        yield
+    finally:
+        random.setstate(state)
+
+
+@contextmanager
+def deterministic_numpy_random(seed: float) -> Generator:
+    """
+    Call np.random.seed(seed) during invocation and then restore to None after.
+
+    :param seed: RNG seed.
+    :return: Deterministic random context.
+    """
+    state = np.random.get_state()
+    np.random.seed(seed)
+    try:
+        yield
+    finally:
+        np.random.set_state(state)
