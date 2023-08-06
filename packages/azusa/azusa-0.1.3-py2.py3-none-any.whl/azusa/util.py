f6e2b4f33ab28f7515d1ed6fb7d3bf58aea0c974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:24 +0000

    Add package azusa/0.1.3/azusa-0.1.3-py2.py3-none-any.whl

---
+++ packages/azusa/azusa-0.1.3-py2.py3-none-any.whl/azusa/util.py
@@ -0,0 +1,6 @@
+class defaultdict(dict):
+    def __init__(self, factory):
+        self.factory=factory
+    def __missing__(self, key):
+        self[key] = self.factory(key)
+        return self[key]
