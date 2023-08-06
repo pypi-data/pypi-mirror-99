aa74ea729f3819aee77cdc6d40669220e6381743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:15 +0000

    Add package awscli-local/0.14/awscli-local-0.14.tar.gz

---
+++ packages/awscli-local/awscli-local-0.14.tar.gz/awscli-local-0.14/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+
+    setup(
+        name='awscli-local',
+        version='0.14',
+        description='Thin wrapper around the "aws" command line interface for use with LocalStack',
+        author='Waldemar Hummer',
+        author_email='waldemar.hummer@gmail.com',
+        url='https://github.com/localstack/awscli-local',
+        packages=[],
+        scripts=['bin/awslocal', 'bin/awslocal.bat'],
+        package_data={},
+        data_files={},
+        install_requires=['localstack-client'],
+        extras_require={
+            'ver1': ['awscli'],
+        },
+        license="Apache License 2.0",
+        classifiers=[
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "License :: OSI Approved :: Apache Software License",
+            "Topic :: Software Development :: Testing"
+        ]
+    )
