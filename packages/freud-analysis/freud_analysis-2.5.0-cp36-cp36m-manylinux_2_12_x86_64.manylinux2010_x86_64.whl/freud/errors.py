ab7af2b7390722b34764797a2877e03eb48cb8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:35 +0000

    Add package freud-analysis/2.5.0/freud_analysis-2.5.0-cp36-cp36m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/freud-analysis/freud_analysis-2.5.0-cp36-cp36m-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/freud/errors.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2010-2020 The Regents of the University of Michigan
+# This file is from the freud project, released under the BSD 3-Clause License.
+
+# \package freud.errors
+#
+# Errors and exceptions internal to freud
+
+
+class FreudDeprecationWarning(UserWarning):
+    """Raised when a freud feature is pending deprecation."""
+
+    pass
+
+
+NO_DEFAULT_QUERY_ARGS_MESSAGE = (
+    "The {} class does not provide default query arguments. Query arguments "
+    "or a neighbor list must be provided to this compute method."
+)
