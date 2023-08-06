eb5fc5b034a1f06db5e0777885d473659a14a928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:46 +0000

    Add package whatsappy-py/0.1.7.1/whatsappy-py-0.1.7.1.tar.gz

---
+++ packages/whatsappy-py/whatsappy-py-0.1.7.1.tar.gz/whatsappy-py-0.1.7.1/examples/leave_group.py
@@ -0,0 +1,10 @@
+from whatsappy import whatsapp
+
+whatsapp.login()
+
+whatsapp.select_chat('Family Group')
+
+if whatsapp.last_message() == 'Leave':
+    whatsapp.leave_group() # <--
+
+whatsapp.exit()
