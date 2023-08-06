f558330fda7920d59d5861ec24821d5262e71c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:34 +0000

    Add package mypy-boto3-storagegateway/1.17.33.0/mypy_boto3_storagegateway-1.17.33.0-py3-none-any.whl

---
+++ packages/mypy-boto3-storagegateway/mypy_boto3_storagegateway-1.17.33.0-py3-none-any.whl/mypy_boto3_storagegateway/__main__.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    print(
+        "Type annotations for boto3.StorageGateway 1.17.33\n"
+        "Version:         1.17.33.0\n"
+        "Builder version: 4.4.0\n"
+        "Docs:            https://pypi.org/project/mypy-boto3-storagegateway/\n"
+        "Other services:  https://pypi.org/project/boto3-stubs/\n"
+        "Changelog:       https://github.com/vemel/mypy_boto3_builder/releases"
+    )
