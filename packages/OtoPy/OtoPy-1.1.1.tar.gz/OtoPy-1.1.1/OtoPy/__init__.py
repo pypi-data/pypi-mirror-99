886988032ec3fa74c084b5ae68b32d62336fccb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:07 +0000

    Add package OtoPy/1.1.1/OtoPy-1.1.1.tar.gz

---
+++ packages/OtoPy/OtoPy-1.1.1.tar.gz/OtoPy-1.1.1/OtoPy/__init__.py
@@ -0,0 +1,9 @@
+"""
+OtoPy.
+
+A Otoma Systems developed Lib, Containing useful Tools.
+"""
+
+__version__ = "1.1.1"
+__author__ = 'Otoma Systems'
+__name__="OtoPy"
---
+++ packages/OtoPy/OtoPy-1.1.1.tar.gz/OtoPy-1.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='OtoPy',
+    version='1.1.1',    
+    description='A Otoma Systems developed Lib, Containing useful Tools',
+    url='https://github.com/Otoma-Systems/OtoPy.git',
+    author='Otoma Systems',
+    author_email='otoma.system@gmail.com',
+    license='BSD 2-clause',
+    packages=['OtoPy'],
+    install_requires=[],
+
+    classifiers=[
+        'Natural Language :: English',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: Console',        
+        'Environment :: Other Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Utilities'
+    ]
+)
