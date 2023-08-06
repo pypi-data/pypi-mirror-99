fd5b8b15af7a2616311ae506a82f7685a6d45cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:43 +0000

    Add package lambda-utility/0.6.2/lambda-utility-0.6.2.tar.gz

---
+++ packages/lambda-utility/lambda-utility-0.6.2.tar.gz/lambda-utility-0.6.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lambda_utility']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiobotocore>=1.2.2,<2.0.0',
+ 'pydantic>=1.8.1,<2.0.0',
+ 'slack-sdk>=3.4.2,<4.0.0']
+
+setup_kwargs = {
+    'name': 'lambda-utility',
+    'version': '0.6.2',
+    'description': '',
+    'long_description': '# lambda-utility\n[![Python](https://img.shields.io/badge/python-v3.9-blue.svg?&logo=python&style=flat)](https://docs.python.org/3.9/)\n[![Black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)\n[![Checked with mypy](http://www.mypy-lang.org/static/mypy_badge.svg)](http://mypy-lang.org/)\n\nAWS Lambda에서 자주 사용하는 기능 구현\n\n## Installation\nPython 3.7 +\n```bash\n$ pip install lambda-utility\n```\n',
+    'author': 'Kyungmin Lee',
+    'author_email': 'rekyungmin@gmail.com',
+    'maintainer': 'Kyungmin Lee',
+    'maintainer_email': 'rekyungmin@gmail.com',
+    'url': 'https://github.com/rekyungmin/lambda-utils',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
