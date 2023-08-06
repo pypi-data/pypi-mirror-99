08cb290e63b5a602f6d9867dbc3f200c47457266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:34 +0000

    Add package Rest-Utils/1.2.4/Rest-Utils-1.2.4.tar.gz

---
+++ packages/Rest-Utils/Rest-Utils-1.2.4.tar.gz/Rest-Utils-1.2.4/rest_utils/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Author  :   windpro
+E-mail  :   windprog@gmail.com
+Date    :   17/11/15
+Desc    :   
+"""
+from .sa_util import migrate_skip
+from .manager import APIManager
+from .exception import RestException
+from .schema import ModelSchema
---
+++ packages/Rest-Utils/Rest-Utils-1.2.4.tar.gz/Rest-Utils-1.2.4/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2014 windpro
+
+Author  :   windpro
+E-mail  :   windprog@gmail.com
+Date    :   14-12-1
+Desc    :   
+"""
+import os
+from setuptools import setup
+
+__version__ = "1.2.4"
+__author__ = "Windpro"
+__author_email__ = "windprog@gmail.com"
+__description__ = "Rest-Utils provides simple generation of Restful APIs for database models defined using SQLAlchemy (or Flask-SQLAlchemy). The generated APIs send and receive messages in JSON format. support gunicorn and multiline process worker. "
+__title__ = 'Rest-Utils'
+__url__ = "https://github.com/windprog/rest-utils"
+
+req = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+with open(req) as f:
+    install_requires = [line for line in f.read().split("\n") if line]
+
+setup(
+    name=__title__,
+    version=__version__,
+    description=__description__,
+    url=__url__,
+    author=__author__,
+    author_email=__author_email__,
+    packages=['rest_utils', 'rest_utils.worker', 'rest_utils.utils', 'rest_utils.libs', 'rest_utils.ma'],
+    install_requires=install_requires,
+    data_files=[(".", ['requirements.txt'])],  # save requirements.txt to install package
+)
