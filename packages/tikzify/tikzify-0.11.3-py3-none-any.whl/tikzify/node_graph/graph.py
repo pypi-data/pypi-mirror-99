5ddad218abe656edb0944f932ee9aff8c6c09f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:09 +0000

    Add package tikzify/0.11.3/tikzify-0.11.3-py3-none-any.whl

---
+++ packages/tikzify/tikzify-0.11.3-py3-none-any.whl/tikzify/node_graph/graph.py
@@ -0,0 +1,257 @@
+from __future__ import annotations
+
+import itertools as it
+from copy import copy
+from typing import Mapping, Optional, Sequence, TextIO, Tuple, Union
+
+import networkx as nx
+
+from .anchor import CoordinateAnchor, IntersectionAnchor, NodeAnchor, RelativeAnchor
+from .edge import Edge
+from .multi_edge import angles, create_waypoints, default_waypoint_names
+from .node import NodeContainer, NodeLabel, NodePosition, NodeText, generate_node
+
+__all__ = ['NodeGraph']
+
+
+class NodeGraph:
+
+    def __init__(self, edge_colors: Optional[Mapping[str, str]] = None):
+        self.digraph = nx.MultiDiGraph()
+        self.edge_colors = {} if edge_colors is None else edge_colors
+
+    # Magic methods --------------------------------------------------------------------------------
+    def __repr__(self) -> str:
+        return repr(self.digraph)
+
+    # Information methods --------------------------------------------------------------------------
+    def nodes(self) -> Sequence[str]:
+        return sorted(self.digraph)
+
+    def node_opacity(self, name: str, dependency_graph: nx.DiGraph) -> float:
+        """
+        Calculate the maximum opacity of the node, its afferent edges, its fit group (if it has
+        one), and set the node's opacity accordingly.
+        """
+        g = self.digraph
+        node_dict = g.nodes[name]
+
+        # Calculate the maximum opacity of the node and its afferent edges.
+        o = node_dict.get('opacity', 0.0)
+        for successor in it.chain([name], dependency_graph.successors(name)):
+            for ed in it.chain(g.succ[successor].values(), g.pred[successor].values()):
+                for tip in ed.values():
+                    o = max(o, tip.get('opacity', 1.0))
+
+        # If the node is in a fit group, consider that too.
+        if 'container' in node_dict:
+            container = node_dict['container']
+            if not isinstance(container, NodeContainer):
+                raise TypeError
+            for node in container.nodes:
+                o = max(o, self.node_opacity(node, dependency_graph))
+
+        return o
+
+    # Graph creation methods -----------------------------------------------------------------------
+    def create_coordinate(self,
+                          name: str,
+                          position: Union[Tuple[float, float], NodePosition]) -> None:
+        if isinstance(position, tuple):
+            position = NodePosition(CoordinateAnchor(*position))
+        self.digraph.add_node(name,
+                              position=position,
+                              is_coordinate=True)
+
+    def create_node(self,
+                    name: str,
+                    position: Optional[NodePosition],
+                    container: Optional[NodeContainer] = None,
+                    *,
+                    # Text
+                    text: Optional[Union[str, NodeText]] = None,
+                    label: Optional[NodeLabel] = None,
+                    # Appearance
+                    size: Optional[Tuple[float, float]] = None,
+                    shape: Optional[str] = None,
+                    color: Optional[str] = None,
+                    dash: Optional[str] = None,
+                    opacity: Optional[float] = None,
+                    inner_sep: Optional[float] = None) -> None:
+        if position is None and container is None:
+            raise ValueError
+        if position is not None and container is not None:
+            raise ValueError
+        if isinstance(text, str):
+            text = NodeText([text])
+        d = dict(text=text,
+                 label=label,
+                 size=size,
+                 shape=shape,
+                 color=color,
+                 dash=dash,
+                 opacity=opacity,
+                 inner_sep=inner_sep,
+                 container=container)
+        d = {key: value
+             for key, value in d.items()
+             if value is not None}
+        self.digraph.add_node(name, position=position, **d)
+
+    def create_edge(self,
+                    source: str,
+                    target: str,
+                    edge: Edge,
+                    *,
+                    via: Optional[Tuple[bool, Sequence[str]]] = None) -> None:
+        if source not in self.digraph:
+            raise ValueError(f"{source} not in graph.")
+        if target not in self.digraph:
+            raise ValueError(f"{target} not in graph.")
+        self.digraph.add_edge(source, target, edge=edge, via=via)
+
+    def create_edges(self,
+                     left: str, right: str, top: str, bottom: str,
+                     margin_left: bool = True,
+                     margin_right: bool = True,
+                     margin_top: bool = True,
+                     margin_bottom: bool = True,
+                     *,
+                     margin: Optional[float] = None) -> None:
+        if left not in self.digraph:
+            raise ValueError(f"{left} not in graph.")
+        if right not in self.digraph:
+            raise ValueError(f"{right} not in graph.")
+        if top not in self.digraph:
+            raise ValueError(f"{top} not in graph.")
+        if bottom not in self.digraph:
+            raise ValueError(f"{bottom} not in graph.")
+        self.create_coordinate('left_edge',
+                               NodePosition(RelativeAnchor(left, 'west'),
+                                            left=margin if margin_left else None))
+        self.create_coordinate('right_edge',
+                               NodePosition(RelativeAnchor(right, 'east'),
+                                            right=margin if margin_right else None))
+        self.create_coordinate('top_edge',
+                               NodePosition(RelativeAnchor(top, 'north'),
+                                            above=margin if margin_top else None))
+        self.create_coordinate('bottom_edge',
+                               NodePosition(RelativeAnchor(bottom, 'south'),
+                                            below=margin if margin_bottom else None))
+
+    def create_node_terminals(self,
+                              node_name: str,
+                              left: Optional[Tuple[int, float]] = None,
+                              right: Optional[Tuple[int, float]] = None,
+                              above: Optional[Tuple[int, float]] = None,
+                              below: Optional[Tuple[int, float]] = None) -> None:
+        for n_step, cardinal, name_prefix, vertical in zip(
+                [left, right, above, below],
+                ['west', 'east', 'north', 'south'],
+                ['left_of', 'right_of', 'above', 'below'],
+                [True, True, False, False]):
+            if n_step is None:
+                continue
+            n, step = n_step
+            for i, j in angles(0, n, step=step):
+                x = 0 if vertical else j
+                y = j if vertical else 0
+                self.create_coordinate(f'{name_prefix}_{node_name}{i}',
+                                       NodePosition(RelativeAnchor(node_name, cardinal),
+                                                    right=x,
+                                                    above=y))
+
+    def create_io(self,
+                  node_name: str,
+                  text: NodeText,
+                  edge: str,
+                  terminal: str,
+                  relative: Optional[float] = None) -> None:
+        """
+        Places an input/output on the edge, perpendicular to the terminal.
+        """
+        if edge in ['bottom_edge', 'top_edge']:
+            intersection = IntersectionAnchor(NodeAnchor(terminal), NodeAnchor(edge))
+            key = 'above' if edge == 'top_edge' else 'below'
+        else:
+            intersection = IntersectionAnchor(NodeAnchor(edge), NodeAnchor(terminal))
+            key = 'right' if edge == 'right_edge' else 'left'
+        position = NodePosition(intersection, **{key: 0.0 if relative is None else relative})
+
+        self.create_node(node_name,
+                         position,
+                         text=text)
+
+    # Output methods -------------------------------------------------------------------------------
+    def generate(self, f: TextIO) -> None:
+        g = self.digraph
+        dependency_graph, topo_sorted = self._dependencies()
+        D = default_waypoint_names()
+
+        # Draw nodes.
+        for name in topo_sorted:
+            node_dict = g.nodes[name]
+            generate_node(name, node_dict, opacity=self.node_opacity(name, dependency_graph),
+                          file=f)
+
+        # Draw edges.
+        for source in topo_sorted:
+            for (target, ed) in sorted(g.succ[source].items()):
+                len_ed = len(ed)
+                for i, edge_dict in enumerate(ed.values()):
+                    edge: Edge = copy(edge_dict['edge'])
+                    via = edge_dict.get('via', None)
+
+                    if edge.loop is None:
+                        bend = edge.bend
+                        bend += (0
+                                 if len_ed == 1 else
+                                 (2 * i - 1) * 14
+                                 if len_ed == 2 else
+                                 (i - 1) * 20
+                                 if len_ed == 3 else
+                                 0)
+                        edge.bend = bend
+
+                    color = edge.solve_for_color(self.edge_colors)
+
+                    if via is not None:
+                        vertical, turns = via
+                        create_waypoints(f,
+                                         edge,
+                                         source,
+                                         list(turns) + [target],
+                                         vertical,
+                                         D,
+                                         color)
+                    else:
+                        edge.pf(f, source, target, color=color)
+
+    # Private methods ------------------------------------------------------------------------------
+    def _dependencies(self) -> Tuple[Sequence[str], nx.DiGraph]:
+        """
+        Returns:
+            dependency_graph: The graph of dependencies.
+            topo_sorted: A topological sort of the dependency graph
+        """
+        g = self.digraph
+        g2 = nx.DiGraph()  # A graph with all dependencies.
+        g2.add_nodes_from(g)
+        for name in g:
+            node_dict = g.nodes[name]
+            position = node_dict.get('position', None)
+            if position is not None:
+                if not isinstance(position, NodePosition):
+                    raise TypeError
+                for node in position.anchor.base_nodes():
+                    g2.add_edge(node, name)
+            if 'container' in node_dict:
+                container = node_dict['container']
+                if not isinstance(container, NodeContainer):
+                    raise TypeError
+                for node in container.nodes:
+                    g2.add_edge(node, name)
+        try:
+            return g2, list(nx.lexicographical_topological_sort(g2))
+        except nx.NetworkXUnfeasible:
+            raise ValueError("Cyclic dependencies: " + str(list(nx.simple_cycles(g2)))) from None
---
+++ packages/tikzify/tikzify-0.11.3-py3-none-any.whl/tikzify/node_graph/node.py
@@ -0,0 +1,243 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Any, Collection, Mapping, Optional, Sequence, TextIO
+
+from ..foundation import formatter
+from ..foundation.pf import pf, tikz_option
+from .anchor import Anchor
+
+__all__ = ['NodeLabel', 'NodePosition', 'NodeText', 'NodeContainer', 'Alignment', 'TextSize']
+
+
+class Alignment(Enum):
+    left = auto()
+    right = auto()
+    center = auto()
+
+
+class TextSize(Enum):
+    tiny = auto()
+    script = auto()
+    footnote = auto()
+    small = auto()
+    normalsize = auto()
+    large = auto()
+    Large = auto()
+    LARGE = auto()
+    huge = auto()
+    Huge = auto()
+
+
+def format_length(length: Optional[float]) -> Optional[str]:
+    if length is None:
+        return None
+    if int(length) == length:
+        length = int(length)
+        if length % 10 == 0:
+            return f"{length // 10}cm"
+    return f"{length}mm"
+
+
+def wrap_text(text_lines: Sequence[str],
+              color: Optional[str],
+              wrap_command: Optional[str],
+              size: Optional[TextSize],
+              standard_height: bool) -> str:
+    def wrap(line: str) -> str:
+        if standard_height:
+            line = r'\vphantom{Ag}' + line
+        if size is not None:
+            size_text = '\\' + (size.name + 'size'
+                                if size in {TextSize.script, TextSize.footnote}
+                                else size.name)
+            line = size_text + line
+        if not isinstance(line, str):
+            raise TypeError
+        if color is not None:
+            line = r'\textcolor{' + color + '}{' + line + '}'
+        elif size is not None:
+            line = '{' + line + '}'
+        if wrap_command is not None:
+            line = rf"\{wrap_command}{{{line}}}"
+        return line
+    retval = r' \\ '.join(wrap(line) for line in text_lines)
+    # if '\\' in retval:
+    #     retval = '{' + retval + '}'
+    return retval
+
+
+@dataclass
+class NodeText:
+    text_lines: Sequence[str]
+    wrap_command: Optional[str] = None
+    color: Optional[str] = None
+    width: Optional[float] = None
+    align: Optional[Alignment] = None
+    size: Optional[TextSize] = None
+    standard_height: bool = False
+
+    def latex(self, inherit_color: Optional[str]) -> str:
+        return wrap_text(self.text_lines,
+                         inherit_color if self.color is None else self.color,
+                         self.wrap_command,
+                         self.size,
+                         self.standard_height)
+
+    def latex_options(self) -> Optional[str]:
+        if self.align is None and len(self.text_lines) >= 2:
+            raise ValueError("No alignment specified")
+        retval = formatter("“text_width,align”",
+                           text_width=tikz_option('text width', format_length(self.width)),
+                           align=(None
+                                  if self.align is None
+                                  else tikz_option('align', self.align.name)))
+        return retval if retval else None
+
+
+@dataclass
+class NodeLabel:
+    text_lines: Sequence[str]
+    location: Optional[str] = None
+    color: Optional[str] = None
+    size: Optional[TextSize] = None
+    standard_height: bool = False
+
+    def latex(self, inherit_color: Optional[str]) -> str:
+        text_lines = self.text_lines
+
+        color = inherit_color if self.color is None else self.color
+        text = wrap_text(text_lines, color, None, self.size, self.standard_height)
+        if self.location:
+            text = f'{self.location}:{text}'
+        return 'label=' + text
+
+
+@dataclass
+class NodePosition:
+    anchor: Anchor
+    left: Optional[float] = None
+    right: Optional[float] = None
+    above: Optional[float] = None
+    below: Optional[float] = None
+
+    def __post_init__(self) -> None:
+        assert self.left is None or self.right is None
+        assert self.above is None or self.below is None
+
+    def is_relative(self) -> bool:
+        return not all(x is None for x in [self.left, self.right, self.above, self.below])
+
+    def latex_position(self) -> str:
+        if self.is_relative():
+            return ""
+        return 'at ({}) '.format(self.anchor.as_tikz())
+
+    def latex_relative_position(self) -> str:
+        if not self.is_relative():
+            return ""
+        words = []
+        values = []
+        if self.above is not None:
+            words.append('above')
+            values.append(self.above)
+        elif self.below is not None:
+            words.append('below')
+            values.append(self.below)
+        if self.left is not None:
+            words.append('left')
+            values.append(self.left)
+        elif self.right is not None:
+            words.append('right')
+            values.append(self.right)
+        words_str = " ".join(words)
+        direction_str = " and ".join(str(v) for v in values)
+        return f'[{words_str}={direction_str} of {self.anchor.as_tikz()}] '
+
+
+@dataclass
+class NodeContainer:
+    nodes: Collection[str]
+    corner_text: Optional[NodeText] = None
+    corner_color: Optional[str] = None
+
+    def latex_fit(self) -> str:
+        nodes = sorted('(' + node + ')' for node in self.nodes)
+        return 'fit={' + " ".join(nodes) + '}'
+
+    def latex_corner(self,
+                     parent_name: str,
+                     opacity: Optional[float],
+                     inherit_color: Optional[str]) -> str:
+        if self.corner_text is None:
+            return ""
+        color = self.corner_color if self.corner_color is not None else inherit_color
+        return formatter(
+            r"\node[“color,opacity,text_options,”align=right, below left] at (“name”.north east) "
+            r"{“text”}",
+            name=parent_name,
+            text_options=None if self.corner_text is None else self.corner_text.latex_options(),
+            text=None if self.corner_text is None else self.corner_text.latex(color),
+            color=color,
+            opacity=None if opacity is None else tikz_option('opacity', str(opacity)))
+
+
+def generate_node(name: Optional[str],
+                  node_dict: Mapping[str, Any],
+                  *,
+                  opacity: Optional[float] = None,
+                  file: TextIO,
+                  end: str = ';\n') -> None:
+    """
+    Generate text for a node.
+    """
+    # Arguments.
+    position = node_dict.get('position', None)
+    coordinate = node_dict.get('is_coordinate', False)
+    text = node_dict.get('text', None)
+    label = node_dict.get('label', None)
+    size = node_dict.get('size', None)
+    shape = node_dict.get('shape', None)
+    color = node_dict.get('color', None)
+    dash = node_dict.get('dash', None)
+    inner_sep = node_dict.get('inner_sep', None)
+    container = node_dict.get('container', None)
+
+    size_latex = (None
+                  if size is None
+                  else (f'minimum width={format_length(size[0])}, '
+                        f'minimum height={format_length(size[1])}'))
+
+    d = dict(name=name,
+             position=None if position is None else position.latex_position(),
+             relative_position=None if position is None else position.latex_relative_position(),
+             text=None if text is None else text.latex(color),
+             text_options=None if text is None else text.latex_options(),
+             label=None if label is None else label.latex(color),
+             size=size_latex,
+             shape=shape,
+             color=color,
+             dash=dash,
+             opacity=None if opacity is None else tikz_option('opacity', str(opacity)),
+             inner_sep=tikz_option('inner sep', format_length(inner_sep)),
+             fit=None if container is None else container.latex_fit())
+    if coordinate:
+        pf(r"\coordinate “relative_position” (“name”) “position”",
+           **d,
+           file=file,
+           end=end)
+    elif name is None:
+        assert position is None
+        pf(r"node[“text_options,label,shape,size,color,dash,opacity,inner_sep,fit”] {“text”}",
+           **d,
+           file=file,
+           end=end)
+    else:
+        pf(r"\node[“text_options,label,shape,size,color,dash,opacity,inner_sep,fit”] "
+           r"“position” (“name”) “relative_position” {“text”}",
+           **d,
+           file=file,
+           end=end)
+    if container is not None:
+        print(container.latex_corner(name, opacity, color),
+              file=file,
+              end=end)
