d4460f460e8301ccf1b2e5ded0a286aed7c313d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:14 +0000

    Add package utilsrobot/2.1/utilsrobot-2.1.tar.gz

---
+++ packages/utilsrobot/utilsrobot-2.1.tar.gz/utilsrobot-2.1/bot/callbacks.py
@@ -0,0 +1,54 @@
+from bot.utilsbot import callback
+from bot.helptexts import (
+    welcomehelpp,
+    filterrhelp,
+    promotehelpp,
+    banhelpp,
+    kickhelpp,
+    pinhelpp,
+    purgehelpp,
+    notesshelp,
+    lockktypes,
+    lockhelpp,
+    fedhelpp,
+)
+
+@callback("banhelp")
+async def _(event):
+    await event.edit(f"{banhelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("promotehelp")
+async def _(event):
+    await event.edit(f"{promotehelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("kickhelp")
+async def _(event):
+    await event.edit(f"{kickhelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("pinhelp")
+async def _(event):
+    await event.edit(f"{pinhelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("purgehelp")
+async def _(event):
+    await event.edit(f"{purgehelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("lockhelp")
+async def _(event):
+    await event.edit(f"{lockhelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("filterhelp")
+async def _(event):
+    await event.edit(f"{filterrhelp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("noteshelp")
+async def _(event):
+    await event.edit(f"{notesshelp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("welcomehelp")
+async def _(event):
+    await event.edit(f"{welcomehelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
+
+@callback("fedhelp")
+async def _(event):
+    await event.edit(f"{fedhelpp}",buttons=[Button.inline(" <-- Back", data="helpstarter")])
