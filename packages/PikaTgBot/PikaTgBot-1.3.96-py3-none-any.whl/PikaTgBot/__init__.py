34485860e13dfb30f1f06015cae5e8326b5265c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:03 +0000

    Add package PikaTgBot/1.3.96/PikaTgBot-1.3.96-py3-none-any.whl

---
+++ packages/PikaTgBot/PikaTgBot-1.3.96-py3-none-any.whl/PikaTgBot/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020 by ItzSjDude@Github, < https://github.com/ItzSjDude/PikachuUserbot >.
+#
+# This file is part of < https://github.com/ItzSjDude/PikachuUserbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+#
+# Please see < https://github.com/ItzSjDude/PikachuUserbot/blob/master/LICENSE >
+#
+# All rights reserved
+
+
+from .clients import *
+from .resources import *
+from .core import *
+from .sysutils import *
