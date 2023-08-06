40a48d10e184f055551079dd3975d24c8248efcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:40 +0000

    Add package microscope-automation/0.3.3/microscope_automation-0.3.3-py2.py3-none-any.whl

---
+++ packages/microscope-automation/microscope_automation-0.3.3-py2.py3-none-any.whl/microscope_automation/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Microscope Automation."""
+
+__author__ = "Fletcher Chapin"
+__email__ = "fletcher.chapin@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.3.3"
+
+
+def get_module_version():
+    return __version__
