729884c34508cf00bf957640c7fbe68d4e5679ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:12 +0000

    Add package invenio-drafts-resources/0.10.6/invenio_drafts_resources-0.10.6-py2.py3-none-any.whl

---
+++ packages/invenio-drafts-resources/invenio_drafts_resources-0.10.6-py2.py3-none-any.whl/invenio_drafts_resources/records/models.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Draft Models API."""
+
+from datetime import datetime
+
+from invenio_db import db
+from invenio_records.models import RecordMetadataBase
+from sqlalchemy.dialects import mysql
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy_utils.types import UUIDType
+
+
+class ParentRecordMixin:
+    """A mixin factory that add the foreign keys to the parent record.
+
+    Usage:
+
+    .. code-block:: python
+
+        class MyRecord(db.Model, RecordMetadataBase, ParentRecordMixin):
+            __parent_record_model__ = ParentRecordMetadata
+    """
+
+    __parent_record_model__ = None
+
+    @declared_attr
+    def parent_id(cls):
+        """Parent identifier."""
+        # We restrict deletion of the parent record in case a record or draft
+        # exists via database-level trigger.
+        return db.Column(UUIDType, db.ForeignKey(
+            cls.__parent_record_model__.id, ondelete="RESTRICT"))
+
+    @declared_attr
+    def parent(cls):
+        """Relationship to parent record."""
+        return db.relationship(cls.__parent_record_model__)
+
+    index = db.Column(db.Integer, nullable=True)
+    """The version index of the record."""
+
+
+class ParentRecordStateMixin:
+    """Database model mixin to keep the state of the latest and next version.
+
+    We keep this data outside the parent record itself, because we want to
+    update it without impacting the parent record's version counter. The
+    version counter in the parent record we use to determine if we have to
+    reindex all record versions.
+
+    Usage:
+
+    .. code-block:: python
+
+        class MyParentState(db.Model, ParentRecordState):
+            __parent_record_model__ = MyParentRecord
+            __draft_model__ = MyDraft
+            __record_model__ = MyRecord
+    """
+
+    __parent_record_model__ = None
+    __record_model__ = None
+    __draft_model__ = None
+
+    @declared_attr
+    def parent_id(cls):
+        """Parent record identifier."""
+        return db.Column(
+            UUIDType,
+            # 1) If the parent record is deleted, we automatically delete
+            # the state as well via database-level on delete trigger.
+            # 2) A parent can only be deleted, if it has no drafts/records via
+            # FKs from drafts/records to the parent.
+            db.ForeignKey(cls.__parent_record_model__.id, ondelete="CASCADE"),
+            primary_key=True,
+        )
+
+    @declared_attr
+    def latest_id(cls):
+        """UUID of the latest published record/draft.
+
+        Note, since a record and draft share the same UUID, the UUID can be
+        used to get both the record or the draft. It's a foreign key to the
+        record to ensure that the record exists (and thus is published).
+
+        If no record has been published, the value is None.
+        """
+        return db.Column(
+            UUIDType,
+            db.ForeignKey(cls.__record_model__.id),
+            nullable=True,
+        )
+
+    latest_index = db.Column(db.Integer, nullable=True)
+    """The index of the latest published record.
+
+    If no record has been published, the value is None.
+    """
+
+    @declared_attr
+    def next_draft_id(cls):
+        """UUID of the draft for the next version (yet to be published).
+
+        Note, since a record and draft share the same UUID, the UUID can be
+        used to get both the record or the draft. It's a foreign key to the
+        draft, because we use it to track if a draft has been created for the
+        next version.
+
+        If no draft for the next version has been created, the value is None.
+        """
+        return db.Column(
+            UUIDType,
+            db.ForeignKey(cls.__draft_model__.id),
+            nullable=True,
+        )
+
+
+class DraftMetadataBase(RecordMetadataBase):
+    """Represent a base class for draft metadata."""
+
+    fork_version_id = db.Column(db.Integer)
+    """Version ID of the record."""
+
+    expires_at = db.Column(
+        db.DateTime().with_variant(mysql.DATETIME(fsp=6), "mysql"),
+        default=datetime.utcnow,
+        nullable=True
+    )
+    """Specifies when the draft expires. If `NULL` the draft doesn't expire."""
---
+++ packages/invenio-drafts-resources/invenio_drafts_resources-0.10.6-py2.py3-none-any.whl/invenio_drafts_resources/records/systemfields/parent.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Parent record system field."""
+
+from invenio_db import db
+from invenio_records.systemfields import RelatedModelField
+from sqlalchemy.exc import IntegrityError
+
+
+class ParentField(RelatedModelField):
+    """Parent record field."""
+
+    def __init__(self, model, key=None, create=False,
+                 soft_delete=True, hard_delete=True):
+        """Initialize the parent field."""
+        self._soft_delete = soft_delete
+        self._hard_delete = hard_delete
+        self._create = create
+        super().__init__(
+            model,
+            key=key,
+            required=True,
+            dump=self.dump_parent,
+            load=self.load_parent,
+        )
+
+    def create(self, record):
+        """Method to create the parent record for the record."""
+        parent = getattr(record, self.attr_name)
+        if parent is None:
+            # Create a parent record
+            parent = record.parent_record_cls.create({})
+            # This bumps revision id to 2 for the first record. Commit needed
+            # because of the system fields defined on the parent record (e.g.
+            # pid).
+            parent.commit()
+
+            # Set using the __set__() method
+            setattr(record, self.attr_name, parent)
+        return parent
+
+    def delete(self, record, force=False):
+        """Method to delete the parent record.
+
+        The parent record will only be hard deleted if this is the last
+        record/draft with links to it.
+        """
+        # A *record* is configured to never delete the parent
+        # automatically.
+        # A *draft* is configured to delete the parent on hard delete but not
+        # on soft delete.
+        #
+        # A draft may be hard-deleted in two cases: 1) a new record 2) a new
+        # record version. In the case 1, only a draft and the parent record
+        # exists. In case 2, one or more drafts/records may exists with a
+        # foreign key to the parent record.
+        #
+        # The logic implemented here, thus on a hard delete tries to delete the
+        # the parent record and relies on the database integrity constraints
+        # to prevent the parent record from being deleted in case more
+        # drafts/records exists.
+        parent = getattr(record, self.attr_name)
+        if parent:
+            if force and self._hard_delete:
+                try:
+                    with db.session.begin_nested():
+                        parent.delete(force=True)
+                except IntegrityError:
+                    # It's ok - the draft/record linking to this parent is not
+                    # the last one.
+                    pass
+            elif not force and self._soft_delete:
+                parent.delete(force=False)
+
+    #
+    # Life-cycle hooks
+    #
+    def pre_create(self, record):
+        """Called before a record is created (but after being initialized)."""
+        if self._create:
+            self.create(record)
+
+    def post_delete(self, record, force=False):
+        """Called after a record is deleted."""
+        if self._hard_delete or self._soft_delete:
+            self.delete(record, force=force)
+
+    def pre_dump(self, record, data, dumper=None):
+        """Called before a record is dumped."""
+        parent = getattr(record, self.attr_name)
+        # Use the default dumper for the parent (since the records/drafts
+        # dumpers won't work on a parent)
+        if parent:
+            data[self.attr_name] = parent.dumps(dumper=None)
+
+    def post_load(self, record, data, loader=None):
+        """Called after a record was loaded."""
+        parent_dump = record.pop(self.attr_name, None)
+        if parent_dump:
+            parent = record.parent_record_cls.loads(parent_dump, loader=loader)
+            setattr(record, self.attr_name, parent)
+
+    #
+    # Helpers
+    #
+    @staticmethod
+    def load_parent(field, record):
+        """Serializer the object into a record."""
+        if record.model.parent_id:
+            return record.parent_record_cls.get_record(record.model.parent_id)
+        return None
+
+    @staticmethod
+    def dump_parent(field, record, parent_record):
+        """Set the object."""
+        record.model.parent_id = parent_record.id
