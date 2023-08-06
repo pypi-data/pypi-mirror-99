2f1d832084e016ba34a139d22ab2f300c718f748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:18 +0000

    Add package conmato/0.0.1/conmato-0.0.1-py3-none-any.whl

---
+++ packages/conmato/conmato-0.0.1-py3-none-any.whl/conmato/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from conmato.check_plagiarism import *
+from conmato.crawl_standings import *
+from conmato.utils import *
+from conmato.mossum import *
+from conmato.csession import *
+from conmato import member
+from conmato import contest
+
+from .cli import cli
