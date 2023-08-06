81915baa00a22882214104099d0bbe420d59ad0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:45 +0000

    Add package aws-solutions-constructs.aws-iot-lambda/1.92.0/aws_solutions_constructs.aws_iot_lambda-1.92.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-iot-lambda/aws_solutions_constructs.aws_iot_lambda-1.92.0-py3-none-any.whl/aws_solutions_constructs/aws_iot_lambda/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_iot._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import aws_solutions_constructs.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-solutions-constructs/aws-iot-lambda",
+    "1.92.0",
+    __name__[0:-6],
+    "aws-iot-lambda@1.92.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
