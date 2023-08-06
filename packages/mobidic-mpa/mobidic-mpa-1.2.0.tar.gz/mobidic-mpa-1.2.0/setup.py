759d61f95455709a6a581d42d09cc974acd54b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:38 +0000

    Add package mobidic-mpa/1.2.0/mobidic-mpa-1.2.0.tar.gz

---
+++ packages/mobidic-mpa/mobidic-mpa-1.2.0.tar.gz/mobidic-mpa-1.2.0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021
+#
+
+__author__ = 'Mobidic'
+__authors__ = [
+    'Henri Pegeot',
+    'Kevin Yauy',
+    'Charles Van Goethem',
+    'David Baux'
+]
+__copyright__ = 'Copyright (C) 2021'
+__license__ = 'Academic License Agreement'
+__version__ = '1.2.0'
+__email__ = 'c-vangoethem@chu-montpellier.fr'
+__status__ = 'prod'
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mobidic-mpa",
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    description="MPA: MoBiDiC Prioritization Algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://neuro-2.iurc.montp.inserm.fr/mpaweb/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=['pyvcf==0.6.8'],
+    scripts=['scripts/mpa'],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/mobidic/MPA/issues',
+        'Source': 'https://github.com/mobidic/MPA',
+    }
+)
