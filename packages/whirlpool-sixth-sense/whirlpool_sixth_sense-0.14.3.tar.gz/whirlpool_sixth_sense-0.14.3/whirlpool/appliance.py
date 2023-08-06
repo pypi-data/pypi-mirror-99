58592ae7ed690689a625287ded14f8aa315ad239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:26 +0000

    Add package whirlpool-sixth-sense/0.14.3/whirlpool_sixth_sense-0.14.3.tar.gz

---
+++ packages/whirlpool-sixth-sense/whirlpool_sixth_sense-0.14.3.tar.gz/whirlpool_sixth_sense-0.14.3/whirlpool/appliance.py
@@ -0,0 +1,148 @@
+import aiohttp
+import asyncio
+import async_timeout
+import logging
+import json
+from datetime import datetime, timedelta, timedelta
+from typing import Callable
+
+from .auth import Auth
+from .eventsocket import EventSocket
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Appliance:
+    def __init__(self, auth: Auth, said: str, attr_changed: Callable):
+        self._auth = auth
+        self._said = said
+        self._attr_changed = attr_changed
+        self._data_dict = None
+
+        self._session: aiohttp.ClientSession = None
+        self._event_socked = EventSocket(
+            auth.get_access_token(), said, self._event_socket_handler
+        )
+
+    def _event_socket_handler(self, msg):
+        json_msg = json.loads(msg)
+        timestamp = json_msg["timestamp"]
+        for (attr, val) in json_msg["attributeMap"].items():
+            if not self.has_attribute(attr):
+                continue
+            self._set_attribute(attr, str(val), timestamp)
+
+        if self._attr_changed:
+            self._attr_changed()
+
+    def _create_headers(self):
+        return {
+            "Authorization": "Bearer " + self._auth.get_access_token(),
+            "Content-Type": "application/json",
+            "Host": "api.whrcloud.eu",
+            "User-Agent": "okhttp/3.12.0",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+        }
+
+    def _set_attribute(self, attribute, value, timestamp):
+        logging.debug(f"Updating attribute {attribute} with {value} ({timestamp})")
+        self._data_dict["attributes"][attribute]["value"] = value
+        self._data_dict["attributes"][attribute]["updateTime"] = timestamp
+
+    @property
+    def said(self):
+        return self._said
+
+    async def fetch_data(self):
+        if not self._session:
+            LOGGER.error("Session not started")
+            return False
+
+        uri = f"https://api.whrcloud.eu/api/v1/appliance/{self._said}"
+        self._data_dict = None
+        with async_timeout.timeout(30):
+            async with self._session.get(uri) as r:
+                self._data_dict = json.loads(await r.text())
+                if r.status == 200:
+                    return True
+                LOGGER.error(f"Fetching data failed ({r.status})")
+        return False
+
+    async def send_attributes(self, attributes):
+        if not self._session:
+            LOGGER.error("Session not started")
+            return False
+
+        LOGGER.info(f"Sending attributes: {attributes}")
+
+        uri = "https://api.whrcloud.eu/api/v1/appliance/command"
+        cmd_data = {
+            "body": attributes,
+            "header": {"said": self._said, "command": "setAttributes"},
+        }
+        for n in range(3):
+            with async_timeout.timeout(30):
+                async with self._session.post(uri, json=cmd_data) as r:
+                    LOGGER.debug(f"Reply: {await r.text()}")
+                    if r.status == 200:
+                        return True
+                    elif r.status == 401:
+                        await self._auth.do_auth()
+                        await self.start_http_session()
+                        continue
+                    LOGGER.error(f"Sending attributes failed ({r.status})")
+        return False
+
+    def get_attribute(self, attribute):
+        return self._data_dict["attributes"][attribute]["value"]
+
+    def has_attribute(self, attribute):
+        return attribute in self._data_dict["attributes"]
+
+    async def connect(self):
+        await self.start_http_session()
+        await self.start_event_listener()
+
+    async def disconnect(self):
+        await self.stop_http_session()
+        await self.stop_event_listener()
+
+    async def start_http_session(self):
+        await self.stop_http_session()
+        self._session = aiohttp.ClientSession(headers=self._create_headers())
+
+    async def stop_http_session(self):
+        if not self._session:
+            return
+        await self._session.close()
+        self._session = None
+
+    async def start_event_listener(self):
+        await self.fetch_data()
+        self._event_socked.start()
+
+    async def stop_event_listener(self):
+        await self._event_socked.stop()
+
+    async def fetch_name(self):
+        account_id = None
+        async with self._session.get(
+            "https://api.whrcloud.eu/api/v1/getUserDetails"
+        ) as r:
+            if r.status != 200:
+                return None
+            account_id = json.loads(await r.text())["accountId"]
+
+        async with self._session.get(
+            "https://api.whrcloud.eu/api/v1/appliancebyaccount/{0}".format(account_id)
+        ) as r:
+            if r.status != 200:
+                return None
+            account_appliances = json.loads(await r.text())[str(account_id)]
+
+            for appliances in account_appliances.values():
+                for appliance in appliances:
+                    if appliance["SAID"] == self.said:
+                        return appliance["APPLIANCE_NAME"]
+        return None
---
+++ packages/whirlpool-sixth-sense/whirlpool_sixth_sense-0.14.3.tar.gz/whirlpool_sixth_sense-0.14.3/whirlpool/auth.py
@@ -0,0 +1,141 @@
+import aiohttp
+import asyncio
+import async_timeout
+import logging
+import json
+from datetime import datetime, timedelta, timedelta
+
+LOGGER = logging.getLogger(__name__)
+
+AUTH_JSON_FILE = ".whirlpool_auth.json"
+AUTO_REFRESH_DELTA = timedelta(minutes=15)
+
+
+class Auth:
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+        self._auth_dict = {}
+
+        self._renew_time: datetime = None
+        self._auto_renewal_task: asyncio.Task = None
+
+    async def _do_auto_renewal(self):
+        if self._renew_time > datetime.now():
+            time_to = (self._renew_time - datetime.now()).seconds
+            LOGGER.info("Renewing in %ds", time_to)
+            await asyncio.sleep(time_to)
+        await self.do_auth()
+
+    def _schedule_auto_renewal(self):
+        return  # disable for now and rely on on-demand renewal
+
+        if not self.is_access_token_valid():
+            LOGGER.warn("Access token is not valid, renewing now")
+            self._renew_time = datetime.now()
+        else:
+            expire_date = datetime.fromtimestamp(self._auth_dict.get("expire_date", 0))
+            self._renew_time = expire_date - AUTO_REFRESH_DELTA
+            LOGGER.info(
+                "Expire date is %s, renewing at %s", expire_date, self._renew_time
+            )
+
+        self.cancel_auto_renewal()
+        self._auto_renewal_task = asyncio.create_task(self._do_auto_renewal())
+
+    def _save_auth_data(self):
+        with open(AUTH_JSON_FILE, "w") as f:
+            json.dump(self._auth_dict, f)
+
+    async def _do_auth(self, refresh_token):
+        auth_url = "https://api.whrcloud.eu/oauth/token"
+        auth_header = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Brand": "Whirlpool",
+            "WP-CLIENT-REGION": "EMEA",
+            "WP-CLIENT-BRAND": "WHIRLPOOL",
+            "WP-CLIENT-COUNTRY": "EN",
+        }
+
+        auth_data = {
+            "client_id": "whirlpool_android",
+            "client_secret": "i-eQ8MD4jK4-9DUCbktfg-t_7gvU-SrRstPRGAYnfBPSrHHt5Mc0MFmYymU2E2qzif5cMaBYwFyFgSU6NTWjZg",
+        }
+
+        if refresh_token:
+            LOGGER.info("Fetching auth with refresh token")
+            auth_data.update(
+                {
+                    "grant_type": "refresh_token",
+                    "refresh_token": refresh_token,
+                }
+            )
+        else:
+            LOGGER.info("Fetching auth with user/pass")
+            auth_data.update(
+                {
+                    "grant_type": "password",
+                    "username": self._username,
+                    "password": self._password,
+                }
+            )
+
+        session = aiohttp.ClientSession()
+
+        try:
+            with async_timeout.timeout(30):
+                async with session.post(
+                    auth_url, data=auth_data, headers=auth_header
+                ) as r:
+                    LOGGER.debug("Auth status: " + str(r.status))
+                    if r.status == 200:
+                        return json.loads(await r.text())
+                    elif refresh_token:
+                        return await self._do_auth(refresh_token=None)
+        finally:
+            await session.close()
+
+    async def do_auth(self):
+        fetched_auth_data = await self._do_auth(
+            self._auth_dict.get("refresh_token", None)
+        )
+        curr_timestamp = datetime.now().timestamp()
+        self._auth_dict = {
+            "access_token": fetched_auth_data.get("access_token", ""),
+            "refresh_token": fetched_auth_data.get("refresh_token", ""),
+            "expire_date": curr_timestamp + fetched_auth_data.get("expires_in", ""),
+            "accountId": fetched_auth_data.get("accountId", ""),
+            "SAID": fetched_auth_data.get("SAID", ""),
+        }
+        self._save_auth_data()
+        self._schedule_auto_renewal()
+
+    async def load_auth_file(self):
+        try:
+            with open(AUTH_JSON_FILE, "r") as f:
+                LOGGER.info("Loading auth from file")
+                self._auth_dict = json.load(f)
+        except FileNotFoundError:
+            pass
+
+        if self.is_access_token_valid():
+            self._schedule_auto_renewal()
+        else:
+            LOGGER.info("Access token expired. Renewing.")
+            await self.do_auth()
+
+    def is_access_token_valid(self):
+        return (
+            "access_token" in self._auth_dict
+            and self._auth_dict.get("expire_date", 0) > datetime.now().timestamp()
+        )
+
+    def get_access_token(self):
+        return self._auth_dict.get("access_token", None)
+
+    def get_said_list(self):
+        return self._auth_dict.get("SAID", None)
+
+    def cancel_auto_renewal(self):
+        if self._auto_renewal_task:
+            self._auto_renewal_task.cancel()
