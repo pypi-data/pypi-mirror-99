60da696b243c6bdc042014bfc32eba99543dbad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:32 +0000

    Add package buildbot-gitea/1.5.0/buildbot-gitea-1.5.0.tar.gz

---
+++ packages/buildbot-gitea/buildbot-gitea-1.5.0.tar.gz/buildbot-gitea-1.5.0/buildbot_gitea/reporter.py
@@ -0,0 +1,220 @@
+# Based on the gitlab reporter from buildbot
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+from twisted.internet import defer
+from twisted.python import log
+
+from buildbot.process.properties import Interpolate
+from buildbot.process.properties import Properties
+from buildbot.process.results import CANCELLED
+from buildbot.process.results import EXCEPTION
+from buildbot.process.results import FAILURE
+from buildbot.process.results import RETRY
+from buildbot.process.results import SKIPPED
+from buildbot.process.results import SUCCESS
+from buildbot.process.results import WARNINGS
+from buildbot.reporters import http
+from buildbot.util import httpclientservice
+from buildbot.warnings import warn_deprecated
+from buildbot.reporters.generators.build import BuildStartEndStatusGenerator
+from buildbot.reporters.message import MessageFormatterRenderable
+
+import re
+
+
+class GiteaStatusPush(http.ReporterBase):
+    name = "GiteaStatusPush"
+    ssh_url_match = re.compile(r"(ssh://)?[\w+\-\_]+@[\w\.\-\_]+:?(\d*/)?(?P<owner>[\w_\-\.]+)/(?P<repo_name>[\w_\-\.]+?)(\.git)?$")
+
+    def checkConfig(self, token, context=None, baseURL=None, verbose=False,
+                    debug=None, verify=None, generators=None,
+                    **kwargs):
+
+        if generators is None:
+            generators = self._create_default_generators()
+
+        super().checkConfig(generators=generators, **kwargs)
+        httpclientservice.HTTPClientService.checkAvailable(
+            self.__class__.__name__)
+
+    @defer.inlineCallbacks
+    def reconfigService(self, baseURL, token,
+                        context=None, context_pr=None, verbose=False,
+                        debug=None, verify=None,
+                        generators=None,
+                        warningAsSuccess=False, **kwargs):
+
+        token = yield self.renderSecrets(token)
+        self.debug = debug
+        self.verify = verify
+        self.verbose = verbose
+        if generators is None:
+            generators = self._create_default_generators()
+
+        yield super().reconfigService(generators=generators, **kwargs)
+
+        self.context = context or Interpolate('buildbot/%(prop:buildername)s')
+        self.context_pr = context_pr or \
+            Interpolate('buildbot/pull_request/%(prop:buildername)s')
+        if baseURL.endswith('/'):
+            baseURL = baseURL[:-1]
+        self.baseURL = baseURL
+        self._http = yield httpclientservice.HTTPClientService.getService(
+            self.master, baseURL,
+            headers={'Authorization': 'token {}'.format(token)},
+            debug=self.debug, verify=self.verify)
+        self.verbose = verbose
+        self.project_ids = {}
+        self.warningAsSuccess = warningAsSuccess
+
+    def _create_default_generators(self):
+        start_formatter = MessageFormatterRenderable('Build started.')
+        end_formatter = MessageFormatterRenderable('Build done.')
+
+        return [
+            BuildStartEndStatusGenerator(start_formatter=start_formatter,
+                                         end_formatter=end_formatter)
+        ]
+
+    def createStatus(self,
+                     project_owner, repo_name, sha, state, target_url=None,
+                     description=None, context=None):
+        """
+        :param project_owner: username of the owning user or organization
+        :param repo_name: name of the repository
+        :param sha: Full sha to create the status for.
+        :param state: one of the following 'pending', 'success', 'failed'
+                      or 'cancelled'.
+        :param target_url: Target url to associate with this status.
+        :param description: Short description of the status.
+        :param context: Context of the result
+        :return: A deferred with the result from GitLab.
+
+        """
+        payload = {'state': state}
+
+        if description is not None:
+            payload['description'] = description
+
+        if target_url is not None:
+            payload['target_url'] = target_url
+
+        if context is not None:
+            payload['context'] = context
+
+        return self._http.post(
+            '/api/v1/repos/{owner}/{repository}/statuses/{sha}'.format(
+                owner=project_owner,
+                repository=repo_name,
+                sha=sha
+            ),
+            json=payload)
+
+    @defer.inlineCallbacks
+    def send(self, build):
+        # the only case when this function is called is when the user derives this class, overrides
+        # send() and calls super().send(build) from there.
+        yield self._send_impl(build)
+
+    @defer.inlineCallbacks
+    def sendMessage(self, reports):
+        build = reports[0]['builds'][0]
+        if self.send.__func__ is not GiteaStatusPush.send:
+            warn_deprecated('2.9.0', 'send() in reporters has been deprecated. Use sendMessage()')
+            yield self.send(build)
+        else:
+            yield self._send_impl(reports)
+
+    @defer.inlineCallbacks
+    def _send_impl(self, reports):
+        report = reports[0]
+        build = report['builds'][0]
+        props = Properties.fromDict(build['properties'])
+        props.master = self.master
+
+        description = report.get('body', None)
+
+        if build['complete']:
+            state = {
+                SUCCESS: 'success',
+                WARNINGS: 'success' if self.warningAsSuccess else 'warning',
+                FAILURE: 'failure',
+                SKIPPED: 'success',
+                EXCEPTION: 'error',
+                RETRY: 'pending',
+                CANCELLED: 'error'
+            }.get(build['results'], 'failure')
+        else:
+            state = 'pending'
+
+        if 'pr_id' in props:
+            context = yield props.render(self.context_pr)
+        else:
+            context = yield props.render(self.context)
+
+        sourcestamps = build['buildset']['sourcestamps']
+
+        for sourcestamp in sourcestamps:
+            sha = sourcestamp['revision']
+            if sha is None:
+                # No special revision for this, so ignore it
+                continue
+            if 'repository_name' in props:
+                repository_name = props['repository_name']
+            else:
+                match = re.match(self.ssh_url_match, sourcestamp['repository'])
+                if match is not None:
+                    repository_name = match.group("repo_name")
+                else:
+                    log.msg(
+                        "Could not send status, "
+                        "build has no repository_name property for Gitea.")
+                    continue
+            if 'owner' in props:
+                repository_owner = props['owner']
+            else:
+                match = re.match(self.ssh_url_match, sourcestamp['repository'])
+                if match is not None:
+                    repository_owner = match.group("owner")
+                else:
+                    log.msg(
+                        "Could not send status, "
+                        "build has no owner property for Gitea.")
+                    continue
+            try:
+                target_url = build['url']
+                res = yield self.createStatus(
+                    project_owner=repository_owner,
+                    repo_name=repository_name,
+                    sha=sha,
+                    state=state,
+                    target_url=target_url,
+                    context=context,
+                    description=description
+                )
+                if res.code not in (200, 201, 204):
+                    message = yield res.json()
+                    message = message.get('message', 'unspecified error')
+                    log.msg(
+                        'Could not send status "{state}" for '
+                        '{repo} at {sha}: {code} : {message}'.format(
+                            state=state,
+                            repo=sourcestamp['repository'], sha=sha,
+                            code=res.code,
+                            message=message))
+                elif self.verbose:
+                    log.msg(
+                        'Status "{state}" sent for '
+                        '{repo} at {sha}.'.format(
+                            state=state,
+                            repo=sourcestamp['repository'], sha=sha))
+            except Exception as e:
+                log.err(
+                    e,
+                    'Failed to send status "{state}" for '
+                    '{repo} at {sha}'.format(
+                        state=state,
+                        repo=sourcestamp['repository'], sha=sha
+                    ))
---
+++ packages/buildbot-gitea/buildbot-gitea-1.5.0.tar.gz/buildbot-gitea-1.5.0/buildbot_gitea/webhook.py
@@ -0,0 +1,174 @@
+import json
+import re
+import hmac
+import hashlib
+from buildbot.util import bytes2unicode
+from buildbot.www.hooks.base import BaseHookHandler
+
+from twisted.python import log
+from dateutil.parser import parse as dateparse
+
+_HEADER_EVENT_TYPE = 'X-Gitea-Event'
+_HEADER_SIGNATURE = 'X-Gitea-Signature'
+
+
+class GiteaHandler(BaseHookHandler):
+
+    def process_push(self, payload, event_type, codebase):
+        refname = payload["ref"]
+
+        changes = []
+
+        # We only care about regular heads or tags
+        match = re.match(r"^refs/(heads|tags)/(.+)$", refname)
+        if not match:
+            log.msg("Ignoring refname '{}': Not a branch or tag".format(refname))
+            return changes
+
+        branch = match.group(2)
+
+        repository = payload['repository']
+        repo_url = repository['ssh_url']
+        project = repository['full_name']
+
+        commits = payload['commits']
+        if isinstance(self.options, dict) and self.options.get('onlyIncludePushCommit', False):
+            commits = commits[:1]
+
+        for commit in commits:
+            files = []
+            for kind in ('added', 'modified', 'removed'):
+                files.extend(commit.get(kind, []))
+            timestamp = dateparse(commit['timestamp'])
+            change = {
+                'author': '{} <{}>'.format(commit['author']['name'],
+                                           commit['author']['email']),
+                'files': files,
+                'comments': commit['message'],
+                'revision': commit['id'],
+                'when_timestamp': timestamp,
+                'branch': branch,
+                'revlink': commit['url'],
+                'repository': repo_url,
+                'project': project,
+                'category': event_type,
+                'properties': {
+                    'event': event_type,
+                    'repository_name': repository['name'],
+                    'owner': repository["owner"]["username"]
+                },
+            }
+            if codebase is not None:
+                change['codebase'] = codebase
+            changes.insert(0, change)
+        return changes
+
+    def process_pull_request(self, payload, event_type, codebase):
+        action = payload['action']
+
+        # Only handle potential new stuff, ignore close/.
+        # Merge itself is handled by the regular branch push message
+        if action not in ['opened', 'synchronized', 'edited', 'reopened']:
+            log.msg("Gitea Pull Request event '{}' ignored".format(action))
+            return []
+        pull_request = payload['pull_request']
+        if not pull_request['mergeable']:
+            log.msg("Gitea Pull Request ignored because it is not mergeable.")
+            return []
+        if pull_request['merged']:
+            log.msg("Gitea Pull Request ignored because it is already merged.")
+            return []
+        timestamp = dateparse(pull_request['updated_at'])
+        base = pull_request['base']
+        head = pull_request['head']
+        repository = payload['repository']
+        change = {
+            'author': '{} <{}>'.format(pull_request['user']['full_name'],
+                                       pull_request['user']['email']),
+            'comments': 'PR#{}: {}\n\n{}'.format(
+                pull_request['number'],
+                pull_request['title'],
+                pull_request['body']),
+            'revision': head['sha'],
+            'when_timestamp': timestamp,
+            'branch': head['ref'],
+            'revlink': pull_request['html_url'],
+            'repository': repository['ssh_url'],
+            'project': repository['full_name'],
+            'category': event_type,
+            'properties': {
+                'event': event_type,
+                'base_branch': base['ref'],
+                'base_sha': base['sha'],
+                'base_repo_id': base['repo_id'],
+                'base_repository': base['repo']['clone_url'],
+                'base_git_ssh_url': base['repo']['ssh_url'],
+                'head_branch': head['ref'],
+                'head_sha': head['sha'],
+                'head_repo_id': head['repo_id'],
+                'head_repository': head['repo']['clone_url'],
+                'head_git_ssh_url': head['repo']['ssh_url'],
+                'pr_id': pull_request['id'],
+                'pr_number': pull_request['number'],
+                'repository_name': repository['name'],
+                'owner': repository["owner"]["username"],
+            },
+        }
+        if codebase is not None:
+            change['codebase'] = codebase
+        return [change]
+
+    def getChanges(self, request):
+        secret = None
+        if isinstance(self.options, dict):
+            secret = self.options.get('secret')
+        try:
+            content = request.content.read()
+            content_text = bytes2unicode(content)
+            payload = json.loads(content_text)
+        except Exception as exception:
+            raise ValueError('Error loading JSON: ' + str(exception))
+
+        if secret is not None:
+            signature = hmac.new(
+                secret.encode("UTF-8"),
+                content_text.strip().encode("UTF-8"),
+                digestmod=hashlib.sha256)
+            header_signature = bytes2unicode(
+                request.getHeader(_HEADER_SIGNATURE))
+            if signature.hexdigest() != header_signature:
+                raise ValueError('Invalid secret')
+
+        event_type = bytes2unicode(request.getHeader(_HEADER_EVENT_TYPE))
+        log.msg("Received event '{}' from gitea".format(event_type))
+
+        codebases = request.args.get('codebase', [None])
+        codebase = bytes2unicode(codebases[0])
+        changes = []
+
+        handler_function = getattr(self, 'process_{}'.format(event_type), None)
+        if not handler_function:
+            log.msg("Ignoring gitea event '{}'".format(event_type))
+        else:
+            changes = handler_function(payload, event_type, codebase)
+
+        return (changes, 'git')
+
+
+class GiteaHandlerPlugin(BaseHookHandler):
+    def __init__(self, master, options):
+        if not options:
+            options = {}
+        super().__init__(master, options)
+
+        handler_class = options.get('class', GiteaHandler)
+        if 'class' in options:
+            del options['class']
+
+        self.handler = handler_class(master, options)
+
+    def getChanges(self, request):
+        return self.handler.getChanges(request)
+
+# Plugin name
+gitea = GiteaHandlerPlugin
