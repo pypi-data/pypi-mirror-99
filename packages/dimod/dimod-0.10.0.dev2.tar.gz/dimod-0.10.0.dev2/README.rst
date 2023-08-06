9c5fba83063e9d58ee5438da3b4992afb70911d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:59 +0000

    Add package dimod/0.10.0.dev2/dimod-0.10.0.dev2.tar.gz

---
+++ packages/dimod/dimod-0.10.0.dev2.tar.gz/dimod-0.10.0.dev2/README.rst
@@ -0,0 +1,100 @@
+.. image:: https://img.shields.io/pypi/v/dimod.svg
+    :target: https://pypi.python.org/pypi/dimod
+
+.. image:: https://codecov.io/gh/dwavesystems/dimod/branch/main/graph/badge.svg
+    :target: https://codecov.io/gh/dwavesystems/dimod
+
+.. image:: https://readthedocs.com/projects/d-wave-systems-dimod/badge/?version=latest
+    :target: https://docs.ocean.dwavesys.com/projects/dimod/en/latest/?badge=latest
+
+.. image:: https://circleci.com/gh/dwavesystems/dimod.svg?style=svg
+    :target: https://circleci.com/gh/dwavesystems/dimod
+
+dimod
+=====
+
+.. index-start-marker1
+
+`dimod` is a shared API for samplers. It provides:
+
+* a binary quadratic model (BQM) class that contains Ising and quadratic
+  unconstrained binary optimization (QUBO) models used by samplers such as the
+  D-Wave system.
+* a discrete quadratic model (DQM) class and higher-order (non-quadratic) models.
+* reference examples of samplers and composed samplers.
+* `abstract base classes <https://docs.python.org/3/library/abc.html>`_ for
+  constructing new samplers and composed samplers.
+
+
+.. index-end-marker1
+
+(For explanations of the terminology, see the
+`Ocean glossary <https://docs.ocean.dwavesys.com/en/stable/concepts/index.html>`_.)
+
+Example Usage
+-------------
+
+.. index-start-marker2
+
+>>> import dimod
+...
+>>> # Construct a problem
+>>> bqm = dimod.BinaryQuadraticModel({0: -1, 1: 1}, {(0, 1): 2}, 0.0, dimod.BINARY)
+...
+>>> # Use dimod's brute force solver to solve the problem
+>>> sampleset = dimod.ExactSolver().sample(bqm)
+>>> print(sampleset)
+   0  1 energy num_oc.
+1  1  0   -1.0       1
+0  0  0    0.0       1
+3  0  1    1.0       1
+2  1  1    2.0       1
+['BINARY', 4 rows, 4 samples, 2 variables]
+
+.. index-end-marker2
+
+See the documentation for more examples.
+
+Installation
+------------
+
+.. installation-start-marker
+
+Compatible with Python 3.5+:
+
+.. code-block:: bash
+
+    pip install dimod
+
+To install with optional components:
+
+.. code-block:: bash
+
+    pip install dimod[all]
+
+To install from source:
+
+.. code-block:: bash
+
+    pip install -r requirements.txt
+    python setup.py install
+
+When developing on dimod, it is often convenient to build the extensions
+in place:
+
+.. code-block:: bash
+
+    pip install -r requirements.txt
+    python setup.py build_ext --inplace
+
+Note that installation from source requires that your system have the Boost_
+C++ libraries installed.
+
+.. _Boost: https://www.boost.org/
+
+.. installation-end-marker
+
+License
+-------
+
+Released under the Apache License 2.0. See LICENSE file.
---
+++ packages/dimod/dimod-0.10.0.dev2.tar.gz/dimod-0.10.0.dev2/setup.py
@@ -0,0 +1,154 @@
+# Copyright 2018 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# =============================================================================
+import os
+
+from setuptools import setup
+from distutils.extension import Extension
+from distutils.command.build_ext import build_ext as _build_ext
+
+# add __version__, __author__, __authoremail__, __description__ to this namespace
+exec(open(os.path.join(os.path.dirname(__file__), "dimod", "package_info.py")).read())
+
+# if the numpy ranges change here, don't forget to update the circle-ci job
+install_requires = ['numpy>=1.17.3,<2.0.0']
+
+setup_requires = ['numpy>=1.17.3,<2.0.0']
+
+extras_require = {'all': ['networkx>=2.0,<3.0',
+                          'pandas>=0.22.0,<0.23.0',
+                          'pymongo>=3.7.0,<3.8.0'],
+                  }
+
+packages = ['dimod',
+            'dimod.bqm',
+            'dimod.core',
+            'dimod.discrete',
+            'dimod.generators',
+            'dimod.higherorder',
+            'dimod.reference',
+            'dimod.reference.composites',
+            'dimod.reference.samplers',
+            'dimod.roof_duality',
+            'dimod.serialization',
+            'dimod.testing',
+            'dimod.views',
+            ]
+
+classifiers = [
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    ]
+
+python_requires = '>=3.5'
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    USE_CYTHON = False
+else:
+    USE_CYTHON = True
+
+ext = '.pyx' if USE_CYTHON else '.cpp'
+
+extra_compile_args = {
+    'msvc': ['/EHsc'],
+    'unix': ['-std=c++11'],
+}
+
+extra_link_args = {
+    'msvc': [],
+    'unix': ['-std=c++11'],
+}
+
+
+class build_ext(_build_ext):
+    def run(self):
+        # add numpy headers
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+        # add dimod headers
+        include = os.path.join(os.path.dirname(__file__), 'dimod', 'include')
+        self.include_dirs.append(include)
+
+        super().run()
+
+    def build_extensions(self):
+        compiler = self.compiler.compiler_type
+
+        compile_args = extra_compile_args[compiler]
+        for ext in self.extensions:
+            ext.extra_compile_args.extend(compile_args)
+
+        link_args = extra_link_args[compiler]
+        for ext in self.extensions:
+            ext.extra_compile_args.extend(link_args)
+
+        super().build_extensions()
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.build_tests = None
+
+
+extensions = [Extension("dimod.roof_duality._fix_variables",
+                        ['dimod/roof_duality/_fix_variables'+ext,
+                         'dimod/roof_duality/src/fix_variables.cpp'],
+                        include_dirs=['dimod/roof_duality/src/']),
+              Extension("dimod.bqm.adjvectorbqm",
+                        ['dimod/bqm/adjvectorbqm'+ext]),
+              Extension("dimod.bqm.utils",
+                        ['dimod/bqm/utils'+ext]),
+              Extension("dimod.bqm.common",
+                        ['dimod/bqm/common'+ext]),
+              Extension("dimod.discrete.cydiscrete_quadratic_model",
+                        ["dimod/discrete/cydiscrete_quadratic_model"+ext])
+              ]
+
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions,
+                           # annotate=True,
+                           )
+
+setup(
+    name='dimod',
+    version=__version__,
+    author=__author__,
+    author_email=__authoremail__,
+    description=__description__,
+    long_description=open('README.rst').read(),
+    url='https://github.com/dwavesystems/dimod',
+    download_url='https://github.com/dwavesystems/dimod/releases',
+    license='Apache 2.0',
+    packages=packages,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    setup_requires=setup_requires,
+    include_package_data=True,
+    classifiers=classifiers,
+    zip_safe=False,
+    python_requires=python_requires,
+    cmdclass=dict(build_ext=build_ext),
+    ext_modules=extensions,
+)
