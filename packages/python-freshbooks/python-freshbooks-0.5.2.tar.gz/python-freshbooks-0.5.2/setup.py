d80806b97f1e8a6aa385b760bce6292bcf46b06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:12 +0000

    Add package python-freshbooks/0.5.2/python-freshbooks-0.5.2.tar.gz

---
+++ packages/python-freshbooks/python-freshbooks-0.5.2.tar.gz/python-freshbooks-0.5.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-freshbooks",
+    author_email="alexandre.baizeau@gmail.com",
+    license="MIT License",
+    version="0.5.2",
+    packages=find_packages(),
+    description='Python wrapper for the FreshBooks API',
+    url='https://github.com/alexbaizeau/python-freshbooks',
+)
