29dc30edca488e2f3041cfa77f2de73f136ecd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:28 +0000

    Add package MangaScraper/0.0.2/MangaScraper-0.0.2.tar.gz

---
+++ packages/MangaScraper/MangaScraper-0.0.2.tar.gz/MangaScraper-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+import pathlib
+VERSION = '0.0.2'
+DESCRIPTION = 'Web Scraper to find and read your favourite Mangas'
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+# Setting up
+setup(
+
+    name="MangaScraper",
+    version=VERSION,
+    author="Tanmya Vishvakarma)",
+    author_email="tanmya2000@gmail.com",
+    license="MIT",
+    description=DESCRIPTION,
+     long_description=README,
+    packages=find_packages(),
+    install_requires=['bs4', 'requests'],
+    keywords=['python', 'manga', 'scraper', 'mangascraper', 'anime', 'manga scraper'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ],
+        entry_points={
+        "console_scripts": [
+            "mangascraper=mangascraper.__main__:main",
+        ]
+    }
+)
