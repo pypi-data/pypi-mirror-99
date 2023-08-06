5a23f78159fb486f8247b2979d5f53fc337da3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:00 +0000

    Add package robotframework-debugger/0.2.0/robotframework-debugger-0.2.0.tar.gz

---
+++ packages/robotframework-debugger/robotframework-debugger-0.2.0.tar.gz/robotframework-debugger-0.2.0/setup.py
@@ -0,0 +1,36 @@
+import re
+
+from setuptools import setup
+from setuptools import find_packages
+from os.path import abspath, dirname, join
+
+CURDIR = dirname(abspath(__file__))
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+with open(join(CURDIR, 'src', 'Debugger', '__init__.py'), encoding='utf-8') as f:
+    VERSION = re.search("\n__version__ = '(.*)'", f.read()).group(1)
+
+setup(
+    name="robotframework-debugger",
+    version=VERSION,
+    author="René Rohner(Snooz82)",
+    author_email="snooz@posteo.de",
+    description="A Robot Framework Listener to try keywords and pause execution on failure",
+    long_description=long_description,
+    url="https://github.com/Snooz82/robotframework-debugger",
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Testing :: Acceptance",
+        "Framework :: Robot Framework",
+    ],
+    install_requires=['robotframework >= 3.2.1'],
+    python_requires='>=3.6'
+)
