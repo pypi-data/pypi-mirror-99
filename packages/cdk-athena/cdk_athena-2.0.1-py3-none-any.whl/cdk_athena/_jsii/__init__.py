bacf0ff7e8fabe98c6f171586e304bae26f6becc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:44 +0000

    Add package cdk-athena/2.0.1/cdk_athena-2.0.1-py3-none-any.whl

---
+++ packages/cdk-athena/cdk_athena-2.0.1-py3-none-any.whl/cdk_athena/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_cloudformation._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import cdk_iam_floyd._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-athena", "2.0.1", __name__[0:-6], "cdk-athena@2.0.1.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
