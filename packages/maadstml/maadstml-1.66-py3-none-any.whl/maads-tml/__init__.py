047b8c6da733c0e53d6c9357aebde33e85450a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:08 +0000

    Add package maadstml/1.66/maadstml-1.66-py3-none-any.whl

---
+++ packages/maadstml/maadstml-1.66-py3-none-any.whl/maads-tml/__init__.py
@@ -0,0 +1,46 @@
+
+from .sendfiles import viperstats
+
+from .sendfiles import viperlisttopics
+
+from .sendfiles import vipersubscribeconsumer
+
+from .sendfiles import viperunsubscribeconsumer
+
+from .sendfiles import viperproducetotopic
+from .sendfiles import viperconsumefromtopic
+from .sendfiles import viperhpdepredict
+
+from .sendfiles import viperhpdeoptimize
+
+from .sendfiles import viperhpdetraining
+
+from .sendfiles import viperproducetotopicstream
+
+from .sendfiles import vipercreatetrainingdata
+    
+
+from .sendfiles import vipercreatetopic
+from .sendfiles import viperconsumefromstreamtopic
+from .sendfiles import vipercreatejointopicstreams
+    
+from .sendfiles import vipercreateconsumergroup
+from .sendfiles import viperconsumergroupconsumefromtopic
+    
+from .sendfiles import vipermodifyconsumerdetails
+    
+from .sendfiles import vipermodifytopicdetails
+    
+from .sendfiles import viperactivatetopic
+    
+from .sendfiles import viperdeactivatetopic
+    
+from .sendfiles import vipergroupactivate
+    
+from .sendfiles import vipergroupdeactivate
+
+from .sendfiles import viperanomalytrain
+from .sendfiles import viperanomalypredict
+
+
+name = "maads-tml"
