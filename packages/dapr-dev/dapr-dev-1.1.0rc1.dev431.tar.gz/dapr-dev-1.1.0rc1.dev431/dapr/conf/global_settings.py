3159cd8d35f982d7b4d79b59bb855c8e9ec84081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:01 +0000

    Add package dapr-dev/1.1.0rc1.dev431/dapr-dev-1.1.0rc1.dev431.tar.gz

---
+++ packages/dapr-dev/dapr-dev-1.1.0rc1.dev431.tar.gz/dapr-dev-1.1.0rc1.dev431/dapr/conf/global_settings.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) Microsoft Corporation and Dapr Contributors.
+Licensed under the MIT License.
+"""
+
+# Default environment settings that environment variables overrides
+
+HTTP_APP_PORT = 3000
+GRPC_APP_PORT = 3010
+
+DAPR_API_TOKEN = None
+DAPR_RUNTIME_HOST = '127.0.0.1'
+DAPR_HTTP_PORT = 3500
+DAPR_GRPC_PORT = 50001
+DAPR_API_VERSION = 'v1.0'
+
+DAPR_API_METHOD_INVOCATION_PROTOCOL = 'http'
+
+DAPR_HTTP_TIMEOUT_SECONDS = 60
