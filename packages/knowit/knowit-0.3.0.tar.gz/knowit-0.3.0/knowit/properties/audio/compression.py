fb60aa58ed09cd44e2832b62854e5716a2c74774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:37 +0000

    Add package knowit/0.3.0/knowit-0.3.0.tar.gz

---
+++ packages/knowit/knowit-0.3.0.tar.gz/knowit-0.3.0/knowit/properties/audio/compression.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from ...property import Configurable
+
+
+class AudioCompression(Configurable):
+    """Audio Compression property."""
---
+++ packages/knowit/knowit-0.3.0.tar.gz/knowit-0.3.0/knowit/properties/video/encoder.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from ...property import Configurable
+
+
+class VideoEncoder(Configurable):
+    """Video Encoder property."""
