409ded00441caa87a057b679af778bc81b1880d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:43 +0000

    Add package testInstall/1.1.0/testInstall-1.1.0.tar.gz

---
+++ packages/testInstall/testInstall-1.1.0.tar.gz/testInstall-1.1.0/testInstall.py
@@ -0,0 +1,8 @@
+def printList (the_list, level = 0):
+    for each_list in the_list:
+        if isinstance(each_list, list):#默认递归不能超过100
+            printList(each_list, level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_list)
