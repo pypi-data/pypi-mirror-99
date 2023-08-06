dd5113a197adacb95cde7e21e364716a461c3c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:18 +0000

    Add package jithub/0.1.0/jithub-0.1.0-py3.7.egg

---
+++ packages/jithub/jithub-0.1.0-py3.7.egg/jithub/__init__.py
@@ -0,0 +1,10 @@
+"""
+JITHUB models
+"""
+
+from .models.model_classes import IzhiModel, MATModel
+from .backends import izhikevich
+from .backends import mat_nu
+from .backends import adexp
+
+#from .backends import izhikevich
---
+++ packages/jithub/jithub-0.1.0-py3.7.egg/jithub/backends/__init__.py
@@ -0,0 +1,3 @@
+from . import izhikevich
+from . import mat_nu
+from . import numba_adexp
