209dc12999c4010692f71a5d56639c783cd4b213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:49 +0000

    Add package featurewiz/0.0.29/featurewiz-0.0.29.tar.gz

---
+++ packages/featurewiz/featurewiz-0.0.29.tar.gz/featurewiz-0.0.29/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="featurewiz",
+    version="0.0.29",
+    author="Ram Seshadri",
+    author_email="rsesha2001@yahoo.com",
+    description="Select Best Features from your data set - any size - now with XGBoost!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    url="https://github.com/AutoViML/featurewiz",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    install_requires=[
+        "ipython",
+        "jupyter",
+        "xgboost>=1.1.1",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scikit-learn>=0.23.1",
+        "networkx",
+        "category_encoders",
+        "xlrd",
+        "imbalanced-learn>=0.7",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
