a9592135bb8add3bbef6f4c2f1bf5bc6991372b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:38 +0000

    Add package beet/0.17.1/beet-0.17.1-py3-none-any.whl

---
+++ packages/beet/beet-0.17.1-py3-none-any.whl/beet/contrib/function_header.py
@@ -0,0 +1,39 @@
+"""Plugin that adds a header to functions automatically."""
+
+
+__all__ = [
+    "function_header",
+]
+
+
+from typing import Iterable, Optional
+
+from beet import Context, Plugin
+
+
+def beet_default(ctx: Context):
+    config = ctx.meta.get("function_header", {})
+
+    match = config.get("match", [])
+    template = config.get("template", "function_header.mcfunction")
+
+    ctx.require(function_header(match, template))
+
+
+def function_header(
+    match: Iterable[str] = (),
+    template: Optional[str] = "function_header.mcfunction",
+) -> Plugin:
+    """Return a plugin that adds a header to functions automatically."""
+
+    def plugin(ctx: Context):
+        if not template:
+            return
+
+        for path in ctx.data.functions.match(*match):
+            with ctx.override(render_path=path, render_group="functions"):
+                header = ctx.template.render(template)
+            function = ctx.data.functions[path]
+            function.text = header + function.text
+
+    return plugin
