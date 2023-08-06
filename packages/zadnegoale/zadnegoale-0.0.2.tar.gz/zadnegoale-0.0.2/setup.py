79cf43d8b6095fcb7b55590968ea10cc34d5145b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:46 +0000

    Add package zadnegoale/0.0.2/zadnegoale-0.0.2.tar.gz

---
+++ packages/zadnegoale/zadnegoale-0.0.2.tar.gz/zadnegoale-0.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="zadnegoale",
+    version="0.0.2",
+    author="Maciej Bieniek",
+    author_email="maciej.bieniek@gmail.com",
+    description="Python wrapper for getting allergen concentration data from Żadnego Ale servers.",
+    include_package_data=True,
+    url="https://github.com/bieniu/zadnegoale",
+    license="Apache-2.0 License",
+    packages=["zadnegoale"],
+    python_requires=">=3.6",
+    install_requires=list(val.strip() for val in open("requirements.txt")),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+    setup_requires=("pytest-runner"),
+)
