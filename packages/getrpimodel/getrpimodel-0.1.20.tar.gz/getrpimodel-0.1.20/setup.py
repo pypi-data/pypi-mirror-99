50607a1a83fd624231f48fcf788f9ce306ba5f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:36 +0000

    Add package getrpimodel/0.1.20/getrpimodel-0.1.20.tar.gz

---
+++ packages/getrpimodel/getrpimodel-0.1.20.tar.gz/getrpimodel-0.1.20/setup.py
@@ -0,0 +1,23 @@
+#from distutils.core import setup
+from setuptools import setup
+
+with open("README.md") as f:
+  long_description = f.read()
+
+setup(
+  name = 'getrpimodel',
+  packages = ['getrpimodel'], # this must be the same as the name above
+  version = '0.1.20',
+  description = 'Get Raspberry Pi model Name(eg: A, B, B+...)',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Takeyuki UEDA',
+  author_email = 'gde00107@nifty.com',
+  license='MIT',
+  url = 'https://github.com/UedaTakeyuki/getrpimodel', # use the URL to the github repo
+  keywords = ['IoT', 'Raspberry Pi','model','revision'], # arbitrary keywords
+  classifiers = ['Development Status :: 4 - Beta',
+                 'Programming Language :: Python',
+                 'Topic :: Terminals'
+  ],
+)
