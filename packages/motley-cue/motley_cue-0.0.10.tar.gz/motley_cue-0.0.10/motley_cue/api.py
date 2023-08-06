f583c4ac2ce1235084872bc2d4d63d1567d4fad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:09 +0000

    Add package motley-cue/0.0.10/motley_cue-0.0.10.tar.gz

---
+++ packages/motley-cue/motley_cue-0.0.10.tar.gz/motley_cue-0.0.10/motley_cue/api.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Depends, Request
+from .mapper import mapper
+from .user import user
+from .admin import admin
+
+
+api = FastAPI()
+
+api.include_router(user.api,
+                   prefix="/user",
+                   tags=["user"],
+                   dependencies=[Depends(mapper.user_security)])
+api.include_router(admin.api,
+                   prefix="/admin",
+                   tags=["admin"],
+                   dependencies=[Depends(mapper.admin_security)])
+
+
+@api.get("/")
+async def read_root():
+    return {
+        "description": "This is the user API for mapping remote identities to local identities.",
+        "usage": "All endpoints are available via a bearer token.",
+        "endpoints": {
+            "/info": "Service-specific information.",
+            "/user": "User API; requires valid access token of an authorised user.",
+            "/admin": "Admin API; requires valid access token of an authorised user with admin role.",
+            "/verify_user": "Verifies if a given token belongs to a given local account via 'username'."
+        }
+    }
+
+
+@api.get('/info', dependencies=[Depends(mapper.user_security)])
+@mapper.login_required()
+async def info(request: Request):
+    return mapper.info()
+
+
+@api.get('/verify_user', dependencies=[Depends(mapper.user_security)])
+@mapper.login_required()
+async def verify_user(request: Request, username: str):
+    return mapper.verify_user(request, username)
---
+++ packages/motley-cue/motley_cue-0.0.10.tar.gz/motley_cue-0.0.10/motley_cue/mapper/config.py
@@ -0,0 +1,62 @@
+import os
+from configparser import ConfigParser
+from pathlib import Path
+import logging
+
+CONFIG = ConfigParser()
+
+def reload():
+    """Reload configuration from disk.
+
+    Config locations, by priority:
+    $MOTLEY_CUE_CONFIG
+    ./motley_cue.conf
+    ~/.config/motley_cue/motley_cue.conf
+    /etc/motley_cue/motley_cue.conf
+
+    processing is stopped, once a give file is found
+    """
+
+    logging.basicConfig(
+        level=os.environ.get("LOG", "INFO")
+    )
+    logger = logging.getLogger(__name__)
+    files = []
+
+    filename = os.environ.get("MOTLEY_CUE_CONFIG")
+    if filename:
+        files += [Path(filename)]
+
+    files += [
+        Path('motley_cue.conf'),
+        Path.home()/'.config'/'motley_cue'/'motley_cue.conf'
+    ]
+
+    files += [Path("/etc/motley_cue/motley_cue.conf")]
+
+    for f in files:
+        if f.exists():
+            files_read = CONFIG.read(f)
+            logger.debug(F"Read config from {files_read}")
+            break
+
+
+def to_list(list_str):
+    """Convert a string to a list.
+    """
+    return [
+        v.strip('"').strip("'") for v in list_str
+        .replace("\n", "").replace(" ", "").replace("\t", "")
+        .strip("]").strip("[")
+        .rstrip(",").split(",")
+    ]
+
+
+def to_bool(bool_str):
+    """Convert a string to bool.
+    """
+    return True if bool_str.lower() == 'true' else False
+
+
+# Load config on import
+reload()
