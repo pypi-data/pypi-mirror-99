fd14aadf971643c4f2a91e0869c0dd0f4fcae478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:23 +0000

    Add package raveberry/0.7.12/raveberry-0.7.12.tar.gz

---
+++ packages/raveberry/raveberry-0.7.12.tar.gz/raveberry-0.7.12/raveberry/setup/user_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+ADMIN_PASSWORD="${ADMIN_PASSWORD:-admin}"
+echo "Performing Migrations"
+DJANGO_MOCK=1 python3 manage.py migrate
+echo "Creating Users"
+DJANGO_MOCK=1 python3 manage.py shell <<-EOF
+	from django.contrib.auth.models import User
+	User.objects.create_superuser('admin', email='', password='$ADMIN_PASSWORD')
+	User.objects.create_user('mod', password='mod')
+EOF
+if [[ ! -d static/bundle.js ]]; then
+	echo "building frontend"
+	cd frontend && yarn build && cd ..
+fi
+
+if [ "$ADMIN_PASSWORD" == "admin" ]; then
+  echo 1>&2
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+  echo "! Warning! Default admin password used, please consider changing it! !" 1>&2
+  echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" 1>&2
+  echo 1>&2
+fi
