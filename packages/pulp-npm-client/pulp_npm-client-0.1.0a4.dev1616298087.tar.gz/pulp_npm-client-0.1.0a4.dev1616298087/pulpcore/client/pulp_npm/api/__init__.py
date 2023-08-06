a677fb89a0e5930a297e88993d995a4e6977d4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:48 +0000

    Add package pulp-npm-client/0.1.0a4.dev1616298087/pulp_npm-client-0.1.0a4.dev1616298087.tar.gz

---
+++ packages/pulp-npm-client/pulp_npm-client-0.1.0a4.dev1616298087.tar.gz/pulp_npm-client-0.1.0a4.dev1616298087/pulpcore/client/pulp_npm/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_npm.api.content_packages_api import ContentPackagesApi
+from pulpcore.client.pulp_npm.api.distributions_npm_api import DistributionsNpmApi
+from pulpcore.client.pulp_npm.api.pulp_npm_packages_api import PulpNpmPackagesApi
+from pulpcore.client.pulp_npm.api.remotes_npm_api import RemotesNpmApi
+from pulpcore.client.pulp_npm.api.repositories_npm_api import RepositoriesNpmApi
+from pulpcore.client.pulp_npm.api.repositories_npm_versions_api import RepositoriesNpmVersionsApi
