839967ef18d73f18754e19b7b9d93cb77c0d801f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:59 +0000

    Add package athenautility/1.0.5/athenautility-1.0.5.tar.gz

---
+++ packages/athenautility/athenautility-1.0.5.tar.gz/athenautility-1.0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.5'
+DESCRIPTION = 'Athena Utility'
+LONG_DESCRIPTION = 'AWS Athena client based on PyAthena'
+
+install_requirements = [
+    'pandas',
+    'requests',
+]
+
+# Setting up
+setup(
+    name="athenautility",
+    version=VERSION,
+    author="KnowledgeLens",
+    author_email="karthik.madhamanchi@knowledgelens.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=install_requirements,
+    python_requires=">=3.6",classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: SQL',
+        'Topic :: Database',        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
