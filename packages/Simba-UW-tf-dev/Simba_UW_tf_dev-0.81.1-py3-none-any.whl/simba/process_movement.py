26061c4d90d881fa16fe66eacbac82cce7b7ec25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:35 +0000

    Add package Simba-UW-tf-dev/0.81.1/Simba_UW_tf_dev-0.81.1-py3-none-any.whl

---
+++ packages/Simba-UW-tf-dev/Simba_UW_tf_dev-0.81.1-py3-none-any.whl/simba/process_movement.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import os
+from configparser import ConfigParser, NoSectionError, NoOptionError
+from datetime import datetime
+import statistics
+import numpy as np
+import glob
+from drop_bp_cords import define_movement_cols
+from simba.rw_dfs import *
+
+
+def analyze_process_movement(configini):
+    dateTime = datetime.now().strftime('%Y%m%d%H%M%S')
+    config = ConfigParser()
+    configFile = str(configini)
+    config.read(configFile)
+    projectPath = config.get('General settings', 'project_path')
+    csv_dir_in = os.path.join(projectPath, 'csv', 'outlier_corrected_movement_location')
+    vidLogFilePath = os.path.join(projectPath, 'logs', 'video_info.csv')
+    vidinfDf = pd.read_csv(vidLogFilePath)
+    try:
+        wfileType = config.get('General settings', 'workflow_file_type')
+    except NoOptionError:
+        wfileType = 'csv'
+    noAnimals = config.getint('process movements', 'no_of_animals')
+    Animal_1_Bp = config.get('process movements', 'animal_1_bp')
+    Animal_2_Bp = config.get('process movements', 'animal_2_bp')
+    VideoNo_list, columnNames1, fileCounter = [], [], 0
+
+    ########### logfile path ###########
+    log_fn = os.path.join(projectPath, 'logs', 'Movement_log_' + dateTime + '.csv')
+    columnNames = define_movement_cols(noAnimals)
+    log_df = pd.DataFrame(columns=columnNames)
+
+    ########### FIND CSV FILES ###########
+    filesFound = glob.glob(csv_dir_in + '/*.' + wfileType)
+    print('Processing movement data for ' + str(len(filesFound)) + ' files...')
+    columnHeaders = [Animal_1_Bp + '_x', Animal_1_Bp + '_y']
+    shifted_columnHeaders = ['shifted_' + Animal_1_Bp + '_x', 'shifted_' + Animal_1_Bp + '_y']
+    if noAnimals == 2:
+        columnHeaders.extend([Animal_2_Bp + '_x', Animal_2_Bp + '_y'])
+        shifted_columnHeaders.extend(['shifted_' + Animal_2_Bp + '_x', 'shifted_' + Animal_2_Bp + '_y'])
+
+    for currentFile in filesFound:
+        frameCounter = 0
+        currVideoName = os.path.basename(currentFile).replace('.' + wfileType, '')
+        videoSettings = vidinfDf.loc[vidinfDf['Video'] == currVideoName]
+        try:
+            fps = int(videoSettings['fps'])
+            currPixPerMM = float(videoSettings['pixels/mm'])
+        except TypeError:
+            print('Error: make sure all the videos that are going to be analyzed are represented in the project_folder/logs/video_info.csv file')
+        csv_df = read_df(currentFile, wfileType)
+        csv_df = csv_df[columnHeaders]
+        csv_df_shifted = csv_df.shift(-1, axis=0)
+        csv_df_shifted.columns = shifted_columnHeaders
+        csv_df = pd.concat([csv_df, csv_df_shifted], axis=1)
+        csv_df['Movement_animal_1'] = (np.sqrt((csv_df[columnHeaders[0]] - csv_df[shifted_columnHeaders[0]]) ** 2 + (csv_df[columnHeaders[1]] - csv_df[shifted_columnHeaders[1]]) ** 2)) / currPixPerMM
+        if noAnimals == 2:
+            csv_df['Movement_animal_2'] = (np.sqrt((csv_df[columnHeaders[2]] - csv_df[shifted_columnHeaders[2]]) ** 2 + (csv_df[columnHeaders[3]] - csv_df[shifted_columnHeaders[3]]) ** 2)) / currPixPerMM
+            csv_df['Animal_distance'] = (np.sqrt((csv_df[columnHeaders[0]] - csv_df[columnHeaders[2]]) ** 2 + (csv_df[columnHeaders[1]] - csv_df[columnHeaders[3]]) ** 2)) / currPixPerMM
+
+        df_lists = [csv_df[i:i + fps] for i in range(0, csv_df.shape[0], fps)]
+        movementListAnimal1, movementListAnimal2, distanceList, velocityAnimal1List, velocityAnimal2List, currentVidList = [], [], [], [], [], []
+        for currentDf in df_lists:
+            if noAnimals == 1:
+                movementListAnimal1.append(currentDf['Movement_animal_1'].mean())
+                velocityAnimal1List.append(currentDf['Movement_animal_1'].mean() / 1)
+            if noAnimals == 2:
+                movementListAnimal1.append(currentDf['Movement_animal_1'].mean())
+                movementListAnimal2.append(currentDf['Movement_animal_2'].mean())
+                velocityAnimal1List.append(currentDf['Movement_animal_1'].mean() / 1)
+                velocityAnimal2List.append(currentDf['Movement_animal_2'].mean() / 1)
+                distanceList.append(currentDf['Animal_distance'].mean())
+                print(currentDf['Animal_distance'].mean())
+            frameCounter += fps
+
+        totalMovement_animal1 = sum(movementListAnimal1)
+        meanVelocity_animal_1 = statistics.mean(velocityAnimal1List)
+        medianVelocity_animal_1 = statistics.median(velocityAnimal1List)
+        currentVidList = [currVideoName, frameCounter, totalMovement_animal1, meanVelocity_animal_1, medianVelocity_animal_1]
+        if noAnimals == 2:
+            totalMovement_animal2 = sum(movementListAnimal2)
+            meanVelocity_animal_2 = statistics.mean(velocityAnimal2List)
+            medianVelocity_animal_2 = statistics.median(velocityAnimal2List)
+            meanDistance = statistics.mean(distanceList) / 10
+            medianDistance = statistics.median(distanceList) / 10
+            currentVidList = [currVideoName, frameCounter, totalMovement_animal1, meanVelocity_animal_1, medianVelocity_animal_1, totalMovement_animal2, meanVelocity_animal_2, medianVelocity_animal_2, meanDistance, medianDistance]
+        log_df.loc[fileCounter] = currentVidList
+        fileCounter += 1
+        print('Files # processed for movement data: ' + str(fileCounter) + '/' + str(len(filesFound)) + '...')
+    log_df = np.round(log_df,decimals=4)
+    log_df.to_csv(log_fn, index=False)
+    print('All files processed for movement data. ' + 'Data saved @ project_folder\logs')
---
+++ packages/Simba-UW-tf-dev/Simba_UW_tf_dev-0.81.1-py3-none-any.whl/simba/read_DLCmulti_h5_function.py
@@ -0,0 +1,233 @@
+import os, glob
+import pandas as pd
+import cv2
+import numpy as np
+from configparser import ConfigParser, MissingSectionHeaderError, NoSectionError, NoOptionError
+from pylab import *
+from simba.rw_dfs import *
+from simba.drop_bp_cords import *
+import pyarrow.parquet as pq
+import pyarrow as pa
+
+def importMultiDLCpose(inifile, dataFolder, filetype, idlist):
+    global currIDcounter
+    def define_ID(event, x, y, flags, param):
+        global currIDcounter
+        if (event == cv2.EVENT_LBUTTONDBLCLK):
+            centerX, centerY, currID = (int(x), int(y), currIDList[currIDcounter])
+            ID_user_cords.append([centerX, centerY, currIDList[currIDcounter]])
+            cv2.putText(overlay, str(currID), (centerX, centerY), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 255, 0), 6)
+            currIDcounter += 1
+
+    configFile = str(inifile)
+    config = ConfigParser()
+    try:
+        config.read(configFile)
+    except MissingSectionHeaderError:
+        print('ERROR:  Not a valid project_config file. Please check the project_config.ini path.')
+    projectPath = config.get('General settings', 'project_path')
+    if filetype == 'skeleton':
+        filesFound = glob.glob(dataFolder + '/*sk.h5') + glob.glob(dataFolder + '/*sk_filtered.h5')
+    if filetype == 'box':
+        filesFound = glob.glob(dataFolder + '/*bx.h5') + glob.glob(dataFolder + '/*bx_filtered.h5')
+    videoFolder = os.path.join(projectPath, 'videos')
+    outputDfFolder = os.path.join(projectPath, 'csv', 'input_csv')
+    bpNamesCSVPath = os.path.join(projectPath, 'logs', 'measures', 'pose_configs', 'bp_names', 'project_bp_names.csv')
+    poseEstimationSetting = config.get('create ensemble settings', 'pose_estimation_body_parts')
+    noAnimals = config.getint('General settings', 'animal_no')
+
+    try:
+        wfileType = config.get('General settings', 'workflow_file_type')
+    except NoOptionError:
+        wfileType = 'csv'
+
+    try:
+        multiAnimalIDList = config.get('Multi animal IDs', 'id_list')
+        multiAnimalIDList = multiAnimalIDList.split(",")
+        if multiAnimalIDList[0] != '':
+            multiAnimalStatus = True
+            print('Applying settings for multi-animal tracking...')
+        else:
+            multiAnimalStatus = False
+            for animal in range(noAnimals):
+                multiAnimalIDList.append('Animal_' + str(animal + 1) + '_')
+            print('Applying settings for classical tracking...')
+    except NoSectionError:
+        multiAnimalIDList = []
+        for animal in range(noAnimals):
+            multiAnimalIDList.append('Animal_' + str(animal + 1) + '_')
+        multiAnimalStatus = False
+        print('Applying settings for classical tracking...')
+
+    print('Importing ' + str(len(filesFound)) + ' multi-animal DLC h5 files to the current project')
+
+    Xcols, Ycols, Pcols = getBpNames(inifile)
+    currIDList = idlist
+
+    for file in filesFound:
+        bpNameList, x_heads, y_heads, xy_heads, indBpCordList, EuclidDistanceList, colorList, bp_cord_names, changeList, projBpNameList = [], [], [], [], [], [], [], [], [], []
+        assigningIDs, completePromt, chooseFrame, assignBpCords = False, False, True, True
+        addSpacer, ID_user_cords, currIDcounter, frameNumber = 2, [], 0, 0
+        currVidName = os.path.basename(file)
+        if os.path.exists(os.path.join(videoFolder, os.path.basename(file).split('DLC_resnet50')[0] + '.mp4')):
+            vidFname = os.path.join(videoFolder, os.path.basename(file).split('DLC_resnet50')[0] + '.mp4')
+        elif os.path.exists(os.path.join(videoFolder, os.path.basename(file).split('DLC_resnet50')[0] + '.avi')):
+            vidFname = os.path.join(videoFolder, os.path.basename(file).split('DLC_resnet50')[0] + '.avi')
+        else:
+            print('Cannot locate video ' + str(currVidName.replace('.' + wfileType, '')) + ' in mp4 or avi format')
+            break
+        vidBasename, VideoExtension = os.path.basename(vidFname), os.path.splitext(vidFname)[1]
+        currDf = pd.read_hdf(file)
+        bpNames, idNames = [lis[2] for lis in list(currDf.columns)] , [lis[1] for lis in list(currDf.columns)]
+        uniqueIds = list(unique(idNames))
+
+        cmaps = ['spring', 'summer', 'autumn', 'cool', 'Wistia', 'Pastel1', 'Set1', 'winter']
+        cMapSize = int(len(Xcols) / noAnimals) + 1
+        colorListofList = createColorListofList(noAnimals, cMapSize)
+        animalBpDict = create_body_part_dictionary(multiAnimalStatus, multiAnimalIDList, noAnimals, Xcols, Ycols, Pcols, colorListofList)
+        for animal in animalBpDict.keys():
+            for currXcol, currYcol, currPcol in zip(animalBpDict[animal]['X_bps'], animalBpDict[animal]['Y_bps'], animalBpDict[animal]['P_bps']):
+                bpNameList.extend((animal + '_' + currXcol, animal + '_' + currYcol, animal + '_' + currPcol))
+        if poseEstimationSetting == 'user_defined':
+            config.set("General settings", "animal_no", str(len(uniqueIds)))
+            with open(inifile, "w+") as f:
+                config.write(f)
+            f.close
+        currDf.columns = bpNameList
+        currDf = currDf.fillna(0)
+        cap = cv2.VideoCapture(vidFname)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        mySpaceScale, myRadius, myResolution, myFontScale = 40, 10, 1500, 1.2
+        maxResDimension = max(width, height)
+        if maxResDimension == 0:
+            print('Make sure you have imported the correct video(s) into your SimBA project.')
+        circleScale = int(myRadius / (myResolution / maxResDimension))
+        fontScale = float(myFontScale / (myResolution / maxResDimension))
+        spacingScale = int(mySpaceScale / (myResolution / maxResDimension))
+        cv2.namedWindow('Define animal IDs', cv2.WINDOW_NORMAL)
+
+        while (1):
+            if (chooseFrame == True) and (assignBpCords == True):
+                cv2.namedWindow('Define animal IDs', cv2.WINDOW_NORMAL)
+                cap.set(1, frameNumber)
+                ret, frame = cap.read()
+                overlay = frame.copy()
+                cv2.namedWindow('Define animal IDs', cv2.WINDOW_NORMAL)
+                for animal in animalBpDict.keys():
+                    for currXcol, currYcol, currColor in zip(animalBpDict[animal]['X_bps'], animalBpDict[animal]['Y_bps'], animalBpDict[animal]['colors']):
+                        y_cord = currDf.loc[currDf.index[frameNumber], animal + '_' + currYcol]
+                        x_cord = currDf.loc[currDf.index[frameNumber], animal + '_' + currXcol]
+                        indBpCordList.append([x_cord, y_cord, animal])
+                        cv2.circle(overlay, (int(x_cord), int(y_cord)), circleScale, currColor, -1, lineType=cv2.LINE_AA)
+                    loop =0
+                    for name in indBpCordList:
+                        currstring = name[2]
+                        for substring in bp_cord_names:
+                            if substring in currstring:
+                                newstring = currstring.replace(substring, '')
+                                indBpCordList[loop][2] = newstring
+                        loop+=1
+                imWithCordsOnly = overlay.copy()
+                chooseFrame = False
+            if (chooseFrame == False) and (assignBpCords == True):
+                sideImage = np.ones((int(height / 2), width, 3))
+                cv2.putText(sideImage, 'Current video: ' + str(vidBasename), (10, int(spacingScale)),cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 255), 3)
+                cv2.putText(sideImage, 'Can you assign identities based on the displayed frame ?', (10, int(spacingScale * (addSpacer * 2))), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 255), 2)
+                cv2.putText(sideImage, 'Press "x" to display new, random, frame', (10, int(spacingScale * (addSpacer * 3))), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 0), 3)
+                cv2.putText(sideImage, 'Press "c" to continue to start assigning identities using this frame', (10, int(spacingScale * (addSpacer * 4))), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 255, 0), 2)
+                imageConcat = np.concatenate((overlay, sideImage), axis=0)
+                imageConcat = np.uint8(imageConcat)
+                cv2.imshow('Define animal IDs', imageConcat)
+                k = cv2.waitKey(10)
+                if k == ord('x'):
+                    cv2.destroyWindow('Define animal IDs')
+                    chooseFrame, assignBpCords = True, True
+                    frameNumber += 50
+                elif k == ord('c'):
+                    chooseFrame, assignBpCords = False, False
+                    assigningIDs, completePromt, assigningIDs = True, False, True
+
+            if assigningIDs == True:
+                sideImage = np.ones((int(height / 2), width, 3))
+                cv2.putText(sideImage, 'Double left mouse click on:', (10,  int(spacingScale)), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 255), 2)
+                cv2.putText(sideImage, str(currIDList[currIDcounter]), (10, int(spacingScale * (addSpacer*2))), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 0), 2)
+                imageConcat = np.concatenate((overlay, sideImage), axis=0)
+                imageConcat = np.uint8(imageConcat)
+                cv2.setMouseCallback('Define animal IDs', define_ID)
+                cv2.imshow('Define animal IDs', imageConcat)
+                cv2.waitKey(10)
+                if currIDcounter >= len(currIDList):
+                    cv2.destroyWindow('Define animal IDs')
+                    assigningIDs, completePromt = False, True
+
+            if completePromt == True:
+                cv2.namedWindow('Define animal IDs', cv2.WINDOW_NORMAL)
+                sideImage = np.ones((int(height/2), width, 3))
+                cv2.putText(sideImage, 'Current video: ' + str(vidBasename), (10, int(spacingScale)), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 255), 3)
+                cv2.putText(sideImage, 'Are you happy with your assigned identities ?', (10, int(spacingScale * (addSpacer*2))), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 255), 2)
+                cv2.putText(sideImage, 'Press "c" to continue (to finish, or proceed to the next video)', (10, int(spacingScale * (addSpacer*3))), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (255, 255, 0), 2)
+                cv2.putText(sideImage, 'Press "x" to re-start assigning identities', (10, int(spacingScale * (addSpacer*4))), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 255, 255), 2)
+                imageConcat = np.concatenate((overlay, sideImage), axis=0)
+                imageConcat = np.uint8(imageConcat)
+                cv2.imshow('Define animal IDs', imageConcat)
+                k = cv2.waitKey(10)
+                if k == ord('c'):
+                    cv2.destroyWindow('Define animal IDs')
+                    break
+                if k == ord('x'):
+                    overlay = imWithCordsOnly.copy()
+                    ID_user_cords, currIDcounter = [], 0
+                    assigningIDs, completePromt = True, False
+
+        print('Re-organizing pose data-frame based on user-assigned identities: ' + str(os.path.basename(vidFname)) + '....')
+
+        for values in ID_user_cords:
+            currClickedX, currClickedY, currClickedID = values[0], values[1], values[2]
+            for bpCords in indBpCordList:
+                currX, currY, ID = bpCords[0], bpCords[1], bpCords[2]
+                currEuclidian = np.sqrt((currClickedX - currX) ** 2 + (currClickedY - currY) ** 2)
+                EuclidDistanceList.append([currEuclidian, currClickedID, ID])
+        euclidDf = pd.DataFrame(EuclidDistanceList)
+        euclidDf.columns = ['Distance', 'clickID', 'pose_ID']
+        for i in currIDList:
+            minDistance = euclidDf.loc[euclidDf['clickID'] == i, 'Distance'].min()
+            animalPoseID = euclidDf.loc[euclidDf['Distance'] == minDistance, 'pose_ID'].iloc[0]
+            changeList.append([animalPoseID, i])
+        for animal in changeList:
+            currPoseName, newName = animal[0], animal[1]
+            loop = 0
+            for header in bpNameList:
+                if header.startswith(currPoseName):
+                    newHeader = header.replace(currPoseName, newName)
+                    bpNameList[loop] = newHeader
+                loop += 1
+
+        currDf.columns = bpNameList
+        outDf = pd.DataFrame()
+        for name in currIDList:
+            currCols = [col for col in currDf.columns if name in col]
+            sliceDf = currDf[currCols]
+            outDf = pd.concat([outDf, sliceDf], axis=1)
+        outDfcols = list(outDf.columns)
+        toBpCSVlist = []
+        # if poseEstimationSetting == 'user_defined':
+        #     for i in outDfcols:
+        #         currBpName = i[:-2]
+        #         toBpCSVlist.append(currBpName) if currBpName not in toBpCSVlist else toBpCSVlist
+        #     f = open(bpNamesCSVPath, 'w+')
+        #     for i in toBpCSVlist:
+        #         f.write(i + '\n')
+        #     f.close
+        MultiIndexCol = []
+        for column in range(len(outDf.columns)):
+            MultiIndexCol.append(tuple(('DLC_multi', 'DLC_multi', outDf.columns[column])))
+        outDf.columns = pd.MultiIndex.from_tuples(MultiIndexCol, names=['scorer', 'bodypart', 'coords'])
+        outputCSVname = os.path.basename(vidFname).replace(VideoExtension, '.' + wfileType)
+        if wfileType == 'parquet':
+            table = pa.Table.from_pandas(outDf)
+            pq.write_table(table, os.path.join(outputDfFolder, outputCSVname))
+        if wfileType == 'csv':
+            outDf.to_csv(os.path.join(outputDfFolder, outputCSVname))
+        print('Imported ', outputCSVname, 'to current project.')
+    print('All multi-animal DLC .h5 tracking files ordered and imported into SimBA project in the chosen workflow file format')
