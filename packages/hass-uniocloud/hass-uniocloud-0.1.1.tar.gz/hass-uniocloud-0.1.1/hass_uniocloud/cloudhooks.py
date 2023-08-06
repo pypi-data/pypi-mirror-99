02f7849b8fbfd81e35806a6059691f3356cb6d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:06 +0000

    Add package hass-uniocloud/0.1.1/hass-uniocloud-0.1.1.tar.gz

---
+++ packages/hass-uniocloud/hass-uniocloud-0.1.1.tar.gz/hass-uniocloud-0.1.1/hass_uniocloud/cloudhooks.py
@@ -0,0 +1,42 @@
+"""Manage cloud cloudhooks."""
+from typing import Dict, Any
+
+
+class Cloudhooks:
+    """Class to help manage cloudhooks."""
+
+    def __init__(self, cloud):
+        """Initialize cloudhooks."""
+        self.cloud = cloud
+
+        cloud.iot.register_on_connect(self.async_publish_cloudhooks)
+
+    async def async_publish_cloudhooks(self) -> None:
+        """Inform the Relayer of the cloudhooks that we support."""
+        if not self.cloud.is_connected:
+            return
+
+        cloudhooks = self.cloud.client.cloudhooks
+        await self.cloud.iot.async_send_message(
+            "webhook-register",
+            {"cloudhook_ids": [info["cloudhook_id"] for info in cloudhooks.values()]},
+            expect_answer=False,
+        )
+
+    async def async_create(self, webhook_id: str, managed: bool) -> Dict[str, Any]:
+        """Create a cloud webhook."""
+
+        cloudhook_id = webhook_id
+        cloudhook_url = "https://server.sni.uniosmarthome.com"
+
+        hook = {
+            "webhook_id": webhook_id,
+            "cloudhook_id": cloudhook_id,
+            "cloudhook_url": cloudhook_url,
+            "managed": managed,
+        }
+
+        return hook
+
+    async def async_delete(self, webhook_id: str) -> None:
+        """Delete a cloud webhook."""
---
+++ packages/hass-uniocloud/hass-uniocloud-0.1.1.tar.gz/hass-uniocloud-0.1.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+VERSION = "0.1.1"
+
+setup(
+    name="hass-uniocloud",
+    version=VERSION,
+    license="GPL v3",
+    author="UNIO Smart Home",
+    author_email="developers@freshminds.com.br",
+    url="https://www.uniosmarthome.com",
+    download_url="https://github.com/uniosmarthome/hass-uniocloud/tarball/{}".format(VERSION),
+    description=("Home Assistant cloud integration by UNIO Smart Home"),
+    long_description=(""),
+    classifiers=[
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: Proxy Servers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords=["homeassistant", "cloud", "unio"],
+    zip_safe=False,
+    platforms="any",
+    packages=["hass_uniocloud"],
+    install_requires=[
+        "pycognito==0.1.4",
+        "snitun==0.20",
+        "acme==1.9.0",
+        "cryptography>=2.8,<4.0",
+        "attrs>=19.3,<20.4",
+        "pytz>=2019.3",
+        "aiohttp>=3.6.1",
+        "atomicwrites==1.4.0",
+    ],
+)
