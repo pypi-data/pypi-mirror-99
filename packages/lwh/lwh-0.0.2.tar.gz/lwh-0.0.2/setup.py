5728506ca421e532a43bbdc1f75d43c2c8f58ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:46 +0000

    Add package lwh/0.0.2/lwh-0.0.2.tar.gz

---
+++ packages/lwh/lwh-0.0.2.tar.gz/lwh-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lwh']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['lwh = lwh.main:app']}
+
+setup_kwargs = {
+    'name': 'lwh',
+    'version': '0.0.2',
+    'description': 'Lacework Helios Project cli',
+    'long_description': '',
+    'author': 'jeffthorne',
+    'author_email': 'jthorne@u.washington.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
