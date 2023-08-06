7b3e45ac635e7906727b7b3569e6ee1b94a35750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:25 +0000

    Add package smoldyn/2.64.dev20210319/smoldyn-2.64.dev20210319-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/smoldyn/smoldyn-2.64.dev20210319-cp37-cp37m-manylinux2010_x86_64.whl/smoldyn/__init__.py
@@ -0,0 +1,9 @@
+"""smoldyn
+
+Python bindings of Smoldyn simulator..
+
+To access the developer Python's API, use `import smoldyn._smodlyn`
+"""
+
+"""Bring symbols from User API (smoldyn.py) to top-level."""
+from smoldyn.smoldyn import *
