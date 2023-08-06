e4cfe73c2444ace5f887dc45596a55466b5510cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:15 +0000

    Add package assisted-service-client/1.0.17.1.post139/assisted_service_client-1.0.17.1.post139-py3-none-any.whl

---
+++ packages/assisted-service-client/assisted_service_client-1.0.17.1.post139-py3-none-any.whl/assisted_service_client/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from assisted_service_client.api.assisted_service_iso_api import AssistedServiceIsoApi
+from assisted_service_client.api.bootfiles_api import BootfilesApi
+from assisted_service_client.api.events_api import EventsApi
+from assisted_service_client.api.installer_api import InstallerApi
+from assisted_service_client.api.managed_domains_api import ManagedDomainsApi
+from assisted_service_client.api.manifests_api import ManifestsApi
+from assisted_service_client.api.operators_api import OperatorsApi
+from assisted_service_client.api.versions_api import VersionsApi
