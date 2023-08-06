45f78865f028dbd559fbd811761bd1e55117a09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:53 +0000

    Add package mypy-boto3-iot-data/1.17.31.0/mypy_boto3_iot_data-1.17.31.0-py3-none-any.whl

---
+++ packages/mypy-boto3-iot-data/mypy_boto3_iot_data-1.17.31.0-py3-none-any.whl/mypy_boto3_iot_data/type_defs.pyi
@@ -0,0 +1,43 @@
+"""
+Main interface for iot-data service type definitions.
+
+Usage::
+
+    ```python
+    from mypy_boto3_iot_data.type_defs import DeleteThingShadowResponseTypeDef
+
+    data: DeleteThingShadowResponseTypeDef = {...}
+    ```
+"""
+import sys
+from typing import IO, List, Union
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "DeleteThingShadowResponseTypeDef",
+    "GetThingShadowResponseTypeDef",
+    "ListNamedShadowsForThingResponseTypeDef",
+    "UpdateThingShadowResponseTypeDef",
+)
+
+DeleteThingShadowResponseTypeDef = TypedDict(
+    "DeleteThingShadowResponseTypeDef", {"payload": Union[bytes, IO[bytes]]}
+)
+
+GetThingShadowResponseTypeDef = TypedDict(
+    "GetThingShadowResponseTypeDef", {"payload": Union[bytes, IO[bytes]]}, total=False
+)
+
+ListNamedShadowsForThingResponseTypeDef = TypedDict(
+    "ListNamedShadowsForThingResponseTypeDef",
+    {"results": List[str], "nextToken": str, "timestamp": int},
+    total=False,
+)
+
+UpdateThingShadowResponseTypeDef = TypedDict(
+    "UpdateThingShadowResponseTypeDef", {"payload": Union[bytes, IO[bytes]]}, total=False
+)
