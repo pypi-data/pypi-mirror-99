786ddb06fe8d2282af007d08c09b8d1a604beaab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:20 +0000

    Add package eGUI/0.0.3/eGUI-0.0.3.tar.gz

---
+++ packages/eGUI/eGUI-0.0.3.tar.gz/eGUI-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='eGUI',
+    version='0.0.3',
+    keywords='Python',
+    description='基于Python的GUI界面，函数使用中文命名，使用简单，界面丰富，上手方便，持续更新。',
+    license='MIT License',
+    url='https://space.bilibili.com/365335499',
+    author='Python生态',
+    author_email='3157024351@qq.com',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'PySimpleGUI~=4.37.0',
+    ],
+)
