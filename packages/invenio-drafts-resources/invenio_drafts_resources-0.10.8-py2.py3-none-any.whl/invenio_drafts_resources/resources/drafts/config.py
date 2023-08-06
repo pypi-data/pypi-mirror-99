03e9ebbeb6f792c25719b4e29ce604883db78b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:54 +0000

    Add package invenio-drafts-resources/0.10.8/invenio_drafts_resources-0.10.8-py2.py3-none-any.whl

---
+++ packages/invenio-drafts-resources/invenio_drafts_resources-0.10.8-py2.py3-none-any.whl/invenio_drafts_resources/resources/drafts/config.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Drafts-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio Drafts Resources module to create REST APIs."""
+
+from invenio_records_resources.resources import RecordResourceConfig
+from invenio_records_resources.resources.actions import ActionResourceConfig
+
+from ..records import RecordLinksSchema
+from .schemas_links import DraftLinksSchema
+
+
+class DraftResourceConfig(RecordResourceConfig):
+    """Draft resource config."""
+
+    list_route = "/records/<pid_value>/draft"
+    item_route = None
+
+    links_config = {
+        "record": DraftLinksSchema
+    }
+
+
+class DraftActionResourceConfig(ActionResourceConfig):
+    """Draft action resource config."""
+
+    list_route = "/records/<pid_value>/draft/actions/<action>"
+    item_route = None  # To avoid issues, due to inheritance.
+
+    action_commands = {
+        "create": {
+            "publish": "publish",
+        }
+    }
+
+    record_links_config = {
+        "record": RecordLinksSchema
+    }
