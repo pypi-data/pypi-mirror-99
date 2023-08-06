5a333255ac6d4a1ab23d82951d92dbb0cd4a35c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:11 +0000

    Add package lusid-sdk-preview/0.11.2800/lusid-sdk-preview-0.11.2800.tar.gz

---
+++ packages/lusid-sdk-preview/lusid-sdk-preview-0.11.2800.tar.gz/lusid-sdk-preview-0.11.2800/lusid/api/__init__.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lusid.api.aggregation_api import AggregationApi
+from lusid.api.allocations_api import AllocationsApi
+from lusid.api.application_metadata_api import ApplicationMetadataApi
+from lusid.api.calendars_api import CalendarsApi
+from lusid.api.configuration_recipe_api import ConfigurationRecipeApi
+from lusid.api.conventions_api import ConventionsApi
+from lusid.api.corporate_action_sources_api import CorporateActionSourcesApi
+from lusid.api.counterparty_api import CounterpartyApi
+from lusid.api.cut_label_definitions_api import CutLabelDefinitionsApi
+from lusid.api.data_types_api import DataTypesApi
+from lusid.api.derived_transaction_portfolios_api import DerivedTransactionPortfoliosApi
+from lusid.api.entities_api import EntitiesApi
+from lusid.api.instruments_api import InstrumentsApi
+from lusid.api.legal_entities_api import LegalEntitiesApi
+from lusid.api.login_api import LoginApi
+from lusid.api.orders_api import OrdersApi
+from lusid.api.persons_api import PersonsApi
+from lusid.api.portfolio_groups_api import PortfolioGroupsApi
+from lusid.api.portfolios_api import PortfoliosApi
+from lusid.api.property_definitions_api import PropertyDefinitionsApi
+from lusid.api.quotes_api import QuotesApi
+from lusid.api.reconciliations_api import ReconciliationsApi
+from lusid.api.reference_portfolio_api import ReferencePortfolioApi
+from lusid.api.relation_definitions_api import RelationDefinitionsApi
+from lusid.api.relations_api import RelationsApi
+from lusid.api.relationship_definitions_api import RelationshipDefinitionsApi
+from lusid.api.relationships_api import RelationshipsApi
+from lusid.api.schemas_api import SchemasApi
+from lusid.api.scopes_api import ScopesApi
+from lusid.api.search_api import SearchApi
+from lusid.api.structured_market_data_api import StructuredMarketDataApi
+from lusid.api.structured_result_data_api import StructuredResultDataApi
+from lusid.api.system_configuration_api import SystemConfigurationApi
+from lusid.api.transaction_portfolios_api import TransactionPortfoliosApi
