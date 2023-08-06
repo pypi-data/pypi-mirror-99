4803a9378bd47a144b3945d5c81ecfccacc265f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:11 +0000

    Add package mpf/0.55.0.dev29/mpf-0.55.0.dev29.tar.gz

---
+++ packages/mpf/mpf-0.55.0.dev29.tar.gz/mpf-0.55.0.dev29/mpf/platforms/fast/fast_gi.py
@@ -0,0 +1,46 @@
+"""GI on fast."""
+import logging
+
+from mpf.core.utility_functions import Util
+from mpf.platforms.interfaces.light_platform_interface import LightPlatformSoftwareFade
+
+
+class FASTGIString(LightPlatformSoftwareFade):
+
+    """A FAST GI string in a WPC machine."""
+
+    __slots__ = ["log", "send"]
+
+    def __init__(self, number, sender, machine, software_fade_ms: int) -> None:
+        """Initialise GI string."""
+        super().__init__(number, machine.clock.loop, software_fade_ms)
+        self.log = logging.getLogger('FASTGIString.0x' + str(number))
+        self.send = sender
+
+    def set_brightness(self, brightness: float):
+        """Set GI string to a certain brightness."""
+        brightness = int(brightness * 255)
+        if brightness >= 255:
+            brightness = 255
+
+        self.log.debug("Turning On GI String to brightness %s", brightness)
+        # self.send('GI:' + self.number + ',' + Util.int_to_hex_string(brightness))
+
+        self.send('GI:{},{}'.format(self.number,
+                                    Util.int_to_hex_string(brightness)))
+
+    def get_board_name(self):
+        """Return the board of this light."""
+        return "FAST WPC"
+
+    def is_successor_of(self, other):
+        """Return true if the other light has the previous number."""
+        raise AssertionError("Not possible in FASTGI.")
+
+    def get_successor_number(self):
+        """Return next number."""
+        raise AssertionError("Not possible in FASTGI.")
+
+    def __lt__(self, other):
+        """Order lights by string."""
+        return self.number < other.number
---
+++ packages/mpf/mpf-0.55.0.dev29.tar.gz/mpf-0.55.0.dev29/mpf/platforms/fast/fast_servo.py
@@ -0,0 +1,35 @@
+"""Fast servo implementation."""
+from mpf.core.utility_functions import Util
+from mpf.platforms.interfaces.servo_platform_interface import ServoPlatformInterface
+
+
+class FastServo(ServoPlatformInterface):
+
+    """A servo in the FAST platform."""
+
+    __slots__ = ["number", "net_connection"]
+
+    def __init__(self, number, net_connection):
+        """Initialise servo."""
+        self.number = number
+        self.net_connection = net_connection
+
+    def go_to_position(self, position):
+        """Set a servo position."""
+        if position < 0 or position > 1:
+            raise AssertionError("Position has to be between 0 and 1")
+
+        # convert from [0,1] to [0, 255]
+        position_numeric = int(position * 255)
+
+        cmd = 'XO:{},{}'.format(
+            self.number,
+            Util.int_to_hex_string(position_numeric))
+
+        self.net_connection.send(cmd)
+
+    def set_speed_limit(self, speed_limit):
+        """Not implemented."""
+
+    def set_acceleration_limit(self, acceleration_limit):
+        """Not implemented."""
