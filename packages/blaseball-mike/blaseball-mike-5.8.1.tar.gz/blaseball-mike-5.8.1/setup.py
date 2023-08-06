034306061dc9e640ea0a09bd7b2b8d445753731e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:58 +0000

    Add package blaseball-mike/5.8.1/blaseball-mike-5.8.1.tar.gz

---
+++ packages/blaseball-mike/blaseball-mike-5.8.1.tar.gz/blaseball-mike-5.8.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+
+install_requires = [
+    'aiohttp',
+    'aiohttp-sse-client',
+    'python-dateutil',
+    'requests',
+    'ujson',
+    'requests-cache'
+    ]
+
+setuptools.setup(
+    name='blaseball-mike',
+    version='5.8.1',
+    author='Joe Maliksi',
+    author_email='joe.maliksi@gmail.com',
+    url='https://github.com/jmaliksi/blaseball-mike',
+    description='Read-only Python wrapper around blaseball game API',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    python_requires="~=3.8",
+)
