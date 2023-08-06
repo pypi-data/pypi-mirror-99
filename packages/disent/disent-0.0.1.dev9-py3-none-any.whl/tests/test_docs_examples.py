009628fdb1a5c4eea07f9691c543563b91a57f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:22 +0000

    Add package disent/0.0.1.dev9/disent-0.0.1.dev9-py3-none-any.whl

---
+++ packages/disent/disent-0.0.1.dev9-py3-none-any.whl/tests/test_docs_examples.py
@@ -0,0 +1,57 @@
+#  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+#  MIT License
+#
+#  Copyright (c) 2021 Nathan Juraj Michlo
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#  ~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+
+import os
+from glob import glob
+
+import pytest
+
+from disent.util import _set_test_run
+from tests.util import no_stdout
+
+
+# ========================================================================= #
+# TESTS                                                                     #
+# ========================================================================= #
+
+
+ROOT_DIR = os.path.abspath(__file__ + '/../..')
+
+
+@pytest.mark.parametrize("module", [
+    os.path.relpath(path, ROOT_DIR).replace('/', '.')[:-3]
+    for path in glob(os.path.join(ROOT_DIR, 'docs/examples/**.py'))
+])
+def test_docs_examples(capsys, module):
+    # make sure everything doesnt take 5 years to run
+    _set_test_run()
+    # run all the files in the examples folder
+    import importlib
+    with no_stdout():
+        importlib.import_module(module)
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
