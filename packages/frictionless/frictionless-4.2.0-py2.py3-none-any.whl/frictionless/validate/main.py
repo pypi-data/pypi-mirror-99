e35a3281b99c2e8228ce6310cdb12435274d819f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:02 +0000

    Add package frictionless/4.2.0/frictionless-4.2.0-py2.py3-none-any.whl

---
+++ packages/frictionless/frictionless-4.2.0-py2.py3-none-any.whl/frictionless/validate/main.py
@@ -0,0 +1,38 @@
+from importlib import import_module
+from ..exception import FrictionlessException
+from ..report import Report
+from ..system import system
+from .. import errors
+
+
+@Report.from_validate
+def validate(source, type=None, **options):
+    """Validate resource
+
+    API      | Usage
+    -------- | --------
+    Public   | `from frictionless import validate`
+
+    Parameters:
+        source (dict|str): a data source
+        type (str): source type - inquiry, package, resource, schema or table
+        **options (dict): options for the underlaying function
+
+    Returns:
+        Report: validation report
+    """
+    if not type:
+        file = system.create_file(source, basepath=options.get("basepath", ""))
+        if file.type in ["table", "schema", "resource", "package", "inquiry"]:
+            type = "resource" if file.type == "table" else file.type
+    module = import_module("frictionless.validate")
+    validate = getattr(module, "validate_%s" % type, None)
+    if validate is None:
+        note = f"Not supported validate type: {type}"
+        raise FrictionlessException(errors.GeneralError(note=note))
+    # NOTE:
+    # Review whether it's a proper place for this (program sends a detector)
+    # We might resolve it when we convert Detector to be a metadata
+    if type in ["inquiry", "schema"]:
+        options.pop("detector", None)
+    return validate(source, **options)
