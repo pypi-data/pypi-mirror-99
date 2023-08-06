5397535a1e7cc17142da0a17ecf2b06d092bc552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:03 +0000

    Add package mutable-primitives/0.0.6/mutable_primitives-0.0.6-py3-none-any.whl

---
+++ packages/mutable-primitives/mutable_primitives-0.0.6-py3-none-any.whl/mutable_primitives/__init__.py
@@ -0,0 +1,5 @@
+''' Mutable Primitives Package '''
+from mutable_primitives.base import Mutable
+from mutable_primitives.bool import Bool
+from mutable_primitives.int import Int
+from mutable_primitives.float import Float
