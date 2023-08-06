233b242ec0c3f89ba26377d7bce8a8e63420adc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:44 +0000

    Add package cryptoadvance.specter/1.3.0rc5/cryptoadvance.specter-1.3.0rc5.tar.gz

---
+++ packages/cryptoadvance.specter/cryptoadvance.specter-1.3.0rc5.tar.gz/cryptoadvance.specter-1.3.0rc5/src/cryptoadvance/specter/devices/electrum.py
@@ -0,0 +1,22 @@
+from binascii import a2b_base64
+from ..util.base43 import b43_encode
+from typing import List
+from ..device import Device
+
+
+class Electrum(Device):
+    device_type = "electrum"
+    name = "Electrum"
+    icon = "electrum_icon.svg"
+
+    sd_card_support = True
+    qr_code_support = True
+
+    def __init__(self, name, alias, keys, fullpath, manager):
+        super().__init__(name, alias, keys, fullpath, manager)
+
+    def create_psbts(self, base64_psbt, wallet):
+        # remove non_witness utxo for QR code
+        updated_psbt = wallet.fill_psbt(base64_psbt, non_witness=False, xpubs=False)
+        psbts = {"qrcode": b43_encode(a2b_base64(updated_psbt)), "sdcard": base64_psbt}
+        return psbts
---
+++ packages/cryptoadvance.specter/cryptoadvance.specter-1.3.0rc5.tar.gz/cryptoadvance.specter-1.3.0rc5/src/cryptoadvance/specter/devices/keepkey.py
@@ -0,0 +1,18 @@
+from .hwi_device import HWIDevice
+from .hwi.keepkey import KeepkeyClient
+
+
+class Keepkey(HWIDevice):
+    device_type = "keepkey"
+    name = "KeepKey"
+    icon = "keepkey_icon.svg"
+
+    supports_hwi_toggle_passphrase = True
+    supports_hwi_multisig_display_address = True
+
+    def __init__(self, name, alias, keys, fullpath, manager):
+        HWIDevice.__init__(self, name, alias, keys, fullpath, manager)
+
+    @classmethod
+    def get_client(cls, *args, **kwargs):
+        return KeepkeyClient(*args, **kwargs)
