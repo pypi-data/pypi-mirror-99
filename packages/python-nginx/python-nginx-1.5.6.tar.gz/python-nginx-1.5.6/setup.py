872f7fdd89f364507d4ecce5f12659d2e79f0f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:16 +0000

    Add package python-nginx/1.5.6/python-nginx-1.5.6.tar.gz

---
+++ packages/python-nginx/python-nginx-1.5.6.tar.gz/python-nginx-1.5.6/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='python-nginx',
+    version='1.5.6',
+    description='Create and modify nginx serverblock configs in Python',
+    author='Jacob Cook',
+    author_email='jacob@peakwinter.net',
+    url='https://github.com/peakwinter/python-nginx',
+    py_modules=['nginx'],
+    keywords=['nginx', 'web servers', 'serverblock', 'server block'],
+    download_url='https://github.com/peakwinter/python-nginx/archive/1.5.4.zip',
+    license='GPLv3',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: Unix",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+    ]
+)
