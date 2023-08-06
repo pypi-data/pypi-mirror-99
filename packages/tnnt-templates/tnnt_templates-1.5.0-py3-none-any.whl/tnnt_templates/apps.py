7db5e49dfb1e401a5dcdf24a7140aed179204bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:21 +0000

    Add package tnnt-templates/1.5.0/tnnt_templates-1.5.0-py3-none-any.whl

---
+++ packages/tnnt-templates/tnnt_templates-1.5.0-py3-none-any.whl/tnnt_templates/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from . import __version__
+
+
+class TnntTemplatesConfig(AppConfig):
+    name = "tnnt_templates"
+    label = "tnnt_templates"
+    verbose_name = "Alliance Auth Templates v{version}".format(version=__version__)
