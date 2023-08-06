25252179f74a92c6d5756597993d4e14bcc257c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:16 +0000

    Add package terra-ai/4.9.24/terra_ai-4.9.24.tar.gz

---
+++ packages/terra-ai/terra_ai-4.9.24.tar.gz/terra_ai-4.9.24/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='terra_ai',
+    version='4.9.24',
+    packages=['terra_ai'],
+    url='',
+    license='',
+    author='AI University',
+    author_email='nejronki@gmail.com'    
+)
