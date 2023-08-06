8dac9152e00aff55beab287d8edb0b675ac00fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:59 +0000

    Add package datto/0.4.8/datto-0.4.8.tar.gz

---
+++ packages/datto/datto-0.4.8.tar.gz/datto-0.4.8/datto/SetupMethods.py
@@ -0,0 +1,43 @@
+import logging
+import sys
+
+import numpy as np
+import pandas as pd
+from pythonjsonlogger import jsonlogger
+
+
+class SetupMethods:
+    def setup_logger(self):
+        """
+        Returns
+        --------
+        logger: jsonlogger
+        """
+        logger = logging.getLogger()
+        logging.basicConfig(
+            format="%(levelname) -10s %(asctime)s %(module)s at line %(lineno)d: %(message)s",
+            level=logging.DEBUG,
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        logHandler = logging.StreamHandler(sys.stdout)
+        formatter = jsonlogger.JsonFormatter()
+        logHandler.setFormatter(formatter)
+        logger.addHandler(logHandler)
+        return logger
+
+    def display_more_data(self, num_to_display):
+        """
+        Overrides Pandas and Numpy settings to display a larger amount of data instead of only a subset.
+
+        Parameters
+        --------
+        num_to_display: int
+            How many rows/columns to display
+        """
+
+        np.set_printoptions(suppress=True, formatter={"float_kind": "{:0.6f}".format})
+        pd.set_option("display.float_format", lambda x: "%.6f" % x)
+
+        np.set_printoptions(threshold=num_to_display)
+        pd.set_option("display.max_columns", num_to_display)
+        pd.set_option("display.max_rows", num_to_display)
