da6f64bf5f2d0d16e7d1a89bc6baa5923fede10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:16 +0000

    Add package aws-cdk.cdk-assets-schema/1.95.0/aws-cdk.cdk-assets-schema-1.95.0.tar.gz

---
+++ packages/aws-cdk.cdk-assets-schema/aws-cdk.cdk-assets-schema-1.95.0.tar.gz/aws-cdk.cdk-assets-schema-1.95.0/setup.py
@@ -0,0 +1,60 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-cdk.cdk-assets-schema",
+    "version": "1.95.0",
+    "description": "Schema definition for the Asset Manifest",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.cdk_assets_schema._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.cdk_assets_schema._jsii": [
+            "cdk-assets-schema@1.95.0.jsii.tgz"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii>=1.26.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 7 - Inactive",
+        "License :: OSI Approved",
+        "Framework :: AWS CDK",
+        "Framework :: AWS CDK :: 1"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
