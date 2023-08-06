f5097d01bfa310f10b054a4844d3e05656c79367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:44 +0000

    Add package pulpcore-client/3.12.0.dev1616212283/pulpcore-client-3.12.0.dev1616212283.tar.gz

---
+++ packages/pulpcore-client/pulpcore-client-3.12.0.dev1616212283.tar.gz/pulpcore-client-3.12.0.dev1616212283/pulpcore/client/pulpcore/api/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulpcore.api.access_policies_api import AccessPoliciesApi
+from pulpcore.client.pulpcore.api.artifacts_api import ArtifactsApi
+from pulpcore.client.pulpcore.api.content_api import ContentApi
+from pulpcore.client.pulpcore.api.contentguards_api import ContentguardsApi
+from pulpcore.client.pulpcore.api.exporters_core_exports_api import ExportersCoreExportsApi
+from pulpcore.client.pulpcore.api.exporters_pulp_api import ExportersPulpApi
+from pulpcore.client.pulpcore.api.groups_api import GroupsApi
+from pulpcore.client.pulpcore.api.groups_model_permissions_api import GroupsModelPermissionsApi
+from pulpcore.client.pulpcore.api.groups_object_permissions_api import GroupsObjectPermissionsApi
+from pulpcore.client.pulpcore.api.groups_users_api import GroupsUsersApi
+from pulpcore.client.pulpcore.api.importers_core_import_check_api import ImportersCoreImportCheckApi
+from pulpcore.client.pulpcore.api.importers_core_imports_api import ImportersCoreImportsApi
+from pulpcore.client.pulpcore.api.importers_pulp_api import ImportersPulpApi
+from pulpcore.client.pulpcore.api.orphans_api import OrphansApi
+from pulpcore.client.pulpcore.api.repair_api import RepairApi
+from pulpcore.client.pulpcore.api.repositories_api import RepositoriesApi
+from pulpcore.client.pulpcore.api.signing_services_api import SigningServicesApi
+from pulpcore.client.pulpcore.api.status_api import StatusApi
+from pulpcore.client.pulpcore.api.task_groups_api import TaskGroupsApi
+from pulpcore.client.pulpcore.api.tasks_api import TasksApi
+from pulpcore.client.pulpcore.api.uploads_api import UploadsApi
+from pulpcore.client.pulpcore.api.users_api import UsersApi
+from pulpcore.client.pulpcore.api.workers_api import WorkersApi
