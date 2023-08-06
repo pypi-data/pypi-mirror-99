cc44887d1eafae5a9e88e4d8bd35fa3ab04a13a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:00 +0000

    Add package confusables/1.2.0/confusables-1.2.0.tar.gz

---
+++ packages/confusables/confusables-1.2.0.tar.gz/confusables-1.2.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import os
+
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setuptools.setup(
+    name="confusables",
+    version="1.2.0",
+    author="Nathan Woodger",
+    author_email="woodgern@gmail.com",
+    description="A python package providing functionality for matching words that can be confused for eachother, but contain different characters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/woodgern/confusables",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={'confusables': ['assets/*.json', 'assets/*.txt']},
+)
+
