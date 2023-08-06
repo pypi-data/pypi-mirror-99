37dfa98bb717cf5498c40a1fe234659ab2eb91aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:43 +0000

    Add package mypy-boto3-acm/1.17.33.0/mypy_boto3_acm-1.17.33.0-py3-none-any.whl

---
+++ packages/mypy-boto3-acm/mypy_boto3_acm-1.17.33.0-py3-none-any.whl/mypy_boto3_acm/waiter.py
@@ -0,0 +1,34 @@
+"""
+Main interface for acm service client waiters.
+
+Usage::
+
+    ```python
+    import boto3
+
+    from mypy_boto3_acm import ACMClient
+    from mypy_boto3_acm.waiter import (
+        CertificateValidatedWaiter,
+    )
+
+    client: ACMClient = boto3.client("acm")
+
+    certificate_validated_waiter: CertificateValidatedWaiter = client.get_waiter("certificate_validated")
+    ```
+"""
+from botocore.waiter import Waiter as Boto3Waiter
+
+from mypy_boto3_acm.type_defs import WaiterConfigTypeDef
+
+__all__ = ("CertificateValidatedWaiter",)
+
+
+class CertificateValidatedWaiter(Boto3Waiter):
+    """
+    [Waiter.CertificateValidated documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.33/reference/services/acm.html#ACM.Waiter.CertificateValidated)
+    """
+
+    def wait(self, CertificateArn: str, WaiterConfig: WaiterConfigTypeDef = None) -> None:
+        """
+        [CertificateValidated.wait documentation](https://boto3.amazonaws.com/v1/documentation/api/1.17.33/reference/services/acm.html#ACM.Waiter.CertificateValidated.wait)
+        """
