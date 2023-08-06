e1b05147d87b5d3785b4f5bedd7789153acbd626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:20 +0000

    Add package torchblocks-chen/1.0.1.9/torchblocks_chen-1.0.1.9.tar.gz

---
+++ packages/torchblocks-chen/torchblocks_chen-1.0.1.9.tar.gz/torchblocks_chen-1.0.1.9/setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+setup(
+    name="torchblocks_chen",
+    version="1.0.1.9",
+    author="chen",
+    author_email="2728870472@qq.com",
+    description="A PyTorch-based toolkit for natural language processing",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    keywords="NLP deep learning transformer pytorch BERT",
+    license="MIT",
+    url="https://github.com/lonePatient/TorchBlocks",
+    # package_dir={"": "torchblocks"},
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn",
+        "tokenizers >= 0.7.0",
+        "torch >= 1.6.0",
+        "transformers >= 4.1.1",
+        "sentencepiece",
+        "sacremoses",
+    ],
+    python_requires=">=3.6.0",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
+
+
