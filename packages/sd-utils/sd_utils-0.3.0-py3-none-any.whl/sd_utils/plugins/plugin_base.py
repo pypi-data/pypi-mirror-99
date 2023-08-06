bb8a863489f6faf293988e6266b3c88fa9bc0e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:36 +0000

    Add package sd-utils/0.3.0/sd_utils-0.3.0-py3-none-any.whl

---
+++ packages/sd-utils/sd_utils-0.3.0-py3-none-any.whl/sd_utils/plugins/plugin_base.py
@@ -0,0 +1,53 @@
+from typing import Any
+
+
+class PluginBase:
+    """
+    """
+
+    def __init__(self, **kwargs):
+        """
+        """
+        pass
+
+    def on_register(self, data: Any = None):
+        """
+        runs when this plugin is registered with
+        the plugin manager
+
+        Args:
+            data (Any): any parameters passed to this
+                plugin from the plugin manager
+        """
+        raise NotImplementedError
+
+    def on_search(self, data: Any = None):
+        """
+        runs when the manager begins a query for
+        any plugin.
+
+        Args:
+            data (Any): any parameters passed to this
+                plugin from the plugin manager
+        """
+        raise NotImplementedError
+
+    def on_find(self, data: Any = None) -> Any:
+        """
+        runs when the manager specifically queries this plugin
+
+        Args:
+            data (Any): any parameters passed to this
+                plugin from the plugin manager
+        """
+        raise NotImplementedError
+
+    def on_iterate(self, data: Any = None):
+        """
+        runs when the manager specifically calls iterate_all
+
+        Args:
+            data (Any): any parameters passed to this
+                plugin from the plugin manager
+        """
+        raise NotImplementedError
