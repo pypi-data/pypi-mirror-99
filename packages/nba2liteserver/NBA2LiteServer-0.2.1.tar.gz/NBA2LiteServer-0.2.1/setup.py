42d282952e4e5ed64f9f692a6a83a9a9076804a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:21 +0000

    Add package nba2liteserver/0.2.1/NBA2LiteServer-0.2.1.tar.gz

---
+++ packages/nba2liteserver/NBA2LiteServer-0.2.1.tar.gz/NBA2LiteServer-0.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.1'
+DESCRIPTION = 'NBA2-lite-server'
+LONG_DESCRIPTION = 'Interface to the NBA2-lite to be replace with any other server handler'
+
+# Setting up
+setup(
+    name="NBA2LiteServer",
+    version=VERSION,
+    author="Test1",
+    author_email="byhouse1@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests', 'psutil', 'paramiko', 'Flask', 'Flask-Cors', 'furl'],
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
