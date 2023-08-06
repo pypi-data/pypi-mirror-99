d423fc007fce31a4d8e08a7ce7c1b136851890d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:55 +0000

    Add package openmc-data-downloader/0.2.0/openmc_data_downloader-0.2.0.tar.gz

---
+++ packages/openmc-data-downloader/openmc_data_downloader-0.2.0.tar.gz/openmc_data_downloader-0.2.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openmc_data_downloader",
+    version="0.2.0",
+    summary="Download cross section h5 files for use in OpenMC",
+    author="Jonathan Shimwell",
+    author_email="mail@jshimwell.com",
+    description="A tool for selectively downloading h5 files for specified isotopes / elements from your libraries of choice",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/openmc-data-storage/openmc_data_downloader",
+    packages=setuptools.find_packages(),
+    zip_safe=True,
+    package_dir={"openmc_data_downloader": "openmc_data_downloader"},
+    scripts=['openmc_data_downloader/openmc_data_downloader'],
+    package_data={
+        "openmc_data_downloader": [
+            "requirements.txt",
+            "README.md",
+            "LICENSE",
+        ]
+    },
+    tests_require=["pytest-cov", "pytest-runner"],
+    install_requires=[
+        "pandas",
+        # 'openmc' is optional for this package but is not available via pip install at the moment
+    ],
+)
