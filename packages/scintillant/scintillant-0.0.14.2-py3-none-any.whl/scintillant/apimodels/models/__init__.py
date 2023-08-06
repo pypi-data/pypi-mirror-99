a0d75fbf526658146646099d244a06d1eb44ad79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:00 +0000

    Add package scintillant/0.0.14.2/scintillant-0.0.14.2-py3-none-any.whl

---
+++ packages/scintillant/scintillant-0.0.14.2-py3-none-any.whl/scintillant/apimodels/models/__init__.py
@@ -0,0 +1,16 @@
+"""
+:mod: `models`
+=====================
+
+Description of models within the skill network.
+
+.. module:: skill_models
+.. module_author:: Niel (Ketov) Gorev <ketov-x@ya.ru>
+.. module_author:: http://t.me/ketov-x
+"""
+
+from scintillant.apimodels.models.client_models import ClientResponse
+from scintillant.apimodels.models.skill_models import (
+    SkillResponse, SkillRequest
+)
+from scintillant.apimodels.models.dialog_models import DialogAnswerRequest
