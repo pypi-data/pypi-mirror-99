5370ffbe3381d770d0d0880dc183ff09e970408d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:13 +0000

    Add package covid-data-by-zip-code/0.1.0/covid_data_by_zip_code-0.1.0.tar.gz

---
+++ packages/covid-data-by-zip-code/covid_data_by_zip_code-0.1.0.tar.gz/covid_data_by_zip_code-0.1.0/covid_data_by_zip_code/covid_data_by_zip_code.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import requests
+
+
+def covid_data_by_state_by_day(state, start, end, columns=None):
+    template = f'https://api.covidtracking.com/v1/states/{state}/daily.csv'
+    rows = []
+    header = []
+    content = requests.get(template).text.split('\n')
+
+    for idx, entry in enumerate(content):
+        if idx != 0:
+            rows.append(entry.split(','))
+        else:
+            header.extend(entry.split(','))
+
+    df = pd.DataFrame(rows, columns=header)
+    df['date'] = pd.to_datetime(df['date'])
+
+    if not columns:
+        columns = ['date', 'state',
+                   'positive', 'positiveIncrease',
+                   'negative', 'negativeIncrease',
+                   'death', 'deathIncrease',
+                   'hospitalized', 'hospitalizedIncrease',
+                   'recovered']
+
+    df = df[columns]
+    df.dropna(subset=['date'], inplace=True)
+
+    if not start and not end:
+        try:
+            start = pd.to_datetime(start)
+            end = pd.to_datetime(end)
+        except RuntimeError as e:
+            print(e)
+            raise
+
+    df = df[df['date'] >= start]
+    df = df[df['date'] <= end]
+    df.reset_index(drop=True, inplace=True)
+
+    return df
+
---
+++ packages/covid-data-by-zip-code/covid_data_by_zip_code-0.1.0.tar.gz/covid_data_by_zip_code-0.1.0/tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for covid_data_by_zip_code."""
