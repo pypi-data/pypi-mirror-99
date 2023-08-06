d08842081cd72944e7935f369a9794b7d2758c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:08 +0000

    Add package django-sql-dashboard/0.4a2/django-sql-dashboard-0.4a2.tar.gz

---
+++ packages/django-sql-dashboard/django-sql-dashboard-0.4a2.tar.gz/django-sql-dashboard-0.4a2/django_sql_dashboard/utils.py
@@ -0,0 +1,48 @@
+import json
+from collections import namedtuple
+
+SQL_SALT = "django_sql_dashboard:query"
+
+
+class Row:
+    def __init__(self, values, columns):
+        self.values = values
+        self.columns = columns
+        self.zipped = dict(zip(columns, values))
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.values[key]
+        else:
+            return self.zipped[key]
+
+    def __repr__(self):
+        return json.dumps(self.zipped)
+
+
+def displayable_rows(rows, columns):
+    fixed = []
+    for row in rows:
+        fixed_row = []
+        for cell in row:
+            if isinstance(cell, (dict, list)):
+                cell = json.dumps(cell)
+            fixed_row.append(cell)
+        fixed.append(dict(zip(columns, fixed_row)))
+    return fixed
+
+
+class _CaptureDict:
+    def __init__(self):
+        self.accessed = []
+
+    def __getitem__(self, key):
+        if key not in self.accessed:
+            self.accessed.append(key)
+        return ""
+
+
+def extract_named_parameters(sql):
+    capture = _CaptureDict()
+    sql % capture
+    return capture.accessed
