b9692bc9b821d58238896af2794b3b6851592d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:56 +0000

    Add package ivystar/0.1.3/ivystar-0.1.3-py3-none-any.whl

---
+++ packages/ivystar/ivystar-0.1.3-py3-none-any.whl/ivystar/src/conf/__init__.py
@@ -0,0 +1,13 @@
+#!encoding=utf-8
+
+# 将路径配置入环境变量
+
+import os
+
+PROJECT_PATH = os.getenv("PROJECT_PATH")
+SIGNATURE_FILE_PATH = os.getenv("SIGNATURE_FILE_PATH")
+BERT_MODEL_PATH = os.getenv("BERT_MODEL_PATH")
+
+print(PROJECT_PATH, SIGNATURE_FILE_PATH, BERT_MODEL_PATH)
+
+# 'bert/chinese_L-12_H-768_A-12/bert_config.json'
