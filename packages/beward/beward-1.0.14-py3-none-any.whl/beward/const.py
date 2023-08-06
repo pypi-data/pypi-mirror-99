b449ac018c86f2aba9c9355733d606ee887acf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:06 +0000

    Add package beward/1.0.14/beward-1.0.14-py3-none-any.whl

---
+++ packages/beward/beward-1.0.14-py3-none-any.whl/beward/const.py
@@ -0,0 +1,43 @@
+#  Copyright (c) 2019-2021, Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
+#  Creative Commons BY-NC-SA 4.0 International Public License
+#  (see LICENSE.md or https://creativecommons.org/licenses/by-nc-sa/4.0/)
+"""Constants."""
+
+# Base library constants
+NAME = "Beward Client"
+VERSION = "1.0.14"
+AUTHOR = 'Andrey "Limych" Khrolenok <andrey@khrolenok.ru>'
+LICENSE = "Creative Commons BY-NC-SA License"
+WEBSITE = "https://github.com/Limych/py-beward"
+ISSUE_URL = "https://github.com/Limych/py-beward/issues"
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+{NAME}
+Version: {VERSION}
+If you have ANY issues with this you need to open an issue here:
+{ISSUE_URL}
+-------------------------------------------------------------------
+"""
+
+
+TIMEOUT = 3
+
+# Error strings
+MSG_GENERIC_FAIL = "Sorry.. Something went wrong..."
+
+# Alarms
+ALARM_ONLINE = "DeviceOnline"
+ALARM_MOTION = "MotionDetection"
+ALARM_SENSOR = "SensorAlarm"
+ALARM_SENSOR_OUT = "SensorOutAlarm"
+
+# Device types
+BEWARD_CAMERA = "camera"
+BEWARD_DOORBELL = "doorbell"
+
+# Beward models
+BEWARD_MODELS = {
+    BEWARD_CAMERA: "",
+    BEWARD_DOORBELL: "DK103M DS03M DS05M(P) DS06M DS06A(P) S06A S06M",
+}
