7987cab972c03ae770a48b1d4b66b6eccd6cb884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:19 +0000

    Add package cidc-schemas/0.24.9/cidc_schemas-0.24.9-py2.py3-none-any.whl

---
+++ packages/cidc-schemas/cidc_schemas-0.24.9-py2.py3-none-any.whl/cidc_schemas/prism/constants.py
@@ -0,0 +1,44 @@
+"""CIDC schemas-specific constants relevant to prismifying/merging functionality."""
+
+PROTOCOL_ID_FIELD_NAME = "protocol_identifier"
+
+SUPPORTED_ASSAYS = [
+    "wes_fastq",
+    "wes_bam",
+    "olink",
+    "cytof",
+    "ihc",
+    "elisa",
+    "rna_fastq",
+    "rna_bam",
+    "mif",
+    "tcr_fastq",
+    "hande",
+    "nanostring",
+]
+
+SUPPORTED_SHIPPING_MANIFESTS = [
+    "pbmc",
+    "plasma",
+    "tissue_slide",
+    "normal_blood_dna",
+    "normal_tissue_dna",
+    "tumor_tissue_dna",
+    "tumor_tissue_rna",
+    "h_and_e",
+]
+# weird non shipping manifest
+SUPPORTED_WEIRD_MANIFESTS = ["tumor_normal_pairing", "participants_annotations"]
+MANIFESTS_WITH_PARTICIPANT_INFO = SUPPORTED_SHIPPING_MANIFESTS + [
+    "participants_annotations"
+]
+SUPPORTED_MANIFESTS = SUPPORTED_SHIPPING_MANIFESTS + SUPPORTED_WEIRD_MANIFESTS
+
+SUPPORTED_ANALYSES = [
+    "cytof_analysis",
+    "wes_analysis",
+    "rna_level1_analysis",
+    "tcr_analysis",
+]
+
+SUPPORTED_TEMPLATES = SUPPORTED_ASSAYS + SUPPORTED_MANIFESTS + SUPPORTED_ANALYSES
