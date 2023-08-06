575682c70256ac7344e87f8a795fc097091873c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:10 +0000

    Add package devicely/0.2.5/devicely-0.2.5.tar.gz

---
+++ packages/devicely/devicely-0.2.5.tar.gz/devicely-0.2.5/devicely/__init__.py
@@ -0,0 +1,6 @@
+from .empatica import EmpaticaReader
+from .everion import EverionReader
+from .faros import FarosReader
+from .tag import TagReader
+from .spacelabs import SpacelabsReader
+from .shimmer_plus import ShimmerPlusReader
