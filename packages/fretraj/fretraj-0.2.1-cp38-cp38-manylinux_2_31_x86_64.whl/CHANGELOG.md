1203ba04252fceaabb1b26f6e4861e8c446ed36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:44 +0000

    Add package fretraj/0.2.1/fretraj-0.2.1-cp38-cp38-manylinux_2_31_x86_64.whl

---
+++ packages/fretraj/fretraj-0.2.1-cp38-cp38-manylinux_2_31_x86_64.whl/CHANGELOG.md
@@ -0,0 +1,23 @@
+## Changelog
+
+### [0.2.1] 2021-03-19
+- Create matrix deployments for different Python versions
+
+### [0.2.0] 2021-03-19
+- Add submodule to simulate photon burst to account for shot-noise and better comparison with single-molecule confocal experiments
+- Add notebook documenting the burst simulation
+- Add pybind11 and build.py to pyproject.toml to build burst.relaxation C++ extension
+- setup.py is omitted when building locally (to avoid breaking the metadata) but generated when building via github actions since conda still depends on it. 
+- Updated docstrings and docs
+
+### [0.1.5] 2021-03-09
+- Improve continuous integration with Github actions
+- Add PyPI and conda deployment
+- Add first unit and integration tests of ACV calculations
+- NGLview for displaying ACVs in jupyter notebooks
+- Updated docs with jupyter-books
+- Add interactive notebooks with Binder
+
+### [0.1.1] 2021-03-03
+- Initial test release of FRETraj, a Python module to calculate 
+accessible-contact volumes (ACV) and predict FRET efficiencies.
---
+++ packages/fretraj/fretraj-0.2.1-cp38-cp38-manylinux_2_31_x86_64.whl/fretraj/__init__.py
@@ -0,0 +1,51 @@
+import sys
+
+try:
+    import importlib.metadata as meta  # Python >=3.8
+except ModuleNotFoundError:
+    import importlib_metadata as meta  # Python 3.7
+
+PACKAGE = 'fretraj'
+
+try:
+    metadata = meta.metadata(PACKAGE)
+except meta.PackageNotFoundError:
+    print(f'{PACKAGE} is not installed yet.')
+
+
+def _get_urls():
+    __urls__ = {}
+    try:
+        for _url in metadata.get_all('Project-URL'):
+            __urls__.update(dict([_url.replace(' ', '').split(',')]))
+    except TypeError:
+        __urls__['Documentation'] = None
+        __urls__['Repository'] = None
+    return __urls__
+
+
+__version__ = metadata['Version']
+__author__ = metadata['Author']
+__keywords__ = metadata['Keywords']
+__license__ = metadata['License']
+__urls__ = _get_urls()
+
+from . import cloud
+from . import export
+from . import fret
+from . import grid
+from . import jupyter
+
+if sys.platform.startswith('linux'):
+    try:
+        from . import burst
+    except (ImportError, ModuleNotFoundError):
+        print('The burst submodule could not be imported\n')
+else:
+    print('The burst submodule is currently only available on Linux\n')
+
+import warnings
+import numba
+
+warnings.simplefilter('ignore', category=numba.errors.NumbaDeprecationWarning)
+warnings.simplefilter('ignore', category=numba.errors.NumbaPendingDeprecationWarning)
