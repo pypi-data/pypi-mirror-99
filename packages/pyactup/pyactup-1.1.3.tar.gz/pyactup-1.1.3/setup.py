cc1370bf087be0c23992facf61314fa3564d3c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:53 +0000

    Add package pyactup/1.1.3/pyactup-1.1.3.tar.gz

---
+++ packages/pyactup/pyactup-1.1.3.tar.gz/pyactup-1.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from pyactup import __version__
+
+DESCRIPTION = open("README.md").read()
+
+setup(name="pyactup",
+      version=__version__,
+      description="A lightweight Python implementation of a subset of the ACT-R cognitive architecture’s Declarative Memory",
+      author="Don Morrison",
+      author_email="dfm2@cmu.edu",
+      url="https://bitbucket.org/dfmorrison/pyactup/",
+      platforms=["any"],
+      long_description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      py_modules=["pyactup"],
+      install_requires=["pylru"],
+      tests_require=["pytest"],
+      python_requires=">=3.7",
+      classifiers=["Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 3 :: Only",
+                   "Programming Language :: Python :: 3.7",
+                   "Programming Language :: Python :: 3.8",
+                   "Programming Language :: Python :: 3.9",
+                   "Operating System :: OS Independent"])
