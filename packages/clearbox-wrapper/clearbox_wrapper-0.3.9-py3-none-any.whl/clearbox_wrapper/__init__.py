acd34827b36b734685ef78224fe1efc274845f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:08 +0000

    Add package clearbox-wrapper/0.3.9/clearbox_wrapper-0.3.9-py3-none-any.whl

---
+++ packages/clearbox-wrapper/clearbox_wrapper-0.3.9-py3-none-any.whl/clearbox_wrapper/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.3.9"
+
+from .exceptions import ClearboxWrapperException
+from .model import Model
+from .wrapper import load_model, save_model
+
+
+__all__ = [ClearboxWrapperException, load_model, Model, save_model]
