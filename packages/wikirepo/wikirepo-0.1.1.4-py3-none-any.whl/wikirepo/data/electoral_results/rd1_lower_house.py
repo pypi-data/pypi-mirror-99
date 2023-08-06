d4b5c2888792f9ac48bd593a1e93127fad9a862c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:45 +0000

    Add package wikirepo/0.1.1.4/wikirepo-0.1.1.4-py3-none-any.whl

---
+++ packages/wikirepo/wikirepo-0.1.1.4-py3-none-any.whl/wikirepo/data/electoral_results/rd1_lower_house.py
@@ -0,0 +1,9 @@
+"""
+Lower House Round 1
+-------------------
+
+Functions querying 'PXYZ' (first round lower house election results) information
+
+Contents
+    query_prop_data
+"""
---
+++ packages/wikirepo/wikirepo-0.1.1.4-py3-none-any.whl/wikirepo/data/electoral_results/rd2_upper_house.py
@@ -0,0 +1,9 @@
+"""
+Upper House Round 2
+-------------------
+
+Functions querying 'PXYZ' (second round upper house election results) information
+
+Contents
+    query_prop_data
+"""
