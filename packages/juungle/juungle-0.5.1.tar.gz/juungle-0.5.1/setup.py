364802dcb4c7c15a7256c6db4e247c69a34ff07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:59 +0000

    Add package juungle/0.5.1/juungle-0.5.1.tar.gz

---
+++ packages/juungle/juungle-0.5.1.tar.gz/juungle-0.5.1/setup.py
@@ -0,0 +1,32 @@
+"""Setup module."""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f_file:
+    long_description = f_file.read()
+
+
+setuptools.setup(
+    name="juungle",
+    version="0.5.1",
+    author="Eduardo Elias",
+    author_email="camponez@gmail.com",
+    description="Juungle python package for juungle.net",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Juungle/juungle-python",
+    project_urls={
+        "Bug Tracker": "https://github.com/Juungle/juungle-python/issues",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests'
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
