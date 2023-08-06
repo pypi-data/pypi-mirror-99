d6e2c9e778289b556f9910a865e4eb65177bf4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:08 +0000

    Add package bilibiliupload/0.2.4/bilibiliupload-0.2.4.tar.gz

---
+++ packages/bilibiliupload/bilibiliupload-0.2.4.tar.gz/bilibiliupload-0.2.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import bilibiliupload
+
+with open('requirements.txt') as f:
+    requires = f.readlines()
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='bilibiliupload',
+    version=bilibiliupload.__version__,
+    packages=['bilibiliupload'],
+    url='https://github.com/comwrg/bilibiliupload',
+    install_requires=requires,
+    license='MIT',
+    author='comwrg',
+    author_email='xcomwrg@gmail.com',
+    description='Upload video to bilibili under command-line interface',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    keywords=['bilibili', 'upload'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
