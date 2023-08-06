8324f79c5a839e5a4a4edcea3bb54171a3d1b721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:08 +0000

    Add package userlib-spinD/0.0.24/userlib_spinD-0.0.24-py3-none-any.whl

---
+++ packages/userlib-spinD/userlib_spinD-0.0.24-py3-none-any.whl/examples/run_numeric_examples.py
@@ -0,0 +1,15 @@
+from examples.functions_numeric_examples import *
+
+
+
+
+#=======================================================
+# REMOVE AND ADD COMMENTS TO RUN EXAMPLES
+#=======================================================
+def main():
+
+    run_example_multilayer_GNEB()
+
+
+if __name__ == '__main__' :
+     main()
