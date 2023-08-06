29d9eb9e103e6aec08c17d3cb9b64917155be927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:40 +0000

    Add package telethon-cryptg/0.0.2/telethon-cryptg-0.0.2.tar.gz

---
+++ packages/telethon-cryptg/telethon-cryptg-0.0.2.tar.gz/telethon-cryptg-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name='telethon-cryptg',
+    version='0.0.2',
+    description='telethon bindings for the tgcrypto module',
+    long_description=open('README.rst').read().strip(),
+    author='Painor',
+    author_email='pi.oussama@gmail.com',
+    url='https://github.com/painor/telethon-tgcrypto',
+    packages=setuptools.find_packages(),
+    install_requires=['tgcrypto'],
+    license='MIT License',
+    keywords='telethon tgcrypto',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires='>=3.6',
+)
