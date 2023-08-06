cebca8f26bfcb0c57fc32778a38bf68450eeb4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:53 +0000

    Add package cdk-fargate-run-task/0.0.73/cdk_fargate_run_task-0.0.73-py3-none-any.whl

---
+++ packages/cdk-fargate-run-task/cdk_fargate_run_task-0.0.73-py3-none-any.whl/cdk_fargate_run_task/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_ecs._jsii
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.core._jsii
+import aws_cdk.custom_resources._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-fargate-run-task",
+    "0.0.73",
+    __name__[0:-6],
+    "cdk-fargate-run-task@0.0.73.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
