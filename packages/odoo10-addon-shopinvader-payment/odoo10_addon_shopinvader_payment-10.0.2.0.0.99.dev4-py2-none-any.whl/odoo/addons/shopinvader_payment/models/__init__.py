a4895ac2228f99234a0ae052b9d6278b010f0022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:45 +0000

    Add package odoo10-addon-shopinvader-payment/10.0.2.0.0.99.dev4/odoo10_addon_shopinvader_payment-10.0.2.0.0.99.dev4-py2-none-any.whl

---
+++ packages/odoo10-addon-shopinvader-payment/odoo10_addon_shopinvader_payment-10.0.2.0.0.99.dev4-py2-none-any.whl/odoo/addons/shopinvader_payment/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import shopinvader_backend
+from . import shopinvader_payment
+from . import sale_order
+from . import payment_transaction
---
+++ packages/odoo10-addon-shopinvader-payment/odoo10_addon_shopinvader_payment-10.0.2.0.0.99.dev4-py2-none-any.whl/odoo/addons/shopinvader_payment/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Sebastien BEAU <sebastien.beau@akretion.com>
+* Denis Roussel <denis.roussel@acsone.eu>
+* Laurent Mignon <laurent.mignon@acsone.eu>
