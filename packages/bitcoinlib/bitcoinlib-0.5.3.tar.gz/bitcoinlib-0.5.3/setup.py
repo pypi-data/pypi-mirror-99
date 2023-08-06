087cc5430f4785acc18a0ede76bf8c7c1a336851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:18 +0000

    Add package bitcoinlib/0.5.3/bitcoinlib-0.5.3.tar.gz

---
+++ packages/bitcoinlib/bitcoinlib-0.5.3.tar.gz/bitcoinlib-0.5.3/setup.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+#    BitcoinLib - Python Cryptocurrency Library
+#    PyPi Setup Tool
+#    © 2018-2020 January - 1200 Web Development <http://1200wd.com/>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from setuptools import setup
+from codecs import open
+import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+version = '0.5.3'
+
+# Get the long description from the relevant file
+readmetxt = ''
+try:
+      with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+          readmetxt = f.read()
+except:
+      pass
+
+kwargs = {}
+
+install_requires = [
+      'requests==2.25.1',
+      'fastecdsa==2.1.5;platform_system!="Windows"',
+      'ecdsa==0.16;platform_system=="Windows"',
+      'pyaes==1.6.1',
+      'scrypt==0.8.17',
+      'SQLAlchemy==1.3.22',
+      'numpy==1.19.5',
+]
+
+kwargs['install_requires'] = install_requires
+
+setup(
+      name='bitcoinlib',
+      version=version,
+      description='Bitcoin and Other cryptocurrency Library',
+      long_description=readmetxt,
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Financial and Insurance Industry',
+            'Intended Audience :: Science/Research',
+            'Intended Audience :: Information Technology',
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+            'Natural Language :: English',
+            'Operating System :: OS Independent',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Security :: Cryptography',
+            'Topic :: Office/Business :: Financial :: Accounting',
+      ],
+      url='http://github.com/1200wd/bitcoinlib',
+      author='1200wd',
+      author_email='info@1200wd.com',
+      license='GNU3',
+      packages=['bitcoinlib'],
+      entry_points={
+          'console_scripts': ['cli-wallet=bitcoinlib.tools.clw:main',
+                              'clw=bitcoinlib.tools.clw:main']
+      },
+      test_suite='tests',
+      include_package_data=True,
+      keywords='bitcoin library cryptocurrency wallet crypto keys segwit litecoin dash',
+      zip_safe=False,
+      **kwargs
+)
