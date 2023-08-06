ad0af7808c60c78660035f4fe96e375ab5d99676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:05 +0000

    Add package EasyBeam/0.3.0/EasyBeam-0.3.0.tar.gz

---
+++ packages/EasyBeam/EasyBeam-0.3.0.tar.gz/EasyBeam-0.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+if __name__ == '__main__':
+    from distutils.core import setup
+    extra_files = package_files('EasyBeam')
+    setup(name='EasyBeam',
+          version='0.3.0',
+          description='Easy Application for Structural analYsis with BEAMs',
+          author='V. Gufler, E.J. Wehrle',
+          author_email='veit.gufler@unibz.it',
+          url='https://github.com/veiguf/EasyBeam',
+          package_data={'': extra_files},
+          license='GNU Lesser General Public License 3.0',
+          packages=['EasyBeam'],
+          copyright="Copyright 2020 V. Gufler",
+          install_requires=['numpy',
+                            'scipy',
+                            'matplotlib'])
