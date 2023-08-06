f2e398f4268878fcca2b865ef64c9e495d688ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:07 +0000

    Add package mte/0.0.2/mte-0.0.2.tar.gz

---
+++ packages/mte/mte-0.0.2.tar.gz/mte-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mte',
+    version='0.0.2',
+    description='MTE Instruments Inc.',
+    long_description='MTE Instruments organization package.',
+    home_page='mteinstruments.com',
+    author='Martin Rioux',
+    author_email='mrioux@mteinstruments.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
