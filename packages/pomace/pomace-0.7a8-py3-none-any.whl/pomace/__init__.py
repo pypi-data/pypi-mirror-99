27c693f0894069967bbeba6ee2e6f16e74478fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:25 +0000

    Add package pomace/0.7a8/pomace-0.7a8-py3-none-any.whl

---
+++ packages/pomace/pomace-0.7a8-py3-none-any.whl/pomace/__init__.py
@@ -0,0 +1,12 @@
+from pkg_resources import (
+    DistributionNotFound as _DistributionNotFound,
+    get_distribution as _get_distribution,
+)
+
+from .api import *
+
+
+try:
+    __version__ = _get_distribution("pomace").version
+except _DistributionNotFound:
+    __version__ = "(local)"
