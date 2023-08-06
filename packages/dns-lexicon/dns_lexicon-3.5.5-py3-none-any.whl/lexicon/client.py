ca7d27b53d87770d307f5ba5226e434c0b14c7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:37 +0000

    Add package dns-lexicon/3.5.5/dns_lexicon-3.5.5-py3-none-any.whl

---
+++ packages/dns-lexicon/dns_lexicon-3.5.5-py3-none-any.whl/lexicon/client.py
@@ -0,0 +1,129 @@
+"""Main module of Lexicon. Defines the Client class, that holds all Lexicon logic."""
+from __future__ import absolute_import
+
+import importlib
+import logging
+import os
+
+import tldextract
+
+from lexicon import config as helper_config
+from lexicon import discovery
+from lexicon.exceptions import ProviderNotAvailableError
+
+
+class Client(object):
+    """This is the Lexicon client, that will execute all the logic."""
+
+    def __init__(self, config=None):
+        if not config:
+            # If there is not config specified, we load a non-interactive configuration.
+            self.config = helper_config.non_interactive_config_resolver()
+        elif not isinstance(config, helper_config.ConfigResolver):
+            # If config is not a ConfigResolver, we are in a legacy situation.
+            # We protect this part of the Client API.
+            self.config = helper_config.legacy_config_resolver(config)
+        else:
+            self.config = config
+
+        # Validate configuration
+        self._validate_config()
+
+        runtime_config = {}
+
+        # Process domain, strip subdomain
+        try:
+            domain_extractor = tldextract.TLDExtract(
+                cache_dir=_get_tldextract_cache_path(), include_psl_private_domains=True
+            )
+        except TypeError:
+            domain_extractor = tldextract.TLDExtract(
+                cache_file=_get_tldextract_cache_path(), include_psl_private_domains=True  # type: ignore
+            )
+        domain_parts = domain_extractor(self.config.resolve("lexicon:domain"))
+        runtime_config["domain"] = f"{domain_parts.domain}.{domain_parts.suffix}"
+
+        if self.config.resolve("lexicon:delegated"):
+            # handle delegated domain
+            delegated = self.config.resolve("lexicon:delegated").rstrip(".")
+            if delegated != runtime_config.get("domain"):
+                # convert to relative name
+                if delegated.endswith(runtime_config.get("domain")):
+                    delegated = delegated[: -len(runtime_config.get("domain"))]
+                    delegated = delegated.rstrip(".")
+                # update domain
+                runtime_config["domain"] = f"{delegated}.{runtime_config.get('domain')}"
+
+        self.action = self.config.resolve("lexicon:action")
+        self.provider_name = self.config.resolve(
+            "lexicon:provider_name"
+        ) or self.config.resolve("lexicon:provider")
+
+        self.config.add_config_source(helper_config.DictConfigSource(runtime_config), 0)
+
+        provider_module = importlib.import_module(
+            "lexicon.providers." + self.provider_name
+        )
+        provider_class = getattr(provider_module, "Provider")
+        self.provider = provider_class(self.config)
+
+    def execute(self):
+        """Execute provided configuration in class constructor to the DNS records"""
+        self.provider.authenticate()
+        identifier = self.config.resolve("lexicon:identifier")
+        record_type = self.config.resolve("lexicon:type")
+        name = self.config.resolve("lexicon:name")
+        content = self.config.resolve("lexicon:content")
+
+        if self.action == "create":
+            return self.provider.create_record(record_type, name, content)
+
+        if self.action == "list":
+            return self.provider.list_records(record_type, name, content)
+
+        if self.action == "update":
+            return self.provider.update_record(identifier, record_type, name, content)
+
+        if self.action == "delete":
+            return self.provider.delete_record(identifier, record_type, name, content)
+
+        raise ValueError(f"Invalid action statement: {self.action}")
+
+    def _validate_config(self):
+        provider_name = self.config.resolve("lexicon:provider_name")
+        if not self.config.resolve("lexicon:provider_name"):
+            raise AttributeError("provider_name")
+
+        try:
+            available = discovery.find_providers()[
+                self.config.resolve("lexicon:provider_name")
+            ]
+        except KeyError:
+            raise ProviderNotAvailableError(
+                f"This provider ({provider_name}) is not supported by Lexicon."
+            )
+        else:
+            if not available:
+                raise ProviderNotAvailableError(
+                    f"This provider ({provider_name}) has required dependencies that are missing. "
+                    f"Please install lexicon[{provider_name}] first."
+                )
+
+        if not self.config.resolve("lexicon:action"):
+            raise AttributeError("action")
+        if not self.config.resolve("lexicon:domain"):
+            raise AttributeError("domain")
+        if not self.config.resolve("lexicon:type"):
+            raise AttributeError("type")
+
+
+def _get_tldextract_cache_path():
+    if os.environ.get("TLDEXTRACT_CACHE_FILE"):
+        logging.warning(
+            "TLD_EXTRACT_CACHE_FILE environment variable is deprecated, please use TLDEXTRACT_CACHE_PATH instead."
+        )
+        os.environ["TLDEXTRACT_CACHE_PATH"] = os.environ["TLDEXTRACT_CACHE_FILE"]
+
+    return os.path.expanduser(
+        os.environ.get("TLDEXTRACT_CACHE_PATH", os.path.join("~", ".lexicon_tld_set"))
+    )
---
+++ packages/dns-lexicon/dns_lexicon-3.5.5-py3-none-any.whl/lexicon/providers/base.py
@@ -0,0 +1,209 @@
+"""Base provider module for all Lexicon providers"""
+from __future__ import absolute_import
+
+import warnings
+
+from lexicon.config import ConfigResolver, legacy_config_resolver
+
+
+class Provider(object):
+    """
+    This is the base class for all lexicon Providers.
+    It provides common functionality and ensures that all implemented
+    Providers follow a standard ducktype.
+    All standardized options will be provided here as defaults, but can be overwritten
+    by environmental variables and cli arguments.
+
+    Common options are:
+
+    action
+    domain
+    type
+    name
+    content
+    ttl
+    priority
+    identifier
+
+    The provider_env_cli_options will also contain any Provider specific options:
+
+    auth_username
+    auth_token
+    auth_password
+    ...
+
+    :param config: is a ConfigResolver object that contains all the options
+    for this provider, merged from CLI and Env variables.
+    """
+
+    def __init__(self, config):
+        if not isinstance(config, ConfigResolver):
+            # If config is a plain dict, we are in a legacy situation.
+            # To protect the Provider API, the legacy dict is handled in a
+            # correctly defined ConfigResolver.
+            # Also, there may be some situation where `provider` key is not set in the config.
+            # It should not happen when Lexicon is called from Client, as it will set itself
+            # this key. However there were no automated logic if the Provider is used directly.
+            # So we provide this logic here.
+            if not config.get("provider_name") and not config.get("provider"):
+                config[
+                    "provider_name"
+                ] = __name__  # Obviously we use the module name itself.
+            self.config = legacy_config_resolver(config)
+        else:
+            self.config = config
+
+        # Default ttl
+        self.config.with_dict({"ttl": 3600})
+
+        self.provider_name = self.config.resolve(
+            "lexicon:provider_name"
+        ) or self.config.resolve("lexicon:provider")
+        self.domain = self.config.resolve("lexicon:domain")
+        self.domain_id = None
+
+    # Provider API
+    def authenticate(self):
+        """
+        Authenticate against provider,
+        Make any requests required to get the domain's id for this provider,
+        so it can be used in subsequent calls.
+        Should throw an error if authentication fails for any reason,
+        of if the domain does not exist.
+        """
+        return self._authenticate()
+
+    def create_record(self, rtype=None, name=None, content=None, **kwargs):
+        """
+        Create record. If record already exists with the same content, do nothing.
+        """
+        if not rtype and kwargs.get("type"):
+            warnings.warn(
+                'Parameter "type" is deprecated, use "rtype" instead.',
+                DeprecationWarning,
+            )
+            rtype = kwargs.get("type")
+
+        return self._create_record(rtype, name, content)
+
+    def list_records(self, rtype=None, name=None, content=None, **kwargs):
+        """
+        List all records. Return an empty list if no records found
+        type, name and content are used to filter records.
+        If possible filter during the query, otherwise filter after response is received.
+        """
+
+        if not rtype and kwargs.get("type"):
+            warnings.warn(
+                'Parameter "type" is deprecated, use "rtype" instead.',
+                DeprecationWarning,
+            )
+            rtype = kwargs.get("type")
+
+        return self._list_records(rtype=rtype, name=name, content=content)
+
+    def update_record(self, identifier, rtype=None, name=None, content=None, **kwargs):
+        """
+        Update a record. Identifier must be specified.
+        """
+        if not rtype and kwargs.get("type"):
+            warnings.warn(
+                'Parameter "type" is deprecated, use "rtype" instead.',
+                DeprecationWarning,
+            )
+            rtype = kwargs.get("type")
+
+        return self._update_record(identifier, rtype=rtype, name=name, content=content)
+
+    def delete_record(
+        self, identifier=None, rtype=None, name=None, content=None, **kwargs
+    ):
+        """
+        Delete an existing record.
+        If record does not exist, do nothing.
+        If an identifier is specified, use it, otherwise do a lookup using type, name and content.
+        """
+        if not rtype and kwargs.get("type"):
+            warnings.warn(
+                'Parameter "type" is deprecated, use "rtype" instead.',
+                DeprecationWarning,
+            )
+            rtype = kwargs.get("type")
+
+        return self._delete_record(
+            identifier=identifier, rtype=rtype, name=name, content=content
+        )
+
+    # Internal abstract implementations
+    def _authenticate(self):
+        raise NotImplementedError("Providers must implement this!")
+
+    def _create_record(self, rtype, name, content):
+        raise NotImplementedError("Providers must implement this!")
+
+    def _list_records(self, rtype=None, name=None, content=None):
+        raise NotImplementedError("Providers must implement this!")
+
+    def _update_record(self, identifier, rtype=None, name=None, content=None):
+        raise NotImplementedError("Providers must implement this!")
+
+    def _delete_record(self, identifier=None, rtype=None, name=None, content=None):
+        raise NotImplementedError("Providers must implement this!")
+
+    # Helpers
+    def _request(self, action="GET", url="/", data=None, query_params=None):
+        raise NotImplementedError("Providers must implement this!")
+
+    # Helpers
+    def _get(self, url="/", query_params=None):
+        return self._request("GET", url, query_params=query_params)
+
+    def _post(self, url="/", data=None, query_params=None):
+        return self._request("POST", url, data=data, query_params=query_params)
+
+    def _put(self, url="/", data=None, query_params=None):
+        return self._request("PUT", url, data=data, query_params=query_params)
+
+    def _patch(self, url="/", data=None, query_params=None):
+        return self._request("PATCH", url, data=data, query_params=query_params)
+
+    def _delete(self, url="/", query_params=None):
+        return self._request("DELETE", url, query_params=query_params)
+
+    def _fqdn_name(self, record_name):
+        # strip trailing period from fqdn if present
+        record_name = record_name.rstrip(".")
+        # check if the record_name is fully specified
+        if not record_name.endswith(self.domain):
+            record_name = f"{record_name}.{self.domain}"
+        return f"{record_name}."  # return the fqdn name
+
+    def _full_name(self, record_name):
+        # strip trailing period from fqdn if present
+        record_name = record_name.rstrip(".")
+        # check if the record_name is fully specified
+        if not record_name.endswith(self.domain):
+            record_name = f"{record_name}.{self.domain}"
+        return record_name
+
+    def _relative_name(self, record_name):
+        # strip trailing period from fqdn if present
+        record_name = record_name.rstrip(".")
+        # check if the record_name is fully specified
+        if record_name.endswith(self.domain):
+            record_name = record_name[: -len(self.domain)]
+            record_name = record_name.rstrip(".")
+        return record_name
+
+    def _clean_TXT_record(self, record):
+        if record["type"] == "TXT":
+            # Some providers have quotes around the TXT records,
+            # so we're going to remove those extra quotes
+            record["content"] = record["content"][1:-1]
+        return record
+
+    def _get_lexicon_option(self, option):
+        return self.config.resolve(f"lexicon:{option}")
+
+    def _get_provider_option(self, option):
+        return self.config.resolve(f"lexicon:{self.provider_name}:{option}")
