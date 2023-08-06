af31b5defa96e6d23923b3249ce0d7fc61574472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:48 +0000

    Add package pyiron-base/0.2.6/pyiron_base-0.2.6.tar.gz

---
+++ packages/pyiron-base/pyiron_base-0.2.6.tar.gz/pyiron_base-0.2.6/pyiron_base/__init__.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+# API of the pyiron_base module - in alphabetical order 
+from pyiron_base.generic.factory import PyironFactory
+from pyiron_base.generic.hdfio import FileHDFio, ProjectHDFio
+from pyiron_base.generic.inputlist import InputList
+from pyiron_base.generic.parameters import GenericParameters
+from pyiron_base.generic.template import PyironObject
+from pyiron_base.generic.util import deprecate, deprecate_soon, ImportAlarm
+from pyiron_base.job.executable import Executable
+from pyiron_base.job.external import Notebook
+from pyiron_base.job.generic import GenericJob
+from pyiron_base.job.interactive import InteractiveBase
+from pyiron_base.job.interactivewrapper import InteractiveWrapper
+from pyiron_base.job.jobstatus import JobStatus, job_status_successful_lst, job_status_finished_lst, job_status_lst
+from pyiron_base.job.jobtype import JOB_CLASS_DICT, JobType, JobTypeChoice
+from pyiron_base.job.template import TemplateJob, PythonTemplateJob
+from pyiron_base.master.generic import GenericMaster, get_function_from_string
+from pyiron_base.master.list import ListMaster
+from pyiron_base.master.parallel import ParallelMaster, JobGenerator
+from pyiron_base.master.serial import SerialMasterBase
+from pyiron_base.master.flexible import FlexibleMaster
+from pyiron_base.project.generic import Project, Creator
+from pyiron_base.pyio.parser import Logstatus, extract_data_from_file
+from pyiron_base.server.queuestatus import validate_que_request
+from pyiron_base.settings.generic import Settings
+from pyiron_base.settings.install import install_dialog
+from pyiron_base.table.datamining import PyironTable, TableJob
+
+# optional API of the pyiron_base module
+try:
+    from pyiron_base.project.gui import ProjectGUI
+except (ImportError, TypeError, AttributeError):
+    pass
+
+# Internal init
+from ._version import get_versions
+from pyiron_base.generic.jedi import fix_ipython_autocomplete
+
+# Set version of pyiron_base
+__version__ = get_versions()["version"]
+del get_versions
+
+# Jedi fix
+fix_ipython_autocomplete()
---
+++ packages/pyiron-base/pyiron_base-0.2.6.tar.gz/pyiron_base-0.2.6/setup.py
@@ -0,0 +1,54 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='pyiron_base',
+    version=versioneer.get_version(),
+    description='pyiron_base - an integrated development environment (IDE) for computational science.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_base',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='janssen@mpie.de',
+    license='BSD',
+
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9'
+                ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'dill>=0.3.3',
+        'future>=0.18.2',
+        'gitpython>=3.1.14',
+        'h5io>=0.1.2',
+        'h5py>=3.1.0',
+        'numpy>=1.20.1',
+        'pandas>=1.2.3',
+        'pathlib2>=2.3.5',
+        'psutil>=5.8.0',
+        'pyfileindex>=0.0.6',
+        'pysqa>=0.0.15',
+        'sqlalchemy>=1.4.2',
+        'tables>=3.6.1',
+        'tqdm>=4.59.0'
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+
+    entry_points = {
+            "console_scripts": [
+                'pyiron=pyiron_base.cli:main'
+            ]
+    }
+    )
