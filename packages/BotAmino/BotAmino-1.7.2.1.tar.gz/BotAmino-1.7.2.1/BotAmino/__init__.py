ae9ad1d6b9a99489452ae8795ca057d4a215d38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:30 +0000

    Add package BotAmino/1.7.2.1/BotAmino-1.7.2.1.tar.gz

---
+++ packages/BotAmino/BotAmino-1.7.2.1.tar.gz/BotAmino-1.7.2.1/BotAmino/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'BotAmino'
+__author__ = 'ThePhoenix78'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-2021 ThePhoenix78'
+__version__ = '1.7.2.1'
+
+from .BotAmino import *
+
+print(f"version : {__version__}")
---
+++ packages/BotAmino/BotAmino-1.7.2.1.tar.gz/BotAmino-1.7.2.1/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name='BotAmino',
+    version='1.7.2.1',
+    url='https://github.com/ThePhoenix78/AminoBot',
+    # download_url = 'https://github.com/Slimakoi/Amino.py/tarball/master',
+    license='MIT',
+    author='ThePhoenix78',
+    author_email='thephoenix788@gmail.com',
+    description='A library to create Amino bots.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=[
+        'aminoapps',
+        'amino-py',
+        'amino',
+        'amino-bot',
+        'narvii',
+        'api',
+        'python',
+        'python3',
+        'python3.x',
+        'ThePhoenix78',
+        'AminoBot',
+        'BotAmino',
+        'botamino',
+        'aminobot'
+    ],
+    install_requires=[
+        'setuptools',
+        'requests',
+        'six',
+        'websocket-client',
+        'Amino.py',
+        'pathlib',
+        'string',
+        'unicodedata'
+    ],
+    setup_requires=[
+        'wheel'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages()
+    # python_requires='>=3.7',
+)
