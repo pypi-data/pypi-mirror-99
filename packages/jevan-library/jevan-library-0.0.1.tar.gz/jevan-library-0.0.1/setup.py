be325392889dcff2a0414328969de0d00c739e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:38 +0000

    Add package jevan-library/0.0.1/jevan-library-0.0.1.tar.gz

---
+++ packages/jevan-library/jevan-library-0.0.1.tar.gz/jevan-library-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jevan-library", # Replace with your own username
+    version="0.0.1",
+    include_package_data=True,
+    author="Jevan Solutions",
+    author_email="jevan.xtrm@gmail.com",
+    description="Core library for each project developed by Jevan Solutions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="", # https://github.com/pypa/sampleproject
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
