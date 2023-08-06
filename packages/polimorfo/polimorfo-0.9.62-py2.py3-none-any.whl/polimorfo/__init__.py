33185be3da9a56a1e6c482f353fbedbfbf92239b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:43 +0000

    Add package polimorfo/0.9.62/polimorfo-0.9.62-py2.py3-none-any.whl

---
+++ packages/polimorfo/polimorfo-0.9.62-py2.py3-none-any.whl/polimorfo/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for polimorfo."""
+
+__author__ = """Fabio Fumarola"""
+__email__ = "fabiofumarola@gmail.com"
+__version__ = "__version__ = '0.9.62'"
+__cache_dir__ = "~/.polimorfo"
