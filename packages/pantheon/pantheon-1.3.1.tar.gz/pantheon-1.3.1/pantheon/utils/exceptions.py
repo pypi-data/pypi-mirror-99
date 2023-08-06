192f8b126b78bbd9c6506d03d16f0cb9cd8d24f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:04 +0000

    Add package pantheon/1.3.1/pantheon-1.3.1.tar.gz

---
+++ packages/pantheon/pantheon-1.3.1.tar.gz/pantheon-1.3.1/pantheon/utils/exceptions.py
@@ -0,0 +1,46 @@
+class NotFound(Exception):
+    def __init__(self):
+        Exception.__init__(self,"404 Not Found")
+        
+        
+class ServerError(Exception):
+    def __init__(self):
+        Exception.__init__(self,"Server error, try again later")
+        
+
+class RateLimit(Exception):
+    def __init__(self, headers):
+        messageToAdd = ""
+        if "Retry-After" in headers:
+            self.timeToWait = headers["Retry-After"]
+            messageToAdd = ", Retry-After = "+str(self.timeToWait)
+        else:
+            self.timeToWait = 1
+        Exception.__init__(self,"Rate limit exceeded" + messageToAdd)
+        
+    def waitFor(self):
+        return int(self.timeToWait)
+
+
+class Forbidden(Exception):
+    def __init__(self):
+        Exception.__init__(self,"Access forbidden, your key may be expired or blacklisted. Check the dev portal : https://developer.riotgames.com/")
+
+
+class Unauthorized(Exception):
+    def __init__(self):
+        Exception.__init__(self,"Unauthorized, check your API key")
+
+
+class BadRequest(Exception):
+    def __init__(self):
+        Exception.__init__(self,"Bad request, the parameters you give might not be handled by the API. Check the API docs : https://developer.riotgames.com/api-methods/")
+
+
+class Timeout(Exception):
+    def __init__(self):
+        Exception.__init__(self,"Timeout reached, try again alter")
+        
+class InvalidServer(Exception):
+    def __init__(self, current_server, accepted_servers):
+        Exception.__init__(self,"Set server is {current_server}, this endpoint only supports {accepted_servers}" .format(current_server=current_server, accepted_servers=",".join(accepted_servers)))
---
+++ packages/pantheon/pantheon-1.3.1.tar.gz/pantheon-1.3.1/pantheon/utils/utils.py
@@ -0,0 +1,38 @@
+
+import time, datetime
+
+def getLimits(headers):
+    if 'X-Method-Rate-Limit' in headers and 'X-App-Rate-Limit' in headers:
+        appLimits = {}
+        for appLimit in headers['X-App-Rate-Limit'].split(","):
+            appLimits[int(appLimit.split(":")[1])] = int(appLimit.split(":")[0])
+
+        methodLimits = {}
+        for methodLimit in headers['X-Method-Rate-Limit'].split(","):
+            methodLimits[int(methodLimit.split(":")[1])] = int(methodLimit.split(":")[0])
+
+        return (appLimits,methodLimits)
+    return None
+
+def dateToTimestamp(date):
+    return int(time.mktime(datetime.datetime.strptime(date, '%a, %d %b %Y %H:%M:%S GMT').timetuple()))
+
+def getTimestamp(headers):
+    try:
+        timestamp = dateToTimestamp(headers['Date'])
+    except Exception as e:
+        timestamp = int(time.time())
+    return timestamp
+
+def urlParams(params):
+    if params is None:
+        return ""
+    else:
+        strParams = "?"
+        for i in params:
+            if type(params[i]) == list:
+                for p in params[i]:
+                    strParams+= i+"="+str(p)+"&"
+            else:
+                strParams+= i+"="+str(params[i])+"&"
+        return strParams[:-1]
