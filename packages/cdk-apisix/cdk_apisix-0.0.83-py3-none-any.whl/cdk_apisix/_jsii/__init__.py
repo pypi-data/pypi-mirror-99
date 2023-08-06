f9559187a721424b798394ccf41f53f6055d51ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:55 +0000

    Add package cdk-apisix/0.0.83/cdk_apisix-0.0.83-py3-none-any.whl

---
+++ packages/cdk-apisix/cdk_apisix-0.0.83-py3-none-any.whl/cdk_apisix/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_ecs._jsii
+import aws_cdk.aws_ecs_patterns._jsii
+import aws_cdk.aws_efs._jsii
+import aws_cdk.aws_elasticloadbalancingv2._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_logs._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-apisix", "0.0.83", __name__[0:-6], "cdk-apisix@0.0.83.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
