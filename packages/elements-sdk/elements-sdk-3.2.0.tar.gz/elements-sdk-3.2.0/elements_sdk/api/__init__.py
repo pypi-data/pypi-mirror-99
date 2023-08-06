ce0c4926c1c5779394d37015b10f646026cfbf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:53 +0000

    Add package elements-sdk/3.2.0/elements-sdk-3.2.0.tar.gz

---
+++ packages/elements-sdk/elements-sdk-3.2.0.tar.gz/elements-sdk-3.2.0/elements_sdk/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from elements_sdk.api.aws_api import AWSApi
+from elements_sdk.api.auth_api import AuthApi
+from elements_sdk.api.automation_api import AutomationApi
+from elements_sdk.api.integrations_api import IntegrationsApi
+from elements_sdk.api.main_api import MainApi
+from elements_sdk.api.media_library_api import MediaLibraryApi
+from elements_sdk.api.status_api import StatusApi
+from elements_sdk.api.storage_api import StorageApi
+from elements_sdk.api.tape_archive_api import TapeArchiveApi
+from elements_sdk.api.tasks_api import TasksApi
