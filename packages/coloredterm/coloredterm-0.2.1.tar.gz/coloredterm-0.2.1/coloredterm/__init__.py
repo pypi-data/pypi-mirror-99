9a22d1eba4975794fd7c0a867c3fdc957744db24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:18 +0000

    Add package coloredterm/0.2.1/coloredterm-0.2.1.tar.gz

---
+++ packages/coloredterm/coloredterm-0.2.1.tar.gz/coloredterm-0.2.1/coloredterm/__init__.py
@@ -0,0 +1,31 @@
+"""Collection of tools for changing the text of your terminal."""
+from coloredterm.coloredterm import (
+    Back,
+    bg,
+    colored,
+    colors,
+    cprint,
+    fg,
+    Fore,
+    names,
+    pattern_input,
+    pattern_print,
+    rand,
+    Style
+)
+
+__version__ = "0.1.9"
+__all__ = [
+    'Back',
+    'bg',
+    'colored',
+    'colors',
+    'cprint',
+    'fg',
+    'Fore',
+    'names',
+    'pattern_input',
+    'pattern_print',
+    'rand',
+    'Style'
+]
