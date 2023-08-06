84e2406c2a4776e3a714fb5a76107d1070a58965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:33 +0000

    Add package chaotools/0.0.4/chaotools-0.0.4.tar.gz

---
+++ packages/chaotools/chaotools-0.0.4.tar.gz/chaotools-0.0.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools.setup(
+    name="chaotools",
+    version="0.0.4",
+    author="chao",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
