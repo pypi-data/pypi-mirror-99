f163bafa0300643f9fa1136b2a06b735becee048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:03 +0000

    Add package open-space-toolkit-mathematics/0.4.1/open_space_toolkit_mathematics-0.4.1-py310-none-any.whl

---
+++ packages/open-space-toolkit-mathematics/open_space_toolkit_mathematics-0.4.1-py310-none-any.whl/ostk/mathematics/test/geometry/d3/transformations/rotations/test_rotation_matrix.py
@@ -0,0 +1,35 @@
+################################################################################################################################################################
+
+# @project        Open Space Toolkit ▸ Mathematics
+# @file           bindings/python/test/geometry/d3/transformations/rotations/test_rotation_matrix.py
+# @author         Lucas Brémond <lucas@loftorbital.com>
+# @license        Apache License 2.0
+
+################################################################################################################################################################
+
+from ostk.mathematics import geometry
+
+################################################################################################################################################################
+
+Angle = geometry.Angle
+Quaternion = geometry.d3.transformations.rotations.Quaternion
+RotationMatrix = geometry.d3.transformations.rotations.RotationMatrix
+
+################################################################################################################################################################
+
+# isDefined
+# getRowAt
+# getColumnAt
+# toTransposed
+# transpose
+# Undefined
+# Unit
+# RX
+# RY
+# RZ
+# Rows
+# Columns
+# Quaternion
+# RotationVector
+
+################################################################################################################################################################
