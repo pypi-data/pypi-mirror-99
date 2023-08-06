224d70a6819b036644c5bc93310924718554c3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:51 +0000

    Add package container2dict/0.0.1/container2dict-0.0.1-py3-none-any.whl

---
+++ packages/container2dict/container2dict-0.0.1-py3-none-any.whl/container2dict/container2dict.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+#############################################
+# File Name: container2dict.py
+# Author: derektmq
+# Mail: derektmq@gmail.com
+# Created Time:  2021-03-21 20:23:50 AM
+#############################################
+
+def container2dict(c1,c2):
+    return(dict(zip(c1,c2)))
+
+def help():
+    print("Bwaaah!")
