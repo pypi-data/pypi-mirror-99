c9ede2c2e933db1f6bcb54798917ef8313d46750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:45 +0000

    Add package iced-x86/1.11.0/iced_x86-1.11.0-cp36-abi3-win32.whl

---
+++ packages/iced-x86/iced_x86-1.11.0-cp36-abi3-win32.whl/iced_x86/__init__.py
@@ -0,0 +1,107 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2018-present iced project and contributors
+
+# pylint: disable=line-too-long
+# pylint: disable=no-name-in-module
+
+"""
+iced-x86 is a blazing fast and correct x86/x64 disassembler, assembler and instruction decoder written in Rust with Python bindings
+"""
+
+from ._iced_x86_py import BlockEncoder, ConstantOffsets, Decoder, Encoder, FastFormatter, \
+	Formatter, FpuStackIncrementInfo, Instruction, InstructionInfo, \
+	InstructionInfoFactory, MemoryOperand, MemorySizeExt, MemorySizeInfo, OpCodeInfo, \
+	RegisterExt, RegisterInfo, UsedMemory, UsedRegister
+
+from . import CC_a
+from . import CC_ae
+from . import CC_b
+from . import CC_be
+from . import CC_e
+from . import CC_g
+from . import CC_ge
+from . import CC_l
+from . import CC_le
+from . import CC_ne
+from . import CC_np
+from . import CC_p
+from . import Code
+from . import CodeSize
+from . import ConditionCode
+from . import CpuidFeature
+from . import DecoderError
+from . import DecoderOptions
+from . import EncodingKind
+from . import FlowControl
+from . import FormatMnemonicOptions
+from . import FormatterSyntax
+from . import MandatoryPrefix
+from . import MemorySize
+from . import MemorySizeOptions
+from . import Mnemonic
+from . import OpAccess
+from . import OpCodeOperandKind
+from . import OpCodeTableKind
+from . import OpKind
+from . import Register
+from . import RepPrefixKind
+from . import RflagsBits
+from . import RoundingControl
+from . import TupleType
+
+__all__ = [
+	"BlockEncoder",
+	"ConstantOffsets",
+	"Decoder",
+	"Encoder",
+	"FastFormatter",
+	"Formatter",
+	"FpuStackIncrementInfo",
+	"Instruction",
+	"InstructionInfo",
+	"InstructionInfoFactory",
+	"MemorySizeExt",
+	"MemorySizeInfo",
+	"OpCodeInfo",
+	"RegisterExt",
+	"RegisterInfo",
+	"UsedMemory",
+	"UsedRegister",
+
+	"CC_a",
+	"CC_ae",
+	"CC_b",
+	"CC_be",
+	"CC_e",
+	"CC_g",
+	"CC_ge",
+	"CC_l",
+	"CC_le",
+	"CC_ne",
+	"CC_np",
+	"CC_p",
+	"Code",
+	"CodeSize",
+	"ConditionCode",
+	"CpuidFeature",
+	"DecoderError",
+	"DecoderOptions",
+	"EncodingKind",
+	"FlowControl",
+	"FormatMnemonicOptions",
+	"FormatterSyntax",
+	"MandatoryPrefix",
+	"MemoryOperand",
+	"MemorySize",
+	"MemorySizeOptions",
+	"Mnemonic",
+	"OpAccess",
+	"OpCodeOperandKind",
+	"OpCodeTableKind",
+	"OpKind",
+	"Register",
+	"RepPrefixKind",
+	"RflagsBits",
+	"RoundingControl",
+	"TupleType",
+]
