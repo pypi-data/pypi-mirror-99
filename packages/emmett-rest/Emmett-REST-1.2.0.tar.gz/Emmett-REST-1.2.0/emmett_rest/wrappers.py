e99d8220ddcb4f32319c0bb3ca4f74b342e61e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:38 +0000

    Add package emmett-rest/1.2.0/Emmett-REST-1.2.0.tar.gz

---
+++ packages/emmett-rest/Emmett-REST-1.2.0.tar.gz/Emmett-REST-1.2.0/emmett_rest/wrappers.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+    emmett_rest.wrappers
+    --------------------
+
+    Provides wrappers for the REST extension
+
+    :copyright: 2017 Giovanni Barillari
+    :license: BSD-3-Clause
+"""
+
+from functools import wraps
+from typing import Any, Callable, List, Optional, Type, Union
+
+from emmett import App
+from emmett.extensions import Extension
+
+from .rest import AppModule, RESTModule
+from .typing import ModelType, ParserType, SerializerType
+
+
+def wrap_module_from_app(ext: Extension) -> Callable[..., RESTModule]:
+    def rest_module_from_app(
+        app: App,
+        import_name: str,
+        name: str,
+        model: ModelType,
+        serializer: Optional[SerializerType] = None,
+        parser: Optional[ParserType] = None,
+        enabled_methods: Optional[List[str]] = None,
+        disabled_methods: Optional[List[str]] = None,
+        list_envelope: Optional[str] = None,
+        single_envelope: Optional[Union[str, bool]] = None,
+        meta_envelope: Optional[str] = None,
+        groups_envelope: Optional[str] = None,
+        use_envelope_on_parse: Optional[bool] = None,
+        serialize_meta: Optional[bool] = None,
+        url_prefix: Optional[str] = None,
+        hostname: Optional[str] = None,
+        module_class: Optional[Type[RESTModule]] = None,
+        **kwargs: Any
+    ) -> RESTModule:
+        module_class = module_class or ext.config.default_module_class
+        return module_class.from_app(
+            ext,
+            import_name,
+            name,
+            model,
+            serializer=serializer,
+            parser=parser,
+            enabled_methods=enabled_methods,
+            disabled_methods=disabled_methods,
+            list_envelope=list_envelope,
+            single_envelope=single_envelope,
+            meta_envelope=meta_envelope,
+            groups_envelope=groups_envelope,
+            use_envelope_on_parse=use_envelope_on_parse,
+            serialize_meta=serialize_meta,
+            url_prefix=url_prefix,
+            hostname=hostname,
+            opts=kwargs
+        )
+    return rest_module_from_app
+
+
+def wrap_module_from_module(ext: Extension) -> Callable[..., RESTModule]:
+    def rest_module_from_module(
+        mod: AppModule,
+        import_name: str,
+        name: str,
+        model: ModelType,
+        serializer: Optional[SerializerType] = None,
+        parser: Optional[ParserType] = None,
+        enabled_methods: Optional[List[str]] = None,
+        disabled_methods: Optional[List[str]] = None,
+        list_envelope: Optional[str] = None,
+        single_envelope: Optional[Union[str, bool]] = None,
+        meta_envelope: Optional[str] = None,
+        groups_envelope: Optional[str] = None,
+        use_envelope_on_parse: Optional[bool] = None,
+        serialize_meta: Optional[bool] = None,
+        url_prefix: Optional[str] = None,
+        hostname: Optional[str] = None,
+        module_class: Optional[Type[RESTModule]] = None,
+        **kwargs: Any
+    ) -> RESTModule:
+        module_class = module_class or ext.config.default_module_class
+        return module_class.from_module(
+            ext,
+            mod,
+            import_name,
+            name,
+            model,
+            serializer=serializer,
+            parser=parser,
+            enabled_methods=enabled_methods,
+            disabled_methods=disabled_methods,
+            list_envelope=list_envelope,
+            single_envelope=single_envelope,
+            meta_envelope=meta_envelope,
+            groups_envelope=groups_envelope,
+            use_envelope_on_parse=use_envelope_on_parse,
+            serialize_meta=serialize_meta,
+            url_prefix=url_prefix,
+            hostname=hostname,
+            opts=kwargs
+        )
+    return rest_module_from_module
+
+
+def wrap_method_on_obj(method, obj):
+    @wraps(method)
+    def wrapped(*args, **kwargs):
+        return method(obj, *args, **kwargs)
+    return wrapped
---
+++ packages/emmett-rest/Emmett-REST-1.2.0.tar.gz/Emmett-REST-1.2.0/tests/conftest.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+
+from emmett import App, sdict
+from emmett.asgi.loops import loops
+from emmett.orm import Database
+from emmett.orm.migrations.utils import generate_runtime_migration
+from emmett.parsers import Parsers
+from emmett.serializers import Serializers
+from emmett_rest import REST
+
+
+@pytest.fixture(scope='session')
+def json_dump():
+    return Serializers.get_for('json')
+
+
+@pytest.fixture(scope='session')
+def json_load():
+    return Parsers.get_for('json')
+
+
+@pytest.yield_fixture(scope='session')
+def event_loop():
+    loop = loops.get('auto')
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope='session')
+def db_config():
+    config = sdict()
+    config.adapter = 'postgres:psycopg2'
+    config.host = os.environ.get('POSTGRES_HOST', 'localhost')
+    config.port = int(os.environ.get('POSTGRES_PORT', 5432))
+    config.user = os.environ.get('POSTGRES_USER', 'postgres')
+    config.password = os.environ.get('POSTGRES_PASSWORD', 'postgres')
+    config.database = os.environ.get('POSTGRES_DB', 'test')
+    return config
+
+
+@pytest.fixture(scope='session')
+def app(event_loop, db_config):
+    rv = App(__name__)
+    rv.config.db = db_config
+    rv.use_extension(REST)
+    return rv
+
+
+def _db_teardown_generator(db, migration):
+    def teardown():
+        with db.connection():
+            migration.down()
+    return teardown
+
+
+@pytest.fixture(scope='function')
+def raw_db(request, app):
+    rv = Database(app)
+    return rv
+
+
+@pytest.fixture(scope='function')
+def migration_db(request, app):
+    def generator(*models):
+        rv = Database(app)
+        rv.define_models(*models)
+        migration = generate_runtime_migration(rv)
+        with rv.connection():
+            migration.up()
+        request.addfinalizer(_db_teardown_generator(rv, migration))
+        return rv
+    return generator
