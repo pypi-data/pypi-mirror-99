9e2aa92045be63fa61d456e8c325c38cb47b4799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:54 +0000

    Add package invprob/0.0.14/invprob-0.0.14.tar.gz

---
+++ packages/invprob/invprob-0.0.14.tar.gz/invprob-0.0.14/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="invprob",
+    version="0.0.14",
+    author="Guillaume Garrigos",
+    author_email="guillaume.garrigos@lpsm.paris",
+    license="MIT",
+    description="A package containing useful functions for solving inverse problems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Guillaume-Garrigos/invprob/",
+    packages=["invprob", ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+            "numpy >= 1.9",
+            "matplotlib",
+            "scipy",
+            "scikit-image"
+    ],
+    include_package_data=True,
+)
