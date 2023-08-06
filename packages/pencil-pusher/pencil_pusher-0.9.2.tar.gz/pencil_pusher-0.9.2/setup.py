1d3a6ae4e3c0922be5dbfbce3fa6f1227444528b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:48 +0000

    Add package pencil-pusher/0.9.2/pencil_pusher-0.9.2.tar.gz

---
+++ packages/pencil-pusher/pencil_pusher-0.9.2.tar.gz/pencil_pusher-0.9.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as file:
+    readme = file.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+
+setup(
+    name="pencil_pusher",
+    version="0.9.2",
+    description="A python package that compiles source documentation and publishes it to the repo wiki",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Stephen Davis",
+    author_email="stephenedavis17@gmail.com",
+    packages=find_packages(),
+    url="https://github.com/stephend017/pencil-pusher",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires=required
+)
