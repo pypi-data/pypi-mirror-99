8cb74155dad0d2091348708d54e086f511ceca9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:13 +0000

    Add package Pixelworlds/0.1.4/Pixelworlds-0.1.4.tar.gz

---
+++ packages/Pixelworlds/Pixelworlds-0.1.4.tar.gz/Pixelworlds-0.1.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+import os
+
+def get_long_description():
+        
+        with open("README.md", encoding="utf-8") as f:
+                readme = f.read()
+
+        return readme
+
+setup(
+  name="Pixelworlds",
+  author="Zenqi",
+  description="🌎 A python wrapper for pixel worlds' data",
+  long_description=get_long_description(),
+  long_description_content_type='text/markdown',
+  url="https://github.com/zenqii/pixelworldsapi",
+  version=__import__("pixelworlds").__version__,
+  python_requires=">=3.5",
+  platforms=["Windows"],
+  packages=["pixelworlds"],
+  license="MIT",
+  keywords=["api", "pixelworlds"],
+  download_url="https://github.com/zenqii/pixelworldsapi/tarball/main",
+  zip_safe=False,
+  include_package_data=True,
+  install_requires = [
+	"bs4",
+	"requests"
+  ],
+  classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Education",
+		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Operating System :: Microsoft :: Windows",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Topic :: Software Development",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+	]
+)
