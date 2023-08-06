94f725b0681ecc15834a4fef2cbf7b06789e2941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:17 +0000

    Add package cbmos/0.1.1/cbmos-0.1.1.tar.gz

---
+++ packages/cbmos/cbmos-0.1.1.tar.gz/cbmos-0.1.1/cbmos/solvers/heun.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+import numpy as np
+from scipy.integrate._ivp.ivp import OdeResult
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+
+
+def solve_ivp(fun, t_span, y0, t_eval=None, dt=0.01):
+
+    t0, tf = float(t_span[0]), float(t_span[-1])
+
+    t = t0
+    y = y0
+
+    ts = [t]
+    ys = [y]
+
+    while t < tf:
+        y = y + dt/2.0*(fun(t, y) + fun(t + dt, y + dt*fun(t,y)))
+        t = t + dt
+
+        ts.append(t)
+        ys.append(y)
+
+    ts = np.hstack(ts)
+    ys = np.vstack(ys).T
+
+    return OdeResult(t=ts, y=ys)
+
+if __name__ == "__main__":
+
+    # stability region for Euler forward for this problem is h<2/50=0.04
+    @np.vectorize
+    def func(t,y):
+        return -50*y
+
+    t_span = (0,1)
+    y0 = 1
+
+    sol = solve_ivp(func, t_span, y0 )
+
+    plt.figure()
+    plt.plot(sol.t, sol.y.T)
+
+
+
