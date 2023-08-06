e7de254ffcf56f20ef920bb1dade2b1f3456acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:04 +0000

    Add package pomace/0.7a10/pomace-0.7a10.tar.gz

---
+++ packages/pomace/pomace-0.7a10.tar.gz/pomace-0.7a10/pomace/tests/conftest.py
@@ -0,0 +1,56 @@
+# pylint: disable=no-self-use,unused-argument
+
+"""Unit tests configuration file."""
+
+
+import datafiles
+import log
+import pytest
+
+from pomace import shared
+
+
+class MockElement(str):
+    @property
+    def outer_html(self):
+        return f"<mockhtml>{self}</>"
+
+    @property
+    def visible(self):
+        return True
+
+
+class MockLinks:
+    def find_by_partial_text(self, value):
+        return [MockElement(f"mockelement:links.partial_text={value}")]
+
+
+class MockBrowser:
+
+    url = "http://example.com"
+
+    html = "Hello, world!"
+
+    def find_by_name(self, value):
+        return [MockElement(f"mockelement:name={value}")]
+
+    links = MockLinks()
+
+
+@pytest.fixture
+def mockbrowser(monkeypatch):
+    browser = MockBrowser()
+    monkeypatch.setattr(shared, "browser", browser)
+    return browser
+
+
+def pytest_configure(config):
+    log.init(debug=True)
+    log.silence("faker", "vcr")
+
+    terminal = config.pluginmanager.getplugin("terminal")
+    terminal.TerminalReporter.showfspath = False
+
+
+def pytest_runtest_setup(item):
+    datafiles.settings.HOOKS_ENABLED = False
