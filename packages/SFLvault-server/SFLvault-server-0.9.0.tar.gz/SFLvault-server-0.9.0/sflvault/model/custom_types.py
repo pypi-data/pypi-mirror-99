3d84e4ddd17a451d71241bafa30c737352f64ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:49 +0000

    Add package SFLvault-server/0.9.0/SFLvault-server-0.9.0.tar.gz

---
+++ packages/SFLvault-server/SFLvault-server-0.9.0.tar.gz/SFLvault-server-0.9.0/sflvault/model/custom_types.py
@@ -0,0 +1,46 @@
+# -=- encoding: utf-8 -=-
+#
+# SFLvault - Secure networked password store and credentials manager.
+#
+# Copyright (C) 2008-2009  Savoir-faire Linux inc.
+#
+# Author: Alexandre Bourget <alexandre.bourget@savoirfairelinux.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+
+from sqlalchemy import types
+
+class JSONEncodedDict(types.TypeDecorator):
+    """Represents an mutable structure as a json-encoded string.
+
+    Usage::
+
+        JSONEncodedDict(255)
+
+    """
+
+    impl = types.Text
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        return json.loads(value) if value else {}
+
+    def copy_value(self, value):
+        return json.loads(simplejson.dumps(value))
