73c4126da48d0f4541dbc8d236a70c43156b3acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:13 +0000

    Add package peach-collector/1.0.2/peach_collector-1.0.2-py3-none-any.whl

---
+++ packages/peach-collector/peach_collector-1.0.2-py3-none-any.whl/peach_collector/_collector.py
@@ -0,0 +1,62 @@
+import logging
+from typing import Dict, List, Any
+
+import httpx
+
+from peach_collector import __version__
+from peach_collector._utils import timestamp
+
+logger = logging.getLogger(__name__)
+
+
+class PeachCollector:
+    def __init__(
+        self,
+        site_key: str,
+        app_id: str,
+        server_url: str = "https://pipe-collect.ebu.io/v3/collect",
+        enabled: bool = True,
+        debug: bool = True,
+    ):
+        self.site_key = site_key
+        self.app_id = app_id
+        self.server_url = server_url
+        self.enabled = enabled
+        self.debug = debug
+        self.events: List[Dict[str, Any]] = []
+
+    def add_event(self, event):
+        event = {**{"timestamp": timestamp()}, **event}
+        self.events.append(event)
+
+    async def send_events(self):
+        if not self.events:
+            return
+
+        assert self.server_url and self.site_key
+
+        payload = {
+            "peach_schema_version": "1.0.3",
+            "peach_implementation_version": __version__,
+            "sent_timestamp": timestamp(),
+            "client": {"type": "web", "app_id": self.app_id},
+            "events": self.events,
+        }
+
+        if self.debug:
+            logger.debug(payload)
+
+        if self.enabled:
+            logger.info(f"Sending {len(self.events)} events to peach")
+            async with httpx.AsyncClient() as client:
+                res = await client.post(self.server_url, params={"s": self.site_key}, json=payload)
+                res.raise_for_status()
+        else:
+            logger.info(
+                f"Peach collector is disabled. Would have sent {len(self.events)} events to peach"
+            )
+
+        self.events = []
+
+    def __call__(self):
+        self.send_events()
---
+++ packages/peach-collector/peach_collector-1.0.2-py3-none-any.whl/tests/test_queue.py
@@ -0,0 +1,104 @@
+import aioredis
+import orjson
+import pytest
+from aioredis import RedisError
+from aioredlock import LockError
+from httpx import HTTPError, Request
+from pytest_httpx import HTTPXMock, to_response
+
+from peach_collector import PeachCollector, PeachCollectorQueue
+
+
+@pytest.fixture()
+async def redis():
+    redis = await aioredis.create_redis_pool("redis://localhost")
+    await redis.flushall()
+    yield redis
+    redis.close()
+    await redis.wait_closed()
+
+
+@pytest.fixture()
+async def queue(redis):
+    collector = PeachCollector("a-site-key", "an-app-id")
+    q = PeachCollectorQueue("redis://localhost", collector, name="my_q", batch_size=2)
+    for i in range(1, 6):
+        await q.push_event({"foo": i})
+    yield q
+    await q.close_redis_conn()
+
+
+@pytest.mark.asyncio
+async def test_peach_collector_queue_normal(httpx_mock: HTTPXMock, redis, queue):
+    # Events are sent in batches
+    call_counter = 0
+
+    def response(request, *args, **kwargs):
+        nonlocal call_counter
+        batches = [[1, 2], [3, 4], [5]]
+        sent = orjson.loads(request.read())
+        assert [e["foo"] for e in sent["events"]] == batches[call_counter]
+        call_counter += 1
+        return to_response(json={"ok": 1})
+
+    httpx_mock.add_callback(response)
+
+    await queue.drain()
+    assert await redis.lrange("my_key", 0, -1) == []
+
+
+@pytest.mark.asyncio
+async def test_peach_collector_queue_peach_failures(httpx_mock: HTTPXMock, redis, queue):
+    # First batch get through to Peach but second blows up, so those and remaining events
+    # remain in queue
+    call_counter = 0
+
+    def response(request: Request, *args, **kwargs):
+        nonlocal call_counter
+        call_counter += 1
+        if call_counter == 2:
+            raise HTTPError(message="boom", request=request)
+        return to_response(json={"ok": 1})
+
+    httpx_mock.add_callback(response)
+
+    await queue.drain()
+    remaining = [orjson.loads(e) for e in await redis.lrange("my_q", 0, -1)]
+    assert [e["foo"] for e in remaining] == [3, 4, 5]
+
+
+@pytest.mark.asyncio
+async def test_peach_collector_queue_redis_lock(queue):
+    lock = await queue._acquire_lock()
+    another_q = PeachCollectorQueue("redis://localhost", queue.collector, name="my_q")
+    with pytest.raises(LockError):
+        await another_q._acquire_lock()
+    await another_q.close_redis_conn()
+    await queue._release_lock(lock)
+
+
+@pytest.mark.asyncio
+async def test_peach_collector_queue_redis_retries(httpx_mock: HTTPXMock, redis, queue):
+    # The first redis delete call fails but the retry succeeds
+    def response(request: Request, *args, **kwargs):
+        return to_response(json={"ok": 1})
+
+    httpx_mock.add_callback(response)
+
+    call_counter = 0
+
+    async def mock(*args, **kwargs):
+        nonlocal call_counter
+        call_counter += 1
+        if call_counter == 1:
+            raise RedisError("boom")
+        return await original_ltrim(*args, **kwargs)
+
+    original_ltrim = queue.conn.ltrim
+    queue.conn.ltrim = mock
+
+    await queue.drain()
+    assert call_counter == 2
+
+    remaining = [orjson.loads(e) for e in await redis.lrange("my_q", 0, -1)]
+    assert [e["foo"] for e in remaining] == []
