e1a15f1eb683e14200c1b4423bcbd434b9665ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:39 +0000

    Add package patta/0.0.2/patta-0.0.2-py3-none-any.whl

---
+++ packages/patta/patta-0.0.2-py3-none-any.whl/patta/__init__.py
@@ -0,0 +1,16 @@
+from .wrappers import (
+    SegmentationTTAWrapper,
+    ClassificationTTAWrapper,
+    KeypointsTTAWrapper
+)
+from .base import Compose
+
+from .transforms import (
+    HorizontalFlip, VerticalFlip, Rotate90, Scale, Add, Multiply, FiveCrops, Resize
+)
+
+from . import aliases
+
+from .load_model import load_model
+
+from .dataloader import SegDataLoader
