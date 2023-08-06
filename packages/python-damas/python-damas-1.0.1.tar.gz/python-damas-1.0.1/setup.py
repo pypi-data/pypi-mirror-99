fea6292d705af28c91ccdbe2dd3de8f032f02164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:53 +0000

    Add package python-damas/1.0.1/python-damas-1.0.1.tar.gz

---
+++ packages/python-damas/python-damas-1.0.1.tar.gz/python-damas-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='python-damas',
+    version='1.0.1',
+    author='Erick Ghuron',
+    author_email='ghuron@usp.br',
+    packages=['damas'],
+    description='Uma bilbioteca do jogo de damas.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ghurone/python-damas',
+    license='MIT',
+    keywords='damas jogo ghuron',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Games/Entertainment :: Board Games'
+    ]
+)
