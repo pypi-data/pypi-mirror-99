d14223808c60391d4dde314209d511b3b25c7fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:32 +0000

    Add package azure-functions/1.7.0/azure-functions-1.7.0.tar.gz

---
+++ packages/azure-functions/azure-functions-1.7.0.tar.gz/azure-functions-1.7.0/setup.py
@@ -0,0 +1,49 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from setuptools import setup
+from azure.functions import __version__
+
+with open("README.md") as readme:
+    long_description = readme.read()
+
+setup(
+    name='azure-functions',
+    version=__version__,
+    description='Azure Functions for Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Microsoft Corporation',
+    author_email='azpysdkhelp@microsoft.com',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS :: MacOS X',
+        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
+    ],
+    license='MIT',
+    packages=['azure.functions', 'azure.functions.extension'],
+    package_data={
+        'azure.functions': ['py.typed']
+    },
+    extras_require={
+        'dev': [
+            'flake8~=3.7.9',
+            'mypy',
+            'pytest',
+            'pytest-cov',
+            'requests==2.*',
+            'coverage'
+        ]
+    },
+    include_package_data=True,
+    test_suite='tests'
+)
