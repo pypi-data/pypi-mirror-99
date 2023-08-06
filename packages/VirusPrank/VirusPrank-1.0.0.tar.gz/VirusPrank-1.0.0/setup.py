3711c0fcf0aa18e4e8017acd2c14415df7180adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:55 +0000

    Add package VirusPrank/1.0.0/VirusPrank-1.0.0.tar.gz

---
+++ packages/VirusPrank/VirusPrank-1.0.0.tar.gz/VirusPrank-1.0.0/setup.py
@@ -0,0 +1,32 @@
+
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="VirusPrank",
+    version="1.0.0",
+    description="Harmless virus prank",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Dev Shah",
+    author_email="dgm82832@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["virus"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "VirusPrank=virus.__main__:main",
+        ]
+    },
+)
