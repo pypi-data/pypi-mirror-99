5b5572e31b2186dda0d41d920ed8eaf5d983f7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:29 +0000

    Add package jsonschema2ddl/0.2.0/jsonschema2ddl-0.2.0.tar.gz

---
+++ packages/jsonschema2ddl/jsonschema2ddl-0.2.0.tar.gz/jsonschema2ddl-0.2.0/src/jsonschema2ddl/types.py
@@ -0,0 +1,55 @@
+POSTGRES_TYPES = {
+    'boolean': 'bool',
+    'number': 'float',
+    'string': 'varchar({})',
+    'enum': 'text',
+    'integer': 'bigint',
+    'date-time': 'timestamp',
+    'timestamp': 'timestamp',
+    'timestampz': 'timestamptz',
+    'date': 'date',
+    'link': 'integer',
+    'array': 'json',
+    'object': 'json',
+    'id': 'serial',
+}
+
+REDSHIFT_TYPES = {
+    **POSTGRES_TYPES,
+    'array': 'varchar(1024)',
+    'object': 'varchar(1024)',
+    'id': 'int identity(1, 1) not null',
+}
+
+COLUMNS_TYPES = {
+    'postgres': POSTGRES_TYPES,
+    'redshift': REDSHIFT_TYPES,
+}
+
+FK_TYPES = {
+    'serial': 'bigint',
+    'int identity(1, 1) not null': 'bigint',
+}
+
+COLUMNS_TYPES_PREFERENCE = {
+    'null': -1,
+    'boolean': 0,
+    'bool': 0,
+    'enum': 1,
+    'link': 2,
+    'integer': 3,
+    'bigint': 4,
+    'number': 5,
+    'float': 5,
+    'date': 5,
+    'date-time': 6,
+    'timestamp': 6,
+    'timestamptz': 6,
+    'array': 7,
+    'text': 8,
+    'string': 8,
+    'object': 9,
+    'json': 9,
+    'id': 10,
+    'serial': 10,
+}
---
+++ packages/jsonschema2ddl/jsonschema2ddl-0.2.0.tar.gz/jsonschema2ddl-0.2.0/src/jsonschema2ddl/utils.py
@@ -0,0 +1,35 @@
+from typing import Dict, Tuple
+
+from change_case import ChangeCase
+from jsonschema import ValidationError
+
+from jsonschema2ddl.types import COLUMNS_TYPES_PREFERENCE
+
+
+def db_table_name(table_name: str, schema_name: str = None) -> str:
+    table_name = ChangeCase.camel_to_snake(table_name)
+    if schema_name is None:
+        return f'"{table_name}"'
+    else:
+        return f'"{schema_name}"."{table_name}"'
+
+
+def db_column_name(col_name: str) -> str:
+    return ChangeCase.camel_to_snake(col_name)
+
+
+def get_one_schema(object_schema) -> Dict:
+    """Get one schema from a list of 'allOf', 'anyOf', 'oneOf'. """
+    types = object_schema.get('allOf', object_schema.get('anyOf', object_schema.get('oneOf', [])))
+    if not types:
+        raise ValidationError("Neither 'type', 'allOf', 'anyOf', 'oneOf' defined for field. Aborting!")
+
+    def get_type_preference(type_definition: Dict) -> Tuple[Dict, float]:
+        schema_type: str = type_definition['type']
+        if 'format' in type_definition and type_definition['format'] in COLUMNS_TYPES_PREFERENCE:
+            schema_type: str = type_definition['format']
+        return type_definition, COLUMNS_TYPES_PREFERENCE[schema_type]
+
+    types_with_preference = map(get_type_preference, types)
+    max_preference_type = max(types_with_preference, key=lambda x: x[1])[0]
+    return max_preference_type
