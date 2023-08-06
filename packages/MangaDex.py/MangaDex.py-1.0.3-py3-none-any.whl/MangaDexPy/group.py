78f9d1c4ae709f1c9ead65c05719ff8003cf1005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:03 +0000

    Add package MangaDex.py/1.0.3/MangaDex.py-1.0.3-py3-none-any.whl

---
+++ packages/MangaDex.py/MangaDex.py-1.0.3-py3-none-any.whl/MangaDexPy/group.py
@@ -0,0 +1,35 @@
+from .partial import PartialChapter, PartialGroup, PartialUser
+
+
+class Group:
+    """Represents a MangaDex Group."""
+    __slots__ = ("id", "name", "names", "language", "leader", "members", "desc", "website", "discord", "irc_server",
+                 "irc_channel", "email", "founded_on", "follows", "views", "chapters", "thread_id", "thread_posts",
+                 "locked", "inactive", "delay", "last_update", "banner", "chapters_uploaded", "collabs")
+
+    def __init__(self, data, chaps=None, groups=None):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.names = data["altNames"]
+        self.language = data["language"]
+        self.leader = PartialUser(data["leader"])
+        self.members = [PartialUser(x) for x in data["members"] or []]
+        self.desc = data["description"]
+        self.website = data["website"]
+        self.discord = data["discord"]
+        self.irc_server = data["ircServer"]
+        self.irc_channel = data["ircChannel"]
+        self.email = data["email"]
+        self.founded_on = data["founded"]
+        self.follows = data["follows"]
+        self.views = data["views"]
+        self.chapters = data["chapters"]
+        self.thread_id = data["threadId"]
+        self.thread_posts = data["threadPosts"]
+        self.locked = data["isLocked"]
+        self.inactive = data["isInactive"]
+        self.delay = data["delay"]
+        self.last_update = data["lastUpdated"]
+        self.banner = data["banner"]
+        self.chapters_uploaded = [PartialChapter(x) for x in chaps or []]
+        self.collabs = [PartialGroup(x) for x in groups or []]
---
+++ packages/MangaDex.py/MangaDex.py-1.0.3-py3-none-any.whl/MangaDexPy/manga.py
@@ -0,0 +1,55 @@
+from .partial import PartialChapter, PartialGroup
+from .relation import Relation
+types = {0: None, 1: "Shounen", 2: "Seinen", 3: "Josei", 4: "Shoujo"}
+statuses = {0: None, 1: "ongoing", 2: "completed"}
+
+
+class Manga:
+    """Represents a MangaDex Manga."""
+    __slots__ = ("id", "title", "titles", "desc", "artist", "author", "language", "status", "type", "tag_ids",
+                 "last_chapter", "last_volume", "hentai", "links", "relations", "ratings", "views", "follows",
+                 "comments", "last_upload", "cover", "chapters", "groups", "session")
+
+    def __init__(self, data, session, chaps=None, groups=None):
+        self.id = data["id"]
+        self.title = data["title"]
+        self.titles = data["altTitles"]
+        self.desc = data["description"]
+        self.artist = data["artist"]
+        self.author = data["author"]
+        self.language = data["publication"]["language"]
+        self.status = statuses[data["publication"]["status"]]
+        self.type = types[data["publication"]["demographic"]]
+        self.tag_ids = data["tags"]
+        self.last_chapter = data["lastChapter"]
+        self.last_volume = data["lastVolume"]
+        self.hentai = data["isHentai"]
+        self.links = data["links"]
+        self.relations = [Relation(x) for x in data["relations"] or []]
+        self.ratings = data["rating"]
+        self.views = data["views"]
+        self.follows = data["follows"]
+        self.comments = data["comments"]
+        self.last_upload = data["lastUploaded"]
+        self.cover = data["mainCover"]
+        self.chapters = [PartialChapter(x) for x in chaps or []]
+        self.groups = [PartialGroup(x) for x in groups or []]
+        self.session = session
+
+    def get_tags(self):
+        tags = self.session.get("https://api.mangadex.org/v2/tag").json()["data"]
+        return [MangaTag(tags[x]) for x in tags if int(x) in self.tag_ids]
+
+    def get_covers(self):
+        return self.session.get(f"https://api.mangadex.org/v2/manga/{self.id}/covers").json()["data"]
+
+
+class MangaTag:
+    """Represents a MangaDex Manga Tag."""
+    __slots__ = ("id", "name", "type", "desc")
+
+    def __init__(self, data):
+        self.id = data["id"]
+        self.name = data["name"]
+        self.type = data["group"]
+        self.desc = data["description"]
