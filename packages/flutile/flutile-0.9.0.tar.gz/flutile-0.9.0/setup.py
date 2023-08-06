595ad142fb9fcb3c1750a272cc42dcb16a8db40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:59 +0000

    Add package flutile/0.9.0/flutile-0.9.0.tar.gz

---
+++ packages/flutile/flutile-0.9.0.tar.gz/flutile-0.9.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from flutile.version import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="flutile",
+    version=__version__,
+    description="sequence analysis tools for flu research",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/flu-crew/flutile",
+    author="Zebulun Arendsee",
+    author_email="zebulun.arendsee@usda.gov",
+    packages=["flutile"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={"console_scripts": ["flutile=flutile.ui:main"]},
+    py_modules=["flutile"],
+    zip_safe=False,
+    include_package_data=True,
+)
