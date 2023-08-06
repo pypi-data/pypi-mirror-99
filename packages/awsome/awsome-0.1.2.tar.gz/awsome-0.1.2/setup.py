bd63961b5da3e13430dc54a496d8146e92b42297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:31 +0000

    Add package awsome/0.1.2/awsome-0.1.2.tar.gz

---
+++ packages/awsome/awsome-0.1.2.tar.gz/awsome-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name='awsome',
+    packages=['awsome'],
+    version='0.1.2',
+    license='MIT',
+    description='aws utilities to make my life easier',
+    author='zdunker',
+    author_email='robinli.forwork@gmail.com',
+    url='https://github.com/zdunker/awsome',
+    download_url='https://github.com/zdunker/awsome/archive/refs/tags/0.1.2.tar.gz',
+    keywords=['aws', 'utility'],
+    install_requires=[
+        'boto3',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
