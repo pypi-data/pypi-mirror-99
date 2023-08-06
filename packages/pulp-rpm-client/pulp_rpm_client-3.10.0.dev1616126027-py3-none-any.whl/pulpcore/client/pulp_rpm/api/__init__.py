53e2f9297862dfab221c0b6915d85c51d4da6302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:18 +0000

    Add package pulp-rpm-client/3.10.0.dev1616126027/pulp_rpm_client-3.10.0.dev1616126027-py3-none-any.whl

---
+++ packages/pulp-rpm-client/pulp_rpm_client-3.10.0.dev1616126027-py3-none-any.whl/pulpcore/client/pulp_rpm/api/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_rpm.api.content_advisories_api import ContentAdvisoriesApi
+from pulpcore.client.pulp_rpm.api.content_distribution_trees_api import ContentDistributionTreesApi
+from pulpcore.client.pulp_rpm.api.content_modulemd_defaults_api import ContentModulemdDefaultsApi
+from pulpcore.client.pulp_rpm.api.content_modulemds_api import ContentModulemdsApi
+from pulpcore.client.pulp_rpm.api.content_packagecategories_api import ContentPackagecategoriesApi
+from pulpcore.client.pulp_rpm.api.content_packageenvironments_api import ContentPackageenvironmentsApi
+from pulpcore.client.pulp_rpm.api.content_packagegroups_api import ContentPackagegroupsApi
+from pulpcore.client.pulp_rpm.api.content_packagelangpacks_api import ContentPackagelangpacksApi
+from pulpcore.client.pulp_rpm.api.content_packages_api import ContentPackagesApi
+from pulpcore.client.pulp_rpm.api.content_repo_metadata_files_api import ContentRepoMetadataFilesApi
+from pulpcore.client.pulp_rpm.api.distributions_rpm_api import DistributionsRpmApi
+from pulpcore.client.pulp_rpm.api.publications_rpm_api import PublicationsRpmApi
+from pulpcore.client.pulp_rpm.api.remotes_rpm_api import RemotesRpmApi
+from pulpcore.client.pulp_rpm.api.repositories_rpm_api import RepositoriesRpmApi
+from pulpcore.client.pulp_rpm.api.repositories_rpm_versions_api import RepositoriesRpmVersionsApi
+from pulpcore.client.pulp_rpm.api.rpm_copy_api import RpmCopyApi
