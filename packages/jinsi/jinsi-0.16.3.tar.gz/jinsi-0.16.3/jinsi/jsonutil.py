e12c101aab52378491be188f926ee5edc266fd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:56 +0000

    Add package jinsi/0.16.3/jinsi-0.16.3.tar.gz

---
+++ packages/jinsi/jinsi-0.16.3.tar.gz/jinsi-0.16.3/jinsi/jsonutil.py
@@ -0,0 +1,171 @@
+import dataclasses
+import datetime
+import json
+from typing import Iterator, Any, Union
+
+from dezimal import Dezimal
+
+INFINITY = float('inf')
+
+
+class Decoder(json.JSONDecoder):
+    def __init__(self):
+        super().__init__(parse_int=Dezimal, parse_float=Dezimal)
+
+
+class Encoder(json.JSONEncoder):
+    def __init__(self, encode_dataclasses: bool = False, ultimate_fallback=None, **kwargs):
+        super().__init__(**kwargs)
+        self._encode_dataclasses = encode_dataclasses
+        self._ultimate_fallback = ultimate_fallback
+
+    # noinspection PyMethodMayBeStatic
+    def default_raw(self, o: Any) -> Union[str, type(None)]:
+        return None
+
+    def encode(self, o: Any) -> str:
+        pieces = self.iterencode(o)
+        return "".join(list(pieces))
+
+    def iterencode(self, obj: Any, **kwargs) -> Iterator[str]:
+
+        def floatstr(
+                o,
+                allow_nan=self.allow_nan,
+                # check for specials. this type of test is platform-specific, so do tests which don't depend internals.
+                _repr=repr,
+                _inf=INFINITY,
+                _neginf=-INFINITY
+        ):
+
+            if o != o:
+                text = 'NaN'
+            elif o == _inf:
+                text = 'Infinity'
+            elif o == _neginf:
+                text = '-Infinity'
+            else:
+                return _repr(o)
+            if not allow_nan:
+                raise ValueError(
+                    "Out of range float values are not JSON compliant: " +
+                    repr(o))
+            return text
+
+        return _make_iterencode(
+            encode_str=super().encode,
+            encode_int=int.__repr__,
+            encode_float=floatstr,
+            encode_dataclasses=self._encode_dataclasses,
+            encode_other_raw=self.default_raw,
+            encode_other=self.default,
+            ultimate_fallback=self._ultimate_fallback,
+        )(obj)
+
+
+# noinspection PyShadowingBuiltins
+def _make_iterencode(
+        encode_str,
+        encode_int,
+        encode_float,
+        encode_dataclasses,
+        encode_other_raw,
+        encode_other,
+        ultimate_fallback,
+        # HACK: hand-optimized bytecode; turn globals into locals
+        iter=iter,
+        next=next,
+        isinstance=isinstance,
+        str=str,
+        list=list,
+        int=int,
+        float=float,
+        dec=Dezimal,
+):
+    r = set()
+
+    def _iterencode_dict(o):
+        it = iter(o.items())
+        if not o:
+            yield '{}'
+            return
+        yield '{'
+        key, value = next(it)
+        yield encode_str(str(key))
+        yield ':'
+        yield from _iterencode(value)
+        for key, value in it:
+            yield ','
+            yield encode_str(str(key))
+            yield ':'
+            yield from _iterencode(value)
+        yield '}'
+
+    def _iterencode_list(o):
+        if not o:
+            yield '[]'
+            return
+        yield '['
+        it = iter(o)
+        item = next(it)
+        yield from _iterencode(item)
+        for item in it:
+            yield ','
+            yield from _iterencode(item)
+        yield ']'
+
+    def _iterencode(o):
+        if isinstance(o, str):
+            yield encode_str(o)
+        elif o is None:
+            yield 'null'
+        elif o is True:
+            yield 'true'
+        elif o is False:
+            yield 'false'
+        elif isinstance(o, list):
+            yield from _iterencode_list(o)
+        elif isinstance(o, dec):
+            yield str(o)
+        elif isinstance(o, int):
+            yield encode_int(o)
+        elif isinstance(o, float):
+            yield encode_float(o)
+        elif isinstance(o, (datetime.date, datetime.datetime)):
+            yield encode_str(str(o))
+        else:
+            try:
+                yield from _iterencode_dict(o)
+            except (TypeError, AttributeError):
+                if encode_dataclasses and dataclasses.is_dataclass(o):
+                    yield from _iterencode_dict(o.__dict__)
+                else:
+                    try:
+                        raw = encode_other_raw(o)
+                        if raw is not None:
+                            yield raw
+                        else:
+                            yield from _iterencode(encode_other(o))
+                    except TypeError as exc:
+                        if ultimate_fallback is not None:
+                            yield from _iterencode(ultimate_fallback(o))
+                        else:
+                            raise exc from None
+    return _iterencode
+
+
+def dumpjson(obj, **kwargs) -> str:
+    return json.dumps(obj, cls=Encoder, **kwargs)
+
+
+def loadjson(s):
+    return json.loads(s)
+
+
+def loadjson_all(s):
+    dec = Decoder()
+    data = s.strip()
+    while data:
+        obj, ix = dec.raw_decode(data)
+        yield obj
+        data = data[ix:].lstrip()
---
+++ packages/jinsi/jinsi-0.16.3.tar.gz/jinsi-0.16.3/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__pkginfo__ = {}
+with open("jinsi/__pkginfo__.py") as fh:
+    exec(fh.read(), __pkginfo__)
+
+
+class Info:
+    version = __pkginfo__.get("version", None)
+
+
+setuptools.setup(
+    name="jinsi",
+    version=Info.version,
+    author="Julian Fleischer",
+    author_email="tirednesscankill@warhog.net",
+    description="JSON/YAML homoiconic templating language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/scravy/jinsi",
+    scripts=[
+        "bin/jinsi"
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'awesome-pattern-matching>=0.19.0',
+        'dezimal>=0.4.0',
+        'pyyaml>=5.0.0',
+    ],
+)
