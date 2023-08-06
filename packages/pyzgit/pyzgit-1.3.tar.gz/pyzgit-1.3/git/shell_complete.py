c748eda345f94b0f6ccab224c872bb3a8f44959c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:31 +0000

    Add package pyzgit/1.3/pyzgit-1.3.tar.gz

---
+++ packages/pyzgit/pyzgit-1.3.tar.gz/pyzgit-1.3/git/shell_complete.py
@@ -0,0 +1,149 @@
+import os
+import re
+import typing
+
+from .gitoptions import (GIT_OPTIONS, is_branch_option, is_commit_option, is_conflict_option, is_fetch_option, is_index_option,
+                         is_log_option, is_merge_option, is_push_option, is_remote_option, is_stash_option, is_tag_option, is_tree_option)
+from .shared import run_shell, run_shell_with_resp, okey, warn, echo
+
+_HOME = os.environ['HOME']
+_DIR = _HOME + '/.config/.pyzgit'
+
+_TEMPLATE_ZSH = '''\
+#compdef g
+
+complete_g(){
+local curcontext="$curcontext" state line ret=1
+typeset -A opt_args
+
+_alternative\\
+  \'args:options arg:((\\
+%s
+  ))\'\\
+  'files:filename:_files'
+return ret
+}
+
+compdef _g g
+'''
+
+_TEMPLATE_BASH = '''\
+#!/usr/env bash
+
+_complete_g(){
+  if [[ "${COMP_CWORD}" == "1" ]];then
+    COMP_WORD="%s"
+    COMPREPLY=($(compgen -W "$COMP_WORD" -- ${COMP_WORDS[${COMP_CWORD}]}))
+  fi
+} 
+
+complete -F _complete_g g
+'''
+
+_re = re.compile(r"\/\.config\/\.pyzgit/([^\s]+)")
+
+
+def get_current_shell() -> str:
+    '''Gets the currently used shell'''
+    return run_shell_with_resp('echo $SHELL').split('/')[-1].strip()
+
+
+def ensure_config_path(file_name: str) -> str:
+    if not os.path.exists(_DIR):
+        try:
+            os.mkdir(_DIR)
+            return '{}/{}'.format(_DIR, file_name)
+        except Exception as e:
+            pass
+    else:
+        return '{}/{}'.format(_DIR, file_name)
+
+
+def generate_complete_script(template: str, fn: typing.Callable, name: str = '_g'):
+    complete_src = fn()
+    script_src = template % (complete_src)
+
+    with open('./%s' % (name), 'w') as f:
+        for line in script_src:
+            f.write(line)
+
+
+def using_completion(file_name: str, path: str, config_path: str):
+    '''Inject the load of completion script into the configuration of shell.
+    If it exists in the configuration, the injection will not be repeated.
+
+    Args:
+        file_name: generated completion script.
+        path: `pyzgit` configuration path.
+        config_path: shell configuration path.
+    '''
+    run_shell('mv {} {}'.format(file_name, _DIR))
+
+    with open(config_path) as f:
+        conf = f.read()
+        files = _re.findall(conf)
+
+    has_injected = False
+    if files:
+        for file in files:
+            if file == file_name:
+                has_injected = True
+
+    if not has_injected:
+        run_shell('echo "source %s" >> %s ' % (path, config_path))
+        okey('\nPlease run: source {}'.format(config_path))
+    else:
+        warn('\nThis configuration already exists.')
+
+
+def add_zsh_completion():
+    _name = '_g'
+    _path = ensure_config_path(_name)
+
+    def gen_completion():
+        avrs = []
+
+        _type = ['Branch', 'Commit', 'Conflict', 'Fetch', 'Index', 'Log',
+                 'Merge', 'Push', 'Remote', 'Stash', 'Tag', 'Working tree', 'Setting']
+
+        for k in GIT_OPTIONS.keys():
+            desc = GIT_OPTIONS[k]['help-msg']
+            if not desc:
+                desc = 'no description.'
+            avrs.append('    {}\\:\"{}\"\\\n'.format(k, desc))
+
+        return ('\n'.join(avrs)).rstrip()
+
+    generate_complete_script(_TEMPLATE_ZSH, gen_completion, _name)
+
+    using_completion(_name, _path, _HOME + '/.zshrc')
+
+
+def add_bash_completion():
+    _name = 'complete_script'
+    _path = ensure_config_path(_name)
+
+    def gen_completion():
+        return ' '.join(GIT_OPTIONS.keys())
+
+    generate_complete_script(_TEMPLATE_BASH, gen_completion, _name)
+
+    using_completion(_name, _path, _HOME + '/.bashrc')
+
+
+def add_completion():
+    echo('\nTry to add completion ...')
+
+    _shell = get_current_shell()
+    if _shell == 'zsh':
+        add_zsh_completion()
+    elif _shell == 'bash':
+        add_bash_completion()
+    else:
+        warn('Dont support completion of %s' % _shell)
+
+
+if __name__ == '__main__':
+    # generate_complete_script()
+    add_completion()
+    pass
---
+++ packages/pyzgit/pyzgit-1.3.tar.gz/pyzgit-1.3/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import sys
+
+PYTHON_VERSION = sys.version_info[:2]
+if PYTHON_VERSION < (3, 5):
+    print('Python version must be greater than or equal to 3.5')
+    sys.exit()
+
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    LONG_DESCRIPTION = ""
+
+VERSION = '1.3'
+
+setup(
+    name='pyzgit',
+    version=VERSION,
+    author="Zachary Zhang",
+    author_email="zlj1997122@outlook.com",
+    description="Simple terminal tool of Git.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zlj-zz/pyzgit.git",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+    entry_points='''
+        [console_scripts]
+        g=git.main:g
+    ''',
+    python_requires='>=3.5',
+)
