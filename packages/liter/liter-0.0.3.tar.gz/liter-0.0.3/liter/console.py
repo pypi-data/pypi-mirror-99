d8fec22bfe9299c965b067ac299718eb0eaf5499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:34 +0000

    Add package liter/0.0.3/liter-0.0.3.tar.gz

---
+++ packages/liter/liter-0.0.3.tar.gz/liter-0.0.3/liter/console.py
@@ -0,0 +1,17 @@
+import typer
+from pathlib import Path
+import subprocess
+from liter.changelog import generate_changelogs
+from liter.version import change_version
+app = typer.Typer()
+
+@app.command()
+def changelog(start_in: str = typer.Option(default=None),
+              last: bool = typer.Option(default=False)):
+    generate_changelogs(start_in, last)
+
+@app.command()
+def version(vtype: str = typer.Argument(default='patch'),
+            force: bool = typer.Option(default=False)):
+
+    change_version(vtype, force)
