b8212e5c22123c00a88b51c6e2e0f15ecfe82f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:26 +0000

    Add package journal-miner/1.2/journal-miner-1.2.tar.gz

---
+++ packages/journal-miner/journal-miner-1.2.tar.gz/journal-miner-1.2/README.md
@@ -0,0 +1,33 @@
+## Pyminer: A Python CLI for Mining Scientific Literature. 🔬⛏
+
+![](/assets/pyminer.gif)
+
+### Prerequisites:
+- Python >=3.8
+- Nodejs >=14.0
+
+### Quickstart:
+```
+pip install journal-miner
+```
+### Usage:
+```
+Usage: pyminer [OPTIONS] QUERY
+
+  Arguments:
+
+  QUERY The main search string.
+
+Options:
+  -l, --limit INTEGER   Number of papers to mine. Default = 1000
+  -kw, --keywords LIST  Keyword to mine.
+  --help                Show this message and exit.
+```
+e.g.
+```
+pyminer polymerase -kw inhibitor -kw rna -kw enzyme -l 100
+```
+### Output:
+A .csv will be created in the current working directory storing paper  
+with: Date, Title, Score, Keywords. Papers will ranked by their  
+score which is the sum of keyword appearances in the main text.
---
+++ packages/journal-miner/journal-miner-1.2.tar.gz/journal-miner-1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.2'
+DESCRIPTION = 'Python CLI for mining scientific literature.'
+LONG_DESCRIPTION = 'This package contains a CLI that allows mining of the EUPMC database for papers that contain hits for keywords.'
+
+# Setting up
+setup(
+        name="journal-miner",
+        version=VERSION,
+        author="George Pearse, James Sanders",
+        author_email="james.sanders1711@gmail.com",
+        url = 'https://github.com/J-E-J-S/pyminer',
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+            'beautifulsoup4==4.9.3',
+            'pandas==1.2.2',
+            'click==7.1.2'
+        ],
+        entry_points = {
+            'console_scripts':['pyminer=journalMiner.pyminer:cli']
+        }
+)
