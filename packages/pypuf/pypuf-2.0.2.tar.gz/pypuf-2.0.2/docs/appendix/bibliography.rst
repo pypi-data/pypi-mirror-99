9386fbc2ce3abb41d141a65daa388ab2ba85ac72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:22 +0000

    Add package pypuf/2.0.2/pypuf-2.0.2.tar.gz

---
+++ packages/pypuf/pypuf-2.0.2.tar.gz/pypuf-2.0.2/docs/appendix/bibliography.rst
@@ -0,0 +1,42 @@
+Bibliography
+============
+
+..
+  Using Zotero "export bibliography" feature to clipboard, using Nature style. Index labels are created manually.
+
+.. [Bec15] Becker, G. T. The Gap Between Promise and Reality: On the Insecurity of XOR Arbiter PUFs. in Cryptographic
+    Hardware and Embedded Systems -- CHES 2015 (eds. Güneysu, T. & Handschuh, H.) 535–555 (Springer Berlin Heidelberg,
+    2015).
+.. [CCLSR11] Chen, Q., Csaba, G., Lugli, P., Schlichtmann, U. & Ruhrmair, U. The Bistable Ring PUF: A new architecture
+    for strong Physical Unclonable Functions. in 2011 IEEE International Symposium on Hardware-Oriented Security and
+    Trust 134–141 (IEEE, 2011). doi:10.1109/HST.2011.5955011.
+.. [DV13] Delvaux, J. & Verbauwhede, I. Side channel modeling attacks on 65nm arbiter PUFs exploiting CMOS device noise.
+    in Hardware-Oriented Security and Trust (HOST), 2013 IEEE International Symposium on 137–142 (IEEE, 2013).
+.. [GCvDD02] Gassend, B., Clarke, D., van Dijk, M. & Devadas, S. Silicon Physical Random Functions. in Proceedings of
+    the 9th ACM Conference on Computer and Communications Security 148–160 (ACM, 2002). doi:10.1145/586110.586132.
+.. [GLCDD04] Gassend, B., Lim, D., Clarke, D., Dijk, M. van & Devadas, S. Identification and authentication of
+    integrated circuits. Concurrency and Computation: Practice and Experience 16, 1077–1098 (2004).
+.. [MKP08] Majzoobi, M., Koushanfar, F. & Potkonjak, M. Lightweight Secure PUFs. in Proceedings of the 2008 IEEE/ACM
+    International Conference on Computer-Aided Design 670–673 (IEEE Press, 2008).
+.. [NSJM19] Nguyen, P. H. et al. The Interpose PUF: Secure PUF Design against State-of-the-art Machine Learning Attacks.
+    IACR Transactions on Cryptographic Hardware and Embedded Systems 243–290 (2019) doi:10.13154/tches.v2019.i4.243-290.
+.. [RSSD10] Rührmair, U. et al. Modeling Attacks on Physical Unclonable Functions. in Proceedings of the 17th ACM
+    Conference on Computer and Communications Security 237–249 (ACM, 2010). doi:10.1145/1866307.1866335.
+.. [SD07] Suh, G. E. & Devadas, S. Physical Unclonable Functions for Device Authentication and Secret Key Generation.
+    in Proceedings of the 44th Annual Design Automation Conference 9–14 (ACM, 2007). doi:10.1145/1278480.1278484.
+.. [SH14] Schuster, D. & Hesselbarth, R. Evaluation of Bistable Ring PUFs Using Single Layer Neural Networks. in Trust
+    and Trustworthy Computing (eds. Holz, T. & Ioannidis, S.) 101–109 (Springer International Publishing, 2014).
+.. [TB15] Tobisch, J. & Becker, G. T. On the scaling of machine learning attacks on PUFs with application to noise
+    bifurcation. in International Workshop on Radio Frequency Identification: Security and Privacy Issues 17–31
+    (Springer, 2015).
+.. [WBMS19] Wisiol, N. et al. Breaking the Lightweight Secure PUF: Understanding the Relation of Input Transformations
+    and Machine Learning Resistance. https://eprint.iacr.org/2019/799 (2019).
+.. [WM19] Wisiol, N. & Margraf, M. Why attackers lose: design and security analysis of arbitrarily large XOR arbiter
+    PUFs. J Cryptogr Eng 9, 221–230 (2019).
+.. [WMPN19] Wisiol, N. et al. Splitting the Interpose PUF: A Novel Modeling Attack Strategy.
+    https://eprint.iacr.org/2019/1473 (2019).
+.. [WP20] Wisiol, N. & Pirnay, N. Short Paper: XOR Arbiter PUFs have Systematic Response Bias.
+    https://eprint.iacr.org/2019/1091 (2019).
+.. [XRHB15] Xu, X., Rührmair, U., Holcomb, D. E. & Burleson, W. Security Evaluation and Enhancement of Bistable Ring
+    PUFs. in Radio Frequency Identification (eds. Mangard, S. & Schaumont, P.) 3–16 (Springer International Publishing,
+    2015).
---
+++ packages/pypuf/pypuf-2.0.2.tar.gz/pypuf-2.0.2/pypuf/metrics/common.py
@@ -0,0 +1,241 @@
+from itertools import combinations
+from typing import List
+
+import numpy as np
+
+from ..io import random_inputs, ChallengeResponseSet
+from ..simulation import Simulation
+
+
+def reliability_data(responses: np.ndarray) -> np.ndarray:
+    r"""
+    Computes the reliability of response data of a PUF instance.
+
+    Given responses of a PUF instance in an array of shape :math:`(N, m, r)`, where
+    :math:`N` is the number of challenges queries,
+    :math:`m` is the response length of the PUF,
+    :math:`r` is the number of repeated queries for each challenge,
+    this function computes the empirical reliability of the PUF responses, i.e. an approximation of
+
+    .. math:: \Pr_\text{eval} \left[ \text{eval}(x) = \text{eval}(x) \right]
+
+    where :math:`\text{eval}(x)` is the response of the (noisy) PUF when evaluated on input :math:`x`, and the
+    probability is taken over the noise in the evaluation process.
+
+    An array of shape :math:`(N,m)` is returned, estimating above probability for each challenge and each response bit.
+    To obtain a the reliability for each response bit, average along the first axis, to obtain the general reliability,
+    average over all axes.
+
+    >>> from pypuf.metrics import reliability_data
+    >>> from numpy import average, array
+    >>> responses = array([[[1, 1, -1]], [[1, 1, 1]], [[1, 1, 1]], [[1, 1, 1]]])
+    >>> average(reliability_data(responses), axis=0)
+    array([0.83333333])
+    """
+    return np.absolute(np.average(responses, axis=-1))
+
+
+def reliability(instance: Simulation, seed: int, N: int = 10000, r: int = 17) -> np.ndarray:
+    r"""
+    For a given simulation, estimates simulated reliability.
+
+    Randomly generates :math:`N` challenges using the :math:`\mathtt{seed}`, then queries each challenge :math:`r`
+    times, obtaining a response array of shape :math:`(N, m, r)`, where :math:`m` is the response length of the given
+    simulation.
+
+    Then applies :func:`reliability_data` to determine the reliability of each respones bit on each challenge.
+
+    Returns a float-array of shape :math:`(N, m)`, giving the reliability of each response bit on each challenge.
+    To obtain the general reliability for each response bit, average along the first axis. To obtain the total
+    reliability of this instance, average across all axes. Note that bad reliability on single response bits may be
+    problematic.
+
+    >>> from pypuf.simulation import XORArbiterPUF
+    >>> from pypuf.metrics import reliability
+    >>> from numpy import average
+    >>> puf = XORArbiterPUF(n=128, k=2, seed=1, noisiness=.2)
+    >>> average(reliability(puf, seed=2), axis=0)
+    array([0.77729412])
+    """
+    inputs = random_inputs(n=instance.challenge_length, N=N, seed=seed)
+    return np.absolute(reliability_data(instance.r_eval(r, inputs)))
+
+
+def uniqueness_data(responses: np.ndarray) -> np.ndarray:
+    r"""
+    Estimates the uniqueness of responses of a set of PUF instances per challenge.
+
+    Given PUF responses in :math:`\{-1,1\}^m` in an array of shape :math:`(l, N, m)`, where
+    :math:`l \in \mathbb{N}` is the number of PUF instances,
+    :math:`N \in \mathbb{N}` is the number of challenges queried, and
+    :math:`m \in \mathbb{N}` is the response length,
+    returns an estimate of the uniqueness of PUFs on challenges in this set, i.e. an approximation of
+
+    .. math::
+        1 - 2 \cdot E_{f, g; f \neq g} \left[
+            \left|
+                \frac{1}{2} - \Pr_x \left[ f(x) = g(x) \right]
+            \right|
+        \right]
+
+    where the challenges :math:`x` are from the (implicitly) provided challenges, :math:`f(x)` and :math:`g(x)` are
+    the responses of PUFs :math:`f` and :math:`g` described in the provided data.
+
+    For perfectly unique PUFs and for any :math:`f` and :math:`g`, we expect
+    :math:`\Pr_x \left[ f(x) = g(x) \right] = 1/2`, hence the uniqueness to be 1.
+    On the other hand, if any two :math:`f` and :math:`g` have similarity higher (or lower) than :math:`1/2`, then
+    the uniqueness will be lower than 1.
+
+    Returns an array of shape :math:`(m,)` with uniqueness values per response bit.
+
+    >>> from numpy import random, array
+    >>> from pypuf.metrics import uniqueness_data, similarity_data
+    >>> prng = random.default_rng(seed=1)
+    >>> # generate **different** random responses using numpy's pseudo-random generator
+    >>> responses = array([2 * random.default_rng(seed).integers(0, 2, size=(1000, 3)) - 1 for seed in range(4)])
+    >>> uniqueness_data(responses)
+    array([0.97333333, 0.979     , 0.971     ])
+    >>> # generate **same** random responses using numpy's pseudo-random generator
+    >>> responses = array([2 * random.default_rng(1).integers(0, 2, size=(1000, 1)) - 1 for seed in range(4)])
+    >>> uniqueness_data(responses)
+    array([0.])
+    """
+    if len(responses.shape) == 2:
+        # TODO fix LTFArray to return responses of shape (N, 1)
+        l, N = responses.shape
+        responses = responses.reshape((l, N, 1))
+
+    l, _, m = responses.shape
+    similarities = np.empty(shape=(l * (l - 1) // 2, m))
+    for idx, (r1, r2) in enumerate(combinations(responses, 2)):
+        similarities[idx] = similarity_data(r1, r2)
+    res = 1 - 2 * np.average(np.absolute(.5 - similarities), axis=0)
+    return res
+
+
+def uniqueness(instances: List[Simulation], seed: int, N: int = 10000) -> np.ndarray:
+    r"""
+    Estimates the uniqueness of a list of given Simulations.
+
+    Randomly generates :math:`N` challenges using seed :math:`\texttt{seed}`, then queries each simulation
+    each challenge, obtaining an array with shape :math:`(l, N, m)` of all responses, where
+    :math:`l` is the number of simulations, and :math:`m` is the response length of the given simulations. (All
+    given simulations must have same challenge and response lenght.)
+
+    Then applies :func:`uniqueness_data` to determine the uniqueness of each response bit.
+
+    Returns a float-array of shape :math:`(m,)`, giving the uniqueness of each output bit.
+    To obtain total uniqueness, average along all axes, but be aware that low uniqueness on individual response bits
+    may be problematic.
+
+    >>> from numpy import average
+    >>> from pypuf.simulation import XORArbiterPUF
+    >>> from pypuf.metrics import uniqueness
+    >>> instances = [XORArbiterPUF(n=64, k=1, seed=seed) for seed in range(5)]
+    >>> uniqueness(instances, seed=31415, N=1000)
+    array([0.9272])
+    """
+    m = instances[0].response_length
+    challenges = random_inputs(instances[0].challenge_length, N, seed)
+    responses = np.empty(shape=(len(instances), N, m))
+    for i, instance in enumerate(instances):
+        responses[i] = instance.eval(challenges).reshape(N, m)  # TODO fixes incompatible LTFArray output format
+    return uniqueness_data(responses)
+
+
+def similarity_data(responses1: np.ndarray, responses2: np.ndarray) -> np.ndarray:
+    r"""
+    Given two arrays of responses of shape :math:`(N, m)`, returns the relative frequency of equal responses
+    in the arrays for each response bit, i.e. an approximation of
+
+    .. math:: \Pr_x \left[ f(x) = g(x) \right],
+
+    where :math:`f` and :math:`g` are the functions given by :math:`\mathtt{responses1}` and
+    :math:`\mathtt{responses2}`, respectively; :math:`x` are the challenges (given implicitly by ordering of the
+    response arrays).
+
+    If response length :math:`m` is greater than 1, the similarity is given for each bit seperately. To obtain the
+    general similarity, average the response.
+
+    Returns an array of shape :math:`(m,)`.
+
+    >>> from pypuf.metrics import similarity_data
+    >>> from numpy import array
+    >>> similarity_data(array([[1, 1], [1, 1], [1, 1], [1, 1]]), array([[1, 1], [1, 1], [1, 1], [-1, 1]]))
+    array([0.75, 1.  ])
+    """
+    # Check for response arrays with repetitions (shape (N, m, r))
+    # and majority vote along the repetition axis
+    if len(responses1.shape) == 3:
+        responses1 = np.sign(np.average(responses1, axis=-1))
+    if len(responses2.shape) == 3:
+        responses2 = np.sign(np.average(responses2, axis=-1))
+
+    # check for broken response arrays with implicit response length 1 (shape (N,))
+    # TODO fix LTFArray to return responses of shape (N, 1)
+    if len(responses1.shape) == 1:
+        responses1 = responses1.reshape(-1, 1)
+    if len(responses2.shape) == 1:
+        responses2 = responses2.reshape(-1, 1)
+
+    return 1 - np.average(np.absolute(responses1 - responses2), axis=0) / 2
+
+
+def accuracy(simulation: Simulation, test_set: ChallengeResponseSet) -> np.ndarray:
+    r"""
+    Given a simulation and a test set, returns the relative frequency of responses by the simulation that match
+    responses recorded in the test set by response bit, i.e. an approximation of
+
+    .. math:: \Pr_x \left[ f(x) = g(x) \right],
+
+    where :math:`f` is given by the provided simulation and :math:`g` is defined by the :math:`\mathtt{test\_set}`;
+    :math:`x` is from the set of challenges given in the :math:`\mathtt{test\_set}`.
+
+    If response length is greater than 1, the similarity is given for each bit seperately. To obtain the
+    general similarity, average the response.
+
+    Returns an array of shape :math:`(m,)`.
+
+    >>> from pypuf.simulation import XORArbiterPUF
+    >>> from pypuf.io import ChallengeResponseSet
+    >>> from pypuf.metrics import accuracy
+    >>> puf = XORArbiterPUF(n=128, k=4, noisiness=.1, seed=1)
+    >>> test_set = ChallengeResponseSet.from_simulation(puf, N=1000, seed=2)
+    >>> accuracy(puf, test_set)
+    array([0.823])
+    >>> puf = XORArbiterPUF(n=64, k=4, noisiness=.3, seed=2)
+    >>> test_set = ChallengeResponseSet.from_simulation(puf, N=1000, seed=2, r=5)
+    >>> accuracy(puf, test_set)
+    array([0.706])
+    """
+    sim_responses = simulation.eval(test_set.challenges)
+    return similarity_data(sim_responses, test_set.responses)
+
+
+def similarity(instance1: Simulation, instance2: Simulation, seed: int, N: int = 1000) -> np.ndarray:
+    r"""
+    Approximate the similarity in response behavior of two simulations with identical challenge and response length,
+    i.e. an approximation of
+
+    .. math:: \Pr_x \left[ f(x) = g(x) \right],
+
+    where :math:`f` and :math:`g` are given by the provided simulations, and the :math:`N` challenges :math:`x` are generated
+    randomly using the provided :math:`\mathtt{seed}`.
+
+    If response length is greater than 1, the similarity is given for each bit seperately. To obtain the
+    general similarity, average the response.
+
+    Returns an array of shape :math:`(m,)`.
+
+    >>> from pypuf.simulation import XORArbiterPUF
+    >>> from pypuf.metrics import similarity
+    >>> similarity(XORArbiterPUF(n=128, k=4, seed=1), XORArbiterPUF(n=128, k=4, seed=1), seed=31415)  # same seed
+    array([1.])
+    >>> similarity(XORArbiterPUF(n=128, k=4, seed=1), XORArbiterPUF(n=128, k=4, seed=2), seed=31415)  # different seed
+    array([0.515])
+    """
+    if instance1.challenge_length != instance2.challenge_length:
+        raise ValueError(f'Cannot compare instances with different challenge spaces of dimension '
+                         f'{instance1.challenge_length} and {instance2.challenge_length}, respectively.')
+    inputs = random_inputs(n=instance1.challenge_length, N=N, seed=seed)
+    return similarity_data(instance1.eval(inputs), instance2.eval(inputs))
