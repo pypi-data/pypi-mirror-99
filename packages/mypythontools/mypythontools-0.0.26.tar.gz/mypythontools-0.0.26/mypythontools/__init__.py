b4d733c2430bdf29ed5fe120abe22a8c45177f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:58 +0000

    Add package mypythontools/0.0.26/mypythontools-0.0.26.tar.gz

---
+++ packages/mypythontools/mypythontools-0.0.26.tar.gz/mypythontools-0.0.26/mypythontools/__init__.py
@@ -0,0 +1,31 @@
+"""
+mypythontools
+=============
+
+Some tools/functions/snippets used across projects.
+
+Usually used from IDE. Used paths are infered and things like building the
+application with pyinstaller incrementing version, pushing to github or deploying
+to Pypi is matter of calling one function, or clicking one button (e.g. Vs code task).
+
+Many projects - one codebase.
+
+If you are not sure whether structure of app that will work with this code, there is python starter repo
+on [github](https://github.com/Malachov/my-python-starter)
+
+Paths are infered, but if you have atypical structure or have more projects in cwd, use `mypythontools.misc.set_paths()`.
+"""
+
+from . import utils
+from . import build
+from . import deploy
+from . import misc
+from . import pyvueeel
+
+__version__ = "0.0.26"
+
+__author__ = "Daniel Malachov"
+__license__ = "MIT"
+__email__ = "malachovd@seznam.cz"
+
+__all__ = ['utils', 'build', 'deploy', 'misc', 'pyvueeel']
