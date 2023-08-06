7e196f837d21fd8976cf8a9141a537487e154aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:19 +0000

    Add package codefast/0.0.1a1/codefast-0.0.1a1-py3-none-any.whl

---
+++ packages/codefast/codefast-0.0.1a1-py3-none-any.whl/codefast/__init__.py
@@ -0,0 +1,18 @@
+import .utils as utils
+
+# Export methods and variables
+client = utils.client
+debug = utils.debug
+download = utils.download
+error = utils.error
+info = utils.info
+jsonread = utils.jsonread
+jsonwrite = utils.jsonwrite
+p = utils.p
+pp = utils.pp
+shell = utils.shell
+textread = utils.textread
+textwrite = utils.textwrite
+warning = utils.warning
+
+
