9dee04c4751daa78d1db8f492b2b0b3e9fafd2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:52 +0000

    Add package sumapi/0.1.3/sumapi-0.1.3-py3-none-any.whl

---
+++ packages/sumapi/sumapi-0.1.3-py3-none-any.whl/sumapi/config.py
@@ -0,0 +1,13 @@
+
+BASE_URL = 'http://api.summarify.io'
+URL = {
+        'tokenURL': f'{BASE_URL}/token',
+        'sentimentURL': f'{BASE_URL}/sentiment-analysis',
+        'nerURL': f'{BASE_URL}/ner',
+        'classificationURL': f'{BASE_URL}/classification',
+        'zeroshotURL': f'{BASE_URL}/zero-shot',
+        'questionURL': f'{BASE_URL}/qa',
+        'multirequestURL': f'{BASE_URL}/arguments',
+        'summarizationURL': f'{BASE_URL}/summarize',
+        'spellCheckURL': f'{BASE_URL}/spell-check'
+}
