3b824033ed4478edf655c875802bd3df6bf50ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:09 +0000

    Add package pkm-python-client/0.1.4/pkm-python-client-0.1.4.tar.gz

---
+++ packages/pkm-python-client/pkm-python-client-0.1.4.tar.gz/pkm-python-client-0.1.4/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pkm_python_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests']
+
+setup_kwargs = {
+    'name': 'pkm-python-client',
+    'version': '0.1.4',
+    'description': "A python client for the Decoder project's PKM server",
+    'long_description': None,
+    'author': 'Gaël de Chalendar',
+    'author_email': 'gael.de-chalendar@cea.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+}
+
+
+setup(**setup_kwargs)
