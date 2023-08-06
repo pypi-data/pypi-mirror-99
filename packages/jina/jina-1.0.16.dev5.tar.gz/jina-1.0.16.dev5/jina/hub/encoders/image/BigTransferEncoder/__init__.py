4b7cecf791e0c29397aebf4da0352cfcd8097ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:51 +0000

    Add package jina/1.0.16.dev5/jina-1.0.16.dev5.tar.gz

---
+++ packages/jina/jina-1.0.16.dev5.tar.gz/jina-1.0.16.dev5/jina/hub/encoders/image/BigTransferEncoder/__init__.py
@@ -0,0 +1,86 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+import os
+from typing import Optional
+
+import numpy as np
+
+from jina.executors.decorators import batching, as_ndarray
+from jina.executors.encoders.frameworks import BaseTFEncoder
+from jina.excepts import PretrainedModelFileDoesNotExist
+
+
+class BigTransferEncoder(BaseTFEncoder):
+    """
+    :class:`BigTransferEncoder` is Big Transfer (BiT) presented by
+    Google (https://github.com/google-research/big_transfer).
+    Uses pretrained BiT to encode data from a ndarray, potentially
+    B x (Channel x Height x Width) into a ndarray of `B x D`.
+    Internally, :class:`BigTransferEncoder` wraps the models from
+    https://storage.googleapis.com/bit_models/.
+
+    .. warning::
+
+        Known issue: this does not work on tensorflow==2.2.0,
+        https://github.com/tensorflow/tensorflow/issues/38571
+
+    :param model_path: the path of the model in the `SavedModel` format.
+        The pretrained model can be downloaded at
+        wget https://storage.googleapis.com/bit_models/Imagenet21k/[model_name]/feature_vectors/saved_model.pb
+        wget https://storage.googleapis.com/bit_models/Imagenet21k/[model_name]/feature_vectors/variables/variables.data-00000-of-00001
+        wget https://storage.googleapis.com/bit_models/Imagenet21k/[model_name]/feature_vectors/variables/variables.index
+
+        ``[model_name]`` includes `R50x1`, `R101x1`, `R50x3`, `R101x3`, `R152x4`
+
+        The `model_path` should be a directory path, which has the following structure.
+
+        .. highlight:: bash
+         .. code-block:: bash
+
+            .
+            ├── saved_model.pb
+            └── variables
+                ├── variables.data-00000-of-00001
+                └── variables.index
+
+        :param channel_axis: the axis id of the channel, -1 indicate the color
+            channel info at the last axis. If given other, then `
+            `np.moveaxis(data, channel_axis, -1)`` is performed before :meth:`encode`.
+    """
+
+    def __init__(self,
+                 model_path: Optional[str] = '/workspace/pretrained',
+                 channel_axis: int = 1,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.channel_axis = channel_axis
+        self.model_path = model_path
+
+    def post_init(self):
+        """Load model. Raise exception if model doesn't exist"""
+        super().post_init()
+        if self.model_path and os.path.exists(self.model_path):
+            self.to_device()
+            import tensorflow as tf
+            self.logger.info(f'model_path: {self.model_path}')
+            _model = tf.saved_model.load(self.model_path)
+            self.model = _model.signatures['serving_default']
+            self._get_input = tf.convert_to_tensor
+        else:
+            raise PretrainedModelFileDoesNotExist(f'model at {self.model_path} does not exist')
+
+    @batching
+    @as_ndarray
+    def encode(self, data: 'np.ndarray', *args, **kwargs) -> 'np.ndarray':
+        """
+        Encode data into a ndarray of `B x D`.
+        Where `B` is the batch size and `D` is the Dimension.
+
+        :param data: an array in size `B`
+        :return: an ndarray in size `B x D`.
+        """
+        if self.channel_axis != -1:
+            data = np.moveaxis(data, self.channel_axis, -1)
+        _output = self.model(self._get_input(data.astype(np.float32)))
+        return _output['output_1'].numpy()
---
+++ packages/jina/jina-1.0.16.dev5.tar.gz/jina-1.0.16.dev5/jina/optimizers/parameters.py
@@ -0,0 +1,113 @@
+from typing import Optional, Sequence, Union
+
+from ..jaml import JAML, JAMLCompatible
+
+
+class OptimizationParameter(JAMLCompatible):
+    """Base class for all optimization parameters."""
+
+    def __init__(
+        self,
+        parameter_name: str = "",
+        executor_name: Optional[str] = None,
+        prefix: str = 'JINA',
+        jaml_variable: Optional[str] = None,
+    ):
+        self.parameter_name = parameter_name
+        if jaml_variable is None:
+            self.jaml_variable = f'{prefix}_{executor_name}_{parameter_name}'.upper()
+        else:
+            self.jaml_variable = jaml_variable
+
+
+class IntegerParameter(OptimizationParameter):
+    """
+    Used for optimizing integer parameters with the FlowOptimizer.
+    For detailed information about sampling and usage see
+    https://optuna.readthedocs.io/en/stable/reference/generated/optuna.trial.Trial.html#optuna.trial.Trial.suggest_int
+    """
+
+    def __init__(
+        self,
+        low: int,
+        high: int,
+        step_size: int = 1,
+        log: bool = False,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.low = low
+        self.high = high
+        if log and step_size != 1:
+            raise ValueError(
+                '''The step_size != 1 and log arguments cannot be used at the same time. When setting log argument to True, set the step argument to 1.'''
+            )
+
+        self.step_size = step_size
+        self.log = log
+
+
+class UniformParameter(OptimizationParameter):
+    """
+    Used for optimizing float parameters with the FlowOptimizer with uniform sampling.
+    For detailed information about sampling and usage see
+    https://optuna.readthedocs.io/en/stable/reference/generated/optuna.trial.Trial.html#optuna.trial.Trial.suggest_discrete_uniform
+    """
+
+    def __init__(self, low: float, high: float, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.low = low
+        self.high = high
+
+
+class LogUniformParameter(OptimizationParameter):
+    """
+    Used for optimizing float parameters with the FlowOptimizer with loguniform sampling.
+    For detailed information about sampling and usage see
+    https://optuna.readthedocs.io/en/stable/reference/generated/optuna.trial.Trial.html#optuna.trial.Trial.suggest_loguniform
+    """
+
+    def __init__(self, low: float, high: float, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.low = low
+        self.high = high
+
+
+class CategoricalParameter(OptimizationParameter):
+    """
+    Used for optimizing categorical parameters with the FlowOptimizer.
+    For detailed information about sampling and usage see
+    https://optuna.readthedocs.io/en/stable/reference/generated/optuna.trial.Trial.html#optuna.trial.Trial.suggest_categorical
+    """
+
+    def __init__(
+        self, choices: Sequence[Union[None, bool, int, float, str]], *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.choices = choices
+
+
+class DiscreteUniformParameter(OptimizationParameter):
+    """
+    Used for optimizing discrete parameters with the FlowOptimizer with uniform sampling.
+    For detailed information about sampling and usage it is used by Jina with optuna see
+    https://optuna.readthedocs.io/en/stable/reference/generated/optuna.trial.Trial.html#optuna.trial.Trial.suggest_discrete_uniform
+    """
+
+    def __init__(self, low: float, high: float, q: float, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.low = low
+        self.high = high
+        self.q = q
+
+
+def load_optimization_parameters(filepath: str):
+    """
+    Loads optimization parameters from a `.yml` file and parses it with the JAML parser.
+    :param filepath: Path to a file that contains optimization parameters.
+    :return:: The loaded :class:`OptimizationParameter` objects.
+    """
+
+    with open(filepath, encoding='utf8') as fp:
+        return JAML.load(fp)
