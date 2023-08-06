c3731494bc801f09b2e2385758b8833ab099e06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:31 +0000

    Add package perceptilabs/0.11.11/perceptilabs-0.11.11-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/perceptilabs/perceptilabs-0.11.11-cp36-cp36m-manylinux1_x86_64.whl/perceptilabs/messaging/__init__.py
@@ -0,0 +1,3 @@
+from perceptilabs.messaging.base import MessageBus, MessageProducer, MessageConsumer, MessagingFactory
+from perceptilabs.messaging.simple import SimpleMessagingFactory
+from perceptilabs.messaging.zmq_wrapper import ZmqMessagingFactory
---
+++ packages/perceptilabs/perceptilabs-0.11.11-cp36-cp36m-manylinux1_x86_64.whl/perceptilabs/trainer/stats/__init__.py
@@ -0,0 +1,4 @@
+from perceptilabs.trainer.stats.sample import SampleStatsTracker, SampleStats
+from perceptilabs.trainer.stats.gradients import GradientStatsTracker, GradientStats
+from perceptilabs.trainer.stats.categorical import CategoricalOutputStatsTracker, CategoricalOutputStats
+
