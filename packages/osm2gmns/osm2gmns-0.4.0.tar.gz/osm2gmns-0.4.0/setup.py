f623ba4033f24eaca5819c14eae567fa83aaa25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:34 +0000

    Add package osm2gmns/0.4.0/osm2gmns-0.4.0.tar.gz

---
+++ packages/osm2gmns/osm2gmns-0.4.0.tar.gz/osm2gmns-0.4.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='osm2gmns',
+    version='0.4.0',
+    author='Jiawei Lu, Xuesong Zhou',
+    author_email='jiaweil9@asu.edu, xzhou74@asu.edu',
+    url='https://github.com/jiawei92/OSM2GMNS',
+    description='convert map data from OpenStreetMap to network files in GMNS format',
+    long_description=open('README_pypi.rst').read(),
+    license='GPLv3+',
+    packages=['osm2gmns'],
+    python_requires=">=3.6.0",
+    install_requires=['pandas >= 0.24.0','shapely >= 1.7','protobuf >= 3.14.0'],
+    classifiers=['License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                 'Programming Language :: Python :: 3']
+)
