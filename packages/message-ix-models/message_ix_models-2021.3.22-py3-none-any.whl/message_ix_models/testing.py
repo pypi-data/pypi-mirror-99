60cdf1ad5816a3c291dcfeb218be097ea8c004c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package message-ix-models/2021.3.22/message_ix_models-2021.3.22-py3-none-any.whl

---
+++ packages/message-ix-models/message_ix_models-2021.3.22-py3-none-any.whl/message_ix_models/testing.py
@@ -0,0 +1,154 @@
+import logging
+from copy import deepcopy
+from pathlib import Path
+
+import message_ix
+import pytest
+from click.testing import CliRunner
+from ixmp import Platform
+from ixmp import config as ixmp_config
+
+from message_ix_models import cli
+from message_ix_models.util.context import Context
+
+log = logging.getLogger(__name__)
+
+# pytest hooks
+
+
+def pytest_addoption(parser):
+    """Add the ``--local-cache`` command-line option to pytest."""
+    parser.addoption(
+        "--local-cache",
+        action="store_true",
+        help="Use existing local cache files in tests",
+    )
+
+
+# Fixtures
+
+
+@pytest.fixture(scope="session")
+def session_context(request, tmp_env):
+    """A Context connected to a temporary, in-memory database.
+
+    Uses the :func:`.tmp_env` fixture from ixmp.
+    """
+    ctx = Context.only()
+
+    # Temporary, empty local directory for local data
+    session_tmp_dir = Path(request.config._tmp_path_factory.mktemp("data"))
+
+    # Set the cache path according to whether pytest --local-cache was given. If True,
+    # pick up the existing setting from the user environment.
+    ctx.cache_path = (
+        ctx.local_data if request.config.option.local_cache else session_tmp_dir
+    ).joinpath("cache")
+
+    # Other local data in the temporary directory
+    ctx.local_data = session_tmp_dir
+
+    platform_name = "message_data"
+
+    # Add a platform connected to an in-memory database
+    # NB cannot call Config.add_platform() here because it does not support supplying a
+    #    URL for a HyperSQL database.
+    # TODO add that feature upstream.
+    ixmp_config.values["platform"][platform_name] = {
+        "class": "jdbc",
+        "driver": "hsqldb",
+        "url": f"jdbc:hsqldb:mem://{platform_name}",
+    }
+
+    # Launch Platform and connect to testdb (reconnect if closed)
+    mp = Platform(name=platform_name)
+    mp.open_db()
+
+    ctx.platform_info["name"] = platform_name
+
+    yield ctx
+
+    ctx.close_db()
+    ixmp_config.remove_platform(platform_name)
+
+
+@pytest.fixture(scope="function")
+def test_context(request, session_context):
+    """A copy of :func:`session_context` scoped to one test function."""
+    ctx = deepcopy(session_context)
+
+    yield ctx
+
+    ctx.delete()
+
+
+@pytest.fixture(scope="function")
+def user_context(request):  # pragma: no cover
+    """Context which can access user's configuration, e.g. platform names."""
+    # Disabled; this is bad practice
+    raise NotImplementedError
+
+
+@pytest.fixture(scope="session")
+def mix_models_cli(request, session_context, tmp_env):
+    """A CliRunner object that invokes the :program:`mix-models` CLI."""
+    # Require the `session_context` fixture in order to set Context.local_data
+
+    class Runner(CliRunner):
+        def invoke(self, *args, **kwargs):
+            return super().invoke(cli.main, *args, env=tmp_env, **kwargs)
+
+    yield Runner()
+
+
+# Testing utility functions
+
+
+def bare_res(request, context: Context, solved: bool = False) -> message_ix.Scenario:
+    """Return or create a Scenario containing the bare RES, for use in testing.
+
+    The Scenario has a model name like "MESSAGEix-GLOBIOM [regions]
+    [start]:[duration]:[end]", e.g. "MESSAGEix-GLOBIOM R14 2020:10:2110" (see
+    :func:`.bare.name`) and the scenario name "baseline".
+
+    This function should:
+
+    - only be called from within test code, i.e. in :mod:`message_data.tests`.
+    - be called once for each test function, so that each test receives a fresh copy of
+      the RES scenario.
+
+    Parameters
+    ----------
+    request : .Request
+        The pytest :fixture:`pytest:request` fixture.
+    context : .Context
+        Passed to :func:`.testing.bare_res`.
+    solved : bool, optional
+        Return a solved Scenario.
+
+    Returns
+    -------
+    .Scenario
+        The scenario is a fresh clone, so can be modified freely without disturbing
+        other tests.
+    """
+    from message_ix_models.model import bare
+
+    context.use_defaults(bare.SETTINGS)
+
+    name = bare.name(context)
+    mp = context.get_platform()
+
+    try:
+        base = message_ix.Scenario(mp, name, "baseline")
+    except ValueError:
+        log.info(f"Create '{name}/baseline' for testing")
+        context.scenario_info.update(dict(model=name, scenario="baseline"))
+        base = bare.create_res(context)
+
+    if solved and not base.has_solution():
+        log.info("Solve")
+        base.solve(solve_options=dict(lpmethod=4))
+
+    log.info(f"Clone to '{name}/{request.node.name}'")
+    return base.clone(scenario=request.node.name, keep_solution=solved)
---
+++ packages/message-ix-models/message_ix_models-2021.3.22-py3-none-any.whl/message_ix_models/tests/test_util.py
@@ -0,0 +1,83 @@
+"""Tests of :mod:`message_ix_models.util`."""
+import logging
+from pathlib import Path
+
+import pytest
+
+from message_ix_models.util import (
+    MESSAGE_DATA_PATH,
+    MESSAGE_MODELS_PATH,
+    as_codes,
+    load_package_data,
+    load_private_data,
+    package_data_path,
+    private_data_path,
+)
+
+_actual_package_data = Path(__file__).parents[1].joinpath("data")
+
+
+def test_as_codes():
+    """Forward reference to a child is silently dropped."""
+    data = dict(
+        foo=dict(child=["bar"]),
+        bar=dict(name="Bar!"),
+    )
+    result = as_codes(data)
+    assert result[1] not in result[0].child
+
+
+@pytest.mark.parametrize(
+    "data",
+    (
+        set(),
+        # dict() with a value that is not a str or a further dict()
+        dict(foo="foo", bar=[1, 2, 3]),
+    ),
+)
+def test_as_codes_invalid(data):
+    """as_codes() rejects invalid data."""
+    with pytest.raises(TypeError):
+        as_codes(data)
+
+
+@pytest.mark.parametrize("path", _actual_package_data.rglob("*.yaml"))
+def test_load_package_data(path):
+    """Existing package data can be loaded."""
+    load_package_data(*path.relative_to(_actual_package_data).parts)
+
+
+def test_load_package_data_twice(caplog):
+    """Loading the same data twice logs a message."""
+    caplog.set_level(logging.DEBUG)
+    load_package_data("node", "R11")
+    load_package_data("node", "R11")
+    assert "'node R11' already loaded; skip" in caplog.messages
+
+
+def test_load_package_data_invalid():
+    """load_package_data() raises an exception for an unsupported file type."""
+    with pytest.raises(ValueError):
+        load_package_data("test.xml")
+
+
+@pytest.mark.xfail(
+    condition=MESSAGE_DATA_PATH is None, reason="Requires message_data to be installed."
+)
+def test_load_private_data(*parts, suffix=None):
+    load_private_data("sources.yaml")
+
+
+def test_package_data_path(*parts, suffix=None):
+    assert MESSAGE_MODELS_PATH.joinpath("data", "foo", "bar") == package_data_path(
+        "foo", "bar"
+    )
+
+
+@pytest.mark.xfail(
+    condition=MESSAGE_DATA_PATH is None, reason="Requires message_data to be installed."
+)
+def test_private_data_path(*parts, suffix=None):
+    assert MESSAGE_DATA_PATH.joinpath("data", "foo", "bar") == private_data_path(
+        "foo", "bar"
+    )
