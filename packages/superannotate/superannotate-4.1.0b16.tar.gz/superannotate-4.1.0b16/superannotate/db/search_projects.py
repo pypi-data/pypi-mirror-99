8bee8e2ac551f193e248fe08e2646b5c2f6ce9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:38 +0000

    Add package superannotate/4.1.0b16/superannotate-4.1.0b16.tar.gz

---
+++ packages/superannotate/superannotate-4.1.0b16.tar.gz/superannotate-4.1.0b16/superannotate/db/search_projects.py
@@ -0,0 +1,51 @@
+import logging
+
+from ..api import API
+from ..exceptions import SABaseException
+
+logger = logging.getLogger("superannotate-python-sdk")
+
+_api = API.get_instance()
+
+
+def search_projects(
+    name=None, return_metadata=False, include_complete_image_count=False
+):
+    """Project name based case-insensitive search for projects.
+    If **name** is None, all the projects will be returned.
+
+    :param name: search string
+    :type name: str
+    :param return_metadata: return metadata of projects instead of names
+    :type return_metadata: bool
+
+    :return: project names or metadatas
+    :rtype: list of strs or dicts
+    """
+    result_list = []
+    params = {
+        'team_id': str(_api.team_id),
+        'offset': 0,
+        'completeImagesCount': include_complete_image_count
+    }
+    if name is not None:
+        params['name'] = name
+    while True:
+        response = _api.send_request(
+            req_type='GET', path='/projects', params=params
+        )
+        if response.ok:
+            new_results = response.json()
+            result_list += new_results["data"]
+            if new_results["count"] <= len(result_list):
+                break
+            params["offset"] = len(result_list)
+        else:
+            raise SABaseException(
+                response.status_code,
+                "Couldn't search projects." + response.text
+            )
+    if return_metadata:
+        return result_list
+    else:
+        return [x["name"] for x in result_list]
