a460fed3bdd719fa44f6521a9db84b0d451c845a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:41 +0000

    Add package cataliistNester/1.0.0/cataliistNester-1.0.0.tar.gz

---
+++ packages/cataliistNester/cataliistNester-1.0.0.tar.gz/cataliistNester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name            = 'cataliistNester',
+    version         = '1.0.0',
+    py_modules      = ['cataliistNester'],
+    author          = 'cataliist',
+    author_email    = 'cataliistceo@gmail.com',
+    url             = 'http://www.cataliist.com',
+    description     = 'a simple printer for nested lists',
+)
