5ca5bf922903a51104e1a236259e04ede2e15c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:01 +0000

    Add package labthings/1.2.4/labthings-1.2.4-py3-none-any.whl

---
+++ packages/labthings/labthings-1.2.4-py3-none-any.whl/labthings/default_views/docs/__init__.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, make_response, render_template
+
+from ...find import current_labthing
+from ...views import View
+
+
+class APISpecView(View):
+    """OpenAPI v3 documentation"""
+
+    def get(self):
+        """OpenAPI v3 documentation"""
+        return current_labthing().spec.to_dict()
+
+
+class SwaggerUIView(View):
+    """Swagger UI documentation"""
+
+    def get(self):
+        """ """
+        return make_response(render_template("swagger-ui.html"))
+
+
+docs_blueprint = Blueprint(
+    "labthings_docs", __name__, static_folder="./static", template_folder="./templates"
+)
+
+docs_blueprint.add_url_rule("/swagger", view_func=APISpecView.as_view("swagger_json"))
+docs_blueprint.add_url_rule(
+    "/swagger-ui", view_func=SwaggerUIView.as_view("swagger_ui")
+)
+SwaggerUIView.endpoint = "labthings_docs.swagger_ui"
