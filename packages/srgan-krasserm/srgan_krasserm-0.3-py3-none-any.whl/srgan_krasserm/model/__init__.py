52f916fea883933ce42be61a195495f3c8489055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:15 +0000

    Add package srgan-krasserm/0.3/srgan_krasserm-0.3-py3-none-any.whl

---
+++ packages/srgan-krasserm/srgan_krasserm-0.3-py3-none-any.whl/srgan_krasserm/model/__init__.py
@@ -0,0 +1,3 @@
+from srgan_krasserm.model.common import resolve
+from srgan_krasserm.model.common import resolve_single
+from srgan_krasserm.model.common import evaluate
---
+++ packages/srgan-krasserm/srgan_krasserm-0.3-py3-none-any.whl/srgan_krasserm/utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import Image
+
+
+def get_mode_pathl() :
+    CWD_PATH =  os.path.dirname(os.path.abspath(__file__))
+    weights_file = os.path.join(CWD_PATH, 'weights', 'srgan','gan_generator.h5')
+    return weights_file
+    
+def load_image(path):
+    return np.array(Image.open(path))
+
+
+def plot_sample(lr, sr):
+    plt.figure(figsize=(20, 10))
+
+    images = [lr, sr]
+    titles = ['LR', f'SR (x{sr.shape[0] // lr.shape[0]})']
+
+    for i, (img, title) in enumerate(zip(images, titles)):
+        plt.subplot(1, 2, i+1)
+        plt.imshow(img)
+        plt.title(title)
+        plt.xticks([])
+        plt.yticks([])
