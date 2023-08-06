0619020c4fa088879878c8add01669d2e7c86ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:37 +0000

    Add package mypy-boto3-lakeformation/1.17.32.0/mypy_boto3_lakeformation-1.17.32.0-py3-none-any.whl

---
+++ packages/mypy-boto3-lakeformation/mypy_boto3_lakeformation-1.17.32.0-py3-none-any.whl/mypy_boto3_lakeformation/__main__.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    print(
+        "Type annotations for boto3.LakeFormation 1.17.32\n"
+        "Version:         1.17.32.0\n"
+        "Builder version: 4.4.0\n"
+        "Docs:            https://pypi.org/project/mypy-boto3-lakeformation/\n"
+        "Other services:  https://pypi.org/project/boto3-stubs/\n"
+        "Changelog:       https://github.com/vemel/mypy_boto3_builder/releases"
+    )
