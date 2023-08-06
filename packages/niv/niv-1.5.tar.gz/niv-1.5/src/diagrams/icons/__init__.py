bcab8e0bd7bd67b3b6498bad2839e2fef76d9115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:39 +0000

    Add package niv/1.5/niv-1.5.tar.gz

---
+++ packages/niv/niv-1.5.tar.gz/niv-1.5/src/diagrams/icons/__init__.py
@@ -0,0 +1,12 @@
+"""
+GCP provides a set of services for Google Cloud Platform provider.
+"""
+
+from src.diagrams import Node
+
+
+class _ICONS(Node):
+    _provider = "icons"
+    _icon_dir = "../resources/icons"
+
+    fontcolor = "#2d3436"
