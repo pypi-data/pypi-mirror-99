8a87f6fb809ee091c8f735508f1d21e518f89c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:49 +0000

    Add package schemathesis/3.5.2/schemathesis-3.5.2.tar.gz

---
+++ packages/schemathesis/schemathesis-3.5.2.tar.gz/schemathesis-3.5.2/src/schemathesis/cli/debug.py
@@ -0,0 +1,22 @@
+import json
+
+import attr
+from click.utils import LazyFile
+
+from ..runner import events
+from .handlers import EventHandler, ExecutionContext
+
+
+@attr.s(slots=True)  # pragma: no mutate
+class DebugOutputHandler(EventHandler):
+    file_handle: LazyFile = attr.ib()  # pragma: no mutate
+
+    def handle_event(self, context: ExecutionContext, event: events.ExecutionEvent) -> None:
+        stream = self.file_handle.open()
+        data = event.asdict()
+        data["event_type"] = event.__class__.__name__
+        stream.write(json.dumps(data))
+        stream.write("\n")
+
+    def shutdown(self) -> None:
+        self.file_handle.close()
