2ea243ceb614bbf5a39c6ccd4867d89fb1a7f407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:08 +0000

    Add package printlog/0.0.5/printlog-0.0.5.tar.gz

---
+++ packages/printlog/printlog-0.0.5.tar.gz/printlog-0.0.5/printlog/color.py
@@ -0,0 +1,46 @@
+from printlog.utils import Color 
+
+def red(text:str, bg:bool=False) -> str:
+    if bg:
+        return f"""{Color.RED_BACKGROUND.value}{text}{Color.RESET.value}"""
+    else:
+        return f"""{Color.RED.value}{text}{Color.RESET.value}"""
+
+def green(text:str, bg:bool=False) -> str:
+    if bg:
+        return f"""{Color.GREEN_BACKGROUND.value}{text}{Color.RESET.value}"""
+    else:
+        return f"""{Color.GREEN.value}{text}{Color.RESET.value}"""
+
+def yellow(text:str, bg:bool=False) -> str:
+    if bg:
+        return f"""{Color.YELLOW_BACKGROUND.value}{text}{Color.RESET.value}"""
+    else:
+        return f"""{Color.YELLOW.value}{text}{Color.RESET.value}"""
+
+def blue(text:str, bg:bool=False) -> str:
+    if bg:
+        return f"""{Color.BLUE_BACKGROUND.value}{text}{Color.RESET.value}"""
+    else:
+        return f"""{Color.BLUE.value}{text}{Color.RESET.value}"""
+
+def magenta(text:str, bg:bool=False) -> str:
+    if bg:
+        return f"""{Color.MAGENTA_BACKGROUND.value}{text}{Color.RESET.value}"""
+    else:
+        return f"""{Color.MAGENTA.value}{text}{Color.RESET.value}"""
+
+def cyan(text:str, bg:bool=False) -> str:
+    if bg:
+        return f"""{Color.CYAN_BACKGROUND.value}{text}{Color.RESET.value}"""
+    else:
+        return f"""{Color.CYAN.value}{text}{Color.RESET.value}"""
+
+def white(text:str, bg:bool=False) -> str:
+    if bg:
+        return f"""{Color.WHITE_BACKGROUND.value}{text}{Color.RESET.value}"""
+    else:
+        return f"""{Color.WHITE.value}{text}{Color.RESET.value}"""
+
+
+
---
+++ packages/printlog/printlog-0.0.5.tar.gz/printlog-0.0.5/printlog/log.py
@@ -0,0 +1,21 @@
+from printlog.utils import Color
+
+def error(text:str, bg:bool=False) -> None:
+    if bg:
+        print(f"""{Color.RED_BACKGROUND_BRIGHT.value}{text}{Color.RESET.value} """)
+    else:
+        print(f"""{Color.RED_BOLD.value}{text}{Color.RESET.value}""")
+
+
+def success(text:str, bg:bool=False) -> None:
+    if bg:
+        print(f"""{Color.GREEN_BACKGROUND.value}{text}{Color.RESET.value}""")
+    else:
+        print(f"""{Color.GREEN_BRIGHT.value}{text}{Color.RESET.value}""")
+
+if __name__ == "__main__":
+    success("It is a success")
+    success("It is a success with background", bg=True)
+    error("It is an error")
+    error("It is an error with background", bg=True)
+
