253ead6ef20d2453e85a3a336a509d868c16778f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:57 +0000

    Add package clicktypes/0.0.4/clicktypes-0.0.4-py2.py3-none-any.whl

---
+++ packages/clicktypes/clicktypes-0.0.4-py2.py3-none-any.whl/clicktypes/web/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Import relevant classes for web related parameters
+
+## Email addresses
+from .email import EmailParam
+
+## Domains
+from .domain import DomainParam
+
+## URL
+from .url import UrlParam
