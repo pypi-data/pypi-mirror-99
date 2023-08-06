4f16409accb970b4abecf8150691077eb9cdcd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:56 +0000

    Add package python-paseto/0.1.0a4/python_paseto-0.1.0a4-py3-none-any.whl

---
+++ packages/python-paseto/python_paseto-0.1.0a4-py3-none-any.whl/paseto/exceptions.py
@@ -0,0 +1,13 @@
+""" This module contains exceptions. """
+
+
+class PasetoException(Exception):
+    """ Base exception for all paseto related errors. """
+
+
+class InvalidFooter(PasetoException):
+    """ Footer could not be verified. """
+
+
+class InvalidHeader(PasetoException):
+    """ Message contains incorrect header. """
