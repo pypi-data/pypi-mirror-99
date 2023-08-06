4aa1c0ccd8e4d966038c55c52f78250b307f24bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:39 +0000

    Add package sphinx-ncs-theme/0.5.0/sphinx_ncs_theme-0.5.0-py3-none-any.whl

---
+++ packages/sphinx-ncs-theme/sphinx_ncs_theme-0.5.0-py3-none-any.whl/sphinx_ncs_theme/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+
+__version__ = "0.5.0"
+
+
+def setup(app):
+    app.add_html_theme(
+        "sphinx_ncs_theme", os.path.dirname(os.path.abspath(__file__))
+    )
+
+    app.add_js_file("js/ncs.js")
+
+    return {
+        'parallel_read_safe': True,
+        'parallel_write_safe': True
+    }
