4e2490982cf1ffd42b68ca94d5d1984131b6f2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:33 +0000

    Add package RBX.py/0.7/RBX.py-0.7.tar.gz

---
+++ packages/RBX.py/RBX.py-0.7.tar.gz/RBX.py-0.7/RBX_py/Client.py
@@ -0,0 +1,30 @@
+from RBX_py.Asset import Asset
+from RBX_py.Player import Player
+
+class Client:
+    def __init__(self):
+        pass
+
+    def FetchAsset(self, id):
+        """
+        Creates an instance of an Asset class using the given ID
+        """
+        TypeErrorCheck = isinstance(id, str)
+        if TypeErrorCheck:
+            raise TypeError(f"Asset ID ({id}) must be in integer form")
+
+        asset_obj = Asset(id=id)
+        asset_obj.fetchData()
+        return asset_obj
+    
+    def FetchUser(self, id):
+        """
+        Creates an instance of a Player class using the given ID
+        """
+        TypeErrorCheck = isinstance(id, str)
+        if TypeErrorCheck:
+            raise TypeError(f"User ID ({id}) must be in integer form")
+
+        player_obj = Player(id=id)
+        player_obj.fetchData()
+        return player_obj
