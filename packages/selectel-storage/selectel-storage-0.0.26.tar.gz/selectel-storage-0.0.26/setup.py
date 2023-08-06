edb13c2352872101df7f9e0db065741c012f3907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:44 +0000

    Add package selectel-storage/0.0.26/selectel-storage-0.0.26.tar.gz

---
+++ packages/selectel-storage/selectel-storage-0.0.26.tar.gz/selectel-storage-0.0.26/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+VERSION = "0.0.26"
+
+setup(
+    name='selectel-storage',
+    description="",
+    version=VERSION,
+    url='https://github.com/KokocGroup/selectel_storage',
+    download_url='https://github.com/KokocGroup/selectel_storage/tarball/v{}'.format(VERSION),
+    packages=find_packages(),
+    install_requires=[
+        'mongoengine==0.10.0',
+        'python-magic==0.4.10',
+        'requests==2.10.0'
+    ],
+)
