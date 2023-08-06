2f51c3ea08d181cde052cfbf11d0298227aa481c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:21 +0000

    Add package mtgeo/0.7.5/mtgeo-0.7.5-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/mtgeo/mtgeo-0.7.5-cp37-cp37m-manylinux2014_x86_64.whl/mt/geo/dilated_isometry.py
@@ -0,0 +1,4 @@
+from ..geond.dilated_isometry import *
+
+from mt.base import logger
+logger.warn_module_move("mt.geo.dilated_isometry", "mt.geond.dilated_isometry")
---
+++ packages/mtgeo/mtgeo-0.7.5-cp37-cp37m-manylinux2014_x86_64.whl/mt/geo/rect.py
@@ -0,0 +1,4 @@
+from ..geo2d.rect import *
+
+from mt.base import logger
+logger.warn_module_move("mt.geo.rect", "mt.geo2d.rect")
