9680899940d639acbe53c51163db1f79e32789da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:42 +0000

    Add package tplink-cloud-api/2.4.1/tplink_cloud_api-2.4.1-py3-none-any.whl

---
+++ packages/tplink-cloud-api/tplink_cloud_api-2.4.1-py3-none-any.whl/tplinkcloud/kp303_child.py
@@ -0,0 +1,40 @@
+from .device_type import TPLinkDeviceType
+from .device import TPLinkDevice
+
+
+class KP303ChildAction:
+    def __init__(self, child_action):
+        self.type = child_action.get('type')
+        self.schd_sec = child_action.get('schd_sec')
+        self.action = child_action.get('action')
+
+
+class KP303ChildSysInfo:
+
+    def __init__(self, child_info):
+        self.id = child_info.get('id')
+        self.state = child_info.get('state')
+        self.alias = child_info.get('alias')
+        self.on_time = child_info.get('on_time')
+        self.next_action = KP303ChildAction(child_info.get('next_action'))
+
+
+class KP303Child(TPLinkDevice):
+
+    def __init__(self, client, parent_device_id, child_device_id, device_info):
+        super().__init__(
+            client,
+            parent_device_id,
+            device_info,
+            child_id=child_device_id
+        )
+        self.model_type = TPLinkDeviceType.KP303CHILD
+
+    def get_sys_info(self):
+        sys_info = self._get_sys_info()
+        
+        if not sys_info:
+            print("Something went wrong with your request; please try again")
+            return None
+        
+        return KP303ChildSysInfo(sys_info)
