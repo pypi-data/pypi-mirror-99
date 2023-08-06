5f43be224f6176df7f8253e9b9f21d5f218de993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:56 +0000

    Add package hypervector-wrapper/0.0.3/hypervector-wrapper-0.0.3.tar.gz

---
+++ packages/hypervector-wrapper/hypervector-wrapper-0.0.3.tar.gz/hypervector-wrapper-0.0.3/README.md
@@ -0,0 +1,27 @@
+# hypervector-wrapper
+
+![workflow](https://github.com/hypervectorio/hypervector-wrapper/actions/workflows/main.yml/badge.svg)
+[![PyPI version](https://badge.fury.io/py/hypervector-wrapper.svg)](https://pypi.python.org/pypi/hypervector-wrapper/)
+
+Python wrapper to access the Hypervector API, providing easy deserialisation of Hypervector
+resources to Python objects for building your data-driven test suite.
+
+## Docs
+
+Full API and wrapper docs are available at [https://docs.hypervector.io](https://docs.hypervector.io), along with
+release history and usage examples.
+
+## Installation
+
+```python
+pip install hypervector-wrapper
+```
+
+Note: requires Python 3.6+
+
+You can install the development dependencies and run tests with:
+
+```python
+pip install -e ".[dev]"
+pytest
+```
---
+++ packages/hypervector-wrapper/hypervector-wrapper-0.0.3.tar.gz/hypervector-wrapper-0.0.3/hypervector/resources/core/definition.py
@@ -0,0 +1,66 @@
+import json
+
+import requests
+
+import hypervector
+from hypervector.resources.core.ensemble import Ensemble
+from hypervector.resources.abstract.api_resource import APIResource
+
+
+class Definition(APIResource):
+    resource_name = 'definition'
+
+    def __init__(self, definition_uuid, definition_name, added, ensembles):
+        self.definition_uuid = definition_uuid
+        self.definition_name = definition_name
+        self.added = added
+        self.ensembles = ensembles
+
+    @classmethod
+    def from_response(cls, dictionary):
+        return cls(
+            definition_uuid=dictionary['definition_uuid'],
+            definition_name=dictionary['definition_name'],
+            added=dictionary['added'],
+            ensembles=None
+        )
+
+    def to_response(self):
+        return {
+            "definition_uuid": self.definition_uuid,
+            "definition_name": self.definition_name,
+            "added": self.added,
+            "ensembles": self.ensembles
+        }
+
+    @classmethod
+    def from_get(cls, response):
+        return cls.from_response(response.json())
+
+    @classmethod
+    def list(cls):
+        endpoint = f"{hypervector.API_BASE}/definitions"
+        response = requests.get(endpoint, headers=cls.get_headers()).json()
+        return [cls.from_response(definition) for definition in response]
+
+    @classmethod
+    def new(cls, definition_file, project_uuid=None):
+        endpoint = f"{hypervector.API_BASE}/{cls.resource_name}/add"
+        data = {
+            "definition": _parse_definition_from_json_file(definition_file),
+            "project_uuid": project_uuid
+        }
+        response = requests.post(endpoint, json=data, headers=cls.get_headers()).json()
+        return cls.from_response(response)
+
+
+def _parse_definition_from_json_file(definition_json_file):
+    return json.load(open(definition_json_file, 'rb'))
+
+
+def _parse_ensembles(ensembles):
+    parsed_ensembles = []
+    for ensemble_uuid, ensemble_meta in ensembles.items():
+        parsed_ensemble = Ensemble.from_dict(ensemble_uuid, ensemble_meta)
+        parsed_ensembles.append(parsed_ensemble)
+    return parsed_ensembles
