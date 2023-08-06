e8ec73339b8574c26dd89f3609c12e08377853b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:09 +0000

    Add package valuecounts/0.1/valuecounts-0.1.tar.gz

---
+++ packages/valuecounts/valuecounts-0.1.tar.gz/valuecounts-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='valuecounts',
+      version='0.1',
+      description='I will greet you',
+      url='https://github.com/klmsathish/greeter',
+      author='klmsathish',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      author_email='klmsathish@gmail.com',
+      keywords=['value_counts'])
---
+++ packages/valuecounts/valuecounts-0.1.tar.gz/valuecounts-0.1/valuecounts/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+__version__ = "0.0.1"
+def value_counts(df,col):
+    return df[col].value_counts()
