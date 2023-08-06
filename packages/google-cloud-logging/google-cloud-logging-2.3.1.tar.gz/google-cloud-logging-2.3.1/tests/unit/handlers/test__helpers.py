804be6dcb18e19e9b451fb5f65543ea41159bd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:30 +0000

    Add package google-cloud-logging/2.3.1/google-cloud-logging-2.3.1.tar.gz

---
+++ packages/google-cloud-logging/google-cloud-logging-2.3.1.tar.gz/google-cloud-logging-2.3.1/tests/unit/handlers/test__helpers.py
@@ -0,0 +1,292 @@
+# Copyright 2017 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import mock
+
+_FLASK_TRACE_ID = "flask-id"
+_FLASK_HTTP_REQUEST = {"requestUrl": "https://flask.palletsprojects.com/en/1.1.x/"}
+_DJANGO_TRACE_ID = "django-id"
+_DJANGO_HTTP_REQUEST = {"requestUrl": "https://www.djangoproject.com/"}
+
+
+class Test_get_request_data_from_flask(unittest.TestCase):
+    @staticmethod
+    def _call_fut():
+        from google.cloud.logging_v2.handlers import _helpers
+
+        return _helpers.get_request_data_from_flask()
+
+    @staticmethod
+    def create_app():
+        import flask
+
+        app = flask.Flask(__name__)
+
+        @app.route("/")
+        def index():
+            return "test flask trace"  # pragma: NO COVER
+
+        return app
+
+    def test_no_context_header(self):
+        app = self.create_app()
+        with app.test_request_context(path="/", headers={}):
+            http_request, trace_id = self._call_fut()
+
+        self.assertIsNone(trace_id)
+        self.assertEqual(http_request["requestMethod"], "GET")
+
+    def test_valid_context_header(self):
+        flask_trace_header = "X_CLOUD_TRACE_CONTEXT"
+        expected_trace_id = _FLASK_TRACE_ID
+        flask_trace_id = expected_trace_id + "/testspanid"
+
+        app = self.create_app()
+        context = app.test_request_context(
+            path="/", headers={flask_trace_header: flask_trace_id}
+        )
+
+        with context:
+            http_request, trace_id = self._call_fut()
+
+        self.assertEqual(trace_id, expected_trace_id)
+        self.assertEqual(http_request["requestMethod"], "GET")
+
+    def test_http_request_populated(self):
+        expected_path = "http://testserver/123"
+        expected_agent = "Mozilla/5.0"
+        expected_referrer = "self"
+        expected_ip = "10.1.2.3"
+        body_content = "test"
+        headers = {
+            "User-Agent": expected_agent,
+            "Referer": expected_referrer,
+        }
+
+        app = self.create_app()
+        with app.test_client() as c:
+            c.put(
+                path=expected_path,
+                data=body_content,
+                environ_base={"REMOTE_ADDR": expected_ip},
+                headers=headers,
+            )
+            http_request, trace_id = self._call_fut()
+
+        self.assertEqual(http_request["requestMethod"], "PUT")
+        self.assertEqual(http_request["requestUrl"], expected_path)
+        self.assertEqual(http_request["userAgent"], expected_agent)
+        self.assertEqual(http_request["referer"], expected_referrer)
+        self.assertEqual(http_request["remoteIp"], expected_ip)
+        self.assertEqual(http_request["requestSize"], len(body_content))
+        self.assertEqual(http_request["protocol"], "HTTP/1.1")
+
+    def test_http_request_sparse(self):
+        expected_path = "http://testserver/123"
+        app = self.create_app()
+        with app.test_client() as c:
+            c.put(path=expected_path)
+            http_request, trace_id = self._call_fut()
+        self.assertEqual(http_request["requestMethod"], "PUT")
+        self.assertEqual(http_request["requestUrl"], expected_path)
+        self.assertEqual(http_request["protocol"], "HTTP/1.1")
+
+
+class Test_get_request_data_from_django(unittest.TestCase):
+    @staticmethod
+    def _call_fut():
+        from google.cloud.logging_v2.handlers import _helpers
+
+        return _helpers.get_request_data_from_django()
+
+    def setUp(self):
+        from django.conf import settings
+        from django.test.utils import setup_test_environment
+
+        if not settings.configured:
+            settings.configure()
+        setup_test_environment()
+
+    def tearDown(self):
+        from django.test.utils import teardown_test_environment
+        from google.cloud.logging_v2.handlers.middleware import request
+
+        teardown_test_environment()
+        request._thread_locals.__dict__.clear()
+
+    def test_no_context_header(self):
+        from django.test import RequestFactory
+        from google.cloud.logging_v2.handlers.middleware import request
+
+        django_request = RequestFactory().get("/")
+
+        middleware = request.RequestMiddleware(None)
+        middleware.process_request(django_request)
+        http_request, trace_id = self._call_fut()
+        self.assertEqual(http_request["requestMethod"], "GET")
+        self.assertIsNone(trace_id)
+
+    def test_valid_context_header(self):
+        from django.test import RequestFactory
+        from google.cloud.logging_v2.handlers.middleware import request
+
+        django_trace_header = "HTTP_X_CLOUD_TRACE_CONTEXT"
+        expected_trace_id = "testtraceiddjango"
+        django_trace_id = expected_trace_id + "/testspanid"
+
+        django_request = RequestFactory().get(
+            "/", **{django_trace_header: django_trace_id}
+        )
+
+        middleware = request.RequestMiddleware(None)
+        middleware.process_request(django_request)
+        http_request, trace_id = self._call_fut()
+
+        self.assertEqual(trace_id, expected_trace_id)
+        self.assertEqual(http_request["requestMethod"], "GET")
+
+    def test_http_request_populated(self):
+        from django.test import RequestFactory
+        from google.cloud.logging_v2.handlers.middleware import request
+
+        expected_path = "http://testserver/123"
+        expected_agent = "Mozilla/5.0"
+        expected_referrer = "self"
+        body_content = "test"
+        django_request = RequestFactory().put(
+            expected_path,
+            data=body_content,
+            HTTP_USER_AGENT=expected_agent,
+            HTTP_REFERER=expected_referrer,
+        )
+        # ensure test passes even after request has been read
+        # context: https://github.com/googleapis/python-logging/issues/159
+        django_request.read()
+
+        middleware = request.RequestMiddleware(None)
+        middleware.process_request(django_request)
+        http_request, trace_id = self._call_fut()
+        self.assertEqual(http_request["requestMethod"], "PUT")
+        self.assertEqual(http_request["requestUrl"], expected_path)
+        self.assertEqual(http_request["userAgent"], expected_agent)
+        self.assertEqual(http_request["referer"], expected_referrer)
+        self.assertEqual(http_request["remoteIp"], "127.0.0.1")
+        self.assertEqual(http_request["requestSize"], len(body_content))
+        self.assertEqual(http_request["protocol"], "HTTP/1.1")
+
+    def test_http_request_sparse(self):
+        from django.test import RequestFactory
+        from google.cloud.logging_v2.handlers.middleware import request
+
+        expected_path = "http://testserver/123"
+        django_request = RequestFactory().put(expected_path)
+        middleware = request.RequestMiddleware(None)
+        middleware.process_request(django_request)
+        http_request, trace_id = self._call_fut()
+        self.assertEqual(http_request["requestMethod"], "PUT")
+        self.assertEqual(http_request["requestUrl"], expected_path)
+        self.assertEqual(http_request["remoteIp"], "127.0.0.1")
+        self.assertEqual(http_request["protocol"], "HTTP/1.1")
+
+
+class Test_get_request_data(unittest.TestCase):
+    @staticmethod
+    def _call_fut():
+        from google.cloud.logging_v2.handlers import _helpers
+
+        return _helpers.get_request_data()
+
+    def _helper(self, django_return, flask_return):
+        django_patch = mock.patch(
+            "google.cloud.logging_v2.handlers._helpers.get_request_data_from_django",
+            return_value=django_return,
+        )
+        flask_patch = mock.patch(
+            "google.cloud.logging_v2.handlers._helpers.get_request_data_from_flask",
+            return_value=flask_return,
+        )
+
+        with django_patch as django_mock:
+            with flask_patch as flask_mock:
+                result = self._call_fut()
+
+        return django_mock, flask_mock, result
+
+    def test_from_django(self):
+        django_expected = (_DJANGO_HTTP_REQUEST, _DJANGO_TRACE_ID)
+        flask_expected = (None, None)
+        django_mock, flask_mock, output = self._helper(django_expected, flask_expected)
+        self.assertEqual(output, django_expected)
+
+        django_mock.assert_called_once_with()
+        flask_mock.assert_not_called()
+
+    def test_from_flask(self):
+        django_expected = (None, None)
+        flask_expected = (_FLASK_HTTP_REQUEST, _FLASK_TRACE_ID)
+
+        django_mock, flask_mock, output = self._helper(django_expected, flask_expected)
+        self.assertEqual(output, flask_expected)
+
+        django_mock.assert_called_once_with()
+        flask_mock.assert_called_once_with()
+
+    def test_from_django_and_flask(self):
+        django_expected = (_DJANGO_HTTP_REQUEST, _DJANGO_TRACE_ID)
+        flask_expected = (_FLASK_HTTP_REQUEST, _FLASK_TRACE_ID)
+
+        django_mock, flask_mock, output = self._helper(django_expected, flask_expected)
+
+        # Django wins.
+        self.assertEqual(output, django_expected)
+
+        django_mock.assert_called_once_with()
+        flask_mock.assert_not_called()
+
+    def test_missing_http_request(self):
+        flask_expected = (None, _FLASK_TRACE_ID)
+        django_expected = (None, _DJANGO_TRACE_ID)
+        django_mock, flask_mock, output = self._helper(django_expected, flask_expected)
+
+        # function only returns trace if http_request data is present
+        self.assertEqual(output, (None, None))
+
+        django_mock.assert_called_once_with()
+        flask_mock.assert_called_once_with()
+
+    def test_missing_trace_id(self):
+        flask_expected = (_FLASK_HTTP_REQUEST, None)
+        django_expected = (None, _DJANGO_TRACE_ID)
+        django_mock, flask_mock, output = self._helper(django_expected, flask_expected)
+
+        # trace_id is optional
+        self.assertEqual(output, flask_expected)
+
+        django_mock.assert_called_once_with()
+        flask_mock.assert_called_once_with()
+
+    def test_missing_both(self):
+        flask_expected = (None, None)
+        django_expected = (None, None)
+        django_mock, flask_mock, output = self._helper(django_expected, flask_expected)
+        self.assertEqual(output, (None, None))
+
+        django_mock.assert_called_once_with()
+        flask_mock.assert_called_once_with()
+
+    def test_wo_libraries(self):
+        output = self._call_fut()
+        self.assertEqual(output, (None, None))
---
+++ packages/google-cloud-logging/google-cloud-logging-2.3.1.tar.gz/google-cloud-logging-2.3.1/tests/unit/handlers/test_handlers.py
@@ -0,0 +1,261 @@
+# Copyright 2016 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import unittest
+from unittest.mock import patch
+import mock
+
+from google.cloud.logging_v2.handlers._monitored_resources import (
+    _FUNCTION_ENV_VARS,
+    _GAE_ENV_VARS,
+)
+
+
+class TestCloudLoggingHandler(unittest.TestCase):
+
+    PROJECT = "PROJECT"
+
+    @staticmethod
+    def _get_target_class():
+        from google.cloud.logging.handlers import CloudLoggingHandler
+
+        return CloudLoggingHandler
+
+    def _make_one(self, *args, **kw):
+        return self._get_target_class()(*args, **kw)
+
+    def test_ctor_defaults(self):
+        import sys
+        from google.cloud.logging_v2.logger import _GLOBAL_RESOURCE
+        from google.cloud.logging_v2.handlers.handlers import DEFAULT_LOGGER_NAME
+
+        patch = mock.patch(
+            "google.cloud.logging_v2.handlers._monitored_resources.retrieve_metadata_server",
+            return_value=None,
+        )
+        with patch:
+            client = _Client(self.PROJECT)
+            handler = self._make_one(client, transport=_Transport)
+            self.assertEqual(handler.name, DEFAULT_LOGGER_NAME)
+            self.assertIs(handler.client, client)
+            self.assertIsInstance(handler.transport, _Transport)
+            self.assertIs(handler.transport.client, client)
+            self.assertEqual(handler.transport.name, DEFAULT_LOGGER_NAME)
+            self.assertEqual(handler.resource, _GLOBAL_RESOURCE)
+            self.assertIsNone(handler.labels)
+            self.assertIs(handler.stream, sys.stderr)
+
+    def test_ctor_explicit(self):
+        import io
+        from google.cloud.logging import Resource
+
+        resource = Resource("resource_type", {"resource_label": "value"})
+        labels = {"handler_lable": "value"}
+        name = "test-logger"
+        client = _Client(self.PROJECT)
+        stream = io.BytesIO()
+        handler = self._make_one(
+            client,
+            name=name,
+            transport=_Transport,
+            resource=resource,
+            labels=labels,
+            stream=stream,
+        )
+        self.assertEqual(handler.name, name)
+        self.assertIs(handler.client, client)
+        self.assertIsInstance(handler.transport, _Transport)
+        self.assertIs(handler.transport.client, client)
+        self.assertEqual(handler.transport.name, name)
+        self.assertIs(handler.resource, resource)
+        self.assertEqual(handler.labels, labels)
+        self.assertIs(handler.stream, stream)
+
+    def test_emit(self):
+        from google.cloud.logging_v2.logger import _GLOBAL_RESOURCE
+
+        client = _Client(self.PROJECT)
+        handler = self._make_one(
+            client, transport=_Transport, resource=_GLOBAL_RESOURCE
+        )
+        logname = "loggername"
+        message = "hello world"
+        record = logging.LogRecord(logname, logging, None, None, message, None, None)
+        handler.emit(record)
+
+        self.assertEqual(
+            handler.transport.send_called_with,
+            (record, message, _GLOBAL_RESOURCE, None, None, None, None),
+        )
+
+    def test_emit_manual_field_override(self):
+        from google.cloud.logging_v2.logger import _GLOBAL_RESOURCE
+        from google.cloud.logging_v2.resource import Resource
+
+        client = _Client(self.PROJECT)
+        handler = self._make_one(
+            client, transport=_Transport, resource=_GLOBAL_RESOURCE
+        )
+        logname = "loggername"
+        message = "hello world"
+        record = logging.LogRecord(logname, logging, None, None, message, None, None)
+        # set attributes manually
+        expected_trace = "123"
+        setattr(record, "trace", expected_trace)
+        expected_span = "456"
+        setattr(record, "span_id", expected_span)
+        expected_http = {"reuqest_url": "manual"}
+        setattr(record, "http_request", expected_http)
+        expected_resource = Resource(type="test", labels={})
+        setattr(record, "resource", expected_resource)
+        expected_labels = {"test-label": "manual"}
+        setattr(record, "labels", expected_labels)
+        handler.emit(record)
+
+        self.assertEqual(
+            handler.transport.send_called_with,
+            (
+                record,
+                message,
+                expected_resource,
+                expected_labels,
+                expected_trace,
+                expected_span,
+                expected_http,
+            ),
+        )
+
+
+class TestSetupLogging(unittest.TestCase):
+    def _call_fut(self, handler, excludes=None):
+        from google.cloud.logging.handlers import setup_logging
+
+        if excludes:
+            return setup_logging(handler, excluded_loggers=excludes)
+        else:
+            return setup_logging(handler)
+
+    def test_setup_logging(self):
+        handler = _Handler(logging.INFO)
+        self._call_fut(handler)
+
+        root_handlers = logging.getLogger().handlers
+        self.assertIn(handler, root_handlers)
+
+    def test_setup_logging_excludes(self):
+        INCLUDED_LOGGER_NAME = "includeme"
+        EXCLUDED_LOGGER_NAME = "excludeme"
+
+        handler = _Handler(logging.INFO)
+        self._call_fut(handler, (EXCLUDED_LOGGER_NAME,))
+
+        included_logger = logging.getLogger(INCLUDED_LOGGER_NAME)
+        self.assertTrue(included_logger.propagate)
+
+        excluded_logger = logging.getLogger(EXCLUDED_LOGGER_NAME)
+        self.assertNotIn(handler, excluded_logger.handlers)
+        self.assertFalse(excluded_logger.propagate)
+
+    @patch.dict("os.environ", {envar: "1" for envar in _FUNCTION_ENV_VARS})
+    def test_remove_handlers_gcf(self):
+        logger = logging.getLogger()
+        # add fake handler
+        added_handler = logging.StreamHandler()
+        logger.addHandler(added_handler)
+
+        handler = _Handler(logging.INFO)
+        self._call_fut(handler)
+        self.assertNotIn(added_handler, logger.handlers)
+        # handler should be removed from logger
+        self.assertEqual(len(logger.handlers), 1)
+
+    @patch.dict("os.environ", {envar: "1" for envar in _GAE_ENV_VARS})
+    def test_remove_handlers_gae(self):
+        logger = logging.getLogger()
+        # add fake handler
+        added_handler = logging.StreamHandler()
+        logger.addHandler(added_handler)
+
+        handler = _Handler(logging.INFO)
+        self._call_fut(handler)
+        self.assertNotIn(added_handler, logger.handlers)
+        # handler should be removed from logger
+        self.assertEqual(len(logger.handlers), 1)
+
+    def test_keep_handlers_others(self):
+        # mock non-cloud environment
+        patch = mock.patch(
+            "google.cloud.logging_v2.handlers._monitored_resources.retrieve_metadata_server",
+            return_value=None,
+        )
+        with patch:
+            # add fake handler
+            added_handler = logging.StreamHandler()
+            logger = logging.getLogger()
+            logger.addHandler(added_handler)
+
+            handler = _Handler(logging.INFO)
+            self._call_fut(handler)
+            # added handler should remain in logger
+            self.assertIn(added_handler, logger.handlers)
+
+    def setUp(self):
+        self._handlers_cache = logging.getLogger().handlers[:]
+
+    def tearDown(self):
+        # cleanup handlers
+        logging.getLogger().handlers = self._handlers_cache[:]
+
+
+class _Handler(object):
+    def __init__(self, level):
+        self.level = level
+
+    def acquire(self):
+        pass  # pragma: NO COVER
+
+    def release(self):
+        pass  # pragma: NO COVER
+
+
+class _Client(object):
+    def __init__(self, project):
+        self.project = project
+
+
+class _Transport(object):
+    def __init__(self, client, name):
+        self.client = client
+        self.name = name
+
+    def send(
+        self,
+        record,
+        message,
+        resource,
+        labels=None,
+        trace=None,
+        span_id=None,
+        http_request=None,
+    ):
+        self.send_called_with = (
+            record,
+            message,
+            resource,
+            labels,
+            trace,
+            span_id,
+            http_request,
+        )
