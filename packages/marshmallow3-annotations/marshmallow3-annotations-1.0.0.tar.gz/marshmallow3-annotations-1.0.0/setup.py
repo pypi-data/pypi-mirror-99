b14bf30c5c4058bcd8a68171400f8059aad72995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:19 +0000

    Add package marshmallow3-annotations/1.0.0/marshmallow3-annotations-1.0.0.tar.gz

---
+++ packages/marshmallow3-annotations/marshmallow3-annotations-1.0.0.tar.gz/marshmallow3-annotations-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+setup(
+    name="marshmallow3-annotations",
+    version="1.0.0",
+    author="Alec Nikolas Reiter",
+    author_email="alecreiter@gmail.com",
+    packages=find_packages("src", exclude=["test"]),
+    package_dir={"": "src"},
+    package_data={"": ["LICENSE", "README.md", "CHANGELOG"]},
+    include_package_data=True,
+    description="Marrying marshmallow3 and annotations",
+    long_description=long_description,
+    install_requires=["marshmallow>=3.0.0"],
+    license="MIT",
+    zip_safe=False,
+    url="https://github.com/dkunitsk/marshmallow3-annotations",
+    keywords=["annotations", "marshmallow"],
+    classifiers=[
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    extras_require={"attrs": ["attrs"]},
+)
