52a06783430fdec5c64107ece8045d1b969c150f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:07 +0000

    Add package udata/2.6.3.dev20380/udata-2.6.3.dev20380-py2.py3-none-any.whl

---
+++ packages/udata/udata-2.6.3.dev20380-py2.py3-none-any.whl/udata/static/home.902c340ece8066c3a08e.js
@@ -0,0 +1,2 @@
+webpackJsonp([45],{0:function(e,u,t){"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}var r=t(80),a=_interopRequireDefault(r);t(964);var i=t(5),n=_interopRequireDefault(i),f=t(31),l=_interopRequireDefault(f),o=t(25),d=_interopRequireDefault(o);t(427),new l.default({mixins:[a.default],ready:function(){n.default.debug("Home page"),(0,d.default)(".carousel").carousel()}})},964:966});
+//# sourceMappingURL=home.902c340ece8066c3a08e.js.map
---
+++ packages/udata/udata-2.6.3.dev20380-py2.py3-none-any.whl/udata/static/site.902c340ece8066c3a08e.js
@@ -0,0 +1,2 @@
+webpackJsonp([44],{0:function(e,t,u){"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}u(961);var i=u(80),n=_interopRequireDefault(i),r=u(5),a=_interopRequireDefault(r),f=u(31),l=_interopRequireDefault(f);new l.default({mixins:[n.default],ready:function(){a.default.debug("Site page")}})},961:966});
+//# sourceMappingURL=site.902c340ece8066c3a08e.js.map
