81431e0c599ff603461d56320998f82e5dd72919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:54 +0000

    Add package static-downsampler/0.0.1/static_downsampler-0.0.1.tar.gz

---
+++ packages/static-downsampler/static_downsampler-0.0.1.tar.gz/static_downsampler-0.0.1/setup.py
@@ -0,0 +1,25 @@
+""" setup for static_downsampler """
+import setuptools
+
+
+INSTALL_REQUIRES = ["numpy", "xarray", "dask", "netCDF4"]
+TESTS_REQUIRE = ['pytest >= 2.8', 'pytest_datafiles']
+
+setuptools.setup(
+    name="static_downsampler",
+    version="0.0.1",
+    author="Raphael Dussin",
+    author_email="raphael.dussin@gmail.com",
+    description=("A tool to downsample MOM static files"),
+    license="GPLv3",
+    keywords="",
+    url="https://github.com/raphaeldussin/static_downsampler",
+    packages=["static_downsampler"],
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    scripts=[
+        "static_downsampler/downsample.py",
+        "static_downsampler/create_hgrid_d2.py",
+        "static_downsampler/make_basin_mask_d2.py",
+    ],
+)
