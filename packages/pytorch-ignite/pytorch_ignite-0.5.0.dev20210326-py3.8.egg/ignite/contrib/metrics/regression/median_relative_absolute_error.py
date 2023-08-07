9d701a052cf4ff4302cd2bf39f1e5e4aaa41c7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:44 +0000

    Add package pytorch-ignite/0.5.0.dev20210326/pytorch_ignite-0.5.0.dev20210326-py3.8.egg

---
+++ packages/pytorch-ignite/pytorch_ignite-0.5.0.dev20210326-py3.8.egg/ignite/contrib/metrics/regression/median_relative_absolute_error.py
@@ -0,0 +1,43 @@
+from typing import Callable
+
+import torch
+
+from ignite.metrics import EpochMetric
+
+
+def median_relative_absolute_error_compute_fn(y_pred: torch.Tensor, y: torch.Tensor) -> float:
+    e = torch.abs(y.view_as(y_pred) - y_pred) / torch.abs(y.view_as(y_pred) - torch.mean(y))
+    return torch.median(e).item()
+
+
+class MedianRelativeAbsoluteError(EpochMetric):
+    r"""Calculates the Median Relative Absolute Error.
+
+    .. math::
+        \text{MdRAE} = \text{MD}_{j=1,n} \left( \frac{|A_j - P_j|}{|A_j - \bar{A}|} \right)
+
+    where :math:`A_j` is the ground truth and :math:`P_j` is the predicted value.
+
+    More details can be found in `Botchkarev 2018`__.
+
+    - ``update`` must receive output of the form ``(y_pred, y)`` or ``{'y_pred': y_pred, 'y': y}``.
+    - `y` and `y_pred` must be of same shape `(N, )` or `(N, 1)` and of type `float32`.
+
+    .. warning::
+
+        Current implementation stores all input data (output and target) in as tensors before computing a metric.
+        This can potentially lead to a memory error if the input data is larger than available RAM.
+
+
+    __ https://arxiv.org/abs/1809.03006
+
+    Args:
+        output_transform: a callable that is used to transform the
+            :class:`~ignite.engine.engine.Engine`'s ``process_function``'s output into the
+            form expected by the metric. This can be useful if, for example, you have a multi-output model and
+            you want to compute the metric with respect to one of the outputs.
+            By default, metrics require the output as ``(y_pred, y)`` or ``{'y_pred': y_pred, 'y': y}``.
+    """
+
+    def __init__(self, output_transform: Callable = lambda x: x):
+        super(MedianRelativeAbsoluteError, self).__init__(median_relative_absolute_error_compute_fn, output_transform)
