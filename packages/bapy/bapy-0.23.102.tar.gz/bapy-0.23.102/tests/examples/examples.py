9b2cb7ed2b7af5932c1be62e0bc0a3c39669c4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:03 +0000

    Add package bapy/0.23.102/bapy-0.23.102.tar.gz

---
+++ packages/bapy/bapy-0.23.102.tar.gz/bapy-0.23.102/tests/examples/examples.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Module."""
+import pytest
+import smtplib
+
+
+@pytest.fixture(scope="module", params=["smtp.gmail.com", "mail.python.org"])
+def smtp_connection(request):
+    smtp_connection = smtplib.SMTP(request.param, 587, timeout=5)
+    yield smtp_connection
+    print("finalizing {}".format(smtp_connection))
+    smtp_connection.close()
+
+#
+
+
+@pytest.fixture(params=[0, 1], ids=["spam", "ham"])
+def a(request):
+    return request.param
+
+
+def test_a(a):
+    pass
+
+#
+
+def idfn(fixture_value):
+    if fixture_value == 0:
+        return "eggs"
+    else:
+        return None
+
+
+
+@pytest.fixture(params=[0, 1], ids=idfn)
+def b(request):
+    return request.param
+
+
+def test_b(b):
+    pass
+
+#
+
+@pytest.fixture(params=[0, 1, pytest.param(2, marks=pytest.mark.skip)])
+def data_set(request):
+    return request.param
+
+
+def test_data(data_set):
+    pass
+
+#
+
+from datetime import datetime, timedelta
+
+testdata = [
+    (datetime(2001, 12, 12), datetime(2001, 12, 11), timedelta(1)),
+    (datetime(2001, 12, 11), datetime(2001, 12, 12), timedelta(-1)),
+]
+
+
+@pytest.mark.parametrize("a,b,expected", testdata)
+def test_timedistance_v0(a, b, expected):
+    diff = a - b
+    assert diff == expected
+
+
+@pytest.mark.parametrize("a,b,expected", testdata, ids=["forward", "backward"])
+def test_timedistance_v1(a, b, expected):
+    diff = a - b
+    assert diff == expected
+
+
+def idfn(val):
+    if isinstance(val, (datetime,)):
+        # note this wouldn't show any hours/minutes/seconds
+        return val.strftime("%Y%m%d")
+
+
+@pytest.mark.parametrize("a,b,expected", testdata, ids=idfn)
+def test_timedistance_v2(a, b, expected):
+    diff = a - b
+    assert diff == expected
+
+
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        pytest.param(
+            datetime(2001, 12, 12), datetime(2001, 12, 11), timedelta(1), id="forward"
+        ),
+        pytest.param(
+            datetime(2001, 12, 11), datetime(2001, 12, 12), timedelta(-1), id="backward"
+        ),
+    ],
+)
+def test_timedistance_v3(a, b, expected):
+    diff = a - b
+    assert diff == expected
+
+
+#
+
+def pytest_generate_tests(metafunc):
+    if "db" in metafunc.fixturenames:
+        metafunc.parametrize("db", ["d1", "d2"], indirect=True)
+
+
+class DB1:
+    "one database object"
+
+
+class DB2:
+    "alternative database object"
+
+
+@pytest.fixture
+def db(request):
+    if request.param == "d1":
+        return DB1()
+    elif request.param == "d2":
+        return DB2()
+    else:
+        raise ValueError("invalid internal test config")
+
+
+#
+
+@pytest.fixture
+def fixt(request):
+    return request.param * 3
+
+
+@pytest.mark.parametrize("fixt", ["a", "b"], indirect=True)
+def test_indirect(fixt):
+    assert len(fixt) == 3
+
+
+#
+
+@pytest.fixture(scope="function")
+def x(request):
+    return request.param * 3
+
+
+@pytest.fixture(scope="function")
+def y(request):
+    return request.param * 2
+
+
+@pytest.mark.parametrize("x, y", [("a", "b")], indirect=["x"])
+def test_indirect(x, y):
+    assert x == "aaa"
+    assert y == "b"
+
+
+#
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        ("3+5", 8),
+        pytest.param("1+7", 8, marks=pytest.mark.basic),
+        pytest.param("2+4", 6, marks=pytest.mark.basic, id="basic_2+4"),
+        pytest.param(
+            "6*9", 42, marks=[pytest.mark.basic, pytest.mark.xfail], id="basic_6*9"
+        ),
+    ],
+)
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
+#
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
+#
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [("3+5", 8), ("2+4", 6), pytest.param("6*9", 42, marks=pytest.mark.xfail)],
+)
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
