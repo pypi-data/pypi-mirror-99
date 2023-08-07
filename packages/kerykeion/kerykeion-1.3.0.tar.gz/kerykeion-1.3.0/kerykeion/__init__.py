c1ceb17692d89bc588ae3dc54fd0770a919a8461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:07 +0000

    Add package kerykeion/1.3.0/kerykeion-1.3.0.tar.gz

---
+++ packages/kerykeion/kerykeion-1.3.0.tar.gz/kerykeion-1.3.0/kerykeion/__init__.py
@@ -0,0 +1,9 @@
+import sys
+from os.path import dirname
+from kerykeion.astrocore import AstroData, Calculator
+from kerykeion.output_exemple import output
+from kerykeion.utilities import kr_settings as settings
+from kerykeion.utilities.general import print_settings_path
+from kerykeion.utilities.charts import MakeSvgInstance as MakeSvgInstance
+
+sys.path.append(dirname(__file__))
---
+++ packages/kerykeion/kerykeion-1.3.0.tar.gz/kerykeion-1.3.0/setup.py
@@ -0,0 +1,28 @@
+"""
+    This is part of Kerykeion (C) 2020 Giacomo Battaglia
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kerykeion",
+    version="1.3.0",
+    author="Giacomo Battaglia",
+    author_email="battaglia.giacomo@yahoo.it",
+    description="An astrology library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/g-battaglia/kerykeion",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires = ['pyswisseph', 'pytz', 'jsonpickle'],
+)
