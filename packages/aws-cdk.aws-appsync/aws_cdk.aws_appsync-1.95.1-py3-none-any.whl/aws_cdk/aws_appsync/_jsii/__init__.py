b4d958c8adc7ad18f61d71cbdc6a4706bd72e9c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:10 +0000

    Add package aws-cdk.aws-appsync/1.95.1/aws_cdk.aws_appsync-1.95.1-py3-none-any.whl

---
+++ packages/aws-cdk.aws-appsync/aws_cdk.aws_appsync-1.95.1-py3-none-any.whl/aws_cdk/aws_appsync/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_cognito._jsii
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_rds._jsii
+import aws_cdk.aws_s3_assets._jsii
+import aws_cdk.aws_secretsmanager._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/aws-appsync", "1.95.1", __name__[0:-6], "aws-appsync@1.95.1.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
