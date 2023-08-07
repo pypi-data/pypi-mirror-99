8c2591d0cccdef5ee499795567cdf527c1222d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:47 +0000

    Add package behavior-machine/0.2.4/behavior_machine-0.2.4.tar.gz

---
+++ packages/behavior-machine/behavior_machine-0.2.4.tar.gz/behavior_machine-0.2.4/behavior_machine/library/sequential_state.py
@@ -0,0 +1,75 @@
+import threading
+from ..core import StateStatus, State, NestedState
+from ..board import Board
+import typing
+
+
+class SequentialState(NestedState):
+
+    _children: typing.List[State]
+    _curr_child: State
+    _lock: threading.RLock
+
+    def __init__(self, name, children: typing.List[State] = None):
+        super(SequentialState, self).__init__(name)
+        self._children = [] if children is None else children
+        self._curr_child = None
+        self._lock = threading.RLock()
+
+    def add_children(self, state: State):
+        self._children.append(state)
+
+    def pre_execute(self):
+        # make sure all children states have the correct states
+        child: State
+        for child in self._children:
+            child._status = StateStatus.NOT_RUNNING
+
+    def execute(self, board: Board):
+        # execute each children one by one in order
+        for child in self._children:
+            with self._lock:
+                # once we in lock, check if we are interrupted
+                if self._interupted_event.is_set():
+                    return StateStatus.INTERRUPTED
+                self._curr_child = child
+                self._curr_child.start(board)
+            self._curr_child.wait()
+            # check if we are done because of interrupt
+            if self._interupted_event.is_set():
+                return StateStatus.INTERRUPTED
+            status = self._curr_child._status
+            if status == StateStatus.EXCEPTION:
+                self.propergate_exception_information(self._curr_child)
+                return StateStatus.EXCEPTION
+            elif status != StateStatus.SUCCESS:
+                return status
+        return StateStatus.SUCCESS
+
+    def interrupt(self, timeout=None):
+        self._interupted_event.set()
+        with self._lock:
+            try:
+                self._curr_child.interrupt(timeout=timeout)
+            except (AttributeError):
+                # Attribution error happens if child node is empty
+                # Possible race condition at the beginning where interupt gets call before we start initializing
+                pass
+        # wait for thread to end
+        self._run_thread.join(timeout)
+        return not self._run_thread.is_alive()
+
+    def tick(self, board):
+        next_state = super().tick(board)
+        if next_state == self:
+            with self._lock:
+                self._curr_child.tick(board)
+        return next_state
+
+    def get_debug_info(self) -> typing.Dict[str, typing.Any]:
+
+        self_info = super().get_debug_info()
+        self_info['children'] = []
+        for child in self._children:
+            self_info['children'].append(child.get_debug_info())
+        return self_info
---
+++ packages/behavior-machine/behavior_machine-0.2.4.tar.gz/behavior_machine-0.2.4/tests/board_test.py
@@ -0,0 +1,199 @@
+from behavior_machine.library import WaitState, IdleState
+import pytest
+
+from behavior_machine.board import Board
+from behavior_machine.core import State, StateStatus, Machine
+
+
+class SetState(State):
+
+    _val: str
+    _key: str
+
+    def __init__(self, name, key, val):
+        super().__init__(name)
+        self._val = val
+        self._key = key
+
+    def execute(self, board):
+        board.set("key", self._val)
+        return StateStatus.SUCCESS
+
+
+class GetState(State):
+
+    _key: str
+
+    def __init__(self, name, key):
+        super().__init__(name)
+        self._key = key
+
+    def execute(self, board):
+        value = board.get(self._key)
+        board.set("output", value)
+        return StateStatus.SUCCESS
+
+
+class DummyState(State):
+    def execute(self, board):
+        return StateStatus.SUCCESS
+
+
+def test_external_set_get():
+    b = Board()
+    b.set("x", "key1")
+    assert b.get('x') == 'key1'
+
+
+def test_replaced_set():
+    b = Board()
+    b.set("x", "hello")
+    b.set("x", "world")
+
+    assert b.get('x') == 'world'
+
+
+def test_get_non_exist():
+    b = Board()
+    assert b.get('key') is None
+
+
+def test_board_set_deep_copy():
+
+    b = Board()
+    test_obj = {
+        'hello': 'world'
+    }
+    b.set('obj', test_obj, deep_copy=False)
+    test_obj['hello'] = 'test'
+    assert b.get('obj')['hello'] == 'test'
+    assert b.get('obj')['hello'] != 'world'
+
+
+def test_board_get_deep_copy():
+
+    b = Board()
+    test_obj = {
+        'hello': 'world'
+    }
+    b.set('obj', test_obj, deep_copy=False)
+    rtn_obj = b.get('obj', False)
+    assert rtn_obj['hello'] == 'world'
+    test_obj['hello'] = 'test'
+    assert rtn_obj['hello'] == 'test'
+
+
+def test_board_exist_func():
+    b = Board()
+    b.set('hello', 'XXX')
+    assert b.exist('hello')
+    assert not b.exist('hello2')
+    assert not b.exist('hell')
+
+
+def test_internal_set():
+    s1 = DummyState('s1')
+    set_state = SetState('set', 'key', 'hello')
+    s1.add_transition_on_success(set_state)
+    exe = Machine("xe", s1)
+    b = Board()
+    exe.start(b, manual_exec=True)
+    exe.update(b, wait=True)
+    assert b.get('key') == 'hello'
+
+
+def test_internal_get():
+    s1 = DummyState('s1')
+    get_state = GetState('set', 'key')
+    s1.add_transition_on_success(get_state)
+    exe = Machine("xe", s1)
+    b = Board()
+    b.set("key", "hello_get")
+    exe.start(b, manual_exec=True)
+    exe.update(b, wait=True)
+    assert b.get('output') == 'hello_get'
+
+
+def test_object_set_get(capsys):
+
+    class SetState(State):
+        def execute(self, board: Board):
+            obj = {
+                'hello': [1, 2, 3],
+                'name': {
+                    'first': 'test'
+                }
+            }
+            board.set('obj', obj)
+            obj['name'] = {}
+            return StateStatus.SUCCESS
+
+    class GetState(State):
+        def execute(self, board):
+            obj = board.get('obj')
+            assert obj['hello'] == [1, 2, 3]
+            assert obj['name']['first'] == 'test'
+            return StateStatus.SUCCESS
+
+    s = SetState('s')
+    g = GetState('g')
+    w = WaitState('w', 1)
+
+    s.add_transition_on_success(w)
+    w.add_transition_on_success(g)
+    exe = Machine('xe', s, end_state_ids=['g'])
+    exe.run()
+    assert exe.is_end()
+    assert exe._curr_state._status == StateStatus.SUCCESS
+    #assert exe._curr_state.checkStatus(StateStatus.SUCCESS)
+
+
+def test_object_get_in_transition(capsys):
+
+    class SetState(State):
+        def execute(self, board: Board):
+            obj = {
+                'hello': [1, 2, 3],
+                'name': {
+                    'first': 'test'
+                }
+            }
+            board.set('obj', obj)
+            obj = {}
+            return StateStatus.SUCCESS
+
+    s = SetState('s')
+    w = WaitState('w', 1)
+    i = IdleState('i')
+    end = IdleState('end')
+
+    s.add_transition_on_success(w)
+    w.add_transition_on_success(i)
+    i.add_transition(lambda state, board: board.get('obj')
+                     ['name']['first'] == 'test', end)
+    exe = Machine('xe', s, end_state_ids=['end'])
+    exe.run()
+    assert exe.is_end()
+    # Idle state returns RUNNING instead of SUCCESS
+    assert exe._curr_state._status == StateStatus.RUNNING
+
+
+def test_load_dictionary():
+
+    b = Board()
+    check_dict = {
+        'k1': 'Hello',
+        'k2': 800
+    }
+    b.load(check_dict)
+    assert b.get('k1') == 'Hello'
+    assert b.get('k2') == 800
+    check_dict['k2'] = 100
+    assert b.get('k2') == 800
+    # try replacing a previous value
+    check_dict_2 = {
+        'k2':1000
+    }
+    b.load(check_dict_2)
+    assert b.get('k2') == 1000
+
