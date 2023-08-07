bd2ca475f4275027059ff11e0fd54df066c56ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:09 +0000

    Add package hypixelaPY/1.6.3/hypixelaPY-1.6.3-py3-none-any.whl

---
+++ packages/hypixelaPY/hypixelaPY-1.6.3-py3-none-any.whl/hypixelaPY/client.py
@@ -0,0 +1,204 @@
+"""
+MIT License
+
+Copyright (c) 2020 Myer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from . import hypixel, mojang
+from .exceptions import NoInputError, NoPlayerFoundError, NoGuildFoundError
+
+
+async def Hypixel(api: str):
+    client = HypixelClient(api)
+    _ = await client.key.get()
+    return client
+
+
+class HypixelClient:
+    """
+    Main client object of the library. This saves the API key provided and creates separate objects for the other
+    Hypixel API methods
+    """
+
+    def __init__(self, api: str):
+        self.api = api
+        # this could error with InvalidAPIKeyError
+        # this acts as a check to see if the API key provided was valid and also will contain the stats of the key
+        self.player = Player(self.api)
+        self.guild = Guild(self.api)
+        self.key = Key(self.api)
+        self.leaderboards = Leaderboards(self.api)
+
+
+class Player:
+    """
+    Wrapper on the /player method of the Hypixel API
+    """
+
+    def __init__(self, api: str):
+        self.api = api
+
+    async def get(self, *, uuid: str = "", name: str = "", query: str = ""):
+        """
+        Takes various inputs and tries to get a user
+        This prioritizes UUID, then name, then an input wildcard that could be either name or UUID
+
+        :param uuid: A Minecraft account UUID
+        :param name: A Minecraft account's name that will be converted to a
+        UUID with the Mojang API
+        :param query: A wildcard that could be either a UUID or a name. The library will
+        try to interpret this as a uuid first, and will fallback to interpreting as a name
+        """
+        if bool(uuid):
+            return await self.get_by_uuid(uuid)
+        elif bool(name):
+            return await self.get_by_name(name)
+        elif bool(query):
+            try:
+                return await self.get_by_uuid(query)
+            except NoPlayerFoundError:
+                return await self.get_by_name(query)  # has the possibly of erroring with NoPlayerFoundError
+        else:
+            raise NoInputError
+
+    async def get_by_uuid(self, uuid: str):
+        """
+        Directly queries Hypixel with a provided UUID
+
+        :param uuid: A Minecraft account UUID
+        """
+        return await hypixel.get_player_by_uuid(uuid, self.api)
+
+    async def get_by_name(self, name: str):
+        """
+        Queries Mojang for the UUID of the player name provided, then queries Hypixel with the newly obtained UUID
+
+        :param name: A Minecraft account's name
+        """
+        uuid = (await mojang.get_player_by_name(name)).uuid
+        return await hypixel.get_player_by_uuid(uuid, self.api)
+
+
+class Leaderboards:
+    def __init__(self, api):
+        self.api = api
+
+    async def get(self):
+        return await hypixel.get_leaderboards(self.api)
+
+
+class Guild:
+    def __init__(self, api):
+        self.api = api
+
+    async def get(self, *, name: str = "", uuid: str = "", query: str = ""):
+        if bool(name):
+            return await self.get_by_name(name)
+        elif bool(uuid):
+            return await self.get_by_uuid(uuid)
+        elif bool(query):
+            try:
+                return await self.get_by_name(query)
+            except NoGuildFoundError:
+                return await self.get_by_uuid(query)  # has the possibility of erroring with NoGuildFoundError
+        else:
+            raise NoInputError
+
+    async def get_by_uuid(self, uuid: str):
+        """
+        Gets a guild from a player UUID
+
+        :param uuid: A player's uuid
+        :return: Guild
+        """
+        return await hypixel.get_guild_by_player_uuid(uuid, self.api)
+
+    async def get_by_name(self, name: str):
+        """
+        Gets a guild from a guild name
+
+        :param name: A guild name
+        :return: Guild
+        """
+        return await hypixel.get_guild_by_name(name, self.api)
+
+
+class Key:
+    def __init__(self, api):
+        self.api = api
+
+    async def get(self):
+        return await hypixel.get_api_stats(self.api)
+
+
+class Mojang:
+    async def get(self, *, uuid: str = "", name: str = "", query: str = ""):
+        """
+        Takes various inputs and tries to get a user
+        This prioritizes UUID, then name, then an input wildcard that could be either name or UUID
+
+        :param uuid: A Minecraft account UUID
+        :param name: A Minecraft account's name that will be converted to a
+        UUID with the Mojang API
+        :param query: A wildcard that could be either a UUID or a name. The library will
+        try to interpret this as a uuid first, and will fallback to interpreting as a name
+        """
+        if bool(uuid):
+            return await self.get_by_uuid(uuid)
+        elif bool(name):
+            return await self.get_by_name(name)
+        elif bool(query):
+            try:
+                return await self.get_by_uuid(query)
+            except NoPlayerFoundError:
+                return await self.get_by_name(query)  # has the possibility of erroring with NoPlayerFoundError
+        else:
+            raise NoInputError
+
+    @staticmethod
+    async def get_by_name(name: str):
+        """
+        Gets the UUID of a player from a name
+
+        :param name: A Minecraft account's name
+        :return: MojangPlayer
+        """
+        return await mojang.get_player_by_name(name)
+
+    @staticmethod
+    async def get_by_uuid(uuid: str):
+        """
+        Gets the UUID of a player from a name
+
+        :param uuid: A Minecraft account's UUID
+        :return: MojangPlayer
+        """
+        return await mojang.get_player_by_uuid(uuid)
+
+    @staticmethod
+    async def get_name_history(uuid: str):
+        """
+        Gets the name history of a Minecraft account from a UUID
+
+        :param uuid: A Minecraft account's UUID
+        :return: NameHistory
+        """
+        return await mojang.get_player_name_history(uuid)
---
+++ packages/hypixelaPY/hypixelaPY-1.6.3-py3-none-any.whl/hypixelaPY/objects/objects.py
@@ -0,0 +1,176 @@
+"""
+MIT License
+
+Copyright (c) 2020 Myer
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import datetime
+
+from .bedwars import Bedwars
+from .blitz import Blitz
+from .duels import Duels
+from .skywars import Skywars
+from .quake import Quake
+from .. import utils
+
+
+class APIKey:
+    def __init__(self, data):
+        self.key = data.get("record", {}).get("key")
+        self.owner = data.get("record", {}).get("owner")
+        self.limit = data.get("record", {}).get("limit")
+        self.queries = APIKeyQueries(data)
+
+
+class APIKeyQueries:
+    def __init__(self, data):
+        self.minute = data.get("record", {}).get("queriesInPastMin")
+        self.all = data.get("record", {}).get("totalQueries")
+
+
+class MojangPlayer:
+    """
+    The response from a call to the Mojang API
+    Represents a player
+    """
+
+    def __init__(self, data):
+        self.name = data["name"]
+        self.uuid = data["id"]
+
+    def __str__(self):
+        return self.name
+
+
+class NameHistory:
+    """
+    The response from a call to the Mojang namehistory API
+    Represents a player's namehistory as an iterable
+    """
+
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data) - 1:
+            raise StopIteration
+        self.index += 1
+        return NameHistoryEntry(self.data[self.index])
+
+
+class NameHistoryEntry:
+    """
+    Represents a name in the namehistory of a player
+    """
+
+    def __init__(self, data):
+        self.name = data["name"]
+        self.time = datetime.datetime.fromtimestamp(data["changedToAt"] / 1000)
+
+    def __str__(self):
+        return self.name
+
+
+class HypixelPlayer:
+    """
+    The response from a call to the Hypixel player endpoint
+    Represents a player
+    """
+
+    def __init__(self, data):
+        self.name = data.get("player", {}).get("displayname")
+        self.uuid = data.get("player", {}).get("uuid")
+        self.rank = Rank(data)
+        self.display = utils.get_profile_display(self.name, self.rank)
+        self.level = Level(data.get("player", {}).get("networkExp", 0))
+        self.karma = data.get("player", {}).get("karma", 0)
+        self.achievement_points = data.get("player", {}).get("achievementPoints", 0)
+        self.logins = Logins(data)
+        self.social = Social(data)
+        self.bedwars = Bedwars(data)
+        self.skywars = Skywars(data)
+        self.blitz = Blitz(data)
+        self.duels = Duels(data)
+        self.quake = Quake(data)
+
+    def __str__(self):
+        return self.name
+
+
+class Logins:
+    """
+    The login times of a player
+    """
+
+    def __init__(self, data):
+        self.first = datetime.datetime.fromtimestamp(data.get("player", {}).get("firstLogin", 0) / 1000)
+        self.last = datetime.datetime.fromtimestamp(data.get("player", {}).get("lastLogin", 0) / 1000)
+
+
+class Social:
+    """
+    The social media links of a player
+    """
+
+    def __init__(self, data):
+        self.twitter = data.get("player", {}).get("socialMedia", {}).get("links", {}).get("TWITTER")
+        self.youtube = data.get("player", {}).get("socialMedia", {}).get("links", {}).get("YOUTUBE")
+        self.instagram = data.get("player", {}).get("socialMedia", {}).get("links", {}).get("INSTAGRAM")
+        self.twitch = data.get("player", {}).get("socialMedia", {}).get("links", {}).get("TWITCH")
+        self.discord = data.get("player", {}).get("socialMedia", {}).get("links", {}).get("DISCORD")
+        self.hypixel_forums = data.get("player", {}).get("socialMedia", {}).get("links", {}).get("HYPIXEL")
+
+
+class Rank:
+    """
+    The rank data of a player
+    """
+
+    def __init__(self, data):
+        self.name = utils.get_rank(
+            data.get("player", {}).get("rank"),
+            data.get("player", {}).get("prefix"),
+            data.get("player", {}).get("monthlyPackageRank"),
+            data.get("player", {}).get("newPackageRank"),
+            data.get("player", {}).get("packageRank"),
+        )
+        self.color = utils.get_rank_color(self.name)
+
+    def __str__(self):
+        return self.name
+
+    def __bool__(self):
+        return bool(self.name)
+
+
+class Level:
+    def __init__(self, experience):
+        self.exact = utils.get_network_level_exact(experience)
+        self.level = utils.get_network_level(experience)
+        self.next = self.level + 1
+        self.percentage = utils.get_level_percentage(self.exact)
+
+    def __int__(self):
+        return self.level
