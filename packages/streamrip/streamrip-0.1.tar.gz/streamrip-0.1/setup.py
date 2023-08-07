e2d610e343e00b15aa083c1aa7e6d5932ea67f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:38:56 +0000

    Add package streamrip/0.1/streamrip-0.1.tar.gz

---
+++ packages/streamrip/streamrip-0.1.tar.gz/streamrip-0.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import find_packages, setup
+
+pkg_name = "streamrip"
+
+
+def read_file(fname):
+    with open(fname, "r") as f:
+        return f.read()
+
+
+requirements = read_file("requirements.txt").strip().split()
+
+
+# https://github.com/pypa/sampleproject/blob/main/setup.py
+setup(
+    name=pkg_name,
+    version="0.1",
+    author='Nathan',
+    author_email='nathanthomas707@gmail.com',
+    keywords='lossless, hi-res, qobuz, tidal, deezer, audio, convert',
+    description="A stream downloader for Qobuz, Tidal, and Deezer.",
+    long_description=read_file("README.md"),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    py_modules=["streamrip"],
+    entry_points={
+        "console_scripts": [
+            "rip = streamrip.cli:main",
+            "streamrip = streamrip.cli:main",
+        ],
+    },
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3.7",
+    project_urls={
+        "Source": "https://github.com/nathom/streamrip",
+        "Bug Reports": "https://github.com/nathom/streamrip/issues",
+    },
+)
+
+# rm -f dist/*
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
---
+++ packages/streamrip/streamrip-0.1.tar.gz/streamrip-0.1/streamrip/config.py
@@ -0,0 +1,156 @@
+import copy
+import logging
+import os
+from pathlib import Path
+from pprint import pformat
+
+from ruamel.yaml import YAML
+
+from .constants import CONFIG_PATH, DOWNLOADS_DIR, FOLDER_FORMAT, TRACK_FORMAT
+from .exceptions import InvalidSourceError
+
+yaml = YAML()
+
+
+logger = logging.getLogger(__name__)
+
+
+class Config:
+    """Config class that handles command line args and config files.
+
+    Usage:
+    >>> config = Config('test_config.yaml')
+
+    If test_config was already initialized with values, this will load them
+    into `config`. Otherwise, a new config file is created with the default
+    values.
+
+    >>> config.update_from_cli(**args)
+
+    This will update the config values based on command line args.
+    """
+
+    defaults = {
+        "qobuz": {
+            "enabled": True,
+            "email": None,
+            "password": None,
+            "app_id": "",  # Avoid NoneType error
+            "secrets": [],
+        },
+        "tidal": {"enabled": True, "email": None, "password": None},
+        "deezer": {"enabled": True},
+        "database": {"enabled": True, "path": None},
+        "conversion": {
+            "enabled": False,
+            "codec": None,
+            "sampling_rate": None,
+            "bit_depth": None,
+        },
+        "filters": {
+            "extras": False,
+            "repeats": False,
+            "non_albums": False,
+            "features": False,
+            "non_studio_albums": False,
+            "non_remaster": False,
+        },
+        "downloads": {"folder": DOWNLOADS_DIR, "quality": 7},
+        "metadata": {
+            "embed_cover": True,
+            "large_cover": False,
+            "default_comment": None,
+            "remove_extra_tags": False,
+        },
+        "path_format": {"folder": FOLDER_FORMAT, "track": TRACK_FORMAT},
+    }
+
+    def __init__(self, path: str = None):
+        # to access settings loaded from yaml file
+        self.file = copy.deepcopy(self.defaults)
+        self.session = copy.deepcopy(self.defaults)
+
+        if path is None:
+            self._path = CONFIG_PATH
+        else:
+            self._path = path
+
+        if not os.path.isfile(self._path):
+            logger.debug(f"Creating yaml config file at '{self._path}'")
+            self.dump(self.defaults)
+        else:
+            self.load()
+
+    def save(self):
+        self.dump(self.file)
+
+    def reset(self):
+        self.dump(self.defaults)
+
+    def load(self):
+        with open(self._path) as cfg:
+            for k, v in yaml.load(cfg).items():
+                self.file[k] = v
+                if hasattr(v, "copy"):
+                    self.session[k] = v.copy()
+                else:
+                    self.session[k] = v
+
+        logger.debug("Config loaded")
+        self.__loaded = True
+
+    def update_from_cli(self, **kwargs):
+        for category in (self.downloads, self.metadata, self.filters):
+            for key in category.keys():
+                if kwargs.get(key) is None:
+                    continue
+
+                # For debugging's sake
+                og_value = category[key]
+                new_value = kwargs[key] or og_value
+                category[key] = new_value
+
+                if og_value != new_value:
+                    logger.debug("Updated %s config key from args: %s", key, new_value)
+
+    def dump(self, info):
+        with open(self._path, "w") as cfg:
+            logger.debug("Config saved: %s", self._path)
+            yaml.dump(info, cfg)
+
+    @property
+    def tidal_creds(self):
+        return {
+            "email": self.file["tidal"]["email"],
+            "pwd": self.file["tidal"]["password"],
+        }
+
+    @property
+    def qobuz_creds(self):
+        return {
+            "email": self.file["qobuz"]["email"],
+            "pwd": self.file["qobuz"]["password"],
+            "app_id": self.file["qobuz"]["app_id"],
+            "secrets": self.file["qobuz"]["secrets"],
+        }
+
+    def creds(self, source: str):
+        if source == "qobuz":
+            return self.qobuz_creds
+        elif source == "tidal":
+            return self.tidal_creds
+        elif source == "deezer":
+            return dict()
+        else:
+            raise InvalidSourceError(source)
+
+    def __getitem__(self, key):
+        assert key in ("file", "defaults", "session")
+        return getattr(self, key)
+
+    def __setitem__(self, key, val):
+        assert key in ("file", "session")
+        setattr(self, key, val)
+
+    def __repr__(self):
+        return f"Config({pformat(self.session)})"
