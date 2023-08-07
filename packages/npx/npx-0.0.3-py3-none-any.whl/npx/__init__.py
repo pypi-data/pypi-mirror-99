8cd53cd103949bef23f5cf90513907832d81641b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:20 +0000

    Add package npx/0.0.3/npx-0.0.3-py3-none-any.whl

---
+++ packages/npx/npx-0.0.3-py3-none-any.whl/npx/__init__.py
@@ -0,0 +1,16 @@
+from .__about__ import __version__
+from ._krylov import cg, gmres, minres
+from ._main import add_at, dot, solve, subtract_at, sum_at, unique_rows
+
+__all__ = [
+    "__version__",
+    "dot",
+    "solve",
+    "sum_at",
+    "add_at",
+    "subtract_at",
+    "unique_rows",
+    "cg",
+    "gmres",
+    "minres",
+]
