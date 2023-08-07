ea65f273b7ebe9d53474870b1f7ecc87fb1b5721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:20 +0000

    Add package tasks/2.7.0/tasks-2.7.0.tar.gz

---
+++ packages/tasks/tasks-2.7.0.tar.gz/tasks-2.7.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='tasks',
+    version='2.7.0',
+    author='Paul Baecher',
+    description='A simple personal task queue to track todo items',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pb-/tasks',
+    packages=find_packages(),
+    install_requires=[
+        'datadispatch',
+    ],
+    entry_points={
+        'console_scripts': [
+            'tasks = tasks.main:run',
+        ],
+    },
+    scripts=[
+        'scripts/tasks-i3status',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
