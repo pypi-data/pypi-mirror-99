a3da5d5b4e82c85c569b1bd616dfdc40f260ed75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:07 +0000

    Add package jinjarecurse/0.0.2/jinjarecurse-0.0.2.tar.gz

---
+++ packages/jinjarecurse/jinjarecurse-0.0.2.tar.gz/jinjarecurse-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup
+
+README = pathlib.Path(__file__).parent / "README.md"
+README = README.read_text()
+
+setup(
+    name="jinjarecurse",
+    version="0.0.2",
+    metadata_version="2.1",
+    author="diginc",
+    author_email="adam@diginc.us",
+    description=("Jinja Recursive Templating from the CLI"),
+    license="MIT",
+    keywords="jinja templater",
+    url="https://github.com/diginc/Jinja-Recurse",
+    packages=["jinjarecurse"],
+    long_description_content_type='text/markdown',
+    long_description=README,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "docopt==0.6.2",
+        "jinja2==2.11.3",
+        "markupsafe==1.1.1",
+        "pathlib2==2.3.5",
+        "pyyaml==5.4",
+        "six==1.15.0",
+    ],
+    entry_points={
+        'console_scripts': ['jinjarecurse = jinjarecurse.main:main']
+    }
+)
