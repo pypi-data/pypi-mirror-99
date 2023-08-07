b9df880a599f8ff4cba1ec5cb80ab6636e16e0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:42 +0000

    Add package mechanica/0.0.25.dev1/mechanica-0.0.25.dev1-cp37-cp37m-win_amd64.whl

---
+++ packages/mechanica/mechanica-0.0.25.dev1-cp37-cp37m-win_amd64.whl/mechanica-0.0.25.dev1.data/purelib/mechanica/examples/epiboly.py
@@ -0,0 +1,58 @@
+import mechanica as m
+import numpy as np
+
+# potential cutoff distance
+cutoff = 8
+
+count = 3000
+
+# dimensions of universe
+dim=np.array([20., 20., 20.])
+center = dim / 2
+
+# new simulator, don't load any example
+m.Simulator(example="", dim=dim, cutoff=cutoff)
+
+class Big(m.Particle):
+    mass = 500000
+    radius = 3
+
+class Small(m.Particle):
+    mass = 5
+    radius = 0.2
+    target_temperature=0
+    dynamics = m.Overdamped
+
+pot_bs = m.Potential.soft_sphere(kappa=5, epsilon=20, r0=2.9, \
+    eta=3, tol = 0.1, min=0, max=9)
+pot_ss = m.Potential.soft_sphere(kappa=10, epsilon=0.000000001, r0=0.2, \
+    eta=2, tol = 0.05, min=0, max=3)
+
+# bind the potential with the *TYPES* of the particles
+m.Universe.bind(pot_bs, Big, Small)
+m.Universe.bind(pot_ss, Small, Small)
+
+# create a random force. In overdamped dynamcis, we neeed a random force to
+# enable the objects to move around, otherwise they tend to get trapped
+# in a potential
+rforce = m.forces.random(0, 0.05)
+
+# bind it just like any other force
+m.bind(rforce, Small)
+
+Big(position=center, velocity=[0., 0., 0.])
+
+for p in m.random_point(m.SolidSphere, count) * \
+    0.5 * Big.radius + center + [0, 0, 1.3 * Big.radius]:
+    Small(p)
+
+# run the simulator interactive
+m.Simulator.run()
+
+
+
+
+
+
+
+
---
+++ packages/mechanica/mechanica-0.0.25.dev1-cp37-cp37m-win_amd64.whl/mechanica-0.0.25.dev1.data/purelib/mechanica/examples/mitosis.dyn.py
@@ -0,0 +1,25 @@
+import mechanica as m
+import numpy as np
+
+# potential cutoff distance
+cutoff = 1
+
+# new simulator
+m.init(dim=[20., 20., 20.])
+
+pot = m.Potential.soft_sphere(kappa=10, epsilon=0.1,
+    r0=0.6, eta=3, tol = 0.1, min=0.05, max=4)
+
+class Cell(m.Particle):
+    mass = 20
+    target_temperature = 0
+    radius=0.5
+
+m.on_time(Cell.fission, period=1, distribution='exponential')
+
+m.Universe.bind(pot, Cell, Cell)
+
+Cell([10., 10., 10.])
+
+# run the simulator interactive
+m.Simulator.run()
