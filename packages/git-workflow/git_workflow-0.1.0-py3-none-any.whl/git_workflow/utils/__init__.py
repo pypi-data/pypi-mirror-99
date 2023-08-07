b3654b1fc3ba5d135179f32897160e5101ba1215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:53 +0000

    Add package git-workflow/0.1.0/git_workflow-0.1.0-py3-none-any.whl

---
+++ packages/git-workflow/git_workflow-0.1.0-py3-none-any.whl/git_workflow/utils/__init__.py
@@ -0,0 +1,6 @@
+"""Utility modules."""
+from . import repository
+from . import cmd
+from . import configs
+from . import files
+
---
+++ packages/git-workflow/git_workflow-0.1.0-py3-none-any.whl/git_workflow/workflow/__init__.py
@@ -0,0 +1,38 @@
+"""The workflow scripts"""
+from .start_branch import StartBranch
+from .set_template import SetTemplate
+from .unset_template import UnsetTemplate
+
+#: Maps command names to WorkflowBase subclasses
+commands = {
+    StartBranch.command: StartBranch,
+    SetTemplate.command: SetTemplate,
+    UnsetTemplate.command: UnsetTemplate,
+}
+
+
+def add_command_subparsers(subparsers, generic_parent_parser):
+    """Add subparsers for each workflow command.
+
+    :param subparsers: Subparsers object
+    :param generic_parent_parser: Instance of argparse.ArgumentParser used
+        as a parent for top level parser
+    """
+    for commandClass in commands.values():
+        commandClass.add_subparser(subparsers, generic_parent_parser)
+
+
+def run_command(repo, parser):
+    """Run a workflow command.
+
+    :param repo: Repo object
+    :param parser: ArgumentParser object
+    """
+    parsed_args = parser.parse_args()
+    command_class = commands.get(parsed_args.command, False)
+    if not command_class:
+        # exit_code = 1 # TODO implement? or raise specific exception
+        parser.print_help()
+    else:
+        command = command_class(repo, parser, parsed_args=parsed_args)
+        command.run()
