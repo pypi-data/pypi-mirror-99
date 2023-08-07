af3ba6a498c44303c674931af7bfe410ced22180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:03 +0000

    Add package read-env-9830/0.0.2/read_env_9830-0.0.2.tar.gz

---
+++ packages/read-env-9830/read_env_9830-0.0.2.tar.gz/read_env_9830-0.0.2/src/read_env_9830/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+__init__
+========
+"""
+
+__version__ = "0.0.2"
+
+# Defines what is exposed when importing this package.
+from .impl import get_env_value, get_env_path, get_env_bool, get_env_int, get_env_float
---
+++ packages/read-env-9830/read_env_9830-0.0.2.tar.gz/read_env_9830-0.0.2/src/read_env_9830/__main__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+# Import
+
+from read_env_9830 import something
+
+
+def main():
+    print(something())
+
+
+# Execution
+
+if __name__ == "__main__":
+    main()
