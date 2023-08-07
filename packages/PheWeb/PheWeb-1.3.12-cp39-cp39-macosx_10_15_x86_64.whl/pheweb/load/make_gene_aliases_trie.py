d84845ef29bcfaa73367a4d03af0b164d8c80470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:53 +0000

    Add package PheWeb/1.3.12/PheWeb-1.3.12-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/PheWeb/PheWeb-1.3.12-cp39-cp39-macosx_10_15_x86_64.whl/pheweb/load/make_gene_aliases_trie.py
@@ -0,0 +1,76 @@
+
+from ..utils import get_gene_tuples, PheWebError
+from ..file_utils import common_filepaths
+
+import os, re, json
+import urllib.request
+import marisa_trie
+
+def get_genenamesorg_ensg_aliases_map(ensgs_to_consider):
+    ensgs_to_consider = set(ensgs_to_consider)
+    r = urllib.request.urlopen('http://ftp.ebi.ac.uk/pub/databases/genenames/new/json/non_alt_loci_set.json')
+    data = r.read().decode('utf-8')
+    ensg_to_aliases = {}
+    for row in json.loads(data)['response']['docs']:
+        try:
+            if not row.get('ensembl_gene_id',None) or row['ensembl_gene_id'] not in ensgs_to_consider: continue
+            assert re.match(r'^ENSG[R0-9\.]+$', row['ensembl_gene_id']), row
+            aliases = [row['symbol']] + row.get('prev_symbol',[]) + row.get('alias_symbol',[])
+            aliases = [alias for alias in aliases if alias != '']
+            aliases = [alias for alias in aliases if re.match(r'^[-\._a-zA-Z0-9]+$', alias)]
+            # for alias in aliases: assert re.match(r'^[-\._a-zA-Z0-9]+$', alias), (alias, [ord(c) for c in alias], row)
+            ensg_to_aliases[row['ensembl_gene_id']] = aliases
+        except Exception:
+            raise PheWebError('Cannot handle genenames row: {}'.format(row))
+    return ensg_to_aliases
+
+def get_gene_aliases():
+    # NOTE: "canonical" refers to the canonical symbol for a gene
+    genes = [{'canonical': canonical, 'ensg':ensg} for _,_,_,canonical,ensg in get_gene_tuples(include_ensg=True)]
+    assert len({g['ensg'] for g in genes}) == len(genes)
+    assert len({g['canonical'] for g in genes}) == len(genes)
+    for g in genes:
+        assert re.match(r'^ENSGR?[0-9]+(?:\.[0-9]+_[0-9]+(?:_PAR_[XY])?)?$', g['ensg']), g
+        #assert re.match(r'^[-\._a-zA-Z0-9]+$', g['canonical']), (g['canonical'], [ord(c) for c in g['canonical']], g)
+    print('num canonical gene names: {}'.format(len(genes)))
+
+    canonicals_upper = {g['canonical'].upper() for g in genes}
+    ensg_to_canonical = {g['ensg']: g['canonical'] for g in genes}
+    ensg_to_aliases = get_genenamesorg_ensg_aliases_map(g['ensg'] for g in genes)
+    canonical_to_aliases = {ensg_to_canonical[ensg]: ensg_to_aliases.get(ensg, []) for ensg in ensg_to_canonical.keys()}
+    for canonical, aliases in canonical_to_aliases.items():
+        aliases = [alias for alias in aliases if alias.upper() not in canonicals_upper]
+        aliases.append(canonical.upper())
+        aliases = list(set(aliases))
+        canonical_to_aliases[canonical] = aliases
+
+    alias_to_canonicals = {}
+    for canonical, aliases in canonical_to_aliases.items():
+        for alias in aliases:
+            alias_to_canonicals.setdefault(alias, []).append(canonical)
+    alias_to_canonicals = {alias: ','.join(canonicals) for alias,canonicals in alias_to_canonicals.items()}
+
+    return alias_to_canonicals
+
+def run(argv):
+
+    if '-h' in argv or '--help' in argv:
+        print('Make a trie of all gene names for easy searching.')
+        exit(1)
+
+
+    if not os.path.exists(common_filepaths['genes']()):
+        print('Downloading genes')
+        from . import download_genes
+        download_genes.run([])
+
+    aliases_filepath = common_filepaths['gene-aliases-trie']()
+    if not os.path.exists(aliases_filepath):
+        print('gene aliases will be stored at {!r}'.format(aliases_filepath))
+        mapping = get_gene_aliases()
+        mapping = [(a, cs.encode('ascii')) for a,cs in mapping.items()]
+        aliases_trie = marisa_trie.BytesTrie(mapping)
+        aliases_trie.save(aliases_filepath)
+
+    else:
+        print('gene aliases are at {!r}'.format(aliases_filepath))
---
+++ packages/PheWeb/PheWeb-1.3.12-cp39-cp39-macosx_10_15_x86_64.whl/pheweb/load/top_loci.py
@@ -0,0 +1,54 @@
+
+from .. import conf
+from ..file_utils import write_json, write_heterogenous_variantfile, get_filepath
+
+from .top_hits import get_all_hits, stringify_assocs
+
+
+def get_loci():
+    hits = get_all_hits()
+
+    hits_by_chrom = dict()
+    for hit in hits:
+        hits_by_chrom.setdefault(hit['chrom'], []).append(hit)
+
+    for hits in hits_by_chrom.values():
+        while hits:
+            best_assoc = min(hits, key=lambda assoc: assoc['pval'])
+            yield best_assoc
+            hits = [h for h in hits if abs(h['pos'] - best_assoc['pos']) > conf.get_between_pheno_mask_around_peak()]
+
+
+def run(argv):
+    out_filepath_json = get_filepath('top-loci', must_exist=False)
+    out_filepath_tsv = get_filepath('top-loci-tsv', must_exist=False)
+
+    if argv and argv[0] == '-h':
+        print('''
+Make lists of top loci for this PheWeb in {} and {}.
+
+To count as a top loci, a variant must:
+- have a p-value < {}
+- be among the top {:,} associations in its phenotype
+- have the smallest p-value within {:,} bases within its phenotype (well, not exactly, but pretty much)
+- have the smallest p-value within {:,} bases (well, not exactly, but pretty much)
+
+Each loci will include the phenotype that has the smallest p-value at that location.
+Even if this loci also contains significant hits for other phenotypes, they won't be
+shown.  If you want all hits, use `pheweb top-hits`.
+'''.format(out_filepath_json,
+           out_filepath_tsv,
+           '{:0.0e}'.format(conf.get_top_hits_pval_cutoff()).replace('e-0', 'e-'),
+           conf.get_manhattan_num_unbinned(),
+           conf.get_within_pheno_mask_around_peak(),
+           conf.get_between_pheno_mask_around_peak(),
+))
+        exit(1)
+
+    loci = sorted(get_loci(), key=lambda l: l['pval'])
+    write_json(filepath=out_filepath_json, data=loci, sort_keys=True)
+    print("wrote {} loci to {}".format(len(loci), out_filepath_json))
+
+    stringify_assocs(loci)
+    write_heterogenous_variantfile(out_filepath_tsv, loci)
+    print("wrote {} loci to {}".format(len(loci), out_filepath_tsv))
