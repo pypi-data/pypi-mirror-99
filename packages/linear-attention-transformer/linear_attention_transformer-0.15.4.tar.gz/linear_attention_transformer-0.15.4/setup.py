400fc501b1b5fb2cdccef0744fe4606d60137b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:34 +0000

    Add package linear-attention-transformer/0.15.4/linear_attention_transformer-0.15.4.tar.gz

---
+++ packages/linear-attention-transformer/linear_attention_transformer-0.15.4.tar.gz/linear_attention_transformer-0.15.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'linear_attention_transformer',
+  packages = find_packages(exclude=['examples']),
+  version = '0.15.4',
+  license='MIT',
+  description = 'Linear Attention Transformer',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/linear-attention-transformer',
+  keywords = ['transformers', 'attention', 'artificial intelligence'],
+  install_requires=[
+      'torch',
+      'local-attention',
+      'axial-positional-embedding',
+      'product-key-memory>=0.1.5',
+      'linformer>=0.1.0'
+  ],
+  classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6',
+  ],
+)
