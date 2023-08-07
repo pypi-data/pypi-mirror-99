eb0d6cab187f346ad21500dc530deee51c14ae73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:10 +0000

    Add package extra-boolean/0.1.15/extra-boolean-0.1.15.tar.gz

---
+++ packages/extra-boolean/extra-boolean-0.1.15.tar.gz/extra-boolean-0.1.15/extra_boolean/not_.py
@@ -0,0 +1,13 @@
+def not_(a):
+  """Checks if value is False. `📘`_
+
+  - a: a boolean
+
+  Example:
+    >>> not_(False) == True
+    >>> not_(True)  == False
+
+  .. _📘:
+    https://github.com/python3f/extra-boolean/wiki/not
+  """
+  return not a
