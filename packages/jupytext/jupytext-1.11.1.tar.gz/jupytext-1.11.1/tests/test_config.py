64e1093e1f8fa5925eb1f61a7e25540f7cbb6605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:33 +0000

    Add package jupytext/1.11.1/jupytext-1.11.1.tar.gz

---
+++ packages/jupytext/jupytext-1.11.1.tar.gz/jupytext-1.11.1/tests/test_config.py
@@ -0,0 +1,168 @@
+import os
+
+import pytest
+
+from jupytext.config import (
+    find_jupytext_configuration_file,
+    load_jupytext_configuration_file,
+)
+
+
+def test_find_jupytext_configuration_file(tmpdir):
+    nested = tmpdir.mkdir("nested")
+
+    # Start with no configuration
+    assert find_jupytext_configuration_file(str(nested)) is None
+
+    # Configuration file in the parent directory
+    root_config = tmpdir.join("jupytext.yml")
+    root_config.write("\n")
+    assert os.path.samefile(
+        find_jupytext_configuration_file(str(tmpdir)), str(root_config)
+    )
+    assert os.path.samefile(
+        find_jupytext_configuration_file(str(nested)), str(root_config)
+    )
+
+    # Local configuration file
+    local_config = nested.join(".jupytext")
+    local_config.write("\n")
+    assert os.path.samefile(
+        find_jupytext_configuration_file(str(tmpdir)), str(root_config)
+    )
+    assert os.path.samefile(
+        find_jupytext_configuration_file(str(nested)), str(local_config)
+    )
+
+
+def test_jupytext_py_is_not_a_configuration_file(tmpdir):
+    jupytext_py = tmpdir.join("jupytext.py")
+    jupytext_py.write("# Not a config file!")
+
+    assert find_jupytext_configuration_file(str(tmpdir)) is None
+
+    dot_jupytext_py = tmpdir.join(".jupytext.py")
+    dot_jupytext_py.write("# This is a config file!")
+
+    assert find_jupytext_configuration_file(str(tmpdir)) == str(dot_jupytext_py)
+
+
+@pytest.mark.parametrize(
+    "config_file",
+    ["jupytext", "jupytext.toml", "jupytext.yml", "jupytext.json", "jupytext.py"],
+)
+def test_load_jupytext_configuration_file(tmpdir, config_file):
+    full_config_path = tmpdir.join(config_file)
+
+    if config_file.endswith(("jupytext", ".toml")):
+        full_config_path.write(
+            """formats = "ipynb,py:percent"
+notebook_metadata_filter = "all"
+cell_metadata_filter = "all"
+"""
+        )
+    elif config_file.endswith(".yml"):
+        full_config_path.write(
+            """formats: ipynb,py:percent
+notebook_metadata_filter: all
+cell_metadata_filter: all
+"""
+        )
+    elif config_file.endswith(".json"):
+        full_config_path.write(
+            """{"formats": "ipynb,py:percent",
+"notebook_metadata_filter": "all",
+"cell_metadata_filter": "all"
+}
+"""
+        )
+    elif config_file.endswith(".py"):
+        full_config_path.write(
+            """c.formats = "ipynb,py:percent"
+c.notebook_metadata_filter = "all"
+c.cell_metadata_filter = "all"
+"""
+        )
+
+    config = load_jupytext_configuration_file(str(full_config_path))
+    assert config.formats == "ipynb,py:percent"
+    assert config.notebook_metadata_filter == "all"
+    assert config.cell_metadata_filter == "all"
+
+
+@pytest.mark.parametrize(
+    "content_toml,formats_short_form",
+    [
+        (
+            """# always pair ipynb notebooks to py:percent files
+formats = "ipynb,py:percent"
+""",
+            "ipynb,py:percent",
+        ),
+        (
+            """# always pair ipynb notebooks to py:percent files
+formats = ['ipynb', 'py:percent']
+""",
+            "ipynb,py:percent",
+        ),
+        (
+            """# always pair ipynb notebooks to py:percent files
+formats = ["ipynb", "py:percent"]
+""",
+            "ipynb,py:percent",
+        ),
+        (
+            """# always pair ipynb notebooks to py:percent files
+formats = "ipynb,py:percent"
+""",
+            "ipynb,py:percent",
+        ),
+        (
+            """# Pair notebooks in subfolders of 'notebooks' to scripts in subfolders of 'scripts'
+[formats]
+"notebooks/" = "ipynb"
+"scripts/" = "py:percent"
+""",
+            "notebooks///ipynb,scripts///py:percent",
+        ),
+        (
+            """# Pair notebooks in subfolders of 'notebooks' to scripts in subfolders of 'scripts'
+[formats]
+"notebooks" = "ipynb"
+"scripts" = "py:percent"
+""",
+            "notebooks///ipynb,scripts///py:percent",
+        ),
+    ],
+)
+def test_jupytext_formats(tmpdir, content_toml, formats_short_form):
+    jupytext_toml = tmpdir.join("jupytext.toml")
+    jupytext_toml.write(content_toml)
+
+    config = load_jupytext_configuration_file(str(jupytext_toml))
+    assert config.formats == formats_short_form
+
+
+def test_deprecated_formats_cause_warning(
+    tmpdir, content_toml="default_jupytext_formats = 'ipynb,md'"
+):
+    jupytext_toml = tmpdir.join("jupytext.toml")
+    jupytext_toml.write(content_toml)
+
+    config = load_jupytext_configuration_file(str(jupytext_toml))
+    with pytest.warns(FutureWarning, match="use 'formats'"):
+        assert config.default_formats(str(tmpdir.join("test.md"))) == "ipynb,md"
+
+
+@pytest.mark.parametrize(
+    "option_name",
+    ["notebook_metadata_filter", "cell_metadata_filter", "cell_markers"],
+)
+def test_deprecated_options_cause_warning(tmpdir, option_name):
+    jupytext_toml = tmpdir.join("jupytext.toml")
+    jupytext_toml.write(f"default_{option_name} = 'value'")
+    config = load_jupytext_configuration_file(str(jupytext_toml))
+    fmt = {}
+    with pytest.warns(FutureWarning, match=f"use '{option_name}'"):
+        config.set_default_format_options(fmt)
+        assert fmt[option_name] == "value"
---
+++ packages/jupytext/jupytext-1.11.1.tar.gz/jupytext-1.11.1/tests/test_trust_notebook.py
@@ -0,0 +1,145 @@
+import os
+import shutil
+
+import pytest
+
+from jupytext.compare import compare_notebooks
+from jupytext.contentsmanager import TextFileContentsManager
+
+from .utils import list_notebooks
+
+
+@pytest.mark.parametrize("nb_file", list_notebooks("python"))
+def test_py_notebooks_are_trusted(nb_file):
+    cm = TextFileContentsManager()
+    root, file = os.path.split(nb_file)
+    cm.root_dir = root
+    nb = cm.get(file)
+    for cell in nb["content"].cells:
+        assert cell.metadata.get("trusted", True)
+
+
+@pytest.mark.parametrize("nb_file", list_notebooks("Rmd"))
+def test_rmd_notebooks_are_trusted(nb_file):
+    cm = TextFileContentsManager()
+    root, file = os.path.split(nb_file)
+    cm.root_dir = root
+    nb = cm.get(file)
+    for cell in nb["content"].cells:
+        assert cell.metadata.get("trusted", True)
+
+
+@pytest.mark.parametrize("nb_file", list_notebooks("ipynb_py", skip="hash sign"))
+def test_ipynb_notebooks_can_be_trusted(nb_file, tmpdir, no_jupytext_version_number):
+    cm = TextFileContentsManager()
+    root, file = os.path.split(nb_file)
+    tmp_ipynb = str(tmpdir.join(file))
+    py_file = file.replace(".ipynb", ".py")
+    tmp_py = str(tmpdir.join(py_file))
+    shutil.copy(nb_file, tmp_ipynb)
+
+    cm.formats = "ipynb,py"
+    cm.root_dir = str(tmpdir)
+    model = cm.get(file)
+    cm.save(model, py_file)
+
+    # Unsign and test notebook
+    nb = model["content"]
+    for cell in nb.cells:
+        cell.metadata.pop("trusted", True)
+
+    cm.notary.unsign(nb)
+
+    model = cm.get(file)
+    for cell in model["content"].cells:
+        assert (
+            "trusted" not in cell.metadata
+            or not cell.metadata["trusted"]
+            or not cell.outputs
+        )
+
+    # Trust and reload
+    cm.trust_notebook(py_file)
+
+    model = cm.get(file)
+    for cell in model["content"].cells:
+        assert cell.metadata.get("trusted", True)
+
+    # Remove py file, content should be the same
+    os.remove(tmp_py)
+    nb2 = cm.get(file)
+    for cell in nb2["content"].cells:
+        assert cell.metadata.get("trusted", True)
+
+    compare_notebooks(nb2["content"], model["content"])
+
+    # Just for coverage
+    cm.trust_notebook(file)
+
+
+@pytest.mark.parametrize("nb_file", list_notebooks("ipynb_py", skip="hash sign"))
+def test_ipynb_notebooks_can_be_trusted_even_with_metadata_filter(
+    nb_file, tmpdir, no_jupytext_version_number
+):
+    cm = TextFileContentsManager()
+    root, file = os.path.split(nb_file)
+    tmp_ipynb = str(tmpdir.join(file))
+    py_file = file.replace(".ipynb", ".py")
+    tmp_py = str(tmpdir.join(py_file))
+    shutil.copy(nb_file, tmp_ipynb)
+
+    cm.formats = "ipynb,py"
+    cm.notebook_metadata_filter = "all"
+    cm.cell_metadata_filter = "-all"
+    cm.root_dir = str(tmpdir)
+    model = cm.get(file)
+    cm.save(model, py_file)
+
+    # Unsign notebook
+    nb = model["content"]
+    for cell in nb.cells:
+        cell.metadata.pop("trusted", True)
+
+    cm.notary.unsign(nb)
+
+    # Trust and reload
+    cm.trust_notebook(py_file)
+
+    model = cm.get(file)
+    for cell in model["content"].cells:
+        assert cell.metadata.get("trusted", True)
+
+    # Remove py file, content should be the same
+    os.remove(tmp_py)
+    nb2 = cm.get(file)
+    for cell in nb2["content"].cells:
+        assert cell.metadata.get("trusted", True)
+
+    compare_notebooks(nb2["content"], model["content"])
+
+
+@pytest.mark.parametrize("nb_file", list_notebooks("percent", skip="hash sign"))
+def test_text_notebooks_can_be_trusted(nb_file, tmpdir, no_jupytext_version_number):
+    cm = TextFileContentsManager()
+    root, file = os.path.split(nb_file)
+    py_file = str(tmpdir.join(file))
+    shutil.copy(nb_file, py_file)
+
+    cm.root_dir = str(tmpdir)
+    model = cm.get(file)
+    model["type"] == "notebook"
+    cm.save(model, file)
+
+    # Unsign notebook
+    nb = model["content"]
+    for cell in nb.cells:
+        cell.metadata.pop("trusted", True)
+
+    cm.notary.unsign(nb)
+
+    # Trust and reload
+    cm.trust_notebook(file)
+
+    model = cm.get(file)
+    for cell in model["content"].cells:
+        assert cell.metadata.get("trusted", True)
