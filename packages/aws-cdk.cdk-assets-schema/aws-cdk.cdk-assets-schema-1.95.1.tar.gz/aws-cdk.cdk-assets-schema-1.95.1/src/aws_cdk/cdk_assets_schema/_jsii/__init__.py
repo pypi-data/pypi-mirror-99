f13391430bd9fa8df785db93921fd3e678581de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:33 +0000

    Add package aws-cdk.cdk-assets-schema/1.95.1/aws-cdk.cdk-assets-schema-1.95.1.tar.gz

---
+++ packages/aws-cdk.cdk-assets-schema/aws-cdk.cdk-assets-schema-1.95.1.tar.gz/aws-cdk.cdk-assets-schema-1.95.1/src/aws_cdk/cdk_assets_schema/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/cdk-assets-schema",
+    "1.95.1",
+    __name__[0:-6],
+    "cdk-assets-schema@1.95.1.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
