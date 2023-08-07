745fc27686a9a1433d775783b09f278b57998e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:38:59 +0000

    Add package dash-mp-components/0.2.9/dash_mp_components-0.2.9.tar.gz

---
+++ packages/dash-mp-components/dash_mp_components-0.2.9.tar.gz/dash_mp_components-0.2.9/dash_mp_components/MatSidebar.py
@@ -0,0 +1,33 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class MatSidebar(Component):
+    """A MatSidebar component.
+Material navigation bar
+
+Keyword arguments:
+- id (string; optional): The ID used to identify this component in Dash callbacks.
+- currentApp (string; optional): The ID of the current selected app
+- appId (string; optional): This is a read-only prop that dash callback can subscribe to
+- layout (a value equal to: 'vertical', 'horizontal'; optional): The layout of the sidebar"""
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, currentApp=Component.UNDEFINED, appId=Component.UNDEFINED, layout=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'currentApp', 'appId', 'layout']
+        self._type = 'MatSidebar'
+        self._namespace = 'dash_mp_components'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'currentApp', 'appId', 'layout']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(MatSidebar, self).__init__(**args)
---
+++ packages/dash-mp-components/dash_mp_components-0.2.9.tar.gz/dash_mp_components-0.2.9/dash_mp_components/_imports_.py
@@ -0,0 +1,35 @@
+from .JsonView import JsonView
+from .MatSidebar import MatSidebar
+from .CameraContextProvider import CameraContextProvider
+from .CrystalToolkitScene import CrystalToolkitScene
+from .Download import Download
+from .GraphComponent import GraphComponent
+from .NavbarDropdown import NavbarDropdown
+from .Scrollspy import Scrollspy
+from .PeriodicContext import PeriodicContext
+from .PeriodicContextTable import PeriodicContextTable
+from .PeriodicElement import PeriodicElement
+from .PeriodicFilter import PeriodicFilter
+from .PeriodicTableInput import PeriodicTableInput
+from .GlobalSearchBar import GlobalSearchBar
+from .SearchUI import SearchUI
+from .Select import Select
+
+__all__ = [
+    "JsonView",
+    "MatSidebar",
+    "CameraContextProvider",
+    "CrystalToolkitScene",
+    "Download",
+    "GraphComponent",
+    "NavbarDropdown",
+    "Scrollspy",
+    "PeriodicContext",
+    "PeriodicContextTable",
+    "PeriodicElement",
+    "PeriodicFilter",
+    "PeriodicTableInput",
+    "GlobalSearchBar",
+    "SearchUI",
+    "Select"
+]
