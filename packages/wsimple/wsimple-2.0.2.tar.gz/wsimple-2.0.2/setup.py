4f303c3d597fb77686d87271dbea981fa3c6d42a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:42 +0000

    Add package wsimple/2.0.2/wsimple-2.0.2.tar.gz

---
+++ packages/wsimple/wsimple-2.0.2.tar.gz/wsimple-2.0.2/setup.py
@@ -0,0 +1,42 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+# source env/bin/activate
+
+#? pypi
+# rm -rf build dist wsimple.egg-info
+# python setup.py sdist bdist_wheel 
+# twine upload --skip-existing dist/*
+
+setup(
+    name="wsimple",
+    version="2.0.2",
+    author="Yusuf Ahmed",
+    author_email="yusufahmed172@gmail.com",
+    long_description=open("README.md").read(),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    description="Wsimple.py: a API and Web interface for Wealthsimple Trade",
+    long_description_content_type="text/markdown",
+    url="https://github.com/yusuf8ahmed/Wsimple",
+    install_requires=[
+        "loguru",
+        "cloudscraper",
+        "requests",
+        "python-box"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "images": ["*.svg"],
+        "": ["*.md"]
+    },
+    platforms = 'any',
+    keywords ='wsimple',
+    python_requires='>=3',
+    zip_safe = False
+)
