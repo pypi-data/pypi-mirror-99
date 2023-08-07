8e133ceb91835367bc2ff0b89f33d39ea1b6096d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:21 +0000

    Add package mypy-boto3-amplifybackend/1.17.37.0/mypy_boto3_amplifybackend-1.17.37.0-py3-none-any.whl

---
+++ packages/mypy-boto3-amplifybackend/mypy_boto3_amplifybackend-1.17.37.0-py3-none-any.whl/mypy_boto3_amplifybackend/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for amplifybackend service.
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_amplifybackend import (
+        AmplifyBackendClient,
+        Client,
+        ListBackendJobsPaginator,
+    )
+
+    session = boto3.Session()
+
+    client: AmplifyBackendClient = boto3.client("amplifybackend")
+    session_client: AmplifyBackendClient = session.client("amplifybackend")
+
+    list_backend_jobs_paginator: ListBackendJobsPaginator = client.get_paginator("list_backend_jobs")
+    ```
+"""
+from mypy_boto3_amplifybackend.client import AmplifyBackendClient
+from mypy_boto3_amplifybackend.paginator import ListBackendJobsPaginator
+
+Client = AmplifyBackendClient
+
+__all__ = ("AmplifyBackendClient", "Client", "ListBackendJobsPaginator")
