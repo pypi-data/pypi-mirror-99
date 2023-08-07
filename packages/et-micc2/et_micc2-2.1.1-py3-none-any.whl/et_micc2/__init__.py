ddeb7cbfea882a657a5a71766e8e638cdf3cdeb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:38:40 +0000

    Add package et-micc2/2.1.1/et_micc2-2.1.1-py3-none-any.whl

---
+++ packages/et-micc2/et_micc2-2.1.1-py3-none-any.whl/et_micc2/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package et-micc2
+================
+
+Top-level package for :py:mod:`et-micc2`.
+"""
+__version__ = "2.1.1"
+
+import et_micc2.cli_micc # avoid attribute error when executing micc when installed from pypi
