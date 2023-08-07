8a05f9dafc4c4204a76c6945ed7e5b11df54d83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:38:41 +0000

    Add package mixmasta/0.3.0/mixmasta-0.3.0.tar.gz

---
+++ packages/mixmasta/mixmasta-0.3.0.tar.gz/mixmasta-0.3.0/tests/test_mixmasta.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Tests for `mixmasta` package."""
+
+
+import unittest
+from click.testing import CliRunner
+
+from mixmasta import mixmasta
+from mixmasta import cli
+
+
+class TestMixmaster(unittest.TestCase):
+    """Tests for `mixmasta` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'mixmasta.cli.main' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
