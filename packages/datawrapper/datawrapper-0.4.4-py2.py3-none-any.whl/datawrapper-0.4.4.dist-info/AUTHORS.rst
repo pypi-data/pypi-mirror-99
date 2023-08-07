6ff8774f3b6c2033e63067a685caf33f7e51be09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:31 +0000

    Add package datawrapper/0.4.4/datawrapper-0.4.4-py2.py3-none-any.whl

---
+++ packages/datawrapper/datawrapper-0.4.4-py2.py3-none-any.whl/datawrapper-0.4.4.dist-info/AUTHORS.rst
@@ -0,0 +1,15 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Sergio Sanchez <chekos@tacosdedatos.com>
+
+Contributors
+------------
+
+@alex-pobeditel-2004
+@hackerlikecomputer
+@joaopalmeiro
---
+++ packages/datawrapper/datawrapper-0.4.4-py2.py3-none-any.whl/datawrapper/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for datawrapper."""
+
+__author__ = """Sergio Sanchez"""
+__email__ = "chekos@tacosdedatos.com"
+__version__ = "0.4.4"
+
+import datawrapper.datawrapper as dw
+from IPython.display import HTML, Image
+
+Datawrapper = dw.Datawrapper
