aa3b45e0f5fdfcff49a19097625f59847ebc654b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:38:57 +0000

    Add package pyramid-oereb/1.9.1.dev20210325/pyramid_oereb-1.9.1.dev20210325.tar.gz

---
+++ packages/pyramid-oereb/pyramid_oereb-1.9.1.dev20210325.tar.gz/pyramid_oereb-1.9.1.dev20210325/pyramid_oereb/lib/records/extract.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+import warnings
+from datetime import datetime
+import uuid
+
+
+class ExtractRecord(object):
+    """The extract base class."""
+
+    creation_date = None
+    """datetime.datetime: The date and time of the extract creation."""
+
+    electronic_signature = None
+    """unicode or None: Digital signature for the extract."""
+
+    concerned_theme = None
+    """list of pyramid_oereb.lib.records.theme.ThemeRecord: List of concerned themes."""
+
+    not_concerned_theme = None
+    """list of pyramid_oereb.lib.records.theme.ThemeRecord: List of not concerned themes."""
+
+    theme_without_data = None
+    """list of pyramid_oereb.lib.records.theme.ThemeRecord: List of themes without data."""
+
+    extract_identifier = None
+    """unicode: The extract identifier (UUID)."""
+
+    qr_code = None
+    """binary or None: QR code for the extract as binary string."""
+
+    def __init__(self, real_estate, logo_plr_cadastre, federal_logo, cantonal_logo, municipality_logo,
+                 plr_cadastre_authority, base_data, embeddable, certification=None, certification_at_web=None,
+                 exclusions_of_liability=None, glossaries=None, concerned_theme=None,
+                 not_concerned_theme=None, theme_without_data=None, general_information=None):
+        """
+        Args:
+            real_estate (pyramid_oereb.lib.records.real_estate.RealEstateRecord): The real estate in its
+                record representation.
+            logo_plr_cadastre (pyramid_oereb.lib.records.image.ImageRecord): Image file of the PLR logo.
+            federal_logo (pyramid_oereb.lib.records.image.ImageRecord):Image file of the federal logo.
+            cantonal_logo (pyramid_oereb.lib.records.image.ImageRecord): Image file of the cantonal logo.
+            municipality_logo (pyramid_oereb.lib.records.image.ImageRecord): Image file of the municipality
+                logo.
+            plr_cadastre_authority (pyramid_oereb.lib.records.office.OfficeRecord): The authority which is
+                responsible for the PLR cadastre.
+            base_data (dict of unicode): A multilingual list of basic data layers used by the extract. For
+                instance the base map from swisstopo.
+            embeddable (pyramid_oereb.lib.records.embeddable.EmbeddableRecord):
+            certification (dict of unicode or None): Multilingual list of certification.
+            certification_at_web (dict of unicode or None): Multilingual list of certification uri.
+            exclusions_of_liability (list of
+                pyramid_oereb.lib.records.exclusion_of_liability.ExclusionOfLiabilityRecord): Exclusions of
+                liability for the extract.
+            glossaries (list of pyramid_oereb.lib.records.glossary.GlossaryRecord): Glossaries for the
+                extract.
+            concerned_theme (list of pyramid_oereb.lib.records.theme.ThemeRecord or None): Concerned themes.
+            not_concerned_theme (list of pyramid_oereb.lib.records.theme.ThemeRecord or None): Not concerned
+                themes.
+            theme_without_data (list of pyramid_oereb.lib.records.theme.ThemeRecord or None): Themes without
+                data.
+            general_information (dict of unicode): General information for the static extract as multilingual
+                text.
+        """
+        if not isinstance(base_data, dict):
+            warnings.warn('Type of "base_data" should be "dict"')
+        if general_information and not isinstance(general_information, dict):
+            warnings.warn('Type of "general_information" should be "dict"')
+
+        self.base_data = base_data
+        self.embeddable = embeddable
+        self.general_information = general_information
+        self.extract_identifier = str(uuid.uuid4())
+        self.real_estate = real_estate
+        if concerned_theme:
+            self.concerned_theme = concerned_theme
+        else:
+            self.concerned_theme = []
+        if not_concerned_theme:
+            self.not_concerned_theme = not_concerned_theme
+        else:
+            self.not_concerned_theme = []
+        if theme_without_data:
+            self.theme_without_data = theme_without_data
+        else:
+            self.theme_without_data = []
+        self.creation_date = datetime.now()
+        self.logo_plr_cadastre = logo_plr_cadastre
+        self.federal_logo = federal_logo
+        self.cantonal_logo = cantonal_logo
+        self.municipality_logo = municipality_logo
+        self.plr_cadastre_authority = plr_cadastre_authority
+        self.certification = certification
+        self.certification_at_web = certification_at_web
+        if exclusions_of_liability:
+            self.exclusions_of_liability = exclusions_of_liability
+        else:
+            self.exclusions_of_liability = []
+        if glossaries:
+            self.glossaries = glossaries
+        else:
+            self.glossaries = []
---
+++ packages/pyramid-oereb/pyramid_oereb-1.9.1.dev20210325.tar.gz/pyramid_oereb-1.9.1.dev20210325/pyramid_oereb/lib/records/real_estate.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+from pyramid_oereb import Config
+from pyramid_oereb.lib.records.view_service import ViewServiceRecord
+from pyramid_oereb.lib.url import add_url_params
+
+
+class RealEstateRecord(object):
+    """
+    Basic characteristics and geometry of the property to be analysed.
+
+    Attributes:
+        plan_for_land_register (pyramid_oereb.lib.records.view_service.ViewServiceRecord): The view service to
+            be used for the land registry map.
+        highlight (pyramid_oereb.lib.records.view_service.ViewServiceRecord): the view service with the wms
+        image of the highlighted real estate.
+        areas_ratio (decimal): Ratio of geometrical area and area from land registry.
+    """
+
+    plan_for_land_register = None
+    plan_for_land_register_main_page = None
+    highlight = None
+    areas_ratio = 1.0
+
+    def __init__(self, type, canton, municipality, fosnr, land_registry_area, limit,
+                 metadata_of_geographical_base_data=None, number=None, identdn=None, egrid=None,
+                 subunit_of_land_register=None, public_law_restrictions=None, references=None):
+        """
+
+        Args:
+            type (unicode): The property type. This attribute will be translated with the configured
+                corresponding value.
+            canton (unicode): The abbreviation of the canton the property is located in
+            municipality (unicode): The municipality the property is located in
+            fosnr (int): The federal number of the municipality defined by the statistics office
+            land_registry_area (int): Area of the property as defined in the land registry
+            limit (shapely.geometry.MultiPolygon): The boundary of the property as geometry in as shapely
+                multi polygon
+            metadata_of_geographical_base_data (uri): Link to the metadata of the geodata
+            number (unicode or None):  The official cantonal number of the property
+            identdn (unicode or None): The unique identifier of the property
+            egrid (unicode or None): The federal property identifier
+            subunit_of_land_register (unicode or None): Subunit of the land register if existing
+            public_law_restrictions (list of pyramid_oereb.lib.records.plr.PlrRecord or None): List of public
+                law restrictions for this real estate
+            references (list of pyramid_oereb.lib.records.documents.DocumentRecord or None): Documents
+                associated with this real estate
+            plan_for_land_register (pyramid_oereb.lib.records.view_service.ViewServiceRecord): The view
+                service to be used for the land registry map
+            plan_for_land_register_main_page (pyramid_oereb.lib.records.view_service.ViewServiceRecord):
+                The view service to be used for the land registry map used on the main page
+        """
+        self.number = number
+        self.identdn = identdn
+        self.egrid = egrid
+        self.type = Config.get_real_estate_type_by_mapping(type)
+        self.canton = canton
+        self.municipality = municipality
+        self.subunit_of_land_register = subunit_of_land_register
+        self.fosnr = fosnr
+        self.metadata_of_geographical_base_data = metadata_of_geographical_base_data
+        self.land_registry_area = int(round(land_registry_area, 0))
+        self.limit = limit
+        if isinstance(public_law_restrictions, list):
+            self.public_law_restrictions = public_law_restrictions
+        else:
+            self.public_law_restrictions = []
+        if isinstance(references, list):
+            self.references = references
+        else:
+            self.references = []
+        self.areas_ratio = self.limit.area / self.land_registry_area
+
+    def set_view_service(self, plan_for_land_register):
+        """
+        Sets the view service to generate the land registry map for the real estate.
+
+        Args:
+            plan_for_land_register (pyramid_oereb.lib.records.view_service.ViewServiceRecord): The view
+                service to be used for the land registry map.
+        """
+        self.plan_for_land_register = plan_for_land_register
+
+    def set_main_page_view_service(self, plan_for_land_register_main_page):
+        """
+        Sets the view service to generate the land registry map for the real estate.
+
+        Args:
+            plan_for_land_register_main_page (pyramid_oereb.lib.records.view_service.ViewServiceRecord):
+                The view service to be used for the land registry map used on the main page
+        """
+        self.plan_for_land_register_main_page = plan_for_land_register_main_page
+
+    def set_highlight_url(self, sld_url):
+        """
+        Set the highlight of the real estate.
+
+        Args:
+            sld_url (str): The URL which provides the sld to style and filter the highlight of the real
+            estate.
+        """
+        configured_params = Config.get_real_estate_config().get('visualisation').get('url_params')
+        additional_url_params = {}
+        for param in configured_params:
+            additional_url_params.update({param: getattr(self, param)})
+        updated_sld_url = add_url_params(sld_url, additional_url_params)
+        self.highlight = ViewServiceRecord(
+            add_url_params(self.plan_for_land_register.reference_wms, {'sld': updated_sld_url}),
+            self.plan_for_land_register.layer_index,
+            self.plan_for_land_register.layer_opacity,
+            legend_at_web={}
+        )
+        self.highlight.download_wms_content()
+
+    def __str__(self):
+        return '<%s -- number: %s identdn: %s egrid: %s type: %s' \
+                    ' canton: %s municipality: %s subunit_of_land_register: %s fosnr: %s' \
+                    ' metadata_of_geographical_base_data: %s land_registry_area: %s' \
+                    ' limit: %s public_law_restrictions: %s references: %s' \
+                    ' areas_ratio: %s>' % (
+                        self.__class__.__name__,
+                        self.number, self.identdn, self.egrid, self.type,
+                        self.canton, self.municipality, self.subunit_of_land_register, self.fosnr,
+                        self.metadata_of_geographical_base_data, self.land_registry_area,
+                        self.limit, self.public_law_restrictions, self.references,
+                        self.areas_ratio)
