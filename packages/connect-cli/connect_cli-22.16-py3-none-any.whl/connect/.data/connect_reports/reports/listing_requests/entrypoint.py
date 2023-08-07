8f145ece8af685ae30d106ea07bd385be01ce498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:38:57 +0000

    Add package connect-cli/22.16/connect_cli-22.16-py3-none-any.whl

---
+++ packages/connect-cli/connect_cli-22.16-py3-none-any.whl/connect/.data/connect_reports/reports/listing_requests/entrypoint.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021, CloudBlue
+# All rights reserved.
+#
+
+from connect.client import R
+
+from datetime import datetime
+
+from reports.utils import convert_to_datetime, get_basic_value, get_value
+
+
+def generate(client, parameters, progress_callback):
+    all_status = ['draft', 'reviewing', 'deploying', 'completed', 'canceled']
+    query = R()
+    if parameters.get('date') and parameters['date']['after'] != '':
+        query &= R().created.ge(parameters['date']['after'])
+        query &= R().created.le(parameters['date']['before'])
+    if parameters.get('product') and parameters['product']['all'] is False:
+        query &= R().listing.product.id.oneof(parameters['product']['choices'])
+    if parameters.get('mkp') and parameters['mkp']['all'] is False:
+        query &= R().listing.contract.marketplace.id.oneof(parameters['mkp']['choices'])
+    if parameters.get('rr_status') and parameters['rr_status']['all'] is False:
+        query &= R().state.oneof(parameters['rr_status']['choices'])
+    else:
+        query &= R().state.oneof(all_status)
+    requests = client.listing_requests.filter(query).order_by("-created")
+    progress = 0
+    total = requests.count()
+    today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+    for request in requests:
+        yield (
+            get_basic_value(request, 'id'),
+            get_basic_value(request, 'type'),
+            get_basic_value(request, 'state'),
+            convert_to_datetime(
+                get_basic_value(request, 'created'),
+            ),
+            convert_to_datetime(
+                get_basic_value(request, 'updated'),
+            ),
+            today,
+            get_value(request, 'listing', 'id'),
+            get_value(request['listing'], 'contract', 'id'),
+            get_value(request, 'product', 'id'),
+            get_value(request, 'product', 'name'),
+            get_value(request['listing'], 'provider', 'id'),
+            get_value(request['listing'], 'provider', 'name'),
+            get_value(request['listing'], 'vendor', 'id'),
+            get_value(request['listing'], 'vendor', 'name'),
+        )
+        progress += 1
+        progress_callback(progress, total)
---
+++ packages/connect-cli/connect_cli-22.16-py3-none-any.whl/connect/.data/connect_reports/reports/tier_configuration_list/entrypoint.py
@@ -0,0 +1,102 @@
+from connect.client import R
+
+from reports.utils import convert_to_datetime, get_basic_value, get_value
+
+from datetime import datetime
+
+
+def generate(client, parameters, progress_callback):
+    all_types = ['active', 'processing']
+    query = R()
+    if parameters.get("date") and parameters['date']['after'] != '':
+        query &= R().events.created.at.ge(parameters['date']['after'])
+        query &= R().events.created.at.le(parameters['date']['before'])
+    if parameters.get('product') and parameters['product']['all'] is False:
+        query &= R().product.id.oneof(parameters['product']['choices'])
+    if parameters.get('mkp') and parameters['mkp']['all'] is False:
+        query &= R().marketplace.id.oneof(parameters['mkp']['choices'])
+    if parameters.get('rr_status') and parameters['rr_status']['all'] is False:
+        query &= R().status.oneof(parameters['rr_status']['choices'])
+    else:
+        query &= R().status.oneof(all_types)
+
+    configurations = client.ns('tier').configs.filter(query).order_by('-created')
+    progress = 0
+    total = configurations.count()
+    today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+    for configuration in configurations:
+        yield(
+            get_basic_value(configuration, 'id'),
+            get_basic_value(configuration, 'tier_level'),
+            convert_to_datetime(get_value(configuration['events'], 'created', 'at')),
+            convert_to_datetime(get_value(configuration['events'], 'updated', 'at')),
+            today,
+            get_basic_value(configuration, 'status'),
+            get_value(
+                configuration,
+                'connection',
+                'type',
+            ),
+            get_value(configuration, 'account', 'name'),
+            get_value(configuration, 'account', 'external_id'),
+            get_value(configuration, 'account', 'id'),
+            get_value(
+                configuration['connection'],
+                'provider',
+                'id',
+            ) if 'connection' in configuration else '-',
+            get_value(
+                configuration['connection'],
+                'provider',
+                'name',
+            ) if 'connection' in configuration else '-',
+            get_value(
+                configuration['connection'],
+                'vendor',
+                'id',
+            ) if 'connection' in configuration else '-',
+            get_value(
+                configuration['connection'],
+                'vendor',
+                'name',
+            ) if 'connection' in configuration else '-',
+            get_value(
+                configuration,
+                'product',
+                'id',
+            ),
+            get_value(
+                configuration,
+                'product',
+                'name',
+            ),
+            get_value(
+                configuration['connection'],
+                'hub',
+                'id',
+            ) if 'connection' in configuration else '-',
+            get_value(
+                configuration['connection'],
+                'hub',
+                'name',
+            ) if 'connection' in configuration else '-',
+            get_value(
+                configuration,
+                'contract',
+                'id',
+            ),
+            get_value(
+                configuration,
+                'marketplace',
+                'id',
+            ),
+            get_value(
+                configuration,
+                'marketplace',
+                'name',
+            ),
+        )
+
+        progress += 1
+        progress_callback(progress, total)
