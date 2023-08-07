27b1d639c70ec2c41c030b3b52c1be53489da594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:42 +0000

    Add package wsimple/2.0.2/wsimple-2.0.2-py3-none-any.whl

---
+++ packages/wsimple/wsimple-2.0.2-py3-none-any.whl/wsimple/__init__.py
@@ -0,0 +1,11 @@
+from .api.api import Wsimple
+
+from .api.tokens import TokensBox
+
+from .api.errors import LoginError
+from .api.errors import InvalidAccessTokenError
+from .api.errors import InvalidRefreshTokenError
+from .api.errors import WSOTPUser 
+from .api.errors import WSOTPError
+from .api.errors import WSOTPLoginError
+
