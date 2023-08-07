5f36ec79fd1d09ee3bbc477e9aaece651ed638ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:32 +0000

    Add package aws-cdk.aws-sns-subscriptions/1.95.1/aws-cdk.aws-sns-subscriptions-1.95.1.tar.gz

---
+++ packages/aws-cdk.aws-sns-subscriptions/aws-cdk.aws-sns-subscriptions-1.95.1.tar.gz/aws-cdk.aws-sns-subscriptions-1.95.1/setup.py
@@ -0,0 +1,70 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-cdk.aws-sns-subscriptions",
+    "version": "1.95.1",
+    "description": "CDK Subscription Constructs for AWS SNS",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_sns_subscriptions",
+        "aws_cdk.aws_sns_subscriptions._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_sns_subscriptions._jsii": [
+            "aws-sns-subscriptions@1.95.1.jsii.tgz"
+        ],
+        "aws_cdk.aws_sns_subscriptions": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.aws-iam==1.95.1",
+        "aws-cdk.aws-lambda==1.95.1",
+        "aws-cdk.aws-sns==1.95.1",
+        "aws-cdk.aws-sqs==1.95.1",
+        "aws-cdk.core==1.95.1",
+        "constructs>=3.3.69, <4.0.0",
+        "jsii>=1.26.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved",
+        "Framework :: AWS CDK",
+        "Framework :: AWS CDK :: 1"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
